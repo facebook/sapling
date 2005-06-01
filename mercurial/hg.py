5064b78681132b9eec17cbb89e69ce5e7315e814
mpm@selenic.com <mpm@selenic.com>
2005-05-31 22:57:53 -0800

    Move ui class to its own module
    -----BEGIN PGP SIGNED MESSAGE-----
    Hash: SHA1
    
    Move ui class to its own module
    
    manifest hash: f75c8f9cdfe16f143ab633d0072c14ba88ac88be
    -----BEGIN PGP SIGNATURE-----
    Version: GnuPG v1.4.0 (GNU/Linux)
    
    iD8DBQFCnVxxywK+sNU5EO8RAgPgAJ48p7w4Do/saCC8WkBvHj/rdnoiEgCgrSs9
    Wu1fOSgST3rn/2JpZAdFRdA=
    =91tt
    -----END PGP SIGNATURE-----

--- mercurial/hg.py
+++ mercurial/hg.py
@@ -944,52 +944,6 @@ def repository(ui, path=None, create=0):
     else:
         return localrepository(ui, path, create)
 
-class ui:
-    def __init__(self, verbose=False, debug=False, quiet=False,
-                 interactive=True):
-        self.quiet = quiet and not verbose and not debug
-        self.verbose = verbose or debug
-        self.debugflag = debug
-        self.interactive = interactive
-    def write(self, *args):
-        for a in args:
-            sys.stdout.write(str(a))
-    def readline(self):
-        return sys.stdin.readline()[:-1]
-    def prompt(self, msg, pat, default = "y"):
-        if not self.interactive: return default
-        while 1:
-            self.write(msg, " ")
-            r = self.readline()
-            if re.match(pat, r):
-                return r
-            else:
-                self.write("unrecognized response\n")
-    def status(self, *msg):
-        if not self.quiet: self.write(*msg)
-    def warn(self, msg):
-        self.write(*msg)
-    def note(self, *msg):
-        if self.verbose: self.write(*msg)
-    def debug(self, *msg):
-        if self.debugflag: self.write(*msg)
-    def edit(self, text):
-        (fd, name) = tempfile.mkstemp("hg")
-        f = os.fdopen(fd, "w")
-        f.write(text)
-        f.close()
-
-        editor = os.environ.get("HGEDITOR") or os.environ.get("EDITOR", "vi")
-        r = os.system("%s %s" % (editor, name))
-
-        if r:
-            raise "Edit failed!"
-
-        t = open(name).read()
-        t = re.sub("(?m)^HG:.*\n", "", t)
-
-        return t
-    
 class httprangereader:
     def __init__(self, url):
         self.url = url
