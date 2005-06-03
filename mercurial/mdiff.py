9fec0c0d8069cf978ef8cfac45c519ed118a4454
mpm@selenic.com <mpm@selenic.com>
2005-06-03 13:44:34 -0800

    mdiff.py: kill #! line, add copyright notice
    -----BEGIN PGP SIGNED MESSAGE-----
    Hash: SHA1
    
    mdiff.py: kill #! line, add copyright notice
    
    manifest hash: acb4e14d5947d0fde5b89a874af0476773ee1cee
    -----BEGIN PGP SIGNATURE-----
    Version: GnuPG v1.4.0 (GNU/Linux)
    
    iD8DBQFCoM9CywK+sNU5EO8RAizVAJ41dqQxxx8nouBxJMcFmoVRRo4GfACbB9RV
    Xc0cqaoPVL7p87opPKjgNWw=
    =HyjH
    -----END PGP SIGNATURE-----

--- mercurial/mdiff.py
+++ mercurial/mdiff.py
@@ -1,4 +1,10 @@
-#!/usr/bin/python
+# mdiff.py - diff and patch routines for mercurial
+#
+# Copyright 2005 Matt Mackall <mpm@selenic.com>
+#
+# This software may be used and distributed according to the terms
+# of the GNU General Public License, incorporated herein by reference.
+
 import difflib, struct, mmap
 from mercurial.mpatch import *
 
