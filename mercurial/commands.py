bd3eea917117bb37d8c898e5f7f66caa9afd3a9c
Thomas Arendsen Hein <thomas@intevation.de>
2005-07-01 07:40:32 +0100

    Add an empty line after description in verbose mode of show_changeset.
    -----BEGIN PGP SIGNED MESSAGE-----
    Hash: SHA1
    
    Add an empty line after description in verbose mode of show_changeset.
    This makes it a little bit easier to see where a new changeset starts.
    
    manifest hash: dd0dc3a135ca2fabed03b86116863c93a451670d
    -----BEGIN PGP SIGNATURE-----
    Version: GnuPG v1.4.1 (GNU/Linux)
    
    iD8DBQFCxOVgW7P1GVgWeRoRAjtdAJ9B/ECd/Rhws2Bd+IcwRyqcYEN0ugCfbug2
    Uhy2yUhV+mlrqFkoNBkcOmM=
    =ttyp
    -----END PGP SIGNATURE-----

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -135,7 +135,7 @@ def show_changeset(ui, repo, rev=0, changenode=None, filelog=None):
         if ui.verbose:
             ui.status("description:\n")
             ui.status(description)
-            ui.status("\n")
+            ui.status("\n\n")
         else:
             ui.status("summary:     %s\n" % description.splitlines()[0])
     ui.status("\n")
