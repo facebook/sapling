5fb8ca60d267f7f84203dac9716627cdf6e4229c
Matt Mackall <mpm@selenic.com>
2005-07-10 16:13:39 -0800

    Move .hg into ignore()

--- mercurial/hg.py
+++ mercurial/hg.py
@@ -397,10 +397,8 @@ class dirstate:
                 if os.path.isdir(f):
                     for dir, subdirs, fl in os.walk(f):
                         d = dir[len(self.root) + 1:]
-                        if ".hg" in subdirs:
-                            subdirs.remove(".hg")
                         for sd in subdirs:
-                            if ignore(os.path.join(d, sd + '/')):
+                            if sd == ".hg" or ignore(os.path.join(d, sd +'/')):
                                 subdirs.remove(sd)
                         for fn in fl:
                             fn = util.pconvert(os.path.join(d, fn))
