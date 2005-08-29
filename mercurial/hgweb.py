21afb811028de210f2094da28d81b43ff3601278
Thomas Arendsen Hein <thomas@intevation.de>
2005-08-29 07:07:07 +0200

    Reverted change 861174d374d5: Reading changelogs is too slow.

--- mercurial/hgweb.py
+++ mercurial/hgweb.py
@@ -947,7 +947,6 @@ class hgwebdir:
 
                 url = os.environ["REQUEST_URI"] + "/" + name
                 url = url.replace("//", "/")
-                changes = repo.changelog.read(repo.changelog.tip())
 
                 yield dict(contact=get("web", "contact") or
                                    get("web", "author", "unknown"),
@@ -955,7 +954,8 @@ class hgwebdir:
                            url=url,
                            parity=parity,
                            shortdesc=get("web", "description", "unknown"),
-                           lastupdate=float(changes[2].split(' ')[0]))
+                           lastupdate=os.stat(os.path.join(path, ".hg",
+                                                "00changelog.d")).st_mtime)
 
                 parity = 1 - parity
 
