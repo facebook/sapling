bddf3b4cc7598f4e1ce3dbb55c966327fccc030b
Thomas Arendsen Hein <thomas@intevation.de>
2006-03-24 15:19:08 +0100

    Added missing gettext import to changegroup.py.

--- mercurial/changegroup.py
+++ mercurial/changegroup.py
@@ -7,6 +7,7 @@ This software may be used and distributed according to the terms
 of the GNU General Public License, incorporated herein by reference.
 """
 import struct
+from i18n import gettext as _
 from demandload import *
 demandload(globals(), "util")
 
