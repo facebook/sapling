4f0a39d96b7b25775c14fa09a149df932b0f44bf
Vadim Gelfer <vadim.gelfer@gmail.com>
2006-07-24 09:22:47 -0700

    util.getuser: better comments

--- mercurial/util.py
+++ mercurial/util.py
@@ -517,10 +517,12 @@ def getuser():
     try:
         return getpass.getuser()
     except ImportError:
+        # import of pwd will fail on windows - try fallback
         if getuser_fallback:
             return getuser_fallback()
-    raise util.Abort(_('user name not available - set USERNAME '
-                       'environment variable'))
+    # raised if win32api not available
+    raise Abort(_('user name not available - set USERNAME '
+                  'environment variable'))
 
 # Platform specific variants
 if os.name == 'nt':
