b10311c5e98b5f41b38df014fafb1ec349ee3f72
Giorgos Keramidas <keramida@ceid.upatras.gr>
2006-10-10 21:03:24 +0300

    Revert changeset d48690da3368.
    It breaks 'hg merge && hg up -c REV' when files exist only in one of the
    manifests, making hg throw a traceback like:

--- mercurial/context.py
+++ mercurial/context.py
@@ -360,7 +360,8 @@ class workingctx(changectx):
                 man.set(f, util.is_exec(self._repo.wjoin(f), man.execf(f)))
 
         for f in deleted + removed:
-            del man[f]
+            if f in man:
+                del man[f]
 
         self._manifest = man
 
