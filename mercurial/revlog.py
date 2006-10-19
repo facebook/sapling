1bccbf1e1e82d0af2ed560e869b5bbcbb03c7608
Benoit Boissinot <benoit.boissinot@ens-lyon.org>
2006-10-19 10:23:27 +0200

    revlog.children: use parentrevs instead of parents

--- mercurial/revlog.py
+++ mercurial/revlog.py
@@ -737,13 +737,9 @@ class revlog(object):
         c = []
         p = self.rev(node)
         for r in range(p + 1, self.count()):
-            n = self.node(r)
-            for pn in self.parents(n):
-                if pn == node:
-                    c.append(n)
-                    continue
-                elif pn == nullid:
-                    continue
+            for pr in self.parentrevs(n):
+                if pr == p:
+                    c.append(self.node(r))
         return c
 
     def lookup(self, id):
