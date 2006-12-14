d1c48b4dadc619da84fc389a3e3b2605741d17e1
Matt Mackall <mpm@selenic.com>
2006-12-14 17:32:00 -0600

    Enable gettext translations
    The new demandimport eliminates most of the gettext performance hit of
    importing gettext, which was doubling the run time for 'hg'.
    
    Now, with 'hg tip':
    
    dummy: .09530s no translations: .09638s translations: .09755s

--- mercurial/i18n.py
+++ mercurial/i18n.py
@@ -7,9 +7,7 @@ This software may be used and distributed according to the terms
 of the GNU General Public License, incorporated herein by reference.
 """
 
-# the import from gettext is _really_ slow
-# for now we use a dummy function
-gettext = lambda x: x
-#import gettext
-#t = gettext.translation('hg', '/usr/share/locale', fallback=1)
-#gettext = t.gettext
+import gettext
+t = gettext.translation('hg', fallback=1)
+gettext = t.gettext
+_ = gettext
