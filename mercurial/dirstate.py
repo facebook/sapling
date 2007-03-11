0458b30ebf3add9b7364baf9409d3857bed9b5c8
Alexis S. L. Carvalho <alexis@cecm.usp.br>
2007-03-10 22:03:22 -0300

    statwalk: don't put self.root in the files list
    That list is supposed to contain internal, normalized names ("/"-separated,
    relative to the repo root), not OS-native paths.

--- mercurial/dirstate.py
+++ mercurial/dirstate.py
@@ -376,7 +376,7 @@ class dirstate(object):
 
         # walk all files by default
         if not files:
-            files = [self.root]
+            files = ['.']
             dc = self.map.copy()
         else:
             files = util.unique(files)
