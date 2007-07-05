cfe7a0a5222d37691d37fbc673c5d05e12432158
Matt Mackall <mpm@selenic.com>
2007-07-05 13:34:18 -0500

    merge: make test for fast-forward merge stricter (issue619)
    don't allow merging with an ancestor
    fix != on contexts
    add a test

--- mercurial/merge.py
+++ mercurial/merge.py
@@ -523,7 +523,7 @@ def update(repo, node, branchmerge, force, partial, wlock):
         raise util.Abort(_("outstanding uncommitted merges"))
     if pa == p1 or pa == p2: # is there a linear path from p1 to p2?
         if branchmerge:
-            if p1.branch() != p2.branch():
+            if p1.branch() != p2.branch() and pa != p2:
                 fastforward = True
             else:
                 raise util.Abort(_("there is nothing to merge, just use "
