9098bb23f2fbcf3062cfbffed40cf856c20c0ba2
Dirkjan Ochtman <dirkjan@ochtman.nl>
2008-02-01 10:31:15 +0100

    update copyright notice in version output

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -2662,7 +2662,7 @@ def version_(ui):
     ui.write(_("Mercurial Distributed SCM (version %s)\n")
              % version.get_version())
     ui.status(_(
-        "\nCopyright (C) 2005-2007 Matt Mackall <mpm@selenic.com> and others\n"
+        "\nCopyright (C) 2005-2008 Matt Mackall <mpm@selenic.com> and others\n"
         "This is free software; see the source for copying conditions. "
         "There is NO\nwarranty; "
         "not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
