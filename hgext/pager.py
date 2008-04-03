5b548b29f2243170a6ba4ff4dcfdb5440bca4384
Gilles Moris <gilles.moris@free.fr>
2008-04-03 11:11:31 +0200

    Pager extension: switch it off if --debugger is set
    The pager is preventing the debugger prompt and much of the
    debugger output to be refreshed. Moreover the pager does not
    make sense when debugging line by line.
    
    (This supersedes the similar ui.debugflag patch. Disabling
    the pager for debug output doesn't make that much sense,
    as this is actually when the pager might be useful.)

--- hgext/pager.py
+++ hgext/pager.py
@@ -29,7 +29,7 @@ import sys, os, signal
 
 def uisetup(ui):
     p = ui.config("pager", "pager", os.environ.get("PAGER"))
-    if p and sys.stdout.isatty() and not ui.debugflag:
+    if p and sys.stdout.isatty() and '--debugger' not in sys.argv:
         if ui.configbool('pager', 'quiet'):
             signal.signal(signal.SIGPIPE, signal.SIG_DFL)
         sys.stderr = sys.stdout = os.popen(p, "wb")
