a4bb4bf5d056a1c8a09106ce9aea2f0d83c151c1
Matt Mackall <mpm@selenic.com>
2008-05-12 11:37:08 -0500

    purge: remove unused 'missing' logic

--- hgext/purge.py
+++ hgext/purge.py
@@ -84,14 +84,11 @@ def purge(ui, repo, *dirs, **opts):
 
     directories = []
     files = []
-    missing = []
     match = cmdutil.match(repo, dirs, opts)
     match.dir = directories.append
     for src, f, st in repo.dirstate.statwalk(match.files(), match,
                                              ignored=ignored):
-        if src == 'm':
-            missing.append(f)
-        elif src == 'f' and f not in repo.dirstate:
+        if src == 'f' and f not in repo.dirstate:
             files.append(f)
 
     directories.sort()
