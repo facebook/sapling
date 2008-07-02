0bf39aa1898613b1867f46f7c53c07a3e418b009
Dirkjan Ochtman <dirkjan@ochtman.nl>
2008-07-02 12:02:33 +0200

    Backed out changeset b6b16fec2a60 (incorrect Content-Length on Windows)

--- mercurial/hgweb/hgweb_mod.py
+++ mercurial/hgweb/hgweb_mod.py
@@ -169,20 +169,20 @@ class hgweb(object):
 
             req.write(content)
             del tmpl
-            return ''.join(content),
+            return req
 
         except revlog.LookupError, err:
             req.respond(HTTP_NOT_FOUND, ctype)
             msg = str(err)
             if 'manifest' not in msg:
                 msg = 'revision not found: %s' % err.name
-            return ''.join(tmpl('error', error=msg)),
+            req.write(tmpl('error', error=msg))
         except (RepoError, revlog.RevlogError), inst:
             req.respond(HTTP_SERVER_ERROR, ctype)
-            return ''.join(tmpl('error', error=str(inst))),
+            req.write(tmpl('error', error=str(inst)))
         except ErrorResponse, inst:
             req.respond(inst.code, ctype)
-            return ''.join(tmpl('error', error=inst.message)),
+            req.write(tmpl('error', error=inst.message))
 
     def templater(self, req):
 
--- mercurial/hgweb/hgwebdir_mod.py
+++ mercurial/hgweb/hgwebdir_mod.py
@@ -70,7 +70,8 @@ class hgwebdir(object):
 
     def __call__(self, env, respond):
         req = wsgirequest(env, respond)
-        return self.run_wsgi(req)
+        self.run_wsgi(req)
+        return req
 
     def run_wsgi(self, req):
 
@@ -89,12 +90,14 @@ class hgwebdir(object):
                         fname = virtual[7:]
                     else:
                         fname = req.form['static'][0]
-                    return staticfile(static, fname, req),
+                    req.write(staticfile(static, fname, req))
+                    return
 
                 # top-level index
                 elif not virtual:
                     req.respond(HTTP_OK, ctype)
-                    return ''.join(self.makeindex(req, tmpl)),
+                    req.write(self.makeindex(req, tmpl))
+                    return
 
                 # nested indexes and hgwebs
 
@@ -105,7 +108,8 @@ class hgwebdir(object):
                         req.env['REPO_NAME'] = virtual
                         try:
                             repo = hg.repository(self.parentui, real)
-                            return hgweb(repo).run_wsgi(req)
+                            hgweb(repo).run_wsgi(req)
+                            return
                         except IOError, inst:
                             msg = inst.strerror
                             raise ErrorResponse(HTTP_SERVER_ERROR, msg)
@@ -116,7 +120,8 @@ class hgwebdir(object):
                     subdir = virtual + '/'
                     if [r for r in repos if r.startswith(subdir)]:
                         req.respond(HTTP_OK, ctype)
-                        return ''.join(self.makeindex(req, tmpl, subdir)),
+                        req.write(self.makeindex(req, tmpl, subdir))
+                        return
 
                     up = virtual.rfind('/')
                     if up < 0:
@@ -125,11 +130,11 @@ class hgwebdir(object):
 
                 # prefixes not found
                 req.respond(HTTP_NOT_FOUND, ctype)
-                return ''.join(tmpl("notfound", repo=virtual)),
+                req.write(tmpl("notfound", repo=virtual))
 
             except ErrorResponse, err:
                 req.respond(err.code, ctype)
-                return ''.join(tmpl('error', error=err.message or '')),
+                req.write(tmpl('error', error=err.message or ''))
         finally:
             tmpl = None
 
