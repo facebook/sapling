5bcc1b653d2d20c8780a929ff990289ce787b743
Augie Fackler <durin42@gmail.com>
2008-10-07 20:43:44 -0500

    Add some basic tests of branching.

--- tests/test_fetch_command.py
+++ tests/test_fetch_command.py
@@ -43,3 +43,37 @@ class TestBasicRepoLayout(unittest.TestCase):
         self.assertEqual(node.hex(repo['tip'].node()),
                          'bf3767835b3b32ecc775a298c2fa27134dd91c11')
         self.assertEqual(repo['tip'], repo[1])
+
+    def test_branches(self):
+        util.load_svndump_fixture(self.repo_path, 'simple_branch.svndump')
+        fetch_command.fetch_revisions(ui.ui(), 
+                                      svn_url='file://%s' % self.repo_path, 
+                                      hg_repo_path=self.wc_path)
+        repo = hg.repository(ui.ui(), self.wc_path)
+        # TODO there must be a better way than repo[0] for this check
+        self.assertEqual(node.hex(repo[0].node()),
+                         'a47d0ce778660a91c31bf2c21c448e9ee296ac90')
+        self.assertEqual(node.hex(repo['tip'].node()),
+                         '9dfb0a19494f45c36e22f3c6d1b21d80638a7f6e')
+        self.assertEqual(len(repo['tip'].parents()), 1)
+        self.assertEqual(repo['tip'].parents()[0], repo['default'])
+        self.assertEqual(len(repo.heads()), 1)
+
+    def test_two_branches_with_heads(self):
+        util.load_svndump_fixture(self.repo_path, 'two_heads.svndump')
+        fetch_command.fetch_revisions(ui.ui(), 
+                                      svn_url='file://%s' % self.repo_path, 
+                                      hg_repo_path=self.wc_path)
+        repo = hg.repository(ui.ui(), self.wc_path)
+        # TODO there must be a better way than repo[0] for this check
+        self.assertEqual(node.hex(repo[0].node()),
+                         'a47d0ce778660a91c31bf2c21c448e9ee296ac90')
+        self.assertEqual(node.hex(repo['tip'].node()),
+                         'a595c77cfcaa3d1ba9e04b2c55c68bc6bf2b0fbf')
+        self.assertEqual(node.hex(repo['the_branch'].node()),
+                         '8ccaba5f0eae124487e413abd904a013f7f6fdeb')
+        self.assertEqual(node.hex(repo['the_branch'].parents()[0].node()),
+                         '9dfb0a19494f45c36e22f3c6d1b21d80638a7f6e')
+        self.assertEqual(len(repo['tip'].parents()), 1)
+        self.assertEqual(repo['tip'], repo['default'])
+        self.assertEqual(len(repo.heads()), 2)
