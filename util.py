5fc9e7c28c62cedc8eeb002eb66a897962790553
Augie Fackler <durin42@gmail.com>
2008-10-08 16:44:05 -0500

    Killed some dead code found by test coverage.

--- util.py
+++ util.py
@@ -18,18 +18,3 @@ def wipe_all_files(hg_wc_path):
             shutil.rmtree(f)
         else:
             os.remove(f)
-
-
-def remove_all_files_with_status(path, rev_paths, strip_path, status):
-    for p in rev_paths:
-        if rev_paths[p].action == status:
-            if p.startswith(strip_path):
-                fi = p[len(strip_path)+1:]
-                if len(fi) > 0:
-                    fi = os.path.join(path, fi)
-                    if os.path.isfile(fi):
-                        os.remove(fi)
-                        print 'D %s' % fi
-                    elif os.path.isdir(fi):
-                        shutil.rmtree(fi)
-                        print 'D %s' % fi
