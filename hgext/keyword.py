a9689af26d17fb303418543d1e29a7ad427378d9
Christian Ebert <blacktrash@gmx.net>
2008-12-16 09:58:41 +0100

    keyword: simplify repo.local and bundle check (as in dddd37603ecd)

--- hgext/keyword.py
+++ hgext/keyword.py
@@ -425,14 +425,10 @@ def reposetup(ui, repo):
     keyword substitutions.
     Monkeypatches patch and webcommands.'''
 
-    try:
-        if (not repo.local() or not kwtools['inc']
-            or kwtools['hgcmd'] in nokwcommands.split()
-            or '.hg' in util.splitpath(repo.root)
-            or repo._url.startswith('bundle:')):
-            return
-    except AttributeError:
-        pass
+    if (not hasattr(repo, 'dirstate') or not kwtools['inc']
+        or kwtools['hgcmd'] in nokwcommands.split()
+        or '.hg' in util.splitpath(repo.root)):
+        return
 
     kwtools['templater'] = kwt = kwtemplater(ui, repo)
 
