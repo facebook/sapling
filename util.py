c74984a398294e2d56f750f1968572a4041e56a0
Augie Fackler <durin42@gmail.com>
2008-12-24 11:23:19 -0600

    parse_revmap: Stop supporting pickled revmaps. I've made enough major changes that any of those still left are useless anyway.

--- util.py
+++ util.py
@@ -42,33 +42,20 @@ REVMAP_FILE_VERSION = 1
 def parse_revmap(revmap_filename):
     revmap = {}
     f = open(revmap_filename)
-    try:
-        # Remove compat code after March of 2009. That should be more than long
-        # enough.
-        revmap = pickle.load(f)
+    ver = int(f.readline())
+    if ver == 1:
+        for l in f:
+            revnum, node_hash, branch = l.split(' ', 2)
+            if branch == '\n':
+                branch = None
+            else:
+                branch = branch[:-1]
+            revmap[int(revnum), branch] = node.bin(node_hash)
         f.close()
-        f = open(revmap_filename, 'w')
-        f.write('1\n')
-        for key, value in sorted(revmap.items()):
-            f.write('%s %s %s\n' % (str(key[0]), node.hex(value), key[1] or ''))
-        f.close()
-    except:
-        f.close()
-        f = open(revmap_filename)
-        ver = int(f.readline())
-        if ver == 1:
-            for l in f:
-                revnum, node_hash, branch = l.split(' ', 2)
-                if branch == '\n':
-                    branch = None
-                else:
-                    branch = branch[:-1]
-                revmap[int(revnum), branch] = node.bin(node_hash)
-            f.close()
-        else:
-            print ('Your revmap was made by a newer version of hgsubversion.'
-                   ' Please upgrade.')
-            raise NotImplementedError
+    else: #pragma: no cover
+        print ('Your revmap was made by a newer version of hgsubversion.'
+               ' Please upgrade.')
+        raise NotImplementedError
     return revmap
 
 
