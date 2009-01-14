7f3f8dbbe2c7126d5c7732fc475394a853aaea51
Martin Geisler <mg@daimi.au.dk>
2009-01-15 00:14:36 +0100

    i18n: encode output in user's local encoding
    This makes the translated output obey the HGENCODING environment
    variable or the preferred encoding as set by the LANG or LC_ALL
    environment variables.
    
    Python 2.4 has a lgettext method which is similar, except that it
    doesn't know about HGENCODING or the settings in .hgrc.

--- mercurial/i18n.py
+++ mercurial/i18n.py
@@ -22,5 +22,30 @@ for dir in ('.', '..'):
         break
 
 t = gettext.translation('hg', localedir, fallback=True)
-gettext = t.gettext
+
+def gettext(message):
+    """Translate message.
+
+    The message is looked up in the catalog to get a Unicode string,
+    which is encoded in the local encoding before being returned.
+
+    Important: message is restricted to characters in the encoding
+    given by sys.getdefaultencoding() which is most likely 'ascii'.
+    """
+    # If message is None, t.ugettext will return u'None' as the
+    # translation whereas our callers expect us to return None.
+    if message is None:
+        return message
+
+    # We cannot just run the text through util.tolocal since that
+    # leads to infinite recursion when util._encoding is invalid.
+    try:
+        u = t.ugettext(message)
+        return u.encode(util._encoding, "replace")
+    except LookupError:
+        return message
+
 _ = gettext
+
+# Moved after _ because of circular import.
+import util
