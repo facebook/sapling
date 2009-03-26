4f628e24de611eb83b152ec7be16057ddf74fe23
Alexander Solovyov <piranha@piranha.org.ua>
2009-03-26 16:49:47 +0200

    diffstat hook example

---
+++ contrib/python-hook-examples.py
@@ -0,0 +1,24 @@
+'''
+These are examples of useful hooks in Python for Mercurial.
+'''
+
+from mercurial import patch, util
+
+
+def diffstat(ui, repo, **kwargs):
+    '''Use it like:
+
+    [hooks]
+    commit.diffstat = python:/path/to/this/file.py:diffstat
+    changegroup.diffstat = python:/path/to/this/file.py:diffstat
+    '''
+    if kwargs.get('parent2'):
+        return
+    node = kwargs['node']
+    first = repo[node].parents()[0].node()
+    if 'url' in kwargs:
+        last = repo['tip'].node()
+    else:
+        last = node
+    diff = patch.diff(repo, first, last)
+    ui.write(patch.diffstat(util.iterlines(diff)))
