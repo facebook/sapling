f1f94caf9af3848ad3ba2f37824effb4f08ff0e0
Martin Geisler <mg@lazybytes.net>
2009-04-26 01:56:52 +0200

    fancyopts: add copyright and license header

--- mercurial/fancyopts.py
+++ mercurial/fancyopts.py
@@ -1,3 +1,10 @@
+# fancyopts.py - better command line parsing
+#
+#  Copyright 2005-2009 Matt Mackall <mpm@selenic.com> and others
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2, incorporated herein by reference.
+
 import getopt
 
 def gnugetopt(args, options, longoptions):
