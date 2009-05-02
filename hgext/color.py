7b8faf1ad9f2ea1766def041a90456bd0eb7a3e5
Martin Geisler <mg@lazybytes.net>
2009-05-02 21:09:37 +0200

    color: look for mq with extensions.find

--- hgext/color.py
+++ hgext/color.py
@@ -232,11 +232,13 @@ def uisetup(ui):
     _setupcmd(ui, 'outgoing', commands.table, None, _diff_effects)
     _setupcmd(ui, 'tip', commands.table, None, _diff_effects)
     _setupcmd(ui, 'status', commands.table, colorstatus, _status_effects)
-    if ui.config('extensions', 'hgext.mq') is not None or \
-            ui.config('extensions', 'mq') is not None:
-        from hgext import mq
+    try:
+        mq = extensions.find('mq')
         _setupcmd(ui, 'qdiff', mq.cmdtable, colordiff, _diff_effects)
         _setupcmd(ui, 'qseries', mq.cmdtable, colorqseries, _patch_effects)
+    except KeyError:
+        # The mq extension is not enabled
+        pass
 
 def _setupcmd(ui, cmd, table, func, effectsmap):
     '''patch in command to command table and load effect map'''
