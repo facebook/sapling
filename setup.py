134653252c88af81a56ba51af4b62f220df66016
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2009-05-03 15:29:59 +0200

    A quick stab at a distutils installation script.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+if not hasattr(sys, 'version_info') or sys.version_info < (2, 4, 0, 'final'):
+    raise SystemExit("Mercurial requires python 2.4 or later.")
+
+from __init__ import __doc__
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+from distutils.core import setup
+
+setup(
+    name = 'HgSubversion',
+    version = '0.0.1',
+    url = 'http://bitbucket.org/durin42/hgsubversion',
+    license = 'GNU GPL',
+    author = 'Augie Fackler, others',
+    author_email = 'hgsubversion@googlegroups.com',
+    description = 'HgSubversion is a Mercurial extension for working with '
+    'Subversion repositories.',
+    long_description = __doc__,
+    keywords = 'mercurial',
+    packages = ['hgext.hgsubversion', 'hgext.hgsubversion.svnwrap'],
+    package_dir = {'hgext.hgsubversion': ''},
+    platforms = 'any',
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Version Control',
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+    cmdclass = {'build_py': build_py},
+)
