dc443da783b218a42848948f4097901e6b3131b4
Augie Fackler <durin42@gmail.com>
2009-05-25 12:44:33 -0500

    setup.py: Use setuptools if available.

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ try:
     from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
     from distutils.command.build_py import build_py
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name = 'hgsubversion',
