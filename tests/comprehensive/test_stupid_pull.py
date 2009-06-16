51e7cf285e5e0c6e4a6141eb9ce1312bf1ae8292
Dirkjan Ochtman <dirkjan@ochtman.nl>
2009-06-16 13:58:39 +0200

    tests: test for heads equality instead of branchtags
    branchtags may exclude other heads which could be dissimilar.

--- tests/comprehensive/test_stupid_pull.py
+++ tests/comprehensive/test_stupid_pull.py
@@ -21,7 +21,7 @@ def _do_case(self, name):
     u.setconfig('hgsubversion', 'stupid', '1')
     hg.clone(u, test_util.fileurl(checkout_path), wc2_path, update=False)
     self.repo2 = hg.repository(ui.ui(), wc2_path)
-    self.assertEqual(self.repo.branchtags(), self.repo2.branchtags())
+    self.assertEqual(self.repo.heads(), self.repo2.heads())
 
 
 def buildmethod(case, name):
