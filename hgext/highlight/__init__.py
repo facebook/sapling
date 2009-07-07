82fe78daac9f61947ed72da9ce8658421ec3d523
Martin Geisler <mg@lazybytes.net>
2009-07-07 23:54:42 +0200

    highlight: wrapped docstrings at 78 characters

--- hgext/highlight/__init__.py
+++ hgext/highlight/__init__.py
@@ -10,8 +10,7 @@
 
 """syntax highlighting for hgweb (requires Pygments)
 
-It depends on the Pygments syntax highlighting library:
-http://pygments.org/
+It depends on the Pygments syntax highlighting library: http://pygments.org/
 
 There is a single configuration option:
 
