8132a91a80e022c47e717ceecd01555f3af10c8e
Martin Geisler <mg@lazybytes.net>
2009-07-07 23:54:42 +0200

    bookmarks: wrapped docstrings at 78 characters

--- hgext/bookmarks.py
+++ hgext/bookmarks.py
@@ -7,25 +7,22 @@
 
 '''track a line of development with movable markers
 
-Bookmarks are local movable markers to changesets. Every bookmark
-points to a changeset identified by its hash. If you commit a
-changeset that is based on a changeset that has a bookmark on it,
-the bookmark shifts to the new changeset.
+Bookmarks are local movable markers to changesets. Every bookmark points to a
+changeset identified by its hash. If you commit a changeset that is based on a
+changeset that has a bookmark on it, the bookmark shifts to the new changeset.
 
-It is possible to use bookmark names in every revision lookup
-(e.g. hg merge, hg update).
+It is possible to use bookmark names in every revision lookup (e.g. hg merge,
+hg update).
 
-By default, when several bookmarks point to the same changeset, they
-will all move forward together. It is possible to obtain a more
-git-like experience by adding the following configuration option to
-your .hgrc:
+By default, when several bookmarks point to the same changeset, they will all
+move forward together. It is possible to obtain a more git-like experience by
+adding the following configuration option to your .hgrc:
 
   [bookmarks]
   track.current = True
 
-This will cause Mercurial to track the bookmark that you are currently
-using, and only update it. This is similar to git's approach to
-branching.
+This will cause Mercurial to track the bookmark that you are currently using,
+and only update it. This is similar to git's approach to branching.
 '''
 
 from mercurial.i18n import _
@@ -124,15 +121,15 @@ def setcurrent(repo, mark):
 def bookmark(ui, repo, mark=None, rev=None, force=False, delete=False, rename=None):
     '''track a line of development with movable markers
 
-    Bookmarks are pointers to certain commits that move when
-    committing. Bookmarks are local. They can be renamed, copied and
-    deleted. It is possible to use bookmark names in 'hg merge' and
-    'hg update' to merge and update respectively to a given bookmark.
+    Bookmarks are pointers to certain commits that move when committing.
+    Bookmarks are local. They can be renamed, copied and deleted. It is
+    possible to use bookmark names in 'hg merge' and 'hg update' to merge and
+    update respectively to a given bookmark.
 
     You can use 'hg bookmark NAME' to set a bookmark on the working
-    directory's parent revision with the given name. If you specify
-    a revision using -r REV (where REV may be an existing bookmark),
-    the bookmark is assigned to that revision.
+    directory's parent revision with the given name. If you specify a revision
+    using -r REV (where REV may be an existing bookmark), the bookmark is
+    assigned to that revision.
     '''
     hexfn = ui.debugflag and hex or short
     marks = parse(repo)
