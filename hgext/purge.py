fc39ad4866c26474d619db8018ebed3b5e4f07c1
Martin Geisler <mg@lazybytes.net>
2009-07-26 01:59:28 +0200

    purge: wrap docstrings at 70 characters

--- hgext/purge.py
+++ hgext/purge.py
@@ -3,8 +3,8 @@
 # This is a small extension for Mercurial (http://mercurial.selenic.com/)
 # that removes files not known to mercurial
 #
-# This program was inspired by the "cvspurge" script contained in CVS utilities
-# (http://www.red-bean.com/cvsutils/).
+# This program was inspired by the "cvspurge" script contained in CVS
+# utilities (http://www.red-bean.com/cvsutils/).
 #
 # For help on the usage of "hg purge" use:
 #  hg help purge
@@ -32,14 +32,14 @@ import os, stat
 def purge(ui, repo, *dirs, **opts):
     '''removes files not tracked by Mercurial
 
-    Delete files not known to Mercurial. This is useful to test local and
-    uncommitted changes in an otherwise-clean source tree.
+    Delete files not known to Mercurial. This is useful to test local
+    and uncommitted changes in an otherwise-clean source tree.
 
     This means that purge will delete:
 
     - Unknown files: files marked with "?" by "hg status"
-    - Empty directories: in fact Mercurial ignores directories unless they
-      contain files under source control management
+    - Empty directories: in fact Mercurial ignores directories unless
+      they contain files under source control management
 
     But it will leave untouched:
 
@@ -50,9 +50,10 @@ def purge(ui, repo, *dirs, **opts):
     If directories are given on the command line, only files in these
     directories are considered.
 
-    Be careful with purge, as you could irreversibly delete some files you
-    forgot to add to the repository. If you only want to print the list of
-    files that this program would delete, use the --print option.
+    Be careful with purge, as you could irreversibly delete some files
+    you forgot to add to the repository. If you only want to print the
+    list of files that this program would delete, use the --print
+    option.
     '''
     act = not opts['print']
     eol = '\n'
