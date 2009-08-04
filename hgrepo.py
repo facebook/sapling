a4c707e94fc1067e2a270cf5f69eec5bd363bb06
Abderrahim Kitouni <a.kitouni@gmail.com>
2009-08-04 18:39:02 +0100

    handle the new tag cache in mercurial crew

--- hgrepo.py
+++ hgrepo.py
@@ -33,15 +33,36 @@ def generate_repo_subclass(baseclass):
             else: #pragma: no cover
                 return super(hgrepo, self).findoutgoing(remote, base, heads, force)
 
+        def _findtags(self):
+            (tags, tagtypes) = super(hgrepo, self)._findtags()
+
+            git = GitHandler(self, self.ui)
+            for tag, rev in git.tags.iteritems():
+                if tag in tags:
+                    continue
+
+                tags[tag] = bin(rev)
+                tagtypes[tag] = 'git'
+
+            return (tags, tagtypes)
+
         def tags(self):
+            if not hasattr(self, 'tagscache'):
+                # mercurial 1.4
+                return super(hgrepo, self).tags()
+
             if self.tagscache:
                 return self.tagscache
 
             git = GitHandler(self, self.ui)
             tagscache = super(hgrepo, self).tags()
-            tagscache.update(dict([(tag, bin(rev)) for (tag,rev) in git.tags.iteritems()]))
-            tagstypes = dict([(tag, 'git') for tag in git.tags])
-            self._tagstypecache.update(tagstypes)
+            for tag, rev in git.tags.iteritems():
+                if tag in tagscache:
+                    continue
+
+                tagscache[tag] = bin(rev)
+                self._tagstypecache[tag] = 'git'
+
             return tagscache
 
     return hgrepo
