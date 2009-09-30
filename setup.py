25a2b1d0a1f220add574d8697a5df357362e6ced
Kevin Bullock <kbullock@ringworld.org>
2009-09-30 14:39:49 -0500

    package with distutils
    (patch tweaked slightly by Augie Fackler)

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='hg-git',
+    version='0.1.0',
+    author='Scott Chacon',
+    maintainer='Augie Fackler',
+    maintainer_email='durin42@gmail.com',
+    url='http://hg-git.github.com/',
+    description='push and pull from a Git server using Mercurial',
+    long_description="""
+This extension lets you communicate (push and pull) with a Git server.
+This way you can use Git hosting for your project or collaborate with a
+project that is in Git.  A bridger of worlds, this plugin be.
+    """.strip(),
+    keywords='hg git mercurial',
+    license='GPLv2',
+    packages=['hggit'],
+    install_requires=['dulwich>=0.4.0'],
+)
