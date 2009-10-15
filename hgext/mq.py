f444222eca7ada61a1141a565987fef6f0a4ca25
Benoit Allard <benoit@aeteurope.nl>
2009-10-16 00:09:18 +0200

    mq: fix traceback for qpush inexistant-patch with no patch applied

--- hgext/mq.py
+++ hgext/mq.py
@@ -977,6 +977,8 @@ class queue(object):
                 self.ui.warn(_('done\n'))
                 raise
 
+            if not self.applied:
+                return ret[0]
             top = self.applied[-1].name
             if ret[0] and ret[0] > 1:
                 msg = _("errors during apply, please fix and refresh %s\n")
