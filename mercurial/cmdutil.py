f181483f31d019e3ff87793e5e1396d7063d343b
Benoit Boissinot <benoit.boissinot@ens-lyon.org>
2009-11-24 20:40:04 +0100

    findrenames: improve coding-style

--- mercurial/cmdutil.py
+++ mercurial/cmdutil.py
@@ -290,18 +290,19 @@ def findrenames(repo, added, removed, threshold):
             equal = 0
             alines = mdiff.splitnewlines(text)
             matches = bdiff.blocks(text, orig)
-            for x1,x2,y1,y2 in matches:
+            for x1, x2, y1, y2 in matches:
                 for line in alines[x1:x2]:
                     equal += len(line)
 
             lengths = len(text) + len(orig)
-            return equal*2.0 / lengths
+            return equal * 2.0 / lengths
 
         for a in added:
             bestscore = copies.get(a, (None, threshold))[1]
             myscore = score(repo.wread(a))
             if myscore >= bestscore:
                 copies[a] = (r, myscore)
+
     for dest, v in copies.iteritems():
         source, score = v
         yield source, dest, score
