f7b37826db34bb79ca6ded3a124261b1000f7475
Matt Mackall <mpm@selenic.com>
2010-01-28 22:44:45 -0600

    check-code: fix copyright date

--- contrib/check-code.py
+++ contrib/check-code.py
@@ -2,7 +2,7 @@
 #
 # check-code - a style and portability checker for Mercurial
 #
-# Copyright 2005-2007 Matt Mackall <mpm@selenic.com>
+# Copyright 2010 Matt Mackall <mpm@selenic.com>
 #
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
