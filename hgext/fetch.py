ff514439e6a71ee1e7179abb45d5312b0f5a2a49
Sune Foldager <cryo@cyanite.org>
2010-03-03 22:32:51 +0100

    fetch: fix newheads calculation (issue2047)

--- hgext/fetch.py
+++ hgext/fetch.py
@@ -81,7 +81,6 @@ def fetch(ui, repo, source='default', **opts):
 
         # Is this a simple fast-forward along the current branch?
         newheads = repo.branchheads(branch)
-        newheads = [head for head in newheads if len(repo[head].children()) == 0]
         newchildren = repo.changelog.nodesbetween([parent], newheads)[2]
         if len(newheads) == 1:
             if newchildren[0] != parent:
