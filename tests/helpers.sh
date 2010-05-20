1717a658d70cf9f0c0c7f69882b6a04e890dac68
Matt Mackall <mpm@selenic.com>
2010-05-19 20:20:12 -0500

    rebase: use helpers.sh in tests

--- tests/helpers.sh
+++ tests/helpers.sh
@@ -2,8 +2,16 @@
 
 hideport() { sed "s/localhost:$HGPORT/localhost:\$HGPORT/"; }
 
-repr() { python -c "import sys; print repr(sys.stdin.read()).replace('\\n', '\n')" }
+repr() { python -c "import sys; print repr(sys.stdin.read()).replace('\\n', '\n')"; }
 
-hidehex() { python -c 'import sys, re; print re.replace("\b[0-9A-Fa-f]{12,40}", "X" * 12)' }
+hidehex() { python -c 'import sys, re; print re.replace("\b[0-9A-Fa-f]{12,40}", "X" * 12)'; }
 
 hidetmp() { sed "s/$HGTMP/\$HGTMP/"; }
+
+hidebackup() { sed 's/\(saving bundle to \).*/\1/'; }
+
+cleanrebase() {
+    sed -e 's/\(Rebase status stored to\).*/\1/'  \
+        -e 's/\(Rebase status restored from\).*/\1/' \
+        -e 's/\(saving bundle to \).*/\1/';
+}
