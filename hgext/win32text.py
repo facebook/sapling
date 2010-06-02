ae18c6a91028db5ffb702eff58800927dca8609d
Martin Geisler <mg@aragost.com>
2010-06-02 14:54:25 +0200

    win32text: mark this extension as deprecated

--- hgext/win32text.py
+++ hgext/win32text.py
@@ -7,6 +7,14 @@
 
 '''perform automatic newline conversion
 
+  Deprecation: The win32text extension requires each user to configure
+  the extension again and again for each clone since the configuration
+  is not copied when cloning.
+
+  We have therefore made the ``eol`` as an alternative. The ``eol``
+  uses a version controlled file for its configuration and each clone
+  will therefore use the right settings from the start.
+
 To perform automatic newline conversion, use::
 
   [extensions]
