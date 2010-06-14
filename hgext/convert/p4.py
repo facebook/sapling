28f1de6e4165efdd14ef99b5032019d060f43632
Frank Kingswood <frank@kingswood-consulting.co.uk>
2010-06-14 16:38:36 +0100

    Fix shell quoting on p4 conversion

--- hgext/convert/p4.py
+++ hgext/convert/p4.py
@@ -53,7 +53,7 @@ class p4_source(converter_source):
 
     def _parse_view(self, path):
         "Read changes affecting the path"
-        cmd = 'p4 -G changes -s submitted "%s"' % path
+        cmd = 'p4 -G changes -s submitted %s' % util.shellquote(path)
         stdout = util.popen(cmd, mode='rb')
         for d in loaditer(stdout):
             c = d.get("change", None)
@@ -72,7 +72,7 @@ class p4_source(converter_source):
             else:
                 views = {"//": ""}
         else:
-            cmd = 'p4 -G client -o "%s"' % path
+            cmd = 'p4 -G client -o %s' % util.shellquote(path)
             clientspec = marshal.load(util.popen(cmd, mode='rb'))
 
             views = {}
@@ -148,7 +148,7 @@ class p4_source(converter_source):
         return self.heads
 
     def getfile(self, name, rev):
-        cmd = 'p4 -G print "%s#%s"' % (self.depotname[name], rev)
+        cmd = 'p4 -G print %s' % util.shellquote("%s#%s"%(self.depotname[name], rev))
         stdout = util.popen(cmd, mode='rb')
 
         mode = None
