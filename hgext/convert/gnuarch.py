e752297abfc1df893f4c7c524efaa344dcdec7f3
Brodie Rao <brodie@bitheap.org>
2010-08-18 10:53:50 -0400

    convert: properly set encoding for gnuarch converter

--- hgext/convert/gnuarch.py
+++ hgext/convert/gnuarch.py
@@ -55,7 +55,7 @@ class gnuarch_source(converter_source, commandline):
         self.parents = {}
         self.tags = {}
         self.catlogparser = Parser()
-        self.locale = locale.getpreferredencoding()
+        self.encoding = locale.getpreferredencoding()
         self.archives = []
 
     def before(self):
