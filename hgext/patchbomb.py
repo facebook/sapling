c6403d4ac6c9f5ee95ada117e34b906c71f3299b
Benoit Boissinot <benoit.boissinot@ens-lyon.org>
2010-10-20 23:39:48 +0200

    patchbomb: hide passwords potentially embedded in urls

--- hgext/patchbomb.py
+++ hgext/patchbomb.py
@@ -76,7 +76,7 @@ hgrc(5) for details.
 import os, errno, socket, tempfile, cStringIO, time
 import email.MIMEMultipart, email.MIMEBase
 import email.Utils, email.Encoders, email.Generator
-from mercurial import cmdutil, commands, hg, mail, patch, util, discovery
+from mercurial import cmdutil, commands, hg, mail, patch, util, discovery, url
 from mercurial.i18n import _
 from mercurial.node import bin
 
@@ -251,7 +251,7 @@ def patchbomb(ui, repo, *revs, **opts):
         if revs:
             revs = [repo.lookup(rev) for rev in revs]
         other = hg.repository(hg.remoteui(repo, opts), dest)
-        ui.status(_('comparing with %s\n') % dest)
+        ui.status(_('comparing with %s\n') % url.hidepassword(dest))
         o = discovery.findoutgoing(repo, other)
         if not o:
             ui.status(_("no changes found\n"))
