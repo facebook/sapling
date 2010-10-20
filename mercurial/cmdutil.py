7ca1d483b24a22e5c8cd4dc875d2081cdf0523e6
Wagner Bruna <wbruna@softwareexpress.com.br>
2010-10-20 12:29:41 -0200

    cmdutil: mark string for i18n

--- mercurial/cmdutil.py
+++ mercurial/cmdutil.py
@@ -117,7 +117,7 @@ def revsingle(repo, revspec, default='.'):
 
     l = revrange(repo, [revspec])
     if len(l) < 1:
-        raise util.Abort("empty revision set")
+        raise util.Abort(_('empty revision set'))
     return repo[l[-1]]
 
 def revpair(repo, revs):
