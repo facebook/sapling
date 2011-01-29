da93c3bd0bd80adab3a2d0d0d892da831cbbe4bb
Yuya Nishihara <yuya@tcha.org>
2011-01-29 23:23:24 +0900

    url: add --insecure option to bypass verification of ssl certificates
    If --insecure specified, it behaves in the same way as no web.cacerts
    configured.
    
    Also shows hint for --insecure option when _verifycert() failed. But currently
    the hint isn't displayed on SSLError, because it needs a certain level of
    changes.

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -3943,6 +3943,8 @@ remoteopts = [
      _('specify ssh command to use'), _('CMD')),
     ('', 'remotecmd', '',
      _('specify hg command to run on the remote side'), _('CMD')),
+    ('', 'insecure', None,
+     _('do not verify server certificate (ignoring web.cacerts config)')),
 ]
 
 walkopts = [
