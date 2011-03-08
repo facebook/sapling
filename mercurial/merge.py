bd975669e62ac61f9d129092bfdf2bb3ad15c356
Mads Kiilerich <mads@kiilerich.com>
2011-03-08 01:21:50 +0100

    merge: remove last traces of fastforward merging
    bed0e312bdf5 introduced fast-forward branch merging.
    e2079edce509 partly removed it again.
    
    The fastforward variable would always be false if branchmerge was false, so the
    fastforward variable makes no difference and we can remove it completely.

--- mercurial/merge.py
+++ mercurial/merge.py
@@ -493,7 +493,6 @@ def update(repo, node, branchmerge, force, partial):
         p1, p2 = pl[0], repo[node]
         pa = p1.ancestor(p2)
         fp1, fp2, xp1, xp2 = p1.node(), p2.node(), str(p1), str(p2)
-        fastforward = False
 
         ### check phase
         if not overwrite and len(pl) > 1:
@@ -503,9 +502,7 @@ def update(repo, node, branchmerge, force, partial):
                 raise util.Abort(_("merging with a working directory ancestor"
                                    " has no effect"))
             elif pa == p1:
-                if p1.branch() != p2.branch():
-                    fastforward = True
-                else:
+                if p1.branch() == p2.branch():
                     raise util.Abort(_("nothing to merge (use 'hg update'"
                                        " or check 'hg heads')"))
             if not force and (wc.files() or wc.deleted()):
@@ -550,7 +547,7 @@ def update(repo, node, branchmerge, force, partial):
         if not partial:
             repo.dirstate.setparents(fp1, fp2)
             recordupdates(repo, action, branchmerge)
-            if not branchmerge and not fastforward:
+            if not branchmerge:
                 repo.dirstate.setbranch(p2.branch())
     finally:
         wlock.release()
