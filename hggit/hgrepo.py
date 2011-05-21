8c05238aa74e03c2e9f3720a2a0d63291e03ce86
Adrian Sampson <adrian@radbox.org>
2011-05-20 22:36:06 -0700

    use new names for tags cache attributes if they're available

--- hggit/hgrepo.py
+++ hggit/hgrepo.py
@@ -60,22 +60,24 @@ def generate_repo_subclass(baseclass):
             return {}
 
         def tags(self):
-            if not hasattr(self, 'tagscache'):
-                # mercurial 1.4
-                tmp = super(hgrepo, self).tags()
-                tmp.update(self.gitrefs())
-                return tmp
-            if self.tagscache:
+            if hasattr(self, 'tagscache') and self.tagscache:
+                # Mercurial 1.4 and earlier.
                 return self.tagscache
+            elif hasattr(self, '_tags') and self._tags:
+                # Mercurial 1.5 and later.
+                return self._tags
 
             git = GitHandler(self, self.ui)
             tagscache = super(hgrepo, self).tags()
+            tagscache.update(self.gitrefs())
             for tag, rev in git.tags.iteritems():
                 if tag in tagscache:
                     continue
 
                 tagscache[tag] = bin(rev)
-                self._tagstypecache[tag] = 'git'
+                if hasattr(self, '_tagstypecache'):
+                    # Only present in Mercurial 1.3 and earlier.
+                    self._tagstypecache[tag] = 'git'
 
             return tagscache
 
