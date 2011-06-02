6842524caa3efc3aee9e659b6707d6da45390112
Augie Fackler <durin42@gmail.com>
2011-06-02 14:33:01 -0500

    progress: remove superfluous parens

--- hgext/progress.py
+++ hgext/progress.py
@@ -53,7 +53,7 @@ def spacejoin(*args):
     return ' '.join(s for s in args if s)
 
 def shouldprint(ui):
-    return (util.isatty(sys.stderr) or ui.configbool('progress', 'assume-tty'))
+    return util.isatty(sys.stderr) or ui.configbool('progress', 'assume-tty')
 
 def fmtremaining(seconds):
     if seconds < 60:
