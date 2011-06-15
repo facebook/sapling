7049209b6ab361b3a523ede4e3f937e628bc1377
Pierre-Yves David <pierre-yves.david@ens-lyon.org>
2011-06-16 01:57:53 +0200

    hidden: Add a hiddenrevs attributes to changelog.
    This attributes hold the set of all revisions that should be ommited by command
    and tools displaying changesets.
    
    This set is given as a hit. Command and tools are responsible to check it in
    order to filter they outpur.
    
    Code adding revisions to the set are responsible to the consistency of it's
    data.

--- mercurial/changelog.py
+++ mercurial/changelog.py
@@ -107,6 +107,8 @@ class changelog(revlog.revlog):
         self._realopener = opener
         self._delayed = False
         self._divert = False
+        # hiddenrevs: revs that should be hidden by command and tools
+        self.hiddenrevs = set()
 
     def delayupdate(self):
         "delay visibility of index updates to other readers"
