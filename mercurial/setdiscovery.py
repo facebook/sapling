b06b887ee8389d0e83f8dfa961a828e3fdcbd9f0
Matt Mackall <mpm@selenic.com>
2011-07-05 14:30:42 -0500

    discovery: quiet note about heads
    This was changing output on in/out -v for no good reason.

--- mercurial/setdiscovery.py
+++ mercurial/setdiscovery.py
@@ -124,7 +124,7 @@ def findcommonheads(ui, local, remote,
 
     srvheads = dag.internalizeall(srvheadhashes, filterunknown=True)
     if len(srvheads) == len(srvheadhashes):
-        ui.note("all remote heads known locally\n")
+        ui.debug("all remote heads known locally\n")
         return (srvheadhashes, False, srvheadhashes,)
 
     if sample and util.all(yesno):
