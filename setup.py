fb2d55d2a91be5c0b1195e38ab7b9935b391b281
Augie Fackler <durin42@gmail.com>
2011-09-09 16:17:28 -0500

    Bump version for release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
 
 setup(
     name='hg-git',
-    version='0.2.6',
+    version='0.3.0',
     author='Scott Chacon',
     maintainer='Augie Fackler',
     maintainer_email='durin42@gmail.com',
