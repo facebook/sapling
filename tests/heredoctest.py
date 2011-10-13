03e76d64acc8f63f46e5a2c4eefec632dc838f16
Idan Kamara <idankk86@gmail.com>
2011-10-13 17:54:35 +0200

    tests: remove temp doctest file when finished running it

--- tests/heredoctest.py
+++ tests/heredoctest.py
@@ -2,8 +2,12 @@ import doctest, tempfile, os, sys
 
 if __name__ == "__main__":
     fd, name = tempfile.mkstemp(suffix='hg-tst')
-    os.write(fd, sys.stdin.read())
-    os.close(fd)
-    failures, _ = doctest.testfile(name, module_relative=False)
-    if failures:
-        sys.exit(1)
+
+    try:
+        os.write(fd, sys.stdin.read())
+        os.close(fd)
+        failures, _ = doctest.testfile(name, module_relative=False)
+        if failures:
+            sys.exit(1)
+    finally:
+        os.remove(name)
