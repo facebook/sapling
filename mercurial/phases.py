8b330946028a6c4ce379c5fca5cd5eabbdc0eec7
Pierre-Yves David <pierre-yves.david@ens-lyon.org>
2011-11-03 23:49:14 +0100

    phases: Minimal first add.

---
+++ mercurial/phases.py
@@ -0,0 +1,11 @@
+# Mercurial phases support code
+#
+# Copyright 2011 Pierre-Yves David <pierre-yves.david@ens-lyon.org>
+#                Logilab SA        <contact@logilab.fr>
+#                Augie Fackler     <durin42@gmail.com>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+allphases = range(2)
+trackedphases = allphases[1:]
