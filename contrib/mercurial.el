d8334151e62bb56e60c98c813d8878a96d75dd22
Martin Geisler <mg@aragost.com>
2012-01-06 16:27:13 +0100

    Remove FSF mailing address from GPL headers
    The GPLv3 FAQ suggests to upgrade by
    
      [...] replace all your existing v2 license notices (usually at the
      top of each file) with the new recommended text available on the GNU
      licenses howto. It's more future-proof because it no longer includes
      the FSF's postal mailing address.
    
    This removes the postal address, but leaves the version number at 2+.

--- contrib/mercurial.el
+++ contrib/mercurial.el
@@ -15,8 +15,7 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with mercurial.el, GNU Emacs, or XEmacs; see the file COPYING
-;; (`C-h C-l').  If not, write to the Free Software Foundation, Inc.,
-;; 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;; (`C-h C-l').  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
--- mercurial/byterange.py
+++ mercurial/byterange.py
@@ -9,10 +9,8 @@
 #   Lesser General Public License for more details.
 #
 #   You should have received a copy of the GNU Lesser General Public
-#   License along with this library; if not, write to the
-#      Free Software Foundation, Inc.,
-#      59 Temple Place, Suite 330,
-#      Boston, MA  02111-1307  USA
+#   License along with this library; if not, see
+#   <http://www.gnu.org/licenses/>.
 
 # This file is part of urlgrabber, a high-level cross-protocol url-grabber
 # Copyright 2002-2004 Michael D. Stenner, Ryan Tomayko
