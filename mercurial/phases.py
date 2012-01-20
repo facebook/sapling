ce7ff22344792f306092eccb50be6b5ff9587617
Wagner Bruna <wbruna@softwareexpress.com.br>
2012-01-20 13:19:32 -0200

    phases: do not mark debug message for translation

--- mercurial/phases.py
+++ mercurial/phases.py
@@ -147,7 +147,7 @@ def filterunknown(repo, phaseroots=None):
         missing = [node for node in nodes if node not in nodemap]
         if missing:
             for mnode in missing:
-                msg = _('Removing unknown node %(n)s from %(p)i-phase boundary')
+                msg = 'Removing unknown node %(n)s from %(p)i-phase boundary'
                 repo.ui.debug(msg, {'n': short(mnode), 'p': phase})
             nodes.symmetric_difference_update(missing)
             repo._dirtyphases = True
