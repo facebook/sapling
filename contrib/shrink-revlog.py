05fadec600c6f88265b9fc32845878c3d466fecc
Greg Ward <greg@gerg.ca>
2012-03-28 15:15:15 -0400

    shrink-revlog: make pyflakes happy

--- contrib/shrink-revlog.py
+++ contrib/shrink-revlog.py
@@ -16,7 +16,7 @@ This is *not* safe to run on a changelog.
 # e.g. by comparing "before" and "after" states of random changesets
 # (maybe: export before, shrink, export after, diff).
 
-import os, tempfile, errno
+import os, errno
 from mercurial import revlog, transaction, node, util, scmutil
 from mercurial import changegroup
 from mercurial.i18n import _
@@ -191,7 +191,6 @@ def shrink(ui, repo, **opts):
                            'will corrupt your repository'))
 
     ui.write(_('shrinking %s\n') % indexfn)
-    prefix = os.path.basename(indexfn)[:-1]
     tmpindexfn = util.mktempcopy(indexfn, emptyok=True)
 
     r1 = revlog.revlog(scmutil.opener(os.getcwd(), audit=False), indexfn)
