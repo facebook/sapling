00e7333b1bcf683efb27ef5b00f545cc33590824
Wolfgang Treutterer <Wolfgang.Treutterer@ipp.mpg.de>
2012-04-13 10:08:08 +0200

    scmutil: fix systemrcpath regression introduced in ebc9c501f13c
    Changeset ebc9c501f13c introduced a 'root' path component to look for
    hgrc files, which is used both as an absolute path and a path relative
    to the <install-root>.
    The latter one was broken since 'root' was set to an absolute location
    and the subsequent os.path.join discarded the <install-root> path prefix.

--- mercurial/scmutil.py
+++ mercurial/scmutil.py
@@ -437,14 +437,14 @@ if os.name != 'nt':
     def systemrcpath():
         path = []
         if sys.platform == 'plan9':
-            root = '/lib/mercurial'
+            root = 'lib/mercurial'
         else:
-            root = '/etc/mercurial'
+            root = 'etc/mercurial'
         # old mod_python does not set sys.argv
         if len(getattr(sys, 'argv', [])) > 0:
             p = os.path.dirname(os.path.dirname(sys.argv[0]))
             path.extend(rcfiles(os.path.join(p, root)))
-        path.extend(rcfiles(root))
+        path.extend(rcfiles('/' + root))
         return path
 
     def userrcpath():
