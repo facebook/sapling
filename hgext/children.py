4604cfdca7e3c914f7898da17a3eeb861becbc93
Augie Fackler <raf@durin42.com>
2012-05-11 06:15:46 -0500

    children: mark extension as deprecated

--- hgext/children.py
+++ hgext/children.py
@@ -8,7 +8,11 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-'''command to display child changesets'''
+'''command to display child changesets (DEPRECATED)
+
+This extension is deprecated. You should use `hg log -r "children($revision)"`
+instead.
+'''
 
 from mercurial import cmdutil
 from mercurial.commands import templateopts
