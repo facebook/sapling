64822182ac6e097d8bc151ac5405da082ef53a25
Patrick Mezard <patrick@mezard.eu>
2012-07-23 19:03:32 +0200

    addremove: mention --similarity defaults to 100 (issue3430)

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -190,15 +190,15 @@ def addremove(ui, repo, *pats, **opts):
     ``.hgignore``. As with add, these changes take effect at the next
     commit.
 
-    Use the -s/--similarity option to detect renamed files. With a
-    parameter greater than 0, this compares every removed file with
-    every added file and records those similar enough as renames. This
+    Use the -s/--similarity option to detect renamed files. This
     option takes a percentage between 0 (disabled) and 100 (files must
-    be identical) as its parameter. Detecting renamed files this way
+    be identical) as its parameter. With a parameter greater than 0,
+    this compares every removed file with every added file and records
+    those similar enough as renames. Detecting renamed files this way
     can be expensive. After using this option, :hg:`status -C` can be
-    used to check which files were identified as moved or renamed.
-    If this option is not specified, only renames of identical files
-    are detected.
+    used to check which files were identified as moved or renamed. If
+    not specified, -s/--similarity defaults to 100 and only renames of
+    identical files are detected.
 
     Returns 0 if all files are successfully added.
     """
