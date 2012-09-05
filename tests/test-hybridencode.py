db33974023f34ed47e831c51183008f06c683d43
Adrian Buehlmann <adrian@cadifra.com>
2012-09-05 13:57:58 +0200

    test-hybridencode: remove x00 character case
    precludes implementation in C

--- tests/test-hybridencode.py
+++ tests/test-hybridencode.py
@@ -21,8 +21,8 @@ show("data/_")
 print "tilde is character-encoded"
 show("data/~")
 
-print "characters in ASCII code range 0..31"
-show('data/\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+print "characters in ASCII code range 1..31"
+show('data/\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
           '\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f')
 
 print "characters in ASCII code range 126..255 (only partially tested)"
