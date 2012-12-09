eb895c5533e931772ad171dde58dfcf1c55838ec
Mads Kiilerich <madski@unity3d.com>
2012-12-09 23:33:16 +0100

    tests: kill daemons early, making breaking at "Accept" prompt safe

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -926,6 +926,8 @@ def runone(options, test):
         times.append((test, endtime - starttime))
     vlog("# Ret was:", ret)
 
+    killdaemons()
+
     mark = '.'
 
     skipped = (ret == SKIPPED_STATUS)
@@ -995,8 +997,6 @@ def runone(options, test):
         sys.stdout.write(mark)
         sys.stdout.flush()
         iolock.release()
-
-    killdaemons()
 
     if not options.keep_tmpdir:
         shutil.rmtree(testtmp, True)
