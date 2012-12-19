83c3a9618913b6b2478e0ad38985c51e4eca348d
Pierre-Yves David <pierre-yves.david@ens-lyon.org>
2012-12-19 14:43:33 +0100

    branchmap: create a mercurial.branchmap module
    This is the foundation stone for an extraction of branches map logic from local
    repository class.  Most of the branches map logic have very few caller and
    therefor does not fit in the current criteria for code held by the localrepo
    class. Important change will be made to this code in relation with revision
    filtering. So we extract it in a dedicated module before adding additional
    complexity.
    
    
    Follow up commit do the actual code movement.

---
+++ mercurial/branchmap.py
@@ -0,0 +1,6 @@
+# branchmap.py - logic to computes, maintain and stores branchmap for local repo
+#
+# Copyright 2005-2007 Matt Mackall <mpm@selenic.com>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
