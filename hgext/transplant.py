747f253c060452aa7ab9418157661b2348d13d52
Bryan O'Sullivan <bryano@fb.com>
2013-04-12 18:44:22 -0700

    transplant: pass source through to recover

--- hgext/transplant.py
+++ hgext/transplant.py
@@ -297,7 +297,7 @@ class transplanter(object):
     def resume(self, repo, source, opts):
         '''recover last transaction and apply remaining changesets'''
         if os.path.exists(os.path.join(self.path, 'journal')):
-            n, node = self.recover(repo, opts)
+            n, node = self.recover(repo, source, opts)
             self.ui.status(_('%s transplanted as %s\n') % (short(node),
                                                            short(n)))
         seriespath = os.path.join(self.path, 'series')
@@ -312,7 +312,7 @@ class transplanter(object):
 
         self.apply(repo, source, revmap, merges, opts)
 
-    def recover(self, repo, opts):
+    def recover(self, repo, source, opts):
         '''commit working directory using journal metadata'''
         node, user, date, message, parents = self.readlog()
         merge = False
