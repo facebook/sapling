071d89abd0626422926d135c9c113046e425264d
FUJIWARA Katsunori <foozy@lares.dti.ne.jp>
2013-04-26 01:12:03 +0900

    win32: use explicit path to "python.exe" only if it exists
    Before this patch, "hg.bat" for Windows environment always uses
    "%~dp0..\python" as explicit path to "python.exe".
    
    This path may not be valid in some cases.
    
    For example, on the environment using "virtualenv" python package,
    both "python.exe" and "hg.bat" are placed in the same directory.  In
    this case, "python.exe" should be found on PATH, because virtualenv
    activation script puts "python.exe" on the PATH.
    
    This patch uses explicit path to "python.exe" only if it exists, and
    expects that "python.exe" can be found on PATH otherwise.

--- contrib/win32/hg.bat
+++ contrib/win32/hg.bat
@@ -4,9 +4,14 @@ rem Windows Driver script for Mercurial
 setlocal
 set HG=%~f0
 
-rem Use a full path to Python (relative to this script) as the standard Python
-rem install does not put python.exe on the PATH...
+rem Use a full path to Python (relative to this script) if it exists,
+rem as the standard Python install does not put python.exe on the PATH...
+rem Otherwise, expect that python.exe can be found on the PATH.
 rem %~dp0 is the directory of this script
 
-"%~dp0..\python" "%~dp0hg" %*
+if exist "%~dp0..\python.exe" (
+    "%~dp0..\python" "%~dp0hg" %*
+) else (
+    python "%~dp0hg" %*
+)
 endlocal
