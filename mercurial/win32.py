79e0e8b442c09964c44846fca671f6da1adc3198
FUJIWARA Katsunori <foozy@lares.dti.ne.jp>
2013-05-07 05:04:11 +0900

    windows: check target type before actual unlinking to follow POSIX semantics
    Creation and writing into target file via vfs (a.k.a opener) is done
    after "unlink()" target file, if it exists.
    
    For example, it is assumed that the revision X consists of file 'A',
    and the revision Y consists of file 'A/B'. Merging revision X into Y
    tries to "unlink()" on directory 'A' of 'A/B', before creation of file
    'A'.
    
    On POSIX environment, directories should be removed by "rmdir(2)", and
    "unlink(2)" on directories fails. "unlink()" of Mercurial (and Python)
    uses "unlink(2)" directly, so unlinking in the merge case above would
    fail.
    
    In the other hand, on Windows environment, "unlink()" of Mercurial
    tries to rename before actual unlinking, to follow POSIX semantics:
    already opened file can be unlinked safely.
    
    This causes unexpected success in unlinking in the merge case above,
    even though directory 'A' is renamed to another. This confuses users.
    
    This patch checks whether target is directory or not before renaming,
    and raises IOError(errno.EPERM) if so, to follow POSIX semantics.

--- mercurial/win32.py
+++ mercurial/win32.py
@@ -344,6 +344,12 @@ def spawndetached(args):
 def unlink(f):
     '''try to implement POSIX' unlink semantics on Windows'''
 
+    if os.path.isdir(f):
+        # use EPERM because it is POSIX prescribed value, even though
+        # unlink(2) on directories returns EISDIR on Linux
+        raise IOError(errno.EPERM,
+                      "Unlinking directory not permitted: '%s'" % f)
+
     # POSIX allows to unlink and rename open files. Windows has serious
     # problems with doing that:
     # - Calling os.unlink (or os.rename) on a file f fails if f or any
