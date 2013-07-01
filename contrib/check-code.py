be961a468d04c3257a0cca30a6a9699681eabd0b
Simon Heimberg <simohe@besonet.ch>
2013-07-01 06:50:58 +0200

    check-code: concatenate "check-code" on compile time
    The python compiler concatenates two string constants. Use this instead of
    doing it on run time or instruct the user how to do it.
    The strings "no-check-code" and "check-code-ignore" has to be specially written
    for not skipping some checking of the code of this file.

--- contrib/check-code.py
+++ contrib/check-code.py
@@ -410,9 +410,9 @@ def checkfile(f, logfunc=_defaultlogger.log, maxerr=None, warnings=False,
         fp = open(f)
         pre = post = fp.read()
         fp.close()
-        if "no-" + "check-code" in pre:
+        if "no-" "check-code" in pre:
             if debug:
-                print "Skipping %s for %s it has no- and check-code" % (
+                print "Skipping %s for %s it has no-" " check-code" % (
                        name, f)
             break
         for p, r in filters:
@@ -451,9 +451,9 @@ def checkfile(f, logfunc=_defaultlogger.log, maxerr=None, warnings=False,
                     n += 1
                 l = prelines[n]
 
-                if "check-code" + "-ignore" in l:
+                if "check-code" "-ignore" in l:
                     if debug:
-                        print "Skipping %s for %s:%s (check-code -ignore)" % (
+                        print "Skipping %s for %s:%s (check-code" "-ignore)" % (
                             name, f, n)
                     continue
                 elif ignore and re.search(ignore, l, re.MULTILINE):
