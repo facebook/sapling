5d4918976ce51b993e2bd92ff01a62a9ab17a7bf
Sean Farley <sean.michael.farley@gmail.com>
2013-08-05 18:26:15 -0500

    basectx: move p1 from changectx

--- mercurial/context.py
+++ mercurial/context.py
@@ -84,6 +84,9 @@ class basectx(object):
         """return contexts for each parent changeset"""
         return self._parents
 
+    def p1(self):
+        return self._parents[0]
+
 class changectx(basectx):
     """A changecontext object makes access to data related to a particular
     changeset convenient. It represents a read-only context already presnt in
@@ -244,9 +247,6 @@ class changectx(basectx):
         return self._repo._phasecache.phase(self._repo, self._rev)
     def hidden(self):
         return self._rev in repoview.filterrevs(self._repo, 'visible')
-
-    def p1(self):
-        return self._parents[0]
 
     def p2(self):
         if len(self._parents) == 2:
