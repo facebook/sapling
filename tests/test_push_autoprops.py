52e91bcd39f704f5a6f3b229bbe9079825e251b4
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2013-08-09 23:45:58 +0200

    test_push_autoprops: use stupid mode metaclass

--- tests/test_push_autoprops.py
+++ tests/test_push_autoprops.py
@@ -22,7 +22,7 @@ class PushAutoPropsTests(test_util.TestBase):
             "*.py = test:prop=success\n")
         changes = [('test.py', 'test.py', 'echo hallo')]
         self.commitchanges(changes)
-        self.pushrevisions(True)
+        self.pushrevisions()
         prop_val = test_util.svnpropget(
             self.repo_path, "trunk/test.py", 'test:prop')
         self.assertEqual('success', prop_val)
