a2870c6b22c99e3405582205bed7c6bf81a892e5
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2013-08-09 23:45:55 +0200

    test_fetch_exec: use metaclass stupid mode tests

--- tests/test_fetch_exec.py
+++ tests/test_fetch_exec.py
@@ -5,26 +5,21 @@ import unittest
 from mercurial import node
 
 class TestFetchExec(test_util.TestBase):
+    stupid_mode_tests = True
+
     def assertexec(self, ctx, files, isexec=True):
         for f in files:
             self.assertEqual(isexec, 'x' in ctx[f].flags())
 
-    def test_exec(self, stupid=False):
-        repo = self._load_fixture_and_fetch('executebit.svndump', stupid=stupid)
+    def test_exec(self):
+        repo = self._load_fixture_and_fetch('executebit.svndump')
         self.assertexec(repo[0], ['text1', 'binary1', 'empty1'], True)
         self.assertexec(repo[0], ['text2', 'binary2', 'empty2'], False)
         self.assertexec(repo[1], ['text1', 'binary1', 'empty1'], False)
         self.assertexec(repo[1], ['text2', 'binary2', 'empty2'], True)
 
-    def test_exec_stupid(self):
-        self.test_exec(True)
-
-    def test_empty_prop_val_executable(self, stupid=False):
-        repo = self._load_fixture_and_fetch('executable_file_empty_prop.svndump',
-                                            stupid=stupid)
+    def test_empty_prop_val_executable(self):
+        repo = self._load_fixture_and_fetch('executable_file_empty_prop.svndump')
         self.assertEqual(node.hex(repo['tip'].node()),
                          '08e6b380bf291b361a418203a1cb9427213cd1fd')
         self.assertEqual(repo['tip']['foo'].flags(), 'x')
-
-    def test_empty_prop_val_executable_stupid(self):
-        self.test_empty_prop_val_executable(True)
