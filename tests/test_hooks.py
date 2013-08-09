425f40d63cd45ccae8a00d29785d1d1cd15aef0f
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2013-08-09 23:45:57 +0200

    test_hooks: use stupid mode metaclass

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -11,7 +11,7 @@ class TestHooks(test_util.TestBase):
 
     def _loadupdate(self, fixture_name, *args, **kwargs):
         kwargs = kwargs.copy()
-        kwargs.update(stupid=False, noupdate=False)
+        kwargs.update(noupdate=False)
         repo, repo_path = self.load_and_fetch(fixture_name, *args, **kwargs)
         return repo, repo_path
 
