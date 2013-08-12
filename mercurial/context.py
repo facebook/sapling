75c2c801b7da49a6e563a32680a9254687315d41
Sean Farley <sean.michael.farley@gmail.com>
2013-08-11 22:53:47 -0500

    basefilectx: move user from filectx

--- mercurial/context.py
+++ mercurial/context.py
@@ -493,6 +493,8 @@ class basefilectx(object):
         return self._changectx.node()
     def hex(self):
         return self._changectx.hex()
+    def user(self):
+        return self._changectx.user()
 
 class filectx(basefilectx):
     """A filecontext object makes access to data related to a particular
@@ -549,8 +551,6 @@ class filectx(basefilectx):
         return filectx(self._repo, self._path, fileid=fileid,
                        filelog=self._filelog)
 
-    def user(self):
-        return self._changectx.user()
     def date(self):
         return self._changectx.date()
     def files(self):
