ee101eac61e7cf705bb5443160fe0e4588973403
Sean Farley <sean.michael.farley@gmail.com>
2013-08-15 15:06:10 -0500

    memctx: remove p1 since it is now inherited

--- mercurial/context.py
+++ mercurial/context.py
@@ -1578,8 +1578,6 @@ class memctx(committablectx):
             self._text = editor(self._repo, self, [])
             self._repo.savecommitmessage(self._text)
 
-    def p1(self):
-        return self._parents[0]
     def p2(self):
         return self._parents[1]
 
