c4acaaf6bfcd2526cb29dd7ce029b1210d4b1b6d
Sean Farley <sean.michael.farley@gmail.com>
2013-09-11 19:30:08 -0500

    svnrepo: remove pre-1.6 code from remotesvn decorator

--- hgsubversion/svnrepo.py
+++ hgsubversion/svnrepo.py
@@ -71,10 +71,6 @@ def generate_repo_class(ui, repo):
         """
         original = getattr(repo, fn.__name__, None)
 
-        # remove when dropping support for hg < 1.6.
-        if original is None and fn.__name__ == 'findoutgoing':
-            return
-
         def wrapper(self, *args, **opts):
             capable = getattr(args[0], 'capable', lambda x: False)
             if capable('subversion'):
