91f0ca42069d6e3bd7eb7065e6d4372daafd049a
Durham Goode <durham@fb.com>
2013-10-28 16:09:47 -0700

    Enable bookmark/head sync

--- hgsql.py
+++ hgsql.py
@@ -15,6 +15,12 @@
 #INDEX linkrev_index (linkrev)
 #);
 
+#CREATE TABLE headsbookmarks(
+#id INT(2) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+#node char(40) NOT NULL,
+#name VARCHAR(256) UNIQUE,
+#);
+
 # SET OPTION SQL_BIG_SELECTS = 1;
 
 testedwith = 'internal'
@@ -23,6 +29,7 @@ from mercurial.node import bin, hex, nullid, nullrev
 from mercurial.i18n import _
 from mercurial.extensions import wrapfunction
 from mercurial import changelog, error, cmdutil, revlog, localrepo, transaction
+from mercurial import wireproto, bookmarks
 import MySQLdb, struct, time
 from MySQLdb import cursors
 
@@ -30,18 +37,29 @@ cmdtable = {}
 command = cmdutil.command(cmdtable)
 testedwith = 'internal'
 
-
+disablesync = False
 
 def uisetup(ui):
     wrapfunction(revlog.revlog, '_addrevision', addrevision)
     wrapfunction(localrepo, 'instance', repoinstance)
     wrapfunction(transaction.transaction, '_abort', transactionclose)
     wrapfunction(transaction.transaction, 'close', transactionclose)
+    wrapfunction(wireproto, 'unbundle', unbundle)
+    wrapfunction(bookmarks.bmstore, 'write', bookmarkwrite)
+
+    wireproto.commands['unbundle'] = (wireproto.unbundle, 'heads')
 
 def repoinstance(orig, *args):
     repo = orig(*args)
     if repo.ui.configbool("hgsql", "enabled"):
-        pulldb(repo)
+        conn = MySQLdb.connect(**dbargs)
+        cur = conn.cursor()
+        try:
+            pulldb(repo, cur)
+        finally:
+            cur.close()
+            conn.close()
+
     return repo
 
 def reposetup(ui, repo):
@@ -51,81 +69,111 @@ def reposetup(ui, repo):
 
 # Sync with db
 
-def pulldb(repo):
-    conn = MySQLdb.connect(**dbargs)
-    cur = conn.cursor()
+def needsync(repo, cur):
+    # Check latest db rev number
+    cur.execute("SELECT * FROM headsbookmarks")
+    sqlheads = set()
+    sqlbookmarks = {}
+    for _, node, name in cur:
+        if not name:
+            sqlheads.add(bin(node))
+        else:
+            sqlbookmarks[name] = bin(node)
+    
+    heads = repo.heads()
+    bookmarks = repo._bookmarks
+
+    if (not sqlheads or len(heads) != len(sqlheads) or 
+        len(bookmarks) != len(sqlbookmarks)):
+        return True
+
+    for head in sqlheads:
+        if head not in heads:
+            return True
+
+    for bookmark in sqlbookmarks:
+        if (not bookmark in bookmarks or
+            bookmarks[bookmark] != sqlbookmarks[bookmark]):
+            return True
+
+    return False
+
+def pulldb(repo, cur):
+    global disablesync
+
+    if not needsync(repo, cur):
+        return
+
+    repo.ui.debug("syncing with mysql\n")
+
+    lock = None
     try:
-        # Check latest db rev number
-        cur.execute("SELECT MAX(linkrev) FROM revs")
-        dbrev = list(cur)[0][0]
-        if dbrev == None:
-            dbrev = -1
+        lock = repo.lock(wait=False)
+    except error.LockHeld:
+        # If the lock is held, someone else is doing the pull for us.
+        # Wait until they are done.
+        # TODO: is actually true?
+        lock = repo.lock()
+        lock.release()
+        return
+
+    transaction = repo.transaction("pulldb")
+    try:
+        # Refresh the changelog now that we have the lock
+        del repo.changelog
         cl = repo.changelog
         clrev = len(cl) - 1
-        if dbrev <= clrev:
-            return
 
-        lock = None
-        try:
-            lock = repo.lock(wait=False)
-        except error.LockHeld:
-            # If the lock is held, someone else is doing the pull for us.
-            # Wait until they are done.
-            lock = repo.lock()
-            lock.release()
-            return
+        revlogs = {}
+        count = 1
+        chunksize = 5000
+        while count:
+            # Fetch new revs from db
+            cur.execute("SELECT * FROM revs WHERE linkrev > %s and linkrev < %s ORDER BY id ASC", (clrev, clrev + chunksize))
 
-        transaction = repo.transaction("pulldb")
-        try:
-            # Refresh the changelog now that we have the lock
-            del repo.changelog
-            cl = repo.changelog
-            clrev = len(cl) - 1
+            # Add the new revs
+            newentries = addentries(repo, cur, transaction, revlogs)
+            clrev += chunksize - 1
 
-            revlogs = {}
-            count = 1
-            chunksize = 5000
-            while count:
-                # Fetch new revs from db
-                cur.execute("SELECT * FROM revs WHERE linkrev > %s and linkrev < %s ORDER BY id ASC", (clrev, clrev + chunksize))
+            if newentries > 35000 and chunksize > 1000:
+                chunksize -= 100
+            if newentries < 25000:
+                chunksize += 100
 
-                # Add the new revs
-                newentries = addentries(repo, cur, transaction, revlogs)
-                clrev += chunksize - 1
+            count += newentries
+            if count > 50000 or newentries == 0:
+                #print "Flushing (chunksize %s)" % chunksize
+                count = 1
+                for revlog in revlogs.itervalues():
+                    revlog.ifh.flush()
+                    revlog.ifh.close()
+                    if revlog.dfh:
+                        revlog.dfh.flush()
+                        revlog.dfh.close()
+                revlogs = {}
 
-                if newentries > 35000 and chunksize > 1000:
-                    chunksize -= 100
-                if newentries < 25000:
-                    chunksize += 100
+            if newentries == 0:
+                break
 
-                count += newentries
-                if count > 50000 or newentries == 0:
-                    #print "Flushing (chunksize %s)" % chunksize
-                    count = 1
-                    for revlog in revlogs.itervalues():
-                        revlog.ifh.flush()
-                        revlog.ifh.close()
-                        if revlog.dfh:
-                            revlog.dfh.flush()
-                            revlog.dfh.close()
-                    revlogs = {}
-
-                if newentries == 0:
-                    break
-
-            transaction.close()
-        except:
-            import traceback
-            traceback.print_exc()
-            raise
-        finally:
-            transaction.release()
-            lock.release()
+        transaction.close()
     finally:
-        cur.close()
-        conn.close()
+        transaction.release()
+        lock.release()
 
     del repo.changelog
+
+    disablesync = True
+    try:
+        cur.execute("SELECT * FROM headsbookmarks WHERE name IS NOT NULL")
+        bm = repo._bookmarks
+        bm.clear()
+        for _, node, name in cur:
+            node = bin(node)
+            if node in repo:
+                bm[name] = node
+        bm.write()
+    finally:
+        disablesync = False
 
 def addentries(repo, revisions, transaction, revlogs):
     opener = repo.sopener
@@ -134,7 +182,6 @@ def addentries(repo, revisions, transaction, revlogs):
     results = False
     latest = 0
     count = 0
-    start = time.time()
 
     # TODO: write filelogs, then manifests, then changelogs
     for revdata in revisions:
@@ -159,9 +206,6 @@ def addentries(repo, revisions, transaction, revlogs):
         revlog.addentry(transaction, revlog.ifh, revlog.dfh, entry,
                         data0, data1)
 
-    duration = time.time() - start
-    #if count > 0:
-        #print "rev: %s (pieces %s) %0.1fs (%0.5f s/p)" % (latest, count, duration, duration / count)
     return count
 
 class EntryRevlog(revlog.revlog):
@@ -196,6 +240,30 @@ class EntryRevlog(revlog.revlog):
             self.checkinlinesize(transaction, ifh)
 
 # Handle incoming commits
+
+conn = None
+cur = None
+
+def unbundle(orig, repo, proto, heads):
+    global conn
+    global cur
+    conn = MySQLdb.connect(**dbargs)
+    conn.query("SELECT GET_LOCK('commit_lock', 60)")
+    result = conn.store_result().fetch_row()[0][0]
+    if result != 1:
+        raise Exception("unable to obtain write lock")
+
+    cur = conn.cursor()
+    try:
+        # TODO: Verify we are synced with the server
+        pulldb(repo, cur)
+        return orig(repo, proto, heads)
+    finally:
+        cur.close()
+        conn.query("SELECT RELEASE_LOCK('commit_lock')")
+        conn.close()
+        cur = None
+        conn = None
 
 pending = []
 
@@ -256,23 +324,55 @@ def addrevision(orig, self, node, text, transaction, link, p1, p2,
     return result
 
 def pretxnchangegroup(ui, repo, *args, **kwargs):
+    if conn == None:
+        raise Exception("Invalid update. Only hg push is allowed.")
+
     # Commit to db
-    conn = MySQLdb.connect(**dbargs)
     try:
-        cur = conn.cursor()
         for revision in pending:
             _, path, linkrev, entry, data0, data1 = revision
             cur.execute("""INSERT INTO revs(path, linkrev, entry, data0, data1)
                 VALUES(%s, %s, %s, %s, %s)""", (path, linkrev, entry, data0, data1))
+
+        cur.execute("""DELETE FROM headsbookmarks WHERE name IS NULL""")
+
+        for head in repo.heads():
+            cur.execute("""INSERT INTO headsbookmarks(node) VALUES(%s)""",
+                (hex(head)))
+
         conn.commit()
-    except:
+    except Exception:
         conn.rollback()
         raise
     finally:
+        del pending[:]
+
+def bookmarkwrite(orig, self):
+    if disablesync:
+        return orig(self)
+
+    conn = MySQLdb.connect(**dbargs)
+    conn.query("SELECT GET_LOCK('bookmark_lock', 60)")
+    result = conn.store_result().fetch_row()[0][0]
+    if result != 1:
+        raise Exception("unable to obtain write lock")
+    try:
+        cur = conn.cursor()
+
+        cur.execute("""DELETE FROM headsbookmarks WHERE name IS NOT NULL""")
+
+        for k, v in self.iteritems():
+            cur.execute("""INSERT INTO headsbookmarks(node, name) VALUES(%s, %s)""",
+                (hex(v), k))
+        conn.commit()
+        return orig(self)
+    finally:
+        cur.close()
+        conn.query("SELECT RELEASE_LOCK('bookmark_lock')")
         conn.close()
 
-    del pending[:]
-
 def transactionclose(orig, self):
-    del pending[:]
-    return orig(self)
+    result = orig(self)
+    if self.count == 0:
+        del pending[:]
+    return result
