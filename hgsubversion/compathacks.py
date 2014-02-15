bb091cfb0cb9a8acd66d94b10546618bc649bfb7
Sean Farley <sean.michael.farley@gmail.com>
2014-02-15 01:53:55 -0600

    compathacks: pep8

--- hgsubversion/compathacks.py
+++ hgsubversion/compathacks.py
@@ -1,11 +1,12 @@
 """Functions to work around API changes inside Mercurial."""
 
-def branchset(repo):
-  """Return the set of branches present in a repo.
 
-  Works around branchtags() vanishing between 2.8 and 2.9.
-  """
-  try:
-    return set(repo.branchmap())
-  except AttributeError:
-    return set(repo.branchtags())
+def branchset(repo):
+    """Return the set of branches present in a repo.
+
+    Works around branchtags() vanishing between 2.8 and 2.9.
+    """
+    try:
+        return set(repo.branchmap())
+    except AttributeError:
+        return set(repo.branchtags())
