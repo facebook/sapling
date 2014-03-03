0cf2db5155b7dcf69480081bac7d745eadce5f1f
Yuya Nishihara <yuya@tcha.org>
2014-03-03 15:50:41 +0900

    dirstate: remove double imports of errno

--- mercurial/dirstate.py
+++ mercurial/dirstate.py
@@ -4,7 +4,6 @@
 #
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
-import errno
 
 from node import nullid
 from i18n import _
