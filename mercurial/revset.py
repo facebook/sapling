f9553b0f975655fa2b4d7544c522dad976b45814
Pierre-Yves David <pierre-yves.david@fb.com>
2014-04-25 23:38:24 -0700

    revset: drop spanset._contained
    All its users inlined it for performance reasons.
    (See fcccbf073394 and 166d6dde9310)

--- mercurial/revset.py
+++ mercurial/revset.py
@@ -2764,10 +2764,6 @@ class spanset(_orderedsetmixin):
         if self._start < self._end:
             self.reverse()
 
-    def _contained(self, rev):
-        return (rev <= self._start and rev > self._end) or (rev >= self._start
-                and rev < self._end)
-
     def __iter__(self):
         if self._start <= self._end:
             iterrange = xrange(self._start, self._end)
