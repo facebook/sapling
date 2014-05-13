1c61c176de4b4bf7f41478813a8853b38267748d
Matt Mackall <mpm@selenic.com>
2014-05-13 15:22:36 -0500

    py3kcompat: drop unused export

--- mercurial/py3kcompat.py
+++ mercurial/py3kcompat.py
@@ -5,7 +5,7 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-import os, builtins
+import builtins
 
 from numbers import Number
 
