58c28a86e94e33b88c9f94d504c5cd887b10f0ac
Siddharth Agarwal <sid0@fb.com>
2014-05-19 18:48:40 -0700

    import: remove redundant imports
    These modules are already imported above.

--- hggit/__init__.py
+++ hggit/__init__.py
@@ -126,8 +126,6 @@ def gclear(ui, repo):
     repo.ui.status(_("clearing out the git cache data\n"))
     repo.githandler.clear()
 
-from mercurial import dirstate
-from mercurial import ignore
 if (getattr(dirstate, 'rootcache', False) and
     getattr(ignore, 'readpats', False)):
     # only install our dirstate wrapper if it has a hope of working
