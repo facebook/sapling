a32b171762d307c5937dac9693b9e59cc75741dd
Sean Farley <sean.michael.farley@gmail.com>
2014-06-02 19:53:49 -0500

    compathacks: add wrapper for memfilectx

--- hgsubversion/compathacks.py
+++ hgsubversion/compathacks.py
@@ -1,11 +1,27 @@
 """Functions to work around API changes inside Mercurial."""
 
 def branchset(repo):
-  """Return the set of branches present in a repo.
+    """Return the set of branches present in a repo.
 
-  Works around branchtags() vanishing between 2.8 and 2.9.
-  """
-  try:
-    return set(repo.branchmap())
-  except AttributeError:
-    return set(repo.branchtags())
+    Works around branchtags() vanishing between 2.8 and 2.9.
+    """
+    try:
+        return set(repo.branchmap())
+    except AttributeError:
+        return set(repo.branchtags())
+
+def pickle_load(f):
+    import cPickle as pickle
+    f.seek(0)
+    return pickle.load(f)
+
+def makememfilectx(repo, path, data, islink, isexec, copied):
+    """Return a memfilectx
+
+    Works around memfilectx() adding a repo argument between 3.0 and 3.1.
+    """
+    from mercurial import context
+    try:
+        return context.memfilectx(repo, path, data, islink, isexec, copied)
+    except TypeError:
+        return context.memfilectx(path, data, islink, isexec, copied)
