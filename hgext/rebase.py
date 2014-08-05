2268a3034cdadf2319b85d4f6622bf913e30e78e
Pierre-Yves David <pierre-yves.david@fb.com>
2014-08-05 13:22:44 -0700

    rebase: do not retract phase boundary by hand
    We rely on the internal mechanism to commit the changeset in the right phase.
    This similar to what the mq extension is doing.
    
    This is an important change as we plan to includes phase movement within the
    transaction. Avoiding phase movement from high-level code will avoid the
    burden of transaction handling. It is also important to limit the need for
    transaction handling as this limits the odds of people messing up. Most common
    expected mess-up is code using a different transaction for changeset creation
    and phase adjustment.

--- hgext/rebase.py
+++ hgext/rebase.py
@@ -469,15 +469,18 @@ def concludenode(repo, rev, p1, p2, commitmsg=None, editor=None, extrafn=None):
         extra = {'rebase_source': ctx.hex()}
         if extrafn:
             extrafn(ctx, extra)
-        # Commit might fail if unresolved files exist
-        newrev = repo.commit(text=commitmsg, user=ctx.user(),
-                             date=ctx.date(), extra=extra, editor=editor)
+
+        backup = repo.ui.backupconfig('phases', 'new-commit')
+        try:
+            targetphase = max(ctx.phase(), phases.draft)
+            repo.ui.setconfig('phases', 'new-commit', targetphase, 'rebase')
+            # Commit might fail if unresolved files exist
+            newrev = repo.commit(text=commitmsg, user=ctx.user(),
+                                 date=ctx.date(), extra=extra, editor=editor)
+        finally:
+            repo.ui.restoreconfig(backup)
+
         repo.dirstate.setbranch(repo[newrev].branch())
-        targetphase = max(ctx.phase(), phases.draft)
-        # retractboundary doesn't overwrite upper phase inherited from parent
-        newnode = repo[newrev].node()
-        if newnode:
-            phases.retractboundary(repo, targetphase, [newnode])
         return newrev
     except util.Abort:
         # Invalidate the previous setparents
