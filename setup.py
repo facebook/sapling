13a5ad2a4d8bf200c284a00dbf0ac49e20bc7b41
Augie Fackler <raf@durin42.com>
2014-08-23 12:45:27 -0400

    setup: require newer dulwich so we can always pass an opener to HttpGitClient

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ project that is in Git.  A bridger of worlds, this plugin be.
     packages=['hggit'],
     package_data={ 'hggit': ['help/git.rst'] },
     include_package_data=True,
-    install_requires=['dulwich>=0.9.4'] + extra_req,
+    install_requires=['dulwich>=0.9.7'] + extra_req,
 )
