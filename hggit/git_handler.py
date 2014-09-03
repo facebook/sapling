bae8da09329c0e60c076bbb1f17ee484debf2d6f
Siddharth Agarwal <sid0@fb.com>
2014-09-03 18:45:18 +0200

    git_handler: remove unnecessary os.path.join call

--- hggit/git_handler.py
+++ hggit/git_handler.py
@@ -1240,7 +1240,7 @@ class GitHandler(object):
                          ' bookmarks enabled?\n'))
 
     def update_remote_branches(self, remote_name, refs):
-        tagfile = self.repo.join(os.path.join('git-remote-refs'))
+        tagfile = self.repo.join('git-remote-refs')
         tags = self.repo.gitrefs()
         # since we re-write all refs for this remote each time, prune
         # all entries matching this remote from our tags list now so
