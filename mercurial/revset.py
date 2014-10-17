593610531ef9a4cd7ea51c1f49bb441e8ce1336a
Pierre-Yves David <pierre-yves.david@fb.com>
2014-10-16 23:11:25 -0700

    revset-ancestorspec: call 'getset' on a 'fullreposet'
    Calling 'baseset(repo.changelog)' builds a list for all revisions in
    the repo. And we already have the lazy and efficient 'fullreposet'
    class for this purpose.
    
    This gives us the usual benefits of the fullreposet:
    
    revset) 100~5
    before) wall 0.002712 comb 0.000000 user 0.000000 sys 0.000000 (best of 918)
    after)  wall 0.000996 comb 0.000000 user 0.000000 sys 0.000000 (best of 2493)
    
    revset) parents(100)~5
    before) wall 0.003812 comb 0.010000 user 0.010000 sys 0.000000 (best of 667)
    after)  wall 0.001038 comb 0.000000 user 0.000000 sys 0.000000 (best of 2361)
    
    revset) (100~5)~5
    before) wall 0.005614 comb 0.000000 user 0.000000 sys 0.000000 (best of 446)
    after)  wall 0.001035 comb 0.000000 user 0.000000 sys 0.000000 (best of 2424)

--- mercurial/revset.py
+++ mercurial/revset.py
@@ -370,7 +370,7 @@ def ancestorspec(repo, subset, x, n):
         raise error.ParseError(_("~ expects a number"))
     ps = set()
     cl = repo.changelog
-    for r in getset(repo, baseset(cl), x):
+    for r in getset(repo, fullreposet(repo), x):
         for i in range(n):
             r = cl.parentrevs(r)[0]
         ps.add(r)
