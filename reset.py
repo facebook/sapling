47b86129f0e456d617e9fd6912424627492da6f9
Durham Goode <durham@fb.com>
2014-10-23 18:56:24 -0700

    Add reset command
    Summary:
    This adds a 'hg reset' command that acts similar to 'git reset'. It moves your
    active bookmark to the desired location, along with your working copy parent. If
    no flags are specified, the working copy contents is left alone. If --clean is
    specified, the working copy contents is overwritten.
    
    By default we will attempt to recover the commit from the backup bundles if it
    is not in the repository. A future commit will add support for using touch/prune
    when evolve is enabled.
    
    Test Plan: Added tests, and ran them
    
    Reviewers: sid0, davidsp, pyd, mpm, akushner, rmcelroy
    
    Differential Revision: https://phabricator.fb.com/D1637658

---
+++ reset.py
@@ -0,0 +1,158 @@
+# Copyright 2014 Facebook Inc.
+#
+
+from mercurial.i18n import _
+from mercurial.node import short, hex
+from mercurial import extensions, merge, dicthelpers, scmutil, hg
+from mercurial import cmdutil, obsolete, repair, util, bundlerepo, error
+from mercurial import exchange
+import struct, os, glob
+
+cmdtable = {}
+command = cmdutil.command(cmdtable)
+testedwith = 'internal'
+
+@command("reset", [
+        ('C', 'clean', None, _('wipe the working copy clean when reseting')),
+        ('k', 'keep', None, _('keeps the old commits the bookmark pointed to')),
+    ], _('hg reset [REV]'))
+def reset(ui, repo, *args, **opts):
+    """moves the active bookmark and working copy parent to the desired rev
+
+    The reset command is for moving your active bookmark and working copy to a
+    different location. This is useful for undoing commits, amends, etc.
+
+    By default, the working copy content is not touched, so you will have
+    pending changes after the reset. If --clean/-C is specified, the working
+    copy contents will be overwritten to match the destination revision, and you
+    will not have any pending changes.
+
+    After your bookmark and working copy have been moved, the command will
+    delete any commits that belonged only to that bookmark. Use --keep/-k to
+    avoid deleting any commits.
+    """
+    repo = repo.unfiltered()
+    rev = args[0] if args else '.'
+    try:
+        revs = repo.revs(rev)
+        if len(revs) > 1:
+            raise util.Abort(_('exactly one revision must be specified'))
+        rev = revs.first()
+    except error.RepoLookupError:
+        # `rev` can be anything, a hash, a partial hash, a revset, etc. If it's
+        # a revset, repo.revs() will convert it.  If it's a hash that only
+        # exists in a bundle, repo.revs() will throw an exception, but it's
+        # still valid input since we'll recover the commit shortly.
+        pass
+    oldctx = repo['.']
+
+    wlock = None
+    try:
+        wlock = repo.wlock()
+        # Ensure we have an active bookmark
+        bookmark = repo._bookmarkcurrent
+        if not bookmark:
+            ui.warn(_('reseting without an active bookmark\n'))
+
+        ctx = _revive(repo, rev)
+        if ctx.node() == oldctx.node() and not opts.get('clean'):
+            ui.status(_('reseting without any arguments does nothing\n'))
+            return
+
+        _moveto(repo, bookmark, ctx, clean=opts.get('clean'))
+        if not opts.get('keep'):
+            _deleteunreachable(repo, oldctx)
+    finally:
+        wlock.release()
+
+def _revive(repo, rev):
+    """Brings the given rev back into the repository. Finding it in backup
+    bundles if necessary.
+    """
+    if rev not in repo:
+        other, rev = _findbundle(repo, rev)
+        if not other:
+            raise util.Abort("could not find '%s' in the repo or the backup"
+                             " bundles" % rev)
+        exchange.pull(repo, other, heads=[rev])
+
+        if rev not in repo:
+            raise util.Abort("unable to get rev %s from repo" % rev)
+
+    return repo[rev]
+
+def _findbundle(repo, rev):
+    """Returns the backup bundle that contains the given rev. If found, it
+    returns the bundle peer and the full rev hash. If not found, it return None
+    and the given rev value.
+    """
+    ui = repo.ui
+    backuppath = repo.join("strip-backup")
+    backups = filter(os.path.isfile, glob.glob(backuppath + "/*.hg"))
+    backups.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+    for backup in backups:
+        # Much of this is copied from the hg incoming logic
+        source = os.path.relpath(backup, os.getcwd())
+        source = ui.expandpath(source)
+        source, branches = hg.parseurl(source)
+        other = hg.peer(repo, {}, source)
+
+        quiet = ui.quiet
+        try:
+            ui.quiet = True
+            localother, chlist, cleanupfn = bundlerepo.getremotechanges(ui, repo, other,
+                                        None, None, None)
+            for node in chlist:
+                if hex(node).startswith(rev):
+                    return other, node
+        except error.LookupError:
+            continue
+        finally:
+            ui.quiet = quiet
+
+    return None, rev
+
+def _moveto(repo, bookmark, ctx, clean=False):
+    """Moves the given bookmark and the working copy to the given revision.
+    By default it does not overwrite the working copy contents unless clean is
+    True.
+
+    Assumes the wlock is already taken.
+    """
+    # Move working copy over
+    if clean:
+        merge.update(repo, ctx.node(),
+                     False, # not a branchmerge
+                     True, # force overwriting files
+                     None) # not a partial update
+    else:
+        # Mark any files that are different between the two as normal-lookup
+        # so they show up correctly in hg status afterwards.
+        wctx = repo[None]
+        m1 = wctx.manifest()
+        m2 = ctx.manifest()
+        diff = m1.diff(m2)
+
+        changedfiles = []
+        changedfiles.extend(diff.iterkeys())
+
+        dirstate = repo.dirstate
+        dirchanges = [f for f in dirstate if dirstate[f] != 'n']
+        changedfiles.extend(dirchanges)
+
+        dirstate.beginparentchange()
+        dirstate.rebuild(ctx.node(), m2, changedfiles)
+        dirstate.endparentchange()
+
+    # Move bookmark over
+    if bookmark:
+        repo._bookmarks[bookmark] = ctx.node()
+        repo._bookmarks.write()
+
+def _deleteunreachable(repo, ctx):
+    """Deletes all ancestor and descendant commits of the given revision that
+    aren't reachable from another bookmark.
+    """
+    hiderevs = repo.revs('::%s - ::(bookmark() + .)', ctx.rev())
+    if hiderevs:
+        repair.strip(repo.ui, repo, [repo.changelog.node(r) for r in hiderevs])
---
+++ tests/test-reset.t
@@ -0,0 +1,127 @@
+  $ extpath=$(dirname $TESTDIR)
+  $ cp $extpath/reset.py $TESTTMP # use $TESTTMP substitution in message
+  $ cat >> $HGRCPATH << EOF
+  > [extensions]
+  > reset=$TESTTMP/reset.py
+  > EOF
+
+  $ hg init repo
+  $ cd repo
+
+  $ echo x > x
+  $ hg commit -qAm x
+  $ hg book foo
+
+Soft reset should leave pending changes
+
+  $ echo y >> x
+  $ hg commit -qAm y
+  $ hg log -G -T '{node|short} {bookmarks}\n'
+  @  66ee28d0328c foo
+  |
+  o  b292c1e3311f
+  
+  $ hg reset .^
+  saved backup bundle to $TESTTMP/repo/.hg/strip-backup/66ee28d0328c-backup.hg
+  $ hg log -G -T '{node|short} {bookmarks}\n'
+  @  b292c1e3311f foo
+  
+  $ hg diff
+  diff -r b292c1e3311f x
+  --- a/x	Thu Jan 01 00:00:00 1970 +0000
+  +++ b/x	* (glob)
+  @@ -1,1 +1,2 @@
+   x
+  +y
+
+Clean reset should overwrite all changes
+
+  $ hg commit -qAm y
+  $ hg reset --clean .^
+  saved backup bundle to $TESTTMP/repo/.hg/strip-backup/66ee28d0328c-backup.hg
+  $ hg diff
+
+Reset should recover from backup bundles
+
+  $ hg log -G -T '{node|short} {bookmarks}\n'
+  @  b292c1e3311f foo
+  
+  $ hg reset --clean 66ee28d0328c
+  searching for changes
+  adding changesets
+  adding manifests
+  adding file changes
+  added 1 changesets with 1 changes to 1 files
+  $ hg log -G -T '{node|short} {bookmarks}\n'
+  @  66ee28d0328c foo
+  |
+  o  b292c1e3311f
+  
+
+Reset should not strip reachable commits
+
+  $ hg book bar
+  $ hg reset --clean .^
+  $ hg log -G -T '{node|short} {bookmarks}\n'
+  o  66ee28d0328c foo
+  |
+  @  b292c1e3311f bar
+  
+
+  $ hg book -d bar
+  $ hg up foo
+  1 files updated, 0 files merged, 0 files removed, 0 files unresolved
+  (activating bookmark foo)
+
+Reset to '.' by default
+
+  $ echo z >> x
+  $ hg st
+  M x
+  $ hg reset
+  reseting without any arguments does nothing
+  $ hg st
+  M x
+  $ hg reset -C
+  $ hg st
+
+Keep old commits
+
+  $ hg reset --keep .^
+  $ hg log -G -T '{node|short} {bookmarks}\n'
+  o  66ee28d0328c
+  |
+  @  b292c1e3311f foo
+  
+Reset without a bookmark
+
+  $ hg up tip
+  1 files updated, 0 files merged, 0 files removed, 0 files unresolved
+  (leaving bookmark foo)
+  $ hg book -d foo
+  $ hg reset .^
+  reseting without an active bookmark
+  saved backup bundle to $TESTTMP/repo/.hg/strip-backup/66ee28d0328c-backup.hg
+  $ hg book foo
+
+Verify file status after reset
+
+  $ hg reset -C 66ee28d0328c
+  searching for changes
+  adding changesets
+  adding manifests
+  adding file changes
+  added 1 changesets with 1 changes to 1 files
+  $ touch toberemoved
+  $ hg commit -qAm 'add file for removal'
+  $ echo z >> x
+  $ touch tobeadded
+  $ hg add tobeadded
+  $ hg rm toberemoved
+  $ hg commit -m 'to be reset'
+  $ hg reset .^
+  saved backup bundle to $TESTTMP/repo/.hg/strip-backup/d36bf00ac47e-backup.hg
+  $ hg status
+  M x
+  ! toberemoved
+  ? tobeadded
