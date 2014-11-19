134022a8b9c46e4b18ee7a6dc9dc65c0475f02e7
Siddharth Agarwal <sid0@fb.com>
2014-11-18 21:47:47 -0800

    notify: explicitly honor all diffopts
    The notify output doesn't seem to be parseable anyway, what with the maxdiff
    config option. Plus it is designed mainly for servers where hopefully the
    admins are doing sensible things.

--- hgext/notify.py
+++ hgext/notify.py
@@ -341,7 +341,8 @@ class notifier(object):
         maxdiff = int(self.ui.config('notify', 'maxdiff', 300))
         prev = ctx.p1().node()
         ref = ref and ref.node() or ctx.node()
-        chunks = patch.diff(self.repo, prev, ref, opts=patch.diffopts(self.ui))
+        chunks = patch.diff(self.repo, prev, ref,
+                            opts=patch.diffallopts(self.ui))
         difflines = ''.join(chunks).splitlines()
 
         if self.ui.configbool('notify', 'diffstat', True):
