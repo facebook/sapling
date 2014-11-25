5a403fa38dc0198fa4421625a33a529b4e90f1b1
Jeff Sickel <jas@corpus-callosum.com>
2014-11-25 02:27:31 -0600

    plan9: setup check for current python build

--- setup.py
+++ setup.py
@@ -141,7 +141,8 @@ except ImportError:
     py2exeloaded = False
 
 def runcmd(cmd, env):
-    if sys.platform == 'plan9':
+    if (sys.platform == 'plan9'
+       and (sys.version_info[0] == 2 and sys.version_info[1] < 7)):
         # subprocess kludge to work around issues in half-baked Python
         # ports, notably bichued/python:
         _, out, err = os.popen3(cmd)
