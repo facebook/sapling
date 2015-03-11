5bc207a8950b5a48d63c3bba901573f925100cbb
Sean Farley <sean.michael.farley@gmail.com>
2015-03-11 14:34:09 -0700

    setup: forgot to update this info

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 try:
     from setuptools import setup
-except:
+except ImportError:
     from distutils.core import setup
 
 setup(
-    name='hg-remotenames',
-    version='1.0.0',
-    author='Augie Fackler',
-    maintainer='Augie Fackler',
-    maintainer_email='durin42@gmail.com',
-#    url='',
-    description='Mark remote branch heads in Mercurial',
+    name='remotenames',
+    version='0.2',
+    author='Sean Farley',
+    maintainer='Sean Farley',
+    maintainer_email='sean@farley.io',
+    url='https://bitbucket.org/seanfarley/hgremotenames',
+    description='Mark remote branch and bookmark heads in Mercurial',
     long_description="""
 This extension automatically creates a local tag-like marker
 during a pull from a remote server that has its path specifed
