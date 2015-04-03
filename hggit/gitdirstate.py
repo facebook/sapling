db378d8134d29aed4749317f43c80e07858d26ea
Siddharth Agarwal <sid0@fb.com>
2015-04-03 10:27:04 -0700

    gitdirstate: use normalized path from _walkexplicit
    Since cce24e8019c8 in Mercurial, _walkexplicit returns a tuple, so ensure
    we are up to date and take the normalized path which is the first part of the
    tuple.
    
    Based on a patch by David Soria Parra <davidsp@fb.com>.

--- hggit/gitdirstate.py
+++ hggit/gitdirstate.py
@@ -163,7 +163,11 @@ class gitdirstate(dirstate.dirstate):
         results, work, dirsnotfound = self._walkexplicit(match, subrepos)
 
         skipstep3 = skipstep3 and not (work or dirsnotfound)
-        work = [d for d in work if not dirignore(d)]
+        if work and isinstance(work[0], tuple):
+            # Mercurial >= 3.3.3
+            work = [nd for nd, d in work if not dirignore(d)]
+        else:
+            work = [d for d in work if not dirignore(d)]
         wadd = work.append
 
         # step 2: visit subdirectories
