e6b221f9d69d5e660fe7312669651631b21ddd40
Durham Goode <durham@fb.com>
2015-04-27 20:20:48 -0700

    extras: add commitextras extension
    Summary:
    This adds an extension which will allow a user to specify commit 'extras' from
    the command line. This will allow our repo sync script to include extra metadata
    in the commits.
    
    For instance, this is required in order to not lose git's committer info when
    syncing into mercurial (which is required in order to make sure two commits with
    the same content but different committers will produce a different hash in
    Mercurial).
    
    Test Plan: Added a simple test
    
    Reviewers: pyd, lcharignon, mpm, sid0, rmcelroy
    
    Reviewed By: rmcelroy
    
    Differential Revision: https://phabricator.fb.com/D2027341
    
    Signature: t1:2027341:1430255572:6866d0ba5564ca977c8fce41e55988883e15ce5b

---
+++ commitextras.py
@@ -0,0 +1,40 @@
+# commitextras.py
+#
+# Copyright 2013 Facebook, Inc.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+from mercurial import util, cmdutil, commands, hg, scmutil
+from mercurial import bookmarks, extensions
+from mercurial.i18n import _
+from hgext import rebase
+import errno, os, stat, subprocess
+
+cmdtable = {}
+command = cmdutil.command(cmdtable)
+testedwith = 'internal'
+
+def extsetup(ui):
+    entry = extensions.wrapcommand(commands.table, 'commit', _commit)
+    options = entry[1]
+    options.append(('', 'extra', [],
+        _('set a commit\'s extra values'), _("KEY=VALUE")))
+
+def _commit(orig, ui, repo, *pats, **opts):
+    origcommit = repo.commit
+    try:
+        def _wrappedcommit(*innerpats, **inneropts):
+            extras = opts.get('extra')
+            if extras:
+                for raw in extras:
+                    k, v = raw.split('=', 1)
+                    inneropts['extra'][k] = v
+            return origcommit(*innerpats, **inneropts)
+
+        # This __dict__ logic is needed because the normal
+        # extension.wrapfunction doesn't seem to work.
+        repo.__dict__['commit'] = _wrappedcommit
+        return orig(ui, repo, *pats, **opts)
+    finally:
+        del repo.__dict__['commit']
---
+++ tests/test-commitextras.t
@@ -0,0 +1,19 @@
+  $ extpath=$(dirname $TESTDIR)
+  $ cp $extpath/commitextras.py $TESTTMP # use $TESTTMP substitution in message
+  $ cat >> $HGRCPATH << EOF
+  > [extensions]
+  > commitextras=$TESTTMP/commitextras.py
+  > EOF
+
+Test stuff
+
+  $ hg init repo
+  $ cd repo
+  $ touch a
+  $ hg commit -Aqm a
+  $ echo a > a
+  $ hg commit -qm a2 --extra oldhash=foo --extra source=bar
+  $ hg log -r . -T '{extras % "{extra}\n"}'
+  branch=default
+  oldhash=foo
+  source=bar
