db5d2b0a12f653f32126b5e39035e99875102489
Pierre-Yves David <pierre-yves.david@fb.com>
2015-05-17 18:00:38 -0700

    revset: use 'next()' to detect end of iteration in 'last'
    The 'next()' built-in can return a default value, allow to get rid of the
    confusing try/except code flow.

--- mercurial/revset.py
+++ mercurial/revset.py
@@ -1172,12 +1172,11 @@ def last(repo, subset, x):
     result = []
     it = iter(os)
     for x in xrange(lim):
-        try:
-            y = it.next()
-            if y in ss:
-                result.append(y)
-        except (StopIteration):
+        y = next(it, None)
+        if y is None:
             break
+        elif y in ss:
+            result.append(y)
     return baseset(result)
 
 def maxrev(repo, subset, x):
