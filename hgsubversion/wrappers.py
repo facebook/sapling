0c60e0b08100f88fb8806c1ed0efc345caf2e86d
Augie Fackler <raf@durin42.com>
2015-06-16 11:47:15 -0400

    wrappers: accept-and-forward oparg pull argument
    It's possible we should do something with this some day, but for now
    this at least keeps hgsubversion from breaking pull when bundle2 is
    enabled.

--- hgsubversion/wrappers.py
+++ hgsubversion/wrappers.py
@@ -515,7 +515,8 @@ def pull(repo, source, heads=[], force=False, meta=None):
     else:
         ui.status("pulled %d revisions\n" % revisions)
 
-def exchangepull(orig, repo, remote, heads=None, force=False, bookmarks=()):
+def exchangepull(orig, repo, remote, heads=None, force=False, bookmarks=(),
+                 opargs=None):
     capable = getattr(remote, 'capable', lambda x: False)
     if capable('subversion'):
         # transaction manager is present in Mercurial >= 3.3
@@ -536,7 +537,13 @@ def exchangepull(orig, repo, remote, heads=None, force=False, bookmarks=()):
             else:
                 pullop.releasetransaction()
     else:
-        return orig(repo, remote, heads, force, bookmarks=bookmarks)
+        if opargs is not None:
+            # hg 3.5
+            return orig(
+                repo, remote, heads, force, bookmarks=bookmarks, opargs=opargs)
+        else:
+            # hg 3.4
+            return orig(repo, remote, heads, force, bookmarks=bookmarks)
 
 def rebase(orig, ui, repo, **opts):
     """rebase current unpushed revisions onto the Subversion head
