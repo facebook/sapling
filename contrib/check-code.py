7a107e3b4a407e78c30314724658f1bfac174248
Gregory Szorc <gregory.szorc@gmail.com>
2015-06-23 22:20:01 -0700

    check-code: detect legacy exception syntax
    We just rewrote all files to use modern exception syntax. Ban the old
    form.
    
    This will detect the "except type, instance" and
    "except (type1, type2), instance" forms.

--- contrib/check-code.py
+++ contrib/check-code.py
@@ -281,6 +281,8 @@ pypats = [
     (r'\.debug\(\_', "don't mark debug messages for translation"),
     (r'\.strip\(\)\.split\(\)', "no need to strip before splitting"),
     (r'^\s*except\s*:', "naked except clause", r'#.*re-raises'),
+    (r'^\s*except\s([^\(,]+|\([^\)]+\))\s*,',
+     'legacy exception syntax; use "as" instead of ","'),
     (r':\n(    )*( ){1,3}[^ ]', "must indent 4 spaces"),
     (r'ui\.(status|progress|write|note|warn)\([\'\"]x',
      "missing _() in ui message (use () to hide false-positives)"),
