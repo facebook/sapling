d25ad15a41434d60533aa45105b78a7068bb4fc0
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-07-06 16:22:57 -0700

    forget: add a note to the command help about remove

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -3332,6 +3332,8 @@ def forget(ui, repo, *pats, **opts):
     entire project history, and it does not delete them from the
     working directory.
 
+    To delete the file from the working directory, see :hg:`remove`.
+
     To undo a forget before the next commit, see :hg:`add`.
 
     .. container:: verbose
