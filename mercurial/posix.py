180d1578ac1094facc7d081299d6980134177a18
timeless@mozdev.org <timeless@mozdev.org>
2015-09-04 11:15:02 -0400

    posix: shellquote do not require quoting for "+" (issue4818)

--- mercurial/posix.py
+++ mercurial/posix.py
@@ -350,7 +350,7 @@ def shellquote(s):
         return '"%s"' % s
     global _needsshellquote
     if _needsshellquote is None:
-        _needsshellquote = re.compile(r'[^a-zA-Z0-9._/-]').search
+        _needsshellquote = re.compile(r'[^a-zA-Z0-9._/+-]').search
     if s and not _needsshellquote(s):
         # "s" shouldn't have to be quoted
         return s
