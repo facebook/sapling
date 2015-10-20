921847dc208a9a83c557ea883aca609ff84ba164
Pierre-Yves David <pierre-yves.david@fb.com>
2015-10-20 15:53:07 +0200

    unbundle: use 'source' argument with applybundle
    This will let the hook get access to the 'source' value.

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -6566,7 +6566,7 @@ def unbundle(ui, repo, fname1, *fnames, **opts):
             if isinstance(gen, bundle2.unbundle20):
                 tr = repo.transaction('unbundle')
                 try:
-                    op = bundle2.applybundle(repo, gen, tr)
+                    op = bundle2.applybundle(repo, gen, tr, source='unbundle')
                     tr.close()
                 except error.BundleUnknownFeatureError as exc:
                     raise error.Abort(_('%s: unknown bundle feature, %s')
