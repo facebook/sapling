186105860d2edc0fc510a03d2d522cb79d47668e
Durham Goode <durham@fb.com>
2015-12-16 18:52:14 -0800

    sparse: don't stat .hg/sparse for every sparsematch call
    Summary:
    The old behavior would check that the cached sparse matcher was up to date by
    performing a checksum on the contents of the .hg/sparse and .hg/tempsparse
    files. This caused a lot of extra syscalls when sparse match was used inside a
    loop.
    
    The fix is to cache the .hg/sparse signature, and only refresh them when the
    lock is acquired (similar to how most caches in Mercurial already work).
    
    Test Plan: Ran the tests
    
    Reviewers: #sourcecontrol, ttung, mitrandir
    
    Reviewed By: mitrandir
    
    Differential Revision: https://phabricator.fb.com/D2758452
    
    Tasks: 9259321
    
    Signature: t1:2758452:1450317072:6b0a962d336eda616cb27ef1b5e761d5153f64a3

--- sparse.py
+++ sparse.py
@@ -397,6 +397,43 @@ def _wraprepo(ui, repo):
             fh = open(filepath)
             return util.sha1(fh.read()).hexdigest()
 
+        def _sparsesignature(self, includetemp=True):
+            """Returns the signature string representing the contents of the
+            current project sparse configuration. This can be used to cache the
+            sparse matcher for a given set of revs."""
+            signaturecache = self.signaturecache
+            signature = signaturecache.get('signature')
+            if includetemp:
+                tempsignature = signaturecache.get('tempsignature')
+            else:
+                tempsignature = 0
+
+            if signature is None or (includetemp and tempsignature is None):
+                signature = 0
+                try:
+                    sparsepath = self.opener.join('sparse')
+                    signature = self.sparsechecksum(sparsepath)
+                except (OSError, IOError):
+                    pass
+                signaturecache['signature'] = signature
+
+                tempsignature = 0
+                if includetemp:
+                    try:
+                        tempsparsepath = self.opener.join('tempsparse')
+                        tempsignature = self.sparsechecksum(tempsparsepath)
+                    except (OSError, IOError):
+                        pass
+                    signaturecache['tempsignature'] = tempsignature
+            return '%s %s' % (str(signature), str(tempsignature))
+
+        def invalidatecaches(self):
+            self.invalidatesignaturecache()
+            return super(SparseRepo, self).invalidatecaches()
+
+        def invalidatesignaturecache(self):
+            self.signaturecache.clear()
+
         def sparsematch(self, *revs, **kwargs):
             """Returns the sparse match function for the given revs.
 
@@ -410,23 +447,11 @@ def _wraprepo(ui, repo):
                 revs = [self.changelog.rev(node) for node in
                     self.dirstate.parents() if node != nullid]
 
-            signature = 0
-            try:
-                sparsepath = self.opener.join('sparse')
-                signature = self.sparsechecksum(sparsepath)
-            except (OSError, IOError):
-                pass
+            includetemp = kwargs.get('includetemp', True)
+            signature = self._sparsesignature(includetemp=includetemp)
 
-            tempsignature = 0
-            try:
-                if kwargs.get('includetemp', True):
-                    tempsparsepath = self.opener.join('tempsparse')
-                    tempsignature = self.sparsechecksum(tempsparsepath)
-            except (OSError, IOError):
-                pass
+            key = '%s %s' % (str(signature), ' '.join([str(r) for r in revs]))
 
-            key = '%s %s %s' % (str(signature), str(tempsignature),
-                ' '.join([str(r) for r in revs]))
             result = self.sparsecache.get(key, None)
             if result:
                 return result
@@ -490,6 +515,7 @@ def _wraprepo(ui, repo):
                 '\n'.join(sorted(include)),
                 '\n'.join(sorted(exclude)))
             self.opener.write("sparse", raw)
+            self.invalidatesignaturecache()
 
         def addtemporaryincludes(self, files):
             includes = self.gettemporaryincludes()
@@ -507,6 +533,7 @@ def _wraprepo(ui, repo):
         def _writetemporaryincludes(self, includes):
             raw = '\n'.join(sorted(includes))
             self.opener.write('tempsparse', raw)
+            self.invalidatesignaturecache()
 
         def prunetemporaryincludes(self):
             if repo.opener.exists('tempsparse'):
@@ -536,6 +563,7 @@ def _wraprepo(ui, repo):
                     dirstate.drop(file)
 
                 self.opener.unlink('tempsparse')
+                self.invalidatesignaturecache()
                 ui.status("cleaned up %d temporarily added file(s) from the sparse checkout\n" %
                     len(tempincludes))
 
@@ -543,6 +571,7 @@ def _wraprepo(ui, repo):
     if 'dirstate' in repo._filecache:
         repo.dirstate.repo = repo
     repo.sparsecache = {}
+    repo.signaturecache = {}
     repo.__class__ = SparseRepo
 
 @command('^sparse', [
