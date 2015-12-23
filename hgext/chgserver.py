1eaa3db7b3792c4b88d8abfe28587944c4979ee7
Yuya Nishihara <yuya@tcha.org>
2015-12-23 22:37:24 +0900

    chgserver: mark as a built-in extension

--- hgext/chgserver.py
+++ hgext/chgserver.py
@@ -48,6 +48,12 @@ from mercurial import (
     util,
 )
 
+# Note for extension authors: ONLY specify testedwith = 'internal' for
+# extensions which SHIP WITH MERCURIAL. Non-mainline extensions should
+# be specifying the version(s) of Mercurial they are tested with, or
+# leave the attribute unspecified.
+testedwith = 'internal'
+
 _log = commandserver.log
 
 # copied from hgext/pager.py:uisetup()
