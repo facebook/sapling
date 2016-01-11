728e89ef801a1da9ee603a0c09e5747216c93d8a
Laurent Charignon <lcharignon@fb.com>
2016-01-11 10:30:13 -0800

    phabstatus: make hg ssl very fast!
    Summary:
    For 10 diffs in fbsource
    
        hg ssl before the patch => 9s
        hg ssl after the patch => 1.5s
        hg sl => 1.2s
    
    This patch is a little tricky, we:
    - improve the method to get diff status by making it accept a list of diffnum
    - wrap smartlog's getdag method to store the smartlogrevs
    - add a hack around the memoization mechanism to memoize all individual calls
      for all the diffs when we make the call for all the diffs together (see code
      for details)
    - resolve all the phabricator revisions requested by smartlog at once when we
      are asked to resolve the first one
    
    Test Plan:
    hg sl does not take longer and works
    hg ssl is faster after the patch and produces the same output as before
    
    Reviewers: durham, rmcelroy, cdelahousse
    
    Subscribers: ssl-diffs@
    
    Differential Revision: https://phabricator.fb.com/D2818828

--- phabstatus.py
+++ phabstatus.py
@@ -5,27 +5,58 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-from mercurial import templatekw
+from mercurial import templatekw, extensions
 from mercurial import util as hgutil
 
 import re
+from pprint import pprint as pp
 import subprocess
 import os
 import json
 import logging
 
 def memoize(f):
+    """
+    NOTE: This is a hack
+    if f args are like (a, b1, b2, b3) and returns [o1, o2, o3] where
+    o1, o2, o3 are output of f respectively for (a, b1), (a, b2) and
+    (a, b3) then we memoize f(a, b1, b2, b3)'s result but also
+    f(a, b1) => o1 , f(a, b2) => o2 and f(a, b3) => o3.
+    Example:
+
+    >>> partialsum = lambda a, *b: [a + bn for bn in b]
+    >>> partialsum = memoize(partialsum)
+
+    Create a class that wraps the integer '3', otherwise we cannot add
+    _phabstatuscache to it for the test
+    >>> class IntWrapperClass(int):
+    ...     def __new__(cls, *args, **kwargs):
+    ...         return  super(IntWrapperClass, cls).__new__(cls, 3)
+
+    >>> three = IntWrapperClass()
+    >>> partialsum(three, 1, 2, 3)
+    [4, 5, 6]
+
+    As expected, we have 4 entries in the cache for a call like f(a, b, c, d)
+    >>> pp(three._phabstatuscache)
+    {(3, 1): [4], (3, 1, 2, 3): [4, 5, 6], (3, 2): [5], (3, 3): [6]}
+    """
     def helper(*args):
         repo = args[0]
         if not hgutil.safehasattr(repo, '_phabstatuscache'):
             repo._phabstatuscache = {}
         if args not in repo._phabstatuscache:
-            repo._phabstatuscache[args] = f(*args)
+            u = f(*args)
+            repo._phabstatuscache[args] = u
+            if isinstance(u, list):
+                revs = args[1:]
+                for x, r in enumerate(revs):
+                    repo._phabstatuscache[(repo, r)] = [u[x]]
         return repo._phabstatuscache[args]
     return helper
 
 @memoize
-def getdiffstatus(repo, diffid):
+def getdiffstatus(repo, *diffid):
     """Perform a Conduit API call by shelling out to `arc`
 
     Returns status of the diff"""
@@ -34,7 +65,7 @@ def getdiffstatus(repo, diffid):
         proc = subprocess.Popen(['arc', 'call-conduit', 'differential.query'],
                      stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                      preexec_fn=os.setsid)
-        input = json.dumps({'ids': [diffid]})
+        input = json.dumps({'ids': diffid})
         repo.ui.debug("[diffrev] echo '%s' | "
                       "arc call-conduit differential.query\n" %
                       input)
@@ -49,21 +80,54 @@ def getdiffstatus(repo, diffid):
         if not resp:
             error = jsresp.get('errorMessage', 'unknown error')
             return error
-        return resp[0].get('statusName')
+
+        # This makes the code more robust in case conduit does not return
+        # what we need
+        result = []
+        for diff in diffid:
+            matchingresponse = [r for r in resp
+                                  if r.get("id", None) == int(diff)]
+            if not matchingresponse:
+                result.append("Error")
+            else:
+                result.append(matchingresponse[0].get('statusName'))
+        return result
     except Exception as e:
         return 'Could not not call "arc call-conduit": %s' % e
 
 def showphabstatus(repo, ctx, templ, **args):
     """:phabstatus: String. Return the diff approval status for a given hg rev
     """
+    if hgutil.safehasattr(repo, '_smartlogrevs'):
+        alldiffnumbers = [getdiffnum(repo, repo[rev])
+                          for rev in repo._smartlogrevs]
+        okdiffnumbers = [d for d in alldiffnumbers if d is not None]
+        # To populate the cache, the result will be used by the templater
+        getdiffstatus(repo, *okdiffnumbers)
+        # Do this once per smartlog call, not for every revs to be displayed
+        del repo._smartlogrevs
+
+    diffnum = getdiffnum(repo, ctx)
+    if diffnum is not None:
+        return getdiffstatus(repo, diffnum)[0]
+
+def getdiffnum(repo, ctx):
     descr = ctx.description()
     match = re.search('Differential Revision: https://phabricator.fb.com/(D\d+)'
                       , descr)
     revstr = match.group(1) if match else ''
     if revstr.startswith('D') and revstr[1:].isdigit():
-        return getdiffstatus(repo, revstr[1:])
+        return revstr[1:]
+    return None
 
+def _getdag(orig, *args):
+    repo = args[1]
+    # We retain the smartlogrevision, this way showphabstatus knows that there
+    # are multiple revisions to resolve
+    repo._smartlogrevs = args[2]
+    return orig(*args)
 
 def extsetup(ui):
     templatekw.keywords['phabstatus'] = showphabstatus
-
+    smartlog = extensions.find("smartlog")
+    extensions.wrapfunction(smartlog, 'getdag', _getdag)
