e1c9a953b7507033ae353f022d52f0502fdaf81e
Mateusz Kwapich <mitrandir@fb.com>
2016-02-08 14:47:54 -0800

    pushrebase: change supportedversions to supportedoutgoingversions
    Summary: the api changed once again
    
    Test Plan: tests are passing now
    
    Reviewers: #sourcecontrol, ttung, durham
    
    Reviewed By: durham
    
    Differential Revision: https://phabricator.fb.com/D2913893
    
    Signature: t1:2913893:1454971559:052b0a2b20dfde337a74991f0ddb9498f08a9cb0

--- pushrebase.py
+++ pushrebase.py
@@ -417,7 +417,7 @@ def _addpushbackchangegroup(repo, reply, outgoing):
     cgversions = set(reply.capabilities.get('changegroup'))
     if not cgversions:
         cgversions.add('01')
-    version = max(cgversions & set(changegroup.supportedversions(repo)))
+    version = max(cgversions & set(changegroup.supportedoutgoingversions(repo)))
 
     cg = changegroup.getlocalchangegroupraw(repo,
                                             'rebase:reply',
