a36c36a4d9debd1ffd3bef1cdec753ed2ce7bed2
timeless <timeless@mozdev.org>
2016-03-01 09:59:58 +0000

    zeroconf: compare singleton using is

--- hgext/zeroconf/Zeroconf.py
+++ hgext/zeroconf/Zeroconf.py
@@ -1145,7 +1145,7 @@ class ServiceInfo(object):
                         value = 0
 
                 # Only update non-existent properties
-                if key and result.get(key) == None:
+                if key and result.get(key) is None:
                     result[key] = value
 
             self.properties = result
