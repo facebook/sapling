a0a844ca84971a73325ec1d495f78d51354a8647
Yuya Nishihara <yuya@tcha.org>
2016-04-05 23:17:43 +0900

    test-filelog: alias ui as uimod

--- tests/test-filelog.py
+++ tests/test-filelog.py
@@ -5,14 +5,14 @@ Tests the behavior of filelog w.r.t. data starting with '\1\n'
 from __future__ import absolute_import, print_function
 from mercurial import (
     hg,
-    ui,
+    ui as uimod,
 )
 from mercurial.node import (
     hex,
     nullid,
 )
 
-myui = ui.ui()
+myui = uimod.ui()
 repo = hg.repository(myui, path='.', create=True)
 
 fl = repo.file('foobar')
