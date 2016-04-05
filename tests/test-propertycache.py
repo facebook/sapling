c2a5918b06c4101226a58f5e6660e665dbd25452
Yuya Nishihara <yuya@tcha.org>
2016-04-05 23:33:55 +0900

    tests: remove unused import of mercurial.repoview from test-propertycache
    I don't see any reason to import it, but if there is a reason, please disregard
    this and the next patch.

--- tests/test-propertycache.py
+++ tests/test-propertycache.py
@@ -8,7 +8,6 @@ from __future__ import absolute_import, print_function
 import os
 import subprocess
 import mercurial.localrepo
-import mercurial.repoview
 import mercurial.util
 import mercurial.hg
 import mercurial.ui as uimod
