a23d34c3e4d48f767d854da32d7d9f5238588cfe
Tony Tung <tonytung@merly.org>
2016-04-08 22:34:43 -0700

    [fastmanifest] extract tree path traversal code to tree_path.[ch]
    Summary: I'm planning on building a find_path traversal that exposes directories, and make it available for tests.
    
    Test Plan: pass unit tests.
    
    Reviewers: #sourcecontrol, rmcelroy
    
    Reviewed By: rmcelroy
    
    Subscribers: rmcelroy, mitrandir, mjpieters
    
    Differential Revision: https://phabricator.fb.com/D3138945
    
    Signature: t1:3138945:1460071138:3eb43a1742c8b0d856f0b97421eb499d9e328310

---
+++ fastmanifest/tree_path.h
@@ -0,0 +1,75 @@
+// Copyright 2016-present Facebook. All Rights Reserved.
+//
+// tree_path.h: declarations for the core path function for parsing and
+//              traversing a path through a tree.
+//
+// no-check-code
+
+#ifndef __FASTMANIFEST_TREE_PATH_H__
+#define __FASTMANIFEST_TREE_PATH_H__
+
+#include "node.h"
+
+typedef enum {
+  TREE_ADD_CHILD_OK,
+  TREE_ADD_CHILD_OOM,
+  TREE_ADD_CHILD_WTF,
+} tree_add_child_code_t;
+typedef struct _tree_add_child_result_t {
+  tree_add_child_code_t code;
+  node_t *newroot;
+  node_t *newchild;
+} tree_add_child_result_t;
+
+typedef enum {
+  // walks the tree.  if the path cannot be found, exit with
+  // `FIND_PATH_NOT_FOUND`.
+      BASIC_WALK,
+
+  // walks the tree.  if the intermediate paths cannot be found, create them.
+  // if a leaf node exists where an intermediate path node needs to be
+  // created, then return `FIND_PATH_CONFLICT`.
+      CREATE_IF_MISSING,
+
+  // walks the tree.  if the path cannot be found, exit with
+  // `FIND_PATH_NOT_FOUND`.  if the operation is successful, then check
+  // intermediate nodes to ensure that they still have children.  any nodes
+  // that do not should be removed.
+      REMOVE_EMPTY_IMPLICIT_NODES,
+} find_path_operation_type;
+typedef enum {
+  FIND_PATH_OK,
+  FIND_PATH_NOT_FOUND,
+  FIND_PATH_OOM,
+  FIND_PATH_CONFLICT,
+  FIND_PATH_WTF,
+} find_path_result_t;
+typedef struct _find_path_callback_result_t {
+  find_path_result_t code;
+  node_t *newroot;
+} find_path_callback_result_t;
+
+extern tree_add_child_result_t tree_add_child(
+    tree_t *tree,
+    node_t *const root_parent,
+    node_t *root,
+    const char *name, const size_t name_sz,
+    tree_state_changes_t *changes);
+
+extern find_path_result_t find_path(
+    tree_t *tree,
+    node_t *const root_parent,
+    node_t *root,
+    const char *path, const size_t path_sz,
+    find_path_operation_type operation_type,
+    tree_state_changes_t *changes,
+    find_path_callback_result_t (*callback)(
+        tree_t *tree,
+        node_t *const dir_parent,
+        node_t *dir,
+        const char *path, const size_t path_sz,
+        tree_state_changes_t *changes,
+        void *context),
+    void *context);
+
+#endif // #ifndef __FASTMANIFEST_TREE_PATH_H__
