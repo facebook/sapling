a40ffad87d8f81d0efa965014ddabfd8ade569a4
Tony Tung <tonytung@merly.org>
2016-04-18 11:32:31 -0700

    [fastmanifest] add mechanism to peek at directories in tests
    Summary: I need this to be able to access directory nodes for checksum testing.
    
    Test Plan: passed unit tests
    
    Reviewers: lcharignon
    
    Reviewed By: lcharignon
    
    Subscribers: mitrandir, mjpieters, #sourcecontrol
    
    Differential Revision: https://phabricator.fb.com/D3140368
    
    Tasks: 10697482
    
    Signature: t1:3140368:1460584956:671cd08ca174795445a297ef75bee53d9f09a8ad

--- fastmanifest/tests.h
+++ fastmanifest/tests.h
@@ -1,6 +1,6 @@
 // Copyright 2016-present Facebook. All Rights Reserved.
 //
-// tests.h: convenience macros for unit tests.
+// tests.h: convenience functions for unit tests.
 //
 // no-check-code
 
@@ -10,11 +10,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "node.h"
+#include "result.h"
+#include "tree.h"
+
 #define ASSERT(cond) if (!(cond)) {             \
     printf("failed on line %d\n", __LINE__);    \
     exit(37);                                   \
   }
 
 #define STRPLUSLEN(__str__) __str__, strlen(__str__)
+
+typedef struct _get_path_unfiltered_result_t {
+  get_path_code_t code;
+  const node_t *node;
+} get_path_unfiltered_result_t;
+
+extern get_path_unfiltered_result_t get_path_unfiltered(
+    tree_t *const tree,
+    const char *path,
+    const size_t path_sz);
 
 #endif /* #ifndef __TESTLIB_TESTS_H__ */
