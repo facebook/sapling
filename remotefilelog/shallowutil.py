ead8969797984f5c619290be73e8ea3b3bef4bbd
Jun Wu <quark@fb.com>
2016-04-18 19:04:58 -0700

    Fix missing errno import
    Summary: Apparently we need to `import errno` in `shallowutil.py`
    
    Test Plan: Code Review
    
    Reviewers: #sourcecontrol, ttung, durham
    
    Reviewed By: durham
    
    Differential Revision: https://phabricator.fb.com/D3195117
    
    Signature: t1:3195117:1461031210:424912a96448a2a8cb37197f006cfa95d4ab1cb1

--- remotefilelog/shallowutil.py
+++ remotefilelog/shallowutil.py
@@ -5,7 +5,7 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-import os
+import errno, os
 from mercurial import filelog, util
 
 def interposeclass(container, classname):
