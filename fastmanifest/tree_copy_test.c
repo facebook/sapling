764465c51c451089a550719276df3c1fc5b3fecb
Tony Tung <tonytung@merly.org>
2016-05-18 17:34:05 -0700

    [fastmanifest] rename structures and macros
    Summary: I want to reuse these in other tests, so I don't want to imply they are only good for this test.
    
    Test Plan: run unit tests
    
    Reviewers: wez, lcharignon
    
    Reviewed By: lcharignon
    
    Subscribers: mitrandir, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3318288
    
    Signature: t1:3318288:1463603748:3dc18243f2150a71485a4be7c452542db1790c48

--- fastmanifest/tree_copy_test.c
+++ fastmanifest/tree_copy_test.c
@@ -46,9 +46,9 @@ typedef struct {
   size_t path_sz;
   uint8_t *checksum;
   uint8_t flags;
-} copy_normal_tree_data_t;
-#define COPY_NORMAL_TREE_DATA(path, checksum, flags)                      \
-  (copy_normal_tree_data_t)                                               \
+} copy_tree_data_t;
+#define COPY_TREE_DATA(path, checksum, flags)                             \
+  (copy_tree_data_t)                                                      \
     {                                                                     \
       path,                                                               \
       sizeof(path) - 1,                                                   \
@@ -57,25 +57,25 @@ typedef struct {
     }
 
 void test_copy_normal_tree() {
-  copy_normal_tree_data_t input[] = {
-      COPY_NORMAL_TREE_DATA("abc",
+  copy_tree_data_t input[] = {
+      COPY_TREE_DATA("abc",
           "\xe7\xf5\xdd\xad\x5e\x13\x86\x4e\x25\x30\x41\x3a\x69\x8e\x19\xd4\x25"
               "\xc8\x12\x02",
           0x23),
-      COPY_NORMAL_TREE_DATA("ab/cde",
+      COPY_TREE_DATA("ab/cde",
           "\x7c\x6a\x4b\x0a\x05\x91\x6c\x89\x9d\x8a\xe6\x38\xcf\x38\x93\x2e"
               "\x4f\x09\xed\x57",
           0x9b),
-      COPY_NORMAL_TREE_DATA("abcd/ef",
+      COPY_TREE_DATA("abcd/ef",
           "\x3e\x4d\xf1\xe0\x46\x4a\x3e\xb9\x6b\x8d\x55\x6c\x3b\x6b\x00\xee"
               "\x4f\x77\x71\x9e",
           0xda),
-      COPY_NORMAL_TREE_DATA("abcd/efg/hi",
+      COPY_TREE_DATA("abcd/efg/hi",
           "\x98\x2f\x46\x90\xfe\xc1\xbc\xe0\x8b\xf7\xa5\x47\x65\xe3\xf4\x16"
               "\x5b\xf4\xba\x7c",
           0x44),
   };
-  size_t input_sz = sizeof(input) / sizeof(copy_normal_tree_data_t);
+  size_t input_sz = sizeof(input) / sizeof(copy_tree_data_t);
   tree_t *src = alloc_tree();
 
   for (size_t ix = 0; ix < input_sz; ix++) {
