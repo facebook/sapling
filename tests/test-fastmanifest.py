b5e5af62d4cec33f8fb2820183c40e97ed861403
Tony Tung <tonytung@merly.org>
2016-05-25 13:23:19 -0700

    [fastmanifest] fix indentation of test-fastmanifest.py
    Summary: 4 space indents
    
    Test Plan: still passes
    
    Reviewers: lcharignon
    
    Reviewed By: lcharignon
    
    Subscribers: mitrandir, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3343502
    
    Signature: t1:3343502:1464134904:276c5de5ad70c22ac523725335f23668666d09b7

--- tests/test-fastmanifest.py
+++ tests/test-fastmanifest.py
@@ -9,27 +9,30 @@ from mercurial import util
 
 class HybridManifest(unittest.TestCase):
 
-  def test_wrap(self):
-    """If a new magic method is added to manifestdict, we want to make sure that
-       hybridmanifest implement it, this test validates that all magic methods
-       of manifestdict are implemented by hybridmanifest to avoid breakage in
-       prod"""
-    vfs = scmutil.vfs('')
-    hd = fastmanifest.hybridmanifest(None, vfs)
-    ismagic = lambda x: x.startswith("__") and x.endswith("__")
-    magicmethods = [k for k, v in  manifest.manifestdict.__dict__.items()
-                      if util.safehasattr(v, '__call__') and ismagic(k)]
-    for method in magicmethods:
-        assert util.safehasattr(hd, method),\
-            "%s missing in hybrid manifest" % method
+    def test_wrap(self):
+        """If a new magic method is added to manifestdict, we want to make sure
+        that hybridmanifest implement it, this test validates that all magic
+        methods of manifestdict are implemented by hybridmanifest to avoid
+        breakage in prod
 
-  def test_cachelimit(self):
-    cachealloc = fastmanifest.systemawarecachelimit.cacheallocation
-    GB = fastmanifest.GB
-    MB = fastmanifest.MB
-    assert cachealloc(0) == 0
-    assert cachealloc(120 * GB) == 6 * GB
-    assert abs(cachealloc(28 * GB) - 5.6 * GB) < 5 * MB
+        """
+        vfs = scmutil.vfs('')
+        hd = fastmanifest.hybridmanifest(None, vfs)
+        ismagic = lambda x: x.startswith("__") and x.endswith("__")
+        magicmethods = [k
+                        for k, v in manifest.manifestdict.__dict__.items()
+                        if util.safehasattr(v, '__call__') and ismagic(k)]
+        for method in magicmethods:
+                assert util.safehasattr(hd, method),\
+                        "%s missing in hybrid manifest" % method
+
+    def test_cachelimit(self):
+        cachealloc = fastmanifest.systemawarecachelimit.cacheallocation
+        GB = fastmanifest.GB
+        MB = fastmanifest.MB
+        assert cachealloc(0) == 0
+        assert cachealloc(120 * GB) == 6 * GB
+        assert abs(cachealloc(28 * GB) - 5.6 * GB) < 5 * MB
 
 if __name__ == "__main__":
     sys.path.insert(0, os.path.join(os.environ["TESTDIR"], ".."))
