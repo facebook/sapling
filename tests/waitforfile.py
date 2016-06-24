c89f8dcf7e11194019b00c23968267aa97b58858
Tony Tung <tonytung@merly.org>
2016-06-24 16:17:18 -0700

    [fastmanifest] add the ability for waitforfile.py to wait for a file to materialize
    Summary: --created is the opposite of --deleted.
    
    Test Plan: meh.  used in later diff.
    
    Reviewers: lcharignon, mjpieters
    
    Reviewed By: mjpieters
    
    Subscribers: mitrandir, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3478780
    
    Signature: t1:3478780:1466759469:47a3af68df2c8d550aa654f135d4f0fcf6df7851

--- tests/waitforfile.py
+++ tests/waitforfile.py
@@ -7,6 +7,10 @@ import time
 
 if __name__ == '__main__':
     parser = OptionParser()
+    parser.add_option('--created', dest='created',
+            type='string', default=[],
+            action='append',
+            help='wait for <FILE> to be created', metavar='FILE')
     parser.add_option('--deleted', dest='deleted',
             type='string', default=[],
             action='append',
@@ -23,14 +27,11 @@ if __name__ == '__main__':
 
     start = time.time()
     while options.max_time is None or time.time() < start + options.max_time:
-        for fpath in options.deleted:
-            if os.access(fpath, os.F_OK):
-                # still exists... :(
-                break
-        else:
-            # nothing still exists, yay!
-            sys.exit(0)
+        if (not all(os.access(f, os.F_OK) for f in options.created) or
+                any(os.access(f, os.F_OK) for f in options.deleted)):
+            time.sleep(options.sleep_interval_ms / 1000.0)
+            continue
 
-        time.sleep(options.sleep_interval_ms / 1000.0)
+        sys.exit(0)
 
     sys.exit(1)
