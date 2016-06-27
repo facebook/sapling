35add0f007ff6de44379f8fdfcb05e4dd6a74317
Caren Thomas <carenthomas@fb.com>
2016-06-27 11:58:55 -0700

    change eden cli to run with python 3
    Summary: Update eden/fs/cli directory to run with Python 3.
    
    Reviewed By: simpkins
    
    Differential Revision: D3479252
    
    fbshipit-source-id: 3e3dc023fc54e99b7839a1a4dc8605dd7ef2d8a3

--- eden/fs/cli/main.py
+++ eden/fs/cli/main.py
@@ -1,12 +1,11 @@
+#!/usr/bin/env python3
+#
 # Copyright (c) 2016, Facebook, Inc.
 # All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
 
 import argparse
 from eden.thrift import EdenNotRunningError
@@ -99,7 +98,7 @@ def _get_git_dir(path):
 def _get_git_commit(git_dir):
     cmd = ['git', '--git-dir', git_dir, 'rev-parse', 'HEAD']
     out = subprocess.check_output(cmd)
-    return out.strip()
+    return out.strip().decode('utf-8', errors='surrogateescape')
 
 
 def _get_hg_repo(path):
@@ -135,7 +134,7 @@ def _get_hg_commit(repo):
     env['HGPLAIN'] = '1'
     cmd = ['hg', '--cwd', repo, 'log', '-T{node}\\n', '-r.']
     out = subprocess.check_output(cmd, env=env)
-    return out.strip()
+    return out.strip().decode('utf-8', errors='surrogateescape')
 
 
 def do_health(args):
--- eden/fs/cli/util.py
+++ eden/fs/cli/util.py
@@ -1,14 +1,11 @@
+#!/usr/bin/env python3
+#
 # Copyright (c) 2016, Facebook, Inc.
 # All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 
 import time
 
