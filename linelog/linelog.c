6fa80e5ccac1aed2d487a8ac96624e2f30b91ebb
Jun Wu <quark@fb.com>
2016-07-26 16:58:19 +0100

    linelog: add empty source files
    Summary: Add (almost) empty `.c` and `.h` files with uninteresting headers.
    
    Test Plan: Code review
    
    Reviewers: #mercurial, mitrandir, ttung
    
    Reviewed By: mitrandir
    
    Subscribers: mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3620623
    
    Tasks: 12416202
    
    Signature: t1:3620623:1469560977:d05a2781499783a2238f602025e6765ad52c6f00

---
+++ linelog/linelog.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2016-present Facebook. All Rights Reserved.
+ *
+ * linelog.c: data structure tracking line changes
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2 or any later version.
+ */
+
+#include "linelog.h"
---
+++ linelog/linelog.h
@@ -0,0 +1,13 @@
+#ifndef LINELOG_H_ZUJREV4L
+#define LINELOG_H_ZUJREV4L
+
+/*
+ * Copyright 2016-present Facebook. All Rights Reserved.
+ *
+ * linelog.h: data structure tracking line changes
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2 or any later version.
+ */
+
+#endif /* end of include guard: LINELOG_H_ZUJREV4L */
