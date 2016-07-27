4f76b602ebabee8953541ddf6f4b6332ab52cf9a
Jun Wu <quark@fb.com>
2016-07-27 17:39:20 +0100

    linelog: declare data structures
    Summary:
    Add essential data structures for upcoming APIs.
    
    This is part of a rewrite of D3334518. The new API removes everything about
    I/O (aka. `stdio.h`) and makes it the caller's responsibility to prepare
    (`malloc` or `mmap`) and resize the memory buffer.
    
    This makes it possible to have a much shorter (thus easier to review) code
    more dedicated to core algorithm, as the following are no longer necessary:
    
    - I/O related APIs: `open`, `close`, `flush`, `saveas`
    - I/O error handling
    
    The error handling part is also changed that we now can use integer error
    numbers instead of a boolean value + an error string.
    
    Test Plan: `gcc -Wall -Wextra linelog.h`
    
    Reviewers: #mercurial, ttung, simonfar
    
    Reviewed By: simonfar
    
    Subscribers: durham, simonfar, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3628571
    
    Tasks: 12416202
    
    Signature: t1:3628571:1469643667:4fbf4fd7e624820ee6d8409e2067c8bd0e99a829

--- linelog/linelog.h
+++ linelog/linelog.h
@@ -10,4 +10,46 @@
  * GNU General Public License version 2 or any later version.
  */
 
+#include <stddef.h>
+#include <stdint.h>
+
+/* static assert sizeof(size_t) >= sizeof(uint32_t) */
+extern char linelog_assert_sizet_[1 / (sizeof(size_t) >= 4)];
+
+typedef uint32_t linelog_linenum; /* line number, starting from 0 */
+typedef uint32_t linelog_revnum; /* rev x is the only parent of rev x + 1 */
+typedef uint32_t linelog_offset; /* internal use, index of linelog_buf.data */
+
+typedef int linelog_result; /* return value of some apis */
+
+#define LINELOG_RESULT_OK (0) /* success */
+#define LINELOG_RESULT_ENOMEM (-1) /* failed to malloc or realloc */
+#define LINELOG_RESULT_EILLDATA (-2) /* illegal data, unexpected values */
+#define LINELOG_RESULT_EOVERFLOW (-3) /* hard limit exceeded */
+#define LINELOG_RESULT_ENEEDRESIZE (-4) /* buf.size should >= neededsize */
+
+/* main storage (memory buffer) for linelog data, allocated by caller
+   same on-disk and in-memory format, endianness-insensitive.
+   designed to be used with mmap for efficient updates. */
+typedef struct {
+	uint8_t *data; /* mmap-friendly, set by caller */
+	size_t size; /* bytes, set by caller */
+	size_t neededsize; /* set by callee on ENEEDRESIZE */
+} linelog_buf;
+
+/* an annotated line */
+typedef struct {
+	linelog_revnum rev; /* revision number at the first appearance */
+	linelog_linenum linenum; /* line number at the first appearance */
+	linelog_offset offset; /* internal use, index of linelog_buf.data */
+} linelog_lineinfo;
+
+/* annotate result, an dynamic array of linelog_lineinfo, allocated by callee
+   memset to 0 before use, call linelog_annotateresult_clear to free memory */
+typedef struct {
+	linelog_lineinfo *lines;
+	linelog_linenum linecount;
+	linelog_linenum maxlinecount;
+} linelog_annotateresult;
+
 #endif /* end of include guard: LINELOG_H_ZUJREV4L */
