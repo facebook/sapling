f918aa6566b8a9730eb7d72eea4d2fa85c8f894c
Jun Wu <quark@fb.com>
2016-07-29 00:49:38 +0100

    linelog: declare core APIs
    Summary:
    Declare core APIs for linelog:
    - `annotate`, the "read" part
    - `replacelines`, the "write" part
    
    As mentioned in D3334518, this is part of a rewrite of D3334518.
    These 2 APIs are the only ones involving core logic of the linelog structure.
    
    Test Plan: `gcc -Wall -Wextra -Wconversion linelog.h`
    
    Reviewers: #mercurial, zamsden, ttung, simonfar
    
    Reviewed By: simonfar
    
    Subscribers: akushner, zamsden, simonfar, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3628848
    
    Tasks: 12416202
    
    Signature: t1:3628848:1470080022:1f434d998c0bf63a494f70d47bc6a6fc05954446

--- linelog/linelog.h
+++ linelog/linelog.h
@@ -65,4 +65,82 @@ size_t linelog_getactualsize(const linelog_buf *buf);
    return 0 if buf is not initialized (by linelog_clear). */
 linelog_revnum linelog_getmaxrev(const linelog_buf *buf);
 
+/* note: some notations are from Python:
+   - range(p, q) means from p (inclusive) to q (exclusive), p <= q
+   - array[p:q] means a slice of the array with indexes in range(p, q) */
+
+/* calculate annotateresult for rev from buf, output result to ar
+
+   on success, let i be the line number at rev, in range(0, ar->linecount),
+     ar->lines[i].rev is the number of the revision introducing the line
+     ar->lines[i].linenum is the corresponding line number at ar->lines[i].rev
+
+   on error, ar may be in an invalid state and needs to be cleared */
+linelog_result linelog_annotate(const linelog_buf *buf,
+		linelog_annotateresult *ar, linelog_revnum rev);
+
+/* update buf and ar, replace existing lines[a1:a2] with lines[b1:b2] in brev
+
+   ar should be obtained using linelog_annotate(brev).
+   brev introduces the change. the change is not present in earlier revisions.
+
+   usually brev is greater than maxrev to do incremental updates, like:
+     rev = linelog_getmaxrev(buf)
+     linelog_annotate(buf, rev, ar)
+     for-each-new-rev {
+       rev += 1
+       // no need to run linelog_annotate(buf, rev, ar) again, because
+       // linelog_replacelines will keep it updated
+       for-each-chunk {
+         linelog_replacelines(buf, ar, rev, ...)
+       }
+     }
+
+   however, it's also possible to edit previous revisions, but be sure to use
+   the corresponding ar, obtained by calling linelog_annotate(brev).
+
+   on error, ar may be in an invalid state and needs to be cleared */
+linelog_result linelog_replacelines(linelog_buf *buf,
+		linelog_annotateresult *ar, linelog_revnum brev,
+		linelog_linenum a1, linelog_linenum a2,
+		linelog_linenum b1, linelog_linenum b2);
+
+/* like linelog_replacelines, but control details about lines being inserted
+
+   line numbers and revision numbers are decided by blinenums and brevs.
+   this table shows the difference from linelog_replacelines:
+
+   # | linelog_replacelines | linelog_replacelines_vec
+     | revnum, linenum      | revnum, linenum
+   --+----------------------+----------------------------------------------
+   0 | rev, b1              | brevs[0], blinenums[0]
+   1 | rev, b1+1            | brevs[1], blinenums[1]
+   . |                      |
+   . | rev, b2-1            | brevs[blinecount-1], blinenums[blinecount-1]
+
+   note: although lines can have revision numbers other than brev, they are
+   still marked as introduced by brev. i.e. visible to brev and later
+   revisions, invisible to earlier revisions.
+
+   this is useful for merge commits. consider the following case where rev 3
+   merges rev 1 and 2:
+
+            2        : feature branch
+           / \
+     0 -- 1 - 3 --   : main branch
+
+   a typical "annotate" operation running at rev 3 would show rev 1 and 2 but
+   hide rev 3 if the merge is clean.
+
+   linelog can only store linear history. typically it only tracks the main
+   branch thus rev 2 won't get stored. when introducing rev 3 (brev = 3),
+   individual lines can have different revisions (brevs[i] != 3) so
+   linelog_annotate(rev=3) works as if rev 2 is stored. be aware that
+   linelog_annotate(rev=2) will be the same as linelog_annotate(rev=1). */
+linelog_result linelog_replacelines_vec(linelog_buf *buf,
+		linelog_annotateresult *ar, linelog_revnum brev,
+		linelog_linenum a1, linelog_linenum a2,
+		linelog_linenum blinecount, const linelog_revnum *brevs,
+		const linelog_linenum *blinenums);
+
 #endif /* end of include guard: LINELOG_H_ZUJREV4L */
