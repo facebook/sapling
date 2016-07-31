7d27fc4948e82316534e28b83ad099be0775268b
FUJIWARA Katsunori <foozy@lares.dti.ne.jp>
2016-08-01 06:08:26 +0900

    doc: use field rst syntax to show keywords in debugdeltachain help correctly
    List of available keywords is well formatted as a list of fields in
    doc string, but is formatted as just normal text in online help
    output.

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -2615,23 +2615,23 @@ def debugdeltachain(ui, repo, file_=None, **opts):
 
     Output can be templatized. Available template keywords are:
 
-       rev          revision number
-       chainid      delta chain identifier (numbered by unique base)
-       chainlen     delta chain length to this revision
-       prevrev      previous revision in delta chain
-       deltatype    role of delta / how it was computed
-       compsize     compressed size of revision
-       uncompsize   uncompressed size of revision
-       chainsize    total size of compressed revisions in chain
-       chainratio   total chain size divided by uncompressed revision size
+    :``rev``:       revision number
+    :``chainid``:   delta chain identifier (numbered by unique base)
+    :``chainlen``:  delta chain length to this revision
+    :``prevrev``:   previous revision in delta chain
+    :``deltatype``: role of delta / how it was computed
+    :``compsize``:  compressed size of revision
+    :``uncompsize``: uncompressed size of revision
+    :``chainsize``: total size of compressed revisions in chain
+    :``chainratio``: total chain size divided by uncompressed revision size
                     (new delta chains typically start at ratio 2.00)
-       lindist      linear distance from base revision in delta chain to end
+    :``lindist``:   linear distance from base revision in delta chain to end
                     of this revision
-       extradist    total size of revisions not part of this delta chain from
+    :``extradist``: total size of revisions not part of this delta chain from
                     base of delta chain to end of this revision; a measurement
                     of how much extra data we need to read/seek across to read
                     the delta chain for this revision
-       extraratio   extradist divided by chainsize; another representation of
+    :``extraratio``: extradist divided by chainsize; another representation of
                     how much unrelated data is needed to load this delta chain
     """
     r = cmdutil.openrevlog(repo, 'debugdeltachain', file_, opts)
