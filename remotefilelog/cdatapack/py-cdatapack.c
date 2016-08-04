a28137668e714cf0a3946076d82e88a76537a389
Tony Tung <tonytung@merly.org>
2016-08-04 13:48:04 -0700

    [cdatapack] skeleton for iterator type
    Summary: cdatapack now has a getiter function, and it returns a cdatapack_iterator.
    
    Test Plan:
    using this toy program, dumped a pack file.
    
    ```
    #!/usr/bin/env python
    
    import cdatapack
    
    a = cdatapack.datapack('d864669a5651d04505ec6e5e9dba1319cde71f7b')
    for x in a:
        print x
    ```
    
    Reviewers: #fastmanifest, durham
    
    Reviewed By: durham
    
    Subscribers: durham, mitrandir
    
    Differential Revision: https://phabricator.intern.facebook.com/D3659005
    
    Signature: t1:3659005:1470339657:aa39cc57a669b9bc4604933ce35ed20b3f81b468

--- remotefilelog/cdatapack/py-cdatapack.c
+++ remotefilelog/cdatapack/py-cdatapack.c
@@ -14,6 +14,10 @@
 #define DATAIDX_EXT  ".dataidx"
 #define DATAPACK_EXT ".datapack"
 
+// ====  Forward declarations ====
+
+static PyTypeObject cdatapack_iterator_type;
+
 // ====  py_cdatapack PyObject declaration ====
 
 typedef struct {
@@ -22,6 +26,16 @@ typedef struct {
   bool initialized;
   datapack_handle_t *handle;
 } py_cdatapack;
+
+// ====  py_cdatapack_iterator PyObject declaration ====
+
+typedef struct {
+  PyObject_HEAD;
+
+  py_cdatapack *datapack;
+  const uint8_t *ptr;
+  const uint8_t *end;
+} py_cdatapack_iterator;
 
 /**
  * Initializes a cdatapack
@@ -39,6 +53,9 @@ static int cdatapack_init(py_cdatapack *self, PyObject *args) {
   char idx_path[nodelen + sizeof(DATAIDX_EXT)];
   char data_path[nodelen + sizeof(DATAPACK_EXT)];
 
+  sprintf(idx_path, "%s%s", node, DATAIDX_EXT);
+  sprintf(data_path, "%s%s", node, DATAPACK_EXT);
+
   self->handle = open_datapack(
       idx_path, strlen(idx_path),
       data_path, strlen(data_path));
@@ -55,6 +72,27 @@ static void cdatapack_dealloc(py_cdatapack *self) {
     close_datapack(self->handle);
   }
   PyObject_Del(self);
+}
+
+/**
+ * Returns an iterator for a cdatapack.
+ */
+static py_cdatapack_iterator *cdatapack_getiter(py_cdatapack *self) {
+  py_cdatapack_iterator *iterator;
+
+  iterator = PyObject_New(py_cdatapack_iterator, &cdatapack_iterator_type);
+  if (iterator == NULL) {
+    return NULL;
+  }
+
+  iterator->datapack = self;
+  Py_INCREF(iterator->datapack);
+  /* TODO: should have a data_version type and use sizeof(..) */
+  iterator->ptr = ((uint8_t *) self->handle->data_mmap) + 1;
+  iterator->end = ((uint8_t *) self->handle->data_mmap) +
+                  self->handle->data_file_sz;
+
+  return iterator;
 }
 
 // ====  cdatapack ctype declaration ====
@@ -90,7 +128,7 @@ static PyTypeObject cdatapack_type = {
   0,                                    /* tp_clear */
   0,                                    /* tp_richcompare */
   0,                                    /* tp_weaklistoffset */
-  0,                                    /* tp_iter */
+  (getiterfunc) cdatapack_getiter,      /* tp_iter */
   0,                                    /* tp_iternext */
   cdatapack_methods,                    /* tp_methods */
   0,                                    /* tp_members */
@@ -102,6 +140,79 @@ static PyTypeObject cdatapack_type = {
   0,                                    /* tp_dictoffset */
   (initproc)cdatapack_init,             /* tp_init */
   0,                                    /* tp_alloc */
+};
+
+// ====  cdatapack_iterator class methods ====
+
+/**
+ * Deallocates a cdatapack iterator
+ */
+static void cdatapack_iterator_dealloc(py_cdatapack_iterator *self) {
+  Py_XDECREF(self->datapack);
+  PyObject_Del(self);
+}
+
+/**
+ * Yields the next item from the iterator.
+ */
+static PyObject *cdatapack_iterator_iternext(py_cdatapack_iterator *iterator) {
+  delta_chain_link_t link;
+
+  if (iterator->ptr >= iterator->end) {
+    return NULL;
+  }
+
+  iterator->ptr = getdeltachainlink(iterator->ptr, &link);
+
+  PyObject *tuple = NULL, *fn = NULL, *node = NULL;
+
+  fn = PyString_FromStringAndSize(link.filename, link.filename_sz);
+  node = PyString_FromStringAndSize((const char *) link.node, NODE_SZ);
+  if (fn == NULL || node == NULL) {
+    goto cleanup;
+  }
+  tuple = PyTuple_Pack(2, fn, node);
+
+cleanup:
+
+  Py_XDECREF(fn);
+  Py_XDECREF(node);
+
+  return tuple;
+}
+
+// ====  cdatapack_iterator ctype declaration ====
+
+static PyTypeObject cdatapack_iterator_type = {
+  PyObject_HEAD_INIT(NULL)
+  0,                                    /* ob_size */
+  "cdatapack.datapack.iterator",        /* tp_name */
+  sizeof(py_cdatapack_iterator),        /* tp_basicsize */
+  0,                                    /* tp_itemsize */
+  (destructor)cdatapack_iterator_dealloc, /* tp_dealloc */
+  0,                                    /* tp_print */
+  0,                                    /* tp_getattr */
+  0,                                    /* tp_setattr */
+  0,                                    /* tp_compare */
+  0,                                    /* tp_repr */
+  0,                                    /* tp_as_number */
+  0,                                    /* tp_as_sequence - length/contains */
+  0,                                    /* tp_as_mapping - getitem/setitem*/
+  0,                                    /* tp_hash */
+  0,                                    /* tp_call */
+  0,                                    /* tp_str */
+  0,                                    /* tp_getattro */
+  0,                                    /* tp_setattro */
+  0,                                    /* tp_as_buffer */
+
+  Py_TPFLAGS_DEFAULT,                   /* tp_flags */
+  "Iterator for entries-tuples in a datapack.", /* tp_doc */
+  0,                                    /* tp_traverse */
+  0,                                    /* tp_clear */
+  0,                                    /* tp_richcompare */
+  0,                                    /* tp_weaklistoffset */
+  PyObject_SelfIter,                    /* tp_iter: __iter__() method */
+  (iternextfunc) cdatapack_iterator_iternext, /* tp_iternext: next() method */
 };
 
 static PyMethodDef mod_methods[] = {
