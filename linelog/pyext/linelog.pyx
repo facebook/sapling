c14c36aa26639d989b2f4742a6faff7bf9163c30
Jun Wu <quark@fb.com>
2016-08-10 21:15:43 +0100

    linelog.pyx: implement file buffer
    Summary: The file buffer uses `mmap` for `linelog_buf::data`.
    
    Test Plan: `cython2 linelog.pyx`
    
    Reviewers: #mercurial, rmcelroy, ttung
    
    Reviewed By: rmcelroy
    
    Subscribers: rmcelroy, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3662790
    
    Signature: t1:3662790:1470860125:b68fc5e3ed14d56fbebe8a7de81ad7f67a622875

--- linelog/pyext/linelog.pyx
+++ linelog/pyext/linelog.pyx
@@ -1,7 +1,11 @@
+from libc.errno cimport errno
 from libc.stdint cimport uint32_t, uint8_t
 from libc.stdlib cimport free, realloc
-from libc.string cimport memcpy, memset
-from posix cimport unistd
+from libc.string cimport memcpy, memset, strdup
+from posix cimport fcntl, mman, stat, unistd
+from posix.types cimport off_t
+
+import os
 
 cdef extern from "../linelog.c":
     # (duplicated) declarations for Cython, as Cython cannot parse .h file
@@ -138,3 +142,87 @@ cdef class _memorybuffer(_buffer): # linelog_buf backed by memory
             raise LinelogError(LINELOG_RESULT_ENOMEM)
         self.buf.data = <uint8_t *>p
         self.buf.size = newsize
+
+cdef _excwitherrno(exctype, hint=None, filename=None):
+    # like PyErr_SetFromErrno-family APIs but we avoid CPython APIs here
+    # (so one day if Cython writes pypy/cffi code, this can be used as-is)
+    message = os.strerror(errno)
+    if hint is not None:
+        message += ' (' + hint + ')'
+    return exctype(errno, message, filename)
+
+cdef class _filebuffer(_buffer): # linelog_buf backed by filesystem
+    cdef int fd
+    cdef size_t maplen
+    cdef char *path
+
+    def __cinit__(self, path):
+        self.fd = -1
+        self.maplen = 0
+        self.path = strdup(path)
+        if self.path == NULL:
+            raise MemoryError()
+        self._open()
+
+    def __dealloc__(self):
+        free(self.path)
+        self.close()
+
+    cdef resize(self, size_t newsize):
+        if self.fd == -1:
+            self._open()
+        self._unmap()
+        unistd.ftruncate(self.fd, <off_t>newsize)
+        self._map()
+
+    cdef flush(self):
+        if self.buf.data == NULL:
+            return
+        r = mman.msync(self.buf.data, self.buf.size, mman.MS_ASYNC)
+        if r != 0:
+            raise _excwitherrno(OSError, 'msync')
+
+    cdef close(self):
+        self.flush()
+        self._unmap()
+        if self.fd == -1:
+            return
+        unistd.close(self.fd)
+        self.fd = -1
+
+    cdef _open(self):
+        self.close()
+        fd = fcntl.open(self.path, fcntl.O_RDWR | fcntl.O_CREAT, 0o644)
+        if fd == -1:
+            raise _excwitherrno(IOError, None, self.path)
+        self.fd = fd
+        self._map()
+
+    cdef _map(self):
+        assert self.fd != -1
+        self._unmap()
+
+        cdef stat.struct_stat st
+        r = stat.fstat(self.fd, &st)
+        if r != 0:
+            raise _excwitherrno(IOError, 'fstat')
+
+        cdef size_t filelen = <size_t>st.st_size
+        self.maplen = (1 if filelen == 0 else filelen) # cannot be 0
+        p = mman.mmap(NULL, self.maplen, mman.PROT_READ | mman.PROT_WRITE,
+                      mman.MAP_SHARED, self.fd, 0)
+        if p == NULL:
+            raise _excwitherrno(OSError, 'mmap')
+
+        self.buf.data = <uint8_t *>p
+        self.buf.size = filelen
+
+    cdef _unmap(self):
+        if self.buf.data == NULL:
+            return
+        r = mman.munmap(self.buf.data, self.maplen)
+        if r != 0:
+            raise _excwitherrno(OSError, 'munmap')
+        memset(&self.buf, 0, sizeof(linelog_buf))
+        self.maplen = 0
+
