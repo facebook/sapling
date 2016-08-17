3f8e8b7f90f70e0d446b71cff0c8ecfb006846d0
Durham Goode <durham@fb.com>
2016-08-17 13:25:13 -0700

    manifest: change changectx to access manifest via manifestlog
    This is the first place where we'll start using manifestctx instances instead of
    manifestdict. This will facilitate using different manifestctx implementations
    in the future.

--- mercurial/context.py
+++ mercurial/context.py
@@ -528,7 +528,7 @@ class changectx(basectx):
 
     @propertycache
     def _manifest(self):
-        return self._repo.manifest.read(self._changeset.manifest)
+        return self._repo.manifestlog[self._changeset.manifest]
 
     @propertycache
     def _manifestdelta(self):
