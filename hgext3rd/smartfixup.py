0087ee33bf2aef2c9a3246dff3cfc4b94afddbfd
Jun Wu <quark@fb.com>
2016-08-23 15:18:36 +0100

    smartfixup: implement fixupstate
    Summary:
    If `filefixupstate` is to `filecontext`, `fixupstate` is to `context`.
    This does the actual committing, creating obsmarkers, moving bookmarks work.
    
    Test Plan: A new test will be included in the next diff
    
    Reviewers: #mercurial, ttung, durham
    
    Reviewed By: durham
    
    Subscribers: durham, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3716954
    
    Signature: t1:3716954:1471639171:9891a0c64365b0592f5a4d86e3c83d521b4c5bb2

--- hgext3rd/smartfixup.py
+++ hgext3rd/smartfixup.py
@@ -371,3 +371,241 @@ class filefixupstate(object):
                 pcurrentchunk[0] = list(chunk)
         pushchunk()
         return result
+
+class fixupstate(object):
+    """state needed to run smartfixup
+
+    internally, it keeps paths and filefixupstates.
+
+    a typical use is like filefixupstates:
+
+        1. call diffwith, to calculate fixups
+        2. (optionally), present fixups to the user, or edit fixups
+        3. call apply, to apply changes to memory
+        4. call commit, to commit changes to hg database
+    """
+
+    def __init__(self, stack, ui=None):
+        """([ctx], ui or None) -> None
+
+        stack: should be linear, and sorted by topo order - oldest first.
+        all commits in stack are considered mutable.
+        """
+        assert stack
+        self.ui = ui or nullui()
+        self.stack = stack
+        self.repo = stack[-1].repo().unfiltered()
+
+        # following fields will be filled later
+        self.paths = [] # [str]
+        self.fixupmap = {} # {path: filefixupstate}
+        self.replacemap = {} # {oldnode: newnode or None}
+        self.finalnode = None # head after all fixups
+
+    def diffwith(self, destctx, match=None):
+        """diff and prepare fixups. update self.fixupmap, self.paths"""
+        # only care about modified files
+        self.paths = self.stack[-1].status(targetctx, match).modified
+        # prepare the filefixupstate
+        pctx = self.stack[0].p1()
+        for path in self.paths:
+            if self.ui.debugflag:
+                self.ui.write(_('calculating fixups for %s\n') % path)
+            targetfctx = targetctx[path]
+            fctxs = uniq(ctx[path] for ctx in self.stack if path in ctx)
+            # ignore symbolic links or binary files
+            if any(f.islink() or util.binary(f.data())
+                   for f in [targetfctx] + fctxs):
+                continue
+            if targetfctx.data() == fctxs[-1].data():
+                continue
+            # insert an immutable (public) fctx at the beginning
+            if path in pctx:
+                fctxs.insert(0, pctx[path])
+            else:
+                fctxs.insert(0, emptyfilecontext())
+            fstate = filefixupstate(fctxs, ui=self.ui)
+            fstate.diffwith(destctx[path])
+            self.fixupmap[path] = fstate
+
+    def apply(self):
+        """apply fixups to individual filefixupstates"""
+        for path, state in self.fixupmap.iteritems():
+            if self.ui.debugflag:
+                self.ui.write(_('applying fixups to %s\n') % path)
+            state.apply()
+
+    @property
+    def chunkstats(self):
+        """-> {path: chunkstats}. collect chunkstats from filefixupstates"""
+        return dict((path, state.chunkstats)
+                    for path, state in self.fixupmap.iteritems())
+
+    def commit(self):
+        """commit changes. update self.finalnode, self.replacemap"""
+        with self.repo.wlock(): # update bookmarks
+            with self.repo.lock(): # commit
+                with self.repo.transaction('smartfixup') as tr:
+                    self._commitstack()
+                    self._movebookmarks(tr)
+                    if self.repo['.'].node() in self.replacemap:
+                        self._moveworkingdirectoryparent()
+                    if self._useobsolete:
+                        self._obsoleteoldcommits()
+                if not self._useobsolete: # strip must be outside transactions
+                    self._stripoldcommits()
+        return self.finalnode
+
+    def printchunkstats(self):
+        """print things like '1 of 2 chunks(s) applied'"""
+        ui = self.ui
+        chunkstats = self.chunkstats
+        if ui.verbose:
+            # chunkstats for each file
+            for path, stat in chunkstats.iteritems():
+                if stat[0]:
+                    ui.write(_('%s: %d of %d chunk(s) applied\n')
+                             % (path, stat[0], stat[1]))
+        elif not ui.quiet:
+            # a summary for all files
+            stats = chunkstats.values()
+            applied, total = (sum(s[i] for s in stats) for i in (0, 1))
+            ui.write(_('%d of %d chunks(s) applied\n') % (applied, total))
+
+    def _commitstack(self):
+        """make new commits. update self.finalnode, self.replacemap.
+        it is splitted from "commit" to avoid too much indentation.
+        """
+        # last node (20-char) committed by us
+        lastcommitted = None
+        # p1 which overrides the parent of the next commit, "None" means use
+        # the original parent unchanged
+        nextp1 = None
+        for ctx in self.stack:
+            memworkingcopy = self._getnewfilecontents(ctx)
+            if not memworkingcopy and not lastcommitted:
+                # nothing changed, nothing commited
+                nextp1 = ctx
+                continue
+            msg = ''
+            if self._willbecomenoop(memworkingcopy, ctx, nextp1):
+                # changeset is no longer necessary
+                self.replacemap[ctx.node()] = None
+                msg = _('became empty and was dropped')
+            else:
+                # changset needs re-commit
+                nodestr = self._commitsingle(memworkingcopy, ctx, p1=nextp1)
+                lastcommitted = self.repo[nodestr]
+                nextp1 = lastcommitted
+                self.replacemap[ctx.node()] = lastcommitted.node()
+                if memworkingcopy:
+                    msg = _('%d file(s) changed, became %s') % (
+                        len(memworkingcopy), self._ctx2str(lastcommitted))
+                else:
+                    msg = _('became %s') % self._ctx2str(lastcommitted)
+            if self.ui.verbose and msg:
+                self.ui.write(_('%s: %s\n') % (self._ctx2str(ctx), msg))
+        self.finalnode = lastcommitted and lastcommitted.node()
+
+    def _ctx2str(self, ctx):
+        if self.ui.debugflag:
+            return ctx.hex()
+        else:
+            return node.short(ctx.node())
+
+    def _getnewfilecontents(self, ctx):
+        """(ctx) -> {path: str}
+
+        fetch file contents from filefixupstates.
+        return the working copy overrides - files different from ctx.
+        """
+        result = {}
+        for path in self.paths:
+            if path not in ctx:
+                continue
+            fctx = ctx[path]
+            content = fctx.data()
+            newcontent = self.fixupmap[path].getfinalcontent(fctx)
+            if content != newcontent:
+                result[path] = newcontent
+        return result
+
+    def _movebookmarks(self, tr):
+        repo = self.repo
+        needupdate = [(name, self.replacemap[hsh])
+                      for name, hsh in repo._bookmarks.iteritems()
+                      if hsh in self.replacemap]
+        for name, hsh in needupdate:
+            if hsh:
+                repo._bookmarks[name] = hsh
+                if self.ui.verbose:
+                    self.ui.write(_('moving bookmark %s to %s\n')
+                                  % (name, node.hex(hsh)))
+            else:
+                del repo._bookmarks[name]
+                if self.ui.verbose:
+                    self.ui.write(_('deleting bookmark %s\n') % name)
+        repo._bookmarks.recordchange(tr)
+
+    def _moveworkingdirectoryparent(self):
+        ctx = self.repo[self.finalnode]
+        self.repo.dirstate.beginparentchange()
+        try:
+            self.repo.dirstate.rebuild(ctx.node(), ctx.manifest(), self.paths)
+        finally:
+            self.repo.dirstate.endparentchange()
+
+    @staticmethod
+    def _willbecomenoop(memworkingcopy, ctx, pctx=None):
+        """({path: content}, ctx, ctx) -> bool. test if a commit will be noop
+
+        if it will become an empty commit (does not change anything, after the
+        memworkingcopy overrides), return True. otherwise return False.
+        """
+        if not pctx:
+            parents = ctx.parents()
+            if len(parents) != 1:
+                return False
+            pctx = parents[0]
+        if set(ctx.files()) != set(memworkingcopy.iterkeys()):
+            return False
+        for path, content in memworkingcopy.iteritems():
+            if path not in pctx or path not in ctx:
+                return False
+            fctx = ctx[path]
+            pfctx = pctx[path]
+            if pfctx.flags() != fctx.flags():
+                return False
+            if pfctx.data() != content:
+                return False
+        return True
+
+    def _commitsingle(self, memworkingcopy, ctx, p1=None):
+        """(ctx, {path: content}, node) -> node. make a single commit
+
+        the commit is a clone from ctx, with a (optionally) different p1, and
+        different file contents replaced by memworkingcopy.
+        """
+        parents = p1 and (p1, node.nullid)
+        extra = ctx.extra()
+        if self._useobsolete and self.ui.configbool('smartfixup', 'addnoise',
+                                                    True):
+            extra['smartfixup_source'] = ctx.hex()
+        mctx = overlaycontext(memworkingcopy, ctx, parents, extra=extra)
+        return mctx.commit()
+
+    @util.propertycache
+    def _useobsolete(self):
+        """() -> bool"""
+        return obsolete.isenabled(self.repo, obsolete.createmarkersopt)
+
+    def _obsoleteoldcommits(self):
+        relations = [(self.repo[k], v and (self.repo[v],) or ())
+                     for k, v in self.replacemap.iteritems()]
+        if relations:
+            obsolete.createmarkers(self.repo, relations)
+
+    def _stripoldcommits(self):
+        nodelist = self.replacemap.keys()
+        if nodelist:
+            repair.strip(self.repo.ui, self.repo, nodelist)
