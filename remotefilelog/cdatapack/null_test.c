79f8190f51c3a2525b2c570444ebadabf8423796
Tony Tung <tonytung@merly.org>
2016-08-26 17:15:06 -0700

    [cdatapack] remove null test
    Summary: No longer needed.
    
    Test Plan: make local && clion build && make unit tests.
    
    Reviewers: #fastmanifest, durham
    
    Reviewed By: durham
    
    Subscribers: mitrandir, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3780786
    
    Signature: t1:3780786:1472255290:cdf238f772234f8a2e15d69dca6489b6b977511d

--- remotefilelog/cdatapack/null_test.c
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-present Facebook. All Rights Reserved.
-//
-// null_test.c: garbage test to make CLion happy.
-//
-// no-check-code
-
-int main(int argc, char *argv[]) {
-  return 0;
-}
