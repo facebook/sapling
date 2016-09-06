f17f92c8a0cd12e4ebfe7144663445eb4806c9cc
Tony Tung <ttung@fb.com>
2016-09-06 12:10:35 -0700

    [cfastmanifest] move Find structures to the top of the file
    Summary: 1 pass compiler!
    
    Test Plan: make local
    
    Reviewers: #fastmanifest, durham
    
    Reviewed By: durham
    
    Subscribers: mitrandir, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3800784
    
    Signature: t1:3800784:1472758844:2cb86d11f422837e88230821b77c802def049e32

--- remotefilelog/ctreemanifest/treemanifest.h
+++ remotefilelog/ctreemanifest/treemanifest.h
@@ -17,6 +17,80 @@
 
 #include "manifest_fetcher.h"
 
+enum FindResult {
+  FIND_PATH_OK,
+  FIND_PATH_NOT_FOUND,
+  FIND_PATH_CONFLICT,
+  FIND_PATH_WTF,
+};
+
+enum FindMode {
+  // walks the tree and searches for a leaf node.  if the path cannot be found,
+  // exit with `FIND_PATH_NOT_FOUND`.
+      BASIC_WALK,
+
+  // walks the tree.  if the intermediate paths cannot be found, create them.
+  // if a leaf node exists where an intermediate path node needs to be
+  // created, then return `FIND_PATH_CONFLICT`.
+      CREATE_IF_MISSING,
+
+  // walks the tree.  if the path cannot be found, exit with
+  // `FIND_PATH_NOT_FOUND`.  if the operation is successful, then check
+  // intermediate nodes to ensure that they still have children.  any nodes
+  // that do not should be removed.
+      REMOVE_EMPTY_IMPLICIT_NODES,
+};
+
+struct FindContext {
+  bool invalidate_checksums;
+  int32_t num_leaf_node_changes;
+  FindMode mode;
+
+  // reuse this space when fetching manifests.
+  std::string nodebuffer;
+
+  // any extra data the callback needs to complete the operation.
+  void *extras;
+};
+
+class PathIterator {
+  private:
+    std::string path;
+    size_t position;
+  public:
+    PathIterator(std::string path) {
+      this->path = path;
+      this->position = 0;
+    }
+
+    bool next(char const ** word, size_t *wordlen) {
+      if (this->isfinished()) {
+        return false;
+      }
+
+      *word = this->path.c_str() + this->position;
+      size_t slashoffset = this->path.find('/', this->position);
+      if (slashoffset == std::string::npos) {
+        *wordlen = this->path.length() - this->position;
+      } else {
+        *wordlen = slashoffset - this->position;
+      }
+
+      this->position += *wordlen + 1;
+
+      return true;
+    }
+
+    bool isfinished() {
+      return this->position >= this->path.length();
+    }
+
+    void getPathToPosition(const char **word, size_t *wordlen) {
+      *word = path.c_str();
+      *wordlen = this->position;
+    }
+};
+
 /**
  * A single instance of a treemanifest.
  */
@@ -86,80 +160,6 @@ struct fileiter {
 
     return *this;
   }
-};
-
-enum FindResult {
-  FIND_PATH_OK,
-  FIND_PATH_NOT_FOUND,
-  FIND_PATH_CONFLICT,
-  FIND_PATH_WTF,
-};
-
-enum FindMode {
-  // walks the tree and searches for a leaf node.  if the path cannot be found,
-  // exit with `FIND_PATH_NOT_FOUND`.
-  BASIC_WALK,
-
-  // walks the tree.  if the intermediate paths cannot be found, create them.
-  // if a leaf node exists where an intermediate path node needs to be
-  // created, then return `FIND_PATH_CONFLICT`.
-  CREATE_IF_MISSING,
-
-  // walks the tree.  if the path cannot be found, exit with
-  // `FIND_PATH_NOT_FOUND`.  if the operation is successful, then check
-  // intermediate nodes to ensure that they still have children.  any nodes
-  // that do not should be removed.
-  REMOVE_EMPTY_IMPLICIT_NODES,
-};
-
-struct FindContext {
-  bool invalidate_checksums;
-  int32_t num_leaf_node_changes;
-  FindMode mode;
-
-  // reuse this space when fetching manifests.
-  std::string nodebuffer;
-
-  // any extra data the callback needs to complete the operation.
-  void *extras;
-};
-
-class PathIterator {
-  private:
-    std::string path;
-    size_t position;
-  public:
-    PathIterator(std::string path) {
-      this->path = path;
-      this->position = 0;
-    }
-
-    bool next(char const ** word, size_t *wordlen) {
-      if (this->isfinished()) {
-        return false;
-      }
-
-      *word = this->path.c_str() + this->position;
-      size_t slashoffset = this->path.find('/', this->position);
-      if (slashoffset == std::string::npos) {
-        *wordlen = this->path.length() - this->position;
-      } else {
-        *wordlen = slashoffset - this->position;
-      }
-
-      this->position += *wordlen + 1;
-
-      return true;
-    }
-
-    bool isfinished() {
-      return this->position >= this->path.length();
-    }
-
-    void getPathToPosition(const char **word, size_t *wordlen) {
-      *word = path.c_str();
-      *wordlen = this->position;
-    }
 };
 
 extern void treemanifest_get(
