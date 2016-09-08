bb07a56a181f1724193d38fc9e116bf14fece7c9
Jun Wu <quark@fb.com>
2016-09-08 16:57:50 +0100

    fastannotate: add logic to format annotate output
    Summary:
    The annotate output is not trivial. It supports a lot of flags, and will do
    padding by default. This diff implements most flags and the padding logic.
    It tries to imitate what the original annotate does by default.
    
    Note that `-T` (and namely `-Tjson`) is not supported currently.
    
    Test Plan: Code Review. A `.t` test will be added later.
    
    Reviewers: #sourcecontrol, stash
    
    Reviewed By: stash
    
    Subscribers: stash, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3836445
    
    Signature: t1:3836445:1473868393:91f6cede059e319eca7015d0d18727afa7ded665

---
+++ fastannotate/formatter.py
@@ -0,0 +1,71 @@
+# Copyright 2016-present Facebook. All Rights Reserved.
+#
+# format: defines the format used to output annotate result
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+from mercurial import (
+    encoding,
+    node,
+    util,
+)
+
+# extracted from mercurial.commands.annotate
+class defaultformatter(object):
+    """the default formatter that does leftpad and support some common flags"""
+
+    def __init__(self, ui, repo, opts):
+        if ui.quiet:
+            datefunc = util.shortdate
+        else:
+            datefunc = util.datestr
+        if ui.debugflag or opts.get('long_hash'):
+            hexfunc = node.hex
+        else:
+            hexfunc = node.short
+        datefunc = util.cachefunc(datefunc)
+        getctx = util.cachefunc(lambda x: repo[x[0]])
+
+        opmap = [('user', ' ', lambda x: ui.shortuser(getctx(x).user())),
+                 ('number', ' ', lambda x: str(getctx(x).rev())),
+                 ('changeset', ' ', lambda x: hexfunc(x[0])),
+                 ('date', ' ', lambda x: datefunc(getctx(x).date())),
+                 ('file', ' ', lambda x: x[2]),
+                 ('line_number', ':', lambda x: str(x[1] + 1))]
+
+        funcmap = [(get, sep) for op, sep, get in opmap
+                   if opts.get(op)]
+        funcmap[0] = (funcmap[0][0], '') # no separator for first column
+
+        self.ui = ui
+        self.funcmap = funcmap
+
+    def write(self, annotatedresult, lines=None):
+        """(annotateresult, [str]) -> None. write content out.
+        annotateresult can be [(node, linenum, path)], or [(node, linenum)]
+        """
+        pieces = [] # [[str]]
+        maxwidths = [] # [int]
+
+        # calculate
+        for f, sep in self.funcmap:
+            l = map(f, annotatedresult)
+            pieces.append(l)
+            widths = map(encoding.colwidth, l)
+            maxwidth = (max(widths) if widths else 0)
+            maxwidths.append(maxwidth)
+
+        # output
+        for i in xrange(len(annotatedresult)):
+            msg = ''
+            for j, p in enumerate(pieces):
+                sep = self.funcmap[j][1]
+                padding = ' ' * (maxwidths[j] - len(p[i]))
+                msg += sep + padding + p[i]
+            if lines:
+                msg += ': ' + lines[i]
+
+            if msg[-1] != '\n':
+                msg += '\n'
+            self.ui.write(msg)
