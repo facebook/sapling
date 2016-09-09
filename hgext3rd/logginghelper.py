e64b96276e23105663620f1d3d35a42254fe54a9
Stanislau Hlebik <stash@fb.com>
2016-09-09 08:04:03 -0700

    logginghelper: rename reporootlog to logginghelper
    Summary:
    We are going to log many different pieces of information in this extensions.
    Therefore the `reporootlog` is not an appropriate name anymore.
    Change it to `logginghelper`.
    
    Test Plan:
    Note that repo root is still logged instead of repo name. It will be changed in subsequent diffs
    {P56637657}
    
    
    
    Reviewers: rmcelroy, quark, ttung
    
    Reviewed By: ttung
    
    Subscribers: mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3840985
    
    Signature: t1:3840985:1473410718:62d83f008f2973b7ed5266b0a5ebf67fe54b48c4

--- hgext3rd/logginghelper.py
+++ hgext3rd/logginghelper.py
@@ -5,14 +5,8 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-"""The SCM wrappers do not have a reliable and easy way of detecting the
-repo root.  For instance, if someone does hg log /full/path/to/repo/content, we
-cannot actually determine that /full/path/to/repo is the repo root.  We have to
-somehow integrate an overly large portion of mercurial's commmand parsing
-infrastrastructure to accomplish this.
-
-However, since mercurial knows the repo root, just extract that knowledge and
-send it down to the client.
+"""This extension logs different pieces of information that will be used
+by SCM wrappers
 """
 
 import os
@@ -25,7 +19,7 @@ from mercurial.i18n import _
 def _localrepoinit(orig, self, baseui, path=None, create=False):
     orig(self, baseui, path, create)
     kwargs = {'repo': path}
-    self.ui.log("reporootlog",
+    self.ui.log("logginghelper",
                 "",           # ui.log requires a format string as args[0].
                 **kwargs)
 
