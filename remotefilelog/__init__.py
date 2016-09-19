6007cd6e15ff0809aef1be6c3754614d0e1dee82
Jun Wu <quark@fb.com>
2016-09-19 15:52:20 +0100

    remotefilelog: remove duplicated testedwith
    Summary: There is a line `testedwith = ''` below.
    
    Test Plan: Code Review
    
    Reviewers: #mercurial, ttung, rmcelroy
    
    Reviewed By: rmcelroy
    
    Subscribers: mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D3886521
    
    Signature: t1:3886521:1474301901:cceadfc1d5b181435362d7ae75014bad1a75576f

--- remotefilelog/__init__.py
+++ remotefilelog/__init__.py
@@ -5,8 +5,6 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-testedwith = 'internal'
-
 import fileserverclient, remotefilelog, remotefilectx, shallowstore, shallowrepo
 import shallowbundle, debugcommands, remotefilelogserver, shallowverifier
 import shallowutil, historypack, datapack, basestore, contentstore
