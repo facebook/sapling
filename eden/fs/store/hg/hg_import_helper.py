b43fa6ada70a55e5433da415c3d48d47364d1309
Michael Bolin <mbolin@fb.com>
2016-11-02 18:07:35 -0700

    Update license header for open source.
    Reviewed By: simpkins
    
    Differential Revision: D4119726
    
    fbshipit-source-id: 0d2d17ca6caed233a26d77f0d197c1462a34e53f

--- eden/fs/store/hg/hg_import_helper.py
+++ eden/fs/store/hg/hg_import_helper.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2
 #
 # Copyright (c) 2016, Facebook, Inc.
+# All rights reserved.
 #
-# This software may be used and distributed according to the terms of the
-# GNU General Public License version 2 or any later version.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
 
 from __future__ import absolute_import
 from __future__ import division
