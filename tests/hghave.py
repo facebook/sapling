a002443acc176e7a18341c845f3da611d8f8fa37
Gregory Szorc <gregory.szorc@gmail.com>
2016-11-10 23:38:41 -0800

    hghave: add check for zstd support
    Not all configurations will support zstd. Add a check so we can
    conditionalize tests.

--- tests/hghave.py
+++ tests/hghave.py
@@ -610,3 +610,12 @@ def has_hypothesis():
 @check("unziplinks", "unzip(1) understands and extracts symlinks")
 def unzip_understands_symlinks():
     return matchoutput('unzip --help', br'Info-ZIP')
+
+@check("zstd", "zstd Python module available")
+def has_zstd():
+    try:
+        import mercurial.zstd
+        mercurial.zstd.__version__
+        return True
+    except ImportError:
+        return False
