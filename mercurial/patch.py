81bdbbebbdbb1249785ad8cf335960bb6902c834
Jun Wu <quark@fb.com>
2016-11-14 23:14:06 +0000

    patch: migrate to util.iterfile

--- mercurial/patch.py
+++ mercurial/patch.py
@@ -1069,7 +1069,7 @@ the hunk is left unchanged.
                     # Remove comment lines
                     patchfp = open(patchfn)
                     ncpatchfp = stringio()
-                    for line in patchfp:
+                    for line in util.iterfile(patchfp):
                         if not line.startswith('#'):
                             ncpatchfp.write(line)
                     patchfp.close()
@@ -2012,7 +2012,7 @@ def _externalpatch(ui, repo, patcher, patchname, strip, files,
     fp = util.popen('%s %s -p%d < %s' % (patcher, ' '.join(args), strip,
                                        util.shellquote(patchname)))
     try:
-        for line in fp:
+        for line in util.iterfile(fp):
             line = line.rstrip()
             ui.note(line + '\n')
             if line.startswith('patching file '):
