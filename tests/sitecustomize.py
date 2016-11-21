d18071a4dea314916ddface30e0832e8310ad353
Augie Fackler <augie@google.com>
2016-11-21 17:51:39 -0500

    tests: update sitecustomize to use uuid1() instead of randrange()
    The comments mention that uuid would be better, so let's go ahead and
    make good on an old idea.

--- tests/sitecustomize.py
+++ tests/sitecustomize.py
@@ -4,11 +4,10 @@ import os
 if os.environ.get('COVERAGE_PROCESS_START'):
     try:
         import coverage
-        import random
+        import uuid
 
-        # uuid is better, but not available in Python 2.4.
         covpath = os.path.join(os.environ['COVERAGE_DIR'],
-                               'cov.%s' % random.randrange(0, 1000000000000))
+                               'cov.%s' % uuid.uuid1())
         cov = coverage.coverage(data_file=covpath, auto_data=True)
         cov._warn_no_data = False
         cov._warn_unimported_source = False
