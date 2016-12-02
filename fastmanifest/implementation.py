d4cfb31a2b461f52122e003c9dfc4b5db9ef0e73
Durham Goode <durham@fb.com>
2016-12-02 14:37:35 -0800

    fastmanifest: move fastdelta to a top level function
    Summary:
    A future patch will make treemanifests able to use fastdelta as well, so let's
    move the function to be global so we can reuse it.
    
    Test Plan: Ran the tests
    
    Reviewers: #mercurial, ikostia
    
    Reviewed By: ikostia
    
    Subscribers: rmcelroy, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D4249364
    
    Signature: t1:4249364:1480507778:a00bb7b6301ca2f716c542f37f7150f2f6231640

--- fastmanifest/implementation.py
+++ fastmanifest/implementation.py
@@ -436,54 +436,59 @@ class fastmanifestdict(object):
         """Given a base manifest text as an array.array and a list of changes
         relative to that text, compute a delta that can be used by revlog.
         """
-        delta = []
-        dstart = None
-        dend = None
-        dline = [""]
-        start = 0
-        # zero copy representation of base as a buffer
-        addbuf = util.buffer(base)
+        return fastdelta(self, self._fm.__getitem__, base, changes)
 
-        changes = list(changes)
-        if len(changes) < 1000:
-            # start with a readonly loop that finds the offset of
-            # each line and creates the deltas
-            for f, todelete in changes:
-                # bs will either be the index of the item or the insert point
-                start, end = manifest._msearch(addbuf, f, start)
-                if not todelete:
-                    h, fl = self._fm[f]
-                    l = "%s\0%s%s\n" % (f, revlog.hex(h), fl)
-                else:
-                    if start == end:
-                        # item we want to delete was not found, error out
-                        raise AssertionError(
-                                (("failed to remove %s from manifest") % f))
-                    l = ""
-                if dstart is not None and dstart <= start and dend >= start:
-                    if dend < end:
-                        dend = end
-                    if l:
-                        dline.append(l)
-                else:
-                    if dstart is not None:
-                        delta.append([dstart, dend, "".join(dline)])
-                    dstart = start
+def fastdelta(mf, mfgetter, base, changes):
+    """Given a base manifest text as an array.array and a list of changes
+    relative to that text, compute a delta that can be used by revlog.
+    """
+    delta = []
+    dstart = None
+    dend = None
+    dline = [""]
+    start = 0
+    # zero copy representation of base as a buffer
+    addbuf = util.buffer(base)
+
+    changes = list(changes)
+    if len(changes) < 1000:
+        # start with a readonly loop that finds the offset of
+        # each line and creates the deltas
+        for f, todelete in changes:
+            # bs will either be the index of the item or the insert point
+            start, end = manifest._msearch(addbuf, f, start)
+            if not todelete:
+                h, fl = mfgetter(f)
+                l = "%s\0%s%s\n" % (f, revlog.hex(h), fl)
+            else:
+                if start == end:
+                    # item we want to delete was not found, error out
+                    raise AssertionError(
+                            (("failed to remove %s from manifest") % f))
+                l = ""
+            if dstart is not None and dstart <= start and dend >= start:
+                if dend < end:
                     dend = end
-                    dline = [l]
+                if l:
+                    dline.append(l)
+            else:
+                if dstart is not None:
+                    delta.append([dstart, dend, "".join(dline)])
+                dstart = start
+                dend = end
+                dline = [l]
 
-            if dstart is not None:
-                delta.append([dstart, dend, "".join(dline)])
-            # apply the delta to the base, and get a delta for addrevision
-            deltatext, arraytext = manifest._addlistdelta(base, delta)
-        else:
-            # For large changes, it's much cheaper to just build the text and
-            # diff it.
-            arraytext = array.array('c', self.text())
-            deltatext = mdiff.textdiff(base, arraytext)
+        if dstart is not None:
+            delta.append([dstart, dend, "".join(dline)])
+        # apply the delta to the base, and get a delta for addrevision
+        deltatext, arraytext = manifest._addlistdelta(base, delta)
+    else:
+        # For large changes, it's much cheaper to just build the text and
+        # diff it.
+        arraytext = array.array('c', mf.text())
+        deltatext = mdiff.textdiff(base, arraytext)
 
-        return arraytext, deltatext
-
+    return arraytext, deltatext
 
 class ondiskcache(object):
     def __init__(self, debugf, opener, ui):
