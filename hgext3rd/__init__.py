5c73644ae82b0ef3d0ea38366696c881774b4f87
Kyle Lippincott <spectral@google.com>
2016-12-19 16:34:49 -0800

    debian: Do not package hgext3rd/__init__.py
    When installed into a path like /usr/lib/python2.7/dist-packages, this file
    conflicts with the hgext3rd/__init__.py from mercurial itself.  Let's keep the
    mercurial version the canonical one, and just skip packaging this one.
    
    I added a warning to this repo's __init__.py about behavior divergences between
    this __init__.py and mercurial's version.

--- hgext3rd/__init__.py
+++ hgext3rd/__init__.py
@@ -3,3 +3,9 @@ import pkgutil
 # Indicate that hgext3rd is a namspace package, and other python path
 # directories may still be searched for hgext3rd extensions.
 __path__ = pkgutil.extend_path(__path__, __name__)
+
+### IMPORTANT ###
+# Do not add logic here that would diverge from mercurial's
+# hgext3rd/__init__.py, the installed version of the file on debian systems is
+# provided by mercurial itself; the packaging rules for remotefilelog explicitly
+# ignore the file you're reading now.
