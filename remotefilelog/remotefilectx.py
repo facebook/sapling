493f4333e4806815f837510683b145a569d54e29
Ryan McElroy <rmcelroy@fb.com>
2017-02-09 03:35:58 -0800

    remotefilelog: introduce helper function to verify linkrevs
    Summary:
    We will be reusing this logic in the next patch; for now we're simply
    factoring it out of the main loop
    
    Test Plan: tests continue to pass
    
    Reviewers: #mercurial, durham, stash
    
    Reviewed By: stash
    
    Subscribers: stash, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D4528290
    
    Tasks: 12958591
    
    Signature: t1:4528290:1486567826:cb17e2fa1ab71f491482eba1a88c107a18333781

--- remotefilelog/remotefilectx.py
+++ remotefilelog/remotefilectx.py
@@ -193,16 +193,8 @@ class remotefilectx(context.filectx):
         else:
             revs = [srcrev]
 
-        # First use the C fastpath to check if the given linknode is correct.
-        try:
-            if revs:
-                srcnode = cl.node(revs[0])
-                if cl.isancestor(linknode, srcnode):
-                    return linknode
-        except error.LookupError:
-            # The node read from the blob may be old and not present, thus not
-            # existing in the changelog.
-            pass
+        if self._verifylinknode(revs, linknode):
+            return linknode
 
         iteranc = cl.ancestors(revs, inclusive=inclusive)
         for a in iteranc:
@@ -214,6 +206,32 @@ class remotefilectx(context.filectx):
                     return cl.node(a)
 
         return linknode
+
+    def _verifylinknode(self, revs, linknode):
+        """
+        Check if a linknode is correct one for the current history.
+
+        That is, return True if the linkrev is the ancestor of the first of the
+        passed in revs (if any), otherwise return False.
+
+        TODO: We may want to consider linkrev being an ancestor of any of the
+        passed-in revs as sufficient, not just the first.
+
+        `revs` is a list that usually has one element -- usually the wdir parent
+        or the user-passed rev we're looking back from. It may contain two revs
+        when there is a merge going on, or zero revs when a root node with no
+        parents is being created.
+        """
+        try:
+            # Use the C fastpath to check if the given linknode is correct.
+            cl = self._repo.changelog
+            return revs and cl.isancestor(linknode, cl.node(revs[0]))
+        except error.LookupError:
+            # The linknode read from the blob may have been stripped or
+            # otherwise not present in the repository anymore. Do not fail hard
+            # in this case. Instead, return false and continue the search for
+            # the correct linknode.
+            return False
 
     def ancestors(self, followfirst=False):
         ancestors = []
