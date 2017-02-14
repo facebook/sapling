e71276b3036256a1cd9811c2a5560fdcfead356d
Arun Kulshreshtha <kulshrax@fb.com>
2017-02-14 11:15:19 -0800

    hiddenerror: make error message configurable
    Summary:
    This diff makes the following modifications to the (former) `hiddenhash` extension:
    
    - Makes the error messages configurable, allowing us to easily change the error messages in `facebook.rc`.
    - Renames the extension to `hiddenerror`, since the primary feature is now the configurable error messages rather than using a hash in the message.
    - Changes the method by which the extension wraps the `changectx` constructor. The old method of wrapping would result in errors in code paths that in `context.py` that performed `isinstance()` checks on contexts.
    
    Test Plan: To test arc patch this extension into your `facebook-hg-rpms` and enable it for a test repo. Then, attempt to access a hidden node. You should see the default error message defined in the extension. Setting `hiddenerror.message` and `hiddenerror.hint` should allow you to customize the messages. See test file for examples.
    
    Reviewers: #mercurial, rmcelroy
    
    Reviewed By: rmcelroy
    
    Subscribers: rmcelroy, durham, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D4548140
    
    Tasks: 15896355
    
    Signature: t1:4548140:1487063552:06395c1eaaa83b00ead8e80e2cd8f9d13f7d08e2

---
+++ hgext3rd/hiddenerror.py
@@ -0,0 +1,68 @@
+# hiddenerror.py
+#
+# Copyright 2017 Facebook, Inc.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+"""configurable error messages for accessing hidden changesets
+
+   Set the following configuration options to customize the error message
+   seen when the user attempts to access a hidden changeset::
+
+   [hiddenerror]
+   message = my custom message
+   hint = my custom hint
+
+   The message and hint can contain an optional `{}` which will be substituted
+   with the hash of the hidden changeset.
+"""
+
+import re
+
+from mercurial import context, error
+from mercurial.i18n import _
+from mercurial.node import short
+
+testedwith = 'ships-with-fb-hgext'
+
+def uisetup(ui):
+    """Wrap context.changectx to catch FilteredRepoLookupError."""
+    class changectxwrapper(context.changectx):
+        def __init__(self, repo, *args, **kwargs):
+            try:
+                # Attempt to call constructor normally.
+                super(changectxwrapper, self).__init__(repo, *args, **kwargs)
+            except error.FilteredRepoLookupError as e:
+                # If we get a FilteredRepoLookupError, attempt to rewrite
+                # the error message and re-raise the exception.
+                match = re.match(r"hidden revision '(\d+)'", str(e))
+                if not match:
+                    raise
+
+                rev = int(match.group(1))
+                node = repo.unfiltered().changelog.node(rev)
+                shorthash = short(node)
+
+                # Get the error messages from the user's configuration and
+                # substitute the hash in. Use a dict for the hint argument
+                # to make it optional via keyword argument unpacking.
+                msg, hint = _getstrings(ui)
+                msg = msg.format(shorthash)
+                hintarg = {}
+                if hint:
+                    hintarg['hint'] = hint.format(shorthash)
+
+                raise error.FilteredRepoLookupError(msg, **hintarg)
+    setattr(context, 'changectx', changectxwrapper)
+
+def _getstrings(ui):
+    """Lood the error messages to show when the user tries to access a
+       hidden commit from the user's configuration file. Fall back to
+       default messages if nothing is configured.
+    """
+    msg = ui.config('hiddenerror', 'message')
+    hint = ui.config('hiddenerror', 'hint')
+    if not msg:
+        msg = _("hidden changeset {}")
+    return msg, hint
---
+++ tests/test-hiddenerror.t
@@ -0,0 +1,44 @@
+  $ extpath=`dirname $TESTDIR`
+  $ cp $extpath/hgext3rd/hiddenerror.py $TESTTMP
+  $ cat >> $HGRCPATH << EOF
+  > [extensions]
+  > hiddenerror=$TESTTMP/hiddenerror.py
+  > [experimental]
+  > evolution=all
+  > EOF
+
+Create hidden changeset.
+  $ hg init repo && cd repo
+  $ hg debugbuilddag +1
+  $ hg debugobsolete 1ea73414a91b0920940797d8fc6a11e447f8ea1e
+
+Test default error message.
+  $ hg log -r 0
+  abort: hidden changeset 1ea73414a91b!
+  [255]
+
+Test custom error message without hash.
+  $ cat >> $HGRCPATH << EOF
+  > [hiddenerror]
+  > message = message without hash
+  > hint = hint without hash
+  > EOF
+  $ hg log -r 0
+  abort: message without hash!
+  (hint without hash)
+  [255]
+
+Test custom error message with hash.
+  $ cat >> $HGRCPATH << EOF
+  > [hiddenerror]
+  > message = message with hash {}
+  > hint = hint with hash {}
+  > EOF
+  $ hg log -r 0
+  abort: message with hash 1ea73414a91b!
+  (hint with hash 1ea73414a91b)
+  [255]
+
+Test that basic operations like `status` don't throw an exception due
+to the wrapped context constructor.
+  $ hg status
