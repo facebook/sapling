d0ce8b60885cfc128c8be26dd00a3eb28dc432af
Dr Rainer Woitok <rainer.woitok@gmail.com>
2017-02-20 18:27:29 +0100

    tests: make test suite more immune to environment variables
    Plenty of tests break when "make tests" is run while environment
    variables "HGPLAIN" or "HGPLAINEXCEPT" are set (test "test-obsolete-
    checkheads.t" is just a single example).
    
    This patch causes script "run-tests.py" to also remove these two
    variables from the environment the tests are executed in.

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -906,6 +906,7 @@ class Test(unittest.TestCase):
         env['TERM'] = 'xterm'
 
         for k in ('HG HGPROF CDPATH GREP_OPTIONS http_proxy no_proxy ' +
+                  'HGPLAIN HGPLAINEXCEPT ' +
                   'NO_PROXY CHGDEBUG').split():
             if k in env:
                 del env[k]
