01fdb5bf0dd6e5d5f4c412ba7e87ad962dc1a184
TJ <tjfryan@fb.com>
2017-02-23 11:27:52 -0800

    edrecord: introduce interactive commits with an editor interface
    Summary: creating a mercurial extension so that `hg record` functionality can be used via a text editor (instead of interactive command prompt or curses menu)
    
    Test Plan: `$ run-tests.py test-edrecord.t`
    
    Reviewers: durham, skreyen, most, #sourcecontrol, asriram, simonfar
    
    Reviewed By: simonfar
    
    Subscribers: quark, rmcelroy, simonfar, mjpieters, #nuclide
    
    Differential Revision: https://phabricator.intern.facebook.com/D4547080
    
    Tasks: 15728734
    
    Signature: t1:4547080:1487842609:8855e35010cac3b6d2b136a80d7effa369eb1da8

---
+++ hgext3rd/edrecord.py
@@ -0,0 +1,108 @@
+# edrecord.py
+#
+# Copyright 2017 Facebook, Inc.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+from mercurial import (
+    cmdutil,
+    crecord as crecordmod,
+    patch as patchmod,
+    util,
+)
+from mercurial.i18n import _
+import inspect
+
+testedwith = 'ships-with-fb-hgext'
+
+stringio = util.stringio
+originaldorecord = cmdutil.dorecord
+originalrecordfilter = cmdutil.recordfilter
+
+def uisetup(ui):
+    # "editor" is otherwise not allowed as a valid option for "ui.interface"
+    class edrecordui(ui.__class__):
+        def interface(self, feature):
+            if feature == "chunkselector":
+                if (self.config("ui", "interface.%s" % feature, None) ==
+                    "editor"):
+                    return "editor"
+                elif self.config("ui", "interface.%s" % feature, None) is None:
+                    if self.config("ui", "interface", None) == "editor":
+                        return "editor"
+            return super(edrecordui, self).interface(feature)
+
+    ui.__class__ = edrecordui
+    cmdutil.recordfilter = recordfilter
+    cmdutil.dorecord = dorecord
+
+def dorecord(ui, repo, commitfunc, cmdsuggest, backupall,
+            filterfn, *pats, **opts):
+    if ui.interface("chunkselector") != "editor":
+        return originaldorecord(ui, repo, commitfunc, cmdsuggest, backupall,
+                                filterfn, *pats, **opts)
+
+    backup = ui.backupconfig('ui', 'interactive')
+    try:
+        ui.setconfig('ui', 'interactive', True)
+        originaldorecord(ui, repo, commitfunc, cmdsuggest, backupall, filterfn,
+                        *pats, **opts)
+    finally:
+        ui.restoreconfig(backup)
+
+def recordfilter(ui, headers, operation=None):
+
+    if ui.interface("chunkselector") != "editor":
+        return originalrecordfilter(ui, headers, operation)
+
+    # construct diff string from headers
+    if len(headers) == 0:
+        return [], {}
+
+    patch = stringio()
+    patch.write(crecordmod.diffhelptext)
+
+    specials = {}
+
+    for header in headers:
+        patch.write('#\n')
+        if header.special():
+            # this is useful for special changes, we are able to get away with
+            # only including the parts of headers that offer useful info
+            specials[header.filename()] = header
+            for h in header.header:
+                if h.startswith('index '):
+                    # starting at 'index', the headers for binary files tend to
+                    # stop offering useful info for the viewer
+                    patch.write(_("""
+# this modifies a binary file (all or nothing)\n
+"""))
+                    break
+                if not h.startswith('diff '):
+                    # For specials, we only care about the filename header.
+                    # The rest can be displayed as comments
+                    patch.write('# ')
+                patch.write(h)
+        else:
+            header.write(patch)
+            for hunk in header.hunks:
+                hunk.write(patch)
+
+    patch = ui.edit(patch.getvalue(), "", extra={'suffix': '.diff'})
+
+    # remove comments from patch
+    # if there's an empty line, add a space to it
+    patch = [(line if len(line) > 0 else ' ') + '\n'
+             for line in patch.splitlines() if not line.startswith('#')]
+
+    headers = patchmod.parsepatch(patch)
+
+    applied = {}
+    for h in headers:
+        if h.filename() in specials:
+            h = specials[h.filename()]
+        applied[h.filename()] = [h] + h.hunks
+
+    return (sum([h for h in applied.itervalues()
+               if h[0].special() or len(h) > 1], []), {})
