96fc868b4a6364b2fe30defa3ceaed88102564ee
Augie Fackler <raf@durin42.com>
2017-03-03 12:55:11 -0500

    config: add sanity assert that files are opened as binary
    This helps with some debugging in Python 3, and shouldn't hurt
    anything in Python 2. The unusual construction using getattr is done
    so that StringIO/BytesIO instances can be used as well as real files.

--- mercurial/config.py
+++ mercurial/config.py
@@ -170,5 +170,8 @@ class config(object):
     def read(self, path, fp=None, sections=None, remap=None):
         if not fp:
             fp = util.posixfile(path, 'rb')
+        assert getattr(fp, 'mode', r'rb') == r'rb', (
+            'config files must be opened in binary mode, got fp=%r mode=%r' % (
+                fp, fp.mode))
         self.parse(path, fp.read(),
                    sections=sections, remap=remap, include=self.read)
