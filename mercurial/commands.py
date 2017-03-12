b9f0d10d43a9fbbe91529978b1303b69d8b2696a
Augie Fackler <augie@google.com>
2017-03-11 20:58:26 -0500

    summary: don't explicitly str() something we're about to %s
    str() is wrong on Python 3 here, and %s implicitly calls str() anyway,
    so this was just extra dancing for no reason.

--- mercurial/commands.py
+++ mercurial/commands.py
@@ -4812,7 +4812,7 @@ def summary(ui, repo, **opts):
         # label with log.changeset (instead of log.parent) since this
         # shows a working directory parent *changeset*:
         # i18n: column positioning for "hg summary"
-        ui.write(_('parent: %d:%s ') % (p.rev(), str(p)),
+        ui.write(_('parent: %d:%s ') % (p.rev(), p),
                  label=cmdutil._changesetlabels(p))
         ui.write(' '.join(p.tags()), label='log.tag')
         if p.bookmarks():
