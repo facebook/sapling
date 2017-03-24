92eadab6857559c67f160b489e3860a12711371f
Jun Wu <quark@fb.com>
2017-03-24 16:19:13 -0700

    hgsql: test non-ascii paths
    Summary:
    The test is to check whether `varchar CHARACTER SET latin1 COLLATE latin1_bin`
    works just fine or not.
    
    Sadly it does not. And `varbinary` makes the test pass, as expected.
    
    For now, this diff only changes the `path` field, other fields like
    `revision_references.name` etc should use `varbinary` in theory. But they are
    less concerning than `path` because they don't impact real repo content. Using
    `varchar`, MySQL clients are happy to display the contents, instead of showing
    a placeholder like "(BLOB)".
    
    Test Plan: `run-tests.py *.t`
    
    Reviewers: #sourcecontrol, rmcelroy
    
    Reviewed By: rmcelroy
    
    Subscribers: rmcelroy
    
    Differential Revision: https://phabricator.intern.facebook.com/D4774735
    
    Signature: t1:4774735:1490700636:61a3dc46476b37b3c32e6a022d5791be2c3ea965

--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `revisions` (
-  `repo` varchar(64) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `path` varchar(512) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `repo` varbinary(64) NOT NULL,
+  `path` varbinary(512) NOT NULL,
   `chunk` int(10) unsigned NOT NULL,
   `chunkcount` int(10) unsigned NOT NULL,
   `linkrev` int(10) unsigned NOT NULL,
   `rev` int(10) unsigned NOT NULL,
-  `node` char(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `node` binary(40) NOT NULL,
   `entry` binary(64) NOT NULL,
-  `data0` char(1) NOT NULL,
+  `data0` varbinary(1) NOT NULL,
   `data1` longblob NOT NULL,
   `createdtime` datetime NOT NULL,
   PRIMARY KEY (`repo`,`path`,`rev`,`chunk`),
@@ -16,10 +16,10 @@ CREATE TABLE `revisions` (
 
 CREATE TABLE `revision_references` (
   `autoid` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `repo` varchar(64) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `namespace` varchar(32) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `name` varchar(256) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
-  `value` char(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `repo` varbinary(64) NOT NULL,
+  `namespace` varbinary(32) NOT NULL,
+  `name` varbinary(256) DEFAULT NULL,
+  `value` varbinary(40) NOT NULL,
   PRIMARY KEY (`autoid`),
   UNIQUE KEY `bookmarkindex` (`repo`,`namespace`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
