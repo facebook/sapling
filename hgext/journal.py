c896b34fa7dbcdaa338a5d0dea2123dced5ce568
Jun Wu <quark@fb.com>
2017-03-23 21:16:29 -0700

    journal: use lowercase for docstring title
    See the previous patch for why.

--- hgext/journal.py
+++ hgext/journal.py
@@ -4,7 +4,7 @@
 #
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
-"""Track previous positions of bookmarks (EXPERIMENTAL)
+"""track previous positions of bookmarks (EXPERIMENTAL)
 
 This extension adds a new command: `hg journal`, which shows you where
 bookmarks were previously located.
