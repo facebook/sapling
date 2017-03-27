368236438f7d57c1d48458828e8f7e474347ac6b
Pierre-Yves David <pierre-yves.david@ens-lyon.org>
2017-03-27 16:00:47 +0200

    tags: deprecated 'repo.tag'
    All user are gone. We can now celebrate the removal of some extra line from the
    'localrepo' class.

--- mercurial/localrepo.py
+++ mercurial/localrepo.py
@@ -650,6 +650,7 @@ class localrepository(object):
         return hook.hook(self.ui, self, name, throw, **args)
 
     def tag(self, names, node, message, local, user, date, editor=False):
+        self.ui.deprecwarn("use 'tagsmod.tag' instead of 'repo.tag'", '4.2')
         tagsmod.tag(self, names, node, message, local, user, date,
                     editor=editor)
 
