cb1cfe7f123521462af636963f8e23285cf5ac0d
FUJIWARA Katsunori <foozy@lares.dti.ne.jp>
2017-05-01 19:58:52 +0900

    lock: avoid unintentional lock acquisition at failure of readlock
    Acquiring lock by vfs.makelock() and getting lock holder (aka
    "locker") information by vfs.readlock() aren't atomic action.
    Therefore, failure of the former doesn't ensure success of the latter.
    
    Before this patch, lock is unintentionally acquired, if
    self.parentlock is None (this is default value), and lock._readlock()
    returns None for ENOENT at vfs.readlock(), because these None are
    recognized as equal to each other.
    
    In this case, lock symlink (or file) isn't created, even though lock
    is treated as acquired in memory.
    
    To avoid this issue, this patch retries lock acquisition immediately,
    if lock._readlock() returns None "locker".
    
    This issue will be covered by a test added in subsequent patch,
    because simple emulation of ENOENT at vfs.readlock() easily causes
    another issue.  "raising ENOENT only at the first vfs.readlock()
    invocation" is too complicated for unit test, IMHO.

--- mercurial/lock.py
+++ mercurial/lock.py
@@ -131,6 +131,9 @@ class lock(object):
             except (OSError, IOError) as why:
                 if why.errno == errno.EEXIST:
                     locker = self._readlock()
+                    if locker is None:
+                        continue
+
                     # special case where a parent process holds the lock -- this
                     # is different from the pid being different because we do
                     # want the unlock and postrelease functions to be called,
