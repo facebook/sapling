320ab6e3cfa5d9825101aca8a6f09f3624e7428b
Siddharth Agarwal <sid0@fb.com>
2017-05-31 19:46:04 -0700

    rebase: drop unnecessary parentchange call
    We're calling localrepo.setparents here, not dirstate.setparents.
    localrepo.setparents calls dirstate.parentchange already.

--- hgext/rebase.py
+++ hgext/rebase.py
@@ -427,8 +427,7 @@ class rebaseruntime(object):
                         mergemod.mergestate.clean(repo)
                 else:
                     # Skip commit if we are collapsing
-                    with repo.dirstate.parentchange():
-                        repo.setparents(repo[p1].node())
+                    repo.setparents(repo[p1].node())
                     newnode = None
                 # Update the state
                 if newnode is not None:
