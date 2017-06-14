e1f58d87e31c80e145730e9d4e55f9bce96820d7
Jun Wu <quark@fb.com>
2017-06-13 17:08:50 -0700

    fbamend: move unamend to a separate file
    Summary:
    Going to split fbamend into individual files by commands. `unamend` seems
    self-contained to be moved out.
    
    Test Plan: arc unit
    
    Reviewers: #mercurial, ikostia
    
    Reviewed By: ikostia
    
    Subscribers: mjpieters, medson
    
    Differential Revision: https://phabricator.intern.facebook.com/D5221159
    
    Signature: t1:5221159:1497261013:d371bcf4406277228fc4f3f6c6aaa4600dab4823

--- hgext3rd/fbamend/__init__.py
+++ hgext3rd/fbamend/__init__.py
@@ -33,6 +33,8 @@ the following config option::
 
 """
 
+from __future__ import absolute_import
+
 from mercurial import (
     bookmarks,
     cmdutil,
@@ -53,8 +55,15 @@ from mercurial.i18n import _
 from collections import defaultdict, deque
 from itertools import count
 
+from . import (
+    unamend,
+)
+
 cmdtable = {}
 command = registrar.command(cmdtable)
+
+cmdtable.update(unamend.cmdtable)
+
 testedwith = 'ships-with-fb-hgext'
 
 rebasemod = None
@@ -86,8 +95,6 @@ def uisetup(ui):
            ('i', 'interactive', None, _('use interactive mode')),
        ] + amendopts + commands.walkopts + commands.commitopts,
        _('hg amend [OPTION]...'))(amend)
-
-    command('^unamend', [])(unamend)
 
     def has_automv(loaded):
         if not loaded:
@@ -213,69 +220,6 @@ def commit(orig, ui, repo, *pats, **opts):
                     badflags[0])
 
         return orig(ui, repo, *pats, **opts)
-
-def unamend(ui, repo, **opts):
-    """undo the amend operation on a current changeset
-
-    This command will roll back to the previous version of a changeset,
-    leaving working directory in state in which it was before running
-    `hg amend` (e.g. files modified as part of an amend will be
-    marked as modified `hg status`)"""
-    try:
-        inhibitmod = extensions.find('inhibit')
-    except KeyError:
-        hint = _("please add inhibit to the list of enabled extensions")
-        e = _("unamend requires inhibit extension to be enabled")
-        raise error.Abort(e, hint=hint)
-
-    unfi = repo.unfiltered()
-
-    # identify the commit from which to unamend
-    curctx = repo['.']
-
-    # identify the commit to which to unamend
-    markers = list(obsolete.precursormarkers(curctx))
-    if len(markers) != 1:
-        e = _("changeset must have one precursor, found %i precursors")
-        raise error.Abort(e % len(markers))
-
-    precnode = markers[0].precnode()
-    precctx = unfi[precnode]
-
-    if curctx.children():
-        raise error.Abort(_("cannot unamend in the middle of a stack"))
-
-    with repo.wlock():
-        with repo.lock():
-            repobookmarks = repo._bookmarks
-            ctxbookmarks = curctx.bookmarks()
-            # we want to inhibit markers that mark precnode obsolete
-            inhibitmod._inhibitmarkers(unfi, [precnode])
-            changedfiles = []
-            wctx = repo[None]
-            wm = wctx.manifest()
-            cm = precctx.manifest()
-            dirstate = repo.dirstate
-            diff = cm.diff(wm)
-            changedfiles.extend(diff.iterkeys())
-
-            tr = repo.transaction('unamend')
-            with dirstate.parentchange():
-                dirstate.rebuild(precnode, cm, changedfiles)
-                # we want added and removed files to be shown
-                # properly, not with ? and ! prefixes
-                for filename, data in diff.iteritems():
-                    if data[0][0] is None:
-                        dirstate.add(filename)
-                    if data[1][0] is None:
-                        dirstate.remove(filename)
-            for book in ctxbookmarks:
-                repobookmarks[book] = precnode
-            repobookmarks.recordchange(tr)
-            tr.close()
-            # we want to mark the changeset from which we were unamending
-            # as obsolete
-            obsolete.createmarkers(repo, [(curctx, ())])
 
 def amend(ui, repo, *pats, **opts):
     '''amend the current changeset with more changes
---
+++ hgext3rd/fbamend/unamend.py
@@ -0,0 +1,83 @@
+# unamend.py - undo an amend operation
+#
+# Copyright 2016 Facebook, Inc.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+from __future__ import absolute_import
+
+from mercurial import (
+    error,
+    extensions,
+    obsolete,
+    registrar,
+)
+from mercurial.i18n import _
+
+cmdtable = {}
+command = registrar.command(cmdtable)
+
+@command('^unamend', [])
+def unamend(ui, repo, **opts):
+    """undo the amend operation on a current changeset
+
+    This command will roll back to the previous version of a changeset,
+    leaving working directory in state in which it was before running
+    `hg amend` (e.g. files modified as part of an amend will be
+    marked as modified `hg status`)"""
+    try:
+        inhibitmod = extensions.find('inhibit')
+    except KeyError:
+        hint = _("please add inhibit to the list of enabled extensions")
+        e = _("unamend requires inhibit extension to be enabled")
+        raise error.Abort(e, hint=hint)
+
+    unfi = repo.unfiltered()
+
+    # identify the commit from which to unamend
+    curctx = repo['.']
+
+    # identify the commit to which to unamend
+    markers = list(obsolete.precursormarkers(curctx))
+    if len(markers) != 1:
+        e = _("changeset must have one precursor, found %i precursors")
+        raise error.Abort(e % len(markers))
+
+    precnode = markers[0].precnode()
+    precctx = unfi[precnode]
+
+    if curctx.children():
+        raise error.Abort(_("cannot unamend in the middle of a stack"))
+
+    with repo.wlock():
+        with repo.lock():
+            repobookmarks = repo._bookmarks
+            ctxbookmarks = curctx.bookmarks()
+            # we want to inhibit markers that mark precnode obsolete
+            inhibitmod._inhibitmarkers(unfi, [precnode])
+            changedfiles = []
+            wctx = repo[None]
+            wm = wctx.manifest()
+            cm = precctx.manifest()
+            dirstate = repo.dirstate
+            diff = cm.diff(wm)
+            changedfiles.extend(diff.iterkeys())
+
+            tr = repo.transaction('unamend')
+            with dirstate.parentchange():
+                dirstate.rebuild(precnode, cm, changedfiles)
+                # we want added and removed files to be shown
+                # properly, not with ? and ! prefixes
+                for filename, data in diff.iteritems():
+                    if data[0][0] is None:
+                        dirstate.add(filename)
+                    if data[1][0] is None:
+                        dirstate.remove(filename)
+            for book in ctxbookmarks:
+                repobookmarks[book] = precnode
+            repobookmarks.recordchange(tr)
+            tr.close()
+            # we want to mark the changeset from which we were unamending
+            # as obsolete
+            obsolete.createmarkers(repo, [(curctx, ())])
