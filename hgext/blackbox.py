85f9bed0b51bbaec0ebaa028f9ed96d35261c02d
Pierre-Yves David <pierre-yves.david@octobus.net>
2017-06-17 13:41:28 +0200

    blackbox: minor code reordering
    The version declaration should come first in my opinion. This will help gather
    the command table with the config table.

--- hgext/blackbox.py
+++ hgext/blackbox.py
@@ -49,13 +49,15 @@ from mercurial import (
     util,
 )
 
-cmdtable = {}
-command = registrar.command(cmdtable)
 # Note for extension authors: ONLY specify testedwith = 'ships-with-hg-core' for
 # extensions which SHIP WITH MERCURIAL. Non-mainline extensions should
 # be specifying the version(s) of Mercurial they are tested with, or
 # leave the attribute unspecified.
 testedwith = 'ships-with-hg-core'
+
+cmdtable = {}
+command = registrar.command(cmdtable)
+
 lastui = None
 
 filehandles = {}
