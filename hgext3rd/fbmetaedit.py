2df622a7241a7e7f26499bae175f37a3dc7c66c1
Jun Wu <quark@fb.com>
2017-06-20 13:58:40 -0700

    cleanup: remove fbmetaedit and inhibitwarn
    Summary: We no longer use those extensions. Remove them.
    
    Test Plan: arc unit
    
    Reviewers: #mercurial
    
    Differential Revision: https://phabricator.intern.facebook.com/D5286709

--- hgext3rd/fbmetaedit.py
+++
@@ -1,8 +0,0 @@
-# fbmetaedit.py - improved metaedit functionality
-#
-# Copyright 2017 Facebook, Inc.
-#
-# This software may be used and distributed according to the terms of the
-# GNU General Public License version 2 or any later version.
-
-# moved to fbamend
