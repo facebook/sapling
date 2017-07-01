e708dda521033687639868ee8935bc7b60fcd0f3
Adam Simpkins <simpkins@fb.com>
2017-06-30 19:00:00 -0700

    add a main function to the inodes unit test
    Summary:
    Add a main() function to the indoes unit test, to allow a `--logging` flag to
    control log levels.
    
    I will eventually update our default test `main()` function to include this
    flag, but for now adding a custom `main()` for the inodes test is the simplest
    fix.
    
    Reviewed By: wez
    
    Differential Revision: D5326022
    
    fbshipit-source-id: 36f497658fdb21639408fc599cf75908b9c9acb3

---
+++ eden/fs/inodes/test/main.cpp
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include <folly/experimental/logging/Init.h>
+#include <folly/init/Init.h>
+#include <gtest/gtest.h>
+
+DEFINE_string(logging, "", "folly::logging configuration");
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  folly::init(&argc, &argv);
+  folly::initLoggingGlogStyle(FLAGS_logging, folly::LogLevel::INFO);
+
+  return RUN_ALL_TESTS();
+}
