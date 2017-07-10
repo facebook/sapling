40503c20fb652793f6713e6e23529284713ae363
Jun Wu <quark@fb.com>
2017-07-10 15:45:07 -0700

    inhibit: completely rewrite the extension
    Summary:
    The main feature we want is to be able to unobsolete an already obsoleted
    changeset. The old inhibit code is causing all kinds of unsolvable weird
    cases and is hard to deubg or maintain.
    
    This patch rewrites it completely. Basically, we now require people to use
    obsmarkers to "unobsolete" changesets. We treat cycles in obsstore as a
    normal case and break the cycle using date information.
    
    It should be a neat and correct solution until we want marker exchange.
    
    A "revive" API was provided for other extensions to use.
    
    Tests and other code changes will be fixed in a follow up.
    
    Test Plan:
    `test-inhibit.t` was rewritten to test the new features.
    
    Other tests are broken and skipped for now. The next diff will fix them.
    
    Reviewers: #mercurial, kulshrax
    
    Reviewed By: kulshrax
    
    Subscribers: medson, mjpieters
    
    Differential Revision: https://phabricator.intern.facebook.com/D5391320
    
    Signature: t1:5391320:1499716172:a946381421cc242411f5175ee3b7a3a0bc5a4f07

--- hgext3rd/inhibit.py
+++ hgext3rd/inhibit.py
@@ -1,299 +1,106 @@
-"""reduce the changesets evolution feature scope for early and noob friendly ui
+# inhibit.py - redefine obsolete(), bumped(), divergent() revsets
+#
+# Copyright 2017 Facebook, Inc.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+"""redefine obsolete(), bumped(), divergent() revsets"""
 
-the full scale changeset evolution have some massive bleeding edge and it is
-very easy for people not very intimate with the concept to end up in intricate
-situation. in order to get some of the benefit sooner, this extension is
-disabling some of the less polished aspect of evolution. it should gradually
-get thinner and thinner as changeset evolution will get more polished. this
-extension is only recommended for large scale organisations. individual user
-should probably stick on using evolution in its current state, understand its
-concept and provide feedback
+from __future__ import absolute_import
+from mercurial import (
+    error,
+    extensions,
+    obsolete,
+)
 
-This extension provides the ability to "inhibit" obsolescence markers. obsolete
-revision can be cheaply brought back to life that way.
-However as the inhibitor are not fitting in an append only model, this is
-incompatible with sharing mutable history.
-"""
-from mercurial import bookmarks
-from mercurial import commands
-from mercurial import extensions
-from mercurial import localrepo
-from mercurial import lock as lockmod
-from mercurial import obsolete
-from mercurial import registrar
-from mercurial import scmutil
-from mercurial import util
-from mercurial.i18n import _
+def _obsoletedrevs(repo):
+    """Redefine "obsolete()" revset. Previously, X is obsoleted if X appears as
+    a precursor in a marker. Now, X is obsoleted if X is a precursor in marker
+    M1, *and* is not a successor in marker M2 where M2.date >= M1.date.
 
-cmdtable = {}
-
-if util.safehasattr(registrar, 'command'):
-    command = registrar.command(cmdtable)
-else: # compat with hg < 4.3
-    from mercurial import cmdutil
-    command = cmdutil.command(cmdtable)
-
-def _inhibitenabled(repo):
-    return util.safehasattr(repo, '_obsinhibit')
-
-def reposetup(ui, repo):
-
-    class obsinhibitedrepo(repo.__class__):
-
-        @localrepo.storecache('obsinhibit')
-        def _obsinhibit(self):
-            # XXX we should make sure it is invalidated by transaction failure
-            obsinhibit = set()
-            raw = self.svfs.tryread('obsinhibit')
-            for i in xrange(0, len(raw), 20):
-                obsinhibit.add(raw[i:i + 20])
-            return obsinhibit
-
-        def commit(self, *args, **kwargs):
-            newnode = super(obsinhibitedrepo, self).commit(*args, **kwargs)
-            if newnode is not None:
-                _inhibitmarkers(repo, [newnode])
-            return newnode
-
-    repo.__class__ = obsinhibitedrepo
-
-def _update(orig, ui, repo, *args, **kwargs):
+    This allows undo to return to old hashes, and is correct as long as
+    obsmarker is not exchanged.
     """
-    When moving to a commit we want to inhibit any obsolete commit affecting
-    the changeset we are updating to. In other words we don't want any visible
-    commit to be obsolete.
-    """
-    wlock = None
-    try:
-        # We take the lock here to make sure that we inhibit the parent before
-        # that hook get a chance to run.
-        wlock = repo.wlock()
-        res = orig(ui, repo, *args, **kwargs)
-        newhead = repo['.'].node()
-        _inhibitmarkers(repo, [newhead])
-        return res
-    finally:
-        lockmod.release(wlock)
+    getnode = repo.changelog.node
+    markersbysuccessor = repo.obsstore.precursors.get
+    markersbyprecursor = repo.obsstore.successors.get
+    result = set()
+    for r in obsolete._mutablerevs(repo):
+        n = getnode(r)
+        m1s = markersbyprecursor(n)
+        m2s = markersbysuccessor(n)
+        if m1s:
+            if m2s:
+                # marker: (prec, [succ], flag, meta, (date, timezone), parent)
+                d1 = max(m[4][0] for m in m1s)
+                d2 = max(m[4][0] for m in m2s)
+                if d2 < d1:
+                    result.add(r)
+            else:
+                result.add(r)
+    return result
 
-def _bookmarkchanged(orig, bkmstoreinst, *args, **kwargs):
-    """ Add inhibition markers to every obsolete bookmarks """
-    repo = bkmstoreinst._repo
-    bkmstorenodes = [repo[v].node() for v in bkmstoreinst.values()]
-    _inhibitmarkers(repo, bkmstorenodes)
-    return orig(bkmstoreinst, *args, **kwargs)
+def _obsstorecreate(orig, self, tr, prec, succs=(), flag=0, parents=None,
+                    date=None, metadata=None, ui=None):
+    # make "prec in succs" in-marker cycle check a no-op
+    succs = _nocontainslist(succs)
+    # if prec is a successor of an existing marker, make default date bigger so
+    # the old marker won't revive the precursor accidentally.
+    if date is None:
+        markers = self.precursors.get(prec)
+        if markers:
+            maxdate = max(m[4] for m in markers)
+            date = (maxdate[0] + 1, maxdate[1])
+    return orig(self, tr, prec, succs, flag, parents, date, metadata, ui)
 
-# obsolescence inhibitor
-########################
+class _nocontainslist(list):
+    # like "list", but __contains__ returns False, used by _obsstorecreate
+    def __contains__(self, x):
+        return False
 
-def _schedulewrite(tr, obsinhibit):
-    """Make sure on disk content will be updated on transaction commit"""
-    def writer(fp):
-        """Serialize the inhibited list to disk.
-        """
-        raw = ''.join(obsinhibit)
-        fp.write(raw)
-    tr.addfilegenerator('obsinhibit', ('obsinhibit',), writer)
-    tr.hookargs['obs_inbihited'] = '1'
-
-def _filterpublic(repo, nodes):
-    """filter out inhibitor on public changeset
-
-    Public changesets are already immune to obsolescence"""
-    getrev = repo.changelog.nodemap.get
-    getphase = repo._phasecache.phase
-    return (n for n in nodes
-            if getrev(n) is not None and getphase(repo, getrev(n)))
-
-def _inhibitmarkers(repo, nodes):
-    """add marker inhibitor for all obsolete revision under <nodes>
-
-    Content of <nodes> and all mutable ancestors are considered. Marker for
-    obsolete revision only are created.
-    """
-    if not _inhibitenabled(repo):
-        return
-
-    # we add (non public()) as a lower boundary to
-    # - use the C code in 3.6 (no ancestors in C as this is written)
-    # - restrict the search space. Otherwise, the ancestors can spend a lot of
-    #   time iterating if you have a check very low in the repo. We do not need
-    #   to iterate over tens of thousand of public revisions with higher
-    #   revision number
-    #
-    # In addition, the revset logic could be made significantly smarter here.
-    newinhibit = repo.revs('(not public())::%ln and obsolete()', nodes)
-    if newinhibit:
-        node = repo.changelog.node
-        lock = tr = None
+def _createmarkers(orig, repo, rels, *args, **kwargs):
+    # make precursor context unfiltered so parents() won't raise
+    unfi = repo.unfiltered()
+    rels = [list(r) for r in rels]  # make mutable
+    for r in rels:
         try:
-            lock = repo.lock()
-            tr = repo.transaction('obsinhibit')
-            repo._obsinhibit.update(node(r) for r in newinhibit)
-            _schedulewrite(tr, _filterpublic(repo, repo._obsinhibit))
-            repo.invalidatevolatilesets()
-            tr.close()
-        finally:
-            lockmod.release(tr, lock)
+            r[0] = unfi[r[0].node()]
+        except error.RepoLookupError:
+            # node could be unknown in current repo
+            pass
+    return orig(unfi, rels, *args, **kwargs)
 
-deinhibittransaction = False
-
-def _deinhibitmarkers(repo, nodes):
-    """lift obsolescence inhibition on a set of nodes
-
-    This will be triggered when inhibited nodes received new obsolescence
-    markers. Otherwise the new obsolescence markers would also be inhibited.
-    """
-    if not _inhibitenabled(repo):
+def revive(ctxlist, operation='revive'):
+    """un-obsolete revisions (public API used by other extensions)"""
+    torevive = [c for c in ctxlist if c.obsolete()]
+    if not torevive:
         return
+    # revive it by creating a self cycle marker
+    repo = torevive[0].repo()
+    with repo.lock(), repo.transaction(operation) as tr:
+        meta = {'user': repo.ui.username(), 'operation': operation}
+        for ctx in torevive:
+            node = ctx.node()
+            # use low-level API instead of obsolete.createmarkers since its
+            # cycle check is problematic
+            repo.obsstore.create(tr, node, (node,), metadata=meta, ui=repo.ui)
 
-    # record nodes so transactioncallback will not inhibit them
-    if deinhibittransaction:
-        tr = repo.currenttransaction()
-        if tr:
-            trnodes = getattr(tr, '_deinhibitnodes', set())
-            trnodes.update(nodes)
-            tr._deinhibitnodes = trnodes
+def uisetup(ui):
+    revsets = obsolete.cachefuncs
 
-    deinhibited = repo._obsinhibit & set(nodes)
-    if deinhibited:
-        tr = repo.transaction('obsinhibit')
-        try:
-            repo._obsinhibit -= deinhibited
-            _schedulewrite(tr, _filterpublic(repo, repo._obsinhibit))
-            repo.invalidatevolatilesets()
-            tr.close()
-        finally:
-            tr.release()
+    # redefine obsolete(): handle cycles and make nodes visible
+    revsets['obsolete'] = _obsoletedrevs
 
-def _createmarkers(orig, repo, relations, *args, **kwargs):
-    """wrap markers create to make sure we de-inhibit target nodes"""
-    # wrapping transactio to unify the one in each function
-    lock = tr = None
-    try:
-        lock = repo.lock()
-        tr = repo.transaction('add-obsolescence-marker')
-        orig(repo, relations, *args, **kwargs)
-        precs = (r[0].node() for r in relations)
-        _deinhibitmarkers(repo, precs)
-        tr.close()
-    finally:
-        lockmod.release(tr, lock)
+    # make divergent() and bumped() empty
+    # NOTE: we should avoid doing this but just change templates to only show a
+    # subset of troubles we care about.
+    revsets['divergent'] = revsets['bumped'] = lambda repo: frozenset()
 
-def _filterobsoleterevswrap(orig, repo, rebasesetrevs, *args, **kwargs):
-    repo._notinhibited = rebasesetrevs
-    try:
-        repo.invalidatevolatilesets()
-        r = orig(repo, rebasesetrevs, *args, **kwargs)
-    finally:
-        del repo._notinhibited
-        repo.invalidatevolatilesets()
-    return r
+    # make obsstore.create not complain about in-marker cycles, since we want
+    # to write X -> X to revive X.
+    extensions.wrapfunction(obsolete.obsstore, 'create', _obsstorecreate)
 
-def transactioncallback(orig, repo, desc, *args, **kwargs):
-    """ Wrap localrepo.transaction to inhibit new obsolete changes """
-    def inhibitposttransaction(transaction):
-        # At the end of the transaction we catch all the new visible and
-        # obsolete commit to inhibit them
-        visibleobsolete = repo.revs('obsolete() - hidden()')
-        ignoreset = set(getattr(repo, '_rebaseset', []))
-        ignoreset |= set(getattr(repo, '_obsoletenotrebased', []))
-        visibleobsolete = list(r for r in visibleobsolete if r not in ignoreset)
-        excludenodes = getattr(transaction, '_deinhibitnodes', set())
-        if visibleobsolete:
-            nodes = [repo[r].node() for r in visibleobsolete]
-            nodes = [n for n in nodes if n not in excludenodes]
-            _inhibitmarkers(repo, nodes)
-    transaction = orig(repo, desc, *args, **kwargs)
-    if desc != 'strip' and _inhibitenabled(repo):
-        transaction.addpostclose('inhibitposttransaction',
-                                 inhibitposttransaction)
-    return transaction
-
-
-# We wrap these two functions to address the following scenario:
-# - Assuming that we have markers between commits in the rebase set and
-#   destination and that these markers are inhibited
-# - At the end of the rebase the nodes are still visible because rebase operate
-#   without inhibition and skip these nodes
-# We keep track in repo._obsoletenotrebased of the obsolete commits skipped by
-# the rebase and lift the inhibition in the end of the rebase.
-
-def _computeobsoletenotrebased(orig, repo, *args, **kwargs):
-    r = orig(repo, *args, **kwargs)
-    repo._obsoletenotrebased = r.keys()
-    return r
-
-def _clearrebased(orig, ui, repo, *args, **kwargs):
-    r = orig(ui, repo, *args, **kwargs)
-    tonode = repo.changelog.node
-    if util.safehasattr(repo, '_obsoletenotrebased'):
-        _deinhibitmarkers(repo, [tonode(k) for k in repo._obsoletenotrebased])
-    return r
-
-
-def extsetup(ui):
-    # lets wrap the computation of the obsolete set
-    # We apply inhibition there
-    obsfunc = obsolete.cachefuncs['obsolete']
-
-    def _computeobsoleteset(repo):
-        """remove any inhibited nodes from the obsolete set
-
-        This will trickle down to other part of mercurial (hidden, log, etc)"""
-        obs = obsfunc(repo)
-        if _inhibitenabled(repo):
-            getrev = repo.changelog.nodemap.get
-            blacklist = getattr(repo, '_notinhibited', set())
-            for n in repo._obsinhibit:
-                if getrev(n) not in blacklist:
-                    obs.discard(getrev(n))
-        return obs
-    try:
-        extensions.find('directaccess')
-    except KeyError:
-        errormsg = _('cannot use inhibit without the direct access extension\n')
-        hint = _("(please enable it or inhibit won\'t work)\n")
-        ui.warn(errormsg)
-        ui.warn(hint)
-        return
-
-    # Wrapping this to inhibit obsolete revs resulting from a transaction
-    extensions.wrapfunction(localrepo.localrepository,
-                            'transaction', transactioncallback)
-
-    obsolete.cachefuncs['obsolete'] = _computeobsoleteset
-    # wrap create marker to make it able to lift the inhibition
+    # make createmarkers use unfiltered precursor ctx, workarounds an issue
+    # that prec.parents() may raise FilteredIndexError.
+    # NOTE: should be fixed upstream once hash-preserving obsstore is a thing.
     extensions.wrapfunction(obsolete, 'createmarkers', _createmarkers)
-    # drop divergence computation since it is incompatible with "light revive"
-    obsolete.cachefuncs['divergent'] = lambda repo: set()
-    # drop bumped computation since it is incompatible with "light revive"
-    obsolete.cachefuncs['bumped'] = lambda repo: set()
-    # wrap update to make sure that no obsolete commit is visible after an
-    # update
-    extensions.wrapcommand(commands.table, 'update', _update)
-    try:
-        rebase = extensions.find('rebase')
-        if rebase:
-            if util.safehasattr(rebase, '_filterobsoleterevs'):
-                extensions.wrapfunction(rebase,
-                                        '_filterobsoleterevs',
-                                        _filterobsoleterevswrap)
-            extensions.wrapfunction(rebase, 'clearrebased', _clearrebased)
-            if util.safehasattr(rebase, '_computeobsoletenotrebased'):
-                extensions.wrapfunction(rebase,
-                                        '_computeobsoletenotrebased',
-                                        _computeobsoletenotrebased)
-
-    except KeyError:
-        pass
-    # There are two ways to save bookmark changes during a transation, we
-    # wrap both to add inhibition markers.
-    extensions.wrapfunction(bookmarks.bmstore, 'recordchange', _bookmarkchanged)
-    if getattr(bookmarks.bmstore, 'write', None) is not None:# mercurial < 3.9
-        extensions.wrapfunction(bookmarks.bmstore, 'write', _bookmarkchanged)
-
-@command('debugobsinhibit', [], '')
-def cmddebugobsinhibit(ui, repo, *revs):
-    """inhibit obsolescence markers effect on a set of revs"""
-    nodes = (repo[r].node() for r in scmutil.revrange(repo, revs))
-    _inhibitmarkers(repo, nodes)
