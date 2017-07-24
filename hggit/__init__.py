38682dd0f9e1c8f302de9414c78d36a584b65926
Kevin Bullock <kbullock@ringworld.org>
2017-07-24 16:05:56 -0500

    version: bump version number to 0.8.8

--- hggit/__init__.py
+++ hggit/__init__.py
@@ -77,7 +77,7 @@ demandimport.ignore.extend([
     'collections',
 ])
 
-__version__ = '0.8.7'
+__version__ = '0.8.8'
 
 testedwith = ('2.8.2 2.9.2 3.0.2 3.1.2 3.2.4 3.3.3 3.4.2 3.5.2 3.6.3 3.7.3'
               '3.8.4 3.9.2 4.0.2 4.1.3 4.2.2 4.3-rc')
