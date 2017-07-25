401c69760d7c5da50096f165585c65d98c34e105
Michael Bolin <mbolin@fb.com>
2017-07-25 11:52:30 -0700

    Pass a proper opener object to the parent constructor in eden_dirstate.
    Summary:
    Now `eden_dirstate` can inherit the implementations of the `branch()` and
    `setbranch()` methods.
    
    More importantly, we have decided to let `dirstate` do its natural management
    of the `.hg` directory (with some exceptions, such as the contents of the
    `.hg/dirstate` file) because there are interactions with `localrepo` and other
    parts of Mercurial that use the filesystem as the API rather than an explicit
    programmatic API, and we want to make sure we maintain the expected invariants.
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: simpkins
    
    Differential Revision: D5464059
    
    fbshipit-source-id: 539e142ba11caea3e8487a3cc70d53cc236d2545

---
+++ eden/integration/hg/branch_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+from .lib.hg_extension_test_base import HgExtensionTestBase
+
+
+class BranchTest(HgExtensionTestBase):
+    def populate_backing_repo(self, repo):
+        repo.write_file('a_file.txt', '')
+        repo.commit('first commit')
+
+    def test_set_branch(self):
+        original_branch = self.hg('branch')
+        self.assertEqual('default', original_branch.rstrip())
+
+        # Note that with tweakdefaults, we discourage the user from creating a
+        # branch, so we require them to specify `--new`.
+        self.hg('branch', '--new', 'foo-bar')
+        new_branch = self.hg('branch')
+        self.assertEqual('foo-bar', new_branch.rstrip())
