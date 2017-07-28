b9b9ba32e9870273fabfacd3d3ce56ad1d0deda9
Michael Bolin <mbolin@fb.com>
2017-07-27 17:18:18 -0700

    Implement savebackup() and restorebackup() in eden_dirstate.
    Summary:
    Up until now, we have largely ignored Mercurial's logic around transactions.
    Previously, the `savebackup()` and `restorebackup()` methods of `eden_dirstate`
    were no-ops, but with this change, we provide real implementations for
    `savebackup()` and `restorebackup()`.
    
    We no longer try to maintain the `_dirty` field in `eden_dirstate` as of this change
    as we should not be exercising any superclass code that relies on its value.
    
    Reviewed By: simpkins
    
    Differential Revision: D5485950
    
    fbshipit-source-id: effa5f83ead8c6eefa613c196d7ec5646da31e4b

---
+++ eden/integration/hg/rollback_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+from .lib.hg_extension_test_base import HgExtensionTestBase
+from ..lib import hgrepo
+
+
+class RollbackTest(HgExtensionTestBase):
+    def populate_backing_repo(self, repo):
+        repo.write_file('first', '')
+        self._commit1 = repo.commit('first commit')
+
+    def test_amend_with_editor_failure_should_trigger_rollback(self):
+        original_commits = self.repo.log()
+
+        self.repo.write_file('first', 'THIS IS CHANGED')
+        self.assert_status({'first': 'M'})
+
+        with self.assertRaises(hgrepo.HgError) as context:
+            self.hg('amend', '--edit', '--config', 'ui.editor=/bin/false')
+        expected_msg = 'transaction abort!\n  rollback completed\n'
+        self.assertIn(expected_msg, str(context.exception))
+
+        self.assertEqual(
+            original_commits,
+            self.repo.log(),
+            msg='Failed editor should abort the change and '
+            'leave Hg in the original state.'
+        )
