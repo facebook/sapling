144bea971aa7ec1053e183bb487aa760f59863e9
Jeremy Fitzhardinge <jsgf@fb.com>
2017-08-02 17:32:20 -0700

    mononoke: bookmarks: add helpers for boxing Bookmarks trait objects
    Summary:
    Add BoxedBookmark to assist in boxing a Bookmark trait, and implement Bookmark
    for Box<_> and Arc<_> Bookmark trait objects.
    
    Reviewed By: sid0
    
    Differential Revision: D5540675
    
    fbshipit-source-id: 7582adbd2858f6814d57b7e54089b922886a4d54

--- bookmarks/src/lib.rs
+++ bookmarks/src/lib.rs
@@ -9,8 +9,11 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-use futures::{Future, Stream};
-use std::error;
+use std::marker::PhantomData;
+use std::sync::Arc;
+
+use futures::{BoxFuture, Future, Stream};
+use futures::stream::BoxStream;
 
 /// Versions are used to ensure consistency of state across all users of the bookmark store.
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Serialize, Deserialize)]
@@ -38,7 +41,7 @@ impl Default for Version {
 /// of names to commit identifiers. Consistency is maintained using versioning.
 pub trait Bookmarks: Send + 'static {
     type Value: Send + 'static;
-    type Error: error::Error + Send + 'static;
+    type Error: Send + 'static;
 
     // Return type for getting a bookmark value. Must be a future that resolves to
     // a tuple of the current value and current version of the bookmark.
@@ -48,6 +51,139 @@ pub trait Bookmarks: Send + 'static {
     // Basic operations.
     fn get(&self, key: &AsRef<[u8]>) -> Self::Get;
     fn keys(&self) -> Self::Keys;
+}
+
+pub struct BoxedBookmarks<B, E>
+where
+    B: Bookmarks,
+{
+    inner: B,
+    cvt_err: fn(B::Error) -> E,
+    _phantom: PhantomData<E>,
+}
+
+impl<B, E> BoxedBookmarks<B, E>
+where
+    B: Bookmarks,
+    E: Send + 'static,
+{
+    pub fn new(
+        inner: B,
+    ) -> Box<
+        Bookmarks<
+            Value = B::Value,
+            Error = E,
+            Get = BoxFuture<Option<(B::Value, Version)>, E>,
+            Keys = BoxStream<Vec<u8>, E>,
+        >,
+    >
+    where
+        E: From<B::Error>,
+    {
+        Self::new_cvt(inner, E::from)
+    }
+
+    pub fn new_cvt(
+        inner: B,
+        cvt_err: fn(B::Error) -> E,
+    ) -> Box<
+        Bookmarks<
+            Value = B::Value,
+            Error = E,
+            Get = BoxFuture<Option<(B::Value, Version)>, E>,
+            Keys = BoxStream<Vec<u8>, E>,
+        >,
+    > {
+        let res = Self {
+            inner,
+            cvt_err,
+            _phantom: PhantomData,
+        };
+        Box::new(res)
+    }
+}
+
+impl<B, E> Bookmarks for BoxedBookmarks<B, E>
+where
+    B: Bookmarks + Send + 'static,
+    B::Value: Send + 'static,
+    E: Send + 'static,
+{
+    type Error = E;
+    type Value = B::Value;
+    type Get = BoxFuture<Option<(Self::Value, Version)>, E>;
+    type Keys = BoxStream<Vec<u8>, E>;
+
+    fn get(&self, key: &AsRef<[u8]>) -> Self::Get {
+        self.inner.get(key).map_err(self.cvt_err).boxed()
+    }
+
+    fn keys(&self) -> Self::Keys {
+        self.inner.keys().map_err(self.cvt_err).boxed()
+    }
+}
+
+// Implement Bookmarks for boxed Bookmarks trait object
+impl<V, E, G, K> Bookmarks for Box<Bookmarks<Value = V, Error = E, Get = G, Keys = K>>
+where
+    V: Send + 'static,
+    E: Send + 'static,
+    G: Future<Item = Option<(V, Version)>, Error = E> + Send + 'static,
+    K: Stream<Item = Vec<u8>, Error = E> + Send + 'static,
+{
+    type Value = V;
+    type Error = E;
+    type Get = G;
+    type Keys = K;
+
+    fn get(&self, key: &AsRef<[u8]>) -> Self::Get {
+        (**self).get(key)
+    }
+
+    fn keys(&self) -> Self::Keys {
+        (**self).keys()
+    }
+}
+
+// Implement Bookmarks for Arced Bookmarks trait object
+impl<V, E, G, K> Bookmarks for Arc<Bookmarks<Value = V, Error = E, Get = G, Keys = K> + Sync>
+where
+    V: Send + 'static,
+    E: Send + 'static,
+    G: Future<Item = Option<(V, Version)>, Error = E> + Send + 'static,
+    K: Stream<Item = Vec<u8>, Error = E> + Send + 'static,
+{
+    type Value = V;
+    type Error = E;
+    type Get = G;
+    type Keys = K;
+
+    fn get(&self, key: &AsRef<[u8]>) -> Self::Get {
+        (**self).get(key)
+    }
+
+    fn keys(&self) -> Self::Keys {
+        (**self).keys()
+    }
+}
+
+// Implement Bookmarks for Arc-wrapped Bookmark type
+impl<B> Bookmarks for Arc<B>
+where
+    B: Bookmarks + Sync,
+{
+    type Value = B::Value;
+    type Error = B::Error;
+    type Get = B::Get;
+    type Keys = B::Keys;
+
+    fn get(&self, key: &AsRef<[u8]>) -> Self::Get {
+        (**self).get(key)
+    }
+
+    fn keys(&self) -> Self::Keys {
+        (**self).keys()
+    }
 }
 
 /// Trait representing write operations on a bookmark store. Consistency is maintained using
