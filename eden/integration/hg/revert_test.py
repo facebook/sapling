f946ae76f90b7a86657b4aebc2a82104223f4fd4
Michael Bolin <mbolin@fb.com>
2017-08-21 17:51:46 -0700

    Wrap exact() from Mercurial's match.py.
    Summary:
    In `hg/eden/__init__.py`, we wrap `match()` in Mercurial's `match.py` in an
    attempt to annotate every `basematcher` created in the system with a special
    `_eden_match_info` property that we can use in `_eden_walk_helper()` to perform
    walks more efficiently. Unfortunately, we missed a case where `scmutil.py`
    has a `matchfiles()` function that calls `exact()` in `match.py` directly rather
    than going through the generic `match()` function.
    
    This was causing a failure when running `hg revert <filename>` in Eden because
    the matcher that was created via `exact()` did not have an `_eden_match_info`.
    This commit wraps `exact()` to add the property.
    
    Reviewed By: wez
    
    Differential Revision: D5674660
    
    fbshipit-source-id: 16d1e7648ebd7a23b43b9b1200d3e284e5bc07b0

---
+++ eden/integration/hg/revert_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2004-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+from .lib.hg_extension_test_base import HgExtensionTestBase
+
+
+class RevertTest(HgExtensionTestBase):
+    def populate_backing_repo(self, repo):
+        repo.write_file('hello.txt', 'hola')
+        repo.commit('Initial commit.\n')
+
+    def test_make_local_change_and_attempt_revert(self):
+        self.write_file('hello.txt', 'hello')
+        self.assert_status({'hello.txt': 'M'})
+        self.hg('revert', '--no-backup', 'hello.txt')
+        self.assert_status_empty()
+        txt_contents = self.read_file('hello.txt')
+        self.assertEqual('hola', txt_contents)
