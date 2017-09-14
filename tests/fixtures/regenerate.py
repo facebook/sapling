2511835a09f6bdab9e19d4ca2468771f91264460
Siddharth Agarwal <sid0@fb.com>
2017-09-14 13:45:44 -0700

    add a small script to regenerate test fixtures
    Summary: Makes a formerly manual process automatic.
    
    Reviewed By: jsgf
    
    Differential Revision: D5832456
    
    fbshipit-source-id: 0bfa01aaf08f0794a3b98ab7290d163a65650a7e

---
+++ tests/fixtures/regenerate.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# Copyright (c) 2004-present, Facebook, Inc.
+# All Rights Reserved.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+"""Given zipped up revlog repos, generate corresponding blob repos."""
+
+import errno
+import os
+import subprocess
+import tempfile
+from typing import Optional
+
+import click
+
+import py_tar_utils
+
+
+@click.command(help='regenerate blob repo fixtures from revlog repos')
+@click.option(
+    '--overwrite', is_flag=True, help='overwrite destination if successful'
+)
+@click.option(
+    '--blobimport',
+    help='location of blobimport binary',
+    type=click.Path(exists=True, dir_okay=False),
+)
+@click.argument(
+    'source', type=click.Path(exists=True, dir_okay=False), nargs=-1
+)
+def main(source, overwrite, blobimport):
+    blobimport = get_blobimport(blobimport)
+
+    if not overwrite:
+        for tar in source:
+            check_dest(tar)
+
+    for tar in source:
+        convert(tar, blobimport)
+
+
+def get_blobimport(blobimport: Optional[str]) -> str:
+    if blobimport is None:
+        try:
+            from .facebook import pathutils
+        except ImportError:
+            raise RuntimeError('--blobimport is required')
+        return pathutils.get_path('//scm/mononoke:blobimport')
+    else:
+        return blobimport
+
+
+def check_dest(tar: str):
+    basename = os.path.basename(tar)
+    dest = basename.split('.', 1)[0]
+
+    if os.path.lexists(dest):
+        raise RuntimeError(
+            "Destination '{}' already exists (use --overwrite to replace it)".
+            format(dest)
+        )
+
+
+def convert(tar: str, blobimport: str):
+    basename = os.path.basename(tar)
+    dest = basename.split('.', 1)[0]
+
+    abs_dest = os.path.abspath(dest)
+    output_dir = os.path.dirname(abs_dest)
+    output_name = os.path.basename(abs_dest)
+
+    # We use two separate directories here: tmpdir and tmpdest.
+    # - tmpdir is not inside the repository because extracting it would cause a
+    #   nested .hg, and fsmonitor isn't very happy with nested .hgs.
+    # - tmpdest is in the same directory as the final destination so that it's
+    #   on the same filesystem, which guarantees that os.rename works.
+    # TODO: support unzipped repos as well?
+    with tempfile.TemporaryDirectory(prefix='mononoke-regenerate') as tmpdir:
+        revlog_repo = py_tar_utils.extractall_safe(tar, tmpdir)
+        with tempfile.TemporaryDirectory(
+            dir=output_dir, prefix=output_name + '.'
+        ) as tmpdest:
+            new_dir = os.path.join(tmpdest, 'new')
+            subprocess.check_call(
+                [blobimport, '--blobstore', 'files', revlog_repo, new_dir]
+            )
+            safe_overwrite_dir(new_dir, abs_dest)
+
+
+def safe_overwrite_dir(src: str, abs_dest: str):
+    output_dir = os.path.dirname(abs_dest)
+    output_name = os.path.basename(abs_dest)
+
+    with tempfile.TemporaryDirectory(
+        dir=output_dir, prefix=output_name + '.old.'
+    ) as to_delete:
+        try:
+            os.rename(abs_dest, os.path.join(to_delete, 'old'))
+        except OSError as ex:
+            if ex.errno != errno.ENOENT:
+                raise
+        os.rename(src, abs_dest)
+
+        # When this block exits, to_delete and therefore the old directory will
+        # be deleted.
+
+
+if __name__ == '__main__':
+    main()
