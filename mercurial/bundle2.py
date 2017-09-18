a00e8f6b04c0e553682cec1cb561655cde1fa182
Augie Fackler <augie@google.com>
2017-09-18 14:03:21 -0400

    bundle2: make ValueError messages native strings

--- mercurial/bundle2.py
+++ mercurial/bundle2.py
@@ -614,9 +614,9 @@ class bundle20(object):
     def addparam(self, name, value=None):
         """add a stream level parameter"""
         if not name:
-            raise ValueError('empty parameter name')
+            raise ValueError(r'empty parameter name')
         if name[0] not in pycompat.bytestr(string.ascii_letters):
-            raise ValueError('non letter first character: %s' % name)
+            raise ValueError(r'non letter first character: %s' % name)
         self._params.append((name, value))
 
     def addpart(self, part):
