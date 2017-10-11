14093a304378c5c374ae2394e50ea39b4f42531f
Boris Feld <boris.feld@octobus.net>
2017-10-12 00:44:45 +0200

    configitems: register the 'hgweb-paths' section

--- mercurial/configitems.py
+++ mercurial/configitems.py
@@ -454,6 +454,10 @@ coreconfigitem('hooks', '.*',
     default=dynamicdefault,
     generic=True,
 )
+coreconfigitem('hgweb-paths', '.*',
+    default=list,
+    generic=True,
+)
 coreconfigitem('hostfingerprints', '.*',
     default=list,
     generic=True,
