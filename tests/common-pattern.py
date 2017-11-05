0305ed990eb52ca80349b5e06d7ec5b78260a798
Boris Feld <boris.feld@octobus.net>
2017-11-05 06:43:40 +0100

    test-pattern: register current the bundlecaps string
    The bundle capabilites sent with every getbundle commands. Every time the
    protocol is updated, that string is altered. We get that string replace by
    $USUAL_BUNDLE_CAPS$ so that we only have to change the substitution whenever
    this happens.

--- tests/common-pattern.py
+++ tests/common-pattern.py
@@ -6,4 +6,17 @@ substitutions = [
     (br'zstd,zlib,none,bzip2',
      br'$USUAL_COMPRESSIONS$'
     ),
+    # capabilities sent through http
+    (br'bundlecaps=HG20%2Cbundle2%3DHG20%250A'
+     br'changegroup%253D01%252C02%250A'
+     br'digests%253Dmd5%252Csha1%252Csha512%250A'
+     br'error%253Dabort%252Cunsupportedcontent%252Cpushraced%252Cpushkey%250A'
+     br'hgtagsfnodes%250A'
+     br'listkeys%250A'
+     br'phases%253Dheads%250A'
+     br'pushkey%250A'
+     br'remote-changegroup%253Dhttp%252Chttps',
+     # (the replacement patterns)
+     br'$USUAL_BUNDLE_CAPS$'
+    ),
 ]
