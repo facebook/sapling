bf03a9420da2a87da903168432c76dd9bed31f67
Chad Austin <chadaustin@fb.com>
2017-11-07 15:57:32 -0800

    add test verifying `eden help` succeeds
    Summary:
    Since Eden's integration tests do not run in CI yet, this adds a
    test that verifies the Eden CLI can start without Python errors.
    
    Reviewed By: wez
    
    Differential Revision: D6250515
    
    fbshipit-source-id: 907bffaff122c9929a7623d97f665de5b2a6f2d3

---
+++ eden/integration/help_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+import unittest
+from .lib import edenclient
+
+
+class HelpTest(unittest.TestCase):
+    """
+    This test verifies the Eden CLI can at least load its Python code.
+    It can be removed when the remaining integration tests are enabled
+    on sandcastle.
+    """
+    def test_eden_cli_help_returns_without_error(self):
+        with edenclient.EdenFS() as client:
+            return_code = client.run_unchecked('help')
+            self.assertEqual(0, return_code)
