9d25f2c80781085b39b0060315713714e5e0bc77
Jun Wu <quark@fb.com>
2017-12-15 11:29:33 -0800

    pybuf: add a simple abstraction around Py_buffer interface
    This makes it possible to do zero-copy reading the changelog.i data from
    Python to Rust because eventually changelog.i does not have to be fully
    accessed and together with mmap, that's at least 100ms perf win.
    
    There is a much more general purposed (support writable, non-contiguous
    buffer) `PyBuffer` implementation in rust-cpython github version [1].
    However, what `NodeMap` really wants is `AsRef<[u8]>` and `AsRef<[u32]>`,
    which are not provided by that.
    
    Since the code is short, let's just add our own wrapper. It also allows us
    to use the published version of rust-cpython instead of pulling from github.
    
    [1]: https://github.com/dgrunwald/rust-cpython/blob/master/src/buffer.rs
    
    Test Plan:
    cargo build
    
    Differential Revision: https://phab.mercurial-scm.org/D1493

--- rust/indexes/src/lib.rs
+++ rust/indexes/src/lib.rs
@@ -3,9 +3,14 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
+extern crate python27_sys;
+
+#[macro_use]
+extern crate cpython;
 #[macro_use]
 extern crate error_chain;
 extern crate radixbuf;
 
 pub mod errors;
 pub mod nodemap;
+mod pybuf;
