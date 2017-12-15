ee1fa72edae208eb067d667a73bf4e8506b7fca7
Jun Wu <quark@fb.com>
2017-12-15 11:29:33 -0800

    nodemap: implement nodemap in rust
    A simple nodemap index (node -> rev, and prefix -> node). See comment in
    the code for the actual format.
    
    Note this does not cover every features of a Mecurial nodemap. For example,
    it does not have `__setitem__`, `__delitem__`, and does not treat rev -1
    specially. Those could be implemented in a higher level.
    
    Differential Revision: https://phab.mercurial-scm.org/D1469

---
+++ rust/indexes/src/errors.rs
@@ -0,0 +1,16 @@
+// Copyright 2017 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+error_chain! {
+    foreign_links {
+        Radix(::radixbuf::errors::Error);
+    }
+
+    errors {
+        IndexCorrupted {
+            description("corrupted index")
+        }
+    }
+}
---
+++ rust/indexes/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright 2017 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+#[macro_use]
+extern crate error_chain;
+extern crate radixbuf;
+
+pub mod errors;
+pub mod nodemap;
