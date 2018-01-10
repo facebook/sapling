35020ab2c75bde29375747018a086a70bd4d4c70
Jun Wu <quark@fb.com>
2018-01-10 14:21:19 -0800

    lfs: remove support for legacy configs
    Summary: They are no longer used.
    
    Test Plan: LFS tests are passing. Also check the config being used today.
    
    Reviewers: durham, #mercurial
    
    Reviewed By: durham
    
    Differential Revision: https://phabricator.intern.facebook.com/D6698242
    
    Signature: 6698242:1515633277:0b8ab80c1c182c0c28066f85be56934c30fe0457

--- hgext/lfs/blobstore.py
+++ hgext/lfs/blobstore.py
@@ -363,19 +363,6 @@ def remote(repo):
     """remotestore factory. return a store in _storemap depending on config"""
     defaulturl = ''
 
-    # convert deprecated configs to the new url. TODO: remove this if other
-    # places are migrated to the new url config.
-    # deprecated config: lfs.remotestore
-    deprecatedstore = repo.ui.config('lfs', 'remotestore')
-    if deprecatedstore == 'dummy':
-        # deprecated config: lfs.remotepath
-        defaulturl = 'file://' + repo.ui.config('lfs', 'remotepath')
-    elif deprecatedstore == 'git-lfs':
-        # deprecated config: lfs.remoteurl
-        defaulturl = repo.ui.config('lfs', 'remoteurl')
-    elif deprecatedstore == 'null':
-        defaulturl = 'null://'
-
     url = util.url(repo.ui.config('lfs', 'url', defaulturl))
     scheme = url.scheme
     if scheme not in _storemap:
