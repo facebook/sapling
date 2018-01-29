fee9564d3b6d4a1690df9a728f0414685b7b7846
Jun Wu <quark@fb.com>
2018-01-29 12:41:27 -0800

    setup: unbreak setup.py
    Summary:
    This "fixes" setup.py error:
    ```
    python setup.py  \
      build_py -c -d . \
      build_clib  \
      build_ext  -i \
      build_rust_ext -i \
      build_hgexe  -i \
      build_mo
    Traceback (most recent call last):
      File "setup.py", line 969, in <module>
        depends=common_depends),
      File "/usr/lib64/python2.7/distutils/extension.py", line 106, in __init__
        assert type(name) is StringType, "'name' must be a string"
    AssertionError: 'name' must be a string
    ```
    
    While the most correct fix would be possibly prepending prefixes
    to strings in `setup.py`, it feels so much churn for so little
    gain. So let's just disable unicode literal for now.
    
    Reviewed By: phillco
    
    Differential Revision: D6836945
    
    fbshipit-source-id: 7d44969661c26b681f9d2e12d2e10026c4673a1c

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __future__ import unicode_literals
 from distutils.version import LooseVersion
 import glob
 import os
