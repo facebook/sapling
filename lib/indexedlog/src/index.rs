6542d0ebf43dd2f1194fd3fb507c3345f717d291
Jun Wu <quark@fb.com>
2018-03-09 18:57:20 -0800

    indexedlog: add comment about index file format
    Summary: Document the format. Actual implementation in later diffs.
    
    Reviewed By: DurhamG
    
    Differential Revision: D7190575
    
    fbshipit-source-id: 243992fd052ca7a9688d54d20694e65daebb9660

---
+++ lib/indexedlog/src/index.rs
@@ -0,0 +1,41 @@
+//! [u8] -> [u64] mapping. Insertion only.
+//!
+//! The index could be backed by a combination of an on-disk file, and in-memory content. Changes
+//! to the index will be buffered in memory forever until an explicit flush. Internally, the index
+//! uses base16 radix tree for keys and linked list of values, though it's possible to extend the
+//! format to support other kinds of trees and values.
+//!
+//! File format:
+//!
+//! ```ignore
+//! INDEX       := HEADER + ENTRY_LIST
+//! HEADER      := '\0'  (takes offset 0, so 0 is not a valid offset for ENTRY)
+//! ENTRY_LIST  := RADIX | ENTRY_LIST + ENTRY
+//! ENTRY       := RADIX | LEAF | LINK | KEY | ROOT
+//! RADIX       := '\2' + JUMP_TABLE (16 bytes) + PTR(LINK) + PTR(RADIX | LEAF) * N
+//! LEAF        := '\3' + PTR(KEY) + PTR(LINK)
+//! LINK        := '\4' + VLQ(VALUE) + PTR(NEXT_LINK | NULL)
+//! KEY         := '\5' + VLQ(KEY_LEN) + KEY_BYTES
+//! ROOT        := '\1' + PTR(RADIX) + ROOT_LEN (1 byte)
+//!
+//! PTR(ENTRY)  := VLQ(the offset of ENTRY)
+//! ```
+//!
+//! Some notes about the format:
+//!
+//! - A "RADIX" entry has 16 children. This is mainly for source control hex hashes. The "N"
+//!   in a radix entry could be less than 16 if some of the children are missing (ex. offset = 0).
+//!   The corresponding jump table bytes of missing children are 0s. If child i exists, then
+//!   `jumptable[i]` is the relative (to the beginning of radix entry) offset of PTR(child offset).
+//! - A "ROOT" entry its length recorded as the last byte. Normally the root entry is written
+//!   at the end. This makes it easier for the caller - it does not have to record the position
+//!   of the root entry. The caller could optionally provide a root location.
+//! - An entry has a 1 byte "type". This makes it possible to do a linear scan from the
+//!   beginning of the file, instead of having to go through a root. Potentially useful for
+//!   recovery purpose, or adding new entry types (ex. tree entries other than the 16-children
+//!   radix entry, value entries that are not u64 linked list, key entries that refers external
+//!   buffer).
+//! - The "JUMP_TABLE" in "RADIX" entry stores relative offsets to the actual value of
+//!   RADIX/LEAF offsets. It has redundant information. The more compact form is a 2-byte
+//!   (16-bit) bitmask but that hurts lookup performance.
+
--- lib/indexedlog/src/lib.rs
+++ lib/indexedlog/src/lib.rs
@@ -15,3 +15,4 @@ extern crate vlqencoding;
 
 pub mod base16;
 mod checksum_table;
+mod index;
