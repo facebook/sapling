545f670504c67cb64b0fa400807aea1af1b746ea
Jun Wu <quark@fb.com>
2018-03-22 10:57:22 -0700

    pathencoding: utility for converting between bytes and paths
    Summary:
    A simple utility that does paths <-> local bytes conversion. It's needed
    since Mercurial stores paths using local encoding in manifests.
    
    For POSIX, the code is zero-cost - no real conversion or error can happen.
    This is in theory cheaper than what treedirstate does.
    
    For Windows, the "local_encoding" crate is selected as Yuya suggested the
    `MultiByteToWideChar` Win32 API [1] and "local_encoding" uses it. It does
    the right thing given my experiment with GBK (Chinese, simplified) encoding.
    
    ```
      ....
      C:\Users\quark\enc>hg debugshell --config extensions.debugshell=
      >>> repo[0].manifest().text()
      '\xc4\xbf\xc2\xbc1/\xce\xc4\xbc\xfe1\x00b80de5d138758541c5f05265ad144ab9fa86d1db\n'
      >>> repo[0].files()
      ['\xc4\xbf\xc2\xbc1/\xce\xc4\xbc\xfe1']
      extern crate local_encoding;
      use std::path::PathBuf;
      use local_encoding::{Encoder, Encoding};
      const mpath: &[u8] = b"\xc4\xbf\xc2\xbc1/\xce\xc4\xbc\xfe1";
      fn main() {
          let p = PathBuf::from(Encoding::OEM.to_string(mpath).unwrap());
          println!("exists: {}", p.exists());
          println!("mpath len: {}, osstr len: {}", mpath.len(), p.as_path().as_os_str().len());
      }
      exists: true
      mpath len: 11, osstr len: 15
    ```
    
    In the future, we might normalize the paths to UTF-8 before storing them in
    manifest to avoid issues.
    
    Differential Revision: D7319604
    
    fbshipit-source-id: a7ed5284be116c4176598b4c742e8228abcc3b02

---
+++ lib/pathencoding/src/lib.rs
@@ -0,0 +1,101 @@
+/// Cross-platform local bytes and paths conversion.
+///
+/// On POSIX, it's a cost-free conversion. No round-trips with UTF-8 strings.
+/// On Windows, it's using `MultiByteToWideChar` under the hood.
+///
+/// Note: The types returned by the functions are different (`Path` vs `PathBuf`)
+/// because allocation is needed on Windows.
+use std::io;
+use std::path::Path;
+
+#[cfg(unix)]
+use std::ffi::OsStr;
+#[cfg(unix)]
+use std::os::unix::ffi::OsStrExt;
+
+/// Convert (usually UTF-8 encoded) `bytes` to `Path`.
+///
+/// Zero-copy. Cannot return errors. But the Windows version can.
+#[cfg(unix)]
+#[inline]
+pub fn local_bytes_to_path(bytes: &[u8]) -> io::Result<&Path> {
+    Ok(Path::new(OsStr::from_bytes(bytes)))
+}
+
+/// Convert `Path` to (usually UTF-8 encoded) `bytes`.
+///
+/// Zero-copy. Cannot return errors. But the Windows version can.
+#[cfg(unix)]
+#[inline]
+pub fn path_to_local_bytes(path: &Path) -> io::Result<&[u8]> {
+    Ok(path.as_os_str().as_bytes())
+}
+
+// On Windows, use "local_encoding" crate to do the conversion.
+// PERF: "local_encoding" API requires round-trip via "String"
+// (aka. UTF-8). We might want to bypass that UTF-8 conversion
+// so it's [u8] <-> OsString <-> PathBuf.
+
+#[cfg(windows)]
+pub extern crate local_encoding;
+#[cfg(windows)]
+use local_encoding::{Encoder, Encoding};
+#[cfg(windows)]
+use std::io::ErrorKind::InvalidInput;
+#[cfg(windows)]
+use std::path::PathBuf;
+
+/// Convert local-encoded `bytes` to `PathBuf`.
+///
+/// Note: `bytes` are what Mercurial stores in manifests, and are affected
+/// by "Language for non-Unicode programs" Windows setting at commit time.
+/// Newer APIs might want to normalize paths to UTF-8 before storing them.
+#[cfg(windows)]
+pub fn local_bytes_to_path(bytes: &[u8]) -> io::Result<PathBuf> {
+    Encoding::OEM.to_string(bytes).map(|s| PathBuf::from(s))
+}
+
+/// Convert `Path` to local-encoded `bytes`.
+///
+/// This is what Mercurial stores. But new programs should probably normalize
+/// the path before storing it.
+#[cfg(windows)]
+pub fn path_to_local_bytes(path: &Path) -> io::Result<Vec<u8>> {
+    match path.as_os_str().to_str() {
+        Some(s) => Encoding::OEM.to_bytes(s),
+        None => Err(InvalidInput.into()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Result;
+
+    #[test]
+    fn test_ascii7bit_roundtrip() {
+        check_roundtrip(b"/var/log/a.log").expect("roundtrip");
+    }
+
+    #[test]
+    fn test_utf8_roundtrip() {
+        let bytes = b"\xE7\xAE\xA1\xE7\x90\x86\xE5\x91\x98\x2F\xE6\xA1\x8C\xE9\x9D\xA2";
+
+        #[cfg(windows)]
+        let bytes = {
+            match Encoding::OEM.to_bytes(::std::str::from_utf8(bytes).expect("from_utf8")) {
+                Ok(s) => s,
+                _ => return, // Cannot be encoded using local encoding. Skip the test.
+            }
+        };
+
+        check_roundtrip(&bytes[..]).expect("roundtrip");
+    }
+
+    fn check_roundtrip(bin_path: &[u8]) -> Result<()> {
+        let path = local_bytes_to_path(bin_path)?;
+        let bin_path_roundtrip = path_to_local_bytes(&path)?;
+        assert_eq!(bin_path[..], bin_path_roundtrip[..]);
+        Ok(())
+    }
+}
