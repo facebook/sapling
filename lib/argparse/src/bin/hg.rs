052d0e3708f5f5f4587888264fe8356b41d9e59d
Mateusz Kwapich <mitrandir@fb.com>
2018-04-25 15:31:24 -0700

    Command trait in rust
    Summary:
    We (me and Aida)  wanted to have something that could be used instead of Command decorator that we have in python
    and we came up with the following trait.
    
    Differential Revision: D7754930
    
    fbshipit-source-id: 15f412d07045e7d8b229801ec3094664f78f801b

--- lib/argparse/src/bin/hg.rs
+++ lib/argparse/src/bin/hg.rs
@@ -1,23 +1,68 @@
 // a demo of commandline argument parsing to-be-used in future hg binary
 extern crate argparse;
 
-use argparse::argparse::{Arg, Command};
+use argparse::argparse::{Arg, Command as ArgParseCommand, ParsedArgs};
+use std::collections::HashMap;
 use std::env;
 
-fn hg_parser() -> Command {
-    Command::with_name("hg")
-        .arg(Arg::with_name("repository").short(b'R').requires_value())
-        .arg(Arg::with_name("cwd").requires_value())
-        .arg(Arg::with_name("config").requires_value())
-        .subcommand(Command::with_name("rebase"))
-        .subcommand(Command::with_name("update").alias("checkout").alias("co"))
-        .subcommand(Command::with_name("commit").alias("ci"))
-        .subcommand(Command::with_name("amend"))
+trait Command {
+    // canonical name of the command, should be consistent with the one in argument parser
+    fn name(&self) -> String;
+    fn run(&self, &ParsedArgs) -> ();
+    fn argparser(&self) -> ArgParseCommand;
+}
+
+/// move commit (and descendats)
+struct RebaseCommand {}
+impl Command for RebaseCommand {
+    fn name(&self) -> String {
+        format!("rebase")
+    }
+    fn argparser(&self) -> ArgParseCommand {
+        ArgParseCommand::with_name("rebase").arg(Arg::with_name("rev"))
+    }
+    fn run(&self, args: &ParsedArgs) -> () {
+        println!("Running rebase with args {:?}....", args);
+    }
+}
+
+/// amend the current commit with more changes
+struct AmendCommand {}
+impl Command for AmendCommand {
+    fn name(&self) -> String {
+        format!("amend")
+    }
+    fn argparser(&self) -> ArgParseCommand {
+        ArgParseCommand::with_name("amend").arg(Arg::with_name("e"))
+    }
+    fn run(&self, args: &ParsedArgs) -> () {
+        println!("Running amend with args {:?}....", args);
+    }
+}
+
+fn build_command_table() -> (HashMap<String, Box<Command>>) {
+    let commands: Vec<Box<Command>> = vec![Box::new(RebaseCommand {}), Box::new(AmendCommand {})];
+    commands.into_iter().map(|c| (c.name(), c)).collect()
+}
+
+fn dispatch(args: Vec<String>) -> () {
+    let commands = build_command_table();
+    let argparser = commands
+        .values()
+        .fold(ArgParseCommand::with_name("hg"), |a, c| {
+            a.subcommand(c.argparser())
+        });
+
+    let cmd = argparser.parse(&args);
+    match cmd.subcommand {
+        Some(ref subcommand) => {
+            commands.get(&subcommand.name).map(|c| c.run(&subcommand));
+        }
+        None => println!("{:?}", cmd),
+    }
 }
 
 fn main() {
     let args: Vec<_> = env::args().skip(1).collect();
-    let parser = hg_parser();
-    let cmd = parser.parse(&args);
-    println!("{:?}", cmd)
+    dispatch(args);
 }
