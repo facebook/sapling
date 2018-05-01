128d7a2424a93158a9deb087b23b2b777616379f
Lukas Piatkowski <luk@fb.com>
2018-05-01 15:59:30 -0700

    cmd utils: add revlogrepo command for reading revlogs in Rust
    Summary: The goal is to be able to read revlogs using Rust code and also parse and serialize them in Rust formats for debugging purposes
    
    Reviewed By: farnz
    
    Differential Revision: D7830358
    
    fbshipit-source-id: 95e257a4482eca22b328b174bce3fceec1b47245

---
+++ cmds/revlogrepo.rs
@@ -0,0 +1,82 @@
+// Copyright (c) 2004-present, Facebook, Inc.
+// All Rights Reserved.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+#![deny(warnings)]
+
+extern crate clap;
+
+extern crate mercurial;
+
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use clap::{App, SubCommand};
+
+use mercurial::{HgNodeHash, RevlogChangeset, revlog::Revlog};
+
+fn setup_app<'a, 'b>() -> App<'a, 'b> {
+    App::new("read revlog repo content")
+        .version("0.0.0")
+        .about("read revlogs")
+        .args_from_usage(
+            r#"
+             <PATH>   'path to .hg folder with revlogs'
+        "#,
+        )
+        .subcommand(
+            SubCommand::with_name("changeset")
+                .about("reads changesets")
+                .args_from_usage(
+                    r#"
+                     <HASH>                 'sha1 hash to read'
+                     -p, --parsed           'parse blob as RevlogChangeset'
+                "#,
+                ),
+        )
+}
+
+fn main() {
+    let matches = setup_app().get_matches();
+
+    let base: PathBuf = matches.value_of("PATH").unwrap().into();
+    let store = base.as_path().join("store");
+
+    match matches.subcommand() {
+        ("changeset", Some(sub_m)) => {
+            let changelog =
+                Revlog::from_idx_with_data(store.join("00changelog.i"), None as Option<String>)
+                    .expect("Failed to load changelog");
+
+            let hash = sub_m.value_of("HASH").unwrap();
+            let hash = HgNodeHash::from_str(hash).expect("Incorrect Sha1 hash provided");
+
+            let raw = changelog
+                .get_idx_by_nodeid(&hash)
+                .and_then(|idx| changelog.get_rev(idx))
+                .expect("Changeset not found");
+
+            println!("RAW: {:?}", raw);
+
+            let changeset = RevlogChangeset::new(raw.clone()).expect("Failed to deserialize CS");
+
+            if sub_m.is_present("parsed") {
+                println!("");
+                println!("PAR: {:?}", changeset);
+            }
+
+            let encoded = changeset.get_node().expect("Failed to serialize CS");
+
+            if raw != encoded {
+                println!("");
+                println!("POTENTIAL PROBLEM: {:?}", encoded);
+            }
+        }
+        _ => {
+            println!("{}", matches.usage());
+            ::std::process::exit(1);
+        }
+    }
+}
