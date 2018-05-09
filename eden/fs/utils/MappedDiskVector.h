c51fea6a852a7be3f7fc8edb83b088d783894eb4
Chad Austin <chadaustin@fb.com>
2018-05-09 16:20:43 -0700

    add support for migrating MappedDiskVector record formats
    Summary:
    simpkins was curious how data format migrations would be handled in
    the upcoming InodeTable. This diff implements the bulk of the logic
    which is largely at the MappedDiskVector level. The existing file
    format supported record version negotiation and this diff hooks it up
    with some type-level operations.
    
    Reviewed By: simpkins
    
    Differential Revision: D7836249
    
    fbshipit-source-id: 00e36bc67068c7524956e908b3872c80a79241c0

--- eden/fs/utils/MappedDiskVector.h
+++ eden/fs/utils/MappedDiskVector.h
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <type_traits>
 
+#include <eden/fs/utils/Bug.h>
 #include <folly/Exception.h>
 #include <folly/File.h>
 #include <folly/FileUtil.h>
@@ -36,6 +37,42 @@ inline size_t roundUpToNonzeroPageSize(size_t s) {
       0 == (kPageSize & (kPageSize - 1)), "kPageSize must be power of two");
   return std::max(kPageSize, (s + kPageSize - 1) & ~(kPageSize - 1));
 }
+
+/**
+ * Enforce required properties of
+ */
+template <typename... T>
+struct RecordTypeRequirements;
+
+template <>
+struct RecordTypeRequirements<> {
+  using type = void;
+};
+
+template <typename T, typename... Rest>
+struct RecordTypeRequirements<T, Rest...> {
+  static_assert(
+      std::is_standard_layout<T>::value,
+      "Records must have standard layout");
+  static_assert(
+      std::is_trivially_destructible<T>::value,
+      "MappedDiskVector does not support custom destructors");
+  static_assert(
+      std::is_trivially_move_assignable<T>::value,
+      "Records will be relocated in memory");
+  static_assert(
+      std::is_convertible<typeof(T::VERSION), uint32_t>::value,
+      "Record's VERSION constant must convert to a uint32_t");
+  static_assert(T::VERSION >= 0, "Record VERSION cannot be negative");
+  static_assert(
+      T::VERSION < std::numeric_limits<uint32_t>::max(),
+      "Record VERSION must fit in 32 bits");
+
+  using type = typename RecordTypeRequirements<Rest...>::type;
+};
+
+template <typename T, typename... OldVersions>
+struct Migrator;
 } // namespace detail
 
 /**
@@ -43,13 +80,22 @@ inline size_t roundUpToNonzeroPageSize(size_t s) {
  * a persistent memory-mapped file.
  *
  * MappedDiskVector is not thread-safe - the caller is
- * responsible for synchronization.  It is safe for multiple threads to read
- * simultaneously, though.
+ * responsible for synchronization. It is safe for multiple threads to
+ * simultaneously read, however.
  *
- * While alive, MappedDiskVector does acquire an exclusive lock on the
+ * While alive, MappedDiskVector does acquire an exclusive flock on the
  * underlying fd to avoid multiple processes manipulating it at the same time.
  *
- * Future work:
+ * MappedDiskVector supports migrating from old formats to new formats via the
+ * OldVersions template parameter. For any given type T, T::VERSION is written
+ * into the header and used for version negotiation. sizeof(T) is also recorded
+ * to prevent accidentally adding a field without changing the version.
+ *
+ * MappedDiskVector<A, B, C> will, if decoding the file as A fails, try to
+ * decode as B and C, and if either succeeds, the file will be migrated to the
+ * new format and reopened. In particular, each record will be constructed with
+ * an instance of the type of the right. When migrating from C to A above,
+ * the new file will contain values constructed with C{B{oldA}}.
  *
  * This type needs to be split into two: the non-template, untyped storage
  * class that manages resizing the file and mapping and parsing the header,
@@ -64,64 +110,163 @@ inline size_t roundUpToNonzeroPageSize(size_t s) {
  */
 template <
     typename T,
-    typename = std::enable_if_t<
-        std::is_standard_layout<T>::value &&
-        std::is_trivially_destructible<T>::value &&
-        std::is_trivially_move_assignable<T>::value>>
+    typename = typename detail::RecordTypeRequirements<T>::type>
 class MappedDiskVector {
  public:
-  MappedDiskVector() = delete;
-  MappedDiskVector(const MappedDiskVector&) = delete;
-  MappedDiskVector& operator=(const MappedDiskVector&) = delete;
-
-  // There's no inherent reason this type could not be movable.  It just hasn't
-  // been necessary yet.
-  MappedDiskVector(MappedDiskVector&&) = delete;
-  MappedDiskVector& operator=(MappedDiskVector&&) = delete;
-
   /**
-   * Opens or recreates the MappedDiskVector at the specified path.  The path is
+   * Opens or creates the MappedDiskVector at the specified path.  The path is
    * only used to open the file - a single file descriptor is used from then on
    * with the underlying inode resized in place.
+   *
+   * If the load fails because of a version mismatch, the types specified in
+   * OldVersions are tried sequentially. If one succeeds, the entries are
+   * converted one-by-one into the new format and the new table replaces the
+   * old.
    */
-  explicit MappedDiskVector(
+  template <typename... OldVersions>
+  static MappedDiskVector open(
       folly::StringPiece path,
-      bool shouldPopulate = false)
-      : file_(path, O_RDWR | O_CREAT | O_CLOEXEC, 0600) {
-    if (!file_.try_lock()) {
+      bool shouldPopulate = false) {
+    folly::File file{path, O_RDWR | O_CREAT | O_CLOEXEC, 0600};
+
+    if (!file.try_lock()) {
       folly::throwSystemError("failed to acquire lock on ", path);
     }
 
     struct stat st;
     folly::checkUnixError(
-        fstat(file_.fd(), &st), "fstat failed on MappedDiskVector path ", path);
+        fstat(file.fd(), &st), "fstat failed on MappedDiskVector path ", path);
 
     if (st.st_size == 0) {
-      initializeFromScratch();
-      return;
+      return initializeFromScratch(std::move(file));
     }
 
     Header header;
     ssize_t readBytes =
-        folly::preadNoInt(file_.fd(), &header, sizeof(header), 0);
+        folly::preadNoInt(file.fd(), &header, sizeof(header), 0);
     if (readBytes == -1) {
       folly::throwSystemError("failed to read MappedDiskVector header");
     } else if (readBytes != sizeof(header)) {
       XLOG(WARNING) << "file contains incomplete header: only read "
                     << readBytes << " bytes";
-      throw std::runtime_error("Include MappedDiskVector header");
+      throw std::runtime_error("Incomplete MappedDiskVector header");
     }
 
     if (kMagic != header.magic || header.version != 1 ||
-        sizeof(header) > st.st_size ||
-        // careful not to overflow by multiplying entryCount by sizeof(T)
-        header.entryCount > (st.st_size - sizeof(header)) / sizeof(T) ||
-        header.recordSize != sizeof(T) || header.unused != 0) {
+        sizeof(header) > st.st_size || header.recordSize == 0 ||
+        // careful not to overflow by multiplying entryCount by recordSize
+        header.entryCount > (st.st_size - sizeof(header)) / header.recordSize ||
+        header.unused != 0) {
       throw std::runtime_error(
           "Invalid header: this is probably not a MappedDiskVector file");
     }
 
-    createMap(st.st_size, header.entryCount, shouldPopulate);
+    // Verify that every given record type has a unique VERSION value.
+    // This check could be done at compile time.
+    static constexpr std::array<uint32_t, 1 + sizeof...(OldVersions)> versions =
+        {T::VERSION, OldVersions::VERSION...};
+    for (size_t i = 0; i < versions.size(); ++i) {
+      for (size_t j = i + 1; j < versions.size(); ++j) {
+        if (versions[i] == versions[j]) {
+          throw std::logic_error(folly::to<std::string>(
+              "Duplicate VERSION detected in record types: ", versions[i]));
+        }
+      }
+    }
+
+    // Does this file match the primary record type? If so, we're done.
+    if (T::VERSION == header.recordVersion) {
+      if (sizeof(T) != header.recordSize) {
+        throw std::runtime_error(folly::to<std::string>(
+            "Record size does not match size recorded in file. Expected ",
+            sizeof(T),
+            " but file has ",
+            header.recordSize));
+      }
+      return MappedDiskVector{
+          std::move(file), st.st_size, header.entryCount, shouldPopulate};
+    }
+
+    // Try to migrate from an old record format if any match.
+    static constexpr std::array<size_t, sizeof...(OldVersions)> sizes = {
+        sizeof(OldVersions)...};
+    for (size_t i = 0; i < sizes.size(); ++i) {
+      if (versions[i + 1] == header.recordVersion) {
+        if (sizes[i] != header.recordSize) {
+          throw std::runtime_error(folly::to<std::string>(
+              "Record version matches old record type but record size differs. ",
+              "Expected ",
+              sizes[i],
+              " but file has ",
+              header.recordSize));
+        }
+        return detail::Migrator<T, OldVersions...>::migrateFrom(
+            path,
+            std::move(file),
+            st.st_size,
+            header.entryCount,
+            i,
+            [](const auto& from) { return T{from}; });
+      }
+    }
+
+    throw std::runtime_error(folly::to<std::string>(
+        "Unexpected record size and version. "
+        "Expected size=",
+        sizeof(T),
+        ", version=",
+        T::VERSION,
+        " but got size=",
+        header.recordSize,
+        ", version=",
+        header.recordVersion));
+  }
+
+  /**
+   * Creates a new MappedDiskVector at the specified path, overwriting any that
+   * was there prior.
+   */
+  static MappedDiskVector createOrOverwrite(folly::StringPiece path) {
+    folly::File file{
+        path, O_RDWR | O_CREAT | O_TRUNC | O_NOFOLLOW | O_CLOEXEC, 0600};
+    if (!file.try_lock()) {
+      folly::throwSystemError("failed to acquire lock on ", path);
+    }
+
+    return initializeFromScratch(std::move(file));
+  }
+
+  MappedDiskVector() = delete;
+  MappedDiskVector(const MappedDiskVector&) = delete;
+  MappedDiskVector& operator=(const MappedDiskVector&) = delete;
+
+  MappedDiskVector(MappedDiskVector&& other) : file_(std::move(other.file_)) {
+    begin_ = other.begin_;
+    end_ = other.end_;
+    map_ = other.map_;
+    mapSizeInBytes_ = other.mapSizeInBytes_;
+
+    other.begin_ = nullptr;
+    other.end_ = nullptr;
+    other.map_ = nullptr;
+    other.mapSizeInBytes_ = 0;
+  }
+
+  MappedDiskVector& operator=(MappedDiskVector&& other) {
+    if (map_) {
+      munmap(map_, mapSizeInBytes_);
+    }
+
+    file_ = std::move(other.file_);
+    begin_ = other.begin_;
+    end_ = other.end_;
+    map_ = other.map_;
+    mapSizeInBytes_ = other.mapSizeInBytes_;
+
+    other.begin_ = nullptr;
+    other.end_ = nullptr;
+    other.map_ = nullptr;
+    other.mapSizeInBytes_ = 0;
   }
 
   ~MappedDiskVector() {
@@ -212,7 +357,7 @@ class MappedDiskVector {
   struct Header {
     uint32_t magic;
     uint32_t version; // 1
-    uint32_t userVersion; // bubbled up to user
+    uint32_t recordVersion; // T::VERSION
     uint32_t recordSize; // sizeof(T)
     uint64_t entryCount; // end() - begin()
     uint64_t unused; // for alignment
@@ -226,14 +371,14 @@ class MappedDiskVector {
 
   static constexpr size_t GROWTH_IN_PAGES = 256;
 
-  void initializeFromScratch() {
+  static MappedDiskVector initializeFromScratch(folly::File file) {
     // Start the file large enough to handle the header and a little under one
     // round one of growth.
     constexpr size_t initialSize = GROWTH_IN_PAGES * detail::kPageSize;
     static_assert(
         initialSize >= sizeof(Header) + sizeof(T),
         "Initial size must include enough space for the header and at least one element.");
-    if (-1 == folly::ftruncateNoInt(file_.fd(), initialSize)) {
+    if (-1 == folly::ftruncateNoInt(file.fd(), initialSize)) {
       folly::throwSystemError(
           "failed to initialize MappedDiskVector: ftruncate() failed");
     }
@@ -241,12 +386,11 @@ class MappedDiskVector {
     Header header;
     header.magic = kMagic;
     header.version = 1;
-    header.userVersion = 0;
+    header.recordVersion = T::VERSION;
     header.recordSize = sizeof(T);
     header.entryCount = 0;
     header.unused = 0;
-    ssize_t written =
-        folly::pwriteNoInt(file_.fd(), &header, sizeof(header), 0);
+    ssize_t written = folly::pwriteNoInt(file.fd(), &header, sizeof(header), 0);
     if (-1 == written) {
       folly::throwSystemError("Failed to write initial header");
     }
@@ -254,13 +398,20 @@ class MappedDiskVector {
       throw std::runtime_error("Failed to write complete initial header");
     }
 
-    return createMap(initialSize, header.entryCount, false);
+    return MappedDiskVector{
+        std::move(file), initialSize, header.entryCount, false};
   }
 
-  void createMap(off_t fileSize, size_t currentEntryCount, bool populate) {
-    // It's worth keeping the file and mapping a whole number of pages to avoid
-    // wasting an partial page at the end.  Note that this is an optimization
-    // and it doesn't matter if kPageSize differs from the system page size.
+  explicit MappedDiskVector(
+      folly::File file,
+      off_t fileSize,
+      size_t currentEntryCount,
+      bool populate)
+      : file_(std::move(file)) {
+    // It's worth keeping the file and mapping a whole number of pages to
+    // avoid wasting an partial page at the end.  Note that this is an
+    // optimization and it doesn't matter if kPageSize differs from the
+    // system page size.
     size_t desiredSize = detail::roundUpToNonzeroPageSize(fileSize);
     if (fileSize != desiredSize) {
       if (fileSize) {
@@ -274,8 +425,8 @@ class MappedDiskVector {
       }
     }
 
-    // Call readahead() here?  Offer it as optional functionality?  InodeTable
-    // needs to traverse every record immediately after opening.
+    // Call readahead() here?  Offer it as optional functionality?
+    // InodeTable needs to traverse every record immediately after opening.
 
     auto map = mmap(
         0,
@@ -348,7 +499,81 @@ class MappedDiskVector {
   size_t mapSizeInBytes_{0}; // must be nonzero, multiple of page size
 
   folly::File file_;
+
+  template <typename T_, typename... OldVersions>
+  friend struct detail::Migrator;
 };
+
+namespace detail {
+template <typename T>
+struct Migrator<T> {
+  template <typename ConvertFn>
+  static MappedDiskVector<T> migrateFrom(
+      folly::StringPiece /*path*/,
+      folly::File /*file*/,
+      off_t /*fileSize*/,
+      size_t /*currentEntryCount*/,
+      size_t /*oldVersionIndex*/,
+      ConvertFn /*convert*/) {
+    auto bug = EDEN_BUG() << "oldVersionIndex >= sizeof...(OldVersions)";
+    bug.throwException();
+  }
+};
+
+template <typename T, typename First, typename... Rest>
+struct Migrator<T, First, Rest...> {
+  template <typename ConvertFn>
+  static MappedDiskVector<T> migrateFrom(
+      folly::StringPiece path,
+      folly::File file,
+      off_t fileSize,
+      size_t currentEntryCount,
+      size_t oldVersionIndex,
+      ConvertFn convert) {
+    using namespace folly::literals;
+
+    // static assert type of fn() is First -> T
+
+    if (oldVersionIndex == 0) {
+      // At this point, it's clear the original file is compatible with First.
+      // Load it, migrate each element to a new temporary file, and move the
+      // temporary file over the original.
+      // Set populate to true because migrating requires reading every element
+      // anyway.
+      MappedDiskVector<First> original{
+          std::move(file), fileSize, currentEntryCount, true};
+
+      auto tmpPath = folly::to<std::string>(path, ".tmp");
+      auto newVector = MappedDiskVector<T>::createOrOverwrite(tmpPath);
+      try {
+        // TODO: newVector.reserve
+        for (size_t i = 0; i < original.size(); ++i) {
+          newVector.emplace_back(convert(original[i]));
+        }
+
+        if (rename(tmpPath.c_str(), path.str().c_str())) {
+          folly::throwSystemError(
+              "rename() failed while migrating MDV formats");
+        }
+
+        return newVector;
+      } catch (const std::exception&) {
+        unlink(tmpPath.c_str());
+        throw;
+      }
+    }
+
+    return Migrator<T, Rest...>::migrateFrom(
+        path,
+        std::move(file),
+        fileSize,
+        currentEntryCount,
+        oldVersionIndex - 1,
+        [=](const auto& from) { return convert(First{from}); });
+  }
+};
+
+} // namespace detail
 
 } // namespace eden
 } // namespace facebook
--- eden/fs/utils/test/MappedDiskVectorTest.cpp
+++ eden/fs/utils/test/MappedDiskVectorTest.cpp
@@ -27,14 +27,31 @@ TEST(MappedDiskVector, roundUpToNonzeroPageSize) {
   EXPECT_EQ(kPageSize * 2, roundUpToNonzeroPageSize(kPageSize * 2));
 }
 
-TEST(MappedDiskVector, grows_file) {
-  auto tmpDir = TemporaryDirectory("eden_mdv_");
-  auto tempPath = tmpDir.path() / "test.mdv";
-  MappedDiskVector<uint64_t> mdv{tempPath.string()};
+namespace {
+struct MappedDiskVectorTest : ::testing::Test {
+  MappedDiskVectorTest()
+      : tmpDir{"eden_mdv_"}, mdvPath{(tmpDir.path() / "test.mdv").string()} {}
+  TemporaryDirectory tmpDir;
+  std::string mdvPath;
+};
+
+struct U64 {
+  enum { VERSION = 0 };
+
+  /* implicit */ U64(uint64_t v) : value{v} {}
+  operator uint64_t() const {
+    return value;
+  }
+  uint64_t value;
+};
+} // namespace
+
+TEST_F(MappedDiskVectorTest, grows_file) {
+  auto mdv = MappedDiskVector<U64>::open(mdvPath);
   EXPECT_EQ(0, mdv.size());
 
   struct stat st;
-  ASSERT_EQ(0, stat(tempPath.c_str(), &st));
+  ASSERT_EQ(0, stat(mdvPath.c_str(), &st));
   auto old_size = st.st_size;
 
   // 8 MB
@@ -44,32 +61,28 @@ TEST(MappedDiskVector, grows_file) {
   }
   EXPECT_EQ(N, mdv.size());
 
-  ASSERT_EQ(0, stat(tempPath.c_str(), &st));
+  ASSERT_EQ(0, stat(mdvPath.c_str(), &st));
   auto new_size = st.st_size;
   EXPECT_GT(new_size, old_size);
 }
 
-TEST(MappedDiskVector, remembers_contents_on_reopen) {
-  auto tmpDir = TemporaryDirectory("eden_mdv_");
-  auto tempPath = tmpDir.path() / "test.mdv";
+TEST_F(MappedDiskVectorTest, remembers_contents_on_reopen) {
   {
-    MappedDiskVector<uint64_t> mdv{tempPath.string()};
+    auto mdv = MappedDiskVector<U64>::open(mdvPath);
     mdv.emplace_back(15ull);
     mdv.emplace_back(25ull);
     mdv.emplace_back(35ull);
   }
 
-  MappedDiskVector<uint64_t> mdv{tempPath.string()};
+  auto mdv = MappedDiskVector<U64>::open(mdvPath);
   EXPECT_EQ(3, mdv.size());
   EXPECT_EQ(15, mdv[0]);
   EXPECT_EQ(25, mdv[1]);
   EXPECT_EQ(35, mdv[2]);
 }
 
-TEST(MappedDiskVector, pop_back) {
-  auto tmpDir = TemporaryDirectory("eden_mdv_");
-  auto tempPath = tmpDir.path() / "test.mdv";
-  MappedDiskVector<uint64_t> mdv{tempPath.string()};
+TEST_F(MappedDiskVectorTest, pop_back) {
+  auto mdv = MappedDiskVector<U64>::open(mdvPath);
   mdv.emplace_back(1ull);
   mdv.emplace_back(2ull);
   mdv.pop_back();
@@ -77,4 +90,162 @@ TEST(MappedDiskVector, pop_back) {
   EXPECT_EQ(2, mdv.size());
   EXPECT_EQ(1, mdv[0]);
   EXPECT_EQ(3, mdv[1]);
+}
+
+namespace {
+struct Small {
+  enum { VERSION = 0 };
+  unsigned x;
+};
+struct Large {
+  enum { VERSION = 0 };
+  unsigned x;
+  unsigned y;
+};
+struct SmallNew {
+  enum { VERSION = 1 };
+  unsigned x;
+};
+} // namespace
+
+TEST_F(MappedDiskVectorTest, throws_if_size_does_not_match) {
+  {
+    auto mdv = MappedDiskVector<Small>::open(mdvPath);
+    mdv.emplace_back(Small{1});
+  }
+
+  try {
+    auto mdv = MappedDiskVector<Large>::open(mdvPath);
+    FAIL() << "MappedDiskVector didn't throw";
+  } catch (const std::runtime_error& e) {
+    EXPECT_EQ(
+        "Record size does not match size recorded in file. "
+        "Expected 8 but file has 4",
+        std::string(e.what()));
+  } catch (const std::exception& e) {
+    FAIL() << "Unexpected exception: " << e.what();
+  }
+}
+
+TEST_F(MappedDiskVectorTest, throws_if_version_does_not_match) {
+  {
+    auto mdv = MappedDiskVector<Small>::open(mdvPath);
+    mdv.emplace_back(Small{1});
+  }
+
+  try {
+    auto mdv = MappedDiskVector<SmallNew>::open(mdvPath);
+    FAIL() << "MappedDiskVector didn't throw";
+  } catch (const std::runtime_error& e) {
+    EXPECT_EQ(
+        "Unexpected record size and version. "
+        "Expected size=4, version=1 but got size=4, version=0",
+        std::string(e.what()));
+  } catch (const std::exception& e) {
+    FAIL() << "Unexpected exception: " << e.what();
+  }
+}
+
+namespace {
+struct Old {
+  enum { VERSION = 0 };
+  unsigned x;
+};
+struct New {
+  enum { VERSION = 1 };
+  explicit New(const Old& old) : x(-old.x), y(old.x) {}
+  unsigned x;
+  unsigned y;
+};
+} // namespace
+
+TEST_F(MappedDiskVectorTest, migrates_from_old_version_to_new) {
+  {
+    auto mdv = MappedDiskVector<Old>::open(mdvPath);
+    mdv.emplace_back(Old{1});
+    mdv.emplace_back(Old{2});
+  }
+
+  {
+    auto mdv = MappedDiskVector<New>::open<Old>(mdvPath);
+    EXPECT_EQ(2, mdv.size());
+    EXPECT_EQ(-1, mdv[0].x);
+    EXPECT_EQ(1, mdv[0].y);
+    EXPECT_EQ(-2, mdv[1].x);
+    EXPECT_EQ(2, mdv[1].y);
+  }
+
+  // and moves the new database over the old one
+  {
+    auto mdv = MappedDiskVector<New>::open(mdvPath);
+    EXPECT_EQ(2, mdv.size());
+    EXPECT_EQ(-1, mdv[0].x);
+    EXPECT_EQ(1, mdv[0].y);
+    EXPECT_EQ(-2, mdv[1].x);
+    EXPECT_EQ(2, mdv[1].y);
+  }
+}
+
+namespace {
+struct V1 {
+  enum { VERSION = 1 };
+  uint8_t value;
+  uint8_t conversionCount{0};
+};
+struct V2 {
+  enum { VERSION = 2 };
+  explicit V2(V1 old)
+      : value(old.value), conversionCount(old.conversionCount + 1) {}
+  uint16_t value;
+  uint16_t conversionCount{0};
+};
+struct V3 {
+  enum { VERSION = 3 };
+  explicit V3(V2 old)
+      : value(old.value), conversionCount(old.conversionCount + 1) {}
+  uint32_t value;
+  uint32_t conversionCount{0};
+};
+struct V4 {
+  enum { VERSION = 4 };
+  explicit V4(V3 old)
+      : value(old.value), conversionCount(old.conversionCount + 1) {}
+  uint64_t value;
+  uint64_t conversionCount{0};
+};
+} // namespace
+
+TEST_F(MappedDiskVectorTest, migrates_across_multiple_versions) {
+  {
+    auto mdv = MappedDiskVector<V1>::open(mdvPath);
+    mdv.emplace_back(V1{1});
+    mdv.emplace_back(V1{2});
+  }
+
+  {
+    auto mdv = MappedDiskVector<V4>::open<V3, V2, V1>(mdvPath);
+    EXPECT_EQ(1, mdv[0].value);
+    EXPECT_EQ(3, mdv[0].conversionCount);
+    EXPECT_EQ(2, mdv[1].value);
+    EXPECT_EQ(3, mdv[1].conversionCount);
+  }
+}
+
+TEST_F(MappedDiskVectorTest, migrate_overwrites_existing_tmp_file) {
+  {
+    auto mdv = MappedDiskVector<Old>::open(mdvPath);
+    mdv.emplace_back(Old{1});
+    mdv.emplace_back(Old{2});
+  }
+
+  folly::writeFileAtomic(mdvPath + ".tmp", "junk data");
+
+  {
+    auto mdv = MappedDiskVector<New>::open<Old>(mdvPath);
+    EXPECT_EQ(2, mdv.size());
+    EXPECT_EQ(-1, mdv[0].x);
+    EXPECT_EQ(1, mdv[0].y);
+    EXPECT_EQ(-2, mdv[1].x);
+    EXPECT_EQ(2, mdv[1].y);
+  }
 }
