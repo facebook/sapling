410cb5dcef466cca14c267ef71af6d9b3af45641
Jun Wu <quark@fb.com>
2018-05-14 11:04:42 -0700

    xdiff: fix types on 32 bit systems
    Summary:
    Similar to upstream patches [1] [2].
    
    [1]: https://www.mercurial-scm.org/repo/hg/rev/1b9f6440506b3434e97eadd6f9b4b39dd0004154
    [2]: https://phab.mercurial-scm.org/D3538
    
    Reviewed By: DurhamG
    
    Differential Revision: D7989782
    
    fbshipit-source-id: fc11569d92f778c0a3509204fc9fa1690c078dcc

--- mercurial/cext/xdiff.c
+++ mercurial/cext/xdiff.c
@@ -10,10 +10,11 @@
 #include "lib/third-party/xdiff/xdiff.h"
 #include "Python.h"
 
-static int hunk_consumer(long a1, long a2, long b1, long b2, void *priv)
+static int hunk_consumer(int64_t a1, int64_t a2, int64_t b1, int64_t b2,
+                         void *priv)
 {
 	PyObject *rl = (PyObject *)priv;
-	PyObject *m = Py_BuildValue("llll", a1, a2, b1, b2);
+	PyObject *m = Py_BuildValue("LLLL", a1, a2, b1, b2);
 	if (!m)
 		return -1;
 	if (PyList_Append(rl, m) != 0) {
