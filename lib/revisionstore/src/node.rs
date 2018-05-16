bcb7ac0b32df41709b36fa8a3831145f6cddaec7
Saurabh Singh <singhsrb@fb.com>
2018-05-15 18:35:48 -0700

    node: add common traits for the node type
    Summary:
    Based on the review comments for D7801615, this commit adds the common traits
    for the `Node` type
    
    Reviewed By: jsgf
    
    Differential Revision: D7872300
    
    fbshipit-source-id: 44dedfc3ec0e18ac0dee0dcfc5f7dfc4aff2511d

--- lib/revisionstore/src/node.rs
+++ lib/revisionstore/src/node.rs
@@ -2,7 +2,24 @@
 
 /// A 20-byte identifier, often a hash. Nodes are used to uniquely identify
 /// commits, file versions, and many other things.
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Node([u8; 20]);
+
+/// The nullid (0x00) is used throughout Mercurial to represent "None".
+/// (For example, a commit will have a nullid p2, if it has no second parent).
+pub const NULL_ID: Node = Node([0; 20]);
+
+impl Node {
+    pub fn is_null(&self) -> bool {
+        self == &NULL_ID
+    }
+}
+
+impl Default for Node {
+    fn default() -> Node {
+        NULL_ID
+    }
+}
 
 impl<'a> From<&'a [u8; 20]> for Node {
     fn from(bytes: &[u8; 20]) -> Node {
