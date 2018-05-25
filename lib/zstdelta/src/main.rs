6210435392fd9971870c92802d6941e3c9560d06
Jun Wu <quark@fb.com>
2018-05-25 15:45:57 -0700

    zstdelta: add a simple command-line utility for debugging
    Summary:
    The command-line utility takes 2 files and compress/decompress them.
    It's not meant to be formally used like `zstd`. But is handy to do simple
    tests without writing Rust code.
    
    Reviewed By: DurhamG
    
    Differential Revision: D7596169
    
    fbshipit-source-id: 6c21a38e21a061fab7032ff823b907b0e586bd42

---
+++ lib/zstdelta/src/main.rs
@@ -0,0 +1,43 @@
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+#[cfg(test)]
+extern crate rand;
+
+extern crate libc;
+extern crate zstd_sys;
+
+mod zstdelta;
+
+use std::env::args;
+use std::fs::File;
+use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
+use std::process::exit;
+use zstdelta::{apply, diff};
+
+fn read(path: &Path) -> Vec<u8> {
+    let mut buf = Vec::new();
+    File::open(path)
+        .expect("open")
+        .read_to_end(&mut buf)
+        .expect("read");
+    buf
+}
+
+fn main() {
+    let args: Vec<_> = args().skip(1).collect();
+    if args.len() < 3 {
+        println!("Usage: zstdelta -c base data > delta\n       zstdelta -d base delta > data\n");
+        exit(1);
+    }
+    let base = read(&PathBuf::from(&args[1]));
+    let data = read(&PathBuf::from(&args[2]));
+    let out = if args[0] == "-c" {
+        diff(&base, &data).expect("diff")
+    } else {
+        apply(&base, &data).expect("apply")
+    };
+
+    io::stdout().write_all(&out).expect("write");
+}
