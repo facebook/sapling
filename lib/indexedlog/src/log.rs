529c79bd33ddfb6e7ddadf9ce546a252cf26f074
Jun Wu <quark@fb.com>
2018-06-11 19:26:40 -0700

    indexedlog: log: implement serialization for the meta file
    Summary: Implemented as the file format specification added by the previous diff.
    
    Reviewed By: DurhamG
    
    Differential Revision: D8156516
    
    fbshipit-source-id: 7153932b9442b3ab5bdb81490f88c40346128afc

--- lib/indexedlog/src/log.rs
+++ lib/indexedlog/src/log.rs
@@ -140,3 +140,107 @@ impl Log {
         unimplemented!()
     }
 }
+
+impl LogMetadata {
+    const HEADER: &'static [u8] = b"meta\0";
+
+    fn read<R: Read>(reader: &mut R) -> io::Result<Self> {
+        let mut header = vec![0; Self::HEADER.len()];
+        reader.read_exact(&mut header)?;
+        if header != Self::HEADER {
+            let msg = "invalid metadata header";
+            return Err(io::Error::new(io::ErrorKind::InvalidData, msg));
+        }
+
+        let hash = reader.read_vlq()?;
+        let buf_len = reader.read_vlq()?;
+
+        let mut buf = vec![0; buf_len];
+        reader.read_exact(&mut buf)?;
+
+        if xxhash(&buf) != hash {
+            let msg = "metadata integrity check failed";
+            return Err(io::Error::new(io::ErrorKind::InvalidData, msg));
+        }
+
+        let mut reader = Cursor::new(buf);
+        let primary_len = reader.read_vlq()?;
+        let index_count: usize = reader.read_vlq()?;
+        let mut indexes = BTreeMap::new();
+        for _ in 0..index_count {
+            let name_len = reader.read_vlq()?;
+            let mut name = vec![0; name_len];
+            reader.read_exact(&mut name)?;
+            let name = String::from_utf8(name).map_err(|_e| {
+                let msg = "non-utf8 index name";
+                io::Error::new(io::ErrorKind::InvalidData, msg)
+            })?;
+            let len = reader.read_vlq()?;
+            indexes.insert(name, len);
+        }
+
+        Ok(Self {
+            primary_len,
+            indexes,
+        })
+    }
+
+    fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
+        let mut buf = Vec::new();
+        buf.write_vlq(self.primary_len)?;
+        buf.write_vlq(self.indexes.len())?;
+        for (name, len) in self.indexes.iter() {
+            let name = name.as_bytes();
+            buf.write_vlq(name.len())?;
+            buf.write_all(name)?;
+            buf.write_vlq(*len)?;
+        }
+        writer.write_all(Self::HEADER)?;
+        writer.write_vlq(xxhash(&buf))?;
+        writer.write_vlq(buf.len())?;
+        writer.write_all(&buf)?;
+
+        Ok(())
+    }
+
+    fn read_file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let mut file = fs::OpenOptions::new().read(true).open(path)?;
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+        let mut cur = Cursor::new(buf);
+        Self::read(&mut cur)
+    }
+
+    fn write_file<P: AsRef<Path>>(&self, path: P) -> io::Result<()> {
+        let mut buf = Vec::new();
+        self.write(&mut buf)?;
+        AtomicFile::new(path, AllowOverwrite).write(|f| f.write_all(&buf))?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempdir::TempDir;
+
+    quickcheck! {
+        fn test_roundtrip_meta(primary_len: u64, indexes: BTreeMap<String, u64>) -> bool {
+            let mut buf = Vec::new();
+            let meta = LogMetadata { primary_len, indexes };
+            meta.write(&mut buf).expect("write");
+            let mut cur = Cursor::new(buf);
+            let meta_read = LogMetadata::read(&mut cur).expect("read");
+            meta_read == meta
+        }
+
+        fn test_roundtrip_meta_file(primary_len: u64, indexes: BTreeMap<String, u64>) -> bool {
+            let dir = TempDir::new("log").expect("tempdir");
+            let meta = LogMetadata { primary_len, indexes };
+            let path = dir.path().join("meta");
+            meta.write_file(&path).expect("write_file");
+            let meta_read = LogMetadata::read_file(&path).expect("read_file");
+            meta_read == meta
+        }
+    }
+}
