91144d493c727a14b8836bc29e2e84f5dfe72e67
Liubov Dmitrieva <liubovd@fb.com>
2018-06-18 07:49:33 -0700

    cloudsyncsubscriber log pid of cloud sync process
    Summary: log the pid of the spawned cloud sync process, it might help with debugging if something is broken
    
    Reviewed By: markbt
    
    Differential Revision: D8478566
    
    fbshipit-source-id: fd9a9a228bc325056fb35d17ee93c865679e6e23

--- lib/commitcloudsubscriber/src/action.rs
+++ lib/commitcloudsubscriber/src/action.rs
@@ -1,6 +1,6 @@
 use error::*;
 use std::path::Path;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 pub struct CloudSyncTrigger;
 
@@ -11,25 +11,34 @@ impl CloudSyncTrigger {
         retries: u32,
         version: Option<u64>,
     ) -> Result<()> {
-        let version_args = if let Some(version) = version {
-            vec!["--workspace-version".to_owned(), version.to_string()]
-        } else {
-            vec![]
-        };
+        let mut version_args = vec![];
+        if let Some(version) = version {
+            version_args.append(&mut vec![
+                "--workspace-version".to_owned(),
+                version.to_string(),
+            ]);
+        }
         for i in 0..retries {
-            info!(
-                "{} Fire `hg cloud sync` {}",
-                sid,
-                if i > 0 { "retry" } else { "" }
-            );
-            let output = Command::new("hg")
+            let child = Command::new("hg")
                 .current_dir(&path)
                 .env("HGPLAIN", "hint")
                 .args(vec!["cloud", "sync"])
                 .arg("--check-autosync-enabled")
                 .arg("--use-bgssh")
                 .args(&version_args)
-                .output()?;
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()?; // do not retry if failed to start
+
+            info!(
+                "{} Fire `hg cloud sync` attempt {}, spawned process id '{}'",
+                sid,
+                i,
+                child.id()
+            );
+
+            let output = child.wait_with_output()?;
+
             info!(
                 "{} stdout: \n{}",
                 sid,
