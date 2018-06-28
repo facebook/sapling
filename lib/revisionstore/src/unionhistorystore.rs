fa3c7b34a3cf93d80c173dcb78a53e6b60393983
Saurabh Singh <singhsrb@fb.com>
2018-06-27 18:50:01 -0700

    add basic tests for unionhistorystore
    Summary:
    This commit adds very basic tests for the Union History Store. These
    tests just test for expected output of operations on bad/empty stores.
    
    Reviewed By: quark-zju
    
    Differential Revision: D8553821
    
    fbshipit-source-id: a0dfa47f10083c37901535e8a810a99693a28c82

--- lib/revisionstore/src/unionhistorystore.rs
+++ lib/revisionstore/src/unionhistorystore.rs
@@ -92,3 +92,125 @@ impl HistoryStore for UnionHistoryStore {
         ))).into())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct BadHistoryStore;
+
+    #[derive(Debug, Fail)]
+    #[fail(display = "Bad history store always has error which is not KeyError")]
+    struct BadHistoryStoreError;
+
+    struct EmptyHistoryStore;
+
+    #[derive(Debug, Fail)]
+    #[fail(display = "History Store is empty")]
+    struct EmptyHistoryStoreError;
+
+    impl From<EmptyHistoryStoreError> for KeyError {
+        fn from(err: EmptyHistoryStoreError) -> Self {
+            KeyError::new(err.into())
+        }
+    }
+
+    impl HistoryStore for EmptyHistoryStore {
+        fn get_ancestors(&self, _key: &Key) -> Result<Ancestors> {
+            Err(KeyError::from(EmptyHistoryStoreError).into())
+        }
+
+        fn get_missing(&self, keys: &[Key]) -> Result<Vec<Key>> {
+            Ok(keys.iter().cloned().collect())
+        }
+
+        fn get_node_info(&self, _key: &Key) -> Result<NodeInfo> {
+            Err(KeyError::from(EmptyHistoryStoreError).into())
+        }
+    }
+
+    impl HistoryStore for BadHistoryStore {
+        fn get_ancestors(&self, _key: &Key) -> Result<Ancestors> {
+            Err(BadHistoryStoreError.into())
+        }
+
+        fn get_missing(&self, _keys: &[Key]) -> Result<Vec<Key>> {
+            Err(BadHistoryStoreError.into())
+        }
+
+        fn get_node_info(&self, _key: &Key) -> Result<NodeInfo> {
+            Err(BadHistoryStoreError.into())
+        }
+    }
+
+    quickcheck! {
+        fn test_empty_unionstore_get_ancestors(key: Key) -> bool {
+            match UnionHistoryStore::new().get_ancestors(&key) {
+                Ok(_) => false,
+                Err(e) => e.downcast_ref::<KeyError>().is_some(),
+            }
+        }
+
+        fn test_empty_historystore_get_ancestors(key: Key) -> bool {
+            let mut unionstore = UnionHistoryStore::new();
+            unionstore.add(Rc::new(EmptyHistoryStore));
+            match unionstore.get_ancestors(&key) {
+                Ok(_) => false,
+                Err(e) => e.downcast_ref::<KeyError>().is_some(),
+            }
+        }
+
+        fn test_bad_historystore_get_ancestors(key: Key) -> bool {
+            let mut unionstore = UnionHistoryStore::new();
+            unionstore.add(Rc::new(BadHistoryStore));
+            match unionstore.get_ancestors(&key) {
+                Ok(_) => false,
+                Err(e) => e.downcast_ref::<KeyError>().is_none(),
+            }
+        }
+
+        fn test_empty_unionstore_get_node_info(key: Key) -> bool {
+            match UnionHistoryStore::new().get_node_info(&key) {
+                Ok(_) => false,
+                Err(e) => e.downcast_ref::<KeyError>().is_some(),
+            }
+        }
+
+        fn test_empty_historystore_get_node_info(key: Key) -> bool {
+            let mut unionstore = UnionHistoryStore::new();
+            unionstore.add(Rc::new(EmptyHistoryStore));
+            match unionstore.get_node_info(&key) {
+                Ok(_) => false,
+                Err(e) => e.downcast_ref::<KeyError>().is_some(),
+            }
+        }
+
+        fn test_bad_historystore_get_node_info(key: Key) -> bool {
+            let mut unionstore = UnionHistoryStore::new();
+            unionstore.add(Rc::new(BadHistoryStore));
+            match unionstore.get_node_info(&key) {
+                Ok(_) => false,
+                Err(e) => e.downcast_ref::<KeyError>().is_none(),
+            }
+        }
+
+        fn test_empty_unionstore_get_missing(keys: Vec<Key>) -> bool {
+            keys == UnionHistoryStore::new().get_missing(&keys).unwrap()
+        }
+
+        fn test_empty_historystore_get_missing(keys: Vec<Key>) -> bool {
+            let mut unionstore = UnionHistoryStore::new();
+            unionstore.add(Rc::new(EmptyHistoryStore));
+            keys == unionstore.get_missing(&keys).unwrap()
+        }
+
+        fn test_bad_historystore_get_missing(keys: Vec<Key>) -> bool {
+            let mut unionstore = UnionHistoryStore::new();
+            unionstore.add(Rc::new(BadHistoryStore));
+            match unionstore.get_missing(&keys) {
+                Ok(_) => false,
+                Err(e) => e.downcast_ref::<KeyError>().is_none(),
+            }
+        }
+    }
+}
