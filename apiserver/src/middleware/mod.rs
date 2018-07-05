dfd3b3c9de3f4e59b0624ca35b413fc5205f4dbd
Zeyi Fan <zeyi@fb.com>
2018-07-05 10:05:58 -0700

    split middlewares
    Summary: This commit splits middleware.rs into mod middleware, and move logic of computing responsive to a separate trait so it can be shared.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D8715657
    
    fbshipit-source-id: cf06249d41b9fbe1a4a7500ed5e8024bbf376d3f

---
+++ apiserver/src/middleware/mod.rs
@@ -0,0 +1,10 @@
+// Copyright (c) 2018-present, Facebook, Inc.
+// All Rights Reserved.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+mod response_time;
+mod slogger;
+
+pub use self::slogger::SLogger;
--- apiserver/src/middleware/slogger.rs
+++ apiserver/src/middleware/slogger.rs
@@ -1,16 +1,15 @@
-// Copyright (c) 2004-present, Facebook, Inc.
+// Copyright (c) 2018-present, Facebook, Inc.
 // All Rights Reserved.
 //
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
-use std::time::Instant;
-
 use actix_web::{HttpRequest, HttpResponse};
 use actix_web::error::Result;
 use actix_web::middleware::{Finished, Middleware, Started};
 use slog::Logger;
-use time_ext::DurationExt;
+
+use super::response_time::ResponseTime;
 
 pub struct SLogger {
     logger: Logger,
@@ -20,29 +19,22 @@ impl SLogger {
     pub fn new(logger: Logger) -> SLogger {
         SLogger { logger: logger }
     }
-
-    fn time_cost<S>(&self, req: &mut HttpRequest<S>) -> Option<String> {
-        req.extensions().get::<Instant>().map(|start| {
-            let delta = start.elapsed().as_micros_unchecked();
-
-            format!("{:.3}\u{00B5}s", delta)
-        })
-    }
 }
+
+impl<S> ResponseTime<S> for SLogger {}
 
 impl<S> Middleware<S> for SLogger {
     fn start(&self, req: &mut HttpRequest<S>) -> Result<Started> {
-        req.extensions_mut().insert(Instant::now());
-
+        self.start_timer(req);
         Ok(Started::Done)
     }
 
     fn finish(&self, req: &mut HttpRequest<S>, resp: &HttpResponse) -> Finished {
-        let cost = self.time_cost(req).unwrap_or("".to_string());
+        let cost = self.time_cost(req).unwrap_or(0);
 
         info!(
             self.logger,
-            "{} {} {} {}",
+            "{} {} {} {:.3}\u{00B5}s",
             resp.status().as_u16(),
             req.method(),
             req.path(),
