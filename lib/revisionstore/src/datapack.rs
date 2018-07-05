c6af00dbc9fcbb5d47459a15ee2751f90402ba45
Durham Goode <durham@fb.com>
2018-07-05 14:44:09 -0700

    datapack: implement getmetadata
    Summary:
    Introduces the DataEntry structure which is able to parse data entries
    from pack files. Uses it to implement getmetadata
    
    Reviewed By: quark-zju
    
    Differential Revision: D8556610
    
    fbshipit-source-id: c25427c3c247970a879ad7d409b821f3695b97d9

--- lib/revisionstore/src/datapack.rs
+++ lib/revisionstore/src/datapack.rs
@@ -72,19 +72,151 @@
 //!
 //! ```
 //! [1]: new in version 1.
+use byteorder::{BigEndian, ReadBytesExt};
+use lz4_pyframe::decompress;
 use memmap::{Mmap, MmapOptions};
+use std::{fmt, result};
+use std::cell::RefCell;
 use std::fs::File;
+use std::io::{Cursor, Read};
 use std::path::Path;
+use std::rc::Rc;
 
 use dataindex::DataIndex;
 use datastore::{DataStore, Delta, Metadata};
 use error::Result;
 use key::Key;
+use node::Node;
+
+#[derive(Debug, Fail)]
+#[fail(display = "Datapack Error: {:?}", _0)]
+struct DataPackError(String);
 
 pub struct DataPack {
     mmap: Mmap,
     version: u8,
     index: DataIndex,
+}
+
+pub struct DataEntry<'a> {
+    offset: u64,
+    filename: &'a [u8],
+    node: Node,
+    delta_base: Node,
+    compressed_data: &'a [u8],
+    data: RefCell<Option<Rc<[u8]>>>,
+    metadata: Metadata,
+    next_offset: u64,
+}
+
+impl<'a> DataEntry<'a> {
+    pub fn new(buf: &'a [u8], offset: u64, version: u8) -> Result<Self> {
+        let mut cur = Cursor::new(buf);
+        cur.set_position(offset);
+
+        // Filename
+        let filename_len = cur.read_u16::<BigEndian>()? as u64;
+        let filename = &buf.get(cur.position() as usize..(cur.position() + filename_len) as usize)
+            .ok_or(DataPackError(format!(
+                "buffer (length {:?}) not long enough to read filename (length {:?})",
+                buf.len(),
+                filename_len
+            )))?;
+        let cur_pos = cur.position();
+        cur.set_position(cur_pos + filename_len);
+
+        // Node
+        let mut node_buf: [u8; 20] = Default::default();
+        cur.read_exact(&mut node_buf)?;
+        let node = Node::from(&node_buf);
+
+        // Delta
+        cur.read_exact(&mut node_buf)?;
+        let delta_base = Node::from(&node_buf);
+
+        let delta_len = cur.read_u64::<BigEndian>()?;
+        let compressed_data = &buf.get(
+            cur.position() as usize..(cur.position() + delta_len) as usize,
+        ).ok_or(DataPackError(format!(
+            "buffer (length {:?}) not long enough to read data (length {:?})",
+            buf.len(),
+            delta_len
+        )))?;
+        let data = RefCell::new(None);
+        let cur_pos = cur.position();
+        cur.set_position(cur_pos + delta_len);
+
+        // Metadata
+        let mut metadata = Metadata {
+            flags: None,
+            size: None,
+        };
+        if version == 1 {
+            metadata = Metadata::read(&mut cur)?;
+        }
+
+        let next_offset = cur.position();
+
+        Ok(DataEntry {
+            offset,
+            filename,
+            node,
+            delta_base,
+            compressed_data,
+            data,
+            metadata,
+            next_offset,
+        })
+    }
+
+    pub fn offset(&self) -> u64 {
+        self.offset
+    }
+
+    pub fn filename(&self) -> &[u8] {
+        self.filename
+    }
+
+    pub fn node(&self) -> &Node {
+        &self.node
+    }
+
+    pub fn delta_base(&self) -> &Node {
+        &self.delta_base
+    }
+
+    pub fn delta(&self) -> Result<Rc<[u8]>> {
+        let mut cell = self.data.borrow_mut();
+        if cell.is_none() {
+            *cell = Some(Rc::<[u8]>::from(decompress(&self.compressed_data)?));
+        }
+
+        Ok(Rc::clone(cell.as_ref().unwrap()))
+    }
+
+    pub fn metadata(&self) -> &Metadata {
+        &self.metadata
+    }
+}
+
+impl<'a> fmt::Debug for DataEntry<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> result::Result<(), fmt::Error> {
+        let delta = self.delta()
+            .unwrap_or_else(|e| Rc::from(format!("{:?}", e).as_ref()));
+        write!(
+            f,
+            "DataEntry {{\n  offset: {:?}\n  filename: {:?}\n  \
+             node: {:?}\n  delta_base: {:?}\n  compressed_len: {:?}\n  \
+             data_len: {:?}\n  data: {:?}\n  metadata: N/A\n}}",
+            self.offset,
+            self.filename,
+            self.node,
+            self.delta_base,
+            self.compressed_data.len(),
+            delta.len(),
+            delta.iter().map(|b| *b as char).collect::<String>(),
+        )
+    }
 }
 
 impl DataPack {
@@ -112,6 +244,10 @@ impl DataPack {
     pub fn len(&self) -> usize {
         self.mmap.len()
     }
+
+    pub fn read_entry(&self, offset: u64) -> Result<DataEntry> {
+        DataEntry::new(self.mmap.as_ref(), offset, self.version)
+    }
 }
 
 impl DataStore for DataPack {
@@ -123,7 +259,8 @@ impl DataStore for DataPack {
     }
 
     fn get_meta(&self, key: &Key) -> Result<Metadata> {
-        unimplemented!();
+        let index_entry = self.index.get_entry(key.node())?;
+        Ok(self.read_entry(index_entry.pack_entry_offset)?.metadata)
     }
 
     fn get_missing(&self, keys: &[Key]) -> Result<Vec<Key>> {
@@ -182,5 +319,41 @@ mod tests {
         let not = Key::new(Box::new([1]), Node::random());
         let missing = pack.get_missing(&vec![not.clone()]).unwrap();
         assert_eq!(missing, vec![not.clone()]);
+    }
+
+    #[test]
+    fn test_get_meta() {
+        let tempdir = TempDir::new().unwrap();
+        let revisions = vec![
+            (
+                Delta {
+                    data: Box::new([1, 2, 3, 4]),
+                    base: Key::new(Box::new([0]), Node::random()),
+                    key: Key::new(Box::new([0]), Node::random()),
+                },
+                None,
+            ),
+            (
+                Delta {
+                    data: Box::new([1, 2, 3, 4]),
+                    base: Key::new(Box::new([0]), Node::random()),
+                    key: Key::new(Box::new([0]), Node::random()),
+                },
+                Some(Metadata {
+                    size: Some(1000),
+                    flags: Some(7),
+                }),
+            ),
+        ];
+
+        let pack = make_pack(&tempdir, &revisions);
+        for &(ref delta, ref metadata) in revisions.iter() {
+            let meta = pack.get_meta(&delta.key).unwrap();
+            let mut metadata = match metadata {
+                &Some(ref m) => m.clone(),
+                &None => Default::default(),
+            };
+            assert_eq!(meta, metadata);
+        }
     }
 }
