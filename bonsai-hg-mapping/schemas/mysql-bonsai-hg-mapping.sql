0733c5340f317fce2600c1c3bbd6f0e1fb3196ce
Pavel Aslanov <aslpavel@fb.com>
2018-07-12 07:16:13 -0700

    id for unique (repo_id, hg_cs_id) in mysql schema
    Summary:
    - [moononoke][bonsai_hg_mapping] id for unique `(repo_id, hg_cs_id)` in  mysql schema
    - NOTE: sqlite does not understand this syntax so it is left unchanged
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D8820198
    
    fbshipit-source-id: f0e2478900dc68d619aef0ed16602b76371c85f8

--- bonsai-hg-mapping/schemas/mysql-bonsai-hg-mapping.sql
+++ bonsai-hg-mapping/schemas/mysql-bonsai-hg-mapping.sql
@@ -2,6 +2,6 @@ CREATE TABLE bonsai_hg_mapping (
   repo_id INTEGER NOT NULL,
   hg_cs_id BINARY(20) NOT NULL,
   bcs_id BINARY(32) NOT NULL,
-  UNIQUE (repo_id, hg_cs_id),
+  UNIQUE hg_cs_id_unique_per_repo (repo_id, hg_cs_id),
   PRIMARY KEY (repo_id, bcs_id)
 );
