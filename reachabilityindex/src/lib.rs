d38177f72b4684ca91f6a4854b238ab0cf4a607c
Matthew Dippel <mdippel@fb.com>
2018-07-23 09:19:10 -0700

    initial GenerationBfs impl of ReachabilityIndex
    Summary: Implementation of reachability queries doing a BFS that stops exploration when the generation numbers seen are smaller than the destination's.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D8808875
    
    fbshipit-source-id: 5856dede27a22417add3882d748c4ce67e8f190d

--- reachabilityindex/src/lib.rs
+++ reachabilityindex/src/lib.rs
@@ -4,11 +4,26 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
+#[deny(warnings)]
 extern crate failure_ext as failure;
 extern crate futures;
+extern crate futures_ext;
 
 extern crate blobrepo;
 extern crate mercurial_types;
+extern crate mononoke_types;
 
 mod index;
 pub use index::ReachabilityIndex;
+
+mod genbfs;
+pub use genbfs::GenerationNumberBFS;
+
+#[cfg(test)]
+pub extern crate async_unit;
+#[cfg(test)]
+pub extern crate branch_wide;
+#[cfg(test)]
+pub extern crate linear;
+#[cfg(test)]
+pub extern crate merge_uneven;
