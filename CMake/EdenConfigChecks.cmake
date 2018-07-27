fae2056037f48f1a90a6f3273e360d8231bf7ec5
Jun Wu <quark@fb.com>
2018-07-27 13:10:02 -0700

    do not require SELinux in GitHub build
    Summary:
    I tried to build eden using GitHub version and ran into SELinux issues.
    This patch fixed related issues, namely:
    
    - `set(X Y)` sets X to string literal Y. Change it to `set(X ${Y})`.
    - `SELINUX_INCLUDE_DIR` could be undefined. Check before use it.
    - `./eden/fs/eden-config.h` and `./build/eden/fs/eden-config.h` both exist and
      the former is used. Set include dirs so the latter gets used.
    
    Reviewed By: bolinfest
    
    Differential Revision: D9029272
    
    fbshipit-source-id: 0c94bbe2f9e3fa90973802ddde16ad4d9ddfc0e8

--- CMake/EdenConfigChecks.cmake
+++ CMake/EdenConfigChecks.cmake
@@ -6,7 +6,7 @@ find_package(FBThrift CONFIG REQUIRED)
 find_package(GMock MODULE REQUIRED)
 
 find_package(SELinux)
-set(EDEN_HAVE_SELINUX SELINUX_FOUND)
+set(EDEN_HAVE_SELINUX ${SELINUX_FOUND})
 
 find_package(LibGit2 REQUIRED)
 
@@ -17,9 +17,9 @@ find_package(gflags CONFIG REQUIRED)
 # TODO: It shouldn't be too hard to turn RocksDB and sqlite3 into optional
 # dependencies, since we have alternate LocalStore implementations.
 find_package(RocksDB CONFIG REQUIRED)
-set(EDEN_HAVE_ROCKSDB RocksDB_FOUND)
+set(EDEN_HAVE_ROCKSDB ${RocksDB_FOUND})
 find_package(Sqlite3 REQUIRED)
-set(EDEN_HAVE_SQLITE3 SQLITE3_FOUND)
+set(EDEN_HAVE_SQLITE3 ${SQLITE3_FOUND})
 
 find_package(cpptoml REQUIRED)
 
