2bf6be2869577a8c21b063db3c585727d99d7fb4
Simon Farnsworth <simonfar@fb.com>
2018-08-07 13:26:55 -0700

    Make all bindings fully exception-safe
    Summary:
    cachelib can throw exceptions from any method. Make all our bindings
    exception-safe
    
    Reviewed By: jsgf
    
    Differential Revision: D9158062
    
    fbshipit-source-id: 57948aa6ead304fac6e037621a1f6641826345c5

--- blobstore/src/cachelib_cache.rs
+++ blobstore/src/cachelib_cache.rs
@@ -21,8 +21,6 @@ use dummy_lease::DummyLease;
 use in_process_lease::InProcessLease;
 use locking_cache::CacheBlobstore;
 
-const CACHELIB_MAX_SIZE: usize = 1024000;
-
 /// A caching layer over an existing blobstore, backed by cachelib
 #[derive(Clone)]
 pub struct CachelibOps {
@@ -71,26 +69,39 @@ where
 
 impl CacheOps for CachelibOps {
     fn get(&self, key: &str) -> BoxFuture<Option<BlobstoreBytes>, ()> {
-        Ok(self.blob_pool.get(key).map(BlobstoreBytes::from_bytes))
+        self.blob_pool
+            .get(key)
+            .map_err(|_| ())
+            .map(|opt| opt.map(BlobstoreBytes::from_bytes))
             .into_future()
             .boxify()
     }
 
     fn put(&self, key: &str, value: BlobstoreBytes) -> BoxFuture<(), ()> {
-        self.presence_pool.set(key, Bytes::from(b"P".as_ref()));
-        if value.len() < CACHELIB_MAX_SIZE {
-            self.blob_pool.set(key, value.into_bytes());
-        }
-        Ok(()).into_future().boxify()
+        // A failure to set presence is considered fine, here.
+        let _ = self.presence_pool.set(key, Bytes::from(b"P".as_ref()));
+        self.blob_pool
+            .set(key, value.into_bytes())
+            .map(|_| ())
+            .map_err(|_| ())
+            .into_future()
+            .boxify()
     }
 
     /// Ask the cache if it knows whether the backing store has a value for this key. Returns
     /// `true` if there is definitely a value (i.e. cache entry in Present or Known state), `false`
     /// otherwise (Empty or Leased states).
     fn check_present(&self, key: &str) -> BoxFuture<bool, ()> {
-        Ok(self.presence_pool.get(key).is_some() || self.blob_pool.get(key).is_some())
-            .into_future()
-            .boxify()
+        let presence_pool = self.presence_pool
+            .get(key)
+            .map(|opt| opt.is_some())
+            .unwrap_or(false);
+        let blob_pool = self.blob_pool
+            .get(key)
+            .map(|opt| opt.is_some())
+            .unwrap_or(false);
+
+        Ok(presence_pool || blob_pool).into_future().boxify()
     }
 }
 
