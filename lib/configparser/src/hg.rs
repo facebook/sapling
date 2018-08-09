39a4651bc3b1ca38908f9866d9f97863b31abfa9
Jun Wu <quark@fb.com>
2018-08-08 17:15:14 -0700

    configparser: implement system and user config loading
    Summary:
    This is to locate system / user config files at some fixed places.
    It will replace most of `mercurial.rcutil`, and be used in native clients.
    
    Reviewed By: DurhamG
    
    Differential Revision: D8895791
    
    fbshipit-source-id: 47166b943a3bd90a8aff1c15674a3da4e14bf8d3

--- lib/configparser/src/hg.rs
+++ lib/configparser/src/hg.rs
@@ -1,9 +1,10 @@
 //! Mercurial-specific config postprocessing
 
 use bytes::Bytes;
-use config::Options;
+use config::{ConfigSet, Options};
 use std::collections::HashSet;
 use std::env;
+use std::path::Path;
 
 const HGPLAIN: &str = "HGPLAIN";
 const HGPLAINEXCEPT: &str = "HGPLAINEXCEPT";
@@ -11,6 +12,15 @@ const HGPLAINEXCEPT: &str = "HGPLAINEXCEPT";
 pub trait OptionsHgExt {
     /// Drop configs according to HGPLAIN and HGPLAINEXCEPT.
     fn process_hgplain(self) -> Self;
+}
+
+pub trait ConfigSetHgExt {
+    /// Load system config files.
+    /// `data_dir` is `mercurial.util.datapath`.
+    fn load_system<P: AsRef<Path>>(&mut self, data_dir: P);
+
+    /// Load user config files (and environment variables).
+    fn load_user(&mut self);
 }
 
 impl OptionsHgExt for Options {
@@ -76,10 +86,87 @@ impl OptionsHgExt for Options {
     }
 }
 
+impl ConfigSetHgExt for ConfigSet {
+    fn load_system<P: AsRef<Path>>(&mut self, data_dir: P) {
+        let opts = Options::new().source("system").process_hgplain();
+        let data_dir = data_dir.as_ref();
+
+        if env::var("HGRCPATH").is_err() {
+            #[cfg(unix)]
+            {
+                self.load_path(data_dir.join("default.d/"), &opts);
+                self.load_path("/etc/mercurial/hgrc", &opts);
+                self.load_path("/etc/mercurial/hgrc.d/", &opts);
+            }
+
+            #[cfg(windows)]
+            {
+                self.load_path(data_dir.join("default.d/"), &opts);
+                self.load_path(data_dir.join("mercurial.ini"), &opts);
+                self.load_path(data_dir.join("hgrc.d/"), &opts);
+            }
+        }
+    }
+
+    fn load_user(&mut self) {
+        // Covert "$VISUAL", "$EDITOR" to "ui.editor".
+        //
+        // Unlike Mercurial, don't convert the "$PAGER" environment variable
+        // to "pager.pager" config.
+        //
+        // The environment variable could be from the system profile (ex.
+        // /etc/profile.d/...), or the user shell rc (ex. ~/.bashrc). There is
+        // no clean way to tell which one it is from.  The value might be
+        // tweaked for sysadmin usecases (ex. -n), which are different from
+        // SCM's usecases.
+        for name in ["VISUAL", "EDITOR"].iter() {
+            if let Ok(editor) = env::var(name) {
+                self.set(
+                    "ui",
+                    "editor",
+                    Some(editor.as_bytes()),
+                    &Options::new().source(format!("${}", name)),
+                );
+                break;
+            }
+        }
+
+        let opts = Options::new().source("user").process_hgplain();
+
+        // If $HGRCPATH is set, use it instead.
+        if let Ok(rcpath) = env::var("HGRCPATH") {
+            #[cfg(unix)]
+            let paths = rcpath.split(':');
+            #[cfg(windows)]
+            let paths = rcpath.split(';');
+            for path in paths {
+                self.load_path(path, &opts);
+            }
+        } else {
+            #[allow(deprecated)]
+            let option_home_dir = env::home_dir();
+            if let Some(home_dir) = option_home_dir {
+                #[cfg(unix)]
+                {
+                    self.load_path(home_dir.join(".hgrc"), &opts);
+                    if let Ok(config_home) = ::std::env::var("XDG_CONFIG_HOME") {
+                        self.load_path(format!("{}/hg/hgrc", config_home), &opts);
+                    }
+                }
+
+                #[cfg(windows)]
+                {
+                    self.load_path(home_dir.join("mercurial.ini"), &opts);
+                    self.load_path(home_dir.join(".hgrc"), &opts);
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use config::ConfigSet;
 
     #[test]
     fn test_basic_hgplain() {
