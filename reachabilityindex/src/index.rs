9e002a21d250bcf101c00300918b311f6c83895b
Matthew Dippel <mdippel@fb.com>
2018-08-10 15:26:16 -0700

    Trait definition for indexes which can compute an "LCA hint"
    Summary:
    Added a definition for a trait `LeastCommonAncestorHint`, for indexes which can compute an advanced frontier from a starting set of nodes.
    Any implementation of this trait will need a method which takes a set of `HgNodeHash` "nodes", and a `Generation` value "gen", and returns a set of nodes "C" which satisfies:
    * Max generation in "C" is <= "gen"
    * Any ancestor of "nodes" with generation <= gen is also an ancestor of "C"
    
    The plan is to implement this trait for `SkiplistIndex` and introduce it into parts of the revset code.
    
    To elaborate, the current way `DifferenceOfUnionOfAncestorsNodeStream` operates, it assumes that the nodes to exclude can be used as a stream, producing (Generation, HashSet<HgNodeHash>) pairs when polled. In order to use this work to improve that, this assumption will have to be removed, because we will want to advance the exclude_nodes based on the current maximum generation of nodes to include that we know about. Then, the only structure that needs to be replaced in `DifferenceOfUnionOfAncestorsNodeStream` is exclude_nodes. It is used in the following way:
    * When trying to determine if an include node should be yielded, the exclude_stream is peeked at. If it is the generation equal to the node being checked, it is checked if that node is in the exclude_stream node set. Else if the generation is less than the node being checked, the node is yielded by default.
    * If the generation of the peeked exclude_stream is larger than that of the node being checked, it cannot yet be determined if that node should be yielded or not. And so the exclude_stream is "polled", which forces it to move to the next generation value.
    
    Replacing this structure with a `NodeFrontier`, and the code that uses it so that it can use `LeastCommonAncestorHint` to move the exclude_node frontier forward, should suffice to introduce this work into `DifferenceOfUnionOfAncestorsNodeStream`.
    
    Reviewed By: jsgf
    
    Differential Revision: D9120503
    
    fbshipit-source-id: 317af81a1e335e66cf72603899aa06f28c85b027

--- reachabilityindex/src/index.rs
+++ reachabilityindex/src/index.rs
@@ -4,6 +4,7 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
+use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 
 use failure::Error;
@@ -11,6 +12,19 @@ use futures_ext::BoxFuture;
 
 use blobrepo::BlobRepo;
 use mercurial_types::HgNodeHash;
+use mononoke_types::Generation;
+
+#[allow(dead_code)]
+pub struct NodeFrontier {
+    pub gen_map: HashMap<Generation, HashSet<HgNodeHash>>,
+}
+
+#[allow(dead_code)]
+impl NodeFrontier {
+    pub fn new(gen_map: HashMap<Generation, HashSet<HgNodeHash>>) -> Self {
+        NodeFrontier { gen_map }
+    }
+}
 
 /// Trait for any method of supporting reachability queries
 pub trait ReachabilityIndex {
@@ -21,4 +35,19 @@ pub trait ReachabilityIndex {
         src: HgNodeHash,
         dst: HgNodeHash,
     ) -> BoxFuture<bool, Error>;
+}
+
+/// Trait for any method supporting computing an "LCA hint"
+pub trait LeastCommonAncestorsHint {
+    /// Return a Future for an advanced frontier of ancestors from a set of nodes.
+    /// Given a set "nodes", and a maximum generation "gen",
+    /// return a set of nodes "C" which satisfies:
+    /// - Max generation number in "C" is <= gen
+    /// - Any ancestor of "nodes" with generation <= gen is also an ancestor of "C"
+    fn lca_hint(
+        &self,
+        repo: Arc<BlobRepo>,
+        node_frontier: NodeFrontier,
+        gen: Generation,
+    ) -> BoxFuture<NodeFrontier, Error>;
 }
