c8d9db34df0efa9ed35945b14a53fc65345ddba1
Durham Goode <durham@fb.com>
2018-08-15 15:05:33 -0700

    revisionstore: add AncestorIterator class
    Summary:
    This moves the ancestor iteration logic for cases where we iterate one
    by one. This will be used by the HistoryPack code in upcoming diffs.
    
    Reviewed By: quark-zju
    
    Differential Revision: D9136978
    
    fbshipit-source-id: e60b0a1e2ee5036938b51bbd910fbaf548d7aa75

--- lib/revisionstore/src/ancestors.rs
+++ lib/revisionstore/src/ancestors.rs
@@ -9,11 +9,29 @@ use key::Key;
 #[fail(display = "Ancestor Iterator Error: {:?}", _0)]
 struct AncestorIteratorError(String);
 
+pub struct AncestorIterator<T: Fn(&Key, &HashSet<Key>) -> Result<NodeInfo>> {
+    get_more: T,
+    seen: HashSet<Key>,
+    queue: VecDeque<Key>,
+}
+
 pub struct BatchedAncestorIterator<T: Fn(&Key, &HashSet<Key>) -> Result<Ancestors>> {
     get_more: T,
     seen: HashSet<Key>,
     queue: VecDeque<Key>,
     pending_infos: HashMap<Key, NodeInfo>,
+}
+
+impl<T: Fn(&Key, &HashSet<Key>) -> Result<NodeInfo>> AncestorIterator<T> {
+    pub fn new(key: &Key, get_more: T) -> Self {
+        let mut iter = AncestorIterator {
+            get_more: get_more,
+            seen: HashSet::new(),
+            queue: VecDeque::new(),
+        };
+        iter.queue.push_back(key.clone());
+        iter
+    }
 }
 
 impl<T: Fn(&Key, &HashSet<Key>) -> Result<Ancestors>> BatchedAncestorIterator<T> {
@@ -26,6 +44,29 @@ impl<T: Fn(&Key, &HashSet<Key>) -> Result<Ancestors>> BatchedAncestorIterator<T>
         };
         iter.queue.push_back(key.clone());
         iter
+    }
+}
+
+impl<T: Fn(&Key, &HashSet<Key>) -> Result<NodeInfo>> Iterator for AncestorIterator<T> {
+    type Item = Result<(Key, NodeInfo)>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(current) = self.queue.pop_front() {
+            match (self.get_more)(&current, &self.seen) {
+                Err(e) => Some(Err(e)),
+                Ok(node_info) => {
+                    self.seen.insert(current.clone());
+                    for parent in node_info.parents.iter() {
+                        if !self.seen.contains(parent) {
+                            self.queue.push_back(parent.clone());
+                        }
+                    }
+                    Some(Ok((current, node_info.clone())))
+                }
+            }
+        } else {
+            None
+        }
     }
 }
 
