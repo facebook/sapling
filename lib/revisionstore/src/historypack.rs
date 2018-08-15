5e01141c7b187788def00877de1eef75bc297585
Durham Goode <durham@fb.com>
2018-08-15 15:05:57 -0700

    historypack: implement HistoryEntry serialization logic
    Summary:
    In an upcoming diff we'll begin writing the actual history data pack
    file. It is primarily composed of HistoryEntry's so let's implement and unit
    test the serialization logic for them.
    
    Reviewed By: markbt
    
    Differential Revision: D9231391
    
    fbshipit-source-id: 1b070ee5d15e06ea0c70a678dc6eb129e0ffaa20

--- lib/revisionstore/src/historypack.rs
+++ lib/revisionstore/src/historypack.rs
@@ -2,6 +2,7 @@ use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use std::io::{Cursor, Read, Write};
 
 use error::Result;
+use node::Node;
 
 #[derive(Debug, Fail)]
 #[fail(display = "Historypack Error: {:?}", _0)]
@@ -41,6 +42,15 @@ struct FileSectionHeader<'a> {
     pub count: u32,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct HistoryEntry<'a> {
+    pub node: Node,
+    pub p1: Node,
+    pub p2: Node,
+    pub link_node: Node,
+    pub copy_from: Option<&'a [u8]>,
+}
+
 fn read_slice<'a, 'b>(cur: &'a mut Cursor<&[u8]>, buf: &'b [u8], size: usize) -> Result<&'b [u8]> {
     let start = cur.position() as usize;
     let end = start + size;
@@ -73,6 +83,66 @@ impl<'a> FileSectionHeader<'a> {
     }
 }
 
+impl<'a> HistoryEntry<'a> {
+    pub(crate) fn read(buf: &[u8]) -> Result<HistoryEntry> {
+        let mut cur = Cursor::new(buf);
+        let mut node_buf: [u8; 20] = Default::default();
+
+        // Node
+        cur.read_exact(&mut node_buf)?;
+        let node = Node::from(&node_buf);
+
+        // Parents
+        cur.read_exact(&mut node_buf)?;
+        let p1 = Node::from(&node_buf);
+        cur.read_exact(&mut node_buf)?;
+        let p2 = Node::from(&node_buf);
+
+        // LinkNode
+        cur.read_exact(&mut node_buf)?;
+        let link_node = Node::from(&node_buf);
+
+        // Copyfrom
+        let copy_from_len = cur.read_u16::<BigEndian>()? as usize;
+        let copy_from = if copy_from_len > 0 {
+            Some(read_slice(&mut cur, &buf, copy_from_len)?)
+        } else {
+            None
+        };
+
+        Ok(HistoryEntry {
+            node,
+            p1,
+            p2,
+            link_node,
+            copy_from,
+        })
+    }
+
+    pub fn write<T: Write>(
+        writer: &mut T,
+        node: &Node,
+        p1: &Node,
+        p2: &Node,
+        linknode: &Node,
+        copy_from: &Option<&[u8]>,
+    ) -> Result<()> {
+        writer.write_all(node.as_ref())?;
+        writer.write_all(p1.as_ref())?;
+        writer.write_all(p2.as_ref())?;
+        writer.write_all(linknode.as_ref())?;
+        match copy_from {
+            Some(file_name) => {
+                writer.write_u16::<BigEndian>(file_name.len() as u16)?;
+                writer.write_all(file_name)?;
+            }
+            None => writer.write_u16::<BigEndian>(0)?,
+        };
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -86,6 +156,30 @@ mod tests {
             let mut buf = vec![];
             header.write(&mut buf).unwrap();
             header == FileSectionHeader::read(&buf).unwrap()
+        }
+
+        fn test_history_entry_serialization(
+            node: Node,
+            p1: Node,
+            p2: Node,
+            link_node: Node,
+            copy_from: Option<Vec<u8>>
+        ) -> bool {
+            let mut buf = vec![];
+            HistoryEntry::write(
+                &mut buf,
+                &node,
+                &p1,
+                &p2,
+                &link_node,
+                &copy_from.as_ref().map(|x| x.as_ref()),
+            ).unwrap();
+            let entry = HistoryEntry::read(&buf).unwrap();
+            assert_eq!(node, entry.node);
+            assert_eq!(p1, entry.p1);
+            assert_eq!(p2, entry.p2);
+            assert_eq!(link_node, entry.link_node);
+            true
         }
     }
 }
