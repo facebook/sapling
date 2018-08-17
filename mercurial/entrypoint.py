ff36c006e648ed0f3fc1541ac741684b8aba87d0
Kostia Balytskyi <ikostia@fb.com>
2018-08-17 10:40:01 -0700

    hg: move hg entrypoint to the mercurial dir
    Summary:
    We want to have two possible Python entry points for Mercurial:
    - pure-python one (`hg/hg` script, the one which is renamed into `hg.real` during FB installation)
    - the one to be called from a Rust binary
    
    The reason we can't reuse the `hg/hg` to be called form the Rust binary is because this script will eventually not exist. We also need something in the known location, so we chose to put `entrypoint.py` into `mercurial/`.
    
    This means that both the `hg` script and the `hg.rust` binary need to be able to find the `mercurial/` directory on the system. Traditionally, `hg` contained a `LIBDIR@` string, which was replaced with the parent directory of a `mercurial/` dir at install time. Thus we could install Mercurial in non-standard locations if we needed to. We keep this functionality for the `hg` script.
    
    `mercurial/entrypoint.py` however knows that it lives under `mercurial/` and that `hgext/` and `hgdemandimport/` live alongside `mercurial/`. Thus, the parent dir of `mercurial/` is added as a first item of `sys,path` in `entrypoint.py`.
    
    In case `entrypoint.py` is called from a Rust binary, Python import logic also adds the entire `mercurial/` dir to `sys.path`. This is not desired, since we use `absolute_import` and want to only be able to import things as `from mercurial import smth`. A good example is `json`: even with `absolute_import` enabled `import json` loads `mercurial/json.py` if hg is called as a Rust binary. Therefore, we explicitly remove `mercurial/` from `sys.path`.
    
    Reviewed By: quark-zju
    
    Differential Revision: D9336157
    
    fbshipit-source-id: 22f69e7782d549915c91ef9a0ad0ed29f62a9304

---
+++ mercurial/entrypoint.py
@@ -0,0 +1,66 @@
+# mercurial - scalable distributed SCM
+#
+# Copyright 2005-2007 Matt Mackall <mpm@selenic.com>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+from __future__ import absolute_import
+
+import os
+import sys
+
+
+def run():
+    # entrypoint is in mercurial/ dir, while we want 'from mercurial import ...',
+    # 'from hgext import ...' and 'from hgdemandimport import ...' to work
+    # so we are adding their parent directory to be the first item of sys.path
+    libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+    libdir = os.path.abspath(libdir)
+    if sys.path[0] != libdir:
+        sys.path.insert(0, libdir)
+
+    filedir = os.path.dirname(os.path.realpath(__file__))
+    if filedir in sys.path:
+        # the directory of entrypoint.py is mercurial/
+        # and it should not be present in sys.path, as we use absolute_import
+        sys.path.remove(filedir)
+
+    from mercurial import encoding
+
+    if encoding.environ.get("HGUNICODEPEDANTRY", False):
+        try:
+            reload(sys)
+            sys.setdefaultencoding("undefined")
+        except NameError:
+            pass
+
+    if (
+        sys.argv[1:5] == ["serve", "--cmdserver", "chgunix", "--address"]
+        and sys.argv[6:8] == ["--daemon-postexec", "chdir:/"]
+        and "CHGINTERNALMARK" in encoding.environ
+    ):
+        # Shortcut path for chg server
+        from mercurial import dispatch
+
+        dispatch.runchgserver()
+    else:
+        # Non-chg path
+        try:
+            if sys.version_info[0] < 3 or sys.version_info >= (3, 6):
+                import hgdemandimport
+
+                hgdemandimport.enable()
+        except ImportError:
+            sys.stderr.write(
+                "abort: couldn't find mercurial libraries in [%s]\n"
+                % " ".join(sys.path)
+            )
+            sys.stderr.write("(check your install and PYTHONPATH)\n")
+            sys.exit(-1)
+        from mercurial import dispatch
+
+        dispatch.run()
+
+
+if __name__ == "__main__":
+    run()
