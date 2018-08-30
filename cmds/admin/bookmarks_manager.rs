8d3b5bfb19c38fe0e902edc4bd275da1ea71877f
Sebastian Lund <selund@fb.com>
2018-08-30 04:13:40 -0700

    mononoke: add bookmark get support to admin tool
    Summary:
    Add the ability to get bookmarks using the mononoke admin tool.
    
    Usage: `mononoke_admin --repo-id <repo-id> bookmarks get --changeset-type <hg|bonsai> <BOOKMARK_NAME>`
    
    The changeset-type defaults to HG.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D9556742
    
    fbshipit-source-id: c5e64981947aabb9059295622501bc359ed57cc6

--- cmds/admin/bookmarks_manager.rs
+++ cmds/admin/bookmarks_manager.rs
@@ -4,9 +4,9 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
-use clap::{App, ArgMatches, SubCommand};
+use clap::{App, Arg, ArgMatches, SubCommand};
 use failure::Error;
-use futures::Future;
+use futures::{future, Future};
 use futures_ext::{BoxFuture, FutureExt};
 use slog::Logger;
 
@@ -14,6 +14,7 @@ use blobrepo::BlobRepo;
 use bookmarks::Bookmark;
 
 const SET_CMD: &'static str = "set";
+const GET_CMD: &'static str = "get";
 
 pub fn prepare_command<'a, 'b>(app: App<'a, 'b>) -> App<'a, 'b> {
     let set = SubCommand::with_name(SET_CMD)
@@ -26,8 +27,22 @@ pub fn prepare_command<'a, 'b>(app: App<'a, 'b>) -> App<'a, 'b> {
              <HG_CHANGESET_ID>      'revision to which the bookmark should point to'",
         );
 
+    let get = SubCommand::with_name(GET_CMD)
+        .about("gets the changeset of a specific bookmark")
+        .args_from_usage("<BOOKMARK_NAME>        'bookmark to target'")
+        .arg(
+            Arg::with_name("changeset-type")
+                .long("changeset-type")
+                .short("cs")
+                .takes_value(true)
+                .possible_values(&["bonsai", "hg"])
+                .required(false)
+                .help("What changeset type to return, either bonsai or hg. Defaults to hg."),
+        );
+
     app.about("set of commands to manipulate bookmarks")
         .subcommand(set)
+        .subcommand(get)
 }
 
 pub fn handle_command<'a>(
@@ -36,11 +51,39 @@ pub fn handle_command<'a>(
     logger: Logger,
 ) -> BoxFuture<(), Error> {
     match matches.subcommand() {
+        (GET_CMD, Some(sub_m)) => handle_get(sub_m, logger, repo.clone()),
         (SET_CMD, Some(sub_m)) => handle_set(sub_m, logger, repo.clone()),
         _ => {
             println!("{}", matches.usage());
             ::std::process::exit(1);
         }
+    }
+}
+
+fn handle_get<'a>(args: &ArgMatches<'a>, _logger: Logger, repo: BlobRepo) -> BoxFuture<(), Error> {
+    let bookmark_name = args.value_of("BOOKMARK_NAME").unwrap().to_string();
+    let bookmark = Bookmark::new(bookmark_name).unwrap();
+    let changeset_type = args.value_of("changeset-type").unwrap_or("hg");
+
+    match changeset_type {
+        "hg" => repo.get_bookmark(&bookmark)
+            .and_then(move |cs| {
+                println!("(HG) {}", cs.expect("bookmark could not be found"));
+                future::ok(())
+            })
+            .boxify(),
+
+        "bonsai" => {
+            cloned!(bookmark);
+            ::fetch_bonsai_changeset(bookmark.to_string().as_str(), &repo)
+                .and_then(move |bonsai_cs| {
+                    println!("(BONSAI) {}", bonsai_cs.get_changeset_id());
+                    future::ok(())
+                })
+                .boxify()
+        }
+
+        _ => panic!("Unknown changeset-type supplied"),
     }
 }
 
