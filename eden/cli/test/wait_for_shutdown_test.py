f39817488a79917d5ac856a7cd55be4c9076ca3b
Matt Glazar <strager@fb.com>
2018-09-21 14:50:41 -0700

    Add tests for wait_for_shutdown
    Summary: I plan on fixing a bug in wait_for_showdown. wait_for_showdown lacks test coverage, so my change could easily break wait_for_showdown. Improve wait_for_showdown's test coverage to prevent bugs.
    
    Reviewed By: chadaustin
    
    Differential Revision: D9980226
    
    fbshipit-source-id: 3f5019a2c5be32b75ca3daa25e799e956c93dab4

---
+++ eden/cli/test/wait_for_shutdown_test.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+import contextlib
+import signal
+import subprocess
+import threading
+import time
+import typing
+import unittest
+
+from ..daemon import wait_for_shutdown
+
+
+class WaitForShutdownTest(unittest.TestCase):
+    def test_waiting_for_exited_process_finishes_immediately(self) -> None:
+        process = AutoReapingChildProcess(["true"])
+        process.wait()
+
+        stop_watch = StopWatch()
+        with stop_watch.measure():
+            wait_for_shutdown(process.pid, timeout=5)
+        self.assertLessEqual(stop_watch.elapsed, 3)
+
+    def test_waiting_for_exiting_process_finishes_without_sigkill(self) -> None:
+        process = AutoReapingChildProcess(["sleep", "1"])
+        wait_for_shutdown(process.pid, timeout=5)
+        returncode = process.wait()
+        self.assertEqual(returncode, 0, "Process should have exited cleanly")
+
+    def test_waiting_for_alive_process_kills_with_sigkill(self) -> None:
+        process = AutoReapingChildProcess(["sleep", "30"])
+        wait_for_shutdown(process.pid, timeout=1)
+        returncode = process.wait()
+        self.assertEqual(
+            returncode, -signal.SIGKILL, "Process should have exited with SIGKILL"
+        )
+
+
+class AutoReapingChildProcess:
+    """A child process (subprocess.Popen) which is promptly reaped."""
+
+    def __init__(self, args) -> None:
+        super().__init__()
+
+        self.__condition = threading.Condition()
+        self.__error: typing.Optional[BaseException] = None
+        self.__pid: typing.Optional[int] = None
+        self.__returncode: typing.Optional[int] = None
+
+        self.__start_thread(args)
+        self.__wait_for_process_start()
+
+    @property
+    def pid(self) -> int:
+        with self.__condition:
+            pid = self.__pid
+            assert pid is not None
+            return pid
+
+    def wait(self) -> int:
+        with self.__condition:
+            while self.__returncode is None:
+                self.__condition.wait()
+            return self.__returncode
+
+    def __wait_for_process_start(self) -> None:
+        with self.__condition:
+            while self.__pid is None and self.__error is None:
+                self.__condition.wait()
+            if self.__error is not None:
+                raise self.__error
+            assert self.__pid is not None
+
+    def __start_thread(self, *args, **kwargs) -> None:
+        thread = threading.Thread(
+            target=self.__run_thread, args=(args, kwargs), daemon=True
+        )
+        thread.start()
+
+    def __run_thread(self, popen_args, popen_kwargs) -> None:
+        try:
+            with subprocess.Popen(*popen_args, **popen_kwargs) as process:
+                with self.__condition:
+                    self.__pid = process.pid
+                    self.__condition.notify_all()
+                process.wait()
+                with self.__condition:
+                    self.__returncode = process.returncode
+                    self.__condition.notify_all()
+        except BaseException as e:
+            with self.__condition:
+                self.__error = e
+                self.__condition.notify_all()
+
+
+class StopWatch:
+    __elapsed: float = 0
+
+    @property
+    def elapsed(self) -> float:
+        return self.__elapsed
+
+    @contextlib.contextmanager
+    def measure(self) -> typing.Iterator[None]:
+        start_time = self.__time()
+        try:
+            yield
+        finally:
+            end_time = self.__time()
+            self.__elapsed += end_time - start_time
+
+    def __time(self) -> float:
+        return time.monotonic()
