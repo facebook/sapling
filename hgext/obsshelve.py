c46886e4152141a0ae4ce6838e76c6ab01361608
Kostia Balytskyi <ikostia@fb.com>
2018-10-16 14:19:25 -0700

    obsshelve: remove dummy extension
    Summary: After D10407265 this is no longer needed.
    
    Reviewed By: quark-zju
    
    Differential Revision: D10407826
    
    fbshipit-source-id: 5291981637d940a6d19cda1b814b81b745d9ca92

--- hgext/obsshelve.py
+++
@@ -1,4 +0,0 @@
-# This is a temporary workaround until T19372873 gets closed.
-# In a nutshell, because we have some messed up configs, we need a dummy
-# extension file to exist, so that `extensions.obsshelve=` in configs does
-# not cause any warnings
