3a7a65a236a2eca1a6ae984cb89d4316b69ff189
Adam Simpkins <simpkins@fb.com>
2018-11-12 20:23:19 -0800

    update the doteden_test code to use pathlib.Path
    Summary:
    Use `Path` objects and its associated APIs in most locations rather than using
    plain strings.
    
    Reviewed By: wez
    
    Differential Revision: D13035415
    
    fbshipit-source-id: ef3fb72e884b1f5dbc2a9bcd9f36ba1397a7c21a

--- eden/integration/doteden_test.py
+++ eden/integration/doteden_test.py
@@ -10,6 +10,8 @@
 import errno
 import os
 import stat
+from pathlib import Path
+from typing import List
 
 from .lib import testcase
 
@@ -20,80 +22,74 @@ class DotEdenTest(testcase.EdenRepoTest):
     Verify manipulating the .eden directory is disallowed.
     """
 
+    dot_eden_path: Path
+    entries: List[Path]
+
     def populate_repo(self) -> None:
         self.repo.write_file("hello", "hola\n")
         self.repo.commit("Initial commit.")
-        self.dot_eden_path = os.path.join(self.mount, ".eden")
+        self.dot_eden_path = Path(self.mount) / ".eden"
 
-    def setUp(self):
+    def setUp(self) -> None:
         super().setUp()
-        self.entries = os.listdir(self.dot_eden_path)
+        self.entries = list(self.dot_eden_path.iterdir())
         self.assertNotEqual([], self.entries)
 
     def test_rm_existing_contents_fails(self) -> None:
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
-                os.unlink(os.path.join(self.dot_eden_path, entry))
+                entry.unlink()
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
     def test_mkdir_fails(self):
         with self.assertRaises(OSError) as cm:
-            os.mkdir(os.path.join(self.dot_eden_path, "subdir"))
+            (self.dot_eden_path / "subdir").mkdir()
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
     def test_rmdir_fails(self):
         with self.assertRaises(OSError) as cm:
-            os.rmdir(os.path.join(self.dot_eden_path, "subdir"))
+            (self.dot_eden_path / "subdir").rmdir()
         # It is no longer possible to create a directory inside .eden -
         # if it was, EPERM would be the right errno value.
         self.assertEqual(errno.ENOENT, cm.exception.errno)
 
     def test_create_file_fails(self):
         with self.assertRaises(OSError) as cm:
-            os.open(os.path.join(self.dot_eden_path, "file"), os.O_CREAT | os.O_RDWR)
+            os.open(bytes(self.dot_eden_path / "file"), os.O_CREAT | os.O_RDWR)
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
     def test_mknod_fails(self):
         with self.assertRaises(OSError) as cm:
-            os.mknod(os.path.join(self.dot_eden_path, "file"), stat.S_IFREG | 0o600)
+            os.mknod(bytes(self.dot_eden_path / "file"), stat.S_IFREG | 0o600)
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
     def test_symlink_fails(self):
         with self.assertRaises(OSError) as cm:
-            os.symlink("/", os.path.join(self.dot_eden_path, "lnk"))
+            (self.dot_eden_path / "lnk").symlink_to("/", target_is_directory=True)
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
     def test_rename_in_fails(self):
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
-                os.rename(
-                    os.path.join(self.dot_eden_path, entry),
-                    os.path.join(self.dot_eden_path, "dst"),
-                )
+                entry.rename(self.dot_eden_path / "dst")
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
     def test_rename_from_fails(self):
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
-                os.rename(
-                    os.path.join(self.dot_eden_path, entry),
-                    os.path.join(self.mount, "dst"),
-                )
+                entry.rename(Path(self.mount) / "dst")
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
     def test_rename_to_fails(self):
         with self.assertRaises(OSError) as cm:
-            os.rename(
-                os.path.join(self.mount, "hello"),
-                os.path.join(self.dot_eden_path, "dst"),
-            )
+            (Path(self.mount) / "hello").rename(self.dot_eden_path / "dst")
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
     def test_chown_fails(self):
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
                 os.chown(
-                    os.path.join(self.dot_eden_path, entry),
+                    bytes(entry),
                     uid=os.getuid(),
                     gid=os.getgid(),
                     follow_symlinks=False,
@@ -104,16 +100,12 @@ class DotEdenTest(testcase.EdenRepoTest):
     def xtest_chmod_fails(self):
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
-                os.chmod(
-                    os.path.join(self.dot_eden_path, entry),
-                    0o543,
-                    follow_symlinks=False,
-                )
+                os.chmod(bytes(entry), 0o543, follow_symlinks=False)
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
     # utime() has no effect on symlinks.
     def xtest_touch_fails(self):
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
-                os.utime(os.path.join(self.dot_eden_path, entry))
+                os.utime(bytes(entry))
             self.assertEqual(errno.EPERM, cm.exception.errno)
