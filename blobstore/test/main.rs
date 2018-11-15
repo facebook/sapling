48ca128c8867a5079c9fc657a3ec74116aa25ee8
Jeremy Fitzhardinge <jsgf@fb.com>
2018-11-15 13:03:09 -0800

    mononoke/blobtest: make blobstore tests properly async
    Summary:
    Prepare for gluster blobstore by making the test framework properly
    async. It needs a full tokio environment (IO reactor, etc) for async operation, rather than
    just `Future::wait`.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D13055656
    
    fbshipit-source-id: 6263ff706b3a374f0868f05989b6880f3c5a16ed

--- blobstore/test/main.rs
+++ blobstore/test/main.rs
@@ -14,15 +14,20 @@ extern crate failure_ext as failure;
 extern crate futures;
 extern crate futures_ext;
 extern crate tempdir;
+extern crate tokio;
 
 extern crate blobstore;
 extern crate fileblob;
 extern crate mononoke_types;
 extern crate rocksblob;
 
+use std::sync::Arc;
+
 use bytes::Bytes;
+use failure::Error;
 use futures::Future;
 use tempdir::TempDir;
+use tokio::{prelude::*, runtime::Runtime};
 
 use blobstore::{Blobstore, EagerMemblob};
 use fileblob::Fileblob;
@@ -31,38 +36,74 @@ use rocksblob::Rocksblob;
 
 fn simple<B>(blobstore: B)
 where
-    B: Blobstore,
+    B: IntoFuture,
+    B::Item: Blobstore,
+    B::Future: Send + 'static,
+    Error: From<B::Error>,
 {
+    let blobstore = blobstore.into_future().map_err(|err| err.into());
+
     let foo = "foo".to_string();
-    let res = blobstore
-        .put(foo.clone(), BlobstoreBytes::from_bytes(&b"bar"[..]))
-        .and_then(|_| blobstore.get(foo));
-    let out = res.wait().expect("pub/get failed").expect("missing");
+
+    let fut = future::lazy(|| {
+        blobstore.and_then(|blobstore| {
+            blobstore
+                .put(foo.clone(), BlobstoreBytes::from_bytes(&b"bar"[..]))
+                .and_then(move |_| blobstore.get(foo))
+        })
+    });
+
+    let mut runtime = Runtime::new().expect("runtime creation failed");
+    let out = runtime
+        .block_on(fut)
+        .expect("pub/get failed")
+        .expect("missing");
 
     assert_eq!(out.into_bytes(), Bytes::from_static(b"bar"));
 }
 
 fn missing<B>(blobstore: B)
 where
-    B: Blobstore,
+    B: IntoFuture,
+    B::Item: Blobstore,
+    B::Future: Send + 'static,
+    Error: From<B::Error>,
 {
-    let res = blobstore.get("missing".to_string());
-    let out = res.wait().expect("get failed");
+    let blobstore = blobstore.into_future().map_err(|err| err.into());
+
+    let fut =
+        future::lazy(move || blobstore.and_then(|blobstore| blobstore.get("missing".to_string())));
+
+    let mut runtime = Runtime::new().expect("runtime creation failed");
+    let out = runtime.block_on(fut).expect("get failed");
 
     assert!(out.is_none());
 }
 
 fn boxable<B>(blobstore: B)
 where
-    B: Blobstore,
+    B: IntoFuture,
+    B::Item: Blobstore,
+    B::Future: Send + 'static,
+    Error: From<B::Error>,
 {
-    let blobstore = Box::new(blobstore);
+    let blobstore = Box::new(blobstore.into_future().map_err(|err| err.into()));
 
     let foo = "foo".to_string();
-    let res = blobstore
-        .put(foo.clone(), BlobstoreBytes::from_bytes(&b"bar"[..]))
-        .and_then(|_| blobstore.get(foo));
-    let out: BlobstoreBytes = res.wait().expect("pub/get failed").expect("missing");
+
+    let fut = future::lazy(|| {
+        blobstore.and_then(|blobstore| {
+            blobstore
+                .put(foo.clone(), BlobstoreBytes::from_bytes(&b"bar"[..]))
+                .and_then(move |_| blobstore.get(foo))
+        })
+    });
+    let mut runtime = Runtime::new().expect("runtime creation failed");
+
+    let out: BlobstoreBytes = runtime
+        .block_on(fut)
+        .expect("pub/get failed")
+        .expect("missing");
 
     assert_eq!(out.into_bytes(), Bytes::from_static(b"bar"));
 }
@@ -79,19 +120,19 @@ macro_rules! blobstore_test_impl {
             #[test]
             fn test_simple() {
                 let state = $state;
-                simple($new_cb(&state));
+                simple($new_cb(state.clone()));
             }
 
             #[test]
             fn test_missing() {
                 let state = $state;
-                missing($new_cb(&state));
+                missing($new_cb(state.clone()));
             }
 
             #[test]
             fn test_boxable() {
                 let state = $state;
-                boxable($new_cb(&state));
+                boxable($new_cb(state.clone()));
             }
         }
     }
@@ -100,24 +141,24 @@ macro_rules! blobstore_test_impl {
 blobstore_test_impl! {
     memblob_test => {
         state: (),
-        new: |_| EagerMemblob::new(),
+        new: move |_| Ok::<_,!>(EagerMemblob::new()),
         persistent: false,
     }
 }
 
 blobstore_test_impl! {
     fileblob_test => {
-        state: TempDir::new("fileblob_test").unwrap(),
-        new: |dir| Fileblob::open(dir).unwrap(),
+        state: Arc::new(TempDir::new("fileblob_test").unwrap()),
+        new: move |dir: Arc<TempDir>| Fileblob::open(&*dir),
         persistent: true,
     }
 }
 
 blobstore_test_impl! {
     rocksblob_test => {
-        state: TempDir::new("rocksblob_test").unwrap(),
+        state: Arc::new(TempDir::new("rocksblob_test").unwrap()),
         // create/open may need to be unified once persistence tests are added
-        new: |dir| Rocksblob::create(dir).unwrap(),
+        new: move |dir: Arc<TempDir>| Rocksblob::create(&*dir),
         persistent: true,
     }
 }
