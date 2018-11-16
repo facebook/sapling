a07b4a97bcf131b530c8ddfab96d100f51df3355
Kostia Balytskyi <ikostia@fb.com>
2018-11-16 02:33:07 -0800

    hg.rust: ensure we rebuild if chg changes
    Summary: For the sake of incremental builds and `make local`.
    
    Reviewed By: quark-zju
    
    Differential Revision: D13058780
    
    fbshipit-source-id: f59dc235e75232b475ca0c2504b47ff0d4a64c8a

--- exec/hgmain/build.rs
+++ exec/hgmain/build.rs
@@ -6,9 +6,14 @@
 use std::env;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
     if let Some(lib_dirs) = env::var_os("LIB_DIRS") {
         for lib_dir in std::env::split_paths(&lib_dirs) {
             println!("cargo:rustc-link-search={}", lib_dir.display());
+            println!(
+                "cargo:rerun-if-changed={}",
+                lib_dir.join("libchg.a").display()
+            );
         }
     }
 }
