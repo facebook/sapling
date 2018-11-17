1b389f52b44c105f4b837aaf15ddcc3d3fa2e0aa
Adam Simpkins <simpkins@fb.com>
2018-11-16 19:39:20 -0800

    restructure storage_engine_test to make the type checkers happy
    Summary:
    Restructure storage_engine_test so that the base class derives from
    EdenTestCase.
    
    Reviewed By: strager
    
    Differential Revision: D13051096
    
    fbshipit-source-id: e89c4b56e361460b2457d1c2e6a22727a25d7646

--- eden/integration/hg/storage_engine_test.py
+++ eden/integration/hg/storage_engine_test.py
@@ -7,73 +7,86 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
+import abc
 import os
 
-from ..lib import hgrepo
+from eden.integration.lib import hgrepo
+
 from .lib.hg_extension_test_base import EdenHgTestCase, hg_test
 from .lib.histedit_command import HisteditCommand
 
 
-class StorageEngineTest:
-    _commit1: str
-    _commit2: str
-    _commit3: str
+class _Hidden:
+    # This _Hidden class exists soley to hide the abstract StorageEngineTest class from
+    # the unittest framework, so it does not find it during test discovery.  The
+    # unittest code is unfortunately not smart enough to skip abstract classes.
 
-    # These tests were originally copied from histedit_test.py. It doesn't
-    # matter which tests are used as long as commits are created and checked out
-    # and a realistic workflow is verified against each storage engine.
-    def populate_backing_repo(self, repo):
-        repo.write_file("first", "")
-        self._commit1 = repo.commit("first commit")
+    class StorageEngineTest(EdenHgTestCase, metaclass=abc.ABCMeta):
+        _commit1: str
+        _commit2: str
+        _commit3: str
 
-        repo.write_file("second", "")
-        self._commit2 = repo.commit("second commit")
+        # These tests were originally copied from histedit_test.py. It doesn't
+        # matter which tests are used as long as commits are created and checked out
+        # and a realistic workflow is verified against each storage engine.
+        def populate_backing_repo(self, repo: hgrepo.HgRepository) -> None:
+            repo.write_file("first", "")
+            self._commit1 = repo.commit("first commit")
 
-        repo.write_file("third", "")
-        self._commit3 = repo.commit("third commit")
+            repo.write_file("second", "")
+            self._commit2 = repo.commit("second commit")
 
-    def test_stop_at_earlier_commit_in_the_stack_without_reordering(self):
-        commits = self.repo.log()
-        self.assertEqual([self._commit1, self._commit2, self._commit3], commits)
+            repo.write_file("third", "")
+            self._commit3 = repo.commit("third commit")
 
-        # histedit, stopping in the middle of the stack.
-        histedit = HisteditCommand()
-        histedit.pick(self._commit1)
-        histedit.stop(self._commit2)
-        histedit.pick(self._commit3)
+        @abc.abstractmethod
+        def select_storage_engine(self) -> str:
+            raise NotImplementedError()
 
-        # We expect histedit to terminate with a nonzero exit code in this case.
-        with self.assertRaises(hgrepo.HgError) as context:
-            histedit.run(self)
-        head = self.repo.log(revset=".")[0]
-        expected_msg = (
-            "Changes committed as %s. " "You may amend the changeset now." % head[:12]
-        )
-        self.assertIn(expected_msg, str(context.exception))
+        def test_stop_at_earlier_commit_in_the_stack_without_reordering(self) -> None:
+            commits = self.repo.log()
+            self.assertEqual([self._commit1, self._commit2, self._commit3], commits)
 
-        # Verify the new commit stack and the histedit termination state.
-        # Note that the hash of commit[0] is unpredictable because Hg gives it a
-        # new hash in anticipation of the user amending it.
-        parent = self.repo.log(revset=".^")[0]
-        self.assertEqual(self._commit1, parent)
-        self.assertEqual(["first commit", "second commit"], self.repo.log("{desc}"))
+            # histedit, stopping in the middle of the stack.
+            histedit = HisteditCommand()
+            histedit.pick(self._commit1)
+            histedit.stop(self._commit2)
+            histedit.pick(self._commit3)
 
-        # Make sure the working copy is in the expected state.
-        self.assert_status_empty(op="histedit")
-        self.assertSetEqual(
-            {".eden", ".hg", "first", "second"},
-            set(os.listdir(self.repo.get_canonical_root())),
-        )
+            # We expect histedit to terminate with a nonzero exit code in this case.
+            with self.assertRaises(hgrepo.HgError) as context:
+                histedit.run(self)
+            head = self.repo.log(revset=".")[0]
+            expected_msg = (
+                "Changes committed as %s. "
+                "You may amend the changeset now." % head[:12]
+            )
+            self.assertIn(expected_msg, str(context.exception))
 
-        self.hg("histedit", "--continue")
-        self.assertEqual(
-            ["first commit", "second commit", "third commit"], self.repo.log("{desc}")
-        )
-        self.assert_status_empty()
-        self.assertSetEqual(
-            {".eden", ".hg", "first", "second", "third"},
-            set(os.listdir(self.repo.get_canonical_root())),
-        )
+            # Verify the new commit stack and the histedit termination state.
+            # Note that the hash of commit[0] is unpredictable because Hg gives it a
+            # new hash in anticipation of the user amending it.
+            parent = self.repo.log(revset=".^")[0]
+            self.assertEqual(self._commit1, parent)
+            self.assertEqual(["first commit", "second commit"], self.repo.log("{desc}"))
+
+            # Make sure the working copy is in the expected state.
+            self.assert_status_empty(op="histedit")
+            self.assertSetEqual(
+                {".eden", ".hg", "first", "second"},
+                set(os.listdir(self.repo.get_canonical_root())),
+            )
+
+            self.hg("histedit", "--continue")
+            self.assertEqual(
+                ["first commit", "second commit", "third commit"],
+                self.repo.log("{desc}"),
+            )
+            self.assert_status_empty()
+            self.assertSetEqual(
+                {".eden", ".hg", "first", "second", "third"},
+                set(os.listdir(self.repo.get_canonical_root())),
+            )
 
 
 # Each LocalStore implementation may complete its futures from different
@@ -81,18 +94,18 @@ class StorageEngineTest:
 
 
 @hg_test
-class HisteditMemoryStorageEngineTest(StorageEngineTest, EdenHgTestCase):
-    def select_storage_engine(self):
+class HisteditMemoryStorageEngineTest(_Hidden.StorageEngineTest):
+    def select_storage_engine(self) -> str:
         return "memory"
 
 
 @hg_test
-class HisteditSQLiteStorageEngineTest(StorageEngineTest, EdenHgTestCase):
-    def select_storage_engine(self):
+class HisteditSQLiteStorageEngineTest(_Hidden.StorageEngineTest):
+    def select_storage_engine(self) -> str:
         return "sqlite"
 
 
 @hg_test
-class HisteditRocksDBStorageEngineTest(StorageEngineTest, EdenHgTestCase):
-    def select_storage_engine(self):
+class HisteditRocksDBStorageEngineTest(_Hidden.StorageEngineTest):
+    def select_storage_engine(self) -> str:
         return "rocksdb"
