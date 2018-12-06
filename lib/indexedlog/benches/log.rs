42c3ef6eb652c3a63c19ad941245f88e41e5c9da
Jun Wu <quark@fb.com>
2018-12-06 14:52:31 -0800

    indexedlog: add benchmark for "log"
    Summary:
    Some simple benchmark for "log". The initial result running from my devserver
    looks like:
    
      log insertion                  33.146 ms
      log insertion with index      106.449 ms
      log flush                       9.623 ms
      log iteration (memory)         10.644 ms
      log iteration (disk)           11.517 ms
        13.75s user 3.61s system 97% cpu 17.778 total
      log insertion                  27.906 ms
      log insertion with index      107.683 ms
      log flush                      19.204 ms
      log iteration (memory)         10.239 ms
      log iteration (disk)           11.118 ms
        12.89s user 3.55s system 97% cpu 16.924 total
      log insertion                  31.645 ms
      log insertion with index      109.403 ms
      log flush                       9.416 ms
      log iteration (memory)         10.226 ms
      log iteration (disk)           10.757 ms
        13.07s user 3.02s system 97% cpu 16.423 total
      log insertion                  31.848 ms
      log insertion with index      109.332 ms
      log flush                      18.345 ms
      log iteration (memory)         10.709 ms
      log iteration (disk)           11.346 ms
        13.12s user 3.70s system 97% cpu 17.276 total
      log insertion                  29.665 ms
      log insertion with index      106.041 ms
      log flush                      16.159 ms
      log iteration (memory)         10.367 ms
      log iteration (disk)           11.110 ms
        12.99s user 3.27s system 97% cpu 16.717 total
    
    Reviewed By: markbt
    
    Differential Revision: D13015464
    
    fbshipit-source-id: 035fee6c8b6d0bea4cfe194eed3d58ba4b5ebcb8

---
+++ lib/indexedlog/benches/log.rs
@@ -0,0 +1,91 @@
+// Copyright 2018 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+extern crate indexedlog;
+extern crate minibench;
+extern crate rand;
+extern crate tempdir;
+
+use indexedlog::log::{IndexDef, IndexOutput, Log};
+use minibench::{bench, elapsed};
+use rand::{ChaChaRng, Rng};
+use tempdir::TempDir;
+
+const N: usize = 204800;
+
+/// Generate random buffer
+fn gen_buf(size: usize) -> Vec<u8> {
+    let mut buf = vec![0u8; size];
+    ChaChaRng::new_unseeded().fill_bytes(buf.as_mut());
+    buf
+}
+
+fn main() {
+    bench("log insertion", || {
+        let dir = TempDir::new("log").expect("TempDir::new");
+        let mut log = Log::open(dir.path(), vec![]).unwrap();
+        let buf = gen_buf(N * 20);
+        elapsed(move || {
+            for i in 0..N {
+                log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+            }
+        })
+    });
+
+    bench("log insertion with index", || {
+        let dir = TempDir::new("log").expect("TempDir::new");
+        let index_func = |_data: &[u8]| vec![IndexOutput::Reference(0..20)];
+        let index_def = IndexDef {
+            func: Box::new(index_func),
+            name: "i",
+            lag_threshold: 0,
+        };
+        let mut log = Log::open(dir.path(), vec![index_def]).unwrap();
+        let buf = gen_buf(N * 20);
+        elapsed(move || {
+            for i in 0..N {
+                log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+            }
+        })
+    });
+
+    bench("log flush", || {
+        let dir = TempDir::new("log").expect("TempDir::new");
+        let mut log = Log::open(dir.path(), vec![]).unwrap();
+        let buf = gen_buf(N * 20);
+        for i in 0..N {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        elapsed(|| {
+            log.flush().unwrap();
+        })
+    });
+
+    bench("log iteration (memory)", || {
+        let dir = TempDir::new("log").expect("TempDir::new");
+        let mut log = Log::open(dir.path(), vec![]).unwrap();
+        let buf = gen_buf(N * 20);
+        for i in 0..N {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        elapsed(move || {
+            log.iter().count();
+        })
+    });
+
+    bench("log iteration (disk)", || {
+        let dir = TempDir::new("log").expect("TempDir::new");
+        let mut log = Log::open(dir.path(), vec![]).unwrap();
+        let buf = gen_buf(N * 20);
+        for i in 0..N {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        log.flush().unwrap();
+        let log = Log::open(dir.path(), vec![]).unwrap();
+        elapsed(move || {
+            log.iter().count();
+        })
+    });
+}
