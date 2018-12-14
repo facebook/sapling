ca135cd33f6900520269c4cc5899145e5eec525b
Mark Thomas <mbthomas@fb.com>
2018-12-14 06:41:09 -0800

    cpython-failure: Integrate cpython PyResult with the failure crate
    Summary:
    Adds a new crate `cpython-result`, which provides a `ResultExt` trait, which
    extends the failure `Result` type to allow coversion to `PyResult` by
    converting the error to an appropriate Python Exception.
    
    Reviewed By: quark-zju
    
    Differential Revision: D12980782
    
    fbshipit-source-id: 44a63d31f9ecf2f77efa3b37c68f9a99eaf6d6fa

---
+++ lib/cpython-failure/src/lib.rs
@@ -0,0 +1,47 @@
+// Copyright 2018 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+//! Integrate cpython with failure
+
+extern crate cpython;
+extern crate failure;
+
+use cpython::{PyErr, PyResult, Python, PythonObjectWithTypeObject};
+use failure::AsFail;
+
+/// Extends the `Result` type to allow conversion to `PyResult` by specifying a
+/// Python Exception class to convert to.
+///
+/// # Examples
+///
+/// ```
+/// extern crate cpython;
+/// extern crate cpython_failure;
+/// #[macro_use] extern crate failure;
+///
+/// use cpython::{exc, Python, PyResult};
+/// use cpython_failure::ResultPyErrExt;
+/// use failure::Error;
+///
+/// fn fail_if_negative(i: i32) -> Result<i32, Error> {
+///    if (i >= 0) {
+///       Ok(i)
+///    } else {
+///       Err(format_err!("{} is negative", i))
+///    }
+/// }
+///
+/// fn py_fail_if_negative(py: Python, i: i32) -> PyResult<i32> {
+///    fail_if_negative(i).map_pyerr::<exc::ValueError>(py)
+/// }
+/// ```
+pub trait ResultPyErrExt<T> {
+    fn map_pyerr<PE: PythonObjectWithTypeObject>(self, py: Python) -> PyResult<T>;
+}
+
+impl<T, E: AsFail> ResultPyErrExt<T> for Result<T, E> {
+    fn map_pyerr<PE: PythonObjectWithTypeObject>(self, py: Python) -> PyResult<T> {
+        self.map_err(|e| PyErr::new::<PE, _>(py, e.as_fail().to_string()))
+    }
+}
