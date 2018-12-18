f116c2c9ff0cdb750a33de4ced430ec905690c7e
Adam Simpkins <simpkins@fb.com>
2018-12-17 19:13:09 -0800

    update the mononoke tests to use ScopedHTTPServer
    Summary:
    Change the code to use ScopedHTTPServer instead of its own custom logic that
    starts a server and runs all of the test logic inside the `onSuccess()`
    callback.
    
    Doing all of the test logic inside `onSuccess()` is generally bad, as most of
    the tests perform blocking `get()` calls on `folly::Future` objects.  Blocking
    work generally should not be done from inside EventBase threads.
    
    When built with ASAN these tests occasionally crash with heap-use-after-free
    errors during the HTTP server shutdown.  I didn't track down the exact bad
    behavior that was causing this, but attempting to stop the server from inside
    the `onSuccess()` callback does seem a little fishy.  Hopefully simply
    switching to `ScopedHTTPServer` will fix this issue.
    
    Reviewed By: wez
    
    Differential Revision: D13476413
    
    fbshipit-source-id: ab92cc16a5bf99a5e7b52529012a03786495c319

--- eden/fs/store/mononoke/test/MononokeTest.cpp
+++ eden/fs/store/mononoke/test/MononokeTest.cpp
@@ -17,6 +17,7 @@
 #include <proxygen/httpserver/HTTPServer.h>
 #include <proxygen/httpserver/RequestHandler.h>
 #include <proxygen/httpserver/ResponseBuilder.h>
+#include <proxygen/httpserver/ScopedHTTPServer.h>
 #include <proxygen/lib/http/HTTPCommonHeaders.h>
 
 #include "eden/fs/model/Blob.h"
@@ -129,22 +130,18 @@ class HandlerFactory : public RequestHandlerFactory {
 
 class MononokeBackingStoreTest : public ::testing::Test {
  protected:
-  std::unique_ptr<HTTPServer> createServer() {
-    std::string ip("localhost");
-    auto port = 0; // choose any free port
-    std::vector<HTTPServer::IPConfig> IPs = {
-        {SocketAddress(ip, port, true), HTTPServer::Protocol::HTTP},
-    };
+  std::unique_ptr<ScopedHTTPServer> createServer() {
+    SocketAddress address;
+    address.setFromLocalPort(uint16_t(0)); // choose any free port
+    HTTPServer::IPConfig ipConfig{address, HTTPServer::Protocol::HTTP};
 
     auto blobs = getBlobs();
     HTTPServerOptions options;
     options.threads = 1;
     options.handlerFactories =
         RequestHandlerChain().addThen<HandlerFactory>(blobs).build();
-    auto server = folly::make_unique<HTTPServer>(std::move(options));
-    server->bind(IPs);
 
-    return server;
+    return ScopedHTTPServer::start(ipConfig, std::move(options));
   }
 
   BlobContents getBlobs() {
@@ -181,32 +178,34 @@ TEST_F(MononokeBackingStoreTest, testGetBlob) {
   auto server = createServer();
   auto blobs = getBlobs();
 
-  server->start([&server, &blobs]() {
-    ScopedEventBaseThread evbThread;
-    MononokeBackingStore store(
-        "localhost",
-        server->addresses()[0].address,
-        "repo",
-        std::chrono::milliseconds(400),
-        evbThread.getEventBase(),
-        nullptr);
-    auto blob = store.getBlob(kZeroHash).get();
-    auto buf = blob->getContents();
-    EXPECT_EQ(blobs[kZeroHash.toString()], buf.moveToFbString());
-    server->stop();
-  });
-}
-
-TEST_F(MononokeBackingStoreTest, testConnectFailed) {
-  auto server = createServer();
-  auto blobs = getBlobs();
-
-  auto port = server->addresses()[0].address.getPort();
-  auto sa = SocketAddress("localhost", port, true);
   ScopedEventBaseThread evbThread;
   MononokeBackingStore store(
       "localhost",
-      sa,
+      server->getAddresses()[0].address,
+      "repo",
+      std::chrono::milliseconds(400),
+      evbThread.getEventBase(),
+      nullptr);
+  auto blob = store.getBlob(kZeroHash).get();
+  auto buf = blob->getContents();
+  EXPECT_EQ(blobs[kZeroHash.toString()], buf.moveToFbString());
+}
+
+TEST_F(MononokeBackingStoreTest, testConnectFailed) {
+  // To get a port that is guaranteed to reject connections,
+  // bind to an ephemeral port but never call listen()
+  SocketAddress address;
+  address.setFromLocalPort(uint16_t(0));
+  auto serverSocket = folly::AsyncServerSocket::newSocket();
+  serverSocket->bind(address);
+  address = serverSocket->getAddress();
+
+  auto blobs = getBlobs();
+
+  ScopedEventBaseThread evbThread;
+  MononokeBackingStore store(
+      "localhost",
+      address,
       "repo",
       std::chrono::milliseconds(300),
       evbThread.getEventBase(),
@@ -222,128 +221,116 @@ TEST_F(MononokeBackingStoreTest, testConnectFailed) {
 TEST_F(MononokeBackingStoreTest, testEmptyBuffer) {
   auto server = createServer();
   auto blobs = getBlobs();
-  auto emptyhash = this->emptyhash;
-  server->start([&server, &blobs, emptyhash]() {
-    ScopedEventBaseThread evbThread;
-    MononokeBackingStore store(
-        "localhost",
-        server->addresses()[0].address,
-        "repo",
-        std::chrono::milliseconds(300),
-        evbThread.getEventBase(),
-        nullptr);
-    auto blob = store.getBlob(emptyhash).get();
-    auto buf = blob->getContents();
-    EXPECT_EQ(blobs[emptyhash.toString()], buf.moveToFbString());
-    server->stop();
-  });
+
+  ScopedEventBaseThread evbThread;
+  MononokeBackingStore store(
+      "localhost",
+      server->getAddresses()[0].address,
+      "repo",
+      std::chrono::milliseconds(300),
+      evbThread.getEventBase(),
+      nullptr);
+  auto blob = store.getBlob(emptyhash).get();
+  auto buf = blob->getContents();
+  EXPECT_EQ(blobs[emptyhash.toString()], buf.moveToFbString());
 }
 
 TEST_F(MononokeBackingStoreTest, testGetTree) {
   auto server = createServer();
   auto blobs = getBlobs();
-  auto treehash = this->treehash;
-  server->start([&server, treehash]() {
-    ScopedEventBaseThread evbThread;
-    MononokeBackingStore store(
-        "localhost",
-        server->addresses()[0].address,
-        "repo",
-        std::chrono::milliseconds(300),
-        evbThread.getEventBase(),
-        nullptr);
-    auto tree = store.getTree(treehash).get();
-    auto tree_entries = tree->getTreeEntries();
 
-    std::vector<TreeEntry> expected_entries{
-        TreeEntry(
-            Hash("b80de5d138758541c5f05265ad144ab9fa86d1db"),
-            "a",
-            TreeEntryType::REGULAR_FILE),
-        TreeEntry(
-            Hash("b8e02f6433738021a065f94175c7cd23db5f05be"),
-            "b",
-            TreeEntryType::REGULAR_FILE),
-        TreeEntry(
-            Hash("3333333333333333333333333333333333333333"),
-            "dir",
-            TreeEntryType::TREE),
-        TreeEntry(
-            Hash("4444444444444444444444444444444444444444"),
-            "exec",
-            TreeEntryType::EXECUTABLE_FILE),
-        TreeEntry(
-            Hash("5555555555555555555555555555555555555555"),
-            "link",
-            TreeEntryType::SYMLINK),
-    };
+  ScopedEventBaseThread evbThread;
+  MononokeBackingStore store(
+      "localhost",
+      server->getAddresses()[0].address,
+      "repo",
+      std::chrono::milliseconds(300),
+      evbThread.getEventBase(),
+      nullptr);
+  auto tree = store.getTree(treehash).get();
+  auto tree_entries = tree->getTreeEntries();
 
-    Tree expected_tree(std::move(expected_entries), treehash);
-    EXPECT_TRUE(expected_tree == *tree);
-    server->stop();
-  });
+  std::vector<TreeEntry> expected_entries{
+      TreeEntry(
+          Hash("b80de5d138758541c5f05265ad144ab9fa86d1db"),
+          "a",
+          TreeEntryType::REGULAR_FILE),
+      TreeEntry(
+          Hash("b8e02f6433738021a065f94175c7cd23db5f05be"),
+          "b",
+          TreeEntryType::REGULAR_FILE),
+      TreeEntry(
+          Hash("3333333333333333333333333333333333333333"),
+          "dir",
+          TreeEntryType::TREE),
+      TreeEntry(
+          Hash("4444444444444444444444444444444444444444"),
+          "exec",
+          TreeEntryType::EXECUTABLE_FILE),
+      TreeEntry(
+          Hash("5555555555555555555555555555555555555555"),
+          "link",
+          TreeEntryType::SYMLINK),
+  };
+
+  Tree expected_tree(std::move(expected_entries), treehash);
+  EXPECT_TRUE(expected_tree == *tree);
 }
 
 TEST_F(MononokeBackingStoreTest, testMalformedGetTree) {
   auto server = createServer();
   auto blobs = getBlobs();
-  auto treehash = this->malformedhash;
-  server->start([&server, treehash]() {
-    ScopedEventBaseThread evbThread;
-    MononokeBackingStore store(
-        "localhost",
-        server->addresses()[0].address,
-        "repo",
-        std::chrono::milliseconds(300),
-        evbThread.getEventBase(),
-        nullptr);
-    EXPECT_THROW(store.getTree(treehash).get(), std::exception);
-    server->stop();
-  });
+
+  ScopedEventBaseThread evbThread;
+  MononokeBackingStore store(
+      "localhost",
+      server->getAddresses()[0].address,
+      "repo",
+      std::chrono::milliseconds(300),
+      evbThread.getEventBase(),
+      nullptr);
+  EXPECT_THROW(store.getTree(malformedhash).get(), std::exception);
 }
 
 TEST_F(MononokeBackingStoreTest, testGetTreeForCommit) {
   auto server = createServer();
   auto blobs = getBlobs();
   auto commithash = this->commithash;
-  auto treehash = this->treehash;
-  server->start([&server, commithash, treehash]() {
-    ScopedEventBaseThread evbThread;
-    MononokeBackingStore store(
-        "localhost",
-        server->addresses()[0].address,
-        "repo",
-        std::chrono::milliseconds(300),
-        evbThread.getEventBase(),
-        nullptr);
-    auto tree = store.getTreeForCommit(commithash).get();
-    auto tree_entries = tree->getTreeEntries();
 
-    std::vector<TreeEntry> expected_entries{
-        TreeEntry(
-            Hash("b80de5d138758541c5f05265ad144ab9fa86d1db"),
-            "a",
-            TreeEntryType::REGULAR_FILE),
-        TreeEntry(
-            Hash("b8e02f6433738021a065f94175c7cd23db5f05be"),
-            "b",
-            TreeEntryType::REGULAR_FILE),
-        TreeEntry(
-            Hash("3333333333333333333333333333333333333333"),
-            "dir",
-            TreeEntryType::TREE),
-        TreeEntry(
-            Hash("4444444444444444444444444444444444444444"),
-            "exec",
-            TreeEntryType::EXECUTABLE_FILE),
-        TreeEntry(
-            Hash("5555555555555555555555555555555555555555"),
-            "link",
-            TreeEntryType::SYMLINK),
-    };
+  ScopedEventBaseThread evbThread;
+  MononokeBackingStore store(
+      "localhost",
+      server->getAddresses()[0].address,
+      "repo",
+      std::chrono::milliseconds(300),
+      evbThread.getEventBase(),
+      nullptr);
+  auto tree = store.getTreeForCommit(commithash).get();
+  auto tree_entries = tree->getTreeEntries();
 
-    Tree expected_tree(std::move(expected_entries), treehash);
-    EXPECT_TRUE(expected_tree == *tree);
-    server->stop();
-  });
+  std::vector<TreeEntry> expected_entries{
+      TreeEntry(
+          Hash("b80de5d138758541c5f05265ad144ab9fa86d1db"),
+          "a",
+          TreeEntryType::REGULAR_FILE),
+      TreeEntry(
+          Hash("b8e02f6433738021a065f94175c7cd23db5f05be"),
+          "b",
+          TreeEntryType::REGULAR_FILE),
+      TreeEntry(
+          Hash("3333333333333333333333333333333333333333"),
+          "dir",
+          TreeEntryType::TREE),
+      TreeEntry(
+          Hash("4444444444444444444444444444444444444444"),
+          "exec",
+          TreeEntryType::EXECUTABLE_FILE),
+      TreeEntry(
+          Hash("5555555555555555555555555555555555555555"),
+          "link",
+          TreeEntryType::SYMLINK),
+  };
+
+  Tree expected_tree(std::move(expected_entries), treehash);
+  EXPECT_TRUE(expected_tree == *tree);
 }
