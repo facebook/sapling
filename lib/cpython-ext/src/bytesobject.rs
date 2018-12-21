f23c6bc7e384deaf253ccc92cec34c6a66a00448
Jun Wu <quark@fb.com>
2018-12-20 17:48:27 -0800

    cpython-ext: add a way to pre-allocate PyBytes
    Summary: Make it possible to write content directly into a PyBytes buffer.
    
    Reviewed By: DurhamG
    
    Differential Revision: D13528202
    
    fbshipit-source-id: 8c0a4ed030439a8dc40cdfbd72b1f6734a8b2036

---
+++ lib/cpython-ext/src/bytesobject.rs
@@ -0,0 +1,34 @@
+use cpython::{PyObject as RustPyObject, Python as RustPythonGILGuard};
+use python27_sys::{PyBytesObject, PyBytes_Type, PyObject, PyTypeObject, PyVarObject, Py_ssize_t,
+                   _PyObject_NewVar};
+use std::mem;
+use std::slice;
+
+/// Create a `PyBytes` object that have `size` bytes. Return the object and
+/// its internal buffer to be written. This is useful to bypass the memcpy
+/// cost creating a large `PyBytesObject`.
+pub fn allocate_pybytes(py: RustPythonGILGuard, size: usize) -> (RustPyObject, &mut [u8]) {
+    unsafe {
+        let ptr: *mut PyVarObject = _PyObject_NewVar(
+            &mut PyBytes_Type as *mut PyTypeObject,
+            (size + mem::size_of::<PyBytesObject>()) as Py_ssize_t,
+        );
+        let mut ptr: *mut PyObject = mem::transmute(ptr);
+        let typed: *mut PyBytesObject = mem::transmute(ptr);
+        (*typed).ob_shash = -1; // hash not calculated
+        (*typed).ob_sstate = 0; // SSTATE_NOT_INTERNED
+        (*typed).ob_size = size as Py_ssize_t;
+        // Set the first byte to '\0'. If the caller forgot to populate the
+        // slice, PyBytes_AsString would still return an empty C string.
+        (*typed).ob_sval[0] = 0;
+        // Set the byte after the slice to '\0' If the caller does populate the
+        // slice, PyBytes_AsString would return a C string with tailing '\0'.
+        *((*typed).ob_sval.as_mut_ptr().offset(size as isize)) = 0;
+        let pptr: *mut *mut PyObject = &mut ptr;
+        ptr = *pptr;
+        assert!(!ptr.is_null(), "out of memory");
+        let sval: *mut u8 = mem::transmute((*typed).ob_sval.as_mut_ptr());
+        let slice = slice::from_raw_parts_mut(sval, size);
+        (RustPyObject::from_owned_ptr(py, ptr), slice)
+    }
+}
--- lib/cpython-ext/src/lib.rs
+++ lib/cpython-ext/src/lib.rs
@@ -2,5 +2,7 @@ extern crate cpython;
 extern crate python27_sys;
 
 mod bytearrayobject;
+mod bytesobject;
 
 pub use bytearrayobject::{boxed_slice_to_pyobj, vec_to_pyobj};
+pub use bytesobject::allocate_pybytes;
