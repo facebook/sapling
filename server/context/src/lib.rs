9e12a9b943a95b483c3a7279d4fd201c7b1388b5
Harvey Hunt <harveyhunt@fb.com>
2019-01-16 09:07:46 -0800

    mononoke: Add PerfCounters struct to CoreContext
    Summary:
    PerfCounters is a small wrapper around a concurrent hashmap
    that can be used to store performance metrics in. Include it in CoreContext
    so that it can be used throughout the codebase.
    
    Reviewed By: aslpavel
    
    Differential Revision: D13528647
    
    fbshipit-source-id: 7c3f26ab8c0c7ba5ee619e85a069af7e7721037f

--- server/context/src/lib.rs
+++ server/context/src/lib.rs
@@ -4,12 +4,16 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
+extern crate chashmap;
 extern crate scuba_ext;
+extern crate serde;
 #[macro_use]
 extern crate slog;
 extern crate tracing;
 extern crate uuid;
 
+use chashmap::CHashMap;
+use serde::{Serialize, Serializer};
 use std::sync::Arc;
 
 use scuba_ext::ScubaSampleBuilder;
@@ -23,6 +27,61 @@ pub struct CoreContext {
 }
 
 #[derive(Debug, Clone)]
+pub struct PerfCounters {
+    // A wrapper around a concurrent HashMap that allows for
+    // tracking of arbitrary counters.
+    counters: CHashMap<&'static str, i64>,
+}
+
+impl Serialize for PerfCounters {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.collect_map(self.counters.clone().into_iter())
+    }
+}
+
+impl PerfCounters {
+    pub fn new() -> Self {
+        Self {
+            counters: CHashMap::new(),
+        }
+    }
+
+    pub fn change_counter<F>(&self, name: &'static str, default: i64, update: F)
+    where
+        F: FnOnce(&mut i64),
+    {
+        self.counters.upsert(name, || default, update);
+    }
+
+    pub fn set_counter(&self, name: &'static str, val: i64) {
+        self.counters.insert(name, val);
+    }
+
+    pub fn increment_counter(&self, name: &'static str) {
+        self.add_to_counter(name, 1);
+    }
+
+    pub fn decrement_counter(&self, name: &'static str) {
+        self.add_to_counter(name, -1);
+    }
+
+    pub fn add_to_counter(&self, name: &'static str, val: i64) {
+        self.change_counter(name, val, |old| *old += val);
+    }
+
+    pub fn set_max_counter(&self, name: &'static str, val: i64) {
+        self.change_counter(name, val, |old| {
+            if val > *old {
+                *old = val
+            }
+        });
+    }
+}
+
+#[derive(Debug, Clone)]
 struct Inner {
     session: Uuid,
     logger: Logger,
@@ -31,6 +90,7 @@ struct Inner {
     // shadow tier.
     wireproto_scribe_category: Option<String>,
     trace: TraceContext,
+    perf_counters: PerfCounters,
 }
 
 impl CoreContext {
@@ -48,6 +108,7 @@ impl CoreContext {
                 scuba,
                 wireproto_scribe_category,
                 trace,
+                perf_counters: PerfCounters::new(),
             }),
         }
     }
@@ -63,6 +124,7 @@ impl CoreContext {
                 scuba: self.inner.scuba.clone(),
                 wireproto_scribe_category: self.inner.wireproto_scribe_category.clone(),
                 trace: self.inner.trace.clone(),
+                perf_counters: self.inner.perf_counters.clone(),
             }),
         }
     }
@@ -78,6 +140,7 @@ impl CoreContext {
                 scuba: init(self.inner.scuba.clone()),
                 wireproto_scribe_category: self.inner.wireproto_scribe_category.clone(),
                 trace: self.inner.trace.clone(),
+                perf_counters: self.inner.perf_counters.clone(),
             }),
         }
     }
@@ -106,5 +169,8 @@ impl CoreContext {
     }
     pub fn trace(&self) -> &TraceContext {
         &self.inner.trace
+    }
+    pub fn perf_counters(&self) -> &PerfCounters {
+        &self.inner.perf_counters
     }
 }
