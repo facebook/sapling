f11c7fbf2634b64db3561cf0d409083657fa517f
Xavier Deguillard <xavierd@fb.com>
2019-02-04 09:26:42 -0800

    revisionstore: remove Clone requirement from UnionStore
    Summary:
    By specifying the IntoIterator differently, we can avoid the clone requirement.
    Since Clone isn't implemented on either DataPack or HistoryPack, this will
    simplify the callers a bit
    
    Reviewed By: DurhamG
    
    Differential Revision: D13928274
    
    fbshipit-source-id: f0261c50d73868689ebb3ae226f84d41c4c40925

--- lib/revisionstore/src/unionstore.rs
+++ lib/revisionstore/src/unionstore.rs
@@ -1,43 +1,37 @@
 // Copyright Facebook, Inc. 2018
 // Union store
 
+use std::slice::Iter;
 use std::vec::IntoIter;
 
 pub struct UnionStore<T> {
     stores: Vec<T>,
 }
 
-pub struct UnionStoreIterator<T>(IntoIter<T>);
-
 impl<T> UnionStore<T> {
     pub fn new() -> UnionStore<T> {
         UnionStore { stores: Vec::new() }
     }
 
-    pub fn add(&mut self, item: T)
-    where
-        T: Clone,
-    {
+    pub fn add(&mut self, item: T) {
         self.stores.push(item)
     }
 }
 
-impl<'a, T> IntoIterator for &'a UnionStore<T>
-where
-    T: Clone,
-{
+impl<T> IntoIterator for UnionStore<T> {
     type Item = T;
-    type IntoIter = UnionStoreIterator<T>;
+    type IntoIter = IntoIter<T>;
 
     fn into_iter(self) -> Self::IntoIter {
-        UnionStoreIterator(self.stores.clone().into_iter())
+        self.stores.into_iter()
     }
 }
 
-impl<T> Iterator for UnionStoreIterator<T> {
-    type Item = T;
+impl<'a, T> IntoIterator for &'a UnionStore<T> {
+    type Item = &'a T;
+    type IntoIter = Iter<'a, T>;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next()
+    fn into_iter(self) -> Self::IntoIter {
+        self.stores.iter()
     }
 }
