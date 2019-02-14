c1b8cd68d87ee8705a100d2160f2e995864600b9
Stefan Filip <sfilip@fb.com>
2019-02-14 12:50:05 -0800

    Add manifest crate
    Summary:
    The seed for the rust implementation of manifests.
    
    We start with the most primitive API for manifests and maps a paths to a `Node`. At the basic level we need the same operations that a map implements so we start with `insert`, `get` and `remove`. We know that retrieving data for Manifests can fail so we encode that in our interface using `Fallible`.
    
    I let for future iterations requiring iterator or returning manifest flags.
    
    Reviewed By: DurhamG
    
    Differential Revision: D14016274
    
    fbshipit-source-id: 8f1f83610933b9e9a96f8c5ba2c6e50567c76e06

---
+++ lib/manifest/src/lib.rs
@@ -0,0 +1,84 @@
+//! manifest - The contents of the repository at a specific commit.
+//!
+//! The history of the repository is recorded in the commit graph. Each commit has a manifest
+//! associated with it. The manifest specifies the revision for all the files available in the
+//! repository. The file path and file revision are then used to retrieve the contents of the
+//! file thus achieving the reconstruction of the entire repository state.
+use failure::Fallible;
+use types::node::Node;
+
+/// Manifest describes a mapping between file path ([`String`]) and file metadata ([`FileMetadata`]).
+/// Fundamentally it is just a Map<file_path, file_metadata>.
+///
+/// It can be assumed that Manifest interacts with an underlying store for persistance. These
+/// interactions may fail due to a variety of reasons. Such failures will be propagated up as Error
+/// return statuses.
+///
+/// Another common failure is passing in a path that the manifest has labeled as a directory. File
+/// paths composed of directory names and file names. Querying for paths that the Manifest has
+/// determined previously to be directories will result in Errors.
+pub trait Manifest {
+    /// Retrieve the FileMetadata that is associated with a path.
+    /// Paths that were not set will return None.
+    fn get(&self, file_path: &str) -> Fallible<Option<&FileMetadata>>;
+
+    /// Associates a file path with specific file metadata.
+    /// A call with a file path that already exists results in an override or the old metadata.
+    fn insert(&mut self, file_path: String, file_metadata: FileMetadata) -> Fallible<()>;
+
+    /// Removes a file from the manifest (equivalent to removing it from the repository).
+    /// A call with a file path that does not exist in the manifest is a no-op.
+    fn remove(&mut self, file_path: &str) -> Fallible<()>;
+}
+
+/// The contents of the Manifest for a file.
+/// * node: used to determine the revision of the file in the repository.
+/// * file_type: determines the type of the file.
+#[derive(Clone, Debug, Default, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct FileMetadata {
+    node: Node,
+    file_type: FileType,
+}
+
+/// The types of files that are supported.
+///
+/// The debate here is whether to use Regular { executable: bool } or an Executable variant.
+/// Technically speaking executable files are regular files. There is no big difference in terms
+/// of the mechanics between the two approaches. The approach using an Executable is more readable
+/// so that is what we have now.
+#[derive(Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub enum FileType {
+    /// Regular files.
+    Regular,
+    /// Executable files. Like Regular files but with the executable flag set.
+    Executable,
+    /// Symlinks. Their targets are not limited to repository paths. They can point anywhere.
+    Symlink,
+}
+
+impl Default for FileType {
+    fn default() -> Self {
+        FileType::Regular
+    }
+}
+
+impl FileMetadata {
+    pub fn new(node: Node, file_type: FileType) -> Self {
+        Self { node, file_type }
+    }
+
+    /// Creates `FileMetadata` with file_type set to `FileType::Regular`.
+    pub fn regular(node: Node) -> Self {
+        Self::new(node, FileType::Regular)
+    }
+
+    /// Creates `FileMetadata` with file_type set to `FileType::Executable`.
+    pub fn executable(node: Node) -> Self {
+        Self::new(node, FileType::Executable)
+    }
+
+    /// Creates `FileMetadata` with file_type set to `FileType::Symlink`.
+    pub fn symlink(node: Node) -> Self {
+        Self::new(node, FileType::Symlink)
+    }
+}
