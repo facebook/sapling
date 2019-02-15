63d868548f3bb6aef204135e804b31e204e70451
Wez Furlong <wez@fb.com>
2019-02-14 21:33:50 -0800

    eden: reinstate treemanifest support on macos
    Summary:
    This got lost partly because the symlink/shipit bit
    was a manual step when I did this first time around, and partly
    because the mercurial code got refactored.
    
    Restoring treemanifest restores `hg up` performance to saner
    levels.
    
    This is fairly straightforward, but now that we have !wez building this we've
    found that my machine had lz4 installed in the default include path and that we
    need to probe and find the correct one, so that is part of this diff also.
    
    Reviewed By: simpkins
    
    Differential Revision: D14078886
    
    fbshipit-source-id: 971cd49757bbb7dcc484439e6d896698598a583b

--- CMake/FBMercurialFeatures.cmake
+++ CMake/FBMercurialFeatures.cmake
@@ -4,11 +4,12 @@
 # the local fbsource repo when building at FB, or from the
 # external dir when building the OSS build.
 if (IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/fb-mercurial)
+  find_package(LZ4 MODULE REQUIRED)
   include_directories(${CMAKE_CURRENT_SOURCE_DIR}/fb-mercurial)
   add_library(
     libmpatch
     STATIC
-      fb-mercurial/mercurial/mpatch.c
+      fb-mercurial/edenscm/mercurial/mpatch.c
   )
 
   add_library(
@@ -20,14 +21,14 @@ if (IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/fb-mercurial)
   add_library(
     datapack
     STATIC
-      fb-mercurial/hgext/extlib/cstore/datapackstore.cpp
-      fb-mercurial/hgext/extlib/cstore/deltachain.cpp
-      fb-mercurial/hgext/extlib/cstore/uniondatapackstore.cpp
-      fb-mercurial/hgext/extlib/ctreemanifest/manifest.cpp
-      fb-mercurial/hgext/extlib/ctreemanifest/manifest_entry.cpp
-      fb-mercurial/hgext/extlib/ctreemanifest/manifest_fetcher.cpp
-      fb-mercurial/hgext/extlib/ctreemanifest/manifest_ptr.cpp
-      fb-mercurial/hgext/extlib/ctreemanifest/treemanifest.cpp
+      fb-mercurial/edenscm/hgext/extlib/cstore/datapackstore.cpp
+      fb-mercurial/edenscm/hgext/extlib/cstore/deltachain.cpp
+      fb-mercurial/edenscm/hgext/extlib/cstore/uniondatapackstore.cpp
+      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/manifest.cpp
+      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/manifest_entry.cpp
+      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/manifest_fetcher.cpp
+      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/manifest_ptr.cpp
+      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/treemanifest.cpp
       fb-mercurial/lib/cdatapack/cdatapack.c
   )
   target_link_libraries(
@@ -36,11 +37,13 @@ if (IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/fb-mercurial)
       libmpatch
       buffer
       ${OPENSSL_LIBRARIES}
+      ${LZ4_LIBRARY}
   )
   target_include_directories(
     datapack
     PUBLIC
     ${OPENSSL_INCLUDE_DIR}
+    ${LZ4_INCLUDE_DIR}
   )
 
   set(EDEN_HAVE_HG_TREEMANIFEST ON)
