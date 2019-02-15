61f9f25a66770344424c9df55961856b8a2ab3b5
Arun Kulshreshtha <kulshrax@fb.com>
2019-02-15 15:13:03 -0800

    edenapi-types: add crate for types shared between Mercurial and Mononoke
    Summary:
    For HTTP data fetching, it will be necessary to have the same Rust types in Mononoke and Mercurial, so that Mononoke can send down the serialized types and Mercurial can deserialize them. These types must live in the Mercurial codebase since Mercurial can't link to code outside of fbcode/scm/hg. As such, this diff adds a new crate to Mercurial that Mononoke can link to, containing these shared types.
    
    Right now the only shared type is a `HistoryEntry`, designed to match the interface of `MutableDatapack::add`. This type will be used as part of the HTTP history fetching API.
    
    In the longer term, it would probably make sense to use something like Thrift for defining the on-the-wire formats used between Mercurial and Mononoke (and eventually for RPC as well). However, given that using Thrift from Mercurial is currently nontrivial (since Mercurial is typically built with Cargo and needs to be compatible with open source tooling), defining the schema in this crate and using `serde` for serialization and HTTP/2 for transport should be sufficient for now.
    
    Reviewed By: quark-zju
    
    Differential Revision: D14079337
    
    fbshipit-source-id: c7880919aeb3fd7e1cf70067a89a17341c1d973f

---
+++ lib/edenapi-types/src/lib.rs
@@ -0,0 +1,21 @@
+// Copyright Facebook, Inc. 2019.
+
+//! edenapi-types: common types shared between Mercurial and Mononoke
+//!
+//! This crate contains types that are shared between the Mercurial and
+//! Mononoke codebases for the purpose of data interchange. It is
+//! necessary to put these types here because Mercurial cannot depend
+//! on anything outside of the fbcode/scm/hg directory, and thus cannot
+//! directly link to Mononoke code. Similarly, we cannot use Thrift data
+//! types since that would require using Buck for builds (whereas Mercurial
+//! must also support Cargo builds).
+
+use serde_derive::{Deserialize, Serialize};
+
+use revisionstore::{Key, NodeInfo};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HistoryEntry {
+    pub key: Key,
+    pub nodeinfo: NodeInfo,
+}
