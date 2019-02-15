20da8e235a044d17a3530ceebcd2ac8c5eaaf4a8
Jun Wu <quark@fb.com>
2019-02-14 17:36:26 -0800

    test-hgsubversion-unaffected-core: remove tests using named branches
    Differential Revision: D14063219
    
    fbshipit-source-id: 80cad750bfc5c0ef66d545e4a6be56342a195f8e

--- tests/test-hgsubversion-unaffected-core.py
+++ tests/test-hgsubversion-unaffected-core.py
@@ -67,42 +67,6 @@ class TestMercurialCore(test_hgsubversion_util.TestBase):
 
         self.assertEqual(str(repo[updaterev]), str(repo2["."]))
 
-    @test_hgsubversion_util.requiresoption("branch")
-    def test_branch(self):
-        """ Test 'clone --branch' """
-        ui = self.ui()
-        _dispatch(ui, ["init", "--quiet", self.wc_path])
-        repo = self.repo
-        repo.ui.setconfig("ui", "username", "anonymous")
-
-        fpath = os.path.join(self.wc_path, "it")
-        f = file(fpath, "w")
-        f.write("C1")
-        f.flush()
-        commands.add(ui, repo)
-        commands.branch(ui, repo, label="B1")
-        commands.commit(ui, repo, message="C1")
-        f.write("C2")
-        f.flush()
-        commands.branch(ui, repo, label="default")
-        commands.commit(ui, repo, message="C2")
-        f.write("C3")
-        f.flush()
-        commands.branch(ui, repo, label="B2")
-        commands.commit(ui, repo, message="C3")
-
-        self.assertEqual(test_hgsubversion_util.repolen(repo), 3)
-
-        branch = "B1"
-        _dispatch(
-            ui,
-            ["clone", "--quiet", self.wc_path, self.wc_path + "2", "--branch", branch],
-        )
-
-        repo2 = hg.repository(ui, self.wc_path + "2")
-
-        self.assertEqual(repo[branch].hex(), repo2["."].hex())
-
 
 if __name__ == "__main__":
     import silenttestrunner
