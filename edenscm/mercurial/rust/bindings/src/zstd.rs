932bb1492cd251ac4476d0e75991ecd702c1ee3f
Jun Wu <quark@fb.com>
2019-03-06 21:31:07 -0800

    zstd: make Python APIs zero-copy
    Summary:
    Make them zero-copy to reduce overhead. A side effect is the types become
    `bytearray`, instead of `bytes`. It seems fine for zstd use-case.
    
    Reviewed By: ikostia
    
    Differential Revision: D14338313
    
    fbshipit-source-id: af43d4cce5559fe877373737a71e1e1678a17ca7

--- edenscm/mercurial/rust/bindings/src/zstd.rs
+++ edenscm/mercurial/rust/bindings/src/zstd.rs
@@ -8,9 +8,9 @@
 use std::io;
 
 use cpython::*;
-use cpython_ext::SimplePyBuf;
+use cpython_ext::{vec_to_pyobj, SimplePyBuf};
 
-use ::zstd::stream::{decode_all, encode_all};
+use zstd::stream::{decode_all, encode_all};
 use zstdelta::{apply, diff};
 
 pub fn init_module(py: Python, package: &str) -> PyResult<PyModule> {
@@ -35,31 +35,31 @@ pub fn init_module(py: Python, package: &str) -> PyResult<PyModule> {
     Ok(m)
 }
 
-/// Convert `io::Result<Vec<u8>>` to a `PyResult<PyBytes>`.
-fn convert(py: Python, result: io::Result<Vec<u8>>) -> PyResult<PyBytes> {
+/// Convert `io::Result<Vec<u8>>` to a `PyResult<PyObject>` (bytearray).
+fn convert(py: Python, result: io::Result<Vec<u8>>) -> PyResult<PyObject> {
     result
         .map_err(|e| PyErr::new::<exc::RuntimeError, _>(py, format!("{}", e)))
-        .map(|buf| PyBytes::new(py, &buf))
+        .map(|buf| vec_to_pyobj(py, buf))
 }
 
-fn diff_py(py: Python, base: &PyObject, data: &PyObject) -> PyResult<PyBytes> {
+fn diff_py(py: Python, base: &PyObject, data: &PyObject) -> PyResult<PyObject> {
     let base = SimplePyBuf::new(py, base);
     let data = SimplePyBuf::new(py, data);
     convert(py, diff(base.as_ref(), data.as_ref()))
 }
 
-fn apply_py(py: Python, base: &PyObject, delta: &PyObject) -> PyResult<PyBytes> {
+fn apply_py(py: Python, base: &PyObject, delta: &PyObject) -> PyResult<PyObject> {
     let base = SimplePyBuf::new(py, base);
     let delta = SimplePyBuf::new(py, delta);
     convert(py, apply(base.as_ref(), delta.as_ref()))
 }
 
-fn decode_all_py(py: Python, data: &PyObject) -> PyResult<PyBytes> {
+fn decode_all_py(py: Python, data: &PyObject) -> PyResult<PyObject> {
     let data = SimplePyBuf::new(py, data);
     convert(py, decode_all(io::Cursor::new(data.as_ref())))
 }
 
-fn encode_all_py(py: Python, data: &PyObject, level: i32) -> PyResult<PyBytes> {
+fn encode_all_py(py: Python, data: &PyObject, level: i32) -> PyResult<PyObject> {
     let data = SimplePyBuf::new(py, data);
     convert(py, encode_all(io::Cursor::new(data.as_ref()), level))
 }
