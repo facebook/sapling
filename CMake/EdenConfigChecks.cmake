f8099e5129d9285b83f28a50d71c4bc9f3900a8f
Wez Furlong <wez@fb.com>
2019-03-28 20:53:29 -0700

    eden: cmake fixup include directories and deps
    Summary:
    While testing out the new getdeps code I found that none
    of the include directories from the probed libraries were being used.
    
    The new getdeps installs each dep into its own prefix, whereas the
    existing getdeps script installed them into the installation
    prefix for eden itself.  That meant that they were being implicitly
    found from a single include directory.
    
    In addition to this, I encountered linker failures for the pretty
    printers; the solution to those was to add appropriate deps for
    the modules that depend upon the pretty printers.
    
    Reviewed By: pkaush
    
    Differential Revision: D14638758
    
    fbshipit-source-id: a4c2b4c79603c268e1b1c707a05c3cb0e3f2757b

--- CMake/EdenConfigChecks.cmake
+++ CMake/EdenConfigChecks.cmake
@@ -11,20 +11,44 @@ set(CMAKE_THREAD_PREFER_PTHREAD ON)
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_package(Threads REQUIRED)
 
+find_package(gflags CONFIG REQUIRED)
+include_directories(${GFLAGS_INCLUDE_DIR})
+
 find_package(glog CONFIG REQUIRED)
+include_directories(${GLOG_INCLUDE_DIR})
+
+# We need to probe for libevent because the current stable version
+# of libevent doesn't publish the -L libdir in its exported interface
+# which means that folly simply exports `event` to us, leaving us
+# unable to resolve and link it.  Pulling in the package via its
+# config causes the event target to be defined and satisfies the
+# linker.
+find_package(Libevent CONFIG QUIET)
+
 find_package(folly CONFIG REQUIRED)
+include_directories(${FOLLY_INCLUDE_DIR})
+
 find_package(fizz CONFIG REQUIRED)
+include_directories(${FIZZ_INCLUDE_DIR})
+
 find_package(wangle CONFIG REQUIRED)
+include_directories(${WANGLE_INCLUDE_DIR})
+
 find_package(FBThrift CONFIG REQUIRED)
+include_directories(${FBTHRIFT_INCLUDE_DIR})
+
 find_package(yarpl CONFIG REQUIRED)
 find_package(rsocket CONFIG REQUIRED)
 find_package(GMock MODULE REQUIRED)
+include_directories(${GMOCK_INCLUDEDIR} ${LIBGMOCK_INCLUDE_DIR})
+
 find_package(OpenSSL MODULE REQUIRED)
 
 find_package(SELinux)
 set(EDEN_HAVE_SELINUX ${SELINUX_FOUND})
 
 find_package(LibGit2 REQUIRED)
+include_directories(${LIBGIT2_INCLUDE_DIRS})
 
 # The following packages ship with their own CMake configuration files
 find_package(cpptoml CONFIG REQUIRED)
