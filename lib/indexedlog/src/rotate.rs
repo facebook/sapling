aecd9edae96948e91921b2c282f1250980af97a6
Jun Wu <quark@fb.com>
2019-04-01 17:08:40 -0700

    logrotate: implement open
    Summary: Implement the open logic.
    
    Reviewed By: kulshrax
    
    Differential Revision: D14688170
    
    fbshipit-source-id: df3d39040e2268b3eddb131b2ae1b1f76d3e4311

--- lib/indexedlog/src/rotate.rs
+++ lib/indexedlog/src/rotate.rs
@@ -10,7 +10,9 @@ use lock::ScopedFileLock;
 use log::{self, IndexDef, Log};
 use std::fs;
 use std::io;
+use std::io::Write;
 use std::path::{Path, PathBuf};
+use utils::open_dir;
 
 /// A collection of [Log]s that get rotated or deleted automatically when they
 /// exceed size or count limits.
@@ -87,6 +89,94 @@ impl OpenOptions {
 
     /// Open [LogRotate] at given location.
     pub fn open(self, dir: impl AsRef<Path>) -> io::Result<LogRotate> {
-        unimplemented!()
+        let dir = dir.as_ref();
+
+        let latest_path = dir.join(LATEST_FILE);
+        let (latest, logs) = if !self.create || latest_path.exists() {
+            let latest = read_latest(dir)?;
+            (latest, read_logs(dir, &self, latest)?)
+        } else {
+            fs::create_dir_all(dir)?;
+            let mut lock_file = open_dir(dir)?;
+            let _lock = ScopedFileLock::new(&mut lock_file, true)?;
+            if latest_path.exists() {
+                // Two creates raced and the other one has created basic files.
+                let latest = read_latest(dir)?;
+                (latest, read_logs(dir, &self, latest)?)
+            } else {
+                (0, vec![create_empty_log(dir, &self, 0)?])
+            }
+        };
+
+        Ok(LogRotate {
+            dir: dir.into(),
+            open_options: self,
+            logs,
+            latest,
+        })
     }
+}
+
+fn create_empty_log(dir: &Path, open_options: &OpenOptions, latest: u64) -> io::Result<Log> {
+    let latest_path = dir.join(LATEST_FILE);
+    let latest_str = format!("{}", latest);
+    let log_path = dir.join(&latest_str);
+    let log = log::OpenOptions::new()
+        .create(true)
+        .index_defs(open_options.index_defs.clone())
+        .open(log_path)?;
+    AtomicFile::new(&latest_path, AllowOverwrite).write(|f| f.write_all(latest_str.as_bytes()))?;
+    Ok(log)
+}
+
+fn read_latest(dir: &Path) -> io::Result<u64> {
+    fs::read_to_string(&dir.join(LATEST_FILE))?
+        .parse()
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+}
+
+fn read_logs(dir: &Path, open_options: &OpenOptions, latest: u64) -> io::Result<Vec<Log>> {
+    let mut logs = Vec::new();
+    let mut current = latest;
+    let mut remaining = open_options.max_log_count;
+    while remaining > 0 {
+        let log_path = dir.join(format!("{}", current));
+        if let Ok(log) = log::OpenOptions::new()
+            .create(false)
+            .index_defs(open_options.index_defs.clone())
+            .open(&log_path)
+        {
+            logs.push(log);
+            current = current.wrapping_sub(1);
+            remaining -= 1;
+        } else {
+            break;
+        }
+    }
+
+    if logs.is_empty() {
+        Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            format!("no logs are found in {:?}", &dir),
+        ))
+    } else {
+        Ok(logs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempdir::TempDir;
+
+    #[test]
+    fn test_open() {
+        let dir = TempDir::new("log").unwrap();
+        let path = dir.path().join("rotate");
+
+        assert!(OpenOptions::new().create(false).open(&path).is_err());
+        assert!(OpenOptions::new().create(true).open(&path).is_ok());
+        assert!(OpenOptions::new().create(false).open(&path).is_ok());
+    }
+
 }
