9e6d77b35ae8cfbed0559f8abd098b282d86c360
Marla Azriel <mazriel@fb.com>
2019-04-19 11:13:40 -0700

    commands: update help text for amend
    Summary: Updated help text for hg amend and moved some options to verbose
    
    Reviewed By: kulshrax
    
    Differential Revision: D15004588
    
    fbshipit-source-id: 4c9e0bffb522184ac8750ed8aa4eb5a53b309bd0

--- edenscm/hgext/amend/__init__.py
+++ edenscm/hgext/amend/__init__.py
@@ -103,9 +103,9 @@ configitem("commands", "amend.autorebase", default=True)
 testedwith = "ships-with-fb-hgext"
 
 amendopts = [
-    ("", "rebase", None, _("rebases children after the amend")),
-    ("", "fixup", None, _("rebase children from a previous amend")),
-    ("", "to", "", _("amend to a specific commit in the current stack")),
+    ("", "rebase", None, _("rebases children after the amend (ADVANCED)")),
+    ("", "fixup", None, _("rebase children from a previous amend (ADVANCED)")),
+    ("", "to", "", _("amend to a specific commit in the current stack (ADVANCED)")),
 ] + cmdutil.templateopts
 
 # Never restack commits on amend.
@@ -274,6 +274,44 @@ def commit(orig, ui, repo, *pats, **opts):
 
 def amend(ui, repo, *pats, **opts):
     """save pending changes to the current commit
+
+    Replaces your current commit with a new commit that contains the contents
+    of the original commit, plus any pending changes.
+
+    By default, all pending changes (in other words, those reported by 'hg
+    status') are committed. To commit only some of your changes,
+    you can:
+
+    - Specify an exact list of files for which you want changes committed.
+
+    - Use the -I or -X flags to pattern match file names to exclude or
+      include by using a fileset. See 'hg help filesets' for more
+      information.
+
+    - Specify the --interactive flag to open a UI that will enable you
+      to select individual insertions or deletions.
+
+    By default, hg amend reuses your existing commit message and does not
+    prompt you for changes. To change your commit message, you can:
+
+    - Specify --edit / -e to open your configured editor to update the
+      existing commit message.
+
+    - Specify --message / -m to replace the entire commit message, including
+      any commit template fields, with a string that you specify.
+
+    .. note::
+
+       Specifying -m overwrites all information in the commit message,
+       including information specified as part of a pre-loaded commit
+       template. For example, any information associating this commit with
+       a code review system will be lost and might result in breakages.
+
+    When you amend a commit that has descendants, those descendants are
+    rebased on top of the amended version of the commit, unless doing so
+    would result in merge conflicts. If this happens, run 'hg restack'
+    to manually trigger the rebase so that you can go through the merge
+    conflict resolution process.
     """
     # 'rebase' is a tristate option: None=auto, True=force, False=disable
     rebase = opts.get("rebase")
