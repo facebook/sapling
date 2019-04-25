8a552db6daff7ad049f28ed8507945ea6961bb88
Jun Wu <quark@fb.com>
2019-04-25 16:59:48 -0700

    spanset: implement set operations
    Summary: Add intersection, union, and difference operations.
    
    Reviewed By: sfilipco
    
    Differential Revision: D15004986
    
    fbshipit-source-id: 8d1b5ca5ac5ba936afb136f5c5a4ddf1f8862161

--- lib/dag/src/spanset.rs
+++ lib/dag/src/spanset.rs
@@ -61,6 +61,148 @@ impl SpanSet {
                 .unwrap_or(false),
         }
     }
+
+    /// Calculates the union of two sets.
+    pub fn union(&self, rhs: &SpanSet) -> SpanSet {
+        let mut spans = Vec::with_capacity((self.spans.len() + rhs.spans.len()).min(32));
+        let mut iter_left = self.spans.iter().cloned();
+        let mut iter_right = rhs.spans.iter().cloned();
+        let mut next_left = iter_left.next();
+        let mut next_right = iter_right.next();
+        let mut push = |span: Span| push_with_union(&mut spans, span);
+
+        loop {
+            match (next_left, next_right) {
+                (Some(left), Some(right)) => {
+                    if left.1 < right.1 {
+                        push(right);
+                        next_right = iter_right.next();
+                    } else {
+                        push(left);
+                        next_left = iter_left.next();
+                    }
+                }
+                (Some(span), None) => {
+                    push(span);
+                    next_left = iter_left.next();
+                }
+                (None, Some(span)) => {
+                    push(span);
+                    next_right = iter_right.next();
+                }
+                (None, None) => {
+                    let result = SpanSet { spans };
+                    debug_assert!(result.is_valid());
+                    return result;
+                }
+            }
+        }
+    }
+
+    /// Calculates the intersection of two sets.
+    pub fn intersection(&self, rhs: &SpanSet) -> SpanSet {
+        let mut spans = Vec::with_capacity(self.spans.len().max(rhs.spans.len()).min(32));
+        let mut iter_left = self.spans.iter().cloned();
+        let mut iter_right = rhs.spans.iter().cloned();
+        let mut next_left = iter_left.next();
+        let mut next_right = iter_right.next();
+        let mut push = |span: Span| push_with_union(&mut spans, span);
+
+        loop {
+            match (next_left, next_right) {
+                (Some(left), Some(right)) => {
+                    let span = (left.0.max(right.0), left.1.min(right.1));
+                    if span.0 <= span.1 {
+                        push(span);
+                    }
+                    let right1 = (right.1 as i64).min(span.0 as i64 - 1);
+                    if right1 >= right.0 as i64 {
+                        next_right = Some((right.0, right1 as Id));
+                    } else {
+                        next_right = iter_right.next();
+                    }
+                    let left1 = (left.1 as i64).min(span.0 as i64 - 1);
+                    if left1 >= left.0 as i64 {
+                        next_left = Some((left.0, left1 as Id));
+                    } else {
+                        next_left = iter_left.next();
+                    }
+                }
+                (_, None) | (None, _) => {
+                    let result = SpanSet { spans };
+                    debug_assert!(result.is_valid());
+                    return result;
+                }
+            }
+        }
+    }
+
+    /// Calculates spans that are included only by this set, not `rhs`.
+    pub fn difference(&self, rhs: &SpanSet) -> SpanSet {
+        let mut spans = Vec::with_capacity(self.spans.len().max(rhs.spans.len()).min(32));
+        let mut iter_left = self.spans.iter().cloned();
+        let mut iter_right = rhs.spans.iter().cloned();
+        let mut next_left = iter_left.next();
+        let mut next_right = iter_right.next();
+        let mut push = |span: Span| push_with_union(&mut spans, span);
+
+        loop {
+            match (next_left, next_right) {
+                (Some(left), Some(right)) => {
+                    if right.0 > left.1 {
+                        next_right = iter_right.next();
+                    } else {
+                        next_left = if right.1 < left.0 {
+                            push(left);
+                            iter_left.next()
+                        } else {
+                            // |----------------- left ------------------|
+                            // |--- span1 ---|--- right ---|--- span2 ---|
+                            let span2 = (right.1 + 1, left.1);
+                            if span2.0 <= span2.1 {
+                                push(span2);
+                            }
+                            if right.0 > 0 {
+                                let span1 = (left.0, right.0 - 1);
+                                if span1.0 <= span1.1 {
+                                    Some(span1)
+                                } else {
+                                    iter_left.next()
+                                }
+                            } else {
+                                None
+                            }
+                        };
+                    }
+                }
+                (Some(left), None) => {
+                    push(left);
+                    next_left = iter_left.next();
+                }
+                (None, _) => {
+                    let result = SpanSet { spans };
+                    debug_assert!(result.is_valid());
+                    return result;
+                }
+            }
+        }
+    }
+}
+
+/// Push a span to `Vec<Span>`. Try to union them in-place.
+fn push_with_union(spans: &mut Vec<Span>, span: Span) {
+    match spans.last_mut() {
+        None => spans.push(span),
+        Some(mut last) => {
+            debug_assert!(last.1 >= span.1);
+            if last.0 <= span.1 + 1 {
+                // Union spans in-place.
+                last.0 = last.0.min(span.0);
+            } else {
+                spans.push(span)
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -123,5 +265,105 @@ mod tests {
         assert!(set.contains(39));
         assert!(set.contains(40));
         assert!(!set.contains(41));
+    }
+
+    fn union(a: Vec<Span>, b: Vec<Span>) -> Vec<Span> {
+        let a = SpanSet::from_sorted_spans(a);
+        let b = SpanSet::from_sorted_spans(b);
+        let spans1 = a.union(&b).spans;
+        let spans2 = b.union(&a).spans;
+        assert_eq!(spans1, spans2);
+        spans1
+    }
+
+    #[test]
+    fn test_union() {
+        assert_eq!(union(vec![(1, 10)], vec![(10, 20)]), vec![(1, 20)]);
+        assert_eq!(union(vec![(1, 30)], vec![(10, 20)]), vec![(1, 30)]);
+        assert_eq!(
+            union(vec![(10, 10), (8, 8), (6, 6)], vec![(9, 9), (7, 7), (5, 5)]),
+            vec![(5, 10)]
+        );
+        assert_eq!(
+            union(vec![(10, 10), (8, 9), (6, 6)], vec![(5, 5)]),
+            vec![(8, 10), (5, 6)]
+        );
+    }
+
+    fn intersect(a: Vec<Span>, b: Vec<Span>) -> Vec<Span> {
+        let a = SpanSet::from_sorted_spans(a);
+        let b = SpanSet::from_sorted_spans(b);
+        let spans1 = a.intersection(&b).spans;
+        let spans2 = b.intersection(&a).spans;
+        assert_eq!(spans1, spans2);
+        spans1
+    }
+
+    #[test]
+    fn test_intersection() {
+        assert_eq!(intersect(vec![(1, 10)], vec![(11, 20)]), vec![]);
+        assert_eq!(intersect(vec![(1, 10)], vec![(10, 20)]), vec![(10, 10)]);
+        assert_eq!(intersect(vec![(1, 30)], vec![(10, 20)]), vec![(10, 20)]);
+        assert_eq!(
+            intersect(vec![(15, 20), (0, 10)], vec![(0, 30)]),
+            vec![(15, 20), (0, 10)]
+        );
+        assert_eq!(
+            intersect(vec![(15, 20), (0, 10)], vec![(5, 19)]),
+            vec![(15, 19), (5, 10)]
+        );
+        assert_eq!(
+            intersect(vec![(10, 10), (9, 9), (8, 8), (7, 7)], vec![(8, 11)]),
+            vec![(8, 10)]
+        );
+        assert_eq!(
+            intersect(vec![(10, 10), (9, 9), (8, 8), (7, 7)], vec![(5, 8)]),
+            vec![(7, 8)]
+        );
+    }
+
+    fn difference(a: Vec<Span>, b: Vec<Span>) -> Vec<Span> {
+        let a = SpanSet::from_sorted_spans(a);
+        let b = SpanSet::from_sorted_spans(b);
+        let spans1 = a.difference(&b).spans;
+        let spans2 = b.difference(&a).spans;
+
+        // |------------- a -------------------|
+        // |--- spans1 ---|--- intersection ---|--- spans2 ---|
+        //                |------------------- b -------------|
+        let intersected = intersect(a.spans.clone(), b.spans.clone());
+        let unioned = union(a.spans.clone(), b.spans.clone());
+        assert_eq!(
+            union(intersected.clone(), spans1.clone()),
+            union(a.spans.clone(), Vec::new())
+        );
+        assert_eq!(
+            union(intersected.clone(), spans2.clone()),
+            union(b.spans.clone(), Vec::new())
+        );
+        assert_eq!(
+            union(spans1.clone(), union(intersected.clone(), spans2.clone())),
+            unioned.clone(),
+        );
+
+        spans1
+    }
+
+    #[test]
+    fn test_difference() {
+        assert_eq!(difference(vec![(0, 5)], Vec::new()), vec![(0, 5)]);
+        assert_eq!(difference(vec![], vec![(0, 5)]), vec![]);
+        assert_eq!(difference(vec![(0, 0)], vec![(1, 1)]), vec![(0, 0)]);
+        assert_eq!(difference(vec![(0, 0)], vec![(0, 1)]), vec![]);
+        assert_eq!(difference(vec![(0, 10)], vec![(0, 5)]), vec![(6, 10)]);
+
+        assert_eq!(
+            difference(vec![(0, 10)], vec![(7, 8), (3, 4)]),
+            vec![(9, 10), (5, 6), (0, 2)]
+        );
+        assert_eq!(
+            difference(vec![(10, 12), (7, 8), (3, 4)], vec![(4, 11)]),
+            vec![(12, 12), (3, 3)]
+        );
     }
 }
