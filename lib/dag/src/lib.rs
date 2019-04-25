26f1f12a2894e6f0762c5baf0af28a9e63ac6ed1
Jun Wu <quark@fb.com>
2019-04-25 16:59:48 -0700

    dag: add a library
    Summary: The scmdag library is going to have things related to the commit graph.
    
    Reviewed By: sfilipco
    
    Differential Revision: D15004984
    
    fbshipit-source-id: f274cceeabae4a57985763216572f7cd055f8e07

---
+++ lib/dag/src/lib.rs
@@ -0,0 +1,10 @@
+// Copyright 2019 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+#![allow(dead_code)]
+
+//! # dag
+//!
+//! Building blocks for the commit graph used by source control.
