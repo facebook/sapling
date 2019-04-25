d128becee7d82347c72f962eee0802464b1dcbcf
Adam Simpkins <simpkins@fb.com>
2019-04-24 19:59:48 -0700

    add a few additional operations to eden_store_util
    Summary:
    Change eden_store_util to take a subcommand argument, and define 3 separate
    subcommands: "gc", "clear", and "compact"
    
    The "gc" was the old default behavior.  The "clear" and "compact" subcommands
    simply clear cached entries and compact the storage, respectively.
    This also adds a `--keySpace` argument.  If this argument is specified the
    command will process only this single key space, instead of all key spaces.
    
    Reviewed By: chadaustin
    
    Differential Revision: D15043207
    
    fbshipit-source-id: 7e76bc89862d9647edde631ed3d82c9afda09f41

--- eden/fs/store/eden_store_util.cpp
+++ eden/fs/store/eden_store_util.cpp
@@ -8,7 +8,10 @@
  *
  */
 #include <sysexits.h>
+#include <memory>
+#include <optional>
 
+#include <folly/Range.h>
 #include <folly/init/Init.h>
 #include <folly/logging/Init.h>
 #include <folly/logging/xlog.h>
@@ -23,8 +26,13 @@
 #include "eden/fs/utils/FaultInjector.h"
 
 using namespace facebook::eden;
+using folly::StringPiece;
+using std::make_unique;
+using std::optional;
 
 FOLLY_INIT_LOGGING_CONFIG("eden=DBG2; default:async=true");
+
+DEFINE_string(keySpace, "", "operate on just a single key space");
 
 namespace {
 class StoreConfig : public ReloadableConfig {
@@ -42,37 +50,172 @@ class StoreConfig : public ReloadableConfig {
  private:
   std::shared_ptr<EdenConfig> config_;
 };
+
+LocalStore::KeySpace stringToKeySpace(StringPiece name) {
+  if (name == "blob") {
+    return LocalStore::BlobFamily;
+  } else if (name == "blobmeta") {
+    return LocalStore::BlobMetaDataFamily;
+  } else if (name == "tree") {
+    return LocalStore::TreeFamily;
+  } else if (name == "hgproxyhash") {
+    return LocalStore::HgProxyHashFamily;
+  } else if (name == "hgcommit2tree") {
+    return LocalStore::HgCommitToTreeFamily;
+  }
+  throw ArgumentError("unknown key space \"", name, "\"");
+}
+
+optional<LocalStore::KeySpace> getKeySpace() {
+  if (FLAGS_keySpace.empty()) {
+    return std::nullopt;
+  }
+  return stringToKeySpace(FLAGS_keySpace);
+}
+
+class Command {
+ public:
+  Command()
+      : userInfo_(UserInfo::lookup()),
+        config_(getEdenConfig(userInfo_)),
+        edenDir_([this]() {
+          XLOG(INFO) << "Using Eden directory: " << config_->getEdenDir();
+          return EdenStateDir(config_->getEdenDir());
+        }()) {
+    if (!edenDir_.acquireLock()) {
+      throw ArgumentError(
+          "error: failed to acquire the Eden lock\n"
+          "This utility cannot be used while edenfs is running.");
+    }
+  }
+  virtual ~Command() {}
+
+  virtual void run() = 0;
+
+ protected:
+  std::unique_ptr<RocksDbLocalStore> openLocalStore() {
+    folly::stop_watch<std::chrono::milliseconds> watch;
+    const auto rocksPath = edenDir_.getPath() + "storage/rocks-db"_relpath;
+    ensureDirectoryExists(rocksPath);
+    auto localStore = make_unique<RocksDbLocalStore>(
+        rocksPath, &faultInjector_, std::make_shared<StoreConfig>(config_));
+    XLOG(INFO) << "Opened RocksDB store in "
+               << (watch.elapsed().count() / 1000.0) << " seconds.";
+    return localStore;
+  }
+
+  UserInfo userInfo_;
+  std::shared_ptr<EdenConfig> config_;
+  EdenStateDir edenDir_;
+  FaultInjector faultInjector_{/*enabled=*/false};
+};
+
+class CommandFactory {
+ public:
+  virtual ~CommandFactory() {}
+  virtual StringPiece name() const = 0;
+  virtual StringPiece help() const = 0;
+  virtual std::unique_ptr<Command> create() = 0;
+};
+
+template <typename CommandType>
+class CommandFactoryT : public CommandFactory {
+  StringPiece name() const override {
+    return CommandType::name;
+  }
+  StringPiece help() const override {
+    return CommandType::help;
+  }
+  std::unique_ptr<Command> create() override {
+    return make_unique<CommandType>();
+  }
+};
+
+class GcCommand : public Command {
+ public:
+  static constexpr auto name = StringPiece("gc");
+  static constexpr auto help =
+      StringPiece("Clear cached data then compact storage");
+
+  void run() override {
+    auto keySpace = getKeySpace();
+    auto localStore = openLocalStore();
+    if (keySpace) {
+      localStore->clearKeySpace(*keySpace);
+      localStore->compactKeySpace(*keySpace);
+    } else {
+      localStore->clearCachesAndCompactAll();
+    }
+  }
+};
+
+class ClearCommand : public Command {
+ public:
+  static constexpr auto name = StringPiece("clear");
+  static constexpr auto help =
+      StringPiece("Clear cached data without compacting storage");
+
+  void run() override {
+    auto keySpace = getKeySpace();
+    auto localStore = openLocalStore();
+    if (keySpace) {
+      localStore->clearKeySpace(*keySpace);
+    } else {
+      localStore->clearCaches();
+    }
+  }
+};
+
+class CompactCommand : public Command {
+ public:
+  static constexpr auto name = StringPiece("compact");
+  static constexpr auto help = StringPiece("Compact the RocksDB storage");
+
+  void run() override {
+    auto keySpace = getKeySpace();
+    auto localStore = openLocalStore();
+    if (keySpace) {
+      localStore->compactKeySpace(*keySpace);
+    } else {
+      localStore->compactStorage();
+    }
+  }
+};
+
+std::unique_ptr<Command> createCommand(StringPiece name) {
+  std::array<std::unique_ptr<CommandFactory>, 3> commands{
+      make_unique<CommandFactoryT<GcCommand>>(),
+      make_unique<CommandFactoryT<ClearCommand>>(),
+      make_unique<CommandFactoryT<CompactCommand>>(),
+  };
+
+  std::unique_ptr<Command> command;
+  for (const auto& factory : commands) {
+    if (factory->name() == name) {
+      return factory->create();
+    }
+  }
+  throw ArgumentError("unknown command \"", name, "\"");
+}
+
 } // namespace
 
 int main(int argc, char** argv) {
   folly::init(&argc, &argv);
+  if (argc != 2) {
+    fprintf(stderr, "error: no command specified\n");
+    fprintf(stderr, "usage: eden_store_util COMMAND\n");
+    return EX_SOFTWARE;
+  }
 
-  auto userInfo = UserInfo::lookup();
-  std::shared_ptr<EdenConfig> config;
+  std::unique_ptr<Command> command;
   try {
-    config = getEdenConfig(userInfo);
+    command = createCommand(argv[1]);
   } catch (const ArgumentError& ex) {
-    fprintf(stderr, "%s\n", ex.what());
+    fprintf(stderr, "error: %s\n", ex.what());
     return EX_SOFTWARE;
   }
 
-  XLOG(INFO) << "Using Eden directory: " << config->getEdenDir();
-  EdenStateDir edenDir(config->getEdenDir());
-  if (!edenDir.acquireLock()) {
-    fprintf(stderr, "error: failed to acquire the Eden lock\n");
-    fprintf(stderr, "This utility cannot be used while edenfs is running.\n");
-    return EX_SOFTWARE;
-  }
-
-  FaultInjector faultInjector(/*enabled=*/false);
-  folly::stop_watch<std::chrono::milliseconds> watch;
-  const auto rocksPath = edenDir.getPath() + "storage/rocks-db"_relpath;
-  ensureDirectoryExists(rocksPath);
-  auto localStore = std::make_unique<RocksDbLocalStore>(
-      rocksPath, &faultInjector, std::make_shared<StoreConfig>(config));
-  XLOG(INFO) << "Opened RocksDB store in " << (watch.elapsed().count() / 1000.0)
-             << " seconds.";
-
-  localStore->clearCachesAndCompactAll();
+  command->run();
   return 0;
 }
