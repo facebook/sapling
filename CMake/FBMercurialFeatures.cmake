12b0708dd8b394322370507bd16a698e668b0b4b
Wez Furlong <wez@fb.com>
2019-05-03 15:52:39 -0700

    eden: getdeps: add fb-mercurial dep to enable treemanifest
    Summary:
    In order to pull in the treemanifest and other libraries
    from our mercurial repo, add a manifest file for it, and then
    adjust the logic in our cmake module to look for it.
    
    The fb-mercurial manifest just copies the source tree to the
    installation dir.  In the future, we could teach it to invoke
    the build for real.
    
    Reviewed By: simpkins
    
    Differential Revision: D14969806
    
    fbshipit-source-id: cb270c5003a1c134eeea92c7481a84938f1c5957

--- CMake/FBMercurialFeatures.cmake
+++ CMake/FBMercurialFeatures.cmake
@@ -5,38 +5,50 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
+# Allow symlinking the fb-mercurial dir in directly
+if (IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/fb-mercurial)
+  set(FB_MERCURIAL_DIR "${CMAKE_CURRENT_SOURCE_DIR}/fb-mercurial")
+else()
+  # Otherwise, look for where getdeps has "installed" the source
+  find_file(EDENSCM_DATAPACKSTORE_CPP "edenscm/hgext/extlib/cstore/datapackstore.cpp")
+  if (EDENSCM_DATAPACKSTORE_CPP)
+    get_filename_component(CSTORE_DIR "${EDENSCM_DATAPACKSTORE_CPP}" DIRECTORY)
+    get_filename_component(FB_MERCURIAL_DIR "${CSTORE_DIR}/../../../../" REALPATH)
+  endif()
+endif()
+
 # TODO: once we've gotten the rust datapack code integrated and
 # building, update getdeps.py to optionally pull from the fb-mercurial
 # repo on github and adjust this logic to use either the code from
 # the local fbsource repo when building at FB, or from the
 # external dir when building the OSS build.
-if (IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/fb-mercurial)
+if (IS_DIRECTORY "${FB_MERCURIAL_DIR}")
   find_package(LZ4 MODULE REQUIRED)
-  include_directories(${CMAKE_CURRENT_SOURCE_DIR}/fb-mercurial)
+  include_directories(${FB_MERCURIAL_DIR})
   add_library(
     libmpatch
     STATIC
-      fb-mercurial/edenscm/mercurial/mpatch.c
+      ${FB_MERCURIAL_DIR}/edenscm/mercurial/mpatch.c
   )
 
   add_library(
     buffer
     STATIC
-      fb-mercurial/lib/clib/buffer.c
+      ${FB_MERCURIAL_DIR}/lib/clib/buffer.c
   )
 
   add_library(
     datapack
     STATIC
-      fb-mercurial/edenscm/hgext/extlib/cstore/datapackstore.cpp
-      fb-mercurial/edenscm/hgext/extlib/cstore/deltachain.cpp
-      fb-mercurial/edenscm/hgext/extlib/cstore/uniondatapackstore.cpp
-      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/manifest.cpp
-      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/manifest_entry.cpp
-      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/manifest_fetcher.cpp
-      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/manifest_ptr.cpp
-      fb-mercurial/edenscm/hgext/extlib/ctreemanifest/treemanifest.cpp
-      fb-mercurial/lib/cdatapack/cdatapack.c
+      ${FB_MERCURIAL_DIR}/edenscm/hgext/extlib/cstore/datapackstore.cpp
+      ${FB_MERCURIAL_DIR}/edenscm/hgext/extlib/cstore/deltachain.cpp
+      ${FB_MERCURIAL_DIR}/edenscm/hgext/extlib/cstore/uniondatapackstore.cpp
+      ${FB_MERCURIAL_DIR}/edenscm/hgext/extlib/ctreemanifest/manifest.cpp
+      ${FB_MERCURIAL_DIR}/edenscm/hgext/extlib/ctreemanifest/manifest_entry.cpp
+      ${FB_MERCURIAL_DIR}/edenscm/hgext/extlib/ctreemanifest/manifest_fetcher.cpp
+      ${FB_MERCURIAL_DIR}/edenscm/hgext/extlib/ctreemanifest/manifest_ptr.cpp
+      ${FB_MERCURIAL_DIR}/edenscm/hgext/extlib/ctreemanifest/treemanifest.cpp
+      ${FB_MERCURIAL_DIR}/lib/cdatapack/cdatapack.c
   )
   target_link_libraries(
     datapack
