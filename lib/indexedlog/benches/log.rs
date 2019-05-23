7e86e0c314a505d851d1a23e9dfb349a13f462c1
Jun Wu <quark@fb.com>
2019-05-23 08:56:03 -0700

    indexedlog: add benchmark about Log::sync with indexes
    Summary:
    There are some "easy" optimizations to the `Log::sync` operation.
    To make sure the optimizations are valid, benchmarks are added first.
    
    The `Log::sync` operation is complex because it does both read and write.
    Either code path will drop the in-memory indexes and rebuild them, which
    makes it expensive.
    
    Add a comprehensive set of benchmarks to measure it.
    
    A sample output is:
    
      log sync (no index)                                 1.846 ms     680 B/212.993MB
      log sync (write)                                  108.103 ms   1.760KB/370.366MB
      log sync (read, no-op)                              0.200 ms   2.960KB/      0 B
      log sync (read, on-disk index lag)                 29.901 ms     840 B/      0 B
      log sync (read, log lag)                            0.168 ms   2.960KB/      0 B
      log sync (read 1, write N)                        108.138 ms   1.760KB/370.364MB
      log sync (read N, write 1)                          0.888 ms   3.000KB/ 16.680KB
    
    Reviewed By: xavierd
    
    Differential Revision: D15446784
    
    fbshipit-source-id: 558e34c8b4e1ecb6f9858b843c00c9e9cecb21ad

--- lib/indexedlog/benches/log.rs
+++ lib/indexedlog/benches/log.rs
@@ -3,14 +3,10 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
-extern crate indexedlog;
-extern crate minibench;
-extern crate rand;
-extern crate tempfile;
-
 use indexedlog::log::{self, ChecksumType, IndexDef, IndexOutput, Log};
-use minibench::{bench, elapsed};
+use minibench::{bench, elapsed, measure, Measure};
 use rand::{ChaChaRng, Rng};
+use std::path::Path;
 use tempfile::tempdir;
 
 const N: usize = 204800;
@@ -21,6 +17,16 @@ fn gen_buf(size: usize) -> Vec<u8> {
     ChaChaRng::new_unseeded().fill_bytes(buf.as_mut());
     buf
 }
+
+/// Get a `Log` with index defined.
+fn log_with_index(path: &Path, lag: u64) -> Log {
+    let index_func = |_data: &[u8]| vec![IndexOutput::Reference(0..20)];
+    let index_def = IndexDef::new("i", index_func).lag_threshold(lag);
+    Log::open(path, vec![index_def]).unwrap()
+}
+
+/// Measure both elapsed and IO.
+type MeasureSync = measure::Both<measure::WallClock, measure::IO>;
 
 fn main() {
     bench("log insertion", || {
@@ -51,9 +57,7 @@ fn main() {
 
     bench("log insertion with index", || {
         let dir = tempdir().unwrap();
-        let index_func = |_data: &[u8]| vec![IndexOutput::Reference(0..20)];
-        let index_def = IndexDef::new("i", index_func).lag_threshold(0);
-        let mut log = Log::open(dir.path(), vec![index_def]).unwrap();
+        let mut log = log_with_index(dir.path(), 0);
         let buf = gen_buf(N * 20);
         elapsed(move || {
             for i in 0..N {
@@ -62,15 +66,105 @@ fn main() {
         })
     });
 
-    bench("log flush", || {
+    bench("log sync (write N, no index)", || {
         let dir = tempdir().unwrap();
         let mut log = Log::open(dir.path(), vec![]).unwrap();
         let buf = gen_buf(N * 20);
         for i in 0..N {
             log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
         }
-        elapsed(|| {
-            log.flush().unwrap();
+        MeasureSync::measure(|| {
+            log.sync().unwrap();
+        })
+    });
+
+    bench("log sync (write)", || {
+        let dir = tempdir().unwrap();
+        let buf = gen_buf(N * 20);
+        let mut log = log_with_index(dir.path(), 0);
+        // Write one entry to make things more interesting.
+        log.append(&buf[0..20]).unwrap();
+        log.sync().unwrap();
+        for i in 1..N {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        MeasureSync::measure(|| {
+            log.sync().unwrap();
+        })
+    });
+
+    bench("log sync (read, no-op)", || {
+        let dir = tempdir().unwrap();
+        let buf = gen_buf(N * 20);
+        let mut log = log_with_index(dir.path(), 0);
+        for i in 0..N {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        log.sync().unwrap();
+        let mut log = log_with_index(dir.path(), 0);
+        MeasureSync::measure(|| {
+            log.sync().unwrap();
+        })
+    });
+
+    bench("log sync (read, on-disk index lag)", || {
+        let dir = tempdir().unwrap();
+        let buf = gen_buf(N * 20);
+        let mut log = log_with_index(dir.path(), u64::max_value());
+        for i in 0..(N / 2) {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        log.sync().unwrap();
+        MeasureSync::measure(|| {
+            log.sync().unwrap();
+        })
+    });
+
+    bench("log sync (read, log lag)", || {
+        let dir = tempdir().unwrap();
+        let buf = gen_buf(N * 20);
+        let mut log = log_with_index(dir.path(), 0);
+        let mut log2 = log_with_index(dir.path(), 0);
+        for i in 0..N {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        log.sync().unwrap();
+        MeasureSync::measure(|| {
+            log2.sync().unwrap();
+        })
+    });
+
+    bench("log sync (read 1, write N)", || {
+        let dir = tempdir().unwrap();
+        let buf = gen_buf(N * 20);
+        let mut log = log_with_index(dir.path(), 0);
+        let mut log2 = log_with_index(dir.path(), 0);
+        log.append(&buf[0..20]).unwrap();
+        log.sync().unwrap();
+        log2.append(&buf[20..40]).unwrap();
+        for i in 2..N {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        log2.sync().unwrap();
+        MeasureSync::measure(|| {
+            log.sync().unwrap();
+        })
+    });
+
+    bench("log sync (read N, write 1)", || {
+        let dir = tempdir().unwrap();
+        let buf = gen_buf(N * 20);
+        let mut log = log_with_index(dir.path(), 0);
+        let mut log2 = log_with_index(dir.path(), 0);
+        log.append(&buf[0..20]).unwrap();
+        log.sync().unwrap();
+        log2.append(&buf[20..40]).unwrap();
+        for i in 2..N {
+            log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
+        }
+        log.sync().unwrap();
+        MeasureSync::measure(|| {
+            log2.sync().unwrap();
         })
     });
 
@@ -93,7 +187,7 @@ fn main() {
         for i in 0..N {
             log.append(&buf[20 * i..20 * (i + 1)]).unwrap();
         }
-        log.flush().unwrap();
+        log.sync().unwrap();
         let log = Log::open(dir.path(), vec![]).unwrap();
         elapsed(move || {
             log.iter().count();
