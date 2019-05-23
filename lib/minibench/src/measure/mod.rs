ce6829770ba4b4aaa3a218a97f0b81551f54a587
Jun Wu <quark@fb.com>
2019-05-23 08:56:03 -0700

    minibench: add a way to measure IO
    Summary:
    This allows us to measure IO in bytes on Linux. It does not count mmap
    read/write, though.
    
    Reviewed By: xavierd
    
    Differential Revision: D15454966
    
    fbshipit-source-id: 576266154492aafa7d525cd6201cff242c4a8054

--- lib/minibench/src/measure/mod.rs
+++ lib/minibench/src/measure/mod.rs
@@ -3,6 +3,9 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
+mod procfs;
+pub use self::procfs::*;
+
 /// Abstracted measured result for benchmark.
 pub trait Measure: Sized {
     /// Expected return value from the function being measured.
