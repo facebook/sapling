d94dde74b2515a025aa3708f2a553776854b636b
Jared Bosco <jbosco@fb.com>
2019-05-30 14:39:31 -0700

    cliparser: create bare cliparser library
    Summary: Initial commit for the cliparser command line argument parsing library.  Can be tested with buck, or cargo.
    
    Reviewed By: quark-zju
    
    Differential Revision: D15487278
    
    fbshipit-source-id: 5f3f65e58a815d2c408e38a3eb9e9981086c6715

---
+++ lib/cliparser/src/lib.rs
@@ -0,0 +1,22 @@
+// Copyright 2019 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+//! # CLI Parser
+//!
+//! CLI Parser is a utility to support the parsing of command-line arguments, commands,
+//! subcommands, and flags.
+//!
+//! # About
+//! CLI Parser is used both to declare flags and commands as well as parse command-line arguments
+//! in a type-safe way.  Flags can have values associated with them, default values, or no value.
+//!
+//! # Goals
+//! Having a simple, easy-to-use CLI Parser in native code allowing for fast execution, parsing,
+//! and validation of command-line arguments.
+//!
+//! Having the flexibility of being able to dynamically load flags from an external source such
+//! as other languages ( python ) or files ( configuration ).
+//!
+pub mod parser;
---
+++ lib/cliparser/src/parser.rs
@@ -0,0 +1,6 @@
+// Copyright 2019 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+#[cfg(test)]
+mod tests {}
