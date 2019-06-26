4f845ca603257c7c9e680db25586e772fcaf8a50
Jun Wu <quark@fb.com>
2019-06-26 10:59:13 -0700

    blackbox: add some benchmarks
    Summary:
    This gives us some data about blackbox performance, and interesting areas to improve.
    
      blackbox insertion (400 entries)                   99.856 ms  12.896 KB/ 54.109 KB  16.012 KB
      blackbox filter by index (4000 entries)             2.259 ms       0 B /      0 B
      blackbox filter by pattern (4000 entries)           9.592 ms       0 B /      0 B
                                                       (wall time) (IO read  / write    ) (log size)
                                                                   (not counting mmap IO)
    
    Takeaways:
    - Insertion performance is bad.
    - Blackbox size looks okay.
    - Pattern matching alone can be slow for large data (ex. 100MB log requires ~6s
      matching). Therefore using indexes to narrow down entries seems worthwhile.
    
    Reviewed By: markbt
    
    Differential Revision: D15685473
    
    fbshipit-source-id: 44ffffefd77f9e22699755b2ec520b1fc57ae244

---
+++ lib/blackbox/benches/blackbox.rs
@@ -0,0 +1,68 @@
+// Copyright 2019 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+use blackbox::{
+    event::{Event, NetworkOp},
+    Blackbox, BlackboxOptions, IndexFilter,
+};
+use minibench::{
+    bench,
+    measure::{Both, Bytes, Measure, WallClock, IO},
+};
+use serde_json::json;
+use tempfile::tempdir;
+
+// Insert 400 entries
+fn insert(blackbox: &mut Blackbox) {
+    for _ in 0..200 {
+        blackbox.log(&Event::Alias {
+            from: "foo".to_string(),
+            to: "bar".to_string(),
+        });
+        blackbox.log(&Event::Network {
+            op: NetworkOp::SshGetFiles,
+            read_bytes: 1000,
+            write_bytes: 9000,
+            duration_ms: 100,
+            calls: 2,
+            latency_ms: 10,
+            result: None,
+        });
+    }
+}
+
+fn main() {
+    bench("blackbox insertion (400 entries)", || {
+        let dir = tempdir().unwrap();
+        let mut blackbox = BlackboxOptions::new().open(dir.path()).unwrap();
+        Both::<Both<WallClock, IO>, Bytes>::measure(move || {
+            insert(&mut blackbox);
+            dir.path().join("0").join("log").metadata().unwrap().len()
+        })
+    });
+
+    {
+        let dir = tempdir().unwrap();
+        let mut blackbox = BlackboxOptions::new().open(dir.path()).unwrap();
+        for _ in 0..10 {
+            insert(&mut blackbox);
+        }
+
+        bench("blackbox filter by index (4000 entries)", || {
+            Both::<WallClock, IO>::measure(|| {
+                blackbox.filter::<Event>(IndexFilter::Time(0, u64::max_value()), None);
+            })
+        });
+
+        bench("blackbox filter by pattern (4000 entries)", || {
+            Both::<WallClock, IO>::measure(|| {
+                blackbox.filter::<Event>(
+                    IndexFilter::Nop,
+                    Some(json!({"network": {"read_bytes": ["range", 10, 2000]}})),
+                );
+            })
+        });
+    }
+}
