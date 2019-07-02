08d573a4bd450a815d4f7cbbec963d6ae2ef1f3c
George-Catalin Tintareanu <georgect@fb.com>
2019-07-02 04:37:24 -0700

    insert censoredblob
    Summary:
    Implemented a method in `SqlCensoredContentStore` to insert rows in
    table `censored_contents`. A row contains a blobstore key, a task and a timestamp.
    
    Reviewed By: krallin
    
    Differential Revision: D15921136
    
    fbshipit-source-id: e5c65fd4120d2122c69e9734c42b38b416334028

--- blobstore/censoredblob/src/store.rs
+++ blobstore/censoredblob/src/store.rs
@@ -5,11 +5,11 @@
 // GNU General Public License version 2 or any later version.
 
 #![deny(warnings)]
-
 use failure_ext::Error;
 use futures::future::Future;
 use futures_ext::BoxFuture;
 use futures_ext::FutureExt;
+use mononoke_types::Timestamp;
 use sql::{queries, Connection};
 use std::collections::HashMap;
 
@@ -19,24 +19,37 @@ use std::iter::FromIterator;
 #[derive(Clone)]
 pub struct SqlCensoredContentStore {
     read_connection: Connection,
+    write_connection: Connection,
 }
 
 queries! {
+
+    write InsertCensoredBlob(
+        values: (content_key: String, task: String, add_timestamp: Timestamp)
+    ) {
+        none,
+        "INSERT into censored_contents(content_key, task, add_timestamp) VALUES {values}"
+    }
+
     read GetAllCensoredBlobs() -> (String, String) {
         "SELECT content_key, task
         FROM censored_contents"
     }
+
 }
 
 impl SqlConstructors for SqlCensoredContentStore {
     const LABEL: &'static str = "censored_contents";
 
     fn from_connections(
-        _write_connection: Connection,
+        write_connection: Connection,
         read_connection: Connection,
         _read_master_connection: Connection,
     ) -> Self {
-        Self { read_connection }
+        Self {
+            write_connection,
+            read_connection,
+        }
     }
 
     fn get_up_query() -> &'static str {
@@ -48,6 +61,23 @@ impl SqlCensoredContentStore {
     pub fn get_all_censored_blobs(&self) -> BoxFuture<HashMap<String, String>, Error> {
         GetAllCensoredBlobs::query(&self.read_connection)
             .map(HashMap::from_iter)
+            .boxify()
+    }
+
+    pub fn insert_censored_blobs(
+        &self,
+        content_keys: &Vec<String>,
+        task: &String,
+        add_timestamp: &Timestamp,
+    ) -> impl Future<Item = (), Error = Error> {
+        let censored_inserts: Vec<_> = content_keys
+            .iter()
+            .map(move |key| (key, task, add_timestamp))
+            .collect();
+
+        InsertCensoredBlob::query(&self.write_connection, &censored_inserts[..])
+            .map_err(Error::from)
+            .map(|_| ())
             .boxify()
     }
 }
