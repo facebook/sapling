fcbc6cf3494207f4aa4027e6bab00df8d389766c
Brian Strauch <brianstrauch@fb.com>
2019-07-15 14:22:51 -0700

    Variable name refactor
    Summary: Changed all variables to snake case, and made variable/function names more uniform/simplistic
    
    Reviewed By: strager
    
    Differential Revision: D16185688
    
    fbshipit-source-id: f4cddcf264c7a0e31317288deb2b517a32046c37

--- eden/cli/top.py
+++ eden/cli/top.py
@@ -18,11 +18,11 @@ from . import cmd_util
 
 REFRESH_SECONDS = 2
 
-process_width = 15
-mount_width = 15
-fuse_width = 12
-pids_width = 25
-padding = " " * 4
+NAME_WIDTH = 15
+MOUNT_WIDTH = 15
+CALLS_WIDTH = 12
+PIDS_WIDTH = 25
+PADDING = " " * 4
 
 
 class Top:
@@ -33,8 +33,8 @@ class Top:
     def start(self, args: argparse.Namespace) -> int:
         self.running = True
 
-        instance = cmd_util.get_eden_instance(args)
-        with instance.get_thrift_client() as client:
+        eden = cmd_util.get_eden_instance(args)
+        with eden.get_thrift_client() as client:
             try:
                 curses.wrapper(self.run(client))
             except KeyboardInterrupt:
@@ -58,42 +58,67 @@ class Top:
         return mainloop
 
     def update(self, client):
-        countsByMountsAndBaseNames = self.get_process_data(client)
-        self.populate_rows(countsByMountsAndBaseNames)
+        processes = self.get_process_data(client)
+        self.populate_rows(processes)
 
     def get_process_data(self, client):
         counts = client.getAccessCounts(REFRESH_SECONDS)
-        exeNames = counts.exeNamesByPid
+        names_by_pid = counts.exeNamesByPid
 
-        countsByMountsAndBaseNames: Dict[
+        processes: Dict[
             Tuple[bytes, bytes], List[Tuple[int, int]]
         ] = collections.defaultdict(lambda: [])
 
-        for mount, fuseMountAccesses in counts.fuseAccessesByMount.items():
-            for pid, accessCount in fuseMountAccesses.fuseAccesses.items():
-                countsByMountsAndBaseNames[
-                    (os.path.basename(mount), exeNames.get(pid, b"<unknown>"))
-                ].append((accessCount.count, pid))
+        for mount, accesses in counts.fuseAccessesByMount.items():
+            for pid, calls in accesses.fuseAccesses.items():
+                processes[
+                    (os.path.basename(mount), names_by_pid.get(pid, b"<unknown>"))
+                ].append((calls.count, pid))
 
-        return countsByMountsAndBaseNames
+        return processes
 
     def populate_rows(
-        self,
-        countsByMountsAndBaseNames: Dict[Tuple[bytes, bytes], List[Tuple[int, int]]],
+        self, processes: Dict[Tuple[bytes, bytes], List[Tuple[int, int]]]
     ):
         self.rows = []
-        for (mount, exe_name), counts_and_pids in sorted(
-            countsByMountsAndBaseNames.items(),
-            key=lambda kv: self.compute_total(kv[1]),
-            reverse=True,
+        for (mount, name), calls_and_pids in sorted(
+            processes.items(), key=lambda kv: self.compute_total(kv[1]), reverse=True
         ):
-            exe_name_printed = self.summarize_exe(os.fsdecode(exe_name))
-            mount_printed = os.fsdecode(mount)[:mount_width]
-            total_count = self.compute_total(counts_and_pids)
-            pids_str = self.format_top_pids(counts_and_pids)
+            name = self.format_name(os.fsdecode(name))
+            mount = self.format_mount(mount)
+            calls = self.compute_total(calls_and_pids)
+            pids = self.format_pids(calls_and_pids)
 
-            row = (exe_name_printed, mount_printed, total_count, pids_str)
+            row = (name, mount, calls, pids)
             self.rows.append(row)
+
+    def format_name(self, name):
+        args = name.split("\x00", 2)
+        # focus on just the basename as the paths can be quite long
+        result = os.path.basename(args[0])[:NAME_WIDTH]
+        if len(args) > 1 and len(result) < NAME_WIDTH:
+            # show cmdline args too, provided they fit in the available space
+            args = args[1].replace("\x00", " ")
+            result += " "
+            result += args[: NAME_WIDTH - len(result)]
+        return result
+
+    def format_mount(self, mount):
+        return os.fsdecode(mount)[:MOUNT_WIDTH]
+
+    def format_pids(self, calls_and_pids):
+        pids_str = ""
+        for _, pid in sorted(calls_and_pids):
+            if not pid:
+                continue
+            if not pids_str:
+                pids_str = str(pid)
+            else:
+                new_str = pids_str + "," + str(pid)
+                if len(new_str) > PIDS_WIDTH:
+                    break
+                pids_str = new_str
+        return pids_str
 
     def compute_total(self, ls):
         return sum(c[0] for c in ls)
@@ -124,10 +149,10 @@ class Top:
         height, width = stdscr.getmaxyx()
 
         heading = (
-            f'{"PROCESS":{process_width}}{padding}'
-            + f'{"MOUNT":{mount_width}}{padding}'
-            + f'{"FUSE CALLS":>{fuse_width}}{padding}'
-            + f'{"TOP PIDS":{pids_width}}'
+            f'{"PROCESS":{NAME_WIDTH}}{PADDING}'
+            + f'{"MOUNT":{MOUNT_WIDTH}}{PADDING}'
+            + f'{"FUSE CALLS":>{CALLS_WIDTH}}{PADDING}'
+            + f'{"TOP PIDS":{PIDS_WIDTH}}'
         )
         stdscr.addnstr(2, 0, heading.ljust(width), width, curses.A_REVERSE)
 
@@ -135,7 +160,7 @@ class Top:
         height, width = stdscr.getmaxyx()
 
         line = 3
-        for exe_name_printed, mount_printed, total_count, pids_str in self.rows:
+        for name, mount, calls, pids in self.rows:
             if line >= height:
                 break
 
@@ -144,38 +169,13 @@ class Top:
             stdscr.addnstr(
                 line,
                 0,
-                f"{exe_name_printed:{process_width}}{padding}"
-                + f"{mount_printed:{mount_width}}{padding}"
-                + f"{total_count:{fuse_width}}{padding}"
-                + f"{pids_str:{pids_width}}",
+                f"{name:{NAME_WIDTH}}{PADDING}"
+                + f"{mount:{MOUNT_WIDTH}}{PADDING}"
+                + f"{calls:{CALLS_WIDTH}}{PADDING}"
+                + f"{pids:{PIDS_WIDTH}}",
                 max_line_width,
             )
             line += 1
-
-    def summarize_exe(self, name):
-        args = name.split("\x00", 2)
-        # focus on just the basename as the paths can be quite long
-        result = os.path.basename(args[0])[:process_width]
-        if len(args) > 1 and len(result) < process_width:
-            # show cmdline args too, provided they fit in the available space
-            args = args[1].replace("\x00", " ")
-            result += " "
-            result += args[: process_width - len(result)]
-        return result
-
-    def format_top_pids(self, counts_and_pids):
-        pids_str = ""
-        for _count, pid in sorted(counts_and_pids):
-            if not pid:
-                continue
-            if not pids_str:
-                pids_str = str(pid)
-            else:
-                new_str = pids_str + "," + str(pid)
-                if len(new_str) > pids_width:
-                    break
-                pids_str = new_str
-        return pids_str
 
     def get_keypress(self, stdscr):
         key = stdscr.getch()
