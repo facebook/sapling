1364b3a3ae1e38ce3d6b3028c2bff96a97e6f07d
Jun Wu <quark@fb.com>
2019-07-17 20:59:57 -0700

    testutil/dott: implement a subset of coreutils
    Summary: Implement a subset of coreutils so functions used by .t can run.
    
    Reviewed By: xavierd
    
    Differential Revision: D16209202
    
    fbshipit-source-id: 953bebbfdc09cf191e8aa04c5ee1468c764b4360

--- tests/testutil/dott/shlib/__init__.py
+++ tests/testutil/dott/shlib/__init__.py
@@ -8,3 +8,169 @@
 This module only aims to support shell commands used in most tests. It does not
 try to match all behavior of existing shell commands.
 """
+
+from __future__ import absolute_import
+
+import glob
+import os
+import shutil
+import stat
+import sys
+
+
+# Use "edenscm" for shell utilities and main hg commands.
+# Try to make "edenscm" available. Assuming sys.path includes "TESTDIR".
+for candidate in sys.path:
+    if os.path.exists(os.path.join(candidate, "../edenscm/__init__.py")):
+        sys.path.insert(0, os.path.dirname(candidate))
+        break
+for candidate in sys.path:
+    if os.path.exists(os.path.join(candidate, "hghave.py")):
+        TESTDIR = os.path.abspath(candidate)
+        break
+else:
+    raise RuntimeError("Cannot find TESTDIR")
+
+
+try:
+    from edenscm.mercurial import dispatch, ui as uimod, util
+except ImportError:
+    raise RuntimeError("Cannot find edenscm")
+
+
+# coreutils
+
+
+def cat(*args, **kwargs):
+    content = "".join(open(path).read() for path in args)
+    stdin = kwargs.get("stdin")
+    if stdin is not None:
+        content += expandpath(stdin)
+    return content
+
+
+cd = os.chdir
+
+
+def cp(*args):
+    copydir = args[0] == "-R"
+    if copydir:
+        args = args[1:]
+    src, dst = args
+    if os.path.isdir(src):
+        if not copydir:
+            raise RuntimeError("cp dir without -R")
+        if os.path.isdir(dst):
+            dst = os.path.join(dst, os.path.basename(src))
+        shutil.copytree(src, dst)
+    else:
+        shutil.copy2(src, dst)
+
+
+def echo(*args):
+    return " ".join(args) + "\n"
+
+
+def ln(*args):
+    if len(args) == 3:
+        assert args[0] == "-s"
+        src, dst = args[1:]
+        os.symlink(src, dst)
+    elif len(args) == 2:
+        src, dst = args
+        os.link(src, dst)
+    else:
+        raise NotImplementedError("ln with more args is not implemented")
+
+
+def ls(*args):
+    entries = []
+    for arg in args:
+        if arg.startswith("-"):
+            raise NotImplementedError("ls with args is not implemented")
+        elif os.path.isdir(arg):
+            entries += os.listdir(arg)
+        else:
+            entries += glob.glob(arg)
+    if not args:
+        entries = os.listdir(".")
+    return "".join("%s\n" % n for n in sorted(entries) if not n.startswith("."))
+
+
+def mkdir(*args):
+    for path in args:
+        if path.startswith("-"):
+            continue
+        util.makedirs(path)
+
+
+def mv(src, dst):
+    shutil.move(src, dst)
+
+
+def printf(*args):
+    return args[0].replace(r"\n", "\n") % args[1:]
+
+
+def rm(*args):
+    for path in args:
+        if path.startswith("-"):
+            continue
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            util.tryunlink(path)
+
+
+rmdir = rm
+
+
+def seq(*args):
+    if len(args) == 1:
+        values = range(1, args[0] + 1)
+    elif len(args) == 2:
+        values = range(args[0], args[1] + 1)
+    return "".join("%s\n" % v for v in values)
+
+
+def test(*args):
+    neg = False
+    func = os.path.exists
+    for arg in args:
+        if arg == "!":
+            neg = True
+        elif arg == "-x":
+            func = lambda p: os.stat(p).st_mode & stat.S_IEXEC
+        elif arg == "-f":
+            func = lambda p: stat.S_ISREG(os.stat(p).st_mode)
+        elif arg == "-d":
+            func = os.path.isdir
+        else:
+            result = func(arg)
+            if neg:
+                result = not result
+            if not result:
+                return "[1]"
+    raise NotImplementedError("test not fully implemented")
+
+
+def touch(*args):
+    for path in args:
+        open(path, "a")
+
+
+def true():
+    return ""
+
+
+globals()["["] = test
+
+
+# helper specific to shlib
+
+
+def expandpath(path):
+    # Replace `pwd`, it is commonly used in tests.
+    pwd = os.getcwd()
+    path = path.replace("`pwd`", pwd).replace("$PWD", pwd)
+    return util.expandpath(path)
