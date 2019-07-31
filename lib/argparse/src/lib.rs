35b2eed1566f3d006b82de152795722d99eecc9c
Jun Wu <quark@fb.com>
2019-07-31 03:20:43 -0700

    lib: remove argparse
    Summary:
    `cliparser` and `clidispatch` are more complete / compatible implemention for
    related feature.
    
    Reviewed By: xavierd
    
    Differential Revision: D16575262
    
    fbshipit-source-id: e78047a826896e1cd8529e14133d2821bc0955d3

--- lib/argparse/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
-#[cfg(unix)]
-extern crate libc;
-
-pub mod argparse;
-pub mod hg_python_commands;
--- lib/argparse/src/regenerate_hg_commands.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-HG=${HG:-"hg"}
-$HG --config extensions.dump_commands=hg_dump_commands_ext.py dump_commands
-rustfmt hg_python_commands.rs
