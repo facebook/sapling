bccbbb85af4598a7cc7b46115205f92f60f01df5
Harvey Hunt <harveyhunt@fb.com>
2019-08-08 10:41:22 -0700

    mononoke: Add an admin command to allow uploading a file using the filestore
    Summary:
    This command allows a file to be uploaded to the blobstore using
    the filestore - which allows for chunking if required.
    
    Additionally, port the other filestore commands to use logger
    rather than println!().
    
    Reviewed By: krallin
    
    Differential Revision: D16712167
    
    fbshipit-source-id: 0fad13c29035a550f8caee7a02d37071eb87cd73

--- cmds/admin/filestore.rs
+++ cmds/admin/filestore.rs
@@ -9,23 +9,28 @@ use cloned::cloned;
 use cmdlib::args;
 use context::CoreContext;
 use failure_ext::{err_msg, format_err, Result};
-use filestore::{self, FetchKey};
-use futures::{Future, IntoFuture};
+use filestore::{self, FetchKey, StoreRequest};
+use futures::{Future, IntoFuture, Stream};
 use futures_ext::{BoxFuture, FutureExt};
 use mononoke_types::{
     hash::{Sha1, Sha256},
     ContentId,
 };
-use slog::Logger;
+use slog::{info, Logger};
+use std::convert::TryInto;
+use std::io::BufReader;
 use std::str::FromStr;
+use tokio::{codec, fs::File};
 
 use crate::error::SubcommandError;
 
 const COMMAND_METADATA: &str = "metadata";
+const COMMAND_STORE: &str = "store";
 const COMMAND_VERIFY: &str = "verify";
 
 const ARG_KIND: &str = "kind";
 const ARG_ID: &str = "id";
+const ARG_FILE: &str = "file";
 
 // NOTE: Fetching by GitSha1 is not concurrently supported since that needs a size to instantiate.
 const VALID_KINDS: [&str; 3] = ["id", "sha1", "sha256"];
@@ -43,11 +48,19 @@ pub fn build_subcommand(name: &str) -> App {
         .required(true);
 
     SubCommand::with_name(name)
-        .about("inspect filestore data")
+        .about("inspect and interact with filestore data")
         .subcommand(
             SubCommand::with_name(COMMAND_METADATA)
                 .arg(kind_arg.clone())
                 .arg(id_arg.clone()),
+        )
+        .subcommand(
+            SubCommand::with_name(COMMAND_STORE).arg(
+                Arg::with_name(ARG_FILE)
+                    .help("File")
+                    .takes_value(true)
+                    .required(true),
+            ),
         )
         .subcommand(
             SubCommand::with_name(COMMAND_VERIFY)
@@ -61,16 +74,55 @@ pub fn execute_command(
     matches: &ArgMatches<'_>,
     sub_matches: &ArgMatches<'_>,
 ) -> BoxFuture<(), SubcommandError> {
+    args::init_cachelib(&matches);
     let blobrepo = args::open_repo(&logger, &matches);
     let ctx = CoreContext::test_mock();
 
     match sub_matches.subcommand() {
         (COMMAND_METADATA, Some(matches)) => (blobrepo, extract_fetch_key(matches).into_future())
             .into_future()
-            .and_then(move |(blobrepo, key)| {
-                filestore::get_metadata(&blobrepo.get_blobstore(), ctx, &key)
-                    .inspect(|r| println!("{:?}", r))
-                    .map(|_| ())
+            .and_then({
+                move |(blobrepo, key)| {
+                    filestore::get_metadata(&blobrepo.get_blobstore(), ctx, &key)
+                        .inspect({
+                            cloned!(logger);
+                            move |r| info!(logger, "{:?}", r)
+                        })
+                        .map(|_| ())
+                }
+            })
+            .from_err()
+            .boxify(),
+        (COMMAND_STORE, Some(matches)) => (
+            blobrepo,
+            File::open(matches.value_of(ARG_FILE).unwrap().to_string())
+                .and_then(|file| file.metadata())
+                .from_err(),
+        )
+            .into_future()
+            .and_then(|(blobrepo, (file, metadata))| {
+                let file_buf = BufReader::new(file);
+                // If the size doesn't fit into a u64, we aren't going to be able to process
+                // it anyway.
+                let len: u64 = metadata.len().try_into().unwrap();
+
+                let data = codec::FramedRead::new(file_buf, codec::BytesCodec::new())
+                    .map(|bytes_mut| bytes_mut.freeze())
+                    .from_err();
+
+                let req = StoreRequest::new(len);
+
+                blobrepo.upload_file(ctx, &req, data)
+            })
+            .map({
+                cloned!(logger);
+                move |metadata| {
+                    info!(
+                        logger,
+                        "Wrote {} ({} bytes)", metadata.content_id, metadata.total_size
+                    );
+                    ()
+                }
             })
             .from_err()
             .boxify(),
@@ -109,11 +161,14 @@ pub fn execute_command(
                             )
                         }
                     })
-                    .map(|(content_id, sha1, sha256, git_sha1)| {
-                        println!("content_id: {:?}", content_id.is_ok());
-                        println!("sha1: {:?}", sha1.is_ok());
-                        println!("sha256: {:?}", sha256.is_ok());
-                        println!("git_sha1: {:?}", git_sha1.is_ok());
+                    .map({
+                        cloned!(logger);
+                        move |(content_id, sha1, sha256, git_sha1)| {
+                            info!(logger, "content_id: {:?}", content_id.is_ok());
+                            info!(logger, "sha1: {:?}", sha1.is_ok());
+                            info!(logger, "sha256: {:?}", sha256.is_ok());
+                            info!(logger, "git_sha1: {:?}", git_sha1.is_ok());
+                        }
                     })
             })
             .from_err()
