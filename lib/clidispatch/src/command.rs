6aeaa40b40d9a4217da5e424558fcc103c5419c8
Jun Wu <quark@fb.com>
2019-08-21 12:08:01 -0700

    clidispatch: extract CommandTable to a separate struct
    Summary:
    The dispatch logic couples with too much stuff - repo, parsing, command,
    config. The command / registration part can be cleanly seprated.
    
    This diff moves the core logic of CommandTable to command.rs.
    
    Reviewed By: sfilipco
    
    Differential Revision: D16796406
    
    fbshipit-source-id: 5a33d6b6452537f07895bd9944122a3b3149d925

--- lib/clidispatch/src/command.rs
+++ lib/clidispatch/src/command.rs
@@ -2,10 +2,9 @@
 //
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
-use crate::errors::DispatchError;
-use crate::io::IO;
-use crate::repo::Repo;
-use cliparser::parser::{Flag, ParseOutput};
+use crate::{errors::DispatchError, io::IO, repo::Repo};
+use cliparser::parser::{Flag, ParseOutput, StructFlags};
+use std::{collections::BTreeMap, ops::Deref};
 
 pub enum CommandFunc {
     NoRepo(Box<dyn Fn(ParseOutput, &mut IO) -> Result<u8, DispatchError>>),
@@ -49,5 +48,72 @@ impl CommandDefinition {
 
     pub fn func(&self) -> &CommandFunc {
         &self.func
+    }
+}
+
+pub struct CommandTable {
+    commands: BTreeMap<String, CommandDefinition>,
+}
+
+impl CommandTable {
+    pub fn new() -> Self {
+        CommandTable {
+            commands: BTreeMap::new(),
+        }
+    }
+}
+
+impl Deref for CommandTable {
+    type Target = BTreeMap<String, CommandDefinition>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.commands
+    }
+}
+
+pub trait Register<FN, T> {
+    fn register(&mut self, f: FN, name: &str, doc: &str);
+}
+
+// NoRepo commands.
+impl<S, FN> Register<FN, (S,)> for CommandTable
+where
+    S: From<ParseOutput> + StructFlags,
+    FN: Fn(S, &mut IO) -> Result<u8, DispatchError> + 'static,
+{
+    fn register(&mut self, f: FN, name: &str, doc: &str) {
+        let func = move |opts: ParseOutput, io: &mut IO| f(opts.into(), io);
+        let func = CommandFunc::NoRepo(Box::new(func));
+        let def = CommandDefinition::new(name, doc, S::flags(), func);
+        self.commands.insert(name.to_string(), def);
+    }
+}
+
+// InferRepo commands.
+impl<S, FN> Register<FN, ((), S)> for CommandTable
+where
+    S: From<ParseOutput> + StructFlags,
+    FN: Fn(S, &mut IO, Option<Repo>) -> Result<u8, DispatchError> + 'static,
+{
+    fn register(&mut self, f: FN, name: &str, doc: &str) {
+        let func =
+            move |opts: ParseOutput, io: &mut IO, repo: Option<Repo>| f(opts.into(), io, repo);
+        let func = CommandFunc::InferRepo(Box::new(func));
+        let def = CommandDefinition::new(name, doc, S::flags(), func);
+        self.commands.insert(name.to_string(), def);
+    }
+}
+
+// Repo commands.
+impl<S, FN> Register<FN, ((), (), S)> for CommandTable
+where
+    S: From<ParseOutput> + StructFlags,
+    FN: Fn(S, &mut IO, Repo) -> Result<u8, DispatchError> + 'static,
+{
+    fn register(&mut self, f: FN, name: &str, doc: &str) {
+        let func = move |opts: ParseOutput, io: &mut IO, repo: Repo| f(opts.into(), io, repo);
+        let func = CommandFunc::Repo(Box::new(func));
+        let def = CommandDefinition::new(name, doc, S::flags(), func);
+        self.commands.insert(name.to_string(), def);
     }
 }
