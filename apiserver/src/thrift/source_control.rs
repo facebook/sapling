121a66da8baf4d2a990b2c5382e2ce710c76eecb
Mark Thomas <mbthomas@fb.com>
2019-09-09 04:20:51 -0700

    apiserver: implement SourceControlService list_repos
    Summary:
    Implement the `list_repos` call, which returns a list of the repos that
    Mononoke knows about.
    
    Reviewed By: krallin
    
    Differential Revision: D16984064
    
    fbshipit-source-id: 872ef5b53453ae7f9eb664044f03ec15c83d2152

--- apiserver/src/thrift/source_control.rs
+++ apiserver/src/thrift/source_control.rs
@@ -7,10 +7,27 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use mononoke_api::Mononoke;
+use mononoke_api::{CoreContext, Mononoke};
 use scuba_ext::ScubaSampleBuilder;
 use slog::Logger;
 use source_control::server::SourceControlService;
+use source_control::services::source_control_service as service;
+use source_control::types as thrift;
+use sshrelay::SshEnvVars;
+use tracing::TraceContext;
+use uuid::Uuid;
+
+trait ScubaInfoProvider {
+    fn scuba_reponame(&self) -> Option<String> {
+        None
+    }
+    fn scuba_commit(&self) -> Option<String> {
+        None
+    }
+    fn scuba_path(&self) -> Option<String> {
+        None
+    }
+}
 
 #[derive(Clone)]
 pub struct SourceControlServiceImpl {
@@ -27,7 +44,50 @@ impl SourceControlServiceImpl {
             scuba_builder,
         }
     }
+
+    fn create_ctx(&self, scuba_info_provider: Option<&dyn ScubaInfoProvider>) -> CoreContext {
+        let mut scuba = self.scuba_builder.clone();
+        scuba.add_common_server_data().add("type", "thrift");
+        if let Some(scuba_info_provider) = scuba_info_provider {
+            if let Some(reponame) = scuba_info_provider.scuba_reponame() {
+                scuba.add("reponame", reponame);
+            }
+            if let Some(commit) = scuba_info_provider.scuba_commit() {
+                scuba.add("commit", commit);
+            }
+            if let Some(path) = scuba_info_provider.scuba_path() {
+                scuba.add("path", path);
+            }
+        }
+        let uuid = Uuid::new_v4();
+        scuba.add("session_uuid", uuid.to_string());
+        CoreContext::new(
+            uuid,
+            self.logger.clone(),
+            scuba,
+            None,
+            TraceContext::default(),
+            None,
+            SshEnvVars::default(),
+            None,
+        )
+    }
 }
 
 #[async_trait]
-impl SourceControlService for SourceControlServiceImpl {}
+impl SourceControlService for SourceControlServiceImpl {
+    async fn list_repos(
+        &self,
+        _params: thrift::ListReposParams,
+    ) -> Result<Vec<thrift::Repo>, service::ListReposExn> {
+        let _ctx = self.create_ctx(None);
+        let rsp = self
+            .mononoke
+            .repo_names()
+            .map(|repo_name| thrift::Repo {
+                name: repo_name.to_string(),
+            })
+            .collect();
+        Ok(rsp)
+    }
+}
