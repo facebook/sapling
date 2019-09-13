960c151540cf23d0c44552113899b1e440517794
Arun Kulshreshtha <kulshrax@fb.com>
2019-09-12 18:26:17 -0700

    manifest: move diff into own module
    Summary: For symmetry with the BFS diff implementation, move the DFS diff implementation into its own module. This will help unclutter mod.rs.
    
    Reviewed By: xavierd
    
    Differential Revision: D17352892
    
    fbshipit-source-id: 61709cd3e430c8676c529fbbbb76a9775c05053d

---
+++ lib/manifest/src/tree/diff.rs
@@ -0,0 +1,213 @@
+// Copyright 2019 Facebook, Inc.
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+use std::cmp::Ordering;
+
+use failure::Fallible;
+
+use pathmatcher::{DirectoryMatch, Matcher};
+use types::{RepoPath, RepoPathBuf};
+
+use super::cursor::{Cursor, Step};
+use super::link::{Durable, Leaf};
+pub use super::store::TreeStore;
+use crate::tree::Tree;
+use crate::FileMetadata;
+
+/// An iterator over all the differences between two [`Tree`]s. Keeping in mind that
+/// manifests operate over files, the difference space is limited to three cases described by
+/// [`DiffType`]:
+///  * a file may be present only in the left tree manifest
+///  * a file may be present only in the right tree manifest
+///  * a file may have different file_metadata between the two tree manifests
+///
+/// For the case where we have the the file "foo" in the `left` tree manifest and we have the "foo"
+/// directory in the `right` tree manifest, the differences returned will be:
+///  1. DiffEntry("foo", LeftOnly(_))
+///  2. DiffEntry(file, RightOnly(_)) for all `file`s under the "foo" directory
+pub struct Diff<'a, M> {
+    left: Cursor<'a>,
+    step_left: bool,
+    right: Cursor<'a>,
+    step_right: bool,
+    matcher: &'a M,
+}
+
+impl<'a, M> Diff<'a, M> {
+    pub fn new(left: &'a Tree, right: &'a Tree, matcher: &'a M) -> Self {
+        Self {
+            left: left.root_cursor(),
+            step_left: false,
+            right: right.root_cursor(),
+            step_right: false,
+            matcher,
+        }
+    }
+}
+
+/// Represents a file that is different between two tree manifests.
+#[derive(Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct DiffEntry {
+    pub path: RepoPathBuf,
+    pub diff_type: DiffType,
+}
+
+#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub enum DiffType {
+    LeftOnly(FileMetadata),
+    RightOnly(FileMetadata),
+    Changed(FileMetadata, FileMetadata),
+}
+
+impl DiffEntry {
+    pub(crate) fn new(path: RepoPathBuf, diff_type: DiffType) -> Self {
+        DiffEntry { path, diff_type }
+    }
+}
+
+impl DiffType {
+    /// Returns the metadata of the file in the left manifest when it exists.
+    pub fn left(&self) -> Option<FileMetadata> {
+        match self {
+            DiffType::LeftOnly(left_metadata) => Some(*left_metadata),
+            DiffType::RightOnly(_) => None,
+            DiffType::Changed(left_metadata, _) => Some(*left_metadata),
+        }
+    }
+
+    /// Returns the metadata of the file in the right manifest when it exists.
+    pub fn right(&self) -> Option<FileMetadata> {
+        match self {
+            DiffType::LeftOnly(_) => None,
+            DiffType::RightOnly(right_metadata) => Some(*right_metadata),
+            DiffType::Changed(_, right_metadata) => Some(*right_metadata),
+        }
+    }
+}
+
+impl<'a, M> Iterator for Diff<'a, M>
+where
+    M: Matcher,
+{
+    type Item = Fallible<DiffEntry>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // This is the standard algorithm for returning the differences in two lists but adjusted
+        // to have the iterator interface and to evaluate the tree lazily.
+
+        fn diff_entry(path: &RepoPath, diff_type: DiffType) -> Option<Fallible<DiffEntry>> {
+            Some(Ok(DiffEntry::new(path.to_owned(), diff_type)))
+        }
+        fn compare<'a>(left: &Cursor<'a>, right: &Cursor<'a>) -> Option<Ordering> {
+            // TODO: cache ordering state so we compare last components at most
+            match (left.finished(), right.finished()) {
+                (true, true) => None,
+                (false, true) => Some(Ordering::Less),
+                (true, false) => Some(Ordering::Greater),
+                (false, false) => Some(left.path().cmp(right.path())),
+            }
+        }
+        fn evaluate_cursor<M: Matcher>(
+            cursor: &mut Cursor<'_>,
+            matcher: &M,
+        ) -> Option<FileMetadata> {
+            if let Leaf(file_metadata) = cursor.link() {
+                if matcher.matches_file(cursor.path()) {
+                    return Some(*file_metadata);
+                }
+            }
+            try_skipping(cursor, matcher);
+            None
+        }
+
+        fn try_skipping<M: Matcher>(cursor: &mut Cursor<'_>, matcher: &M) {
+            if matcher.matches_directory(cursor.path()) == DirectoryMatch::Nothing {
+                cursor.skip_subtree();
+            }
+        }
+        loop {
+            if self.step_left {
+                if let Step::Err(error) = self.left.step() {
+                    return Some(Err(error));
+                }
+                self.step_left = false;
+            }
+            if self.step_right {
+                if let Step::Err(error) = self.right.step() {
+                    return Some(Err(error));
+                }
+                self.step_right = false;
+            }
+            match compare(&self.left, &self.right) {
+                None => return None,
+                Some(Ordering::Less) => {
+                    self.step_left = true;
+                    if let Some(file_metadata) = evaluate_cursor(&mut self.left, &self.matcher) {
+                        return diff_entry(self.left.path(), DiffType::LeftOnly(file_metadata));
+                    }
+                }
+                Some(Ordering::Greater) => {
+                    self.step_right = true;
+                    if let Some(file_metadata) = evaluate_cursor(&mut self.right, &self.matcher) {
+                        return diff_entry(self.right.path(), DiffType::RightOnly(file_metadata));
+                    }
+                }
+                Some(Ordering::Equal) => {
+                    self.step_left = true;
+                    self.step_right = true;
+                    match (self.left.link(), self.right.link()) {
+                        (Leaf(left_metadata), Leaf(right_metadata)) => {
+                            if left_metadata != right_metadata
+                                && self.matcher.matches_file(self.left.path())
+                            {
+                                return diff_entry(
+                                    self.left.path(),
+                                    DiffType::Changed(*left_metadata, *right_metadata),
+                                );
+                            }
+                        }
+                        (Leaf(file_metadata), _) => {
+                            try_skipping(&mut self.right, &self.matcher);
+                            if self.matcher.matches_file(self.left.path()) {
+                                return diff_entry(
+                                    self.left.path(),
+                                    DiffType::LeftOnly(*file_metadata),
+                                );
+                            }
+                        }
+                        (_, Leaf(file_metadata)) => {
+                            try_skipping(&mut self.left, &self.matcher);
+                            if self.matcher.matches_file(self.right.path()) {
+                                return diff_entry(
+                                    self.right.path(),
+                                    DiffType::RightOnly(*file_metadata),
+                                );
+                            }
+                        }
+                        (Durable(left_entry), Durable(right_entry)) => {
+                            if left_entry.node == right_entry.node
+                                || self.matcher.matches_directory(self.left.path())
+                                    == DirectoryMatch::Nothing
+                            {
+                                self.left.skip_subtree();
+                                self.right.skip_subtree();
+                            }
+                        }
+                        _ => {
+                            // All other cases are two directories that we would iterate if not
+                            // for the matcher
+                            if self.matcher.matches_directory(self.left.path())
+                                == DirectoryMatch::Nothing
+                            {
+                                self.left.skip_subtree();
+                                self.right.skip_subtree();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
