632beb55500b79df9b5531655da0351d9fb96de8
Simon Farnsworth <simonfar@fb.com>
2019-09-23 13:38:01 -0700

    Make sqlite synced_commit_mapping align with D17526345
    Summary: We've improved the MySQL version in review - make the SQLite version match, to avoid nasty test/prod issues
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D17528682
    
    fbshipit-source-id: 498467210608cce0ec27d014d64a22115355cda2

--- commit_rewriting/synced_commit_mapping/schemas/sqlite-synced-commit-mapping.sql
+++ commit_rewriting/synced_commit_mapping/schemas/sqlite-synced-commit-mapping.sql
@@ -1,9 +1,9 @@
-CREATE TABLE synced_commit_mapping (
-  small_repo_id INTEGER NOT NULL,
-  small_bcs_id BINARY(32) NOT NULL,
-  large_repo_id INTEGER NOT NULL,
-  large_bcs_id BINARY(32) NOT NULL,
-  UNIQUE (small_repo_id, small_bcs_id, large_repo_id),
-  UNIQUE (small_repo_id, small_bcs_id, large_repo_id, large_bcs_id),
-  PRIMARY KEY (small_repo_id, small_bcs_id)
+CREATE TABLE `synced_commit_mapping` (
+  `mapping_id` INTEGER PRIMARY KEY,
+  `small_repo_id` int(11) NOT NULL,
+  `small_bcs_id` binary(32) NOT NULL,
+  `large_repo_id` int(11) NOT NULL,
+  `large_bcs_id` binary(32) NOT NULL,
+  UNIQUE (`large_repo_id`,`small_repo_id`,`small_bcs_id`),
+  UNIQUE (`small_repo_id`,`large_repo_id`,`large_bcs_id`)
 );
