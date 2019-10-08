186bdeb1bde5c44939ab0a255fa9c6b94a43aafe
Jun Wu <quark@fb.com>
2019-10-08 11:07:04 -0700

    hgtime: parse a range
    Summary:
    `hg` also supports matching a date range via `util.matchdate`. For example,
    `Apr 2000` would mean `Apr 1, 2000 0:0:0 to Apr 30, 2000 23:59:59`.
    
    Implement that behavior. Also support other formats covered by `hg help dates`
    including `> DATE`, `< DATE`, `DATE to DATE`, and `-DAYS`.
    
    Reviewed By: sfilipco
    
    Differential Revision: D17504706
    
    fbshipit-source-id: db1c280b62a72f4e8128909ca6fd814d85d22491

--- lib/hgtime/src/lib.rs
+++ lib/hgtime/src/lib.rs
@@ -9,13 +9,13 @@
 
 use chrono::prelude::*;
 use chrono::Duration;
-use std::ops::{Add, Sub};
+use std::ops::{Add, Range, Sub};
 use std::sync::atomic::{AtomicI32, Ordering};
 
 /// A simple time structure that matches hg's time representation.
 ///
 /// Internally it's unixtime (in GMT), and offset (GMT -1 = +3600).
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct HgTime {
     pub unixtime: u64,
     pub offset: i32,
@@ -90,14 +90,88 @@ impl HgTime {
                     .ok()
                     .map(|duration| Self::now() - duration.as_secs())
             }
-            _ => Self::parse_absolute(date),
+            _ => Self::parse_absolute(date, default_date_lower),
+        }
+    }
+
+    /// Parse a date string as a range.
+    ///
+    /// For example, `Apr 2000` covers range `Apr 1, 2000` to `Apr 30, 2000`.
+    /// Also support more explicit ranges:
+    /// - START to END
+    /// - > START
+    /// - < END
+    pub fn parse_range(date: &str) -> Option<Range<Self>> {
+        match date {
+            "now" => {
+                let now = Self::now();
+                Some(now..now + 1)
+            }
+            "today" => {
+                let date = Local::today();
+                let start = Self::from(date.and_hms(0, 0, 0)).use_default_offset();
+                let end = Self::from(date.and_hms(23, 59, 59)).use_default_offset() + 1;
+                Some(start..end)
+            }
+            "yesterday" => {
+                let date = Local::today() - Duration::days(1);
+                let start = Self::from(date.and_hms(0, 0, 0)).use_default_offset();
+                let end = Self::from(date.and_hms(23, 59, 59)).use_default_offset() + 1;
+                Some(start..end)
+            }
+            date if date.starts_with(">") => {
+                Self::parse(&date[1..]).map(|start| start..Self::max_value())
+            }
+            date if date.starts_with("since ") => {
+                Self::parse(&date[6..]).map(|start| start..Self::max_value())
+            }
+            date if date.starts_with("<") => {
+                Self::parse(&date[1..]).map(|end| Self::min_value()..end)
+            }
+            date if date.starts_with("-") => {
+                // This does not really make much sense. But is supported by hg
+                // (see 'hg help dates').
+                Self::parse_range(&format!("since {} days ago", &date[1..]))
+            }
+            date if date.starts_with("before ") => {
+                Self::parse(&date[7..]).map(|end| Self::min_value()..end)
+            }
+            date if date.contains(" to ") => {
+                let phrases: Vec<_> = date.split(" to ").collect();
+                if phrases.len() == 2 {
+                    if let (Some(start), Some(end)) =
+                        (Self::parse(&phrases[0]), Self::parse(&phrases[1]))
+                    {
+                        Some(start..end)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            }
+            _ => {
+                let start = Self::parse_absolute(date, default_date_lower);
+                let end = Self::parse_absolute(date, default_date_upper::<N31>)
+                    .or_else(|| Self::parse_absolute(date, default_date_upper::<N30>))
+                    .or_else(|| Self::parse_absolute(date, default_date_upper::<N29>))
+                    .or_else(|| Self::parse_absolute(date, default_date_upper::<N28>));
+                if let (Some(start), Some(end)) = (start, end) {
+                    Some(start..end + 1)
+                } else {
+                    None
+                }
+            }
         }
     }
 
     /// Parse date in an absolute form.
     ///
     /// Return None if it cannot be parsed.
-    fn parse_absolute(date: &str) -> Option<Self> {
+    ///
+    /// `default_date` takes a format char, for example, `H`, and returns a
+    /// default value of it.
+    fn parse_absolute(date: &str, default_date: fn(char) -> &'static str) -> Option<Self> {
         let date = date.trim();
 
         // Hg internal format. "unixtime offset"
@@ -152,11 +226,8 @@ impl HgTime {
                         // For example, if the user only specified
                         // "hour:minute", then we should use "second 0", instead
                         // of the current second.
-                        match format_char {
-                            'H' | 'M' | 'S' => date_with_defaults += " @00",
-                            'Y' | 'm' | 'd' => date_with_defaults += " @1",
-                            _ => unreachable!(),
-                        }
+                        date_with_defaults += " @";
+                        date_with_defaults += default_date(format_char);
                     }
                 }
             }
@@ -187,6 +258,20 @@ impl HgTime {
         }
         self
     }
+
+    pub fn min_value() -> Self {
+        Self {
+            unixtime: 0,
+            offset: 0,
+        }
+    }
+
+    pub fn max_value() -> Self {
+        Self {
+            unixtime: u64::max_value() >> 2,
+            offset: 0,
+        }
+    }
 }
 
 impl Add<u64> for HgTime {
@@ -209,6 +294,12 @@ impl Sub<u64> for HgTime {
             unixtime: self.unixtime.max(seconds) - seconds,
             offset: self.offset,
         }
+    }
+}
+
+impl PartialOrd for HgTime {
+    fn partial_cmp(&self, other: &HgTime) -> Option<std::cmp::Ordering> {
+        self.unixtime.partial_cmp(&other.unixtime)
     }
 }
 
@@ -241,6 +332,59 @@ pub fn set_default_offset(offset: i32) {
 fn is_valid_offset(offset: i32) -> bool {
     // UTC-12 to UTC+14.
     offset >= -50400 && offset <= 43200
+}
+
+/// Lower bound for default values in dates.
+fn default_date_lower(format_char: char) -> &'static str {
+    match format_char {
+        'H' | 'M' | 'S' => "00",
+        'm' | 'd' => "1",
+        _ => unreachable!(),
+    }
+}
+
+trait ToStaticStr {
+    fn to_static_str() -> &'static str;
+}
+
+struct N31;
+struct N30;
+struct N29;
+struct N28;
+
+impl ToStaticStr for N31 {
+    fn to_static_str() -> &'static str {
+        "31"
+    }
+}
+
+impl ToStaticStr for N30 {
+    fn to_static_str() -> &'static str {
+        "30"
+    }
+}
+
+impl ToStaticStr for N29 {
+    fn to_static_str() -> &'static str {
+        "29"
+    }
+}
+
+impl ToStaticStr for N28 {
+    fn to_static_str() -> &'static str {
+        "28"
+    }
+}
+
+/// Upper bound. Assume a month has `N::to_static_str()` days.
+fn default_date_upper<N: ToStaticStr>(format_char: char) -> &'static str {
+    match format_char {
+        'H' => "23",
+        'M' | 'S' => "59",
+        'm' => "12",
+        'd' => N::to_static_str(),
+        _ => unreachable!(),
+    }
 }
 
 #[cfg(test)]
@@ -324,6 +468,30 @@ mod tests {
         assert_eq!(t("9999999 years ago"), "0 7200");
     }
 
+    #[test]
+    fn test_parse_range() {
+        set_default_offset(7200);
+
+        assert_eq!(c("since 1 month ago", "now"), "contains");
+        assert_eq!(c("since 1 month ago", "2 months ago"), "does not contain");
+        assert_eq!(c("> 1 month ago", "2 months ago"), "does not contain");
+        assert_eq!(c("< 1 month ago", "2 months ago"), "contains");
+        assert_eq!(c("< 1 month ago", "now"), "does not contain");
+
+        assert_eq!(c("-3", "now"), "contains");
+        assert_eq!(c("-3", "2 days ago"), "contains");
+        assert_eq!(c("-3", "4 days ago"), "does not contain");
+
+        assert_eq!(c("2018", "2017-12-31 23:59:59"), "does not contain");
+        assert_eq!(c("2018", "2018-1-1"), "contains");
+        assert_eq!(c("2018", "2018-12-31 23:59:59"), "contains");
+        assert_eq!(c("2018", "2019-1-1"), "does not contain");
+
+        assert_eq!(c("2018-5-1 to 2018-6-2", "2018-4-30"), "does not contain");
+        assert_eq!(c("2018-5-1 to 2018-6-2", "2018-5-30"), "contains");
+        assert_eq!(c("2018-5-1 to 2018-6-2", "2018-6-30"), "does not contain");
+    }
+
     /// String representation of parse result.
     fn t(date: &str) -> String {
         match HgTime::parse(date) {
@@ -341,6 +509,20 @@ mod tests {
                 format!("{}", value)
             }
             None => "fail".to_string(),
+        }
+    }
+
+    /// String "contains" (if range contains date) or "does not contain"
+    /// or "fail" (if either range or date fails to parse).
+    fn c(range: &str, date: &str) -> &'static str {
+        if let (Some(range), Some(date)) = (HgTime::parse_range(range), HgTime::parse(date)) {
+            if range.contains(&date) {
+                "contains"
+            } else {
+                "does not contain"
+            }
+        } else {
+            "fail"
         }
     }
 }
