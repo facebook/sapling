5408a9527d10f41856872f33a7d1a1f186f3daaf
Zeyi (Rice) Fan <zeyi@fb.com>
2019-10-19 18:46:34 -0700

    backingstore: strip metadata header from blob retrieved from hgcache
    Summary:
    For blobs stored in Mercurial's pack/indexedlogdatastore, some of these blobs have a metadata
    header at the beginning of the blob for storing copyrev or LFS related
    metadata. We do not want to serve this header to the user, and EdenFS does not
    need these metadata.
    
    Reviewed By: xavierd
    
    Differential Revision: D17864753
    
    fbshipit-source-id: 91dc47df6ae7f1361cb0869491b4ab855a04cf1b

--- lib/backingstore/src/backingstore.rs
+++ lib/backingstore/src/backingstore.rs
@@ -32,6 +32,59 @@ impl BackingStore {
         let node = Node::from_slice(node)?;
         let key = Key::new(path, node);
 
-        self.store.get(&key)
+        self.store
+            .get(&key)
+            .map(|blob| blob.map(discard_metadata_header))
     }
+}
+
+/// Removes the possible metadata header at the beginning of a blob.
+///
+/// The metadata header is defined as the block surrounded by '\x01\x0A' at the beginning of the
+/// blob. If there is no closing tag found in the blob, this function will simply return the
+/// original blob.
+///
+/// See `edenscm/mercurial/filelog.py` for the Python implementation.
+fn discard_metadata_header(data: Vec<u8>) -> Vec<u8> {
+    // Returns when the blob less than 2 bytes long or no metadata header starting tag at the
+    // beginning
+    if data.len() < 2 || !(data[0] == 0x01 && data[1] == 0x0A) {
+        return data;
+    }
+
+    // Finds the position of the closing tag
+    let closing_tag = data.windows(2).skip(2).position(|bytes| match *bytes {
+        [a, b] => a == 0x01 && b == 0x0A,
+        // Rust cannot infer that `.windows` only gives us a two-element slice so we have to write
+        // the predicate this way to provide the default clause.
+        _ => false,
+    });
+
+    if let Some(idx) = closing_tag {
+        // Skip two bytes for the starting tag and two bytes for the closing tag
+        data.into_iter().skip(2 + idx + 2).collect()
+    } else {
+        data
+    }
+}
+
+#[test]
+fn test_discard_metadata_header() {
+    assert_eq!(discard_metadata_header(vec![]), vec![]);
+    assert_eq!(discard_metadata_header(vec![0x1]), vec![0x1]);
+    assert_eq!(discard_metadata_header(vec![0x1, 0x1]), vec![0x1, 0x1]);
+    assert_eq!(discard_metadata_header(vec![0x1, 0xA]), vec![0x1, 0xA]);
+
+    // Empty metadata header and empty blob
+    assert_eq!(discard_metadata_header(vec![0x1, 0xA, 0x1, 0xA]), vec![]);
+    // Metadata header with some data but empty blob
+    assert_eq!(
+        discard_metadata_header(vec![0x1, 0xA, 0xA, 0xB, 0xC, 0x1, 0xA]),
+        vec![]
+    );
+    // Metadata header with data and blob
+    assert_eq!(
+        discard_metadata_header(vec![0x1, 0xA, 0xA, 0xB, 0xC, 0x1, 0xA, 0xA, 0xB, 0xC]),
+        vec![0xA, 0xB, 0xC]
+    );
 }
