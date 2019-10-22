6f5d5cf14fe07ae72329436631854c39a1c0bf1e
Mark Thomas <mbthomas@fb.com>
2019-10-22 05:51:28 -0700

    warm_bookmarks_cache: don't rewarm bookmarks each loop
    Summary:
    Most bookmarks don't change.  Don't keep trying to derive data for them,
    instead just derive data for bookmarks that changed since the previous
    iteration.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D17978196
    
    fbshipit-source-id: 45e72bd35136c58b66ee98f35652588edcab6b83

--- bookmarks/warm_bookmarks_cache/lib.rs
+++ bookmarks/warm_bookmarks_cache/lib.rs
@@ -8,7 +8,7 @@
 
 #![deny(warnings)]
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::sync::{Arc, RwLock};
 use std::time::{Duration, Instant};
 
@@ -20,6 +20,7 @@ use failure::Error;
 use futures::{future, stream, sync, Future, Stream};
 use futures_ext::{BoxFuture, FutureExt, StreamExt};
 use futures_stats::Timed;
+use lock_ext::RwLockExt;
 use mononoke_types::ChangesetId;
 use slog::{info, Logger};
 use stats::{define_stats, Timeseries};
@@ -48,6 +49,7 @@ impl WarmBookmarksCache {
         let warmers = Arc::new(warmers);
         let bookmarks = Arc::new(RwLock::new(HashMap::new()));
         let (sender, receiver) = sync::oneshot::channel();
+        let warm_cs_ids = Arc::new(RwLock::new(HashSet::new()));
         spawn_bookmarks_updater(
             bookmarks.clone(),
             receiver,
@@ -55,8 +57,9 @@ impl WarmBookmarksCache {
             logger,
             repo.clone(),
             warmers.clone(),
+            warm_cs_ids.clone(),
         );
-        update_bookmarks(bookmarks.clone(), ctx, repo, warmers).map(move |()| Self {
+        update_bookmarks(bookmarks.clone(), ctx, repo, warmers, warm_cs_ids).map(move |()| Self {
             bookmarks,
             terminate: Some(sender),
         })
@@ -87,12 +90,13 @@ fn spawn_bookmarks_updater(
     logger: Logger,
     repo: BlobRepo,
     warmers: Arc<Vec<Box<WarmerFn>>>,
+    warm_cs_ids: Arc<RwLock<HashSet<ChangesetId>>>,
 ) {
     tokio::spawn(future::lazy(move || {
         info!(logger, "Starting warm bookmark cache updater");
         stream::repeat(())
             .and_then(move |()| {
-                update_bookmarks(bookmarks.clone(), ctx.clone(), repo.clone(), warmers.clone())
+                update_bookmarks(bookmarks.clone(), ctx.clone(), repo.clone(), warmers.clone(), warm_cs_ids.clone())
                 .timed(|stats, _| {
                     STATS::cached_bookmark_update_time_ms
                         .add_value(stats.completion_time.as_millis_unchecked() as i64);
@@ -120,31 +124,56 @@ fn update_bookmarks(
     ctx: CoreContext,
     repo: BlobRepo,
     warmers: Arc<Vec<Box<WarmerFn>>>,
+    warm_cs_ids: Arc<RwLock<HashSet<ChangesetId>>>,
 ) -> impl Future<Item = (), Error = Error> {
     repo.get_bonsai_publishing_bookmarks_maybe_stale(ctx.clone())
         .map({
+            cloned!(warm_cs_ids);
             move |(bookmark, cs_id)| {
-                // Derive all the necessary derive data.
-                // This makes sure that read path don't have to generate
-                // derived data if a bookmark is requested (which is the most
-                // common case). Ignore any errors during derivation - we
-                // don't want that to affect the set of bookmarks.
-                stream::futures_unordered(warmers.iter().map({
-                    cloned!(ctx, repo);
-                    move |warmer| (*warmer)(ctx.clone(), repo.clone(), cs_id).then(|_| Ok(()))
-                }))
-                .for_each(|_| Ok(()))
-                .map(move |_| (bookmark.into_name(), cs_id))
+                if warm_cs_ids.read().unwrap().contains(&cs_id) {
+                    // This changeset was warmed on the previous iteration.
+                    future::ok(true).left_future()
+                } else {
+                    // Derive all the necessary data to make the changeset
+                    // warm. This makes sure the read path doesn't have to
+                    // generate derived data if a bookmark is requested
+                    // (which is the most common case).  Ignore any errors
+                    // during derivation - we don't want that to affect
+                    // the set of bookmarks - but keep track of which ones
+                    // succeeded, and only count successfully warmed
+                    // bookmarks as warm. Changesets that fail derivation
+                    // will be tried again on the next iteration if they are
+                    // still bookmarked.
+                    stream::futures_unordered(warmers.iter().map({
+                        cloned!(ctx, repo);
+                        move |warmer| {
+                            (*warmer)(ctx.clone(), repo.clone(), cs_id).then(|res| Ok(res.is_ok()))
+                        }
+                    }))
+                    .fold(true, |a, b| -> Result<bool, Error> { Ok(a && b) })
+                    .right_future()
+                }
+                .map(move |success| (bookmark.into_name(), cs_id, success))
             }
         })
         .buffered(100)
-        .collect_to::<HashMap<_, _>>()
-        .map({
-            cloned!(bookmarks);
-            move |map| {
-                let mut bookmarks = bookmarks.write().unwrap();
-                *bookmarks = map;
-            }
+        .collect_to::<Vec<_>>()
+        .map(move |warmed_bookmarks| {
+            // The new set of warm changesets are those which were
+            // already warm, or for which all derivations succeeded.
+            let new_warm_cs_ids: HashSet<_> = warmed_bookmarks
+                .iter()
+                .filter(|(_name, _cs_id, success)| *success)
+                .map(|(_name, cs_id, _success)| cs_id.clone())
+                .collect();
+            warm_cs_ids.with_write(|warm_cs_ids| *warm_cs_ids = new_warm_cs_ids);
+            // The new bookmarks are all the bookmarks, regardless of
+            // whether derivation succeeded.
+            let new_bookmarks: HashMap<_, _> = warmed_bookmarks
+                .into_iter()
+                .map(|(name, cs_id, _success)| (name, cs_id))
+                .collect();
+            bookmarks.with_write(|bookmarks| *bookmarks = new_bookmarks);
         })
 }
 
