e7f69ccbc23c2725713e79444120d87f30b21fb5
Stanislau Hlebik <stash@fb.com>
2019-10-28 04:20:49 -0700

    mononoke: remove unused methods from SyncedCommitMapping
    Summary:
    We are not using them now, and we decide to use them later we can always add
    the back
    
    Reviewed By: aslpavel
    
    Differential Revision: D18169226
    
    fbshipit-source-id: 9b964abde2eda4361ad9610075e37d3b11bd0efe

--- commit_rewriting/synced_commit_mapping/src/lib.rs
+++ commit_rewriting/synced_commit_mapping/src/lib.rs
@@ -8,7 +8,6 @@
 
 #![deny(warnings)]
 
-use std::collections::{BTreeMap, HashMap};
 use std::sync::Arc;
 
 use sql::Connection;
@@ -16,22 +15,12 @@ pub use sql_ext::SqlConstructors;
 
 use cloned::cloned;
 use context::CoreContext;
-use failure::Fail;
 use failure_ext::Error;
-use futures::{future, stream, Future, Stream};
+use futures::{future, Future};
 use futures_ext::{BoxFuture, FutureExt};
-use itertools::Itertools;
 use mononoke_types::{ChangesetId, RepositoryId};
 use sql::queries;
 use stats::{define_stats, Timeseries};
-
-const GET_MULTIPLE_CHUNK_SIZE: usize = 100;
-
-#[derive(Debug, Eq, Fail, PartialEq)]
-pub enum ErrorKind {
-    #[fail(display = "find_first_synced_in_list cannot operate on an empty list")]
-    FindFirstSyncedNeedsAtLeastOneChangeset,
-}
 
 // TODO(simonfar): Once we've proven the concept, we want to cache these
 define_stats! {
@@ -39,10 +28,6 @@ define_stats! {
     gets: timeseries(RATE, SUM),
     gets_master: timeseries(RATE, SUM),
     adds: timeseries(RATE, SUM),
-    get_alls: timeseries(RATE, SUM),
-    get_alls_master: timeseries(RATE, SUM),
-    get_multiples: timeseries(RATE, SUM),
-    find_fist_synceds: timeseries(RATE, SUM),
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
@@ -82,74 +67,6 @@ pub trait SyncedCommitMapping: Send + Sync {
         bcs_id: ChangesetId,
         target_repo_id: RepositoryId,
     ) -> BoxFuture<Option<ChangesetId>, Error>;
-
-    /// Get all mapping entries for a given source commit
-    fn get_all(
-        &self,
-        ctx: CoreContext,
-        repo_id: RepositoryId,
-        bcs_id: ChangesetId,
-    ) -> BoxFuture<BTreeMap<RepositoryId, ChangesetId>, Error>;
-
-    /// Find the mapping entries for multiple commits and a target repo
-    fn get_multiple(
-        &self,
-        ctx: CoreContext,
-        source_repo_id: RepositoryId,
-        target_repo_id: RepositoryId,
-        source_bcs_ids: &[ChangesetId],
-    ) -> BoxFuture<Vec<Option<ChangesetId>>, Error>;
-
-    /// Find the first among `changesets` in `source_repo_id`, which has an equivalent in `target_repo_id`
-    /// and return that equivalent. Note that "first" in this context refers to the oder
-    /// in the `changesets`, not to any changeset date/time property.
-    fn find_first_synced_in_list(
-        &self,
-        ctx: CoreContext,
-        source_repo_id: RepositoryId,
-        target_repo_id: RepositoryId,
-        changesets: &[ChangesetId],
-    ) -> BoxFuture<Option<(ChangesetId, ChangesetId)>, Error> {
-        STATS::find_fist_synceds.add_value(1);
-        if changesets.len() == 0 {
-            return future::err(Error::from(
-                ErrorKind::FindFirstSyncedNeedsAtLeastOneChangeset,
-            ))
-            .boxify();
-        }
-        let changesets_chunks = changesets.iter().chunks(GET_MULTIPLE_CHUNK_SIZE);
-        let chunk_futures = changesets_chunks.into_iter().map(|changesets_chunk| {
-            let changesets_chunk_vec: Vec<ChangesetId> =
-                changesets_chunk.into_iter().cloned().collect();
-            self.get_multiple(
-                ctx.clone(),
-                source_repo_id.clone(),
-                target_repo_id.clone(),
-                &changesets_chunk_vec[..],
-            )
-            .map({
-                cloned!(changesets_chunk_vec);
-                move |changeset_mappings| {
-                    changeset_mappings
-                        .iter()
-                        .enumerate()
-                        .find_map(|(pos, mapping)| {
-                            mapping.map(|mapped_cs_id| {
-                                (changesets_chunk_vec[pos].clone(), mapped_cs_id)
-                            })
-                        })
-                }
-            })
-        });
-        stream::futures_ordered(chunk_futures)
-            .filter_map(|el| el)
-            .into_future()
-            .map(|(maybe_mapping, _rest_of_the_stream)| {
-                maybe_mapping.map(|(cs_id, cs_id_ref)| (cs_id, cs_id_ref.clone()))
-            })
-            .map_err(|(e, _)| e)
-            .boxify()
-    }
 }
 
 impl SyncedCommitMapping for Arc<dyn SyncedCommitMapping> {
@@ -165,23 +82,6 @@ impl SyncedCommitMapping for Arc<dyn SyncedCommitMapping> {
         target_repo_id: RepositoryId,
     ) -> BoxFuture<Option<ChangesetId>, Error> {
         (**self).get(ctx, source_repo_id, bcs_id, target_repo_id)
-    }
-    fn get_all(
-        &self,
-        ctx: CoreContext,
-        repo_id: RepositoryId,
-        bcs_id: ChangesetId,
-    ) -> BoxFuture<BTreeMap<RepositoryId, ChangesetId>, Error> {
-        (**self).get_all(ctx, repo_id, bcs_id)
-    }
-    fn get_multiple(
-        &self,
-        ctx: CoreContext,
-        source_repo_id: RepositoryId,
-        target_repo_id: RepositoryId,
-        source_bcs_ids: &[ChangesetId],
-    ) -> BoxFuture<Vec<Option<ChangesetId>>, Error> {
-        (**self).get_multiple(ctx, source_repo_id, target_repo_id, source_bcs_ids)
     }
 }
 
@@ -212,27 +112,6 @@ queries! {
          FROM synced_commit_mapping
          WHERE (large_repo_id = {source_repo_id} AND large_bcs_id = {bcs_id} AND small_repo_id = {target_repo_id}) OR
          (small_repo_id = {source_repo_id} AND small_bcs_id = {bcs_id} AND large_repo_id = {target_repo_id})"
-    }
-
-    read SelectMultiple(
-        source_repo_id: RepositoryId,
-        target_repo_id: RepositoryId,
-        >list cs_ids: ChangesetId
-    ) -> (RepositoryId, ChangesetId, ChangesetId) {
-        "SELECT large_repo_id, large_bcs_id, small_bcs_id
-         FROM synced_commit_mapping
-         WHERE (large_repo_id = {source_repo_id} AND small_repo_id = {target_repo_id} AND large_bcs_id IN {cs_ids}) OR
-         (small_repo_id = {source_repo_id} AND large_repo_id = {target_repo_id} AND small_bcs_id IN {cs_ids})"
-    }
-
-    read SelectAllMapping(
-        source_repo_id: RepositoryId,
-        bcs_id: ChangesetId,
-    ) -> (RepositoryId, ChangesetId, RepositoryId, ChangesetId) {
-        "SELECT large_repo_id, large_bcs_id, small_repo_id, small_bcs_id
-         FROM synced_commit_mapping
-         WHERE (large_repo_id = {source_repo_id} AND large_bcs_id = {bcs_id}) OR
-         (small_repo_id = {source_repo_id} AND small_bcs_id = {bcs_id})"
     }
 }
 
@@ -323,100 +202,6 @@ impl SyncedCommitMapping for SqlSyncedCommitMapping {
                 }
             } else {
                 None
-            }
-        })
-        .boxify()
-    }
-
-    fn get_all(
-        &self,
-        _ctx: CoreContext,
-        repo_id: RepositoryId,
-        bcs_id: ChangesetId,
-    ) -> BoxFuture<BTreeMap<RepositoryId, ChangesetId>, Error> {
-        STATS::get_alls.add_value(1);
-
-        SelectAllMapping::query(&self.read_connection, &repo_id, &bcs_id)
-            .and_then({
-                cloned!(self.read_master_connection);
-                move |rows| {
-                    if rows.is_empty() {
-                        STATS::get_alls_master.add_value(1);
-                        SelectAllMapping::query(&read_master_connection, &repo_id, &bcs_id)
-                            .left_future()
-                    } else {
-                        future::ok(rows).right_future()
-                    }
-                }
-            })
-            .map(move |rows| {
-                rows.into_iter()
-                    .map(
-                        |(large_repo_id, large_bcs_id, small_repo_id, small_bcs_id)| {
-                            if repo_id == large_repo_id {
-                                (small_repo_id, small_bcs_id)
-                            } else {
-                                (large_repo_id, large_bcs_id)
-                            }
-                        },
-                    )
-                    .collect()
-            })
-            .boxify()
-    }
-
-    fn get_multiple(
-        &self,
-        _ctx: CoreContext,
-        source_repo_id: RepositoryId,
-        target_repo_id: RepositoryId,
-        source_bcs_ids: &[ChangesetId],
-    ) -> BoxFuture<Vec<Option<ChangesetId>>, Error> {
-        STATS::get_multiples.add_value(1);
-        let source_bcs_ids_to_move: Vec<ChangesetId> = source_bcs_ids.iter().cloned().collect();
-        SelectMultiple::query(
-            &self.read_connection,
-            &source_repo_id,
-            &target_repo_id,
-            &source_bcs_ids,
-        )
-        .map({
-            move |rows| {
-                let mapping1: HashMap<&ChangesetId, &ChangesetId> = rows
-                    .iter()
-                    .filter_map(|(large_repo_id, large_cs_id, small_cs_id)| {
-                        // large_cs_id can be on the left side of
-                        // the mapping only if large_repo is a source repo
-                        if &source_repo_id == large_repo_id {
-                            Some((large_cs_id, small_cs_id))
-                        } else {
-                            None
-                        }
-                    })
-                    .collect();
-                let mapping2: HashMap<&ChangesetId, &ChangesetId> = rows
-                    .iter()
-                    .filter_map(|(large_repo_id, large_cs_id, small_cs_id)| {
-                        // small_cs_id can be on the left side of
-                        // the mapping only if large_repo is a target repo
-                        if &target_repo_id == large_repo_id {
-                            Some((small_cs_id, large_cs_id))
-                        } else {
-                            None
-                        }
-                    })
-                    .collect();
-                let res: Vec<Option<ChangesetId>> = source_bcs_ids_to_move
-                    .iter()
-                    .map(|source_bcs_id| {
-                        mapping1
-                            .get(source_bcs_id)
-                            .or_else(|| mapping2.get(source_bcs_id))
-                            .cloned()
-                            .cloned()
-                    })
-                    .collect();
-                res
             }
         })
         .boxify()
--- commit_rewriting/synced_commit_mapping/test/main.rs
+++ commit_rewriting/synced_commit_mapping/test/main.rs
@@ -12,11 +12,10 @@
 
 use fbinit::FacebookInit;
 use futures::Future;
-use maplit::btreemap;
 
 use context::CoreContext;
 use mononoke_types_mocks::changesetid as bonsai;
-use mononoke_types_mocks::repo::{REPO_ONE, REPO_TWO, REPO_ZERO};
+use mononoke_types_mocks::repo::{REPO_ONE, REPO_ZERO};
 use synced_commit_mapping::{
     SqlConstructors, SqlSyncedCommitMapping, SyncedCommitMapping, SyncedCommitMappingEntry,
 };
@@ -52,113 +51,12 @@ fn add_and_get<M: SyncedCommitMapping>(fb: FacebookInit, mapping: M) {
     assert_eq!(result, Some(bonsai::ONES_CSID));
 }
 
-fn get_all<M: SyncedCommitMapping>(fb: FacebookInit, mapping: M) {
-    let ctx = CoreContext::test_mock(fb);
-    let entry =
-        SyncedCommitMappingEntry::new(REPO_ZERO, bonsai::ONES_CSID, REPO_ONE, bonsai::TWOS_CSID);
-    mapping
-        .add(ctx.clone(), entry.clone())
-        .wait()
-        .expect("Adding new entry failed");
-    let entry =
-        SyncedCommitMappingEntry::new(REPO_ZERO, bonsai::ONES_CSID, REPO_TWO, bonsai::THREES_CSID);
-    mapping
-        .add(ctx.clone(), entry.clone())
-        .wait()
-        .expect("Adding new entry failed");
-
-    let result = mapping
-        .get_all(ctx.clone(), REPO_ZERO, bonsai::ONES_CSID)
-        .wait()
-        .expect("Get failed");
-    assert_eq!(
-        result,
-        btreemap! {REPO_ONE => bonsai::TWOS_CSID, REPO_TWO => bonsai::THREES_CSID}
-    );
-
-    let result = mapping
-        .get_all(ctx.clone(), REPO_ONE, bonsai::TWOS_CSID)
-        .wait()
-        .expect("Get failed");
-    assert_eq!(result, btreemap! {REPO_ZERO => bonsai::ONES_CSID});
-}
-
 fn missing<M: SyncedCommitMapping>(fb: FacebookInit, mapping: M) {
     let ctx = CoreContext::test_mock(fb);
     let result = mapping
         .get(ctx.clone(), REPO_ONE, bonsai::TWOS_CSID, REPO_ZERO)
         .wait()
         .expect("Failed to fetch missing changeset (should succeed with None instead)");
-    assert_eq!(result, None);
-}
-
-fn get_multiple<M: SyncedCommitMapping>(fb: FacebookInit, mapping: M) {
-    let ctx = CoreContext::test_mock(fb);
-    let entry12 =
-        SyncedCommitMappingEntry::new(REPO_ZERO, bonsai::ONES_CSID, REPO_ONE, bonsai::TWOS_CSID);
-    let entry34 =
-        SyncedCommitMappingEntry::new(REPO_ZERO, bonsai::THREES_CSID, REPO_ONE, bonsai::FOURS_CSID);
-    mapping
-        .add(ctx.clone(), entry12.clone())
-        .wait()
-        .expect("Adding new entry failed");
-    mapping
-        .add(ctx.clone(), entry34.clone())
-        .wait()
-        .expect("Adding new entry failed");
-
-    let query = vec![bonsai::ONES_CSID, bonsai::TWOS_CSID, bonsai::FIVES_CSID];
-
-    let result = mapping
-        .get_multiple(ctx.clone(), REPO_ZERO, REPO_ONE, &query[..])
-        .wait()
-        .expect("get_multiple failed");
-    assert_eq!(result, vec![Some(bonsai::TWOS_CSID), None, None]);
-    let result = mapping
-        .get_multiple(ctx.clone(), REPO_ONE, REPO_ZERO, &query[..])
-        .wait()
-        .expect("get_multiple failed");
-    assert_eq!(result, vec![None, Some(bonsai::ONES_CSID), None]);
-}
-
-fn find_first_synced_in_list<M: SyncedCommitMapping>(fb: FacebookInit, mapping: M) {
-    let ctx = CoreContext::test_mock(fb);
-    let entry12 =
-        SyncedCommitMappingEntry::new(REPO_ZERO, bonsai::ONES_CSID, REPO_ONE, bonsai::TWOS_CSID);
-    let entry34 =
-        SyncedCommitMappingEntry::new(REPO_ZERO, bonsai::THREES_CSID, REPO_ONE, bonsai::FOURS_CSID);
-    mapping
-        .add(ctx.clone(), entry12.clone())
-        .wait()
-        .expect("Adding new entry failed");
-    mapping
-        .add(ctx.clone(), entry34.clone())
-        .wait()
-        .expect("Adding new entry failed");
-
-    let query = vec![bonsai::ONES_CSID, bonsai::TWOS_CSID, bonsai::FOURS_CSID];
-    let result = mapping
-        .find_first_synced_in_list(ctx.clone(), REPO_ZERO, REPO_ONE, &query[..])
-        .wait()
-        .expect("find_first_synced_in_list failed");
-    assert_eq!(result, Some((bonsai::ONES_CSID, bonsai::TWOS_CSID)));
-    let result = mapping
-        .find_first_synced_in_list(ctx.clone(), REPO_ONE, REPO_ZERO, &query[..])
-        .wait()
-        .expect("find_first_synced_in_list failed");
-    assert_eq!(result, Some((bonsai::TWOS_CSID, bonsai::ONES_CSID)));
-
-    let query = vec![bonsai::SIXES_CSID, bonsai::FIVES_CSID, bonsai::THREES_CSID];
-    let result = mapping
-        .find_first_synced_in_list(ctx.clone(), REPO_ZERO, REPO_ONE, &query[..])
-        .wait()
-        .expect("find_first_synced_in_list failed");
-    assert_eq!(result, Some((bonsai::THREES_CSID, bonsai::FOURS_CSID)));
-
-    let result = mapping
-        .find_first_synced_in_list(ctx.clone(), REPO_ONE, REPO_ZERO, &query[..])
-        .wait()
-        .expect("find_first_synced_in_list failed");
     assert_eq!(result, None);
 }
 
@@ -174,25 +72,4 @@ fn test_missing(fb: FacebookInit) {
     async_unit::tokio_unit_test(move || {
         missing(fb, SqlSyncedCommitMapping::with_sqlite_in_memory().unwrap())
     });
-}
-
-#[fbinit::test]
-fn test_get_all(fb: FacebookInit) {
-    async_unit::tokio_unit_test(move || {
-        get_all(fb, SqlSyncedCommitMapping::with_sqlite_in_memory().unwrap())
-    });
-}
-
-#[fbinit::test]
-fn test_get_multiple(fb: FacebookInit) {
-    async_unit::tokio_unit_test(move || {
-        get_multiple(fb, SqlSyncedCommitMapping::with_sqlite_in_memory().unwrap())
-    })
-}
-
-#[fbinit::test]
-fn test_find_first_synced_in_list(fb: FacebookInit) {
-    async_unit::tokio_unit_test(move || {
-        find_first_synced_in_list(fb, SqlSyncedCommitMapping::with_sqlite_in_memory().unwrap())
-    })
 }
