1f46e89c1b80d5a651c5047c1d2ac44cab38ccbb
Xavier Deguillard <xavierd@fb.com>
2019-11-06 15:32:22 -0800

    hgcommands: make it compile with Python3
    Summary:
    Python argument API no longer take a `const char *` but a `const wchar_t *`,
    let's try to do the right thing to get hgcommand to compile with Python3. I
    have no clue whether the code is correct.
    
    Reviewed By: quark-zju
    
    Differential Revision: D18218071
    
    fbshipit-source-id: 88153bf4dfd435c922701e0334e4582bb07817d2

--- lib/hgcommands/src/python.rs
+++ lib/hgcommands/src/python.rs
@@ -5,7 +5,11 @@
  * GNU General Public License version 2.
  */
 
+#[cfg(feature = "python2")]
+use libc::c_char;
 use libc::c_int;
+#[cfg(feature = "python3")]
+use libc::wchar_t;
 
 #[cfg(feature = "python2")]
 use python27_sys::{
@@ -14,14 +18,37 @@ use python27_sys::{
 };
 #[cfg(feature = "python3")]
 use python3_sys::{
-    PyEval_InitThreads, PySys_SetArgv, Py_Finalize, Py_Initialize, Py_IsInitialized, Py_Main,
-    Py_SetProgramName,
+    PyEval_InitThreads, PySys_SetArgv, PyUnicode_AsWideCharString, PyUnicode_FromString,
+    Py_Finalize, Py_Initialize, Py_IsInitialized, Py_Main, Py_SetProgramName,
 };
 use std::ffi::CString;
 
-pub fn py_set_argv(args: Vec<CString>) {
-    let mut argv: Vec<_> = args.into_iter().map(|x| x.into_raw()).collect();
+#[cfg(feature = "python2")]
+type PyChar = c_char;
+#[cfg(feature = "python3")]
+type PyChar = wchar_t;
+
+#[cfg(feature = "python2")]
+fn to_py_str(s: CString) -> *mut PyChar {
+    s.into_raw()
+}
+
+#[cfg(feature = "python3")]
+fn to_py_str(s: CString) -> *mut PyChar {
+    unsafe {
+        let pyobj = PyUnicode_FromString(s.as_ptr());
+        PyUnicode_AsWideCharString(pyobj, std::ptr::null_mut())
+    }
+}
+
+fn to_py_argv(args: Vec<CString>) -> Vec<*mut PyChar> {
+    let mut argv: Vec<_> = args.into_iter().map(|x| to_py_str(x)).collect();
     argv.push(std::ptr::null_mut());
+    argv
+}
+
+pub fn py_set_argv(args: Vec<CString>) {
+    let mut argv = to_py_argv(args);
     unsafe {
         // This inserts argv[0] path to sys.path, useful for running local builds.
         PySys_SetArgv((argv.len() - 1) as c_int, argv.as_mut_ptr());
@@ -30,8 +57,7 @@ pub fn py_set_argv(args: Vec<CString>) {
 }
 
 pub fn py_main(args: Vec<CString>) -> u8 {
-    let mut argv: Vec<_> = args.into_iter().map(|x| x.into_raw()).collect();
-    argv.push(std::ptr::null_mut());
+    let mut argv = to_py_argv(args);
     let result = unsafe {
         let argc = (argv.len() - 1) as c_int;
         // Py_Main may not return.
@@ -43,7 +69,7 @@ pub fn py_main(args: Vec<CString>) -> u8 {
 
 pub fn py_set_program_name(name: CString) {
     unsafe {
-        Py_SetProgramName(name.into_raw());
+        Py_SetProgramName(to_py_str(name));
     }
 }
 
