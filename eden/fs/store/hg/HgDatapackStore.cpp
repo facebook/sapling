34c255890d078de79c1526bfebdf19b21d835c83
Zeyi (Rice) Fan <zeyi@fb.com>
2019-11-19 17:56:59 -0800

    eden: implement conversion functions from Rust Tree to EdenFS Tree
    Summary:
    This diff adds the C++ conversation methods that converts the Rust struct introduced in the previous Diff into EdenFS's `Tree`.
    
    We have to keep the RocksDB write batch in for storing proxy hashes.
    
    Reviewed By: wez
    
    Differential Revision: D18365504
    
    fbshipit-source-id: 2a2cd2cfebd9f0ae4e279db608becc5890c5ccdf

--- eden/fs/store/hg/HgDatapackStore.cpp
+++ eden/fs/store/hg/HgDatapackStore.cpp
@@ -14,10 +14,75 @@
 #include <optional>
 
 #include "eden/fs/model/Blob.h"
+#include "eden/fs/model/Hash.h"
+#include "eden/fs/model/Tree.h"
+#include "eden/fs/model/TreeEntry.h"
 #include "eden/fs/store/hg/HgProxyHash.h"
 
 namespace facebook {
 namespace eden {
+namespace {
+TreeEntryType fromRawTreeEntryType(RustTreeEntryType type) {
+  switch (type) {
+    case RustTreeEntryType::RegularFile:
+      return TreeEntryType::REGULAR_FILE;
+    case RustTreeEntryType::Tree:
+      return TreeEntryType::TREE;
+    case RustTreeEntryType::ExecutableFile:
+      return TreeEntryType::EXECUTABLE_FILE;
+    case RustTreeEntryType::Symlink:
+      return TreeEntryType::SYMLINK;
+  }
+}
+
+TreeEntry fromRawTreeEntry(
+    RustTreeEntry entry,
+    RelativePathPiece path,
+    LocalStore::WriteBatch* writeBatch) {
+  std::optional<uint64_t> size;
+  std::optional<Hash> contentSha1;
+
+  if (entry.size != nullptr) {
+    size = *entry.size;
+  }
+
+  if (entry.content_sha1 != nullptr) {
+    contentSha1 = Hash{*entry.content_sha1};
+  }
+
+  auto name = folly::StringPiece{entry.name.asByteRange()};
+  auto hash = Hash{entry.hash};
+
+  auto fullPath = path + RelativePathPiece(name);
+  auto proxyHash = HgProxyHash::store(fullPath, hash, writeBatch);
+
+  return TreeEntry{
+      proxyHash, name, fromRawTreeEntryType(entry.ttype), size, contentSha1};
+}
+
+FOLLY_MAYBE_UNUSED std::unique_ptr<Tree> fromRawTree(
+    const RustTree* tree,
+    const Hash& edenTreeId,
+    RelativePathPiece path,
+    LocalStore::WriteBatch* writeBatch) {
+  std::vector<TreeEntry> entries;
+
+  for (uintptr_t i = 0; i < tree->length; i++) {
+    auto entry = fromRawTreeEntry(tree->entries[i], path, writeBatch);
+    entries.push_back(entry);
+  }
+
+  auto edenTree = std::make_unique<Tree>(std::move(entries), edenTreeId);
+  auto serialized = LocalStore::serializeTree(edenTree.get());
+  writeBatch->put(
+      LocalStore::KeySpace::TreeFamily,
+      edenTreeId,
+      serialized.second.coalesce());
+  writeBatch->flush();
+
+  return edenTree;
+}
+} // namespace
 
 std::unique_ptr<Blob> HgDatapackStore::getBlob(
     const Hash& id,
