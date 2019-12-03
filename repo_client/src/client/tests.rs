d8b6840e6c3b0886277d008effeecc692888d7c1
Stanislau Hlebik <stash@fb.com>
2019-12-03 03:57:17 -0800

    mononoke: add tests for get_changed_manifests_stream
    Summary:
    I'm going to change it quite substantially soon, so I'd like to get some
    confidence I didn't screw it up. In particular, it's very easy to forget to
    pass correct depth parameter and correct base path. Tests cover exactly that.
    
    Note that I found one weird hack - if depth is 1 then there's a hardcoded
    condition that returns only one entry. For now I left it untouched, but I'll
    see if I can get rid of it later.
    
    Reviewed By: krallin
    
    Differential Revision: D18764362
    
    fbshipit-source-id: 3d33829766e86cd5957ed07b047073487b50370f

--- repo_client/src/client/tests.rs
+++ repo_client/src/client/tests.rs
@@ -7,9 +7,15 @@
  */
 
 use super::*;
+use blobstore::Loadable;
+use fbinit::FacebookInit;
+use fixtures::many_files_dirs;
+use futures_preview::compat::Future01CompatExt;
 use maplit::hashset;
+use mercurial_types::Changeset;
 use mononoke_types_mocks::changesetid::ONES_CSID;
 use std::collections::HashSet;
+use tokio_preview as tokio;
 
 #[test]
 fn test_parsing_caps_simple() {
@@ -192,4 +198,201 @@ fn test_pushredirect_config() {
         .unwrap(),
         true,
     );
+}
+
+#[fbinit::test]
+async fn get_changed_manifests_stream_test(fb: FacebookInit) -> Result<(), Error> {
+    let ctx = CoreContext::test_mock(fb);
+    let repo = many_files_dirs::getrepo(fb);
+
+    // Commit that has only dir2 directory
+    let root_mf_id = HgChangesetId::from_str("051946ed218061e925fb120dac02634f9ad40ae2")?
+        .load(ctx.clone(), &repo.get_blobstore())
+        .compat()
+        .await?
+        .manifestid();
+
+    let fetched_mfs = fetch_mfs(
+        ctx.clone(),
+        &repo,
+        root_mf_id,
+        HgManifestId::new(NULL_HASH),
+        None,
+        65536,
+    )
+    .await?;
+
+    let mut res = fetched_mfs
+        .into_iter()
+        .map(|(_, path)| path)
+        .collect::<Vec<_>>();
+    res.sort();
+    let mut expected = vec![None, Some(MPath::new("dir2")?)];
+    expected.sort();
+    assert_eq!(res, expected);
+
+    // Now commit that added a few files and directories
+
+    let root_mf_id = HgChangesetId::from_str("d261bc7900818dea7c86935b3fb17a33b2e3a6b4")?
+        .load(ctx.clone(), &repo.get_blobstore())
+        .compat()
+        .await?
+        .manifestid();
+
+    let base_root_mf_id = HgChangesetId::from_str("2f866e7e549760934e31bf0420a873f65100ad63")?
+        .load(ctx.clone(), &repo.get_blobstore())
+        .compat()
+        .await?
+        .manifestid();
+
+    let fetched_mfs =
+        fetch_mfs(ctx.clone(), &repo, root_mf_id, base_root_mf_id, None, 65536).await?;
+
+    let mut res = fetched_mfs
+        .into_iter()
+        .map(|(_, path)| path)
+        .collect::<Vec<_>>();
+    res.sort();
+    let mut expected = vec![
+        None,
+        Some(MPath::new("dir1")?),
+        Some(MPath::new("dir1/subdir1")?),
+        Some(MPath::new("dir1/subdir1/subsubdir1")?),
+        Some(MPath::new("dir1/subdir1/subsubdir2")?),
+    ];
+    expected.sort();
+    assert_eq!(res, expected);
+
+    Ok(())
+}
+
+#[fbinit::test]
+async fn get_changed_manifests_stream_test_depth(fb: FacebookInit) -> Result<(), Error> {
+    let ctx = CoreContext::test_mock(fb);
+    let repo = many_files_dirs::getrepo(fb);
+
+    let root_mf_id = HgChangesetId::from_str("d261bc7900818dea7c86935b3fb17a33b2e3a6b4")?
+        .load(ctx.clone(), &repo.get_blobstore())
+        .compat()
+        .await?
+        .manifestid();
+
+    let base_mf_id = HgManifestId::new(NULL_HASH);
+    let fetched_mfs = fetch_mfs(ctx.clone(), &repo, root_mf_id, base_mf_id, None, 65536).await?;
+
+    let paths = fetched_mfs
+        .into_iter()
+        .map(|(_, path)| path)
+        .collect::<Vec<_>>();
+
+    let max_depth = paths
+        .iter()
+        .map(|path| match path {
+            Some(path) => path.num_components(),
+            None => 0,
+        })
+        .max()
+        .unwrap();
+
+    for depth in 0..max_depth + 1 {
+        println!("depth: {}", depth);
+        let fetched_mfs =
+            fetch_mfs(ctx.clone(), &repo, root_mf_id, base_mf_id, None, depth).await?;
+        let mut actual = fetched_mfs
+            .into_iter()
+            .map(|(_, path)| path)
+            .collect::<Vec<_>>();
+        actual.sort();
+        let iter = paths.clone().into_iter();
+        // We have a weird hard-coded behaviour for depth=1 that we are preserving for now
+        let mut expected: Vec<_> = if depth == 1 {
+            let expected: Vec<_> = iter.filter(|path| path.is_none()).collect();
+            assert_eq!(expected.len(), 1);
+            expected
+        } else {
+            iter.filter(|path| match path {
+                Some(path) => path.num_components() <= depth,
+                None => true,
+            })
+            .collect()
+        };
+        expected.sort();
+        assert_eq!(actual, expected);
+    }
+
+    Ok(())
+}
+
+#[fbinit::test]
+async fn get_changed_manifests_stream_test_base_path(fb: FacebookInit) -> Result<(), Error> {
+    let ctx = CoreContext::test_mock(fb);
+    let repo = many_files_dirs::getrepo(fb);
+
+    let root_mf_id = HgChangesetId::from_str("d261bc7900818dea7c86935b3fb17a33b2e3a6b4")?
+        .load(ctx.clone(), &repo.get_blobstore())
+        .compat()
+        .await?
+        .manifestid();
+
+    let base_mf_id = HgManifestId::new(NULL_HASH);
+    let fetched_mfs = fetch_mfs(ctx.clone(), &repo, root_mf_id, base_mf_id, None, 65536).await?;
+
+    for (hash, path) in &fetched_mfs {
+        println!("base path: {:?}", path);
+        let mut actual =
+            fetch_mfs(ctx.clone(), &repo, *hash, base_mf_id, path.clone(), 65536).await?;
+        actual.sort();
+
+        let mut expected: Vec<_> = fetched_mfs
+            .clone()
+            .into_iter()
+            .filter(|(_, curpath)| match &path {
+                Some(path) => {
+                    let elems = MPath::iter_opt(curpath.as_ref());
+                    path.is_prefix_of(elems)
+                }
+                None => true,
+            })
+            .collect();
+        expected.sort();
+        assert_eq!(actual, expected);
+    }
+
+    Ok(())
+}
+
+async fn fetch_mfs(
+    ctx: CoreContext,
+    repo: &BlobRepo,
+    root_mf_id: HgManifestId,
+    base_root_mf_id: HgManifestId,
+    base_path: Option<MPath>,
+    depth: usize,
+) -> Result<Vec<(HgManifestId, Option<MPath>)>, Error> {
+    let fetched_mfs = get_changed_manifests_stream(
+        ctx.clone(),
+        &repo,
+        root_mf_id,
+        base_root_mf_id,
+        base_path,
+        CombinatorPruner::new(FilePruner, DeletedPruner),
+        depth,
+    )
+    .map(|(entry, path)| {
+        (
+            HgManifestId::new(entry.get_hash().into_nodehash()),
+            MPath::join_opt(path.as_ref(), entry.get_name()),
+        )
+    })
+    .collect()
+    .compat()
+    .await?;
+
+    // Make sure that Manifest ids are present in the repo
+    for (hash, _) in &fetched_mfs {
+        repo.get_manifest_by_nodeid(ctx.clone(), *hash)
+            .compat()
+            .await?;
+    }
+    Ok(fetched_mfs)
 }
