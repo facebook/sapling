f1040f34205da1ae8d2d8032964654d9bfbc7eab
Mark Thomas <mbthomas@fb.com>
2020-01-16 11:49:09 -0800

    scs_server: implement compact blame format
    Summary: Implement the compact blame format in the Source Control Service server.
    
    Reviewed By: mitrandir77
    
    Differential Revision: D19430567
    
    fbshipit-source-id: 4b3d3ca00f147d5af6a7733e63324a1571d4340a

--- scs_server/src/methods/commit_path.rs
+++ scs_server/src/methods/commit_path.rs
@@ -7,17 +7,17 @@
  */
 
 use context::CoreContext;
+use dedupmap::DedupMap;
 use futures_util::future;
 use mononoke_api::{ChangesetSpecifier, MononokeError, PathEntry};
 use source_control as thrift;
+use std::borrow::Cow;
 use std::collections::{BTreeSet, HashMap};
 use std::iter::FromIterator;
 
 use crate::commit_id::map_commit_identities;
 use crate::errors;
-
 use crate::into_response::IntoResponse;
-
 use crate::source_control_impl::SourceControlServiceImpl;
 
 impl SourceControlServiceImpl {
@@ -81,6 +81,10 @@ impl SourceControlServiceImpl {
         match params.format {
             thrift::BlameFormat::VERBOSE => {
                 self.commit_path_blame_verbose(ctx, commit_path, params)
+                    .await
+            }
+            thrift::BlameFormat::COMPACT => {
+                self.commit_path_blame_compact(ctx, commit_path, params)
                     .await
             }
             other_format => Err(errors::invalid_request(format!(
@@ -168,6 +172,101 @@ impl SourceControlServiceImpl {
 
         Ok(thrift::CommitPathBlameResponse {
             blame: thrift::Blame::blame_verbose(blame),
+        })
+    }
+
+    async fn commit_path_blame_compact(
+        &self,
+        ctx: CoreContext,
+        commit_path: thrift::CommitPathSpecifier,
+        params: thrift::CommitPathBlameParams,
+    ) -> Result<thrift::CommitPathBlameResponse, errors::ServiceError> {
+        let (repo, changeset) = self.repo_changeset(ctx, &commit_path.commit).await?;
+        let path = changeset.path(&commit_path.path)?;
+
+        let mut commit_ids = DedupMap::new();
+        let mut commit_id_indexes = HashMap::new();
+        let mut paths = DedupMap::new();
+        let mut authors = DedupMap::new();
+        let mut dates = DedupMap::new();
+
+        let identity_schemes = if let Some(identity_schemes) = params.identity_schemes {
+            identity_schemes
+        } else if let Some(identity_scheme) = params.identity_scheme {
+            BTreeSet::from_iter(Some(identity_scheme))
+        } else {
+            BTreeSet::new()
+        };
+
+        // Map all the changeset IDs into the requested identity schemes.  Keep a mapping of
+        // which bonsai changeset ID corresponds to which mapped commit ID index, so we can look
+        // them up later.
+        let (content, blame) = path.blame().await?;
+        let csids: Vec<_> = blame.ranges().iter().map(|range| range.csid).collect();
+        for (id, mapped_ids) in map_commit_identities(&repo, csids.clone(), &identity_schemes)
+            .await?
+            .into_iter()
+        {
+            let index = commit_ids.insert(Cow::Owned(mapped_ids));
+            commit_id_indexes.insert(id, index);
+        }
+
+        // Collect author and date fields from the commit info.
+        let info: HashMap<_, _> = future::try_join_all(csids.into_iter().map(move |csid| {
+            let repo = repo.clone();
+            async move {
+                let changeset = repo
+                    .changeset(ChangesetSpecifier::Bonsai(csid))
+                    .await?
+                    .ok_or_else(|| {
+                        MononokeError::InvalidRequest(format!("failed to resolve commit: {}", csid))
+                    })?;
+                let date = changeset.author_date().await?;
+                let date = thrift::DateTime {
+                    timestamp: date.timestamp(),
+                    tz: date.offset().local_minus_utc(),
+                };
+                let author = changeset.author().await?;
+                Ok::<_, MononokeError>((csid, (author, date)))
+            }
+        }))
+        .await?
+        .into_iter()
+        .collect();
+
+        let lines = String::from_utf8_lossy(content.as_ref())
+            .lines()
+            .zip(blame.lines())
+            .enumerate()
+            .map(
+                |(line, (contents, (csid, path)))| -> Result<_, thrift::RequestError> {
+                    let commit_id_index = commit_id_indexes.get(&csid).ok_or_else(|| {
+                        errors::commit_not_found(format!("failed to resolve commit: {}", csid))
+                    })?;
+                    let (author, date) = info.get(&csid).ok_or_else(|| {
+                        errors::commit_not_found(format!("failed to resolve commit: {}", csid))
+                    })?;
+                    Ok(thrift::BlameCompactLine {
+                        line: (line + 1) as i32,
+                        contents: contents.to_string(),
+                        commit_id_index: *commit_id_index as i32,
+                        path_index: paths.insert(&path.to_string()) as i32,
+                        author_index: authors.insert(author) as i32,
+                        date_index: dates.insert(Cow::Borrowed(date)) as i32,
+                    })
+                },
+            )
+            .collect::<Result<Vec<_>, _>>()?;
+        let blame = thrift::BlameCompact {
+            lines,
+            commit_ids: commit_ids.into_items(),
+            paths: paths.into_items(),
+            authors: authors.into_items(),
+            dates: dates.into_items(),
+        };
+
+        Ok(thrift::CommitPathBlameResponse {
+            blame: thrift::Blame::blame_compact(blame),
         })
     }
 }
