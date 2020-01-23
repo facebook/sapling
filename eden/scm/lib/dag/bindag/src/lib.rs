a98d288938cc7d28fe7ab3c0e2390c0b04f03253
Jun Wu <quark@fb.com>
2020-01-22 19:26:26 -0800

    bindag: apply smallvec optimization
    Summary:
    This is similar to D17581248. It will make the old linear-scan algorithm (which
    will be added later) about 5x faster.
    
    Reviewed By: sfilipco
    
    Differential Revision: D19503372
    
    fbshipit-source-id: c65d7217e7b144603dadd57f54a5e70f513c8e51

--- eden/scm/lib/dag/bindag/src/lib.rs
+++ eden/scm/lib/dag/bindag/src/lib.rs
@@ -5,11 +5,53 @@
  * GNU General Public License version 2.
  */
 
+use std::ops::Deref;
 use vlqencoding::VLQDecode;
 
 pub static MOZILLA: &[u8] = include_bytes!("mozilla-central.bindag");
+pub static GIT: &[u8] = include_bytes!("git.bindag");
 
-pub fn parse_bindag(bindag: &[u8]) -> Vec<Vec<usize>> {
+/// "smallvec" optimization
+#[derive(Clone, Copy)]
+pub struct ParentRevs([usize; 2]);
+
+impl ParentRevs {
+    const NONE: usize = usize::max_value();
+}
+
+impl From<Vec<usize>> for ParentRevs {
+    fn from(revs: Vec<usize>) -> Self {
+        assert!(revs.len() <= 2);
+        match revs.len() {
+            0 => Self([Self::NONE, Self::NONE]),
+            1 => Self([revs[0], Self::NONE]),
+            2 => Self([revs[0], revs[1]]),
+            n => panic!("unsupported len: {}", n),
+        }
+    }
+}
+
+impl AsRef<[usize]> for ParentRevs {
+    fn as_ref(&self) -> &[usize] {
+        if self.0[0] == Self::NONE {
+            &self.0[0..0]
+        } else if self.0[1] == Self::NONE {
+            &self.0[0..1]
+        } else {
+            &self.0[..]
+        }
+    }
+}
+
+impl Deref for ParentRevs {
+    type Target = [usize];
+
+    fn deref(&self) -> &[usize] {
+        self.as_ref()
+    }
+}
+
+pub fn parse_bindag(bindag: &[u8]) -> Vec<ParentRevs> {
     let mut parents = Vec::new();
     let mut cur = std::io::Cursor::new(bindag);
     let mut read_next = move || -> Result<usize, _> { cur.read_vlq() };
@@ -19,36 +61,36 @@ pub fn parse_bindag(bindag: &[u8]) -> Vec<Vec<usize>> {
         match i {
             0 => {
                 // no parents
-                parents.push(vec![]);
+                parents.push(vec![].into());
             }
             1 => {
                 // 1 specified parent
                 let p1 = next_id - read_next().unwrap() - 1;
-                parents.push(vec![p1]);
+                parents.push(vec![p1].into());
             }
             2 => {
                 // 2 specified parents
                 let p1 = next_id - read_next().unwrap() - 1;
                 let p2 = next_id - read_next().unwrap() - 1;
-                parents.push(vec![p1, p2]);
+                parents.push(vec![p1, p2].into());
             }
             3 => {
                 // 2 parents, p2 specified
                 let p1 = next_id - 1;
                 let p2 = next_id - read_next().unwrap() - 1;
-                parents.push(vec![p1, p2]);
+                parents.push(vec![p1, p2].into());
             }
             4 => {
                 // 2 parents, p1 specified
                 let p1 = next_id - read_next().unwrap() - 1;
                 let p2 = next_id - 1;
-                parents.push(vec![p1, p2]);
+                parents.push(vec![p1, p2].into());
             }
             _ => {
                 // n commits
                 for _ in 0..(i - 4) {
                     let p1 = parents.len() - 1;
-                    parents.push(vec![p1]);
+                    parents.push(vec![p1].into());
                 }
             }
         }
