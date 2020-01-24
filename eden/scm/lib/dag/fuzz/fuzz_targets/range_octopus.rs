29c749ef7df9d5c80142b1536fc67417668a6708
Jun Wu <quark@fb.com>
2020-01-23 17:56:49 -0800

    dag: add fuzz tests on the octopus DAG
    Summary: This gives us some confidence about octopus merge handling.
    
    Reviewed By: DurhamG
    
    Differential Revision: D19540726
    
    fbshipit-source-id: e84de74aecae54429483edd185d39fd1bd858f87

---
+++ eden/scm/lib/dag/fuzz/fuzz_targets/range_octopus.rs
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#![no_main]
+
+use bindag::{octopus, OctopusTestContext};
+use lazy_static::lazy_static;
+use libfuzzer_sys::fuzz_target;
+
+mod tests;
+
+lazy_static! {
+    static ref CONTEXT: OctopusTestContext =
+        OctopusTestContext::from_parents(octopus::cross_octopus());
+}
+
+fuzz_target!(|input: (Vec<u8>, Vec<u8>)| {
+    let roots = CONTEXT.clamp_revs(&input.0);
+    let heads = CONTEXT.clamp_revs(&input.1);
+    tests::test_range(&CONTEXT, roots, heads);
+});
