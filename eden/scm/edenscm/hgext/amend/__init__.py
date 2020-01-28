23562ea499e7d16ba7fd231258a5246f9bef4274
Durham Goode <durham@fb.com>
2020-01-28 10:09:32 -0800

    amend: backout transaction change
    Summary:
    D18213028 made amend and metaedit use a transaction around the
    dirstate. This appears to have broken some auto-restack flows (T61073763). Let's
    back it out for now then I'll debug later.
    
    Reviewed By: quark-zju
    
    Differential Revision: D19581912
    
    fbshipit-source-id: cce32efdc91be9ca301da26a0e26b50aa6aa92d4

--- eden/scm/edenscm/hgext/amend/__init__.py
+++ eden/scm/edenscm/hgext/amend/__init__.py
@@ -402,10 +402,9 @@ def amend(ui, repo, *pats, **opts):
         if opts.get("interactive"):
             # Strip the interactive flag to avoid infinite recursive loop
             opts.pop("interactive")
-            with repo.transaction("record"):
-                cmdutil.dorecord(
-                    ui, repo, amend, None, False, cmdutil.recordfilter, *pats, **opts
-                )
+            cmdutil.dorecord(
+                ui, repo, amend, None, False, cmdutil.recordfilter, *pats, **opts
+            )
             return
 
         else:
