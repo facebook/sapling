5ca1a925b5e0df9657de0390814fcced4013eff9
Arun Kulshreshtha <kulshrax@fb.com>
2020-02-04 16:26:49 -0800

    gotham_ext: factor out common HTTP boilerplate into new crate
    Summary:
    A lot of the code in the LFS server is generally useful for any Gotham-based HTTP server, so let's create a place to put the common pieces. Mononoke's HTTP-based services like the LFS server and EdenAPI server can then depend on this common crate.
    
    In this initial diff, I've simply factored out the `Middleware` trait, as well as the custom Handler struct, which right now appears to simply be a mechanism to run the middleware before and after each request.
    
    Given that none of this code appears to be LFS-specific, I hope this is reasonable. I'm open to suggestions for naming things; for now I've named the new crate `gotham_ext` since it's built on Gotham, but I could see us using another name if we intend to move in the direction of plain `hyper` in the future.
    
    In later diffs, I intend to start moving over some of the more general Middleware from the LFS server to the new crate (though LFS-specific Middleware will remain a part of the LFS server code).
    
    Reviewed By: krallin
    
    Differential Revision: D19714662
    
    fbshipit-source-id: 21494d18be659a8ec399c61eb22ee10185efcc14

---
+++ mononoke/gotham_ext/src/handler.rs
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
+use failure::Error; // required by gotham's NewHandler trait
+use futures::Future;
+use gotham::{
+    handler::{Handler, HandlerFuture, IntoResponse, NewHandler},
+    router::Router,
+    state::State,
+};
+use std::sync::Arc;
+
+use crate::middleware::Middleware;
+
+#[derive(Clone)]
+pub struct MononokeHttpHandler {
+    router: Router,
+    middleware: Arc<Vec<Box<dyn Middleware>>>,
+}
+
+// This trait is boilerplate to let us bind this in Gotham as the service. This is essentially
+// patterned match off of how the Router Handler works in Gotham.
+impl NewHandler for MononokeHttpHandler {
+    type Instance = MononokeHttpHandler;
+
+    fn new_handler(&self) -> Result<Self::Instance, Error> {
+        Ok(self.clone())
+    }
+}
+
+// Gotham's router only runs middleware on route matches, which means we don't get any visibility
+// into e.g. 404's. So, we use this handler to wrap Gotham's router and run our own middleware
+// stack. Our middleware is also fairly minimal in the sense that it doesn't need to replace
+// responses or prevent requests, so we expose a slightly simpler API.
+impl Handler for MononokeHttpHandler {
+    fn handle(self, mut state: State) -> Box<HandlerFuture> {
+        // On request, middleware is called in order, then called the other way around on response.
+        // This is what regular Router middleware in Gotham would do.
+        let middleware = self.middleware.clone();
+        for m in middleware.iter() {
+            m.inbound(&mut state);
+        }
+
+        // NOTE: It's a bit unfortunate that we have to return a HandlerFuture here when really
+        // we'd rather be working with just (State, HttpResponse<Body>) everywhere, but that's how
+        // it is in Gotham.
+        let fut = self.router.handle(state).then(move |res| {
+            let (mut state, mut response) = res.unwrap_or_else(|(state, err)| {
+                let response = err.into_response(&state);
+                (state, response)
+            });
+
+            for m in middleware.iter().rev() {
+                m.outbound(&mut state, &mut response);
+            }
+
+            Ok((state, response))
+        });
+
+        Box::new(fut)
+    }
+}
+
+impl MononokeHttpHandler {
+    pub fn builder() -> MononokeHttpHandlerBuilder {
+        MononokeHttpHandlerBuilder { middleware: vec![] }
+    }
+}
+
+pub struct MononokeHttpHandlerBuilder {
+    middleware: Vec<Box<dyn Middleware>>,
+}
+
+impl MononokeHttpHandlerBuilder {
+    pub fn add<T: Middleware>(mut self, middleware: T) -> Self {
+        self.middleware.push(Box::new(middleware));
+        self
+    }
+
+    pub fn build(self, router: Router) -> MononokeHttpHandler {
+        MononokeHttpHandler {
+            router,
+            middleware: Arc::new(self.middleware),
+        }
+    }
+}
