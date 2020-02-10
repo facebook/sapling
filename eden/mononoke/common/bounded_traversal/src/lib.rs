c88ad00fcfbb63257e564282886c5fe5df520cea
Alex Hornby <ahornby@fb.com>
2020-02-10 05:58:31 -0800

    mononoke: create bounded_traversal crate with bounded_traversal_stream
    Summary: create a bounded traversal crate with the 0.1 version of bounded_traversal_stream functionality,  prior to updating to futures 0.3
    
    Reviewed By: farnz
    
    Differential Revision: D19804873
    
    fbshipit-source-id: e98e00111fee5b1a9fcfc20bb054eeae1263fb26

---
+++ eden/mononoke/common/bounded_traversal/src/lib.rs
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
+mod stream;
+pub use stream::bounded_traversal_stream;
+
+#[cfg(test)]
+mod tests;
