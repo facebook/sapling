cbb399614194644959e07b036b5a9297f4575045
Alex Hornby <ahornby@fb.com>
2020-03-05 05:36:39 -0800

    mononoke: walker: fix waiting on tail
    Summary:
    Fix the tail interval delay, it wasn't triggering.
    
    Took the opportunity to structure the code as a loop as well which simplified it a bit.
    
    Reviewed By: markbt
    
    Differential Revision: D20247077
    
    fbshipit-source-id: 1786ef1528a4b0493f5e454d28450d7198af8ad4

--- eden/mononoke/walker/src/tail.rs
+++ eden/mononoke/walker/src/tail.rs
@@ -12,10 +12,7 @@ use anyhow::Error;
 use cloned::cloned;
 use context::CoreContext;
 use fbinit::FacebookInit;
-use futures::{
-    future::Future,
-    stream::{repeat, BoxStream, StreamExt},
-};
+use futures::{future::Future, stream::BoxStream};
 use scuba_ext::ScubaSampleBuilder;
 use slog::Logger;
 use tokio_preview::time::{Duration, Instant};
@@ -45,49 +42,38 @@ where
     let scuba_builder = datasources.scuba_builder;
     let repo = datasources.blobrepo.await?;
     let tail_secs = walk_params.tail_secs.clone();
-    let mut stream: BoxStream<'static, Result<_, Error>> = repeat(())
-        .then({
-            move |_| {
-                cloned!(make_run, repo, mut scuba_builder, walk_state,);
+    loop {
+        cloned!(make_run, repo, mut scuba_builder, walk_state,);
 
-                let ctx = CoreContext::new_with_logger(fb, logger.clone());
-                scuba_builder.add("session", ctx.session().session_id().to_string());
-                let walk_run = RepoWalkRun {
-                    ctx: ctx.clone(),
-                    scuba_builder: scuba_builder.clone(),
-                };
+        let ctx = CoreContext::new_with_logger(fb, logger.clone());
+        scuba_builder.add("session", ctx.session().session_id().to_string());
+        let walk_run = RepoWalkRun {
+            ctx: ctx.clone(),
+            scuba_builder: scuba_builder.clone(),
+        };
 
-                let walk_output = walk_exact(
-                    ctx,
-                    repo,
-                    walk_params.enable_derive,
-                    walk_params.walk_roots.clone(),
-                    walk_state,
-                    walk_params.scheduled_max,
-                    walk_params.error_as_data_node_types.clone(),
-                    walk_params.error_as_data_edge_types.clone(),
-                    scuba_builder,
-                );
+        let walk_output = walk_exact(
+            ctx,
+            repo,
+            walk_params.enable_derive,
+            walk_params.walk_roots.clone(),
+            walk_state,
+            walk_params.scheduled_max,
+            walk_params.error_as_data_node_types.clone(),
+            walk_params.error_as_data_edge_types.clone(),
+            scuba_builder,
+        );
 
-                let make_sink = make_run(walk_run);
-                make_sink(walk_output)
+        let make_sink = make_run(walk_run);
+        make_sink(walk_output).await?;
+
+        match tail_secs {
+            Some(interval) => {
+                let start = Instant::now();
+                let next_iter_deadline = start + Duration::from_secs(interval);
+                tokio_preview::time::delay_until(next_iter_deadline).await;
             }
-        })
-        .boxed();
-    match tail_secs {
-        None => match stream.next().await {
-            None => Ok(()),
-            Some(r) => r,
-        },
-        Some(interval) => {
-            stream
-                .for_each(async move |_| {
-                    let start = Instant::now();
-                    let next_iter_deadline = start + Duration::from_secs(interval);
-                    let _ = tokio_preview::time::delay_until(next_iter_deadline);
-                })
-                .await;
-            Ok(())
+            None => return Ok(()),
         }
     }
 }
