1d651b0a07906a2a67e994ecceec582a8e3f08c0
Adam Simpkins <simpkins@fb.com>
2020-03-30 19:24:43 -0700

    replace the old getdeps.py script with a build.sh script
    Summary:
    Replace Eden's old custom `getdeps.py` script with a simple `build.sh` wrapper
    that invokes the modern `getdeps.py` tool.
    
    Note that this build script was largely cribbed from the `getdeps.sh` script
    in the [fboss](https://github.com/facebook/fboss/) repository.
    
    Reviewed By: wez
    
    Differential Revision: D20688291
    
    fbshipit-source-id: 6f119479280e82a0c28b8b1481fa01611908c566

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+TOOLCHAIN_DIR=/opt/rh/devtoolset-8/root/usr/bin
+if [[ -d "$TOOLCHAIN_DIR" ]]; then
+    PATH="$TOOLCHAIN_DIR:$PATH"
+fi
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+GETDEPS_PATHS=(
+    "$SCRIPT_DIR/build/fbcode_builder/getdeps.py"
+    "$SCRIPT_DIR/../../opensource/fbcode_builder/getdeps.py"
+)
+for getdeps in "${GETDEPS_PATHS[@]}"; do
+    if [[ -x "$getdeps" ]]; then
+        exec "$getdeps" build eden "$@"
+    fi
+done
+echo "Could not find getdeps.py" >&2
+exit 1
