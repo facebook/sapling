975ddc043fb0110304e18d6c1678baafe1906c5a
Mateusz Kwapich <mitrandir@fb.com>
2020-04-06 09:54:36 -0700

    test showing the problem with repeating blob_keys
    Summary:
    When we have more entries with a single blobkey we always select all of them
    regardles when and how they were added. That's why we need to select basing on
    operation_key.
    
    Reviewed By: krallin
    
    Differential Revision: D20557699
    
    fbshipit-source-id: 77ccf992bb24d1a46ea28a13ab0780e6c92935ae

--- eden/mononoke/blobstore_sync_queue/test/main.rs
+++ eden/mononoke/blobstore_sync_queue/test/main.rs
@@ -34,25 +34,31 @@ fn test_simple(fb: FacebookInit) -> Result<(), Error> {
     let key1 = String::from("key1");
     let t0 = DateTime::from_rfc3339("2018-11-29T12:00:00.00Z").unwrap();
     let t1 = DateTime::from_rfc3339("2018-11-29T12:01:00.00Z").unwrap();
+    let t2 = DateTime::from_rfc3339("2018-11-29T12:02:00.00Z").unwrap();
 
     let node_id = [1, 2, 2, 4, 5, 6, 7, 8];
     let op0 = OperationKey(Uuid::from_fields(0, 0, 1, &node_id)?); // for key0
     let op1 = OperationKey(Uuid::from_fields(0, 0, 2, &node_id)?); // for key1
+    let op2 = OperationKey(Uuid::from_fields(0, 0, 3, &node_id)?); // for second put of key0
 
     let entry0 = BlobstoreSyncQueueEntry::new(key0.clone(), bs0, mp, t0, op0.clone());
     let entry1 = BlobstoreSyncQueueEntry::new(key0.clone(), bs1, mp, t1, op0.clone());
     let entry2 = BlobstoreSyncQueueEntry::new(key1.clone(), bs0, mp, t1, op1.clone());
+    let entry3 = BlobstoreSyncQueueEntry::new(key0.clone(), bs0, mp, t2, op2.clone());
+    let entry4 = BlobstoreSyncQueueEntry::new(key0.clone(), bs1, mp, t2, op2);
 
     // add
     assert!(rt.block_on(queue.add(ctx.clone(), entry0.clone())).is_ok());
     assert!(rt.block_on(queue.add(ctx.clone(), entry1.clone())).is_ok());
     assert!(rt.block_on(queue.add(ctx.clone(), entry2.clone())).is_ok());
+    assert!(rt.block_on(queue.add(ctx.clone(), entry3.clone())).is_ok());
+    assert!(rt.block_on(queue.add(ctx.clone(), entry4.clone())).is_ok());
 
     // get
     let entries1 = rt
         .block_on(queue.get(ctx.clone(), key0.clone()))
         .expect("Get failed");
-    assert_eq!(entries1.len(), 2);
+    assert_eq!(entries1.len(), 4);
     assert_eq!(entries1[0].operation_key, op0);
     let entries2 = rt
         .block_on(queue.get(ctx.clone(), key1.clone()))
@@ -64,19 +70,19 @@ fn test_simple(fb: FacebookInit) -> Result<(), Error> {
     let some_entries = rt
         .block_on(queue.iter(ctx.clone(), None, mp, t1, 1))
         .expect("DateTime range iteration failed");
-    assert_eq!(some_entries.len(), 2);
+    assert_eq!(some_entries.len(), 4);
     let some_entries = rt
         .block_on(queue.iter(ctx.clone(), None, mp, t1, 2))
         .expect("DateTime range iteration failed");
-    assert_eq!(some_entries.len(), 3);
+    assert_eq!(some_entries.len(), 5);
     let some_entries = rt
         .block_on(queue.iter(ctx.clone(), None, mp, t0, 1))
         .expect("DateTime range iteration failed");
-    assert_eq!(some_entries.len(), 2);
+    assert_eq!(some_entries.len(), 4);
     let some_entries = rt
         .block_on(queue.iter(ctx.clone(), None, mp, t0, 100))
         .expect("DateTime range iteration failed");
-    assert_eq!(some_entries.len(), 2);
+    assert_eq!(some_entries.len(), 4);
 
     // delete
     rt.block_on(queue.del(ctx.clone(), vec![entry0]))
