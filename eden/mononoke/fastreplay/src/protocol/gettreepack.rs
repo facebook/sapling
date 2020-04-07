7e2ad0b529afa1cea65aa8b7ee48705ee2ddf39f
Thomas Orozco <torozco@fb.com>
2020-04-07 04:34:01 -0700

    mononoke/fastreplay: handle Gettreepack for designated nodes
    Summary: We need to parse `directories` here. Let's do so.
    
    Reviewed By: HarveyHunt
    
    Differential Revision: D20869830
    
    fbshipit-source-id: 74830aa0045b801fba089812447fb61d7d09ad14

--- eden/mononoke/fastreplay/src/protocol/gettreepack.rs
+++ eden/mononoke/fastreplay/src/protocol/gettreepack.rs
@@ -5,14 +5,14 @@
  * GNU General Public License version 2.
  */
 
-use anyhow::Error;
-use hgproto::GettreepackArgs;
+use anyhow::{format_err, Error};
+use hgproto::{batch, GettreepackArgs};
 use mononoke_types::MPath;
 use serde::Deserialize;
 use std::borrow::Cow;
 use std::str::FromStr;
 
-use super::util::{extract_separated_list, split_separated_list};
+use super::util::extract_separated_list;
 
 pub struct RequestGettreepackArgs(pub GettreepackArgs);
 
@@ -25,6 +25,35 @@ pub struct ReplayData<'a> {
     depth: Option<Cow<'a, str>>,
 }
 
+fn parse_directories(dirs: &str) -> Result<Vec<Vec<u8>>, Error> {
+    // We do this early check, because later we'll pop off the last element and check that it's
+    // empty, so this has to go here.
+    if dirs.is_empty() {
+        return Ok(vec![]);
+    }
+
+    // Directories have a trailing comma, and are wireproto encoded. So, we decode each entry, and
+    // skip the last one (which we verify is empty).
+    let mut ret = dirs
+        .split(',')
+        .map(|d| batch::unescape(d.as_bytes()))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    match ret.pop() {
+        Some(v) if v.is_empty() => {
+            // Noop
+        }
+        e => {
+            return Err(format_err!(
+                "Invalid trailing element in parse_directories: {:?}",
+                e
+            ));
+        }
+    };
+
+    Ok(ret)
+}
+
 impl FromStr for RequestGettreepackArgs {
     type Err = Error;
 
@@ -34,16 +63,14 @@ impl FromStr for RequestGettreepackArgs {
             .next()
             .ok_or_else(|| Error::msg(format!("Invalid Gettreepack ReplayData: {}", args)))?;
 
-        // See wireproto.escapearg in Mercurial
-        let directories = split_separated_list(json.directories.as_ref(), ",")
-            .map(|_| Err(Error::msg("Gettreepack directories are not supported yet")))
-            .collect::<Result<_, _>>()?;
-
         let args = GettreepackArgs {
             rootdir: MPath::new_opt(&*json.rootdir)?,
             mfnodes: extract_separated_list(json.mfnodes.as_ref(), " ")?,
             basemfnodes: extract_separated_list(json.basemfnodes.as_ref(), " ")?,
-            directories,
+            directories: parse_directories(json.directories.as_ref())?
+                .into_iter()
+                .map(|d| d.into())
+                .collect(),
             depth: json.depth.map(|d| d.parse()).transpose()?,
         };
 
@@ -58,6 +85,26 @@ mod test {
     #[test]
     fn test_parse() -> Result<(), Error> {
         RequestGettreepackArgs::from_str(include_str!("./fixtures/gettreepack.json"))?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_directories() -> Result<(), Error> {
+        assert_eq!(parse_directories("foo,")?, vec![b"foo".to_vec()]);
+
+        assert_eq!(parse_directories("foo:obar,")?, vec![b"foo,bar".to_vec()]);
+
+        assert_eq!(
+            parse_directories("foo,bar,")?,
+            vec![b"foo".to_vec(), b"bar".to_vec()]
+        );
+
+        assert_eq!(parse_directories(",")?, vec![b"".to_vec()]);
+
+        assert_eq!(parse_directories("")?, Vec::<Vec<u8>>::new());
+
+        assert!(parse_directories(",foo").is_err());
+
         Ok(())
     }
 }
