d588fe8a35e2464ab7269cecafb79b0d4ea61a62
Jun Wu <quark@fb.com>
2020-04-09 18:41:01 -0700

    py3: enable test-import-unknown-t.py
    Reviewed By: xavierd
    
    Differential Revision: D20952869
    
    fbshipit-source-id: 10cbe7fc1e7cc18270c7b320156325cc48dc58f3

--- eden/scm/tests/test-import-unknown-t.py
+++ eden/scm/tests/test-import-unknown-t.py
@@ -9,9 +9,6 @@ from __future__ import absolute_import
 from testutil.dott import feature, sh, testtmp  # noqa: F401
 
 
-feature.require(["py2"])
-
-
 sh % "hg init test"
 sh % "cd test"
 sh % "echo a" > "changed"
