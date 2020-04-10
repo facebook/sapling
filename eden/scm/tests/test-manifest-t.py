dd81246300ff869f8667b025aa2a58f449964617
Xavier Deguillard <xavierd@fb.com>
2020-04-10 11:20:28 -0700

    tests: enable more python3 tests
    Reviewed By: DurhamG
    
    Differential Revision: D20931013
    
    fbshipit-source-id: 018a5268ad9fd59030c06217e7f7e6eee000ece6

--- eden/scm/tests/test-manifest-t.py
+++ eden/scm/tests/test-manifest-t.py
@@ -9,9 +9,6 @@ from __future__ import absolute_import
 from testutil.dott import feature, sh, testtmp  # noqa: F401
 
 
-feature.require(["py2"])
-
-
 sh % "setconfig 'extensions.treemanifest=!'"
 # Source bundle was generated with the following script:
 
