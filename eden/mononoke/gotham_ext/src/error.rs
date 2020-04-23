43eae850911332670feb1082d3ff7d9e1fbdb35e
Arun Kulshreshtha <kulshrax@fb.com>
2020-04-23 13:50:56 -0700

    gotham_ext: add into_handler_response method to HttpError
    Summary: Add a convenience method to HttpError to return a JSON-formatted representation of the error to the client.
    
    Reviewed By: krallin
    
    Differential Revision: D21193939
    
    fbshipit-source-id: e1ff1555b0016f46dbcd1847239d96daf8b45685

--- eden/mononoke/gotham_ext/src/error.rs
+++ eden/mononoke/gotham_ext/src/error.rs
@@ -5,8 +5,17 @@
  * GNU General Public License version 2.
  */
 
+use std::iter;
+
 use anyhow::Error;
-use hyper::StatusCode;
+use gotham::{
+    handler::{HandlerError, IntoHandlerError},
+    helpers::http::response::create_response,
+    state::{request_id, State},
+};
+use hyper::{Body, Response, StatusCode};
+use itertools::Itertools;
+use serde_derive::{Deserialize, Serialize};
 
 /// Wrapper around an anyhow::Error to indicate which
 /// HTTP status code should be returned to the client.
@@ -57,4 +66,39 @@ impl HttpError {
             status_code: StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
+
+    /// Turn this error into a type corresponding to the return type
+    /// of a Gotham handler, so that it may be directly returned from
+    /// a handler function.
+    pub fn into_handler_response(
+        self,
+        state: State,
+    ) -> Result<(State, Response<Body>), (State, HandlerError)> {
+        // Concatenate all chained errors into a single string.
+        let message = iter::once(self.error.to_string())
+            .chain(self.error.chain().skip(1).map(|c| c.to_string()))
+            .join(": ");
+
+        // Package the error message into a JSON response.
+        let res = JsonError {
+            message,
+            request_id: request_id(&state).to_string(),
+        };
+
+        // Convert to JSON; should not fail but return a handler error if so.
+        match serde_json::to_string(&res) {
+            Ok(res) => {
+                let res = create_response(&state, self.status_code, mime::APPLICATION_JSON, res);
+                Ok((state, res))
+            }
+            Err(error) => Err((state, error.into_handler_error())),
+        }
+    }
+}
+
+/// JSON representation of an error to send to the client.
+#[derive(Clone, Serialize, Debug, Deserialize)]
+struct JsonError {
+    message: String,
+    request_id: String,
 }
