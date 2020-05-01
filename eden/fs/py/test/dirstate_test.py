14ed2e817df3c902a1c4b194f59a8cca8bb65d4c
Sergey Firsov <sfirsov@fb.com>
2020-04-30 21:48:14 -0700

    Add new workloads to fiosynth
    Summary:
    add new 2 workloads
    fixed unit tests
    
    Reviewed By: darryleg
    
    Differential Revision: D21333448
    
    fbshipit-source-id: 2673dc3a62cdf227559f33e5301fab9293c3eb01

--- eden/fs/py/test/dirstate_test.py
+++ eden/fs/py/test/dirstate_test.py
@@ -1,9 +1,8 @@
+#!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
-
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 import io
 import unittest
