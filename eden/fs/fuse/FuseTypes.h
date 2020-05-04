63158b56f67d137038548778865fa8783a2e4004
Chad Austin <chadaustin@fb.com>
2020-05-04 11:28:42 -0700

    move third-party/ into eden/fs/
    Summary: These third-party includes are edenfs-specific, so move them into eden/fs/
    
    Reviewed By: simpkins
    
    Differential Revision: D21314642
    
    fbshipit-source-id: c52b0a00d5080934e1f07e4cd55373602f2f6b0a

--- eden/fs/fuse/FuseTypes.h
+++ eden/fs/fuse/FuseTypes.h
@@ -8,9 +8,9 @@
 #pragma once
 #include <folly/File.h>
 #ifdef __linux__
-#include "eden/third-party/fuse_kernel_linux.h" // @manual=//eden/third-party:fuse_kernel
+#include "eden/fs/third-party/fuse_kernel_linux.h" // @manual=//eden/fs/third-party:fuse_kernel
 #elif defined(__APPLE__)
-#include "eden/third-party/fuse_kernel_osxfuse.h" // @manual=//eden/third-party:fuse_kernel
+#include "eden/fs/third-party/fuse_kernel_osxfuse.h" // @manual=//eden/fs/third-party:fuse_kernel
 #else
 #error need a fuse kernel header to be included for your OS!
 #endif
