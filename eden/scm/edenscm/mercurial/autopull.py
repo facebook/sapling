a5c1b003a301888b8e43dc38c96fd9bf9ef7ad4d
Jun Wu <quark@fb.com>
2020-05-06 15:26:13 -0700

    autopull: move to a module
    Summary:
    We're going to make autopull more complicated. Move it to a separate module as
    it is not directly related to revset.
    
    Reviewed By: sfilipco
    
    Differential Revision: D21320631
    
    fbshipit-source-id: fe60bc53ebf1c75f8bf66156805cbe2801fe6532

---
+++ eden/scm/edenscm/mercurial/autopull.py
@@ -0,0 +1,117 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+# autopull.py - utilities to pull commits automatically.
+
+from __future__ import absolute_import
+
+import re
+
+from . import bookmarks, error, util
+from .i18n import _
+from .node import hex
+
+
+_commithashre = re.compile(r"\A[0-9a-f]{6,40}\Z")
+
+
+def _cachedstringmatcher(pattern, _cache={}):
+    # _cache is shared across function calls
+    result = _cache.get(pattern)
+    if result is None:
+        result = util.stringmatcher(pattern)[-1]
+        _cache[pattern] = result
+    return result
+
+
+def trypull(repo, x):
+    """Pull the given name x. Return true if pull succeeded. Does not raise."""
+    repo._autopulled = getattr(repo, "_autopulled", set())
+    if x in repo._autopulled:
+        # Do not attempt to pull the same name twice.
+        return False
+    repo._autopulled.add(x)
+
+    # If paths.default is not set. Do not attempt to pull.
+    if repo.ui.paths.get("default") is None:
+        return False
+
+    def _trypull(source, bookmarknames=(), headnames=()):
+        """Attempt to pull from source. Writes to ui.ferr. Returns True on success."""
+        try:
+            path = repo.ui.paths.getpath(source)
+        except error.RepoError:
+            # path does not exist. Skip
+            return False
+
+        url = str(path.url)
+
+        if bookmarknames:
+            displayname = _("bookmark %r") % ", ".join(bookmarknames)
+            assert not headnames or headnames == bookmarknames
+        else:
+            displayname = ", ".join(headnames)
+        repo.ui.status_err(_("pulling %s from %r\n") % (displayname, url))
+
+        # When pulling a single commit. Also pull selectivepull bookmarks so
+        # it does not end up with lagged master issues.
+        if not bookmarknames and headnames:
+            bookmarknames = bookmarks.selectivepullbookmarknames(repo, source)
+
+        try:
+            repo.pull(source, bookmarknames=bookmarknames, headnames=headnames)
+        except Exception as ex:
+            repo.ui.status_err(_("pull failed: %s\n") % ex)
+            return False
+
+        # Double-check that the names are actually pulled. This is needed
+        # because the pull API does not make sure the names will become
+        # resolvable (ex. it ignores bookmarks that do not exist, and uses
+        # "lookup" to resolve names to hashes without storing the names)
+        if x not in repo.unfiltered():
+            return False
+
+        return True
+
+    # TODO: Once Mononoke handles all infinitepush pull requests, remove
+    # _trypulls and just use a single path paths.default for pulling.
+    def _trypulls(sources, bookmarknames=(), headnames=()):
+        """Attempt to pull from the given sources. Remove this method once
+        Mononoke serves all pull requests via the default path.
+        """
+        for source in sources:
+            if _trypull(source, bookmarknames, headnames):
+                return True
+        return False
+
+    # Pull remote names like "remote/foo" automatically.
+    pattern = repo.ui.config("remotenames", "autopullpattern")
+    if pattern and "/" in x:
+        matchfn = _cachedstringmatcher(pattern)
+        if matchfn(x):
+            remotename, name = bookmarks.splitremotename(x)
+            if _trypulls(
+                ["infinitepushbookmark", "infinitepush", remotename],
+                bookmarknames=[name],
+                headnames=[name],
+            ):
+                return True
+
+    # Pull commit hashes automatically.
+    if repo.ui.configbool("ui", "autopullcommits") and _commithashre.match(x):
+        if _trypulls(["infinitepush", "default"], headnames=[x]):
+            return True
+
+    # Pull hoist remote names automatically. For example, "foo" -> "remote/foo".
+    hoistpattern = repo.ui.config("remotenames", "autopullhoistpattern")
+    if hoistpattern:
+        hoist = repo.ui.config("remotenames", "hoist")
+        matchfn = _cachedstringmatcher(hoistpattern)
+        if matchfn(x) and _trypulls(
+            ["infinitepushbookmark", "infinitepush", hoist], bookmarknames=[x]
+        ):
+            return True
+
+    return False
--- eden/scm/edenscm/mercurial/revset.py
+++ eden/scm/edenscm/mercurial/revset.py
@@ -16,6 +16,7 @@ import re
 import time
 
 from . import (
+    autopull,
     bookmarks,
     dagop,
     encoding,
@@ -190,116 +191,8 @@ def _warnrevnum(ui, x):
         raise error.Abort(_("local revision number is disabled in this repo"))
 
 
-_commithashre = re.compile(r"\A[0-9a-f]{6,40}\Z")
-
-
-def _cachedstringmatcher(pattern, _cache={}):
-    # _cache is shared across function calls
-    result = _cache.get(pattern)
-    if result is None:
-        result = util.stringmatcher(pattern)[-1]
-        _cache[pattern] = result
-    return result
-
-
-def _autopull(repo, x):
-    """Pull the given name x. Return true if pull succeeded. Does not raise."""
-    repo._autopulled = getattr(repo, "_autopulled", set())
-    if x in repo._autopulled:
-        # Do not attempt to pull the same name twice.
-        return False
-    repo._autopulled.add(x)
-
-    # If paths.default is not set. Do not attempt to pull.
-    if repo.ui.paths.get("default") is None:
-        return False
-
-    def _trypull(source, bookmarknames=(), headnames=()):
-        """Attempt to pull from source. Writes to ui.ferr. Returns True on success."""
-        try:
-            path = repo.ui.paths.getpath(source)
-        except error.RepoError:
-            # path does not exist. Skip
-            return False
-
-        url = str(path.url)
-
-        if bookmarknames:
-            displayname = _("bookmark %r") % ", ".join(bookmarknames)
-            assert not headnames or headnames == bookmarknames
-        else:
-            displayname = ", ".join(headnames)
-        repo.ui.status_err(_("pulling %s from %r\n") % (displayname, url))
-
-        # When pulling a single commit. Also pull selectivepull bookmarks so
-        # it does not end up with lagged master issues.
-        if not bookmarknames and headnames:
-            bookmarknames = bookmarks.selectivepullbookmarknames(repo, source)
-
-        try:
-            repo.pull(source, bookmarknames=bookmarknames, headnames=headnames)
-        except Exception as ex:
-            repo.ui.status_err(_("pull failed: %s\n") % ex)
-            return False
-
-        # Double-check that the names are actually pulled. This is needed
-        # because the pull API does not make sure the names will become
-        # resolvable (ex. it ignores bookmarks that do not exist, and uses
-        # "lookup" to resolve names to hashes without storing the names)
-        if x not in repo.unfiltered():
-            return False
-
-        return True
-
-    # TODO: Once Mononoke handles all infinitepush pull requests, remove
-    # _trypulls and just use a single path paths.default for pulling.
-    def _trypulls(sources, bookmarknames=(), headnames=()):
-        """Attempt to pull from the given sources. Remove this method once
-        Mononoke serves all pull requests via the default path.
-        """
-        for source in sources:
-            if _trypull(source, bookmarknames, headnames):
-                return True
-        return False
-
-    # Pull remote names like "remote/foo" automatically.
-    pattern = repo.ui.config("remotenames", "autopullpattern")
-    if pattern and "/" in x:
-        matchfn = _cachedstringmatcher(pattern)
-        if matchfn(x):
-            remotename, name = bookmarks.splitremotename(x)
-            if _trypulls(
-                ["infinitepushbookmark", "infinitepush", remotename],
-                bookmarknames=[name],
-                headnames=[name],
-            ):
-                return True
-
-    # Pull commit hashes automatically.
-    if repo.ui.configbool("ui", "autopullcommits") and _commithashre.match(x):
-        if _trypulls(["infinitepush", "default"], headnames=[x]):
-            return True
-
-    # Pull hoist remote names automatically. For example, "foo" -> "remote/foo".
-    hoistpattern = repo.ui.config("remotenames", "autopullhoistpattern")
-    if hoistpattern:
-        hoist = repo.ui.config("remotenames", "hoist")
-        matchfn = _cachedstringmatcher(hoistpattern)
-        if matchfn(x) and _trypulls(
-            ["infinitepushbookmark", "infinitepush", hoist], bookmarknames=[x]
-        ):
-            return True
-
-    return False
-
-
-def stringset(repo, subset, x, order, autopull=True):
-    try:
-        i = scmutil.intrev(repo[x])
-    except error.FilteredRepoLookupError:
-        raise
-    except error.RepoLookupError:
-        raise
+def stringset(repo, subset, x, order):
+    i = scmutil.intrev(repo[x])
 
     if x.startswith("-") or x == str(i):
         # 'x' was used as a revision number. Maybe warn and log it.
@@ -2754,13 +2647,13 @@ def matchany(ui, specs, repo=None, localalias=None):
             resolved = False
             for spec in specs:
                 if not lookup(spec):
-                    resolved = resolved or _autopull(repo, spec)
+                    resolved = resolved or autopull.trypull(repo, spec)
             if resolved:
                 # Re-parse the revset specs.
                 return matchany(ui, specs, repo, localalias)
             # Auto-pull individual names in the parsed revset.
             for name in unknownnames:
-                if not _autopull(repo, name):
+                if not autopull.trypull(repo, name):
                     break
 
     posttreebuilthook(tree, repo)
