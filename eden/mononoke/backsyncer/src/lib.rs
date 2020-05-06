179a72ef4e45d7f27b626b4364880a0b2ebd0855
Lukas Piatkowski <luk@fb.com>
2020-05-06 09:58:07 -0700

    mononoke: fixup licenses in mononoke
    Reviewed By: krallin
    
    Differential Revision: D21427558
    
    fbshipit-source-id: dc46b4269836bbb70636efcea24c3ead090df84c

--- eden/mononoke/backsyncer/src/lib.rs
+++ eden/mononoke/backsyncer/src/lib.rs
@@ -1,4 +1,9 @@
-// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
 
 #![deny(warnings)]
 
