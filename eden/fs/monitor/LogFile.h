603711d31441b07f89553792e84a19bbdd2b2eef
Adam Simpkins <simpkins@fb.com>
2020-05-07 20:03:54 -0700

    add log rotation support to edenfs_monitor
    Summary:
    Update the LogFile class in edenfs_monitor to support log rotation when the
    file size exceeds a set limit.
    
    There is a generic API to abstract out the log rotation details.  For now
    I have only implemented a relatively straightforward implementation that
    appends the current timestamp to the log path, and prunes old log files.
    
    Reviewed By: chadaustin
    
    Differential Revision: D20427310
    
    fbshipit-source-id: f6aefc8f2fe604fdd1d6a72c3d30666612912f41

--- eden/fs/monitor/LogFile.h
+++ eden/fs/monitor/LogFile.h
@@ -7,19 +7,30 @@
 
 #pragma once
 
+#include <condition_variable>
+#include <deque>
 #include <memory>
+#include <mutex>
+#include <thread>
 
 #include <folly/File.h>
-#include <folly/io/async/EventHandler.h>
+#include <folly/Synchronized.h>
 
 #include "eden/fs/utils/PathFuncs.h"
 
 namespace facebook {
 namespace eden {
 
+class LogRotationStrategy;
+
 class LogFile {
  public:
   explicit LogFile(const AbsolutePath& path);
+  LogFile(
+      const AbsolutePath& path,
+      size_t maxSize,
+      std::unique_ptr<LogRotationStrategy> rotationStrategy);
+  ~LogFile();
 
   /**
    * Write data to the log file.
@@ -34,9 +45,22 @@ class LogFile {
   }
 
  private:
-  static constexpr size_t kBufferSize = 64 * 1024;
+  using RotateQueue = std::deque<std::optional<AbsolutePath>>;
 
+  void rotate();
+  folly::File mainThreadRotation();
+  void triggerBackgroundRotation(std::optional<AbsolutePath>&& path);
+  void runRotateThread();
+
+  AbsolutePath const path_;
   folly::File log_;
+  size_t logSize_{0};
+  size_t maxLogSize_{100 * 1024 * 1024};
+  std::unique_ptr<LogRotationStrategy> const rotationStrategy_;
+
+  std::condition_variable rotationCV_;
+  folly::Synchronized<RotateQueue, std::mutex> rotationQueue_;
+  std::thread rotationThread_;
 };
 
 } // namespace eden
