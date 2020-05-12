647a91647b1563371619d319aef75b6bb476d371
Arun Kulshreshtha <kulshrax@fb.com>
2020-05-12 16:20:20 -0700

    edenapi: add history support to data_util
    Summary: Add the ability to parse EdenAPI history responses to `data_util`.
    
    Reviewed By: quark-zju
    
    Differential Revision: D21489228
    
    fbshipit-source-id: 42dda64273673431a6f3e4d7bd430689c76c387f

--- eden/scm/lib/edenapi/utils/data_util/src/main.rs
+++ eden/scm/lib/edenapi/utils/data_util/src/main.rs
@@ -8,19 +8,23 @@
 //! data_util -- Extract data from EdenAPI responses
 //!
 //! This program allows querying the contents of
-//! an EdenAPI CBOR data response, and extracting
-//! the raw data contained therein.
+//! EdenAPI CBOR data and history responses.
 
 #![deny(warnings)]
 
+use std::collections::{BTreeMap, BTreeSet};
 use std::fs::File;
 use std::io::{prelude::*, stdin, stdout};
 use std::path::PathBuf;
 
 use anyhow::{anyhow, Result};
+use serde::de::DeserializeOwned;
 use structopt::StructOpt;
 
-use types::{api::DataResponse, Key, RepoPathBuf, Validity};
+use types::{
+    api::{DataResponse, HistoryResponse},
+    Key, Parents, RepoPathBuf, Validity, WireHistoryEntry,
+};
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "data_util", about = "Extract data from EdenAPI responses")]
@@ -28,6 +32,7 @@ enum Args {
     Ls(LsArgs),
     Cat(CatArgs),
     Check(CheckArgs),
+    History(HistoryArgs),
 }
 
 #[derive(Debug, StructOpt)]
@@ -57,16 +62,42 @@ struct CheckArgs {
     input: Option<PathBuf>,
 }
 
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Read the content of a CBOR history response")]
+enum HistoryArgs {
+    Ls(HistLsArgs),
+    Show(HistShowArgs),
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "List files in this history response")]
+struct HistLsArgs {
+    #[structopt(help = "Input CBOR file (stdin is used if omitted)")]
+    input: Option<PathBuf>,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Show history for a single file")]
+struct HistShowArgs {
+    #[structopt(help = "Input CBOR file (stdin is used if omitted)")]
+    input: Option<PathBuf>,
+    #[structopt(long, short, help = "Only show entries for given file")]
+    file: Option<String>,
+    #[structopt(long, short, help = "Only show number of entries per file")]
+    count: bool,
+}
+
 fn main() -> Result<()> {
     match Args::from_args() {
         Args::Ls(args) => cmd_ls(args),
         Args::Cat(args) => cmd_cat(args),
         Args::Check(args) => cmd_check(args),
+        Args::History(args) => cmd_history(args),
     }
 }
 
 fn cmd_ls(args: LsArgs) -> Result<()> {
-    let response = read_input(args.input)?;
+    let response: DataResponse = read_input(args.input)?;
     for entry in response.entries {
         println!("{}", entry.key());
     }
@@ -78,7 +109,7 @@ fn cmd_cat(args: CatArgs) -> Result<()> {
     let hgid = args.hgid.parse()?;
     let key = Key::new(path, hgid);
 
-    let response = read_input(args.input)?;
+    let response: DataResponse = read_input(args.input)?;
     let entry = response
         .entries
         .into_iter()
@@ -89,7 +120,7 @@ fn cmd_cat(args: CatArgs) -> Result<()> {
 }
 
 fn cmd_check(args: CheckArgs) -> Result<()> {
-    let response = read_input(args.input)?;
+    let response: DataResponse = read_input(args.input)?;
     for entry in response.entries {
         match entry.data().1 {
             Validity::Valid => {}
@@ -107,7 +138,77 @@ fn cmd_check(args: CheckArgs) -> Result<()> {
     Ok(())
 }
 
-fn read_input(path: Option<PathBuf>) -> Result<DataResponse> {
+fn cmd_history(args: HistoryArgs) -> Result<()> {
+    match args {
+        HistoryArgs::Ls(args) => cmd_history_ls(args),
+        HistoryArgs::Show(args) => cmd_history_show(args),
+    }
+}
+
+fn cmd_history_ls(args: HistLsArgs) -> Result<()> {
+    let response: HistoryResponse = read_input(args.input)?;
+    // Deduplicate and sort paths.
+    let mut paths = BTreeSet::new();
+    for (path, _) in response.entries {
+        paths.insert(path.into_string());
+    }
+    for path in paths {
+        println!("{}", path);
+    }
+    Ok(())
+}
+
+fn cmd_history_show(args: HistShowArgs) -> Result<()> {
+    let response: HistoryResponse = read_input(args.input)?;
+    let map = make_history_map(response);
+    match args.file {
+        Some(ref path) => match map.get(path) {
+            Some(entries) => print_history(path, entries, args.count),
+            None => println!("Path not found in input: {}", path),
+        },
+        None => {
+            for (path, entries) in &map {
+                print_history(path, entries, args.count);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn make_history_map(response: HistoryResponse) -> BTreeMap<String, Vec<WireHistoryEntry>> {
+    let mut map = BTreeMap::new();
+    for (path, entry) in response.entries {
+        map.entry(path.into_string())
+            .or_insert_with(Vec::new)
+            .push(entry);
+    }
+    map
+}
+
+fn print_history(path: &str, entries: &[WireHistoryEntry], counts_only: bool) {
+    if counts_only {
+        println!("{}: {}", path, entries.len());
+    } else {
+        println!("{}:", path);
+        for entry in entries {
+            println!("  node: {}", entry.node);
+            let parents = match entry.parents {
+                Parents::Two(p1, p2) => format!("{} {}", p1, p2),
+                Parents::One(p1) => format!("{}", p1),
+                Parents::None => "None".to_string(),
+            };
+            println!("  parents: {}", parents);
+            println!("  linknode: {}", entry.linknode);
+            if let Some(path) = &entry.copyfrom {
+                println!("  copyfrom: {}", path);
+            }
+            println!()
+        }
+        println!()
+    }
+}
+
+fn read_input<T: DeserializeOwned>(path: Option<PathBuf>) -> Result<T> {
     Ok(match path {
         Some(path) => {
             eprintln!("Reading from file: {:?}", &path);
