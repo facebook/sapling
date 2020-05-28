0c4f3b108d4f10f162561b78e3ac1c44bdc287e1
Xavier Deguillard <xavierd@fb.com>
2020-05-28 08:58:59 -0700

    revert: Run Eden as a Windows service
    Summary:
    Wanting to have EdenFS run as a service on Windows cause a couple of issues
    on Windows:
     - Needing to log out after installing
     - Forcing Sandcastle to use --foreground, which means the edenfs.log is
       empty, making it impossible to debug anything on Sandcastle,
     - Services can only be started by an elevated user, while the rest of EdenFS
       doesn't have this requirement,
     - Uses undocumented template services
    
    The next diffs in the stack will attempt to solve all of these by starting
    EdenFS as a scheduled task instead, which should solve all the above.
    
    Reviewed By: fanzeyi
    
    Differential Revision: D21732280
    
    fbshipit-source-id: b959344da1e77819220d26695ff6634f13ac4e0d

--- eden/fs/win/service/main.cpp
+++ eden/fs/win/service/main.cpp
@@ -21,7 +21,6 @@
 #include "eden/fs/service/EdenServer.h"
 #include "eden/fs/service/StartupLogger.h"
 #include "eden/fs/telemetry/SessionInfo.h"
-#include "eden/fs/win/service/WinService.h"
 #include "eden/fs/win/utils/StringConv.h"
 #include "folly/io/IOBuf.h"
 #include "folly/portability/Windows.h"
@@ -34,6 +33,16 @@ using namespace facebook::eden;
 using namespace std;
 using namespace folly;
 
+// TODO(puneetk): Logging on Windows doesn't work when the async is set. Fix the
+// Async logging and enable the default logging below.
+
+// Set the default log level for all eden logs to DBG2
+// Also change the "default" log handler (which logs to stderr) to log
+// messages asynchronously rather than blocking in the logging thread.
+// FOLLY_INIT_LOGGING_CONFIG("eden=DBG2; default:async=true");
+
+FOLLY_INIT_LOGGING_CONFIG("eden=DBG4");
+
 // The --edenfs flag is defined to help make the flags consistent across Windows
 // and non-Windows platforms.  On non-Windows platform this flag is required, as
 // a check to help ensure that users do not accidentally invoke `edenfs` when
@@ -44,7 +53,79 @@ DEFINE_bool(
     false,
     "This optional argument is currently ignored on Windows");
 
+namespace {
+
+constexpr StringPiece kEdenVersion = "edenwin";
+
+} // namespace
+
 int __cdecl main(int argc, char** argv) {
+  XLOG(INFO) << "Eden Windows - starting";
+  std::vector<std::string> originalCommandLine{argv, argv + argc};
+
+  // Make sure to run this before any flag values are read.
   folly::init(&argc, &argv);
-  WinService::create(argc, argv);
-}
+
+  auto identity = UserInfo::lookup();
+  auto privHelper = make_unique<PrivHelper>();
+
+  std::unique_ptr<EdenConfig> edenConfig;
+  try {
+    edenConfig = getEdenConfig(identity);
+  } catch (const ArgumentError& ex) {
+    fprintf(stderr, "%s\n", ex.what());
+    return -1;
+  }
+
+  // Set some default glog settings, to be applied unless overridden on the
+  // command line
+  gflags::SetCommandLineOptionWithMode(
+      "logtostderr", "1", gflags::SET_FLAGS_DEFAULT);
+  gflags::SetCommandLineOptionWithMode(
+      "minloglevel", "0", gflags::SET_FLAGS_DEFAULT);
+
+  auto prepareFuture = folly::Future<folly::Unit>::makeEmpty();
+  std::shared_ptr<StartupLogger> startupLogger;
+  try {
+    auto logPath = getLogPath(edenConfig->edenDir.getValue());
+    startupLogger = daemonizeIfRequested(logPath);
+  } catch (std::exception& ex) {
+    // If the log redirection fails this error will show up on stderr. When Eden
+    // is running in the background, this error will be lost. If the log file is
+    // empty we should run the edenfs.exe on the console to get the error.
+    fprintf(stderr, "%s\n", ex.what());
+    return -1;
+  }
+
+  SessionInfo sessionInfo;
+  sessionInfo.username = identity.getUsername();
+  sessionInfo.hostname = getHostname();
+  sessionInfo.os = getOperatingSystemName();
+  sessionInfo.osVersion = getOperatingSystemVersion();
+  sessionInfo.edenVersion = kEdenVersion.str();
+
+  std::optional<EdenServer> server;
+  try {
+    server.emplace(
+        std::move(originalCommandLine),
+        std::move(identity),
+        std::move(sessionInfo),
+        std::move(privHelper),
+        std::move(edenConfig));
+    prepareFuture = server->prepare(startupLogger);
+  } catch (const std::exception& ex) {
+    fprintf(stderr, "Error: failed to start EdenFS: %s\n", ex.what());
+    return -1;
+  }
+
+  try {
+    server->getServer()->serve();
+    server->performCleanup();
+  } catch (const std::exception& ex) {
+    fprintf(stderr, "Error while running EdenFS: %s\n", ex.what());
+    return -1;
+  }
+
+  XLOG(INFO) << "Eden Windows - exiting";
+  return 0;
+};
