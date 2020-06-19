6926c80fe8463d58ad21704c9ddbb57447f8bac5
Arun Kulshreshtha <kulshrax@fb.com>
2020-06-19 12:54:08 -0700

    edenapi_types: add doc comment
    Summary: Add a crate-level doc comment clarifying the purpose of this crate. In particular, the motivation behind moving some of EdenAPI's types into this crate was to make it easier to share types between the server and client. As such, types that are not shared (which may pull in more complex dependencies and therefore cause build issues for either the server or client) should not be put in this crate.
    
    Reviewed By: quark-zju
    
    Differential Revision: D22142180
    
    fbshipit-source-id: 7258ef33b73a87acf72d4f6bcbe8b27cbc361735

--- eden/scm/lib/edenapi/types/src/lib.rs
+++ eden/scm/lib/edenapi/types/src/lib.rs
@@ -5,6 +5,24 @@
  * GNU General Public License version 2.
  */
 
+//! Types shared between the EdenAPI client and server.
+//!
+//! This crate exists primarily to provide a lightweight place to
+//! put types that need to be used by both the client and server.
+//! Types that are exclusive used by either the client or server
+//! SHOULD NOT be added to this crate.
+//!
+//! Given that the client and server are each part of different
+//! projects (Mercurial and Mononoke, respectively) which have
+//! different build processes, putting shared types in their own
+//! crate decreases the likelihood of build failures caused by
+//! dependencies with complex or esoteric build requirements.
+//!
+//! Most of the types in this crate are used for data interchange
+//! between the client and server. As such, CHANGES TO THE THESE
+//! TYPES MAY CAUSE VERSION SKEW, so any changes should proceed
+//! with caution.
+
 #![deny(warnings)]
 
 pub mod data;
