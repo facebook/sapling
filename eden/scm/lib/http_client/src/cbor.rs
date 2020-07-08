014f1a5289d2b2c2898983e44f31e866c5a3fa59
Arun Kulshreshtha <kulshrax@fb.com>
2020-07-08 12:49:06 -0700

    http_client: add buffering to CborStream
    Summary:
    As was pointed out in the review for D22280745 (https://github.com/facebookexperimental/eden/commit/d73c63d8620f8e1e7fccd663ba02412514b84861), `CborStream` is inefficient in situations where the underlying stream produces chunks that are much smaller than the size of the serialized items. To avoid pathological behavior, make `CborStream` buffer the incoming data, and only attempt deserialization if enough data has accumulated.
    
    For now, the buffer size is fixed (with a default of 1MB, chosen arbitrarily). In the future, it might make sense to have the stream adjust the buffer size based on the average size of observed deserialized values.
    
    Reviewed By: quark-zju
    
    Differential Revision: D22370164
    
    fbshipit-source-id: ed940c56ca2cbbfc07f01d47becf6f1d71872872

--- eden/scm/lib/http_client/src/cbor.rs
+++ eden/scm/lib/http_client/src/cbor.rs
@@ -6,6 +6,7 @@
  */
 
 use std::marker::PhantomData;
+use std::mem;
 use std::pin::Pin;
 
 use futures::{
@@ -17,13 +18,19 @@ use pin_project::pin_project;
 use serde::de::DeserializeOwned;
 use serde_cbor::Deserializer;
 
+const DEFAULT_BUFFER_SIZE: usize = 1024 * 1024;
+
 /// A wrapper around a `TryStream` of bytes that will attempt to deserialize
 /// CBOR-encoded values from the data stream as it is received.
+///
+/// Data from the underlying stream will be buffered until a sufficient amount
+/// has accumulated before attempting deserialization. This is important for
+/// efficiency since libcurl will typically return a stream of small packets.
 #[pin_project]
 #[must_use = "streams do nothing unless polled"]
 pub struct CborStream<T, S, B, E> {
     #[pin]
-    incoming: S,
+    incoming: BufferedStream<S, B, E>,
     buffer: Vec<u8>,
     position: usize,
     terminated: bool,
@@ -32,8 +39,12 @@ pub struct CborStream<T, S, B, E> {
 
 impl<T, S, B, E> CborStream<T, S, B, E> {
     pub(crate) fn new(body: S) -> Self {
+        Self::with_buffer_size(body, DEFAULT_BUFFER_SIZE)
+    }
+
+    pub(crate) fn with_buffer_size(body: S, size: usize) -> Self {
         Self {
-            incoming: body,
+            incoming: BufferedStream::new(body, size),
             buffer: Vec::new(),
             position: 0,
             terminated: false,
@@ -73,8 +84,10 @@ where
                 _ => {}
             }
 
-            // Copy any remaining data to the front of the buffer.
-            // (This data is the prefix of an incomplete serialized item.)
+            // At this point we've deserialized most of the data in the buffer.
+            // Any remaining trailing data is the prefix of a single, incomplete
+            // item. We should move it to the front of the buffer to reclaim the
+            // space from the items that have already been deserialized.
             let pos = *this.position;
             let len = this.buffer.len() - pos;
             for i in 0..len {
@@ -94,6 +107,62 @@ where
                 }
             }
         }
+    }
+}
+
+/// A wrapper around a byte stream that buffers the data until it exceeds
+/// the given size threshold. This can improve the efficiency of processing
+/// data from bytes streams that consist largely of small chunks.
+#[pin_project]
+#[must_use = "streams do nothing unless polled"]
+struct BufferedStream<S, B, E> {
+    #[pin]
+    inner: S,
+    buffer: Vec<u8>,
+    threshold: usize,
+    done: bool,
+    _phantom: PhantomData<(B, E)>,
+}
+
+impl<S, B, E> BufferedStream<S, B, E> {
+    fn new(inner: S, n: usize) -> Self {
+        Self {
+            inner,
+            buffer: Vec::with_capacity(n),
+            threshold: n,
+            done: false,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<S, B, E> Stream for BufferedStream<S, B, E>
+where
+    S: Stream<Item = Result<B, E>> + Send + 'static,
+    B: AsRef<[u8]>,
+{
+    type Item = Result<Vec<u8>, E>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+
+        if *this.done {
+            return Poll::Ready(None);
+        }
+
+        while this.buffer.len() < *this.threshold {
+            match ready!(this.inner.as_mut().poll_next(cx)) {
+                Some(Ok(bytes)) => this.buffer.extend_from_slice(bytes.as_ref()),
+                Some(Err(e)) => return Poll::Ready(Some(Err(e))),
+                None => {
+                    *this.done = true;
+                    break;
+                }
+            }
+        }
+
+        let buf = mem::replace(this.buffer, Vec::with_capacity(*this.threshold));
+        Poll::Ready(Some(Ok(buf)))
     }
 }
 
@@ -179,8 +248,10 @@ mod tests {
         let res2: Result<Option<TestItem>> = cbor_stream.try_next().await;
         let res3: Result<Option<TestItem>> = cbor_stream.try_next().await;
 
-        assert_eq!(res1?, Some(items[0].clone()));
-        assert!(res2.is_err());
+        // Due to internal buffering, the error will be returned first
+        // even though it was second in the input stream.
+        assert!(res1.is_err());
+        assert_eq!(res2?, Some(items[0].clone()));
         assert_eq!(res3?, Some(items[1].clone()));
 
         Ok(())
@@ -232,6 +303,39 @@ mod tests {
 
         let res: Vec<TestItem> = cbor_stream.try_collect().await?;
         assert_eq!(res, items);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_buffered_stream() -> Result<()> {
+        let words: Vec<Result<Vec<u8>>> = vec![
+            Ok(b"The ".to_vec()),
+            Ok(b"quick ".to_vec()),
+            Ok(b"brown ".to_vec()),
+            Ok(b"fox ".to_vec()),
+            Ok(b"jumped ".to_vec()),
+            Ok(b"over ".to_vec()),
+            Ok(b"the ".to_vec()),
+            Ok(b"lazy ".to_vec()),
+            Ok(b"dog".to_vec()),
+        ];
+
+        let input = stream::iter(words);
+        let buf_size = 20;
+        let buffered = Box::pin(BufferedStream::new(input, buf_size));
+        let output = buffered.try_collect::<Vec<_>>().await?;
+
+        assert_eq!(output.len(), 3);
+
+        // Exactly 20 bytes long.
+        assert_eq!(&*output[0], &b"The quick brown fox "[..]);
+
+        // 21 bytes long because last chunk pushed buffer over limit.
+        assert_eq!(&*output[1], &b"jumped over the lazy "[..]);
+
+        // Short last item.
+        assert_eq!(&*output[2], &b"dog"[..]);
 
         Ok(())
     }
