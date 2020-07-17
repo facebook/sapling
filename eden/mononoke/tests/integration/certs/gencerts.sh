48ef39a0b56992f7e0b74e18645ed2b6633ac514
Lukas Piatkowski <luk@fb.com>
2020-07-17 05:24:33 -0700

    mononoke/integration tests: provide localhost certs for OSS test runs
    Differential Revision: D22596741
    
    fbshipit-source-id: 6b899963873822ffa3e2a4d1a65202b95d506247

---
+++ eden/mononoke/tests/integration/certs/gencerts.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+
+set -x
+
+if [[ -z "$1" ]]; then
+    echo "Provide path to openssl.cnf file"
+    exit 1
+fi
+
+# Generate private key for root certificate
+openssl genrsa -out root-ca.key 2048
+# Generate root certificate
+openssl req -x509 -new -nodes -key root-ca.key -subj "/C=US/ST=CA/O=FakeRootCanal/CN=mononoke.com" -sha256 -days 365 -out root-ca.crt
+# Generate private key for server/client certificate
+# (could be separate as well, but keep them as one for ease)
+openssl genrsa -out localhost.key 2048
+# Generate certificate signing request for server/client certificate
+openssl req -new -sha256 -key localhost.key -config "$1" -out localhost.csr
+
+# Sign server/client certificate with root certificate
+if grep -q '\[ v3_ca \]' "$1"; then
+  # ...and apply custom v3_ca extensions
+  openssl x509 -extfile "$1" -extensions v3_ca -req -in localhost.csr -CA root-ca.crt -CAkey root-ca.key -CAcreateserial -out localhost.crt -days 365 -sha256
+else
+  openssl x509 -req -in localhost.csr -CA root-ca.crt -CAkey root-ca.key -CAcreateserial -out localhost.crt -days 365 -sha256
+fi
