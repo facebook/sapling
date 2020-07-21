c5e880c239de9c8f67fcee369cd90e9ee150decf
Viet Hung Nguyen <vietnguyen19@fb.com>
2020-07-21 06:45:53 -0700

    mononoke/repo_import: Add bookmark moving functionality
    Summary: After we derived the bonsaichangesets (D22455297 (https://github.com/facebookexperimental/eden/commit/c315c56c05a14044d2b31283165319d675ae6509)), we want to move a bookmark in small increments to reveal the commits to the users (https://fburl.com/wiki/zp9hgd7z step 8). This diff adds this functionality to repo_import and automate this step.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D22598159
    
    fbshipit-source-id: 01db898f07a0b7be50c3f595e78931204f33bb46

--- eden/mononoke/repo_import/src/main.rs
+++ eden/mononoke/repo_import/src/main.rs
@@ -6,8 +6,9 @@
  */
 
 #![type_length_limit = "4522397"]
-use anyhow::Error;
+use anyhow::{format_err, Error};
 use blobrepo::{save_bonsai_changesets, BlobRepo};
+use bookmarks::{BookmarkName, BookmarkUpdateReason};
 use clap::Arg;
 use cmdlib::args;
 use cmdlib::helpers::block_execute;
@@ -26,10 +27,14 @@ use mononoke_types::{BonsaiChangeset, ChangesetId};
 use movers::DefaultAction;
 use slog::info;
 use std::collections::HashMap;
+use std::num::NonZeroUsize;
 use std::path::Path;
+use topo_sort::sort_topological;
 
 const ARG_GIT_REPOSITORY_PATH: &str = "git-repository-path";
-const ARG_DEST_PATH_PREFIX: &str = "destination-path-prefix";
+const ARG_DEST_PATH: &str = "dest-path";
+const ARG_BATCH_SIZE: &str = "batch-size";
+const ARG_BOOKMARK_SUFFIX: &str = "bookmark-suffix";
 
 async fn rewrite_file_paths(
     ctx: &CoreContext,
@@ -106,6 +111,95 @@ async fn derive_bonsais(
         .await
 }
 
+async fn move_bookmark(
+    ctx: &CoreContext,
+    repo: &BlobRepo,
+    shifted_bcs: &[BonsaiChangeset],
+    batch_size: usize,
+    bookmark_suffix: &str,
+) -> Result<(), Error> {
+    if shifted_bcs.is_empty() {
+        return Err(format_err!("There is no bonsai changeset present"));
+    }
+
+    let bookmark = BookmarkName::new(format!("repo_import_{}", bookmark_suffix))?;
+    let first_bcs = match shifted_bcs.first() {
+        Some(first) => first,
+        None => {
+            return Err(format_err!("There is no bonsai changeset present"));
+        }
+    };
+    let mut old_csid = first_bcs.get_changeset_id();
+    let mut transaction = repo.update_bookmark_transaction(ctx.clone());
+    transaction.create(&bookmark, old_csid, BookmarkUpdateReason::ManualMove, None)?;
+    if !transaction.commit().await? {
+        return Err(format_err!("Logical failure while creating {:?}", bookmark));
+    }
+    info!(
+        ctx.logger(),
+        "Created bookmark {:?} pointing to {}", bookmark, old_csid
+    );
+    for chunk in shifted_bcs.chunks(batch_size) {
+        transaction = repo.update_bookmark_transaction(ctx.clone());
+        let curr_csid = match chunk.last() {
+            Some(bcs) => bcs.get_changeset_id(),
+            None => {
+                return Err(format_err!("There is no bonsai changeset present"));
+            }
+        };
+        transaction.update(
+            &bookmark,
+            curr_csid,
+            old_csid,
+            BookmarkUpdateReason::ManualMove,
+            None,
+        )?;
+
+        if !transaction.commit().await? {
+            return Err(format_err!("Logical failure while setting {:?}", bookmark));
+        }
+        info!(
+            ctx.logger(),
+            "Set bookmark {:?} to point to {:?}", bookmark, curr_csid
+        );
+        old_csid = curr_csid;
+    }
+    Ok(())
+}
+
+fn is_valid_bookmark_suffix(bookmark_suffix: &str) -> bool {
+    let spec_chars = "./-_";
+    bookmark_suffix
+        .chars()
+        .all(|c| c.is_alphanumeric() || spec_chars.contains(c))
+}
+
+fn sort_bcs(shifted_bcs: &[BonsaiChangeset]) -> Result<Vec<BonsaiChangeset>, Error> {
+    let mut bcs_parents = HashMap::new();
+    let mut id_bcs = HashMap::new();
+    for bcs in shifted_bcs {
+        let parents: Vec<_> = bcs.parents().collect();
+        let bcs_id = bcs.get_changeset_id();
+        bcs_parents.insert(bcs_id, parents);
+        id_bcs.insert(bcs_id, bcs);
+    }
+
+    let sorted_commits = sort_topological(&bcs_parents).expect("loop in commit chain!");
+    let mut sorted_bcs: Vec<BonsaiChangeset> = vec![];
+    for csid in sorted_commits {
+        match id_bcs.get(&csid) {
+            Some(&bcs) => sorted_bcs.push(bcs.clone()),
+            _ => {
+                return Err(format_err!(
+                    "Could not find mapping for changeset id {}",
+                    csid
+                ))
+            }
+        }
+    }
+    Ok(sorted_bcs)
+}
+
 #[fbinit::main]
 fn main(fb: FacebookInit) -> Result<(), Error> {
     let app = args::MononokeApp::new("Import Repository")
@@ -119,17 +213,41 @@ fn main(fb: FacebookInit) -> Result<(), Error> {
                 .help("Path to a git repository to import"),
         )
         .arg(
-            Arg::with_name(ARG_DEST_PATH_PREFIX)
-                .long(ARG_DEST_PATH_PREFIX)
+            Arg::with_name(ARG_DEST_PATH)
+                .long(ARG_DEST_PATH)
                 .required(true)
                 .takes_value(true)
-                .help("Prefix of the destination folder we import to"),
+                .help("Path to the destination folder we import to"),
+        )
+        .arg(
+            Arg::with_name(ARG_BATCH_SIZE)
+                .long(ARG_BATCH_SIZE)
+                .takes_value(true)
+                .default_value("100")
+                .help("Number of commits we make visible when moving the bookmark"),
+        )
+        .arg(
+            Arg::with_name(ARG_BOOKMARK_SUFFIX)
+                .long(ARG_BOOKMARK_SUFFIX)
+                .required(true)
+                .takes_value(true)
+                .help("Suffix of the bookmark (repo_import_<suffix>)"),
         );
 
     let matches = app.get_matches();
 
     let path = Path::new(matches.value_of(ARG_GIT_REPOSITORY_PATH).unwrap());
-    let prefix = matches.value_of(ARG_DEST_PATH_PREFIX).unwrap();
+    let prefix = matches.value_of(ARG_DEST_PATH).unwrap();
+    let bookmark_suffix = matches.value_of(ARG_BOOKMARK_SUFFIX).unwrap();
+    let batch_size = matches.value_of(ARG_BATCH_SIZE).unwrap();
+    let batch_size = batch_size.parse::<NonZeroUsize>()?;
+    if !is_valid_bookmark_suffix(&bookmark_suffix) {
+        return Err(format_err!(
+            "The bookmark suffix contains invalid character(s).
+            You can only use alphanumeric and \"./-_\" characters"
+        ));
+    }
+
     args::init_cachelib(fb, &matches, None);
 
     let logger = args::init_logging(fb, &matches);
@@ -138,8 +256,17 @@ fn main(fb: FacebookInit) -> Result<(), Error> {
     block_execute(
         async {
             let repo = repo.compat().await?;
-            let shifted_bcs = rewrite_file_paths(&ctx, &repo, &path, &prefix).await?;
-            derive_bonsais(&ctx, &repo, &shifted_bcs).await
+            let mut shifted_bcs = rewrite_file_paths(&ctx, &repo, &path, &prefix).await?;
+            shifted_bcs = sort_bcs(&shifted_bcs)?;
+            derive_bonsais(&ctx, &repo, &shifted_bcs).await?;
+            move_bookmark(
+                &ctx,
+                &repo,
+                &shifted_bcs,
+                batch_size.get(),
+                &bookmark_suffix,
+            )
+            .await
         },
         fb,
         "repo_import",
@@ -147,4 +274,64 @@ fn main(fb: FacebookInit) -> Result<(), Error> {
         &matches,
         cmdlib::monitoring::AliveService,
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::move_bookmark;
+    use crate::sort_bcs;
+
+    use anyhow::Result;
+    use blobstore::Loadable;
+    use bookmarks::{BookmarkName, BookmarkUpdateLog, BookmarkUpdateReason, Freshness};
+    use context::CoreContext;
+    use fbinit::FacebookInit;
+    use futures::stream::TryStreamExt;
+    use tests_utils::drawdag::create_from_dag;
+
+    #[fbinit::compat_test]
+    async fn move_bookmark_test(fb: FacebookInit) -> Result<()> {
+        let ctx = CoreContext::test_mock(fb);
+        let blob_repo = blobrepo_factory::new_memblob_empty(None)?;
+        let batch_size: usize = 2;
+        let changesets = create_from_dag(
+            &ctx,
+            &blob_repo,
+            r##"
+                A-B-C-D-E-F-G
+            "##,
+        )
+        .await?;
+        let mut bonsais = vec![];
+        for (_, csid) in &changesets {
+            bonsais.push(csid.load(ctx.clone(), &blob_repo.get_blobstore()).await?);
+        }
+        bonsais = sort_bcs(&bonsais)?;
+        move_bookmark(&ctx, &blob_repo, &bonsais, batch_size, "test_repo").await?;
+        // Check the bookmark moves created BookmarkLogUpdate entries
+        let entries = blob_repo
+            .attribute_expected::<dyn BookmarkUpdateLog>()
+            .list_bookmark_log_entries(
+                ctx.clone(),
+                BookmarkName::new("repo_import_test_repo")?,
+                5,
+                None,
+                Freshness::MostRecent,
+            )
+            .map_ok(|(cs, rs, _ts)| (cs, rs)) // dropping timestamps
+            .try_collect::<Vec<_>>()
+            .await?;
+
+        assert_eq!(
+            entries,
+            vec![
+                (Some(changesets["G"]), BookmarkUpdateReason::ManualMove),
+                (Some(changesets["F"]), BookmarkUpdateReason::ManualMove),
+                (Some(changesets["D"]), BookmarkUpdateReason::ManualMove),
+                (Some(changesets["B"]), BookmarkUpdateReason::ManualMove),
+                (Some(changesets["A"]), BookmarkUpdateReason::ManualMove),
+            ]
+        );
+        Ok(())
+    }
 }
--- eden/mononoke/tests/integration/test-repo_import.t
+++ eden/mononoke/tests/integration/test-repo_import.t
@@ -35,38 +35,35 @@
 
 # Import it into Mononoke
   $ cd "$TESTTMP"
-  $ repo_import "$GIT_REPO" --destination-path-prefix "new_repo"
+  $ repo_import "$GIT_REPO" --dest-path "new_dir/new_repo" --batch-size 3 --bookmark-suffix "new_repo"
   * using repo "repo" repoid RepositoryId(0) (glob)
   * Created ce435b03d4ef526648f8654c61e26ae5cc1069cc => ChangesetId(Blake2(f7cbf75d9c08ff96896ed2cebd0327aa514e58b1dd9901d50129b9e08f4aa062)) (glob)
   * Created 2c01e4a5658421e2bfcd08e31d9b69399319bcd3 => ChangesetId(Blake2(f7708ed066b1c23591f862148e0386ec704a450e572154cc52f87ca0e394a0fb)) (glob)
   * 2 bonsai changesets have been committed (glob)
   * Ref: Some("refs/heads/master"): Some(ChangesetId(Blake2(f7708ed066b1c23591f862148e0386ec704a450e572154cc52f87ca0e394a0fb))) (glob)
-  * Remapped ChangesetId(Blake2(f7cbf75d9c08ff96896ed2cebd0327aa514e58b1dd9901d50129b9e08f4aa062)) => ChangesetId(Blake2(4c9a9394cb65d5b57286d866bb012a0a4553ea05ba82755c0ed9e977e51d0da0)) (glob)
-  * Remapped ChangesetId(Blake2(f7708ed066b1c23591f862148e0386ec704a450e572154cc52f87ca0e394a0fb)) => ChangesetId(Blake2(cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f)) (glob)
-
-# Set master (repo_import does not do this yet)
-  $ mononoke_admin bookmarks set master cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
-  * using repo "repo" repoid RepositoryId(0) (glob)
-  * changeset resolved as: ChangesetId(Blake2(cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f)) (glob)
-  * Current position of BookmarkName { bookmark: "master" } is None (glob)
+  * Remapped ChangesetId(Blake2(f7cbf75d9c08ff96896ed2cebd0327aa514e58b1dd9901d50129b9e08f4aa062)) => ChangesetId(Blake2(a159bc614d2dbd07a5ecc6476156fa464b69e884d819bbc2e854ade3e4c353b9)) (glob)
+  * Remapped ChangesetId(Blake2(f7708ed066b1c23591f862148e0386ec704a450e572154cc52f87ca0e394a0fb)) => ChangesetId(Blake2(a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5)) (glob)
+  * Created bookmark BookmarkName { bookmark: "repo_import_new_repo" } pointing to * (glob)
+  * Set bookmark BookmarkName { bookmark: "repo_import_new_repo" } to * (glob)
 
 # Check if we derived all the types
-  $ mononoke_admin derived-data exists changeset_info master 2> /dev/null
-  Derived: cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
-  $ mononoke_admin derived-data exists blame master 2> /dev/null
-  Derived: cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
-  $ mononoke_admin derived-data exists deleted_manifest master 2> /dev/null
-  Derived: cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
-  $ mononoke_admin derived-data exists fastlog master 2> /dev/null
-  Derived: cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
-  $ mononoke_admin derived-data exists filenodes master 2> /dev/null
-  Derived: cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
-  $ mononoke_admin derived-data exists fsnodes master 2> /dev/null
-  Derived: cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
-  $ mononoke_admin derived-data exists hgchangesets master 2> /dev/null
-  Derived: cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
-  $ mononoke_admin derived-data exists unodes master 2> /dev/null
-  Derived: cf29a57c1ba299f835ea2546e26c8eb8fd5b981067162579924b58000306e96f
+  $ BOOKMARK_NAME="repo_import_new_repo"
+  $ mononoke_admin derived-data exists changeset_info $BOOKMARK_NAME 2> /dev/null
+  Derived: a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5
+  $ mononoke_admin derived-data exists blame $BOOKMARK_NAME 2> /dev/null
+  Derived: a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5
+  $ mononoke_admin derived-data exists deleted_manifest $BOOKMARK_NAME 2> /dev/null
+  Derived: a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5
+  $ mononoke_admin derived-data exists fastlog $BOOKMARK_NAME 2> /dev/null
+  Derived: a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5
+  $ mononoke_admin derived-data exists filenodes $BOOKMARK_NAME 2> /dev/null
+  Derived: a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5
+  $ mononoke_admin derived-data exists fsnodes $BOOKMARK_NAME 2> /dev/null
+  Derived: a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5
+  $ mononoke_admin derived-data exists hgchangesets $BOOKMARK_NAME 2> /dev/null
+  Derived: a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5
+  $ mononoke_admin derived-data exists unodes $BOOKMARK_NAME 2> /dev/null
+  Derived: a2e6329ed60e3dd304f53efd0f92c28b849404a47979fcf48bb43b6fe3a0cad5
 
 # Start Mononoke
   $ mononoke
@@ -76,9 +73,9 @@
   $ cd "$TESTTMP"
   $ hgmn_clone 'ssh://user@dummy/repo' "$HG_REPO"
   $ cd "$HG_REPO"
-  $ cat "new_repo/file1"
+  $ cat "new_dir/new_repo/file1"
   this is file1
-  $ cat "new_repo/file2_repo/file2"
+  $ cat "new_dir/new_repo/file2_repo/file2"
   this is file2
-  $ cat "new_repo/file3_repo/file3"
+  $ cat "new_dir/new_repo/file3_repo/file3"
   this is file3
