db06969e0da03c6bf8de234f2f8c25fa83da1a06
Lukas Piatkowski <luk@fb.com>
2020-07-27 14:32:57 -0700

    mononoke/integration tests: create exclusion list of integration tests
    Summary: Have a comprehensive list of OSS tests that do not pass yet.
    
    Reviewed By: krallin
    
    Differential Revision: D22762196
    
    fbshipit-source-id: 19ab920c4c143179db65a6d8ee32974db16c5e3d

--- eden/mononoke/tests/integration/run_tests_getdeps.py
+++ eden/mononoke/tests/integration/run_tests_getdeps.py
@@ -9,7 +9,9 @@ import json
 import os
 import subprocess
 import sys
-from os.path import abspath, dirname, join
+from glob import iglob
+from os.path import abspath, basename, dirname, join
+from pathlib import Path
 
 
 parser = argparse.ArgumentParser(
@@ -21,6 +23,15 @@ parser.add_argument(
 )
 parser.add_argument(
     "build_dir", help="Location where to put generated manifest.json file"
+)
+parser.add_argument(
+    "tests", nargs="*", help="Optional list of tests to run. Run all if none provided"
+)
+parser.add_argument(
+    "-r",
+    "--rerun-failed",
+    action="store_true",
+    help="Rerun failed tests based on '.testfailed' file",
 )
 args = parser.parse_args()
 
@@ -45,7 +56,134 @@ os.makedirs(build_dir, exist_ok=True)
 with open(join(build_dir, "manifest.json"), "w") as f:
     f.write(json.dumps(MANIFEST_DEPS, sort_keys=True, indent=4))
 
-tests = ["test-init.t"]
+if args.tests or args.rerun_failed:
+    tests = {basename(p) for p in args.tests or []}
+
+    if args.rerun_failed:
+        # Based on eden/scm/tests/run-tests.py
+        for title in ("failed", "errored"):
+            failed = Path(repo_root) / "eden/mononoke/tests/integration/.test{}".format(
+                title
+            )
+            if failed.is_file():
+                tests.update(t for t in failed.read_text().splitlines() if t)
+
+    tests = list(tests)
+else:
+    excluded_tests = {
+        "test-alias-verify.t",  # "wc -l" problem
+        "test-backfill-derived-data.t",  # "wc -l" problem
+        "test-backfill-git-mapping.t",  # "${ENV^^}" problem
+        "test-backsync-forever.t",  # Unknown issue
+        "test-backsyncer-merges.t",  # Missing BACKSYNCER
+        "test-blobimport-derived-data.t",  # "|&" problem
+        "test-blobimport-fix-parent-order.t",  # Missing jq
+        "test-blobimport-inline.t",  # Illegal byte sequence and "sed -i"
+        "test-blobimport-lfs.t",  # Timed out
+        "test-blobimport.t",  # Case insensitivity of paths in MacOS
+        "test-blobstore_healer.t",  # PANIC not implemented in sql_ext
+        "test-bookmark-hg-kind.t",  # sqlite issue with LIKE on BLOB
+        "test-bookmarks-filler.t",  # Probably missing binary
+        "test-bundle2-preservation.t",  # "wc -l" problem
+        "test-cmd-manual-scrub.t",  # Just wrong outout
+        "test-cross-repo-commit-sync-live.t",  # Unknown issue
+        "test-cross-repo-commit-sync-merge.t",  # requires FB-specific command
+        "test-cross-repo-commit-sync.t",  # requires FB-specific command
+        "test-cross-repo-commit-validator.t",  # requires FB-specific command
+        "test-edenapi-server-complete-trees.t",  # Missing eden/scm's commands
+        "test-edenapi-server-files.t",  # Missing eden/scm's commands
+        "test-edenapi-server-history.t",  # Missing eden/scm's commands
+        "test-edenapi-server-trees.t",  # Missing eden/scm's commands
+        "test-fastreplay-inline-args.t",  # Returns different data in OSS
+        "test-fastreplay-remote-args.t",  # "grep -A" problem
+        "test-gettreepack-designated-nodes.t",  # "wc -l" problem
+        "test-gettreepack.t",  # "wc -l" problem
+        "test-gitimport-octopus.t",  # Missing MONONOKE_REGENERATE_HG_FILENODES
+        "test-gitimport.t",  # Issue with hggit extension
+        "test-hook-tailer.t",  # Issue with hggit extension
+        "test-hooks.t",  # Hooks are not in OSS yet
+        "test-inconsistent-hash.t",  # Illegal byte sequence and "sed -i"
+        "test-infinitepush-hydrated.t",  # "|&" problem
+        "test-infinitepush-lfs.t",  # Timed out
+        "test-infinitepush-vs-push.t",  # Missing jq
+        "test-large-path-and-content.t",  # Complex bash issues
+        "test-lfs-copytracing.t",  # Timed out
+        "test-lfs-server-acl-check.t",  # Timed out
+        "test-lfs-server-consistent-hashing.t",  # Timed out
+        "test-lfs-server-disabled-hostname-resolution.t",  # Timed out
+        "test-lfs-server-error-formatting.t",  # Missing jq
+        "test-lfs-server-identity-parsing-from-header.t",  # Timed out
+        "test-lfs-server-identity-parsing-untrusted.t",  # Timed out
+        "test-lfs-server-identity-parsing.t",  # Timed out
+        "test-lfs-server-live-config.t",  # Missing jq
+        "test-lfs-server-max-upload-size.t",  # Timed out
+        "test-lfs-server-proxy-skip-upstream.t",  # Complex bash issues
+        "test-lfs-server-proxy-sync.t",  # Timed out
+        "test-lfs-server-proxy.t",  # Timed out
+        "test-lfs-server-rate-limiting.t",  # Timed out
+        "test-lfs-server-scuba-logging.t",  # Timed out
+        "test-lfs-server.t",  # Timed out
+        "test-lfs-to-mononoke.t",  # Timed out
+        "test-lfs-upload-alias-on-fetch.t",  # "wc -l" problem
+        "test-lfs-wantslfspointers.t",  # Timed out
+        "test-lfs.t",  # Timed out
+        "test-megarepo-tool.t",  # Missing MONONOKE_HG_SYNC
+        "test-mononoke-admin.t",  # Missing MEGAREPO_TOOL
+        "test-mononoke-hg-sync-job-generate-bundles-force.t",  # Missing MONONOKE_HG_SYNC
+        "test-mononoke-hg-sync-job-generate-bundles-lfs-verification.t",  # Timed out
+        "test-mononoke-hg-sync-job-generate-bundles-lfs.t",  # Timed out
+        "test-mononoke-hg-sync-job-generate-bundles-loop.t",  # Missing MONONOKE_HG_SYNC
+        "test-mononoke-hg-sync-job-generate-bundles-other-books.t",  # Missing MONONOKE_HG_SYNC
+        "test-mononoke-hg-sync-job-generate-bundles.t",  # Missing MONONOKE_HG_SYNC
+        "test-mononoke-hg-sync-job-sync-globalrevs.t",  # Missing MONONOKE_HG_SYNC
+        "test-mononoke-hg-sync-job-with-copies.t",  # Missing MONONOKE_HG_SYNC
+        "test-mononoke-hg-sync-job.t",  # Missing MONONOKE_HG_SYNC
+        "test-multiplexed-chaosblob.t",  # "wc -l" problem
+        "test-multiplexed-packblob.t",  # "du --bytes" problem
+        "test-post-push-logging.t",  # Missing jq
+        "test-push-protocol-lfs.t",  # Timed out
+        "test-push-redirector-pushrebase-hooks.t",  # Hooks are not in OSS yet
+        "test-push-redirector-pushrebase-onesided.t",  # Missing MONONOKE_X_REPO_SYNC
+        "test-push-redirector-pushrebase.t",  # "[[ -v ... ]]" in library-push-redirector.sh and "ls" problem
+        "test-push-redirector-sync-job.t",  # Missing BACKSYNCER
+        "test-pushrebase-block-casefolding.t",  # Most likely MacOS path case insensitivity
+        "test-pushrebase-discovery.t",  # Hooks are not in OSS yet
+        "test-rechunker.t",  # "wc -l" problem
+        "test-remotefilelog-lfs.t",  # Timed out
+        "test-scs-blame.t",  # Missing SCS_SERVER
+        "test-scs-common-base.t",  # Missing SCS_SERVER
+        "test-scs-diff.t",  # Missing SCS_SERVER
+        "test-scs-list-bookmarks.t",  # Missing SCS_SERVER
+        "test-scs-log.t",  # Missing SCS_SERVER
+        "test-scs-lookup.t",  # Missing SCS_SERVER
+        "test-scs-x-repo.t",  # Missing SCS_SERVER
+        "test-scs.t",  # Missing SCS_SERVER
+        "test-server.t",  # Returns different data in OSS
+        "test-traffic-replay.t",  # Missing TRAFFIC_REPLAY
+        "test-unbundle-replay-hg-bookmark.t",  # "kill" printing problem
+        "test-unbundle-replay-hg-recording.t",  # Returns different data in OSS
+        "test-walker-compression-benefit.t",  # strip_glog problem
+        "test-walker-corpus.t",  # "du --bytes" problem
+        "test-walker-count-objects.t",  # strip_glog problem
+        "test-walker-enable-derive.t",  # strip_glog problem
+        "test-walker-error-as-data.t",  # "sed -r" problem
+        "test-walker-sample-by-node.t",  # strip_glog problem
+        "test-walker-sample-by-path.t",  # strip_glog problem
+        "test-walker-scrub-blobstore.t",  # "sed -r" problem
+        "test-walker-scrub-queue.t",  # "sed -r" problem
+        "test-walker-throttle.t",  # "date" and arithmetic problem
+        "test-walker-validate-non-public.t",  # strip_glog problem
+        "test-walker-validate.t",  # strip_glog problem
+    }
+
+    tests = [
+        t
+        for t in (
+            basename(p)
+            for p in iglob(join(repo_root, "eden/mononoke/tests/integration/*.t"))
+        )
+        if t not in excluded_tests
+    ]
 
 env = dict(os.environ.items())
 env["NO_LOCAL_PATHS"] = "1"
@@ -53,16 +191,18 @@ eden_scm_packages = join(install_dir, "eden_scm/lib/python2.7/site-packages")
 pythonpath = env.get("PYTHONPATH")
 env["PYTHONPATH"] = eden_scm_packages + (":{}".format(pythonpath) if pythonpath else "")
 
-sys.exit(
-    subprocess.run(
-        [
-            sys.executable,
-            join(
-                repo_root, "eden/mononoke/tests/integration/integration_runner_real.py"
-            ),
-            join(build_dir, "manifest.json"),
-        ]
-        + tests,
-        env=env,
-    ).returncode
-)
+if tests:
+    sys.exit(
+        subprocess.run(
+            [
+                sys.executable,
+                join(
+                    repo_root,
+                    "eden/mononoke/tests/integration/integration_runner_real.py",
+                ),
+                join(build_dir, "manifest.json"),
+            ]
+            + tests,
+            env=env,
+        ).returncode
+    )
