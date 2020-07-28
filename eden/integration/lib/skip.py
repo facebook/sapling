29e8b1ee18109c6df7ea60e59026c08888f9f3fd
Xavier Deguillard <xavierd@fb.com>
2020-07-28 16:40:52 -0700

    integration: enable sparse_test on Windows
    Summary: The test is passing, enable it.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D22798424
    
    fbshipit-source-id: 76b99457aacf5a81c2b9b3ebaedd0e6e1cf2a1e8

--- eden/integration/lib/skip.py
+++ eden/integration/lib/skip.py
@@ -131,7 +131,6 @@ if sys.platform == "win32":
             "test_rm_directory_with_modification",
             "test_rm_modified_file_permissions",
         ],
-        "hg.sparse_test.SparseTestTreeOnly": True,
         "hg.split_test.SplitTestTreeOnly": True,
         "hg.status_deadlock_test.StatusDeadlockTestTreeOnly": True,
         "hg.status_test.StatusTestTreeOnly": [
