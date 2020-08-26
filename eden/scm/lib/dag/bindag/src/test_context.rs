89570e223a6db90a8881db30011af0f0a223e349
Jun Wu <quark@fb.com>
2020-08-26 15:26:49 -0700

    dag: use non-master group in fuzz test
    Summary:
    Currently the fuzz test only uses the master group. Let it exercise non-master
    group too.
    
    Reviewed By: DurhamG
    
    Differential Revision: D23214388
    
    fbshipit-source-id: 7108a1055fbdda2b012f93c5948fb83ef3b9a96f

--- eden/scm/lib/dag/bindag/src/test_context.rs
+++ eden/scm/lib/dag/bindag/src/test_context.rs
@@ -6,8 +6,11 @@
  */
 
 use crate::{parse_bindag, ParentRevs};
+use dag::ops::DagAlgorithm;
+use dag::ops::IdConvert;
 use dag::OnDiskIdDag;
 use dag::{namedag::LowLevelAccess, ops::DagPersistent, spanset::SpanSet, Id, NameDag, VertexName};
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::ops::Range;
 use tempfile::TempDir;
@@ -21,8 +24,11 @@ pub struct GeneralTestContext<T> {
     /// Complex DAG, with ids re-assigned
     pub dag: NameDag,
 
-    /// Simple IdMap. NameDag id -> Plain DAG id
-    pub idmap: Vec<usize>,
+    /// Simple IdMap. NameDag Id -> Plain DAG id
+    pub idmap: HashMap<Id, usize>,
+
+    /// Plain DAG id -> NameDag Id.
+    pub rev_idmap: Vec<Id>,
 
     /// Temporary dir containing the NameDag daga.
     pub dir: TempDir,
@@ -58,36 +64,52 @@ impl<T: AsRef<[usize]>> GeneralTestContext<T> {
                 .map(|p| format!("{}", p).as_bytes().to_vec().into())
                 .collect())
         };
-        let mut heads: HashSet<usize> = (0..parents.len()).collect();
-        for ps in &parents {
-            for p in ps.as_ref().iter() {
-                heads.remove(&p);
+        // Pick heads from 0..n
+        let get_heads = |n: usize| -> Vec<VertexName> {
+            let mut heads: HashSet<usize> = (0..n).collect();
+            for ps in parents.iter().take(n) {
+                for p in ps.as_ref().iter() {
+                    heads.remove(&p);
+                }
             }
-        }
-        let mut head_names: Vec<VertexName> = Vec::new();
-        for h in heads {
-            head_names.push(VertexName::copy_from(format!("{}", h).as_bytes()));
-        }
+            let mut names: Vec<VertexName> = Vec::new();
+            for h in heads {
+                names.push(VertexName::copy_from(format!("{}", h).as_bytes()));
+            }
+            names
+        };
+        let head_names: Vec<VertexName> = get_heads(parents.len());
+        let master_names: Vec<VertexName> = get_heads(parents.len() / 2);
 
         let dir = tempfile::tempdir().unwrap();
         let mut dag = NameDag::open(dir.path()).unwrap();
-        dag.add_heads_and_flush(&parents_by_name, &head_names, &[])
+        dag.add_heads_and_flush(&parents_by_name, &master_names, &head_names)
             .unwrap();
 
         // Prepare idmap
-        let idmap = (0..parents.len())
-            .map(|i| {
-                std::str::from_utf8(dag.map().find_name_by_id(Id(i as u64)).unwrap().unwrap())
-                    .unwrap()
-                    .parse()
-                    .unwrap()
+        let idmap: HashMap<Id, usize> = dag
+            .all()
+            .unwrap()
+            .iter()
+            .unwrap()
+            .map(|name| {
+                let name = name.unwrap();
+                let dag_id: Id = dag.vertex_id(name.clone()).unwrap();
+                let plain_id: usize = std::str::from_utf8(name.as_ref()).unwrap().parse().unwrap();
+                (dag_id, plain_id)
             })
             .collect();
+        let mut rev_idmap = vec![Id(0); idmap.len()];
+        for (&k, &v) in idmap.iter() {
+            rev_idmap[v] = k;
+        }
+        assert_eq!(rev_idmap.len(), parents.len());
 
         Self {
             parents,
             dag,
             idmap,
+            rev_idmap,
             dir,
         }
     }
@@ -104,7 +126,7 @@ impl<T> GeneralTestContext<T> {
 
     /// Convert a SpanSet (used by IdDag) to plain revs (used by `parents`).
     pub fn to_plain_revs(&self, set: &SpanSet) -> Vec<usize> {
-        set.iter().map(|i| self.idmap[i.0 as usize]).collect()
+        set.iter().map(|i| self.idmap[&i]).collect()
     }
 
     /// Get the IdDag reference.
@@ -118,5 +140,11 @@ impl<T> GeneralTestContext<T> {
             .cloned()
             .map(|i| Into::<usize>::into(i) % self.parents.len())
             .collect()
+    }
+
+    /// Convert `usize` plain revs to SpanSet used by IdDag.
+    pub fn to_dag_revs(&self, revs: &[usize]) -> SpanSet {
+        let iter = revs.iter().map(|&i| self.rev_idmap[i].clone());
+        SpanSet::from_spans(iter)
     }
 }
--- eden/scm/lib/dag/fuzz/fuzz_targets/tests.rs
+++ eden/scm/lib/dag/fuzz/fuzz_targets/tests.rs
@@ -6,44 +6,46 @@
  */
 
 use bindag::GeneralTestContext;
-use dag::{spanset::SpanSet, Id};
 
 #[allow(dead_code)]
-pub fn test_gca<T: AsRef<[usize]>>(context: &GeneralTestContext<T>, namedag_revs: Vec<usize>) {
-    let namedag_revs = SpanSet::from_spans(namedag_revs.iter().map(|&i| Id(i as u64)));
-    let plain_revs = context.to_plain_revs(&namedag_revs);
+pub fn test_gca<T: AsRef<[usize]>>(context: &GeneralTestContext<T>, mut plain_revs: Vec<usize>) {
+    plain_revs.sort_unstable();
+    plain_revs.dedup();
+    let iddag_revs = context.to_dag_revs(&plain_revs);
     let mut plain_gca = bindag::gca(&context.parents, &plain_revs);
-    let mut namedag_gca = context.to_plain_revs(&context.id_dag().gca_all(namedag_revs).unwrap());
+    let mut iddag_gca = context.to_plain_revs(&context.id_dag().gca_all(iddag_revs).unwrap());
 
     plain_gca.sort_unstable();
-    namedag_gca.sort_unstable();
-    assert_eq!(plain_gca, namedag_gca, "gca({:?})", &plain_revs);
+    iddag_gca.sort_unstable();
+    assert_eq!(plain_gca, iddag_gca, "gca({:?})", &plain_revs);
 }
 
 #[allow(dead_code)]
 pub fn test_range<T: AsRef<[usize]>>(
     context: &GeneralTestContext<T>,
-    roots: Vec<usize>,
-    heads: Vec<usize>,
+    plain_roots: Vec<usize>,
+    plain_heads: Vec<usize>,
 ) {
-    let plain_roots: Vec<_> = roots.iter().map(|&i| context.idmap[i]).collect();
-    let plain_heads: Vec<_> = heads.iter().map(|&i| context.idmap[i]).collect();
+    let plain_roots = context.clamp_revs(&plain_roots);
+    let plain_heads = context.clamp_revs(&plain_heads);
+    // let plain_roots: Vec<_> = roots.iter().map(|i| context.idmap[i]).collect();
+    // let plain_heads: Vec<_> = heads.iter().map(|i| context.idmap[i]).collect();
 
     let mut plain_range = bindag::range(&context.parents, &plain_roots, &plain_heads);
     plain_range.sort_unstable();
 
-    let namedag_roots = SpanSet::from_spans(roots.iter().map(|&i| Id(i as u64)));
-    let namedag_heads = SpanSet::from_spans(heads.iter().map(|&i| Id(i as u64)));
-    let mut namedag_range = context.to_plain_revs(
+    let iddag_roots = context.to_dag_revs(&plain_roots);
+    let iddag_heads = context.to_dag_revs(&plain_heads);
+    let mut iddag_range = context.to_plain_revs(
         &context
             .id_dag()
-            .range(namedag_roots, namedag_heads)
+            .range(iddag_roots, iddag_heads)
             .expect("IdDag::range"),
     );
 
-    namedag_range.sort_unstable();
+    iddag_range.sort_unstable();
     assert_eq!(
-        plain_range, namedag_range,
+        plain_range, iddag_range,
         "range({:?}::{:?})",
         &plain_roots, &plain_heads
     );
