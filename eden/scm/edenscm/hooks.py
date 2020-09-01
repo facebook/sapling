6e2a90ddb5bf34ffc3b4eba9ea6f89b9c062aefd
Jun Wu <quark@fb.com>
2020-08-31 17:32:44 -0700

    hooks: add predefined hook to run fsync
    Reviewed By: DurhamG
    
    Differential Revision: D22993217
    
    fbshipit-source-id: 2cfb6b26479cd7dad02419fb76fa5d3ca5dd66db

---
+++ eden/scm/edenscm/hooks.py
@@ -0,0 +1,21 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+"""predefined hooks"""
+
+from .mercurial import util
+
+
+def backgroundfsync(ui, repo, hooktype, **kwargs):
+    """run fsync in background
+
+    Example config::
+
+        [hooks]
+        postwritecommand.fsync = python:edenscm.hooks.backgroundfsync
+    """
+    if not repo:
+        return
+    util.spawndeetached(util.gethgcmd() + ["debugfsync"], cwd=repo.svfs.join(""))
