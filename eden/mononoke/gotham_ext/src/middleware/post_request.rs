cc0f2e4c40b4fb924907316e4866652b5e25e75e
Arun Kulshreshtha <kulshrax@fb.com>
2020-09-03 11:56:33 -0700

    gotham_ext: add PostRequestMiddleware
    Summary: Add new middleware that allows HTTP handlers and other middleware to register callbacks that will be run once the current request completes. This is heavily based on the post-request callback functionality from the LFS server's `RequestContext`. The intention here is to expose this functionality in a manner that's independent of other, application-specific logic.
    
    Reviewed By: krallin
    
    Differential Revision: D23298419
    
    fbshipit-source-id: e4b1534b02c35f685ce544de13e331947e187818

---
+++ eden/mononoke/gotham_ext/src/middleware/post_request.rs
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+use std::time::{Duration, Instant};
+
+use async_trait::async_trait;
+use futures::{
+    channel::oneshot::{self, Receiver, Sender},
+    prelude::*,
+};
+use gotham::state::{FromState, State};
+use gotham_derive::StateData;
+use hyper::{body::Body, Response};
+use tokio::task;
+
+use crate::response::ResponseContentLength;
+
+use super::{ClientIdentity, Middleware, RequestStartTime};
+
+type Callback = Box<dyn FnOnce(&PostRequestInfo) + Send + 'static>;
+
+/// Information passed to each post-request callback.
+pub struct PostRequestInfo {
+    pub duration: Option<Duration>,
+    pub bytes_sent: Option<u64>,
+    pub client_hostname: Option<String>,
+}
+
+/// Middleware that allows the application to register callbacks which will
+/// be run upon request completion.
+#[derive(Clone)]
+pub struct PostRequestMiddleware;
+
+impl PostRequestMiddleware {
+    pub fn new() -> Self {
+        PostRequestMiddleware
+    }
+}
+
+#[async_trait]
+impl Middleware for PostRequestMiddleware {
+    async fn inbound(&self, state: &mut State) -> Option<Response<Body>> {
+        state.put(PostRequestCallbacks::new());
+        None
+    }
+
+    async fn outbound(&self, state: &mut State, _response: &mut Response<Body>) {
+        let start_time = RequestStartTime::try_borrow_from(&state).map(|t| t.0);
+        let content_length = ResponseContentLength::try_borrow_from(&state).map(|l| l.0);
+        let hostname_future = ClientIdentity::try_borrow_from(&state).map(|id| id.hostname());
+
+        if let Some(callbacks) = state.try_take::<PostRequestCallbacks>() {
+            task::spawn(callbacks.run(start_time, content_length, hostname_future));
+        }
+    }
+}
+
+/// A collection of callbacks that will run once the request has completed.
+#[derive(StateData)]
+pub struct PostRequestCallbacks {
+    callbacks: Vec<Callback>,
+    delay_signal: Option<Receiver<u64>>,
+    resolve_hostname: bool,
+}
+
+impl PostRequestCallbacks {
+    fn new() -> Self {
+        Self {
+            callbacks: Vec::new(),
+            delay_signal: None,
+            resolve_hostname: true,
+        }
+    }
+
+    /// Add a callback that will be run once the request has completed. This is
+    /// primarily useful for things like logging.
+    ///
+    /// Note that the callbacks are run serially in a task on the Tokio runtime.
+    /// Although these callbacks are not asynchronous, they SHOULD NOT BLOCK as
+    /// this could block the server from handling other requests.
+    pub fn add<F>(&mut self, callback: F)
+    where
+        F: FnOnce(&PostRequestInfo) + Send + 'static,
+    {
+        self.callbacks.push(Box::new(callback));
+    }
+
+    /// Delay execution of post-request callbacks until a value is sent over the
+    /// given channel. This will typically be used in conjunction with something
+    /// like `SignalStream` to delay execution until the entire request body has
+    /// been sent. The value sent over the channel should be the number of bytes
+    /// actually sent to the client (which may differ from the Content-Length).
+    ///
+    /// Note: If this method is called multiple times, only the channel from the
+    /// most recent call will have any effect.
+    pub fn delay(&mut self) -> Sender<u64> {
+        let (sender, receiver) = oneshot::channel();
+        self.delay_signal = Some(receiver);
+        sender
+    }
+
+    /// Enable or disable reverse DNS lookup of the client's hostname.
+    pub fn resolve_hostname(&mut self, enable: bool) {
+        self.resolve_hostname = enable;
+    }
+
+    async fn run<H>(
+        self,
+        start_time: Option<Instant>,
+        content_length: Option<u64>,
+        hostname_future: Option<H>,
+    ) where
+        H: Future<Output = Option<String>> + Send + 'static,
+    {
+        let Self {
+            callbacks,
+            delay_signal,
+            resolve_hostname,
+        } = self;
+
+        // If a delay has been set, wait until the entire response has been
+        // sent before running the callbacks. If the delay channel returns
+        // an error (meaning the sender was dropped), the callbacks will
+        // still run, but the bytes sent will be reported as `None` as this
+        // suggests that the response body may not have been fully sent.
+        let bytes_sent = match delay_signal {
+            Some(rx) => rx.await.ok(),
+            None => content_length,
+        };
+
+        // Capture elapsed time before waiting for the client hostname to resolve.
+        let duration = start_time.map(|start| start.elapsed());
+
+        // Resolve client hostname if enabled.
+        let client_hostname = match hostname_future {
+            Some(hostname) if resolve_hostname => hostname.await,
+            _ => None,
+        };
+
+        let info = PostRequestInfo {
+            duration,
+            bytes_sent,
+            client_hostname,
+        };
+
+        for callback in callbacks {
+            callback(&info);
+        }
+    }
+}
