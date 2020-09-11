f2cd32ea4e8055be1ffd30fdfd3174b4e86c3fbb
Xavier Deguillard <xavierd@fb.com>
2020-09-11 08:35:51 -0700

    win: add PrjfsRequestContext class
    Summary:
    Similarly to FuseRequestContext, this will be allocated whenever ProjectedFS
    calls onto EdenFS to keep track of timing, stats, etc.
    
    For now, this just holds the callback data passed in, we need to copy it as
    ProjectedFS will deallocate it when the EdenFS callback returns, but since we
    intend to complete these asynchronously, the callback data needs to outlive the
    callback, hence the copy. It's likely that this is copying too much, and only
    part of it actually needs to be copied, this will be tackled later.
    
    Reviewed By: wez
    
    Differential Revision: D23505511
    
    fbshipit-source-id: ece00183e3194611d3d63465878470d6e53b790c

---
+++ eden/fs/win/mount/PrjfsRequestContext.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "folly/portability/Windows.h"
+
+#include <ProjectedFSLib.h>
+#include "eden/fs/inodes/RequestContext.h"
+#include "eden/fs/utils/PathFuncs.h"
+#include "eden/fs/win/mount/PrjfsChannel.h"
+
+namespace facebook::eden {
+
+class PrjfsRequestContext : public RequestContext {
+ public:
+  PrjfsRequestContext(const PrjfsRequestContext&) = delete;
+  PrjfsRequestContext& operator=(const PrjfsRequestContext&) = delete;
+  PrjfsRequestContext(PrjfsRequestContext&&) = delete;
+  PrjfsRequestContext& operator=(PrjfsRequestContext&&) = delete;
+
+  explicit PrjfsRequestContext(
+      PrjfsChannel* channel,
+      const PRJ_CALLBACK_DATA& prjfsData)
+      : RequestContext(channel->getProcessAccessLog()),
+        channel_(channel),
+        commandId_(prjfsData.CommandId) {}
+
+ private:
+  PrjfsChannel* channel_;
+  int32_t commandId_;
+};
+
+} // namespace facebook::eden
