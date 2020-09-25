29b855b25608a45ce518dc9a75098d9a501bca56
Arun Kulshreshtha <kulshrax@fb.com>
2020-09-24 21:03:00 -0700

    edenapi: add server load to ResponseMeta
    Summary: Now that the EdenAPI server is using the `LoadMiddleware` from `gotham_ext`, each response will contain an `X-Load` header that contains the number of active requests that the server is currently handling.
    
    Reviewed By: quark-zju
    
    Differential Revision: D23922809
    
    fbshipit-source-id: 973143de5ddccf074d28aa3ef38d73f9fc1501b6

--- eden/scm/lib/edenapi/src/response.rs
+++ eden/scm/lib/edenapi/src/response.rs
@@ -20,6 +20,7 @@ const REQUEST_ID_HEADER: &str = "x-request-id";
 const TW_TASK_HEADER: &str = "x-tw-task";
 const TW_VERSION_HEADER: &str = "x-tw-task-version";
 const TW_CANARY_HEADER: &str = "x-tw-canary-id";
+const SERVER_LOAD_HEADER: &str = "x-load";
 
 /// A generic `Stream` of "entries" representing the deserialized content
 /// of a streaming response from the server.
@@ -83,6 +84,7 @@ pub struct ResponseMeta {
     pub tw_task_handle: Option<String>,
     pub tw_task_version: Option<String>,
     pub tw_canary_id: Option<String>,
+    pub server_load: Option<usize>,
 }
 
 impl ResponseMeta {
@@ -95,6 +97,7 @@ impl ResponseMeta {
             tw_task_handle: get_header(headers, TW_TASK_HEADER),
             tw_task_version: get_header(headers, TW_VERSION_HEADER),
             tw_canary_id: get_header(headers, TW_CANARY_HEADER),
+            server_load: get_header(headers, SERVER_LOAD_HEADER).and_then(|l| l.parse().ok()),
         }
     }
 }
