31107a525f2d32357dd6207272e3fd232cc5b66c
Arun Kulshreshtha <kulshrax@fb.com>
2020-09-30 11:18:30 -0700

    pyprogress: add Rust wrapper for Python progress bars
    Summary: Add Rust wrappers around Mercurial's Python `progress` module, allowing Rust code to create and use Python progress bars. The wrapper types implement the traits from the `progress` crate, so they can be passed to pure Rust crates in `scm/lib`. In typical usage, the Rust bindings will create a `PyProgressFactory`, which will be passed to pure Rust code as a trait object or via generics.
    
    Reviewed By: markbt
    
    Differential Revision: D23982317
    
    fbshipit-source-id: 4c0fde0b2423b6449c7c5155fdfd98f5da042b0d

---
+++ eden/scm/edenscmnative/bindings/modules/pyprogress/src/lib.rs
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+//! Rust bindings for Mercurial's Python `progress` module.
+//!
+//! This crate provides wrappers around Mercurial's Python progress bar objects
+//! so that they may be used by pure Rust code, as well as compatibility shims
+//! so that Python code can also use the Rust progress API. This will enable
+//! the eventual transition to a pure Rust progress bar implementation.
+
+#![allow(non_camel_case_types)]
+
+use cpython::*;
+
+pub use rust::PyProgressFactory;
+
+mod rust;
+
+pub fn init_module(py: Python, package: &str) -> PyResult<PyModule> {
+    let name = [package, "progress"].join(".");
+    let m = PyModule::new(py, &name)?;
+
+    Ok(m)
+}
