b2f1773e4ec72d1a6f08c169aa8fa1693129b8f6
Katie Mancini <kmancini@fb.com>
2020-10-07 18:35:29 -0700

    CLI to prefetch profiles
    Summary:
    Adds a subcommand to `eden debug prefetch_profile` to fetch all the files
    for a list of profiles or all the active profiles for a checkout.
    
    These profiles (files) can contain lists of file names, or globs of files, eden
    will be able to resolve them since this uses the existing prefetch code.
    
    I opted to put this command under `eden debug prefetch_profile` instead
    of `eden prefetch` since the command may change to call into eden with out
    the list of files (letting eden resolve the active profiles and names). Then
    it will no longer resemble prefetch and so long term it may be better homed
    here.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D23771483
    
    fbshipit-source-id: 12af81d40cc495efd381e3c3a2df645d72053ccd

--- eden/fs/cli/prefetch_profile.py
+++ eden/fs/cli/prefetch_profile.py
@@ -5,13 +5,59 @@
 
 import argparse
 import os
+import sys
+from typing import List, Set
+
+from facebook.eden.ttypes import GlobParams
 
 from . import subcmd as subcmd_mod, tabulate
 from .cmd_util import get_eden_instance, require_checkout
+from .config import EdenCheckout, EdenInstance
 from .subcmd import Subcmd
 
 
 prefetch_profile_cmd = subcmd_mod.Decorator()
+
+# find the profile inside the given checkout and return a set of its contents.
+def get_contents_for_profile(
+    checkout: EdenCheckout, profile: str, silent: bool
+) -> Set[str]:
+    k_relative_profiles_location = "tools/scm/prefetch_profiles/profiles"
+
+    profile_path = checkout.path / k_relative_profiles_location / profile
+
+    if not profile_path.is_file():
+        if not silent:
+            print(f"Profile {profile} not found for checkout {checkout.path}.")
+        return set()
+
+    with open(profile_path) as f:
+        return {pat.strip() for pat in f.readlines()}
+
+
+# prefetch all of the files specified by a profile in the given checkout
+def prefetch_profiles(
+    checkout: EdenCheckout,
+    instance: EdenInstance,
+    profiles: List[str],
+    enable_prefetch: bool,
+    silent: bool,
+):
+    all_profile_contents = set()
+
+    for profile in profiles:
+        all_profile_contents |= get_contents_for_profile(checkout, profile, silent)
+
+    with instance.get_thrift_client_legacy() as client:
+        return client.globFiles(
+            GlobParams(
+                mountPoint=bytes(checkout.path),
+                globs=list(all_profile_contents),
+                includeDotfiles=False,
+                prefetchFiles=enable_prefetch,
+                suppressFileList=silent,
+            )
+        )
 
 
 @prefetch_profile_cmd("record", "Start recording fetched file paths.")
@@ -121,6 +167,74 @@ class DeactivateProfileCmd(Subcmd):
         instance, checkout, _rel_path = require_checkout(args, checkout)
 
         return checkout.deactivate_profile(args.profile_name)
+
+
+@prefetch_profile_cmd(
+    "fetch",
+    "Prefetch all the active prefetch profiles or specified prefetch profiles. "
+    "This is intended for use in after checkout and pull.",
+)
+class FetchProfileCmd(Subcmd):
+    def setup_parser(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument(
+            "--checkout",
+            help="The checkout for which the profiles should be fetched.",
+            default=None,
+        )
+        parser.add_argument(
+            "--verbose",
+            help="Print extra info including warnings and the names of the "
+            "matching files to fetch.",
+            default=False,
+            action="store_true",
+        )
+        parser.add_argument(
+            "--skip-prefetch",
+            help="Do not prefetch profiles only find all the files that match "
+            "them. This will still list the names of matching files when the "
+            "verbose flag is also used",
+            default=False,
+            action="store_true",
+        )
+        parser.add_argument(
+            "--profile-names",
+            nargs="*",
+            help="Fetch only these named profiles instead of the active set of "
+            "profiles.",
+            default=None,
+        )
+
+    def run(self, args: argparse.Namespace) -> int:
+        if sys.platform == "win32":
+            # TODO(kmancini) prefetch profiles is not supported on windows yet
+            return 0
+
+        checkout = args.checkout
+
+        instance, checkout, _rel_path = require_checkout(args, checkout)
+
+        if args.profile_names is not None:
+            profiles_to_fetch = args.profile_names
+        else:
+            profiles_to_fetch = checkout.get_config().active_prefetch_profiles
+
+        if not profiles_to_fetch:
+            if args.verbose:
+                print("No profiles to fetch.")
+            return 0
+
+        result = prefetch_profiles(
+            checkout,
+            instance,
+            profiles_to_fetch,
+            enable_prefetch=not args.skip_prefetch,
+            silent=not args.verbose,
+        )
+
+        if args.verbose:
+            for name in result.matchingFiles:
+                print(os.fsdecode(name))
+        return 0
 
 
 class PrefetchProfileCmd(Subcmd):
