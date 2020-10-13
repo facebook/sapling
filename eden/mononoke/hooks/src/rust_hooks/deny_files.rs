49c9c3e4f7cd3a4cd63f752d1a68315d9ddb3420
Kostia Balytskyi <ikostia@fb.com>
2020-10-13 10:38:49 -0700

    hooks: deny_files act upon cross-repo-push-origin
    Summary: For the purposes of megarepo, we need to treat certain paths differently depending on whether the push is native to a large repo, or is push-redirected from a small repo. Let's add a separate patterns list to the `deny_files` hook. Of course, we can also add a list, specific for push-redirected pushes, but it seems unneeded atm.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D24278409
    
    fbshipit-source-id: 9fd815940bb656ceac6ab234f3a0647a5c57db06

--- eden/mononoke/hooks/src/rust_hooks/deny_files.rs
+++ eden/mononoke/hooks/src/rust_hooks/deny_files.rs
@@ -18,37 +18,68 @@ use crate::{CrossRepoPushSource, FileContentFetcher, FileHook, HookExecution, Ho
 
 #[derive(Default)]
 pub struct DenyFilesBuilder {
-    deny_patterns: Option<Vec<String>>,
+    /// Deny patterns for all sources of pushes
+    all_push_sources_deny_patterns: Option<Vec<String>>,
+    /// Deny patterns for pushes, originally intended to this repo
+    /// (as opposed to push-redirected ones)
+    native_push_only_deny_patterns: Option<Vec<String>>,
 }
 
 impl DenyFilesBuilder {
     pub fn set_from_config(mut self, config: &HookConfig) -> Self {
         if let Some(v) = config.string_lists.get("deny_patterns") {
-            self = self.deny_patterns(v)
+            self = self.all_push_sources_deny_patterns(v)
+        }
+        if let Some(v) = config.string_lists.get("native_push_only_deny_patterns") {
+            self = self.native_push_only_deny_patterns(v)
         }
         self
     }
 
-    pub fn deny_patterns(mut self, strs: impl IntoIterator<Item = impl AsRef<str>>) -> Self {
-        self.deny_patterns = Some(strs.into_iter().map(|s| String::from(s.as_ref())).collect());
+    pub fn all_push_sources_deny_patterns(
+        mut self,
+        strs: impl IntoIterator<Item = impl AsRef<str>>,
+    ) -> Self {
+        self.all_push_sources_deny_patterns =
+            Some(strs.into_iter().map(|s| String::from(s.as_ref())).collect());
+        self
+    }
+
+    pub fn native_push_only_deny_patterns(
+        mut self,
+        strs: impl IntoIterator<Item = impl AsRef<str>>,
+    ) -> Self {
+        self.native_push_only_deny_patterns =
+            Some(strs.into_iter().map(|s| String::from(s.as_ref())).collect());
         self
     }
 
     pub fn build(self) -> Result<DenyFiles> {
         Ok(DenyFiles {
-            deny_patterns: self
-                .deny_patterns
+            all_push_sources_deny_patterns: self
+                .all_push_sources_deny_patterns
                 .unwrap_or_else(Vec::new)
                 .into_iter()
                 .map(LuaPattern::try_from)
                 .collect::<Result<Vec<_>, _>>()
-                .context("Failed to create LuaPattern for deny_patterns")?,
+                .context("Failed to create LuaPattern for all_push_sources_deny_patterns")?,
+            native_push_only_deny_patterns: self
+                .native_push_only_deny_patterns
+                .unwrap_or_else(Vec::new)
+                .into_iter()
+                .map(LuaPattern::try_from)
+                .collect::<Result<Vec<_>, _>>()
+                .context("Failed to create LuaPattern for native_push_only_deny_patterns")?,
         })
     }
 }
 
 pub struct DenyFiles {
-    deny_patterns: Vec<LuaPattern>,
+    /// Deny patterns for all sources of pushes
+    all_push_sources_deny_patterns: Vec<LuaPattern>,
+    /// Deny patterns for pushes, originally intended to this repo
+    /// (as opposed to push-redirected ones)
+    native_push_only_deny_patterns: Vec<LuaPattern>,
 }
 
 impl DenyFiles {
@@ -65,25 +96,108 @@ impl FileHook for DenyFiles {
         _content_fetcher: &'fetcher dyn FileContentFetcher,
         change: Option<&'change FileChange>,
         path: &'path MPath,
-        _cross_repo_push_source: CrossRepoPushSource,
+        cross_repo_push_source: CrossRepoPushSource,
     ) -> Result<HookExecution> {
         if change.is_none() {
             // It is acceptable to delete any file
             return Ok(HookExecution::Accepted);
         }
 
-        let path = path.to_string();
-        for pattern in &self.deny_patterns {
-            if pattern.is_match(&path) {
-                return Ok(HookExecution::Rejected(HookRejectionInfo::new_long(
-                    "Denied filename matched name pattern",
-                    format!(
-                        "Denied filename '{}' matched name pattern '{}'. Rename or remove this file and try again.",
-                        path, pattern
-                    ),
-                )));
-            }
+        deny_unacceptable_patterns(
+            &self.all_push_sources_deny_patterns,
+            &self.native_push_only_deny_patterns,
+            path,
+            cross_repo_push_source,
+        )
+    }
+}
+
+/// Easily-testable business logic of the `DenyFiles` hook
+fn deny_unacceptable_patterns<'a, 'b>(
+    all_patterns: &'a [LuaPattern],
+    native_patterns: &'a [LuaPattern],
+    path: &'b MPath,
+    cross_repo_push_source: CrossRepoPushSource,
+) -> Result<HookExecution> {
+    let patterns: Vec<&LuaPattern> = {
+        let mut patterns: Vec<&LuaPattern> = all_patterns.iter().collect();
+
+        if CrossRepoPushSource::NativeToThisRepo == cross_repo_push_source {
+            patterns.extend(native_patterns.iter());
         }
-        Ok(HookExecution::Accepted)
+
+        patterns
+    };
+
+    let path = path.to_string();
+    for pattern in patterns {
+        if pattern.is_match(&path) {
+            return Ok(HookExecution::Rejected(HookRejectionInfo::new_long(
+                "Denied filename matched name pattern",
+                format!(
+                    "Denied filename '{}' matched name pattern '{}'. Rename or remove this file and try again.",
+                    path, pattern
+                ),
+            )));
+        }
+    }
+    Ok(HookExecution::Accepted)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::convert::TryInto;
+
+    fn setup_patterns() -> (Vec<LuaPattern>, Vec<LuaPattern>) {
+        let all: Vec<LuaPattern> = vec!["all".try_into().unwrap()];
+        let native: Vec<LuaPattern> = vec!["native".try_into().unwrap()];
+        (all, native)
+    }
+
+    fn mpath(s: &str) -> MPath {
+        MPath::new(s).unwrap()
+    }
+
+    #[test]
+    fn test_denied_in_any_push() {
+        let (all, native) = setup_patterns();
+        let mp = mpath("all/1");
+        let r =
+            deny_unacceptable_patterns(&all, &native, &mp, CrossRepoPushSource::NativeToThisRepo)
+                .unwrap();
+        assert!(matches!(r, HookExecution::Rejected(_)));
+
+        let r = deny_unacceptable_patterns(&all, &native, &mp, CrossRepoPushSource::PushRedirected)
+            .unwrap();
+        assert!(matches!(r, HookExecution::Rejected(_)));
+    }
+
+    #[test]
+    fn test_denied_only_in_native_push() {
+        let (all, native) = setup_patterns();
+        let mp = mpath("native/1");
+        let r =
+            deny_unacceptable_patterns(&all, &native, &mp, CrossRepoPushSource::NativeToThisRepo)
+                .unwrap();
+        assert!(matches!(r, HookExecution::Rejected(_)));
+
+        let r = deny_unacceptable_patterns(&all, &native, &mp, CrossRepoPushSource::PushRedirected)
+            .unwrap();
+        assert!(matches!(r, HookExecution::Accepted));
+    }
+
+    #[test]
+    fn test_allowed_in_any_push() {
+        let (all, native) = setup_patterns();
+        let mp = mpath("ababagalamaga");
+        let r =
+            deny_unacceptable_patterns(&all, &native, &mp, CrossRepoPushSource::NativeToThisRepo)
+                .unwrap();
+        assert!(matches!(r, HookExecution::Accepted));
+
+        let r = deny_unacceptable_patterns(&all, &native, &mp, CrossRepoPushSource::PushRedirected)
+            .unwrap();
+        assert!(matches!(r, HookExecution::Accepted));
     }
 }
