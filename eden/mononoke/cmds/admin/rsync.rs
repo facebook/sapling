e4e4b33bb836f22a105bf355c418d42f6c9586be
Stanislau Hlebik <stash@fb.com>
2020-10-27 15:10:09 -0700

    mononoke: add --overwrite option to rsync
    Summary:
    Previously `mononoke_admin rsync` didn't overwrite files i.e. if a target
    directory has a file with the same name as in source directory then it won't be overwritten.
    
    This diff adds an option to make it possible to overwrite a file if a target
    directory has a file with the same name but its content is different. However
    note that if a file has to be overwritten then target file is going to be removed in one
    commit and then copied in the second (i.e. we'll create two commits instead of
    one). The reason for doing that is to preserve the history of the original file
    (i.e. history from `from-directory`).
    
    Reviewed By: aslpavel
    
    Differential Revision: D24538199
    
    fbshipit-source-id: 792162c4e5ad81fb6949dd95eb1322696ec011ea

--- eden/mononoke/cmds/admin/rsync.rs
+++ eden/mononoke/cmds/admin/rsync.rs
@@ -35,6 +35,7 @@ pub const ARG_FILE_NUM_LIMIT: &str = "file-num-limit";
 pub const ARG_TOTAL_SIZE_LIMIT: &str = "total-size-limit";
 pub const ARG_FROM_DIR: &str = "from-dir";
 pub const ARG_LFS_THRESHOLD: &str = "lfs-threshold";
+pub const ARG_OVERWRITE: &str = "overwrite";
 pub const ARG_TO_DIR: &str = "to-dir";
 pub const RSYNC: &str = "rsync";
 
@@ -112,6 +113,13 @@ pub fn build_subcommand<'a, 'b>() -> App<'a, 'b> {
                 .takes_value(true)
                 .required(false),
         )
+        .arg(
+            Arg::with_name(ARG_OVERWRITE)
+                .long(ARG_OVERWRITE)
+                .help("overwrite a file if it exists in the destination directory")
+                .takes_value(false)
+                .required(false),
+        )
 }
 
 pub async fn subcommand_rsync<'a>(
@@ -150,21 +158,7 @@ pub async fn subcommand_rsync<'a>(
         .value_of(ARG_COMMIT_MESSAGE)
         .ok_or_else(|| anyhow!("{} arg is not specified", ARG_COMMIT_MESSAGE))?;
 
-    let maybe_file_num_limit =
-        args::get_and_parse_opt::<NonZeroU64>(sub_matches, ARG_FILE_NUM_LIMIT);
-
-    let maybe_exclude_file_regex = sub_matches.value_of(ARG_EXCLUDE_FILE_REGEX);
-    let maybe_exclude_file_regex = maybe_exclude_file_regex
-        .map(Regex::new)
-        .transpose()
-        .map_err(Error::from)?;
-
-    let maybe_total_size_limit =
-        args::get_and_parse_opt::<NonZeroU64>(sub_matches, ARG_TOTAL_SIZE_LIMIT);
-
-    let maybe_lfs_threshold = args::get_and_parse_opt::<NonZeroU64>(sub_matches, ARG_LFS_THRESHOLD);
-
-    let result_cs_id = rsync(
+    let cs_ids = rsync(
         &ctx,
         &repo,
         cs_id,
@@ -172,14 +166,15 @@ pub async fn subcommand_rsync<'a>(
         to_dir,
         author.to_string(),
         msg.to_string(),
-        Limits {
-            file_num_limit: maybe_file_num_limit,
-            total_size_limit: maybe_total_size_limit,
-            lfs_threshold: maybe_lfs_threshold,
-        },
-        maybe_exclude_file_regex,
+        Limits::new(sub_matches),
+        Options::new(sub_matches)?,
     )
     .await?;
+
+    let result_cs_id = cs_ids
+        .last()
+        .copied()
+        .ok_or_else(|| anyhow!("nothing to move!"))?;
 
     println!("{}", result_cs_id);
 
@@ -193,6 +188,45 @@ struct Limits {
     lfs_threshold: Option<NonZeroU64>,
 }
 
+impl Limits {
+    pub fn new(sub_m: &ArgMatches<'_>) -> Self {
+        let maybe_file_num_limit = args::get_and_parse_opt::<NonZeroU64>(sub_m, ARG_FILE_NUM_LIMIT);
+        let maybe_total_size_limit =
+            args::get_and_parse_opt::<NonZeroU64>(sub_m, ARG_TOTAL_SIZE_LIMIT);
+        let maybe_lfs_threshold = args::get_and_parse_opt::<NonZeroU64>(sub_m, ARG_LFS_THRESHOLD);
+
+
+        Self {
+            file_num_limit: maybe_file_num_limit,
+            total_size_limit: maybe_total_size_limit,
+            lfs_threshold: maybe_lfs_threshold,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+struct Options {
+    maybe_exclude_file_regex: Option<Regex>,
+    overwrite: bool,
+}
+
+impl Options {
+    pub fn new(sub_m: &ArgMatches<'_>) -> Result<Self, Error> {
+        let maybe_exclude_file_regex = sub_m.value_of(ARG_EXCLUDE_FILE_REGEX);
+        let maybe_exclude_file_regex = maybe_exclude_file_regex
+            .map(Regex::new)
+            .transpose()
+            .map_err(Error::from)?;
+
+        let overwrite = sub_m.is_present(ARG_OVERWRITE);
+
+        Ok(Self {
+            maybe_exclude_file_regex,
+            overwrite,
+        })
+    }
+}
+
 async fn rsync(
     ctx: &CoreContext,
     repo: &BlobRepo,
@@ -202,8 +236,8 @@ async fn rsync(
     author: String,
     msg: String,
     limits: Limits,
-    maybe_exclude_file_regex: Option<Regex>,
-) -> Result<ChangesetId, Error> {
+    options: Options,
+) -> Result<Vec<ChangesetId>, Error> {
     let (from_entries, to_entries) = try_join(
         list_directory(&ctx, &repo, cs_id, &from_dir),
         list_directory(&ctx, &repo, cs_id, &to_dir),
@@ -212,21 +246,32 @@ async fn rsync(
     let from_entries = from_entries.ok_or_else(|| Error::msg("from directory does not exist!"))?;
     let to_entries = to_entries.unwrap_or_else(BTreeMap::new);
 
+    // These are the file changes that have to be removed first
+    let mut remove_file_changes = BTreeMap::new();
+    // These are the file changes that have to be copied
     let mut file_changes = BTreeMap::new();
     let mut total_file_size = 0;
     for (from_suffix, fsnode_file) in from_entries {
-        if let Some(ref regex) = maybe_exclude_file_regex {
+        if let Some(ref regex) = options.maybe_exclude_file_regex {
             if from_suffix.matches_regex(&regex) {
                 continue;
             }
         }
 
-        if to_entries.contains_key(&from_suffix) {
-            continue;
-        }
-
         let from_path = from_dir.join(&from_suffix);
         let to_path = to_dir.join(&from_suffix);
+
+        if let Some(to_fsnode) = to_entries.get(&from_suffix) {
+            if to_fsnode == &fsnode_file {
+                continue;
+            }
+
+            if options.overwrite {
+                remove_file_changes.insert(to_path.clone(), None);
+            } else {
+                continue;
+            }
+        }
 
         debug!(
             ctx.logger(),
@@ -235,13 +280,7 @@ async fn rsync(
             to_path,
             fsnode_file.size()
         );
-        let file_change = FileChange::new(
-            *fsnode_file.content_id(),
-            *fsnode_file.file_type(),
-            fsnode_file.size(),
-            Some((from_path, cs_id)),
-        );
-        file_changes.insert(to_path, Some(file_change));
+        file_changes.insert(to_path, Some((from_path, fsnode_file)));
         if let Some(lfs_threshold) = limits.lfs_threshold {
             if fsnode_file.size() < lfs_threshold.get() {
                 total_file_size += fsnode_file.size();
@@ -267,23 +306,61 @@ async fn rsync(
         }
     }
 
-    if file_changes.is_empty() {
-        return Err(anyhow!("nothing to move!"));
+    create_changesets(
+        ctx,
+        repo,
+        vec![remove_file_changes, file_changes],
+        cs_id,
+        author,
+        msg,
+    )
+    .await
+}
+
+async fn create_changesets(
+    ctx: &CoreContext,
+    repo: &BlobRepo,
+    file_changes: Vec<BTreeMap<MPath, Option<(MPath, FsnodeFile)>>>,
+    mut parent: ChangesetId,
+    author: String,
+    msg: String,
+) -> Result<Vec<ChangesetId>, Error> {
+    let mut changesets = vec![];
+    let mut cs_ids = vec![];
+    for path_to_maybe_fsnodes in file_changes {
+        if path_to_maybe_fsnodes.is_empty() {
+            continue;
+        }
+
+        let mut fc = BTreeMap::new();
+        for (to_path, maybe_fsnode) in path_to_maybe_fsnodes {
+            let maybe_file_change = match maybe_fsnode {
+                Some((from_path, fsnode_file)) => Some(FileChange::new(
+                    *fsnode_file.content_id(),
+                    *fsnode_file.file_type(),
+                    fsnode_file.size(),
+                    Some((from_path, parent)),
+                )),
+                None => None,
+            };
+
+            fc.insert(to_path, maybe_file_change);
+        }
+
+        info!(ctx.logger(), "creating csid with {} file changes", fc.len());
+        let bcs = create_bonsai_changeset(vec![parent], fc, author.clone(), msg.clone())?;
+
+        let cs_id = bcs.get_changeset_id();
+        changesets.push(bcs);
+        cs_ids.push(cs_id);
+        parent = cs_id;
     }
 
-    info!(
-        ctx.logger(),
-        "creating csid with {} file changes",
-        file_changes.len()
-    );
-    let parents = vec![cs_id];
-    let bcs = create_bonsai_changeset(parents, file_changes, author, msg)?;
-    let result_cs_id = bcs.get_changeset_id();
-    save_bonsai_changesets(vec![bcs], ctx.clone(), repo.clone())
+    save_bonsai_changesets(changesets, ctx.clone(), repo.clone())
         .compat()
         .await?;
 
-    Ok(result_cs_id)
+    Ok(cs_ids)
 }
 
 // Recursively lists all the files under `path` if this is a directory.
@@ -396,9 +473,12 @@ mod test {
             "author".to_string(),
             "msg".to_string(),
             Limits::default(),
-            None,
+            Options::default(),
         )
-        .await?;
+        .await?
+        .last()
+        .copied()
+        .unwrap();
 
         assert_eq!(
             list_working_copy_utf8(&ctx, &repo, new_cs_id,).await?,
@@ -440,9 +520,12 @@ mod test {
             "author".to_string(),
             "msg".to_string(),
             limit.clone(),
-            None,
+            Options::default(),
         )
-        .await?;
+        .await?
+        .last()
+        .copied()
+        .unwrap();
 
         assert_eq!(
             list_working_copy_utf8(&ctx, &repo, first_cs_id,).await?,
@@ -464,9 +547,12 @@ mod test {
             "author".to_string(),
             "msg".to_string(),
             limit,
-            None,
+            Options::default(),
         )
-        .await?;
+        .await?
+        .last()
+        .copied()
+        .unwrap();
 
         assert_eq!(
             list_working_copy_utf8(&ctx, &repo, second_cs_id,).await?,
@@ -505,9 +591,15 @@ mod test {
             "author".to_string(),
             "msg".to_string(),
             Limits::default(),
-            Some(Regex::new("(BUCK|.*\\.bzl|TARGETS)$")?),
+            Options {
+                maybe_exclude_file_regex: Some(Regex::new("(BUCK|.*\\.bzl|TARGETS)$")?),
+                ..Default::default()
+            },
         )
-        .await?;
+        .await?
+        .last()
+        .copied()
+        .unwrap();
 
         assert_eq!(
             list_working_copy_utf8(&ctx, &repo, cs_id,).await?,
@@ -549,9 +641,12 @@ mod test {
                 total_size_limit: NonZeroU64::new(5),
                 lfs_threshold: None,
             },
-            None,
+            Options::default(),
         )
-        .await?;
+        .await?
+        .last()
+        .copied()
+        .unwrap();
 
         assert_eq!(
             list_working_copy_utf8(&ctx, &repo, first_cs_id,).await?,
@@ -576,9 +671,12 @@ mod test {
                 total_size_limit: NonZeroU64::new(5),
                 lfs_threshold: None,
             },
-            None,
+            Options::default(),
         )
-        .await?;
+        .await?
+        .last()
+        .copied()
+        .unwrap();
 
         assert_eq!(
             list_working_copy_utf8(&ctx, &repo, second_cs_id,).await?,
@@ -608,7 +706,7 @@ mod test {
             .commit()
             .await?;
 
-        let cs_id = rsync(
+        let cs_ids = rsync(
             &ctx,
             &repo,
             cs_id,
@@ -621,9 +719,11 @@ mod test {
                 total_size_limit: NonZeroU64::new(5),
                 lfs_threshold: NonZeroU64::new(2),
             },
-            None,
+            Options::default(),
         )
         .await?;
+        assert_eq!(cs_ids.len(), 1);
+        let cs_id = cs_ids.last().copied().unwrap();
 
         assert_eq!(
             list_working_copy_utf8(&ctx, &repo, cs_id,).await?,
@@ -634,6 +734,76 @@ mod test {
                 MPath::new("dir_from/a")? => "aaaaaaaaaa".to_string(),
                 MPath::new("dir_from/b")? => "b".to_string(),
                 MPath::new("dir_from/c")? => "c".to_string(),
+            }
+        );
+
+        Ok(())
+    }
+
+    #[fbinit::compat_test]
+    async fn test_rsync_with_overwrite(fb: FacebookInit) -> Result<(), Error> {
+        let ctx = CoreContext::test_mock(fb);
+        let repo = new_memblob_empty(None)?;
+        let cs_id = CreateCommitContext::new_root(&ctx, &repo)
+            .add_file("dir_from/a", "aa")
+            .add_file("dir_from/b", "b")
+            .add_file("dir_to/a", "a")
+            .add_file("dir_to/b", "b")
+            .commit()
+            .await?;
+
+        // No overwrite - nothing should be copied
+        let cs_ids = rsync(
+            &ctx,
+            &repo,
+            cs_id,
+            MPath::new("dir_from")?,
+            MPath::new("dir_to")?,
+            "author".to_string(),
+            "msg".to_string(),
+            Limits::default(),
+            Options {
+                overwrite: false,
+                ..Default::default()
+            },
+        )
+        .await?;
+        assert!(cs_ids.is_empty());
+
+        // Use overwrite - it should create two commits.
+        // First commit removes dir_to/a, second commit copies dir_form/a to dir_to/a
+        let cs_ids = rsync(
+            &ctx,
+            &repo,
+            cs_id,
+            MPath::new("dir_from")?,
+            MPath::new("dir_to")?,
+            "author".to_string(),
+            "msg".to_string(),
+            Limits::default(),
+            Options {
+                overwrite: true,
+                ..Default::default()
+            },
+        )
+        .await?;
+
+        assert_eq!(
+            list_working_copy_utf8(&ctx, &repo, *cs_ids.get(0).unwrap()).await?,
+            hashmap! {
+                MPath::new("dir_from/a")? => "aa".to_string(),
+                MPath::new("dir_from/b")? => "b".to_string(),
+                MPath::new("dir_to/b")? => "b".to_string(),
+            }
+        );
+
+        assert_eq!(
+            list_working_copy_utf8(&ctx, &repo, *cs_ids.last().unwrap()).await?,
+            hashmap! {
+                MPath::new("dir_from/a")? => "aa".to_string(),
+                MPath::new("dir_from/b")? => "b".to_string(),
+                MPath::new("dir_to/a")? => "aa".to_string(),
+                MPath::new("dir_to/b")? => "b".to_string(),
             }
         );
 
