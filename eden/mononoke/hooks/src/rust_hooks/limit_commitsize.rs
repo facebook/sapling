e7fe9e4ea978224107e268f17c19cc4f825460f6
Stanislau Hlebik <stash@fb.com>
2020-11-06 11:21:00 -0800

    mononoke: do not allow too big commits to be landed
    Reviewed By: krallin
    
    Differential Revision: D24730106
    
    fbshipit-source-id: e2b116c437a557f1b795c3b07eb124b8dd1a3ef8

--- eden/mononoke/hooks/src/rust_hooks/limit_commitsize.rs
+++ eden/mononoke/hooks/src/rust_hooks/limit_commitsize.rs
@@ -21,6 +21,7 @@ use regex::Regex;
 pub struct LimitCommitsizeBuilder {
     commit_size_limit: Option<u64>,
     ignore_path_regexes: Option<Vec<String>>,
+    changed_files_limit: Option<u64>,
 }
 
 impl LimitCommitsizeBuilder {
@@ -30,6 +31,9 @@ impl LimitCommitsizeBuilder {
         }
         if let Some(v) = config.string_lists.get("ignore_path_regexes") {
             self = self.ignore_path_regexes(v)
+        }
+        if let Some(v) = config.ints.get("changed_files_limit") {
+            self = self.changed_files_limit(*v as u64)
         }
         self
     }
@@ -45,6 +49,11 @@ impl LimitCommitsizeBuilder {
         self
     }
 
+    pub fn changed_files_limit(mut self, changed_files_limit: u64) -> Self {
+        self.changed_files_limit = Some(changed_files_limit);
+        self
+    }
+
     pub fn build(self) -> Result<LimitCommitsize> {
         Ok(LimitCommitsize {
             commit_size_limit: self
@@ -57,6 +66,7 @@ impl LimitCommitsizeBuilder {
                 .map(|s| Regex::new(&s))
                 .collect::<Result<Vec<_>, _>>()
                 .context("Failed to create regex for ignore_path_regex")?,
+            changed_files_limit: self.changed_files_limit,
         })
     }
 }
@@ -64,6 +74,7 @@ impl LimitCommitsizeBuilder {
 pub struct LimitCommitsize {
     commit_size_limit: u64,
     ignore_path_regexes: Vec<Regex>,
+    changed_files_limit: Option<u64>,
 }
 
 impl LimitCommitsize {
@@ -87,13 +98,9 @@ impl ChangesetHook for LimitCommitsize {
             return Ok(HookExecution::Accepted);
         }
 
+        let mut num_changed_files = 0;
         let mut totalsize = 0;
         for (path, file_change) in changeset.file_changes() {
-            let file = match file_change {
-                None => continue,
-                Some(file) => file,
-            };
-
             let path = format!("{}", path);
             if self
                 .ignore_path_regexes
@@ -102,6 +109,24 @@ impl ChangesetHook for LimitCommitsize {
             {
                 continue;
             }
+
+            num_changed_files += 1;
+            if let Some(changed_files_limit) = self.changed_files_limit {
+                if num_changed_files > changed_files_limit {
+                    return Ok(HookExecution::Rejected(HookRejectionInfo::new_long(
+                        "Commit too large",
+                        format!(
+                            "Commit changed {} files but at most {} are allowed. Reach out to Source control @ fb for instructions.",
+                            num_changed_files, changed_files_limit,
+                        ),
+                    )));
+                }
+            }
+
+            let file = match file_change {
+                None => continue,
+                Some(file) => file,
+            };
 
             totalsize += file.size();
             if totalsize > self.commit_size_limit {
@@ -115,5 +140,160 @@ impl ChangesetHook for LimitCommitsize {
             }
         }
         Ok(HookExecution::Accepted)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use anyhow::Error;
+    use blobrepo_factory::new_memblob_empty;
+    use blobstore::Loadable;
+    use fbinit::FacebookInit;
+    use hooks_content_stores::BlobRepoFileContentFetcher;
+    use maplit::hashmap;
+    use std::collections::HashMap;
+    use tests_utils::CreateCommitContext;
+
+    #[fbinit::compat_test]
+    async fn test_limitcommitsize(fb: FacebookInit) -> Result<(), Error> {
+        let ctx = CoreContext::test_mock(fb);
+        let repo = new_memblob_empty(None)?;
+
+        let cs_id = CreateCommitContext::new_root(&ctx, &repo)
+            .add_file("dir/a", "a")
+            .add_file("dir/b", "b")
+            .commit()
+            .await?;
+
+        let bcs = cs_id.load(ctx.clone(), repo.blobstore()).await?;
+
+        let content_fetcher = BlobRepoFileContentFetcher::new(repo);
+        let hook = build_hook(hashmap! {
+            "commitsizelimit".to_string() => 2,
+            "changed_files_limit".to_string() => 2,
+        })?;
+        let hook_execution = hook
+            .run(
+                &ctx,
+                &BookmarkName::new("book")?,
+                &bcs,
+                &content_fetcher,
+                CrossRepoPushSource::NativeToThisRepo,
+            )
+            .await?;
+        assert_eq!(hook_execution, HookExecution::Accepted);
+
+
+        let hook = build_hook(hashmap! {
+            "commitsizelimit".to_string() => 2,
+            "changed_files_limit".to_string() => 1,
+        })?;
+        let hook_execution = hook
+            .run(
+                &ctx,
+                &BookmarkName::new("book")?,
+                &bcs,
+                &content_fetcher,
+                CrossRepoPushSource::NativeToThisRepo,
+            )
+            .await?;
+        match hook_execution {
+            HookExecution::Rejected(_) => {}
+            HookExecution::Accepted => {
+                return Err(anyhow!("should be rejected"));
+            }
+        };
+
+        let hook = build_hook(hashmap! {
+            "commitsizelimit".to_string() => 1,
+            "changed_files_limit".to_string() => 2,
+        })?;
+        let hook_execution = hook
+            .run(
+                &ctx,
+                &BookmarkName::new("book")?,
+                &bcs,
+                &content_fetcher,
+                CrossRepoPushSource::NativeToThisRepo,
+            )
+            .await?;
+        match hook_execution {
+            HookExecution::Rejected(_) => {}
+            HookExecution::Accepted => {
+                return Err(anyhow!("should be rejected"));
+            }
+        };
+        Ok(())
+    }
+
+    #[fbinit::compat_test]
+    async fn test_limitcommitsize_removed_files(fb: FacebookInit) -> Result<(), Error> {
+        let ctx = CoreContext::test_mock(fb);
+        let repo = new_memblob_empty(None)?;
+
+        let parent_cs_id = CreateCommitContext::new_root(&ctx, &repo)
+            .add_file("dir/a", "a")
+            .add_file("dir/b", "b")
+            .commit()
+            .await?;
+
+        let cs_id = CreateCommitContext::new(&ctx, &repo, vec![parent_cs_id])
+            .delete_file("dir/a")
+            .delete_file("dir/b")
+            .commit()
+            .await?;
+
+        let bcs = cs_id.load(ctx.clone(), repo.blobstore()).await?;
+
+        let content_fetcher = BlobRepoFileContentFetcher::new(repo);
+        let hook = build_hook(hashmap! {
+            "commitsizelimit".to_string() => 100,
+            "changed_files_limit".to_string() => 2,
+        })?;
+        let hook_execution = hook
+            .run(
+                &ctx,
+                &BookmarkName::new("book")?,
+                &bcs,
+                &content_fetcher,
+                CrossRepoPushSource::NativeToThisRepo,
+            )
+            .await?;
+        assert_eq!(hook_execution, HookExecution::Accepted);
+
+        let hook = build_hook(hashmap! {
+            "commitsizelimit".to_string() => 100,
+            "changed_files_limit".to_string() => 1,
+        })?;
+        let hook_execution = hook
+            .run(
+                &ctx,
+                &BookmarkName::new("book")?,
+                &bcs,
+                &content_fetcher,
+                CrossRepoPushSource::NativeToThisRepo,
+            )
+            .await?;
+        match hook_execution {
+            HookExecution::Rejected(_) => {}
+            HookExecution::Accepted => {
+                return Err(anyhow!("should be rejected"));
+            }
+        };
+        Ok(())
+    }
+
+    fn build_hook(ints: HashMap<String, i32>) -> Result<LimitCommitsize> {
+        let config = HookConfig {
+            bypass: None,
+            strings: hashmap! {},
+            ints,
+            string_lists: hashmap! {},
+            int_lists: hashmap! {},
+        };
+        let mut builder = LimitCommitsize::builder();
+        builder = builder.set_from_config(&config);
+        builder.build()
     }
 }
---
+++ eden/mononoke/tests/integration/test-hook-limit-commitsize-num-changed-files.t
@@ -0,0 +1,48 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License found in the LICENSE file in the root
+# directory of this source tree.
+
+  $ . "${TEST_FIXTURES}/library.sh"
+
+  $ hook_test_setup \
+  > limit_commitsize <(
+  >   cat <<CONF
+  > config_ints={commitsizelimit=10, changed_files_limit=5}
+  > CONF
+  > )
+
+Small commit, one file changed
+  $ hg up -q 0
+  $ echo file > file
+  $ hg ci -Aqm 1
+  $ hgmn push -r . --to master_bookmark
+  pushing rev 4f751d63133d to destination ssh://user@dummy/repo bookmark master_bookmark
+  searching for changes
+  adding changesets
+  adding manifests
+  adding file changes
+  added 1 changesets with 0 changes to 0 files
+  updating bookmark master_bookmark
+
+
+Large commit, a lot of files changed
+  $ for x in $(seq 6); do echo $x > $x; done
+  $ hg ci -Aqm 2
+  $ hgmn push -r . --to master_bookmark
+  pushing rev bb41d2a5d8c3 to destination ssh://user@dummy/repo bookmark master_bookmark
+  searching for changes
+  remote: Command failed
+  remote:   Error:
+  remote:     hooks failed:
+  remote:     limit_commitsize for bb41d2a5d8c3492f085f4d276927533e79f269ae: Commit changed 6 files but at most 5 are allowed. Reach out to Source control @ fb for instructions.
+  remote: 
+  remote:   Root cause:
+  remote:     hooks failed:
+  remote:     limit_commitsize for bb41d2a5d8c3492f085f4d276927533e79f269ae: Commit changed 6 files but at most 5 are allowed. Reach out to Source control @ fb for instructions.
+  remote: 
+  remote:   Debug context:
+  remote:     "hooks failed:\nlimit_commitsize for bb41d2a5d8c3492f085f4d276927533e79f269ae: Commit changed 6 files but at most 5 are allowed. Reach out to Source control @ fb for instructions."
+  abort: stream ended unexpectedly (got 0 bytes, expected 4)
+  [255]
