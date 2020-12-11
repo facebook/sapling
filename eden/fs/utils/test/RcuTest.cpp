88dcfa7bb3cb6289444211543a94d9b0250357dd
Xavier Deguillard <xavierd@fb.com>
2020-12-11 14:09:39 -0800

    utils: add an RcuPtr class
    Summary:
    RCU is a synchronization mechanism that allows for very fast reads, at the
    expense of slower writes. This is achieved by having the reader sometimes
    reading a stale pointer when concurrent to a write, at which point the writer
    will delay reclaiming the old data to a later moment where it is known that no
    reader can hold a pointer to the old data.
    
    Doing that allows for the read operations to be significantly faster than using
    a Synchronized lock. Folly's documentation claims a read lock/unlock of RCU
    runs in ~4ns, while the same for Synchronized is ~26ns.
    
    Due to the writers cost, RCU is perfectly suited for places where reads needs
    to be as fast as possible, and writes are very infrequent. One typical example
    is when caching an application's configuration, we can expect reading the
    configuration values more frequently than it is being reloaded, and in the case
    where the configuration mismatch, a stale configuration can be tolerated by the
    application.
    
    In EdenFS, we can use RCU on Windows to make sure that unmounting a repository
    will wait on all the pending callbacks.
    
    Reviewed By: kmancini
    
    Differential Revision: D25351536
    
    fbshipit-source-id: 050ca0337e67ae195f4f16062dddb60f584af692

---
+++ eden/fs/utils/test/RcuTest.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "eden/fs/utils/Rcu.h"
+#include <gtest/gtest.h>
+
+using namespace facebook::eden;
+
+TEST(RcuTest, rlock) {
+  RcuPtr<int> rcu{*folly::rcu_default_domain(), 42};
+  auto guard = rcu.rlock();
+  EXPECT_EQ(*guard, 42);
+}
+
+TEST(RcuTest, update) {
+  RcuPtr<int> rcu{*folly::rcu_default_domain(), 42};
+  auto guard = rcu.rlock();
+  rcu.update(10);
+  EXPECT_EQ(*guard, 42);
+
+  auto guard2 = rcu.rlock();
+  EXPECT_EQ(*guard2, 10);
+}
+
+TEST(RcuTest, exchange) {
+  RcuPtr<int> rcu{*folly::rcu_default_domain(), 42};
+  auto guard = rcu.rlock();
+  auto old = rcu.exchange(10);
+  EXPECT_EQ(*old, 42);
+  EXPECT_EQ(*guard, *old);
+  // Silence LeakSanitizer, do not manually delete the pointer without calling
+  // synchronize first.
+  delete old;
+}
+
+TEST(RcuTest, synchronize) {
+  RcuPtr<int> rcu{*folly::rcu_default_domain(), 42};
+  rcu.synchronize();
+  auto guard = rcu.rlock();
+  EXPECT_EQ(*guard, 42);
+}
+
+bool updateAndSynchronizeDeleted = false;
+TEST(RcuTest, updateAndSynchronize) {
+  struct Deleter {
+    void operator()(int* value) {
+      delete value;
+      updateAndSynchronizeDeleted = true;
+    }
+  };
+
+  RcuPtr<int, folly::RcuTag, Deleter> rcu{*folly::rcu_default_domain(), 42};
+  rcu.update(10);
+  rcu.synchronize();
+  EXPECT_TRUE(updateAndSynchronizeDeleted);
+}
