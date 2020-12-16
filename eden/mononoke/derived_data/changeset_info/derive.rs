590b84f62d269ac1dc0a9fe87892049ff804c996
Mark Juggurnauth-Thomas <mbthomas@fb.com>
2020-12-16 10:36:27 -0800

    changeset_info: implement batch_derive
    Summary:
    ChangesetInfo derivation doesn't depend on the parent ChangesetInfo being
    available, so we can derive them in batches very easily.
    
    Reviewed By: krallin
    
    Differential Revision: D25470721
    
    fbshipit-source-id: cc8ce305990eb6c9846158f0e9e3917cf35e169d

--- eden/mononoke/derived_data/changeset_info/derive.rs
+++ eden/mononoke/derived_data/changeset_info/derive.rs
@@ -5,17 +5,20 @@
  * GNU General Public License version 2.
  */
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use anyhow::{Error, Result};
 use async_trait::async_trait;
 use blobrepo::BlobRepo;
-use blobstore::Blobstore;
+use blobstore::{Blobstore, Loadable};
 use context::CoreContext;
 use derived_data::{
-    impl_bonsai_derived_mapping, BlobstoreRootIdMapping, BonsaiDerivable, DerivedDataTypesConfig,
+    impl_bonsai_derived_mapping, BlobstoreRootIdMapping, BonsaiDerivable, BonsaiDerivedMapping,
+    DerivedDataTypesConfig,
 };
-use mononoke_types::BonsaiChangeset;
+use futures::stream::{self, StreamExt, TryStreamExt};
+use mononoke_types::{BonsaiChangeset, ChangesetId};
 
 use crate::ChangesetInfo;
 
@@ -34,6 +37,33 @@ impl BonsaiDerivable for ChangesetInfo {
     ) -> Result<Self, Error> {
         let csid = bonsai.get_changeset_id();
         Ok(ChangesetInfo::new(csid, bonsai))
+    }
+
+    async fn batch_derive<BatchMapping>(
+        ctx: &CoreContext,
+        repo: &BlobRepo,
+        csids: Vec<ChangesetId>,
+        mapping: &BatchMapping,
+    ) -> Result<HashMap<ChangesetId, Self>, Error>
+    where
+        BatchMapping: BonsaiDerivedMapping<Value = Self> + Send + Sync + Clone + 'static,
+    {
+        let cs_infos = stream::iter(csids.into_iter().map(|csid| async move {
+            let bonsai = csid.load(ctx, repo.blobstore()).await?;
+            let cs_info = ChangesetInfo::new(csid, bonsai);
+            Ok::<_, Error>((csid, cs_info))
+        }))
+        .buffered(100)
+        .try_collect::<HashMap<_, _>>()
+        .await?;
+
+        stream::iter(cs_infos.iter().map(Ok))
+            .try_for_each_concurrent(100, |(csid, cs_info)| async move {
+                mapping.put(ctx.clone(), *csid, cs_info.clone()).await
+            })
+            .await?;
+
+        Ok(cs_infos)
     }
 }
 
@@ -74,10 +104,13 @@ mod test {
     use derived_data::BonsaiDerived;
     use fbinit::FacebookInit;
     use fixtures::linear;
+    use futures::compat::Stream01CompatExt;
     use mercurial_types::HgChangesetId;
     use mononoke_types::BonsaiChangeset;
+    use revset::AncestorsNodeStream;
     use std::collections::BTreeMap;
     use std::str::FromStr;
+    use tests_utils::resolve_cs_id;
     use tokio_compat::runtime::Runtime;
 
     #[fbinit::test]
@@ -118,5 +151,31 @@ mod test {
             info.extra().collect::<BTreeMap<_, _>>(),
             bcs.extra().collect::<BTreeMap<_, _>>()
         );
+    }
+
+    #[fbinit::compat_test]
+    async fn batch_derive(fb: FacebookInit) -> Result<(), Error> {
+        let ctx = CoreContext::test_mock(fb);
+        let repo = linear::getrepo(fb).await;
+        let master_cs_id = resolve_cs_id(&ctx, &repo, "master").await?;
+
+        let mapping = ChangesetInfo::default_mapping(&ctx, &repo)?;
+        let mut cs_ids =
+            AncestorsNodeStream::new(ctx.clone(), &repo.get_changeset_fetcher(), master_cs_id)
+                .compat()
+                .try_collect::<Vec<_>>()
+                .await?;
+        cs_ids.reverse();
+        let cs_infos = ChangesetInfo::batch_derive(&ctx, &repo, cs_ids.clone(), &mapping).await?;
+
+        for cs_id in cs_ids {
+            let bonsai = cs_id.load(&ctx, repo.blobstore()).await?;
+            let cs_info = cs_infos
+                .get(&cs_id)
+                .expect("ChangesetInfo should have been derived");
+            check_info(cs_info, &bonsai);
+        }
+
+        Ok(())
     }
 }
