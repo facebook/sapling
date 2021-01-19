0d52cff58cadc063901deff149681ca61bc22b89
Kostia Balytskyi <ikostia@fb.com>
2021-01-19 07:11:44 -0800

    iterhelpers: add chunk_by_accumulation
    Summary:
    This implements chunking of the original iterator by saturation of some
    accumulator until a condition starts to be satisfied.
    
    Note: I tried looking through `Vec`, `itertools` and `Iterator` methods, and
    did not find anything that would allow me to express this easily.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D25947821
    
    fbshipit-source-id: 9e4dd738ecd2ab06ebb69123e4a03059f96b3fb6

--- eden/mononoke/common/iterhelpers/src/lib.rs
+++ eden/mononoke/common/iterhelpers/src/lib.rs
@@ -29,10 +29,78 @@ where
     }
 }
 
+/// Break iterable down into chunks, by saturating an accumulator
+pub fn chunk_by_accumulation<T, A: Copy>(
+    items: impl IntoIterator<Item = T>,
+    zero: A,
+    add: impl Fn(A, &T) -> A,
+    overflows: impl Fn(A) -> bool,
+) -> Vec<Vec<T>> {
+    let mut res = vec![];
+    let mut acc = zero;
+    let mut current = vec![];
+    for item in items.into_iter() {
+        let new_acc = add(acc, &item);
+        if current.is_empty() || !overflows(new_acc) {
+            current.push(item);
+            acc = new_acc;
+        } else {
+            res.push(current);
+            acc = add(zero, &item);
+            current = vec![item];
+        }
+    }
+
+    // current can only be empty
+    // if the whole `items` was empty
+    if !current.is_empty() {
+        res.push(current);
+    }
+
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use anyhow::{anyhow, Error};
+
+    #[test]
+    fn test_chunk_by_accumulation_simple() {
+        let v = vec![1, 2, 3, 1, 1, 1];
+        let chunks = chunk_by_accumulation(v, 0, |a, x| a + x, |a| a > 3);
+        assert_eq!(chunks, vec![vec![1, 2], vec![3], vec![1, 1, 1]]);
+    }
+
+    #[test]
+    fn test_chunk_by_accumulation_one_item_overflows() {
+        // even though 3 on its own overflows the accumulator,
+        // we don't drop it
+        let v = vec![1, 2, 3, 1, 1, 1];
+        let chunks = chunk_by_accumulation(v, 0, |a, x| a + x, |a| a >= 3);
+        assert_eq!(chunks, vec![vec![1], vec![2], vec![3], vec![1, 1], vec![1]]);
+    }
+
+    #[test]
+    fn test_chunk_by_accumulation_empty() {
+        let v = vec![];
+        let chunks = chunk_by_accumulation(v, 0, |a, x| a + x, |a| a > 3);
+        assert!(chunks.is_empty());
+    }
+
+    #[test]
+    fn test_chunk_by_accumulation_single_item() {
+        let v = vec![5];
+        let chunks = chunk_by_accumulation(v, 0, |a, x| a + x, |a| a > 3);
+        assert_eq!(chunks, vec![vec![5]]);
+    }
+
+    #[test]
+    fn test_chunk_by_accumulation_all_fit_in_single_chunk() {
+        let v = vec![1, 2, 3, 1, 1, 1];
+        let chunks = chunk_by_accumulation(v.clone(), 0, |a, x| a + x, |a| a > 1000);
+        assert_eq!(chunks, vec![v]);
+    }
 
     #[test]
     fn test_success() {
