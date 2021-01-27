1076f2dc58114cb5936964aba65148b591815a48
Adam Simpkins <simpkins@fb.com>
2021-01-27 12:02:25 -0800

    improve the glob tests
    Summary:
    The `assert_glob()` function in the glob tests previously only checked that
    the expected number of results were returned.  This updates the tests to
    actually verify the returned paths.  This also adds a few checks that verify
    the behavior when the `wantDtype` parameter is True
    
    Reviewed By: chadaustin
    
    Differential Revision: D18903993
    
    fbshipit-source-id: 5ed4c88160ffbda2e5fdcf089e210fda92868d56

--- eden/integration/glob_test.py
+++ eden/integration/glob_test.py
@@ -4,7 +4,8 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
-from typing import List, Optional
+import logging
+from typing import List, Optional, Tuple
 
 from facebook.eden.ttypes import EdenError, EdenErrorType, GlobParams
 
@@ -52,63 +53,64 @@ class GlobTest(testcase.EdenRepoTest):
         self.addCleanup(self.client.close)
 
     def test_exact_path_component_match(self) -> None:
-        self.assert_glob(["hello"], [b"hello"])
-        self.assert_glob(["ddir/subdir/.dotfile"], [b"ddir/subdir/.dotfile"])
+        self.assert_glob(["hello"], ["hello"])
+        self.assert_glob(["ddir/subdir/.dotfile"], ["ddir/subdir/.dotfile"])
 
     def test_wildcard_path_component_match(self) -> None:
-        self.assert_glob(["hel*"], [b"hello"])
-        self.assert_glob(["ad*"], [b"adir"])
-        self.assert_glob(["a*/file"], [b"adir/file"])
+        self.assert_glob(["hel*"], ["hello"])
+        self.assert_glob(["ad*"], ["adir"])
+        self.assert_glob_with_dtypes(["ad*"], [("adir", "d")])
+        self.assert_glob(["a*/file"], ["adir/file"])
+        self.assert_glob_with_dtypes(["a*/file"], [("adir/file", "f")])
 
     def test_no_accidental_substring_match(self) -> None:
         self.assert_glob(["hell"], [], msg="No accidental substring match")
 
     def test_match_all_files_in_directory(self) -> None:
-        self.assert_glob(["bdir/*"], [b"bdir/file", b"bdir/otherfile"])
+        self.assert_glob(["bdir/*"], ["bdir/file", "bdir/otherfile"])
 
     def test_match_all_files_in_directory_with_dotfile(self) -> None:
-        self.assert_glob(["ddir/subdir/*"], [b"ddir/subdir/notdotfile"])
+        self.assert_glob(["ddir/subdir/*"], ["ddir/subdir/notdotfile"])
 
     def test_overlapping_globs(self) -> None:
         self.assert_glob(
             ["adir/*", "**/file"],
-            [b"adir/file", b"bdir/file"],
+            ["adir/file", "bdir/file"],
             msg="De-duplicate results from multiple globs",
         )
 
     def test_recursive_wildcard_prefix(self) -> None:
-        self.assert_glob(["**/file"], [b"adir/file", b"bdir/file"])
+        self.assert_glob(["**/file"], ["adir/file", "bdir/file"])
 
     def test_recursive_wildcard_suffix(self) -> None:
-        self.assert_glob(["adir/**"], [b"adir/file"])
-        self.assert_glob(["adir/**/*"], [b"adir/file"])
+        self.assert_glob(["adir/**"], ["adir/file"])
+        self.assert_glob(["adir/**/*"], ["adir/file"])
 
     def test_recursive_wildcard_suffix_with_dotfile(self) -> None:
         self.assert_glob(
-            ["ddir/**"], [b"ddir/notdotfile", b"ddir/subdir", b"ddir/subdir/notdotfile"]
+            ["ddir/**"], ["ddir/notdotfile", "ddir/subdir", "ddir/subdir/notdotfile"]
         )
         self.assert_glob(
             ["ddir/**"],
             [
-                b"ddir/subdir",
-                b"ddir/subdir/.dotfile",
-                b"ddir/notdotfile",
-                b"ddir/subdir/notdotfile",
+                "ddir/notdotfile",
+                "ddir/subdir",
+                "ddir/subdir/.dotfile",
+                "ddir/subdir/notdotfile",
             ],
             include_dotfiles=True,
         )
 
         self.assert_glob(
-            ["ddir/**/*"],
-            [b"ddir/notdotfile", b"ddir/subdir", b"ddir/subdir/notdotfile"],
+            ["ddir/**/*"], ["ddir/notdotfile", "ddir/subdir", "ddir/subdir/notdotfile"]
         )
         self.assert_glob(
             ["ddir/**/*"],
             [
-                b"ddir/subdir",
-                b"ddir/subdir/.dotfile",
-                b"ddir/notdotfile",
-                b"ddir/subdir/notdotfile",
+                "ddir/notdotfile",
+                "ddir/subdir",
+                "ddir/subdir/.dotfile",
+                "ddir/subdir/notdotfile",
             ],
             include_dotfiles=True,
         )
@@ -117,14 +119,14 @@ class GlobTest(testcase.EdenRepoTest):
         self.assert_glob(
             ["java/com/**/*.java"],
             [
-                b"java/com/example/Example.java",
-                b"java/com/example/foo/Foo.java",
-                b"java/com/example/foo/bar/Bar.java",
-                b"java/com/example/foo/bar/baz/Baz.java",
+                "java/com/example/Example.java",
+                "java/com/example/foo/Foo.java",
+                "java/com/example/foo/bar/Bar.java",
+                "java/com/example/foo/bar/baz/Baz.java",
             ],
         )
         self.assert_glob(
-            ["java/com/example/*/*.java"], [b"java/com/example/foo/Foo.java"]
+            ["java/com/example/*/*.java"], ["java/com/example/foo/Foo.java"]
         )
 
     def test_malformed_query(self) -> None:
@@ -152,37 +154,37 @@ class GlobTest(testcase.EdenRepoTest):
         self.assertEqual(EdenErrorType.ARGUMENT_ERROR, ctx.exception.errorType)
 
     def test_glob_on_non_current_commit(self) -> None:
-        self.assert_glob(["hello"], [b"hello"], commits=[bytes.fromhex(self.commit0)])
-        self.assert_glob(["hola"], [b"hola"], commits=[bytes.fromhex(self.commit0)])
+        self.assert_glob(["hello"], ["hello"], commits=[bytes.fromhex(self.commit0)])
+        self.assert_glob(["hola"], ["hola"], commits=[bytes.fromhex(self.commit0)])
 
     def test_glob_multiple_commits(self) -> None:
         self.assert_glob(
             ["hello"],
-            [b"hello", b"hello"],
+            ["hello", "hello"],
             commits=[bytes.fromhex(self.commit0), bytes.fromhex(self.commit1)],
         )
         self.assert_glob(
             ["h*"],
-            [b"hello", b"hello", b"hola"],
+            ["hello", "hello", "hola"],
             commits=[bytes.fromhex(self.commit0), bytes.fromhex(self.commit1)],
         )
         self.assert_glob(
             ["a*/*ile"],
-            [b"adir/file", b"adir/phile"],
+            ["adir/file", "adir/phile"],
             commits=[bytes.fromhex(self.commit0), bytes.fromhex(self.commit1)],
         )
 
     def test_prefetch_matching_files(self) -> None:
-        self.assert_glob(["hello"], [b"hello"], prefetching=True)
+        self.assert_glob(["hello"], ["hello"], prefetching=True)
         self.assert_glob(
             ["hello"],
-            [b"hello"],
+            ["hello"],
             prefetching=True,
             commits=[bytes.fromhex(self.commit0)],
         )
         self.assert_glob(
             ["hello"],
-            [b"hello", b"hello"],
+            ["hello", "hello"],
             prefetching=True,
             commits=[bytes.fromhex(self.commit0), bytes.fromhex(self.commit1)],
         )
@@ -190,13 +192,13 @@ class GlobTest(testcase.EdenRepoTest):
     def test_simple_matching_commit(self) -> None:
         self.assert_glob(
             ["hello"],
-            expected_matches=[b"hello"],
+            expected_matches=["hello"],
             expected_commits=[bytes.fromhex(self.commit1)],
         )
 
         self.assert_glob(
             ["hello"],
-            expected_matches=[b"hello"],
+            expected_matches=["hello"],
             expected_commits=[bytes.fromhex(self.commit0)],
             commits=[bytes.fromhex(self.commit0)],
         )
@@ -204,7 +206,7 @@ class GlobTest(testcase.EdenRepoTest):
     def test_duplicate_file_multiple_commits(self) -> None:
         self.assert_glob(
             ["hello"],
-            expected_matches=[b"hello", b"hello"],
+            expected_matches=["hello", "hello"],
             expected_commits=[
                 bytes.fromhex(self.commit0),
                 bytes.fromhex(self.commit1),
@@ -226,7 +228,7 @@ class GlobTest(testcase.EdenRepoTest):
     def assert_glob(
         self,
         globs: List[str],
-        expected_matches: List[bytes],
+        expected_matches: List[str],
         include_dotfiles: bool = False,
         msg: Optional[str] = None,
         commits: Optional[List[bytes]] = None,
@@ -240,11 +242,56 @@ class GlobTest(testcase.EdenRepoTest):
             prefetchFiles=prefetching,
             revisions=commits,
         )
-        self.assertCountEqual(
-            expected_matches, self.client.globFiles(params).matchingFiles, msg=msg
+        result = self.client.globFiles(params)
+        path_results = (
+            path.decode("utf-8", errors="surrogateescape")
+            for path in result.matchingFiles
         )
+        self.assertEqual(expected_matches, sorted(path_results), msg=msg)
+        self.assertFalse(result.dtypes)
 
         if expected_commits:
             self.assertCountEqual(
                 expected_commits, self.client.globFiles(params).originHashes, msg=msg
             )
+
+    def assert_glob_with_dtypes(
+        self,
+        globs: List[str],
+        expected_matches: List[Tuple[str, str]],
+        include_dotfiles: bool = False,
+        msg: Optional[str] = None,
+    ) -> None:
+        params = GlobParams(
+            self.mount_path_bytes,
+            globs,
+            includeDotfiles=include_dotfiles,
+            wantDtype=True,
+        )
+        result = self.client.globFiles(params)
+        actual_results = zip(
+            (
+                path.decode("utf-8", errors="surrogateescape")
+                for path in result.matchingFiles
+            ),
+            (_dtype_to_str(dtype) for dtype in result.dtypes),
+        )
+        self.assertEqual(expected_matches, sorted(actual_results), msg=msg)
+
+
+# Mac and Linux fortunately appear to share the same dtype definitions
+_DT_DIR = 4
+_DT_REG = 8
+_DT_LNK = 10
+
+
+def _dtype_to_str(value: int) -> str:
+    if value == _DT_REG:
+        return "f"
+    elif value == _DT_DIR:
+        return "d"
+    elif value == _DT_LNK:
+        return "l"
+    else:
+        logging.error(f"unexpected dtype {value!r}")
+        return "?"
