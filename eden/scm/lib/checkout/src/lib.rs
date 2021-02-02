a0fd97854bb4d6b4080b72c312f062cefb6c1264
Andrey Chursin <achursin@fb.com>
2021-02-02 15:32:17 -0800

    checkout: data structure for CheckoutPlan
    Reviewed By: quark-zju
    
    Differential Revision: D26189792
    
    fbshipit-source-id: 9afa1682055d088e5c6e4c166f73d6e0b836806a

--- eden/scm/lib/checkout/src/lib.rs
+++ eden/scm/lib/checkout/src/lib.rs
@@ -4,3 +4,26 @@
  * This software may be used and distributed according to the terms of the
  * GNU General Public License version 2.
  */
+
+use manifest::FileType;
+use types::{HgId, RepoPathBuf};
+
+/// Contains lists of files to be removed / updated during checkout.
+#[allow(dead_code)]
+pub struct CheckoutPlan {
+    /// Files to be removed.
+    remove: Vec<RepoPathBuf>,
+    /// Files to be updated or created.
+    update: Vec<UpdateFileAction>,
+}
+
+/// Contains update action on the file.
+#[allow(dead_code)]
+struct UpdateFileAction {
+    /// Path to file.
+    path: RepoPathBuf,
+    /// If content has changed, HgId of new content.
+    content: Option<HgId>,
+    /// New file type.
+    file_type: FileType,
+}
