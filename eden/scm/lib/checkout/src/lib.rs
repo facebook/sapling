6be082f3bdfa54047d10ab97a82625a2a5bb323c
Andrey Chursin <achursin@fb.com>
2021-02-02 15:32:17 -0800

    checkout: implement CheckoutPlan::from_diff
    Reviewed By: quark-zju
    
    Differential Revision: D26191428
    
    fbshipit-source-id: d26dd58a1bd9a3bab27f38abbd15f0405c646294

--- eden/scm/lib/checkout/src/lib.rs
+++ eden/scm/lib/checkout/src/lib.rs
@@ -5,7 +5,8 @@
  * GNU General Public License version 2.
  */
 
-use manifest::FileType;
+use anyhow::Result;
+use manifest::{DiffEntry, DiffType, FileType};
 use types::{HgId, RepoPathBuf};
 
 /// Contains lists of files to be removed / updated during checkout.
@@ -26,4 +27,36 @@ struct UpdateFileAction {
     content: Option<HgId>,
     /// New file type.
     file_type: FileType,
+}
+
+impl CheckoutPlan {
+    /// Processes diff into checkout plan.
+    /// Left in the diff is a current commit.
+    /// Right is a commit to be checked out.
+    pub fn from_diff<D: Iterator<Item = Result<DiffEntry>>>(iter: D) -> Result<Self> {
+        let mut remove = vec![];
+        let mut update = vec![];
+        for item in iter {
+            let item: DiffEntry = item?;
+            match item.diff_type {
+                DiffType::LeftOnly(_) => remove.push(item.path),
+                DiffType::RightOnly(meta) => update.push(UpdateFileAction {
+                    path: item.path,
+                    content: Some(meta.hgid),
+                    file_type: meta.file_type,
+                }),
+                DiffType::Changed(old, new) => update.push(UpdateFileAction {
+                    path: item.path,
+                    content: if_changed(old.hgid, new.hgid),
+                    file_type: new.file_type,
+                }),
+            };
+        }
+        Ok(Self { remove, update })
+    }
+}
+
+/// Returns Some(new) if old != new, None otherwise.
+fn if_changed<T: PartialEq>(old: T, new: T) -> Option<T> {
+    if old == new { None } else { Some(new) }
 }
