39928181369cafde932d900799e214076a703653
Andrey Chursin <achursin@fb.com>
2021-02-02 23:12:12 -0800

    checkout: split updates with and without content fetch
    Summary: This is needed becase later during checkout list of updates that needs fetch will go into storage stream
    
    Reviewed By: quark-zju
    
    Differential Revision: D26209983
    
    fbshipit-source-id: 9fb54d48c6f0afc4fb67320aafc2e981c96ab5a9

--- eden/scm/lib/checkout/src/lib.rs
+++ eden/scm/lib/checkout/src/lib.rs
@@ -5,7 +5,7 @@
  * GNU General Public License version 2.
  */
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use manifest::{DiffEntry, DiffType, FileType};
 use types::{HgId, RepoPathBuf};
 
@@ -14,19 +14,30 @@ use types::{HgId, RepoPathBuf};
 pub struct CheckoutPlan {
     /// Files to be removed.
     remove: Vec<RepoPathBuf>,
-    /// Files to be updated or created.
-    update: Vec<UpdateFileAction>,
+    /// Files that needs their content updated.
+    update_content: Vec<UpdateContentAction>,
+    /// Files that only need X flag updated.
+    update_meta: Vec<UpdateMetaAction>,
 }
 
-/// Contains update action on the file.
+/// Update content and (possibly) metadata on the file
 #[allow(dead_code)]
-struct UpdateFileAction {
+struct UpdateContentAction {
     /// Path to file.
     path: RepoPathBuf,
     /// If content has changed, HgId of new content.
-    content: Option<HgId>,
+    content_hgid: HgId,
     /// New file type.
     file_type: FileType,
+}
+
+/// Only update metadata on the file, do not update content
+#[allow(dead_code)]
+struct UpdateMetaAction {
+    /// Path to file.
+    path: RepoPathBuf,
+    /// true if need to set executable flag, false if need to remove it.
+    set_x_flag: bool,
 }
 
 impl CheckoutPlan {
@@ -35,28 +46,50 @@ impl CheckoutPlan {
     /// Right is a commit to be checked out.
     pub fn from_diff<D: Iterator<Item = Result<DiffEntry>>>(iter: D) -> Result<Self> {
         let mut remove = vec![];
-        let mut update = vec![];
+        let mut update_content = vec![];
+        let mut update_meta = vec![];
         for item in iter {
             let item: DiffEntry = item?;
             match item.diff_type {
                 DiffType::LeftOnly(_) => remove.push(item.path),
-                DiffType::RightOnly(meta) => update.push(UpdateFileAction {
+                DiffType::RightOnly(meta) => update_content.push(UpdateContentAction {
                     path: item.path,
-                    content: Some(meta.hgid),
+                    content_hgid: meta.hgid,
                     file_type: meta.file_type,
                 }),
-                DiffType::Changed(old, new) => update.push(UpdateFileAction {
-                    path: item.path,
-                    content: if_changed(old.hgid, new.hgid),
-                    file_type: new.file_type,
-                }),
+                DiffType::Changed(old, new) => {
+                    if old.hgid == new.hgid {
+                        let set_x_flag = match (old.file_type, new.file_type) {
+                            (FileType::Executable, FileType::Regular) => false,
+                            (FileType::Regular, FileType::Executable) => true,
+                            // todo - address this case
+                            // Since this is rare case we are going to handle it by submitting
+                            // delete and then create operation to avoid complexity
+                            (o, n) => bail!(
+                                "Can not update {}: hg id has not changed and file type changed {:?}->{:?}",
+                                item.path,
+                                o,
+                                n
+                            ),
+                        };
+                        update_meta.push(UpdateMetaAction {
+                            path: item.path,
+                            set_x_flag,
+                        });
+                    } else {
+                        update_content.push(UpdateContentAction {
+                            path: item.path,
+                            content_hgid: new.hgid,
+                            file_type: new.file_type,
+                        })
+                    }
+                }
             };
         }
-        Ok(Self { remove, update })
+        Ok(Self {
+            remove,
+            update_content,
+            update_meta,
+        })
     }
-}
-
-/// Returns Some(new) if old != new, None otherwise.
-fn if_changed<T: PartialEq>(old: T, new: T) -> Option<T> {
-    if old == new { None } else { Some(new) }
 }
