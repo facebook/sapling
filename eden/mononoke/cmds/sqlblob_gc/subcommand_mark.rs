acd24deb414c74f12491e094e2b5ac34f0bed063
Simon Farnsworth <simonfar@fb.com>
2021-02-09 11:57:12 -0800

    Make it possible to only set initial generations in sqlblob_gc
    Summary:
    It looks like the load created by GC marking can cause setting the initial generation for new blobs to fail as the DB is loaded and cannot lock the `chunk_generation` table.
    
    Make it possible to run that before starting the mark process, and to run the sweep without marking new blobs. This will allow the Chronos job to avoid running both the initial generation set *and* the sweep concurrently, while still ensuring that any manual runs outside Chronos do both by default.
    
    Reviewed By: ahornby
    
    Differential Revision: D26232760
    
    fbshipit-source-id: 94122c7b72588201800a45e41b26fbd03c6656ad

--- eden/mononoke/cmds/sqlblob_gc/subcommand_mark.rs
+++ eden/mononoke/cmds/sqlblob_gc/subcommand_mark.rs
@@ -8,7 +8,7 @@
 use std::{ops::Range, sync::Arc, time::Duration};
 
 use anyhow::{anyhow, Result};
-use clap::{App, ArgMatches, SubCommand};
+use clap::{App, Arg, ArgMatches, SubCommand};
 use fbinit::FacebookInit;
 use futures::{
     channel::mpsc,
@@ -16,18 +16,36 @@ use futures::{
     stream::{self, StreamExt, TryStreamExt},
 };
 use rand::{thread_rng, Rng};
-use slog::Logger;
+use slog::{info, Logger};
 use tokio::time::delay_for;
 
 use sqlblob::Sqlblob;
 
 pub const MARK_SAFE: &str = "mark";
+const ARG_INITIAL_GENERATION_ONLY: &str = "initial-generation-only";
+const ARG_SKIP_INITIAL_GENERATION: &str = "skip-initial-generation";
+
 const MIN_RETRY_DELAY: Duration = Duration::from_millis(10);
 const MAX_RETRY_DELAY: Duration = Duration::from_millis(100);
 const RETRIES: usize = 3;
 
 pub fn build_subcommand<'a, 'b>() -> App<'a, 'b> {
-    SubCommand::with_name(MARK_SAFE).about("mark referenced blobs as not safe to delete")
+    SubCommand::with_name(MARK_SAFE)
+        .about("mark referenced blobs as not safe to delete")
+        .arg(
+            Arg::with_name(ARG_INITIAL_GENERATION_ONLY)
+                .long(ARG_INITIAL_GENERATION_ONLY)
+                .takes_value(false)
+                .required(false)
+                .help("Only set generation on blobs that have no generation set yet. Do not do a full sweep.")
+        )
+        .arg(
+            Arg::with_name(ARG_SKIP_INITIAL_GENERATION)
+                .long(ARG_SKIP_INITIAL_GENERATION)
+                .takes_value(false)
+                .required(false)
+                .help("Only do the sweep; do not set generation on blobs with no generation set yet.")
+        )
 }
 
 async fn handle_one_key(key: String, store: Arc<Sqlblob>) -> Result<()> {
@@ -72,22 +90,31 @@ async fn handle_initial_generation(store: &Sqlblob, shard: usize) -> Result<()> 
 
 pub async fn subcommand_mark<'a>(
     _fb: FacebookInit,
-    _logger: Logger,
-    _sub_matches: &'a ArgMatches<'_>,
+    logger: Logger,
+    sub_matches: &'a ArgMatches<'_>,
     max_parallelism: usize,
     sqlblob: Sqlblob,
     shard_range: Range<usize>,
 ) -> Result<()> {
-    let set_initial_generation_futures: Vec<_> = shard_range
-        .clone()
-        .map(|shard| Ok(handle_initial_generation(&sqlblob, shard)))
-        .collect();
-    stream::iter(set_initial_generation_futures.into_iter())
-        .try_for_each_concurrent(max_parallelism, |fut| fut)
-        .await?;
+    if !sub_matches.is_present(ARG_SKIP_INITIAL_GENERATION) {
+        info!(logger, "Starting initial generation set");
+        let set_initial_generation_futures: Vec<_> = shard_range
+            .clone()
+            .map(|shard| Ok(handle_initial_generation(&sqlblob, shard)))
+            .collect();
+        stream::iter(set_initial_generation_futures.into_iter())
+            .try_for_each_concurrent(max_parallelism, |fut| fut)
+            .await?;
+        info!(logger, "Completed initial generation set");
+    }
+
+    if sub_matches.is_present(ARG_INITIAL_GENERATION_ONLY) {
+        return Ok(());
+    }
 
     let sqlblob = Arc::new(sqlblob);
 
+    info!(logger, "Starting sweep");
     // Set up a task to process each key in parallel in its own task.
     let (key_channel, processor) = {
         let sqlblob = Arc::clone(&sqlblob);
@@ -107,6 +134,7 @@ pub async fn subcommand_mark<'a>(
 
     // Foreach shard in shard_range
     for shard in shard_range {
+        info!(logger, "Starting sweep on data keys from shard {}", shard);
         let res = sqlblob
             .get_keys_from_shard(shard)
             .forward(key_channel.clone().sink_err_into())
@@ -122,5 +150,7 @@ pub async fn subcommand_mark<'a>(
     // Drop the spare sender so that the processor task can exit
     std::mem::drop(key_channel);
 
-    processor.await?
+    processor.await??;
+    info!(logger, "Completed all sweeps");
+    Ok(())
 }
