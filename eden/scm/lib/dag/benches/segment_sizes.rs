ceb965456bf4d2770912f56908ca39925e46ffea
Jun Wu <quark@fb.com>
2021-02-10 12:26:22 -0800

    dag: fix benches/segment_sizes
    Summary:
    Benchmark code is out of sync.  Fix it.
    Two example runs:
    
    Run 1:
    
      segments: 50039  log len: 1237879
      ancestor calcuation segment_size=4                449.747 ms
      segments: 41174  log len: 999428
      ancestor calcuation segment_size=8                606.643 ms
      segments: 39708  log len: 958176
      ancestor calcuation segment_size=10               612.112 ms
      segments: 38816  log len: 932717
      ancestor calcuation segment_size=12               684.228 ms
      segments: 38138  log len: 911815
      ancestor calcuation segment_size=14               682.280 ms
      segments: 37642  log len: 895854
      ancestor calcuation segment_size=16               727.111 ms
      segments: 37279  log len: 884784
      ancestor calcuation segment_size=18               654.083 ms
      segments: 37012  log len: 876470
      ancestor calcuation segment_size=20               641.833 ms
      segments: 36794  log len: 869558
      ancestor calcuation segment_size=22               698.835 ms
      segments: 36600  log len: 863383
      ancestor calcuation segment_size=24               752.355 ms
      segments: 36128  log len: 847833
      ancestor calcuation segment_size=32               726.615 ms
      segments: 35466  log len: 825696
      ancestor calcuation segment_size=64               770.747 ms
      segments: 35154  log len: 814629
      ancestor calcuation segment_size=128                1.075 s
    
    Run 2:
    
      segments: 50039  log len: 1237879
      ancestor calcuation segment_size=4                423.746 ms
      segments: 41174  log len: 999428
      ancestor calcuation segment_size=8                571.049 ms
      segments: 39708  log len: 958176
      ancestor calcuation segment_size=10               610.347 ms
      segments: 38816  log len: 932717
      ancestor calcuation segment_size=12               656.025 ms
      segments: 38138  log len: 911815
      ancestor calcuation segment_size=14               647.035 ms
      segments: 37642  log len: 895854
      ancestor calcuation segment_size=16               674.061 ms
      segments: 37279  log len: 884784
      ancestor calcuation segment_size=18               687.186 ms
      segments: 37012  log len: 876470
      ancestor calcuation segment_size=20               635.952 ms
      segments: 36794  log len: 869558
      ancestor calcuation segment_size=22               679.665 ms
      segments: 36600  log len: 863383
      ancestor calcuation segment_size=24               781.148 ms
      segments: 36128  log len: 847833
      ancestor calcuation segment_size=32               741.290 ms
      segments: 35466  log len: 825696
      ancestor calcuation segment_size=64               761.951 ms
      segments: 35154  log len: 814629
      ancestor calcuation segment_size=128                1.049 s
    
    Seems a good default can is in the 10 to 20 range.
    
    Reviewed By: sfilipco
    
    Differential Revision: D26360560
    
    fbshipit-source-id: 1a2c0d97fe55c9f7e4621ab87da26072cd854bf8

--- eden/scm/lib/dag/benches/segment_sizes.rs
+++ eden/scm/lib/dag/benches/segment_sizes.rs
@@ -6,14 +6,13 @@
  */
 
 use dag::idmap::IdMapAssignHead;
-use dag::idmap::IdMapBuildParents;
 use dag::ops::Persist;
 use dag::{idmap::IdMap, Group, Id, IdDag, VertexName};
-use minibench::{
-    bench, elapsed,
-    measure::{self, Measure},
-};
+use minibench::{bench, elapsed};
+use nonblocking::non_blocking_result as nbr;
 use tempfile::tempdir;
+
+type ParentsFunc<'a> = Box<dyn Fn(VertexName) -> dag::Result<Vec<VertexName>> + Send + Sync + 'a>;
 
 fn main() {
     let parents = bindag::parse_bindag(bindag::MOZILLA);
@@ -32,37 +31,31 @@ fn main() {
 
     let id_map_dir = tempdir().unwrap();
     let mut id_map = IdMap::open(id_map_dir.path()).unwrap();
-    id_map
-        .assign_head(head_name.clone(), &parents_by_name, Group::MASTER)
-        .unwrap();
-
-    let head_id = id_map.find_id_by_name(head_name.as_ref()).unwrap().unwrap();
-    let parents_by_id = id_map.build_get_parents_by_id(&parents_by_name);
+    let prepared_segments = nbr(id_map.assign_head(
+        head_name,
+        &(Box::new(parents_by_name) as ParentsFunc),
+        Group::MASTER,
+    ))
+    .unwrap();
 
     // Test the size, and generation speed, and ancestor calcuation speed
     // with some different segment sizes.
     for &segment_size in [4, 8, 10, 12, 14, 16, 18, 20, 22, 24, 32, 64, 128].iter() {
         let dag_dir = tempdir().unwrap();
-        let mut built = false;
-        bench(format!("building segment_size={}", segment_size), || {
-            built = true;
-            measure::Both::<measure::WallClock, String>::measure(|| {
-                let mut dag = IdDag::open(&dag_dir.path()).unwrap();
-                dag.set_new_segment_size(segment_size);
-                let mut syncable = dag.prepare_filesystem_sync().unwrap();
-                let segment_len = syncable
-                    .build_segments_volatile(head_id, &parents_by_id)
-                    .unwrap();
-                syncable.sync().unwrap();
-                let log_len = dag_dir.path().join("log").metadata().unwrap().len();
-                format!("segments: {}  log len: {}", segment_len, log_len)
-            })
-        });
+        let mut dag = IdDag::open(&dag_dir.path()).unwrap();
+        dag.set_new_segment_size(segment_size);
+        let mut syncable = dag.prepare_filesystem_sync().unwrap();
+        let segment_len = syncable
+            .build_segments_volatile_from_prepared_flat_segments(&prepared_segments)
+            .unwrap();
+        syncable.sync().unwrap();
+
+        let log_len = dag_dir.path().join("log").metadata().unwrap().len();
+        eprintln!("segments: {}  log len: {}", segment_len, log_len);
 
         bench(
             format!("ancestor calcuation segment_size={}", segment_size),
             || {
-                assert!(built, "segments must be built to run this benchmak");
                 let dag = IdDag::open(&dag_dir.path()).unwrap();
                 elapsed(|| {
                     for i in (0..parents.len() as u64).step_by(10079) {
