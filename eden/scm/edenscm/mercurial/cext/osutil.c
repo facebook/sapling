d7a4ce47838c3d0303b7f4634ce5f8fb2aebe6b9
Durham Goode <durham@fb.com>
2021-02-11 09:44:03 -0800

    py3: remove duplicate constant definition
    Summary: This is defined else where in Python 3, so let's get rid of it here.
    
    Reviewed By: quark-zju
    
    Differential Revision: D26381054
    
    fbshipit-source-id: 9746d2c53f83209d9c795ffdd5841d58ef1153ef

--- eden/scm/edenscm/mercurial/cext/osutil.c
+++ eden/scm/edenscm/mercurial/cext/osutil.c
@@ -167,7 +167,9 @@ static int to_python_time(const FILETIME* tm) {
 static PyObject* make_item(const WIN32_FIND_DATAA* fd, int wantstat) {
   PyObject* py_st;
   struct hg_stat* stp;
+#ifndef IS_PY3K
   const int S_IFLNK = 40960; /* stat.S_IFLINK defined by Python on Windows */
+#endif
 
   int kind = (fd->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
       ? ((fd->dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) ? S_IFLNK
