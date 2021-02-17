51a29c5d7ea2a3356062b92c6799ab37dcf59715
Andrey Chursin <achursin@fb.com>
2021-02-17 12:00:18 -0800

    pycheckout: implement checkoutplan::__new__
    Summary: This diff introduces a Python checkoutplan, which will be used later for Python code to interact with the Rust checkout infrastructure
    
    Reviewed By: DurhamG
    
    Differential Revision: D26437080
    
    fbshipit-source-id: fe3517f4e2f15244a7c2cd290a41394e904db51b

--- eden/scm/edenscmnative/bindings/modules/pycheckout/src/lib.rs
+++ eden/scm/edenscmnative/bindings/modules/pycheckout/src/lib.rs
@@ -4,3 +4,43 @@
  * This software may be used and distributed according to the terms of the
  * GNU General Public License version 2.
  */
+
+#![allow(non_camel_case_types)]
+
+use checkout::CheckoutPlan;
+use cpython::*;
+use cpython_ext::ResultPyErrExt;
+use manifest_tree::Diff;
+use pathmatcher::{AlwaysMatcher, Matcher};
+use pymanifest::treemanifest;
+use pypathmatcher::PythonMatcher;
+
+pub fn init_module(py: Python, package: &str) -> PyResult<PyModule> {
+    let name = [package, "checkout"].join(".");
+    let m = PyModule::new(py, &name)?;
+    m.add_class::<checkoutplan>(py)?;
+    Ok(m)
+}
+
+py_class!(class checkoutplan |py| {
+    data plan: CheckoutPlan;
+
+    def __new__(
+        _cls,
+        current_manifest: &treemanifest,
+        target_manifest: &treemanifest,
+        matcher: Option<PyObject> = None,
+    ) -> PyResult<checkoutplan> {
+        let matcher: Box<dyn Matcher> = match matcher {
+            None => Box::new(AlwaysMatcher::new()),
+            Some(pyobj) => Box::new(PythonMatcher::new(py, pyobj)),
+        };
+
+        let current = current_manifest.borrow_underlying(py);
+        let target = target_manifest.borrow_underlying(py);
+        let diff = Diff::new(&current, &target, &matcher);
+        let plan = CheckoutPlan::from_diff(diff).map_pyerr(py)?;
+        checkoutplan::create_instance(py, plan)
+    }
+
+});
