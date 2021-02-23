279eb2b5382e407666ba9cfe2c2c812586cd258e
Lukas Piatkowski <luk@fb.com>
2021-02-23 11:35:00 -0800

    autocargo v1: changes to how thrift-related generation is done to match v2
    Summary: Done some reordering of fields in Cargo.toml, added test and doctest = false, name of the target that generated the Cargo.toml file and sorted the cratemap.
    
    Reviewed By: ahornby
    
    Differential Revision: D26581275
    
    fbshipit-source-id: 4c363369438c72d43d8ccf4799f103ff092457cc

--- eden/mononoke/reachabilityindex/if/thrift_build.rs
+++ eden/mononoke/reachabilityindex/if/thrift_build.rs
@@ -11,8 +11,8 @@ fn main() {
     let out_dir: &Path = out_dir.as_ref();
     fs::write(
         out_dir.join("cratemap"),
-        "skiplist crate
-mononoke_types_thrift mononoke_types_thrift",
+        "mononoke_types_thrift mononoke_types_thrift
+skiplist crate",
     ).expect("Failed to write cratemap");
 
     let conf = {
