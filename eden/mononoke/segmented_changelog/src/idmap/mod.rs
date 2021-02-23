c6fe0565ec47729e3b2bab6282f92b7cd4fe989b
Stefan Filip <sfilip@fb.com>
2021-02-22 18:14:45 -0800

    segmented_changelog: add OverlayIdMap
    Summary:
    I am not sure if this too abstract. It might be. This however has separation of
    concerns :)
    The goal here is to end up with an in memory IdMap that we write to and read
    from first. For things that are not found in the in memory IdMap we fall back
    to the SqlIdMap. We'll end up with something like:
    `OverlayIdMap(ConcurrentMemIdMap, SqlIdMap)`
    
    Reviewed By: quark-zju
    
    Differential Revision: D26417642
    
    fbshipit-source-id: b2b310306db4dc9fc3427bbf50b19366160882a9

--- eden/mononoke/segmented_changelog/src/idmap/mod.rs
+++ eden/mononoke/segmented_changelog/src/idmap/mod.rs
@@ -116,3 +116,116 @@ impl IdMap for Arc<dyn IdMap> {
         (**self).get_last_entry(ctx).await
     }
 }
+
+pub struct OverlayIdMap {
+    a: Arc<dyn IdMap>,
+    b: Arc<dyn IdMap>,
+}
+
+impl OverlayIdMap {
+    #[allow(dead_code)]
+    pub fn new(a: Arc<dyn IdMap>, b: Arc<dyn IdMap>) -> Self {
+        Self { a, b }
+    }
+}
+
+#[async_trait]
+impl IdMap for OverlayIdMap {
+    async fn insert_many(
+        &self,
+        ctx: &CoreContext,
+        mappings: Vec<(Vertex, ChangesetId)>,
+    ) -> Result<()> {
+        self.a.insert_many(ctx, mappings).await
+    }
+
+    async fn find_many_changeset_ids(
+        &self,
+        ctx: &CoreContext,
+        vertexes: Vec<Vertex>,
+    ) -> Result<HashMap<Vertex, ChangesetId>> {
+        let mut result = self
+            .a
+            .find_many_changeset_ids(ctx, vertexes.clone())
+            .await?;
+        let to_get_b = vertexes
+            .into_iter()
+            .filter(|v| !result.contains_key(&v))
+            .collect();
+        let from_b = self.b.find_many_changeset_ids(ctx, to_get_b).await?;
+        for (v, cs) in from_b {
+            result.insert(v, cs);
+        }
+        Ok(result)
+    }
+
+    async fn find_many_vertexes(
+        &self,
+        ctx: &CoreContext,
+        cs_ids: Vec<ChangesetId>,
+    ) -> Result<HashMap<ChangesetId, Vertex>> {
+        let mut result = self.a.find_many_vertexes(ctx, cs_ids.clone()).await?;
+        let to_get_b = cs_ids
+            .into_iter()
+            .filter(|cs_id| !result.contains_key(&cs_id))
+            .collect();
+        let from_b = self.b.find_many_vertexes(ctx, to_get_b).await?;
+        for (cs, v) in from_b {
+            result.insert(cs, v);
+        }
+        Ok(result)
+    }
+
+    async fn get_last_entry(&self, ctx: &CoreContext) -> Result<Option<(Vertex, ChangesetId)>> {
+        match self.a.get_last_entry(ctx).await? {
+            Some(x) => Ok(Some(x)),
+            None => self.b.get_last_entry(ctx).await,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use maplit::hashmap;
+
+    use fbinit::FacebookInit;
+
+    use mononoke_types_mocks::changesetid::{AS_CSID, ONES_CSID, THREES_CSID, TWOS_CSID};
+
+    #[fbinit::compat_test]
+    async fn test_write_a_read_ab(fb: FacebookInit) -> Result<()> {
+        let ctx = CoreContext::test_mock(fb);
+
+        let a = Arc::new(ConcurrentMemIdMap::new());
+        let b = Arc::new(ConcurrentMemIdMap::new());
+
+        b.insert_many(&ctx, vec![(Vertex(0), AS_CSID), (Vertex(1), ONES_CSID)])
+            .await?;
+
+        let both = OverlayIdMap::new(a, b);
+
+        assert_eq!(
+            both.find_many_changeset_ids(&ctx, vec![Vertex(0), Vertex(1), Vertex(2)])
+                .await?,
+            hashmap![Vertex(0) => AS_CSID, Vertex(1) => ONES_CSID]
+        );
+
+        both.insert_many(&ctx, vec![(Vertex(2), TWOS_CSID), (Vertex(3), THREES_CSID)])
+            .await?;
+        assert_eq!(
+            both.find_many_changeset_ids(&ctx, vec![Vertex(2), Vertex(3)])
+                .await?,
+            hashmap![Vertex(2) => TWOS_CSID, Vertex(3) => THREES_CSID]
+        );
+
+        assert_eq!(
+            both.b
+                .find_many_changeset_ids(&ctx, vec![Vertex(2), Vertex(3)])
+                .await?,
+            hashmap![]
+        );
+        Ok(())
+    }
+}
