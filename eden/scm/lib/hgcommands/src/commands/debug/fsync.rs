4c79cf6af0bb2d8a702bf7c8516d8d20d961f242
Jun Wu <quark@fb.com>
2021-02-27 13:14:33 -0800

    hgcommands: fsync more critical files
    Summary:
    Recently there are reports about "remote/master" pointing to a non-existed
    commit, missing tree data, and general hard-reboot issues. Attempt to reduce
    the chance of losing data by adding the critical paths to the fsync list.
    
    Reviewed By: kulshrax
    
    Differential Revision: D26708225
    
    fbshipit-source-id: ae52534fdbf8d9d35498d30c24bdce7f5d9a96a3

--- eden/scm/lib/hgcommands/src/commands/debug/fsync.rs
+++ eden/scm/lib/hgcommands/src/commands/debug/fsync.rs
@@ -13,12 +13,20 @@ use super::IO;
 pub fn run(_opts: NoOpts, _io: &IO, repo: Repo) -> Result<u8> {
     let store_path = repo.store_path();
     let patterns = [
+        "indexedlogdatastore/*",
+        "indexedloghistorystore/*",
         "00changelog.*",
         "hgcommits/**/*",
-        "metalog/**/*",
+        "segments/**/*",
         "mutation/**/*",
+        "metalog/**/*",
     ];
     fsyncglob::fsync_glob(store_path, &patterns, None);
+
+    let dot_hg_path = repo.dot_hg_path();
+    let patterns = ["treestate/*", "dirstate"];
+    fsyncglob::fsync_glob(dot_hg_path, &patterns, None);
+
     Ok(0)
 }
 
