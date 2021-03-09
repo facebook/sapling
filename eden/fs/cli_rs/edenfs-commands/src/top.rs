ea96dbbc2fafaa673dc511980a42fc89aeefb1fc
Katie Mancini <kmancini@fb.com>
2021-03-08 18:01:56 -0800

    make input loop eden top
    Summary:
    This teaches eden top to read input from the command line which we will use to
    switch between help and main page as well as manipulate the process table.
    
    Similar to the python version of eden top q exits. For now we spit back out all
    of the other input we get.
    
    Reviewed By: fanzeyi
    
    Differential Revision: D26892622
    
    fbshipit-source-id: 30039d2c2475c036c500c6cb1055c0b2d739691a

--- eden/fs/cli_rs/edenfs-commands/src/top.rs
+++ eden/fs/cli_rs/edenfs-commands/src/top.rs
@@ -8,7 +8,16 @@
 //! edenfsctl top
 
 use async_trait::async_trait;
+use std::time::Duration;
 use structopt::StructOpt;
+
+use termwiz::{
+    caps::Capabilities,
+    input::{InputEvent, KeyCode, KeyEvent},
+    terminal::buffered::BufferedTerminal,
+    terminal::{new_terminal, Terminal},
+    Error,
+};
 
 use edenfs_client::EdenFsInstance;
 use edenfs_error::Result;
@@ -28,10 +37,58 @@ pub struct TopCmd {
     refresh_rate: u64,
 }
 
+impl TopCmd {
+    fn main_loop(&self) -> Result<(), Error> {
+        let caps = Capabilities::new_from_env()?;
+        let terminal = new_terminal(caps)?;
+        let mut buf = BufferedTerminal::new(terminal)?;
+
+        buf.add_change("eden top\r\n");
+        buf.flush()?;
+
+        buf.terminal().set_raw_mode()?;
+        loop {
+            match buf
+                .terminal()
+                .poll_input(Some(Duration::from_secs(self.refresh_rate)))
+            {
+                Ok(Some(input)) => match input {
+                    InputEvent::Key(KeyEvent {
+                        key: KeyCode::Char('q'),
+                        ..
+                    }) => {
+                        break;
+                    }
+                    InputEvent::Key(KeyEvent {
+                        key: KeyCode::Char(c),
+                        ..
+                    }) => {
+                        // TODO: delete, this is just used for testing
+                        buf.add_change(format!("{}\r\n", c));
+                        buf.flush()?;
+                    }
+                    _ => {}
+                },
+                Ok(None) => {}
+                Err(e) => {
+                    print!("{:?}\r\n", e);
+                    break;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
 #[async_trait]
 impl crate::Subcommand for TopCmd {
     async fn run(&self, _instance: EdenFsInstance) -> Result<ExitCode> {
-        println!("eden top");
-        Ok(0)
+        match self.main_loop() {
+            Ok(_) => Ok(0),
+            Err(cause) => {
+                println!("Error: {}", cause);
+                Ok(1)
+            }
+        }
     }
 }
