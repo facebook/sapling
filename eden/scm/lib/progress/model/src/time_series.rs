1af1506ba7e560fc754973f52a4b86ecf90d241a
Jun Wu <quark@fb.com>
2021-03-11 17:16:35 -0800

    progress: render time series into scaled values
    Summary: Add an API to render the time series samples into an array that can be further turned into an ASCII graph.
    
    Reviewed By: andll
    
    Differential Revision: D26615170
    
    fbshipit-source-id: 4a2f178869c235e7dda3edef7930a6e04bc4bda3

--- eden/scm/lib/progress/model/src/time_series.rs
+++ eden/scm/lib/progress/model/src/time_series.rs
@@ -136,6 +136,62 @@ impl IoTimeSeries {
         let new = &samples[cur];
         new.bytes_per_second(&old)
     }
+
+    /// Get the count associated with the time series (from the last sample).
+    pub fn count(&self) -> u64 {
+        let len = self.samples.len();
+        let head = (self.samples_head.load(Acquire) + len - 1) % len;
+        self.samples[head].count()
+    }
+
+    /// Are the samples empty (no progress at all)?
+    pub fn is_stale(&self) -> bool {
+        let min = self.samples.iter().map(|s| s.total_bytes()).min();
+        let max = self.samples.iter().map(|s| s.total_bytes()).max();
+        min == max
+    }
+
+    /// Render the time series into an array with max value being `max`.
+    pub fn scaled_speeds(&self, max: u8) -> Vec<u8> {
+        let max = max.max(1);
+        let samples = &self.samples;
+        let head = self.samples_head.load(Acquire);
+        let len = samples.len();
+        let bytes_per_second_list: Vec<u64> = {
+            (1..len)
+                .map(|i| {
+                    let prev = (head + i - 1) % len;
+                    let cur = (prev + 1) % len;
+                    let (i, o) = samples[cur].bytes_per_second(&samples[prev]);
+                    i + o
+                })
+                .collect()
+        };
+
+        let bytes_per_second_max = bytes_per_second_list
+            .iter()
+            .cloned()
+            .max()
+            .unwrap_or(1)
+            .max(1);
+        let bytes_per_second_max = bytes_per_second_max.max(
+            self.max_bytes_per_second
+                .fetch_max(bytes_per_second_max, AcqRel),
+        );
+
+        let values: Vec<u8> = bytes_per_second_list
+            .into_iter()
+            .map(|bytes_per_second| {
+                if bytes_per_second == 0 {
+                    0u8
+                } else {
+                    (1 + ((bytes_per_second - 1) * (max - 1) as u64) / (bytes_per_second_max - 1))
+                        as u8
+                }
+            })
+            .collect();
+        values.into_iter().collect()
+    }
 }
 
 impl fmt::Debug for IoTimeSeries {
@@ -175,5 +231,55 @@ mod tests {
 
         // The sampling task exits when the main series gets dropped.
         sampling_task.await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_ascii_graph() {
+        let count = Arc::new(AtomicU64::new(0));
+        let last = Arc::new(AtomicU64::new(0));
+        let count2 = count.clone();
+        const N: u64 = GAUGE_CHARS.len() as u64;
+        let take_sample = move || -> IoSample {
+            // Generate ▁▂▃▄▅▆▇█ graph.
+            let i = count.fetch_add(1, AcqRel);
+            let ii = i % N;
+            let v = last.fetch_add(ii, AcqRel);
+            IoSample::from_io_bytes(v, 0).test_at(i)
+        };
+
+        let series = IoTimeSeries::new("IO", "files");
+        let interval = Duration::from_millis(10);
+        let sampling_task = tokio::task::spawn(series.async_sampling(take_sample, interval));
+
+        let sample_count = series.samples.len() as u64;
+        loop {
+            let count = count2.load(Acquire);
+            let ascii = series.ascii_graph();
+            assert_eq!(ascii.chars().count() as u64, sample_count - 1);
+            tokio::time::sleep(Duration::from_millis(10)).await;
+            if count <= sample_count + 1 {
+                // Wait for enough samples.
+                continue;
+            }
+            assert!(
+                "▁▂▃▄▅▆▇█ ▁▂▃▄▅▆▇█ ▁▂▃▄▅▆▇█".contains(ascii.trim()),
+                "unexpected graph: {:?}",
+                &ascii
+            );
+            if count > sample_count * 3 + 1 {
+                // Tested enough.
+                break;
+            }
+        }
+
+        drop(series);
+        sampling_task.await.unwrap();
+    }
+
+    impl IoTimeSeries {
+        fn ascii_graph(&self) -> String {
+            let v = self.scaled_speeds((GAUGE_CHARS.len() - 1) as u8);
+            v.into_iter().map(|i| GAUGE_CHARS[i as usize]).collect()
+        }
     }
 }
