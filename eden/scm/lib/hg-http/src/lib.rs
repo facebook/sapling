e9d5bed65968374a75d4eaacaf2066c9566e4f0d
Jun Wu <quark@fb.com>
2021-03-11 17:16:35 -0800

    hg-http: add API to integrate with progress/model
    Summary:
    Add a way to register an IO time series showing total network I/O, and register
    download progress bars for requests.
    
    Reviewed By: andll
    
    Differential Revision: D26853750
    
    fbshipit-source-id: 42c4ab9724c60db9612a2bc419ca3bceaa667647

--- eden/scm/lib/hg-http/src/lib.rs
+++ eden/scm/lib/hg-http/src/lib.rs
@@ -10,7 +10,29 @@
 ///! a topic should be treated. Topics may include monitoring, request setup,
 ///! paths, error handling, etc.
 use hg_metrics::increment_counter;
+use http_client::Request;
 use http_client::{HttpClient, Stats};
+use once_cell::sync::Lazy;
+use progress_model::IoSample;
+use progress_model::IoTimeSeries;
+use progress_model::ProgressBar;
+use progress_model::Registry;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering::Relaxed;
+
+#[derive(Default)]
+struct Total {
+    download_bytes: AtomicUsize,
+    upload_bytes: AtomicUsize,
+    request_count: AtomicUsize,
+}
+
+// Total progress from all clients.
+static TOTAL: Total = Total {
+    download_bytes: AtomicUsize::new(0),
+    upload_bytes: AtomicUsize::new(0),
+    request_count: AtomicUsize::new(0),
+};
 
 pub fn http_client(client_id: impl ToString) -> HttpClient {
     let client_id = client_id.to_string();
@@ -21,6 +43,72 @@ pub fn http_client(client_id: impl ToString) -> HttpClient {
         l.on_stats(reporter);
     })
 }
+
+/// Setup progress reporting to the main progress registry for the lifetime of
+/// this process.
+pub fn enable_progress_reporting() {
+    let _state = Lazy::force(&PROGRESS_REPORTING_STATE);
+}
+
+/// State for progress reporting. Lazily initialized.
+static PROGRESS_REPORTING_STATE: Lazy<Box<dyn Drop + Send + Sync>> = Lazy::new(|| {
+    Request::on_new_request(move |req| {
+        TOTAL.request_count.fetch_add(1, Relaxed);
+        let req_listeners = req.event_listeners();
+        req_listeners.on_download_bytes({
+            move |_req, n| {
+                TOTAL.download_bytes.fetch_add(n, Relaxed);
+            }
+        });
+        req_listeners.on_upload_bytes({
+            move |_req, n| {
+                TOTAL.upload_bytes.fetch_add(n, Relaxed);
+            }
+        });
+
+        // Create a progress bar to the main progress registry.
+        // TODO: How to tell whether it is downloading or uploading?
+        let bar = ProgressBar::new("Downloading", 0, "bytes");
+        bar.set_message(req.url().to_string());
+
+        let req_listeners = req.event_listeners();
+        req_listeners.on_content_length({
+            let bar = bar.clone();
+            move |_req, n| {
+                bar.set_total(n as _);
+            }
+        });
+        req_listeners.on_download_bytes({
+            let bar = bar.clone();
+            move |_req, n| {
+                bar.increase_position(n as _);
+            }
+        });
+
+        let registry = Registry::main();
+        registry.register_progress_bar(&bar);
+    });
+
+    // HTTP I/O time series.
+    let take_sample = {
+        || {
+            IoSample::from_io_bytes_count(
+                TOTAL.download_bytes.load(Relaxed) as _,
+                TOTAL.upload_bytes.load(Relaxed) as _,
+                TOTAL.request_count.load(Relaxed) as _,
+            )
+        }
+    };
+
+    let net_time_series = IoTimeSeries::new("HTTP", "requests");
+    let task = net_time_series.async_sampling(take_sample, IoTimeSeries::default_sample_interval());
+    async_runtime::spawn(task);
+
+    let registry = Registry::main();
+    registry.register_io_time_series(&net_time_series);
+
+    Box::new(net_time_series)
+});
 
 fn bump_counters(client_id: &str, stats: &Stats) {
     let n = |suffix: &'static str| -> String { format!("http.{}.{}", client_id, suffix) };
