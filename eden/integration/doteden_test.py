50f5138b6f45b7b2000b3db18587e63bff1dc971
generatedunixname89002005307016 <generatedunixname89002005307016@fb.com>
2021-03-12 09:55:51 -0800

    Add annotations to `eden/integration/doteden_test.py`
    Reviewed By: xavierd
    
    Differential Revision: D27012538
    
    fbshipit-source-id: fa8ccea1aa218323ded54115bf5a3fd9728e19bb

--- eden/integration/doteden_test.py
+++ eden/integration/doteden_test.py
@@ -39,51 +39,51 @@ class DotEdenTest(testcase.EdenRepoTest):
                 entry.unlink()
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
-    def test_mkdir_fails(self):
+    def test_mkdir_fails(self) -> None:
         with self.assertRaises(OSError) as cm:
             (self.dot_eden_path / "subdir").mkdir()
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
-    def test_rmdir_fails(self):
+    def test_rmdir_fails(self) -> None:
         with self.assertRaises(OSError) as cm:
             (self.dot_eden_path / "subdir").rmdir()
         # It is no longer possible to create a directory inside .eden -
         # if it was, EPERM would be the right errno value.
         self.assertEqual(errno.ENOENT, cm.exception.errno)
 
-    def test_create_file_fails(self):
+    def test_create_file_fails(self) -> None:
         with self.assertRaises(OSError) as cm:
             os.open(bytes(self.dot_eden_path / "file"), os.O_CREAT | os.O_RDWR)
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
-    def test_mknod_fails(self):
+    def test_mknod_fails(self) -> None:
         with self.assertRaises(OSError) as cm:
             os.mknod(bytes(self.dot_eden_path / "file"), stat.S_IFREG | 0o600)
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
-    def test_symlink_fails(self):
+    def test_symlink_fails(self) -> None:
         with self.assertRaises(OSError) as cm:
             (self.dot_eden_path / "lnk").symlink_to("/", target_is_directory=True)
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
-    def test_rename_in_fails(self):
+    def test_rename_in_fails(self) -> None:
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
                 entry.rename(self.dot_eden_path / "dst")
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
-    def test_rename_from_fails(self):
+    def test_rename_from_fails(self) -> None:
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
                 entry.rename(Path(self.mount) / "dst")
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
-    def test_rename_to_fails(self):
+    def test_rename_to_fails(self) -> None:
         with self.assertRaises(OSError) as cm:
             (Path(self.mount) / "hello").rename(self.dot_eden_path / "dst")
         self.assertEqual(errno.EPERM, cm.exception.errno)
 
-    def test_chown_fails(self):
+    def test_chown_fails(self) -> None:
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
                 os.chown(
@@ -95,14 +95,14 @@ class DotEdenTest(testcase.EdenRepoTest):
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
     # Linux does not allow setting permissions on a symlink.
-    def xtest_chmod_fails(self):
+    def xtest_chmod_fails(self) -> None:
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
                 os.chmod(bytes(entry), 0o543, follow_symlinks=False)
             self.assertEqual(errno.EPERM, cm.exception.errno)
 
     # utime() has no effect on symlinks.
-    def xtest_touch_fails(self):
+    def xtest_touch_fails(self) -> None:
         for entry in self.entries:
             with self.assertRaises(OSError) as cm:
                 os.utime(bytes(entry))
