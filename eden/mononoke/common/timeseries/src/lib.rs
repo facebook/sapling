e64012ad9e76f37f4bf69f2b7d03fab71c293bc3
Thomas Orozco <torozco@fb.com>
2021-04-12 05:21:39 -0700

    mononoke/timeseries: introduce a basic crate for tracking time series
    Summary:
    I'd like to be able to track time series for access within Mononoke. The
    underlying use case here is that I want to be able to track the max count of
    connections in our SQL connection pools over time (and possibly other things in
    the future).
    
    Now, the obvious question is: why am I rolling my own? Well, as it turns out,
    there isn't really an implementation of this that I can reuse:
    
    - You might expect to be able to track the max of a value via fb303, but you
      can't:
    
    https://www.internalfb.com/intern/diffusion/FBS/browse/master/fbcode/fb303/ExportType.h?commit=0405521ec858e012c0692063209f3e13a2671043&lines=26-29
    
    - You might go look in Folly, but you'll find that the time series there only
      supports tracking Sum & Average, but I want my timeseries to track Max (and
      in fact I'd like it to be sufficiently flexible to track anything I want):
    
    https://www.internalfb.com/intern/diffusion/FBS/browse/master/fbcode/folly/stats/BucketedTimeSeries.h
    
    It's not the first time I've ran into a need for something like this. I needed
    it in RendezVous to track connections over the last 2 N millisecond intervals,
    and we needed it in metagit for host draining as well (note that the
    implementation here is somewhat inspired by the implementation there).
    
    Reviewed By: mzr
    
    Differential Revision: D27678388
    
    fbshipit-source-id: ba6d244b8bb848d4e1a12f9c6f54e3aa729f6c9c

---
+++ eden/mononoke/common/timeseries/src/lib.rs
@@ -0,0 +1,301 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+use anyhow::Error;
+use std::convert::TryInto;
+use std::time::{Duration, Instant};
+
+#[derive(thiserror::Error, Debug)]
+pub enum TimeseriesError {
+    #[error("Conversion is out of bounds")]
+    ConversionOutOfBounds(#[source] Error),
+}
+
+pub trait TimeseriesInstant<D>:
+    std::ops::Sub<Self, Output = D>
+    + std::ops::Add<D, Output = Self>
+    + std::cmp::PartialOrd
+    + std::cmp::Ord
+    + Copy
+    + Sized
+{
+}
+
+pub trait TimeseriesDelta: Copy + Sized {
+    fn div(&self, other: Self) -> Result<usize, Error>;
+}
+
+pub trait TimeseriesAccumulator: Default {
+    type Value;
+
+    fn insert(&mut self, value: Self::Value);
+}
+
+#[derive(Debug)]
+pub struct Timeseries<A, D, I> {
+    /// buckets represent [start, start + duration) intervals.
+    buckets: Vec<A>,
+    start_instant: I,
+    start_idx: usize,
+    interval: D,
+}
+
+impl<A, D, I> Timeseries<A, D, I>
+where
+    A: TimeseriesAccumulator,
+    I: TimeseriesInstant<D>,
+    D: TimeseriesDelta,
+{
+    pub fn new(start_instant: I, interval: D, buckets: usize) -> Self {
+        let buckets = (0..buckets).map(|_| A::default()).collect();
+
+        Self {
+            start_instant,
+            start_idx: 0,
+            interval,
+            buckets,
+        }
+    }
+
+    pub fn insert(&mut self, ts: I, value: A::Value) -> Result<(), TimeseriesError> {
+        if let Some(ref mut bucket) = self.bucket_for_ts(ts)? {
+            bucket.insert(value);
+        }
+
+        Ok(())
+    }
+
+    pub fn update(&mut self, ts: I) -> Result<(), TimeseriesError> {
+        self.bucket_for_ts(ts)?;
+
+        Ok(())
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &A> {
+        self.buckets[self.start_idx..self.buckets.len()]
+            .iter()
+            .chain(self.buckets[0..self.start_idx].iter())
+    }
+
+    fn bucket_for_ts(&mut self, ts: I) -> Result<Option<&mut A>, TimeseriesError> {
+        if ts < self.start_instant {
+            return Ok(None);
+        }
+
+        let offset = (ts - self.start_instant)
+            .div(self.interval)
+            .map_err(TimeseriesError::ConversionOutOfBounds)?;
+
+        let buckets_to_add = offset.saturating_sub(self.buckets.len() - 1);
+
+        let offset = if buckets_to_add >= self.buckets.len() {
+            self.reset_buckets(ts);
+            0
+        } else {
+            self.add_buckets(buckets_to_add);
+            offset - buckets_to_add
+        };
+
+        let pos = (self.start_idx + offset) % self.buckets.len();
+
+        Ok(Some(&mut self.buckets[pos]))
+    }
+
+    fn reset_buckets(&mut self, ts: I) {
+        for b in self.buckets.iter_mut() {
+            *b = A::default();
+        }
+
+        self.start_instant = ts;
+        self.start_idx = 0
+    }
+
+    fn add_buckets(&mut self, n: usize) {
+        let mut idx = self.start_idx;
+
+        for _ in 0..n {
+            let next_idx = (idx + 1) % self.buckets.len();
+            self.buckets[idx] = A::default();
+            self.start_idx = next_idx;
+            self.start_instant = self.start_instant + self.interval;
+            idx = next_idx;
+        }
+    }
+}
+
+impl TimeseriesInstant<Duration> for Instant {}
+
+impl TimeseriesDelta for Duration {
+    fn div(&self, other: Self) -> Result<usize, Error> {
+        let res = (self.as_micros() / other.as_micros()).try_into()?;
+        Ok(res)
+    }
+}
+
+impl TimeseriesInstant<usize> for usize {}
+
+impl TimeseriesDelta for usize {
+    fn div(&self, other: Self) -> Result<usize, Error> {
+        Ok(self / other)
+    }
+}
+
+impl<T> TimeseriesAccumulator for Vec<T> {
+    type Value = T;
+
+
+    fn insert(&mut self, value: Self::Value) {
+        self.push(value);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_iter() -> Result<(), Error> {
+        let mut ts = Timeseries::<Vec<u64>, _, _>::new(0usize, 1usize, 3);
+
+        ts.insert(0, 0)?;
+        ts.insert(1, 1)?;
+
+        let d = ts.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(d, vec![vec![0], vec![1], vec![]]);
+
+        ts.insert(3, 3)?;
+        let d = ts.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(d, vec![vec![1], vec![], vec![3]]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_basic() -> Result<(), Error> {
+        let mut ts = Timeseries::<Vec<u64>, _, _>::new(0usize, 2usize, 2);
+        assert_eq!(ts.start_instant, 0);
+        assert_eq!(ts.start_idx, 0);
+        assert_eq!(ts.buckets, vec![vec![], vec![]]);
+
+        ts.insert(0, 1)?;
+        assert_eq!(ts.start_instant, 0);
+        assert_eq!(ts.start_idx, 0);
+        assert_eq!(ts.buckets, vec![vec![1], vec![]]);
+
+
+        ts.insert(1, 2)?;
+        assert_eq!(ts.start_instant, 0);
+        assert_eq!(ts.start_idx, 0);
+        assert_eq!(ts.buckets, vec![vec![1, 2], vec![]]);
+
+        ts.insert(2, 3)?;
+        assert_eq!(ts.start_instant, 0);
+        assert_eq!(ts.start_idx, 0);
+        assert_eq!(ts.buckets, vec![vec![1, 2], vec![3]]);
+
+        ts.insert(3, 4)?;
+        assert_eq!(ts.start_instant, 0);
+        assert_eq!(ts.start_idx, 0);
+        assert_eq!(ts.buckets, vec![vec![1, 2], vec![3, 4]]);
+
+        ts.insert(4, 5)?;
+        assert_eq!(ts.start_instant, 2);
+        assert_eq!(ts.start_idx, 1);
+        assert_eq!(ts.buckets, vec![vec![5], vec![3, 4]]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_add_buckets() -> Result<(), Error> {
+        let mut ts = Timeseries::<Vec<u64>, _, _>::new(0usize, 2usize, 3);
+
+        ts.insert(0, 1)?;
+        ts.insert(2, 2)?;
+        ts.insert(4, 3)?;
+        let d = ts.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(d, vec![vec![1], vec![2], vec![3]]);
+
+        ts.insert(8, 10)?;
+        let d = ts.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(d, vec![vec![3], vec![], vec![10]]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_reset_buckets() -> Result<(), Error> {
+        let mut ts = Timeseries::<Vec<u64>, _, _>::new(0usize, 2usize, 3);
+
+        ts.insert(0, 1)?;
+        ts.insert(2, 2)?;
+        ts.insert(4, 3)?;
+        let d = ts.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(d, vec![vec![1], vec![2], vec![3]]);
+
+        ts.insert(20, 20)?;
+        let d = ts.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(d, vec![vec![20], vec![], vec![]]);
+
+        ts.insert(22, 22)?;
+        let d = ts.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(d, vec![vec![20], vec![22], vec![]]);
+
+        ts.insert(30, 30)?;
+        let d = ts.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(d, vec![vec![30], vec![], vec![]]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_sequence() -> Result<(), Error> {
+        #[derive(Default, Debug)]
+        struct Last(usize);
+
+        impl TimeseriesAccumulator for Last {
+            type Value = usize;
+
+            fn insert(&mut self, value: Self::Value) {
+                self.0 = value;
+            }
+        }
+
+        let mut ts = Timeseries::<Last, _, _>::new(0usize, 1usize, 15);
+
+        for i in 0..30 {
+            ts.insert(i, i)?;
+
+            // Check that the timeseries always has x, x+1, x+2, etc.
+
+            let mut last = None;
+
+            for v in ts.iter() {
+                let v = v.0;
+
+                // Not initialized yet.
+                if v == 0 {
+                    continue;
+                }
+
+                if let Some(last) = last {
+                    assert_eq!(v, last + 1);
+                }
+                last = Some(v);
+            }
+
+            // Check that it ends with i (which we just inserted). This will only be true stating
+            // from the 14th element since we have 15 buckets so the last bucket is [14-15).
+
+            if i >= 14 {
+                assert_eq!(ts.iter().last().map(|v| v.0), Some(i));
+            }
+        }
+
+        Ok(())
+    }
+}
