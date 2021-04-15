f2c0b69e6dadfb1644c350fc700786c41ba87dbf
Maxime Arthaud <arthaud@fb.com>
2021-04-14 18:04:02 -0700

    suppress errors in `fbcode/eden` - batch 1
    Reviewed By: MaggieMoss
    
    Differential Revision: D27738056
    
    fbshipit-source-id: 82957c165d968da999d021e9745b1bf967c615ea

--- eden/hg-server/edenscm/hgext/commitcloud/__init__.py
+++ eden/hg-server/edenscm/hgext/commitcloud/__init__.py
@@ -170,6 +170,7 @@ from . import (
 )
 from . import debugephemeralcommit
 
+# pyre-fixme[16]: Module `` has no attribute `debugephemeralcommit`.
 debugephemeralcommit.command  # Suppressing "unused import" lint
 
 cmdtable = cccommands.cmdtable
