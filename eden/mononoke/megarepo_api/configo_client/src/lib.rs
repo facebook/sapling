815b5ad04a04b5c1686d0108891c336c06ac4555
Kostia Balytskyi <ikostia@fb.com>
2021-04-19 08:33:00 -0700

    megarepo_api: introduce a basic configo client
    Summary:
    This is not used on its on, but in subsequent diffs I will add a use-case, by
    the megarepo configs crate.
    
    When built in non-fbcode mode, this crate does not export anything. I chose this approach as opposed to the approach of exporting no-op stubs to force the clients to pay attention and implement gating their side too. This seems reasonable for a rather generic configo client.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D27790753
    
    fbshipit-source-id: d6dcec884ed7aa88abe5796ef0e58be8525893e2

---
+++ eden/mononoke/megarepo_api/configo_client/src/lib.rs
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#![deny(warnings)]
+
+#[cfg(fbcode_build)]
+mod facebook;
+#[cfg(fbcode_build)]
+pub use facebook::{ConfigObject, MononokeConfigoClient};
+
+// There is no way to implement this for non-fbcode builds
+// and it's worth having this crate's users know this, so
+// let's not even export any stubs.
