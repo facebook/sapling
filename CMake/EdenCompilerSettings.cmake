205eb80a765b067418ea5aea308c3de9f8167ec4
Xavier Deguillard <xavierd@fb.com>
2021-04-19 15:07:16 -0700

    oss: compile with -Wno-nullability-completeness
    Summary:
    On macOS, the compiler shows tons of warnings due to our use of pointers where
    nullability attributes aren't set, and the compiler complains about it. This
    makes finding legitimate warnings/errors almost impossible, thus for now let's
    disable these.
    
    Reviewed By: fanzeyi
    
    Differential Revision: D27862116
    
    fbshipit-source-id: 8ea2c27a0cfd6cbd484fca6be5f15c772d9f4589

---
+++ CMake/EdenCompilerSettings.cmake
@@ -0,0 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+if (NOT WIN32)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-nullability-completeness")
+endif()
