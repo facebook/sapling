f527306576119752898fe1859e25f00ea50db5c5
Jun Wu <quark@fb.com>
2021-04-28 12:21:16 -0700

    edenapi: move EdenApi trait to edenapi_trait
    Summary:
    This allows an external crate C that implements `EdenApi` to depend on a more
    lightweight library just providing `EdenApi` without things like `hg_http`.
    Then the `edenapi` crate can depend on C too.
    
    Didn't move it to `edenapi_types` because it would add extra dependencies
    (http, http_client, auth, etc.)
    
    Reviewed By: kulshrax
    
    Differential Revision: D28006548
    
    fbshipit-source-id: 6e828974fd3f78fec70d4a04ae7be85abc459b36

--- eden/scm/lib/edenapi/src/response.rs
+++ eden/scm/lib/edenapi/src/response.rs
@@ -5,55 +5,15 @@
  * GNU General Public License version 2.
  */
 
-use std::pin::Pin;
-
 use futures::prelude::*;
-use http::{header, HeaderMap, StatusCode, Version};
 
 use async_runtime::block_on_exclusive as block_on_future;
-use http_client::{AsyncResponse, Response, Stats};
+use http_client::Stats;
 
 use crate::errors::EdenApiError;
 
-const SERVER_HEADER: &str = "server";
-const REQUEST_ID_HEADER: &str = "x-request-id";
-const TW_TASK_HEADER: &str = "x-tw-task";
-const TW_VERSION_HEADER: &str = "x-tw-task-version";
-const TW_CANARY_HEADER: &str = "x-tw-canary-id";
-const SERVER_LOAD_HEADER: &str = "x-load";
-
-/// A generic `Stream` of "entries" representing the deserialized content
-/// of a streaming response from the server.
-pub type Entries<T> = Pin<Box<dyn Stream<Item = Result<T, EdenApiError>> + Send + 'static>>;
-pub type StatsFuture = Pin<Box<dyn Future<Output = Result<Stats, EdenApiError>> + Send + 'static>>;
-
-/// The result of a data fetching operation, which may have involved
-/// several individual HTTP requests.
-pub struct Fetch<T> {
-    /// Metadata about each of the requests that were sent during fetching,
-    /// arranged in the order in which the responses arrived.
-    pub meta: Vec<ResponseMeta>,
-
-    /// A `Stream` containing the combined responses for all of the requests.
-    /// There are no ordering guarantees; entries from different HTTP responses
-    /// may be arbitrarily interleaved.
-    pub entries: Entries<T>,
-
-    /// A `Future` that returns the aggregated transfer stastics for the
-    /// all of the HTTP requests involved in the fetching operation. Will
-    /// only resolve once all of the requests have completed.
-    pub stats: StatsFuture,
-}
-
-impl<T: Send + 'static> Fetch<T> {
-    pub fn empty() -> Self {
-        Self {
-            meta: Vec::new(),
-            entries: stream::empty().boxed(),
-            stats: future::ok(Stats::default()).boxed(),
-        }
-    }
-}
+pub use edenapi_trait::Fetch;
+pub use edenapi_trait::ResponseMeta;
 
 /// Non-async version of `Fetch`.
 pub struct BlockingFetch<T> {
@@ -82,51 +42,4 @@ impl<T> BlockingFetch<T> {
             stats,
         })
     }
-}
-
-/// Metadata extracted from the headers of an individual HTTP response.
-#[derive(Debug, Default)]
-pub struct ResponseMeta {
-    pub version: Version,
-    pub status: StatusCode,
-    pub server: Option<String>,
-    pub request_id: Option<String>,
-    pub tw_task_handle: Option<String>,
-    pub tw_task_version: Option<String>,
-    pub tw_canary_id: Option<String>,
-    pub server_load: Option<usize>,
-    pub content_length: Option<usize>,
-}
-
-impl ResponseMeta {
-    fn from_parts(version: Version, status: StatusCode, headers: &HeaderMap) -> Self {
-        Self {
-            version,
-            status,
-            server: get_header(headers, SERVER_HEADER),
-            request_id: get_header(headers, REQUEST_ID_HEADER),
-            tw_task_handle: get_header(headers, TW_TASK_HEADER),
-            tw_task_version: get_header(headers, TW_VERSION_HEADER),
-            tw_canary_id: get_header(headers, TW_CANARY_HEADER),
-            server_load: get_header(headers, SERVER_LOAD_HEADER).and_then(|l| l.parse().ok()),
-            content_length: get_header(headers, header::CONTENT_LENGTH.as_str())
-                .and_then(|l| l.parse().ok()),
-        }
-    }
-}
-
-impl From<&Response> for ResponseMeta {
-    fn from(res: &Response) -> Self {
-        Self::from_parts(res.version, res.status, &res.headers)
-    }
-}
-
-impl From<&AsyncResponse> for ResponseMeta {
-    fn from(res: &AsyncResponse) -> Self {
-        Self::from_parts(res.version, res.status, &res.headers)
-    }
-}
-
-fn get_header(headers: &HeaderMap, name: &str) -> Option<String> {
-    Some(headers.get(name)?.to_str().ok()?.into())
 }
--- eden/scm/lib/edenapi/trait/src/lib.rs
+++ eden/scm/lib/edenapi/trait/src/lib.rs
@@ -5,25 +5,15 @@
  * GNU General Public License version 2.
  */
 
-mod blocking;
-mod builder;
-mod client;
-mod response;
-
-pub use edenapi_trait::api;
-pub use edenapi_trait::errors;
+pub mod api;
+pub mod errors;
+pub mod response;
 
 pub use crate::api::{EdenApi, ProgressCallback};
-pub use crate::blocking::EdenApiBlocking;
-pub use crate::builder::Builder;
-pub use crate::builder::HttpClientBuilder;
-pub use crate::client::Client;
 pub use crate::errors::{ConfigError, EdenApiError};
-pub use crate::response::BlockingFetch;
-pub use edenapi_trait::{Entries, Fetch, ResponseMeta};
+pub use crate::response::{Entries, Fetch, ResponseMeta};
 
 // Re-export for convenience.
 pub use edenapi_types as types;
-pub use http_client::{Progress, Stats};
 
 pub type Result<T> = std::result::Result<T, EdenApiError>;
