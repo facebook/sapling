1044dd545d57bf7a8fb93b597166fc2e32b9fbb1
Stanislau Hlebik <stash@fb.com>
2021-06-24 07:31:55 -0700

    mononoke: support mononoke admin convert for git
    Summary:
    Previously only conversion between bonsai and hg was supported. Let's add git
    as well.
    
    Obviously you can use `scsc lookup`, but mononoke_admin can be useful for repos
    that are not on scs yet.
    
    Reviewed By: farnz
    
    Differential Revision: D29360793
    
    fbshipit-source-id: eb2b71eab192b3456ba3d580f7eb8c4a85b2fd1d

--- eden/mononoke/cmds/admin/hash_convert.rs
+++ eden/mononoke/cmds/admin/hash_convert.rs
@@ -9,13 +9,13 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 use fbinit::FacebookInit;
 use std::str::FromStr;
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Error};
 use blobrepo::BlobRepo;
 use blobrepo_hg::BlobRepoHg;
 use cmdlib::args::{self, MononokeMatches};
 use context::CoreContext;
 use mercurial_types::HgChangesetId;
-use mononoke_types::ChangesetId;
+use mononoke_types::{hash::GitSha1, ChangesetId};
 use slog::Logger;
 
 use crate::error::SubcommandError;
@@ -33,7 +33,7 @@ pub fn build_subcommand<'a, 'b>() -> App<'a, 'b> {
                 .short("f")
                 .required(true)
                 .takes_value(true)
-                .possible_values(&["hg", "bonsai"])
+                .possible_values(&["hg", "bonsai", "git"])
                 .help("Source hash type"),
         )
         .arg(
@@ -42,7 +42,7 @@ pub fn build_subcommand<'a, 'b>() -> App<'a, 'b> {
                 .short("t")
                 .required(true)
                 .takes_value(true)
-                .possible_values(&["hg", "bonsai"])
+                .possible_values(&["hg", "bonsai", "git"])
                 .help("Target hash type"),
         )
         .args_from_usage("<HASH>  'source hash'")
@@ -58,38 +58,69 @@ pub async fn subcommand_hash_convert<'a>(
     let source = sub_m.value_of("from").unwrap().to_string();
     let target = sub_m.value_of("to").unwrap();
     // Check that source and target are different types.
-    assert_eq!(
-        false,
-        (source == "hg") ^ (target == "bonsai"),
-        "source and target should be different"
-    );
+    if source == target {
+        return Err(anyhow!("source and target should be different").into());
+    }
     let ctx = CoreContext::new_with_logger(fb, logger.clone());
     let repo: BlobRepo = args::open_repo(fb, &logger, &matches).await?;
-    if source == "hg" {
+
+    let cs_id = convert_to_bonsai(&ctx, &repo, &source, &source_hash).await?;
+    println!("{}", convert_from_bonsai(&ctx, &repo, cs_id, target).await?);
+
+    Ok(())
+}
+
+async fn convert_to_bonsai(
+    ctx: &CoreContext,
+    repo: &BlobRepo,
+    from: &str,
+    hash: &str,
+) -> Result<ChangesetId, Error> {
+    if from == "hg" {
         let maybebonsai = repo
             .get_bonsai_from_hg(
-                ctx,
-                HgChangesetId::from_str(&source_hash)
-                    .expect("source hash is not valid hg changeset id"),
+                ctx.clone(),
+                HgChangesetId::from_str(&hash).expect("source hash is not valid hg changeset id"),
             )
             .await?;
-        match maybebonsai {
-            Some(bonsai) => {
-                println!("{}", bonsai);
-            }
-            None => {
-                return Err(anyhow!("no matching mononoke id found").into());
-            }
-        }
-        Ok(())
+
+        maybebonsai.ok_or_else(|| anyhow!("bonsai not found for {}", hash))
+    } else if from == "git" {
+        let maybebonsai = repo
+            .bonsai_git_mapping()
+            .get_bonsai_from_git_sha1(ctx, GitSha1::from_str(hash)?)
+            .await?;
+
+        maybebonsai.ok_or_else(|| anyhow!("git not found for {}", hash))
+    } else if from == "bonsai" {
+        ChangesetId::from_str(hash)
     } else {
-        let mercurial = repo
-            .get_hg_from_bonsai_changeset(
-                ctx,
-                ChangesetId::from_str(&source_hash).expect("source hash is not valid mononoke id"),
-            )
+        return Err(anyhow!("unknown source {}", from));
+    }
+}
+
+async fn convert_from_bonsai(
+    ctx: &CoreContext,
+    repo: &BlobRepo,
+    cs_id: ChangesetId,
+    to: &str,
+) -> Result<String, Error> {
+    if to == "hg" {
+        let hg = repo
+            .get_hg_from_bonsai_changeset(ctx.clone(), cs_id)
             .await?;
-        println!("{}", mercurial);
-        Ok(())
+        Ok(format!("{}", hg))
+    } else if to == "git" {
+        let maybegit = repo
+            .bonsai_git_mapping()
+            .get_git_sha1_from_bonsai(ctx, cs_id)
+            .await?;
+
+        let git = maybegit.ok_or_else(|| anyhow!("git not found for {}", cs_id))?;
+        Ok(format!("{}", git))
+    } else if to == "bonsai" {
+        Ok(format!("{}", cs_id))
+    } else {
+        return Err(anyhow!("unknown target {}", to));
     }
 }
