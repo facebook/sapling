bbe46dcf06cc7aabf730fced8ee1ca5a6eaca181
Meyer Jacobs <meyer@fb.com>
2021-06-23 19:26:34 -0700

    scmstore: avoid empty remote requests in TreeStore
    Summary: Verify we actually have pending keys to fetch before attempting a remote request in scmstore TreeStore.
    
    Reviewed By: kulshrax
    
    Differential Revision: D29345214
    
    fbshipit-source-id: 328bdcbc41429e59de6ceb488533bafa97518fcc

--- eden/scm/lib/revisionstore/src/scmstore/specialized/tree.rs
+++ eden/scm/lib/revisionstore/src/scmstore/specialized/tree.rs
@@ -104,62 +104,68 @@ impl TreeStore {
         if let Some(ref memcache) = self.memcache {
             let pending: Vec<_> = incomplete.iter().cloned().collect();
 
-            for entry in memcache.get_data_iter(&pending)? {
-                let entry: Entry = entry?.into();
-                incomplete.remove(entry.key());
-                if self.indexedlog_cache.is_some() && self.cache_to_local_cache {
-                    write_to_local_cache.insert(entry.key().clone());
+            if !pending.is_empty() {
+                for entry in memcache.get_data_iter(&pending)? {
+                    let entry: Entry = entry?.into();
+                    incomplete.remove(entry.key());
+                    if self.indexedlog_cache.is_some() && self.cache_to_local_cache {
+                        write_to_local_cache.insert(entry.key().clone());
+                    }
+                    complete.insert(entry.key().clone(), entry);
                 }
-                complete.insert(entry.key().clone(), entry);
             }
         }
 
         if let Some(ref edenapi) = self.edenapi {
             let pending: Vec<_> = incomplete.iter().cloned().collect();
-
-            for entry in edenapi.trees_blocking(pending, None, None)?.entries {
-                // TODO(meyer): Should probably remove the From impls and add TryFrom instead
-                // TODO(meyer): Again, handle errors better. This will turn EdenApi NotFound into failing
-                // the entire batch
-                let entry: Entry = entry?.into();
-                incomplete.remove(entry.key());
-                if self.indexedlog_cache.is_some() && self.cache_to_local_cache {
-                    write_to_local_cache.insert(entry.key().clone());
+            if !pending.is_empty() {
+                for entry in edenapi.trees_blocking(pending, None, None)?.entries {
+                    // TODO(meyer): Should probably remove the From impls and add TryFrom instead
+                    // TODO(meyer): Again, handle errors better. This will turn EdenApi NotFound into failing
+                    // the entire batch
+                    let entry: Entry = entry?.into();
+                    incomplete.remove(entry.key());
+                    if self.indexedlog_cache.is_some() && self.cache_to_local_cache {
+                        write_to_local_cache.insert(entry.key().clone());
+                    }
+                    if self.memcache.is_some() && self.cache_to_memcache {
+                        write_to_memcache.insert(entry.key().clone());
+                    }
+                    complete.insert(entry.key().clone(), entry);
                 }
-                if self.memcache.is_some() && self.cache_to_memcache {
-                    write_to_memcache.insert(entry.key().clone());
-                }
-                complete.insert(entry.key().clone(), entry);
             }
         }
 
         if let Some(ref contentstore) = self.contentstore {
             let pending: Vec<_> = incomplete.iter().cloned().map(StoreKey::HgId).collect();
-            contentstore.prefetch(&pending)?;
+            if !pending.is_empty() {
+                contentstore.prefetch(&pending)?;
 
-            let pending = pending.into_iter().map(|key| match key {
-                StoreKey::HgId(key) => key,
-                // Safe because we constructed pending with only StoreKey::HgId above
-                // we're just re-using the already allocated paths in the keys
-                _ => unreachable!("unexpected non-HgId StoreKey"),
-            });
-            for key in pending {
-                let store_key = StoreKey::HgId(key.clone());
-                let blob = match contentstore.get(store_key.clone())? {
-                    StoreResult::Found(v) => Some(v),
-                    StoreResult::NotFound(_k) => None,
-                };
-                let meta = match contentstore.get_meta(store_key)? {
-                    StoreResult::Found(v) => Some(v),
-                    StoreResult::NotFound(_k) => None,
-                };
+                let pending = pending.into_iter().map(|key| match key {
+                    StoreKey::HgId(key) => key,
+                    // Safe because we constructed pending with only StoreKey::HgId above
+                    // we're just re-using the already allocated paths in the keys
+                    _ => unreachable!("unexpected non-HgId StoreKey"),
+                });
 
-                if let (Some(blob), Some(meta)) = (blob, meta) {
-                    incomplete.remove(&key);
-                    // We don't write to local indexedlog or memcache for contentstore fallbacks because
-                    // contentstore handles that internally.
-                    let entry = Entry::new(key, blob.into(), meta);
-                    complete.insert(entry.key().clone(), entry);
+                for key in pending {
+                    let store_key = StoreKey::HgId(key.clone());
+                    let blob = match contentstore.get(store_key.clone())? {
+                        StoreResult::Found(v) => Some(v),
+                        StoreResult::NotFound(_k) => None,
+                    };
+                    let meta = match contentstore.get_meta(store_key)? {
+                        StoreResult::Found(v) => Some(v),
+                        StoreResult::NotFound(_k) => None,
+                    };
+
+                    if let (Some(blob), Some(meta)) = (blob, meta) {
+                        incomplete.remove(&key);
+                        // We don't write to local indexedlog or memcache for contentstore fallbacks because
+                        // contentstore handles that internally.
+                        let entry = Entry::new(key, blob.into(), meta);
+                        complete.insert(entry.key().clone(), entry);
+                    }
                 }
             }
         }
