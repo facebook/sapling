76179e66aa376bc1ada481deed79a1cdba3e4db5
Jan Mazur <mzr@fb.com>
2021-06-28 05:59:59 -0700

    remove ssh-related debugnetwork functionality
    Summary: No longer used: https://fburl.com/scuba/ssh_prod_logs/n6hn2cg3
    
    Reviewed By: DurhamG
    
    Differential Revision: D29100680
    
    fbshipit-source-id: 6ee154d9348cd5201780aba723ea1793a5132aee

--- eden/scm/edenscm/hgext/debugnetwork.py
+++ eden/scm/edenscm/hgext/debugnetwork.py
@@ -8,9 +8,6 @@
 ::
 
     [debugnetwork]
-    # Define the location of the speed test command on the server.
-    speed-test-command = /path/to/speed-test
-
     # Set how many bytes to download in the download test.
     speed-test-download-size = 10M
 
@@ -30,7 +27,6 @@ from edenscm.mercurial import (
     httpclient,
     httpconnection,
     registrar,
-    sshpeer,
     sslutil,
     util,
 )
@@ -123,66 +119,6 @@ def checkmononokehost(ui, url, opts):
     return True
 
 
-def checksshcommand(ui, url, opts):
-    rui = hg.remoteui(ui, opts)
-    sshcmd = rui.config("ui", "ssh")
-    sshaddenv = dict(rui.configitems("sshenv"))
-    sshenv = util.shellenviron(sshaddenv)
-    args = util.sshargs(sshcmd, url.host, url.user, url.port)
-    cmd = "%s %s %s" % (sshcmd, args, "hostname")
-    ui.status(
-        _("Testing SSH connection to the server: running 'hostname'\n"),
-        component="debugnetwork",
-    )
-    ui.pushbuffer(subproc=True)
-    starttime = util.timer()
-    res = ui.system(cmd, blockedtag="debugnetwork", environ=sshenv)
-    endtime = util.timer()
-    hostname = pycompat.decodeutf8(ui.popbufferbytes()).strip()
-    if res == 0:
-        ui.status(
-            _("Connected ok: %s\n") % util.timecount(endtime - starttime),
-            component="debugnetwork",
-        )
-        ui.status(_("Server hostname is %s\n") % hostname, component="debugnetwork")
-        return True
-    else:
-        ui.status(_("Failed to connect: ssh returned %s\n") % res, error=_("error"))
-        return False
-
-
-def checkhgserver(ui, repo, opts, path):
-    ui.status(
-        _("Testing connection to Mercurial on the server: querying master bookmark\n"),
-        component="debugnetwork",
-    )
-    starttime = util.timer()
-    peer = None
-    try:
-        peer = hg.peer(repo, opts, path)
-        bookmarks = peer.listkeys("bookmarks")
-        master = bookmarks.get("master")
-    except Exception as e:
-        ui.status(_("failed to connect to Mercurial: %s\n") % e, error=_("error"))
-        return False
-    finally:
-        if peer:
-            peer.close()
-    endtime = util.timer()
-
-    ui.status(
-        _("Connected ok: %s\n") % util.timecount(endtime - starttime),
-        component="debugnetwork",
-    )
-    if master:
-        ui.status(
-            _("Server master bookmark is %s\n") % master, component="debugnetwork"
-        )
-    else:
-        ui.status(_("Server has no master bookmark\n"), component="debugnetwork")
-    return True
-
-
 def checkspeedhttp(ui, url, opts):
     ui.status(_("Testing connection speed to the server\n"), component="debugnetwork")
     download = ui.configbytes("debugnetwork", "speed-test-download-size", 10000000)
@@ -258,108 +194,6 @@ def checkspeedhttp(ui, url, opts):
     return res
 
 
-def checkhgspeed(ui, url, opts):
-    speedcmd = ui.config("debugnetwork", "speed-test-command")
-    if speedcmd is None:
-        ui.status(
-            _(
-                "Not testing connection speed: 'debugnetwork.speed-test-command' is not set"
-            ),
-            component="debugnetwork",
-        )
-        return True
-    ui.status(_("Testing connection speed to the server\n"), component="debugnetwork")
-    rui = hg.remoteui(ui, opts)
-    sshcmd = rui.config("ui", "ssh")
-    sshaddenv = dict(rui.configitems("sshenv"))
-    sshenv = util.shellenviron(sshaddenv)
-    args = util.sshargs(sshcmd, url.host, url.user, url.port)
-    download = ui.configbytes("debugnetwork", "speed-test-download-size", 10000000)
-    upload = ui.configbytes("debugnetwork", "speed-test-upload-size", 1000000)
-
-    cmd = "%s %s %s" % (sshcmd, args, util.shellquote(sshpeer._serverquote(speedcmd)))
-    pipeo, pipei, pipee, sub = util.popen4(cmd, bufsize=0, env=sshenv)
-    pipee = sshpeer.threadedstderr(rui, pipee)
-    pipee.start()
-
-    def latencytest(count):
-        # Use the upload endpoint for the latency test.  We will time how long it
-        # takes for the server to return the "upload complete" response for a
-        # single byte upload.
-        latencies = []
-        with progress.spinner(ui, "testing connection latency"):
-            for i in range(count):
-                pipeo.write(b"upload 1\n")
-                pipeo.flush()
-                l = pipei.readline()
-                if l != b"upload bytes 1\n":
-                    raise error.Abort("invalid response from server: %r" % l)
-                starttime = util.timer()
-                pipeo.write(b"\n")
-                pipeo.flush()
-                l = pipei.readline()
-                endtime = util.timer()
-                if l != b"upload complete\n":
-                    raise error.Abort("invalid response from server: %r" % l)
-                latencies.append(endtime - starttime)
-        return latencies
-
-    def downloadtest(description, bytecount):
-        pipeo.write(b"download %i\n" % bytecount)
-        pipeo.flush()
-        l = pipei.readline()
-        if not l or not l.startswith(b"download bytes"):
-            raise error.Abort("invalid response from server: %r" % l)
-        bytecount = int(l.split()[2])
-        with progress.bar(
-            ui, description, total=bytecount, formatfunc=util.bytecount
-        ) as prog:
-            starttime = util.timer()
-            remaining = bytecount
-            while remaining > 0:
-                data = pipei.read(min(remaining, BLOCK_SIZE))
-                if not data:
-                    raise error.Abort("premature end of speed-test download stream")
-                remaining -= len(data)
-                prog.value = bytecount - remaining
-            l = pipei.readline()
-            if not l or not l.startswith(b"download complete"):
-                raise error.Abort("invalid response from server: %r" % l)
-            endtime = util.timer()
-        return endtime - starttime
-
-    def uploadtest(description, bytecount):
-        pipeo.write(b"upload %i\n" % bytecount)
-        pipeo.flush()
-        l = pipei.readline()
-        if not l or not l.startswith(b"upload bytes"):
-            raise error.Abort("invalid response from server: %r" % l)
-        bytecount = int(l.split()[2])
-        with progress.bar(
-            ui, description, total=bytecount, formatfunc=util.bytecount
-        ) as prog:
-            starttime = util.timer()
-            remaining = bytecount
-            while remaining > 0:
-                data = os.urandom(min(remaining, BLOCK_SIZE))
-                remaining -= len(data)
-                pipeo.write(data)
-                prog.value = bytecount - remaining
-            pipeo.flush()
-            l = pipei.readline()
-            if not l or not l.startswith(b"upload complete"):
-                raise error.Abort("invalid response from server: %r" % l)
-            endtime = util.timer()
-        return endtime - starttime
-
-    return drivespeedtests(
-        ui,
-        (latencytest, 5),
-        (downloadtest, "download", download),
-        (uploadtest, "upload", upload),
-    )
-
-
 def drivespeedtests(ui, latency, upload, download):
     latencytest, latency_ntests = latency
     uploadtest, testname, bytecount = upload
@@ -428,16 +262,8 @@ def debugnetwork(ui, repo, remote="default", **opts):
 
     url = util.url(path)
 
-    if url.scheme == "ssh":
-        checkspeed = checkhgspeed
-        checkserverhostname = checksshcommand
-        servertype = "Mercurial"
-    else:
-        checkspeed = checkspeedhttp
-        checkserverhostname = checkmononokehost
-        servertype = "Mononoke"
-        if url.port is None:
-            url.port = "443"
+    if url.port is None:
+        url.port = "443"
 
     if alltests or opts.get("connection"):
         addrinfos = checkdnsresolution(ui, url)
@@ -451,18 +277,13 @@ def debugnetwork(ui, repo, remote="default", **opts):
             ui.status(msg, component=_("debugnetwork"))
             return 3
 
-        if not checkserverhostname(ui, url, opts):
-            msg = _("Failed to connect to check %s server hostname.\n") % servertype
+        if not checkmononokehost(ui, url, opts):
+            msg = _("Failed to connect to check Mononoke server hostname.\n")
             ui.status(msg, component=_("debugnetwork"))
             return 4
 
-        if servertype == "Mercurial" and not checkhgserver(ui, repo, opts, path):
-            msg = _("Failed to connect to Mercurial on the server.\n")
-            ui.status(msg, component=_("debugnetwork"))
-            return 5
-
     if alltests or opts.get("speed"):
-        if not checkspeed(ui, url, opts):
-            msg = _("Failed to check %s server connection speed.\n") % servertype
+        if not checkspeedhttp(ui, url, opts):
+            msg = _("Failed to check Mononoke server connection speed.\n")
             ui.status(msg, component=_("debugnetwork"))
             return 6
