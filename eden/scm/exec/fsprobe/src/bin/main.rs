ea75c06d58d08b9e039e7b7581249b7c29470898
Andrey Chursin <achursin@fb.com>
2021-07-22 16:10:59 -0700

    fsprobe: read probe plan
    Reviewed By: kulshrax
    
    Differential Revision: D29824746
    
    fbshipit-source-id: 40528a4408d7f682a1611b05c6ff788eee89a7d5

--- eden/scm/exec/fsprobe/src/bin/main.rs
+++ eden/scm/exec/fsprobe/src/bin/main.rs
@@ -5,8 +5,10 @@
  * GNU General Public License version 2.
  */
 
-use std::path::PathBuf;
-
+use anyhow::{bail, Result};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -16,5 +18,50 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    println!("{:?}", args.path);
+    let plan = ProbePlan::load(&args.path).expect("Failed to load fsprobe plan");
+    println!("Loaded {} actions", plan.0.len());
+}
+
+struct ProbePlan(Vec<ProbeAction>);
+
+enum ProbeAction {
+    Read(PathBuf),
+}
+
+// Probe plan file format is a new line separated list of actions
+// Each action has a format <action> [<params>]
+// Currently supported actions:
+//   * cat <path> - read full file at <path>
+impl ProbePlan {
+    fn load(path: &Path) -> Result<Self> {
+        let file = File::open(path)?;
+        let mut actions = vec![];
+        for line in BufReader::new(file).lines() {
+            let line = line?;
+            let action = ProbeAction::parse(&line)?;
+            actions.push(action);
+        }
+        Ok(Self(actions))
+    }
+}
+
+impl ProbeAction {
+    pub fn parse(s: &str) -> Result<Self> {
+        let space = s.find(' ');
+        if let Some(space) = space {
+            let cmd = &s[..space];
+            match cmd {
+                "cat" => {
+                    let path = &s[space..];
+                    if path.len() == 0 {
+                        bail!("cat requires path");
+                    }
+                    Ok(ProbeAction::Read(path.into()))
+                }
+                _ => bail!("Unknown command {}", cmd),
+            }
+        } else {
+            bail!("Invalid action {}", s);
+        }
+    }
 }
