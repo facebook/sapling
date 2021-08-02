dde44f8d724a27723981d20ec40cce5e9ae8a392
Xavier Deguillard <xavierd@fb.com>
2021-08-02 12:02:59 -0700

    integration: enable pull_test on Windows
    Summary: The test is passing, enable it.
    
    Reviewed By: chadaustin
    
    Differential Revision: D30028894
    
    fbshipit-source-id: b268fc63220704466c72c688a1f0f04a47573641

--- eden/integration/lib/skip.py
+++ eden/integration/lib/skip.py
@@ -110,7 +110,6 @@ if sys.platform == "win32":
         ],
         "hg.move_test.MoveTestTreeOnly": True,
         "hg.post_clone_test.SymlinkTestTreeOnly": True,
-        "hg.pull_test.PullTestTreeOnly": True,
         "hg.rebase_test.RebaseTestTreeOnly": [
             "test_rebase_commit_with_independent_folder"
         ],
