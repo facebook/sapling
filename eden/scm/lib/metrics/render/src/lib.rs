89fb80e0da2dd7e336508c33feaaf53e9cfb6e26
Andrey Chursin <achursin@fb.com>
2021-08-10 14:08:24 -0700

    metrics: read render config from env variable
    Summary: See docstring to init_from_env for the description
    
    Reviewed By: quark-zju
    
    Differential Revision: D30137304
    
    fbshipit-source-id: ad3fb5e0cbb52ca405959eb0d394f5a2c9761ad9

---
+++ eden/scm/lib/metrics/render/src/lib.rs
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+use metrics::{Counter, Registry};
+use std::env;
+use std::sync::Weak;
+use std::thread;
+use std::time::Duration;
+use tracing::error;
+
+/// Initializes metrics reporting while guard is not dropped.
+/// Configuration is read from env variable EDENSCM_METRICS.
+/// This variable holds coma separated list of reporting parameters in form Filter:Reporter.
+///
+/// Filter is the name of the metric, either full name, or a prefix (crate name without '.').
+/// Reporter is optional, by default progress reporter is used - it renders progress bar with the counter.
+pub fn init_from_env(guard: Weak<()>) {
+    if let Ok(config) = env::var("EDENSCM_METRICS") {
+        init(&config, guard)
+    }
+}
+
+fn init(config: &str, guard: Weak<()>) {
+    let config = parse_config(config);
+    if config.is_empty() {
+        return;
+    }
+    thread::Builder::new()
+        .name("metrics-report".to_string())
+        .spawn(move || worker(config, guard))
+        .unwrap();
+}
+
+fn worker(config: Vec<(MetricsFilter, Renderer)>, guard: Weak<()>) {
+    let registry = Registry::global();
+    while guard.upgrade().is_some() {
+        let metrics = registry.counters();
+        for (filter, renderer) in &config {
+            let filtered = metrics.iter().filter_map(|(name, v)| {
+                if filter.matches(name) {
+                    Some((*name, *v))
+                } else {
+                    None
+                }
+            });
+            renderer.render(filtered.collect());
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn parse_config(config: &str) -> Vec<(MetricsFilter, Renderer)> {
+    let mut result = vec![];
+    for item in config.split(',') {
+        let mut split = item.split(':');
+        let filter = split.next().expect("First element in split always exist");
+        let render_type = split.next();
+        let renderer = match render_type {
+            None | Some("p") | Some("progress") => {
+                if split.next().is_some() {
+                    error!("Progress render does not have parameters, extra parameters ignored");
+                }
+                Renderer::ProgressBar
+            }
+            Some(other) => {
+                error!("Invalid metrics renderer: {}", other);
+                continue;
+            }
+        };
+        let filter = MetricsFilter::parse(filter);
+        result.push((filter, renderer));
+    }
+    result
+}
+
+struct MetricsFilter {
+    prefix: String,
+    exact: bool, // Either exact(true) or prefix(false)
+}
+
+impl MetricsFilter {
+    pub fn parse(s: &str) -> MetricsFilter {
+        let exact = s.contains('.');
+        MetricsFilter {
+            prefix: s.to_string(),
+            exact,
+        }
+    }
+
+    pub fn matches(&self, name: &str) -> bool {
+        if self.exact {
+            self.prefix.as_str() == name
+        } else {
+            name.starts_with(&self.prefix)
+        }
+    }
+}
+
+enum Renderer {
+    ProgressBar,
+}
+
+impl Renderer {
+    pub fn render(&self, metrics: Vec<(&'static str, &'static Counter)>) {
+        match self {
+            Self::ProgressBar => self.render_progress(metrics),
+        }
+    }
+
+    fn render_progress(&self, _metrics: Vec<(&'static str, &'static Counter)>) {
+        unimplemented!()
+    }
+}
