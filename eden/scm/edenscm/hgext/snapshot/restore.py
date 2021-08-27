475ebe8c17329ac05d2301f12217a50abcb9b195
Yan Soares Couto <yancouto@fb.com>
2021-08-27 03:38:26 -0700

    Use fetch_snapshot endpoint from restore command
    Summary:
    This diff calls the `/:repo/snapshot` EdenApi endpoint added on D30514854 (https://github.com/facebookexperimental/eden/commit/ab17c4d181b7f39b275a02cf12a89598c2f38651) from the `hg snapshot restore` command.
    
    For now, it just prints the parent of the snapshot, but in next diffs it will update to it and restore the dirty changes.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D30517984
    
    fbshipit-source-id: e1381eaed561a7184ee02ab99d0282f11a1d944f

--- eden/scm/edenscm/hgext/snapshot/restore.py
+++ eden/scm/edenscm/hgext/snapshot/restore.py
@@ -3,6 +3,23 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
+from edenscm.mercurial.edenapi_upload import (
+    getreponame,
+)
+
 
 def restore(ui, repo, csid, **opts):
     ui.status(f"Will restore snapshot {csid}\n", component="snapshot")
+
+    snapshot = repo.edenapi.fetchsnapshot(
+        getreponame(repo),
+        {
+            "cs_id": bytes.fromhex(csid),
+            # TODO(yancouto): Find bubble id from snapshot id
+            "bubble_id": 1,
+        },
+    )
+
+    ui.status(
+        f"Snapshot info: parent {snapshot['hg_parents'].hex()}\n", component="snapshot"
+    )
