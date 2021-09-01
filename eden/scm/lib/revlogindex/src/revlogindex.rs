7eb11cb392f369494ba70d5e0b739f53da45ed2e
Jun Wu <quark@fb.com>
2021-09-01 09:01:27 -0700

    revlogindex: implement octopus merge support
    Summary:
    Support octopus merge defined in the following format:
    
    - The revlog flag has `1 << 12` set.
    - Extra `stepparents` is set to `hexnode1,hexnode2,...` format.
    
    This is mainly used to support revlog from stream clone.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D30686450
    
    fbshipit-source-id: d5aa2f18a02f5f0d7aa033210fb4f79b729c0d26

--- eden/scm/lib/revlogindex/src/revlogindex.rs
+++ eden/scm/lib/revlogindex/src/revlogindex.rs
@@ -63,6 +63,8 @@ use util::path::remove_file;
 // Revset is non-lazy. Sync APIs can be used.
 use dag::nameset::SyncNameSetQuery;
 
+const REVIDX_OCTOPUS_MERGE: u16 = 1 << 12;
+
 impl RevlogIndex {
     /// Calculate `heads(ancestors(revs))`.
     pub fn headsancestors(&self, revs: Vec<u32>) -> dag::Result<Vec<u32>> {
@@ -409,6 +411,10 @@ impl ParentRevs {
         Self::Compact([p1, p2])
     }
 
+    fn from_vec(v: Vec<i32>) -> Self {
+        Self::Octopus(Box::new(v))
+    }
+
     pub fn as_revs(&self) -> &[u32] {
         let slice: &[i32] = match self {
             ParentRevs::Compact(s) => {
@@ -464,6 +470,16 @@ impl<T: UnsafeSliceCast> AsRef<[T]> for BytesSlice<T> {
 }
 impl UnsafeSliceCast for u32 {}
 impl UnsafeSliceCast for RevlogEntry {}
+
+impl RevlogEntry {
+    fn flags(&self) -> u16 {
+        (self.offset_flags.to_be() & 0xffff) as u16
+    }
+
+    fn is_octopus_merge(&self) -> bool {
+        (self.flags() & REVIDX_OCTOPUS_MERGE) != 0
+    }
+}
 
 impl RevlogIndex {
     /// Constructs a RevlogIndex. The NodeRevMap is automatically manage>
@@ -550,9 +566,87 @@ impl RevlogIndex {
         }
 
         let data = self.data();
-        let p1 = i32::from_be(data[rev as usize].p1);
-        let p2 = i32::from_be(data[rev as usize].p2);
-        Ok(ParentRevs::from_p1p2(p1, p2))
+        let entry = &data[rev as usize];
+        let p1 = i32::from_be(entry.p1);
+        let p2 = i32::from_be(entry.p2);
+        if entry.is_octopus_merge() {
+            let mut parents = Vec::with_capacity(3);
+            if p1 >= 0 {
+                parents.push(p1);
+            }
+            if p2 >= 0 {
+                parents.push(p2);
+            }
+            // Read from the "stepparents" extra.
+            let data = self.raw_data(rev)?;
+            let stepparents = self.get_stepparents(&data)?;
+            parents.extend(stepparents);
+            Ok(ParentRevs::from_vec(parents))
+        } else {
+            Ok(ParentRevs::from_p1p2(p1, p2))
+        }
+    }
+
+    /// Get the extra parents from raw data of a revision.
+    fn get_stepparents(&self, data: &[u8]) -> Result<Vec<i32>> {
+        // `data` format:
+        // <manifest sha1> + '\n'
+        // author + '\n'
+        // date + ' ' + timezone + ' ' + (... + '\0')* + 'stepparents:' + (hexnode + ',')+
+
+        let mut parents = Vec::new();
+
+        #[derive(Copy, Clone)]
+        enum State {
+            ManifestLine,
+            AuthorLine,
+            Date,
+            Timezone,
+            Extras,
+        }
+
+        let mut state: State = State::ManifestLine;
+        let mut extra_start = 0;
+        for (i, b) in data.iter().enumerate() {
+            match (state, b) {
+                (State::ManifestLine, b'\n') => {
+                    state = State::AuthorLine;
+                }
+                (State::AuthorLine, b'\n') => {
+                    state = State::Date;
+                }
+                (State::Date, b' ') => {
+                    state = State::Timezone;
+                }
+                (State::Timezone, b' ') => {
+                    state = State::Extras;
+                    extra_start = i + 1;
+                }
+                (State::Extras, b'\0') | (State::Extras, b'\n') => {
+                    let extra = &data[extra_start..i]; // name:value
+                    if let Some(value) = extra.strip_prefix(b"stepparents:") {
+                        // Parse the stepparents field.
+                        if let Ok(value) = std::str::from_utf8(value) {
+                            for hex_node in value.split(',') {
+                                let node = Vertex::from_hex(hex_node.as_bytes())?;
+                                if let Some(id) = self.nodemap.node_to_rev(node.as_ref())? {
+                                    parents.push(id as i32);
+                                } else {
+                                    return Err(crate::errors::CommitNotFound(node).into());
+                                }
+                            }
+                        }
+                    }
+                    if *b == b'\n' {
+                        break;
+                    } else {
+                        extra_start = i + 1;
+                    }
+                }
+                (_, _) => {}
+            }
+        }
+        Ok(parents)
     }
 
     /// Get raw content from a revision.
@@ -618,10 +712,15 @@ impl RevlogIndex {
         if non_blocking_result(self.contains_vertex_name(&node)).unwrap_or(false) {
             return;
         }
-        let p1 = parents.get(0).map(|r| *r as i32).unwrap_or(-1);
-        let p2 = parents.get(1).map(|r| *r as i32).unwrap_or(-1);
+        let parent_revs = if parents.len() <= 2 {
+            let p1 = parents.get(0).map(|r| *r as i32).unwrap_or(-1);
+            let p2 = parents.get(1).map(|r| *r as i32).unwrap_or(-1);
+            ParentRevs::from_p1p2(p1, p2)
+        } else {
+            ParentRevs::from_vec(parents.into_iter().map(|i| i as i32).collect())
+        };
         let idx = self.pending_parents.len();
-        self.pending_parents.push(ParentRevs::from_p1p2(p1, p2));
+        self.pending_parents.push(parent_revs);
         self.pending_nodes.push(node.clone());
 
         self.pending_nodes_index.insert(node, idx);
@@ -752,12 +851,17 @@ impl RevlogIndex {
             existing_nodes.insert(node.as_ref().to_vec(), rev as u32);
 
             let mut parent_revs: [i32; 2] = [-1, -1];
-            for (p_id, p_node) in parent_map[node.as_ref()].iter().enumerate() {
+            let parents = &parent_map[node.as_ref()];
+            for (p_id, p_node) in parents.iter().enumerate() {
                 parent_revs[p_id] = get_rev(&existing_nodes, p_node)? as i32;
             }
 
+            let mut flags = 0;
+            if parents.len() > 2 || find_bytes_in_bytes(&raw, b"stepparents:").is_some() {
+                flags |= REVIDX_OCTOPUS_MERGE;
+            };
             let entry = RevlogEntry {
-                offset_flags: u64::to_be(offset << 16),
+                offset_flags: u64::to_be((offset << 16) | (flags as u64)),
                 compressed: i32::to_be(chunk.len() as i32),
                 len: i32::to_be(raw_len as i32),
                 base: i32::to_be(rev as i32),
@@ -1792,6 +1896,10 @@ fn unsupported_dag_error<T>() -> dag::Result<T> {
     Err(dag::errors::BackendError::Generic("unsupported by revlog index".to_string()).into())
 }
 
+fn find_bytes_in_bytes(bytes: &[u8], to_find: &[u8]) -> Option<usize> {
+    bytes.windows(to_find.len()).position(|b| b == to_find)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -2099,6 +2207,41 @@ commit 3"#
         );
 
         Ok(())
+    }
+
+    #[test]
+    fn test_octopus_merge() {
+        let dir = tempdir().unwrap();
+        let dir = dir.path();
+        let changelog_i_path = dir.join("00changelog.i");
+        let nodemap_path = dir.join("00changelog.nodemap");
+
+        let mut rlog = RevlogIndex::new(&changelog_i_path, &nodemap_path).unwrap();
+        rlog.insert(v(0), vec![], b"A".to_vec().into()); // rev 0
+        rlog.insert(v(1), vec![], b"B".to_vec().into()); // rev 1
+        rlog.insert(v(2), vec![], b"C".to_vec().into()); // rev 2
+        rlog.insert(v(3), vec![], b"D".to_vec().into()); // rev 3
+        rlog.insert(
+            v(4),
+            vec![0, 3],
+            format!(
+                concat!(
+                    "deadbeef000000000000\n",
+                    "test <test@example.com>\n",
+                    "100 300 bar:foo\0stepparents:{},{}\0foo:bar\n",
+                    "foobar"
+                ),
+                v(2).to_hex(),
+                v(1).to_hex()
+            )
+            .as_bytes()
+            .to_vec()
+            .into(),
+        ); // rev 4
+        rlog.flush().unwrap();
+
+        let rlog = RevlogIndex::new(&changelog_i_path, &nodemap_path).unwrap();
+        assert_eq!(rlog.parent_revs(4).unwrap().as_revs(), [0, 3, 2, 1]);
     }
 
     #[test]
