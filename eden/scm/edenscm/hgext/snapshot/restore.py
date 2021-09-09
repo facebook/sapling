29606df2fe20a786fb854ae19086e477cec831d0
Yan Soares Couto <yancouto@fb.com>
2021-09-09 07:41:57 -0700

    Restore to correct parent and deleted files
    Summary:
    This adds basic logic for `snapshot restore` command.
    - It updates to the parent of the snapshot
    - It loads the snapshot changes
    
    For now I did not do changes/tracked changes, as it will need to download the file contents, which will need a new edenapi endpoint, so I'll leave it for a future diff. It just restores your deleted files for now.
    
    Reviewed By: StanislavGlebik
    
    Differential Revision: D30543507
    
    fbshipit-source-id: 080588ceff0ecd595ce739044f0d4118fb8e1a3f

--- eden/scm/edenscm/hgext/snapshot/restore.py
+++ eden/scm/edenscm/hgext/snapshot/restore.py
@@ -3,6 +3,7 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
+from edenscm.mercurial import hg, scmutil, cmdutil
 from edenscm.mercurial.edenapi_upload import (
     getreponame,
 )
@@ -20,6 +21,31 @@ def restore(ui, repo, csid, **opts):
         },
     )
 
+    # Once merges/conflicted states are supported, we'll need to support more
+    # than one parent
+    assert isinstance(snapshot["hg_parents"], bytes)
+
     ui.status(
-        f"Snapshot info: parent {snapshot['hg_parents'].hex()}\n", component="snapshot"
+        f"Updating to parent {snapshot['hg_parents'].hex()}\n", component="snapshot"
     )
+
+    with repo.wlock():
+        hg.updatetotally(
+            ui, repo, repo[snapshot["hg_parents"]], None, updatecheck="abort"
+        )
+
+        for (path, fc) in snapshot["file_changes"]:
+            matcher = scmutil.matchfiles(repo, [path])
+            if "Deletion" in fc:
+                cmdutil.remove(ui, repo, matcher, "", False, False)
+            elif "UntrackedDeletion" in fc:
+                f = repo[None][path]
+                if not f.exists():
+                    # File was hg added and is now missing. Let's add an empty file first
+                    repo.wwrite(path, b"", "")
+                    cmdutil.add(ui, repo, matcher, "", False)
+                f.remove()
+            elif "Change" in fc:
+                ui.status(f"[{path}] Changes not supported yet\n")
+            else:
+                ui.status(f"[{path}] Untracked changes not supported yet\n")
