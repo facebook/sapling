07d0da77d87b8f105e3ff6f1960854d3346b9ac9
Yan Soares Couto <yancouto@fb.com>
2021-10-06 07:32:07 -0700

    Fix quickcheck mincode test
    Summary:
    Another test that was broken by quickcheck update. I notice it breaking a run of `hg_windows` [here](https://www.internalfb.com/intern/sandcastle/log/?instance_id=4503600123928895&step_id=4503604846860319&step_index=9&name=Run%20cargo%20test#479) (though it's not windows specific).
    
    The problem is quickcheck started using NaN on floats, which surfaced some invalid code. In roundtrip tests, we need our struct to implement `Eq`, which f32 does not (since NaN != NaN). Workaround was implementing something that also considers NaN == NaN.
    
    Question: Do I also fix the hg-server version of the test?
    
    Reviewed By: krallin
    
    Differential Revision: D31401277
    
    fbshipit-source-id: b3eef1a3aef395a1194308788ec74f1bb5a33a42

--- eden/scm/lib/mincode/src/tests.rs
+++ eden/scm/lib/mincode/src/tests.rs
@@ -8,17 +8,29 @@
 use quickcheck::quickcheck;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
+struct Wrap(f64, i32, u8);
+
+// workaround for f64 not implementing Eq
+impl PartialEq for Wrap {
+    fn eq(&self, other: &Self) -> bool {
+        let (f1, f2) = (self.0, other.0);
+        (self.1, self.2) == (other.1, other.2) && ((f1.is_nan() && f2.is_nan()) || f1 == f2)
+    }
+}
+impl Eq for Wrap {}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 struct Foo {
     bar: String,
-    baz: Option<(f64, i32, u8)>,
+    baz: Option<Wrap>,
     derp: bool,
     list: Vec<u32>,
 }
 
 quickcheck! {
     fn test_roundtrip(bar: String, baz: Option<(f64, i32, u8)>, derp: bool, list: Vec<u32>) -> bool {
-        let foo = Foo { bar, baz, derp, list };
+        let foo = Foo { bar, baz: baz.map(|(a, b, c)| Wrap(a, b, c)), derp, list };
         let bytes = crate::serialize(&foo).unwrap();
         let foo_deserialized: Foo = crate::deserialize(&bytes).unwrap();
         foo == foo_deserialized
