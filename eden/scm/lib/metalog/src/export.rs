79572f71c2e498c7c75d815db26916434e5a3597
Jun Wu <quark@fb.com>
2021-10-19 15:00:27 -0700

    metalog: support exporting non-linear history to git
    Summary:
    With D30970502 (https://github.com/facebookexperimental/eden/commit/c3695fa2bc3ef5d7bb26ff69c4d39d16460d5160) it's now possible to commit metalog based on a "detached" root
    with pending changes, as noted by the `Parent: ` field. Detached roots do not
    get listed via the usual `list_roots`.
    
    Revise the git export logic to read the `Parent: ` field to construct the
    graph that might include detached roots.
    
    Reviewed By: DurhamG
    
    Differential Revision: D31126654
    
    fbshipit-source-id: d8ec184aeb95faea351eb5254b4ee7124a7a69f7

--- eden/scm/lib/metalog/src/export.rs
+++ eden/scm/lib/metalog/src/export.rs
@@ -6,12 +6,14 @@
  */
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io::Write;
 use std::path::Path;
 
 use git2::Repository;
 
 use crate::metalog::load_root;
+use crate::metalog::Id20;
 use crate::metalog::SerId20;
 use crate::MetaLog;
 use crate::Result;
@@ -22,14 +24,70 @@ impl MetaLog {
         let repo = Repository::init(repo_path)?;
         let root_ids = Self::list_roots(&self.path)?;
 
-        // We use a packfile per key for efficient deltaing.
-        let mut blob_id_map = HashMap::new();
-        let mut last_commit = None;
+        let mut blob_id_map = HashMap::new(); // Metalog Blob SHA1 -> Git Blob SHA1
+        let mut commit_id_map = HashMap::new(); // Metalog Root SHA1 -> Git Commit
+        let listed: HashSet<_> = root_ids.iter().copied().collect();
         let mut count = 0;
-        for root_id in root_ids {
-            // Stop at the current RootId.
-            if root_id == self.orig_root_id {
-                break;
+
+        // Figure out the "parents" relationship.
+        let parents: HashMap<Id20, Vec<Id20>> = {
+            let mut parents: HashMap<Id20, Vec<Id20>> = HashMap::new();
+
+            // From the root_id list.
+            for slice in root_ids.windows(2) {
+                if let [parent, child] = slice {
+                    parents.insert(*child, vec![*parent]);
+                }
+            }
+
+            // From the implicit "Parent: " messages.
+            // They might include pending changes. See D30970502.
+            for root_id in root_ids.iter().copied() {
+                let root = load_root(&self.blobs, root_id)?;
+                for line in root.message.lines() {
+                    if let Some(hex_parent) = line.strip_prefix("Parent: ") {
+                        if let Ok(parent_root_id) = Id20::from_hex(hex_parent.as_bytes()) {
+                            if let Ok(_parent_root) = load_root(&self.blobs, parent_root_id) {
+                                let parents = parents.entry(root_id).or_default();
+                                if !parents.contains(&parent_root_id) {
+                                    parents.push(parent_root_id);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            parents
+        };
+
+        // Export everything reachable from the "current" root.
+        let mut to_visit: Vec<Id20> = vec![self.orig_root_id];
+        while let Some(root_id) = to_visit.pop() {
+            if commit_id_map.contains_key(&root_id) {
+                // Already committed.
+                continue;
+            }
+
+            let root_parents: &[Id20] = match parents.get(&root_id) {
+                Some(parents) => parents.as_ref(),
+                None => &[],
+            };
+
+            {
+                // Need to commit missing parents first?
+                let mut missing_parents = false;
+                for parent in root_parents {
+                    if !commit_id_map.contains_key(parent) {
+                        if !missing_parents {
+                            to_visit.push(root_id);
+                            missing_parents = true;
+                        }
+                        to_visit.push(*parent);
+                    }
+                }
+                if missing_parents {
+                    continue;
+                }
             }
 
             let root = load_root(&self.blobs, root_id)?;
@@ -61,14 +119,25 @@ impl MetaLog {
             // Add commit.
             let time = git2::Time::new(root.timestamp as _, 0);
             let sig = git2::Signature::new("metalog", "metalog@example.com", &time)?;
-            let parents = match last_commit {
-                None => Vec::new(),
-                Some(ref parent) => vec![parent],
+            let git_parents = root_parents
+                .iter()
+                .filter_map(|p| commit_id_map.get(p))
+                .collect::<Vec<_>>();
+            let detach_message = if listed.contains(&root_id) {
+                ""
+            } else {
+                "\nDetached: true"
             };
-            let message = format!("{}\n\nRootId: {}", root.message, root_id.to_hex());
+            let message = format!(
+                "{}\n\nRootId: {}{}",
+                root.message,
+                root_id.to_hex(),
+                detach_message
+            );
             let tree = repo.find_tree(tree_id)?;
-            last_commit =
-                Some(repo.find_commit(repo.commit(None, &sig, &sig, &message, &tree, &parents)?)?);
+            let commit_oid = repo.commit(None, &sig, &sig, &message, &tree, &git_parents)?;
+            let commit = repo.find_commit(commit_oid)?;
+            commit_id_map.insert(root_id, commit);
             count += 1;
             if count % 100 == 0 {
                 eprintln!("count: {}", count);
@@ -76,10 +145,10 @@ impl MetaLog {
         }
 
         // Make 'master' point to the last commit.
-        if let Some(last_commit) = last_commit {
+        if let Some(main_commit) = commit_id_map.get(&self.orig_root_id) {
             repo.reference(
                 "refs/heads/master",
-                last_commit.id(),
+                main_commit.id(),
                 true, /* force */
                 "move master",
             )?;
