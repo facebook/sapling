9a12c6786a94e0f23b7d9810f40628ac01753bb8
Grace Ku <gku@fb.com>
2021-11-04 11:27:01 -0700

    cli_rs: Add refresh_rate option for minitop subcommand
    Summary: The same as the option that is available in `eden top`.
    
    Reviewed By: fanzeyi
    
    Differential Revision: D31863321
    
    fbshipit-source-id: 3571cc4866fc43e5f43dc5f390cf737b33a7c0b4

--- eden/fs/cli_rs/edenfs-commands/src/minitop.rs
+++ eden/fs/cli_rs/edenfs-commands/src/minitop.rs
@@ -8,6 +8,9 @@
 //! edenfsctl minitop
 
 use async_trait::async_trait;
+use std::collections::BTreeMap;
+use std::path::Path;
+use std::time::Duration;
 use structopt::StructOpt;
 
 use anyhow::Error;
@@ -18,7 +21,27 @@ use crate::ExitCode;
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "Simple monitoring of EdenFS accesses by process.")]
-pub struct MinitopCmd {}
+pub struct MinitopCmd {
+    // TODO: For minitop, we may want to allow querying for < 1s, but this
+    // requires modifying the thrift call and the eden service itself.
+    // < 1s may be more useful for the realtime stats we see in minitop/top.
+    #[structopt(
+        long,
+        short,
+        help = "Specify the rate (in seconds) at which eden top updates.",
+        default_value = "1",
+        parse(from_str = parse_refresh_rate),
+    )]
+    refresh_rate: Duration,
+}
+
+fn parse_refresh_rate(arg: &str) -> Duration {
+    let seconds = arg
+        .parse::<u64>()
+        .expect("Please enter a valid whole positive number for refresh_rate.");
+
+    Duration::new(seconds, 0)
+}
 
 #[async_trait]
 impl crate::Subcommand for MinitopCmd {
