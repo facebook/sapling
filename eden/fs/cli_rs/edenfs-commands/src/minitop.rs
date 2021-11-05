62315132a1276459fc7ef1cc44a04b49214206f4
Grace Ku <gku@fb.com>
2021-11-05 16:21:19 -0700

    cli_rs: Initial code for minitop
    Summary:
    This diff gets access counts and prints them out in a simple table laid out with tabs.
    
    Still a lot to do, but wanted to get this code out there to get feedback earlier. Some things I need to do:
    - format last access time
    - sort by last access time
    - laying out in a nicer table (I plan on using comfy-table: https://fburl.com/code/k353fp6i)
    - updating every `refresh_rate` seconds (I plan on using cross-term: https://fburl.com/code/ahuoqy7t)
    
    Reviewed By: fanzeyi
    
    Differential Revision: D32000885
    
    fbshipit-source-id: 6fc60d13e9519ed45ad6e18628013f9b3ac88bc7

--- eden/fs/cli_rs/edenfs-commands/src/minitop.rs
+++ eden/fs/cli_rs/edenfs-commands/src/minitop.rs
@@ -11,11 +11,14 @@ use async_trait::async_trait;
 use std::collections::BTreeMap;
 use std::path::Path;
 use std::time::Duration;
+use std::time::SystemTime;
 use structopt::StructOpt;
 
-use anyhow::Error;
+use anyhow::anyhow;
 use edenfs_client::EdenFsInstance;
-use edenfs_error::{EdenFsError, Result};
+use edenfs_error::{Result, ResultExt};
+
+use thrift_types::edenfs::types::{pid_t, AccessCounts, GetAccessCountsResult};
 
 use crate::ExitCode;
 
@@ -43,9 +46,241 @@ fn parse_refresh_rate(arg: &str) -> Duration {
     Duration::new(seconds, 0)
 }
 
+const UNKNOWN_COMMAND: &str = "<unknown>";
+
+trait GetAccessCountsResultExt {
+    fn get_cmd_for_pid(&self, pid: &i32) -> Result<String>;
+}
+
+impl GetAccessCountsResultExt for GetAccessCountsResult {
+    fn get_cmd_for_pid(&self, pid: &i32) -> Result<String> {
+        match self.cmdsByPid.get(pid) {
+            Some(cmd) => Ok(String::from_utf8(cmd.to_vec()).from_err()?),
+            None => Ok(String::from(UNKNOWN_COMMAND)),
+        }
+    }
+}
+
+trait AccessCountsExt {
+    fn add(&mut self, other: &AccessCounts);
+}
+
+impl AccessCountsExt for AccessCounts {
+    fn add(&mut self, other: &AccessCounts) {
+        self.fsChannelTotal += other.fsChannelTotal;
+        self.fsChannelReads += other.fsChannelReads;
+        self.fsChannelWrites += other.fsChannelWrites;
+        self.fsChannelBackingStoreImports += other.fsChannelBackingStoreImports;
+        self.fsChannelDurationNs += other.fsChannelDurationNs;
+        self.fsChannelMemoryCacheImports += other.fsChannelMemoryCacheImports;
+        self.fsChannelDiskCacheImports += other.fsChannelDiskCacheImports;
+    }
+}
+
+#[derive(Clone)]
+struct Process {
+    pid: u32,
+    mount: String,
+    cmd: String,
+    access_counts: AccessCounts,
+    fetch_counts: u64,
+    last_access_time: SystemTime,
+}
+
+impl Process {
+    fn is_running(&self) -> bool {
+        Path::new(&format!("/proc/{}", self.pid)).is_dir()
+    }
+}
+
+struct TrackedProcesses {
+    processes: BTreeMap<u32, Process>,
+}
+
+impl TrackedProcesses {
+    fn new() -> Self {
+        TrackedProcesses {
+            processes: BTreeMap::<u32, Process>::new(),
+        }
+    }
+
+    fn extract_mount(path: &[u8]) -> anyhow::Result<String> {
+        let path = std::str::from_utf8(path)?;
+        let path = Path::new(&path);
+        let filename = path
+            .file_name()
+            .ok_or_else(|| anyhow!("filename is missing"))?;
+
+        filename
+            .to_os_string()
+            .into_string()
+            .map_err(|_| anyhow!("mount name is not UTF-8"))
+    }
+
+    /// Starts to track a given process. If the process is already being tracked,
+    /// then it updates the process's information (counts, last update time, etc).
+    ///
+    /// At any given time, a single pid may have multiple access logs.
+    fn update_process(
+        &mut self,
+        pid: &pid_t,
+        mount: &[u8],
+        cmd: String,
+        access_counts: AccessCounts,
+        fetch_counts: &i64,
+    ) -> Result<()> {
+        let pid = u32::try_from(*pid).from_err()?;
+        let mount = TrackedProcesses::extract_mount(mount)?;
+        let fetch_counts = u64::try_from(*fetch_counts).from_err()?;
+
+        match self.processes.get_mut(&pid) {
+            Some(existing_proc) => {
+                assert!(existing_proc.mount == mount && existing_proc.cmd == cmd);
+
+                // We increment access counts, but overwrite fetch counts
+                // (this matches behavior in original python implementation)
+                existing_proc.access_counts.add(&access_counts);
+                existing_proc.fetch_counts = fetch_counts;
+
+                existing_proc.last_access_time = SystemTime::now();
+            }
+            None => {
+                self.processes.insert(
+                    pid,
+                    Process {
+                        pid,
+                        mount,
+                        cmd,
+                        access_counts,
+                        fetch_counts,
+                        last_access_time: SystemTime::now(),
+                    },
+                );
+            }
+        }
+
+        Ok(())
+    }
+
+    /// We aggregate all tracked processes in a separate step right before rendering
+    /// (as opposed to aggregating eagerly as we receive process logs in `update_process`)
+    /// because tracked processes could stop running which may change the top_pid.
+    fn aggregated_processes(&self) -> Vec<Process> {
+        // Technically, it's more correct to aggregate this by TGID
+        // Because that's hard to get, we instead aggregate by mount & cmd
+        // (mount, cmd) => Process
+        let mut aggregated_processes = BTreeMap::<(&str, &str), Process>::new();
+
+        for (_pid, process) in self.processes.iter() {
+            match aggregated_processes.get_mut(&(&process.mount, &process.cmd)) {
+                Some(agg_proc) => {
+                    // We aggregate access counts, but we don't change fetch counts
+                    // (this matches behavior in original python implementation)
+                    agg_proc.access_counts.add(&process.access_counts);
+
+                    // Figure out what the most relevant process id is
+                    if process.is_running() || agg_proc.last_access_time < process.last_access_time
+                    {
+                        agg_proc.pid = process.pid;
+                        agg_proc.last_access_time = process.last_access_time;
+                    }
+                }
+                None => {
+                    aggregated_processes.insert((&process.mount, &process.cmd), process.clone());
+                }
+            }
+        }
+
+        aggregated_processes.into_values().collect()
+    }
+}
+
 #[async_trait]
 impl crate::Subcommand for MinitopCmd {
     async fn run(&self, instance: EdenFsInstance) -> Result<ExitCode> {
-        Err(EdenFsError::Other(Error::msg("Not implemented yet.")))
+        let client = instance.connect(None).await?;
+
+        let counts = client
+            .getAccessCounts(self.refresh_rate.as_secs().try_into().from_err()?)
+            .await
+            .from_err()?;
+
+        // Update currently tracked processes (and add new ones if they haven't been tracked yet)
+        let mut tracked_processes = TrackedProcesses::new();
+        for (mount, accesses) in &counts.accessesByMount {
+            for (pid, access_counts) in &accesses.accessCountsByPid {
+                tracked_processes.update_process(
+                    pid,
+                    mount,
+                    counts.get_cmd_for_pid(pid)?,
+                    access_counts.clone(),
+                    accesses.fetchCountsByPid.get(pid).unwrap_or(&0),
+                )?;
+            }
+
+            for (pid, fetch_counts) in &accesses.fetchCountsByPid {
+                tracked_processes.update_process(
+                    pid,
+                    mount,
+                    counts.get_cmd_for_pid(pid)?,
+                    AccessCounts::default(),
+                    fetch_counts,
+                )?;
+            }
+        }
+
+        const COLUMN_TITLES: &[&str] = &[
+            "TOP PID",
+            "MOUNT",
+            "FUSE R",
+            "FUSE W",
+            "FUSE COUNT",
+            "FUSE FETCH",
+            "MEMORY",
+            "DISK",
+            "IMPORTS",
+            "FUSE TIME",
+            "FUSE LAST",
+            "CMD",
+        ];
+
+        println!("{}", COLUMN_TITLES.join("\t"));
+        for aggregated_process in tracked_processes.aggregated_processes() {
+            println!(
+                "{top_pid}\t \
+                {mount}\t \
+                {fuse_reads}\t \
+                {fuse_writes}\t \
+                {fuse_total}\t \
+                {fuse_fetch}\t \
+                {fuse_memory_cache_imports}\t \
+                {fuse_disk_cache_imports}\t \
+                {fuse_backing_store_imports}\t \
+                {fuse_duration}\t \
+                {fuse_last_access}\t \
+                {command}",
+                top_pid = aggregated_process.pid,
+                mount = aggregated_process.mount,
+                fuse_reads = aggregated_process.access_counts.fsChannelReads,
+                fuse_writes = aggregated_process.access_counts.fsChannelWrites,
+                fuse_total = aggregated_process.access_counts.fsChannelTotal,
+                fuse_fetch = aggregated_process.fetch_counts,
+                fuse_memory_cache_imports =
+                    aggregated_process.access_counts.fsChannelMemoryCacheImports,
+                fuse_disk_cache_imports =
+                    aggregated_process.access_counts.fsChannelDiskCacheImports,
+                fuse_backing_store_imports = aggregated_process
+                    .access_counts
+                    .fsChannelBackingStoreImports,
+                fuse_duration = aggregated_process.access_counts.fsChannelDurationNs,
+                fuse_last_access = aggregated_process
+                    .last_access_time
+                    .elapsed()
+                    .from_err()?
+                    .as_nanos(),
+                command = aggregated_process.cmd,
+            )
+        }
+        Ok(0)
     }
 }
