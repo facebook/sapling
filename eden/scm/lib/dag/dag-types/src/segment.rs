b88fa54156a5e16819a20a84fe660e07280cbc77
Jun Wu <quark@fb.com>
2021-11-05 19:07:39 -0700

    dag-types: support out-of-order insertion for PreparedFlatSegments
    Summary: Its callsite (assign_head) is going to provide the `Id`s without an order.
    
    Reviewed By: DurhamG
    
    Differential Revision: D32041501
    
    fbshipit-source-id: b815ce3176a6c7bb364aa36d0575f45b13107673

--- eden/scm/lib/dag/dag-types/src/segment.rs
+++ eden/scm/lib/dag/dag-types/src/segment.rs
@@ -123,35 +123,47 @@ impl PreparedFlatSegments {
 
     /// Add graph edges: id -> parent_ids. Used by `assign_head`.
     pub fn push_edge(&mut self, id: Id, parent_ids: &[Id]) {
-        let new_seg = || FlatSegment {
+        let new_seg = FlatSegment {
             low: id,
             high: id,
             parents: parent_ids.to_vec(),
         };
 
-        // sanity check: this should be easy to verify - assign_head gets new ids
-        // by `next_free_id()`, which should be incremental.
-        debug_assert!(
-            self.segments.last().map_or(Id(0), |s| s.high + 1) < id + 1,
-            "push_edge(id={}, parent_ids={:?}) called out of order ({:?})",
-            id,
-            parent_ids,
-            self
-        );
+        // Find the position to insert the new segment.
+        let idx = match self.segments.binary_search_by_key(&id, |seg| seg.high) {
+            Ok(i) => i,
+            Err(i) => i,
+        };
 
-        if parent_ids.len() != 1 || parent_ids[0] + 1 != id {
+        if parent_ids.len() != 1 || parent_ids[0] + 1 != id || idx == 0 {
             // Start a new segment.
-            self.segments.push(new_seg());
+            self.segments.insert(idx, new_seg);
         } else {
-            // Try to reuse the existing last segment.
-            if let Some(seg) = self.segments.last_mut() {
+            // Try to reuse the existing segment.
+            if let Some(seg) = self.segments.get_mut(idx - 1) {
                 if seg.high + 1 == id {
                     seg.high = id;
                 } else {
-                    self.segments.push(new_seg());
+                    self.segments.insert(idx, new_seg);
                 }
             } else {
-                self.segments.push(new_seg());
+                self.segments.insert(idx, new_seg);
+            }
+        }
+
+        // Check that segments are sorted and merged.
+        if cfg!(debug_assertions) {
+            let mut last_high = None;
+            for seg in &self.segments {
+                // Sorted?
+                assert!(Some(seg.low) > last_high);
+                // Merged?
+                    if let Some(last_high) = last_high {
+                if seg.parents.len() == 1 && seg.parents[0] + 1 == seg.low {
+                    assert_ne!(last_high + 1, seg.low);
+                }
+                    }
+                last_high = Some(seg.high);
             }
         }
     }
@@ -182,5 +194,50 @@ impl PreparedFlatSegments {
                 );
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_edge_out_of_order() {
+        let mut segs = PreparedFlatSegments::default();
+        segs.push_edge(Id(0), &[]);
+        segs.push_edge(Id(50), &[]);
+        segs.push_edge(Id(100), &[]);
+        segs.push_edge(Id(1), &[Id(0)]);
+        segs.push_edge(Id(51), &[Id(50)]);
+        segs.push_edge(Id(101), &[Id(100)]);
+        segs.push_edge(Id(2), &[]);
+        segs.push_edge(Id(52), &[Id(51), Id(50)]);
+        segs.push_edge(Id(102), &[Id(100)]);
+        segs.push_edge(Id(103), &[Id(102)]);
+        segs.push_edge(Id(53), &[Id(52)]);
+        segs.push_edge(Id(105), &[Id(103)]);
+        segs.push_edge(Id(106), &[Id(105)]);
+        segs.push_edge(Id(104), &[Id(103)]);
+        segs.push_edge(Id(3), &[Id(2)]);
+        segs.push_edge(Id(4), &[Id(3)]);
+        segs.push_edge(Id(54), &[Id(53)]);
+        segs.push_edge(Id(49), &[Id(3)]);
+        segs.push_edge(Id(107), &[Id(106)]);
+
+        // Check that adjacent segments are merged.
+        let dbg: Vec<String> = segs.segments.iter().map(|s| format!("{:?}", s)).collect();
+        assert_eq!(
+            dbg,
+            [
+                "FlatSegment { low: 0, high: 1, parents: [] }",
+                "FlatSegment { low: 2, high: 4, parents: [] }",
+                "FlatSegment { low: 49, high: 49, parents: [3] }",
+                "FlatSegment { low: 50, high: 51, parents: [] }",
+                "FlatSegment { low: 52, high: 54, parents: [51, 50] }",
+                "FlatSegment { low: 100, high: 101, parents: [] }",
+                "FlatSegment { low: 102, high: 104, parents: [100] }",
+                "FlatSegment { low: 105, high: 107, parents: [103] }"
+            ]
+        );
     }
 }
