3b1a57d1d0e2e2182c3afad5ada5ec553a199098
Yan Soares Couto <yancouto@fb.com>
2021-11-17 09:44:36 -0800

    Move file check out of check_request_item
    Summary: This simply moves the logic to lookup files to another function, as I plan to make it more complicated, and don't want the main function to get more complicated.
    
    Reviewed By: markbt
    
    Differential Revision: D32467695
    
    fbshipit-source-id: 97df6d1b3fb8fa4bb62e5d367e8a28209420ce72

--- eden/mononoke/edenapi_service/src/handlers/lookup.rs
+++ eden/mononoke/edenapi_service/src/handlers/lookup.rs
@@ -5,13 +5,13 @@
  * GNU General Public License version 2.
  */
 
-use anyhow::{bail, Error};
+use anyhow::{bail, Result};
 use async_trait::async_trait;
 use futures::{stream, StreamExt};
 
 use edenapi_types::{
-    AnyId, Batch, FileContentTokenMetadata, IndexableId, LookupRequest, LookupResponse,
-    LookupResult, UploadToken, UploadTokenMetadata,
+    AnyFileContentId, AnyId, Batch, FileContentTokenMetadata, IndexableId, LookupRequest,
+    LookupResponse, LookupResult, UploadToken, UploadTokenMetadata,
 };
 use ephemeral_blobstore::{BubbleId, StorageLocation};
 use mercurial_types::{HgChangesetId, HgFileNodeId, HgManifestId, HgNodeHash};
@@ -21,6 +21,49 @@ use super::{EdenApiHandler, EdenApiMethod, HandlerResult};
 
 const MAX_CONCURRENT_LOOKUPS_PER_REQUEST: usize = 10000;
 
+enum Lookup {
+    NotPresent,
+    Present(Option<UploadTokenMetadata>),
+}
+
+impl From<bool> for Lookup {
+    fn from(b: bool) -> Self {
+        if b {
+            Self::Present(None)
+        } else {
+            Self::NotPresent
+        }
+    }
+}
+
+async fn check_file(
+    repo: HgRepoContext,
+    id: AnyFileContentId,
+    bubble_id: Option<BubbleId>,
+) -> Result<Lookup> {
+    let content_id = repo.convert_file_to_content_id(id, bubble_id).await?;
+    let lookup = if let Some(content_id) = content_id {
+        // Reasons why check if content id is present:
+        // 1. If content_id is provided, we haven't yet checked it is actually
+        // in the blobstore
+        // 2. Maybe alias was written to blobstore but the actual blob has not
+        // 3. We want to do a comprehensive lookup here
+        if repo.is_file_present_by_contentid(content_id).await? {
+            Lookup::Present(Some(
+                FileContentTokenMetadata {
+                    content_size: repo.fetch_file_content_size(content_id, bubble_id).await?,
+                }
+                .into(),
+            ))
+        } else {
+            Lookup::NotPresent
+        }
+    } else {
+        Lookup::NotPresent
+    };
+    Ok(lookup)
+}
+
 /// Check if the item is present already and generate a token if it is.
 /// Return None if the item has to be uploaded
 /// Item can be of any type:
@@ -28,51 +71,12 @@ const MAX_CONCURRENT_LOOKUPS_PER_REQUEST: usize = 10000;
 ///     * hg filenode id
 ///     * hg tree id
 ///     * hg changeset id
-async fn check_request_item(
-    repo: HgRepoContext,
-    item: LookupRequest,
-) -> Result<LookupResponse, Error> {
-    enum Lookup {
-        NotPresent,
-        Present(Option<UploadTokenMetadata>),
-    }
-    impl From<bool> for Lookup {
-        fn from(b: bool) -> Self {
-            if b {
-                Self::Present(None)
-            } else {
-                Self::NotPresent
-            }
-        }
-    }
+async fn check_request_item(repo: HgRepoContext, item: LookupRequest) -> Result<LookupResponse> {
     let old_bubble_id = item.bubble_id;
     let bubble_id = old_bubble_id.map(BubbleId::new);
     let hg_on_bubble_error = "Hg derived data cannot be stored in bubbles";
     let lookup = match item.id {
-        AnyId::AnyFileContentId(id) => {
-            let content_id = repo.convert_file_to_content_id(id, bubble_id).await?;
-            if let Some(content_id) = content_id {
-                // Reasons why check if content id is present:
-                // 1. If content_id is provided, we haven't yet checked it is actually
-                // in the blobstore
-                // 2. Maybe alias was written to blobstore but the actual blob has not
-                // 3. We want to do a comprehensive lookup here
-                if repo.is_file_present_by_contentid(content_id).await? {
-                    Lookup::Present(Some(
-                        FileContentTokenMetadata {
-                            content_size: repo
-                                .fetch_file_content_size(content_id, bubble_id)
-                                .await?,
-                        }
-                        .into(),
-                    ))
-                } else {
-                    Lookup::NotPresent
-                }
-            } else {
-                Lookup::NotPresent
-            }
-        }
+        AnyId::AnyFileContentId(id) => check_file(repo, id, bubble_id).await?,
         AnyId::BonsaiChangesetId(id) => repo
             .changeset_exists_by_bonsai(
                 id.into(),
