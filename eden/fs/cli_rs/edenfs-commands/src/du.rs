7d31562801e88bf3cd969a1f41c1d865762e1b78
Grace Ku <gku@fb.com>
2021-12-02 18:02:07 -0800

    cli_rs: Adding command line args
    Summary:
    Same options available here in the python version:
    https://www.internalfb.com/code/fbsource/[c306d043f707]/fbcode/eden/fs/cli/main.py?lines=160
    
    Reviewed By: fanzeyi
    
    Differential Revision: D32433738
    
    fbshipit-source-id: 6e66ea7e486c2687a12fa3bbc6f2443b8b3210ca

--- eden/fs/cli_rs/edenfs-commands/src/du.rs
+++ eden/fs/cli_rs/edenfs-commands/src/du.rs
@@ -8,6 +8,7 @@
 //! edenfsctl du
 
 use async_trait::async_trait;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 use anyhow::Error;
@@ -18,7 +19,24 @@ use crate::ExitCode;
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "Show disk space usage for a checkout")]
-pub struct DiskUsageCmd {}
+pub struct DiskUsageCmd {
+    #[structopt(help = "Names of the mount points")]
+    mounts: Vec<PathBuf>,
+
+    #[structopt(long, help = "Performs automated cleanup")]
+    clean: bool,
+
+    #[structopt(
+        long,
+        help = "Performs automated cleanup (--clean) and removes fsck dirs. \
+        Unlike --clean this will destroy unrecoverable data. If you have any \
+        local changes you hope to recover, recover them before you run this command."
+    )]
+    deep_clean: bool,
+
+    #[structopt(long, help = "Print the output in JSON format")]
+    json: bool,
+}
 
 #[async_trait]
 impl crate::Subcommand for DiskUsageCmd {
