b93974a990e19a9c3f05e76d4ce4356f5ac88e05
Alex Hornby <ahornby@fb.com>
2021-12-10 03:29:39 -0800

    mononoke: add shard to logged sqlblob_gc generation sizes
    Summary: Include the shard number so we can see if results are unbalanced
    
    Reviewed By: HarveyHunt
    
    Differential Revision: D32951927
    
    fbshipit-source-id: 9ca0ae3afe5a6c50165d715e137f077f464a2898

--- eden/mononoke/cmds/sqlblob_gc/subcommand_log_size.rs
+++ eden/mononoke/cmds/sqlblob_gc/subcommand_log_size.rs
@@ -11,6 +11,7 @@ use anyhow::Result;
 use bytesize::ByteSize;
 use clap::{App, ArgMatches, SubCommand};
 use futures::stream::{self, StreamExt, TryStreamExt};
+use futures::TryFutureExt;
 use scuba_ext::MononokeScubaSampleBuilder;
 use slog::Logger;
 
@@ -50,27 +51,30 @@ pub async fn subcommand_log_size(
     shard_range: Range<usize>,
     scuba_sample_builder: MononokeScubaSampleBuilder,
 ) -> Result<()> {
-    let sizes: Vec<_> = shard_range
-        .map(|shard| sqlblob.get_chunk_sizes_by_generation(shard))
-        .collect();
-    let sizes = stream::iter(sizes.into_iter())
-        .buffer_unordered(max_parallelism)
-        .try_fold(HashMap::new(), |mut acc, sizes| async move {
-            for (gen, size) in sizes {
-                *acc.entry(gen).or_insert(0u64) += size;
-            }
-            Ok(acc)
+    let shard_sizes: Vec<(usize, HashMap<Option<u64>, u64>)> = stream::iter(shard_range)
+        .map(|shard| {
+            sqlblob
+                .get_chunk_sizes_by_generation(shard)
+                .map_ok(move |gen_to_size| (shard, gen_to_size))
         })
+        .buffer_unordered(max_parallelism)
+        .try_collect::<Vec<(usize, HashMap<Option<u64>, u64>)>>()
         .await?;
-    for (generation, size) in &sizes {
-        let mut sample = scuba_sample_builder.clone();
-        sample.add_opt("generation", *generation);
-        sample.add("size", *size);
-        sample.log();
+
+    let mut size_summary = HashMap::new();
+    for (shard, sizes) in shard_sizes {
+        for (generation, size) in sizes.into_iter() {
+            let mut sample = scuba_sample_builder.clone();
+            sample.add("shard", shard);
+            sample.add_opt("generation", generation);
+            sample.add("size", size);
+            sample.log();
+            *size_summary.entry(generation).or_insert(0u64) += size;
+        }
     }
 
     if scuba_sample_builder.is_discard() {
-        print_sizes(&sizes);
+        print_sizes(&size_summary);
     }
     Ok(())
 }
