a5978edae1421d0a67fc8ad74dc865fa118c14e2
Yan Soares Couto <yancouto@fb.com>
2022-01-10 13:30:04 -0800

    Don't fail on parallel create
    Summary:
    This will fail a snapshot creation if two commands are run in parallel. We don't want this. Since writing latest to the metalog is of secondary importance, let's not fail the command if it fails.
    
    In the future we want to always overwrite the metalog, but that doesn't seem so straightforward so I'll leave it to another diff.
    
    We grab a lock when running create, but metalog is created when hg starts running, so it conflicts anyway. If we could reload after grabbing the lock, the issue would also be fixed.
    
    Reviewed By: markbt
    
    Differential Revision: D33168496
    
    fbshipit-source-id: f4927c3bbca1a76669079b232d323636e56e8918

--- eden/scm/edenscm/hgext/snapshot/metalog.py
+++ eden/scm/edenscm/hgext/snapshot/metalog.py
@@ -3,6 +3,8 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
+from edenscm.mercurial import error
+
 LATESTBUBBLE = "latestbubble"
 LATESTSNAPSHOT = "latestsnapshot"
 
@@ -21,8 +23,14 @@ def fetchlatestsnapshot(ml):
 
 
 def storelatest(ml, snapshot, bubble):
-    if snapshot is not None:
-        ml.set(LATESTSNAPSHOT, snapshot)
-    if bubble is not None:
-        ml.set(LATESTBUBBLE, str(bubble).encode("ascii"))
-    ml.commit("Save latest bubble/snapshot")
+    try:
+        if snapshot is not None:
+            ml.set(LATESTSNAPSHOT, snapshot)
+        if bubble is not None:
+            ml.set(LATESTBUBBLE, str(bubble).encode("ascii"))
+        ml.commit("Save latest bubble/snapshot")
+    except error.MetaLogError:
+        # Writing bubbles to metalog is of secondary importance, we don't want
+        # to fail everything. Ideally we want to overwrite the metalog entries,
+        # but that's not easy right now.
+        pass
