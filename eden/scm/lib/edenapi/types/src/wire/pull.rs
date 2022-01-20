360dbdf66df8823ff0d65a7aef8fbf1d97e25df3
Jun Wu <quark@fb.com>
2022-01-19 16:35:42 -0800

    edenapi/types: add types for the new fast pull API
    Summary:
    Instead of passing just the old and new master nodes. Pass a list to support
    more complicated cases.
    
    Reviewed By: DurhamG
    
    Differential Revision: D33594530
    
    fbshipit-source-id: 2087d4fce79eb5cff3c1d381cfc82f9bd6ad89c4

--- eden/scm/lib/edenapi/types/src/wire/pull.rs
+++ eden/scm/lib/edenapi/types/src/wire/pull.rs
@@ -19,6 +19,16 @@ pub struct PullFastForwardRequest {
     pub new_master: HgId,
 }
 
+/// Pull `missing % common` in the master group.
+#[auto_wire]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct PullLazyRequest {
+    #[id(1)]
+    pub common: Vec<HgId>,
+    #[id(2)]
+    pub missing: Vec<HgId>,
+}
+
 #[cfg(any(test, feature = "for-tests"))]
 impl Arbitrary for PullFastForwardRequest {
     fn arbitrary(g: &mut quickcheck::Gen) -> Self {
@@ -29,10 +39,21 @@ impl Arbitrary for PullFastForwardRequest {
     }
 }
 
+#[cfg(any(test, feature = "for-tests"))]
+impl Arbitrary for PullLazyRequest {
+    fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+        Self {
+            common: Arbitrary::arbitrary(g),
+            missing: Arbitrary::arbitrary(g),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::wire::tests::auto_wire_tests;
 
     auto_wire_tests!(WirePullFastForwardRequest);
+    auto_wire_tests!(WirePullLazyRequest);
 }
