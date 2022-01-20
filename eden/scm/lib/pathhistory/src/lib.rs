ed91af1e9167cedc0335d2eeb1ce4264a3f1aaba
Jun Wu <quark@fb.com>
2022-01-19 16:57:59 -0800

    pathhistory: path (file or dir) history based on segmented changelog
    Summary:
    The path history area is problematic in multiple ways:
    - Visiting commits and checking their trees following commit graph can be too
      slow.
    - The "fastlog" service in Mononoke can provide faster path history for a
      single file or a single directory. However, it requires complex infra to
      maintain the indexes and do not handle following multiple paths nicely.
    - hg's linkrev is a tech-debt we'd like to remove but it's not easy to do so.
      Partially because a replacement will need new storage and protocol design,
      and might face questions like offline UX, etc.
    
    This crate is an attempt to tackle problems above:
    - Visiting segments and skip segments aggressively.
    - If we use commit graph and regular tree reads, then there is no need for an
      external service, or hg's linkrev.
    
    with one main downside caused by bisect:
    - "change + revert" might cancel out. History can be incomplete.
    
    But that downside seems acceptable considered the other wins.
    
    This diff adds the crate with some high level commments.
    
    Reviewed By: DurhamG
    
    Differential Revision: D33265881
    
    fbshipit-source-id: e29be3d8e9fa8cd9f011144a7104429edcb25ec4

---
+++ eden/scm/lib/pathhistory/src/lib.rs
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+//! # pathhistory
+//!
+//! This crate provides file or directory history algorithms that does not
+//! depend on per-path indexes, is better than scanning commits one by one,
+//! and is friendly for lazy stores.
+//!
+//! The basic idea is to use the segment struct from the `dag` crate and
+//! (aggressively) skip large chunks of commits without visiting all commits
+//! one by one.
+//!
+//! This might miss some "change then revert" commits but practically it
+//! might be good enough.
+//!
+//! See `PathHistory` for the main structure.
+
+#[cfg(test)]
+dev_logger::init!();
