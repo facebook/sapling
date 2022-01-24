b1b8a5218aca62261428b51d0cb10e8cf47b0eec
Jan Mazur <mzr@fb.com>
2022-01-24 15:52:40 -0800

    print testtmp dir before test run
    Summary: This is especially useful when there is long-running test or a test that's stuck. With this path it will be possible to at least look at logs or configs that are being used in the test environment.
    
    Reviewed By: DurhamG
    
    Differential Revision: D33751471
    
    fbshipit-source-id: 6d131266acfef04b0aedad09eae8e5165175705f

--- eden/scm/tests/run-tests.py
+++ eden/scm/tests/run-tests.py
@@ -1151,6 +1151,7 @@ class Test(unittest.TestCase):
 
         name = self._tmpname
         self._testtmp = os.path.join(self._threadtmp, name)
+        log("testtmp dir: %s" % self._testtmp)
         os.mkdir(self._testtmp)
 
         # Remove any previous output files.
