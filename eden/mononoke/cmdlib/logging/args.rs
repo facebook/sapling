4d0efd735a359766bda2607831e8d90739080a68
Mark Juggurnauth-Thomas <mbthomas@fb.com>
2022-01-25 02:00:19 -0800

    cmdlib_logging: neaten up logging help text
    Summary: Add missing doc-comment prefixes, and tidy up some of the formatting so that --help is easier to read.
    
    Reviewed By: mitrandir77
    
    Differential Revision: D33719644
    
    fbshipit-source-id: 3f62ff06bb54a504f7f8f1459cbf48da364197f9

--- eden/mononoke/cmdlib/logging/args.rs
+++ eden/mononoke/cmdlib/logging/args.rs
@@ -11,18 +11,22 @@ use clap::{ArgEnum, Args};
 #[derive(Args, Debug)]
 pub struct LoggingArgs {
     /// Print debug output
+    ///
+    /// Equivalent to --log-level=DEBUG.
     #[clap(long)]
     pub debug: bool,
 
-    /// Log level to use (does not work with --debug)
+    /// Log level to use
     #[clap(long, conflicts_with = "debug", possible_values = &slog::LOG_LEVEL_NAMES)]
     pub log_level: Option<String>,
 
-    /// Include only log messages with these slog::Record::tags()/log::Record::targets
+    /// Include only log messages with these slog::Record::tags() or
+    /// log::Record::targets
     #[clap(long, short = 'l')]
     pub log_include_tag: Vec<String>,
 
-    // Exclude log messages with these slog::Record::tags()/log::Record::targets
+    /// Exclude log messages with these slog::Record::tags() or
+    /// log::Record::targets
     #[clap(long, short = 'L')]
     pub log_exclude_tag: Vec<String>,
 
@@ -30,10 +34,13 @@ pub struct LoggingArgs {
     #[clap(long)]
     pub logview_category: Option<String>,
 
-    /// Logview level to filter. If logview category is not set then this is
-    /// ignored. Note that this level is applied AFTER --log-level/--debug was
-    /// applied, so it doesn't make sense to set this parameter to a lower level
-    /// than --log-level
+    /// Logview level to filter
+    ///
+    /// If logview-category is not set then this is ignored.
+    ///
+    /// Note that this level is applied AFTER --log-level/--debug was applied,
+    /// so it doesn't make sense to set this parameter to a lower level than
+    /// --log-level.
     #[clap(long, requires = "logview-category", possible_values = &slog::LOG_LEVEL_NAMES)]
     pub logview_additional_level_filter: Option<String>,
 
