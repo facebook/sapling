d61ec1a974b6344a450d7b9e5446b6ab15a0de4a
Simon Farnsworth <simonfar@fb.com>
2022-01-27 04:38:07 -0800

    Support incremental dumping of public changesets
    Summary: We want to maintain prefetched changesets files for all big repos, to help in SEV recovery. As a step along the way, make it possible to dump from a starting point or the end of a file.
    
    Reviewed By: mitrandir77
    
    Differential Revision: D33639869
    
    fbshipit-source-id: 407eea1f3053a91d2305042d95e96505394ffa97

--- eden/mononoke/cmds/dump_public_changeset_entries.rs
+++ eden/mononoke/cmds/dump_public_changeset_entries.rs
@@ -5,21 +5,27 @@
  * GNU General Public License version 2.
  */
 
-use anyhow::{anyhow, Error};
+use anyhow::{anyhow, Result};
 use blobrepo::BlobRepo;
 use bulkops::{Direction, PublicChangesetBulkFetch};
-use changesets::serialize_cs_entries;
-use clap::Arg;
+use bytes::Bytes;
+use changesets::{deserialize_cs_entries, serialize_cs_entries};
+use clap::{Arg, ArgGroup};
 use cmdlib::args::{self, RepoRequirement};
+use cmdlib::helpers::csid_resolve;
 use context::CoreContext;
 use fbinit::FacebookInit;
 use futures::TryStreamExt;
+use mononoke_types::ChangesetId;
 use phases::PhasesArc;
+use std::path::Path;
 
 const ARG_OUT_FILENAME: &str = "out-filename";
+const ARG_START_COMMIT: &str = "start-commit";
+const ARG_START_FROM_FILE_END: &str = "start-from-file-end";
 
 #[fbinit::main]
-fn main(fb: FacebookInit) -> Result<(), Error> {
+fn main(fb: FacebookInit) -> Result<()> {
     let app = args::MononokeAppBuilder::new("Dump all public changeset entries to a file")
         .with_advanced_args_hidden()
         .with_fb303_args()
@@ -35,6 +41,22 @@ fn main(fb: FacebookInit) -> Result<(), Error> {
                 .takes_value(true)
                 .required(true)
                 .help("file name where commits will be saved"),
+        )
+        .arg(
+            Arg::with_name(ARG_START_COMMIT)
+                .long(ARG_START_COMMIT)
+                .takes_value(true)
+                .help("start fetching from this commit rather than the beginning of time"),
+        )
+        .arg(
+            Arg::with_name(ARG_START_FROM_FILE_END)
+                .long(ARG_START_FROM_FILE_END)
+                .takes_value(true)
+                .help("start fetching from the last commit in this file, for incremental updates"),
+        )
+        .group(
+            ArgGroup::with_name("starting-commit")
+                .args(&[ARG_START_COMMIT, ARG_START_FROM_FILE_END]),
         );
     let matches = app.get_matches(fb)?;
     let runtime = matches.runtime();
@@ -45,6 +67,10 @@ fn main(fb: FacebookInit) -> Result<(), Error> {
         .value_of(ARG_OUT_FILENAME)
         .ok_or_else(|| anyhow!("missing required argument: {}", ARG_OUT_FILENAME))?
         .to_string();
+
+    let opt_start_file = matches.value_of_os(ARG_START_FROM_FILE_END);
+    let opt_start_commit = matches.value_of(ARG_START_COMMIT);
+
     let blob_repo_fut = args::open_repo(fb, &logger, &matches);
 
     runtime.block_on(async move {
@@ -53,8 +79,21 @@ fn main(fb: FacebookInit) -> Result<(), Error> {
         let fetcher =
             PublicChangesetBulkFetch::new(repo.get_changesets_object(), repo.phases_arc());
 
+        let start_commit = {
+            if let Some(path) = opt_start_file {
+                load_last_commit(path.as_ref()).await?
+            } else if let Some(start_commit) = opt_start_commit {
+                Some(csid_resolve(&ctx, &repo, start_commit).await?)
+            } else {
+                None
+            }
+        };
+
+        let bounds = fetcher
+            .get_repo_bounds_after_commits(&ctx, start_commit.into_iter().collect())
+            .await?;
         let css = fetcher
-            .fetch(&ctx, Direction::OldestFirst)
+            .fetch_bounded(&ctx, Direction::OldestFirst, Some(bounds))
             .try_collect()
             .await?;
 
@@ -63,4 +102,10 @@ fn main(fb: FacebookInit) -> Result<(), Error> {
 
         Ok(())
     })
+}
+
+async fn load_last_commit(filename: &Path) -> Result<Option<ChangesetId>> {
+    let file_contents = Bytes::from(tokio::fs::read(filename).await?);
+    let cs_entries = deserialize_cs_entries(&file_contents)?;
+    Ok(cs_entries.last().map(|e| e.cs_id))
 }
