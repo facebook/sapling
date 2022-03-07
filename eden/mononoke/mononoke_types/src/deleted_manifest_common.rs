5068ce28ea591304a7d3dd3839dbec2cd0fe2c3e
Yan Soares Couto <yancouto@fb.com>
2022-03-07 13:37:02 -0800

    Add some documentation to DeletedManifestCommon
    Summary: Let's make it clearer what each method does.
    
    Reviewed By: farnz
    
    Differential Revision: D34611974
    
    fbshipit-source-id: c04c400926d2ae42834f525e79a9a84ce50481e6

--- eden/mononoke/mononoke_types/src/deleted_manifest_common.rs
+++ eden/mononoke/mononoke_types/src/deleted_manifest_common.rs
@@ -9,16 +9,10 @@ use blobstore::Loadable;
 
 use crate::{blob::BlobstoreValue, ChangesetId, MPathElement, MononokeId};
 
+/// This trait has common behaviour that should be shared among all versions
+/// of deleted manifest, and should be used to generalize usage of them.
 pub trait DeletedManifestCommon: BlobstoreValue<Key = Self::Id> + Clone + Send {
     type Id: MononokeId<Value = Self> + Loadable<Value = Self>;
-
-    fn is_deleted(&self) -> bool;
-
-    fn into_subentries(self) -> Box<dyn Iterator<Item = (MPathElement, Self::Id)>>;
-
-    fn id(&self) -> Self::Id;
-
-    fn lookup(&self, basename: &MPathElement) -> Option<&Self::Id>;
 
     /// Create a new deleted manifest by copying subentries from `current` and then
     /// adding the subentries from `subentries_to_add` (where `None` means "remove")
@@ -28,5 +22,20 @@ pub trait DeletedManifestCommon: BlobstoreValue<Key = Self::Id> + Clone + Send {
         subentries_to_add: impl IntoIterator<Item = (MPathElement, Option<Self::Id>)>,
     ) -> Self;
 
+    /// Lookup a specific subentry on this manifest.
+    fn lookup(&self, basename: &MPathElement) -> Option<&Self::Id>;
+
+    /// List all subentries on this manifest. Use with care, some manifests can
+    /// have hundreds of thousands of subentries.
+    fn into_subentries(self) -> Box<dyn Iterator<Item = (MPathElement, Self::Id)>>;
+
+    /// Returns whether this node has no subentries.
     fn is_empty(&self) -> bool;
+
+    /// Whether the file/directory represented by this manifest node currently exists
+    /// in the changeset.
+    fn is_deleted(&self) -> bool;
+
+    /// Calculate id of manifest object.
+    fn id(&self) -> Self::Id;
 }
