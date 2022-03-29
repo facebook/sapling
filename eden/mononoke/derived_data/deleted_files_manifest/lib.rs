919b5242fb2dc970ff75749208c2b4a4d4d06aad
Yan Soares Couto <yancouto@fb.com>
2022-03-29 08:25:23 -0700

    Move DM tests to a separate file
    Summary:
    This diff moves all tests from deleted_files_manifest/derive.rs to a `test_utils.rs` file and uses them from there.
    
    Reasoning: I will next make these tests generic over any DM, and re-use them on DMv2.
    
    Reviewed By: farnz
    
    Differential Revision: D34755699
    
    fbshipit-source-id: fb044f101d96446f131e32e614da54ccc660bbb1

--- eden/mononoke/derived_data/deleted_files_manifest/lib.rs
+++ eden/mononoke/derived_data/deleted_files_manifest/lib.rs
@@ -10,6 +10,8 @@
 mod derive;
 mod mapping;
 mod ops;
+#[cfg(test)]
+pub mod test_utils;
 
 pub use mapping::RootDeletedManifestId;
 pub use ops::{find_entries, find_entry, list_all_entries, resolve_path_state, PathState};
