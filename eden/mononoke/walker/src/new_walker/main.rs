e4e9c213e4c9d02ae6f6dd5072a06c013eb8c93f
Aida Getoeva <aida@fb.com>
2022-04-12 17:27:17 -0700

    mononoke/walker: setup thrift service
    Reviewed By: markbt
    
    Differential Revision: D35546266
    
    fbshipit-source-id: 358afd42b332f9c0daecf5518560f633c3ac7a52

--- eden/mononoke/walker/src/new_walker/main.rs
+++ eden/mononoke/walker/src/new_walker/main.rs
@@ -20,7 +20,7 @@ use cmdlib::args::CachelibSettings;
 use cmdlib_scrubbing::ScrubAppExtension;
 use fbinit::FacebookInit;
 use mononoke_app::args::MultiRepoArgs;
-use mononoke_app::fb303::Fb303AppExtension;
+use mononoke_app::fb303::{Fb303AppExtension, ReadyFlagService};
 use mononoke_app::{MononokeApp, MononokeAppBuilder};
 use multiplexedblob::ScrubWriteMostly;
 use std::num::NonZeroU32;
@@ -35,6 +35,8 @@ struct WalkerArgs {
 fn main(fb: FacebookInit) -> Result<(), Error> {
     // FIXME: Investigate why some SQL queries kicked off by the walker take 30s or more.
     newfilenodes::disable_sql_timeouts();
+
+    let service = ReadyFlagService::new();
 
     let cachelib_defaults = CachelibSettings {
         cache_size: 2 * 1024 * 1024 * 1024,
@@ -56,14 +58,21 @@ fn main(fb: FacebookInit) -> Result<(), Error> {
     let read_only_storage = ReadOnlyStorage(true);
 
     let subcommands = commands::subcommands();
-    MononokeAppBuilder::new(fb)
+    let app = MononokeAppBuilder::new(fb)
         .with_app_extension(scrub_extension)
         .with_default_cachelib_settings(cachelib_defaults)
         .with_arg_defaults(blobstore_defaults)
         .with_arg_defaults(read_only_storage)
         .with_app_extension(Fb303AppExtension {})
-        .build_with_subcommands::<WalkerArgs>(subcommands)?
-        .run(async_main)
+        .build_with_subcommands::<WalkerArgs>(subcommands)?;
+
+    // TODO: we may want to set_ready after the repo setup is done
+    service.set_ready();
+
+    let fb303_args = app.extension_args::<Fb303AppExtension>()?;
+    fb303_args.start_fb303_server(fb, "walker", app.logger(), service)?;
+
+    app.run(async_main)
 }
 
 async fn async_main(app: MononokeApp) -> Result<(), Error> {
