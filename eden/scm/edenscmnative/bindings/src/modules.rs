4af09806fdd346e63cd42f0a08eb1d4ab04478b6
Jun Wu <quark@fb.com>
2022-04-20 11:43:36 -0700

    bindings: simplify module initialization using proc macro
    Summary: Make it easier to add a new module to bindings.
    
    Reviewed By: DurhamG
    
    Differential Revision: D35585550
    
    fbshipit-source-id: 9aade63c586a69413f977dccced030ebddddb039

--- eden/scm/edenscmnative/bindings/src/modules.rs
+++ eden/scm/edenscmnative/bindings/src/modules.rs
@@ -9,70 +9,79 @@ use cpython::PyModule;
 use cpython::PyResult;
 use cpython::Python;
 use cpython_ext::PyNone;
+use paste::paste;
+
+macro_rules! add_modules {
+    ( $py:ident, $m:ident, [ $( $name:ident, )* ] ) => {
+        let name = $m.get($py, "__name__")?.extract::<String>($py)?;
+        $(
+            paste! {
+                $m.add($py, stringify!($name), ::[<py $name>]::init_module($py, &name)?)?;
+            }
+         )*
+    }
+}
 
 /// Populate an existing empty module so it contains utilities.
 pub(crate) fn populate_module(py: Python<'_>, module: &PyModule) -> PyResult<PyNone> {
     let m = module;
-    let name = m.get(py, "__name__")?.extract::<String>(py)?;
     m.add(py, "__doc__", "Mercurial Rust Bindings")?;
-    m.add(py, "auth", pyauth::init_module(py, &name)?)?;
-    m.add(py, "cats", pycats::init_module(py, &name)?)?;
-    m.add(py, "blackbox", pyblackbox::init_module(py, &name)?)?;
-    m.add(py, "bytes", pybytes::init_module(py, &name)?)?;
-    m.add(py, "checkout", pycheckout::init_module(py, &name)?)?;
-    m.add(py, "cliparser", pycliparser::init_module(py, &name)?)?;
-    m.add(py, "configparser", pyconfigparser::init_module(py, &name)?)?;
-    m.add(py, "dag", pydag::init_module(py, &name)?)?;
-    m.add(py, "diffhelpers", pydiffhelpers::init_module(py, &name)?)?;
-    m.add(py, "dirs", pydirs::init_module(py, &name)?)?;
-    m.add(py, "doctor", pydoctor::init_module(py, &name)?)?;
-    m.add(py, "drawdag", pydrawdag::init_module(py, &name)?)?;
-    m.add(py, "eagerepo", pyeagerepo::init_module(py, &name)?)?;
-    m.add(py, "edenapi", pyedenapi::init_module(py, &name)?)?;
-    m.add(py, "error", pyerror::init_module(py, &name)?)?;
-    m.add(py, "fail", pyfail::init_module(py, &name)?)?;
-    m.add(py, "fs", pyfs::init_module(py, &name)?)?;
-    m.add(py, "gitstore", pygitstore::init_module(py, &name)?)?;
-    m.add(py, "hgmetrics", pyhgmetrics::init_module(py, &name)?)?;
-    m.add(py, "hgtime", pyhgtime::init_module(py, &name)?)?;
-    m.add(py, "lock", pylock::init_module(py, &name)?)?;
-    m.add(py, "lz4", pylz4::init_module(py, &name)?)?;
-    m.add(py, "manifest", pymanifest::init_module(py, &name)?)?;
-    m.add(py, "metalog", pymetalog::init_module(py, &name)?)?;
-    m.add(
+    add_modules!(
         py,
-        "mutationstore",
-        pymutationstore::init_module(py, &name)?,
-    )?;
-    m.add(py, "nodemap", pynodemap::init_module(py, &name)?)?;
-    m.add(py, "io", pyio::init_module(py, &name)?)?;
-    m.add(py, "pathhistory", pypathhistory::init_module(py, &name)?)?;
-    m.add(py, "pathmatcher", pypathmatcher::init_module(py, &name)?)?;
-    m.add(py, "pprint", pypprint::init_module(py, &name)?)?;
-    m.add(py, "process", pyprocess::init_module(py, &name)?)?;
-    m.add(py, "progress", pyprogress::init_module(py, &name)?)?;
-    m.add(py, "exchange", pyexchange::init_module(py, &name)?)?;
-    m.add(py, "refencode", pyrefencode::init_module(py, &name)?)?;
-    m.add(py, "regex", pyregex::init_module(py, &name)?)?;
-    m.add(py, "renderdag", pyrenderdag::init_module(py, &name)?)?;
-    m.add(py, "repo", pyrepo::init_module(py, &name)?)?;
-    m.add(
-        py,
-        "revisionstore",
-        pyrevisionstore::init_module(py, &name)?,
-    )?;
-    m.add(py, "revlogindex", pyrevlogindex::init_module(py, &name)?)?;
-    m.add(py, "sptui", pysptui::init_module(py, &name)?)?;
-    m.add(py, "status", pystatus::init_module(py, &name)?)?;
-    m.add(py, "threading", pythreading::init_module(py, &name)?)?;
-    m.add(py, "tracing", pytracing::init_module(py, &name)?)?;
-    m.add(py, "treestate", pytreestate::init_module(py, &name)?)?;
-    m.add(py, "vlq", pyvlq::init_module(py, &name)?)?;
-    m.add(py, "workingcopy", pyworkingcopy::init_module(py, &name)?)?;
-    m.add(py, "worker", pyworker::init_module(py, &name)?)?;
-    m.add(py, "zstd", pyzstd::init_module(py, &name)?)?;
-    m.add(py, "clientinfo", pyclientinfo::init_module(py, &name)?)?;
-    m.add(py, "zstore", pyzstore::init_module(py, &name)?)?;
+        m,
+        [
+            auth,
+            blackbox,
+            bytes,
+            cats,
+            checkout,
+            clientinfo,
+            cliparser,
+            configparser,
+            dag,
+            diffhelpers,
+            dirs,
+            doctor,
+            drawdag,
+            eagerepo,
+            edenapi,
+            error,
+            exchange,
+            fail,
+            fs,
+            gitstore,
+            hgmetrics,
+            hgtime,
+            io,
+            lock,
+            lz4,
+            manifest,
+            metalog,
+            mutationstore,
+            nodemap,
+            pathhistory,
+            pathmatcher,
+            pprint,
+            process,
+            progress,
+            refencode,
+            regex,
+            renderdag,
+            repo,
+            revisionstore,
+            revlogindex,
+            sptui,
+            status,
+            threading,
+            tracing,
+            treestate,
+            vlq,
+            worker,
+            workingcopy,
+            zstd,
+            zstore,
+        ]
+    );
 
     Ok(PyNone)
 }
