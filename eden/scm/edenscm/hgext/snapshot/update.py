5489153987aacd88d4df3434e999e9f8d1dc5dbd
Rajiv Sharma <rajshar@fb.com>
2022-04-20 10:45:14 -0700

    Instrumenting snapshot update client command
    Summary: Current console output includes only high level snapshot update/restore data. This diff adds instrumentation over the individual steps within the snapshot update/restore command.
    
    Reviewed By: yancouto
    
    Differential Revision: D35750512
    
    fbshipit-source-id: 0d7dfb9550f5fb72ecaf08091e5a5501d652bfe4

--- eden/scm/edenscm/hgext/snapshot/update.py
+++ eden/scm/edenscm/hgext/snapshot/update.py
@@ -3,6 +3,8 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
+import time
+
 from edenscm.mercurial import hg, scmutil, cmdutil, error
 from edenscm.mercurial.i18n import _
 
@@ -17,6 +19,7 @@ def _hasanychanges(repo):
 
 
 def _fullclean(ui, repo, exclude):
+    start_time = time.perf_counter()
     ui.status(_("cleaning up uncommitted code\n"), component="snapshot")
     # The order of operations to cleanup here is very deliberate, to avoid errors.
     # Most errors happen due to file/dir clashes, see https://fburl.com/jwyhd0fk
@@ -48,6 +51,13 @@ def _fullclean(ui, repo, exclude):
         no_backup=True,
         exclude=exclude,
     )
+    duration = time.perf_counter() - start_time
+    ui.status(
+        _("cleaned up uncommitted code in {duration:0.5f} seconds\n").format(
+            duration=duration
+        ),
+        component="snapshot",
+    )
 
 
 def fetchsnapshot(repo, csid):
@@ -59,10 +69,13 @@ def fetchsnapshot(repo, csid):
 
 
 def update(ui, repo, csid, clean=False):
-    ui.status(_("Will restore snapshot {}\n").format(csid), component="snapshot")
-    csid = bytes.fromhex(csid)
+    ui.status(
+        _("Will restore snapshot {}\n").format(csid.format()), component="snapshot"
+    )
+    start_snapshot = time.perf_counter()
+    csid_bytes = bytes.fromhex(csid)
 
-    snapshot = fetchsnapshot(repo, csid)
+    snapshot = fetchsnapshot(repo, csid_bytes)
 
     # Once merges/conflicted states are supported, we'll need to support more
     # than one parent
@@ -81,6 +94,7 @@ def update(ui, repo, csid, clean=False):
         if parent != repo.dirstate.p1():
             if haschanges:
                 _fullclean(ui, repo, [])
+            start_parent_update = time.perf_counter()
             ui.status(
                 _("Updating to parent {}\n").format(parent.hex()),
                 component="snapshot",
@@ -92,6 +106,13 @@ def update(ui, repo, csid, clean=False):
                 repo.pull(headnodes=(parent,))
 
             hg.updatetotally(ui, repo, parent, None, clean=False, updatecheck="abort")
+            duration = time.perf_counter() - start_parent_update
+            ui.status(
+                _("Updated to parent {parent} in {duration:0.5f} seconds\n").format(
+                    parent=parent.hex(), duration=duration
+                ),
+                component="snapshot",
+            )
         else:
             if haschanges:
                 # We might be able to reuse files that were already downloaded locally,
@@ -139,13 +160,30 @@ def update(ui, repo, csid, clean=False):
                         filetype,
                     )
                 )
-
+        ui.status(
+            _("Downloading files for restoring snapshot\n"),
+            component="snapshot",
+        )
+        start_download = time.perf_counter()
         repo.edenapi.downloadfiles(repo.root, files2download)
-
+        duration = time.perf_counter() - start_download
+        ui.status(
+            _(
+                "Downloaded files for restoring snapshot in {duration:0.5f} seconds\n"
+            ).format(duration=duration),
+            component="snapshot",
+        )
         # Need to add changed files after they are populated in the working dir
         wctx.add(
             [path for (path, fc) in snapshot["file_changes"] if "Change" in fc],
             quiet=True,
         )
 
-        storelatest(repo.metalog(), csid, snapshot["bubble_id"])
+        storelatest(repo.metalog(), csid_bytes, snapshot["bubble_id"])
+        duration = time.perf_counter() - start_snapshot
+        ui.status(
+            _("Restored snapshot in {duration:0.5f} seconds\n").format(
+                duration=duration
+            ),
+            component="snapshot",
+        )
