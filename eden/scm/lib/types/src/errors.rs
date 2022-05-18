723a99e4dd1f84f376bb7bbac2ce52fcb2a9ef97
Muir Manders <muirdm@fb.com>
2022-05-18 15:05:05 -0700

    types: add wrapper error to mark network errors
    Summary: Add an error type NetworkError meant to be used as a marker for network related errors. Libraries can use this to mark certain errors, and then higher level application logic can search through the error chain for the NetworkError marker to decide if there was a network error.
    
    Reviewed By: quark-zju
    
    Differential Revision: D36391258
    
    fbshipit-source-id: 767ad879c3daeaa3ac4d84305a2fc9ee6d8c8cfb

--- eden/scm/lib/types/src/errors.rs
+++ eden/scm/lib/types/src/errors.rs
@@ -17,3 +17,52 @@ impl KeyError {
         KeyError(err)
     }
 }
+
+/// NeworkError is a wrapper/tagging error meant for libraries to use
+/// to mark errors that may imply a network problem.
+#[derive(Debug, Error)]
+#[error("Network Error: {0:?}")]
+pub struct NetworkError(#[source] Error);
+
+impl NetworkError {
+    pub fn wrap(err: impl Into<Error>) -> Error {
+        Self(err.into()).into()
+    }
+}
+
+pub fn is_network_error(err: &Error) -> bool {
+    has_in_chain::<NetworkError>(err)
+}
+
+fn has_in_chain<E>(err: &Error) -> bool
+where
+    E: std::error::Error + 'static,
+{
+    err.chain().any(|e| e.is::<E>())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    use super::*;
+
+    #[test]
+    fn test_network_error() {
+        let network: &dyn std::error::Error =
+            &NetworkError(io::Error::from(io::ErrorKind::Other).into());
+        assert!(network.is::<NetworkError>());
+        assert!(network.source().unwrap().is::<io::Error>());
+
+        let network: Error = NetworkError(io::Error::from(io::ErrorKind::Other).into()).into();
+        assert!(network.is::<NetworkError>());
+        assert!(network.source().unwrap().is::<io::Error>());
+        assert!(is_network_error(&network));
+
+        let with_context = network.context("hello");
+        assert!(is_network_error(&with_context));
+
+        let wrapped: Error = KeyError(with_context).into();
+        assert!(is_network_error(&wrapped));
+    }
+}
