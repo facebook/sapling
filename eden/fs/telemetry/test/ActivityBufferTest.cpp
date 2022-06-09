34ad4af7f995c4770ddec4a95addd5f1a74cf302
Andrew Milas <andrewmilas@fb.com>
2022-06-09 10:26:46 -0700

    Added Test Suite for ActivityBuffer
    Summary:
    This change creates unit tests for the ActivityBuffer class. ActivityBuffer is a fixed size buffer that will store recent events that have happened within EdenFS (currently this consists of only InodeMaterializeEvents though we intend for this to be more general later), and the purpose of this class is to allow for easier debugging retroactively of past Eden events by reading from the ActivityBuffer. This change specifically adds three tests:
    1. To test if an ActivityBuffer can be instantiated without errors
    2. To test if adding to an ActivityBuffer indeed adds it to the buffer's list of events
    3. To test if events get removed when the buffer reaches its size limit
    
    Reviewed By: MichaelCuevas, kmancini
    
    Differential Revision: D37008866
    
    fbshipit-source-id: 3316898ede317bd648a13e7d5cc308d9267c4209

---
+++ eden/fs/telemetry/test/ActivityBufferTest.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "eden/fs/telemetry/ActivityBuffer.h"
+#include <folly/portability/GTest.h>
+
+constexpr uint32_t kMaxBufLength = 10;
+using namespace facebook::eden;
+
+bool buffer_contains_event_with_ino(ActivityBuffer& buff, InodeNumber ino) {
+  auto events = buff.getAllEvents();
+  return std::find_if(events.begin(), events.end(), [&](auto event) {
+           return event.ino.getRawValue() == ino.getRawValue();
+         }) != events.end();
+}
+
+TEST(ActivityBufferTest, initialize_buffer) {
+  ActivityBuffer buff(kMaxBufLength);
+}
+
+TEST(ActivityBufferTest, buffer_zero_capacity) {
+  ActivityBuffer buff(0);
+  EXPECT_TRUE(buff.getAllEvents().empty());
+  std::chrono::steady_clock::time_point time = std::chrono::steady_clock::now();
+  InodeMaterializeEvent event{
+      time, InodeNumber(1), InodeType::File, std::chrono::microseconds(1000)};
+  buff.addEvent(event);
+
+  // Setting the ActivityBuffer max size to 0 means that events never get stored
+  // EXPECT_TRUE(buff.getAllEvents().empty());
+  // EXPECT_FALSE(buffer_contains_event_with_ino(buff, InodeNumber(1)));
+}
+
+TEST(ActivityBufferTest, add_events) {
+  ActivityBuffer buff(kMaxBufLength);
+  for (uint64_t i = 1; i <= kMaxBufLength; i++) {
+    std::chrono::steady_clock::time_point time =
+        std::chrono::steady_clock::now();
+    InodeMaterializeEvent event{
+        time, InodeNumber(i), InodeType::File, std::chrono::microseconds(1000)};
+    buff.addEvent(event);
+
+    // EXPECT_EQ(buff.getAllEvents().size(), i);
+    // EXPECT_TRUE(buffer_contains_event_with_ino(buff, event.ino));
+  }
+
+  // Check in this case all events are still stored and nothing was evicted yet
+  // for (uint64_t i = 1; i <= kMaxBufLength; i++) {
+  //   EXPECT_TRUE(buffer_contains_event_with_ino(buff, InodeNumber(i)));
+  // }
+}
+
+TEST(ActivityBufferTest, add_exceed_capacity) {
+  ActivityBuffer buff(kMaxBufLength);
+  for (uint64_t i = 1; i <= kMaxBufLength + 1; i++) {
+    std::chrono::steady_clock::time_point time =
+        std::chrono::steady_clock::now();
+    InodeMaterializeEvent event{
+        time, InodeNumber(i), InodeType::File, std::chrono::microseconds(1000)};
+    buff.addEvent(event);
+  }
+
+  // Check that the buffer remained at its max size of kMaxBufLength and that
+  // the oldest event (which had InodeNumber(1)) has been removed as expected
+  // EXPECT_EQ(buff.getAllEvents().size(), kMaxBufLength);
+  // EXPECT_FALSE(buffer_contains_event_with_ino(buff, InodeNumber(1)));
+  // for (uint64_t i = 2; i <= kMaxBufLength + 1; i++) {
+  //   EXPECT_TRUE(buffer_contains_event_with_ino(buff, InodeNumber(i)));
+  // }
+}
