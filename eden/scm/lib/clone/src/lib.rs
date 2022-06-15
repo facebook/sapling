e6dc65f02ddcdce1e2d01d32be05da8681865027
Carolyn Busch <carolynbusch@fb.com>
2022-06-15 13:18:52 -0700

    clone: add eden clone method
    Summary: Add method for creating eden checkout by running eden clone command. Note we need to decide what user facing messaging to use with the eden clone.
    
    Differential Revision: D36927200
    
    fbshipit-source-id: 57c581bb248fa2597c4ed7c69afe86e174ee3717

--- eden/scm/lib/clone/src/lib.rs
+++ eden/scm/lib/clone/src/lib.rs
@@ -6,9 +6,13 @@
  */
 
 use std::env;
+use std::ffi::OsStr;
 use std::io::Write;
+use std::path::Path;
 use std::path::PathBuf;
+use std::process::Command;
 
+use anyhow::Context;
 use anyhow::Result;
 use async_runtime::block_unless_interrupted as block_on;
 use configmodel::config::Config;
@@ -91,6 +95,63 @@ pub fn init_working_copy(
         target,
     )?;
 
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum EdenCloneError {
+    #[error("Failed cloning eden checkout\n Stdout: '{0}'\n Stderr: '{1}'")]
+    ExeuctionFailure(String, String),
+    #[error("edenfs.command config is not set")]
+    MissingCommandConfig(),
+}
+
+pub fn eden_clone(backing_repo: &Repo, working_copy: &Path, target: Option<HgId>) -> Result<()> {
+    let config = backing_repo.config();
+    let eden_command = config.get_opt::<String>("edenfs", "command")?;
+    let mut clone_command = match eden_command {
+        Some(cmd) => Command::new(cmd),
+        None => return Err(EdenCloneError::MissingCommandConfig().into()),
+    };
+
+    // allow tests to specify different configuration directories from prod defaults
+    if let Some(base_dir) = config.get_opt::<PathBuf>("edenfs", "basepath")? {
+        clone_command.args([
+            "--config-dir".into(),
+            base_dir.join("eden"),
+            "--etc-eden-dir".into(),
+            base_dir.join("etc_eden"),
+            "--home-dir".into(),
+            base_dir.join("home"),
+        ]);
+    }
+
+    clone_command.args([
+        OsStr::new("clone"),
+        backing_repo.path().as_os_str(),
+        working_copy.as_os_str(),
+    ]);
+
+    if let Some(rev) = target {
+        clone_command.args(["-r", &rev.to_hex()]);
+    } else {
+        clone_command.arg("--allow-empty-repo");
+    }
+
+    let output = clone_command.output().with_context(|| {
+        format!(
+            "failed to execute {}",
+            clone_command.get_program().to_string_lossy()
+        )
+    })?;
+
+    if !output.status.success() {
+        return Err(EdenCloneError::ExeuctionFailure(
+            String::from_utf8_lossy(&output.stdout).to_string(),
+            String::from_utf8_lossy(&output.stderr).to_string(),
+        )
+        .into());
+    }
     Ok(())
 }
 
