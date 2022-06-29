c36dcf8de2261f91de8403b1ef5e0696035c9929
Yipu Miao <yipu@fb.com>
2022-06-29 13:40:50 -0700

    Do not enable use_write_back_cache for RECAS
    Summary: There is an issue on write_back_cache for flash disk, that might cause builds very slow so disable for now.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D37510215
    
    fbshipit-source-id: 6246682f8ce78766d1cd4599854f6b565d4474c5

--- eden/fs/cli/main.py
+++ eden/fs/cli/main.py
@@ -876,12 +876,6 @@ want nested checkouts, re-run `eden clone` with --allow-nested-checkout or -n.""
 
         return overlay_type == "tree"
 
-    def _get_use_write_back_cache(self, backing_store_type: Optional[str]) -> bool:
-        if backing_store_type is None:
-            return False
-        # Enable write back cache feature for RE CAS backing store
-        return backing_store_type == "recas"
-
     def _get_repo_info(
         self,
         instance: EdenInstance,
@@ -912,8 +906,6 @@ want nested checkouts, re-run `eden clone` with --allow-nested-checkout or -n.""
 
         enable_tree_overlay = self._get_enable_tree_overlay(instance, overlay_type)
 
-        use_write_back_cache = self._get_use_write_back_cache(backing_store_type)
-
         # New clones on macOS and Windows are case insensitive.
         case_sensitive = sys.platform == "linux"
 
@@ -932,7 +924,7 @@ want nested checkouts, re-run `eden clone` with --allow-nested-checkout or -n.""
             predictive_prefetch_profiles_active=False,
             predictive_prefetch_num_dirs=0,
             enable_tree_overlay=enable_tree_overlay,
-            use_write_back_cache=use_write_back_cache,
+            use_write_back_cache=False,
         )
 
         return repo, repo_config
