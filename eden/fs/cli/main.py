98aae840048106ddd06dbba7d0f776883c80428f
Durham Goode <durham@fb.com>
2022-07-06 15:53:53 -0700

    Make 'eden rm' remove normal directories as well
    Summary:
    Users get confused about when to use eden rm and when to use rm -rf.
    Usually they just want a big hammer that gets rid of a repository, so let's make
    'eden rm' do 'rm -rf' if necessary.
    
    Reviewed By: xavierd
    
    Differential Revision: D37390756
    
    fbshipit-source-id: 9484026b6df94561f05a9ca5f5c10c8ee65f131d

--- eden/fs/cli/main.py
+++ eden/fs/cli/main.py
@@ -1270,6 +1270,7 @@ class RemoveType(enum.Enum):
     ACTIVE_MOUNT = 0
     INACTIVE_MOUNT = 1
     CLEANUP_ONLY = 2
+    NON_EDEN_DIR = 3
 
 
 @subcmd("remove", "Remove an EdenFS checkout", aliases=["rm"])
@@ -1341,8 +1342,7 @@ class RemoveCmd(Subcmd):
                 elif self.is_prjfs_path(path):
                     remove_type = RemoveType.CLEANUP_ONLY
                 else:
-                    print(f"error: {ex}")
-                    return 1
+                    remove_type = RemoveType.NON_EDEN_DIR
             except Exception as ex:
                 print(f"error: cannot determine mount point for {path}: {ex}")
                 return 1
@@ -1357,7 +1357,16 @@ class RemoveCmd(Subcmd):
 
         # Warn the user since this operation permanently destroys data
         if args.prompt and sys.stdin.isatty():
-            mounts_list = "\n  ".join(path for path, _ in mounts)
+            mounts_list = "\n  ".join(
+                "%s  %s"
+                % (
+                    path,
+                    "(NOT AN EDEN DIRECTORY BUT WILL BE DELETED)"
+                    if type == RemoveType.NON_EDEN_DIR
+                    else "",
+                )
+                for path, type in mounts
+            )
             print(
                 f"""\
 Warning: this operation will permanently delete the following checkouts:
@@ -1410,7 +1419,10 @@ Any uncommitted changes and shelves in this checkout will be lost forever."""
                     # ahead delete the mount from the config in this case.
 
             try:
-                if remove_type != RemoveType.CLEANUP_ONLY:
+                if (
+                    remove_type != RemoveType.CLEANUP_ONLY
+                    and remove_type != RemoveType.NON_EDEN_DIR
+                ):
                     instance.destroy_mount(mount, args.preserve_mount_point)
             except Exception as ex:
                 print_stderr(f"error deleting configuration for {mount}: {ex}")
