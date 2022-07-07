c1626be77189ef8bb3c8ac11c3e2a520d84d4455
Muir Manders <muirdm@fb.com>
2022-07-07 15:19:21 -0700

    util: add some utils for opening/reading files
    Summary:
    Add open()/read() which automatically contextualize errors to include the path.
    
    I modeled open()'s API after dynamic languages (e.g. `open("/tmp/foo", "cwt")` is create(true).write(true).truncate(true)). This gets rid of the option builder boilerplate and allows to concisely put the attempted open() options into the error message.
    
    Reviewed By: DurhamG
    
    Differential Revision: D37489846
    
    fbshipit-source-id: b79eba672e5374ed56041f4f315262dc22a5dec8

--- eden/scm/lib/util/src/file.rs
+++ eden/scm/lib/util/src/file.rs
@@ -6,11 +6,15 @@
  */
 
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io;
 use std::path::Path;
 
 #[cfg(unix)]
 use once_cell::sync::Lazy;
+
+use crate::errors::IOContext;
+use crate::errors::IOResult;
 
 #[cfg(unix)]
 static UMASK: Lazy<u32> = Lazy::new(|| unsafe {
@@ -32,4 +36,58 @@ pub fn atomic_write(path: &Path, op: impl FnOnce(&mut File) -> io::Result<()>) -
 /// Open a path for atomic writing.
 pub fn atomic_open(path: &Path) -> io::Result<atomicfile::AtomicFile> {
     atomicfile::AtomicFile::open(path, 0o644, false)
+}
+
+pub fn open(path: impl AsRef<Path>, mode: &str) -> IOResult<File> {
+    let path = path.as_ref();
+
+    let mut opts = OpenOptions::new();
+    for opt in mode.chars() {
+        match opt {
+            'r' => opts.read(true),
+            'w' => opts.write(true),
+            'a' => opts.append(true),
+            'c' => opts.create(true),
+            't' => opts.truncate(true),
+            'x' => opts.create_new(true),
+            _ => {
+                return Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    format!("invalid open() mode {}", opt),
+                ))
+                .path_context("error opening file", path);
+            }
+        };
+    }
+
+    opts.open(path).path_context("error opening file", path)
+}
+
+pub fn read(path: impl AsRef<Path>) -> IOResult<Vec<u8>> {
+    std::fs::read(path.as_ref()).path_context("error reading file", path.as_ref())
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+    use tempfile::tempdir;
+
+    use super::*;
+
+    #[test]
+    fn test_open_context() -> Result<()> {
+        let dir = tempdir()?;
+
+        let path = dir.path().join("doesnt").join("exist");
+        let err_str = format!("{}", open(&path, "cwa").unwrap_err());
+
+        // Make sure error contains path.
+        assert!(err_str.contains(path.display().to_string().as_str()));
+
+        // And the original error.
+        let orig_err = format!("{}", std::fs::File::open(&path).unwrap_err());
+        assert!(err_str.contains(&orig_err));
+
+        Ok(())
+    }
 }
