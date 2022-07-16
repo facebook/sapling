7b05744665c07ead40776204d9820f0ea9719ffa
Saul Gutierrez <sggutier@fb.com>
2022-07-15 19:04:09 -0700

    config: implement basic config display in the Rust config command
    Summary:
    The config command has two main features: displaying the value of configs and launching an editor for editing configs. This commit implements the former, but support for templates and the `--debug` option (which shows the origin of each config) is still pending.
    
    In the new implementation sections and individual items are sorted alphabetically instead of using the default ordering the Python version had.
    
    Reviewed By: DurhamG
    
    Differential Revision: D37512487
    
    fbshipit-source-id: 0d6d0a6f017c46544afcdb19a28dd9fd2cf7fd35

--- eden/scm/lib/hgcommands/src/commands/config.rs
+++ eden/scm/lib/hgcommands/src/commands/config.rs
@@ -5,13 +5,21 @@
  * GNU General Public License version 2.
  */
 
+use std::collections::BTreeSet;
+use std::iter::FromIterator;
+
 use anyhow::bail;
 use clidispatch::abort;
+use clidispatch::abort_if;
 use clidispatch::errors;
 use clidispatch::global_flags::HgGlobalOpts;
+use clidispatch::io::IsTty;
 use cliparser::define_flags;
+use configparser::Config;
+use minibytes::Text;
 use repo::repo::OptionalRepo;
 
+use super::ConfigSet;
 use super::Result;
 use super::IO;
 
@@ -43,9 +51,9 @@ define_flags! {
 }
 
 pub fn run(
-    _config_opts: ConfigOpts,
-    _global_opts: HgGlobalOpts,
-    _io: &IO,
+    config_opts: ConfigOpts,
+    global_opts: HgGlobalOpts,
+    io: &IO,
     repo: &mut OptionalRepo,
 ) -> Result<u8> {
     let config = repo.config();
@@ -58,7 +66,95 @@ pub fn run(
         bail!(errors::FallbackToPython(short_name()));
     }
 
-    abort!("Not implemented")
+    if config_opts.edit
+        || config_opts.local
+        || config_opts.global
+        || !config_opts.formatter_opts.template.is_empty()
+        || global_opts.debug
+    {
+        bail!(errors::FallbackToPython(short_name()));
+    }
+
+    let config = repo.config();
+
+    if io.output().is_tty() {
+        io.start_pager(config)?;
+    }
+
+    show_configs(config_opts.args, io, config)
+}
+
+fn show_configs(requested_configs: Vec<String>, io: &IO, config: &ConfigSet) -> Result<u8> {
+    let requested_items: Vec<_> = requested_configs
+        .iter()
+        .filter(|a| a.contains('.'))
+        .cloned()
+        .collect();
+    let requested_sections: BTreeSet<_> = requested_configs
+        .into_iter()
+        .filter_map(|a| {
+            if !a.contains('.') {
+                Some(Text::from(a))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    abort_if!(requested_items.len() > 1, "only one config item permitted");
+    abort_if!(
+        !requested_items.is_empty() && !requested_sections.is_empty(),
+        "combining sections and items not permitted"
+    );
+
+    if requested_items.len() == 1 {
+        let item = &requested_items[0];
+        let parts: Vec<_> = item.splitn(2, '.').collect();
+        if let Some(value) = config.get_nonempty(parts[0], parts[1]) {
+            io.write(format!("{}\n", value))?;
+            return Ok(0);
+        }
+        // Config is expected to return an empty string if anything goes wrong
+        return Ok(1);
+    }
+
+    let config_sections: BTreeSet<_> = BTreeSet::from_iter(config.sections());
+    let empty_selection = requested_sections.is_empty();
+    let selected_sections: Box<dyn Iterator<Item = &Text>> = if empty_selection {
+        Box::new(config_sections.iter())
+    } else {
+        Box::new(requested_sections.intersection(&config_sections))
+    };
+    let mut selected_sections = selected_sections.peekable();
+
+    if selected_sections.peek().is_none() {
+        return Ok(1);
+    }
+
+    for section in selected_sections {
+        let mut keys = config.keys(section);
+        keys.sort();
+        for key in keys {
+            if empty_selection
+                && config
+                    .get_sources(&section, &key)
+                    .iter()
+                    .any(|source| source.source().to_string().as_str() == "builtin.rc")
+            {
+                continue;
+            }
+            if let Some(value) = config.get(&section, &key) {
+                io.write(format!(
+                    "{}.{}={}\n",
+                    section,
+                    key,
+                    value.replace('\n', "\\n")
+                ))?;
+            }
+        }
+    }
+
+    Ok(0)
 }
 
 pub fn name() -> &'static str {
