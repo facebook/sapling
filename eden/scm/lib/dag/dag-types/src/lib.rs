c093f592e2f50d3467c33a1a8610e7f096743416
Jun Wu <quark@fb.com>
2022-07-22 13:01:44 -0700

    lib: relicense 11 libraries from GPL2 to MIT (headers)
    Summary:
    Update license headers for libraries being relicensed (atomicfile, dag
    (excluding bindag, benches, fuzz), dag/dag-types, dev-logger, drawdag,
    indexedlog, mincode, minibench, minibytes, nonblocking, vlqencoding).
    
    Differential Revision: D38053010
    
    fbshipit-source-id: f352e3cbc69c7e34d26e242485cdd2631b6f98e0

--- eden/scm/lib/dag/dag-types/src/lib.rs
+++ eden/scm/lib/dag/dag-types/src/lib.rs
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
- * This software may be used and distributed according to the terms of the
- * GNU General Public License version 2.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 //! A subset of types that are related to wire protocols and used by the dag crate.
--- eden/scm/lib/dag/src/vertex_options.rs
+++ eden/scm/lib/dag/src/vertex_options.rs
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
- * This software may be used and distributed according to the terms of the
- * GNU General Public License version 2.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 use std::collections::HashSet;
