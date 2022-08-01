f6a52160c59c4feefe2a380e908a416aedca5ecd
Philip Pronin <philipp@fb.com>
2022-08-01 07:09:24 -0700

    use appendCodePointToUtf8
    Summary:
    Replace `codePointToUtf8` with `appendCodePointToUtf8` (D38305991)
    where it makes sense.
    
    Reviewed By: luciang
    
    Differential Revision: D38306295
    
    fbshipit-source-id: 6203f3509cc5bedd8cf8d484305ac8be583c16d6

--- eden/fs/utils/Utf8.cpp
+++ eden/fs/utils/Utf8.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "eden/fs/utils/Utf8.h"
+
 #include <folly/Unicode.h>
 
 namespace facebook::eden {
@@ -13,14 +14,11 @@ namespace facebook::eden {
 std::string ensureValidUtf8(folly::ByteRange str) {
   std::string output;
   output.reserve(str.size());
-
   const unsigned char* begin = str.begin();
   const unsigned char* const end = str.end();
   while (begin != end) {
-    // codePointToUtf8 returns a std::string which is inefficient for something
-    // that always fits in 32 bits, but with SSO it probably never allocates at
-    // least.
-    output += folly::codePointToUtf8(folly::utf8ToCodePoint(begin, end, true));
+    folly::appendCodePointToUtf8(
+        folly::utf8ToCodePoint(begin, end, true), output);
   }
   return output;
 }
