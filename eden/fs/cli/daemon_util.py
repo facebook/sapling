d1b820a34a5c72e495a51cf61ef57c5d8a908d81
Genevieve Helsel <helsel@fb.com>
2022-08-03 15:22:12 -0700

    check for buck-out binary for arm64 while looking for executable
    Reviewed By: xavierd
    
    Differential Revision: D38392699
    
    fbshipit-source-id: 51048492340494eef5ee06844c22d0f3713bf79c

--- eden/fs/cli/daemon_util.py
+++ eden/fs/cli/daemon_util.py
@@ -10,6 +10,8 @@ import os
 import sys
 from typing import Optional
 
+from eden.fs.cli.util import is_apple_silicon
+
 
 class DaemonBinaryNotFound(Exception):
     def __init__(self) -> None:
@@ -42,6 +44,15 @@ def _find_default_daemon_binary() -> Optional[str]:
     permissions = os.R_OK | os.X_OK
     if os.access(candidate, permissions):
         return candidate
+
+    if is_apple_silicon():
+        # This is where the binary will be found relative to this file when it is
+        # run out of buck-out in debug mode for ARM64
+        candidate = os.path.normpath(
+            os.path.join(cli_dir, "../service/edenfs#macosx-arm64")
+        )
+        if os.access(candidate, permissions):
+            return candidate
 
     # This is where the binary will be found relative to this file when it is
     # run out of buck-out in debug mode.
