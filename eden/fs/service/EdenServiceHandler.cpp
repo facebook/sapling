9950d126afbf04b10303c1bf2f7ade036a05608f
Xavier Deguillard <xavierd@fb.com>
2022-09-01 16:53:58 -0700

    service: make sure globbing runs in the background when requested
    Summary:
    Now that the glob code relies exclusively on ImmediateFuture, the globs are
    resolved eagerly during the Thrift call. This is usually what is desired,
    except when a background glob is requested, in which case the intent is for
    the Thrift call to complete as fast as possible and for the globbing to occur
    in the background. By using the newly introduced makeNotReadyImmediateFuture
    API, we can achieve this by keeping the rest of the code unchanged.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D37331421
    
    fbshipit-source-id: 26685cbfbf5b229f0e987cda483b876a55aa998b

--- eden/fs/service/EdenServiceHandler.cpp
+++ eden/fs/service/EdenServiceHandler.cpp
@@ -2529,35 +2529,27 @@ EdenServiceHandler::semifuture_globFiles(std::unique_ptr<GlobParams> params) {
       toLogArg(*params->globs_ref()),
       globber.logString());
   auto& context = helper->getPrefetchFetchContext();
-  auto mount = server_->getMount(AbsolutePathPiece{*params->mountPoint_ref()});
-  auto serverState = server_->getServerState();
-  auto& globs = *params->globs();
+  auto isBackground = *params->background();
 
-  ImmediateFuture<std::unique_ptr<Glob>> globFut;
-  if (*params->background()) {
-    globFut = ImmediateFuture{folly::makeSemiFuture().deferValue(
-        [globber = std::move(globber),
-         mount = std::move(mount),
-         serverState = std::move(serverState),
-         globs = std::move(globs),
-         &context](auto&&) mutable {
-          return globber
-              .glob(
-                  std::move(mount),
-                  std::move(serverState),
-                  std::move(globs),
-                  context)
-              .semi();
-        })};
-  } else {
-    globFut = globber.glob(mount, serverState, std::move(globs), context);
+  ImmediateFuture<folly::Unit> backgroundFuture;
+  if (isBackground) {
+    backgroundFuture = makeNotReadyImmediateFuture();
   }
 
-  globFut = std::move(globFut).ensure([helper = std::move(helper)] {});
+  auto globFut =
+      std::move(backgroundFuture)
+          .thenValue([mount = server_->getMount(
+                          AbsolutePathPiece{*params->mountPoint()}),
+                      serverState = server_->getServerState(),
+                      globs = std::move(*params->globs()),
+                      globber = std::move(globber),
+                      &context](auto&&) mutable {
+            return globber.glob(mount, serverState, std::move(globs), context);
+          });
+  globFut = std::move(globFut).ensure(
+      [helper = std::move(helper), params = std::move(params)] {});
   return detachIfBackgrounded(
-             std::move(globFut),
-             server_->getServerState(),
-             *params->background())
+             std::move(globFut), server_->getServerState(), isBackground)
       .semi();
 }
 
