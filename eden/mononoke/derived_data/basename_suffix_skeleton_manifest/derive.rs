dc53920c95a3db2e8e6ca50202aeee6bbf3e272e
Yan Soares Couto <yancouto@fb.com>
2022-09-01 09:00:11 -0700

    Derive first layer of manifest using sharded map optimisation
    Summary:
    This optimises BSSM, by using the benefits of sharded maps on the first layer of directories.
    
    **Why is this necessary?** The first layer (root directories) will be very big on BSSM, since we'll have a root directory for each different basename in the repo. The only way this can work is by taking advantage of the sharded maps improvement.
    
    **Why not use it everywhere, only the first layer?** Simply because it would need some way more advanced refactoring of the common manifest libraries, while this change is simple enough.
    
    Differential Revision: D38795647
    
    fbshipit-source-id: 4abf1ea953c608e0f830aa7a1b45408e8b7b22d8

--- eden/mononoke/derived_data/basename_suffix_skeleton_manifest/derive.rs
+++ eden/mononoke/derived_data/basename_suffix_skeleton_manifest/derive.rs
@@ -7,15 +7,20 @@
 
 //! See docs/basename_suffix_skeleton_manifest.md for more information
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use anyhow::Result;
 use blobstore::Blobstore;
+use blobstore::Loadable;
 use blobstore::Storable;
 use cloned::cloned;
 use context::CoreContext;
 use derived_data_manager::DerivationContext;
 use futures::future::FutureExt;
+use futures::stream;
+use futures::StreamExt;
+use futures::TryStreamExt;
 use manifest::derive_manifest_with_io_sender;
 use manifest::Entry;
 use manifest::LeafInfo;
@@ -29,6 +34,7 @@ use mononoke_types::BonsaiChangeset;
 use mononoke_types::ContentId;
 use mononoke_types::FileType;
 use mononoke_types::MPath;
+use mononoke_types::MPathElement;
 use skeleton_manifest::mapping::get_file_changes;
 
 use crate::mapping::RootBasenameSuffixSkeletonManifest;
@@ -41,6 +47,18 @@ fn get_fixed_up_changes(bcs: &BonsaiChangeset) -> Vec<(MPath, Option<(ContentId,
         .into_iter()
         .map(|(path, content)| (BsmPath::transform(path).into_raw(), content))
         .collect()
+}
+
+fn split_by_root_dir<X>(changes: Vec<(MPath, X)>) -> HashMap<MPathElement, Vec<(MPath, X)>> {
+    let mut map = HashMap::new();
+    for (p, x) in changes {
+        let (root_dir, rest) = p.split_first();
+        let rest = rest.expect("We always add a sentinel suffix to the path");
+        map.entry(root_dir.clone())
+            .or_insert_with(Vec::new)
+            .push((rest, x));
+    }
+    map
 }
 
 async fn empty_mf(
@@ -124,12 +142,48 @@ pub(crate) async fn derive_single(
     bonsai: BonsaiChangeset,
     parents: Vec<RootBasenameSuffixSkeletonManifest>,
 ) -> Result<RootBasenameSuffixSkeletonManifest> {
+    const CONCURRENCY: usize = 100;
     let parents = parents.into_iter().map(|root| root.0).collect::<Vec<_>>();
     let changes = get_fixed_up_changes(&bonsai);
     let blobstore = derivation_ctx.blobstore();
-    // TODO(yancouto): Optimise by doing the first query separately using the optimisations
-    // in sharded map, which are unused in common manifest code recently
-    let root = inner_derive(ctx, blobstore, parents, changes).await?;
+    // TODO(T123518092): Once T123518092 is done, this optimisation can be removed and
+    // we can call `inner_derive` as in the `else` clause.
+    let root = if parents.len() <= 1 {
+        let (parent, previous_rollup) = match parents.into_iter().next() {
+            Some(p) => (Some(p.load(ctx, blobstore).await?), p.rollup_count),
+            None => (None, 1),
+        };
+        let entries_to_update = stream::iter(split_by_root_dir(changes))
+            .map(|(root_dir, changes)| async {
+                let new_parent = match parent.as_ref() {
+                    None => vec![],
+                    Some(p) => match p.lookup(ctx, blobstore, &root_dir).await? {
+                        Some(BssmEntry::Directory(dir)) => vec![dir],
+                        None => vec![],
+                        other => anyhow::bail!("Invalid directory {:?}", other),
+                    },
+                };
+                Ok((
+                    root_dir,
+                    inner_derive(ctx, blobstore, new_parent, changes)
+                        .await?
+                        .map(BssmEntry::Directory),
+                ))
+            })
+            .buffer_unordered(CONCURRENCY)
+            .try_collect()
+            .await?;
+        let (mf, rollup_diff) = parent
+            .unwrap_or_else(BasenameSuffixSkeletonManifest::empty)
+            .update(ctx, blobstore, entries_to_update)
+            .await?;
+        Some(BssmDirectory {
+            id: mf.into_blob().store(ctx, blobstore).await?,
+            rollup_count: ((previous_rollup as i64) + rollup_diff) as u64,
+        })
+    } else {
+        inner_derive(ctx, blobstore, parents, changes).await?
+    };
     Ok(RootBasenameSuffixSkeletonManifest(match root {
         Some(root) => root,
         // Only happens on empty repo
