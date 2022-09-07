de1b0ee706c87792c191d4adedaa559d402961d6
Gustavo Andrade Do Vale <gustavovale@fb.com>
2022-09-07 04:37:44 -0700

    Create land_service hello world binary
    Reviewed By: mitrandir77
    
    Differential Revision: D39283967
    
    fbshipit-source-id: 8ef1b242fba1644ce48c5731be46d33cfddd4fa4

---
+++ eden/mononoke/land_service/src/main.rs
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+fn main() -> anyhow::Result<()> {
+    println!("Hello, world!");
+    Ok(())
+}
