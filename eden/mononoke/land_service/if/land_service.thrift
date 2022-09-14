ca423c5d48d044e933b57823029117dab1403283
Gustavo Andrade Do Vale <gustavovale@fb.com>
2022-09-14 05:57:21 -0700

    Modify thirft file and lib of local server
    Summary: It creates some structs that are expected to handle the land service
    
    Reviewed By: yancouto
    
    Differential Revision: D39463922
    
    fbshipit-source-id: ea5670768d12919cf8fe7e02a2087cad07e001cf

--- eden/mononoke/land_service/if/land_service.thrift
+++ eden/mononoke/land_service/if/land_service.thrift
@@ -7,19 +7,107 @@
 
 include "fb303/thrift/fb303_core.thrift"
 
-struct FooRequest {
-  1: string bar;
+typedef binary ChangesetId
+/// The UTF-8 path of the file or directory.
+typedef string Path
+
+enum CrossRepoPushSource {
+  NATIVE_TO_THIS_REPO = 0,
+  PUSH_REDIRECTED = 1,
 }
 
-struct FooResult {
-  1: bool is_valid;
+enum BookmarkKindRestrictions {
+  ANY_KIND = 0,
+  ONLY_SCRATCH = 1,
+  ONLY_PUBLISHING = 2,
 }
 
-exception FooExn {
+struct LandChangesetRequest {
+  /// The name of the bookmark to land to.
+  1: string bookmark;
+
+  /// The set of changesets to be landed.
+  /// MUST BE A STACK
+  2: set<ChangesetId> changesets;
+
+  /// The pushvars to use when landing the stack.
+  3: optional map<string, binary> pushvars;
+
+  /// Override push source. Leave as the default.
+  4: CrossRepoPushSource cross_repo_push_source = CrossRepoPushSource.NATIVE_TO_THIS_REPO;
+
+  /// What kind of bookmark can be pushed.
+  5: BookmarkKindRestrictions bookmark_restrictions = BookmarkKindRestrictions.ANY_KIND;
+} (rust.exhaustive)
+
+struct BonsaiHashPairs {
+  /// The old bonsai hash.
+  1: ChangesetId old_id;
+
+  /// The new bonsai hash.
+  2: ChangesetId new_id;
+} (rust.exhaustive)
+
+struct PushrebaseOutcome {
+  /// The new changeset for the rebased head.
+  1: ChangesetId head;
+
+  /// A list of bonsai hash (changeset) pairs represeting old and new ids.
+  2: list<BonsaiHashPairs> rebased_commits;
+
+  /// How far away was the commit rebased.
+  3: i64 pushrebase_distance;
+
+  /// How many retries it took to do the rebase successfully, due to race conditions.
+  4: i64 retry_num;
+
+  /// The old id where the bookmark was before the pushrebase operation.
+  5: optional ChangesetId old_bookmark_value;
+} (rust.exhaustive)
+
+struct LandChangesetsResponse {
+  1: PushrebaseOutcome pushrebase_outcome;
+} (rust.exhaustive)
+
+struct PushrebaseConflicts {
+  1: Path left;
+  2: Path right;
+} (rust.exhaustive)
+
+safe permanent client exception PushrebaseConflictsException {
   1: string reason;
-} (message = 'reason')
+  /// Always non-empty
+  2: list<PushrebaseConflicts> conflicts;
+} (message = "reason")
+
+struct HookRejection {
+  /// The hook that rejected the output
+  1: string hook_name;
+  /// The changeset that was reject, in bonsai format.
+  2: ChangesetId cs_id;
+  /// Why the hook rejected the changeset.
+  3: HookOutcomeRejected reason;
+}
+
+struct HookOutcomeRejected {
+  /// A short description for summarizing this failure with similar failures
+  1: string description;
+  /// A full explanation of what went wrong, suitable for presenting to the user (should include guidance for fixing this failure, where possible)
+  2: string long_description;
+} (rust.exhaustive)
+
+safe stateful client exception HookRejectionsException {
+  1: string reason;
+  /// Always non-empty
+  2: list<HookRejection> rejections;
+} (message = "reason")
 
 service LandService extends fb303_core.BaseService {
-  # TODO: Add thrift functions here, and then implement them in the land_service_impl.rs file!
-  FooResult validateFoo(1: FooRequest inputs) throws (1: FooExn ex);
+  /// Land a stack of commits via land_changesets.
+  LandChangesetsResponse land_changesets(
+    1: LandChangesetRequest land_changesets,
+  ) throws (
+    2: PushrebaseConflictsException pushrebase_conflicts,
+    3: HookRejectionsException hook_rejections,
+  );
 }
--- eden/mononoke/land_service/src/land_service_impl.rs
+++ eden/mononoke/land_service/src/land_service_impl.rs
@@ -8,7 +8,7 @@
 use async_trait::async_trait;
 use fbinit::FacebookInit;
 use land_service_if::server::LandService;
-use land_service_if::services::land_service::ValidateFooExn;
+use land_service_if::services::land_service::*;
 use land_service_if::types::*;
 use slog::Logger;
 
@@ -33,10 +33,23 @@ impl LandServiceImpl {
 
 #[async_trait]
 impl LandService for LandServiceThriftImpl {
-    async fn validateFoo(&self, input: FooRequest) -> Result<FooResult, ValidateFooExn> {
-        Ok(FooResult {
-            is_valid: input.bar.is_empty(),
-            ..Default::default()
-        })
+    async fn land_changesets(
+        &self,
+        _land_changesets: LandChangesetRequest,
+    ) -> Result<LandChangesetsResponse, LandChangesetsExn> {
+        let head = Vec::new();
+        let rebased_commits = Vec::new();
+        let pushrebase_distance = 20;
+        let retry_num = 5;
+        let old_bookmark_value = Some(Vec::new());
+
+        let pushrebase_outcome = PushrebaseOutcome {
+            head,
+            rebased_commits,
+            pushrebase_distance,
+            retry_num,
+            old_bookmark_value,
+        };
+        Ok(LandChangesetsResponse { pushrebase_outcome })
     }
 }
