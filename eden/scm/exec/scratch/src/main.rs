4e09897232108494aea4505056880d3ad2cfb7c8
Michael Cuevas <cuev@fb.com>
2022-09-14 20:50:39 -0700

    turn mkscratch into a library
    Reviewed By: fanzeyi
    
    Differential Revision: D39190185
    
    fbshipit-source-id: 4f24f7ee1f9b617a68d8de45a1cb1993e2aa670f

--- eden/scm/exec/scratch/src/main.rs
+++ eden/scm/exec/scratch/src/main.rs
@@ -32,6 +32,7 @@ use clap::App;
 use clap::AppSettings;
 use clap::Arg;
 use clap::SubCommand;
+use mkscratch::zzencode;
 use serde::Deserialize;
 
 /// Configuration for scratch space style. This decides whether the directory
@@ -422,7 +423,7 @@ fn scratch_root(config: &Config, path: &Path) -> Result<PathBuf> {
             .replace("$HOME", &home),
     );
 
-    root.push(encode(
+    root.push(zzencode(
         path.to_str()
             .ok_or(format_err!("{:?} cannot be converted to utf8", path))?,
     ));
@@ -522,7 +523,7 @@ fn path_command(
                 bail!("subdir path contains parent component: {:?}", subdir);
             }
         } else {
-            result.push(encode(subdir));
+            result.push(zzencode(subdir));
         }
     }
 
@@ -543,116 +544,4 @@ fn path_command(
 
     println!("{}", result.display());
     Ok(())
-}
-
-/// Normalizes various path format on Windows. This function will convert
-/// various Windows path format to full path form. Note this function does not
-/// canonicalize the given path. So it does not collapse dots nor expand
-/// relative paths.
-/// Ref: https://googleprojectzero.blogspot.com/2016/02/the-definitive-guide-on-win32-to-nt.html
-#[cfg(windows)]
-fn normalize_windows_path(path: &str) -> String {
-    let path = path.replace("/", r"\");
-    if let Some(path) = path.strip_prefix(r"\??\UNC\") {
-        // NT UNC path
-        format!(r"\\{}", path)
-    } else if let Some(path) = path.strip_prefix(r"\??\") {
-        // NT path
-        path.to_owned()
-    } else if let Some(path) = path.strip_prefix(r"\\?\UNC\") {
-        // Extend-length UNC path
-        format!(r"\\{}", path)
-    } else if let Some(path) = path.strip_prefix(r"\\?\") {
-        // Extend-length path
-        path.to_owned()
-    } else {
-        path
-    }
-}
-
-/// Given a string representation of a path, encode it such that all
-/// file/path special characters are replaced with non-special characters.
-/// This has the effect of flattening a relative path fragment like
-/// `foo/bar` into a single level path component like `fooZbar`.
-/// Scratch uses this to give the appearance of hierarchy to clients
-/// without having an actual hierarchy.  This is important on systems
-/// such as Windows and macOS where the filesystem watchers are always
-/// recursive.
-/// The mapping is not and does not need to be reversible.
-/// Why not just compute a SHA or MD5 hash?  It is nicer for the user
-/// to have an idea of what the path is when they list the scratch container
-/// path, which is something they'll likely end up doing when their disk
-/// gets full, and they'll appreciate knowing which of these dirs have
-/// value to them.
-fn encode(path: &str) -> String {
-    let mut result = String::with_capacity(path.len());
-
-    // `std::fs::canonicalize` on Windows will normalize path into
-    // extended-length format, which has a prefix `\\?\`. This function will
-    // incorrect generate a path with the question mark which is an invalid
-    // path.
-    #[cfg(windows)]
-    let path = &normalize_windows_path(path);
-
-    for (i, b) in path.chars().enumerate() {
-        if cfg!(unix) && i == 0 && b == '/' {
-            // On unix, most paths begin with a slash, which
-            // means that we'd use a Z prefix everything.
-            // Let's just skip the first character.
-            continue;
-        }
-        match b {
-            '/' | '\\' => result.push('Z'),
-            'Z' => result.push_str("_Z"),
-            ':' => result.push_str("_"),
-            _ => result.push(b),
-        }
-    }
-
-    result
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_encode() {
-        if cfg!(unix) {
-            assert_eq!(encode("/foo/bar"), "fooZbar");
-        } else {
-            assert_eq!(encode("/foo/bar"), "ZfooZbar");
-        }
-        assert_eq!(encode("foo"), "foo");
-        assert_eq!(encode("foo/bar"), "fooZbar");
-        assert_eq!(encode(r"foo\bar"), "fooZbar");
-        assert_eq!(encode("fooZbar"), "foo_Zbar");
-        assert_eq!(encode("foo_Zbar"), "foo__Zbar");
-        assert_eq!(encode(r"C:\foo\bar"), "C_ZfooZbar");
-        assert_eq!(encode(r"\\unc\path"), "ZZuncZpath");
-
-        if cfg!(windows) {
-            assert_eq!(encode(r"\\?\C:\foo\bar"), "C_ZfooZbar");
-            assert_eq!(encode(r"\\?\UNC\unc\path"), "ZZuncZpath");
-            assert_eq!(encode(r"\??\C:\foo\bar"), "C_ZfooZbar");
-            assert_eq!(encode(r"\??\UNC\unc\path"), "ZZuncZpath");
-        }
-    }
-
-    #[cfg(windows)]
-    #[test]
-    fn test_normalize_windows_path() {
-        assert_eq!(normalize_windows_path(r"c:\foo\bar"), r"c:\foo\bar");
-        assert_eq!(normalize_windows_path(r"c:/foo/bar"), r"c:\foo\bar");
-        assert_eq!(normalize_windows_path(r"\??\c:\foo\bar"), r"c:\foo\bar");
-        assert_eq!(normalize_windows_path(r"\\?\c:\foo\bar"), r"c:\foo\bar");
-        assert_eq!(
-            normalize_windows_path(r"\??\UNC\server\foo\bar"),
-            r"\\server\foo\bar"
-        );
-        assert_eq!(
-            normalize_windows_path(r"\\?\UNC\server\foo\bar"),
-            r"\\server\foo\bar"
-        );
-    }
 }
