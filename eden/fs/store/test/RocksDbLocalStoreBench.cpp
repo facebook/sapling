d806784cb300bb9ed50011e0206e6361f329e106
Chad Austin <chadaustin@meta.com>
2022-09-23 17:45:28 -0700

    add a benchmark for RocksDbLocalStore::getBlobMetadata
    Summary:
    I was curious how long it takes to fetch metadata from LocalStore. So
    add a benchmark.
    
    Reviewed By: xavierd
    
    Differential Revision: D39590670
    
    fbshipit-source-id: a95ba55adb911103a676100fec8dba9954d16307

---
+++ eden/fs/store/test/RocksDbLocalStoreBench.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "eden/common/utils/benchharness/Bench.h"
+#include "eden/fs/store/RocksDbLocalStore.h"
+#include "eden/fs/telemetry/NullStructuredLogger.h"
+#include "eden/fs/testharness/TempFile.h"
+#include "eden/fs/utils/FaultInjector.h"
+
+namespace {
+using namespace facebook::eden;
+
+void getBlobMetadata(benchmark::State& st) {
+  auto tempDir = makeTempDir();
+  FaultInjector faultInjector{false};
+  auto store = std::make_unique<RocksDbLocalStore>(
+      AbsolutePathPiece{tempDir.path().string()},
+      std::make_shared<NullStructuredLogger>(),
+      &faultInjector);
+  store->open();
+
+  const size_t N = 1'000'000;
+
+  std::vector<ObjectId> ids;
+  ids.reserve(N);
+  for (size_t i = 0; i < N; ++i) {
+    ids.push_back(ObjectId{fmt::format("{:08x}", i)});
+  }
+
+  std::vector<BlobMetadata> metadata;
+  metadata.reserve(N);
+  for (size_t i = 0; i < N; ++i) {
+    metadata.push_back(BlobMetadata{Hash20{}, i});
+  }
+
+  for (size_t i = 0; i < N; ++i) {
+    store->putBlobMetadata(ids[i], metadata[i]);
+  }
+
+  // Reopen the database to exercise the read-from-disk path.
+  store.reset();
+  store = std::make_unique<RocksDbLocalStore>(
+      AbsolutePathPiece{tempDir.path().string()},
+      std::make_shared<NullStructuredLogger>(),
+      &faultInjector);
+  store->open();
+
+  size_t i = 0;
+  for (auto _ : st) {
+    benchmark::DoNotOptimize(store->getBlobMetadata(ids[i]).get());
+    if (++i == N) {
+      i = 0;
+    }
+  }
+}
+BENCHMARK(getBlobMetadata);
+
+} // namespace
+
+EDEN_BENCHMARK_MAIN();
