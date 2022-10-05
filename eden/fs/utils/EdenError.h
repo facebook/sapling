2ae7ffa43f30d94066e06ab7f634fe3c8f8225fd
Chad Austin <chadaustin@meta.com>
2022-10-04 21:42:44 -0700

    migrate EdenError to fmt
    Summary:
    EdenError is another header file that pulls in folly/Conv.h. It also
    uses folly::to's concatenation capabilities. Migrate it to fmt just
    like Throw.h.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D39992358
    
    fbshipit-source-id: 09d1d3446f664ec55b91e5b37ee9299ef253e991

--- eden/fs/utils/EdenError.h
+++ eden/fs/utils/EdenError.h
@@ -7,9 +7,9 @@
 
 #pragma once
 
-#include <folly/Conv.h>
+#include <fmt/ranges.h>
 #include <folly/ExceptionWrapper.h>
-#include <folly/Range.h>
+#include <string_view>
 #include <system_error>
 #include "eden/fs/service/gen-cpp2/eden_types.h"
 #include "eden/fs/utils/Utf8.h"
@@ -22,16 +22,16 @@ namespace facebook::eden {
 /**
  * Construct an EdenError from an error code, error type, and message.
  *
- * The message arguments will be joined with folly::to<std::string>().
+ * The message arguments will be concatenated, each formatted with fmt.
  */
 template <class Arg1, class... Args>
 EdenError newEdenError(
     int errorCode,
     EdenErrorType errorType,
-    Arg1&& msg,
-    Args&&... args) {
-  auto e = EdenError{ensureValidUtf8(folly::to<std::string>(
-      std::forward<Arg1>(msg), std::forward<Args>(args)...))};
+    const Arg1& msg,
+    const Args&... args) {
+  auto e = EdenError{ensureValidUtf8(fmt::to_string(fmt::join(
+      std::make_tuple<const Arg1&, const Args&...>(msg, args...), "")))};
   e.errorCode_ref() = errorCode;
   e.errorType_ref() = errorType;
   return e;
@@ -41,7 +41,7 @@ EdenError newEdenError(
  * Construct an EdenError with an error message and error type but no error
  * code.
  *
- * The message arguments will be joined with folly::to<std::string>().
+ * The message arguments will be concatenated, each formatted with fmt.
  *
  * The first message argument must be a string, primarily to help distinguish
  * this version of newEdenError() from the one above that takes an error code as
@@ -49,10 +49,13 @@ EdenError newEdenError(
  * numeric type other than `int` as the first argument.)
  */
 template <class... Args>
-EdenError
-newEdenError(EdenErrorType errorType, folly::StringPiece msg, Args&&... args) {
-  auto e = EdenError{ensureValidUtf8(
-      folly::to<std::string>(msg, std::forward<Args>(args)...))};
+EdenError newEdenError(
+    EdenErrorType errorType,
+    std::string_view msg,
+    const Args&... args) {
+  auto e = EdenError{ensureValidUtf8(fmt::to_string(fmt::join(
+      std::make_tuple<const std::string_view&, const Args&...>(msg, args...),
+      "")))};
   e.errorType_ref() = errorType;
   return e;
 }
