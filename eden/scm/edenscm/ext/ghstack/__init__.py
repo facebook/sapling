90a2f1c17d4e652a753e94f990c49a6d3c6a581d
Michael Bolin <mbolin@meta.com>
2022-10-07 20:42:25 -0700

    wire up ghstack subcommand in Sapling
    Summary:
    This wires up the `sl ghstack` subcommand by mirroring the logic
    in ghstack's own `__main__.py`:
    
    https://www.internalfb.com/code/fbsource/[528d083ee35a]/xplat/scm/ghstack-0.6.0/ghstack/__main__.py
    
    Differential Revision: D40161187
    
    fbshipit-source-id: 47b24c146fca6e8f0c9c3c1532b6180d29e70d68

--- eden/scm/edenscm/ext/ghstack/__init__.py
+++ eden/scm/edenscm/ext/ghstack/__init__.py
@@ -6,19 +6,202 @@
 """ghstack for Sapling (EXPERIMENTAL)
 """
 
-from edenscm import registrar
+from edenscm import error, registrar
 from edenscm.i18n import _
 
 cmdtable = {}
 command = registrar.command(cmdtable)
 
 import ghstack
+import ghstack.action
+import ghstack.checkout
+import ghstack.config
+import ghstack.eden_shell
+import ghstack.github_real
+import ghstack.land
+import ghstack.submit
+import ghstack.unlink
 
 
 @command(
     "ghstack",
     [],
-    _("SUBCOMMAND ..."),
+    _("{submit,unlink,land,checkout,action} (default: submit)"),
 )
-def ghstack_command(ui, repo, *pats, **opts) -> None:
-    print(ghstack.__version__)
+def ghstack_command(ui, repo, *args, **opts) -> None:
+    """submits a stack of commits to GitHub as individual pull requests using ghstack
+
+    Uses the scheme employed by ghstack (https://github.com/ezyang/ghstack) to
+    submit a stack of commits to GitHub as individual pull requests. Pull
+    requests managed by ghstack are never force-pushed.
+
+    Currently, you must configure ghstack by creating a ~/.ghstackrc file as
+    explained on https://github.com/ezyang/ghstack. Ultimately, we will likely
+    replace this use of the GitHub CLI to manage API requests to GitHub.
+
+    Note that you must have *write* access to the GitHub repository in order to
+    use ghstack. If you do not have write access, consider using the `pr`
+    subcommand instead.
+    """
+    return submit_cmd(ui, repo, *args, **opts)
+
+
+subcmd = ghstack_command.subcommand(
+    categories=[
+        (
+            "Create or update pull requests",
+            ["submit", "unlink", "land", "checkout", "action"],
+        ),
+    ]
+)
+
+
+@subcmd(
+    "submit",
+    [
+        ("m", "message", "Update", _("message describing changes to updated commits")),
+        (
+            "u",
+            "update-fields",
+            False,
+            _("update GitHub pull request summary from the local commit"),
+        ),
+        ("", "short", False, _("print only the URL of the latest opened PR to stdout")),
+        (
+            "",
+            "force",
+            False,
+            _("force push the branch even if your local branch is stale"),
+        ),
+        (
+            "",
+            "skip",
+            False,
+            _(
+                "never skip pushing commits, even if the contents didn't change"
+                " (use this if you've only updated the commit message)."
+            ),
+        ),
+        (
+            "",
+            "draft",
+            False,
+            _(
+                "create the pull request in draft mode "
+                "(only if it has not already been created)"
+            ),
+        ),
+    ],
+)
+def submit_cmd(ui, repo, *args, **opts) -> None:
+    """submit stack of commits to GitHub"""
+    conf, sh, github = _create_ghstack_context()
+    ghstack.submit.main(
+        msg=opts.get("message"),
+        username=conf.github_username,
+        sh=sh,
+        github=github,
+        update_fields=opts.get("update-fields"),
+        short=opts.get("short"),
+        force=opts.get("force"),
+        no_skip=not opts.get("skip"),
+        draft=opts.get("draft"),
+        github_url=conf.github_url,
+        remote_name=conf.remote_name,
+    )
+
+
+@subcmd(
+    "unlink",
+    [],
+    _("COMMITS..."),
+)
+def unlink_cmd(ui, repo, *args, **opts) -> None:
+    """remove the association of a commit with a pull request"""
+    conf, sh, github = _create_ghstack_context()
+    commits = list(args)
+    ghstack.unlink.main(
+        commits=commits,
+        github=github,
+        sh=sh,
+        github_url=conf.github_url,
+        remote_name=conf.remote_name,
+    )
+
+
+@subcmd(
+    "land",
+    [],
+    _("PR"),
+)
+def land_cmd(ui, repo, *args, **opts) -> None:
+    """lands the stack for the specified pull request URL"""
+    conf, sh, github = _create_ghstack_context()
+    if len(args) != 1:
+        raise error.Abort(_("must specify a URL for a pull request"))
+
+    pull_request = args[0]
+    ghstack.land.main(
+        pull_request=pull_request,
+        github=github,
+        sh=sh,
+        github_url=conf.github_url,
+        remote_name=conf.remote_name,
+    )
+
+
+@subcmd(
+    "checkout",
+    [],
+    _("PR"),
+)
+def checkout_cmd(ui, repo, *args, **opts) -> None:
+    """goto the stack for the specified pull request URL"""
+    conf, sh, github = _create_ghstack_context()
+    if len(args) != 1:
+        raise error.Abort(_("must specify a URL for a pull request"))
+
+    pull_request = args[0]
+    ghstack.checkout.main(
+        pull_request=pull_request,
+        github=github,
+        sh=sh,
+        remote_name=conf.remote_name,
+    )
+
+
+@subcmd(
+    "action",
+    [
+        (
+            "",
+            "close",
+            False,
+            _("close the specified pull request"),
+        ),
+    ],
+    _("PR"),
+)
+def action_cmd(ui, repo, *args, **opts) -> None:
+    """goto the stack for the specified pull request URL"""
+    conf, sh, github = _create_ghstack_context()
+    if len(args) != 1:
+        raise error.Abort(_("must specify a URL for a pull request"))
+
+    pull_request = args[0]
+    ghstack.action.main(
+        pull_request=pull_request,
+        github=github,
+        close=opts.get("close"),
+    )
+
+
+def _create_ghstack_context():
+    conf = ghstack.config.read_config()
+    sh = ghstack.eden_shell.EdenShell(conf=conf)
+    github = ghstack.github_real.RealGitHubEndpoint(
+        oauth_token=conf.github_oauth,
+        proxy=conf.proxy,
+        github_url=conf.github_url,
+    )
+    return conf, sh, github
