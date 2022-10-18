d3afec56009435b3f57e1f15fbceafa0cda2e3e1
Chad Austin <chadaustin@meta.com>
2022-10-17 19:03:32 -0700

    add a rough syscall benchmark using getxattr
    Summary:
    I'm about to refactor FuseRequestContext. First, add a rough benchmark
    that measures the cost of an uncached syscall.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D40313184
    
    fbshipit-source-id: 61869dc47b98fd5b8c869bbce0ee662d6c19e059

---
+++ eden/fs/benchmarks/getxattr.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include <folly/Exception.h>
+#include <folly/File.h>
+#include <folly/portability/GFlags.h>
+#include <sys/xattr.h>
+#include "eden/common/utils/benchharness/Bench.h"
+
+namespace {
+
+DEFINE_string(
+    filename,
+    "getxattr.tmp",
+    "Path which should be opened and repeatedly getxattr'd");
+
+/*
+ * Benchmark getxattr in particular because it's not cached. This makes it a
+ * good proxy for syscall overhead.
+ */
+void call_getxattr(benchmark::State& state) {
+  folly::File file{FLAGS_filename, O_CREAT | O_EXCL | O_RDONLY | O_CLOEXEC};
+
+  char buf[1000];
+  for (auto _ : state) {
+    // We don't check for errors because EdenFS does not support arbitrary
+    // xattrs.
+#ifdef __APPLE__
+    (void)::fgetxattr(file.fd(), "user.benchmark", buf, std::size(buf), 0, 0);
+#else
+    (void)::fgetxattr(file.fd(), "user.benchmark", buf, std::size(buf));
+#endif
+  }
+
+  folly::checkUnixError(::unlink(FLAGS_filename.c_str()));
+}
+BENCHMARK(call_getxattr);
+
+} // namespace
+
+EDEN_BENCHMARK_MAIN();
