170d90304668be49fd686a96ed23a0c87b5d300f
Jun Wu <quark@meta.com>
2022-10-24 13:51:10 -0700

    cpython-ext: drop usage of rustc flags
    Summary:
    Drop the usage of `Py_38` rustc flag so one does not have to set it manually to
    compile cpython-ext.
    
    The flags were used since Python 3.9 changed the last field `ob_exports` from
    `int` to `Py_ssize_t`, potentially a breaking change. By using the
    `PyByteArray_FromStringAndSize` API, cpython will allocate and initialize the
    `ob_exports` field so we don't have to deal with the `ob_exports` field manually.
    
    Reviewed By: DurhamG
    
    Differential Revision: D40649342
    
    fbshipit-source-id: 2b4e441e0c5209e02b0560445a2712f4c0eaa8c7

--- eden/scm/lib/cpython-ext/src/bytearrayobject.rs
+++ eden/scm/lib/cpython-ext/src/bytearrayobject.rs
@@ -6,87 +6,101 @@
  */
 
 use std::mem;
-use std::os::raw::c_int;
 
 use cpython::PyObject as RustPyObject;
 use cpython::Python as RustPythonGILGuard;
 use ffi::PyByteArray_Size;
-use ffi::PyByteArray_Type;
 use ffi::PyObject;
-use ffi::PyTypeObject;
 #[cfg(feature = "python3")]
 use ffi::PyVarObject;
 use ffi::Py_ssize_t;
-use ffi::_PyObject_New;
 #[cfg(feature = "python3")]
 use python3_sys as ffi;
 
+// Ideally this struct comes from bindgen to ensure it's up-to-date
+// with what the current cpython has. However that significantly
+// complicates the build step.
 #[cfg(feature = "python3")]
-#[cfg(Py_38)]
 #[repr(C)]
 struct PyByteArrayObject {
     pub ob_base: PyVarObject,
     pub ob_alloc: Py_ssize_t,
     pub ob_bytes: *mut u8,
     pub ob_start: *mut u8,
-    pub ob_exports: c_int,
-}
-
-#[cfg(feature = "python3")]
-#[cfg(any(Py_39, Py_310))]
-#[repr(C)]
-struct PyByteArrayObject {
-    pub ob_base: PyVarObject,
-    pub ob_alloc: Py_ssize_t,
-    pub ob_bytes: *mut u8,
-    pub ob_start: *mut u8,
-    pub ob_exports: Py_ssize_t,
+    // These fields exist in cpython but we don't care about them.
+    // pub ob_exports: c_int | Py_ssize_t,
 }
 
 /// Consume a `Vec<u8>`. Create a Python `bytearray` object.
 /// Bytes stored are not copied.
 pub fn vec_to_pyobj(py: RustPythonGILGuard<'_>, mut value: Vec<u8>) -> RustPyObject {
-    unsafe {
-        let ptr: *mut PyObject = _PyObject_New(&mut PyByteArray_Type as *mut PyTypeObject);
-        let typed: *mut PyByteArrayObject = mem::transmute(ptr);
-        (*typed).ob_exports = 0;
-        (*typed).ob_alloc = value.capacity() as Py_ssize_t;
-        (*typed).ob_bytes = value.as_mut_ptr();
-        #[cfg(feature = "python3")]
-        {
-            (*typed).ob_base.ob_size = value.len() as Py_ssize_t;
-            (*typed).ob_start = (*typed).ob_bytes;
-        }
-        assert_eq!(
-            PyByteArray_Size(ptr) as usize,
-            value.len(),
-            "PyByteArray struct mismatch"
-        );
-        mem::forget(value);
-        RustPyObject::from_owned_ptr(py, ptr)
-    }
+    let capacity = value.capacity();
+    let obj = unsafe { pybytearray_from_slice(py, value.as_mut(), Some(capacity)) };
+    // now cpython is responsible to free `value`.
+    mem::forget(value);
+    obj
 }
 
 /// Consume a `Box<[u8]>`. Create a Python `bytearray` object.
 /// Bytes stored are not copied.
 pub fn boxed_slice_to_pyobj(py: RustPythonGILGuard<'_>, mut value: Box<[u8]>) -> RustPyObject {
-    unsafe {
-        let ptr: *mut PyObject = _PyObject_New(&mut PyByteArray_Type as *mut PyTypeObject);
-        let typed: *mut PyByteArrayObject = mem::transmute(ptr);
-        (*typed).ob_exports = 0;
-        (*typed).ob_alloc = value.len() as Py_ssize_t;
-        (*typed).ob_bytes = value.as_mut_ptr();
-        #[cfg(feature = "python3")]
-        {
-            (*typed).ob_base.ob_size = value.len() as Py_ssize_t;
-            (*typed).ob_start = value.as_mut_ptr();
-        }
-        assert_eq!(
-            PyByteArray_Size(ptr) as usize,
-            value.len(),
-            "PyByteArray struct mismatch"
-        );
-        mem::forget(value);
-        RustPyObject::from_owned_ptr(py, ptr)
+    let obj = unsafe { pybytearray_from_slice(py, value.as_mut(), None) };
+    // now cpython is responsible to free `value`.
+    mem::forget(value);
+    obj
+}
+
+/// Similar to `PyByteArray_FromStringAndSize(value)` without copying `value`.
+///
+/// CPython will free `value` when releasing the python object. When `value` is
+/// a prefix of a larger buffer, set `alloc_size` so CPython can free the buffer
+/// properly.
+///
+/// To avoid double-free from both CPython and Rust, the callsite must
+/// `mem::forget` the owner of `value` after this function.
+unsafe fn pybytearray_from_slice(
+    py: RustPythonGILGuard,
+    value: &mut [u8],
+    alloc_size: Option<usize>,
+) -> RustPyObject {
+    let ptr: *mut PyObject = ffi::PyByteArray_FromStringAndSize(std::ptr::null(), 0);
+    let typed: *mut PyByteArrayObject = mem::transmute(ptr);
+
+    // We want to rewrite ob_alloc and ob_bytes directly. Ensure that
+    // they are not allocated by cpython.
+    assert_eq!(
+        (*typed).ob_alloc,
+        0,
+        "PyByteArray_FromStringAndSize(size=0) should not alloc"
+    );
+    assert!(
+        (*typed).ob_bytes.is_null(),
+        "PyByteArray_FromStringAndSize(size=0) should not alloc"
+    );
+
+    // There are no public cpython APIs to update these fields.
+    // So we update them directly based on our understanding of
+    // the PyByteArrayObject struct.
+    (*typed).ob_alloc = alloc_size.unwrap_or_else(|| value.len()) as Py_ssize_t;
+    (*typed).ob_bytes = value.as_mut_ptr();
+    #[cfg(feature = "python3")]
+    {
+        (*typed).ob_base.ob_size = value.len() as Py_ssize_t;
+        (*typed).ob_start = (*typed).ob_bytes;
     }
+
+    // Sanity check: cpython's understanding of the bytes matches ours.
+    let ptr: *mut PyObject = typed as *mut PyObject;
+    assert_eq!(
+        PyByteArray_Size(ptr) as usize,
+        value.len(),
+        "PyByteArray struct mismatch (ob_size)"
+    );
+    assert_eq!(
+        ffi::PyByteArray_AsString(ptr) as *const u8,
+        value.as_ptr(),
+        "PyByteArray struct mismatch (ob_bytes)"
+    );
+
+    RustPyObject::from_owned_ptr(py, ptr)
 }
