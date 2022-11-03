1c52857ccbce8065e9b5f7a3e49bc2f526380e07
Xavier Deguillard <xavierd@meta.com>
2022-11-03 11:37:39 -0700

    oss: remove Windows specific try/catch
    Summary:
    When this code was written, EdenFS had no ability to handle unhandled
    exceptions and display them. This is no longer true and this try/cache is in
    fact detrimental in debugging test failures. Thus remove it.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D40960334
    
    fbshipit-source-id: 2cca99b3ade072d98fb16058e388528a8c8e0622

--- eden/fs/service/oss/main.cpp
+++ eden/fs/service/oss/main.cpp
@@ -12,18 +12,5 @@
 using namespace facebook::eden;
 
 int main(int argc, char** argv) {
-  if (!folly::kIsWindows) {
-    return runEdenMain(DefaultEdenMain{}, argc, argv);
-  } else {
-    // TODO(xavierd): on Windows, unhandled exceptions are just simply ignored,
-    // giving absolutely no clue to the user as to what is wrong. Let's catch
-    // them and display them properly until we have the same infrastructure as
-    // on Linux/macOS.
-    try {
-      return runEdenMain(DefaultEdenMain{}, argc, argv);
-    } catch (const std::exception& ex) {
-      XLOG(ERR) << folly::exceptionStr(ex);
-      return 1;
-    }
-  }
+  return runEdenMain(DefaultEdenMain{}, argc, argv);
 }
