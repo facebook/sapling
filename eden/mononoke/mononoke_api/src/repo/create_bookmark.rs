064046fa643666e8cc942b5eb58d96b414e30903
Yan Soares Couto <yancouto@meta.com>
2022-11-10 12:19:41 -0800

    Support pushredirection in repo_create_bookmark
    Summary:
    **Context**: We want to support pushredirection by default in all SCS methods. We need to implement it for the bookmark handling endpoints. I'm disallowing modifying common bookmarks from the small repo, the same as happens on wireproto [here](https://fburl.com/code/rtigbjxk).
    
    This diff implements pushredirection on the create_bookmark endpoint.
    
    What it does not do: add pushredirection on edenapi service
    
    Reviewed By: RajivTS
    
    Differential Revision: D41124940
    
    fbshipit-source-id: b6044eebca37ade74ae344447034ceb8daf4bbf6

--- eden/mononoke/mononoke_api/src/repo/create_bookmark.rs
+++ eden/mononoke/mononoke_api/src/repo/create_bookmark.rs
@@ -10,9 +10,12 @@ use std::sync::Arc;
 
 use bookmarks::BookmarkName;
 use bookmarks::BookmarkUpdateReason;
+use bookmarks_movement::CreateBookmarkOp;
 use bytes::Bytes;
+use hooks::HookManagerRef;
 use mononoke_types::ChangesetId;
 use reachabilityindex::LeastCommonAncestorsHint;
+use skiplist::SkiplistIndexArc;
 use tunables::tunables;
 
 use crate::errors::MononokeError;
@@ -31,28 +34,52 @@ impl RepoContext {
         let bookmark = bookmark.as_ref();
         let bookmark = BookmarkName::new(bookmark)?;
 
-        let lca_hint: Arc<dyn LeastCommonAncestorsHint> = self.skiplist_index_arc();
-
-        // Create the bookmark.
-        let mut op = bookmarks_movement::CreateBookmarkOp::new(
-            &bookmark,
-            target,
-            BookmarkUpdateReason::ApiRequest,
-        )
-        .with_pushvars(pushvars);
-
-        if !tunables().get_disable_commit_scribe_logging_scs() {
-            op = op.log_new_public_commits_to_scribe();
+        fn make_create_op<'a>(
+            bookmark: &'a BookmarkName,
+            target: ChangesetId,
+            pushvars: Option<&'a HashMap<String, Bytes>>,
+        ) -> CreateBookmarkOp<'a> {
+            let mut op = CreateBookmarkOp::new(bookmark, target, BookmarkUpdateReason::ApiRequest)
+                .with_pushvars(pushvars);
+            if !tunables().get_disable_commit_scribe_logging_scs() {
+                op = op.log_new_public_commits_to_scribe();
+            }
+            op
         }
-
-        op.run(
-            self.ctx(),
-            self.authorization_context(),
-            self.inner_repo(),
-            &lca_hint,
-            self.hook_manager().as_ref(),
-        )
-        .await?;
+        if let Some(redirector) = self.push_redirector.as_ref() {
+            let large_bookmark = redirector.small_to_large_bookmark(&bookmark).await?;
+            if large_bookmark == bookmark {
+                return Err(MononokeError::InvalidRequest(format!(
+                    "Cannot create shared bookmark '{}' from small repo",
+                    bookmark
+                )));
+            }
+            let ctx = self.ctx();
+            let target = redirector
+                .get_small_to_large_commit_equivalent(ctx, target)
+                .await?;
+            make_create_op(&large_bookmark, target, pushvars)
+                .run(
+                    self.ctx(),
+                    self.authorization_context(),
+                    redirector.repo.inner_repo(),
+                    &(redirector.repo.skiplist_index_arc() as Arc<dyn LeastCommonAncestorsHint>),
+                    redirector.repo.hook_manager(),
+                )
+                .await?;
+            // Wait for bookmark to catch up on small repo
+            redirector.backsync_latest(ctx).await?;
+        } else {
+            make_create_op(&bookmark, target, pushvars)
+                .run(
+                    self.ctx(),
+                    self.authorization_context(),
+                    self.inner_repo(),
+                    &(self.skiplist_index_arc() as Arc<dyn LeastCommonAncestorsHint>),
+                    self.hook_manager().as_ref(),
+                )
+                .await?;
+        }
 
         Ok(())
     }
