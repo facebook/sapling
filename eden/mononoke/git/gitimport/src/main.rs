4c09f71ee3d3e01eb2a3735a5299849992b66b86
Pierre Chevalier <pierrec@meta.com>
2022-11-11 04:02:52 -0800

    Migrate gitimport to new CLI framework
    Summary:
    * Migrate from `clap` to `clap3`
    * Migrate from `cmdlib` to `mononoke_app`
    
    Note: there are opportunities to improve the UX of this CLI tool further, for instance by replacing positional flags with named flags, but this change aims to be a pure refactoring.
    
    Reviewed By: RajivTS
    
    Differential Revision: D41182759
    
    fbshipit-source-id: b9b4134219196049deb64ae244ab224c808f011f

--- eden/mononoke/git/gitimport/src/main.rs
+++ eden/mononoke/git/gitimport/src/main.rs
@@ -23,11 +23,8 @@ use cacheblob::dummy::DummyLease;
 use cacheblob::LeaseOps;
 use cacheblob::MemWritesBlobstore;
 use changesets::ArcChangesets;
-use clap::Arg;
-use clap::SubCommand;
-use cmdlib::args;
-use cmdlib::args::RepoRequirement;
-use cmdlib::helpers::block_execute;
+use clap::Parser;
+use clap::Subcommand;
 use context::CoreContext;
 use fbinit::FacebookInit;
 use futures::future;
@@ -37,6 +34,11 @@ use import_tools::GitimportTarget;
 use linked_hash_map::LinkedHashMap;
 use mercurial_derived_data::get_manifest_from_bonsai;
 use mercurial_derived_data::DeriveHgChangeset;
+use mononoke_app::args::RepoArgs;
+use mononoke_app::fb303::AliveService;
+use mononoke_app::fb303::Fb303AppExtension;
+use mononoke_app::MononokeApp;
+use mononoke_app::MononokeAppBuilder;
 use mononoke_types::ChangesetId;
 use slog::info;
 
@@ -44,24 +46,6 @@ use crate::mem_writes_changesets::MemWritesChangesets;
 
 // Refactor this a bit. Use a thread pool for git operations. Pass that wherever we use store repo.
 // Transform the walk into a stream of commit + file changes.
-
-const SUBCOMMAND_FULL_REPO: &str = "full-repo";
-const SUBCOMMAND_GIT_RANGE: &str = "git-range";
-const SUBCOMMAND_MISSING_FOR_COMMIT: &str = "missing-for-commit";
-const SUBCOMMAND_IMPORT_TREE_AS_SINGLE_BONSAI_CHANGESET: &str =
-    "import-tree-as-single-bonsai-changeset";
-
-const ARG_GIT_REPOSITORY_PATH: &str = "git-repository-path";
-const ARG_DERIVE_HG: &str = "derive-hg";
-const ARG_SUPPRESS_REF_MAPPING: &str = "suppress-ref-mapping";
-const ARG_GIT_COMMAND_PATH: &str = "git-command-path";
-
-const ARG_GIT_FROM: &str = "git-from";
-const ARG_GIT_TO: &str = "git-to";
-
-const ARG_GIT_COMMIT: &str = "git-commit";
-
-const ARG_REUPLOAD_COMMITS: &str = "reupload-commits";
 
 async fn derive_hg(
     ctx: &CoreContext,
@@ -105,173 +89,136 @@ async fn derive_hg(
     Ok(())
 }
 
+/// Mononoke Git Importer
+#[derive(Parser)]
+struct GitimportArgs {
+    #[clap(long)]
+    derive_hg: bool,
+    /// This is used to suppress the printing of the potentially really long git Reference -> BonzaiID mapping.
+    #[clap(long)]
+    suppress_ref_mapping: bool,
+    /// Set the path to the git binary - preset to git.real
+    #[clap(long)]
+    git_command_path: Option<String>,
+    /// Path to a git repository to import
+    git_repository_path: String,
+    /// Reupload git commits, even if they already exist in Mononoke
+    #[clap(long)]
+    reupload_commits: bool,
+    #[clap(subcommand)]
+    subcommand: GitimportSubcommand,
+    #[clap(flatten)]
+    repo_args: RepoArgs,
+}
+
+#[derive(Subcommand)]
+enum GitimportSubcommand {
+    FullRepo,
+    GitRange { git_from: String, git_to: String },
+    MissingForCommit { git_commit: String },
+    ImportTreeAsSingleBonsaiChangeset { git_commit: String },
+}
+
 #[fbinit::main]
 fn main(fb: FacebookInit) -> Result<(), Error> {
-    let app = args::MononokeAppBuilder::new("Mononoke Git Importer")
-        .with_repo_required(RepoRequirement::ExactlyOne)
-        .with_fb303_args()
-        .build()
-        .arg(
-            Arg::with_name(ARG_DERIVE_HG)
-                .long(ARG_DERIVE_HG)
-                .required(false)
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name(ARG_SUPPRESS_REF_MAPPING)
-                .long(ARG_SUPPRESS_REF_MAPPING)
-                .help("This is used to suppress the printing of the potentially really long git Reference -> BonzaiID mapping.")
-                .required(false)
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name(ARG_GIT_COMMAND_PATH)
-                .long(ARG_GIT_COMMAND_PATH)
-                .help("Set the path to the git binary - preset to git.real")
-                .required(false)
-                .takes_value(true),
-        )
-        .arg(Arg::with_name(ARG_GIT_REPOSITORY_PATH).help("Path to a git repository to import"))
-        .arg(
-            Arg::with_name(ARG_REUPLOAD_COMMITS)
-                .long(ARG_REUPLOAD_COMMITS)
-                .help("Reupload git commits, even if they already exist in Mononoke")
-                .required(false)
-                .takes_value(false)
-        )
-        .subcommand(SubCommand::with_name(SUBCOMMAND_FULL_REPO))
-        .subcommand(
-            SubCommand::with_name(SUBCOMMAND_GIT_RANGE)
-                .arg(
-                    Arg::with_name(ARG_GIT_FROM)
-                        .required(true)
-                        .takes_value(true),
-                )
-                .arg(Arg::with_name(ARG_GIT_TO).required(true).takes_value(true)),
-        )
-        .subcommand(
-            SubCommand::with_name(SUBCOMMAND_MISSING_FOR_COMMIT).arg(
-                Arg::with_name(ARG_GIT_COMMIT)
-                    .required(true)
-                    .takes_value(true),
-            ),
-        )
-        .subcommand(
-            SubCommand::with_name(SUBCOMMAND_IMPORT_TREE_AS_SINGLE_BONSAI_CHANGESET).arg(
-                Arg::with_name(ARG_GIT_COMMIT)
-                    .required(true)
-                    .takes_value(true),
-            ),
-        );
+    let app = MononokeAppBuilder::new(fb)
+        .with_app_extension(Fb303AppExtension {})
+        .build::<GitimportArgs>()?;
 
+    app.run_with_fb303_monitoring(async_main, "gitimport", AliveService)
+}
+
+async fn async_main(app: MononokeApp) -> Result<(), Error> {
+    let logger = app.logger();
+    let ctx = CoreContext::new_with_logger(app.fb, logger.clone());
+    let args: GitimportArgs = app.args()?;
     let mut prefs = GitimportPreferences::default();
-
-    let matches = app.get_matches(fb)?;
 
     // if we are readonly, then we'll set up some overrides to still be able to do meaningful
     // things below.
-    let dry_run = matches.readonly_storage().0;
+    let dry_run = app.readonly_storage().0;
     prefs.dry_run = dry_run;
 
-    let derive_hg_data = matches.is_present(ARG_DERIVE_HG);
-
-    if let Some(path) = matches.value_of(ARG_GIT_COMMAND_PATH) {
+    if let Some(path) = args.git_command_path {
         prefs.git_command_path = PathBuf::from(path);
     }
 
-    let path = Path::new(matches.value_of(ARG_GIT_REPOSITORY_PATH).unwrap());
+    let path = Path::new(&args.git_repository_path);
 
-    let logger = matches.logger();
-    let ctx = CoreContext::new_with_logger(fb, logger.clone());
-
-    let reupload = if matches.is_present(ARG_REUPLOAD_COMMITS) {
+    let reupload = if args.reupload_commits {
         import_direct::ReuploadCommits::Always
     } else {
         import_direct::ReuploadCommits::Never
     };
 
-    let repo = args::not_shardmanager_compatible::create_repo(fb, logger, &matches);
-    block_execute(
-        async {
-            let repo: BlobRepo = repo.await?;
-
-            let repo = if dry_run {
-                repo.dangerous_override(|blobstore| -> Arc<dyn Blobstore> {
-                    Arc::new(MemWritesBlobstore::new(blobstore))
-                })
-                .dangerous_override(|changesets| -> ArcChangesets {
-                    Arc::new(MemWritesChangesets::new(changesets))
-                })
-                .dangerous_override(|bonsai_hg_mapping| -> ArcBonsaiHgMapping {
-                    Arc::new(MemWritesBonsaiHgMapping::new(bonsai_hg_mapping))
-                })
-                .dangerous_override(|_| Arc::new(DummyLease {}) as Arc<dyn LeaseOps>)
-            } else {
-                repo
-            };
-
-            let uploader = import_direct::DirectUploader::new(repo.clone(), reupload);
-
-            let target = match matches.subcommand() {
-                (SUBCOMMAND_FULL_REPO, Some(..)) => GitimportTarget::full(),
-                (SUBCOMMAND_GIT_RANGE, Some(range_matches)) => {
-                    let from = range_matches.value_of(ARG_GIT_FROM).unwrap().parse()?;
-                    let to = range_matches.value_of(ARG_GIT_TO).unwrap().parse()?;
-                    import_direct::range(from, to, &ctx, &repo).await?
-                }
-                (SUBCOMMAND_MISSING_FOR_COMMIT, Some(matches)) => {
-                    let commit = matches.value_of(ARG_GIT_COMMIT).unwrap().parse()?;
-                    import_direct::missing_for_commit(
-                        commit,
-                        &ctx,
-                        &repo,
-                        &prefs.git_command_path,
-                        path,
-                    )
-                    .await?
-                }
-                (SUBCOMMAND_IMPORT_TREE_AS_SINGLE_BONSAI_CHANGESET, Some(matches)) => {
-                    let commit = matches.value_of(ARG_GIT_COMMIT).unwrap().parse()?;
-                    let bcs_id = import_tree_as_single_bonsai_changeset(
-                        &ctx, path, uploader, commit, &prefs,
-                    )
-                    .await?;
-                    info!(ctx.logger(), "imported as {}", bcs_id);
-                    if derive_hg_data {
-                        derive_hg(&ctx, &repo, [(&commit, &bcs_id)].into_iter()).await?;
-                    }
-                    return Ok(());
-                }
-                _ => {
-                    return Err(Error::msg("A valid subcommand is required"));
-                }
-            };
-
-            let gitimport_result: LinkedHashMap<_, _> =
-                import_tools::gitimport(&ctx, path, uploader, &target, &prefs).await?;
-            if derive_hg_data {
-                derive_hg(&ctx, &repo, gitimport_result.iter()).await?;
-            }
-
-            if !matches.is_present(ARG_SUPPRESS_REF_MAPPING) {
-                let refs = import_tools::read_git_refs(path, &prefs).await?;
-                for (name, commit) in refs {
-                    let bcs_id = gitimport_result.get(&commit);
-                    info!(
-                        ctx.logger(),
-                        "Ref: {:?}: {:?}",
-                        String::from_utf8_lossy(&name),
-                        bcs_id
-                    );
-                }
-            }
-
-            Ok(())
-        },
-        fb,
-        "gitimport",
+    let repo: BlobRepo = app.open_repo(&args.repo_args).await?;
+    info!(
         logger,
-        &matches,
-        cmdlib::monitoring::AliveService,
-    )
+        "using repo \"{}\" repoid {:?}",
+        repo.name(),
+        repo.get_repoid(),
+    );
+
+    let repo = if dry_run {
+        repo.dangerous_override(|blobstore| -> Arc<dyn Blobstore> {
+            Arc::new(MemWritesBlobstore::new(blobstore))
+        })
+        .dangerous_override(|changesets| -> ArcChangesets {
+            Arc::new(MemWritesChangesets::new(changesets))
+        })
+        .dangerous_override(|bonsai_hg_mapping| -> ArcBonsaiHgMapping {
+            Arc::new(MemWritesBonsaiHgMapping::new(bonsai_hg_mapping))
+        })
+        .dangerous_override(|_| Arc::new(DummyLease {}) as Arc<dyn LeaseOps>)
+    } else {
+        repo
+    };
+
+    let uploader = import_direct::DirectUploader::new(repo.clone(), reupload);
+
+    let target = match args.subcommand {
+        GitimportSubcommand::FullRepo {} => GitimportTarget::full(),
+        GitimportSubcommand::GitRange { git_from, git_to } => {
+            let from = git_from.parse()?;
+            let to = git_to.parse()?;
+            import_direct::range(from, to, &ctx, &repo).await?
+        }
+        GitimportSubcommand::MissingForCommit { git_commit } => {
+            let commit = git_commit.parse()?;
+            import_direct::missing_for_commit(commit, &ctx, &repo, &prefs.git_command_path, path)
+                .await?
+        }
+        GitimportSubcommand::ImportTreeAsSingleBonsaiChangeset { git_commit } => {
+            let commit = git_commit.parse()?;
+            let bcs_id =
+                import_tree_as_single_bonsai_changeset(&ctx, path, uploader, commit, &prefs)
+                    .await?;
+            info!(ctx.logger(), "imported as {}", bcs_id);
+            if args.derive_hg {
+                derive_hg(&ctx, &repo, [(&commit, &bcs_id)].into_iter()).await?;
+            }
+            return Ok(());
+        }
+    };
+
+    let gitimport_result: LinkedHashMap<_, _> =
+        import_tools::gitimport(&ctx, path, uploader, &target, &prefs).await?;
+    if args.derive_hg {
+        derive_hg(&ctx, &repo, gitimport_result.iter()).await?;
+    }
+
+    if !args.suppress_ref_mapping {
+        let refs = import_tools::read_git_refs(path, &prefs).await?;
+        for (name, commit) in refs {
+            let bcs_id = gitimport_result.get(&commit);
+            info!(
+                ctx.logger(),
+                "Ref: {:?}: {:?}",
+                String::from_utf8_lossy(&name),
+                bcs_id
+            );
+        }
+    }
+
+    Ok(())
 }
