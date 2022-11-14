00dc8b2c23521b3f646194eb604fc3658cf293aa
Aaron Kushner <akushner@meta.com>
2022-11-14 12:23:25 -0800

    push: update the docstring
    Summary:
    Fix the formatting and make the docs match the product. Lots
    of old stuff that should be replaced.
    
    Reviewed By: bolinfest
    
    Differential Revision: D41257244
    
    fbshipit-source-id: ed43fd53e5f02ee2d769a35e7c24c23ed6402d46

--- eden/scm/edenscm/commands/__init__.py
+++ eden/scm/edenscm/commands/__init__.py
@@ -4817,59 +4817,30 @@ def _newpull(ui, repo, source, **opts):
 def push(ui, repo, dest=None, **opts):
     """push changes to the specified destination
 
-    Push changesets from the local repository to the specified
+    Push commits from the local repository to the specified
     destination.
 
-    This operation is symmetrical to pull: it is identical to a pull
-    in the destination repository from the current one.
-
-    By default, push will not allow creation of new heads at the
-    destination, since multiple heads would make it unclear which head
+    By default, push does not allow creation of new heads at the
+    destination since multiple heads make it unclear which head
     to use. In this situation, it is recommended to pull and merge
     before pushing.
 
     .. note::
 
-       Extra care should be taken with the -f/--force option,
+       Extra care should be taken with the ``-f/--force`` option,
        which will push all new heads on all branches, an action which will
        almost always cause confusion for collaborators.
 
-    If -r/--rev is used, the specified revision and all its ancestors
+    If ``-r/--rev`` is used, the specified revision and all its ancestors
     will be pushed to the remote repository.
 
-    If -B/--bookmark is used, the specified bookmarked revision, its
+    If ``-B/--bookmark`` is used, the specified bookmarked revision, its
     ancestors, and the bookmark will be pushed to the remote
     repository. Specifying ``.`` is equivalent to specifying the active
     bookmark's name.
 
     Please see :prog:`help urls` for important details about ``ssh://``
     URLs. If DESTINATION is omitted, a default path will be used.
-
-    .. container:: verbose
-
-        The --pushvars option sends strings to the server that become
-        environment variables prepended with ``HG_USERVAR_``. For example,
-        ``--pushvars ENABLE_FEATURE=true``, provides the server side hooks with
-        ``HG_USERVAR_ENABLE_FEATURE=true`` as part of their environment.
-
-        pushvars can provide for user-overridable hooks as well as set debug
-        levels. One example is having a hook that blocks commits containing
-        conflict markers, but enables the user to override the hook if the file
-        is using conflict markers for testing purposes or the file format has
-        strings that look like conflict markers.
-
-        By default, servers will ignore `--pushvars`. To enable it add the
-        following to your configuration file::
-
-            [push]
-            pushvars.server = true
-
-        If ``push.requirereason`` is set to true, users will need to pass
-        ``--pushvars PUSH_REASON="..."`` in order to push, and their reason will
-        be logged via ``ui.log(...)``.
-
-        ``push.requirereasonmsg`` can be used to set the message shown to users
-        when they don't provide a reason.
 
     Returns 0 if push was successful, 1 if nothing to push.
     """
