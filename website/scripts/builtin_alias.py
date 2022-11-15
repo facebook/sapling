f1bce925a3a21debf51f62e4673f76dc073e67c7
Zhaolong Zhu <zhaolong@meta.com>
2022-11-15 09:16:09 -0800

    website: add doc for 'restack'
    Summary: as title
    
    Reviewed By: DurhamG
    
    Differential Revision: D41306278
    
    fbshipit-source-id: 356f1599da133a32c2baf372ae91633dd63c1e8d

--- website/scripts/builtin_alias.py
+++ website/scripts/builtin_alias.py
@@ -5,6 +5,21 @@
 # LICENSE file in the root directory of this source tree.
 
 DEFAULT_ALIAS_DICT = {
+    "restack": {
+        "name": "restack",
+        "aliases": ["restack"],
+        "doc": """
+rebase all commits in the current stack onto the latest version of their respective parents
+
+`restack` is a built-in alias for `rebase --restack`
+
+When commits are modified by commands like `amend` and `absorb`, their descendant
+commits may be left behind as orphans. Rebase these orphaned commits onto the newest
+versions of their ancestors, making the stack linear again.
+""",
+        "args": [],
+        "subcommands": None,
+    },
     "ssl": {
         "name": "ssl",
         "aliases": ["ssl"],
