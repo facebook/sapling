9495a2733b597ee495f8a7eae5e7d59075a6625d
Jun Wu <quark@meta.com>
2022-11-29 16:29:00 -0800

    pyfs: expose Rust's std::fs::canonicalize
    Summary:
    We want a path normalization function that can normalize cases on
    case-insensitive file systems. It seems Rust's `std::fs::canonicalize` (backed
    by `GetFinalPathNameByHandle` on Windows, and `realpath` on posix) can do that.
    
    Namely, it can normalize `/users` to `\\?\C:\Users` on Windows, and `/users` to
    `/Users` on macOS.
    
    Note the Python `os.path.realpath` can normalize case on Windows
    (`ntpath.realpath` calls `GetFinalPathNameByHandle`), but not on macOS
    (`posixpath.realpath` does not call native libc `realpath`) so it does not
    satisfy our needs.
    
    Reviewed By: DurhamG
    
    Differential Revision: D41571812
    
    fbshipit-source-id: 3de3059bc857b738d489843dc562c89279f3ab45

--- eden/scm/edenscmnative/bindings/modules/pyfs/src/lib.rs
+++ eden/scm/edenscmnative/bindings/modules/pyfs/src/lib.rs
@@ -13,8 +13,20 @@ use cpython_ext::Str;
 pub fn init_module(py: Python, package: &str) -> PyResult<PyModule> {
     let name = [package, "fs"].join(".");
     let m = PyModule::new(py, &name)?;
+    m.add(py, "canonicalize", py_fn!(py, canonicalize(path: &str)))?;
     m.add(py, "fstype", py_fn!(py, fstype(path: &PyPath)))?;
     Ok(m)
+}
+
+fn canonicalize(py: Python, path: &str) -> PyResult<String> {
+    let canonical_path = std::fs::canonicalize(path).map_pyerr(py)?;
+    match canonical_path.to_str() {
+        Some(s) => Ok(s.to_string()),
+        None => {
+            let message = format!("canonicalized({}) is non-utf-8", path);
+            Err(PyErr::new::<exc::ValueError, _>(py, message))
+        }
+    }
 }
 
 fn fstype(py: Python, path: &PyPath) -> PyResult<Str> {
