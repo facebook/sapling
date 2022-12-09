70a57bf40f00159e12a84878885facd4a2818605
Yan Soares Couto <yancouto@meta.com>
2022-12-09 11:22:09 -0800

    Add configurable rendezvous controller and use it on tunables one
    Summary:
    This is a refactoring only change. It adds ConfigurableRendezVousController, which allows customising the controller using any provided functions. We implement `TunablesRendezVousController` by using this new, more generic controller.
    
    The new controller isn't used without tunables on this diff, but this refactor is good because I plan to start some controllers with some more hardcoded values, not reusing the same tunables.
    
    Reviewed By: markbt
    
    Differential Revision: D41875741
    
    fbshipit-source-id: fb4b7bb0e31b802c8bb044840c21a17bc23ab377

--- eden/mononoke/common/rendezvous/src/tunables.rs
+++ eden/mononoke/common/rendezvous/src/tunables.rs
@@ -41,59 +41,99 @@ impl MultiRendezVousController for TunablesMultiRendezVousController {
 /// in flight at any point in time. Batching does not kick in until these are exhausted. This cannot be
 /// changed after the RendezVousController is initialized, so it is not controlled by a tunable.
 ///
-/// Tunables control what we do once the free connections are exhausted. there are two relevant
-/// tunables here. Both of those control when batches will depart:
+/// Provided functions control what we do once the free connections are exhausted. The functions are:
+/// - max_threshold: number of keys after which we'll dispatch a full-size batch.
+/// - max_delay: controls how long we wait before dispatching a small batch.
 ///
-/// - rendezvous_dispatch_max_threshold: number of keys after which we'll dispatch a full-size batch.
-/// - rendezvous_dispatch_delay_ms: controls how long we wait before dispatching a small batch.
-
+/// The provided functions should run very fast, please make sure they are hard-coded or cached.
 ///
 /// Note that if a batch departs when either of those criteria are met, it will not count against
 /// the count of free connections: free connections are just connections not subject to batching,
 /// but once batching kicks in there is no limit to how many batches can be in flight concurrently
 /// (though unless we receive infinite requests the concurrency will tend to approach the free
 /// connection count).
-pub struct TunablesRendezVousController {
+
+///
+pub struct ConfigurableRendezVousController {
     semaphore: Arc<Semaphore>,
+    max_delay: fn() -> Duration,
+    max_threshold: fn() -> usize,
 }
 
-impl TunablesRendezVousController {
-    pub fn new(opts: RendezVousOptions) -> Self {
+impl ConfigurableRendezVousController {
+    pub fn new(
+        opts: RendezVousOptions,
+        max_delay: fn() -> Duration,
+        max_threshold: fn() -> usize,
+    ) -> Self {
         Self {
             semaphore: Arc::new(Semaphore::new(opts.free_connections)),
+            max_delay,
+            max_threshold,
         }
-    }
-
-    pub fn max_dispatch_delay(&self) -> Duration {
-        Duration::from_millis(
-            ::tunables::tunables()
-                .get_rendezvous_dispatch_delay_ms()
-                .try_into()
-                .unwrap_or(0),
-        )
     }
 }
 
 #[async_trait::async_trait]
-impl RendezVousController for TunablesRendezVousController {
+impl RendezVousController for ConfigurableRendezVousController {
     // NOTE: We don't actually care about AcquireError here, since that can only happen when the
     // Semaphore is closed, but we don't close it.
     type RendezVousToken = Option<Result<OwnedSemaphorePermit, AcquireError>>;
 
     /// Wait for the configured dispatch delay.
     async fn wait_for_dispatch(&self) -> Self::RendezVousToken {
-        tokio::time::timeout(
-            self.max_dispatch_delay(),
-            self.semaphore.clone().acquire_owned(),
-        )
-        .await
-        .ok()
+        tokio::time::timeout((self.max_delay)(), self.semaphore.clone().acquire_owned())
+            .await
+            .ok()
     }
 
     fn early_dispatch_threshold(&self) -> usize {
+        (self.max_threshold)()
+    }
+}
+
+fn max_delay() -> Duration {
+    Duration::from_millis(
         ::tunables::tunables()
-            .get_rendezvous_dispatch_max_threshold()
+            .get_rendezvous_dispatch_delay_ms()
             .try_into()
-            .unwrap_or(0)
+            .unwrap_or(0),
+    )
+}
+
+fn max_threshold() -> usize {
+    ::tunables::tunables()
+        .get_rendezvous_dispatch_max_threshold()
+        .try_into()
+        .unwrap_or(0)
+}
+
+/// This uses `ConfigurableRendezVousController`, but uses tunables to fetch the limits,
+/// for ease of use and being able to quickly change them. The used tunables are:
+/// - rendezvous_dispatch_max_threshold: number of keys after which we'll dispatch a full-size batch.
+/// - rendezvous_dispatch_delay_ms: controls how long we wait before dispatching a small batch.
+pub struct TunablesRendezVousController {
+    inner: ConfigurableRendezVousController,
+}
+
+impl TunablesRendezVousController {
+    pub fn new(opts: RendezVousOptions) -> Self {
+        Self {
+            inner: ConfigurableRendezVousController::new(opts, max_delay, max_threshold),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl RendezVousController for TunablesRendezVousController {
+    type RendezVousToken = Option<Result<OwnedSemaphorePermit, AcquireError>>;
+
+    /// Wait for the configured dispatch delay.
+    async fn wait_for_dispatch(&self) -> Self::RendezVousToken {
+        self.inner.wait_for_dispatch().await
+    }
+
+    fn early_dispatch_threshold(&self) -> usize {
+        self.inner.early_dispatch_threshold()
     }
 }
