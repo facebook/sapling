e17e3e3285d8400250a845487fdd6dfe4f4b4dd1
Egor Tkachenko <egor@meta.com>
2022-12-09 16:36:54 -0800

    Find underived batches
    Summary: This diff adds functionality to find underived commits in batches and enqueue it into Derivation DAG deduplicating batches along the way.
    
    Differential Revision: D41615371
    
    fbshipit-source-id: 2fb34a5a357283f63ea33d5d5c532b0d0fc8e64c

--- eden/mononoke/derived_data/manager/manager.rs
+++ eden/mononoke/derived_data/manager/manager.rs
@@ -193,6 +193,10 @@ impl DerivedDataManager {
         self.inner.changesets.as_ref()
     }
 
+    pub fn changesets_arc(&self) -> Arc<dyn Changesets> {
+        self.inner.changesets.clone()
+    }
+
     pub fn repo_blobstore(&self) -> &RepoBlobstore {
         &self.inner.repo_blobstore
     }
