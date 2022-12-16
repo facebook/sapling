78ac4befd2f4814e97c086f1fbbce60876aad40d
Jun Wu <quark@meta.com>
2022-12-16 14:04:17 -0800

    website: improve sapling output rendering
    Summary:
    - Use production config to pick up color and template definitions.
    - Ensure `sl` command (shell function) is available.
    - Specify a better-than-default colors.
    
    Reviewed By: muirdm
    
    Differential Revision: D42102950
    
    fbshipit-source-id: 7ceef74f515ebb836c8b59af698cfa9794e046b4

--- website/src/plugins/sapling-output/src/index.ts
+++ website/src/plugins/sapling-output/src/index.ts
@@ -15,18 +15,29 @@ const tmp = require('tmp-promise');
 
 // Header of `.t` to setup environment.
 // Force color output by setting ui.color.
-const EXAMPLE_HEADER = `
+// Set TEST_PROD_CONFIGS to load fancy templates and stuff.
+const EXAMPLE_HEADER = String.raw`
 # hide begin
   $ cat >> "$HGRCPATH" << 'EOF'
   > [ui]
   > color=always
+  > [init]
+  > prefer-git=false
+  > [templatealias]
+  > sl_difflink="{if(github_pull_request_number,'#{github_pull_request_number}')}"
+  > github_pull_request_number="{get(extras,'pr_number')}"
+  > github_pr_state="{get(extras,'pr_state')}"
+  > github_pull_request_status_check_rollup="{get(extras,'pr_status')}"
   > EOF
+  $ export TEST_PROD_CONFIGS=1
+  $ export HGCOLORS=16
+  $ export SL_COLORS=16
 # hide end
 `
 
 // Compatibility for internal systems where sl is not installed but hg can be
 // used as an alternative.
-const HG_COMPAT_HEADER = `
+const HG_COMPAT_HEADER = String.raw`
 # hide begin
   $ sl() {
   >   HGIDENTITY=sl hg "$@"
@@ -58,9 +69,8 @@ function processInput(text: string): string {
     }
     if (firstLine) {
       let header = EXAMPLE_HEADER;
-      if (!fs.existsSync('/usr/bin/sl')) {
-        header += HG_COMPAT_HEADER;
-      }
+      // Currently debugruntest only knows about "hg".
+      header += HG_COMPAT_HEADER;
       newLine = header + newLine;
       firstLine = false;
     }
@@ -95,6 +105,26 @@ function processOutput(output: string): string {
   });
 };
 
+const COLORS = {
+  // Campbell
+  0: "#0C0C0C",
+  1: "#C50F1F",
+  2: "#13A10E",
+  3: "#C19C00",
+  4: "#0037DA",
+  5: "#881798",
+  6: "#3A96DD",
+  7: "#CCCCCC",
+  8: "#767676",
+  9: "#E74856",
+  10: "#16C60C",
+  11: "#F9F1A5",
+  12: "#3B78FF",
+  13: "#B4009E",
+  14: "#61D6D6",
+  15: "#F2F2F2",
+}
+
 // Render `example` (in .t test format without "  " prefix) into HTML.
 async function renderExample(example: string): Promise<string> {
   // Prepare input.
@@ -119,7 +149,7 @@ async function renderExample(example: string): Promise<string> {
   // Convert to HTML.
   const rawOutput = await fs.promises.readFile(examplePath, {"encoding": "utf8"});
   const output = processOutput(rawOutput);
-  const convert = new Convert();
+  const convert = new Convert({colors: COLORS});
   const body = convert.toHtml(output);
   const html = `<pre class="sapling-example">${body}</pre>`;
 
