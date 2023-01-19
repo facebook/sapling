15a18d5b716321a355fa719bc9785ece6e191c43
Mateusz Kwapich <mitrandir@meta.com>
2023-01-19 06:02:05 -0800

    pretty-print hashes in `async-requests show`
    Summary:
    Printing raw thrift params has one big drawback - commit hashes are unreadable.
    This diff is adding function that prints the requests in a nicer way.
    
    Differential Revision: D42501626
    
    fbshipit-source-id: a20d39ab7bf431b8242fec3fe8001ce6ef532fdd

--- eden/mononoke/tools/admin/src/commands/async_requests/show.rs
+++ eden/mononoke/tools/admin/src/commands/async_requests/show.rs
@@ -8,10 +8,18 @@
 use anyhow::anyhow;
 use anyhow::Error;
 use anyhow::Result;
+use async_requests::types::IntoConfigFormat;
+use async_requests::types::MegarepoAsynchronousRequestParams;
+use async_requests::types::MegarepoAsynchronousRequestResult;
 use async_requests::types::RowId;
+use async_requests::types::ThriftMegarepoAsynchronousRequestParams;
+use async_requests::types::ThriftMegarepoAsynchronousRequestResult;
+use async_requests::types::ThriftMegarepoSyncChangesetResult;
 use clap::Args;
 use context::CoreContext;
 use megarepo_api::MegarepoApi;
+use mononoke_api::Mononoke;
+use mononoke_types::ChangesetId;
 
 #[derive(Args)]
 /// Subcommand responsible for showing the request
@@ -20,6 +28,85 @@ pub struct AsyncRequestsShowArgs {
     /// ID of the request.
     #[clap(long)]
     request_id: u64,
+}
+
+struct ParamsWrapper<'a>(&'a Mononoke, MegarepoAsynchronousRequestParams);
+
+impl<'a> std::fmt::Debug for ParamsWrapper<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // impl Debug for HexArray here
+        match self.1.thrift() {
+            ThriftMegarepoAsynchronousRequestParams::megarepo_add_target_params(params) => f
+                .debug_struct("MegarepoAddTargetParams")
+                .field(
+                    "config_with_new_target",
+                    &params
+                        .config_with_new_target
+                        .clone()
+                        .into_config_format(self.0),
+                )
+                .field(
+                    "changesets_to_merge",
+                    &params
+                        .changesets_to_merge
+                        .iter()
+                        .map(|(k, v)| (k.clone(), ChangesetId::from_bytes(v)))
+                        .collect::<Vec<_>>(),
+                )
+                .field("message", &params.message)
+                .finish()?,
+            ThriftMegarepoAsynchronousRequestParams::megarepo_change_target_params(params) => f
+                .debug_struct("MegarepoAddTargetParams")
+                .field("target", &params.target)
+                .field("new_version", &params.new_version)
+                .field(
+                    "target_location",
+                    &ChangesetId::from_bytes(&params.target_location),
+                )
+                .field(
+                    "changesets_to_merge",
+                    &params
+                        .changesets_to_merge
+                        .iter()
+                        .map(|(k, v)| (k.clone(), ChangesetId::from_bytes(v)))
+                        .collect::<Vec<_>>(),
+                )
+                .field("message", &params.message)
+                .finish()?,
+            ThriftMegarepoAsynchronousRequestParams::megarepo_sync_changeset_params(params) => f
+                .debug_struct("MegarepoSyncChangesetParams")
+                .field("source_name", &params.source_name)
+                .field("target", &params.target)
+                .field("cs_id", &ChangesetId::from_bytes(&params.cs_id))
+                .field(
+                    "target_location",
+                    &ChangesetId::from_bytes(&params.target_location),
+                )
+                .finish()?,
+            other => f.write_str(format!("{:?}", other).as_str())?,
+        }
+        Ok(())
+    }
+}
+
+struct ResultsWrapper<'a>(&'a Mononoke, Option<MegarepoAsynchronousRequestResult>);
+impl<'a> std::fmt::Debug for ResultsWrapper<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // impl Debug for HexArray here
+        match &self.1 {
+            Some(res) => match res.thrift() {
+                ThriftMegarepoAsynchronousRequestResult::megarepo_sync_changeset_result(
+                    ThriftMegarepoSyncChangesetResult::success(result),
+                ) => f
+                    .debug_struct("MegarepoSyncChangesetResponse")
+                    .field("cs_id", &ChangesetId::from_bytes(&result.cs_id))
+                    .finish()?,
+                other => f.write_str(format!("{:?}", other).as_str())?,
+            },
+            None => (),
+        }
+        Ok(())
+    }
 }
 
 pub async fn show_request(
@@ -36,8 +123,10 @@ pub async fn show_request(
             queue.get_request_by_id(&ctx, &RowId(row_id)).await?
         {
             println!(
-                "Entry: {:?}\nParams: {:?}\nResult: {:?}",
-                entry, params, maybe_result
+                "Entry: {:#?}\nParams: {:#?}\nResult: {:#?}",
+                entry,
+                ParamsWrapper(&megarepo.mononoke(), params),
+                ResultsWrapper(&megarepo.mononoke(), maybe_result),
             );
             return Ok(());
         }
