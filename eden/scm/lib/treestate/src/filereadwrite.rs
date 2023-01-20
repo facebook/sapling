8633a5ed41dd4b5f3fab3da927087ca2edd47f09
Jun Wu <quark@meta.com>
2023-01-19 21:38:51 -0800

    treestate: add locking methods to trait FileReadWrite
    Summary:
    trait FileReadWrite is used for both on-disk and in-memory files. Add locking
    so the next change can use it to avoid races on the physical filesystem.
    
    Reviewed By: zzl0
    
    Differential Revision: D42492668
    
    fbshipit-source-id: 06c02ea4fb52213c3a64fc17a336a5edba385dc4

--- eden/scm/lib/treestate/src/filereadwrite.rs
+++ eden/scm/lib/treestate/src/filereadwrite.rs
@@ -6,6 +6,7 @@
  */
 
 use std::fs::File;
+use std::io;
 use std::io::BufWriter;
 use std::io::Cursor;
 use std::io::Read;
@@ -13,19 +14,47 @@ use std::io::Seek;
 use std::io::SeekFrom;
 use std::io::Write;
 
+use fs2::FileExt;
+
 pub trait FileSync {
-    fn sync_all(&mut self) -> Result<(), std::io::Error>;
+    fn sync_all(&mut self) -> io::Result<()>;
 }
 
-pub trait FileReadWrite: std::io::Read + std::io::Write + std::io::Seek + FileSync + Send {}
+pub trait FileLock {
+    /// Lock exclusively (across threads and processes).
+    /// Block if the lock was held by others.
+    ///
+    /// If the lock is held by `self`, then the function will just increase an
+    /// internal lock counter and return immediately.
+    ///
+    /// Use `unlock` or `drop` to unlock.
+    fn lock_exclusive(&mut self) -> io::Result<()>;
+
+    /// Cancel one `lock_exclusive` invocation.
+    ///
+    /// If `lock_exclusive` is called `N` times, it requires `unlock` to be
+    /// called `N` times to unlock.
+    ///
+    /// If `self` gets dropped, then the lock is automatically released.
+    fn unlock(&mut self) -> io::Result<()>;
+
+    /// Test if `lock_exclusive` is called more times than `unlock`.
+    fn is_locked(&self) -> bool;
+}
+
+pub trait FileReadWrite:
+    std::io::Read + std::io::Write + std::io::Seek + FileSync + FileLock + Send
+{
+}
 
 pub struct FileReaderWriter {
     writer: BufWriter<File>,
+    locked: usize,
 }
 
 impl FileReaderWriter {
     pub fn new(writer: BufWriter<File>) -> Self {
-        Self { writer }
+        Self { writer, locked: 0 }
     }
 }
 
@@ -57,6 +86,30 @@ impl FileSync for FileReaderWriter {
     }
 }
 
+impl FileLock for FileReaderWriter {
+    fn lock_exclusive(&mut self) -> io::Result<()> {
+        if self.locked == 0 {
+            self.writer.get_mut().lock_exclusive()?;
+        }
+        self.locked += 1;
+        Ok(())
+    }
+
+    fn unlock(&mut self) -> io::Result<()> {
+        if self.locked == 1 {
+            self.writer.get_mut().unlock()?;
+        }
+        if self.locked > 0 {
+            self.locked -= 1;
+        }
+        Ok(())
+    }
+
+    fn is_locked(&self) -> bool {
+        self.locked > 0
+    }
+}
+
 impl FileReadWrite for FileReaderWriter {}
 
 impl<T> FileSync for Cursor<T> {
@@ -70,4 +123,18 @@ where
     Cursor<T>: Write,
     T: std::convert::AsRef<[u8]> + Send,
 {
+}
+
+impl<T> FileLock for Cursor<T> {
+    fn lock_exclusive(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn unlock(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn is_locked(&self) -> bool {
+        true
+    }
 }
