899696ce258da295e83a2a32239507b26d515ce2
Evan Krause <evangrayk@meta.com>
2023-01-25 15:10:27 -0800

    Add Tracker for analytics
    Summary:
    This stack adds an API for analytics tracking in ISL.
    
    In open source builds, this will be completely disabled. It's only internally that we want to be able to track usage to understand how ISL is used.
    
    Reviewed By: bolinfest
    
    Differential Revision: D42466433
    
    fbshipit-source-id: cd5e08f37be541b62b29603581880771b5af9c20

---
+++ addons/isl-server/src/analytics/tracker.ts
@@ -0,0 +1,128 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {TrackErrorName, TrackEventName} from './eventNames';
+import type {TrackData, TrackDataWithEventName, TrackResult} from './types';
+
+import {randomId} from 'shared/utils';
+
+type SendData<T> = (data: TrackDataWithEventName, context: T) => void;
+
+/**
+ * Handles analytics tracking for both the server and client sides.
+ * Each instance provides a callback to define how to send data for logging.
+ *  - The client sends a message to the server to further process
+ *  - The server sends the data to finally get processed
+ */
+export class Tracker<T> {
+  constructor(private sendData: SendData<T>, public context: T) {}
+
+  /**
+   * Record an analytics error event `eventName`.
+   * Like `track`, but also fills out `errorName` and `errorMessage`.
+   */
+  error(
+    eventName: TrackEventName,
+    errorName: TrackErrorName,
+    error: Error | string | undefined,
+    data?: TrackData,
+  ): void {
+    const errorMessage = error instanceof Error ? error.message || String(error) : error;
+    return this.track(eventName, {...(data ?? {}), errorMessage, errorName});
+  }
+
+  /**
+   * Wrap a function with `track()`.
+   * If the function throws (or rejects for async), the error is tracked.
+   * The execution time is measured and included in the `duration` field.
+   */
+  operation<T>(
+    eventName: TrackEventName,
+    errorName: TrackErrorName,
+    data: TrackData | undefined,
+    operation: (parent: TrackResult) => T,
+  ): T {
+    const startTime = Date.now();
+    const id = data?.id ?? randomId();
+    try {
+      const result = operation({parentId: id});
+      if (isPromise(result)) {
+        return result
+          .then(finalResult => {
+            const endTime = Date.now();
+            const duration = endTime - startTime;
+            this.track(eventName, {...(data ?? {}), duration, id});
+            return finalResult;
+          })
+          .catch(err => {
+            const endTime = Date.now();
+            const duration = endTime - startTime;
+            this.error(eventName, errorName, err, {
+              ...(data ?? {}),
+              duration,
+              id,
+            });
+            return Promise.reject(err);
+          }) as unknown as T;
+      } else {
+        const endTime = Date.now();
+        const duration = endTime - startTime;
+        this.track(eventName, {...(data ?? {}), duration, id});
+        return result;
+      }
+    } catch (err) {
+      const endTime = Date.now();
+      const duration = endTime - startTime;
+      this.error(eventName, errorName, err as Error | string, {
+        ...(data ?? {}),
+        duration,
+        id,
+      });
+      throw err;
+    }
+  }
+
+  /**
+   * Track an event, then return a child `Tracker` that correlates future track calls via the `parentId` field.
+   * This way, you can recover the relationship of a tree of events.
+   */
+  public trackAsParent(eventName: TrackEventName, data?: TrackData): Tracker<{parentId: string}> {
+    const id = data?.id ?? randomId();
+    this.trackData({...data, eventName, id});
+    const childTracker = new Tracker((childData, ctx) => this.trackData({...childData, ...ctx}), {
+      parentId: id,
+    });
+    return childTracker;
+  }
+
+  /**
+   * Record an analytics event `eventName`.
+   * Optionally provide additional fields, like arbitrary JSON `extras`.
+   */
+  public track(eventName: TrackEventName, data?: TrackData): void {
+    return this.trackData({...data, eventName});
+  }
+
+  /**
+   * Record analytics event with filled in data struct.
+   * `track()` is an easier to use wrapper around this function.
+   */
+  public trackData(data: TrackDataWithEventName): void {
+    const id = data?.id ?? randomId();
+    const timestamp = data?.timestamp ?? Date.now();
+    const trackData: TrackDataWithEventName = {
+      timestamp,
+      id,
+      ...(data ?? {}),
+    };
+    this.sendData(trackData, this.context);
+  }
+}
+
+function isPromise<T>(o: unknown): o is Promise<T> {
+  return typeof (o as {then?: () => void})?.then === 'function';
+}
---
+++ addons/isl-server/src/analytics/types.ts
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {TrackErrorName, TrackEventName} from './eventNames';
+
+type JSONSerializable =
+  | {[key: string]: JSONSerializable}
+  | Array<JSONSerializable>
+  | string
+  | number
+  | boolean
+  | null
+  | undefined;
+
+export type TrackResult = {
+  parentId: string;
+};
+
+/**
+ * Data attached to each analytics event which is likely different for every event.
+ * This can be overwritten for each event, in the client or server.
+ */
+export type TrackData = {
+  eventName?: TrackEventName;
+  /** Timestamp in ms since unix epoch */
+  timestamp?: number;
+  /** duration of the event in ms */
+  duration?: number;
+  /** additional fields and custom data for this event */
+  extras?: JSONSerializable;
+  /** string enum describing what category of error this is */
+  errorName?: TrackErrorName;
+  /** thrown error message */
+  errorMessage?: string;
+  /** every event gets a unique id */
+  id?: string;
+  /** id field from another track event, for cross-referencing */
+  parentId?: string;
+};
+
+export type TrackDataWithEventName = TrackData & {eventName: TrackEventName};
+
+/**
+ * Data attached to each analytics event, which is common among all events for this instance of ISL.
+ * This is only known on the server side, where it's cached as part of the Tracker instance.
+ */
+export type ApplicationInfo = {
+  /** vscode, browser, etc */
+  platform: string;
+  /** platform-specific version string */
+  version: string;
+  /** unique identifier for this ISL session */
+  sessionId: string;
+  unixname: string;
+  /* Currently mounted repository identifier */
+  repo?: string | undefined;
+  /* e.g. 'darwin' or 'win32' or 'linux' */
+  osType: string;
+  /* e.g. 'x64' or 'arm64' */
+  osArch: string;
+  /* e.g. '21.6.0 */
+  osRelease: string;
+  hostname: string;
+};
+
+/**
+ * Combined data attached to analytics events.
+ */
+export type FullTrackData = ApplicationInfo & TrackData;
