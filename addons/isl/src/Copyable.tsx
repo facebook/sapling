cf9bbdb9b70976bccaad5b5cea7ae05bceabd993
Evan Krause <evangrayk@meta.com>
2023-01-25 12:24:35 -0800

    Add <Copyable> component
    Summary:
    Add a component that handles clicking to copy the text content. It shows a copy icon on hover. After clicking to copy, it will show a success tooltip for a moment then auto-dismiss.
    
    Such a component will be useful to have a consistent experience to click-to-copy text.
    
    I think it's nice when you can just click to copy. Of course, I don't want to get in the way of good ole' selecting text + cmd-c. So that should still work here.
    
    I added a small animation when you click to give some more tactile feedback.
    
    I tried adding a hover tooltip to say "click to copy", but it kind of got in the way of the confirmation tooltip. Might be nice to still add that later.
    
    Reviewed By: bolinfest
    
    Differential Revision: D42732039
    
    fbshipit-source-id: 2dbbb61bb72bde62878f8388c32720d86c1ccced

---
+++ addons/isl/src/Copyable.tsx
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {Icon} from './Icon';
+import {Tooltip} from './Tooltip';
+import {T} from './i18n';
+import platform from './platform';
+import {useEffect, useState} from 'react';
+
+import './Copyable.css';
+
+/** Click to copy text and show a confirmation tooltip */
+export function Copyable({children, className}: {children: string; className?: string}) {
+  const [showingSuccess, setShowingSuccess] = useState(false);
+  useEffect(() => {
+    if (showingSuccess) {
+      const timeout = setTimeout(() => setShowingSuccess(false), 1500);
+      return () => clearTimeout(timeout);
+    }
+  }, [showingSuccess, setShowingSuccess]);
+
+  return (
+    <Tooltip
+      trigger="manual"
+      shouldShow={showingSuccess}
+      component={CopiedSuccessTooltipContent(children)}>
+      <div
+        className={'copyable' + (className ? ` ${className}` : '')}
+        tabIndex={0}
+        onClick={() => {
+          platform.clipboardCopy(children);
+          setShowingSuccess(true);
+        }}>
+        {children}
+        <Icon icon="copy" />
+      </div>
+    </Tooltip>
+  );
+}
+
+function CopiedSuccessTooltipContent(text: string) {
+  return () => (
+    <span className="copyable-success-tooltip">
+      <T replace={{$copiedText: <span className="copyable-success-overflow">{text}</span>}}>
+        Copied '$copiedText'.
+      </T>
+    </span>
+  );
+}
