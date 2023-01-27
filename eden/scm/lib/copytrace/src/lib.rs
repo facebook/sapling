4ba25be7e15569a8c53ada73bc8c02412097c99a
Zhaolong Zhu <zhaolong@meta.com>
2023-01-27 13:01:54 -0800

    copytrace: add GitCopyTrace Rust lib
    Summary: This is a temporary solution to unblock hg-over-git use case, specifically detect renames
    
    Reviewed By: quark-zju
    
    Differential Revision: D42787244
    
    fbshipit-source-id: 37edfc8efbb8a6229a3c2a5bdc3309cf2a89ae3c

---
+++ eden/scm/lib/copytrace/src/lib.rs
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+mod git_copy_trace;
+
+pub use crate::git_copy_trace::GitCopyTrace;
