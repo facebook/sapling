a43e665e8da0ac98cd6b6650af43eeb273644b82
Jun Wu <quark@meta.com>
2023-02-01 11:16:38 -0800

    Disable pull button if any is running or pending
    Summary:
    See the related task. While it's technically possible to queue up multiple
    pulls. Practically it's not that useful. So let's just disable the button while
    pull is running or queued.
    
    Reviewed By: evangrayk
    
    Differential Revision: D42867761
    
    fbshipit-source-id: 6861fdaa2a6c2a08b1e82d1449ef529ca0de2567

--- addons/isl/src/PullButton.tsx
+++ addons/isl/src/PullButton.tsx
@@ -9,7 +9,7 @@ import {DOCUMENTATION_DELAY, Tooltip} from './Tooltip';
 import {t, T} from './i18n';
 import {PullOperation} from './operations/PullOperation';
 import {relativeDate, RelativeDate} from './relativeDate';
-import {latestCommitTree, useRunOperation} from './serverAPIState';
+import {latestCommitTree, operationList, queuedOperations, useRunOperation} from './serverAPIState';
 import {VSCodeButton} from '@vscode/webview-ui-toolkit/react';
 import {useRecoilValue} from 'recoil';
 import {Icon} from 'shared/Icon';
@@ -17,29 +17,43 @@ import {Icon} from 'shared/Icon';
 import './PullButton.css';
 
 export function PullButton() {
+  const list = useRecoilValue(operationList);
+  const queued = useRecoilValue(queuedOperations);
   const runOperation = useRunOperation();
   // no need to use previews here, we only need the latest commits to find the last pull timestamp.
   const latestCommits = useRecoilValue(latestCommitTree);
   // assuming master is getting updated frequently, last pull time should equal the newest commit in the history.
   const lastSync = Math.max(...latestCommits.map(commit => commit.info.date.valueOf()));
+
+  let title =
+    t('Fetch latest repository and branch information from remote.') +
+    '\n\n' +
+    t('Last synced with remote:') +
+    ' ' +
+    relativeDate(lastSync, {});
+  let inProgress = false;
+
+  if (
+    list.currentOperation?.operation instanceof PullOperation &&
+    list.currentOperation?.exitCode == null
+  ) {
+    inProgress = true;
+    title += '\n\n' + t('Pull is currently running.');
+  } else if (queued.some(op => op instanceof PullOperation)) {
+    inProgress = true;
+    title += '\n\n' + t('Pull is already scheduled.');
+  }
+
   return (
-    <Tooltip
-      placement="bottom"
-      delayMs={DOCUMENTATION_DELAY}
-      title={
-        t('Fetch latest repository and branch information from remote.') +
-        '\n\n' +
-        t('Last synced with remote:') +
-        ' ' +
-        relativeDate(lastSync, {})
-      }>
+    <Tooltip placement="bottom" delayMs={DOCUMENTATION_DELAY} title={title}>
       <div className="pull-info">
         <VSCodeButton
           appearance="secondary"
+          disabled={inProgress}
           onClick={() => {
             runOperation(new PullOperation());
           }}>
-          <Icon slot="start" icon="cloud-download" />
+          <Icon slot="start" icon={inProgress ? 'loading' : 'cloud-download'} />
           <T>Pull</T>
         </VSCodeButton>
         <RelativeDate date={lastSync} useShortVariant />
