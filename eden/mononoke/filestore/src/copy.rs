fbb376053a683052bd87398b1e95b2e9731e718d
Rajiv Sharma <rajshar@meta.com>
2023-03-17 03:06:23 -0700

    Generate missing Blake3 alias on x-repo file copy
    Summary: Due to backfilling `ContentMetadataV2` in phases, there are some cases where the metadata exists but the `Blake3` alias doesn't. When copying the alias, if the alias is not present, let's regenerate it.
    
    Reviewed By: markbt
    
    Differential Revision: D44132513
    
    fbshipit-source-id: 4185ece38db350b0069dff485a40166bb0cb1c4d

--- eden/mononoke/filestore/src/copy.rs
+++ eden/mononoke/filestore/src/copy.rs
@@ -5,10 +5,12 @@
  * GNU General Public License version 2.
  */
 
+use anyhow::Context;
 use anyhow::Result;
 use blobstore::BlobCopier;
 use blobstore::Blobstore;
 use blobstore::Loadable;
+use blobstore::Storable;
 use context::CoreContext;
 use futures::future;
 use futures::stream;
@@ -16,16 +18,19 @@ use futures::stream::StreamExt;
 use futures::stream::TryStreamExt;
 use mononoke_types::BlobstoreKey;
 use mononoke_types::BlobstoreValue;
+use mononoke_types::ContentAlias;
 use mononoke_types::ContentMetadataV2;
+use slog::info;
 use strum::IntoEnumIterator;
 
 use crate::Alias;
+use crate::AliasBlob;
 use crate::FileContents;
 use crate::FilestoreConfig;
 
 pub async fn copy(
     original_blobstore: &impl Blobstore,
-    copier: &impl BlobCopier,
+    copier: &(impl BlobCopier + Sync),
     config: FilestoreConfig,
     ctx: &CoreContext,
     data: &ContentMetadataV2,
@@ -33,15 +38,43 @@ pub async fn copy(
     // See reasoning about order of writes in ./finalize.rs::finalize (https://fburl.com/code/3w8dncr3)
 
     // Ensure that all aliases are covered, and missing out an alias gives a compile time error.
-    future::try_join_all(Alias::iter().map(|alias| match alias {
-        Alias::Sha1(_) => copier.copy(ctx, Alias::Sha1(data.sha1).blobstore_key()),
-        Alias::GitSha1(_) => copier.copy(ctx, Alias::GitSha1(data.git_sha1.sha1()).blobstore_key()),
-        Alias::Sha256(_) => copier.copy(ctx, Alias::Sha256(data.sha256).blobstore_key()),
-        Alias::SeededBlake3(_) => {
-            copier.copy(ctx, Alias::SeededBlake3(data.seeded_blake3).blobstore_key())
+    future::try_join_all(Alias::iter().map(|alias| {
+        match alias {
+            Alias::Sha1(_) => copier.copy(ctx, Alias::Sha1(data.sha1).blobstore_key()),
+            Alias::GitSha1(_) => {
+                copier.copy(ctx, Alias::GitSha1(data.git_sha1.sha1()).blobstore_key())
+            }
+            Alias::Sha256(_) => copier.copy(ctx, Alias::Sha256(data.sha256).blobstore_key()),
+            Alias::SeededBlake3(_) => Box::pin(async move {
+                let blake3 = Alias::SeededBlake3(data.seeded_blake3);
+                match copier.copy(ctx, blake3.blobstore_key()).await
+                {
+                    resp @ Ok(_) => resp,
+                    // The backfilling for ContentMetadataV2 has happened in different stages so the alias#
+                    // might be missing. Regenerate it.
+                    Err(_) => {
+                        info!(
+                            ctx.logger(),
+                            "Failure in copying seeded blake3 ({:?}) alias for content ID {:?}. Generating Alias.",
+                            data.seeded_blake3,
+                            data.content_id
+                        );
+                        let content_alias = ContentAlias::from_content_id(data.content_id);
+                        AliasBlob(blake3, content_alias).store(ctx, original_blobstore).await?;
+                        // Now that the alias has been regenerated, try copying again.
+                        copier.copy(ctx, blake3.blobstore_key()).await
+                    }
+                }
+            })
         }
     }))
-    .await?;
+    .await
+    .with_context(|| {
+        format!(
+            "Failure in copying alias for content id {:?}",
+            data.content_id
+        )
+    })?;
 
     // Files are stored inline or in chunks, depending on their size. If they're chunked,
     // we need to copy all chunks. Unfortunately, the only way to know how they're stored is
