d3b53a9bfdcf18a4560a607de5a410a95ad6647d
Mark Juggurnauth-Thomas <mbthomas@meta.com>
2023-03-23 02:22:35 -0700

    virtually_sharded_blobstore: move awaited check to inner struct of Ticket
    Summary:
    The `awaited` boolean on the `Ticket` structure and corresponding `Drop` makes it impossible to move out of the fields of `Ticket`.  This is inconvenient, especially since it only enables a unit test check.
    
    Move the field to a structure that's internal to the `Ticket`.  We still check for failure to await, as dropping the `Ticket` with a `CheckAwait` that hasn't been marked as awaited will still panic during tests when the inner `CheckAwait` gets dropped.
    
    Reviewed By: Croohand
    
    Differential Revision: D44270321
    
    fbshipit-source-id: 29429c40243b31dcc1a5046969b85fb7a15fd0aa

--- eden/mononoke/blobstore/virtually_sharded_blobstore/src/ratelimit.rs
+++ eden/mononoke/blobstore/virtually_sharded_blobstore/src/ratelimit.rs
@@ -21,6 +21,33 @@ pub enum AccessReason {
     Write,
 }
 
+/// Keep track of whether the ticket has been awaited.  This is used in tests
+/// to validate that we always await the ticket.
+#[derive(Clone, Eq, PartialEq, Default)]
+pub struct CheckAwaited {
+    awaited: bool,
+}
+
+impl CheckAwaited {
+    /// Mark that the ticket that contains this `CheckAwaited` has been
+    /// awaited and thus is now safe to drop.
+    fn mark(&mut self) {
+        self.awaited = true;
+    }
+}
+
+/// Implementation of drop to assist with validation during tests.  If the
+/// `Ticket` that owns this `CheckAwaited` has not been awaited before being
+/// dropped, we fail the test.
+#[cfg(test)]
+impl Drop for CheckAwaited {
+    fn drop(&mut self) {
+        if !self.awaited {
+            panic!("Dropped a Pending ticket. This should normally not happen");
+        }
+    }
+}
+
 /// A state machine representing access to the underlying blobstore. This lets us acquire access,
 /// as well as attempt to cancel requests.
 pub enum Ticket<'a> {
@@ -28,12 +55,15 @@ pub enum Ticket<'a> {
     NoTicket,
     /// A ticket was requested, but may not have been awaited yet.
     Pending {
-        inner: TicketInner<'a>,
+        ctx: &'a CoreContext,
+        reason: AccessReason,
+        access: Fuse<BoxFuture<'static, Result<(), Error>>>,
+        limiter: &'a AsyncLimiter,
         /// We keep track of whether this was awaited so that in tests we can warn if we're failing
         /// to await things we should be awaiting. This isn't used outside of tests (and shouldn't:
         /// if the runtime is shutting down, our futures can be dropped with those unused tickets),
         /// and is only used to assist in debugging / validation.
-        awaited: bool,
+        awaited: CheckAwaited,
     },
     /// A ticket was requested and awaited.
     Acquired(&'a AsyncLimiter),
@@ -54,13 +84,11 @@ impl<'a> Ticket<'a> {
         };
 
         Self::Pending {
-            inner: TicketInner {
-                ctx,
-                reason,
-                access: limiter.access().boxed().fuse(),
-                limiter,
-            },
-            awaited: false,
+            ctx,
+            reason,
+            access: limiter.access().boxed().fuse(),
+            limiter,
+            awaited: CheckAwaited::default(),
         }
     }
 
@@ -69,12 +97,18 @@ impl<'a> Ticket<'a> {
         match self {
             Self::NoTicket => Ok(true),
             Self::Pending {
-                ref mut inner,
+                ref mut access,
                 ref mut awaited,
+                ..
             } => {
-                let ready = inner.is_ready().await?;
+                // Race access future against one that is always ready. Since the select is biased, it will
+                // favor the access future if it is indeed ready.
+                let ready = futures::select_biased! {
+                    r = &mut *access => r.map(|()| true),
+                    default => Ok(false)
+                }?;
                 if ready {
-                    *awaited = true;
+                    awaited.mark();
                 }
                 Ok(ready)
             }
@@ -84,15 +118,26 @@ impl<'a> Ticket<'a> {
 
     /// Wait for this ticket. Calling finish again on a ticket that has already finished will just
     /// return immediately.
-    pub async fn finish(mut self) -> Result<Ticket<'a>, Error> {
+    pub async fn finish(self) -> Result<Ticket<'a>, Error> {
         match self {
             Self::Pending {
-                ref mut inner,
-                ref mut awaited,
+                ctx,
+                reason,
+                access,
+                limiter,
+                mut awaited,
             } => {
-                inner.wait_for_access().await?;
-                *awaited = true;
-                Ok(Self::Acquired(inner.limiter))
+                let (stats, ()) = access.try_timed().await?;
+                awaited.mark();
+
+                let counter = match reason {
+                    AccessReason::Read => PerfCounterType::BlobGetsAccessWait,
+                    AccessReason::Write => PerfCounterType::BlobPutsAccessWait,
+                };
+                ctx.perf_counters()
+                    .add_to_counter(counter, stats.completion_time.as_millis_unchecked() as i64);
+
+                Ok(Self::Acquired(limiter))
             }
             x => Ok(x),
         }
@@ -109,59 +154,11 @@ impl<'a> Ticket<'a> {
             } => {
                 // If we never polled this ticket, there is nothing to do. If we did, then we
                 // cannot cancel synchronously (since we didn't wait)
-                *awaited = true;
+                awaited.mark();
             }
             Self::Acquired(l) => {
                 l.cancel();
             }
         };
-    }
-}
-
-pub struct TicketInner<'a> {
-    ctx: &'a CoreContext,
-    reason: AccessReason,
-    access: Fuse<BoxFuture<'static, Result<(), Error>>>,
-    limiter: &'a AsyncLimiter,
-}
-
-impl<'a> TicketInner<'a> {
-    /// Check if the ticket is ready right now. This is async but will return immediately. It will
-    /// request a ticket from the underlying rate limiter.
-    async fn is_ready(&mut self) -> Result<bool, Error> {
-        // Race access future against one that is always ready. Since the select is biased, it will
-        // favor the access future if it is indeed ready.
-        futures::select_biased! {
-            r = &mut self.access => r.map(|()| true),
-            default => Ok(false)
-        }
-    }
-
-    /// Wait for this ticket to be ready. This will block until the underlying access future
-    /// indicates that it is OK to proceed.
-    async fn wait_for_access(&mut self) -> Result<(), Error> {
-        let (stats, ()) = (&mut self.access).try_timed().await?;
-
-        let counter = match self.reason {
-            AccessReason::Read => PerfCounterType::BlobGetsAccessWait,
-            AccessReason::Write => PerfCounterType::BlobPutsAccessWait,
-        };
-
-        self.ctx
-            .perf_counters()
-            .add_to_counter(counter, stats.completion_time.as_millis_unchecked() as i64);
-
-        Ok(())
-    }
-}
-
-/// This Drop impementation is here to assist in validation: if a ticket is dropped but was pending
-/// and not awaited, we fail the test.
-#[cfg(test)]
-impl<'a> Drop for Ticket<'a> {
-    fn drop(&mut self) {
-        if let Self::Pending { awaited: false, .. } = self {
-            panic!("Dropped a Pending ticket. This should normally not happen");
-        }
     }
 }
