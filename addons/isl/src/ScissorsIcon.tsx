e5613aefb837e61677485839eb01de63520af9e0
Evan Krause <evangrayk@meta.com>
2023-03-28 13:05:40 -0700

    Add papercuts button to help menu
    Reviewed By: quark-zju
    
    Differential Revision: D44385038
    
    fbshipit-source-id: 32a09766f229033b634c311689870d259084f8c1

---
+++ addons/isl/src/ScissorsIcon.tsx
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export function ScissorsIcon() {
+  return (
+    <svg width="16" height="16" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M47 9L25 44" stroke="currentColor" strokeWidth="6" strokeLinecap="round" />
+      <path
+        d="M58.0617 24.8059C59.6113 24.2195 60.3922 22.488 59.8059 20.9383C59.2195 19.3887 57.488 18.6078 55.9383 19.1941L58.0617 24.8059ZM21.0617 38.8059L58.0617 24.8059L55.9383 19.1941L18.9383 33.1941L21.0617 38.8059Z"
+        fill="currentColor"
+      />
+      <circle cx="16" cy="29" r="8" stroke="currentColor" strokeWidth="6" />
+      <circle cx="32" cy="48" r="8" stroke="currentColor" strokeWidth="6" />
+    </svg>
+  );
+}
