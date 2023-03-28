23f1b480fe7d545d30d0c37b7a99b647220a970c
Wei Dai <daiweix@meta.com>
2023-03-27 19:08:05 -0700

    implement a basic working version of MonotonicCounter
    Summary:
    Before fbcode/common/stats/ codes are open sourced , implement a basic
    working version of MonotonicCounter so that all hw tests using fb303 APIs like
     fb303::fbData->getStatMap() should just work
    
    Reviewed By: jasmeetbagga
    
    Differential Revision: D44441336
    
    fbshipit-source-id: f3e0fe0a59523bf90dd1e99bdca7bf5eca212c58

--- common/stats/MonotonicCounter.h
+++ common/stats/MonotonicCounter.h
@@ -12,6 +12,7 @@
 #include <fb303/ExportedHistogramMap.h>
 #include <fb303/ExportedHistogramMapImpl.h>
 #include <fb303/ExportedStatMapImpl.h>
+#include <fb303/ServiceData.h>
 #include <folly/Range.h>
 
 namespace facebook {
@@ -19,16 +20,43 @@ namespace stats {
 
 class MonotonicCounter {
  public:
-  MonotonicCounter(folly::StringPiece, fb303::ExportType, fb303::ExportType) {}
-  void updateValue(std::chrono::seconds, int64_t) {}
+  MonotonicCounter(
+      folly::StringPiece name,
+      fb303::ExportType,
+      fb303::ExportType) {
+    auto statMap = facebook::fb303::fbData->getStatMap();
+    stat_ = statMap->getLockableStatNoExport(name);
+    name_ = name;
+  }
+  void updateValue(std::chrono::seconds now, int64_t value) {
+    auto guard = stat_.lock();
+    if (!init_) {
+      prevValue_ = value;
+      init_ = true;
+      return;
+    }
+    if (prevValue_ > value) {
+      delta_ = 0;
+    } else {
+      delta_ = value - prevValue_;
+    }
+    prevValue_ = value;
+    stat_.addValueLocked(guard, now.count(), delta_);
+  }
   void swap(MonotonicCounter& counter) {}
   int64_t get() const {
-    return 0;
+    return delta_;
   }
   const std::string& getName() const {
-    static const std::string kName;
-    return kName;
+    return name_;
   }
+
+ private:
+  bool init_{false};
+  int64_t prevValue_{0};
+  int64_t delta_{0};
+  std::string name_;
+  facebook::fb303::ExportedStatMapImpl::LockableStat stat_;
 };
 } // namespace stats
 } // namespace facebook
