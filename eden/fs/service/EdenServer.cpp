ce0287d81c48c65edfd0bff5ca6cf387f8f3cb89
Katie Mancini <kmancini@meta.com>
2023-03-29 12:52:44 -0700

    get oss windows build back to functioning
    Summary:
    The external OSS build is broken because thrift has a really long path and
    cargo/git on Windows do not lick such long paths.
    
    I'm not fixing that. But the external OSS build is the only one we run in CI any
    more. In the meantime the internal build has broken because no one has been
    watching.
    
    There were 4 different breakages.
    
    Reviewed By: chadaustin
    
    Differential Revision: D44189633
    
    fbshipit-source-id: 2eedbc2b3bbf5d1def075d99f11f2273dbb1f4ab

--- eden/fs/service/EdenServer.cpp
+++ eden/fs/service/EdenServer.cpp
@@ -2227,7 +2227,7 @@ ImmediateFuture<uint64_t> EdenServer::garbageCollectWorkingCopy(
   if (!lease) {
     XLOG(DBG6) << "Not running GC for: " << mount->getPath()
                << ", another GC is already in progress";
-    return 0;
+    return (uint64_t)0;
   }
 
   auto mountPath = mount->getPath();
