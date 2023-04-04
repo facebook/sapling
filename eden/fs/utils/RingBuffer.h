baf60f0a42c79422505c75efb48ddae0a930109a
Chad Austin <chadaustin@meta.com>
2023-04-03 23:40:55 -0700

    add a RingBuffer
    Summary:
    `std::deque` has poor performance and bucketing in GNU libstdc++, and
    sometimes we just need a ring buffer without paying for folly's
    LockFreeRingBuffer.
    
    Introduce a simple RingBuffer atop `std::vector` for later use in
    ActivityBuffer.
    
    Reviewed By: xavierd
    
    Differential Revision: D44593248
    
    fbshipit-source-id: 13303c27b184002fc74aec9ae6e9133c70bab6bb

---
+++ eden/fs/utils/RingBuffer.h
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#pragma once
+
+#include <vector>
+
+namespace facebook::eden {
+
+/**
+ * Non-synchronized ring buffer with a fixed capacity.
+ */
+template <typename T>
+class RingBuffer {
+ public:
+  /**
+   * Constructs a RingBuffer with a given capacity. Zero is legal.
+   */
+  explicit RingBuffer(size_t capacity);
+
+  /**
+   * Returns the capacity.
+   */
+  size_t capacity() const {
+    return capacity_;
+  }
+
+  /**
+   * Returns the number of entries pushed minus the number of entries evicted.
+   *
+   * size() <= capacity().
+   */
+  size_t size() const;
+
+  /**
+   * Pushes an entry into the RingBuffer. This replaces the oldest existing
+   * entry if capacity has been reached.
+   */
+  template <typename U>
+  void push(U&& entry);
+
+  /**
+   * Returns the contents of this RingBuffer in order from oldest to newest.
+   */
+  std::vector<T> toVector() const;
+
+ private:
+  size_t capacity_;
+  std::vector<T> entries_;
+  size_t write_ = 0;
+};
+
+template <typename T>
+RingBuffer<T>::RingBuffer(size_t capacity) : capacity_{capacity} {
+  // vector does not require that reserve sets capacity precisely.
+  // Overshooting the desired capacity may be undesirable if T has its own
+  // externally-allocated memory.
+  entries_.reserve(capacity);
+}
+
+template <typename T>
+size_t RingBuffer<T>::size() const {
+  return entries_.size();
+}
+
+template <typename T>
+template <typename U>
+void RingBuffer<T>::push(U&& entry) {
+  static_assert(std::is_constructible_v<T, U&&>);
+
+  if (capacity_ == 0) {
+    return;
+  }
+  if (entries_.size() < capacity_) {
+    entries_.push_back(std::forward<U>(entry));
+  } else {
+    entries_[write_] = std::forward<U>(entry);
+    ++write_;
+    if (write_ == capacity_) {
+      write_ = 0;
+    }
+  }
+}
+
+template <typename T>
+std::vector<T> RingBuffer<T>::toVector() const {
+  if (write_ == 0) {
+    return entries_;
+  } else {
+    std::vector<T> entries;
+    entries.reserve(capacity_);
+    for (size_t i = write_; i < capacity_; ++i) {
+      entries.push_back(entries_[i]);
+    }
+    for (size_t i = 0; i < write_; ++i) {
+      entries.push_back(entries_[i]);
+    }
+
+    return entries;
+  }
+}
+
+} // namespace facebook::eden
---
+++ eden/fs/utils/test/RingBufferTest.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "eden/fs/utils/RingBuffer.h"
+#include <folly/portability/GMock.h>
+#include <folly/portability/GTest.h>
+
+namespace {
+
+using testing::ElementsAre;
+using namespace facebook::eden;
+
+TEST(RingBufferTest, starts_empty) {
+  RingBuffer<int> b{4};
+  EXPECT_EQ(0, b.size());
+}
+
+TEST(RingBufferTest, elements_can_be_retrieved_as_vector) {
+  RingBuffer<int> b{4};
+  b.push(1);
+  b.push(2);
+
+  EXPECT_THAT(b.toVector(), ElementsAre(1, 2));
+}
+
+TEST(RingBufferTest, exact_size) {
+  RingBuffer<int> b{4};
+  b.push(1);
+  b.push(2);
+  b.push(3);
+  b.push(4);
+
+  EXPECT_THAT(b.toVector(), ElementsAre(1, 2, 3, 4));
+}
+
+TEST(RingBufferTest, wraps_around) {
+  RingBuffer<int> b{4};
+  b.push(1);
+  b.push(2);
+  b.push(3);
+  b.push(4);
+  b.push(5);
+  b.push(6);
+
+  EXPECT_THAT(b.toVector(), ElementsAre(3, 4, 5, 6));
+}
+
+TEST(RingBufferTest, insert_non_temporary) {
+  RingBuffer<int> b{4};
+  int x = 10;
+  b.push(x);
+  EXPECT_EQ(1, b.toVector().size());
+}
+
+TEST(RingBufferTest, zero_size) {
+  RingBuffer<int> b{0};
+  b.push(1);
+  b.push(2);
+  b.push(3);
+  EXPECT_EQ(0, b.toVector().size());
+}
+
+} // namespace
