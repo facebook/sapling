222aea47f2ba38624f506914cc444aa7538a43e8
Liubov Dmitrieva <liubovd@meta.com>
2023-04-06 07:00:57 -0700

    intro high level transaction for cloud sync
    Summary:
    intro high level transaction for cloud sync
    
    this is for syncs that are not running with best effort option
    
    the transaction is needed to eliminate repo changed while backing up errors
    
    this is a follow up after https://www.internalfb.com/diff/D41871718 that hasn't resolved the errors fully
    
    Reviewed By: markbt
    
    Differential Revision: D44664729
    
    fbshipit-source-id: da411cd20fb909c22ff8976dd31bd46034a28226

--- eden/scm/edenscm/ext/commitcloud/sync.py
+++ eden/scm/edenscm/ext/commitcloud/sync.py
@@ -104,9 +104,13 @@ def sync(repo, *args, **kwargs):
     with backuplock.lock(repo):
         try:
             besteffort = kwargs.pop("besteffort", False)
-            nc = util.nullcontextmanager
-            with nc() if besteffort else repo.wlock(), nc() if besteffort else repo.lock():
+            if besteffort:
                 rc, synced = _sync(repo, *args, **kwargs)
+            else:
+                with repo.wlock(), repo.lock(), repo.transaction(
+                    "cloudsync_metalog_global"
+                ):
+                    rc, synced = _sync(repo, *args, **kwargs)
             if synced is not None:
                 with repo.svfs(_syncstatusfile, "w+") as fp:
                     fp.write(encodeutf8("Success" if synced else "Failed"))
