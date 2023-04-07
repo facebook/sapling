3d4cf0f9341b6c464f77bc47c57d85d88d62b4eb
Chad Austin <chadaustin@meta.com>
2023-04-07 13:34:05 -0700

    add a script to force unmount every EdenFS mount under TMPDIR
    Summary:
    We have a force-unmount-all.sh script as a last resort when a Linux
    machine has a pile of stuck FUSE mounts.
    
    Introduce a new script that only unmounts the ones under $TMPDIR,
    which were probably created by failed integration tests.
    
    Reviewed By: mshroyer
    
    Differential Revision: D44797173
    
    fbshipit-source-id: 41f31cf1e2e57ad07076cf1b2571c739b67b77f0

--- eden/fs/scripts/force-unmount-tmp.sh
+++ eden/fs/scripts/force-unmount-tmp.sh
@@ -4,13 +4,13 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
-ALL_EDENFS_MOUNTS=$(grep -e '^edenfs:' /proc/mounts | awk '{print $2}')
+ALL_EDENFS_MOUNTS=$(grep -e "^edenfs:$TMPDIR" /proc/mounts | awk '{print $2}')
 for n in $ALL_EDENFS_MOUNTS; do
     # Sometimes, lazy unmounting causes other mounts to unmount.
     sudo umount -v -l "$n"
 done
 
-ALL_EDENFS_MOUNTS=$(grep -e '^edenfs:' /proc/mounts | awk '{print $2}')
+ALL_EDENFS_MOUNTS=$(grep -e "^edenfs:$TMPDIR" /proc/mounts | awk '{print $2}')
 for n in $ALL_EDENFS_MOUNTS; do
     sudo umount -v -f "%f"
 done
