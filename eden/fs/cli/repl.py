1d7bbe6d6da3e34210fb35b547fb8a624aca40a6
Mark Shroyer <mshroyer@meta.com>
2023-04-20 16:03:57 -0700

    Add eden/fs/cli:repl target
    Summary:
    Adds a buck target to drop us into a Python repl, to ease interactive
    development of Python commands in the CLI.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D45159857
    
    fbshipit-source-id: 31ff61ea29ab080cc1131905c6cf363368f3c617

---
+++ eden/fs/cli/repl.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+
+import asyncio
+import selectors
+
+import IPython
+
+
+def main() -> None:
+    # Work around issue with ProactorEventLoop on Python 3.8.6:
+    # https://github.com/prompt-toolkit/python-prompt-toolkit/issues/1023#issuecomment-563337948
+    selector = selectors.SelectSelector()
+    loop = asyncio.SelectorEventLoop(selector)
+    asyncio.set_event_loop(loop)
+
+    IPython.embed()
+
+
+if __name__ == "__main__":
+    main()
