1cbc2a062e2455c1a26a7e751d801be4da18d157
Evan Krause <evangrayk@meta.com>
2023-04-25 16:02:03 -0700

    Add getting started utility components and styles
    Summary:
    Add components and styles for getting started modal. This is used to show what's new in the vscode extension for internal first-time users.
    
    later on, we may use this format for other information, such as a new user getting started documentation or changelog.
    
    Reviewed By: quark-zju
    
    Differential Revision: D45253594
    
    fbshipit-source-id: c6ff88636a96a199bd0154867b64f497987b1fec

---
+++ addons/isl/src/gettingStarted/Components.tsx
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {T} from '../i18n';
+import platform from '../platform';
+import {themeState} from '../theme';
+import {Ribbon} from './Ribbon';
+import {VSCodeLink, VSCodeButton} from '@vscode/webview-ui-toolkit/react';
+import {useRecoilValue} from 'recoil';
+import {Icon} from 'shared/Icon';
+
+export function DismissButton({dismiss}: {dismiss: () => void}) {
+  return (
+    <div className="dismiss">
+      <VSCodeButton appearance="icon" onClick={dismiss}>
+        <Icon icon="x" />
+      </VSCodeButton>
+    </div>
+  );
+}
+
+export function Link({
+  children,
+  href,
+  className,
+}: {
+  children: React.ReactNode;
+  href: string;
+  className?: string;
+}) {
+  return (
+    <VSCodeLink className={className} onClick={() => platform.openExternalLink(href)}>
+      {children}
+    </VSCodeLink>
+  );
+}
+
+export function Subtitle({children}: {children: React.ReactNode}) {
+  return <h2 className="subtitle">{children}</h2>;
+}
+
+export function Squares({children}: {children: React.ReactNode}) {
+  return <div className="squares">{children}</div>;
+}
+
+export function SquareLink({children, href}: {children: React.ReactNode; href: string}) {
+  return (
+    <a
+      className="square"
+      tabIndex={0}
+      onKeyDown={e => {
+        if (e.key === 'Enter') {
+          platform.openExternalLink(href);
+          e.preventDefault();
+        }
+      }}
+      onClick={e => {
+        platform.openExternalLink(href);
+        e.preventDefault();
+      }}>
+      {children}
+    </a>
+  );
+}
+
+export function Card({
+  title,
+  imgDark,
+  imgLight,
+  alt,
+  description,
+  side,
+  comingSoon,
+}: {
+  title: React.ReactNode;
+  imgLight: string;
+  imgDark: string;
+  alt: string;
+  description: React.ReactNode;
+  side: 'left' | 'right';
+  comingSoon?: boolean;
+}) {
+  const theme = useRecoilValue(themeState);
+  const imgEl = (
+    <img src={theme === 'light' ? imgLight : imgDark} alt={alt} className="card-image" />
+  );
+  return (
+    <div className="card">
+      {side === 'left' && imgEl}
+      <div className="card-details">
+        <div className="card-title">{title}</div>
+        <div className="card-description">
+          <p>{description}</p>
+        </div>
+      </div>
+      {side === 'right' && imgEl}
+      {comingSoon ? (
+        <Ribbon>
+          <T>Coming Soon!</T>
+        </Ribbon>
+      ) : null}
+    </div>
+  );
+}
+
+export function CallToAction({children}: {children: React.ReactNode}) {
+  return (
+    <div className="call-to-action">
+      <Icon icon="alert" size="L" />
+      <p>{children}</p>
+    </div>
+  );
+}
