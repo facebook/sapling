05a9b6530235bfd3d40531a0485ab1dde658663c
Jun Wu <quark@meta.com>
2023-04-26 18:02:41 -0700

    pyxdiff: expose edit_cost
    Summary: This makes it possible to use `edit_cost` in Python.
    
    Reviewed By: muirdm
    
    Differential Revision: D45303297
    
    fbshipit-source-id: f948f317f907f9e808aec66f5e388abe632c768a

--- eden/scm/edenscmnative/bindings/modules/pyxdiff/src/lib.rs
+++ eden/scm/edenscmnative/bindings/modules/pyxdiff/src/lib.rs
@@ -14,6 +14,11 @@ pub fn init_module(py: Python, package: &str) -> PyResult<PyModule> {
     let name = [package, "xdiff"].join(".");
     let m = PyModule::new(py, &name)?;
     m.add(py, "blocks", py_fn!(py, blocks(a: PyObject, b: PyObject)))?;
+    m.add(
+        py,
+        "edit_cost",
+        py_fn!(py, edit_cost(a: PyObject, b: PyObject, max_edit_cost: u64)),
+    )?;
     Ok(m)
 }
 
@@ -26,6 +31,16 @@ fn blocks(py: Python, a: PyObject, b: PyObject) -> PyResult<Serde<Vec<(u64, u64,
     let b_data = b.as_bytes(py)?;
     let result = py.allow_threads(|| xdiff::blocks(&a_data, &b_data));
     Ok(Serde(result))
+}
+
+// (a: bytes | str, b: bytes | str, max_edit_cost) -> edit_cost
+fn edit_cost(py: Python, a: PyObject, b: PyObject, max_edit_cost: u64) -> PyResult<u64> {
+    let a = a.extract::<BytesOrStr>(py)?;
+    let b = b.extract::<BytesOrStr>(py)?;
+    let a_data = a.as_bytes(py)?;
+    let b_data = b.as_bytes(py)?;
+    let result = py.allow_threads(|| xdiff::edit_cost(&a_data, &b_data, max_edit_cost));
+    Ok(result)
 }
 
 enum BytesOrStr {
