f127e75a09f656247ba4365a6dec8b6b5376273e
Jun Wu <quark@meta.com>
2023-05-02 20:29:43 -0700

    animate: minimal container component for reorder animation
    Summary:
    I have done some exploration in the reorder/animation/drag-n-drop area and
    concluded it's probably best to just DIY our needs. Summary of my learning:
    
    Animation:
    - The best API to use is Web Animation APIs. It's fast, flexible, supported
      by major browsers for years, and does not churn the `style` attributes.
      See https://mozdevs.github.io/Animation-examples/
    - Only `opacity` and `transform` changes are fast. They trigger "composite",
      no slow "layout" or "paint".
      See https://aerotwist.com/blog/flip-your-animations/ ("FLIP") and
      https://huijing.github.io/slides/03-css-triggers/
    - Most popular animation libraries use `requestAnimationFrame` and changes
      the `style` of DOM elements in Javascript. They follow the "FLIP" best
      practice, but somehow don't use the great Web Animation APIs.
    - The actual work to implement animations with React is not that difficult
      as I initially thought.
    
    Reordering:
    - Can be done by animating the x, y offsets.
    - If we ignore the "insert" and "delete" animations, the code is much simpler
      (not tracking `children` in `state`). Otherwise it'll somewhat re-invent
      `react-transition-group` internals.
    - `react-transition-group` can probably be used together if we want "insert"
      and "delete" animations.
    
    Drag-and-drop:
    - Pointer events cover both mouse and touch. They are nice to use. We don't
      have to use drag and drop events.
    - Does NOT have to be coupled with reordering animation. For example, we can
      just "render" the "preview reordered" items and send them to the reordering
      component. The reordering component gets the preview and does not need to
      maintain an extra "preview" state, which can be quite annoying.
    
    This diff implements the reorder animation support. Drag-and-drop will be done
    next.
    
    ChatGPT contributed a lot of the initial implementation. I like its use of the
    `React.FC` type to simplify the code a bit. I provided the concrete requirements
    and did some clean up. ChatGPT used `ResizeObserver` to observe bounding box
    changes but that's not enough (ResizeObserver does not observe offset changes)
    and can be tricky to know if the states are using the old children or new
    children.
    
    What we actually want is the "old" states just before the animation
    (useLayoutEffect). I added a side effect that runs before calculating the
    rendered elements to do that.
    
    Reviewed By: evangrayk
    
    Differential Revision: D45405113
    
    fbshipit-source-id: f57d99edc95237bcef099cf2f8636059885d69a5

---
+++ addons/isl/src/AnimatedReorderGroup.tsx
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import deepEqual from 'fast-deep-equal';
+import React, {useRef, useLayoutEffect} from 'react';
+
+type ReorderGroupProps = {
+  children: React.ReactElement[];
+};
+
+type PreviousState = {
+  // Ordered list of `data-reorder-id`s. Can ONLY be updated inside `useLayoutEffect`.
+  // Useful to test if `children` has changed or not.
+  idList: Array<string>;
+
+  // Locations of the old children, keyed by `data-reorder-id`.
+  rectMap: Map<string, DOMRect>;
+};
+
+const emptyPreviousState: Readonly<PreviousState> = {
+  idList: [],
+  rectMap: new Map(),
+};
+
+/**
+ * AnimatedReorderGroup tracks and animates elements with the `data-reorder-id` attribute.
+ * Elements with the same `data-reorder-id` will be animated on position change.
+ *
+ * Beware that while `data-reorder-id` can be put on nested elements, animation is
+ * only triggered when the `children` of this component is changed.
+ *
+ * This component only handles reordering, if you want drag and drop support or animations
+ * on inserted or deleted items, you might want to use other components together.
+ */
+export const AnimatedReorderGroup: React.FC<ReorderGroupProps> = ({children, ...props}) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const previousStateRef = useRef<Readonly<PreviousState>>(emptyPreviousState);
+
+  useLayoutEffect(() => {
+    updatePreviousState(containerRef, previousStateRef, true);
+  }, [children]);
+
+  // Try to get the rects of old children right before rendering new children
+  // and calling the LayoutEffect callback. This captures position changes
+  // since the last useLayoutEffect. The position changes might be caused by
+  // scrolling or resizing the window.
+  updatePreviousState(containerRef, previousStateRef, false);
+
+  return (
+    <div {...props} ref={containerRef}>
+      {children}
+    </div>
+  );
+};
+
+function scanElements(containerRef: React.RefObject<HTMLDivElement | null>): HTMLElement[] {
+  const container = containerRef.current;
+  if (container == null) {
+    return [];
+  }
+  const elements = container.querySelectorAll<HTMLElement>('[data-reorder-id]');
+  return [...elements];
+}
+
+function updatePreviousState(
+  containerRef: React.RefObject<HTMLDivElement>,
+  previousStateRef: React.MutableRefObject<Readonly<PreviousState>>,
+  animate = false,
+) {
+  const elements = scanElements(containerRef);
+  const idList: Array<string> = [];
+  const rectMap = new Map<string, DOMRect>();
+  elements.forEach(element => {
+    const reorderId = element.getAttribute('data-reorder-id');
+    if (reorderId == null || reorderId === '') {
+      return;
+    }
+    idList.push(reorderId);
+    const newBox = element.getBoundingClientRect();
+    if (animate) {
+      const oldBox = previousStateRef.current.rectMap.get(reorderId);
+      if (oldBox && (oldBox.x !== newBox.x || oldBox.y !== newBox.y)) {
+        // Animate from old to the new (current) rect.
+        const dx = oldBox.left - newBox.left;
+        const dy = oldBox.top - newBox.top;
+        element.animate(
+          [{transform: `translate(${dx}px,${dy}px)`}, {transform: 'translate(0,0)'}],
+          {duration: 200, easing: 'ease-out'},
+        );
+      }
+    }
+    rectMap.set(reorderId, newBox);
+  });
+
+  if (!animate && !deepEqual(idList, previousStateRef.current.idList)) {
+    // If animate is false, we want to get the rects of the old children.
+    // If the idList mismatches, it's not the "old" children so we discard
+    // the result.
+    return;
+  }
+
+  previousStateRef.current = {
+    idList,
+    rectMap,
+  };
+}
