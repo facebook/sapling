b4f626a4196ce0edc8e66a2993809992d9f16ad5
Jun Wu <quark@meta.com>
2023-05-02 20:29:43 -0700

    shared: make LRU friendly for immutable data structures
    Summary:
    I want to use a LRU cache where keys are tuples of immutable values
    for linelog. But that's not possible with a native `Map` that uses
    `Object.is` to compare keys. Change the LRU implementation to use
    immutable map so key comparison is done via the desired `Immutable.is`.
    
    Reviewed By: evangrayk
    
    Differential Revision: D45154108
    
    fbshipit-source-id: 2adc353a95cd4e3920f16285c887a9fd6534b1e1

--- addons/shared/LRU.ts
+++ addons/shared/LRU.ts
@@ -5,12 +5,23 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import {OrderedMap} from 'immutable';
+
 /**
  * Simple least-recently-used cache which holds at most `max` entries.
+ *
+ * The map uses `Immutable.is` [1] instead of `Object.is` to compare keys.
+ * So it can support complex key types (ex. a tuple of immutable
+ * collections and other types). Right now this is done by using data
+ * structures from `immutable`. If the native `Map` supports customizing
+ * the compare function we can replace `Object.is` with `Immutable.is`
+ * and use native `Map` for performance.
+ *
+ * [1]: https://immutable-js.com/docs/v4.3.0/is()/.
  */
 export class LRU<K, V> {
   // Implementation is based on Map having stable insertion order and O(1) insertion
-  private cache = new Map<K, V>();
+  private cache = OrderedMap<K, V>();
 
   constructor(private maxItems: number) {}
 
@@ -20,48 +31,52 @@ export class LRU<K, V> {
       return undefined;
     }
 
-    // refresh by re-inserting
-    this.cache.delete(key);
-    this.cache.set(key, val);
+    this.cache = this.cache.withMutations(origCache => {
+      let cache = origCache;
+      // refresh by re-inserting
+      cache = cache.delete(key);
+      cache = cache.set(key, val);
+      return cache;
+    });
 
     return val;
   }
 
   set(key: K, value: V) {
-    if (this.cache.has(key)) {
-      // ensure refresh by deleting before setting
-      this.cache.delete(key);
-    }
+    this.cache = this.cache.withMutations(origCache => {
+      let cache = origCache;
 
-    if (value === undefined) {
-      // `set(key, undefined)` is indistinguishable from `key` not being in the cache,
-      // as far as you can tell from `get(key)`.
-      // Save a bit of space by not re-inserting into the cache after deleting.
-      return;
-    }
+      if (cache.has(key)) {
+        // ensure refresh by deleting before setting
+        cache = cache.delete(key);
+      }
 
-    this.cache.set(key, value);
+      if (value !== undefined) {
+        // `set(key, undefined)` is indistinguishable from `key` not being in the cache,
+        // as far as you can tell from `get(key)`.
+        // Save a bit of space by not re-inserting into the cache after deleting.
+        cache = cache.set(key, value);
+      }
 
-    if (this.cache.size > this.maxItems) {
-      // evict oldest
-      this.deleteOldestKey();
-    }
+      if (cache.size > this.maxItems) {
+        // evict oldest
+        // iteration order guarantees oldest item is first
+        const next = cache.keys().next();
+        // undefined is a valid value, so use iterator `done` to know whether to delete or not.
+        if (!next.done) {
+          cache = cache.delete(next.value);
+        }
+      }
+
+      return cache;
+    });
   }
 
   delete(key: K) {
-    this.cache.delete(key);
+    this.cache = this.cache.delete(key);
   }
 
   clear() {
-    this.cache.clear();
-  }
-
-  private deleteOldestKey() {
-    // iteration order guarantees oldest item is first
-    const next = this.cache.keys().next();
-    // undefined is a valid value, so use iterator `done` to know whether to delete or not.
-    if (!next.done) {
-      this.cache.delete(next.value);
-    }
+    this.cache = this.cache.clear();
   }
 }
