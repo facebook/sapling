14b78706b2b752b0496990500614e766d5400e87
Evan Krause <evangrayk@meta.com>
2023-05-08 15:30:28 -0700

    Add scaffolding for tokenization and autocomplete in commit info text fields
    Summary:
    We want to show a typeahead/autocomplete while you're typing reviewers / subscribers / tasks/ etc. This will need to fetch data and show a list of values, then let you accept the results.
    
    To make this nicer, we also want to show the values as tokens, instead of just text. These visual tokens distinguishes values from each other, and lets you click on the x to dismiss them more easily.
    
    This is just the first steps in building this UX. Remaining in further commits, we need to actually fetch the right data depending on the type of field, handle the data, insert suggestions, let you delete suggestions, and deal with focus / other subtleties.
    
    Reviewed By: quark-zju
    
    Differential Revision: D45627742
    
    fbshipit-source-id: d9133c04291bf6165bdf4b7efc6c22330ef68afe

--- addons/isl/src/CommitInfoView/TextField.tsx
+++ addons/isl/src/CommitInfoView/TextField.tsx
@@ -5,9 +5,23 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import {Subtle} from '../Subtle';
 import {getInnerTextareaForVSCodeTextArea} from './utils';
-import {VSCodeTextField} from '@vscode/webview-ui-toolkit/react';
-import {useRef, useEffect} from 'react';
+import {VSCodeButton, VSCodeTextField} from '@vscode/webview-ui-toolkit/react';
+import {useRef, useEffect, useState} from 'react';
+import {Icon} from 'shared/Icon';
+
+/** Extract comma-separated tokens into an array, plus any remaining non-tokenized text */
+function extractTokens(raw: string): [Array<string>, string] {
+  const tokens = raw.split(',').map(token => token.trim());
+  const remaining = tokens.length === 0 ? raw : tokens.pop();
+  return [tokens, remaining ?? ''];
+}
+
+/** Combine tokens back into a string to be stored in the commit message */
+function tokensToString(tokens: Array<string>, remaining: string): string {
+  return tokens.join(',') + ',' + remaining;
+}
 
 export function CommitInfoTextField({
   name,
@@ -28,20 +42,79 @@ export function CommitInfoTextField({
     }
   }, [autoFocus, ref]);
 
+  const [tokens, remaining] = extractTokens(editedMessage);
+
+  const [autocompleteSuggestions, setAutocompleteSuggestions] =
+    useState<AutocompleteSuggestions>(undefined);
+
   const onInput = (event: {target: EventTarget | null}) => {
-    setEditedCommitMessage((event?.target as HTMLInputElement)?.value);
+    const newValue = (event?.target as HTMLInputElement)?.value;
+    setEditedCommitMessage(tokensToString(tokens, newValue));
+    setAutocompleteSuggestions({type: 'loading'});
+    fetchNewSuggestions(newValue).then(values =>
+      setAutocompleteSuggestions({type: 'success', values}),
+    );
   };
 
   const fieldKey = name.toLowerCase().replace(/\s/g, '-');
 
   return (
-    <div className="commit-info-field">
-      <VSCodeTextField
-        ref={ref}
-        value={editedMessage}
-        data-testid={`commit-info-${fieldKey}-field`}
-        onInput={onInput}
-      />
+    <div className="commit-info-tokenized-field">
+      {tokens
+        .filter(token => token != '')
+        .map((token, i) => (
+          <span key={i} className="token">
+            {token}
+            <VSCodeButton appearance="icon">
+              <Icon icon="x" />
+            </VSCodeButton>
+          </span>
+        ))}
+      <div className="commit-info-field-with-autocomplete">
+        <VSCodeTextField
+          ref={ref}
+          value={remaining}
+          data-testid={`commit-info-${fieldKey}-field`}
+          onInput={onInput}
+        />
+        {autocompleteSuggestions?.type === 'loading' ||
+        (autocompleteSuggestions?.values?.length ?? 0) > 0 ? (
+          <div className="autocomplete-suggestions">
+            {autocompleteSuggestions?.type === 'loading' ? (
+              <Icon icon="loading" />
+            ) : (
+              autocompleteSuggestions?.values.map(suggestion => (
+                <span key={suggestion.value} className="suggestion">
+                  <span>{suggestion.display}</span>
+                  <Subtle>{suggestion.value}</Subtle>
+                </span>
+              ))
+            )}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
+}
+
+type AutocompleteSuggestions =
+  | {
+      type: 'loading';
+    }
+  | {type: 'success'; values: Array<AutocompleteSuggestion>}
+  | undefined;
+type AutocompleteSuggestion = {
+  /** The display text of the suggestion */
+  display: string;
+  /**
+   * The literal value of the suggestion,
+   * shown de-emphasized next to the display name
+   * and placed literally as text into the commit message
+   */
+  value: string;
+};
+
+// eslint-disable-next-line require-await
+async function fetchNewSuggestions(_text: string): Promise<Array<AutocompleteSuggestion>> {
+  return [];
 }
