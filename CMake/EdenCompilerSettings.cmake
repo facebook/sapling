74c1a0511a4f34e449d0d664639b9584a602896a
Chad Austin <chadaustin@meta.com>
2023-05-12 19:41:24 -0700

    unset WIN32_LEAN_AND_MEAN on Linux and macOS
    Summary: We can limit the -DWIN32_LEAN_AND_MEAN flag to Windows.
    
    Reviewed By: xavierd
    
    Differential Revision: D45823417
    
    fbshipit-source-id: 47fab430363b4b4e7f256002e5a0043494154d06

--- CMake/EdenCompilerSettings.cmake
+++ CMake/EdenCompilerSettings.cmake
@@ -3,6 +3,10 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
+if (WIN32)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DWIN32_LEAN_AND_MEAN -DNOMINMAX")
+endif()
+
 if (NOT WIN32)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-nullability-completeness")
 endif()
