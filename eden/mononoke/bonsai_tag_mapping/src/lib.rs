1b2606508425dfd803b6fc3cd8dc1986277e15ba
Rajiv Sharma <rajshar@meta.com>
2023-05-18 11:08:05 -0700

    Implement BonsaiTagMapping in SQL
    Summary: This diff implements the SQL logic for creating and fetching entries from `bonsai_tag_mapping` table. It also defines a new facet which will be used in gitimport, remote-gitimport and mononoke-admin tool later on. I will send a follow-up diff with tests to validate the behavior of the SQL implementation.
    
    Reviewed By: mitrandir77
    
    Differential Revision: D45904551
    
    fbshipit-source-id: 31a99d89b10b39ce7aeda5f4e46b0c94ec6b60ba

--- eden/mononoke/bonsai_tag_mapping/src/lib.rs
+++ eden/mononoke/bonsai_tag_mapping/src/lib.rs
@@ -7,7 +7,10 @@
 
 mod sql;
 
+use anyhow::Result;
+use async_trait::async_trait;
 use mononoke_types::ChangesetId;
+use mononoke_types::RepositoryId;
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct BonsaiTagMappingEntry {
@@ -22,4 +25,27 @@ impl BonsaiTagMappingEntry {
             tag_name,
         }
     }
+}
+
+#[facet::facet]
+#[async_trait]
+/// Facet trait representing Bonsai Changeset to Git Tag mapping
+pub trait BonsaiTagMapping: Send + Sync {
+    /// The repository for which this mapping has been created
+    fn repo_id(&self) -> RepositoryId;
+
+    /// Fetch the changeset id correponding to the tag name in the
+    /// given repo, if one exists
+    async fn get_changeset_by_tag_name(&self, tag_name: String) -> Result<Option<ChangesetId>>;
+
+    /// Fetch the tag names corresponding to the input changeset id
+    /// for the given repo. Note that there can potentially be multiple
+    /// tags that map to the same changeset
+    async fn get_tag_names_by_changeset(
+        &self,
+        changeset_id: ChangesetId,
+    ) -> Result<Option<Vec<String>>>;
+
+    /// Add new tag name to bonsai changeset mappings
+    async fn add_mappings(&self, entries: Vec<BonsaiTagMappingEntry>) -> Result<()>;
 }
--- eden/mononoke/bonsai_tag_mapping/src/sql.rs
+++ eden/mononoke/bonsai_tag_mapping/src/sql.rs
@@ -5,10 +5,47 @@
  * GNU General Public License version 2.
  */
 
+use ::sql_ext::mononoke_queries;
+use anyhow::Context;
+use anyhow::Result;
+use async_trait::async_trait;
+use mononoke_types::ChangesetId;
 use mononoke_types::RepositoryId;
 use sql_construct::SqlConstruct;
 use sql_construct::SqlConstructFromMetadataDatabaseConfig;
 use sql_ext::SqlConnections;
+
+use super::BonsaiTagMapping;
+use super::BonsaiTagMappingEntry;
+
+mononoke_queries! {
+    write AddBonsaiTagMapping(values: (
+        repo_id: RepositoryId,
+        tag_name: String,
+        changeset_id: ChangesetId,
+    )) {
+        insert_or_ignore,
+        "{insert_or_ignore} INTO bonsai_tag_mapping (repo_id, tag_name, changeset_id) VALUES {values}"
+    }
+
+    read SelectMappingByChangeset(
+        repo_id: RepositoryId,
+        changeset_id: ChangesetId
+    ) -> (String, ChangesetId) {
+        "SELECT tag_name, changeset_id
+         FROM bonsai_tag_mapping
+         WHERE repo_id = {repo_id} AND changeset_id = {changeset_id}"
+    }
+
+    read SelectMappingByTagName(
+        repo_id: RepositoryId,
+        tag_name: String,
+    ) -> (String, ChangesetId) {
+        "SELECT tag_name, changeset_id
+         FROM bonsai_tag_mapping
+         WHERE repo_id = {repo_id} AND tag_name = {tag_name}"
+    }
+}
 
 pub struct SqlBonsaiTagMapping {
     connections: SqlConnections,
@@ -38,5 +75,79 @@ impl SqlBonsaiTagMappingBuilder {
             connections: self.connections,
             repo_id,
         }
+    }
+}
+
+#[async_trait]
+impl BonsaiTagMapping for SqlBonsaiTagMapping {
+    fn repo_id(&self) -> RepositoryId {
+        self.repo_id
+    }
+
+    async fn get_changeset_by_tag_name(&self, tag_name: String) -> Result<Option<ChangesetId>> {
+        let results = SelectMappingByTagName::query(
+            &self.connections.read_connection,
+            &self.repo_id,
+            &tag_name,
+        )
+        .await
+        .with_context(|| {
+            format!(
+                "Failure in fetching changeset for tag {} in repo {}",
+                tag_name, self.repo_id
+            )
+        })?;
+        // This should not happen but since this is new code, extra checks dont hurt.
+        if results.len() > 1 {
+            anyhow::bail!(
+                "Multiple changesets returned for tag {} in repo {}",
+                tag_name,
+                self.repo_id
+            )
+        }
+        Ok(results
+            .into_iter()
+            .next()
+            .map(|(_, changeset_id)| changeset_id))
+    }
+
+    async fn get_tag_names_by_changeset(
+        &self,
+        changeset_id: ChangesetId,
+    ) -> Result<Option<Vec<String>>> {
+        let results = SelectMappingByChangeset::query(
+            &self.connections.read_connection,
+            &self.repo_id,
+            &changeset_id,
+        )
+        .await
+        .with_context(|| {
+            format!(
+                "Failure in fetching tag for changeset {:?} in repo {}",
+                changeset_id, self.repo_id
+            )
+        })?;
+
+        let output = results
+            .into_iter()
+            .map(|(tag_name, _)| Some(tag_name))
+            .collect::<Option<_>>();
+        return Ok(output);
+    }
+
+    async fn add_mappings(&self, entries: Vec<BonsaiTagMappingEntry>) -> Result<()> {
+        let entries: Vec<_> = entries
+            .iter()
+            .map(|entry| (&self.repo_id, &entry.tag_name, &entry.changeset_id))
+            .collect();
+        AddBonsaiTagMapping::query(&self.connections.write_connection, entries.as_slice())
+            .await
+            .with_context(|| {
+                format!(
+                    "Failed to add mappings in repo {} for entries {:?}",
+                    self.repo_id, entries,
+                )
+            })?;
+        Ok(())
     }
 }
