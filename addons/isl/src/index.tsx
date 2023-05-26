28a830aaf657dd5f03509d03f5475a86292fe95e
Evan Krause <evangrayk@meta.com>
2023-05-26 10:55:04 -0700

    Don't include light theme variables in vscode
    Summary:
    vscode/webview-ui-toolkit includes dark theme variable values, but not light theme variable values. This means for the web ISL, we need to include the light theme values our selves (e.g. `--background: #fff`).
    
    This is wrong when rendered inside vscode, since vscode provides the theme variables itself. It's more than redundant, because it means we don't respect some light theme variables.
    
    We can fix this by extracting the light theme variable definitions and only including them in the `index.tsx` used to render the web ISL, and not include them in the vscode `islWebviewEntry.tsx`.
    
    Reviewed By: zzl0
    
    Differential Revision: D46227804
    
    fbshipit-source-id: 8a4424e0f764f14bd6bcad4eccd0e0ffae29b991

--- addons/isl/src/index.tsx
+++ addons/isl/src/index.tsx
@@ -9,6 +9,10 @@ import App from './App';
 import 'react';
 import ReactDOM from 'react-dom/client';
 
+// @vscode/webview-ui-toolkit doesn't ship with light theme variables,
+// we need to include them ourselves in non-vscode renders of <App />.
+import './themeLightVariables.css';
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
