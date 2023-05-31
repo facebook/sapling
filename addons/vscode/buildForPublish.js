afa3c5a58784ff79b91505dda88155a211a02bb4
Jun Wu <quark@meta.com>
2023-05-31 16:04:46 -0700

    vscode: update buildForPublish to work on Windows
    Summary:
    Windows does not support shell scripts.
    
    The code translation is mostly done by ChatGPT.
    
    Reviewed By: evangrayk
    
    Differential Revision: D46334170
    
    fbshipit-source-id: aeca554f364104c49d810a532a6bdf5a55def9ad

---
+++ addons/vscode/buildForPublish.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+function deleteFolderRecursive(pathToDelete) {
+  if (fs.existsSync(pathToDelete)) {
+    fs.readdirSync(pathToDelete).forEach((file, index) => {
+      const curPath = path.join(pathToDelete, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(pathToDelete);
+  }
+}
+
+async function build() {
+  try {
+    console.log('Building Extension');
+    let {stdout} = await exec('webpack --config extension.webpack.config.ts');
+    console.log(stdout);
+
+    console.log('Building Webview');
+    let webViewOutput = await exec('webpack --config webview.webpack.config.ts');
+    console.log(webViewOutput.stdout);
+    console.log('Build complete');
+  } catch (err) {
+    console.error(`exec error: ${err}`);
+  }
+}
+
+// Run production build for publishing to the vscode marketplace
+
+// We only want to publish open source builds, not internal ones.
+// Fail if we see facebook-only files in the repo.
+const internalPath = './facebook/README.facebook.md';
+if (fs.existsSync(internalPath)) {
+  console.error(
+    `${internalPath} exists. Make sure you only publish the vscode extension from the external repo.`,
+  );
+  process.exit(1);
+}
+
+process.env.NODE_ENV = 'production';
+console.log('Cleaning dist');
+deleteFolderRecursive('./dist');
+build();
