f76eb1f9c6cffefc303e5d4d49f8620f6581588c
Haitao Mei <haitaomei@meta.com>
2023-06-15 07:51:33 -0700

    new admin tool bulk unlink keys subcommand read keys from all files
    Summary:
    This diff allows the new admin tool to do the following for the new bulk unlking keys subcommand:
    * Read the key list from the files
    * Print all the keys extracted when running in dry-run mode
    * Report the progress
    * Fix the argument to take a boolean value
    
    Reviewed By: mitrandir77
    
    Differential Revision: D46725312
    
    fbshipit-source-id: c039b89e00a9ffbbbee41e20617f45543ef147ce

--- eden/mononoke/tools/admin/src/commands/blobstore_bulk_unlink.rs
+++ eden/mononoke/tools/admin/src/commands/blobstore_bulk_unlink.rs
@@ -6,11 +6,17 @@
  */
 
 use std::fs;
+use std::fs::metadata;
+use std::fs::File;
 use std::io;
+use std::io::BufRead;
 use std::io::Write;
+use std::path::Path;
 use std::path::PathBuf;
 
+use anyhow::bail;
 use anyhow::Result;
+use clap::ArgAction;
 use clap::Parser;
 use mononoke_app::MononokeApp;
 
@@ -21,17 +27,62 @@ pub struct CommandArgs {
     keys_dir: String,
 
     /// If we're dry running the command, print out the blobstore keys to be deleted
-    #[arg(short, long, default_value_t = true)]
+    #[arg(short, long, default_value_t = true, action = ArgAction::Set)]
     dry_run: bool,
 }
 
-async fn bulk_unlink_keys_in_file(path: PathBuf, dry_run: bool) -> Result<()> {
+fn read_lines<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(file_path)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+async fn unlink_the_key_from_blobstore(key: &str, dry_run: bool) -> Result<()> {
+    if dry_run {
+        writeln!(std::io::stdout(), "\tUnlink key: {}", key);
+        return Ok(());
+    }
+    bail!("unimplemented unlink {} from blobstore yet!", key)
+}
+
+async fn bulk_unlink_keys_in_file(
+    path: &PathBuf,
+    cur: usize,
+    total_file_count: usize,
+    dry_run: bool,
+) -> Result<()> {
+    let md = metadata(path.clone()).unwrap();
+    if !md.is_file() {
+        writeln!(
+            std::io::stdout(),
+            "Skip path: {} because it is not a file.",
+            path.display(),
+        )?;
+        return Ok(());
+    }
+
     writeln!(
         std::io::stdout(),
         "Processing keys in file (with dry-run={}): {}",
         dry_run,
         path.display()
     )?;
+
+    if let Ok(lines) = read_lines(path) {
+        for line in lines {
+            if let Ok(key) = line {
+                unlink_the_key_from_blobstore(&key, dry_run).await?;
+            }
+        }
+        writeln!(
+            std::io::stdout(),
+            "Progress: {:.3}%",
+            (cur + 1) as f32 * 100.0 / total_file_count as f32
+        )?;
+    }
+
     Ok(())
 }
 
@@ -51,8 +102,10 @@ pub async fn run(app: MononokeApp, args: CommandArgs) -> Result<()> {
     let entries = fs::read_dir(keys_dir)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
-    for entry in entries {
-        bulk_unlink_keys_in_file(entry, dry_run).await?;
+
+    let total_file_count = entries.len();
+    for (cur, entry) in entries.iter().enumerate() {
+        bulk_unlink_keys_in_file(entry, cur, total_file_count, dry_run).await?;
     }
 
     Ok(())
