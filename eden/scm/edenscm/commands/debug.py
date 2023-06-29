2619910ea1004fd957fd1d8c3d7c7ddc57c304e0
Alex Coleman <alex@statsig.com>
2023-06-29 13:15:38 -0700

    Allow config for custom ISL commit message template
    # Summary
    Regarding https://github.com/facebook/sapling/issues/632, allows a sapling config to be set which gives a default template for ISL title/description.
    
    Currently namespaced under `committemplate.isl` (with other committemplate configs), but could flip this to `isl.committemplate` (with other isl configs).
    
    Also could see this being set by a version controlled file in the repo to standardize by repo instead of sapling config (ex. `<root>/.sapling-isl-committemplate`), open to thoughts on that
    
    This is necessary because debugcommitmessage isn't a public command, so this could potentially be fixed by adding that to sapling CLI instead.
    
    # Test Plan
    Templated this commit using it, after running `sl config -l committemplate.isl "<title># Summary\n\n# Test Plan\n\n"`

--- eden/scm/edenscm/commands/debug.py
+++ eden/scm/edenscm/commands/debug.py
@@ -4322,3 +4322,36 @@ def debugcopytrace(ui, repo, *files, **opts) -> None:
     }
     ui.write(json.dumps(res))
     ui.write("\n")
+
+@command("debugcommitmessage", [], _("FORM"))
+def debugcommitmessage(ui, repo, *args):
+    form = None
+    if len(args) > 1:
+        raise error.Abort(_("provide at most one form"))
+    elif len(args) > 0:
+        form = args[0]
+
+    status = repo.status()
+    text = ""
+    user = None
+    date = None
+    extra = None
+
+    ctx = context.workingcommitctx(repo, status, text, user, date, extra)
+
+    editform = form or "commit.normal.normal"
+    extramsg = _("Leave message empty to abort commit.")
+
+    forms = [e for e in editform.split(".") if e]
+    forms.insert(0, "changeset")
+    while forms:
+        ref = ".".join(forms)
+        tmpl = repo.ui.config("committemplate", ref)
+        if tmpl:
+            committext = cmdutil.buildcommittemplate(repo, ctx, extramsg, ref)
+            break
+        forms.pop()
+    else:
+        committext = cmdutil.buildcommittext(repo, ctx, extramsg)
+
+    ui.status(committext)
