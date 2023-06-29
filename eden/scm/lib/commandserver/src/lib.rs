4540ea9ea7bdcea9b32c0aa18140a17f5ba832a0
Jun Wu <quark@meta.com>
2023-06-28 22:22:02 -0700

    commandserver: initial crate
    Summary: Initial crate for the commandserver.
    
    Reviewed By: sggutier
    
    Differential Revision: D47068008
    
    fbshipit-source-id: 3a6a88bc7f18139b7725b979e18e6a488017f93f

---
+++ eden/scm/lib/commandserver/src/lib.rs
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+//! Client-server with the ability to preload content server-side to reduce
+//! startup overhead.
