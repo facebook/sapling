13f34a7448ee831354b1b4336814ce41fdb000cc
Jun Wu <quark@meta.com>
2023-07-03 12:58:22 -0700

    stack: add ChunkSelectState
    Summary:
    The `ChunkSelectState` tracks the "chunk selection" state for a file of 2
    adjacent versions. It provides APIs to make line rendering, selection easier.
    It also supports free-form editing.
    
    Reviewed By: zzl0
    
    Differential Revision: D45882553
    
    fbshipit-source-id: 922a105532ab78fdcec0f590dcaae0d4064fdbc0

---
+++ addons/isl/src/stackEdit/__tests__/chunkSelectState.test.ts
@@ -0,0 +1,177 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {ChunkSelectState} from '../chunkSelectState';
+
+describe('ChunkSelectState', () => {
+  const a = 'aa\nbb\ncc\ndd\n';
+  const b = 'cc\ndd\nee\nff\n';
+
+  describe('fromText()', () => {
+    it('with none selected', () => {
+      const state = ChunkSelectState.fromText(a, b, false);
+      expect(renderLines(state)).toMatchObject([
+        '[ ] -  1    aa',
+        '[ ] -  2    bb',
+        '       3  1 cc',
+        '       4  2 dd',
+        '[ ] +     3 ee',
+        '[ ] +     4 ff',
+      ]);
+      expect(state.getSelectedText()).toBe(a);
+    });
+
+    it('with all changes selected', () => {
+      const state = ChunkSelectState.fromText(a, b, true);
+      expect(renderLines(state)).toMatchObject([
+        '[x] -  1    aa',
+        '[x] -  2    bb',
+        '       3  1 cc',
+        '       4  2 dd',
+        '[x] +     3 ee',
+        '[x] +     4 ff',
+      ]);
+      expect(state.getSelectedText()).toBe(b);
+    });
+
+    it('with free-form partial selection', () => {
+      const text = 'aa\ncc\ndd\nee\n';
+      const state = ChunkSelectState.fromText(a, b, text);
+      expect(renderLines(state)).toMatchObject([
+        '[ ] -  1    aa',
+        '[x] -  2    bb',
+        '       3  1 cc',
+        '       4  2 dd',
+        '[x] +     3 ee',
+        '[ ] +     4 ff',
+      ]);
+      expect(state.getSelectedText()).toBe(text);
+    });
+
+    it('with free-form extra deletions', () => {
+      const text = '';
+      const state = ChunkSelectState.fromText(a, b, text);
+      expect(renderLines(state)).toMatchObject([
+        '[x] -  1    aa',
+        '[x] -  2    bb',
+        '   !-  3    cc',
+        '   !-  4    dd',
+        '[ ] +     3 ee',
+        '[ ] +     4 ff',
+      ]);
+      expect(state.getSelectedText()).toBe(text);
+    });
+
+    it('with free-form extra insertions', () => {
+      const text = 'aa\ncc\n<insertion>\ndd\nee\n';
+      const state = ChunkSelectState.fromText(a, b, text);
+      expect(renderLines(state)).toMatchObject([
+        '[ ] -  1    aa',
+        '[x] -  2    bb',
+        '       3  1 cc',
+        '   !+       <insertion>',
+        '       4  2 dd',
+        '[x] +     3 ee',
+        '[ ] +     4 ff',
+      ]);
+      expect(state.getSelectedText()).toBe(text);
+    });
+
+    it('sorts changes, deletion is before insertion', () => {
+      const state = ChunkSelectState.fromText('aa\naa\n', 'bb\nbb\nbb\n', true);
+      expect(renderLines(state)).toMatchObject([
+        '[x] -  1    aa',
+        '[x] -  2    aa',
+        '[x] +     1 bb',
+        '[x] +     2 bb',
+        '[x] +     3 bb',
+      ]);
+    });
+  });
+
+  describe('setSelectedLines()', () => {
+    it('toggles deleted lines', () => {
+      let state = ChunkSelectState.fromText(a, b, false);
+      state = state.setSelectedLines([
+        [0, true],
+        [1, false],
+      ]);
+      expect(renderLines(state).slice(0, 2)).toMatchObject(['[x] -  1    aa', '[ ] -  2    bb']);
+      state = state.setSelectedLines([
+        [0, false],
+        [1, true],
+      ]);
+      expect(renderLines(state).slice(0, 2)).toMatchObject(['[ ] -  1    aa', '[x] -  2    bb']);
+    });
+
+    it('toggles added lines', () => {
+      let state = ChunkSelectState.fromText(a, b, false);
+      state = state.setSelectedLines([
+        [4, true],
+        [5, false],
+      ]);
+      expect(renderLines(state).slice(4, 6)).toMatchObject(['[x] +     3 ee', '[ ] +     4 ff']);
+      state = state.setSelectedLines([
+        [4, false],
+        [5, true],
+      ]);
+      expect(renderLines(state).slice(4, 6)).toMatchObject(['[ ] +     3 ee', '[x] +     4 ff']);
+    });
+
+    it('does nothing to other lines', () => {
+      const text = 'aa\ncc\n<insertion>\ndd\nee\n';
+      let state = ChunkSelectState.fromText(a, b, text);
+      state = state.setSelectedLines([
+        [2, false],
+        [3, false],
+        [4, true],
+      ]);
+      expect(renderLines(state)).toMatchObject([
+        '[ ] -  1    aa',
+        '[x] -  2    bb',
+        '       3  1 cc',
+        '   !+       <insertion>',
+        '       4  2 dd',
+        '[x] +     3 ee',
+        '[ ] +     4 ff',
+      ]);
+      expect(state.getSelectedText()).toBe(text);
+    });
+  });
+
+  describe('setSelectedText()', () => {
+    it('round-trips with getSelectedText()', () => {
+      let state = ChunkSelectState.fromText(a, b, false);
+      const lines = ['aa\n', 'bb\n', 'cc\n', 'ii\n', 'dd\n', 'ee\n', 'ff\n'];
+      // eslint-disable-next-line no-bitwise
+      const end = 1 << lines.length;
+      for (let bits = 0; bits < end; ++bits) {
+        // eslint-disable-next-line no-bitwise
+        const text = lines.map((l, i) => ((bits & (1 << i)) === 0 ? '' : l)).join('');
+        state = state.setSelectedText(text);
+        expect(state.getSelectedText()).toBe(text);
+      }
+    });
+  });
+});
+
+/** Visualize line selections in ASCII. */
+function renderLines(state: ChunkSelectState): string[] {
+  return state.getLines().map(l => {
+    const checkbox = {true: '[x]', false: '[ ]', null: '   '}[`${l.selected}`];
+    const aLine = l.aLine === null ? '' : l.aLine.toString();
+    const bLine = l.bLine === null ? '' : l.bLine.toString();
+    return [
+      checkbox.padStart(3),
+      l.sign.padStart(2),
+      aLine.padStart(3),
+      bLine.padStart(3),
+      ' ',
+      l.data.trimEnd(),
+    ].join('');
+  });
+}
---
+++ addons/isl/src/stackEdit/chunkSelectState.ts
@@ -0,0 +1,347 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {FlattenLine, LineIdx} from '../linelog';
+import type {RecordOf} from 'immutable';
+
+import {FileStackState} from './fileStackState';
+import {List, Record} from 'immutable';
+import {cached} from 'shared/LRU';
+import {SelfUpdate} from 'shared/immutableExt';
+
+/**
+ * Represents selections of changes between 2 texts (`a` and `b`), optionally
+ * the selection text can be edited free-form.
+ *
+ * Based on a 3-rev `FlattenLine`s representation:
+ * - Rev 0: The `a` side (not editable by this `ChunkSelectState`).
+ * - Rev 1: The selection (editable by this `ChunkSelectState`).
+ * - Rev 2: The `b` side (not editable by this `ChunkSelectState`).
+ *
+ * Support operations:
+ * - getLines: Obtain lines for rendering. See `SelectLine` for details.
+ * - setSelectedLines: Set line selections. Only added or removed lines can be selected.
+ * - getSelectedText: Obtain selected or edited text.
+ * - setSelectedText: Set edited text. Useful for free-form editing.
+ *
+ * With free-from editing, there are two "special" cases:
+ *
+ *   revs    | meaning
+ *   -----------------------------
+ *   {0,1,2} | unchanged
+ *   {0,1}   | deletion; not selected
+ *   {0,2}   | special: extra deletion [*]
+ *   {0}     | deletion; selected
+ *   {1,2}   | insertion; selected
+ *   {1}     | special: extra insertion
+ *   {2}     | insertion; not selected
+ *
+ *   [*]: LineLog.flatten() never produces {0,2}. It generates {0} and {2} as
+ *   separate lines. But `getLines()` post-processing might produce {0,2} lines.
+ *
+ * The callsite might want to treat the "special: extra insertion" like an
+ * insertion with a different highlighting.
+ *
+ * This state does not care about collapsing unmodified lines, "context lines"
+ * or "code expansion" states. They do not affect editing state, and belong to
+ * the UI components.
+ */
+export class ChunkSelectState extends SelfUpdate<ChunkSelectRecord> {
+  /**
+   * Initialize ChunkSelectState from text `a` and `b`.
+   *
+   * If `selected` is `true`, then all changes are selected, selection result is `b`.
+   * If `selected` is `false`, none of the changes are selected, selection result is `a`.
+   * If `selected` is a string, then the selections are "inferred" from the string.
+   */
+  static fromText(a: string, b: string, selected: boolean | string): ChunkSelectState {
+    const mid = selected === true ? b : selected === false ? a : selected;
+    const fileStack = new FileStackState([a, mid, b]);
+    const lines = fileStack.convertToFlattenLines().map(l => toLineBits(l));
+    return new ChunkSelectState(ChunkSelectRecord({a, b, lines}));
+  }
+
+  /** Get the text of the "a" side. */
+  get a(): string {
+    return this.inner.a;
+  }
+
+  /** Get the text of the "b" side. */
+  get b(): string {
+    return this.inner.b;
+  }
+
+  /**
+   * Get the `SelectLine`s. Useful for rendering the lines.
+   * See `SelectLine` for details.
+   */
+  @cached()
+  getLines(): Readonly<SelectLine[]> {
+    let nextALine = 1;
+    let nextBLine = 1;
+    let nextSelLine = 1;
+    let result: SelectLine[] = [];
+
+    // Modified lines to sort before appending to `result`.
+    let buffer: SelectLine[] = [];
+    const pushBuffer = () => {
+      buffer.sort((a, b) => {
+        // In this order: Deletion, insertion.
+        const aOrder = bitsToOrder[a.bits];
+        const bOrder = bitsToOrder[b.bits];
+        if (aOrder !== bOrder) {
+          return aOrder - bOrder;
+        }
+        return a.rawIndex - b.rawIndex;
+      });
+      // Merge "selected deletion + deselected insertion" with
+      // the same content into "unselectable deletion".
+      let nextDelIndex = 0;
+      buffer.forEach((line, i) => {
+        if (line.bits === 0b001 /* deselected insertion */) {
+          // Try to find the matched "selected deletion" line.
+          while (nextDelIndex < i) {
+            const otherLine = buffer[nextDelIndex];
+            if (otherLine.data === line.data && otherLine.bits === 0b100) {
+              // Change otherLine to "unselectable deletion",
+              // then remove this line.
+              otherLine.bits = 0b101;
+              otherLine.selected = null;
+              otherLine.sign = '!-';
+              line.bits = 0;
+              break;
+            }
+            nextDelIndex += 1;
+          }
+        }
+      });
+      buffer = buffer.filter(line => line.bits !== 0);
+      result = result.concat(buffer);
+      buffer = [];
+    };
+
+    this.inner.lines.forEach((line, rawIndex) => {
+      const bits = line.bits;
+      let sign: Sign = '';
+      let selected: boolean | null = null;
+      let aLine: LineIdx | null = null;
+      let bLine: LineIdx | null = null;
+      let selLine: LineIdx | null = null;
+      // eslint-disable-next-line no-bitwise
+      if (bits >> 2 !== 0) {
+        aLine = nextALine;
+        nextALine += 1;
+      }
+      // eslint-disable-next-line no-bitwise
+      if ((bits & 1) !== 0) {
+        bLine = nextBLine;
+        nextBLine += 1;
+      }
+      // eslint-disable-next-line no-bitwise
+      if ((bits & 2) !== 0) {
+        selLine = nextSelLine;
+        nextSelLine += 1;
+      }
+      switch (bits) {
+        case 0b001:
+          sign = '+';
+          selected = false;
+          break;
+        case 0b010:
+          sign = '!+';
+          break;
+        case 0b011:
+          sign = '+';
+          selected = true;
+          break;
+        case 0b100:
+          sign = '-';
+          selected = true;
+          break;
+        case 0b101:
+          sign = '!-';
+          break;
+        case 0b110:
+          sign = '-';
+          selected = false;
+          break;
+        case 0b111:
+          break;
+      }
+      const selectLine: SelectLine = {
+        rawIndex,
+        aLine,
+        bLine,
+        selLine,
+        sign,
+        selected,
+        bits,
+        data: line.data,
+      };
+      if (sign === '') {
+        pushBuffer();
+        result.push(selectLine);
+      } else {
+        buffer.push(selectLine);
+      }
+    });
+    pushBuffer();
+    return result;
+  }
+
+  /**
+   * Get the text of selected lines. It is the editing result.
+   *
+   * Note: passing `getSelectedText` to `fromText` does not maintain the selection
+   * state. For example, from an empty text to `1\n1\n1\n`. The user might select
+   * the 1st, 2nd, or 3rd line. That's 3 different ways of selections with the same
+   * `getSelectedText` output.
+   */
+  @cached()
+  getSelectedText(): string {
+    return (
+      this.inner.lines
+        // eslint-disable-next-line no-bitwise
+        .filter(l => (l.bits & 0b010) !== 0)
+        .map(l => l.data)
+        .join('')
+    );
+  }
+
+  /**
+   * Select or deselect lines.
+   *
+   * `selects` is a list of tuples. Each tuple has a `rawIndex` and whether that
+   * line is selected or not.
+   * Note if a line is deleted (sign is '-'), then selected means deleting that line.
+   *
+   * Note all lines are editable. Lines that are not editable are silently ignored.
+   */
+  setSelectedLines(selects: Array<[LineIdx, boolean]>): ChunkSelectState {
+    const newLines = this.inner.lines.withMutations(mutLines => {
+      let lines = mutLines;
+      selects.forEach(([idx, selected]) => {
+        const line = lines.get(idx);
+        if (line === undefined) {
+          return;
+        }
+        const {bits} = line;
+        // eslint-disable-next-line no-bitwise
+        const bits101 = bits & 0b101;
+        if (bits101 === 0 || bits101 === 0b101) {
+          // Not changed in v0 and v2 - ignore editing.
+          return;
+        }
+        const oldSelected: boolean =
+          // eslint-disable-next-line no-bitwise
+          bits101 === 0b100 ? (bits & 0b010) === 0 : (bits & 0b010) !== 0;
+        if (oldSelected !== selected) {
+          // Update selection by toggling (xor) rev 1.
+          // eslint-disable-next-line no-bitwise
+          const newBits = bits ^ 0b010;
+          const newLine = line.set('bits', newBits as Bits);
+          lines = lines.set(idx, newLine);
+        }
+      });
+      return lines;
+    });
+    return new ChunkSelectState(this.inner.set('lines', newLines));
+  }
+
+  /**
+   * Free-form edit selected text.
+   *
+   * Runs analysis to mark lines as selected. Consider only calling this once
+   * when switching from free-form editing to line selections.
+   */
+  setSelectedText(text: string): ChunkSelectState {
+    const {a, b} = this.inner;
+    return ChunkSelectState.fromText(a, b, text);
+  }
+
+  /**
+   * The constructor is for internal use only.
+   * Use static methods to construct `ChunkSelectState`.
+   */
+  constructor(record: ChunkSelectRecord) {
+    super(record);
+  }
+}
+
+/** A line and its position on both sides, and selection state. */
+export type SelectLine = {
+  /** Index in the `lines` internal state. Starting from 0. */
+  rawIndex: LineIdx;
+
+  /** Line index on the `a` side for rendering, or `null` if the line does not exist on the `a` side. */
+  aLine: LineIdx | null;
+
+  /** Line index on the `b` side for rendering, or `null` if the line does not exist on the `b` side. */
+  bLine: LineIdx | null;
+
+  /** Line index for "selected" lines, for rendering, or `null` if the line is not in the "selected" side. */
+  selLine: LineIdx | null;
+
+  /** See `Sign` for description. */
+  sign: Sign;
+
+  /**
+   * Whether the line is selected or not. Only used when sign is '-' or '+'.
+   * Note if a line is deleted (sign is '-'), then selected means deleting that line.
+   */
+  selected: boolean | null;
+
+  /** Line selection bits. */
+  bits: Bits;
+
+  /** Content of the line. */
+  data: string;
+};
+
+/** '-': deletion; '+': insertion; '': unchanged; '!+', '!-': forced insertion or deletion, not selectable. */
+type Sign = '' | '+' | '-' | '!+' | '!-';
+
+type ChunkSelectProps = {
+  a: string;
+  b: string;
+  lines: List<LineBitsRecord>;
+};
+const ChunkSelectRecord = Record<ChunkSelectProps>({a: '', b: '', lines: List()});
+type ChunkSelectRecord = RecordOf<ChunkSelectProps>;
+
+type Bits = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+
+/** Similar to `FlattenLine` but compress Set<Rev> into 3 bits for easier access. */
+type LineBitsProps = {
+  /** Line content including `\n` EOL. */
+  data: string;
+  /** Bitset. 0b100: left side; 0b001: right side; 0b010: editing / selecting text. */
+  bits: Bits;
+};
+const LineBitsRecord = Record<LineBitsProps>({data: '', bits: 0});
+type LineBitsRecord = RecordOf<LineBitsProps>;
+
+/**
+ * Converts `FlattenLine` to `LineBits`.
+ * `line.revs` (`ImSet<Rev>`) is converted to 3 bits. 0b100: rev 0; 0b010: rev 1; 0b001: rev 2
+ */
+function toLineBits(line: FlattenLine): LineBitsRecord {
+  // eslint-disable-next-line no-bitwise
+  const bits = line.revs.reduce((acc, rev) => acc | (4 >> rev), 0);
+  return LineBitsRecord({data: line.data, bits: bits as Bits});
+}
+
+const bitsToOrder = [
+  0, // 0b000: unused
+  2, // 0b001: normal insertion
+  2, // 0b010: unselectable insertion
+  2, // 0b011: normal insertion
+  1, // 0b100: normal deletion
+  1, // 0b101: unselectable deletion
+  1, // 0b110: normal deletion
+  0, // 0b111: normal
+];
