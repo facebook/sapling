dabb147d23ae311e74f0b72fad083911a5835725
Jun Wu <quark@meta.com>
2023-07-03 16:33:52 -0700

    select: add UseUncommittedSelection for selection of files in wdir()
    Summary:
    Build upon the `PartialSelection`, this handles extra complexities around
    the pending working copy changes - the `wdir()` commit. The main complexity
    is around file content loading in `getChunkSelect`.
    
    Initially I didn't use async function. To "return" result from the callback to
    `onMessageOfType`, as a comparison the initial code looks like:
    
        getChunkSelect(path: RepoRelativePath): Promise<ChunkSelectState> {
          const fileSelection = this.selection.inner.fileMap.get(path);
          if (fileSelection instanceof ChunkSelectState) {
            return Promise.resolve(fileSelection);
          }
    
          const cache = UseUncommittedSelection.fileContentCache;
          const maybeReadFromCache = (): ChunkSelectState | null => {
            ...
          };
    
          return cache.asyncLoadingLock.synchronize(() => {
            const chunkState = maybeReadFromCache();
            if (chunkState !== null) {
              // The file was loaded (by a previous async load attempt).
              return Promise.resolve(chunkState);
            }
    
            // Not found in cache. Load the file via the server.
            return new Promise((resolve, reject) => {
              // Use assumeTracked so the server returns the file even if it's untracked.
              // Note this export has side effects of populating other tracked files in cache.
              const revs = 'wdir()';
              const disposable = clientToServerAPI.onMessageOfType('exportedStack', event => {
                if (event.revs !== revs) {
                  // Unrelated stack. Ignore.
                  return;
                }
                if (event.error) {
                  reject(event.error);
                  disposable.dispose();
                } else if (event.stack.some(c => !c.requested && c.node !== cache.wdirHash)) {
                  // The wdir hash has changed. Fail the load.
                  // The exported stack usually has a non-requested commit that is the parent
                  // of the requested "wdir()". For an empty repo there is no such commit.
                  reject(t('Working copy has changed'));
                  disposable.dispose();
                } else {
                  // Update cache.
                  event.stack.forEach(commit => {
                    if (commit.requested) {
                      mergeObjectToMap(commit.files, cache.files);
                    } else {
                      mergeObjectToMap(commit.relevantFiles, cache.parentFiles);
                    }
                  });
                  const chunkState = maybeReadFromCache();
                  if (chunkState === null) {
                    if (event.assumeTracked.includes(path)) {
                      // We explicitly requested the file, but the server does not provide
                      // it somehow.
                      reject(t('Unable to get file content unexpectedly'));
                      disposable.dispose();
                    }
                  } else {
                    resolve(chunkState);
                  }
                }
              });
    
              // Kick off the loading.
              clientToServerAPI.postMessage({type: 'exportStack', revs, assumeTracked: [path]});
            });
          });
        }
    
    Noticeable changes of the new async-function version:
    - Removed 2-level indentation: `Promise` and `onMessageOfType`.
    - No need to call `dispose()` manually.
    
    Reviewed By: evangrayk
    
    Differential Revision: D46150701
    
    fbshipit-source-id: de9406723383b16e86e7ef826df20bc9d8667ac7

--- addons/isl/src/partialSelection.ts
+++ addons/isl/src/partialSelection.ts
@@ -6,10 +6,19 @@
  */
 
 import type {RepoRelativePath} from './types';
+import type {SetterOrUpdater} from 'recoil';
+import type {Hash, RepoPath} from 'shared/types/common';
+import type {ExportFile} from 'shared/types/stack';
 
+import clientToServerAPI from './ClientToServerAPI';
+import {t} from './i18n';
+import {treeWithPreviews, uncommittedChangesWithPreviews} from './previews';
+import {clearOnCwdChange} from './recoilUtils';
 import {ChunkSelectState} from './stackEdit/chunkSelectState';
 import {assert} from './utils';
 import Immutable from 'immutable';
+import {useRecoilState, useRecoilValue, atom} from 'recoil';
+import {RateLimiter} from 'shared/RateLimiter';
 import {SelfUpdate} from 'shared/immutableExt';
 
 type SingleFileSelection =
@@ -147,5 +156,245 @@ export class PartialSelection extends SelfUpdate<PartialSelectionRecord> {
     return this.inner.fileMap
       .keySeq()
       .some(p => typeof this.getSimplifiedSelection(p) !== 'boolean');
+  }
+}
+
+/** Default: select all files. */
+const defaultUncommittedPartialSelection = PartialSelection.empty({
+  selectByDefault: true,
+});
+
+/** PartialSelection for `wdir()`. See `UseUncommittedSelection` for the public API. */
+const uncommittedSelection = atom<PartialSelection>({
+  key: 'uncommittedSelection',
+  default: defaultUncommittedPartialSelection,
+  effects: [clearOnCwdChange()],
+});
+
+/** PartialSelection for `wdir()` that handles loading file contents. */
+export class UseUncommittedSelection {
+  // Persist across `UseUncommittedSelection` life cycles.
+  // Not an atom so updating the cache does not trigger re-render.
+  static fileContentCache: {
+    wdirHash: Hash;
+    files: Map<RepoPath, ExportFile | null>;
+    parentFiles: Map<RepoPath, ExportFile | null>;
+    asyncLoadingLock: RateLimiter;
+  } = {
+    wdirHash: '',
+    files: new Map(),
+    parentFiles: new Map(),
+    asyncLoadingLock: new RateLimiter(1),
+  };
+
+  constructor(
+    public selection: PartialSelection,
+    private setSelection: SetterOrUpdater<PartialSelection>,
+    wdirHash: Hash,
+    private getPaths: () => Array<RepoRelativePath>,
+  ) {
+    const cache = UseUncommittedSelection.fileContentCache;
+    if (wdirHash !== cache.wdirHash) {
+      // Invalidate existing cache when `.` changes.
+      cache.files.clear();
+      cache.parentFiles.clear();
+      cache.wdirHash = wdirHash;
+    }
+  }
+
+  /** Explicitly select a file. */
+  select(path: RepoRelativePath) {
+    const newSelection = this.selection.select(path);
+    this.setSelection(newSelection);
+  }
+
+  selectAll() {
+    const newSelection = defaultUncommittedPartialSelection;
+    this.setSelection(newSelection);
+  }
+
+  /** Explicitly deselect a file. Also drops the related file content cache. */
+  deselect(path: RepoRelativePath) {
+    const cache = UseUncommittedSelection.fileContentCache;
+    cache.files.delete(path);
+    const newSelection = this.selection.deselect(path);
+    this.setSelection(newSelection);
+  }
+
+  deselectAll() {
+    let newSelection = this.selection;
+    this.getPaths().forEach(path => (newSelection = newSelection.deselect(path)));
+    this.setSelection(newSelection);
+  }
+
+  /** Restore to the default selection (select all). */
+  clear() {
+    const newSelection = this.selection.clear();
+    this.setSelection(newSelection);
+  }
+
+  /**
+   * Get the chunk select state for the given path.
+   * The file content will be loaded on demand.
+   */
+  getChunkSelect(path: RepoRelativePath): ChunkSelectState | Promise<ChunkSelectState> {
+    const fileSelection = this.selection.inner.fileMap.get(path);
+    if (fileSelection instanceof ChunkSelectState) {
+      return fileSelection;
+    }
+
+    const cache = UseUncommittedSelection.fileContentCache;
+    const maybeReadFromCache = (): ChunkSelectState | null => {
+      const file = cache.files.get(path);
+      if (file === undefined) {
+        return null;
+      }
+      const parentPath = file?.copyFrom ?? path;
+      const parentFile = cache.parentFiles.get(parentPath);
+      if (parentFile?.dataBase85 || file?.dataBase85) {
+        throw new Error(t('Cannot edit non-utf8 file'));
+      }
+      const a = parentFile?.data ?? '';
+      const b = file?.data ?? '';
+      const selected = this.getSelection(path);
+      if (selected instanceof ChunkSelectState) {
+        return selected;
+      }
+      const newSelection = this.selection.startChunkSelect(path, a, b, selected);
+      this.setSelection(newSelection);
+      const newSelected = newSelection.getSelection(path);
+      assert(
+        newSelected instanceof ChunkSelectState,
+        'startChunkSelect() should provide ChunkSelectState',
+      );
+      return newSelected;
+    };
+
+    return cache.asyncLoadingLock.enqueueRun(async () => {
+      const chunkState = maybeReadFromCache();
+      if (chunkState !== null) {
+        return chunkState;
+      }
+
+      // Not found in cache. Need to (re)load the file via the server.
+
+      const revs = 'wdir()';
+      // Setup event listener before sending the request.
+      const iter = clientToServerAPI.iterateMessageOfType('exportedStack');
+      // Explicitly ask for the file via assumeTracked. Note this also provides contents
+      // of other tracked files.
+      clientToServerAPI.postMessage({type: 'exportStack', revs, assumeTracked: [path]});
+      for await (const event of iter) {
+        if (event.revs !== revs) {
+          // Ignore unrelated response.
+          continue;
+        }
+        if (event.error) {
+          throw new Error(event.error);
+        }
+        if (event.stack.some(c => !c.requested && c.node !== cache.wdirHash)) {
+          // The wdirHash has changed. Fail the load.
+          // The exported stack usually has a non-requested commit that is the parent of
+          // the requested "wdir()", which is the "." commit that should match `wdirHash`.
+          // Note: for an empty repo there is no such non-requested commit exported so
+          // we skip the check in that case.
+          throw new Error(t('Working copy has changed'));
+        }
+
+        // Update cache.
+        event.stack.forEach(commit => {
+          if (commit.requested) {
+            mergeObjectToMap(commit.files, cache.files);
+          } else {
+            mergeObjectToMap(commit.relevantFiles, cache.parentFiles);
+          }
+        });
+
+        // Try read from cache again.
+        const chunkState = maybeReadFromCache();
+        if (chunkState === null) {
+          if (event.assumeTracked.includes(path)) {
+            // We explicitly requested the file, but the server does not provide
+            // it somehow.
+            break;
+          } else {
+            // It's possible that there are multiple export requests.
+            // This one does not provide the file we want, continue checking other responses.
+            continue;
+          }
+        } else {
+          return chunkState;
+        }
+      }
+
+      // Handles the `break` above. Tells tsc that we don't return undefined.
+      throw new Error(t('Unable to get file content unexpectedly'));
+    });
+  }
+
+  /** Edit chunk selection for a file. */
+  editChunkSelect(
+    path: RepoRelativePath,
+    newValue: ((chunkState: ChunkSelectState) => ChunkSelectState) | ChunkSelectState,
+  ) {
+    const newSelection = this.selection.editChunkSelect(path, newValue);
+    this.setSelection(newSelection);
+  }
+
+  /**
+   * Return true if a file is selected (default), false if deselected,
+   * or a string with the edited content.
+   */
+  getSelection(path: RepoRelativePath): SingleFileSelection {
+    return this.selection.getSelection(path);
+  }
+
+  isFullyOrPartiallySelected(path: RepoRelativePath): boolean {
+    return this.selection.isFullyOrPartiallySelected(path);
+  }
+
+  isPartiallySelected(path: RepoRelativePath): boolean {
+    return this.selection.isPartiallySelected(path);
+  }
+
+  isFullySelected(path: RepoRelativePath): boolean {
+    return this.selection.isFullySelected(path);
+  }
+
+  isDeselected(path: RepoRelativePath): boolean {
+    return this.selection.isDeselected(path);
+  }
+
+  isEverythingSelected(): boolean {
+    return this.selection.isEverythingSelected(this.getPaths);
+  }
+
+  isNothingSelected(): boolean {
+    return this.selection.isNothingSelected(this.getPaths);
+  }
+
+  hasChunkSelection(): boolean {
+    return this.selection.hasChunkSelection();
+  }
+}
+
+/** Get the uncommitted selection state. */
+export function useUncommittedSelection() {
+  const [selection, setSelection] = useRecoilState(uncommittedSelection);
+  const uncommittedChanges = useRecoilValue(uncommittedChangesWithPreviews);
+  const tree = useRecoilValue(treeWithPreviews);
+  const wdirHash = tree.headCommit?.hash ?? '';
+  const getPaths = () => uncommittedChanges.map(c => c.path);
+
+  return new UseUncommittedSelection(selection, setSelection, wdirHash, getPaths);
+}
+
+function mergeObjectToMap<V>(obj: {[path: string]: V} | undefined, map: Map<string, V>) {
+  if (obj === undefined) {
+    return;
+  }
+  for (const k in obj) {
+    const v = obj[k];
+    map.set(k, v);
   }
 }
