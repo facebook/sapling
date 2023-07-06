9a4b2e465db82b9441f12a5bba328ad075e75cee
Evan Krause <evangrayk@meta.com>
2023-07-06 15:47:24 -0700

    Dropdown button component
    Summary:
    Add a dropdown button component. This behaves like a button with a selection dropdown chevron next to it. Changing the selection via the chevron's dropdown menu changes the button text. Then, clicking the button runs that particular action.
    
    This lets us have buttons which have built-in options / multiple valid choices. For example, we can use this for changing the submit button to Submit Draft for a draft PR/Diff.
    
    This component is implemented as a controlled component, so the caller tracks the current selection and update the state outside the component. Typically, we'll want to persist your button choice or otherwise use an atom for it, so the responsibility is in the caller.
    
    Reviewed By: quark-zju
    
    Differential Revision: D43557610
    
    fbshipit-source-id: 557418a2cf0a42255355c65fa6a21f439ac129c3

---
+++ addons/isl/src/VSCodeButtonDropdown.tsx
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {ButtonAppearance} from '@vscode/webview-ui-toolkit';
+import type {ReactNode} from 'react';
+
+import {VSCodeButton} from '@vscode/webview-ui-toolkit/react';
+
+import './VSCodeButtonDropdown.css';
+
+export function VSCodeButtonDropdown<T extends {label: ReactNode; id: string}>({
+  options,
+  appearance,
+  onClick,
+  selected,
+  onChangeSelected,
+  buttonDisabled,
+  pickerDisabled,
+  icon,
+}: {
+  options: ReadonlyArray<T>;
+  appearance: Exclude<ButtonAppearance, 'icon'>; // icon-type buttons don't have a natrual spot for the dropdown
+  onClick: (selected: T) => unknown;
+  selected: T;
+  onChangeSelected: (newSelected: T) => unknown;
+  buttonDisabled?: boolean;
+  pickerDisabled?: boolean;
+  /** Icon to place in the button */
+  icon?: React.ReactNode;
+}) {
+  const selectedOption = options.find(opt => opt.id === selected.id) ?? options[0];
+  return (
+    <div className="vscode-button-dropdown">
+      <VSCodeButton
+        appearance={appearance}
+        onClick={buttonDisabled ? undefined : () => onClick(selected)}
+        disabled={buttonDisabled}>
+        {icon ?? null} {selectedOption.label}
+      </VSCodeButton>
+      <select
+        disabled={pickerDisabled}
+        onClick={e => e.stopPropagation()}
+        onChange={event => {
+          const matching = options.find(opt => opt.id === (event.target.value as T['id']));
+          if (matching != null) {
+            onChangeSelected(matching);
+          }
+        }}>
+        {options.map(option => (
+          <option key={option.id} value={option.id} selected={option.id === selected.id}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
