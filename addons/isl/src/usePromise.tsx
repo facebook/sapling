69675f7b8c3de1d1afa25c6ac04d58274505ead9
Jun Wu <quark@meta.com>
2023-07-17 15:32:39 -0700

    async: update usePromise approach
    Summary:
    Update `usePromise` to work better with the `<SuspenseBoundary />`.
    Namely, we now `throw` directly so the callsite no longer need manual
    error handling.
    
    See D46198649 for the previous approach, which tries to use fancy
    `useDeferredValue` but has an unsolved problem: `useState`s inside async
    rendering functions might not work.
    
    Reviewed By: evangrayk
    
    Differential Revision: D47248520
    
    fbshipit-source-id: ec1ad46c214d1bcaa15de41d5870a1b84ce70a13

--- addons/isl/src/usePromise.tsx
+++ addons/isl/src/usePromise.tsx
@@ -5,42 +5,69 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import {isPromise} from 'shared/utils';
+
 /**
- * Return the promise result (either 'ok' or 'error'), or "suspend" React's rendering.
- * The callsite should have `<Suspense>` in a parent component to support suspension.
+ * Return the promise result, raise the promise error, or "suspend" React's rendering.
+ * The callsite should have `<Suspense>` and `<ErrorBoundary>` in a parent component
+ * to support suspension and error handling, or use `<SuspenseBoundary>`.
  *
- * Note this function translates error to a regular value without throwing.
- * This avoids the need of another "ErrorBoundary". The callsite could rethrow
- * if it needs to work with an "ErrorBoundary".
- *
- * Be aware that the `promise` should not be created inside `<Suspense>`, because
- * those components do not have states if `<Suspense>` renders `fallback`. Instead,
- * the `promise` should be passed from the parent component that renders `<Suspense>`.
+ * Be aware that the function that produces `promise` should returning
+ * `Promise.resolve(data)`, and instead return `data` directly. This is becasuse
+ * Javascript does not provide a way to test if a Promise is resolved without async.
+ * So the Promise will be treated as "pending" temporarily, rendering the Suspense
+ * fallback. The actual Suspense children will lose their states because the
+ * fallback replaces them. See the `maybePromise` below.
  *
  * Example:
  *
  * ```
  * // Parent component with `<Suspense>`.
  * function Container(props: {path: string}) {
+ *   return <SuspenseBoundary><Inner /></SuspenseBoundary>;
+ * }
+ *
+ * // Child component using `usePromise`.
+ * function Inner() {
+ *   const data = usePromise(maybePromise());
+ *   return <Data data={data} />;
+ * }
+ *
+ * function maybePromise(): Data | Promise<Data> {
+ *   if (isDataReady()) {
+ *     // Do not return Promise.resolve(data). That loses <Inner /> state.
+ *     return data;
+ *   }
+ *   ...
+ * }
+ * ```
+ *
+ * Alternatively, the promise can be passed from a more "stable" stateful
+ * parent component that keeps the promise object unchanged when <Suspense>
+ * switches between fallback to non-fallback:
+ *
+ * ```
+ * // Parent component with `<Suspense>`.
+ * function Container(props: {path: string}) {
  *   const loader = useLoader();
  *   const promise = loader.load(props.path);
- *   return <Suspense fallback={<Loading/>}><Inner promise={promise} /></Suspense>
+ *   return <SuspenseBoundary><Inner promise={promise} /></SuspenseBoundary>;
  * }
  *
  * // Child component using `usePromise`.
  * function Inner(props: {promise: ...}) {
- *   const [status, data] = usePromise(props.promise);
- *   if (status === 'error') {
- *     return <ErrorMessage error={data} />;
- *   } else {
- *     return <Data data={data} />;
- *   }
+ *   // The promise is from the parent of <Suspense />.
+ *   const data = usePromise(promise);
+ *   return <Data data={data} />;
  * }
  * ```
  *
  * See also https://github.com/reactjs/react.dev/blob/3364c93feb358a7d1ac2e8d8b0468c3e32214062/src/content/reference/react/Suspense.md?plain=1#L141
  */
-export function usePromise<T>(promise: PromiseExt<T>): ['ok', T] | ['error', Error] {
+export function usePromise<T>(promise: T | PromiseExt<T>): T {
+  if (!isPromise(promise)) {
+    return promise;
+  }
   const status = promise.usePromiseStatus;
   if (status === undefined) {
     promise.usePromiseStatus = 'pending';
@@ -57,8 +84,10 @@ export function usePromise<T>(promise: PromiseExt<T>): ['ok', T] | ['error', Err
     throw promise;
   } else if (status === 'pending') {
     throw promise;
+  } else if (status[0] === 'ok') {
+    return status[1];
   } else {
-    return status;
+    throw status[1];
   }
 }
 
