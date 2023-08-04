d600275442a043e3b29c78ce2368e6e14b390050
Zhaolong Zhu <zhaolong@meta.com>
2023-08-04 15:20:26 -0700

    smerge_bench: add a basic smerge_bench
    Summary: Added a basic smerge_bench, and tried it on existing `Merge3Text` class
    
    Reviewed By: quark-zju
    
    Differential Revision: D48013057
    
    fbshipit-source-id: e869f8f4886c6289168f256bec4fff6af6f4b380

--- eden/scm/contrib/smerge_benchmark.py
+++ eden/scm/contrib/smerge_benchmark.py
@@ -3,23 +3,64 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
+from dataclasses import dataclass
+
+from edenscm import commands, registrar
+from edenscm.simplemerge import Merge3Text, wordmergemode
+
+
+cmdtable = {}
+command = registrar.command(cmdtable)
+
 
 class SmartMerge3Text:
     def __init__(self, basetext, atext, btext, wordmerge=False):
         pass
 
 
-"""
-todo:
-- 'sl clone' a Git repo
-- find merge commits
-- for each merge commit c
-    - find p1 (master), p2 (local) commits and base (gca of p1 and p2)
-    - for each updated file in p2
-        - if it is in all the 3 commits
-            - check merge_algorithm(f_p1, f_p2, f_base) == f_c
-"""
+@dataclass
+class BenchStats:
+    changed_files: int = 0
+    unresolved_files: int = 0
+    unmatched_files: int = 0
 
 
-def debugmergebenchmark():
-    pass
+@command("smerge_bench", commands.dryrunopts)
+def smerge_bench(ui, repo, **opts):
+    merge_commits = repo.dageval(lambda: merges(all()))  # noqa
+    ui.write(f"len(merge_commits)={len(merge_commits)}\n")
+
+    bench_stats = BenchStats()
+    for i, merge_commit in enumerate(merge_commits, start=1):
+        mergectx = repo[merge_commit]
+        p1, p2 = mergectx.p1(), mergectx.p2()
+        base = repo.dageval(lambda: gcaone([p1.node(), p2.node()]))
+        basectx = repo[base]
+
+        for filepath in mergectx.files():
+            if all(filepath in ctx for ctx in [basectx, p1, p2]):
+                merge_file(repo, p1, p2, basectx, mergectx, filepath, bench_stats)
+
+        if i % 100 == 0:
+            ui.write(f"{i} {bench_stats}\n")
+    ui.write(f"{bench_stats}\n")
+
+
+def merge_file(repo, dstctx, srcctx, basectx, mergectx, filepath, bench_stats):
+    srctext = srcctx[filepath].data()
+    dsttext = dstctx[filepath].data()
+    basetext = basectx[filepath].data()
+
+    if srctext == dsttext:
+        return
+
+    bench_stats.changed_files += 1
+
+    m3 = Merge3Text(basetext, dsttext, srctext, wordmerge=wordmergemode.disabled)
+    # merge_lines() has side effect setting conflictscount
+    mergedtext = b"".join(m3.merge_lines())
+
+    if m3.conflictscount:
+        bench_stats.unresolved_files += 1
+    elif mergedtext != mergectx[filepath].data():
+        bench_stats.unmatched_files += 1
