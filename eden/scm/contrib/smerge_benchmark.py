1ac141b1bcd4f251ff5e6d62b651d658a5577709
Zhaolong Zhu <zhaolong@meta.com>
2023-08-04 15:20:26 -0700

    smerge_benchmark: support multiple m3 merge algorithms
    Summary:
    run muliple m3 merge algorithms and collect the execution time
    durations
    BenchStats(changed_files=8979, unresolved_files=35, unmatched_files=1)
    Execution time: 13.36 seconds
    
    ============== SmartMerge3Text ==============
    100 BenchStats(changed_files=281, unresolved_files=0, unmatched_files=0)
    200 BenchStats(changed_files=1116, unresolved_files=1, unmatched_files=0)
    300 BenchStats(changed_files=8047, unresolved_files=30, unmatched_files=1)
    400 BenchStats(changed_files=8391, unresolved_files=30, unmatched_files=1)
    500 BenchStats(changed_files=8582, unresolved_files=31, unmatched_files=2)
    BenchStats(changed_files=8979, unresolved_files=31, unmatched_files=2)
    Execution time: 13.19 seconds
    ```
    
    Reviewed By: quark-zju
    
    Differential Revision: D48068127
    
    fbshipit-source-id: e736659fcdff68ee355cce196fddddb3a15793aa

--- eden/scm/contrib/smerge_benchmark.py
+++ eden/scm/contrib/smerge_benchmark.py
@@ -3,6 +3,7 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
+import time
 from dataclasses import dataclass
 
 from edenscm import commands, registrar
@@ -13,9 +14,10 @@ cmdtable = {}
 command = registrar.command(cmdtable)
 
 
-class SmartMerge3Text:
-    def __init__(self, basetext, atext, btext, wordmerge=False):
-        pass
+class SmartMerge3Text(Merge3Text):
+    def __init__(self, basetext, atext, btext, wordmerge=wordmergemode.ondemand):
+        # a dummy implementation by enabling wordmerge in `Merge3Text`
+        Merge3Text.__init__(self, basetext, atext, btext, wordmerge=wordmerge)
 
 
 @dataclass
@@ -30,23 +32,32 @@ def smerge_bench(ui, repo, **opts):
     merge_commits = repo.dageval(lambda: merges(all()))  # noqa
     ui.write(f"len(merge_commits)={len(merge_commits)}\n")
 
-    bench_stats = BenchStats()
-    for i, merge_commit in enumerate(merge_commits, start=1):
-        mergectx = repo[merge_commit]
-        p1, p2 = mergectx.p1(), mergectx.p2()
-        base = repo.dageval(lambda: gcaone([p1.node(), p2.node()]))
-        basectx = repo[base]
+    for m3merger in [Merge3Text, SmartMerge3Text]:
+        ui.write(f"\n============== {m3merger.__name__} ==============\n")
+        start = time.time()
+        bench_stats = BenchStats()
+        for i, merge_commit in enumerate(merge_commits, start=1):
+            mergectx = repo[merge_commit]
+            p1, p2 = mergectx.p1(), mergectx.p2()
+            base = repo.dageval(lambda: gcaone([p1.node(), p2.node()]))
+            basectx = repo[base]
 
-        for filepath in mergectx.files():
-            if all(filepath in ctx for ctx in [basectx, p1, p2]):
-                merge_file(repo, p1, p2, basectx, mergectx, filepath, bench_stats)
+            for filepath in mergectx.files():
+                if all(filepath in ctx for ctx in [basectx, p1, p2]):
+                    merge_file(
+                        repo, p1, p2, basectx, mergectx, filepath, m3merger, bench_stats
+                    )
 
-        if i % 100 == 0:
-            ui.write(f"{i} {bench_stats}\n")
-    ui.write(f"{bench_stats}\n")
+            if i % 100 == 0:
+                ui.write(f"{i} {bench_stats}\n")
+
+        ui.write(f"\nSummary: {bench_stats}\n")
+        ui.write(f"Execution time: {time.time() - start:.2f} seconds\n")
 
 
-def merge_file(repo, dstctx, srcctx, basectx, mergectx, filepath, bench_stats):
+def merge_file(
+    repo, dstctx, srcctx, basectx, mergectx, filepath, m3merger, bench_stats
+):
     srctext = srcctx[filepath].data()
     dsttext = dstctx[filepath].data()
     basetext = basectx[filepath].data()
@@ -56,7 +67,7 @@ def merge_file(repo, dstctx, srcctx, basectx, mergectx, filepath, bench_stats):
 
     bench_stats.changed_files += 1
 
-    m3 = Merge3Text(basetext, dsttext, srctext, wordmerge=wordmergemode.disabled)
+    m3 = m3merger(basetext, dsttext, srctext)
     # merge_lines() has side effect setting conflictscount
     mergedtext = b"".join(m3.merge_lines())
 
