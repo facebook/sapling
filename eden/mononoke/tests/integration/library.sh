9a2630ab9e1cb3d41eb2a5255dc220c0f2df91c6
Mark Juggurnauth-Thomas <mbthomas@meta.com>
2023-08-15 02:36:15 -0700

    deletion: extract backfill bonsai blob mapping from the deletion tool
    Summary: This part of the tool uses sharding, which means we pull in shard manager.  We don't want that in the admin tool, as linking to shard manager prevents us from testing with network disabled.
    
    Differential Revision: D48262204
    
    fbshipit-source-id: e80430e6b64bf94712b6b47a079c1891a9ddcea1

--- eden/mononoke/tests/integration/library.sh
+++ eden/mononoke/tests/integration/library.sh
@@ -422,6 +422,13 @@ function mononoke_testtool {
     --mononoke-config-path "$TESTTMP"/mononoke-config "$@"
 }
 
+function mononoke_backfill_bonsai_blob_mapping {
+  GLOG_minloglevel=5 "$MONONOKE_BACKFILL_BONSAI_BLOB_MAPPING" \
+    "${CACHE_ARGS[@]}" \
+    "${COMMON_ARGS[@]}" \
+    --mononoke-config-path "$TESTTMP"/mononoke-config "$@"
+}
+
 function mononoke_admin_source_target {
   local source_repo_id=$1
   shift
