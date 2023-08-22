2b7fa9c51447d964a359b9c96286f00329253cb3
Saul Gutierrez <sggutier@meta.com>
2023-08-22 13:22:39 -0700

    rage: report whether symlinks are enabled on Windows
    Summary: Info about whether symlinks are enabled or not on EdenFS could be useful when debugging issues with mount points, which this diff enables.
    
    Reviewed By: xavierd
    
    Differential Revision: D48566818
    
    fbshipit-source-id: 181c85e78c355d65cfbcb2ca440174e8a8f8a0aa

--- eden/fs/cli/config.py
+++ eden/fs/cli/config.py
@@ -509,6 +509,9 @@ class EdenInstance(AbstractEdenInstance):
             ]
         )
 
+        if sys.platform == "win32":
+            ret["symlinks_enabled"] = checkout_config.enable_windows_symlinks
+
         if snapshot is not None:
             ret["checked_out_revision"] = snapshot.last_checkout_hash
             ret["working_copy_parent"] = snapshot.working_copy_parent
