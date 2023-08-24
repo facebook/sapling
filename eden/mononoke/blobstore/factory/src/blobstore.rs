70e03b16a41fab08d7eecd2039fd0087576efbf1
Clara Rull <clararull@meta.com>
2023-08-24 08:03:34 -0700

    Implement an AWS S3 client on the S3 blob crate
    Summary:
    As part of the Mononoke Oprational Shadow for SEV0 scenarios, we need to make Mononoke capable of interacting with AWS. This cmmit is a first iteration of the implementation. It gets the credentials for an AWS account fron `AWSume` and hooks up to the forward proxy in order to allow writes from prod.
    
    At some point we will modify this to work from within AWS as well, i. e. not using forward proxy, or credentials provided from Cloud Auth
    
    This isn't yet called by anything.
    
    Differential Revision: D48232507
    
    fbshipit-source-id: 8a72b461224d2b0ddd22d2a99950abd01302fcba

--- eden/mononoke/blobstore/factory/src/blobstore.rs
+++ eden/mononoke/blobstore/factory/src/blobstore.rs
@@ -54,6 +54,10 @@ use multiplexedblob_wal::WalMultiplexedBlobstore;
 use packblob::PackBlob;
 use packblob::PackOptions;
 use readonlyblob::ReadOnlyBlobstore;
+#[cfg(fbcode_build)]
+use s3blob::s3client::S3ClientPool;
+#[cfg(fbcode_build)]
+use s3blob::store::S3Blob;
 use samplingblob::ComponentSamplingHandler;
 use samplingblob::SamplingBlobstoreUnlinkOps;
 use scuba_ext::MononokeScubaSampleBuilder;
@@ -549,21 +553,22 @@ pub fn make_blobstore_unlink_ops<'a>(
             } => {
                 #[cfg(fbcode_build)]
                 {
-                    ::s3blob::S3Blob::new(
+                    let client_backend = S3ClientPool::new(
                         fb,
-                        bucket,
                         keychain_group,
                         secret_name,
                         region_name,
                         endpoint,
-                        blobstore_options.put_behaviour,
                         logger,
                         num_concurrent_operations,
                     )
-                    .watched(logger)
-                    .await
-                    .context(ErrorKind::StateOpen)
-                    .map(|store| Arc::new(store) as Arc<dyn BlobstoreUnlinkOps>)?
+                    .await?;
+
+                    S3Blob::new(bucket, client_backend, blobstore_options.put_behaviour)
+                        .watched(logger)
+                        .await
+                        .context(ErrorKind::StateOpen)
+                        .map(|store| Arc::new(store) as Arc<dyn BlobstoreUnlinkOps>)?
                 }
                 #[cfg(not(fbcode_build))]
                 {
