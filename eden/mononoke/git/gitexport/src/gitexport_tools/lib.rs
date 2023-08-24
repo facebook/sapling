095fe93718391b7ea4c4dfcf58d2f8649ce95654
Gustavo Galvao Avena <gustavoavena@meta.com>
2023-08-24 02:33:49 -0700

    Rewrite stripped commits to temp repo
    Summary:
    ## This Stack
    See D48563980.
    
    ## This diff
    Implement the code that actually writes the relevant commits to the temp repo containing only changes to the exported paths.
    
    The functions being implemented expect a list of topologically sorted changesets and a hashmap mapping changeset ids to their parents, which I'm referring to as "partial graph information".
    
    As I mentioned on D48563980, currently the data being create works only for a single path and expects a linear history with no branches (e.g. the example in D48563980).
    
    ## Next steps
    Not strictly in that order:
    - Start looking on how I can export the temporary repo to a git repo. IIUC RajivTS is a good POC for this?
    - Look into `list_file_history` to see how I can properly get a proper "history graph", i.e. the commit graph containing only the relevant commits for the exported paths. IIUC YousefSalama is the best POC to help me with this.
    - Update the integration tests to use the tool and test what I already implemented.
    
    Differential Revision: D48558621
    
    fbshipit-source-id: 8aba4b8927e9c277b0338f8ae3e66aaad1a2c355

--- eden/mononoke/git/gitexport/src/gitexport_tools/lib.rs
+++ eden/mononoke/git/gitexport/src/gitexport_tools/lib.rs
@@ -6,15 +6,27 @@
  */
 
 mod partial_commit_graph;
-
 use std::collections::HashMap;
+use std::str::FromStr;
+use std::sync::Arc;
 
+use anyhow::Error;
+use blobstore::Loadable;
+use borrowed::borrowed;
+use cloned::cloned;
+use commit_transformation::rewrite_commit;
+use commit_transformation::upload_commits;
+use commit_transformation::MultiMover;
+use futures::stream::TryStreamExt;
+use futures::stream::{self};
+use mononoke_api::BookmarkKey;
 use mononoke_api::ChangesetContext;
 use mononoke_api::MononokeError;
 use mononoke_api::RepoContext;
 use mononoke_types::BonsaiChangeset;
 use mononoke_types::ChangesetId;
 use mononoke_types::MPath;
+use repo_blobstore::RepoBlobstoreArc;
 
 pub use crate::partial_commit_graph::build_partial_commit_graph_for_export;
 use crate::partial_commit_graph::ChangesetParents;
@@ -23,24 +35,134 @@ use crate::partial_commit_graph::ChangesetParents;
 /// copies in a target mononoke repository containing only changes that
 /// were made on the given paths.
 pub async fn rewrite_partial_changesets(
-    _source_repo_ctx: RepoContext,
-    _changesets: Vec<ChangesetContext>,
-    _changeset_parents: &ChangesetParents,
+    source_repo_ctx: RepoContext,
+    changesets: Vec<ChangesetContext>,
+    changeset_parents: &ChangesetParents,
     // Repo that will hold the partial changesets that will be exported to git
-    _target_repo_ctx: RepoContext,
-    _export_paths: Vec<MPath>,
+    target_repo_ctx: RepoContext,
+    export_paths: Vec<MPath>,
 ) -> Result<(), MononokeError> {
-    todo!();
+    println!("export_paths: {:#?}", &export_paths);
+    println!("changeset_parents: {:#?}", &changeset_parents);
+
+    let multi_mover: MultiMover = Arc::new(move |source_path: &MPath| {
+        let should_export = export_paths.iter().any(|p| p.is_prefix_of(source_path));
+
+        if !should_export {
+            println!("Path {:#?} will NOT be exported.", &source_path);
+            return Ok(vec![]);
+        }
+
+        println!("Path {:#?} will be exported.", &source_path);
+        Ok(vec![source_path.clone()])
+    });
+
+    let cs_results: Vec<Result<ChangesetContext, MononokeError>> =
+        changesets.into_iter().map(Ok).collect::<Vec<_>>();
+
+    let (new_bonsai_changesets, _) = stream::iter(cs_results)
+        .try_fold(
+            (Vec::new(), HashMap::new()),
+            |(mut new_bonsai_changesets, remapped_parents), changeset| {
+                borrowed!(source_repo_ctx);
+                cloned!(multi_mover);
+                async move {
+                    let (new_bcs, remapped_parents) = create_bonsai_for_new_repo(
+                        source_repo_ctx,
+                        multi_mover,
+                        changeset_parents,
+                        remapped_parents,
+                        changeset,
+                    )
+                    .await?;
+                    new_bonsai_changesets.push(new_bcs);
+                    Ok((new_bonsai_changesets, remapped_parents))
+                }
+            },
+        )
+        .await?;
+
+    println!("new_bonsai_changesets: {:#?}", &new_bonsai_changesets);
+
+    let head_cs_id = new_bonsai_changesets
+        .last()
+        .ok_or(Error::msg("No changesets were moved"))?
+        .get_changeset_id();
+
+    upload_commits(
+        source_repo_ctx.ctx(),
+        new_bonsai_changesets,
+        source_repo_ctx.repo(),
+        target_repo_ctx.repo(),
+    )
+    .await?;
+
+    // Set master bookmark to point to the latest changeset
+    target_repo_ctx
+        .create_bookmark(&BookmarkKey::from_str("master")?, head_cs_id, None)
+        .await?;
+
+    println!("Finished copying all changesets!");
+    Ok(())
 }
 
 /// Given a changeset and a set of paths being exported, build the
 /// BonsaiChangeset containing only the changes to those paths.
-async fn _create_bonsai_for_new_repo(
-    _source_repo_ctx: RepoContext,
-    _export_paths: Vec<MPath>,
-    _changeset_parents: &ChangesetParents,
-    _remapped_parents: &mut HashMap<ChangesetId, ChangesetId>,
-    _changeset_ctx: ChangesetContext,
-) -> Result<BonsaiChangeset, MononokeError> {
-    todo!();
+async fn create_bonsai_for_new_repo(
+    source_repo_ctx: &RepoContext,
+    multi_mover: MultiMover,
+    changeset_parents: &ChangesetParents,
+    mut remapped_parents: HashMap<ChangesetId, ChangesetId>,
+    changeset_ctx: ChangesetContext,
+) -> Result<(BonsaiChangeset, HashMap<ChangesetId, ChangesetId>), MononokeError> {
+    println!(
+        "Rewriting changeset: {:#?} | {:#?}",
+        &changeset_ctx.id(),
+        &changeset_ctx.message().await?
+    );
+
+    let blobstore = source_repo_ctx.blob_repo().repo_blobstore_arc();
+    let bcs: BonsaiChangeset = changeset_ctx
+        .id()
+        .load(source_repo_ctx.ctx(), &blobstore)
+        .await
+        .map_err(MononokeError::from)?;
+
+    // The BonsaiChangeset was built with the full graph, so the parents are
+    // not necessarily part of the provided changesets. So we first need to
+    // update the parents to reference the parents from the partial graph.
+    //
+    // These values will be used to read the `remapped_parents` map in
+    // `rewrite_commit` and get the correct changeset id for the parents in
+    // the new bonsai changeset.
+    let orig_parent_ids: Vec<ChangesetId> = changeset_parents
+        .get(&bcs.get_changeset_id())
+        .cloned()
+        .unwrap_or_default();
+    let mut mut_bcs = bcs.into_mut();
+    mut_bcs.parents = orig_parent_ids.clone();
+
+    let rewritten_bcs_mut = rewrite_commit(
+        source_repo_ctx.ctx(),
+        mut_bcs,
+        &remapped_parents,
+        multi_mover,
+        source_repo_ctx.repo(),
+        None,
+        Default::default(),
+    )
+    .await?
+    // This shouldn't happen because every changeset provided is modifying
+    // at least one of the exported files.
+    .ok_or(Error::msg(
+        "Commit wasn't rewritten because it had no signficant changes",
+    ))?;
+
+    let rewritten_bcs = rewritten_bcs_mut.freeze()?;
+
+    // Update the remapped_parents map so that children of this commit use
+    // its new id when moved.
+    remapped_parents.insert(changeset_ctx.id(), rewritten_bcs.get_changeset_id());
+
+    Ok((rewritten_bcs, remapped_parents))
 }
