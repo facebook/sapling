f651a48e7684fe98d51be922507dd88d64bb883b
Zhaolong Zhu <zhaolong@meta.com>
2023-08-30 08:04:00 -0700

    simplemerge: update license comment of test file
    Summary: update license of the test file
    
    Reviewed By: muirdm
    
    Differential Revision: D48783638
    
    fbshipit-source-id: 41f3d9a34c6ff0c8d250736f576714da6a9cac27

--- eden/scm/tests/test-simplemerge.py
+++ eden/scm/tests/test-simplemerge.py
@@ -1,4 +1,8 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Portions Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
 # Copyright (C) 2004, 2005 Canonical Ltd
 #
 # This program is free software; you can redistribute it and/or modify
