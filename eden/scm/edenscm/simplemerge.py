c6a729266b0c3eb1ee305b4b9226740cdd3299a3
Zhaolong Zhu <zhaolong@meta.com>
2023-08-31 14:26:57 -0700

    simplemerge: make minimize() a free function
    Summary:
    Written by Martin von Zweigbergk at https://phab.mercurial-scm.org/D11980
    
    Original commit message:
    
    IMO, Merge3Text should be about merging text without knowing about
    conflict markers. "Conflict minimization" is about making conflict
    markers smaller, so it should be moved out. This patch does that. I'll
    refactor it next.
    
    Reviewed By: quark-zju
    
    Differential Revision: D48866238
    
    fbshipit-source-id: 232c9f2e4cdcd519446672e4c0f9805d40a807d4

--- eden/scm/edenscm/simplemerge.py
+++ eden/scm/edenscm/simplemerge.py
@@ -273,48 +273,6 @@ class Merge3Text:
                 ia = aend
                 ib = bend
 
-    def minimize(self, merge_groups):
-        """Trim conflict regions of lines where A and B sides match.
-
-        Lines where both A and B have made the same changes at the beginning
-        or the end of each merge region are eliminated from the conflict
-        region and are instead considered the same.
-        """
-        for what, group_lines in merge_groups:
-            if what != "conflict":
-                yield what, group_lines
-                continue
-            base_lines, a_lines, b_lines = group_lines
-            alen = len(a_lines)
-            blen = len(b_lines)
-
-            # find matches at the front
-            ii = 0
-            while ii < alen and ii < blen and a_lines[ii] == b_lines[ii]:
-                ii += 1
-            startmatches = ii
-
-            # find matches at the end
-            ii = 0
-            while ii < alen and ii < blen and a_lines[-ii - 1] == b_lines[-ii - 1]:
-                ii += 1
-            endmatches = ii
-
-            if startmatches > 0:
-                yield "same", a_lines[:startmatches]
-
-            yield (
-                "conflict",
-                (
-                    base_lines,
-                    a_lines[startmatches : alen - endmatches],
-                    b_lines[startmatches : blen - endmatches],
-                ),
-            )
-
-            if endmatches > 0:
-                yield "same", a_lines[alen - endmatches :]
-
     def find_sync_regions(self):
         """Return a list of sync regions, where both descendants match the base.
 
@@ -395,6 +353,49 @@ class Merge3Text:
         return sl
 
 
+def _minimize(merge_groups):
+    """Trim conflict regions of lines where A and B sides match.
+
+    Lines where both A and B have made the same changes at the beginning
+    or the end of each merge region are eliminated from the conflict
+    region and are instead considered the same.
+    """
+    for what, group_lines in merge_groups:
+        if what != "conflict":
+            yield what, group_lines
+            continue
+        base_lines, a_lines, b_lines = group_lines
+        alen = len(a_lines)
+        blen = len(b_lines)
+
+        # find matches at the front
+        ii = 0
+        while ii < alen and ii < blen and a_lines[ii] == b_lines[ii]:
+            ii += 1
+        startmatches = ii
+
+        # find matches at the end
+        ii = 0
+        while ii < alen and ii < blen and a_lines[-ii - 1] == b_lines[-ii - 1]:
+            ii += 1
+        endmatches = ii
+
+        if startmatches > 0:
+            yield "same", a_lines[:startmatches]
+
+        yield (
+            "conflict",
+            (
+                base_lines,
+                a_lines[startmatches : alen - endmatches],
+                b_lines[startmatches : blen - endmatches],
+            ),
+        )
+
+        if endmatches > 0:
+            yield "same", a_lines[alen - endmatches :]
+
+
 def render_minimized(
     m3,
     name_a=None,
@@ -412,7 +413,7 @@ def render_minimized(
         end_marker = end_marker + b" " + name_b
 
     merge_groups = m3.merge_groups(automerge=True)
-    merge_groups = m3.minimize(merge_groups)
+    merge_groups = _minimize(merge_groups)
     lines = []
     for what, group_lines in merge_groups:
         if what == "conflict":
