03a1231e6d347fed83e5c0374e8df5444dc200fc
Evan Krause <evangrayk@meta.com>
2023-09-06 13:10:12 -0700

    Add tests for shelve list
    Summary: Add a set of tests for features for the shelved changes list
    
    Reviewed By: quark-zju
    
    Differential Revision: D49018146
    
    fbshipit-source-id: 82ddf739d9893d0d609decce48603080c5e7c33b

--- addons/isl/src/__tests__/Shelve.test.tsx
+++ addons/isl/src/__tests__/Shelve.test.tsx
@@ -6,6 +6,8 @@
  */
 
 import App from '../App';
+import {__TEST__} from '../Tooltip';
+import {ignoreRTL} from '../testQueries';
 import {
   resetTestMessages,
   expectMessageSentToServer,
@@ -14,10 +16,12 @@ import {
   simulateUncommittedChangedFiles,
   closeCommitInfoSidebar,
   simulateRepoConnected,
+  simulateMessageFromServer,
 } from '../testUtils';
-import {fireEvent, render, screen} from '@testing-library/react';
+import {fireEvent, render, screen, waitFor, within} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import {act} from 'react-dom/test-utils';
+import {ComparisonType} from 'shared/Comparison';
 import {unwrap} from 'shared/utils';
 
 jest.mock('../MessageBus');
@@ -50,6 +54,10 @@ describe('Shelve', () => {
         ],
       });
     });
+  });
+
+  afterEach(() => {
+    __TEST__.resetMemoizedTooltipContainer();
   });
 
   describe('Shelve button', () => {
@@ -131,6 +139,187 @@ describe('Shelve', () => {
       const shelveButton = screen.getByText('Shelve');
       expect(shelveButton).toBeInTheDocument();
       expect(shelveButton).toBeDisabled();
+    });
+  });
+
+  describe('Shelved changes list', () => {
+    const SHELVES = [
+      {
+        hash: 'aaa',
+        name: 'my shelve',
+        date: new Date(),
+        description: 'shelved on commit b',
+        filesSample: [{path: 'src/shelvedfile.js', status: 'M' as const}],
+        totalFileCount: 1,
+      },
+    ];
+
+    it('shows shelved changes list tooltip', () => {
+      fireEvent.click(screen.getByTestId('shelved-changes-button'));
+      expect(screen.getByText('Shelved Changes')).toBeInTheDocument();
+
+      expectMessageSentToServer({
+        type: 'fetchShelvedChanges',
+      });
+    });
+
+    it('renders errors from fetching shelved changes', async () => {
+      fireEvent.click(screen.getByTestId('shelved-changes-button'));
+      act(() => {
+        simulateMessageFromServer({
+          type: 'fetchedShelvedChanges',
+          shelvedChanges: {
+            error: new Error('failed to fetch shelved changes'),
+          },
+        });
+      });
+
+      await waitFor(() => {
+        expect(
+          within(screen.getByTestId('shelved-changes-dropdown')).getByText(
+            'Could not fetch shelved changes',
+          ),
+        ).toBeInTheDocument();
+      });
+    });
+
+    it('renders empty state', async () => {
+      fireEvent.click(screen.getByTestId('shelved-changes-button'));
+      act(() => {
+        simulateMessageFromServer({
+          type: 'fetchedShelvedChanges',
+          shelvedChanges: {
+            value: [],
+          },
+        });
+      });
+
+      await waitFor(() => {
+        expect(
+          within(screen.getByTestId('shelved-changes-dropdown')).getByText('No shelved changes'),
+        ).toBeInTheDocument();
+      });
+    });
+
+    describe('with shelved change rendered', () => {
+      beforeEach(async () => {
+        fireEvent.click(screen.getByTestId('shelved-changes-button'));
+        act(() => {
+          simulateMessageFromServer({
+            type: 'fetchedShelvedChanges',
+            shelvedChanges: {
+              value: SHELVES,
+            },
+          });
+        });
+
+        await waitFor(() => {
+          expect(
+            within(screen.getByTestId('shelved-changes-dropdown')).getByText('my shelve'),
+          ).toBeInTheDocument();
+        });
+      });
+
+      it('renders shelved changes', () => {
+        expect(
+          within(screen.getByTestId('shelved-changes-dropdown')).getByText(
+            ignoreRTL('shelvedfile.js'),
+          ),
+        ).toBeInTheDocument();
+      });
+
+      it('runs unshelve', () => {
+        fireEvent.click(screen.getByText('Unshelve'));
+
+        expectMessageSentToServer({
+          type: 'runOperation',
+          operation: expect.objectContaining({
+            args: ['unshelve', '--name', 'my shelve'],
+          }),
+        });
+      });
+
+      it('dismisses tooltip while running unshelve', () => {
+        fireEvent.click(screen.getByText('Unshelve'));
+
+        expect(screen.queryByTestId('shelved-changes-dropdown')).not.toBeInTheDocument();
+      });
+
+      it('can open comparison view for the shelve', async () => {
+        fireEvent.click(
+          within(screen.getByTestId('shelved-changes-dropdown')).getByText('View Changes'),
+        );
+
+        await waitFor(() => {
+          expectMessageSentToServer({
+            type: 'requestComparison',
+            comparison: {
+              type: ComparisonType.Committed,
+              hash: 'aaa',
+            },
+          });
+        });
+      });
+
+      it('refetches shelves when reopening dropdown', async () => {
+        expect(
+          within(screen.getByTestId('shelved-changes-dropdown')).getByText('my shelve'),
+        ).toBeInTheDocument();
+
+        resetTestMessages();
+
+        act(() => {
+          userEvent.type(document.body, '{Escape}');
+        });
+
+        // reopen the dropdown
+        fireEvent.click(screen.getByTestId('shelved-changes-button'));
+        expectMessageSentToServer({
+          type: 'fetchShelvedChanges',
+        });
+
+        // send a new set of changes
+        const NEW_SHELVES = [
+          {
+            hash: 'a',
+            name: 'my new shelve',
+            date: new Date(),
+            description: 'shelved on commit b',
+            filesSample: [{path: 'src/shelvedfile.js', status: 'M' as const}],
+            totalFileCount: 1,
+          },
+        ];
+        act(() => {
+          simulateMessageFromServer({
+            type: 'fetchedShelvedChanges',
+            shelvedChanges: {
+              value: NEW_SHELVES,
+            },
+          });
+        });
+
+        // make sure we get the new name and not the old one
+        expect(
+          within(screen.getByTestId('shelved-changes-dropdown')).queryByText('my shelve'),
+        ).not.toBeInTheDocument();
+        await waitFor(() => {
+          expect(
+            within(screen.getByTestId('shelved-changes-dropdown')).getByText('my new shelve'),
+          ).toBeInTheDocument();
+        });
+      });
+
+      it('shows optimistic state for unshelve', () => {
+        expect(
+          within(screen.getByTestId('commit-tree-root')).queryByText(ignoreRTL('shelvedfile.js')),
+        ).not.toBeInTheDocument();
+
+        fireEvent.click(screen.getByText('Unshelve'));
+
+        expect(
+          within(screen.getByTestId('commit-tree-root')).getByText(ignoreRTL('shelvedfile.js')),
+        ).toBeInTheDocument();
+      });
     });
   });
 });
