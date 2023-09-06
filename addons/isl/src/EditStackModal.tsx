cbc925c605d12b111937345cfc4249b4b82de444
Jun Wu <quark@meta.com>
2023-09-05 18:16:59 -0700

    EditStackModal: add a tab bar
    Summary:
    The tab bar selects between "Commits" and "Files". The "Files" tab does not do
    much yet.
    
    Reviewed By: evangrayk
    
    Differential Revision: D48859619
    
    fbshipit-source-id: 7485cdea40a275cdf1d6d51ebc33251d7e372f58

--- addons/isl/src/EditStackModal.tsx
+++ addons/isl/src/EditStackModal.tsx
@@ -9,7 +9,10 @@ import {FlexRow, ScrollY} from './ComponentUtils';
 import {Modal} from './Modal';
 import {StackEditConfirmButtons} from './StackEditConfirmButtons';
 import {StackEditSubTree} from './StackEditSubTree';
+import {T} from './i18n';
 import {loadingStackState, editingStackHashes} from './stackEditState';
+import {VSCodePanels, VSCodePanelTab, VSCodePanelView} from '@vscode/webview-ui-toolkit/react';
+import {useState} from 'react';
 import {useRecoilValue} from 'recoil';
 
 /// Show a <Modal /> when editing a stack.
@@ -25,11 +28,28 @@ export function MaybeEditStackModal() {
 
 /// A <Modal /> for stack editing UI.
 function LoadedEditStackModal() {
+  type Tab = 'commits' | 'files';
+  const [activeTab, setActiveTab] = useState<Tab>('commits');
   return (
     <Modal>
-      <ScrollY maxSize="70vh">
-        <StackEditSubTree />
-      </ScrollY>
+      <VSCodePanels
+        onChange={e => {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const tab: Tab | undefined = (e.target as any)?.activetab?.id?.replace('tab-', '');
+          tab && setActiveTab(tab);
+        }}>
+        <VSCodePanelTab id="tab-commits">
+          <T>Commits</T>
+        </VSCodePanelTab>
+        <VSCodePanelTab id="tab-files">
+          <T>Files</T>
+        </VSCodePanelTab>
+        <VSCodePanelView id="view-commits">
+          {/* Skip rendering (which might trigger slow dependency calculation) if the tab is inactive */}
+          <ScrollY maxSize="70vh">{activeTab === 'commits' && <StackEditSubTree />}</ScrollY>
+        </VSCodePanelView>
+        <VSCodePanelView id="view-files"></VSCodePanelView>
+      </VSCodePanels>
       <FlexRow style={{padding: 'var(--pad) 0', justifyContent: 'flex-end'}}>
         <StackEditConfirmButtons />
       </FlexRow>
