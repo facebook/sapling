59ea31acca27b1593fee9982e86a9a6c58aed120
Evan Krause <evangrayk@meta.com>
2023-09-22 13:21:25 -0700

    Make unused variables a warning in eslint
    Summary:
    I was getting annoyed having the create react app error overlay appear for unused variables. It's super common for me to be developing and want to see my code change even if I have an unused variable.
    
    Let's just make this a warning instead of an error
    
    Reviewed By: quark-zju
    
    Differential Revision: D49551312
    
    fbshipit-source-id: 1567098cf3a4cbb65ad8cea8c1b0150e01decbf5

--- addons/.eslintrc.js
+++ addons/.eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
   rules: {
     // Need to use the TypeScript version of no-unused-vars so it understands
     // "private" constructor args.
-    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}],
     '@typescript-eslint/consistent-type-imports': 'error',
 
     curly: 'error',
