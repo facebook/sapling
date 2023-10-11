7b5ab0e7a0aa23fa12ada62d9941545727234665
Evan Krause <evangrayk@meta.com>
2023-10-11 10:01:21 -0700

    Make webview macOS app bundle spawn its own ISL server
    Summary:
    This is the start of a number of changes to actually use the new webview stuff for `sl web`.
    
    This change lays the groundwork by refactoring the basic control flow.
    
    Before webviews, `sl web` would spawn a node process, then that node process would open the browser
    
    Before this diff, the way the new webview infra was set up, we woudl expect to have `sl web` in python spawn the isl server, then create a macOS app bundle pointing back to sl, then `open` that app bundle, while passing in cli args with the node server URL.
    
    However, this architecture means that the app bundle is completely dependent on being called with the server args. If you launched sl web once, then saved the app icon in your dock, then closed and reopened, it would fail because it's not launched with the right URL in the process args.
    
    The new design in this diff is to move the responsibility of spawning the node ISL server into the app bundle process.
    
    So now `sl web` in python will determine the appropriate node args to spawn the isl server, then call into rust to construct the app bundle, then write all the node server args the `sl web` call got into a server_args.json file inside the app bundle, then call open on the app bundle.
    
    Then, the newly spawned app will look for that server_args.json file, read it, and use those options to spawn a node ISL server.
    
    This means it supports being reopened from the dock because it knows the last server spawn options so it can spawn its own server when it needs to.
    
    There's still quite a lot more to do for this, but this is the minimal example that can open the webview from `sl web` invocation.
    
    Some notable TODOs:
    - revive the chrome --app code path which I sort of killed during this change
    - add --browser to the server args
    - add width&height to the server args
    - check for --no-open and just spawn a regular server without opening the app
    - actually save multiple windows worth of server spawn options to server_args.json, so we could support opening multiple windows in the same app
    - Do some kind of gating so we can land this and control the rollout
    
    Some more minor or later TODOs:
    - eventually, move all the rest of the code out of isl.py and into rust (including the tar stuff)
    - Fix some bugs when launched via webview, like syntax highlighting not working, no file dialogs, and no file uploads
    - think about how to handle if the app is already open and you run `sl web` again
    - do some version checking in the app so that if you leave the app open for a long time, it warns you to restart to update
    
    P.S. my rust is a bit weak, let me know if I'm doing things in a silly way.
    
    Reviewed By: quark-zju
    
    Differential Revision: D49703009
    
    fbshipit-source-id: df3405512f32138295946171b4a4b9bec43b55f3

--- eden/scm/lib/webview-app/src/lib.rs
+++ eden/scm/lib/webview-app/src/lib.rs
@@ -8,76 +8,193 @@
 use std::fs;
 use std::io;
 use std::path::Path;
+use std::path::PathBuf;
+use std::process::Child;
 use std::process::Command;
+use std::process::Stdio;
 
-/// Attempt to open a webview application window. By default, this function
+use serde::Deserialize;
+use serde::Serialize;
+
+/// Attempt to open a webview application window and spawn ISL servers to handle it. By default, this function
 /// returns without waiting for the webview application. If `browser` is
 /// `builtin-webview` on macOS, this function will wait for the webview to
 /// close.
 ///
 /// By default, on macOS:
 /// - Creates an app bundle with "Sapling" branding.
-/// - The app bundle launches a new process as `CFBundleExecutable URL SIZE`.
-/// - The new process uses `webview-sys` to create a browser window.
+/// - The current server spawn arguments are written into this app bundle.
+/// - The app bundle is launched as new process.
+/// - An ISL Server process is spawned by this app process, using the saved server arguments.
+/// - The new process uses `webview-sys` to create a browser window, and connect to the server url.
 ///
 /// By default, on Windows and Linux:
-/// - Try to find a chrome/edge browser and use its `--app`.
-///
-/// `browser` can be a path to a Chrome-like browser to override
-/// the default behavior. It can be `builtin-webview` to launch
-/// the webview without app bundle.
-pub fn open(url: &str, width: i32, height: i32, browser: Option<String>) -> io::Result<()> {
-    let opts = WebviewOptions { url, width, height };
+/// - An ISL server process is spawned by the current process to get the url for the browser to open.
+/// - Try to find a chrome/edge browser and use its `--app` with the url.
+pub fn open_isl(opts: ISLSpawnOptions) -> io::Result<()> {
+    // TODO: check for --no-open and do simple server spawn without webview or browser.
+    // TODO: check for --browser in opts to control which browser to use.
 
     #[cfg(target_os = "macos")]
-    if browser.is_none() {
-        opts.run_app_bundle()?;
-    } else if browser.as_deref() == Some("builtin-webview") {
-        opts.run_webview_sys();
-    } else {
-        opts.run_chrome_like(browser)?;
+    setup_and_spawn_app_bundle(opts)?;
+
+    // TODO: reenable this code path for non-macOS.
+    // #[cfg(not(target_os = "macos"))]
+    // opts.run_chrome_like(None)?;
+
+    Ok(())
+}
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[allow(dead_code)]
+pub struct ISLSpawnResult {
+    port: i32,
+    url: String,
+    token: String,
+    pid: i32,
+    was_server_reused: bool,
+    log_file_location: String,
+    cwd: String,
+    command: String,
+    // TODO: browser: Option<String>,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct ISLSpawnOptions {
+    pub port: i32,
+    pub platform: String,
+    pub slcommand: String,
+    pub slversion: String,
+    /// Path to the repository to open in ISL
+    pub repo_cwd: String,
+    pub json: bool,
+    pub no_open: bool,
+    pub foreground: bool,
+    pub force: bool,
+    pub kill: bool,
+    /// Path to the cwd to spawn the isl bundle, from which entrypoint is a valid path.
+    pub server_cwd: String,
+    pub nodepath: String,
+    pub entrypoint: String,
+}
+
+impl ISLSpawnOptions {
+    pub fn spawn_isl_server(&self) -> io::Result<Child> {
+        let mut cmd = Command::new(&self.nodepath);
+        cmd.current_dir(&self.server_cwd);
+        cmd.arg(&self.entrypoint);
+        cmd.args(["--port", &self.port.to_string()]);
+        cmd.args(["--command", &self.slcommand]);
+        cmd.args(["--sl-version", &self.slversion]);
+        cmd.args(["--cwd", &self.repo_cwd]);
+        cmd.args(["--platform", &self.platform]);
+        if self.json {
+            cmd.arg("--json");
+        }
+        if self.no_open {
+            cmd.arg("--no-open");
+        }
+        if self.foreground {
+            cmd.arg("--foreground");
+        }
+        if self.kill {
+            cmd.arg("--kill");
+        }
+        if self.force {
+            cmd.arg("--force");
+        }
+        cmd.stdin(Stdio::null());
+        cmd.stdout(Stdio::piped());
+        cmd.spawn()
     }
 
-    #[cfg(not(target_os = "macos"))]
-    opts.run_chrome_like(browser)?;
+    pub fn spawn_isl_server_json(&self) -> io::Result<ISLSpawnResult> {
+        let child = self.spawn_isl_server()?;
+        let output = child.wait_with_output()?;
+        let stdout = String::from_utf8(output.stdout).expect("invalid utf-8");
+
+        let json = serde_json::from_str::<ISLSpawnResult>(&stdout).expect("failed to parse JSON");
+        Ok(json)
+    }
+
+    /// Override arguments that make the spawned server compatible with connecting to the webview.
+    pub fn replace_args_for_webview_spawn(self) -> ISLSpawnOptions {
+        let mut opts = self.clone();
+        opts.json = true;
+        // no_open is slightly overloaded: it's used to prevent the app from spawning at all, but also passed
+        // into the node server to tell it to open the browser or not.
+        // If we've made it to this function call, we assume we've passed the test for opening the app bundle,
+        // but if we want to open the app bundle then we shouldn't also open the browser, so we want to forward
+        // "true" to the node's no_open option.
+        // TODO: it might be better if we move browser opening outside of node and just handle it here instead.
+        opts.no_open = true;
+        opts.foreground = false;
+        opts.kill = false;
+        opts.platform = "webview".to_owned();
+        opts
+    }
+}
+
+/// Setup macOS app bundle, save the configured server settings, and spawn the application in a new process.
+#[cfg(target_os = "macos")]
+pub fn setup_and_spawn_app_bundle(opts: ISLSpawnOptions) -> Result<(), io::Error> {
+    let opts = opts.replace_args_for_webview_spawn();
+
+    let app = ISLAppBundle::get_or_create_app_bundle()?;
+    app.write_server_args(opts)?;
+    app.run_app_bundle()?;
 
     Ok(())
 }
 
 /// Entry point for the app bundle.
-/// Run webview-sys and does not return if started as the app.
-/// Return if this function does nothing.
+/// Read past (or current) server args, spawn ISL server, then open the webview to that url.
 #[cfg(target_os = "macos")]
 pub fn maybe_become_webview_app() -> Option<()> {
+    // this function is called from hgmain itself on all invocations, we need to only become the app
+    // if it's being spawned by macOS as an app.
     let mut args = std::env::args();
     let arg0 = args.next()?;
     if !arg0.ends_with(CF_BUNDLE_EXECUTABLE) {
         return None;
     }
-    let url = args.next()?;
-    let width: i32 = args.next()?.parse().ok().unwrap_or(800);
-    let height: i32 = args.next()?.parse().ok().unwrap_or(600);
-    let opts = WebviewOptions {
-        url: &url,
-        width,
-        height,
-    };
-    opts.run_webview_sys();
+
+    let app = ISLAppBundle::get_or_create_app_bundle().expect("could not create app bundle");
+
+    let server_options = app.read_server_args().expect("could not read server args");
+    println!("Found spawn options: {:?}", server_options);
+
+    // TODO: It might be a better idea to save an array of servers in the app state instead of just one.
+    // Then, we can handle opening multiple windows (repos) in the app at the same time.
+    // This would of course mean we would also spawn multiple node servers.
+    // We would probably want to also save window size and position, so they can be restored fully.
+
+    let server_output = server_options
+        .spawn_isl_server_json()
+        .expect("could not start server");
+    println!("Started ISL server: {:?}", server_output);
+
+    // TODO: save & read these from saved server state.
+    let width = 1280;
+    let height = 720;
+
+    app.run_webview_sys(&server_output.url, width, height);
     std::process::exit(0);
 }
 
-struct WebviewOptions<'a> {
-    url: &'a str,
-    width: i32,
-    height: i32,
+struct ISLAppBundle {
+    app_dir: PathBuf,
 }
 
 #[cfg(target_os = "macos")]
-impl WebviewOptions<'_> {
-    /// Create an app bundle and launch it via 'open'.
-    /// Requires the main() logic to handle "webview" arg0 by calling
-    /// `maybe_start_webview()`.
-    fn run_app_bundle(&self) -> io::Result<()> {
+const SERVER_ARGS_DIR: &str = "Contents/Resources/server_args.json";
+
+#[cfg(target_os = "macos")]
+impl ISLAppBundle {
+    /// Create an app bundle for the application.
+    fn get_or_create_app_bundle() -> Result<ISLAppBundle, io::Error> {
         let dir = match dirs::data_local_dir() {
             None => return Err(io::Error::new(io::ErrorKind::NotFound, "no data local dir")),
             Some(dir) => dir,
@@ -96,45 +213,65 @@ impl WebviewOptions<'_> {
 
         let current_exe = std::env::current_exe()?;
         let app_exe_path = app_dir.join("Contents/MacOS/").join(CF_BUNDLE_EXECUTABLE);
-        let is_symlink_ok = match fs::read_link(&app_exe_path) {
-            Ok(target) => target == current_exe,
-            Err(_) => false,
-        };
-        if !is_symlink_ok {
-            // Recreate the symlink.
-            let _ = fs::remove_file(&app_exe_path);
-            std::os::unix::fs::symlink(&current_exe, app_exe_path)?;
+        if current_exe != app_exe_path {
+            let is_symlink_ok = match fs::read_link(&app_exe_path) {
+                Ok(target) => target == current_exe || target == app_exe_path,
+                Err(_) => false,
+            };
+            if !is_symlink_ok {
+                // Recreate the symlink.
+                let _ = fs::remove_file(&app_exe_path);
+                std::os::unix::fs::symlink(&current_exe, app_exe_path)?;
+            }
         }
 
+        Ok(ISLAppBundle { app_dir })
+    }
+
+    fn read_server_args(&self) -> Result<ISLSpawnOptions, io::Error> {
+        let server_args_json = fs::read_to_string(self.app_dir.join(SERVER_ARGS_DIR))?;
+        let json = serde_json::from_str::<ISLSpawnOptions>(&server_args_json)?;
+        // // TODO: read args from the app bundle.
+        // let width = 640;
+        // let height = 480;
+        Ok(json)
+    }
+
+    fn write_server_args(&self, opts: ISLSpawnOptions) -> Result<(), io::Error> {
+        fs::write(
+            self.app_dir.join(SERVER_ARGS_DIR),
+            serde_json::to_vec(&opts)?,
+        )?;
+        Ok(())
+    }
+
+    /// Launch the app bundle in a new process via 'open'.
+    fn run_app_bundle(&self) -> io::Result<()> {
         // Use 'open' to run the app.
         let mut command = Command::new("/usr/bin/open");
-        command
-            .arg(app_dir)
-            .args(["--args", self.url])
-            .args([self.width.to_string(), self.height.to_string()])
-            .spawn()?;
+        command.arg(&self.app_dir).spawn()?;
 
         Ok(())
     }
 
     /// Open a browser window using webview-sys.
     /// Block until the webview is closed.
-    fn run_webview_sys(&self) {
+    fn run_webview_sys(&self, url: &str, width: i32, height: i32) {
         // Use webview-sys directly in this process.
-        let url_cstr = std::ffi::CString::new(self.url).unwrap_or_default();
+        let url_cstr = std::ffi::CString::new(url).unwrap_or_default();
         unsafe {
             let resizable = true;
             let debug = true;
             let frameless = false;
             let visible = true;
-            let min_width = 0;
-            let min_height = 0;
+            let min_width = 320;
+            let min_height = 240;
             let hide_instead_of_close = false;
             let inner = webview_sys::webview_new(
-                b"Sapling\0" as *const u8 as _,
+                b"Sapling Interactive Smartlog\0" as *const u8 as _,
                 url_cstr.as_bytes_with_nul().as_ptr() as _,
-                self.width,
-                self.height,
+                width,
+                height,
                 resizable as _,
                 debug as _,
                 frameless as _,
@@ -155,7 +292,15 @@ impl WebviewOptions<'_> {
     }
 }
 
-impl WebviewOptions<'_> {
+#[allow(dead_code)]
+struct ISLWebviewOptions<'a> {
+    url: &'a str,
+    width: i32,
+    height: i32,
+}
+
+#[allow(dead_code)]
+impl ISLWebviewOptions<'_> {
     /// Spawn a chrome-like browser to fulfil the webview request.
     fn run_chrome_like(&self, browser_path: Option<String>) -> io::Result<()> {
         let browser_path = match browser_path {
@@ -178,6 +323,7 @@ impl WebviewOptions<'_> {
     }
 }
 
+#[allow(dead_code)]
 fn find_chrome_like() -> io::Result<String> {
     if cfg!(target_os = "windows") {
         let program_files = [
--- eden/scm/saplingnative/bindings/modules/pywebview/src/lib.rs
+++ eden/scm/saplingnative/bindings/modules/pywebview/src/lib.rs
@@ -11,17 +11,46 @@ use cpython_ext::ResultPyErrExt;
 pub fn init_module(py: Python, package: &str) -> PyResult<PyModule> {
     let name = [package, "webview"].join(".");
     let m = PyModule::new(py, &name)?;
-    m.add(py, "open", py_fn!(py, open(url: &str, width: i32 = 800, height: i32 = 600, browser: Option<String> = None)))?;
+    m.add(
+         py,
+         "open_isl",
+         py_fn!(py, open_isl(
+             repo_cwd: &str, port: i32, no_open: bool, json_output: bool, foreground: bool, force: bool, kill: bool, platform: &str, slcommand: &str, slversion: &str, server_cwd: &str, nodepath: &str, entrypoint: &str,
+         )))?;
     Ok(m)
 }
 
-fn open(
+fn open_isl(
     py: Python,
-    url: &str,
-    width: i32,
-    height: i32,
-    browser: Option<String>,
+    repo_cwd: &str,
+    port: i32,
+    no_open: bool,
+    json: bool,
+    foreground: bool,
+    force: bool,
+    kill: bool,
+    platform: &str,
+    slcommand: &str,
+    slversion: &str,
+    server_cwd: &str,
+    nodepath: &str,
+    entrypoint: &str,
 ) -> PyResult<PyNone> {
-    webview_app::open(url, width, height, browser).map_pyerr(py)?;
+    let opts = webview_app::ISLSpawnOptions {
+        repo_cwd: repo_cwd.to_string(),
+        port,
+        no_open,
+        json,
+        foreground,
+        force,
+        kill,
+        platform: platform.to_string(),
+        slcommand: slcommand.to_string(),
+        slversion: slversion.to_string(),
+        server_cwd: server_cwd.to_string(),
+        nodepath: nodepath.to_string(),
+        entrypoint: entrypoint.to_string(),
+    };
+    webview_app::open_isl(opts).map_pyerr(py)?;
     Ok(PyNone)
 }
