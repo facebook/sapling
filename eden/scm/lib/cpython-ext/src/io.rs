71ff3018f0326121e60f690ce85be89aa6711b21
Jun Wu <quark@meta.com>
2023-10-17 10:53:02 -0700

    cpython-ext: add ways to convert a Rust io::Read to a Python object
    Summary:
    This will be used by the upcoming changes.
    
    It seems the Python binding types might be better moved to `pyio`.
    
    Reviewed By: sggutier
    
    Differential Revision: D50347195
    
    fbshipit-source-id: d49280daaa389f85a12bb502c48cf48e27451000

--- eden/scm/lib/cpython-ext/src/io.rs
+++ eden/scm/lib/cpython-ext/src/io.rs
@@ -7,55 +7,116 @@
 
 use std::cell::RefCell;
 use std::io;
+use std::io::Read;
 
 use cpython::*;
 
 use crate::error::ResultPyErrExt;
 use crate::none::PyNone;
 
-py_class!(pub class PyRustWrite |py| {
-    data io: RefCell<Box<dyn ::io::Write + Send>>;
+py_class!(pub class PyRustIO |py| {
+    data r: RefCell<Option<Box<dyn ::io::Read + Send>>>;
+    data w: RefCell<Option<Box<dyn ::io::Write + Send>>>;
+
+    /// Read at most `n` bytes from the input.
+    /// If `n` is negative, read everything till the end.
+    def read(&self, n: i64 = -1) -> PyResult<PyBytes> {
+        let mut io = self.r(py).borrow_mut();
+        let io = match io.as_mut() {
+            Some(io) => io,
+            None => return Err(not_readable(py)),
+        };
+        let mut buf = Vec::<u8>::new();
+        if n < 0 {
+            io.read_to_end(&mut buf).map_pyerr(py)?;
+        } else {
+            buf.resize(n as usize, 0u8);
+            let read_bytes = io.read(&mut buf).map_pyerr(py)?;
+            buf.truncate(read_bytes);
+        }
+        Ok(PyBytes::new(py, &buf))
+    }
 
     def write(&self, bytes: PyBytes) -> PyResult<usize> {
-        let mut io = self.io(py).borrow_mut();
+        let mut io = self.w(py).borrow_mut();
+        let io = match io.as_mut() {
+            Some(io) => io,
+            None => return Err(not_writable(py)),
+        };
         let bytes = bytes.data(py);
         io.write_all(bytes).map_pyerr(py)?;
         Ok(bytes.len())
     }
 
     def flush(&self) -> PyResult<PyNone> {
-        let mut io = self.io(py).borrow_mut();
+        let mut io = self.w(py).borrow_mut();
+        let io = match io.as_mut() {
+            Some(io) => io,
+            None => return Ok(PyNone),
+        };
         io.flush().map_pyerr(py)?;
         Ok(PyNone)
     }
 
     def isatty(&self) -> PyResult<bool> {
-        let io = self.io(py).borrow();
-        Ok(io.is_tty())
+        let io = self.w(py).borrow();
+        match io.as_ref() {
+            Some(io) => Ok(io.is_tty()),
+            None => {
+                let io = self.r(py).borrow();
+                match io.as_ref() {
+                    Some(io) => Ok(io.is_tty()),
+                    None => Ok(false),
+                }
+            }
+        }
     }
 
     def isstdin(&self) -> PyResult<bool> {
-        let io = self.io(py).borrow();
-        Ok(io.is_stdin())
+        let io = self.r(py).borrow();
+        match io.as_ref() {
+            Some(io) => Ok(io.is_stdin()),
+            None => Ok(false),
+        }
     }
 
     def isstdout(&self) -> PyResult<bool> {
-        let io = self.io(py).borrow();
-        Ok(io.is_stdout())
+        let io = self.w(py).borrow();
+        match io.as_ref() {
+            Some(io) => Ok(io.is_stdout()),
+            None => Ok(false),
+        }
     }
 
     def isstderr(&self) -> PyResult<bool> {
-        let io = self.io(py).borrow();
-        Ok(io.is_stderr())
+        let io = self.w(py).borrow();
+        match io.as_ref() {
+            Some(io) => Ok(io.is_stderr()),
+            None => Ok(false),
+        }
     }
 
     def close(&self) -> PyResult<PyNone> {
-        let mut io = self.io(py).borrow_mut();
-        io.flush().map_pyerr(py)?;
-        *io = Box::new(ClosedIO);
+        let mut io = self.w(py).borrow_mut();
+        if let Some(inner_io) = io.as_mut() {
+            inner_io.flush().map_pyerr(py)?;
+            *io = Some(Box::new(ClosedIO));
+        };
+        let mut io = self.r(py).borrow_mut();
+        if io.is_some() {
+            *io = Some(Box::new(ClosedIO));
+        }
         Ok(PyNone)
     }
 });
+
+fn not_readable(py: Python) -> PyErr {
+    PyErr::new::<exc::IOError, _>(py, "stream is not readable")
+}
+
+fn not_writable(py: Python) -> PyErr {
+    PyErr::new::<exc::IOError, _>(py, "stream is not writable")
+}
 
 struct ClosedIO;
 
@@ -75,6 +136,15 @@ impl io::Write for ClosedIO {
     }
 }
 
+impl io::Read for ClosedIO {
+    fn read(&mut self, _buf: &mut [u8]) -> io::Result<usize> {
+        Err(io::Error::new(
+            io::ErrorKind::NotConnected,
+            "stream was closed",
+        ))
+    }
+}
+
 impl ::io::IsTty for ClosedIO {
     fn is_tty(&self) -> bool {
         false
@@ -82,6 +152,11 @@ impl ::io::IsTty for ClosedIO {
 }
 
 /// Wrap a Rust Write trait object into a Python object.
-pub fn wrap_rust_write(py: Python, w: impl ::io::Write + Send + 'static) -> PyResult<PyRustWrite> {
-    PyRustWrite::create_instance(py, RefCell::new(Box::new(w)))
+pub fn wrap_rust_write(py: Python, w: impl ::io::Write + Send + 'static) -> PyResult<PyRustIO> {
+    PyRustIO::create_instance(py, RefCell::new(None), RefCell::new(Some(Box::new(w))))
+}
+
+/// Wrap a Rust Read trait object into a Python object.
+pub fn wrap_rust_read(py: Python, r: impl ::io::Read + Send + 'static) -> PyResult<PyRustIO> {
+    PyRustIO::create_instance(py, RefCell::new(Some(Box::new(r))), RefCell::new(None))
 }
