efdf35eda02e873f17d7b08695ac4ae84cc8d8bf
Mateusz Kwapich <mitrandir@meta.com>
2023-10-19 10:56:37 -0700

    remove enable_new_commit_graph_common_base_with tunable
    Summary: It's no longer used anywhere
    
    Reviewed By: YousefSalama
    
    Differential Revision: D50262305
    
    fbshipit-source-id: 9853dd8fe3864cdc216e756c38de701e004b54d3

--- eden/mononoke/tunables/src/lib.rs
+++ eden/mononoke/tunables/src/lib.rs
@@ -338,8 +338,6 @@ pub struct MononokeTunables {
 
     // Enable using the new commit graph for is_ancestor queries
     enable_new_commit_graph_is_ancestor: TunableBoolByRepo,
-    // Enable using new commit graph in common_base_with
-    enable_new_commit_graph_common_base_with: TunableBoolByRepo,
     // Enable using new commit graph in finding commit history
     enable_new_commit_graph_commit_history: TunableBoolByRepo,
     // Enable using new commit graph in finding commit path history
