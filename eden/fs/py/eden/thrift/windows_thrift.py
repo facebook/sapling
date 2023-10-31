693af8af230275b2125cd14f7fd2d43588208521
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2023-10-31 09:50:57 -0700

    Add annotations to `eden/fs/py/eden/thrift/windows_thrift.py`
    Reviewed By: genevievehelsel
    
    Differential Revision: D50835324
    
    fbshipit-source-id: b8c94dd3daa9c5f1344ea1aca8f9fe0c9d9ecf8d

--- eden/fs/py/eden/thrift/windows_thrift.py
+++ eden/fs/py/eden/thrift/windows_thrift.py
@@ -182,7 +182,7 @@ class WindowsSocketHandle:
     address: str = ""
 
     @staticmethod
-    def _checkReturnCode(retcode):
+    def _checkReturnCode(retcode) -> None:
         if retcode == -1:
             errcode = WSAGetLastError()
             if errcode == WSAECONNREFUSED:
@@ -205,7 +205,7 @@ class WindowsSocketHandle:
             else:
                 raise WindowsSocketException(errcode)
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._io_refs = 0  # stub to make socket.makefile work on this object
         wsa_data = WSAData64()
         # ctypes.c_ushort(514) = MAKE_WORD(2,2) which is for the winsock
@@ -283,7 +283,7 @@ class WindowsSocketHandle:
         self._checkReturnCode(retsize)
         return buff.raw[0:retsize]
 
-    def recv_into(self, buffer, size=0):
+    def recv_into(self, buffer, size: int = 0):
         if size == 0:
             size = len(buffer)
 
