d3fc81821c8987773b45671d76fcf54c6d518fac
Evan Krause <evangrayk@meta.com>
2023-11-06 14:26:08 -0800

    Make update box resize to multiple lines
    Summary:
    The update message box for adding a message when resubmitting was only one line long. Let's make it multiple lines long so you can more easily write longer messages.
    
    It still defaults to one line so that it's not really in your way if you don't add an update message. But it then expands to fit all the content as you type.
    
    Reviewed By: muirdm
    
    Differential Revision: D50820374
    
    fbshipit-source-id: e59ce6f5eade052075a70ca9bb4b54cf53f6780d

--- addons/isl/src/SubmitUpdateMessageInput.tsx
+++ addons/isl/src/SubmitUpdateMessageInput.tsx
@@ -8,13 +8,15 @@
 import type {CommitInfo} from './types';
 
 import {diffUpdateMessagesState} from './CommitInfoView/CommitInfoState';
+import {MinHeightTextField} from './CommitInfoView/MinHeightTextField';
 import {codeReviewProvider} from './codeReview/CodeReviewInfo';
 import {T} from './i18n';
-import {VSCodeTextField} from '@vscode/webview-ui-toolkit/react';
+import {useRef} from 'react';
 import {useRecoilState, useRecoilValue} from 'recoil';
 
 export function SubmitUpdateMessageInput({commits}: {commits: Array<CommitInfo>}) {
   const provider = useRecoilValue(codeReviewProvider);
+  const ref = useRef(null);
 
   // typically only one commit, but if you've selected multiple, we key the message on all hashes together.
   const key = commits.map(c => c.hash).join(',');
@@ -23,11 +25,13 @@ export function SubmitUpdateMessageInput({commits}: {commits: Array<CommitInfo>}
     return null;
   }
   return (
-    <VSCodeTextField
+    <MinHeightTextField
+      ref={ref}
+      keepNewlines
       style={{width: '100%'}}
       value={message}
-      onChange={e => setMessage((e.target as HTMLInputElement).value)}>
+      onInput={(e: KeyboardEvent) => setMessage((e.target as HTMLInputElement).value)}>
       <T>Update Message</T>
-    </VSCodeTextField>
+    </MinHeightTextField>
   );
 }
