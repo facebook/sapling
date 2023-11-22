dbd3d27e5680d29054c36e7333a0112a7e34b6c4
Muir Manders <muirdm@meta.com>
2023-11-21 16:18:56 -0800

    dirstate: kill dead Python eden dirstate reading code
    Summary: We use the Rust code to read the eden dirstate.
    
    Reviewed By: quark-zju
    
    Differential Revision: D51505486
    
    fbshipit-source-id: b7170985d65dc9a3b95af365c5e02ab42a82a662

--- eden/scm/sapling/eden_dirstate_serializer.py
+++ eden/scm/sapling/eden_dirstate_serializer.py
@@ -7,10 +7,8 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import binascii
 import hashlib
 import struct
-import sys
 from typing import Callable, Dict, IO, Tuple
 
 
@@ -87,139 +85,7 @@ def write(
     file.write(sha.digest())
 
 
-def read(
-    fp: "IO[bytes]", filename: str
-) -> "Tuple[Tuple[bytes, bytes], Dict[str, Tuple[str, int, int]], Dict[str, str]]":  # noqa: C901
-    """Returns a tuple of (parents, tuples_dict, copymap) if successful.
-
-    Any exception from create_file(), such as IOError with errno == ENOENT, will
-    be bubbled up to the caller.
-
-    If contents of the dirstate file do not match the expected format, then a
-    DirstateParseException will be thrown.
-    """
-    parents = None
-    tuples_dict = {}
-    copymap = {}
-
-    sha = hashlib.sha256()
-
-    def hashing_read(num):
-        data = fp.read(num)
-        sha.update(data)
-        return data
-
-    parent_bytes = hashing_read(40)
-    num_parents_bytes = len(parent_bytes)
-    if num_parents_bytes != 40:
-        raise DirstateParseException(
-            "Reached EOF while reading dirstate parents in {}.\n".format(filename)
-        )
-    parents = parent_bytes[:20], parent_bytes[20:40]
-
-    binary_version = hashing_read(4)
-    if len(binary_version) != 4:
-        raise DirstateParseException(
-            "Reached EOF while reading the version number in {}.\n".format(filename)
-        )
-    version: int = struct.unpack(">I", binary_version)[0]
-    if version != CURRENT_DIRSTATE_VERSION:
-        raise DirstateParseException(
-            "Unknown dirstate version in {}. Found {} but expected {}.\n".format(
-                filename, version, CURRENT_DIRSTATE_VERSION
-            )
-        )
-
-    while True:
-        header = hashing_read(1)
-        if not header:
-            # We have reached the end of the file.
-            break
-        elif header == b"\x01":
-            scalars = hashing_read(6)
-            if len(scalars) != 6:
-                raise DirstateParseException(
-                    "Malformed dirstate tuple in {}".format(filename)
-                    + ". Aborting read().\n"
-                )
-            path = _read_path(hashing_read, filename)
-            status: int = 0
-            mode: int = 0
-            merge: int = 0
-            status, mode, merge = struct.unpack(">BIb", scalars)
-            # TODO(mbolin): Verify status and merge?
-            tuples_dict[path] = (chr(status), mode, merge)
-        elif header == b"\x02":
-            dest = _read_path(hashing_read, filename)
-            source = _read_path(hashing_read, filename)
-            copymap[dest] = source
-        elif header == b"\xFF":
-            # Reading the checksum, so we use fp.read() instead of
-            # hashing_read().
-            binary_checksum = fp.read(32)
-            if len(binary_checksum) != 32:
-                raise DirstateParseException(
-                    "Reached EOF while reading checksum hash in {}.\n".format(filename)
-                )
-            digest = sha.digest()
-            if binary_checksum == digest:
-                if fp.read(1) == b"":
-                    # There is no more data, as expected.
-                    break
-                else:
-                    raise DirstateParseException(
-                        "Suspicious data is present after "
-                        "the end of the valid checksum in {}.\n".format(filename)
-                    )
-            else:
-                raise DirstateParseException(
-                    "Checksum mismatch when reading {}. Observed checksum is "
-                    "{}, but the checksum in the file is {}.\n".format(
-                        filename,
-                        binascii.hexlify(digest),
-                        binascii.hexlify(binary_checksum),
-                    )
-                )
-        else:
-            raise DirstateParseException(
-                "Unexpected header byte "
-                "when reading {:s}: 0x{:x}.".format(filename, header)
-                + " Ignoring remaining dirstate data.\n"
-            )
-
-    return parents, tuples_dict, copymap
-
-
 def _write_path(writer: "Callable[[bytes], None]", path: str) -> None:
-    if sys.version_info[0] < 3:
-        byte_path = path
-    else:
-        byte_path = path.encode("utf-8")
+    byte_path = path.encode("utf-8")
     writer(struct.pack(">H", len(byte_path)))
     writer(byte_path)
-
-
-def _read_path(reader: "Callable[[int], bytes]", filename: str) -> str:
-    binary_path_len = reader(2)
-    if len(binary_path_len) != 2:
-        raise DirstateParseException(
-            "Reached EOF while reading path length in {}.\n".format(filename)
-        )
-
-    path_len: int = struct.unpack(">H", binary_path_len)[0]
-    path = reader(path_len)
-    if len(path) == path_len:
-        if isinstance(path, str):
-            # Python 2.
-            return path
-        else:
-            # Python 3
-            return str(path, "utf8")
-    else:
-        raise DirstateParseException(
-            "Reached EOF while reading path in {}.\n".format(filename)
-        )
-
-
-class DirstateParseException(Exception):
-    pass
