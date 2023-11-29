1016b917070336bbbcbbcf6dc29f12ab1aea2c1f
John Elliott <jdelliot@fb.com>
2023-11-28 18:23:56 -0800

    Move sapling_backingstore_get_tree_batch to cxx_bridge
    Summary:
    X-link: https://github.com/facebookincubator/velox/pull/7781
    
    cxx.rs provides a more ergonomic and opinionated interop layer between Rust and C++ that we would like to leverage for future API chages.
    
    This moves  sapling_backingstore_get_tree_batch from the existing cbindgen implemenation to the new cxxbridge.
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D51645547

--- eden/scm/lib/backingstore/include/ffi.h
+++ eden/scm/lib/backingstore/include/ffi.h
@@ -11,4 +11,31 @@
 #include <rust/cxx.h>
 #include <memory>
 
-namespace sapling {} // namespace sapling
+namespace sapling {
+
+class SaplingFetchError : public std::runtime_error {
+ public:
+  using std::runtime_error::runtime_error;
+};
+
+struct Tree;
+
+/**
+ * Resolver used in the processing of getTreeBatch requests.
+ */
+struct GetTreeBatchResolver {
+  explicit GetTreeBatchResolver(
+      folly::FunctionRef<void(size_t, folly::Try<std::shared_ptr<Tree>>)>
+          resolve)
+      : resolve{std::move(resolve)} {}
+
+  folly::FunctionRef<void(size_t, folly::Try<std::shared_ptr<Tree>>)> resolve;
+};
+
+void sapling_backingstore_get_tree_batch_handler(
+    std::shared_ptr<GetTreeBatchResolver> resolver,
+    size_t index,
+    rust::String error,
+    std::shared_ptr<Tree> tree);
+
+} // namespace sapling
