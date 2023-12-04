c3b235a8f7b9a14b4747b2dd7adc5899091d57f4
Jun Wu <quark@meta.com>
2023-12-04 15:30:39 -0800

    previews: migrate AmendMessageOperation to `optimisticDag`
    Summary: The new version seems easier to understand.
    
    Reviewed By: muirdm, evangrayk
    
    Differential Revision: D51712895
    
    fbshipit-source-id: 67cb31dfd8a51c8340b9b19bb56f8a0d859cad0e

--- addons/isl/src/operations/AmendMessageOperation.ts
+++ addons/isl/src/operations/AmendMessageOperation.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import type {ApplyPreviewsFuncType, PreviewContext} from '../previews';
+import type {DagPreviewContext, DagWithPreview} from '../previews';
 import type {CommandArg, ExactRevset, SucceedableRevset} from '../types';
 
 import {Operation} from './Operation';
@@ -22,32 +22,16 @@ export class AmendMessageOperation extends Operation {
     return args;
   }
 
-  makeOptimisticApplier(context: PreviewContext): ApplyPreviewsFuncType | undefined {
-    const commitToMetaedit = context.treeMap.get(this.revset.revset);
-    if (commitToMetaedit == null) {
-      // metaedit succeeds when we no longer see original commit
-      // Note: this assumes we always restack children and never render old commit as obsolete.
-      return undefined;
-    }
-
-    const [title] = this.message.split(/\n+/, 1);
-    const description = this.message.slice(title.length);
-
-    const func: ApplyPreviewsFuncType = (tree, _previewType) => {
-      if (tree.info.hash === this.revset.revset) {
-        // use fake title/description on the changed commit
-        return {
-          info: {
-            ...tree.info,
-            title,
-            description: description ?? '',
-          },
-          children: tree.children,
-        };
-      } else {
-        return {info: tree.info, children: tree.children};
+  optimisticDag(dag: DagWithPreview, _context: DagPreviewContext): DagWithPreview {
+    return dag.replaceWith([this.revset.revset], (_h, c) => {
+      if (c === undefined) {
+        // metaedit succeeds when we no longer see original commit
+        // Note: this assumes we always restack children and never render old commit as obsolete.
+        return c;
       }
-    };
-    return func;
+      const [title] = this.message.split(/\n+/, 1);
+      const description = this.message.slice(title.length);
+      return {...c, title, description};
+    });
   }
 }
