6d990e48e4f3825861bece04b1a6cd12acc645cd
Jun Wu <quark@meta.com>
2023-12-04 15:30:39 -0800

    previews: migrate RebaseOperation to optimisticDag
    Summary:
    With the ordering issue fixed in the previous diff, now it's straightforward to
    migrate rebase to use the dag preview interface.
    
    Reviewed By: evangrayk, zzl0
    
    Differential Revision: D51784285
    
    fbshipit-source-id: deb0082c185adcbbc2b084aca8ec8dcc1c774166

--- addons/isl/src/operations/RebaseOperation.ts
+++ addons/isl/src/operations/RebaseOperation.ts
@@ -5,7 +5,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import type {ApplyPreviewsFuncType, PreviewContext} from '../previews';
+import type {
+  ApplyPreviewsFuncType,
+  DagPreviewContext,
+  DagWithPreview,
+  PreviewContext,
+} from '../previews';
 import type {ExactRevset, Hash, SucceedableRevset} from '../types';
 
 import {latestSuccessor} from '../SuccessionTracker';
@@ -87,52 +92,9 @@ export class RebaseOperation extends Operation {
     return func;
   }
 
-  makeOptimisticApplier(context: PreviewContext): ApplyPreviewsFuncType | undefined {
-    const {treeMap} = context;
-    const originalSourceNode = treeMap.get(latestSuccessor(context, this.source));
-    if (originalSourceNode == null) {
-      // once we don't see the source anymore, we don't have optimistic state to apply (that commit has been rebased).
-      return undefined;
-    }
-
-    const newSourceNode = {
-      ...originalSourceNode,
-      info: {...originalSourceNode.info},
-    };
-    let parentHash: Hash;
-
-    const func: ApplyPreviewsFuncType = (tree, previewType, childPreviewType) => {
-      if (tree.info.hash === latestSuccessor(context, this.source)) {
-        if (tree.info.parents[0] === parentHash) {
-          // this is the newly added node
-          return {
-            info: tree.info,
-            children: tree.children,
-            previewType: CommitPreview.REBASE_OPTIMISTIC_ROOT, // root will show spinner
-            childPreviewType: CommitPreview.REBASE_OPTIMISTIC_DESCENDANT, // children should also show as previews, but don't all need spinner
-          };
-        } else {
-          // this is the original source node, it's hidden
-          return {info: null};
-        }
-      } else if (
-        tree.info.hash === latestSuccessor(context, this.destination) ||
-        tree.info.remoteBookmarks.includes(this.destination.revset)
-      ) {
-        parentHash = tree.info.hash;
-        newSourceNode.info.parents = [parentHash];
-        // we always want the rebase preview to be the lowest child aka last in list
-        return {info: tree.info, children: [...tree.children, newSourceNode]};
-      } else {
-        return {
-          info: tree.info,
-          children: tree.children,
-          previewType,
-          // inherit previews so entire subtree is previewed
-          childPreviewType,
-        };
-      }
-    };
-    return func;
+  optimisticDag(dag: DagWithPreview, context: DagPreviewContext): DagWithPreview {
+    const src = dag.resolve(latestSuccessor(context, this.source));
+    const dest = dag.resolve(latestSuccessor(context, this.destination));
+    return dag.rebase(dag.descendants(src?.hash), dest?.hash);
   }
 }
