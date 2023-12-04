99c8f3c5571e3b9d63ed329e055021fa26680f7e
Jun Wu <quark@meta.com>
2023-12-04 15:30:39 -0800

    previews: migrate RebaseAllDraftCommitsOperation to optimisticDag
    Summary:
    The new logic is shorter and seems less error prone.
    
    We might need a way to "stabilize" the stack order. But that could be
    non-trivial and will be a later task.
    
    Since we can assume there are no "gaps" in `draft()`, I originally wrote a
    simpler version of `rebase`, which gets extended in D51784277.
    
         optimisticDag(dag: DagWithPreview, context: DagPreviewContext): DagWithPreview {
           const dest = dag.resolve(latestSuccessor(context, this.destination))?.hash;
           const draft = dag.draft();
      -    const alreadyRebased = dag.descendants(dag.children(dest).intersect(draft));
      -    const src = draft.subtract(alreadyRebased);
      -    // Skip if nothing left to rebase.
      -    if (dest == null || src.size === 0) {
      -      return dag;
      -    }
      -    // Reparent source roots to dest. Set previewType and timestamp for descendants.
      -    const srcRoots = dag.roots(src);
      -    return dag.touch(src).replaceWith(src, (h, c) => {
      -      const isRoot = srcRoots.contains(h);
      -      const previewType = isRoot
      -        ? CommitPreview.REBASE_OPTIMISTIC_ROOT
      -        : CommitPreview.REBASE_OPTIMISTIC_DESCENDANT;
      -      const parents = isRoot ? [dest] : c?.parents ?? [];
      -      return c && {...c, previewType, parents};
      -    });
      +    return dag.rebase(draft, dest);
         }
    
    Reviewed By: evangrayk, zzl0
    
    Differential Revision: D51784284
    
    fbshipit-source-id: 080ba0563d1a80984c4eca66f5eb9b99e1c6ed1f

--- addons/isl/src/operations/RebaseAllDraftCommitsOperation.ts
+++ addons/isl/src/operations/RebaseAllDraftCommitsOperation.ts
@@ -5,11 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import type {ApplyPreviewsFuncType, PreviewContext} from '../previews';
-import type {ExactRevset, Hash, SucceedableRevset} from '../types';
+import type {DagPreviewContext, DagWithPreview} from '../previews';
+import type {ExactRevset, SucceedableRevset} from '../types';
 
 import {latestSuccessor} from '../SuccessionTracker';
-import {CommitPreview} from '../previews';
 import {exactRevset} from '../types';
 import {Operation} from './Operation';
 
@@ -35,71 +34,9 @@ export class RebaseAllDraftCommitsOperation extends Operation {
     ];
   }
 
-  makeOptimisticApplier(context: PreviewContext): ApplyPreviewsFuncType | undefined {
-    const {treeMap} = context;
-    const stackBasesToRebase = Array.from(treeMap.values())
-      .filter(node => {
-        const parent = treeMap.get(node.info.parents[0]);
-        // Only commits which are the base of a stack that aren't already on the destination
-        return (
-          parent != null &&
-          parent.info.phase === 'public' &&
-          parent.info.hash !== latestSuccessor(context, this.destination) &&
-          !parent.info.remoteBookmarks.includes(this.destination.revset)
-        );
-      })
-      .map(tree => {
-        // make a copy of the tree
-        return {
-          ...tree,
-          info: {
-            ...tree.info,
-          },
-        };
-      });
-
-    if (stackBasesToRebase.length === 0) {
-      // once there's no stacks on public commits that aren't the destination, then we're done
-      return undefined;
-    }
-
-    let parentHash: Hash;
-
-    const func: ApplyPreviewsFuncType = (tree, previewType, childPreviewType) => {
-      if (stackBasesToRebase.find(toRebase => toRebase.info.hash === tree.info.hash)) {
-        if (tree.info.parents[0] === parentHash) {
-          // this is a newly added node
-          return {
-            info: tree.info,
-            children: tree.children,
-            previewType: CommitPreview.REBASE_OPTIMISTIC_ROOT, // root will show spinner
-            childPreviewType: CommitPreview.REBASE_OPTIMISTIC_DESCENDANT, // children should also show as previews, but don't all need spinner
-          };
-        } else {
-          // this is an original source node, it's hidden
-          return {info: null};
-        }
-      } else if (
-        tree.info.hash === latestSuccessor(context, this.destination) ||
-        tree.info.remoteBookmarks.includes(this.destination.revset)
-      ) {
-        parentHash = tree.info.hash;
-
-        stackBasesToRebase.forEach(toRebase => {
-          toRebase.info.parents = [parentHash];
-        });
-        // we always want the rebase previews to be the lowest child aka last in list
-        return {info: tree.info, children: [...tree.children, ...stackBasesToRebase]};
-      } else {
-        return {
-          info: tree.info,
-          children: tree.children,
-          previewType,
-          // inherit previews so entire subtree is previewed
-          childPreviewType,
-        };
-      }
-    };
-    return func;
+  optimisticDag(dag: DagWithPreview, context: DagPreviewContext): DagWithPreview {
+    const dest = dag.resolve(latestSuccessor(context, this.destination))?.hash;
+    const draft = dag.draft();
+    return dag.rebase(draft, dest);
   }
 }
