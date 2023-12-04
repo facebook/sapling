e15867dff221fe6b951d0da37936c102088b66e5
Jun Wu <quark@meta.com>
2023-12-04 15:30:39 -0800

    dag: initial class for "set" representation
    Summary:
    To provide revsetlang-like queries. We need a dedicated "Set" type to
    represent a set of commit hashes. This diff adds it. For now it's just
    a thin wrapper around `immutable.Set<Hash>`. We might want to add more
    information to the set type in the future, like preserving the topology
    order (which hg's revset already does), etc. But this is a start.
    
    Reviewed By: muirdm, evangrayk
    
    Differential Revision: D51712900
    
    fbshipit-source-id: 1379886ac98da348c65268b6f26049910ce6ab3c

---
+++ addons/isl/src/dag/set.ts
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {Hash} from '../types';
+import type {List, Seq} from 'immutable';
+
+import {Set as ImSet} from 'immutable';
+import {SelfUpdate} from 'shared/immutableExt';
+
+/**
+ * Set of commit hashes, with extra methods.
+ * Internally maintains the order of the hashes information.
+ */
+export class HashSet extends SelfUpdate<ImSet<Hash>> {
+  constructor(set?: ImSet<Hash>) {
+    super(set ?? ImSet());
+  }
+
+  static fromHashes(hashes: SetLike): HashSet {
+    if (hashes == null) {
+      return new HashSet(ImSet());
+    } else if (hashes instanceof HashSet) {
+      return hashes;
+    } else if (typeof hashes === 'string') {
+      return new HashSet(ImSet([hashes]));
+    } else {
+      return new HashSet(ImSet(hashes));
+    }
+  }
+
+  toHashes(): ImSet<Hash> {
+    return this.set;
+  }
+
+  toSeq(): Seq.Set<Hash> {
+    return this.set.toSeq();
+  }
+
+  toList(): List<Hash> {
+    return this.set.toList();
+  }
+
+  toArray(): Array<Hash> {
+    return this.set.toArray();
+  }
+
+  /** Union with another set. */
+  union(other: SetLike): HashSet {
+    const set = this.set.union(HashSet.fromHashes(other).set);
+    return new HashSet(set);
+  }
+
+  /** Interset with another set. */
+  intersect(other: SetLike): HashSet {
+    const set = this.set.intersect(HashSet.fromHashes(other).set);
+    return new HashSet(set);
+  }
+
+  /** Remove items that exist in another set. */
+  subtract(other: SetLike): HashSet {
+    const set = this.set.subtract(HashSet.fromHashes(other).set);
+    return new HashSet(set);
+  }
+
+  /** Test if this set contains the given hash. */
+  contains(hash: Hash): boolean {
+    return this.set.has(hash);
+  }
+
+  /** Convert to sorted array. Mainly for testing. */
+  toSortedArray(): Array<Hash> {
+    return this.set.toArray().sort();
+  }
+
+  [Symbol.iterator](): IterableIterator<Hash> {
+    return this.set[Symbol.iterator]();
+  }
+
+  get size(): number {
+    return this.set.size;
+  }
+
+  private get set(): ImSet<Hash> {
+    return this.inner;
+  }
+}
+
+/** A convenient type that converts to HashSet. `null` converts to an empty set. */
+export type SetLike = HashSet | Iterable<Hash> | Hash | null | undefined;
