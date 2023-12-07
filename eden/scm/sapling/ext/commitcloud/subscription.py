1fe5cb3ea87b1e28aeeaf98ca934a9dd98e2830b
Muir Manders <muirdm@meta.com>
2023-12-06 18:06:46 -0800

    commitcloud: track subscriptions using shared repo path
    Summary: We previously tracked subscriptions using the "local" repo.path instead of repo.sharedpath. These two differ after sharing a repo using the "share" extension, or more commonly for EdenFS working copies sharing a single backing repo.
    
    Reviewed By: quark-zju
    
    Differential Revision: D51912195
    
    fbshipit-source-id: 01f395a4aaec4c465cc144a907bb0f5c4179c386

--- eden/scm/sapling/ext/commitcloud/subscription.py
+++ eden/scm/sapling/ext/commitcloud/subscription.py
@@ -17,6 +17,11 @@ from . import error as ccerror, util as ccutil, workspace
 
 
 def _uniquefilename(reporoot, reponame, workspacename):
+    # Stabilize filename for tests.
+    if testtmp := os.getenv("TESTTMP"):
+        if reporoot.startswith(testtmp):
+            reporoot = "$TESTTMP/" + reporoot.removeprefix(testtmp).replace("\\", "/")
+
     hash = hashlib.sha256(
         encodeutf8("\0".join([reporoot, reponame, workspacename]))
     ).hexdigest()
@@ -41,17 +46,37 @@ def check(repo):
     workspacename = workspace.currentworkspace(repo)
     if not workspacename:
         return
-    filename = _uniquefilename(repo.path, reponame, workspacename)
+
+    # We previously tracked subscriptions using the "local" repo.path, but that
+    # results in duplicate subscriptions recorded for shared repos. Now we use
+    # the shared path, but also clean up old entries, if present.
+    filename = _uniquefilename(repo.sharedpath, reponame, workspacename)
+    oldfilename = _uniquefilename(repo.path, reponame, workspacename)
+
     vfs = _subscriptionvfs(repo)
+    didsomething = False
     if not vfs.exists(filename):
         with vfs.open(filename, "wb") as configfile:
+            repo.ui.debug(
+                "check: writing subscription %s\n" % filename, component="commitcloud"
+            )
             configfile.write(
                 encodeutf8(
                     "[commitcloud]\nworkspace=%s\nrepo_name=%s\nrepo_root=%s\n"
-                    % (workspacename, reponame, repo.path)
+                    % (workspacename, reponame, repo.sharedpath)
                 )
             )
-            _restart_subscriptions(repo.ui)
+            didsomething = True
+
+    if filename != oldfilename and vfs.exists(oldfilename):
+        repo.ui.debug(
+            "check: cleaning up non-shared subscription %s\n" % oldfilename,
+            component="commitcloud",
+        )
+        vfs.tryunlink(oldfilename)
+
+    if didsomething:
+        _restart_subscriptions(repo.ui)
     else:
         _test_service_is_running(repo.ui)
 
@@ -61,10 +86,32 @@ def remove(repo):
     workspacename = workspace.currentworkspace(repo)
     if not workspacename:
         return
-    filename = _uniquefilename(repo.path, reponame, workspacename)
+
+    # We previously tracked subscriptions using the "local" repo.path, but that
+    # results in duplicate subscriptions recorded for shared repos. Now we use
+    # the shared path, but also clean up old entries, if present.
+    filename = _uniquefilename(repo.sharedpath, reponame, workspacename)
+    oldfilename = _uniquefilename(repo.path, reponame, workspacename)
+
     vfs = _subscriptionvfs(repo)
+
+    didsomething = False
     if vfs.exists(filename):
         vfs.tryunlink(filename)
+        didsomething = True
+        repo.ui.debug(
+            "remove: cleaning up shared subscription %s\n" % filename,
+            component="commitcloud",
+        )
+    if vfs.exists(oldfilename):
+        vfs.tryunlink(oldfilename)
+        didsomething = True
+        repo.ui.debug(
+            "remove: cleaning up non-shared subscription %s\n" % oldfilename,
+            component="commitcloud",
+        )
+
+    if didsomething:
         _restart_subscriptions(repo.ui, warn_service_not_running=False)
 
 
@@ -72,18 +119,35 @@ def move(repo, workspace, new_workspace):
     reponame = ccutil.getreponame(repo)
     if not workspace or not new_workspace:
         return
-    src = _uniquefilename(repo.path, reponame, workspace)
-    dst = _uniquefilename(repo.path, reponame, new_workspace)
+
+    # We previously tracked subscriptions using the "local" repo.path, but that
+    # results in duplicate subscriptions recorded for shared repos. Now we use
+    # the shared path, but also clean up old entries, if present.
+
+    src = _uniquefilename(repo.sharedpath, reponame, workspace)
+    oldsrc = _uniquefilename(repo.path, reponame, workspace)
+
+    dst = _uniquefilename(repo.sharedpath, reponame, new_workspace)
+    olddst = _uniquefilename(repo.path, reponame, new_workspace)
+
     vfs = _subscriptionvfs(repo)
-    if vfs.exists(src):
+
+    if vfs.exists(src) or vfs.exists(oldsrc):
         vfs.tryunlink(src)
+        vfs.tryunlink(oldsrc)
         with vfs.open(dst, "wb") as configfile:
             configfile.write(
                 encodeutf8(
                     "[commitcloud]\nworkspace=%s\nrepo_name=%s\nrepo_root=%s\n"
-                    % (new_workspace, reponame, repo.path)
+                    % (new_workspace, reponame, repo.sharedpath)
                 )
             )
+        if olddst != dst and vfs.exists(olddst):
+            repo.ui.debug(
+                "move: cleaning up non-shared subscription %s\n" % olddst,
+                component="commitcloud",
+            )
+            vfs.tryunlink(olddst)
         _restart_subscriptions(repo.ui, warn_service_not_running=False)
 
 
@@ -101,6 +165,8 @@ def _warn_service_not_running(ui):
 
 def _test_service_is_running(ui):
     port = ui.configint("commitcloud", "scm_daemon_tcp_port")
+    if port < 0:
+        return
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if s.connect_ex(("127.0.0.1", port)):
         _warn_service_not_running(ui)
@@ -111,6 +177,8 @@ def testservicestatus(repo):
     if not repo.ui.configbool("commitcloud", "subscription_enabled"):
         return False
     port = repo.ui.configint("commitcloud", "scm_daemon_tcp_port")
+    if port < 0:
+        return
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     status = s.connect_ex(("127.0.0.1", port)) == 0
     s.close()
@@ -119,6 +187,8 @@ def testservicestatus(repo):
 
 def _restart_subscriptions(ui, warn_service_not_running=True):
     port = ui.configint("commitcloud", "scm_daemon_tcp_port")
+    if port < 0:
+        return
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect(("127.0.0.1", port))
