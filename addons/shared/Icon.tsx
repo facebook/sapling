88dd52994ce6685cbf3b816f463796841ee29ef3
Evan Krause <evangrayk@meta.com>
2023-12-08 13:24:19 -0800

    Polish land styles
    Reviewed By: quark-zju
    
    Differential Revision: D51862009
    
    fbshipit-source-id: ad3a089642716a7986efee4e6b45c8f137849ad7

--- addons/shared/Icon.tsx
+++ addons/shared/Icon.tsx
@@ -18,7 +18,7 @@ export function Icon({
   slot?: 'start';
   icon: string;
   size?: 'S' | 'M' | 'L';
-  color?: 'blue' | 'red' | 'green';
+  color?: 'blue' | 'red' | 'green' | 'yellow';
 } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
   return (
     <div
