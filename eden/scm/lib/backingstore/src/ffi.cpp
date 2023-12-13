13b10e8a2ce15cfee6aa6e6d94bf831f174adae5
John Elliott <jdelliot@meta.com>
2023-12-13 11:56:35 -0800

    Fix issue where valid, empty blobs are treated as errors
    Summary:
    When migrating cxx, code needed to be written to determine when a blob fetch was an error. The code was written with the assumption that an empty blob was an error. This is not the case - there are valid, empty blobs. Let's fix that by instead using the absense or presense of an error message as the differentiator.
    
    Also, there was an apparent use after free condition that required the resolve method to ensure that the error string was only used during its lifetime. This was likely true, but not certain. Addressed by converting to a string rather than using char*.
    
    Reviewed By: xavierd
    
    Differential Revision: D52126320
    
    fbshipit-source-id: e9c36c1cf28de211db25271d600cd6c0129801c7

--- eden/scm/lib/backingstore/src/ffi.cpp
+++ eden/scm/lib/backingstore/src/ffi.cpp
@@ -21,13 +21,14 @@ void sapling_backingstore_get_tree_batch_handler(
     std::shared_ptr<Tree> tree) {
   using ResolveResult = folly::Try<std::shared_ptr<Tree>>;
 
-  resolver->resolve(index, folly::makeTryWith([&] {
-                      if (tree) {
-                        return ResolveResult{tree};
-                      } else {
-                        return ResolveResult{SaplingFetchError{error.c_str()}};
-                      }
-                    }));
+  resolver->resolve(
+      index, folly::makeTryWith([&] {
+        if (error.empty()) {
+          return ResolveResult{tree};
+        } else {
+          return ResolveResult{SaplingFetchError{std::string(error)}};
+        }
+      }));
 }
 
 void sapling_backingstore_get_blob_batch_handler(
@@ -37,22 +38,22 @@ void sapling_backingstore_get_blob_batch_handler(
     rust::Box<Blob> blob) {
   using ResolveResult = folly::Try<std::unique_ptr<folly::IOBuf>>;
 
-  resolver->resolve(index, folly::makeTryWith([&] {
-                      auto result = blob.into_raw();
-                      if (result->bytes.empty()) {
-                        auto box = rust::Box<Blob>::from_raw(result);
-                        return ResolveResult{SaplingFetchError{error.c_str()}};
-                      } else {
-                        return ResolveResult{folly::IOBuf::takeOwnership(
-                            reinterpret_cast<void*>(result->bytes.data()),
-                            result->bytes.size(),
-                            [](void* /* buf */, void* blob) mutable {
-                              auto box = rust::Box<Blob>::from_raw(
-                                  reinterpret_cast<Blob*>(blob));
-                            },
-                            reinterpret_cast<void*>(result))};
-                      }
-                    }));
+  resolver->resolve(
+      index, folly::makeTryWith([&] {
+        if (error.empty()) {
+          auto result = blob.into_raw();
+          return ResolveResult{folly::IOBuf::takeOwnership(
+              reinterpret_cast<void*>(result->bytes.data()),
+              result->bytes.size(),
+              [](void* /* buf */, void* blob) mutable {
+                auto box =
+                    rust::Box<Blob>::from_raw(reinterpret_cast<Blob*>(blob));
+              },
+              reinterpret_cast<void*>(result))};
+        } else {
+          return ResolveResult{SaplingFetchError{std::string(error)}};
+        }
+      }));
 }
 
 void sapling_backingstore_get_file_aux_batch_handler(
@@ -62,13 +63,14 @@ void sapling_backingstore_get_file_aux_batch_handler(
     std::shared_ptr<FileAuxData> aux) {
   using ResolveResult = folly::Try<std::shared_ptr<FileAuxData>>;
 
-  resolver->resolve(index, folly::makeTryWith([&] {
-                      if (aux) {
-                        return ResolveResult{aux};
-                      } else {
-                        return ResolveResult{SaplingFetchError{error.c_str()}};
-                      }
-                    }));
+  resolver->resolve(
+      index, folly::makeTryWith([&] {
+        if (error.empty()) {
+          return ResolveResult{aux};
+        } else {
+          return ResolveResult{SaplingFetchError{std::string(error)}};
+        }
+      }));
 }
 
 } // namespace sapling
