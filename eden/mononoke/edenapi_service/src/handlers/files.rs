c928963a39e75e25e2e8b7cd35a1cd9738603093
Liubov Dmitrieva <liubovd@meta.com>
2024-01-17 06:13:42 -0800

    adjust edenapi concurrency for fetching files.
    Summary:
    EdenFs uses the batch size 32. So, it is the most loggical to allow
    concurrency up to the same size. Otherwise we just create an artificial 3x slow
    down for these requests.
    
    We have other protection like cachelib shards limit, etc, to keep out throuput
    bounded, so it doesn't make much sense to configure this number to be 3x
    smaller than the EdenFs batch.
    
    https://fburl.com/scuba/mononoke_test_perf/bz3zda7t
    
    We would improve a good amount of fetches.
    
    https://fburl.com/scuba/mononoke_test_perf/8z6kz6le
    
    In case, we would like to set it lower due to some reason, then we would need to lower the eden fs
    batch size as well, so that they would go with a smaller batch at a time but to more
    hosts concurrently to avoid artificial slow downs when we still have headroom
    in throughput. The idea is that optimal performance is reached when these
    two values are matched.
    
    Differential Revision: D52833986
    
    fbshipit-source-id: f4dc0b81c7a82b117b851ca38e9c6e84e0aee420

--- eden/mononoke/edenapi_service/src/handlers/files.rs
+++ eden/mononoke/edenapi_service/src/handlers/files.rs
@@ -63,8 +63,7 @@ use crate::middleware::RequestContext;
 use crate::utils::cbor_stream_filtered_errors;
 use crate::utils::get_repo;
 
-/// XXX: This number was chosen arbitrarily.
-const MAX_CONCURRENT_FILE_FETCHES_PER_REQUEST: usize = 10;
+const MAX_CONCURRENT_FILE_FETCHES_PER_REQUEST: usize = 32;
 
 const MAX_CONCURRENT_UPLOAD_FILENODES_PER_REQUEST: usize = 1000;
 
