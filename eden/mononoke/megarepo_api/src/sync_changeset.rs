10df10533e01d7f1b5f54f244f6beecb0692e8f3
Mateusz Kwapich <mitrandir@meta.com>
2024-01-23 13:11:57 -0800

    migrate away from tunables
    Summary:
    There were 3 tunables at play in this crate controlling the commit squashing:
     * megarepo_squashing_config_override_targets - the list of branches that the next tunables should be applied to
     * megarepo_override_squashing_limit - the maximum branch length for suqasting - if not overriden can be also set by client
     * megaarepo override author check - whether we check if all commits on branch are from the same author
    
    Currently we have the list of few branches in the first, limit is set to 32 and
    author check is disabled.
    
    I'm replacing this system with two justknobs switched by "repo:branch" string.
    This will allow me to emulate the current behaviour and migrate to saner
    settings in the future.
    
    Reviewed By: liubov-dmitrieva
    
    Differential Revision: D53001224
    
    fbshipit-source-id: f7f245047c0f4b2d69dce499e23a39db4c5cd88a

--- eden/mononoke/megarepo_api/src/sync_changeset.rs
+++ eden/mononoke/megarepo_api/src/sync_changeset.rs
@@ -72,21 +72,27 @@ pub enum MergeMode {
     },
 }
 
-fn get_squashing_overrides(repo_name: &str, target_bookmark: &str) -> (Option<i64>, Option<bool>) {
-    let targets = tunables::tunables()
-        .by_repo_megarepo_squashing_config_override_targets(repo_name)
-        .unwrap_or_default();
-    if targets
-        .iter()
-        .any(|target| target.as_str() == target_bookmark)
-    {
-        (
-            tunables::tunables().by_repo_megarepo_override_squashing_limit(repo_name),
-            tunables::tunables().by_repo_megarepo_override_author_check(repo_name),
-        )
+fn get_squashing_overrides(repo_name: &str, target_bookmark: &str) -> (Option<i64>, bool) {
+    let switchval = format!("{}:{}", repo_name, target_bookmark);
+    let squashing_limit = justknobs::get(
+        "scm/mononoke:megarepo_override_squashing_limit",
+        Some(&switchval),
+    )
+    .unwrap_or(0);
+
+    let maybe_squashing_limit = if squashing_limit == 0 {
+        None
     } else {
-        (None, None)
-    }
+        Some(squashing_limit)
+    };
+
+    let author_check = justknobs::eval(
+        "scm/mononoke:megarepo_override_author_check",
+        None,
+        Some(&switchval),
+    )
+    .unwrap_or(true);
+    (maybe_squashing_limit, author_check)
 }
 
 pub struct SquashingConfig {
@@ -172,14 +178,14 @@ impl<'a> SyncChangeset<'a> {
         // merged-in commits or squash side-branch.
         let maybe_squashing_config = match &source_config.merge_mode {
             Some(megarepo_config::MergeMode::squashed(sq)) => {
-                let (maybe_squash_limit, maybe_check_author) =
+                let (maybe_squash_limit, check_author) =
                     get_squashing_overrides(target_repo.name(), &target.bookmark);
                 Some(SquashingConfig {
                     squash_limit: maybe_squash_limit
                         .unwrap_or(sq.squash_limit)
                         .try_into()
                         .context("couldn't convert squash commits limit")?,
-                    check_author: maybe_check_author.unwrap_or(true),
+                    check_author,
                 })
             }
             None | Some(megarepo_config::MergeMode::with_move_commit(_)) => None,
