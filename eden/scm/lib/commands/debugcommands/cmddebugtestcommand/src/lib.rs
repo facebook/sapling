b48f796a63915656289a06a6c4d506c7e9a1e516
Muir Manders <muirdm@meta.com>
2024-02-02 09:32:43 -0800

    debugtestcommand: add some more features
    Summary:
    Add the ability to:
    - force a command abort
    - control the exit code
    - optionally emit some output
    
    Going to use in tests in upcoming diffs.
    
    Reviewed By: sggutier
    
    Differential Revision: D53195176
    
    fbshipit-source-id: bd0efd2c581031c883dffc326db8a150b1f4ca2e

--- eden/scm/lib/commands/debugcommands/cmddebugtestcommand/src/lib.rs
+++ eden/scm/lib/commands/debugcommands/cmddebugtestcommand/src/lib.rs
@@ -5,15 +5,41 @@
  * GNU General Public License version 2.
  */
 
+use std::io::Write;
 use std::sync::Arc;
 
+use clidispatch::abort;
+use clidispatch::OptionalRepo;
 use clidispatch::ReqCtx;
-use cmdutil::Config;
-use cmdutil::NoOpts;
+use cmdutil::define_flags;
 use cmdutil::Result;
 
-pub fn run(_ctx: ReqCtx<NoOpts>, _config: &Arc<dyn Config>) -> Result<u8> {
-    Ok(0)
+define_flags! {
+    pub struct TestCommandOpts {
+        /// set result code
+        result: i64 = 0,
+
+        /// force command to abort
+        abort: bool = false,
+
+        /// print this back out, followed by newline
+        echo: Option<String>,
+
+        #[args]
+        args: Vec<String>,
+    }
+}
+
+pub fn run(ctx: ReqCtx<TestCommandOpts>, _repo: &mut OptionalRepo) -> Result<u8> {
+    if ctx.opts.abort {
+        abort!("aborting");
+    }
+
+    if let Some(output) = &ctx.opts.echo {
+        writeln!(ctx.io().output(), "{output}")?;
+    }
+
+    Ok(ctx.opts.result as u8)
 }
 
 pub fn aliases() -> &'static str {
