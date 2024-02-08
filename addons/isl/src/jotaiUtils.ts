0de069075f05af8665f000bb35302ecf4baceb4b
Jun Wu <quark@meta.com>
2024-02-07 17:44:53 -0800

    jotaiUtils: add refreshAtom
    Summary: This can be used outside React to "refresh" an atom.
    
    Reviewed By: evangrayk
    
    Differential Revision: D53531878
    
    fbshipit-source-id: a21fd2e9c22aab16ea3f2b449955a5295865fc2c

--- addons/isl/src/jotaiUtils.ts
+++ addons/isl/src/jotaiUtils.ts
@@ -195,6 +195,10 @@ export function writeAtom<T>(atom: MutAtom<T>, value: T | ((prev: T) => T)) {
   store.set(atom, value);
 }
 
+export function refreshAtom<T>(atom: WritableAtom<T, [], void>) {
+  store.set(atom);
+}
+
 // Once we are pure Jotai, consider adding a `cwd` atom then update `resetOnCwdChange`
 // to be something that depends on the `cwd` atom.
 export function resetOnCwdChange<T>(atom: WritableAtom<T, [T], unknown>, defaultValue: T) {
