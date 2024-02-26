c8eddacd33b74f60a79dc3b6fd08fb898d950ca7
Jun Wu <quark@meta.com>
2024-02-26 12:43:42 -0800

    repositoryData: add atomResetOnCwdChange
    Summary: This will replace the callback version of reset on cwd change.
    
    Reviewed By: evangrayk
    
    Differential Revision: D54196982
    
    fbshipit-source-id: 702f83722291e43c7be0ead8f542714ccb382f82

--- addons/isl/src/repositoryData.ts
+++ addons/isl/src/repositoryData.ts
@@ -7,11 +7,13 @@
 
 import type {RepoInfo} from './types';
 
+import {atomResetOnDepChange} from './jotaiUtils';
 import {initialParams} from './urlParams';
 import {atom} from 'jotai';
 
 export const repositoryData = atom<{info?: RepoInfo; cwd?: string}>({});
 
+/** "cwd" is not always the repo root. It can be a path inside the repo. */
 export const serverCwd = atom(get => {
   const data = get(repositoryData);
   if (data.info?.type === 'cwdNotARepository') {
@@ -19,3 +21,19 @@ export const serverCwd = atom(get => {
   }
   return data?.cwd ?? initialParams.get('cwd') ?? '';
 });
+
+const repoRoot = atom(get => {
+  const data = get(repositoryData);
+  return data.info?.type === 'success' ? data.info.repoRoot : '';
+});
+
+/**
+ * A string of repo root and the "cwd". Note a same "cwd" does not infer the same repo,
+ * when there are nested (ex. submodule) repos.
+ */
+const repoRootAndCwd = atom<string>(get => `${get(serverCwd)}\n${get(repoRoot)}`);
+
+/** A specific version of `atomResetOnDepChange`. */
+export function atomResetOnCwdChange<T>(defaultValue: T) {
+  return atomResetOnDepChange(defaultValue, repoRootAndCwd);
+}
