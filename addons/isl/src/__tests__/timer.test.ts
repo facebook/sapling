f44ced9d8921d1ecf709709496dfa8c3bee0dac4
Jun Wu <quark@meta.com>
2024-02-29 10:25:35 -0800

    timer: declaractive setInterval alternative
    Summary:
    The "Timer" concept is common in classic GUI programming. But seems to be
    missing in the JS world. There is `setInterval` but it is not declaractive.
    A "Timer" has an (declaractive) "enabled" attribute to control whether it
    should work or not. I think this API is nice, partially because setting
    `enabled = true` twice does not register duplicated callbacks.
    
    This diff adds a "Timer" that has the `enabled = boolean` API, and integrate
    with GC - if the timer object is GC-ed then the callback will auto stop without
    worrying about leaking.
    
    Reviewed By: evangrayk
    
    Differential Revision: D54348076
    
    fbshipit-source-id: 2593c2f6b633940fd03155bf14ff804a97f6b3b5

---
+++ addons/isl/src/__tests__/timer.test.ts
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {Timer} from '../timer';
+import {gc} from 'shared/testUtils';
+
+describe('Timer', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('can enable and disable', () => {
+    const callback = jest.fn();
+    const timer = new Timer(callback, 100);
+
+    timer.enabled = true;
+    jest.advanceTimersByTime(250);
+    expect(callback).toHaveBeenCalledTimes(2);
+
+    timer.enabled = false;
+    jest.advanceTimersByTime(500);
+    expect(callback).toHaveBeenCalledTimes(2);
+
+    timer.enabled = true;
+    jest.advanceTimersByTime(200);
+    expect(callback).toHaveBeenCalledTimes(4);
+  });
+
+  it('error once cancels the timer', () => {
+    const callback = jest.fn(() => {
+      throw new Error('x');
+    });
+
+    // Initially enabled.
+    const timer = new Timer(callback, 100, true);
+    expect(timer.enabled).toBe(true);
+
+    // Try to call 3 times, but the first time it will throw.
+    try {
+      jest.advanceTimersByTime(350);
+    } catch (_e) {}
+
+    // After throw the timer is disabled.
+    expect(timer.enabled).toBe(false);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('returning false stops the timer', () => {
+    let count = 0;
+    const callback = jest.fn(() => {
+      count += 1;
+      return count < 3;
+    });
+    const timer = new Timer(callback, 100, true);
+    expect(timer.enabled).toBe(true);
+    jest.advanceTimersByTime(250);
+    expect(timer.enabled).toBe(true);
+    jest.advanceTimersByTime(500);
+    expect(timer.enabled).toBe(false);
+    expect(callback).toHaveBeenCalledTimes(3);
+  });
+
+  it('dispose cancels the timer forever', () => {
+    const callback = jest.fn();
+    const timer = new Timer(callback, 100);
+
+    timer.enabled = true;
+    jest.advanceTimersByTime(50);
+
+    timer.dispose();
+
+    // Cannot be re-enabled.
+    timer.enabled = true;
+    jest.advanceTimersByTime(500);
+    expect(timer.enabled).toBe(false);
+    expect(callback).toHaveBeenCalledTimes(0);
+  });
+
+  it('GC cancels the timer forever', async () => {
+    const callback = jest.fn();
+    let timer: Timer | null = new Timer(callback, 100, true);
+    expect(timer.enabled).toBe(true);
+
+    // GC the timer.
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    timer = null;
+    await gc();
+
+    // Callback should not be called after the time is gone.
+    jest.advanceTimersByTime(500);
+    expect(callback).toHaveBeenCalledTimes(0);
+  });
+});
---
+++ addons/isl/src/timer.ts
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {Disposable} from './types';
+
+export class Timer implements Disposable {
+  private timerId: null | number = null;
+  private disposed = false;
+  private callback: () => void;
+
+  /**
+   * The `callback` can return `false` to auto-stop the timer.
+   * The timer auto stops if being GC-ed.
+   */
+  constructor(callback: () => void | boolean, public intervalMs = 1000, enabled = false) {
+    const thisRef = new WeakRef(this);
+    this.callback = () => {
+      const timer = thisRef.deref();
+      if (timer == null) {
+        // The "timer" object is GC-ed. Do not run this interval.
+        return;
+      }
+      // Run the callback and schedules the next interval.
+      timer.timerId = null;
+      const shouldContinue = callback();
+      if (shouldContinue !== false) {
+        timer.enabled = true;
+      }
+    };
+    this.enabled = enabled;
+  }
+
+  set enabled(value: boolean) {
+    if (value && this.timerId === null && !this.disposed) {
+      this.timerId = window.setTimeout(this.callback, this.intervalMs);
+    } else if (!value && this.timerId !== null) {
+      window.clearTimeout(this.timerId);
+      this.timerId = null;
+    }
+  }
+
+  get enabled(): boolean {
+    return this.timerId !== null;
+  }
+
+  dispose(): void {
+    this.enabled = false;
+    this.disposed = true;
+  }
+}
