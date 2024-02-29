d59b692d09b1fdff099b0204f33f8f98b9a9090e
Pierre Chevalier <pierrec@meta.com>
2024-02-29 08:15:03 -0800

    derived data: Implement new BulkDerivation trait
    Summary:
    Before this, places like the derivation worker needed to exhaustively list all derived data types to derive them.
    
    That's also done (in a different way) in the backill-derived-data utility CLI tool.
    
    It's also necessary in [`repo.rs: prepare_derived_data`](https://www.internalfb.com/code/fbsource/[1b217ab2dffb26d1e91c23046f0b82e9d17a913d]/fbcode/eden/mononoke/mononoke_api/src/repo.rs?lines=1843).
    
    By making this part of the data derivation interface, we avoid duplication in these places.
    
    We also add the ability to derive multiple types at once, which will allow us to improve the backfiller tool's UX
    at the fraction of the complexity.
    
    Currently, this is mostly a code move, but it opens the door to allowing gitimport to derive all types and to refactoring the data derivation backfilling.
    
    Reviewed By: RajivTS
    
    Differential Revision: D54262000
    
    fbshipit-source-id: d2c3237d21b9a50e46e5242ae8873019849a6eeb

---
+++ eden/mononoke/derived_data/bulk_derivation/lib.rs
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+use std::sync::Arc;
+use std::time::Duration;
+
+use basename_suffix_skeleton_manifest_v3::RootBssmV3DirectoryId;
+use blame::RootBlameV2;
+use changeset_info::ChangesetInfo;
+use cloned::cloned;
+use context::CoreContext;
+use deleted_manifest::RootDeletedManifestV2Id;
+use derived_data_manager::DerivableType;
+use derived_data_manager::DerivationError;
+use derived_data_manager::DerivedDataManager;
+use derived_data_manager::Rederivation;
+use fastlog::RootFastlog;
+use filenodes_derivation::FilenodesOnlyPublic;
+use fsnodes::RootFsnodeId;
+use futures::stream;
+use futures::stream::StreamExt;
+use git_types::MappedGitCommitId;
+use git_types::RootGitDeltaManifestId;
+use git_types::TreeHandle;
+use mercurial_derivation::MappedHgChangesetId;
+use mononoke_types::ChangesetId;
+use skeleton_manifest::RootSkeletonManifestId;
+use test_manifest::RootTestManifestDirectory;
+use test_sharded_manifest::RootTestShardedManifestDirectory;
+use unodes::RootUnodeManifestId;
+
+pub trait BulkDerivation {
+    fn derive_bulk(
+        &self,
+        ctx: &CoreContext,
+        csids: Vec<ChangesetId>,
+        rederivation: Option<Arc<dyn Rederivation>>,
+        derived_data_types: &[DerivableType],
+    ) -> impl std::future::Future<Output = Result<Duration, DerivationError>> + Send;
+}
+
+impl BulkDerivation for DerivedDataManager {
+    /// Derive all the desired derived data types for all the desired csids
+    ///
+    /// The provided batch of csids must be in topological
+    /// order.
+    ///
+    /// The caller must have arranged for the dependencies
+    /// and ancestors of the batch to have already been derived for all the derived
+    /// datat types requested.
+    ///
+    /// If any dependency or ancestor is not already derived, an error
+    /// will be returned.
+    /// If a dependent derived data type has not been derived for the batch of csids prior to
+    /// this, it will be derived first. The same pre-conditions apply on the dependent derived data
+    /// type.
+    fn derive_bulk(
+        &self,
+        ctx: &CoreContext,
+        csids: Vec<ChangesetId>,
+        rederivation: Option<Arc<dyn Rederivation>>,
+        derived_data_types: &[DerivableType],
+    ) -> impl std::future::Future<Output = Result<Duration, DerivationError>> + Send {
+        // Note: We could skip the ones that are dependent on others that are present in this list to
+        // avoid racing with ourselves
+        stream::iter(derived_data_types)
+            .then(move |derived_data_type| {
+                cloned!(csids, rederivation);
+                async move {
+                    match derived_data_type {
+                        DerivableType::Unodes => {
+                            self.derive_exactly_batch::<RootUnodeManifestId>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                        DerivableType::BlameV2 => {
+                            self.derive_exactly_batch::<RootBlameV2>(ctx, csids, rederivation)
+                                .await
+                        }
+                        DerivableType::FileNodes => {
+                            self.derive_exactly_batch::<FilenodesOnlyPublic>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                        DerivableType::HgChangesets => {
+                            self.derive_exactly_batch::<MappedHgChangesetId>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                        DerivableType::Fsnodes => {
+                            self.derive_exactly_batch::<RootFsnodeId>(ctx, csids, rederivation)
+                                .await
+                        }
+                        DerivableType::Fastlog => {
+                            self.derive_exactly_batch::<RootFastlog>(ctx, csids, rederivation)
+                                .await
+                        }
+                        DerivableType::DeletedManifests => {
+                            self.derive_exactly_batch::<RootDeletedManifestV2Id>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                        DerivableType::SkeletonManifests => {
+                            self.derive_exactly_batch::<RootSkeletonManifestId>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                        DerivableType::ChangesetInfo => {
+                            self.derive_exactly_batch::<ChangesetInfo>(ctx, csids, rederivation)
+                                .await
+                        }
+                        DerivableType::GitTree => {
+                            self.derive_exactly_batch::<TreeHandle>(ctx, csids, rederivation)
+                                .await
+                        }
+                        DerivableType::GitCommit => {
+                            self.derive_exactly_batch::<MappedGitCommitId>(ctx, csids, rederivation)
+                                .await
+                        }
+                        DerivableType::GitDeltaManifest => {
+                            self.derive_exactly_batch::<RootGitDeltaManifestId>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                        DerivableType::BssmV3 => {
+                            self.derive_exactly_batch::<RootBssmV3DirectoryId>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                        DerivableType::TestManifest => {
+                            self.derive_exactly_batch::<RootTestManifestDirectory>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                        DerivableType::TestShardedManifest => {
+                            self.derive_exactly_batch::<RootTestShardedManifestDirectory>(
+                                ctx,
+                                csids,
+                                rederivation,
+                            )
+                            .await
+                        }
+                    }
+                }
+            })
+            .fold(Ok(Duration::ZERO), |acc, x| async move {
+                match (acc, x) {
+                    (Ok(duration), Ok(acc)) => Ok(duration + acc),
+                    (Err(e), _) | (_, Err(e)) => Err(e),
+                }
+            })
+    }
+}
