f7c17b4643a7d9125612414db4b02e4ef8d8c245
Pierre Chevalier <pierrec@meta.com>
2024-03-01 10:22:55 -0800

    derived data: Add canonical thrift struct for DerivedDataType
    Summary:
    This thrift enum will be used to interact with derived data over services boundaries.
    
    It can be used in SCS, replacing [this partial one](https://www.internalfb.com/code/fbsource/[71db333aa4d373073b3c7592cb272c1215b8edd1]/fbcode/eden/mononoke/scs/if/source_control.thrift?lines=1069).
    
    It can also be used in the derived data service, replacing [this "stringly typed" one](https://www.internalfb.com/code/fbsource/[71db333aa4d373073b3c7592cb272c1215b8edd1]/fbcode/eden/mononoke/derived_data/remote/if/derived_data_service.thrift?lines=17)
    
    These changes will come if future diffs to safely respect thrift backward compatibility needs.
    
    Reviewed By: markbt
    
    Differential Revision: D54304252
    
    fbshipit-source-id: 0e0b754773fac680726eb21ef29a86120cf4b166

---
+++ eden/mononoke/derived_data/if/derived_data_type.thrift
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+namespace cpp2 facebook.scm.service
+namespace php DerivedData
+namespace py scm.derived_data.thrift.derived_data_type
+namespace py3 scm.derived_data.thrift
+
+/// An enum representing the DerivedDataType tied to the compiled time enforced rust enum, and
+/// compatible with both SCS and the Derived Data Service interfaces
+enum DerivedDataType {
+  FSNODE = 1,
+  UNODE = 2,
+  FILENODE = 3,
+  FASTLOG = 4,
+  BLAME = 5,
+  HG_CHANGESET = 6,
+  CHANGESET_INFO = 7,
+  // DELETED_MANIFEST = 8, // Deprecated -  please use equivalent to DELETED_MANIFEST_V2
+  SKELETON_MANIFEST = 9,
+  TREE_HANDLE = 10,
+  DELETED_MANIFEST_V2 = 11,
+  // BASENAME_SUFFIX_SKELETON_MANIFEST // Deprecated - please use BSSM_V3
+  COMMIT_HANDLE = 13,
+  GIT_DELTA_MANIFEST = 14,
+  TEST_MANIFEST = 15,
+  TEST_SHARDED_MANIFEST = 16,
+  BSSM_V3 = 17,
+} (rust.exhaustive)
--- eden/mononoke/derived_data/manager/derivable.rs
+++ eden/mononoke/derived_data/manager/derivable.rs
@@ -13,6 +13,7 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use anyhow::anyhow;
+use anyhow::bail;
 use anyhow::Result;
 use async_trait::async_trait;
 use context::CoreContext;
@@ -25,9 +26,11 @@ use mononoke_types::BonsaiChangeset;
 use mononoke_types::ChangesetId;
 use strum::AsRefStr;
 use strum::Display;
+use strum::EnumIter;
 use strum::EnumString;
 
 use crate::context::DerivationContext;
+use crate::thrift;
 use crate::DerivationError;
 use crate::DerivedDataManager;
 use crate::Rederivation;
@@ -39,7 +42,9 @@ use crate::Rederivation;
 /// particular derived data type.
 /// It also provides `as_ref()` method for serialization.
 /// and implements FromStr trait for deserialization.
-#[derive(AsRefStr, EnumString, Display, Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(
+    AsRefStr, EnumString, Display, Debug, Clone, PartialEq, Eq, Hash, EnumIter
+)]
 pub enum DerivableType {
     BlameV2,
     BssmV3,
@@ -76,6 +81,49 @@ impl DerivableType {
             DerivableType::TestManifest => "testmanifest",
             DerivableType::TestShardedManifest => "testshardedmanifest",
             DerivableType::Unodes => "unodes",
+        }
+    }
+    pub fn from_thrift(other: thrift::DerivedDataType) -> Result<Self> {
+        Ok(match other {
+            thrift::DerivedDataType::BLAME => Self::BlameV2,
+            thrift::DerivedDataType::BSSM_V3 => Self::BssmV3,
+            thrift::DerivedDataType::CHANGESET_INFO => Self::ChangesetInfo,
+            thrift::DerivedDataType::DELETED_MANIFEST_V2 => Self::DeletedManifests,
+            thrift::DerivedDataType::FASTLOG => Self::Fastlog,
+            thrift::DerivedDataType::FILENODE => Self::FileNodes,
+            thrift::DerivedDataType::FSNODE => Self::Fsnodes,
+            thrift::DerivedDataType::HG_CHANGESET => Self::HgChangesets,
+            thrift::DerivedDataType::TREE_HANDLE => Self::GitTree,
+            thrift::DerivedDataType::COMMIT_HANDLE => Self::GitCommit,
+            thrift::DerivedDataType::GIT_DELTA_MANIFEST => Self::GitDeltaManifest,
+            thrift::DerivedDataType::SKELETON_MANIFEST => Self::SkeletonManifests,
+            thrift::DerivedDataType::TEST_MANIFEST => Self::TestManifest,
+            thrift::DerivedDataType::TEST_SHARDED_MANIFEST => Self::TestShardedManifest,
+            thrift::DerivedDataType::UNODE => Self::Unodes,
+            _ => bail!("invalid thrift value for DerivedDataType: {:?}", other),
+        })
+    }
+    pub fn into_thrift(&self) -> thrift::DerivedDataType {
+        match self {
+            Self::BlameV2 => thrift::DerivedDataType::BLAME,
+            Self::BssmV3 => thrift::DerivedDataType::BSSM_V3,
+            Self::ChangesetInfo => thrift::DerivedDataType::CHANGESET_INFO,
+            Self::DeletedManifests => thrift::DerivedDataType::DELETED_MANIFEST_V2,
+            Self::Fastlog => thrift::DerivedDataType::FASTLOG,
+            Self::FileNodes => thrift::DerivedDataType::FILENODE,
+            Self::Fsnodes => thrift::DerivedDataType::FSNODE,
+            Self::HgChangesets => thrift::DerivedDataType::HG_CHANGESET,
+            Self::GitTree => thrift::DerivedDataType::TREE_HANDLE,
+            Self::GitCommit => thrift::DerivedDataType::COMMIT_HANDLE,
+            Self::GitDeltaManifest => thrift::DerivedDataType::GIT_DELTA_MANIFEST,
+            Self::SkeletonManifests => thrift::DerivedDataType::SKELETON_MANIFEST,
+            Self::TestManifest => thrift::DerivedDataType::TEST_MANIFEST,
+            Self::TestShardedManifest => thrift::DerivedDataType::TEST_SHARDED_MANIFEST,
+            Self::Unodes => thrift::DerivedDataType::UNODE,
+            // If the compiler reminds you to add something here, please don't forget to also
+            // update the `from_thrift` implementation above.
+            // The unit test: `thrift_derived_data_type_conversion_must_be_bidirectional` in this
+            // file should prevent you from forgetting at diff time.
         }
     }
 }
@@ -375,4 +423,22 @@ macro_rules! dependencies {
     () => { () };
     ($dep:ty) => { ( $dep , () ) };
     ($dep:ty, $( $rest:tt )*) => { ( $dep , dependencies!($( $rest )*) ) };
+}
+
+#[cfg(test)]
+mod tests {
+    use strum::IntoEnumIterator;
+
+    use super::DerivableType;
+
+    #[test]
+    fn thrift_derived_data_type_conversion_must_be_bidirectional() {
+        for variant in DerivableType::iter() {
+            assert_eq!(
+                variant,
+                DerivableType::from_thrift(variant.into_thrift())
+                    .expect("Failed to convert back to DerivableType from thrift")
+            )
+        }
+    }
 }
