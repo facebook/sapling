08af0c9c1c565638dc838a8c10d3976f3cb740ed
Jun Wu <quark@meta.com>
2024-03-13 10:29:13 -0700

    SelectAllCommits: decouple from linearizedCommitHistory
    Summary: I'm going to drop `linearizedCommitHistory`. This removes one remaining user.
    
    Reviewed By: evangrayk
    
    Differential Revision: D54831198
    
    fbshipit-source-id: 3134978cdd5b1208a9e58cc9412b5c75658e2ca5

--- addons/isl/src/SelectAllCommits.tsx
+++ addons/isl/src/SelectAllCommits.tsx
@@ -11,22 +11,21 @@ import {Tooltip} from './Tooltip';
 import {islDrawerState} from './drawerState';
 import {t, T} from './i18n';
 import {readAtom, writeAtom} from './jotaiUtils';
-import {linearizedCommitHistory, selectedCommits} from './selection';
+import {dagWithPreviews} from './previews';
+import {selectedCommits} from './selection';
 import {VSCodeButton} from '@vscode/webview-ui-toolkit/react';
 import {useCallback} from 'react';
 import {Icon} from 'shared/Icon';
 import {KeyCode, Modifier} from 'shared/KeyboardShortcuts';
 
-export function getAllDraftCommits(): Array<string> {
-  const commits = readAtom(linearizedCommitHistory);
-  if (commits == null) {
-    return [];
-  }
-  const draftCommits = commits
-    .filter(commit => commit.phase !== 'public' && !commit.hash.startsWith('OPTIMISTIC'))
-    .map(commit => commit.hash);
-  draftCommits.reverse();
-  return draftCommits;
+function getAllDraftCommits(): Set<string> {
+  const dag = readAtom(dagWithPreviews);
+  return new Set(
+    dag
+      .draft()
+      .toArray()
+      .filter(hash => !hash.startsWith('OPTIMISTIC')),
+  );
 }
 
 export function useSelectAllCommitsShortcut() {
@@ -37,7 +36,7 @@ export function useSelectAllCommitsShortcut() {
 export function useSelectAllCommits() {
   return useCallback(() => {
     const draftCommits = getAllDraftCommits();
-    writeAtom(selectedCommits, new Set(draftCommits));
+    writeAtom(selectedCommits, draftCommits);
     // pop open sidebar so you can act on the bulk selection
     writeAtom(islDrawerState, last => ({
       ...last,
