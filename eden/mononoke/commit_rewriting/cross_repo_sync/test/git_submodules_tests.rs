98ff48b3c3fcec1e7503fffb77ca3995d165d864
Gustavo Galvao Avena <gustavoavena@meta.com>
2024-03-20 06:50:10 -0700

    Unit tests for deletion in submodule expansion
    Summary:
    ## This stack
    See D55064943.
    
    ## This diff
    Adds unit tests for
    1. Deleting a submodule in the source repo (repo A),
    2. Implicitly deleting **a submodule** in the source repo (repo A)
    3. Implicitly deleting directories in the submodule repo (repo B).
    
    The first test is already working as expected, but the other two are not. These will be updated to match the expected behaviour soon, when I work on properly supporting implicit deletes in the submodule expansion.
    
    I'm also adding TODO comments for other test cases I want to add in the next diffs.
    
    Reviewed By: mitrandir77
    
    Differential Revision: D55064942
    
    fbshipit-source-id: e8ea822180337c9ced1b387ecb0a61a164dc6920

--- eden/mononoke/commit_rewriting/cross_repo_sync/test/git_submodules_tests.rs
+++ eden/mononoke/commit_rewriting/cross_repo_sync/test/git_submodules_tests.rs
@@ -16,6 +16,7 @@ use anyhow::Result;
 use context::CoreContext;
 use fbinit::FacebookInit;
 use git_types::MappedGitCommitId;
+use maplit::btreemap;
 use mononoke_types::ChangesetId;
 use mononoke_types::FileType;
 use mononoke_types::NonRootMPath;
@@ -151,3 +152,241 @@ async fn test_submodule_expansion_basic(fb: FacebookInit) -> Result<()> {
 
     Ok(())
 }
+
+// ------------------------- Deletions ----------------------------
+
+/// Deleting an entire submodule in the small repo (i.e. repo_a) should delete
+/// its expansion and its metadata file in repo_a folder in the large repo.
+#[fbinit::test]
+async fn test_submodule_deletion(fb: FacebookInit) -> Result<()> {
+    let ctx = CoreContext::test_mock(fb.clone());
+
+    let SubmoduleSyncTestData {
+        repo_a_info: (repo_a, repo_a_cs_map),
+        large_repo,
+        commit_syncer,
+        ..
+    } = build_submodule_sync_test_data(fb).await?;
+
+    const MESSAGE: &str = "Delete repo_b submodule in repo_a";
+    let cs_id = CreateCommitContext::new(&ctx, &repo_a, vec![*repo_a_cs_map.get("A_C").unwrap()])
+        .set_message(MESSAGE)
+        .delete_file("submodules/repo_b")
+        .commit()
+        .await?;
+
+    let large_repo_cs_id = sync_to_master(ctx.clone(), &commit_syncer, cs_id)
+        .await?
+        .ok_or(anyhow!("Failed to sync commit deleting submodule B"))?;
+
+    println!("large_repo_cs_id: {}", large_repo_cs_id);
+
+    check_mapping(
+        ctx.clone(),
+        &commit_syncer,
+        cs_id,
+        ChangesetId::from_str("04fc8faa78bf6eb8e3f75fa34ba823577c8b78cb4428e881b0d7b7db956630b5")
+            .ok(),
+    )
+    .await;
+
+    let large_repo_changesets = get_all_changeset_data_from_repo(&ctx, &large_repo).await?;
+
+    compare_expected_changesets_from_basic_setup(
+        large_repo_changesets,
+        vec![ExpectedChangeset::new_by_file_change(
+            MESSAGE,
+            // No regular file changes
+            vec![],
+            // Files being deleted
+            vec![
+                "repo_a/submodules/.x-repo-submodule-repo_b",
+                "repo_a/submodules/repo_b/B_A",
+                "repo_a/submodules/repo_b/B_B",
+            ],
+        )],
+    )?;
+
+    Ok(())
+}
+
+// ------------------------- Implicit deletes ----------------------------
+
+// Cover implicit deletions.
+// Implicitly deleting **a submodule** means adding a file in **repo A** at the
+// same path of the repo B submodule.
+// This should delete repo B's expansion in the large repo along with its
+// metadata file.
+//
+// This differs from implicitly deleting directories **in the submodule repo**,
+// which corresponds to an implicit deletion **in repo B** that should be propagated
+// to its expansion in the large repo.
+
+/// Implicitly deleting a submodule in the source repo (i.e. by adding a file
+/// with the same path) should delete the **entire submodule expansion and its
+/// metadata file**, like when the submodule itself is manually deleted.
+#[fbinit::test]
+async fn test_implicitly_deleting_submodule(fb: FacebookInit) -> Result<()> {
+    let ctx = CoreContext::test_mock(fb.clone());
+
+    let SubmoduleSyncTestData {
+        repo_a_info: (repo_a, repo_a_cs_map),
+        large_repo,
+        commit_syncer,
+        ..
+    } = build_submodule_sync_test_data(fb).await?;
+
+    const MESSAGE: &str = "Implicitly delete repo_b submodule in repo_a";
+
+    let cs_id = CreateCommitContext::new(&ctx, &repo_a, vec![*repo_a_cs_map.get("A_C").unwrap()])
+        .set_message(MESSAGE)
+        .add_file("submodules/repo_b", "File implicitly deleting submodule")
+        .commit()
+        .await?;
+
+    let large_repo_cs_id = sync_to_master(ctx.clone(), &commit_syncer, cs_id)
+        .await?
+        .ok_or(anyhow!("Failed to sync commit"))?;
+
+    println!("large_repo_cs_id: {}", large_repo_cs_id);
+
+    let large_repo_changesets = get_all_changeset_data_from_repo(&ctx, &large_repo).await?;
+
+    let expected_cs_id =
+        ChangesetId::from_str("b0db847efd159d8c84d9227c6ae2ac74caee7ff0c07543c034472b596f1af52c")
+            .unwrap();
+
+    check_mapping(ctx.clone(), &commit_syncer, cs_id, Some(expected_cs_id)).await;
+
+    compare_expected_changesets_from_basic_setup(
+        large_repo_changesets,
+        vec![ExpectedChangeset::new_by_file_change(
+            MESSAGE,
+            // Add a regular file in the same path as the submodule expansion
+            vec!["repo_a/submodules/repo_b"],
+            // Files being deleted
+            vec![
+                // The submodule metadata file should also be deleted
+                // TODO(T179534458): delete metadata file when submodule is implicitly deleted
+                // "repo_a/submodules/.x-repo-submodule-repo_b",
+
+                // The submodule expansion should be entirely deleted
+                // TODO(T179534458): properly support submodule implicit deletion
+                // "repo_a/submodules/repo_b/B_A",
+                // "repo_a/submodules/repo_b/B_B",
+            ],
+        )],
+    )?;
+    Ok(())
+}
+
+/// Implicitly deleting files in the submodule repo (repo_b) should generate the
+/// proper deletions in its expansion.
+#[fbinit::test]
+async fn test_implicit_deletions_inside_submodule_repo(fb: FacebookInit) -> Result<()> {
+    let ctx = CoreContext::test_mock(fb.clone());
+
+    let SubmoduleSyncTestData {
+        repo_a_info: (repo_a, repo_a_cs_map),
+        repo_b_info: (repo_b, repo_b_cs_map),
+        large_repo,
+        commit_syncer,
+        ..
+    } = build_submodule_sync_test_data(fb).await?;
+
+    // Create a directory with 2 files in repo B
+    let add_directory_in_repo_b =
+        CreateCommitContext::new(&ctx, &repo_b, vec![*repo_b_cs_map.get("B_B").unwrap()])
+            .set_message("Create directory in repo B")
+            .add_files(btreemap! {"some_dir/file_x" => "File X", "some_dir/file_y" => "File Y"})
+            .commit()
+            .await?;
+
+    // Create a file in the same path to implicitly delete the directory
+    let repo_b_cs_id = CreateCommitContext::new(&ctx, &repo_b, vec![add_directory_in_repo_b])
+        .set_message("Implicitly delete directory in repo B")
+        .add_file("some_dir", "some_dir is now a file")
+        .commit()
+        .await?;
+
+    let repo_b_mapped_git_commit = repo_b
+        .repo_derived_data()
+        .derive::<MappedGitCommitId>(&ctx, repo_b_cs_id)
+        .await?;
+
+    let repo_b_git_commit_hash = *repo_b_mapped_git_commit.oid();
+
+    // Update repo B submodule pointer in repo A to point to the last commit
+    // with the implicit deletions.
+    const MESSAGE: &str = "Update submodule after implicit deletions";
+    let cs_id = CreateCommitContext::new(&ctx, &repo_a, vec![*repo_a_cs_map.get("A_C").unwrap()])
+        .set_message(MESSAGE)
+        .add_file_with_type(
+            "submodules/repo_b",
+            repo_b_git_commit_hash.into_inner(),
+            FileType::GitSubmodule,
+        )
+        .commit()
+        .await?;
+
+    let _large_repo_cs_id = sync_to_master(ctx.clone(), &commit_syncer, cs_id)
+        .await?
+        .ok_or(anyhow!("Failed to sync commit"))?;
+
+    let large_repo_changesets = get_all_changeset_data_from_repo(&ctx, &large_repo).await?;
+
+    let expected_cs_id =
+        ChangesetId::from_str("7fc76cfa1906ccc74f86322cf529c5508867b5cfef8b80fb65a425e835f4b92b")
+            .unwrap();
+
+    check_mapping(ctx.clone(), &commit_syncer, cs_id, Some(expected_cs_id)).await;
+
+    compare_expected_changesets_from_basic_setup(
+        large_repo_changesets,
+        vec![ExpectedChangeset::new_by_file_change(
+            MESSAGE,
+            // Submodule metadata file is updated
+            vec![
+                "repo_a/submodules/.x-repo-submodule-repo_b",
+                "repo_a/submodules/repo_b/some_dir",
+            ],
+            // Files being implicitly deleted
+            // TODO(T179534458): properly support submodule implicit deletion
+            vec![
+                // "repo_a/submodules/repo_b/some_dir/file_x",
+                // "repo_a/submodules/repo_b/some_dir/file_y"
+            ],
+        )],
+    )?;
+
+    check_submodule_metadata_file_in_large_repo(
+        &ctx,
+        &large_repo,
+        expected_cs_id,
+        NonRootMPath::new("repo_a/submodules/.x-repo-submodule-repo_b")?,
+        &repo_b_git_commit_hash,
+    )
+    .await?;
+
+    Ok(())
+}
+
+// TODO(T179534458): test implicitly deleting file by adding submodule in same
+// path
+
+// TODO(T179534458): test implicitly deleting directory by adding submodule in same
+// path
+
+// ------------------ Unexpected state / Error handling ------------------
+
+// TODO(T181473986): modify expansion manually without updating metadata file
+// and assert that sync fails.
+
+// TODO(T179533620): test that modifying submodule metadata file manually without
+// the correct changes to the expansion will fail the sync.
+
+// TODO(T179533620): test that deleting submodule metadata file manually
+// fails the sync
+
+// TODO(T181473986): test that sync fails when submodule action is expand
+// and submodule dependency repo is not available
