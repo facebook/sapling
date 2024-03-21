da76fc010369c71e3ef621b3029c6e664a6e2e4d
Jun Wu <quark@meta.com>
2024-03-21 16:48:06 -0700

    workingcopy: implement WorkingCopyClient for git
    Summary: One step torwards makig the Python edenfs dirstate to work with git.
    
    Reviewed By: muirdm
    
    Differential Revision: D54218729
    
    fbshipit-source-id: ea491846380a1be4a2370aebbec88081a1829f6b

--- eden/scm/lib/workingcopy/src/client.rs
+++ eden/scm/lib/workingcopy/src/client.rs
@@ -9,6 +9,7 @@ use std::any::Any;
 use std::collections::BTreeMap;
 
 use anyhow::Result;
+use gitcompat::rungit::RunGitOptions;
 use types::workingcopy_client::CheckoutConflict;
 use types::workingcopy_client::CheckoutMode;
 use types::workingcopy_client::FileStatus;
@@ -70,6 +71,93 @@ impl WorkingCopyClient for edenfs_client::EdenFsClient {
     ) -> Result<Vec<CheckoutConflict>> {
         tracing::debug!(p1=?node, p1_tree=?tree_node, mode=?mode, "checkout");
         edenfs_client::EdenFsClient::checkout(self, node, tree_node, mode)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self as &dyn Any
+    }
+}
+
+impl WorkingCopyClient for RunGitOptions {
+    fn get_status(
+        &self,
+        node: HgId,
+        list_ignored: bool,
+    ) -> Result<BTreeMap<RepoPathBuf, FileStatus>> {
+        tracing::debug!(node=?node, list_ignored=list_ignored, "get_status");
+        let args = [
+            "--porcelain=1",
+            "--ignore-submodules=dirty",
+            "--untracked-files=all",
+            "--no-renames",
+            "-z",
+            if list_ignored {
+                "--ignored"
+            } else {
+                "--ignored=no"
+            },
+        ];
+        let out = self.call("status", &args)?;
+
+        // Example output:
+        // AD file/path   (added to index, deleted on disk)
+        // See https://github.com/git/git/blob/2162f9f6f86df4f49c3a716b5beb3952104ea8b8/Documentation/git-status.txt#L218-L244
+
+        let changes = out
+            .stdout
+            .split(|&c| c == 0)
+            .filter_map(|line| {
+                if line.get(2) != Some(&b' ') {
+                    // Unknown format. Ignore.
+                    return None;
+                }
+                let path_bytes = line.get(3..)?;
+                let path = RepoPathBuf::from_utf8(path_bytes.to_vec()).ok()?;
+                let (mut x, y) = (line[0] /* index */, line[1] /* working copy */);
+                // If deleted in working copy, consider as deletion.
+                if y == b'D' {
+                    x = y
+                };
+                let status = match x {
+                    b'D' => FileStatus::Removed,
+                    b'A' | b'?' => FileStatus::Added,
+                    b'!' => FileStatus::Ignored,
+                    _ => FileStatus::Modified,
+                };
+                Some((path, status))
+            })
+            .collect();
+        Ok(changes)
+    }
+
+    fn set_parents(&self, p1: HgId, p2: Option<HgId>, p1_tree: HgId) -> Result<()> {
+        tracing::debug!(?p1, ?p2, ?p1_tree, "set_parents");
+        let p1_hex = p1.to_hex();
+        // TODO: What to do with p2 and p1 tree?
+        self.call("update-ref", &["HEAD", &p1_hex])?;
+        Ok(())
+    }
+
+    fn checkout(
+        &self,
+        node: HgId,
+        tree_node: HgId,
+        mode: CheckoutMode,
+    ) -> Result<Vec<CheckoutConflict>> {
+        tracing::debug!(p1=?node, p1_tree=?tree_node, mode=?mode, "checkout");
+        // TODO: Conflicts are not reported properly. Are they needed?
+        let hex = node.to_hex();
+        match mode {
+            CheckoutMode::Normal => {
+                self.run("checkout", &["-d", "--recurse-submodules", &hex])?;
+                Ok(Vec::new())
+            }
+            CheckoutMode::Force => {
+                self.run("checkout", &["-f", "-d", "--recurse-submodules", &hex])?;
+                Ok(Vec::new())
+            }
+            CheckoutMode::DryRun => Ok(Vec::new()),
+        }
     }
 
     fn as_any(&self) -> &dyn Any {
