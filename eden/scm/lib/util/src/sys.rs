c66fa30274af0f9ce80d6243f14131fd1eac9e56
Muir Manders <muirdm@meta.com>
2024-03-21 09:31:05 -0700

    util: use stub hostname in tests
    Summary: Tests probably want a stable hostname. But the real reason I'm doing this is that I discovered whoami::hostname() silently crashes on mac, sometimes. I assume it is the same fork detection we have seen in some mac libraries in the past. This may also be the recent reason why chg no longer works on the mac (similar symptom: crashes silently with no output).
    
    Reviewed By: sggutier
    
    Differential Revision: D55171091
    
    fbshipit-source-id: f39980fffdbe226fe0b6e2f022582656dba51bc5

--- eden/scm/lib/util/src/sys.rs
+++ eden/scm/lib/util/src/sys.rs
@@ -8,7 +8,15 @@
 use std::borrow::Cow;
 
 pub fn hostname() -> String {
-    whoami::devicename()
+    if std::env::var_os("TESTTMP").is_some() || cfg!(test) {
+        // Doesn't seem like we want to use the real hostname in tests.
+        // Also, this may fix some debugruntest issues on mac due to whoami issues.
+        "test-hostname".to_owned()
+    } else {
+        // On Mac this calls system library functions, so may not be post fork safe.
+        // This could be one of the reasons chg doesn't work on mac anymore...
+        whoami::devicename()
+    }
 }
 
 pub fn username() -> String {
