280d600ff2aeff493bb7db28754630602162ffa4
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2024-03-25 09:15:11 -0700

    mononoke: cc: Create commit cloud service folder
    Summary: This implements a very simple rust_binary dependant on a rust library (where most logic will be constructed). Not yet wired into mononoke, just outlying the dir structure.
    
    Reviewed By: markbt
    
    Differential Revision: D55076442
    
    fbshipit-source-id: e41aa2912a6945fabd799975f247505684947a6b

---
+++ eden/mononoke/commit_cloud/src/lib.rs
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+pub fn dummy_fn() {
+    println!("Welcome to the commit cloud service!");
+}
