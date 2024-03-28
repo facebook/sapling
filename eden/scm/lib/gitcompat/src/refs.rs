1987a67607c78fa82dd9fc4dac0527d378d2e675
Jun Wu <quark@meta.com>
2024-03-28 09:01:32 -0700

    gitcompat: report dangling HEAD as null
    Summary:
    In Git it's possible to have `HEAD` points to a non-existed reference like `refs/heads/master`.
    It happens after creating a new repo but could also mean that the working copy does not
    checkout any commit yet while the repo is non-empty.
    
    In Sapling/Mercurial those situations are handled by a "null" dirstate p1. Translate the dangling
    reference to "null" so Sapling can handle those cases.
    
    Reviewed By: zzl0
    
    Differential Revision: D55382720
    
    fbshipit-source-id: 715ab45b2eef5f9b2a60a945f7cd10c1f5b4110d

--- eden/scm/lib/gitcompat/src/refs.rs
+++ eden/scm/lib/gitcompat/src/refs.rs
@@ -14,17 +14,30 @@ use crate::rungit::RunGitOptions;
 impl RunGitOptions {
     /// Resolve the hash of Git "HEAD", aka, ".".
     pub fn resolve_head(&self) -> Result<HgId> {
+        // Whether HEAD might be a dancling pointer, like "ref: refs/heads/...".
+        // Used to appromiately detect the "null" case.
+        let mut maybe_dancling = false;
         // Attempt to look at ".git/HEAD" directly for performance.
         if let Some(git_dir) = self.git_dir.as_ref() {
             let data = fs::read_to_string(git_dir.join("HEAD"))?;
-            let data = data.trim_end();
-            if let Ok(id) = HgId::from_hex(data.as_bytes()) {
+            if let Ok(id) = HgId::from_hex(data.trim_end().as_bytes()) {
                 return Ok(id);
             }
+            maybe_dancling = data.starts_with("ref: refs/heads/");
         }
 
         // Fallback to `git show-ref`
-        let out = self.call("show-ref", &["--head", "--hash", "HEAD"])?;
+        let out = match self.call("show-ref", &["--head", "--hash", "HEAD"]) {
+            Ok(out) => out,
+            Err(e) => {
+                // If the reference does not exist (newly created empty repo, or repo with commits
+                // but without a working copy), the command will fail. Report as "null".
+                if maybe_dancling {
+                    return Ok(*HgId::null_id());
+                }
+                return Err(e.into());
+            }
+        };
         if let Some(data) = out.stdout.get(..HgId::hex_len()) {
             let id = HgId::from_hex(data)?;
             return Ok(id);
