7bf89f39a273de817468e13ffacc497e6870f36e
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2024-03-29 22:05:52 -0700

    mononoke: cc: add commit cloud connector
    Summary: Based off the [mercurial connection]( https://www.internalfb.com/code/fbsource/fbcode/eden/mononoke/mercurial/mutation/src/store.rs) and the [bookmarks service](https://www.internalfb.com/code/fbsource/[d407ba831217283fccda2fe34cd55451a3e129ba]/fbcode/eden/mononoke/bookmarks/dbbookmarks/src/builder.rs?lines=20)
    
    Reviewed By: markbt
    
    Differential Revision: D55163853
    
    fbshipit-source-id: 42188e0a6d2a362ec71dba278d931565a49ba148

---
+++ eden/mononoke/commit_cloud/src/builder.rs
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+use sql_construct::SqlConstruct;
+use sql_ext::SqlConnections;
+
+pub struct SqlCommitCloudBuilder {
+    #[allow(unused)]
+    pub(crate) connections: SqlConnections,
+}
+
+impl SqlConstruct for SqlCommitCloudBuilder {
+    const LABEL: &'static str = "commit_cloud";
+
+    const CREATION_QUERY: &'static str = include_str!("../schemas/sqlite-commit-cloud.sql");
+
+    fn from_sql_connections(connections: SqlConnections) -> Self {
+        Self { connections }
+    }
+}
--- eden/mononoke/commit_cloud/src/lib.rs
+++ eden/mononoke/commit_cloud/src/lib.rs
@@ -5,6 +5,7 @@
  * GNU General Public License version 2.
  */
 
+pub mod builder;
 pub fn dummy_fn() {
     println!("Welcome to the commit cloud service!");
 }
