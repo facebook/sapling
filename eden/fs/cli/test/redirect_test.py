57aefda2cdad23cc4c26d2f9dfdff334f7d04de9
Carlos Fernandez <carlosfsanz@meta.com>
2024-04-10 09:41:20 -0700

    Eden CLI: Remove dead code in redirect.py
    Summary:
    the redirect code has been rewritten in Rust, so changes in redirect.py are useless.
    Let's not keep dead code around, it's just confusing.
    
    - Promote "redirect" by removing it from EXPERIMENTAL_COMMANDS, which forces that only the rust implementation will be used.
    - Removed the command parser from the python code, meaning that "eden redirect" is no longer a valid command if the python code is being used.
    - Removed the tests that tested the python code (only).
    - Some code in redirect.py is still used (utility functions, unmount) from other places. I've left those alone, except some minor refactoring required to leave things working.
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D55426809
    
    fbshipit-source-id: b26e3bc2c6ed153225e0792ce878f2a95e058174

--- eden/fs/cli/test/redirect_test.py
+++ eden/fs/cli/test/redirect_test.py
@@ -6,14 +6,13 @@
 
 # pyre-strict
 
-import argparse
 import os
 import unittest
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 from eden.fs.cli.doctor.test.lib.fake_eden_instance import FakeEdenInstance
-from eden.fs.cli.redirect import FixupCmd, RedirectionState, RedirectionType
+from eden.fs.cli.redirect import RedirectionState, RedirectionType
 from eden.test_support.temporary_directory import TemporaryDirectoryMixin
 
 from ..redirect import Redirection, RepoPathDisposition
@@ -54,111 +53,3 @@ class RedirectTest(unittest.TestCase, TemporaryDirectoryMixin):
 
         error_msg = f"Failed to remove {repo_path} since the bind unmount failed"
         self.assertEqual(str(ex.exception), error_msg)
-
-    @patch("eden.fs.cli.redirect.Redirection.apply")
-    @patch("eden.fs.cli.redirect.Redirection.remove_existing")
-    @patch("eden.fs.cli.cmd_util.require_checkout")
-    @patch("eden.fs.cli.redirect.get_effective_redirections")
-    def test_fixup_all_resources(
-        self,
-        mock_get_effective_redirections: MagicMock,
-        mock_require_checkout: MagicMock,
-        mock_remove_existing: MagicMock,
-        mock_apply: MagicMock,
-    ) -> None:
-        temp_dir = self.make_temporary_directory()
-        repo_path = os.path.join(temp_dir, "test")
-        instance = FakeEdenInstance(temp_dir)
-        checkout = instance.create_test_mount("mount_dir")
-        eden_path = os.path.join(temp_dir, "mount_dir")
-        mock_require_checkout.return_value = (instance, checkout, eden_path)
-
-        mock_argument_parser = MagicMock(spec=argparse.ArgumentParser)
-        args = argparse.Namespace(mount=eden_path, only_repo_source=False)
-
-        redir = Redirection(
-            repo_path=Path(repo_path),
-            redir_type=RedirectionType.BIND,
-            target=None,
-            source="mount",
-            state=RedirectionState.UNKNOWN_MOUNT,
-        )
-        mock_get_effective_redirections.return_value = {repo_path: redir}
-
-        test_fixup_cmd = FixupCmd(mock_argument_parser)
-        test_fixup_cmd.run(args)
-
-        mock_remove_existing.assert_called_once()
-        mock_apply.assert_called_once()
-
-    @patch("eden.fs.cli.redirect.Redirection.apply")
-    @patch("eden.fs.cli.redirect.Redirection.remove_existing")
-    @patch("eden.fs.cli.cmd_util.require_checkout")
-    @patch("eden.fs.cli.redirect.get_effective_redirections")
-    def test_fixup_only_eden_redirection(
-        self,
-        mock_get_effective_redirections: MagicMock,
-        mock_require_checkout: MagicMock,
-        mock_remove_existing: MagicMock,
-        mock_apply: MagicMock,
-    ) -> None:
-        temp_dir = self.make_temporary_directory()
-        repo_path = os.path.join(temp_dir, "test")
-        instance = FakeEdenInstance(temp_dir)
-        checkout = instance.create_test_mount("mount_dir")
-        eden_path = os.path.join(temp_dir, "mount_dir")
-        mock_require_checkout.return_value = (instance, checkout, eden_path)
-
-        mock_argument_parser = MagicMock(spec=argparse.ArgumentParser)
-        args = argparse.Namespace(mount=eden_path, only_repo_source=True)
-
-        redir = Redirection(
-            repo_path=Path(repo_path),
-            redir_type=RedirectionType.BIND,
-            target=None,
-            source="mount",
-            state=RedirectionState.UNKNOWN_MOUNT,
-        )
-        mock_get_effective_redirections.return_value = {repo_path: redir}
-
-        test_fixup_cmd = FixupCmd(mock_argument_parser)
-        test_fixup_cmd.run(args)
-
-        mock_remove_existing.assert_not_called()
-        mock_apply.assert_not_called()
-
-    @patch("eden.fs.cli.redirect.Redirection.apply")
-    @patch("eden.fs.cli.redirect.Redirection.remove_existing")
-    @patch("eden.fs.cli.cmd_util.require_checkout")
-    @patch("eden.fs.cli.redirect.get_effective_redirections")
-    def test_fixup_dir_in_eden_redirection(
-        self,
-        mock_get_effective_redirections: MagicMock,
-        mock_require_checkout: MagicMock,
-        mock_remove_existing: MagicMock,
-        mock_apply: MagicMock,
-    ) -> None:
-        temp_dir = self.make_temporary_directory()
-        repo_path = os.path.join(temp_dir, "test")
-        instance = FakeEdenInstance(temp_dir)
-        checkout = instance.create_test_mount("mount_dir")
-        eden_path = os.path.join(temp_dir, "mount_dir")
-        mock_require_checkout.return_value = (instance, checkout, eden_path)
-
-        mock_argument_parser = MagicMock(spec=argparse.ArgumentParser)
-        args = argparse.Namespace(mount=eden_path, only_repo_source=True)
-
-        redir = Redirection(
-            repo_path=Path(repo_path),
-            redir_type=RedirectionType.BIND,
-            target=None,
-            source=".eden-redirections",
-            state=RedirectionState.UNKNOWN_MOUNT,
-        )
-        mock_get_effective_redirections.return_value = {repo_path: redir}
-
-        test_fixup_cmd = FixupCmd(mock_argument_parser)
-        test_fixup_cmd.run(args)
-
-        mock_remove_existing.assert_called_once()
-        mock_apply.assert_called_once()
