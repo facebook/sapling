b3aa9e4ef8865092aa070e790564cc6000a9edd6
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2024-04-10 09:08:28 -0700

    commit cloud: test for history SQL ops
    Summary: I usually put this in the same diff but previous one was big enough so putting it here. Simple CRUD test like other structs.
    
    Reviewed By: mitrandir77
    
    Differential Revision: D55900632
    
    fbshipit-source-id: 50b8c9d7d9d606f4c9256d7f8f5ba8ef23da0ddc

--- eden/mononoke/commit_cloud/tests/main.rs
+++ eden/mononoke/commit_cloud/tests/main.rs
@@ -13,7 +13,6 @@ use commit_cloud::sql::checkout_locations::WorkspaceCheckoutLocation;
 use commit_cloud::sql::heads::WorkspaceHead;
 use commit_cloud::sql::local_bookmarks::WorkspaceLocalBookmark;
 use commit_cloud::sql::ops::Delete;
-use commit_cloud::sql::ops::Get;
 use commit_cloud::sql::ops::Insert;
 use commit_cloud::sql::remote_bookmarks::WorkspaceRemoteBookmark;
 use commit_cloud::sql::snapshots::WorkspaceSnapshot;
@@ -25,6 +24,7 @@ use sql_construct::SqlConstruct;
 
 #[fbinit::test]
 async fn test_checkout_locations(_fb: FacebookInit) -> anyhow::Result<()> {
+    use commit_cloud::sql::ops::Get;
     let sql = SqlCommitCloudBuilder::with_sqlite_in_memory()?.new();
     let reponame = "test_repo".to_owned();
     let workspace = "user_testuser_default".to_owned();
@@ -50,6 +50,7 @@ async fn test_checkout_locations(_fb: FacebookInit) -> anyhow::Result<()> {
 
 #[fbinit::test]
 async fn test_snapshots(_fb: FacebookInit) -> anyhow::Result<()> {
+    use commit_cloud::sql::ops::Get;
     use commit_cloud::sql::snapshots::DeleteArgs;
 
     let sql = SqlCommitCloudBuilder::with_sqlite_in_memory()?.new();
@@ -92,6 +93,7 @@ async fn test_snapshots(_fb: FacebookInit) -> anyhow::Result<()> {
 #[fbinit::test]
 async fn test_heads(_fb: FacebookInit) -> anyhow::Result<()> {
     use commit_cloud::sql::heads::DeleteArgs;
+    use commit_cloud::sql::ops::Get;
     let sql = SqlCommitCloudBuilder::with_sqlite_in_memory()?.new();
     let reponame = "test_repo".to_owned();
     let workspace = "user_testuser_default".to_owned();
@@ -131,6 +133,7 @@ async fn test_heads(_fb: FacebookInit) -> anyhow::Result<()> {
 #[fbinit::test]
 async fn test_local_bookmarks(_fb: FacebookInit) -> anyhow::Result<()> {
     use commit_cloud::sql::local_bookmarks::DeleteArgs;
+    use commit_cloud::sql::ops::Get;
 
     let sql = SqlCommitCloudBuilder::with_sqlite_in_memory()?.new();
     let reponame = "test_repo".to_owned();
@@ -172,6 +175,7 @@ async fn test_local_bookmarks(_fb: FacebookInit) -> anyhow::Result<()> {
 
 #[fbinit::test]
 async fn test_remote_bookmarks(_fb: FacebookInit) -> anyhow::Result<()> {
+    use commit_cloud::sql::ops::Get;
     use commit_cloud::sql::remote_bookmarks::DeleteArgs;
     let sql = SqlCommitCloudBuilder::with_sqlite_in_memory()?.new();
     let reponame = "test_repo".to_owned();
@@ -218,6 +222,7 @@ async fn test_remote_bookmarks(_fb: FacebookInit) -> anyhow::Result<()> {
 
 #[fbinit::test]
 async fn test_versions(_fb: FacebookInit) -> anyhow::Result<()> {
+    use commit_cloud::sql::ops::Get;
     let sql = SqlCommitCloudBuilder::with_sqlite_in_memory()?.new();
     let reponame = "test_repo".to_owned();
     let workspace = "user_testuser_default".to_owned();
@@ -234,6 +239,114 @@ async fn test_versions(_fb: FacebookInit) -> anyhow::Result<()> {
 
     let res: Vec<WorkspaceVersion> = sql.get(reponame.clone(), workspace.clone()).await?;
     assert_eq!(vec![args], res);
+
+    Ok(())
+}
+
+#[fbinit::test]
+async fn test_history(_fb: FacebookInit) -> anyhow::Result<()> {
+    use commit_cloud::sql::history::DeleteArgs;
+    use commit_cloud::sql::history::GetOutput;
+    use commit_cloud::sql::history::GetType;
+    use commit_cloud::sql::history::WorkspaceHistory;
+    use commit_cloud::sql::ops::GenericGet;
+
+    // Create a workspace with heads and bookmarks
+    let sql = SqlCommitCloudBuilder::with_sqlite_in_memory()?.new();
+    let reponame = "test_repo".to_owned();
+    let workspace = "user_testuser_default".to_owned();
+    let timestamp = Timestamp::now();
+
+    let head1 = WorkspaceHead {
+        commit: HgChangesetId::from_str("2d7d4ba9ce0a6ffd222de7785b249ead9c51c536").unwrap(),
+    };
+
+    let remote_bookmark1 = WorkspaceRemoteBookmark {
+        name: "my_bookmark1".to_owned(),
+        commit: HgChangesetId::from_str("2d7d4ba9ce0a6ffd222de7785b249ead9c51c536").unwrap(),
+        remote: "remote".to_owned(),
+    };
+
+    let local_bookmark1 = WorkspaceLocalBookmark {
+        name: "my_bookmark1".to_owned(),
+        commit: HgChangesetId::from_str("2d7d4ba9ce0a6ffd222de7785b249ead9c51c536").unwrap(),
+    };
+
+    let args1 = WorkspaceHistory {
+        version: 1,
+        timestamp: Some(Timestamp::now()),
+        heads: vec![head1.clone()],
+        local_bookmarks: vec![local_bookmark1.clone()],
+        remote_bookmarks: vec![remote_bookmark1.clone()],
+    };
+
+    // Insert a history entry, retrieve it and cast it to Rust struct
+    sql.insert(reponame.clone(), workspace.clone(), args1.clone())
+        .await?;
+
+    let res: Vec<GetOutput> = sql
+        .get(
+            reponame.clone(),
+            workspace.clone(),
+            GetType::GetHistoryVersion { version: 1 },
+        )
+        .await?;
+
+    let res_as_history: Vec<WorkspaceHistory> = res
+        .into_iter()
+        .map(|output| match output {
+            GetOutput::WorkspaceHistory(history) => history,
+            _ => panic!("Output doesn't match query type"),
+        })
+        .collect::<Vec<WorkspaceHistory>>();
+
+    assert_eq!(vec![args1], res_as_history);
+
+    // Insert a new history entry
+    let args2 = WorkspaceHistory {
+        version: 2,
+        timestamp: Some(Timestamp::now()),
+        heads: vec![head1],
+        local_bookmarks: vec![local_bookmark1],
+        remote_bookmarks: vec![remote_bookmark1],
+    };
+
+    sql.insert(reponame.clone(), workspace.clone(), args2.clone())
+        .await?;
+
+    // Delete first history entry, validate only second entry is left
+    Delete::<WorkspaceHistory>::delete(
+        &sql,
+        reponame.clone(),
+        workspace.clone(),
+        DeleteArgs {
+            keep_days: 0,
+            keep_version: 1,
+            delete_limit: 1,
+        },
+    )
+    .await?;
+
+    let res: Vec<GetOutput> = sql
+        .get(
+            reponame.clone(),
+            workspace.clone(),
+            GetType::GetHistoryDate {
+                timestamp,
+                limit: 2,
+            },
+        )
+        .await?;
+
+    let res_as_history: Vec<WorkspaceHistory> = res
+        .into_iter()
+        .map(|output| match output {
+            GetOutput::WorkspaceHistory(history) => history,
+            _ => panic!("Output doesn't match query type"),
+        })
+        .collect::<Vec<WorkspaceHistory>>();
+
+    assert_eq!(vec![args2], res_as_history);
 
     Ok(())
 }
