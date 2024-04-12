046c1964b8d212a7209431faaebf4e7fb0f72bc2
David Barsky <dbarsky@meta.com>
2024-04-12 13:51:29 -0700

    Migrate eden away from mod.rs
    Summary:
    > Note: Prior to rustc 1.30, using mod.rs files was the way to load a module with nested children. It is encouraged to use the new naming convention as it is more consistent, and avoids having many files named mod.rs within a project.
    
    (From https://doc.rust-lang.org/reference/items/modules.html#module-source-filenames. See also: https://fb.workplace.com/groups/rust.language/posts/25887414700880467.)
    
    Reviewed By: zertosh
    
    Differential Revision: D56032103
    
    fbshipit-source-id: 1f0f019ac36cfa08cc93c5a9b7fa2c9c574080aa

---
+++ eden/scm/lib/commands/commands/cmdversion/src/fb.rs
@@ -0,0 +1,9 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+pub(crate) static VERSION_TEXT: &str = r#"
+Sapling is also known as "Mercurial" or "hg" internally at Meta.
+
+User guide: https://www.internalfb.com/intern/wiki/Mercurial-workflow/
+Support group: https://fb.workplace.com/groups/mercurialusers/
+Sapling developer guide: https://www.internalfb.com/intern/wiki/Mercurial/dev/Contributing/
+"#;
