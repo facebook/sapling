0305dff40ed316e622a2a38ef6978c1c2b4fba24
Evan Krause <evangrayk@meta.com>
2024-04-25 12:04:16 -0700

    Bump waitFor timeouts in CI jobs
    Summary:
    I noticed some flakiness with test jobs in the isl tests. I typically see isl timing out on a waitFor for something like syntax highlighting (which loads async). Let's bump the timeout to 5s instead of 1s to help account for slower jobs.
    
    I only do this for CI jobs (sandcastle internally or github actions externally), so local testing doesn't take 5x longer if there's a failure. I haven't noticed flakiness when working locally.
    
    Reviewed By: muirdm
    
    Differential Revision: D56549952
    
    fbshipit-source-id: 17be31f4a7c159eeaf85d1be5385cee11dab4f07

--- addons/isl/src/setupTests.ts
+++ addons/isl/src/setupTests.ts
@@ -23,16 +23,21 @@ jest.mock('./MessageBus');
 
 import {configure} from '@testing-library/react';
 
-if (process.env.HIDE_RTL_DOM_ERRORS) {
-  configure({
-    getElementError: (message: string | null) => {
-      const error = new Error(message ?? '');
-      error.name = 'TestingLibraryElementError';
-      error.stack = undefined;
-      return error;
-    },
-  });
-}
+const IS_CI = !!process.env.SANDCASTLE || !!process.env.GITHUB_ACTIONS;
+configure({
+  // bump waitFor timeouts in CI where jobs may run slower
+  asyncUtilTimeout: IS_CI ? 5_000 : undefined,
+  ...(process.env.HIDE_RTL_DOM_ERRORS
+    ? {
+        getElementError: (message: string | null) => {
+          const error = new Error(message ?? '');
+          error.name = 'TestingLibraryElementError';
+          error.stack = undefined;
+          return error;
+        },
+      }
+    : {}),
+});
 
 global.ResizeObserver = require('resize-observer-polyfill');
 
