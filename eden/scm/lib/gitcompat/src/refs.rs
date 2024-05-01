98da5075c610aad373172a88514cb89463aa420b
Jun Wu <quark@meta.com>
2024-05-01 16:28:49 -0700

    gitcompat: implement fast path for resolving git ref
    Summary:
    While the "idea" is to use git commands for best compatibility, resolving HEAD
    seems common enough that seems to worth some kind of fast path. Imagine in the
    submodule `diff` case it seems a bit suboptimal to run `git show-ref` per
    submodule.
    
    Merge pull request #6796 from ConradIrwin/no-oob
    
    Reviewed By: zzl0
    
    Differential Revision: D56613770
    
    fbshipit-source-id: 14e23a628ffdd0d363ddb7c5b2b340260c098f69

--- eden/scm/lib/gitcompat/src/refs.rs
+++ eden/scm/lib/gitcompat/src/refs.rs
@@ -5,8 +5,12 @@
  * GNU General Public License version 2.
  */
 
+use std::path::Component;
+use std::path::Path;
+
 use anyhow::Result;
 use fs_err as fs;
+use tracing::debug;
 use types::HgId;
 
 use crate::rungit::RunGitOptions;
@@ -20,13 +24,22 @@ impl RunGitOptions {
         // Attempt to look at ".git/HEAD" directly for performance.
         if let Some(git_dir) = self.git_dir.as_ref() {
             let data = fs::read_to_string(git_dir.join("HEAD"))?;
-            if let Ok(id) = HgId::from_hex(data.trim_end().as_bytes()) {
+            let data = data.trim_end();
+            if let Ok(id) = HgId::from_hex(data.as_bytes()) {
+                debug!("HEAD is a hash: {}", data);
                 return Ok(id);
+            }
+            if let Some(ref_name) = data.strip_prefix("ref: ") {
+                // Attempt to resolve the reference directly by reading files.
+                if let Some(id) = resolve_ref(git_dir, ref_name) {
+                    debug!("HEAD is a ref: {} -> {}", ref_name, id);
+                    return Ok(id);
+                }
             }
             maybe_dancling = data.starts_with("ref: refs/heads/");
         }
 
-        // Fallback to `git show-ref`
+        // Fallback to `git show-ref`, the authentic way to resolve the ref.
         let out = match self.call("show-ref", &["--head", "--hash", "HEAD"]) {
             Ok(out) => out,
             Err(e) => {
@@ -40,10 +53,44 @@ impl RunGitOptions {
         };
         if let Some(data) = out.stdout.get(..HgId::hex_len()) {
             let id = HgId::from_hex(data)?;
+            debug!("HEAD resolved via show-ref: {}", id);
             return Ok(id);
         }
 
         let str_out = String::from_utf8_lossy(&out.stdout);
         anyhow::bail!("Cannot resolve HEAD from {:?}", str_out);
     }
+}
+
+/// Attempt to resolve ref_name (starting with ref/) to a commit hash.
+/// This is a best effort and might fail silently.
+fn resolve_ref(git_dir: &Path, ref_name: &str) -> Option<HgId> {
+    // Read loose ref.
+    let ref_path = Path::new(ref_name);
+    // Reject malicious file name.
+    if ref_path
+        .components()
+        .all(|c| matches!(c, Component::Normal(_)))
+    {
+        let path = git_dir.join(ref_path);
+        if let Ok(mut data) = fs::read(path) {
+            if data.ends_with(&[b'\n']) {
+                data.pop();
+            }
+            return HgId::from_hex(&data).ok();
+        }
+    }
+
+    // Read packed ref.
+    if let Ok(packed_refs) = fs::read_to_string(git_dir.join("packed-refs")) {
+        for line in packed_refs.lines() {
+            if let Some((hex, name)) = line.split_once(' ') {
+                if name == ref_name {
+                    return HgId::from_hex(hex.as_bytes()).ok();
+                }
+            }
+        }
+    }
+
+    None
 }
