5406ef1ccbb908cfdc9b84e1a1fc2d0a73b496c1
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2024-05-14 10:27:43 -0700

    mononoke: integration tests: Add error message when misusing incremental scripts
    Summary: Simply streamlining usage to whoever want to use these
    
    Reviewed By: mitrandir77
    
    Differential Revision: D57329424
    
    fbshipit-source-id: 9d0a2221fa0c294067c0322fa4f48ecccfae0768

--- eden/mononoke/tests/integration/incremental_integration_run.sh
+++ eden/mononoke/tests/integration/incremental_integration_run.sh
@@ -6,6 +6,15 @@
 
 set -e
 
+if [ $# -eq 0 ]; then
+    bold=$(tput bold)
+    normal=$(tput sgr0)
+    >&2 echo " ${bold} No arguments provided, make sure you first run incremental_integration_setup.sh with one of: ${normal}"
+    buck2 uquery "kind('sh_test', fbcode//eden/mononoke/tests/integration:)" --output-attribute name -v 0  2> /dev/null | jq 'to_entries[] | select(.key | contains("disable-all-network-access") | not) | .value.name'| tr '\n' ', ' | sed 's/,$//'
+    printf "\n"
+    exit 1
+fi
+
 NAME=$1
 if [[ "$NAME" == facebook/* ]]; then
     SUBDIR="/facebook"
--- eden/mononoke/tests/integration/incremental_integration_setup.sh
+++ eden/mononoke/tests/integration/incremental_integration_setup.sh
@@ -8,6 +8,16 @@ set -e
 
 BUCK=buck2
 
+if [ $# -eq 0 ]; then
+    bold=$(tput bold)
+    normal=$(tput sgr0)
+    >&2 echo " ${bold} No arguments provided, use one of: ${normal}"
+    buck2 uquery "kind('sh_test', fbcode//eden/mononoke/tests/integration:)" --output-attribute name -v 0  2> /dev/null | jq 'to_entries[] | select(.key | contains("disable-all-network-access") | not) | .value.name'| tr '\n' ', ' | sed 's/,$//'
+    printf "\n"
+    exit 1
+fi
+
+
 if [[ "$1" == facebook/* ]]; then
     # Strip facebook prefix
     TARGET="/facebook:${1#*/}-manifest"
