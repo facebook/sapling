438d71de94b7ce079c2ec9c883c9b47f4971b1c9
Pierre Chevalier <pierrec@meta.com>
2024-05-29 08:20:05 -0700

    affected_changesets: Only load additional changesets once
    Summary:
    Move the loading of additional changesets to `check_restrictions`, if any check is needed.
    
    This is more functional. It allows to remove the mutability from each individual check and makes the data flow one step clearer.
    
    This makes the OOM fixing at the end of this stack more straightforward.
    
    Reviewed By: gustavoavena
    
    Differential Revision: D57906017
    
    fbshipit-source-id: 7bb720fff430b547f3ab4d4c8816a287899b3db5

--- eden/mononoke/bookmarks/bookmarks_movement/src/affected_changesets.rs
+++ eden/mononoke/bookmarks/bookmarks_movement/src/affected_changesets.rs
@@ -249,28 +249,46 @@ impl AffectedChangesets {
         additional_changesets: AdditionalChangesets,
         cross_repo_push_source: CrossRepoPushSource,
     ) -> Result<(), BookmarkMovementError> {
-        self.check_extras(ctx, repo, bookmark, kind, additional_changesets)
-            .await?;
+        let needs_extras_check = Self::needs_extras_check(repo, kind);
+        let needs_case_conflicts_check = Self::needs_case_conflicts_check(repo, kind);
+        let needs_hooks_check =
+            Self::needs_hooks_check(kind, authz, reason, hook_manager, bookmark);
+        let needs_path_permissions_check =
+            Self::needs_path_permissions_check(ctx, authz, repo).await;
+        if needs_extras_check
+            || needs_case_conflicts_check
+            || needs_hooks_check
+            || needs_path_permissions_check
+        {
+            self.load_additional_changesets(ctx, repo, bookmark, additional_changesets)
+                .await
+                .context("Failed to load additional affected changesets to check restrictions")?;
+        }
 
-        self.check_case_conflicts(ctx, repo, bookmark, kind, additional_changesets)
-            .await?;
+        if needs_extras_check {
+            self.check_extras().await?;
+        }
 
-        self.check_hooks(
-            ctx,
-            authz,
-            repo,
-            hook_manager,
-            bookmark,
-            pushvars,
-            reason,
-            kind,
-            additional_changesets,
-            cross_repo_push_source,
-        )
-        .await?;
+        if needs_case_conflicts_check {
+            self.check_case_conflicts(ctx, repo).await?;
+        }
 
-        self.check_path_permissions(ctx, authz, repo, bookmark, additional_changesets)
+        if needs_hooks_check {
+            self.check_hooks(
+                ctx,
+                authz,
+                repo,
+                hook_manager,
+                bookmark,
+                pushvars,
+                cross_repo_push_source,
+            )
             .await?;
+        }
+
+        if needs_path_permissions_check {
+            self.check_path_permissions(ctx, authz, repo).await?;
+        }
 
         Ok(())
     }
@@ -283,34 +301,21 @@ impl AffectedChangesets {
                 .allow_change_xrepo_mapping_extra
     }
 
-    async fn check_extras(
-        &mut self,
-        ctx: &CoreContext,
-        repo: &impl Repo,
-        bookmark: &BookmarkKey,
-        kind: BookmarkKind,
-        additional_changesets: AdditionalChangesets,
-    ) -> Result<(), BookmarkMovementError> {
-        if Self::needs_extras_check(repo, kind) {
-            self.load_additional_changesets(ctx, repo, bookmark, additional_changesets)
-                .await
-                .context("Failed to load additional affected changesets to check extras")?;
-
-            for bcs in self.iter() {
-                if bcs
-                    .hg_extra()
-                    .any(|(name, _)| name == CHANGE_XREPO_MAPPING_EXTRA)
-                {
-                    // This extra is used in backsyncer, and it changes how commit
-                    // is rewritten from a large repo to a small repo. This is dangerous
-                    // operation, and we don't allow landing a commit with this extra set.
-                    return Err(anyhow!(
-                        "Disallowed extra {} is set on {}.",
-                        CHANGE_XREPO_MAPPING_EXTRA,
-                        bcs.get_changeset_id()
-                    )
-                    .into());
-                }
+    async fn check_extras(&self) -> Result<(), BookmarkMovementError> {
+        for bcs in self.iter() {
+            if bcs
+                .hg_extra()
+                .any(|(name, _)| name == CHANGE_XREPO_MAPPING_EXTRA)
+            {
+                // This extra is used in backsyncer, and it changes how commit
+                // is rewritten from a large repo to a small repo. This is dangerous
+                // operation, and we don't allow landing a commit with this extra set.
+                return Err(anyhow!(
+                    "Disallowed extra {} is set on {}.",
+                    CHANGE_XREPO_MAPPING_EXTRA,
+                    bcs.get_changeset_id()
+                )
+                .into());
             }
         }
 
@@ -326,72 +331,62 @@ impl AffectedChangesets {
     /// If the push is to a public bookmark, and the casefolding check is
     /// enabled, check that no affected changeset has case conflicts.
     async fn check_case_conflicts(
-        &mut self,
+        &self,
         ctx: &CoreContext,
         repo: &impl Repo,
-        bookmark: &BookmarkKey,
-        kind: BookmarkKind,
-        additional_changesets: AdditionalChangesets,
     ) -> Result<(), BookmarkMovementError> {
-        if Self::needs_case_conflicts_check(repo, kind) {
-            self.load_additional_changesets(ctx, repo, bookmark, additional_changesets)
-                .await
-                .context("Failed to load additional affected changesets to check case conflicts")?;
+        stream::iter(self.iter().map(Ok))
+            .try_for_each_concurrent(100, |bcs| {
+                async move {
+                    let bcs_id = bcs.get_changeset_id();
 
-            stream::iter(self.iter().map(Ok))
-                .try_for_each_concurrent(100, |bcs| {
-                    async move {
-                        let bcs_id = bcs.get_changeset_id();
+                    let sk_mf = repo
+                        .repo_derived_data()
+                        .derive::<RootSkeletonManifestId>(ctx, bcs_id)
+                        .await
+                        .map_err(Error::from)?
+                        .into_skeleton_manifest_id()
+                        .load(ctx, repo.repo_blobstore())
+                        .await
+                        .map_err(Error::from)?;
+                    if sk_mf.has_case_conflicts() {
+                        // We only reject a commit if it introduces new case
+                        // conflicts compared to its parents.
+                        let parents = stream::iter(bcs.parents().map(|parent_bcs_id| async move {
+                            repo.repo_derived_data()
+                                .derive::<RootSkeletonManifestId>(ctx, parent_bcs_id)
+                                .await
+                                .map_err(Error::from)?
+                                .into_skeleton_manifest_id()
+                                .load(ctx, repo.repo_blobstore())
+                                .await
+                                .map_err(Error::from)
+                        }))
+                        .buffered(10)
+                        .try_collect::<Vec<_>>()
+                        .await?;
+                        let config = &repo.repo_config().pushrebase.flags;
 
-                        let sk_mf = repo
-                            .repo_derived_data()
-                            .derive::<RootSkeletonManifestId>(ctx, bcs_id)
-                            .await
-                            .map_err(Error::from)?
-                            .into_skeleton_manifest_id()
-                            .load(ctx, repo.repo_blobstore())
-                            .await
-                            .map_err(Error::from)?;
-                        if sk_mf.has_case_conflicts() {
-                            // We only reject a commit if it introduces new case
-                            // conflicts compared to its parents.
-                            let parents =
-                                stream::iter(bcs.parents().map(|parent_bcs_id| async move {
-                                    repo.repo_derived_data()
-                                        .derive::<RootSkeletonManifestId>(ctx, parent_bcs_id)
-                                        .await
-                                        .map_err(Error::from)?
-                                        .into_skeleton_manifest_id()
-                                        .load(ctx, repo.repo_blobstore())
-                                        .await
-                                        .map_err(Error::from)
-                                }))
-                                .buffered(10)
-                                .try_collect::<Vec<_>>()
-                                .await?;
-                            let config = &repo.repo_config().pushrebase.flags;
-
-                            if let Some((path1, path2)) = sk_mf
-                                .first_new_case_conflict(
-                                    ctx,
-                                    repo.repo_blobstore(),
-                                    parents,
-                                    &config.casefolding_check_excluded_paths,
-                                )
-                                .await?
-                            {
-                                return Err(BookmarkMovementError::CaseConflict {
-                                    changeset_id: bcs_id,
-                                    path1,
-                                    path2,
-                                });
-                            }
+                        if let Some((path1, path2)) = sk_mf
+                            .first_new_case_conflict(
+                                ctx,
+                                repo.repo_blobstore(),
+                                parents,
+                                &config.casefolding_check_excluded_paths,
+                            )
+                            .await?
+                        {
+                            return Err(BookmarkMovementError::CaseConflict {
+                                changeset_id: bcs_id,
+                                path1,
+                                path2,
+                            });
                         }
-                        Ok(())
                     }
-                })
-                .await?;
-        }
+                    Ok(())
+                }
+            })
+            .await?;
         Ok(())
     }
 
@@ -423,70 +418,63 @@ impl AffectedChangesets {
     /// service-initiated pushrebase but hooks will run with taking this
     /// into account.
     async fn check_hooks(
-        &mut self,
+        &self,
         ctx: &CoreContext,
         authz: &AuthorizationContext,
         repo: &impl Repo,
         hook_manager: &HookManager,
         bookmark: &BookmarkKey,
         pushvars: Option<&HashMap<String, Bytes>>,
-        reason: BookmarkUpdateReason,
-        kind: BookmarkKind,
-        additional_changesets: AdditionalChangesets,
         cross_repo_push_source: CrossRepoPushSource,
     ) -> Result<(), BookmarkMovementError> {
-        if Self::needs_hooks_check(kind, authz, reason, hook_manager, bookmark) {
-            self.load_additional_changesets(ctx, repo, bookmark, additional_changesets)
-                .await
-                .context("Failed to load additional affected changesets to check hooks")?;
+        let skip_running_hooks_if_public: bool = repo
+            .repo_bookmark_attrs()
+            .select(bookmark)
+            .map(|attr| attr.params().allow_move_to_public_commits_without_hooks)
+            .any(|x| x);
+        if skip_running_hooks_if_public && !self.adding_new_changesets_to_repo() {
+            // For some bookmarks we allow to skip running hooks if:
+            // 1) this is just a bookmark move i.e. no new commits are added or pushrebased to the repo
+            // 2) we are allowed to skip commits for a bookmark like that
+            // 3) if all commits that are affectd by this bookmark move are public (which means
+            //  we should have already ran hooks for these commits).
 
-            let skip_running_hooks_if_public: bool = repo
-                .repo_bookmark_attrs()
-                .select(bookmark)
-                .map(|attr| attr.params().allow_move_to_public_commits_without_hooks)
-                .any(|x| x);
-            if skip_running_hooks_if_public && !self.adding_new_changesets_to_repo() {
-                // For some bookmarks we allow to skip running hooks if:
-                // 1) this is just a bookmark move i.e. no new commits are added or pushrebased to the repo
-                // 2) we are allowed to skip commits for a bookmark like that
-                // 3) if all commits that are affectd by this bookmark move are public (which means
-                //  we should have already ran hooks for these commits).
-
-                let cs_ids = self
-                    .iter()
-                    .map(|bcs| bcs.get_changeset_id())
-                    .collect::<Vec<_>>();
-
-                let public = repo
-                    .phases()
-                    .get_public(ctx, cs_ids.clone(), false /* ephemeral_derive */)
-                    .await?;
-                if public == cs_ids.into_iter().collect::<HashSet<_>>() {
-                    return Ok(());
-                }
-            }
-
-            if !self.is_empty() {
-                let push_authored_by = if authz.is_service() {
-                    PushAuthoredBy::Service
-                } else {
-                    PushAuthoredBy::User
-                };
-                run_hooks(
-                    ctx,
-                    hook_manager,
-                    bookmark,
-                    self.iter(),
-                    pushvars,
-                    cross_repo_push_source,
-                    push_authored_by,
-                )
+            let cs_ids = self
+                .iter()
+                .map(|bcs| bcs.get_changeset_id())
+                .collect::<Vec<_>>();
+            let public = repo
+                .phases()
+                .get_public(ctx, cs_ids.clone(), false /* ephemeral_derive */)
                 .await?;
+            if public == cs_ids.into_iter().collect::<HashSet<_>>() {
+                return Ok(());
             }
         }
+
+        if !self.is_empty() {
+            let push_authored_by = if authz.is_service() {
+                PushAuthoredBy::Service
+            } else {
+                PushAuthoredBy::User
+            };
+            run_hooks(
+                ctx,
+                hook_manager,
+                bookmark,
+                self.iter(),
+                pushvars,
+                cross_repo_push_source,
+                push_authored_by,
+            )
+            .await?;
+        }
+
         Ok(())
     }
 
+    // User is not permitted to write to all paths, check if the paths
+    // touched by the changesets are permitted.
     async fn needs_path_permissions_check(
         ctx: &CoreContext,
         authz: &AuthorizationContext,
@@ -498,28 +486,13 @@ impl AffectedChangesets {
     /// Check whether the user has permissions to modify the paths that are
     /// modified by the changesets that are affected by the bookmark move.
     async fn check_path_permissions(
-        &mut self,
+        &self,
         ctx: &CoreContext,
         authz: &AuthorizationContext,
         repo: &impl Repo,
-        bookmark: &BookmarkKey,
-        additional_changesets: AdditionalChangesets,
     ) -> Result<(), BookmarkMovementError> {
-        // For optimization, first check if the user is permitted to modify
-        // all paths.  In that case we don't need to find out which paths were
-        // affected.
-        if Self::needs_path_permissions_check(ctx, authz, repo).await {
-            // User is not permitted to write to all paths, check if the paths
-            // touched by the changesets are permitted.
-            self.load_additional_changesets(ctx, repo, bookmark, additional_changesets)
-                .await
-                .context(
-                    "Failed to load additional affected changesets to check path permissions",
-                )?;
-
-            for cs in self.iter() {
-                authz.require_changeset_paths_write(ctx, repo, cs).await?;
-            }
+        for cs in self.iter() {
+            authz.require_changeset_paths_write(ctx, repo, cs).await?;
         }
         Ok(())
     }
