295c7735ed0b3a1baaafbbca7652444c93f17635
Liubov Dmitrieva <liubovd@meta.com>
2024-05-31 15:37:05 -0700

    implement full manifest traversal with prior lookups, and uploaddata to a cas backend.
    Summary:
    Implement full manifest traversal with prior lookups, and upload
    data to a cas backend.
    
    This is required for our initial mononoke admin command to sparse populate augmented manifests for a given revision (by traversing entire manifests)
    
    Reviewed By: mitrandir77
    
    Differential Revision: D57910047
    
    fbshipit-source-id: 5b08ef0b18badce2d0efb79063d209cd7c021eb0

--- eden/mononoke/cas_client/scm_client/lib.rs
+++ eden/mononoke/cas_client/scm_client/lib.rs
@@ -6,6 +6,8 @@
  */
 
 mod errors;
+use std::cmp;
+
 use anyhow::Error;
 use blobstore::Blobstore;
 use blobstore::Loadable;
@@ -14,9 +16,12 @@ use context::CoreContext;
 pub use errors::ErrorKind;
 use futures::stream;
 use futures::StreamExt;
+use futures::TryStreamExt;
+use mercurial_types::HgAugmentedManifestEntry;
 use mercurial_types::HgAugmentedManifestId;
 use mercurial_types::HgFileNodeId;
 use mononoke_types::MononokeDigest;
+use slog::info;
 
 #[cfg(fbcode_build)]
 pub type MononokeCasClient<'a> = ScmCasClient<cas_client::RemoteExecutionCasdClient<'a>>;
@@ -37,6 +42,10 @@ impl<Client> ScmCasClient<Client>
 where
     Client: CasClient,
 {
+    pub fn new(client: Client) -> Self {
+        Self { client }
+    }
+
     async fn lookup_digest(&self, digest: &MononokeDigest) -> Result<bool, Error> {
         self.client.lookup_blob(digest).await
     }
@@ -86,15 +95,17 @@ where
     /// Upload given file contents to a Cas backend (batched API)
     /// This implementation doesn't use batched API on CAS side yet, but it is planned to be implemented.
     /// Prior lookup flag is used to check if a digest already exists in the CAS backend before fetching data from Mononoke blobstore.
+    /// Digests of files can be known a priori (from Augmented Manifest, for example), but it is not required.
+    /// Caller can analyse individual results.
     pub async fn upload_file_contents<'a>(
         &self,
         ctx: &'a CoreContext,
         blobstore: &'a impl Blobstore,
-        ids: Vec<(HgFileNodeId, Option<&MononokeDigest>)>,
+        ids: Vec<(HgFileNodeId, Option<MononokeDigest>)>,
         prior_lookup: bool,
     ) -> Vec<Result<HgFileNodeId, Error>> {
         let uploads = ids.into_iter().map(move |id| async move {
-            self.upload_file_content(ctx, blobstore, &id.0, id.1, prior_lookup)
+            self.upload_file_content(ctx, blobstore, &id.0, id.1.as_ref(), prior_lookup)
                 .await?;
             Ok(id.0)
         });
@@ -104,21 +115,43 @@ where
             .await
     }
 
+    pub async fn ensure_upload_file_contents<'a>(
+        &self,
+        ctx: &'a CoreContext,
+        blobstore: &'a impl Blobstore,
+        ids: Vec<(HgFileNodeId, Option<MononokeDigest>)>,
+        prior_lookup: bool,
+    ) -> Result<Vec<HgFileNodeId>, Error> {
+        self.upload_file_contents(ctx, blobstore, ids, prior_lookup)
+            .await
+            .into_iter()
+            .collect()
+    }
+
     /// Upload given augmented tree to a CAS backend.
     /// Prior lookup flag is used to check if a digest already exists in the CAS backend before fetching data from Mononoke blobstore.
+    /// Digest of a tree can be known a priori (from Augmented Manifest, for example), but it is not required.
     pub async fn upload_augmented_tree<'a>(
         &self,
         ctx: &'a CoreContext,
         blobstore: &'a impl Blobstore,
         manifest_id: &HgAugmentedManifestId,
+        digest: Option<&MononokeDigest>,
         prior_lookup: bool,
     ) -> Result<(), Error> {
+        let mut digest_checked = false;
+        if let Some(digest) = digest {
+            if prior_lookup && self.lookup_digest(digest).await? {
+                return Ok(());
+            }
+            digest_checked = true;
+        }
         let augmented_manifest_envelope = manifest_id.load(ctx, blobstore).await?;
         let digest = MononokeDigest(
             augmented_manifest_envelope.augmented_manifest_id(),
             augmented_manifest_envelope.augmented_manifest_size(),
         );
-        if prior_lookup && self.lookup_digest(&digest).await? {
+        if !digest_checked && prior_lookup && self.lookup_digest(&digest).await? {
             return Ok(());
         }
         let bytes_stream =
@@ -132,21 +165,147 @@ where
     /// Upload given augmented trees to a Cas backend (batched API)
     /// This implementation doesn't use batched API on CAS side yet, but it is planned to be implemented.
     /// Prior lookup flag is used to check if a digest already exists in the CAS backend before fetching data from Mononoke blobstore.
+    /// Digests of trees can be known a priori (from Augmented Manifest, for example), but it is not required.
+    /// Caller can analyse individual results.
     pub async fn upload_augmented_trees<'a>(
         &self,
         ctx: &'a CoreContext,
         blobstore: &'a impl Blobstore,
-        manifest_ids: Vec<HgAugmentedManifestId>,
+        manifest_ids: Vec<(HgAugmentedManifestId, Option<MononokeDigest>)>,
         prior_lookup: bool,
     ) -> Vec<Result<HgAugmentedManifestId, Error>> {
         let uploads = manifest_ids.into_iter().map(move |id| async move {
-            self.upload_augmented_tree(ctx, blobstore, &id, prior_lookup)
+            self.upload_augmented_tree(ctx, blobstore, &id.0, id.1.as_ref(), prior_lookup)
                 .await?;
-            Ok(id)
+            Ok(id.0)
         });
         stream::iter(uploads)
             .buffer_unordered(MAX_CONCURRENT_UPLOADS_TREES)
             .collect()
             .await
+    }
+
+    pub async fn ensure_upload_augmented_trees<'a>(
+        &self,
+        ctx: &'a CoreContext,
+        blobstore: &'a impl Blobstore,
+        manifest_ids: Vec<(HgAugmentedManifestId, Option<MononokeDigest>)>,
+        prior_lookup: bool,
+    ) -> Result<Vec<HgAugmentedManifestId>, Error> {
+        self.upload_augmented_trees(ctx, blobstore, manifest_ids, prior_lookup)
+            .await
+            .into_iter()
+            .collect()
+    }
+
+    /// Upload given root augmented tree to a CAS backend (recursively by walking the tree)
+    // TODO: rewrite it to use the "bounded_traversal" crate.
+    pub async fn upload_root_augmented_tree_recursive<'a>(
+        &self,
+        ctx: &'a CoreContext,
+        blobstore: &'a impl Blobstore,
+        root_manifest_id: &HgAugmentedManifestId,
+    ) -> Result<(), Error> {
+        let mut manifests_queue = vec![(root_manifest_id.clone(), None)];
+        let mut blobs_queue: Vec<(HgFileNodeId, Option<MononokeDigest>)> = Vec::new();
+
+        while !manifests_queue.is_empty() {
+            // If blobs queue is big, let's drain it first
+            if blobs_queue.len() > MAX_CONCURRENT_UPLOADS_FILES {
+                let uploaded_file_ids = self
+                    .ensure_upload_file_contents(
+                        ctx,
+                        blobstore,
+                        blobs_queue
+                            .drain(0..MAX_CONCURRENT_UPLOADS_FILES)
+                            .collect::<Vec<_>>(),
+                        true,
+                    )
+                    .await?;
+
+                info!(
+                    ctx.logger(),
+                    "Uploaded {} files into cas backend",
+                    uploaded_file_ids.len()
+                );
+            }
+
+            // Upload chunk of manifests from the current queue up to MAX_CONCURRENT_UPLOADS_TREES amount
+            let uploaded_manifest_ids = self
+                .ensure_upload_augmented_trees(
+                    ctx,
+                    blobstore,
+                    manifests_queue
+                        .drain(0..cmp::min(manifests_queue.len(), MAX_CONCURRENT_UPLOADS_TREES))
+                        .collect::<Vec<_>>(),
+                    true,
+                )
+                .await?;
+
+            info!(
+                ctx.logger(),
+                "Uploaded {} augmented manifests into cas backend",
+                uploaded_manifest_ids.len()
+            );
+
+            // Fetch all children of the uploaded manifests chunk
+            let fetches = uploaded_manifest_ids
+                .into_iter()
+                .map(move |manifest_id| async move {
+                    let manifest = manifest_id.load(ctx, blobstore).await?.augmented_manifest;
+                    let childrens: Vec<HgAugmentedManifestEntry> = manifest
+                        .into_subentries(ctx, blobstore)
+                        // strip the paths
+                        .map(|res| res.map(|(_k, v)| v))
+                        .try_collect()
+                        .await?;
+                    Ok::<Vec<HgAugmentedManifestEntry>, Error>(childrens)
+                });
+
+            let children: Vec<HgAugmentedManifestEntry> = stream::iter(fetches)
+                .buffer_unordered(MAX_CONCURRENT_UPLOADS_TREES)
+                .try_collect::<Vec<Vec<HgAugmentedManifestEntry>>>()
+                .await?
+                .into_iter()
+                .flatten()
+                .collect();
+
+            for child in children {
+                match child {
+                    HgAugmentedManifestEntry::FileNode(file) => {
+                        blobs_queue.push((
+                            HgFileNodeId::new(file.filenode),
+                            Some(MononokeDigest(file.content_blake3, file.total_size)),
+                        ));
+                    }
+                    HgAugmentedManifestEntry::DirectoryNode(tree) => {
+                        manifests_queue.push((
+                            HgAugmentedManifestId::new(tree.treenode),
+                            Some(MononokeDigest(
+                                tree.augmented_manifest_id,
+                                tree.augmented_manifest_size,
+                            )),
+                        ));
+                    }
+                }
+            }
+        }
+
+        // Upload remaining blobs
+        let uploaded_file_ids = self
+            .ensure_upload_file_contents(
+                ctx,
+                blobstore,
+                blobs_queue.into_iter().collect::<Vec<_>>(),
+                true,
+            )
+            .await?;
+
+        info!(
+            ctx.logger(),
+            "Uploaded {} files into cas backend",
+            uploaded_file_ids.len()
+        );
+        Ok(())
     }
 }
