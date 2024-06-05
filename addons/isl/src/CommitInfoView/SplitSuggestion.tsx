84d93f927360fb79bc61c05023f65718a225b3c3
Evan Krause <evangrayk@meta.com>
2024-06-05 11:40:04 -0700

    Design adjustments for split suggestion
    Summary:
    Some design tweaks for the split suggestion on large commits.
    
    - use grey background instead of green, since the green was quite overwhelming IMO
    - Increase icon size to "M", change icon to lightbulb, make icon blue to compensate for lack of color after removing green bg
    - simplify text so it sometimes fits on two lines
    - add title part, make it bold to draw attention
    - change tooltip to be on entire text section
    - change tooltip text slightly
    - remove "pro tip"
    - more proper use of `t()` replacement for tooltip text
    
    Very open to feedback! Just thought this could use a little extra polish before going out.
    
    Reviewed By: benmonro, quark-zju
    
    Differential Revision: D58168906
    
    fbshipit-source-id: 7efe3fe94a58d3141e97743f5568d847d12c8bf1

--- addons/isl/src/CommitInfoView/SplitSuggestion.tsx
+++ addons/isl/src/CommitInfoView/SplitSuggestion.tsx
@@ -6,11 +6,11 @@
  */
 
 import {Banner, BannerKind} from '../Banner';
+import {Column} from '../ComponentUtils';
 import {Internal} from '../Internal';
-import {Tooltip} from '../Tooltip';
 import {Divider} from '../components/Divider';
 import GatedComponent from '../components/GatedComponent';
-import {T} from '../i18n';
+import {T, t} from '../i18n';
 import {
   MAX_FILES_ALLOWED_FOR_DIFF_STAT,
   SLOC_THRESHOLD_FOR_SPLIT_SUGGESTIONS,
@@ -32,30 +32,19 @@ function SplitSuggestionImpl({commit}: {commit: CommitInfo}) {
     <>
       <Divider />
       <Banner
-        tooltip=""
-        kind={BannerKind.green}
-        icon={<Icon icon="info" />}
+        tooltip={t('This commit has $sloc significant lines of code (top 10%)', {
+          replace: {$sloc: String(significantLinesOfCode)},
+        })}
+        kind={BannerKind.default}
+        icon={<Icon size="M" icon="lightbulb" color="blue" />}
         alwaysShowButtons
-        buttons={
-          <SplitButton
-            style={{
-              border: '1px solid var(--button-secondary-hover-background)',
-            }}
-            trackerEventName="SplitOpenFromSplitSuggestion"
-            commit={commit}
-          />
-        }>
-        <div>
-          <T>Pro tip: Small Diffs lead to less SEVs, quicker review times and happier teams.</T>
-          &nbsp;
-          <Tooltip
-            inline={true}
-            trigger="hover"
-            title={`Significant Lines of Code (SLOC): ${significantLinesOfCode}, this puts your diff in the top 10% of diffs. `}>
-            <T>This diff is a bit big</T>
-          </Tooltip>
-          <T>, consider splitting it up</T>
-        </div>
+        buttons={<SplitButton trackerEventName="SplitOpenFromSplitSuggestion" commit={commit} />}>
+        <Column alignStart style={{gap: 0}}>
+          <b>
+            <T>Consider splitting up this commit</T>
+          </b>
+          <T>Small Diffs lead to less SEVs & quicker review times</T>
+        </Column>
       </Banner>
     </>
   );
