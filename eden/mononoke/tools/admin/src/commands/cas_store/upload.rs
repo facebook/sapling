5496820685c5c81f01aa944c6f68c8abdeea5d49
Liubov Dmitrieva <liubovd@meta.com>
2024-06-05 10:31:48 -0700

    improve the way of specifying changeset for cas upload command
    Summary: improve the way of specifying changeset for cas upload command
    
    Reviewed By: markbt
    
    Differential Revision: D58162375
    
    fbshipit-source-id: eb3775eaf4ab3bfdef1caffb55b267866c6fce90

--- eden/mononoke/tools/admin/src/commands/cas_store/upload.rs
+++ eden/mononoke/tools/admin/src/commands/cas_store/upload.rs
@@ -5,8 +5,6 @@
  * GNU General Public License version 2.
  */
 
-use std::str::FromStr;
-
 use anyhow::anyhow;
 use anyhow::Result;
 #[cfg(not(fbcode_build))]
@@ -25,9 +23,12 @@ use super::Repo;
 /// Subcommand to upload (augmented) tree and blob data into the cas store.
 /// This command can also upload the entire changeset.
 pub struct CasStoreUploadArgs {
-    /// The ID of any one of the changeset that needs to be uploaded into the cas store.
+    /// Bonsai changeset id that needs to be uploaded into the cas store.
     #[clap(long, short = 'i')]
-    changeset_id: Option<String>,
+    changeset_id: Option<ChangesetId>,
+    /// Hg changeset id that needs to be uploaded into the cas store.
+    #[clap(long)]
+    hg_id: Option<HgChangesetId>,
     /// Upload the entire changeset's working copy data recursively.
     #[clap(long)]
     full: bool,
@@ -54,43 +55,29 @@ pub async fn cas_store_upload(
         repo.repo_identity.name(),
         args.verbose,
     )?);
-
-    match args.changeset_id {
-        Some(changeset_id) => {
-            let mut cs_id = ChangesetId::from_str(&changeset_id);
-            if cs_id.is_err() {
-                let hgid = HgChangesetId::from_str(&changeset_id)?;
-                cs_id = repo
-                    .bonsai_hg_mapping
-                    .get_bonsai_from_hg(ctx, hgid)
-                    .await
-                    .map(|cs| {
-                        cs.ok_or(anyhow!(
-                            "unknown commit identifier (only hg and bonsai are supported)"
-                        ))
-                    })?;
-            }
-            let changeset_id = cs_id?;
-            match args.full {
-                true => {
-                    cas_changesets_uploader
-                        .upload_single_changeset_recursively(
-                            ctx,
-                            repo,
-                            &changeset_id,
-                            args.blobs_only,
-                        )
-                        .await?;
-                }
-                false => {
-                    cas_changesets_uploader
-                        .upload_single_changeset(ctx, repo, &changeset_id, args.blobs_only)
-                        .await?;
-                }
-            }
+    let changeset_id = match args.changeset_id {
+        Some(changeset_id) => Ok(changeset_id),
+        None => match args.hg_id {
+            Some(hg_id) => repo
+                .bonsai_hg_mapping
+                .get_bonsai_from_hg(ctx, hg_id)
+                .await?
+                .ok_or(anyhow!("No bonsai changeset found for hg id {}", hg_id)),
+            None => Err(anyhow!(
+                "No changeset id provided. Either hg or bonsai changeset id must be provided."
+            )),
+        },
+    }?;
+    match args.full {
+        true => {
+            cas_changesets_uploader
+                .upload_single_changeset_recursively(ctx, repo, &changeset_id, args.blobs_only)
+                .await?;
         }
-        None => {
-            println!("No changeset id provided. Currently this is the only supported option.");
+        false => {
+            cas_changesets_uploader
+                .upload_single_changeset(ctx, repo, &changeset_id, args.blobs_only)
+                .await?;
         }
     }
     Ok(())
