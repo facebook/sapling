f8a5ffdae6f401c8e34ab66fac61621ee8797e47
Liubov Dmitrieva <liubovd@meta.com>
2024-06-13 15:59:55 -0700

    add serialize method
    Summary: add serialize method
    
    Reviewed By: quark-zju
    
    Differential Revision: D58497337
    
    fbshipit-source-id: c156dd7a6174883255d3ec17e546da9076e262d0

--- eden/scm/lib/types/src/augmented_tree.rs
+++ eden/scm/lib/types/src/augmented_tree.rs
@@ -50,19 +50,19 @@ pub struct AugmentedTreeEntry {
 
 impl AugmentedTreeEntry {
     pub fn sapling_tree_blob_size(&self) -> usize {
-        let mut capacity: usize = 0;
+        let mut size: usize = 0;
         for (path, subentry) in self.subentries.iter() {
-            capacity += path.len() + 2;
+            size += path.len() + 2;
             match subentry {
                 AugmentedTreeChildEntry::DirectoryNode(_) => {
-                    capacity += HgId::hex_len() + 1;
+                    size += HgId::hex_len() + 1;
                 }
                 AugmentedTreeChildEntry::FileNode(_) => {
-                    capacity += HgId::hex_len();
+                    size += HgId::hex_len();
                 }
             };
         }
-        capacity
+        size
     }
 
     pub fn write_sapling_tree_blob(&self, mut w: impl Write) -> Result<()> {
@@ -91,6 +91,85 @@ impl AugmentedTreeEntry {
     //               | <cas-blake3-hex> ' ' <size-dec>
     //
     // tree ::= <version> ' ' <sha1-hex> ' ' <computed_sha1-hex (if different) or '-'> ' ' <p1-hex or '-'> ' ' <p2-hex or '-'> '\n' <entry>*
+
+    /// Methods estimates the size of the serialize in bytes, so that it can be used to preallocate memory.
+    pub fn augmented_tree_blob_size(&self) -> usize {
+        let mut size: usize = 3;
+        size += HgId::hex_len() + 3;
+        if let Some(_p1) = &self.p1 {
+            size += HgId::hex_len();
+        } else {
+            size += 1;
+        }
+        size += 1;
+        if let Some(_p2) = &self.p2 {
+            size += HgId::hex_len();
+        } else {
+            size += 1;
+        }
+        size += 1;
+        for (path, subentry) in self.subentries.iter() {
+            size += path.len() + 1;
+            match subentry {
+                AugmentedTreeChildEntry::FileNode(f) => {
+                    size += HgId::hex_len() + Blake3::hex_len() + 3;
+                    size += f.total_size.to_string().len() + 1;
+                    size += Id20::hex_len();
+                }
+                AugmentedTreeChildEntry::DirectoryNode(d) => {
+                    size += HgId::hex_len() + Blake3::hex_len() + 3;
+                    size += d.augmented_manifest_size.to_string().len();
+                }
+            };
+            size += 1;
+        }
+        size
+    }
+
+    /// Serialize the AugmentedTreeEntry to a blob.
+    pub fn try_serialize(&self, mut w: impl Write) -> Result<()> {
+        w.write_all(b"v1 ")?;
+        w.write_all(self.hg_node_id.to_hex().as_ref())?;
+        w.write_all(b" - ")?; // computed nodeid placeholder
+        if let Some(p1) = &self.p1 {
+            w.write_all(p1.to_hex().as_ref())?;
+        } else {
+            w.write_all(b"-")?;
+        };
+        w.write_all(b" ")?;
+        if let Some(p2) = &self.p2 {
+            w.write_all(p2.to_hex().as_ref())?;
+        } else {
+            w.write_all(b"-")?;
+        };
+        w.write_all(b"\n")?;
+        for (path, subentry) in self.subentries.iter() {
+            w.write_all(path.as_ref())?;
+            w.write_all(b"\0")?;
+            match subentry {
+                AugmentedTreeChildEntry::FileNode(file) => {
+                    w.write_all(file.filenode.to_hex().as_ref())?;
+                    w.write_all(b"r")?;
+                    w.write_all(b" ")?;
+                    w.write_all(file.content_blake3.to_hex().as_ref())?;
+                    w.write_all(b" ")?;
+                    w.write_all(file.total_size.to_string().as_bytes())?;
+                    w.write_all(b" ")?;
+                    w.write_all(file.content_sha1.to_hex().as_ref())?;
+                }
+                AugmentedTreeChildEntry::DirectoryNode(directory) => {
+                    w.write_all(directory.treenode.to_hex().as_ref())?;
+                    w.write_all(b"t")?;
+                    w.write_all(b" ")?;
+                    w.write_all(directory.augmented_manifest_id.to_hex().as_ref())?;
+                    w.write_all(b" ")?;
+                    w.write_all(directory.augmented_manifest_size.to_string().as_ref())?;
+                }
+            }
+            w.write_all(b"\n")?;
+        }
+        Ok(())
+    }
 
     /// Constructs an AugmentedTreeEntry from serialised blob in the format used by Mononoke to store the Augmented Trees in CAS.
     pub fn try_deserialize(mut reader: impl BufRead) -> anyhow::Result<Self, Error> {
@@ -330,5 +409,33 @@ mod tests {
             ))
         );
         assert_eq!(buf.len(), augmented_tree_entry.sapling_tree_blob_size());
+    }
+
+    #[test]
+    fn test_augmented_manifest_parsing_roundtrips() {
+        let tree = concat!(
+            "v1 2d6429cc6d9576d412493d30c700c58a4ac38fbe - 5d09d8b81f6c097d294cb081389428baa9ef96f4 -\n",
+            "AssetWithZoneReclassifications.php\x002f09c0be8738b7256452133d790cc39f9da885b8r 8b2e323f74febd9dce4583c5af41b76d6cc79c8fc87b2400aa090df5af497a35 1137 7a207d1d8ae552303b111bd6030b074a673b918f\n",
+            "ZoneAssetReclassificationsAnnotation.php\x00a7006d0256d90b83b8e8834e3a8d74a57f669364r c3bb30c1b5462c56d178c457a43a30655c305780ae5b2b6fd5711a9288ddd5ae 2940 ba9327007f237bd7f2453ff02aadc1449ac483b9\n",
+            "ZonePolicySetGenerator.php\x0032c4117a356ddd5a284dd55866e4c609e4002c99r eb0c0415ecb4c5461eda8cd52b0d8a5a4bad3ea8bad56b9ad5e6f78ded05de35 6569 b67a3f5383d979f780844812b947b77b4348e475\n",
+            "__tests__\x009f0e8ffab4c1e1adfdea446d3c91b3c8ad525685t 8be9967f8ce1a6c8799f372acb81f57208a7eee78a6e60b6fa6426785fee31d6 248\n",
+            "bounded_policies\x00e057d09012b275e5aa8f3d31ecb334ea7bd0e2dft 79d40367d5845d90c764b4febd8f4671d84645ca749156e11bdf4ed4683fdf3d 274\n",
+            "config\x00ef0295d493a767db31bd2ad6e3c118a5ec2dc094t c1ca36f561ced429b3fdcbb46ea9959cd0db0f1d8c2a06a217aefcacdec53656 1139\n",
+            "enforcement\x00519f107814932a6aaedf68e82a673710461c1a16t 7f275e452b69bdec740341663d123665d48d68ef20d3fd64bf66747c9cd291b3 1872\n",
+            "integration\x0019ff1a891b88dad37743bb22fe29709b15ac195dt 1657e8938e7b48109879d1fcc649c80384e7dcec735bc7361dae86677f6b888f 415\n",
+            "reclassifications\x00f1e2432047947bb339f2d6a2608eca729703bb65t a35546f72429461323d102a6ddfca129e8c7b74fd7cb9b76ec0efa9b4abf250d 649\n",
+            "row_level_policy\x000238567d1c15525d8b2f2d366bd4aa306e20d8dft 3291f1c1ae09629423da93672b2fc52f009ab372a68d57844fef6d7c523d4527 937\n"
+        );
+        let mut reader = std::io::Cursor::new(tree);
+        let augmented_tree_entry =
+            AugmentedTreeEntry::try_deserialize(&mut reader).expect("parsing failed");
+
+        let mut buf: Vec<u8> = Vec::with_capacity(1024);
+        augmented_tree_entry
+            .try_serialize(&mut buf)
+            .expect("writing failed");
+
+        assert_eq!(std::str::from_utf8(&buf), Ok(tree));
+        assert_eq!(augmented_tree_entry.augmented_tree_blob_size(), tree.len());
     }
 }
