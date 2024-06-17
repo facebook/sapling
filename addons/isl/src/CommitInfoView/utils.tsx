5fb20c7dc2bd7e7df6e8fb7633e1c3332c165f64
Evan Krause <evangrayk@meta.com>
2024-06-17 11:46:53 -0700

    Migrate file a bug VSCodeTextAreas
    Summary: Move these VSCodeTextAreas to be TextAreas. This also lets us remove some logic to extract the inner textarea in the tests
    
    Reviewed By: quark-zju
    
    Differential Revision: D58596588
    
    fbshipit-source-id: 92182d9d8b5d43509bd881669e346fc689230eb9

--- addons/isl/src/CommitInfoView/utils.tsx
+++ addons/isl/src/CommitInfoView/utils.tsx
@@ -90,16 +90,6 @@ export function getFieldToAutofocus(
   return undefined;
 }
 
-/**
- * VSCodeTextArea elements use custom components, which renders in a shadow DOM.
- * Most often, we want to access the inner <textarea>, which acts like a normal textarea.
- */
-export function getInnerTextareaForVSCodeTextArea(
-  outer: HTMLElement | null,
-): HTMLTextAreaElement | null {
-  return outer == null ? null : (outer as unknown as {control: HTMLTextAreaElement}).control;
-}
-
 export function getOnClickToken(
   field: FieldConfig & {type: 'field'},
 ): ((token: string) => unknown) | undefined {
