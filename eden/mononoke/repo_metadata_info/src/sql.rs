b89d0c1c6e23ef2e4dd02aed73516ad778b6230e
Rajiv Sharma <rajshar@meta.com>
2024-06-18 09:23:28 -0700

    Implement RepoMetadataInfo facet
    Summary: The previous diff introduced this facet, this diff adds the necessary implementation
    
    Reviewed By: YousefSalama
    
    Differential Revision: D58728157
    
    fbshipit-source-id: b6fc972f69e97919adcdbe4178a43a6b67e95fe3

--- eden/mononoke/repo_metadata_info/src/sql.rs
+++ eden/mononoke/repo_metadata_info/src/sql.rs
@@ -5,24 +5,57 @@
  * GNU General Public License version 2.
  */
 
+use anyhow::Context;
 use anyhow::Result;
 use async_trait::async_trait;
+use mononoke_types::ChangesetId;
 use mononoke_types::RepositoryId;
+use mononoke_types::Timestamp;
 use sql_construct::SqlConstruct;
 use sql_construct::SqlConstructFromMetadataDatabaseConfig;
+use sql_ext::mononoke_queries;
 use sql_ext::SqlConnections;
 
 use super::RepoMetadataInfo;
 use super::RepoMetadataInfoEntry;
 
+mononoke_queries! {
+    write AddOrUpdateRepoMetadataInfo(values: (
+        repo_id: RepositoryId,
+        bookmark_name: String,
+        changeset_id: ChangesetId,
+        last_updated_timestamp: Timestamp,
+    )) {
+        none,
+        "REPLACE INTO repo_metadata_info (repo_id, bookmark_name, changeset_id, last_updated_timestamp) VALUES {values}"
+    }
+
+    read SelectAllEntries(
+        repo_id: RepositoryId,
+    ) -> (String, ChangesetId, Timestamp) {
+        "SELECT bookmark_name, changeset_id, last_updated_timestamp
+         FROM repo_metadata_info
+         WHERE repo_id = {repo_id}"
+    }
+
+    read SelectEntryByBookmark(
+        repo_id: RepositoryId,
+        bookmark_name: String,
+    ) -> (String, ChangesetId, Timestamp) {
+        "SELECT bookmark_name, changeset_id, last_updated_timestamp
+         FROM repo_metadata_info
+         WHERE repo_id = {repo_id} AND bookmark_name = {bookmark_name}"
+    }
+}
+
 pub struct SqlRepoMetadataInfo {
-    _connections: SqlConnections,
+    connections: SqlConnections,
     repo_id: RepositoryId,
 }
 
 #[derive(Clone)]
 pub struct SqlRepoMetadataInfoBuilder {
-    _connections: SqlConnections,
+    connections: SqlConnections,
 }
 
 impl SqlConstruct for SqlRepoMetadataInfoBuilder {
@@ -30,8 +63,8 @@ impl SqlConstruct for SqlRepoMetadataInfoBuilder {
 
     const CREATION_QUERY: &'static str = include_str!("../schemas/sqlite-repo-metadata-info.sql");
 
-    fn from_sql_connections(_connections: SqlConnections) -> Self {
-        Self { _connections }
+    fn from_sql_connections(connections: SqlConnections) -> Self {
+        Self { connections }
     }
 }
 
@@ -40,7 +73,7 @@ impl SqlConstructFromMetadataDatabaseConfig for SqlRepoMetadataInfoBuilder {}
 impl SqlRepoMetadataInfoBuilder {
     pub fn build(self, repo_id: RepositoryId) -> SqlRepoMetadataInfo {
         SqlRepoMetadataInfo {
-            _connections: self._connections,
+            connections: self.connections,
             repo_id,
         }
     }
@@ -55,17 +88,75 @@ impl RepoMetadataInfo for SqlRepoMetadataInfo {
 
     /// Fetch all the metadata info entries for the given repo
     async fn get_all_entries(&self) -> Result<Vec<RepoMetadataInfoEntry>> {
-        Ok(vec![])
+        let results = SelectAllEntries::query(&self.connections.read_connection, &self.repo_id)
+            .await
+            .with_context(|| {
+                format!("Failure in fetching all entries for repo {}", self.repo_id)
+            })?;
+
+        let values = results
+            .into_iter()
+            .map(|(bookmark_name, changeset_id, last_updated_timestamp)| {
+                RepoMetadataInfoEntry::new(changeset_id, bookmark_name, last_updated_timestamp)
+            })
+            .collect::<Vec<_>>();
+        return Ok(values);
     }
 
     /// Fetch the repo metadata entries corresponding to the input bookmark name
     /// for the given repo
-    async fn get_entry(&self, _bookmark_name: String) -> Result<Option<RepoMetadataInfoEntry>> {
-        Ok(None)
+    async fn get_entry(&self, bookmark_name: String) -> Result<Option<RepoMetadataInfoEntry>> {
+        let results = SelectEntryByBookmark::query(
+            &self.connections.read_connection,
+            &self.repo_id,
+            &bookmark_name,
+        )
+        .await
+        .with_context(|| {
+            format!(
+                "Failure in fetching entry for repo {} and bookmark {}",
+                self.repo_id, bookmark_name
+            )
+        })?;
+        // This should not happen but since this is new code, extra checks dont hurt.
+        if results.len() > 1 {
+            anyhow::bail!(
+                "Multiple entries returned for bookmark {} in repo {}",
+                bookmark_name,
+                self.repo_id
+            )
+        }
+        Ok(results.into_iter().next().map(
+            |(bookmark_name, changeset_id, last_updated_timestamp)| {
+                RepoMetadataInfoEntry::new(changeset_id, bookmark_name, last_updated_timestamp)
+            },
+        ))
     }
 
     /// Add new or update existing repo metadata entries for the given repo
-    async fn add_or_update_entries(&self, _entries: Vec<RepoMetadataInfoEntry>) -> Result<()> {
+    async fn add_or_update_entries(&self, entries: Vec<RepoMetadataInfoEntry>) -> Result<()> {
+        let converted_entries: Vec<_> = entries
+            .iter()
+            .map(|entry| {
+                (
+                    &self.repo_id,
+                    &entry.bookmark_name,
+                    &entry.changeset_id,
+                    &entry.last_updated_timestamp,
+                )
+            })
+            .collect();
+        AddOrUpdateRepoMetadataInfo::query(
+            &self.connections.write_connection,
+            converted_entries.as_slice(),
+        )
+        .await
+        .with_context(|| {
+            format!(
+                "Failed to add mappings in repo {} for entries {:?}",
+                self.repo_id, entries,
+            )
+        })?;
         Ok(())
     }
 }
--- eden/mononoke/repo_metadata_info/test/main.rs
+++ eden/mononoke/repo_metadata_info/test/main.rs
@@ -5,10 +5,120 @@
  * GNU General Public License version 2.
  */
 
+use std::collections::HashSet;
+
 use anyhow::Error;
 use fbinit::FacebookInit;
+use mononoke_types::Timestamp;
+use mononoke_types_mocks::changesetid as bonsai;
+use mononoke_types_mocks::repo::REPO_ZERO;
+use repo_metadata_info::RepoMetadataInfo;
+use repo_metadata_info::RepoMetadataInfoEntry;
+use repo_metadata_info::SqlRepoMetadataInfoBuilder;
+use sql_construct::SqlConstruct;
 
 #[fbinit::test]
-async fn dummy_test(_: FacebookInit) -> Result<(), Error> {
+async fn test_add_and_get(_: FacebookInit) -> Result<(), Error> {
+    let metadata_info = SqlRepoMetadataInfoBuilder::with_sqlite_in_memory()?.build(REPO_ZERO);
+    let bookmark_name = "JustABookmark";
+    let timestamp = Timestamp::now();
+    let entry = RepoMetadataInfoEntry {
+        changeset_id: bonsai::ONES_CSID,
+        bookmark_name: bookmark_name.to_string(),
+        last_updated_timestamp: timestamp,
+    };
+    metadata_info
+        .add_or_update_entries(vec![entry.clone()])
+        .await?;
+    let result = metadata_info.get_entry(entry.bookmark_name.clone()).await?;
+
+    assert_eq!(
+        result.as_ref().map(|entry| (
+            entry.bookmark_name.to_string(),
+            entry.changeset_id,
+            entry.last_updated_timestamp
+        )),
+        Some((bookmark_name.to_string(), bonsai::ONES_CSID, timestamp))
+    );
+
+    Ok(())
+}
+
+#[fbinit::test]
+async fn test_update_and_get(_: FacebookInit) -> Result<(), Error> {
+    let metadata_info = SqlRepoMetadataInfoBuilder::with_sqlite_in_memory()?.build(REPO_ZERO);
+    let bookmark_name = "JustABookmark";
+    let timestamp = Timestamp::now();
+    let entry = RepoMetadataInfoEntry {
+        changeset_id: bonsai::ONES_CSID,
+        bookmark_name: bookmark_name.to_string(),
+        last_updated_timestamp: timestamp,
+    };
+
+    metadata_info
+        .add_or_update_entries(vec![entry.clone()])
+        .await?;
+    let result = metadata_info.get_entry(entry.bookmark_name.clone()).await?;
+    assert_eq!(result, Some(entry.clone()));
+
+    // Update the changeset id and try storing the same entry
+    let updated_timestamp = Timestamp::now();
+    let new_entry = RepoMetadataInfoEntry {
+        changeset_id: bonsai::TWOS_CSID,
+        bookmark_name: bookmark_name.to_string(),
+        last_updated_timestamp: updated_timestamp,
+    };
+
+    metadata_info
+        .add_or_update_entries(vec![new_entry.clone()])
+        .await?;
+
+    let result = metadata_info
+        .get_entry(new_entry.bookmark_name.clone())
+        .await?;
+    assert_eq!(result, Some(new_entry.clone()));
+
+    Ok(())
+}
+
+#[fbinit::test]
+async fn test_get_without_add(_: FacebookInit) -> Result<(), Error> {
+    let metadata_info = SqlRepoMetadataInfoBuilder::with_sqlite_in_memory()?.build(REPO_ZERO);
+    let result = metadata_info.get_entry("master".to_string()).await?;
+    assert_eq!(result, None);
+
+    let result = metadata_info.get_all_entries().await?;
+    assert_eq!(result.len(), 0);
+
+    Ok(())
+}
+
+#[fbinit::test]
+async fn test_get_multiple(_: FacebookInit) -> Result<(), Error> {
+    let metadata_info = SqlRepoMetadataInfoBuilder::with_sqlite_in_memory()?.build(REPO_ZERO);
+    let entry = RepoMetadataInfoEntry {
+        changeset_id: bonsai::ONES_CSID,
+        bookmark_name: "master".to_string(),
+        last_updated_timestamp: Timestamp::now(),
+    };
+    let another_entry = RepoMetadataInfoEntry {
+        changeset_id: bonsai::TWOS_CSID,
+        bookmark_name: "release".to_string(),
+        last_updated_timestamp: Timestamp::now(),
+    };
+
+    metadata_info
+        .add_or_update_entries(vec![entry.clone(), another_entry.clone()])
+        .await?;
+
+    let result = metadata_info
+        .get_all_entries()
+        .await?
+        .into_iter()
+        .map(|entry| entry.bookmark_name);
+    assert_eq!(
+        HashSet::from_iter(result),
+        HashSet::from(["master".to_string(), "release".to_string()])
+    );
     Ok(())
 }
