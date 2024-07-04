0944e2f159e28ed93fb355caf64e83a6e745bfb2
Michael Cuevas <cuev@meta.com>
2024-07-04 13:39:33 -0700

    add integration test
    Summary:
    # Context
    
    We'd like to add log rotation for EdenFS (and eventually Watchman, Myles, etc). There are services that already exists on Linux and macOS (logrotate and newsyslog respectively). However, these services require our daemon's to implement log-reopening logic in order to work correctly (on Linux) or at all (on macOS).
    
    We could deal with slightly correctness issues on Linux, but since we need log-reopening logic for log rotation to work at all on macOS, we should implement log-reopening so that we can use log rotation on at least two of our environments.
    
    # Implementation
    
    The basic way to implement log-reopening in Eden is to add a signal handler for SIGHUP. SIGHUP is used to indicate "hanging up" (in other words, the file we're writing to is now closed). When we receive SIGHUP, we can reopen the log file that we're writing to so that we can continue to persist our logs.
    
    # This diff
    
    This diff adds an integration test to show that SIGHUP handling doesn't work right now. The next diff should fix the test and prove that we handle SIGHUP correctly (and it fixes writing to a closed log file).
    
    Reviewed By: chadaustin
    
    Differential Revision: D59303688
    
    fbshipit-source-id: dbbd83ba7078995918fd592d78bcae95f574e183

--- eden/integration/service_log_test.py
+++ eden/integration/service_log_test.py
@@ -6,7 +6,13 @@
 
 # pyre-unsafe
 
+import os
 import pathlib
+import signal
+import subprocess
+from typing import Dict, List, Tuple
+
+from eden.integration.lib.find_executables import FindExe
 
 from .lib.service_test_case import fake_service_test, service_test, ServiceTestCaseBase
 from .lib.start import run_eden_start_with_real_daemon
@@ -48,6 +54,38 @@ class ServiceLogFakeEdenFSTest(ServiceLogTestBase):
 
 @fake_service_test
 class ServiceLogRealEdenFSTest(ServiceLogTestBase):
+    def get_cli_args_and_env(self) -> Tuple[List[str], Dict[str, str]]:
+        edenfsctl, edenfsctl_env = FindExe.get_edenfsctl_env()
+        env = dict(os.environ)
+        env.update(edenfsctl_env)
+        eden_cli_args: List[str] = [
+            edenfsctl,
+            "--config-dir",
+            str(self.eden_dir),
+            "--etc-eden-dir",
+            str(self.etc_eden_dir),
+            "--home-dir",
+            str(self.home_dir),
+        ]
+
+        return (eden_cli_args, env)
+
+    def get_running_daemon_pid(self) -> str:
+        eden_cli_args, env = self.get_cli_args_and_env()
+
+        return subprocess.check_output(
+            eden_cli_args + ["pid"], env=env, encoding="utf-8"
+        )
+
+    def set_running_daemon_log_level(self, log_level: str) -> str:
+        eden_cli_args, env = self.get_cli_args_and_env()
+
+        return subprocess.check_output(
+            eden_cli_args + ["debug", "logging", f".={log_level}"],
+            env=env,
+            encoding="utf-8",
+        )
+
     def test_real_edenfs_writes_logs_to_file_in_eden_dir(self) -> None:
         self.assertFalse(
             self.log_file_path.exists(),
@@ -64,3 +102,49 @@ class ServiceLogRealEdenFSTest(ServiceLogTestBase):
             self.log_file_path.exists(), f"edenfs should create {self.log_file_path}"
         )
         self.assertIn("Starting edenfs", self.log_file_path.read_text())
+
+    def test_eden_reopens_log_file_on_sighup(self) -> None:
+        self.assertFalse(
+            self.log_file_path.exists(),
+            f"{self.log_file_path} should not exist before starting edenfs",
+        )
+
+        self.exit_stack.enter_context(
+            run_eden_start_with_real_daemon(
+                eden_dir=self.eden_dir,
+                etc_eden_dir=self.etc_eden_dir,
+                home_dir=self.home_dir,
+            )
+        )
+
+        self.assertTrue(
+            self.log_file_path.exists(),
+            f"{self.log_file_path} should exist after starting edenfs",
+        )
+
+        # remove and recreate the log file so that the daemon continues to
+        # write to the unlinked log file
+        self.log_file_path.unlink()
+        self.log_file_path.touch(mode=0o644, exist_ok=True)
+        new_log_size = os.stat(self.log_file_path).st_size
+        self.assertEqual(
+            new_log_size, 0, f"{self.log_file_path} should be 0 size after creation"
+        )
+
+        # crank log level to ensure verbose logging occurs
+        self.set_running_daemon_log_level("DBG4")
+
+        # trigger an EdenFS thrift call to force daemon to "log" something
+        pid = int(self.get_running_daemon_pid().strip())
+        self.assertEqual(
+            0,
+            os.stat(self.log_file_path).st_size,
+            f"{self.log_file_path} should be unwriteable after removal",
+        )
+
+        # Send a signal to trigger reopening Eden logs
+        os.kill(pid, signal.SIGHUP)
+
+        # retrigger an EdenFS thrift call and ensure the daemon logged to the reopened logs
+        self.get_running_daemon_pid()
+        self.assertEqual(0, os.stat(self.log_file_path).st_size)
