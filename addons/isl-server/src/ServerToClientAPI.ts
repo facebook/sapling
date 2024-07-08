c284ff2149232d90ba6d7fe461449c2b5e449574
Evan Krause <evangrayk@meta.com>
2024-07-08 14:43:08 -0700

    Improve jf upload error message
    Reviewed By: quark-zju
    
    Differential Revision: D59400836
    
    fbshipit-source-id: 5bfe44ad60b554d803353d0399d74d48aa56aec0

--- addons/isl-server/src/ServerToClientAPI.ts
+++ addons/isl-server/src/ServerToClientAPI.ts
@@ -246,14 +246,14 @@ export default class ServerToClientAPI {
         }
         this.tracker
           .operation('UploadImage', 'UploadImageError', {}, () =>
-            uploadFile(nullthrows(this.connection.logger), {filename, data: payload}),
+            uploadFile(this.logger, {filename, data: payload}),
           )
           .then((result: string) => {
-            this.connection.logger?.info('sucessfully uploaded file', filename, result);
+            this.logger.info('sucessfully uploaded file', filename, result);
             this.postMessage({type: 'uploadFileResult', id, result: {value: result}});
           })
           .catch((error: Error) => {
-            this.connection.logger?.info('error uploading file', filename, error);
+            this.logger.info('error uploading file', filename, error);
             this.postMessage({type: 'uploadFileResult', id, result: {error}});
           });
         break;
