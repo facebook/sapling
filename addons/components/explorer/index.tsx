aa583361ead4fb4c5b0738905cb830115dc9cd02
Evan Krause <evangrayk@meta.com>
2024-07-10 20:28:47 -0700

    Add vite build for component explorer
    Summary: This vite build is for a separate component explorer for the `components/` dir, so that you can test the components without using ISL
    
    Reviewed By: zzl0
    
    Differential Revision: D59305294
    
    fbshipit-source-id: 1c02a2a835c7b09a0482b1f9f78e2f9b46004d4e

---
+++ addons/components/explorer/index.tsx
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {ComponentExplorer} from './ComponentExplorer';
+import 'react';
+import ReactDOM from 'react-dom/client';
+
+// Include CSS variables we use, originally from vscode-webview-ui-toolkit
+import './themeDarkVariables.css';
+import './themeLightVariables.css';
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<ComponentExplorer />);
