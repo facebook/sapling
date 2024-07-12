d2bf3a906fa7e34587d73ded48d324b87fbfc554
Saurav Prakash <sprak@meta.com>
2024-07-12 14:21:31 -0700

    Manual doctor run telemetry (1/4): Add get current process id method to `procinfo` crate
    Summary:
    # Background:
    Eden doctor manual runs count per user is a critical metric for Source Control in H2 and directly relate to user disruptions. It is also one of the [committed KR for RL Source Control](https://docs.google.com/document/d/1rFS4qUj-tfcssbe-gWtm3eegoPqJdozBoad2ZBBlpus/edit#bookmark=id.8k2lgt91uh5y). Currently, there is no way to distinguish between manual and automated runs of Eden doctor via some tools, nor is there a way to track which tools are running Eden doctor automatically.
    # Solution:
    To address this issue, we propose logging the current process ID and parent process ID, along with the parent process name. This will allow us to differentiate between manual and automated runs of Eden doctor, assuming, current process ID and parent process ID will be the same for manual runs but different for automated runs. Additionally, the parent process name will help us track which tools are calling automated Eden doctor runs.
    # Implementation:
    - **Add get current process id method to `procinfo` crate**
    - Refer `procinfo` crate in edenfs-telemetry to retrieve process info and introduce a method to `collect_caller_process_info`
    - Add a wrapper method to `collect_system_info` which optionally `collect_caller_process_info`
    - Use this wrapper method in eden `cli_usage` to collect process info onlyh in case of `doctor` run
    
    # Tracking:
    [Scuba](https://fburl.com/scuba/edenfs_cli_usage/1nkhbsx5)
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D59670676
    
    fbshipit-source-id: d3aa4fbe1f70353524b4e2fabf7fe23a4da493eb

--- eden/scm/lib/procinfo/src/lib.rs
+++ eden/scm/lib/procinfo/src/lib.rs
@@ -142,6 +142,11 @@ pub fn max_rss_bytes() -> u64 {
     0
 }
 
+// Get current process id.
+pub fn current_pid() -> u32 {
+    unsafe { libc::getpid() as u32 }
+}
+
 /// Get the parent pid. Return 0 on error or unsupported platform.
 /// If pid is 0, return the parent pid of the current process.
 pub fn parent_pid(pid: u32) -> u32 {
