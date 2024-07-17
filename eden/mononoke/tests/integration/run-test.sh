8491cd9c5248e84b135aa839a5f4950178c5df73
Mateusz Kwapich <mitrandir@meta.com>
2024-07-17 15:25:26 -0700

    make run_test.sh take a build mode as argument
    Summary:
    I've been using this script for last few days and I find it useful so let's
    expand it a little bit to support other build modes. (as suggested in comments
    for D59583560)
    
    I've chosen `--flagfile` to be consistent with name for the flag in tools like
    `buck` and `arc rust-check`.
    
    The `oss` mode has special `--oss` shortcut to encourage its usage.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D59858547
    
    fbshipit-source-id: da2ff8292f5460e977b34611e78c918dc067e478

--- eden/mononoke/tests/integration/run-test.sh
+++ eden/mononoke/tests/integration/run-test.sh
@@ -4,14 +4,57 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
-if [ "$#" -lt 1 ]; then
-    echo "This command runs individual test file"
-    echo "Usage: $0 test-file.t"
+function print_help {
+    echo "This command runs individual test file."
+    echo
+    echo "Usage: $0 [--oss | --flagfile <FLAGFILE>] test-file.t ..."
+    echo
+    echo "The default build mode is dev-nosan-lg."
+    echo "--oss    change build mode to fbcode//mode/dev-rust-oss"
+    echo "--flagfile FLAGFILE change build mode to specified one"
     exit 1
+}
+
+function fail_error {
+  >&2 echo -e "$(tput setf 5)ERROR: $1$(tput sgr0)"
+  exit "1"
+}
+
+if [ $# -eq 0 ]; then
+    print_help
 fi
-test_file="$1"
-shift
-dott_target="$(buck2 uquery @fbcode//mode/dev-nosan-lg "owner('$test_file')" | grep -- -dott | head -n 1)"
+
+mode="fbcode//mode/dev-nosan-lg"
+
+while [[ $# -gt 0 ]]; do
+  arg="$1"
+  shift
+  case "$arg" in
+    "-h")
+      print_help
+      ;;
+    "--help")
+      print_help
+      ;;
+    "--flagfile")
+      mode="$1"
+      shift
+      ;;
+    "--oss")
+      mode="fbcode//mode/dev-rust-oss"
+      ;;
+    *)
+      break;
+  esac
+done
+
+if [[ "$arg" != *.t ]]; then
+    fail_error "the first positional argument should be a test file, not $arg"
+fi
+
+test_file="$arg"
+
+dott_target="$(buck2 uquery -v 0 --no-interactive-console --flagfile "$mode" "owner('$test_file')" | grep -- -dott | head -n 1)"
 test_target=${dott_target%"-dott"}
-echo '$' buck run @fbcode//mode/dev-nosan-lg "$test_target" -- "$(basename "$test_file")" "$@"
-buck run @fbcode//mode/dev-nosan-lg "$test_target" -- "$(basename "$test_file")" "$@"
+echo "$(tput bold)"'$' buck run --flagfile "$mode" "$test_target" -- "$(basename "$test_file")" "$@" "$(tput sgr0)"
+buck run --flagfile "$mode" "$test_target" -- "$(basename "$test_file")" "$@"
