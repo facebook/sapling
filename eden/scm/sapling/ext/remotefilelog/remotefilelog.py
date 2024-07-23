bff69ca6e09f7e35903454dcb88ca25dcf6c6b5f
Muir Manders <muirdm@meta.com>
2024-07-23 07:08:24 -0700

    remotefilelog: remove linknode() method
    Summary: Doesn't seem like anything calls this.
    
    Reviewed By: quark-zju
    
    Differential Revision: D60027058
    
    fbshipit-source-id: dede136edd836420dacb5c4f3598133dd8443dba

--- eden/scm/sapling/ext/remotefilelog/remotefilelog.py
+++ eden/scm/sapling/ext/remotefilelog/remotefilelog.py
@@ -277,12 +277,6 @@ class remotefilelog:
 
         return p1, p2
 
-    def linknode(self, node):
-        p1, p2, linknode, copyfrom = self.repo.fileslog.metadatastore.getnodeinfo(
-            self.filename, node
-        )
-        return linknode
-
     def revdiff(self, node1, node2):
         if node1 != nullid and (self.flags(node1) or self.flags(node2)):
             raise error.ProgrammingError("cannot revdiff revisions with non-zero flags")
