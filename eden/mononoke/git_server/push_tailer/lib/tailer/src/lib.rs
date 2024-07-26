3798869a86c9b896b2be7ca837fd28e4f642b34c
Saurabh Singh <singhsrb@meta.com>
2024-07-26 02:09:52 -0700

    git_server_push_tailer: print mononoke git repositories while tailing
    Summary:
    This diff is just to plug and play all the code added thus far. It output's all
    the Mononoke git repositories while tailing right now which is more useful than
    what it did earlier.
    
    Reviewed By: YousefSalama
    
    Differential Revision: D60238380
    
    fbshipit-source-id: d9b913f4684df8d016e0752fdafe5cb1ac0c1c2b

--- eden/mononoke/git_server/push_tailer/lib/tailer/src/lib.rs
+++ eden/mononoke/git_server/push_tailer/lib/tailer/src/lib.rs
@@ -7,10 +7,24 @@
 
 #![deny(warnings)]
 
+use anyhow::Error;
 use anyhow::Result;
 use clap::Parser;
+use context::CoreContext;
 use fbinit::FacebookInit;
+use logger::create_logger;
+use mononoke_types::RepositoryId;
+use mysql_client::ConnectionOptionsBuilder;
+use mysql_client::ConnectionPoolOptionsBuilder;
+use push_redirect_config::GitPushRedirectConfig;
+use push_redirect_config::GitPushRedirectConfigEntry;
+use push_redirect_config::SqlGitPushRedirectConfigBuilder;
+use sql_construct::SqlConstruct;
+use storage::Destination;
+use storage::XdbFactory;
 use tokio::time::Duration;
+
+const MONONOKE_PRODUCTION_SHARD_NAME: &str = "xdb.mononoke_production";
 
 #[derive(Debug, Parser)]
 pub struct Args {
@@ -19,11 +33,45 @@ pub struct Args {
     mononoke_polling_interval: u64,
 }
 
-pub async fn tail(_fb: FacebookInit, args: Args) -> Result<()> {
-    let mut interval = tokio::time::interval(Duration::from_secs(args.mononoke_polling_interval));
+fn create_prod_xdb_factory(fb: FacebookInit) -> Result<XdbFactory> {
+    let pool_options = ConnectionPoolOptionsBuilder::default()
+        .build()
+        .map_err(Error::msg)?;
+    let conn_options = ConnectionOptionsBuilder::default()
+        .build()
+        .map_err(Error::msg)?;
+    let destination = Destination::Prod;
+    XdbFactory::new(fb, destination, pool_options, conn_options)
+}
 
+async fn current_mononoke_git_repositories(
+    ctx: &CoreContext,
+    xdb_factory: &XdbFactory,
+) -> Result<Vec<RepositoryId>> {
+    let xdb = xdb_factory
+        .create_or_get_shard(MONONOKE_PRODUCTION_SHARD_NAME)
+        .await?;
+    let connections = xdb.read_conns().await?;
+    let git_push_redirect_config: &dyn GitPushRedirectConfig =
+        &SqlGitPushRedirectConfigBuilder::from_sql_connections(connections).build();
+
+    let entries: Vec<GitPushRedirectConfigEntry> = git_push_redirect_config
+        .get_redirected_to_mononoke(ctx)
+        .await?;
+    Ok(entries.into_iter().map(|entry| entry.repo_id).collect())
+}
+
+pub async fn tail(fb: FacebookInit, args: Args) -> Result<()> {
+    let logger = create_logger();
+    let ctx = CoreContext::new_with_logger(fb, logger);
+    let xdb_factory = create_prod_xdb_factory(fb)?;
+
+    let mut interval = tokio::time::interval(Duration::from_secs(args.mononoke_polling_interval));
     loop {
         interval.tick().await;
-        println!("Future goodness yet to be implemented!");
+        println!(
+            "Current Mononoke Git server repositories: {:?}",
+            current_mononoke_git_repositories(&ctx, &xdb_factory).await?
+        );
     }
 }
