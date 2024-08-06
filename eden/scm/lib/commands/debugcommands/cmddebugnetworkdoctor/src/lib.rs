5f765e8e0d40ddef4059112cd7a6505c0194fe6e
Chris Dinh <cqd@meta.com>
2024-08-06 12:25:57 -0700

    run hg debugnetworkdoctor returns 1 on error
    Summary:
    # Context
    Hg debugnetworkdoctor is a utility that diagnoses network issues. We already run this as part of eden rage. We now want to add this to eden doctor to add some additional signal on potential issues.
    
    # This Diff
    Update return value to be 1 when there is an error diagnosed
    # Technical Details
    used next diff
    
    Reviewed By: zzl0
    
    Differential Revision: D60774516
    
    fbshipit-source-id: 6e982fbec59ecdd4cbaf3b2a3b602a75deff397c

--- eden/scm/lib/commands/debugcommands/cmddebugnetworkdoctor/src/lib.rs
+++ eden/scm/lib/commands/debugcommands/cmddebugnetworkdoctor/src/lib.rs
@@ -15,10 +15,15 @@ pub fn run(ctx: ReqCtx<NoOpts>) -> Result<u8> {
     let config = ctx.config();
     let mut stdout = ctx.io().output();
     match network_doctor::Doctor::new().diagnose(config) {
-        Ok(()) => write!(stdout, "No network problems detected.\n")?,
-        Err(d) => write!(stdout, "{}\n\n{}\n", d.treatment(config), d)?,
-    };
-    Ok(0)
+        Ok(()) => {
+            write!(stdout, "No network problems detected.\n")?;
+            Ok(0)
+        }
+        Err(d) => {
+            write!(stdout, "{}\n\n{}\n", d.treatment(config), d)?;
+            Ok(1)
+        }
+    }
 }
 
 pub fn aliases() -> &'static str {
