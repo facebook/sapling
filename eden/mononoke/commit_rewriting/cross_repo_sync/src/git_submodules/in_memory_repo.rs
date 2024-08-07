3bdf78f132fb9cc42577d69cb667d42ee16e8a65
Youssef Ibrahim <youssefsalama@meta.com>
2024-08-07 04:31:12 -0700

    cross_repo_sync/in_memory_repo: remove changesets facet from InMemoryRepo
    Summary: The `InMemoryRepo` is used only for deriving data, it has no use for the `Changesets` facet.
    
    Reviewed By: gustavoavena
    
    Differential Revision: D60831509
    
    fbshipit-source-id: 9f756bcd057ea42028c02d191bbebf37b5030d44

--- eden/mononoke/commit_rewriting/cross_repo_sync/src/git_submodules/in_memory_repo.rs
+++ eden/mononoke/commit_rewriting/cross_repo_sync/src/git_submodules/in_memory_repo.rs
@@ -9,7 +9,6 @@ use std::fmt;
 use std::fmt::Debug;
 use std::sync::Arc;
 
-use anyhow::bail;
 use anyhow::Result;
 use async_trait::async_trait;
 use blobstore::Blobstore;
@@ -17,33 +16,17 @@ use blobstore::BlobstoreBytes;
 use blobstore::BlobstoreGetData;
 use bonsai_hg_mapping::MemWritesBonsaiHgMapping;
 use cacheblob::MemWritesBlobstore;
-use changesets::ChangesetEntry;
-use changesets::ChangesetInsert;
-use changesets::Changesets;
-use changesets::SortOrder;
-use changesets_impl::SqlChangesets;
-use changesets_impl::SqlChangesetsBuilder;
-use commit_graph::CommitGraph;
 use context::CoreContext;
 use derivative::Derivative;
 use filestore::FilestoreConfig;
 use futures::future;
 use futures::stream;
-use futures::stream::BoxStream;
-use futures::try_join;
 use futures::StreamExt;
-use itertools::Itertools;
-use mononoke_types::ChangesetId;
-use mononoke_types::ChangesetIdPrefix;
-use mononoke_types::ChangesetIdsResolvedFromPrefix;
-use mononoke_types::RepositoryId;
 use readonlyblob::ReadOnlyBlobstore;
 use repo_blobstore::RepoBlobstore;
 use repo_derived_data::RepoDerivedData;
 use repo_identity::RepoIdentity;
 use scuba_ext::MononokeScubaSampleBuilder;
-use sql_construct::SqlConstruct;
-use vec1::Vec1;
 
 use crate::git_submodules::dummy_struct::DummyStruct;
 use crate::types::Repo;
@@ -64,10 +47,6 @@ pub struct InMemoryRepo {
     #[facet]
     filestore_config: FilestoreConfig,
 
-    // Only needed for creation of RepoDerivedData and for unit test
-    #[facet]
-    pub changesets: dyn Changesets,
-
     #[facet]
     repo_identity: RepoIdentity,
 }
@@ -80,12 +59,9 @@ impl InMemoryRepo {
         fallback_repos: Vec<Arc<R>>,
     ) -> Result<Self> {
         let repo_identity = repo.repo_identity().clone();
-
-        let scuba = MononokeScubaSampleBuilder::with_discard();
-
         let original_blobstore = repo.repo_blobstore().clone();
-
         let repo_prefix = repo_identity.id().prefix();
+        let filestore_config = repo.filestore_config().clone();
 
         let mem_writes_repo_blobstore =
             RepoBlobstore::new_with_wrapped_inner_blobstore(original_blobstore, |blobstore| {
@@ -98,155 +74,26 @@ impl InMemoryRepo {
                     fallback_repos,
                 ))
             });
-        let orig_derived_data = repo.repo_derived_data();
+        let memwrites_bonsai_hg_mapping =
+            Arc::new(MemWritesBonsaiHgMapping::new(repo.bonsai_hg_mapping_arc()));
+        let dummy_filenodes = Arc::new(DummyStruct);
+        let dummy_bonsai_git_mapping = Arc::new(DummyStruct);
 
-        let orig_changesets = repo.changesets_arc().clone();
-        let in_memory_changesets = Arc::new(InMemoryChangesets::new(orig_changesets)?);
-        let bonsai_hg_mapping = MemWritesBonsaiHgMapping::new(repo.bonsai_hg_mapping_arc());
-
-        let filenodes = Arc::new(DummyStruct);
-        let bonsai_git_mapping = Arc::new(DummyStruct);
-
-        let commit_graph =
-            Arc::new(<CommitGraph as Clone>::clone(repo.commit_graph()).with_memwrites_storage());
-
-        let lease = orig_derived_data.lease().clone();
-
-        let derived_data_config = orig_derived_data.config().clone();
-
-        let derivation_service_client = None;
-
-        let filestore_config = repo.filestore_config().clone();
-
-        let repo_derived_data = RepoDerivedData::new(
-            repo_identity.id(),
-            repo_identity.name().to_string(),
-            in_memory_changesets.clone(),
-            commit_graph,
-            Arc::new(bonsai_hg_mapping),
-            bonsai_git_mapping,
-            filenodes,
-            mem_writes_repo_blobstore.clone(),
-            filestore_config,
-            lease,
-            scuba,
-            derived_data_config,
-            derivation_service_client,
-        )?;
+        let repo_derived_data = repo
+            .repo_derived_data()
+            .with_mutated_scuba(|_| MononokeScubaSampleBuilder::with_discard())
+            .with_replaced_blobstore(mem_writes_repo_blobstore.clone())
+            .with_replaced_bonsai_hg_mapping(memwrites_bonsai_hg_mapping)
+            .with_replaced_filenodes(dummy_filenodes)
+            .with_replaced_bonsai_git_mapping(dummy_bonsai_git_mapping)
+            .with_replaced_derivation_service_client(None);
 
         Ok(Self {
             repo_blobstore: mem_writes_repo_blobstore.into(),
             derived_data: repo_derived_data.into(),
             filestore_config: filestore_config.into(),
-            changesets: in_memory_changesets,
             repo_identity: Arc::new(repo_identity.clone()),
         })
-    }
-}
-
-#[derive(Derivative, Clone)]
-pub struct InMemoryChangesets {
-    inner: Arc<dyn Changesets>,
-    sql_in_memory: Arc<SqlChangesets>,
-}
-
-impl InMemoryChangesets {
-    pub fn new(inner: Arc<dyn Changesets>) -> Result<Self> {
-        let sql_in_memory: SqlChangesets = SqlChangesetsBuilder::with_sqlite_in_memory()?
-            .build(Default::default(), inner.repo_id());
-        Ok(Self {
-            inner,
-            sql_in_memory: Arc::new(sql_in_memory),
-        })
-    }
-
-    async fn get_ephemeral(
-        &self,
-        ctx: &CoreContext,
-        cs_ids: &[ChangesetId],
-    ) -> Result<Vec<ChangesetEntry>> {
-        self.sql_in_memory.get_many(ctx, cs_ids.to_vec()).await
-    }
-}
-
-#[async_trait]
-impl Changesets for InMemoryChangesets {
-    fn repo_id(&self) -> RepositoryId {
-        self.inner.repo_id()
-    }
-
-    async fn add(&self, ctx: &CoreContext, cs: ChangesetInsert) -> Result<bool> {
-        let parents_len = cs.parents.len();
-        let parents = self.get_many(ctx, cs.parents.clone()).await?;
-        if parents.len() != parents_len {
-            bail!(
-                "Not all parents found, expected [{}], found [{}]",
-                cs.parents.into_iter().map(|id| id.to_string()).join(", "),
-                parents
-                    .into_iter()
-                    .map(|entry| entry.cs_id.to_string())
-                    .join(", ")
-            );
-        }
-        self.sql_in_memory.add(ctx, cs).await
-    }
-
-    async fn add_many(&self, ctx: &CoreContext, css: Vec1<ChangesetInsert>) -> Result<()> {
-        // If necessary, this can be optimised.
-        for cs in css {
-            self.add(ctx, cs).await?;
-        }
-        Ok(())
-    }
-
-    async fn get(&self, ctx: &CoreContext, cs_id: ChangesetId) -> Result<Option<ChangesetEntry>> {
-        Ok(self.get_many(ctx, vec![cs_id]).await?.into_iter().next())
-    }
-
-    async fn get_many(
-        &self,
-        ctx: &CoreContext,
-        cs_ids: Vec<ChangesetId>,
-    ) -> Result<Vec<ChangesetEntry>> {
-        let ephemeral = self.get_ephemeral(ctx, &cs_ids);
-        let persistent = self.inner.get_many(ctx, cs_ids.clone());
-        let (mut ephemeral, persistent) = try_join!(ephemeral, persistent)?;
-        ephemeral.extend(persistent);
-        Ok(ephemeral)
-    }
-
-    /// Use caching for the full changeset ids and slower path otherwise.
-    async fn get_many_by_prefix(
-        &self,
-        _ctx: &CoreContext,
-        _cs_prefix: ChangesetIdPrefix,
-        _limit: usize,
-    ) -> Result<ChangesetIdsResolvedFromPrefix> {
-        unimplemented!()
-    }
-
-    fn prime_cache(&self, _ctx: &CoreContext, _changesets: &[ChangesetEntry]) {
-        // no caching involved
-    }
-
-    async fn enumeration_bounds(
-        &self,
-        _ctx: &CoreContext,
-        _read_from_master: bool,
-        _known_heads: Vec<ChangesetId>,
-    ) -> Result<Option<(u64, u64)>> {
-        unimplemented!()
-    }
-
-    fn list_enumeration_range(
-        &self,
-        _ctx: &CoreContext,
-        _min_id: u64,
-        _max_id: u64,
-        _sort_and_limit: Option<(SortOrder, u64)>,
-        _read_from_master: bool,
-    ) -> BoxStream<'_, Result<(ChangesetId, u64)>> {
-        unimplemented!()
     }
 }
 
--- eden/mononoke/commit_rewriting/cross_repo_sync/test/git_submodules/in_memory_repo_tests.rs
+++ eden/mononoke/commit_rewriting/cross_repo_sync/test/git_submodules/in_memory_repo_tests.rs
@@ -12,16 +12,15 @@ use std::sync::Arc;
 use anyhow::Context;
 use anyhow::Result;
 use blobstore::Loadable;
-use changesets::ChangesetEntry;
-use changesets::ChangesetsRef;
 use changesets_creation::save_changesets;
 use context::CoreContext;
 use cross_repo_sync::InMemoryRepo;
 use fbinit::FacebookInit;
+use fsnodes::RootFsnodeId;
 use mononoke_types::BonsaiChangesetMut;
 use mononoke_types::DateTime;
-use mononoke_types::RepositoryId;
 use repo_blobstore::RepoBlobstoreRef;
+use repo_derived_data::RepoDerivedDataRef;
 use sorted_vector_map::SortedVectorMap;
 
 use crate::git_submodules::git_submodules_test_utils::*;
@@ -34,67 +33,27 @@ async fn test_original_blobstore_and_changesets_are_the_same_after_validation(
 
     let (fallback_repo, _fallback_repo_cs_map) = build_repo_c(fb).await?;
 
-    let (orig_repo, _orig_repo_cs_map) = build_repo_b(fb).await?;
+    let (orig_repo, orig_repo_cs_map) = build_repo_b(fb).await?;
 
     let fallback_repos = vec![Arc::new(fallback_repo.clone())];
     // Create an InMemoryRepo from the original repo
     let in_memory_repo = InMemoryRepo::from_repo(&orig_repo, fallback_repos)?;
 
-    // Create a new bonsai changeset using the in_memory_repo
-    let new_bonsai_mut = BonsaiChangesetMut {
-        parents: vec![],
-        message: "Create directory in repo B".into(),
-        file_changes: SortedVectorMap::new(),
-        author_date: DateTime::from_timestamp(0, 0).unwrap(),
-        ..Default::default()
-    };
+    let orig_repo_commit = *orig_repo_cs_map.get("B_B").unwrap();
 
-    let bonsai = new_bonsai_mut.freeze().unwrap();
-    let cs_id = bonsai.get_changeset_id();
-    // Save that changeset
-    save_changesets(&ctx, &in_memory_repo, vec![bonsai])
-        .await
-        .context("Failed to save changesets")?;
+    // Derive Fsnodes for a commit in the InMemoryRepo
+    in_memory_repo
+        .repo_derived_data()
+        .derive::<RootFsnodeId>(&ctx, orig_repo_commit)
+        .await?;
 
-    // 1. Changeset can be loaded from in_memory_repo
-    let from_in_memory = cs_id.load(&ctx, &in_memory_repo.repo_blobstore()).await?;
-    println!("from_in_memory: {0:#?}", from_in_memory);
-
-    let from_orig_repo = cs_id.load(&ctx, &orig_repo.repo_blobstore()).await;
-
-    // 2. Changeset can't be loaded from original repo
-    println!(
-        "Loading changeset from original repo: {0:#?}",
-        from_orig_repo
-    );
-    assert!(from_orig_repo.is_err_and(|e| {
-        // Fails to find changeset blob
-        e.to_string().contains("Blob is missing: changeset.blake2")
-    }));
-
-    // 3. Changeset entry can be found using the in_memory_repo
-    let in_memory_changesets = in_memory_repo.changesets();
-    let in_mememory_changeset = in_memory_changesets.get(&ctx, cs_id).await?;
-
-    assert_eq!(
-        in_mememory_changeset,
-        Some(ChangesetEntry {
-            repo_id: RepositoryId::new(2),
-            cs_id,
-            parents: vec![],
-            gen: 1,
-        })
-    );
-
-    // 4. Changeset entry can't be found using the original repo
-    let orig_changesets = orig_repo.changesets();
-    let orig_changeset = orig_changesets.get(&ctx, cs_id).await?;
-
-    println!("orig_changeset: {0:#?}", &orig_changeset);
-
+    // Check that Fsnodes are not derived for that commit in the original repo
     assert!(
-        orig_changeset.is_none(),
-        "Changeset was found in original repo when it shouldn't be"
+        orig_repo
+            .repo_derived_data()
+            .fetch_derived::<RootFsnodeId>(&ctx, orig_repo_commit)
+            .await?
+            .is_none()
     );
 
     // ------------------ Test fallback repo ------------------
@@ -109,7 +68,7 @@ async fn test_original_blobstore_and_changesets_are_the_same_after_validation(
     };
 
     let bonsai = new_bonsai_mut.freeze().unwrap();
-    let _ = bonsai.get_changeset_id();
+    let cs_id = bonsai.get_changeset_id();
 
     println!("fallback_changeset_bonsai: {0:#?}", &bonsai);
 
@@ -118,14 +77,14 @@ async fn test_original_blobstore_and_changesets_are_the_same_after_validation(
         .await
         .context("Failed to save changesets")?;
 
-    // 5. Fallback repo changeset can be loaded from in_memory repo
+    // Fallback repo changeset can be loaded from in_memory repo
     let fallback_changeset_in_memory = cs_id.load(&ctx, &in_memory_repo.repo_blobstore()).await?;
     println!(
         "fallback_changeset_in_memory: {0:#?}",
         fallback_changeset_in_memory
     );
 
-    // 6. Fallback repo bonsai can't be loaded from original repo
+    // Fallback repo bonsai can't be loaded from original repo
     let fallback_changeset_orig_repo = cs_id.load(&ctx, &orig_repo.repo_blobstore()).await;
     println!(
         "Loading Fallback repo changeset from original repo: {0:#?}",
