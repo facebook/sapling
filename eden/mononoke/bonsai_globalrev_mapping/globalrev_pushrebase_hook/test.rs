e95257effb111736412a2d8da56520cd28b1393d
Youssef Ibrahim <youssefsalama@meta.com>
2024-08-13 09:13:23 -0700

    de-blobrepo-ify globalrev_pushrebase_hook crate
    Summary: Remove all dependencies on `BlobRepo` in the `globalrev_pushrebase_hook` crate.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D61205807
    
    fbshipit-source-id: e14431588e6c278958a963e9ff2adca5f5588eba

--- eden/mononoke/bonsai_globalrev_mapping/globalrev_pushrebase_hook/test.rs
+++ eden/mononoke/bonsai_globalrev_mapping/globalrev_pushrebase_hook/test.rs
@@ -9,13 +9,18 @@ use std::time::Duration;
 
 use anyhow::Error;
 use async_trait::async_trait;
-use blobrepo::BlobRepo;
 use blobstore::Loadable;
+use bonsai_globalrev_mapping::BonsaiGlobalrevMapping;
 use bonsai_globalrev_mapping::BonsaiGlobalrevMappingArc;
+use bonsai_hg_mapping::BonsaiHgMapping;
 use bookmarks::BookmarkTransactionError;
+use bookmarks::Bookmarks;
 use borrowed::borrowed;
+use commit_graph::CommitGraph;
+use commit_graph::CommitGraphWriter;
 use context::CoreContext;
 use fbinit::FacebookInit;
+use filestore::FilestoreConfig;
 use futures::future::try_join_all;
 use maplit::hashset;
 use mononoke_types::globalrev::Globalrev;
@@ -29,7 +34,10 @@ use pushrebase_hook::PushrebaseHook;
 use pushrebase_hook::PushrebaseTransactionHook;
 use pushrebase_hook::RebasedChangesets;
 use rand::Rng;
+use repo_blobstore::RepoBlobstore;
 use repo_blobstore::RepoBlobstoreRef;
+use repo_derived_data::RepoDerivedData;
+use repo_identity::RepoIdentity;
 use repo_identity::RepoIdentityRef;
 use sql::Transaction;
 use test_repo_factory::TestRepoFactory;
@@ -39,6 +47,37 @@ use tests_utils::CreateCommitContext;
 
 use crate::GlobalrevPushrebaseHook;
 
+#[facet::container]
+#[derive(Clone)]
+struct Repo {
+    #[facet]
+    repo_identity: RepoIdentity,
+
+    #[facet]
+    bonsai_globalrev_mapping: dyn BonsaiGlobalrevMapping,
+
+    #[facet]
+    bonsai_hg_mapping: dyn BonsaiHgMapping,
+
+    #[facet]
+    repo_derived_data: RepoDerivedData,
+
+    #[facet]
+    repo_blobstore: RepoBlobstore,
+
+    #[facet]
+    commit_graph: CommitGraph,
+
+    #[facet]
+    commit_graph_writer: dyn CommitGraphWriter,
+
+    #[facet]
+    filestore_config: FilestoreConfig,
+
+    #[facet]
+    bookmarks: dyn Bookmarks,
+}
+
 #[fbinit::test]
 fn pushrebase_assigns_globalrevs(fb: FacebookInit) -> Result<(), Error> {
     let runtime = tokio::runtime::Runtime::new()?;
@@ -47,7 +86,7 @@ fn pushrebase_assigns_globalrevs(fb: FacebookInit) -> Result<(), Error> {
 
 async fn pushrebase_assigns_globalrevs_impl(fb: FacebookInit) -> Result<(), Error> {
     let ctx = CoreContext::test_mock(fb);
-    let repo: BlobRepo = TestRepoFactory::new(fb)?
+    let repo: Repo = TestRepoFactory::new(fb)?
         .with_id(RepositoryId::new(1))
         .build()
         .await?;
@@ -202,7 +241,7 @@ async fn pushrebase_race_assigns_monotonic_globalrevs(fb: FacebookInit) -> Resul
     }
 
     let ctx = CoreContext::test_mock(fb);
-    let repo: BlobRepo = TestRepoFactory::new(fb)?
+    let repo: Repo = TestRepoFactory::new(fb)?
         .with_id(RepositoryId::new(1))
         .build()
         .await?;
