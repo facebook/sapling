998250831560717f383ccea96941d64cbc49d1c1
Zhaolong Zhu <zhaolong@meta.com>
2024-08-19 12:09:39 -0700

    tests: remove duplicated copyright info
    Reviewed By: lmvasquezg
    
    Differential Revision: D61476618
    
    fbshipit-source-id: 78e96fcbf73626644b937554898f33752f49b00e

--- eden/scm/tests/default_hgrc.py
+++ eden/scm/tests/default_hgrc.py
@@ -3,11 +3,6 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This software may be used and distributed according to the terms of the
-# GNU General Public License version 2.
-
 """
 Default config file for testing
 """
