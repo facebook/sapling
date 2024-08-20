2e9f292539c27dc318f16a789f0f913c39f01d89
Jun Wu <quark@meta.com>
2024-08-20 11:27:33 -0700

    localrepo: do not run commit hook when lock is released
    Summary:
    Reverts https://repo.mercurial-scm.org/hg/rev/d0e419b0f7de. It seems no longer necessary.
    Without this, `test-import.t` fails with D61211474, like:
    
      $ touch empty.patch
      $ hg import a.patch empty.patch --traceback
      # parents: ['2e6efcdab50a732b0adf226b60a15b7031e39f4f']
      applying a.patch
      applying empty.patch
      Traceback (most recent call last):
        File "sapling/commands/__init__.py", line 3946, in import_
          release(lock, dsguard, wlock)
        File "sapling/lock.py", line 400, in release
          lock.release()
        File "sapling/lock.py", line 380, in release
          callback()
        File "sapling/localrepo.py", line 2832, in commithook
          if self.changelog.hasnode(ret):
          # ret = bin('41cb93bb6d693a192627b942c00695b513e92f39')
        File "sapling/localrepo.py", line 97, in __get__
          return super(_basefilecache, self).__get__(repo, type)
        File "sapling/scmutil.py", line 1227, in __get__
          entry.obj = self.func(obj)
        File "sapling/localrepo.py", line 1362, in changelog
          cl = _openchangelog(self)
        File "sapling/localrepo.py", line 3396, in _openchangelog
          repo._rsrepo.invalidatechangelog()
      error.CommitLookupError: '41cb93bb6d693a192627b942c00695b513e92f39 cannot be found'
      abort: 41cb93bb6d693a192627b942c00695b513e92f39 cannot be found!
      [255]
    
    Reviewed By: muirdm
    
    Differential Revision: D61506282
    
    fbshipit-source-id: 21b2fc9f416dd2a765f3958ae248bf3f358f508d

--- eden/scm/sapling/localrepo.py
+++ eden/scm/sapling/localrepo.py
@@ -2826,13 +2826,8 @@ class localrepository:
         finally:
             lockmod.release(tr, lock, wlock)
 
-        def commithook(node=hex(ret), parent1=hookp1, parent2=hookp2):
-            # hack for command that use a temporary commit (eg: histedit)
-            # temporary commit got stripped before hook release
-            if self.changelog.hasnode(ret):
-                self.hook("commit", node=node, parent1=parent1, parent2=parent2)
+        self.hook("commit", node=hex(ret), parent1=hookp1, parent2=hookp2)
 
-        self._afterlock(commithook)
         return ret
 
     def commitctx(self, ctx, error=False):
