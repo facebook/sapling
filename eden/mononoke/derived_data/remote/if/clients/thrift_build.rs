714cdbef24dff955e899da8cc66197f836acb99f
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2024-08-20 04:47:21 -0700

    mononoke: update thrift files
    Summary: ^
    
    Reviewed By: RajivTS
    
    Differential Revision: D61533990
    
    fbshipit-source-id: 100101c923488bb725c91c1d0980547549fada68

--- eden/mononoke/derived_data/remote/if/clients/thrift_build.rs
+++ eden/mononoke/derived_data/remote/if/clients/thrift_build.rs
@@ -29,6 +29,9 @@ eden/mononoke/mononoke_types/serialization/test_manifest.thrift mononoke_types_s
 eden/mononoke/mononoke_types/serialization/time.thrift mononoke_types_serialization //eden/mononoke/mononoke_types/serialization:mononoke_types_serialization-rust
 eden/mononoke/mononoke_types/serialization/unodes.thrift mononoke_types_serialization //eden/mononoke/mononoke_types/serialization:mononoke_types_serialization-rust
 fb303/thrift/fb303_core.thrift fb303_core //fb303/thrift:fb303_core-rust
+thrift/annotation/cpp.thrift fb303_core->cpp //thrift/annotation:cpp-rust
+thrift/annotation/scope.thrift fb303_core->cpp->scope //thrift/annotation:scope-rust
+thrift/annotation/thrift.thrift fb303_core->cpp->thrift //thrift/annotation:thrift-rust
 ";
 #[rustfmt::skip]
 fn main() {
--- eden/mononoke/megarepo_api/if/thrift_build.rs
+++ eden/mononoke/megarepo_api/if/thrift_build.rs
@@ -29,6 +29,7 @@ eden/mononoke/mononoke_types/serialization/time.thrift mononoke_types_serializat
 eden/mononoke/mononoke_types/serialization/unodes.thrift mononoke_types_serialization //eden/mononoke/mononoke_types/serialization:mononoke_types_serialization-rust
 eden/mononoke/scs/if/source_control.thrift source_control //eden/mononoke/scs/if:source_control-rust
 fb303/thrift/fb303_core.thrift source_control->fb303_core //fb303/thrift:fb303_core-rust
+thrift/annotation/cpp.thrift source_control->fb303_core->cpp //thrift/annotation:cpp-rust
 thrift/annotation/rust.thrift source_control->megarepo_configs->rust //thrift/annotation:rust-rust
 thrift/annotation/scope.thrift source_control->thrift->scope //thrift/annotation:scope-rust
 thrift/annotation/thrift.thrift source_control->thrift //thrift/annotation:thrift-rust
