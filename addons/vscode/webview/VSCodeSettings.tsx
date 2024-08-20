f8e6b3bd1757ac0772f12a8ea8b42e57251841b9
Evan Krause <evangrayk@meta.com>
2024-08-20 10:07:54 -0700

    Add vscode setting for isl.openBeside into UI settings
    Summary: Add a vscodeConfigBacked atom util so we can easily make atoms for vscode configs. Then, use this to show a setting checkbox for "open beside".
    
    Reviewed By: quark-zju
    
    Differential Revision: D61496115
    
    fbshipit-source-id: fa9bbbd9b62ac702b2e702e60f326547cecbab54

--- addons/vscode/webview/VSCodeSettings.tsx
+++ addons/vscode/webview/VSCodeSettings.tsx
@@ -5,15 +5,21 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import type {Json} from 'shared/typeUtils';
+
+import serverAPI from '../../isl/src/ClientToServerAPI';
 import {ComparisonPanelMode, comparisonPanelMode, setComparisonPanelMode} from './state';
+import {Checkbox} from 'isl-components/Checkbox';
 import {Dropdown} from 'isl-components/Dropdown';
 import {Tooltip} from 'isl-components/Tooltip';
 import {Setting} from 'isl/src/Setting';
 import {T, t} from 'isl/src/i18n';
-import {useAtomValue} from 'jotai';
+import {writeAtom} from 'isl/src/jotaiUtils';
+import {atom, useAtom, useAtomValue} from 'jotai';
 
 export default function VSCodeSettings() {
   const panelMode = useAtomValue(comparisonPanelMode);
+  const [openBesides, setOpenBesides] = useAtom(openBesidesSetting);
   return (
     <Setting title={<T>VS Code Settings</T>}>
       <Tooltip
@@ -33,6 +39,48 @@ export default function VSCodeSettings() {
           />
         </div>
       </Tooltip>
+      <Tooltip
+        title={t(
+          'If true, files, diffs, and comparisons will open beside the existing ISL panel instead of in the same View Column. Useful to keep ISL open and visible when clicking on files.',
+        )}>
+        <Checkbox checked={openBesides} onChange={checked => setOpenBesides(checked)}>
+          <T>Open Besides</T>
+        </Checkbox>
+      </Tooltip>
     </Setting>
+  );
+}
+
+const openBesidesSetting = vscodeConfigBackedAtom<boolean>('sapling.isl.openBeside', false);
+
+function vscodeConfigBackedAtom<T extends Json>(
+  configName: string,
+  defaultValue: T,
+  scope: 'global' | 'workspace' = 'global',
+) {
+  const primitiveAtom = atom<T>(defaultValue);
+
+  serverAPI.postMessage({
+    type: 'platform/subscribeToVSCodeConfig',
+    config: configName,
+  });
+  serverAPI.onMessageOfType('platform/vscodeConfigChanged', config => {
+    if (config.config === configName) {
+      writeAtom(primitiveAtom, config.value as T);
+    }
+  });
+
+  return atom<T, [T | ((prev: T) => T)], void>(
+    get => get(primitiveAtom),
+    (get, set, update) => {
+      const newValue = typeof update === 'function' ? update(get(primitiveAtom)) : update;
+      set(primitiveAtom, newValue);
+      serverAPI.postMessage({
+        type: 'platform/setVSCodeConfig',
+        config: configName,
+        value: newValue,
+        scope,
+      });
+    },
   );
 }
