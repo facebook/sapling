0678a491add6e6726471004004acd48977cad212
Xiaowei Lu <lxw@meta.com>
2024-08-22 11:29:08 -0700

    assert_status should return the number of tries
    Summary:
    # Background
    
    Since we added the status cache which checks the Journal sequence to decide if a cached status can be reused instead of computing the diff every time `hg status` is called, there is a chance that the Journal sequence fails to advance in time so the old status is returned. This is the cost we have to pay.
    
    To make the status tests more stable, we added the timeout parameter for `assert_status` helper function and made it by default 1 sec.
    
    For the new tests added for status cache, we are relying on checking the counters to make sure the cache is working as expected. However, the retries from this function can mess the counters. See how we call `self.counter_check()` after `assert_status()` in `test_scm_status_cache`.
    
    # This Diff
    
    By returning the total number of tries from `assert_status()`, we can easily check the counters based on it because we now know how many misses happened ( `num_of_tries - 1`).
    
    Reviewed By: kmancini
    
    Differential Revision: D61643516
    
    fbshipit-source-id: b973a2f006e93f44cf8fd7fb3f322024f8cd4d4b

--- eden/integration/hg/lib/hg_extension_test_base.py
+++ eden/integration/hg/lib/hg_extension_test_base.py
@@ -284,8 +284,8 @@ class EdenHgTestCase(testcase.EdenTestCase, metaclass=abc.ABCMeta):
         op: Optional[str] = None,
         check_ignored: bool = True,
         rev: Optional[str] = None,
-        timeout_seconds: float = 1.0,
-    ) -> None:
+        timeout_seconds: float = 1.0,  # after adding status cache, we need to wait for edenfs to pick up the working copy modifications
+    ) -> int:
         """Asserts the output of `hg status` matches the expected state.
 
         `expected` is a dict where keys are paths relative to the repo
@@ -296,11 +296,15 @@ class EdenHgTestCase(testcase.EdenTestCase, metaclass=abc.ABCMeta):
 
         Use timeout to wait for EdenFS pick up the working copy modifications.
         For details of this see 'SyncBehavior' in eden.thrift
+
+        Returns the total number of tries.
         """
         poll_interval_seconds = 0.1
         deadline = time.monotonic() + timeout_seconds
+        num_of_tries = 0
         while True:
             try:
+                num_of_tries += 1
                 actual_status = self.repo.status(include_ignored=check_ignored, rev=rev)
                 self.assertDictEqual(expected, actual_status, msg=msg)
                 self.assert_unfinished_operation(op)
@@ -310,6 +314,7 @@ class EdenHgTestCase(testcase.EdenTestCase, metaclass=abc.ABCMeta):
                     raise e
                 time.sleep(poll_interval_seconds)
                 continue
+        return num_of_tries
 
     def assert_status_empty(
         self,
