6397918d518530ad4d08f901817ff4c3326d6c4e
Youssef Ibrahim <youssefsalama@meta.com>
2024-08-29 09:08:06 -0700

    just_knobs_defaults: add defaults for JKs used in tests
    Summary: Add default values for some JKs currently used in integration/unit tests. Defaults are choosen to keep current test behaviour.
    
    Reviewed By: gustavoavena
    
    Differential Revision: D61910172
    
    fbshipit-source-id: 25c98c9dbb4c04e15cc66ea4f856521aa8316cac

--- eden/mononoke/bookmarks/bookmarks_movement/src/restrictions.rs
+++ eden/mononoke/bookmarks/bookmarks_movement/src/restrictions.rs
@@ -23,13 +23,13 @@ use crate::Repo;
 
 pub(crate) fn should_run_hooks(authz: &AuthorizationContext, reason: BookmarkUpdateReason) -> bool {
     if authz.is_service() {
-        let disable_running_hooks_in_pushredirected_repo = justknobs::eval(
+        let enable_hooks_on_service_pushrebase = justknobs::eval(
             "scm/mononoke:enable_hooks_on_service_pushrebase",
             None,
             None,
         )
         .unwrap_or_default();
-        reason == BookmarkUpdateReason::Pushrebase && disable_running_hooks_in_pushredirected_repo
+        reason == BookmarkUpdateReason::Pushrebase && enable_hooks_on_service_pushrebase
     } else {
         true
     }
