bddec6e9dde5359d5b9b1b328de20807b03682a1
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2024-08-30 07:18:55 -0700

    scs: implement get workspaces
    Summary: This allows callers to know which workspaces belong to a certain user.
    
    Reviewed By: RajivTS
    
    Differential Revision: D61982069
    
    fbshipit-source-id: ce9a26e6d69f1f2607fd412653f90b1f7a113449

--- eden/mononoke/scs_server/src/methods/cloud.rs
+++ eden/mononoke/scs_server/src/methods/cloud.rs
@@ -7,26 +7,28 @@
 
 use context::CoreContext;
 use mononoke_api_hg::RepoContextHgExt;
+use mononoke_types::commit_cloud::WorkspaceData;
 
-use crate::errors;
 use crate::errors::invalid_request;
 use crate::errors::ServiceError;
 use crate::into_response::IntoResponse;
 use crate::methods::thrift;
 use crate::source_control_impl::SourceControlServiceImpl;
 
+const USER_WORKSPACE_PREFIX: &str = "user/";
+
 impl SourceControlServiceImpl {
     pub async fn cloud_workspace_info(
         &self,
         ctx: CoreContext,
         params: thrift::CloudWorkspaceInfoParams,
-    ) -> Result<thrift::CloudWorkspaceInfoResponse, errors::ServiceError> {
+    ) -> Result<thrift::CloudWorkspaceInfoResponse, ServiceError> {
         let repo = self.repo(ctx, &params.workspace.repo).await?;
         let info = repo
             .hg()
             .cloud_workspace(&params.workspace.name, &params.workspace.repo.name)
             .await
-            .map_err(errors::invalid_request)?;
+            .map_err(invalid_request)?;
 
         Ok(thrift::CloudWorkspaceInfoResponse {
             workspace_info: info.into_response(),
@@ -36,11 +38,29 @@ impl SourceControlServiceImpl {
 
     pub async fn cloud_user_workspaces(
         &self,
-        _ctx: CoreContext,
-        _params: thrift::CloudUserWorkspacesParams,
-    ) -> Result<thrift::CloudUserWorkspacesResponse, errors::ServiceError> {
-        Err(ServiceError::Request(invalid_request(
-            "'cloud_user_workspaces' is not implemented yet'".to_string(),
-        )))
+        ctx: CoreContext,
+        params: thrift::CloudUserWorkspacesParams,
+    ) -> Result<thrift::CloudUserWorkspacesResponse, ServiceError> {
+        if !commit_cloud_helpers::is_valid_linux_user(&params.user) {
+            return Err(ServiceError::Request(invalid_request(format!(
+                "{} is not a valid unixname",
+                &params.user
+            ))));
+        }
+
+        let repo = self.repo(ctx, &params.repo).await?;
+        let prefix = format!("{}{}", USER_WORKSPACE_PREFIX, &params.user);
+        let info = repo
+            .hg()
+            .cloud_workspaces(&prefix, &params.repo.name)
+            .await
+            .map_err(invalid_request)?;
+
+        let workspaces = info.into_iter().map(WorkspaceData::into_response).collect();
+
+        Ok(thrift::CloudUserWorkspacesResponse {
+            workspaces,
+            ..Default::default()
+        })
     }
 }
