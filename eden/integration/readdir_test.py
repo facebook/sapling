cf56bea2949295a6f68f7c7bdac9cf62494cac84
Michael Cuevas <cuev@meta.com>
2024-09-09 11:40:18 -0700

    address Pyre lints
    Reviewed By: genevievehelsel
    
    Differential Revision: D61859773
    
    fbshipit-source-id: f1574cb0e91c338bde19c91eaab43094eb07e0ac

--- eden/integration/readdir_test.py
+++ eden/integration/readdir_test.py
@@ -69,33 +69,20 @@ class RawObjectId:
 
 @testcase.eden_repo_test
 class ReaddirTest(testcase.EdenRepoTest):
-    # pyre-fixme[13]: Attribute `commit1` is never initialized.
-    commit1: str
-    # pyre-fixme[13]: Attribute `commit2` is never initialized.
-    commit2: str
-    # pyre-fixme[13]: Attribute `commit3` is never initialized.
-    commit3: str
+    commit1: str = ""
+    commit2: str = ""
+    commit3: str = ""
 
-    # pyre-fixme[13]: Attribute `adir_file_id` is never initialized.
-    adir_file_id: bytes
-    # pyre-fixme[13]: Attribute `bdir_file_id` is never initialized.
-    bdir_file_id: bytes
-    # pyre-fixme[13]: Attribute `hello_id` is never initialized.
-    hello_id: bytes
-    # pyre-fixme[13]: Attribute `slink_id` is never initialized.
-    slink_id: bytes
-    # pyre-fixme[13]: Attribute `adir_id` is never initialized.
-    adir_id: bytes
-    # pyre-fixme[13]: Attribute `adir_size_result` is never initialized.
-    adir_size_result: SizeOrError
-    # pyre-fixme[13]: Attribute `adir_blake3_result` is never initialized.
-    adir_blake3_result: Blake3OrError
-    # pyre-fixme[13]: Attribute `cdir_subdir_id` is never initialized.
-    cdir_subdir_id: bytes
-    # pyre-fixme[13]: Attribute `cdir_subdir_size_result` is never initialized.
-    cdir_subdir_size_result: SizeOrError
-    # pyre-fixme[13]: Attribute `cdir_subdir_blake3_result` is never initialized.
-    cdir_subdir_blake3_result: Blake3OrError
+    adir_file_id: bytes = b""
+    bdir_file_id: bytes = b""
+    hello_id: bytes = b""
+    slink_id: bytes = b""
+    adir_id: bytes = b""
+    adir_size_result: SizeOrError = SizeOrError()
+    adir_blake3_result: Blake3OrError = Blake3OrError()
+    cdir_subdir_id: bytes = b""
+    cdir_subdir_size_result: SizeOrError = SizeOrError()
+    cdir_subdir_blake3_result: Blake3OrError = Blake3OrError()
 
     def setup_eden_test(self) -> None:
         self.enable_windows_symlinks = True
