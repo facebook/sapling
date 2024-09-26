b744a4c9ba062dc4c3dca936225f9edc34ec9c7b
Evan Krause <evangrayk@meta.com>
2024-09-25 18:11:17 -0700

    Expose promise based feature flag API
    Summary:
    Previously, feature flags were implemented with `lazyAtom`. This made it hard to get at the underlying atom to cache the promise so you could await a fetch without going through the UI.
    
    It's arguably simpler if we don't use lazyAtom at all and just have the atom be an `Atom<Promise<boolean>>`, and then use `loadable` to get at it synchronously.
    
    In my testing, this doesn't make it overfetch the GKs.
    
    Reviewed By: muirdm
    
    Differential Revision: D63416529
    
    fbshipit-source-id: 54e7cac21de2b33b20a7e5b6932b8c6208353751

--- addons/isl/src/featureFlags.ts
+++ addons/isl/src/featureFlags.ts
@@ -6,34 +6,51 @@
  */
 
 import serverAPI from './ClientToServerAPI';
-import {atomFamilyWeak, lazyAtom} from './jotaiUtils';
+import {atomFamilyWeak, readAtom} from './jotaiUtils';
 import {atom, useAtomValue} from 'jotai';
+import {loadable} from 'jotai/utils';
 
 /**
  * Boolean values to enable features via remote config.
  * TODO: we could cache values in localstorage to avoid async lookup time if you've previously fetched it
  */
-export const featureFlag = atomFamilyWeak((name?: string) => {
+export const featureFlagAsync = atomFamilyWeak((name?: string) => {
   if (name == null) {
     // OSS doesn't have access to feature flags, so they are always "false" by setting the name to null
-    return atom(false);
+    return atom(Promise.resolve(false));
   }
 
-  return lazyAtom(async () => {
-    serverAPI.postMessage({
-      type: 'fetchFeatureFlag',
-      name,
-    });
-    const response = await serverAPI.nextMessageMatching(
-      'fetchedFeatureFlag',
-      message => message.name === name,
-    );
-    return response.passes;
-  }, undefined);
+  return atom(fetchFeatureFlag(name));
+});
+
+const featureFlagLoadable = atomFamilyWeak((name?: string) => {
+  return loadable(featureFlagAsync(name));
 });
 
 /** Access recoil featureFlag state without suspending or throwing */
 export function useFeatureFlagSync(name: string | undefined) {
-  const maybe = useAtomValue(featureFlag(name));
-  return maybe ?? false;
+  const flag = useAtomValue(featureFlagLoadable(name));
+  return flag.state === 'hasData' ? flag.data : false;
+}
+
+export function getFeatureFlag(name: string | undefined, default_?: boolean): Promise<boolean> {
+  if (name == null) {
+    return Promise.resolve(default_ ?? false);
+  }
+  return readAtom(featureFlagAsync(name));
+}
+
+async function fetchFeatureFlag(name: string | undefined, default_?: boolean): Promise<boolean> {
+  if (name == null) {
+    return default_ ?? false;
+  }
+  serverAPI.postMessage({
+    type: 'fetchFeatureFlag',
+    name,
+  });
+  const response = await serverAPI.nextMessageMatching(
+    'fetchedFeatureFlag',
+    message => message.name === name,
+  );
+  return response.passes;
 }
