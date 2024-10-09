65aeed45b18fe8f7b3a559cae3e83e4794231c66
Michael Cuevas <cuev@meta.com>
2024-10-08 22:35:44 -0700

    fix more ambiguous calls
    Summary:
    # This diff
    
    Prefixes our call to format_to with `fmt::` to remove the ambiguous function call error during compilation.
    
    # Context
    
    This is the exact same issue we hit in D63902777, which fixed references within Watchman. To recap:
    
    C++ 20 added std::format_to, which causes bare format_to calls to be ambiguous. We can easily fix the issue by qualifying the calls with fmt:: as done in this diff.
    
    Reviewed By: kavehahmadi60
    
    Differential Revision: D64085858
    
    fbshipit-source-id: 5f1536baa5cfd8dc31ae8c5eba7e1b4e029138f3

--- eden/fs/store/ImportPriority.h
+++ eden/fs/store/ImportPriority.h
@@ -183,7 +183,7 @@ template <>
 struct formatter<facebook::eden::ImportPriority> : formatter<std::string> {
   template <typename Context>
   auto format(facebook::eden::ImportPriority priority, Context& ctx) const {
-    return format_to(
+    return fmt::format_to(
         ctx.out(),
         "({}, {:+d})",
         priority.className(),
