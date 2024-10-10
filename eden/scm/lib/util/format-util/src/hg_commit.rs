550c7df08bed42a5f6f076355dfc5d1e127c1f6c
Jun Wu <quark@meta.com>
2024-10-10 11:46:50 -0700

    format-util: add functions to extract root tree from commit text
    Summary: This will be used in other places.
    
    Reviewed By: muirdm
    
    Differential Revision: D64078082
    
    fbshipit-source-id: 8b391aceed8c700d552b05919128ecb98cf1c6ab

---
+++ eden/scm/lib/util/format-util/src/hg_commit.rs
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+use anyhow::Context as _;
+use anyhow::Result;
+use types::Id20;
+
+pub fn hg_commit_text_to_root_tree_id(text: &[u8]) -> Result<Id20> {
+    let hex = text.get(0..Id20::hex_len()).context("invalid git commit")?;
+    Ok(Id20::from_hex(hex)?)
+}
--- eden/scm/lib/util/format-util/src/lib.rs
+++ eden/scm/lib/util/format-util/src/lib.rs
@@ -7,14 +7,29 @@
 
 //! Utilities interacting with store serialization formats (git or hg).
 
+use anyhow::Result;
+use types::Id20;
+
+mod git_commit;
 mod git_sha1;
+mod hg_commit;
 mod hg_filelog;
 mod hg_sha1;
 
+pub use git_commit::git_commit_text_to_root_tree_id;
 pub use git_sha1::git_sha1_deserialize;
 pub use git_sha1::git_sha1_serialize;
+pub use hg_commit::hg_commit_text_to_root_tree_id;
 pub use hg_filelog::parse_copy_from_hg_file_metadata;
 pub use hg_filelog::split_hg_file_metadata;
 pub use hg_filelog::strip_hg_file_metadata;
 pub use hg_sha1::hg_sha1_deserialize;
 pub use hg_sha1::hg_sha1_serialize;
+use storemodel::SerializationFormat;
+
+pub fn commit_text_to_root_tree_id(text: &[u8], format: SerializationFormat) -> Result<Id20> {
+    match format {
+        SerializationFormat::Hg => hg_commit_text_to_root_tree_id(text),
+        SerializationFormat::Git => git_commit_text_to_root_tree_id(text),
+    }
+}
