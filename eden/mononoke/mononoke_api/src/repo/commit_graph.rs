39b1c1efe1c799fa836990e9a11678bc00a3c6ac
Mark Juggurnauth-Thomas <mbthomas@meta.com>
2024-10-14 08:07:02 -0700

    mononoke_api: add graph segments method for git
    Summary:
    Add a Git equivalent of `graph_segments`.
    
    There seems to be a lot of repetition here, but it's converting to different types.  We should improve the ergonomics so that it seems less repetetive, but that will be a follow-up.
    
    Reviewed By: andreacampi
    
    Differential Revision: D64134400
    
    fbshipit-source-id: 68ee9257c3af6f885f559c604eedf6203a98cd0b

--- eden/mononoke/mononoke_api/src/repo/commit_graph.rs
+++ eden/mononoke/mononoke_api/src/repo/commit_graph.rs
@@ -9,6 +9,7 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 use anyhow::Context;
+use bonsai_git_mapping::BonsaiGitMappingRef;
 use bonsai_hg_mapping::BonsaiHgMappingRef;
 use commit_graph::CommitGraphRef;
 use dag_types::Location;
@@ -17,6 +18,7 @@ use futures::stream::Stream;
 use futures::stream::StreamExt;
 use futures::stream::TryStreamExt;
 use mercurial_types::HgChangesetId;
+use mononoke_types::hash::GitSha1;
 use mononoke_types::ChangesetId;
 
 use super::MononokeRepo;
@@ -97,6 +99,102 @@ impl<R: MononokeRepo> RepoContext<R> {
                         .ok_or_else(|| {
                             MononokeError::InvalidRequest(format!(
                                 "failed to find hg equivalent for {} {}",
+                                name, csid,
+                            ))
+                        })
+                        .copied()
+                };
+                anyhow::Ok(stream::iter(segments.into_iter().map(move |segment| {
+                    Ok(ChangesetSegment {
+                        head: map_id("segment head", segment.head)?,
+                        base: map_id("segment base", segment.base)?,
+                        length: segment.length,
+                        parents: segment
+                            .parents
+                            .into_iter()
+                            .map(|parent| {
+                                Ok(ChangesetSegmentParent {
+                                    id: map_id("segment parent", parent.cs_id)?,
+                                    location: parent
+                                        .location
+                                        .map(|location| {
+                                            anyhow::Ok(Location::new(
+                                                map_id("location head", location.head)?,
+                                                location.distance,
+                                            ))
+                                        })
+                                        .transpose()?,
+                                })
+                            })
+                            .collect::<Result<_, MononokeError>>()?,
+                    })
+                })))
+            })
+            .buffered(25)
+            .try_flatten())
+    }
+
+    /// Get a stream of the linear segments of the commit graph between the common and heads as GitSha1.
+    pub async fn graph_segments_git(
+        &self,
+        common: Vec<GitSha1>,
+        heads: Vec<GitSha1>,
+    ) -> Result<
+        impl Stream<Item = Result<ChangesetSegment<GitSha1>, MononokeError>> + '_,
+        MononokeError,
+    > {
+        let bonsai_common = self
+            .repo()
+            .bonsai_git_mapping()
+            .get(self.ctx(), common.into())
+            .await?
+            .into_iter()
+            .map(|e| e.bcs_id)
+            .collect();
+        let bonsai_heads = self
+            .repo()
+            .bonsai_git_mapping()
+            .get(self.ctx(), heads.into())
+            .await?
+            .into_iter()
+            .map(|e| e.bcs_id)
+            .collect();
+
+        let segments = self
+            .repo()
+            .commit_graph()
+            .ancestors_difference_segments(self.ctx(), bonsai_heads, bonsai_common)
+            .await?;
+
+        Ok(stream::iter(segments.into_iter())
+            .chunks(25)
+            .map(move |segments| async move {
+                let mut ids = HashSet::with_capacity(segments.len() * 4);
+                for segment in segments.iter() {
+                    ids.insert(segment.head);
+                    ids.insert(segment.base);
+                    for parent in segment.parents.iter() {
+                        ids.insert(parent.cs_id);
+                        if let Some(location) = &parent.location {
+                            ids.insert(location.head);
+                        }
+                    }
+                }
+                let mapping: HashMap<ChangesetId, GitSha1> = self
+                    .repo()
+                    .bonsai_git_mapping()
+                    .get(self.ctx(), ids.into_iter().collect::<Vec<_>>().into())
+                    .await
+                    .context("error fetching bonsai-git mapping")?
+                    .into_iter()
+                    .map(|e| (e.bcs_id, e.git_sha1))
+                    .collect();
+                let map_id = move |name, csid| {
+                    mapping
+                        .get(&csid)
+                        .ok_or_else(|| {
+                            MononokeError::InvalidRequest(format!(
+                                "failed to find git equivalent for {} {}",
                                 name, csid,
                             ))
                         })
