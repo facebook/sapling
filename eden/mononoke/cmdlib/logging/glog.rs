73408f65f211559f1582f28fe9c129738b98f4d2
Mateusz Kwapich <mitrandir@meta.com>
2024-10-16 09:46:06 -0700

    force glog log level
    Summary:
    zeus rust library overrides the log levels for all cpp logging which makes it
    spew stuff in integration tests. Let's battle such behaviour by
    resetting the glog minlelvel back to what was set in environment variabl.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D64471249
    
    fbshipit-source-id: 0c9291f117ecc4d3384eb68cfa52fab0b950fa65

--- eden/mononoke/cmdlib/logging/glog.rs
+++ eden/mononoke/cmdlib/logging/glog.rs
@@ -12,18 +12,30 @@ use slog::Level;
 /// Sets the log level used by glog library to match the level use we use in slog.
 /// It can be overridden by setting the GLOG_minloglevel env variable.
 pub fn set_glog_log_level(fb: FacebookInit, level: Level) -> Result<()> {
-    if std::env::var("GLOG_minloglevel").is_err() {
-        let level_glog_num = match level {
-            Level::Critical => 4,
-            Level::Error => 3,
-            Level::Warning => 2,
-            // Normally glog 1 corresponds to our INFO but since we normally
-            // care very little about things logged by our deps it might be
-            // easier to delegate those to DEBUG output.
-            Level::Info => 2,
-            _ => 0,
-        };
-        gflags::set_gflag_value(fb, "minloglevel", gflags::GflagValue::U32(level_glog_num))?;
+    match std::env::var("GLOG_minloglevel") {
+        Ok(level) => {
+            if let Ok(level_glog_num) = level.parse::<u32>() {
+                // set the flag again, some libraries we use might have overriden it.
+                gflags::set_gflag_value(
+                    fb,
+                    "minloglevel",
+                    gflags::GflagValue::U32(level_glog_num),
+                )?;
+            }
+        }
+        _ => {
+            let level_glog_num = match level {
+                Level::Critical => 4,
+                Level::Error => 3,
+                Level::Warning => 2,
+                // Normally glog 1 corresponds to our INFO but since we normally
+                // care very little about things logged by our deps it might be
+                // easier to delegate those to DEBUG output.
+                Level::Info => 2,
+                _ => 0,
+            };
+            gflags::set_gflag_value(fb, "minloglevel", gflags::GflagValue::U32(level_glog_num))?;
+        }
     }
     Ok(())
 }
