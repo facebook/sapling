d9744c95159fe797e26b05945cc20498d166126e
Xiaowei Lu <lxw@meta.com>
2024-10-15 17:34:38 -0700

    a state machine implementation for Rust Remove
    Summary:
    ## Background
    In [this doc](https://fburl.com/doc_oxidation_rm) we introduced a graph to describe how a path should be completely removed. The process is represented as a state machine.
    
    ## This Diff
    In this diff, we defined some basic data structures and a very simple workflow to show how the state machine should work under this definition.
    
    ### State Impl
    For each `State` variant, we define a type with the same name to hold the actual logic of what we want to do for a given state (the `next` method). And the next state will be the return value of this logic.
    
    There are three kinds of results when "next()" runs:
    1. `OK(nest_state)`: current state runs successfully; the state should tarnsition to the next state.
    2. `OK(none)`: current state runs successfully; current state is one of the terminal state that there is no next state; command should exit after this state.
    3. `Err`: current state sees exception and we should terminate the command and show the exception/error messages to the user.
    
    ### Main Loop
    In the main loop, we keep calling `run()` method until we reach any one of the "end states" (represented by `None`).
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D62390953
    
    fbshipit-source-id: fc1f5bedc584d3a2c799991499c2f882e8248898

--- eden/fs/cli_rs/edenfs-commands/src/remove.rs
+++ eden/fs/cli_rs/edenfs-commands/src/remove.rs
@@ -7,6 +7,9 @@
 
 //! edenfsctl remove
 
+use std::path::Path;
+use std::path::PathBuf;
+
 use anyhow::anyhow;
 use anyhow::Result;
 use async_trait::async_trait;
@@ -38,15 +41,170 @@ pub struct RemoveCmd {
     preserve_mount_point: bool,
 }
 
+struct RemoveContext {
+    original_path: String,
+    canonical_path: PathBuf,
+}
+
+impl RemoveContext {
+    fn new(original_path: String) -> RemoveContext {
+        RemoveContext {
+            original_path,
+            canonical_path: PathBuf::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct SanityCheck {}
+impl SanityCheck {
+    /// This is the first step of the remove process. It will verify that the path is valid and exists.
+    fn next(&self, context: &mut RemoveContext) -> Result<Option<State>> {
+        match Path::new(&context.original_path).canonicalize() {
+            // cannonicalize() will check if the path exists for us so this is all we need
+            Ok(path) => {
+                context.canonical_path = path;
+                Ok(Some(State::Determination(Determination {})))
+            }
+            Err(e) => Err(anyhow!(
+                "Error canonicalizing path {}: {}",
+                context.original_path,
+                e
+            )),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Determination {}
+impl Determination {
+    fn next(&self, _context: &mut RemoveContext) -> Result<Option<State>> {
+        if Confirm::new()
+            .with_prompt("Determination State is not implemented yet... proceed?")
+            .interact()?
+        {
+            return Err(anyhow!("Rust remove(Determination) is not implemented!"));
+        }
+        // TODO: this is an example of indicating a terminal state
+        Ok(None)
+    }
+}
+
+#[derive(Debug)]
+enum State {
+    // function states (no real action performed)
+    SanityCheck(SanityCheck),
+    Determination(Determination),
+    // Validation,
+
+    // // removal states (harmful operations)
+    // ActiveEdenMount,
+    // InactiveEdenMount,
+    // CleanUp,
+    // RegFile,
+    // Unknown,
+}
+
+impl State {
+    fn start() -> State {
+        State::SanityCheck(SanityCheck {})
+    }
+
+    /// Runs the actions defined for this state
+    /// There are three cases for the return value:
+    /// 1. Ok(Some(State)) - we succeed in moving to the next state
+    /// 2. Ok(None) - we are in a terminal state and the removal is successful
+    /// 3. Err - the removal failed
+    fn run(&self, context: &mut RemoveContext) -> Result<Option<State>> {
+        match self {
+            State::SanityCheck(inner) => inner.next(context),
+            State::Determination(inner) => inner.next(context),
+        }
+    }
+}
+
 #[async_trait]
 impl Subcommand for RemoveCmd {
     async fn run(&self) -> Result<ExitCode> {
-        if Confirm::new()
-            .with_prompt("This command is not ready yet... proceed?")
-            .interact()?
-        {
-            return Err(anyhow!("Rust remove is unimplemented!"));
+        // TODO: remove this check eventually because we should be able to remove multiple paths
+        assert!(
+            self.paths.len() == 1,
+            "Currently supporting only one path given per run"
+        );
+
+        let mut context = RemoveContext::new(self.paths[0].clone());
+        let mut state = Some(State::start());
+
+        while state.is_some() {
+            match state.unwrap().run(&mut context) {
+                Ok(next_state) => state = next_state,
+                Err(e) => {
+                    // TODO: handling error processing like logging, etc
+                    return Err(e);
+                }
+            }
         }
+
         Ok(0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Context;
+    use tempfile::tempdir;
+    use tempfile::TempDir;
+
+    use super::*;
+
+    #[cfg(unix)]
+    const PATH_NOT_FOUND_ERROR_MSG: &str = "No such file or directory";
+
+    #[cfg(windows)]
+    const PATH_NOT_FOUND_ERROR_MSG: &str = "The system cannot find the path specified";
+
+    /// This helper function creates a directory structure that looks like this:
+    /// "some_tmp_dir/test/nested/inner"
+    /// then it returns the path to the "some_tmp_dir" directory
+    fn prepare_directory() -> TempDir {
+        let temp_dir = tempdir().context("couldn't create temp dir").unwrap();
+        let path = temp_dir.path().join("test").join("nested").join("inner");
+        let prefix = path.parent().unwrap();
+        println!("creating dirs: {}", prefix.to_str().unwrap());
+        std::fs::create_dir_all(prefix).unwrap();
+        temp_dir
+    }
+
+    #[test]
+    fn test_sanity_check_pass() {
+        let tmp_dir = prepare_directory();
+        let path = format!("{}/test/nested/../nested", tmp_dir.path().to_str().unwrap());
+        let mut context = RemoveContext::new(path);
+        let state = State::start().run(&mut context).unwrap().unwrap();
+
+        assert!(
+            matches!(state, State::Determination(_)),
+            "Expected Determination state"
+        );
+        assert!(context.canonical_path.ends_with("test/nested"));
+    }
+
+    #[test]
+    fn test_sanity_check_fail() {
+        let tmp_dir = prepare_directory();
+        let path = format!(
+            "{}/test/nested/../../nested/inner",
+            tmp_dir.path().to_str().unwrap()
+        );
+        let mut context = RemoveContext::new(path);
+        let state: std::result::Result<Option<State>, anyhow::Error> =
+            State::start().run(&mut context);
+        assert!(state.is_err());
+        assert!(
+            state
+                .unwrap_err()
+                .to_string()
+                .contains(PATH_NOT_FOUND_ERROR_MSG)
+        );
     }
 }
