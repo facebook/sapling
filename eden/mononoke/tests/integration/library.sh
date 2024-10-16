77545100974b7105f3a9095688ae46fdaabfc8d7
Saul Gutierrez <sggutier@meta.com>
2024-10-15 23:22:05 -0700

    tests: move port ephemeral_db_config to Python
    Reviewed By: quark-zju
    
    Differential Revision: D64313869
    
    fbshipit-source-id: 2ad4f5994944e94d0609594d554b14b18c238c6e

--- eden/mononoke/tests/integration/library.sh
+++ eden/mononoke/tests/integration/library.sh
@@ -745,14 +745,7 @@ function db_config() {
 }
 
 function ephemeral_db_config() {
-  local blobstorename="$1"
-  if [[ -n "$DB_SHARD_NAME" ]]; then
-    echo "[$blobstorename.metadata.remote]"
-    echo "db_address = \"$DB_SHARD_NAME\""
-  else
-    echo "[$blobstorename.metadata.local]"
-    echo "local_db_path = \"$TESTTMP/monsql\""
-  fi
+  python_fn ephemeral_db_config "$@"
 }
 
 function blobstore_db_config() {
