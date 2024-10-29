fd7ebacaad2189b910c8f92db86763c197de9f5d
Chris Dinh <cqd@meta.com>
2024-10-29 13:35:23 -0700

    Split of "[Eden] Add integration test for locked files on windows"
    Summary:
    # Context
    Processes having open handles to files when source control actions happen can leave eden in a corrupted state
    
    # This Diff
    This diff adds tests for the cases when a file handle is open with the listed parameters
    open flags(read, write, read+write) x share flags combinations(read, write, delete)
    
    # Technical Details
    Default state: commits A->B->C
    There is a test file in commit B that is modified from commit A. In commit C, the file is removed.
    
    Each test performs the following:
    checks if the test file can be read
    checks if checking out commit A succeeds
    checks if checking out commit C succeeds
    
    # Discussion Points
    The following bugs are revealed by this integration test:
     - When a process has a handle with only share_read, when checking out a commit where that file does not exist, the checkout will succeed. The file will not be changed, and diff/status will not report that the file is modified from the expected state(aka the file shouldn't exist on that commit so it should show up as a new file, or the checkout should fail/partial checkout state)
     - When a process has a handle with share_read and share_delete, checkouts will succeed completely. I'm not sure if this is actually a bug or if share_delete is a superset of share_write
    
    Reviewed By: ViniGupta08
    
    Differential Revision: D63797366
    
    fbshipit-source-id: 8a56a4a035b36a80355aabcb2bea88a03fbb78ed

--- eden/integration/lock_test.py
+++ eden/integration/lock_test.py
@@ -8,8 +8,25 @@
 
 import os
 import subprocess
+import sys
 
 from .lib import testcase
+
+if sys.platform == "win32":
+    try:
+        from .lib.ntapi import open_file_handle
+    except ImportError:
+        # TODO(T150221518): We should add the ntapi extension module to the
+        # getdeps build, but for now we have to account for the possibility that
+        # it may not be present.
+        pass
+
+GENERIC_READ = 0x80000000
+GENERIC_WRITE = 0x40000000
+
+FILE_SHARE_DELETE = 0x00000004
+FILE_SHARE_READ = 0x00000001
+FILE_SHARE_WRITE = 0x00000002
 
 
 @testcase.eden_repo_test
@@ -18,38 +35,41 @@ class LockTest(testcase.EdenRepoTest):
     add_file_commit: str = ""
     update_file_commit: str = ""
     remove_file_commit: str = ""
+    BASE_FILE_NAME = "mint"
+    BASE_FILE_CONTENTS = "mint\n"
+    UPDATED_FILE_CONTENTS = "mintoo\n"
 
     def populate_repo(self) -> None:
         self.maxDiff = None
-        self.repo.write_file("mint", "mint\n")
+        self.repo.write_file(self.BASE_FILE_NAME, self.BASE_FILE_CONTENTS)
         self.repo.write_file("adir/file", "foo!\n")
         self.repo.write_file("bdir/test.sh", "#!/bin/bash\necho test\n", mode=0o755)
         self.repo.write_file("bdir/noexec.sh", "#!/bin/bash\necho test\n")
-        self.repo.symlink("slink", "mint")
+        self.repo.symlink("slink", self.BASE_FILE_NAME)
         self.add_file_commit = self.repo.commit("Adds a new file.")
-        self.repo.write_file("mint", "mintoo\n")
+        self.repo.write_file(self.BASE_FILE_NAME, self.UPDATED_FILE_CONTENTS)
         self.update_file_commit = self.repo.commit("Updates the file.")
-        self.repo.remove_files([self.repo.get_path("mint")])
+        self.repo.remove_files([self.repo.get_path(self.BASE_FILE_NAME)])
         self.remove_file_commit = self.repo.commit("Remove newly added file.")
         self.repo.update(self.update_file_commit)
 
     def check_read_allowed(self, expected_text: str) -> None:
         # Checks that external programs are allowed to read the file
-        with open(self.repo.get_path("mint"), "r") as f:
+        with open(self.repo.get_path(self.BASE_FILE_NAME), "r") as f:
             self.assertEqual(f.read(), expected_text)
 
     def check_read_blocked(self) -> None:
         # Checks that external programs are not allowed to read the file
-        with self.assertRaises("PermissionError", msg="Reading should be blocked"):
-            with open(self.repo.get_path("mint"), "r") as f:  # noqa: F841
+        with self.assertRaises(PermissionError, msg="Reading should be blocked"):
+            with open(self.repo.get_path(self.BASE_FILE_NAME), "r") as f:  # noqa: F841
                 f.read()
 
     def check_commit_edit_allowed(self) -> None:
         # Checks that external programs are allowed to
         # change the file
         self.repo.update(self.add_file_commit)
-        with open(self.repo.get_path("mint"), "r") as f:
-            self.assertEqual(f.read(), "mint\n")
+        with open(self.repo.get_path(self.BASE_FILE_NAME), "r") as f:
+            self.assertEqual(f.read(), self.BASE_FILE_CONTENTS)
 
     def check_commit_edit_blocked(self, errmsg=b"abort:") -> None:
         # Checks that external programs are not allowed to change the file
@@ -64,7 +84,7 @@ class LockTest(testcase.EdenRepoTest):
     def check_commit_remove_allowed(self) -> None:
         # Checks that external programs are allowed to remove the file
         self.repo.update(self.remove_file_commit)
-        self.assertFalse(os.path.isfile(self.repo.get_path("mint")))
+        self.assertFalse(os.path.isfile(self.repo.get_path(self.BASE_FILE_NAME)))
 
     def check_commit_remove_blocked(self, errmsg=b"abort:") -> None:
         # Checks that external programs are not allowed to remove the file
@@ -76,11 +96,210 @@ class LockTest(testcase.EdenRepoTest):
         ):
             self.repo.update(self.remove_file_commit)
 
+    def _test_share_none(self, open_mode) -> None:
+        # Need to hold open the file until the end of the test
+        handle = open_file_handle(  # noqa: F841
+            self.repo.get_path(self.BASE_FILE_NAME),
+            open_mode,
+            0,
+        )
+
+        self.check_read_blocked()
+        self.check_commit_edit_blocked()
+        self.check_commit_remove_blocked()
+
+        # Handle is closed when it is deleted
+        del handle
+
+    def _test_share_read(self, open_mode) -> None:
+        # Need to hold open the file until the end of the test
+        handle = open_file_handle(  # noqa: F841
+            self.repo.get_path(self.BASE_FILE_NAME),
+            open_mode,
+            FILE_SHARE_READ,
+        )
+
+        # Check that reading is allowed
+        self.check_read_allowed(self.UPDATED_FILE_CONTENTS)
+        self.check_commit_edit_blocked(b"abort: error writing files")
+
+        # BUG ZONE
+        # TODO: This should be blocked, but it isn't
+        try:
+            self.check_commit_remove_blocked(b"abort: error writing files")
+        except RuntimeError as e:
+            self.assertEqual(
+                e.args[0], "Removing via changing commits should be blocked"
+            )
+
+        # BUG1 See that the commit has changed
+        cmdoutput = self.repo.run_hg("sl")
+        sl = cmdoutput.stdout.decode("utf-8").split("\n")
+        self.assertEqual(sl[0], f"@  commit:      {self.remove_file_commit[:12]}")
+
+        # BUG1 No changes to the file are reported
+        cmdoutput = self.repo.run_hg("diff")
+        sl = cmdoutput.stdout.decode("utf-8")
+        self.assertEqual(sl, "")
+        cmdoutput = self.repo.run_hg("status")
+        sl = cmdoutput.stdout.decode("utf-8")
+        self.assertEqual(sl, "")
+
+        # BUG1 See that the file still exists
+        self.assertTrue(os.path.isfile(self.repo.get_path(self.BASE_FILE_NAME)))
+
+        # Handle is closed when it is deleted
+        del handle
+
+        # BUG1 See that file contents have not changed
+        with open(self.repo.get_path(self.BASE_FILE_NAME), "r") as f:
+            self.assertEqual(f.read(), self.UPDATED_FILE_CONTENTS)
+
+    def _test_share_write(self, open_mode) -> None:
+        # Need to hold open the file until the end of the test
+        handle = open_file_handle(  # noqa: F841
+            self.repo.get_path(self.BASE_FILE_NAME),
+            open_mode,
+            FILE_SHARE_WRITE,
+        )
+
+        self.check_read_blocked()
+        self.check_commit_edit_blocked(
+            b"abort: The process cannot access the file because it is being used by another process."
+        )
+        self.check_commit_remove_blocked(
+            b"abort: The process cannot access the file because it is being used by another process."
+        )
+
+        # Handle is closed when it is deleted
+        del handle
+
+    def _test_share_delete(self, open_mode) -> None:
+        # Need to hold open the file until the end of the test
+        handle = open_file_handle(  # noqa: F841
+            self.repo.get_path(self.BASE_FILE_NAME),
+            open_mode,
+            FILE_SHARE_DELETE,
+        )
+
+        self.check_read_blocked()
+        self.check_commit_edit_blocked(
+            b"abort: The process cannot access the file because it is being used by another process."
+        )
+        self.check_commit_remove_blocked(
+            b"abort: The process cannot access the file because it is being used by another process."
+        )
+
+        # Handle is closed when it is deleted
+        del handle
+
+    def _test_share_read_write(self, open_mode) -> None:
+        # Need to hold open the file until the end of the test
+        handle = open_file_handle(  # noqa: F841
+            self.repo.get_path(self.BASE_FILE_NAME),
+            open_mode,
+            FILE_SHARE_READ | FILE_SHARE_WRITE,
+        )
+
+        self.check_read_allowed(self.UPDATED_FILE_CONTENTS)
+        self.check_commit_edit_allowed()
+        # Same bug as in _test_share_read
+        try:
+            self.check_commit_remove_blocked(
+                b"abort: The process cannot access the file because it is being used by another process."
+            )
+        except RuntimeError:
+            pass
+        cmdoutput = self.repo.run_hg("sl")
+        sl = cmdoutput.stdout.decode("utf-8").split("\n")
+        self.assertEqual(sl[0], f"@  commit:      {self.remove_file_commit[:12]}")
+
+        # BUG1 See that the file still exists
+        self.assertTrue(os.path.isfile(self.repo.get_path(self.BASE_FILE_NAME)))
+
+        # Handle is closed when it is deleted
+        del handle
+
+        # BUG1 See that file contents have not changed
+        with open(self.repo.get_path(self.BASE_FILE_NAME), "r") as f:
+            self.assertEqual(f.read(), self.BASE_FILE_CONTENTS)
+
+    def _test_share_read_delete(self, open_mode) -> None:
+        # Need to hold open the file until the end of the test
+        handle = open_file_handle(  # noqa: F841
+            self.repo.get_path(self.BASE_FILE_NAME),
+            open_mode,
+            FILE_SHARE_READ | FILE_SHARE_DELETE,
+        )
+
+        self.check_read_allowed(self.UPDATED_FILE_CONTENTS)
+
+        # BUG ZONE
+        # I don't know if this is supposed to work or not
+        # checkout passes and file is changed to the correct value
+        # despite not having write access
+        try:
+            self.check_commit_edit_blocked(
+                b"abort: The process cannot access the file because it is being used by another process."
+            )
+        except RuntimeError:
+            pass
+        cmdoutput = self.repo.run_hg("sl")
+        sl = cmdoutput.stdout.decode("utf-8").split("\n")
+        self.assertEqual(sl[10], f"@  commit:      {self.add_file_commit[:12]}")
+
+        with open(self.repo.get_path(self.BASE_FILE_NAME), "r") as f:
+            self.assertEqual(f.read(), self.BASE_FILE_CONTENTS)
+        # END BUG ZONE
+
+        self.check_commit_remove_allowed()
+        cmdoutput = self.repo.run_hg("sl")
+        sl = cmdoutput.stdout.decode("utf-8").split("\n")
+        self.assertEqual(sl[0], f"@  commit:      {self.remove_file_commit[:12]}")
+        self.assertFalse(os.path.isfile(self.repo.get_path(self.BASE_FILE_NAME)))
+
+        # Handle is closed when it is deleted
+        del handle
+
+    def _test_share_write_delete(self, open_mode) -> None:
+        # Need to hold open the file until the end of the test
+        handle = open_file_handle(  # noqa: F841
+            self.repo.get_path(self.BASE_FILE_NAME),
+            open_mode,
+            FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+        )
+
+        self.check_read_blocked()
+        self.check_commit_edit_blocked(
+            b"abort: The process cannot access the file because it is being used by another process."
+        )
+        self.check_commit_remove_blocked(
+            b"abort: The process cannot access the file because it is being used by another process."
+        )
+
+        # Handle is closed when it is deleted
+        del handle
+
+    def _test_share_read_write_delete(self, open_mode) -> None:
+        # Need to hold open the file until the end of the test
+        handle = open_file_handle(  # noqa: F841
+            self.repo.get_path(self.BASE_FILE_NAME),
+            open_mode,
+            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+        )
+
+        self.check_read_allowed(self.UPDATED_FILE_CONTENTS)
+        self.check_commit_edit_allowed()
+        self.check_commit_remove_allowed()
+
+        # Handle is closed when it is deleted
+        del handle
+
     def test_no_lock(self) -> None:
-        self.check_read_allowed("mintoo\n")
+        self.check_read_allowed(self.UPDATED_FILE_CONTENTS)
         self.check_commit_edit_allowed()
         self.check_commit_remove_allowed()
         self.repo.update(self.update_file_commit)
-        self.check_read_allowed("mintoo\n")
+        self.check_read_allowed(self.UPDATED_FILE_CONTENTS)
         self.repo.update(self.add_file_commit)
-        self.check_read_allowed("mint\n")
+        self.check_read_allowed(self.BASE_FILE_CONTENTS)
