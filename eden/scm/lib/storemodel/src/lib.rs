7e5edc071e09f179f5351d62949b4f6732268968
Jun Wu <quark@meta.com>
2024-10-29 15:53:48 -0700

    storemodel: move SerializationFormat to types
    Summary:
    I'd like to use SerializationFormat in edenapi/trait. However that would cause
    cyclic dependency. Solve it by moving SerializationFormat.
    
    Reviewed By: zzl0
    
    Differential Revision: D64948072
    
    fbshipit-source-id: e1e96638e1e7624a5c68b546be24a853fc8f5f6b

--- eden/scm/lib/storemodel/src/lib.rs
+++ eden/scm/lib/storemodel/src/lib.rs
@@ -47,6 +47,7 @@ use types::Key;
 use types::PathComponent;
 use types::PathComponentBuf;
 use types::RepoPath;
+pub use types::SerializationFormat;
 
 /// Boxed dynamic iterator. Similar to `BoxStream`.
 pub type BoxIterator<T> = Box<dyn Iterator<Item = T> + Send + 'static>;
@@ -479,52 +480,6 @@ pub trait TreeStore: KeyStore {
     /// Usually it is just `Arc::clone` under the hood.
     /// Used to relax lifetime requirements for various `BoxIterator` outputs.
     fn clone_tree_store(&self) -> Box<dyn TreeStore>;
-}
-
-/// Decides the serialization format. This exists so different parts of the code
-/// base can agree on how to generate a SHA1, how to lookup in a tree, etc.
-/// Ideally this information is private and the differences are behind
-/// abstractions too.
-#[derive(
-    Clone,
-    Copy,
-    Debug,
-    Eq,
-    PartialEq,
-    Hash,
-    Ord,
-    PartialOrd,
-    Default,
-    Serialize,
-    Deserialize
-)]
-#[serde(rename_all = "snake_case")]
-pub enum SerializationFormat {
-    // Hg SHA1:
-    //   SORTED_PARENTS CONTENT
-    //
-    // Hg file:
-    //   FILELOG_METADATA CONTENT
-    //
-    // Hg tree:
-    //   NAME '\0' HEX_SHA1 MODE '\n'
-    //   MODE: 't' (tree), 'l' (symlink), 'x' (executable)
-    //   (sorted by name)
-    #[default]
-    Hg,
-
-    // Git SHA1:
-    //   TYPE LENGTH CONTENT
-    //
-    // Git file:
-    //   CONTENT
-    //
-    // Git tree:
-    //   MODE ' ' NAME '\0' BIN_SHA1
-    //   MODE: '40000' (tree), '100644' (regular), '100755' (executable),
-    //         '120000' (symlink), '160000' (gitlink)
-    //   (sorted by name, but directory names are treated as ending with '/')
-    Git,
 }
 
 /// Options used by `insert_data`
