823b89b13504d5a79c4e0f50324773adb8f707fe
Gustavo Galvao Avena <gustavoavena@meta.com>
2024-11-04 06:30:15 -0800

    Fix bug in submodule expansion update command
    Reviewed By: mitrandir77
    
    Differential Revision: D64822415
    
    fbshipit-source-id: 62319e2ad8131a31d65e7c3e9d896d64d19ecbd6

--- eden/mononoke/mononoke_api/src/repo/update_submodule_expansion.rs
+++ eden/mononoke/mononoke_api/src/repo/update_submodule_expansion.rs
@@ -10,6 +10,7 @@ use std::collections::BTreeMap;
 use anyhow::anyhow;
 use anyhow::Context;
 use maplit::btreemap;
+use metaconfig_types::CommitSyncConfig;
 use metaconfig_types::DefaultSmallToLargeCommitSyncPathAction;
 use mononoke_types::hash::GitSha1;
 use mononoke_types::ChangesetId;
@@ -19,10 +20,11 @@ use mononoke_types::NonRootMPath;
 use mononoke_types::RepositoryId;
 use repo_cross_repo::RepoCrossRepoRef;
 use repo_identity::RepoIdentityRef;
-use synced_commit_mapping::WorkingCopyEquivalence;
+use synced_commit_mapping::ArcSyncedCommitMapping;
 
 use super::RepoContextBuilder;
 use crate::changeset::ChangesetContext;
+use crate::changeset_path::ChangesetPathHistoryContext;
 use crate::errors::MononokeError;
 use crate::file::FileType;
 use crate::repo::create_changeset::CreateInfo;
@@ -128,36 +130,23 @@ impl<R: MononokeRepo> RepoContext<R> {
     ) -> Result<ChangesetContext<R>, MononokeError> {
         let large_repo_ctx = self; // For readability
 
-        let synced_commit_mapping = large_repo_ctx
-            .repo()
-            .repo_cross_repo()
-            .synced_commit_mapping()
-            .clone();
-        let large_repo_id = large_repo_ctx.repo().repo_identity().id();
-
         // Get small repo changeset with working copy equivalence to the provided
         // large repo base changeset. This will be the parent of the small repo
         // changeset modifying the submodule expansion.
-        let small_repo_base_commit_wc = synced_commit_mapping
-            .get_equivalent_working_copy(
-                &self.ctx,
-                large_repo_id,
+        let small_repo_base_cs_ctx = large_repo_ctx
+            .xrepo_commit_lookup(
+                small_repo_ctx,
                 base_changeset_id,
-                small_repo_ctx.repo().repo_identity().id(),
+                None,
+                XRepoLookupSyncBehaviour::SyncIfAbsent,
+                XRepoLookupExactBehaviour::WorkingCopyEquivalence,
             )
-            .await?
-            .ok_or(anyhow!(
+            .await
+            .context("Failed to sync small repo commit updating submodule to large repo")?.ok_or(anyhow!(
                 "Couldn't find small repo commit that's working copy equivalent to base commit {base_changeset_id}")
             )?;
 
-        let small_repo_base_cs = match small_repo_base_commit_wc {
-            WorkingCopyEquivalence::WorkingCopy(cs_id, _) => cs_id,
-            WorkingCopyEquivalence::NoWorkingCopy(_) => {
-                return Err(anyhow!(
-                    "No working copy equivalent small repo changeset found for large repo changeset {base_changeset_id}"
-                ).into());
-            }
-        };
+        let small_repo_base_cs = small_repo_base_cs_ctx.id();
 
         let default_commit_msg = match &submodule_expansion_update {
             SubmoduleExpansionUpdate::UpdateCommit(git_commit) => format!(
@@ -218,33 +207,10 @@ impl<R: MononokeRepo> RepoContext<R> {
         base_changeset_id: ChangesetId,
         submodule_expansion_path: &NonRootMPath,
     ) -> Result<(RepositoryId, NonRootMPath), MononokeError> {
-        let large_repo_ctx = self; // For readability
-
-        let synced_commit_mapping = large_repo_ctx
-            .repo()
-            .repo_cross_repo()
-            .synced_commit_mapping()
-            .clone();
-        let large_repo_id = large_repo_ctx.repo().repo_identity().id();
-
-        // Get the commit sync mapping from the provided large repo base commit
-        let commit_sync_version = synced_commit_mapping
-            .get_large_repo_commit_version(&self.ctx, large_repo_id, base_changeset_id)
-            .await
-            .context("Failed to find large repo commit sync config version for base commit")?
-            .ok_or(anyhow!(
-                "No large repo commit sync config version found for base commit"
-            ))?;
-        let live_commit_sync_config = large_repo_ctx.live_commit_sync_config();
-
-        // Get the large and small repo sync configs from the mapping
-        let large_repo_sync_config = live_commit_sync_config
-            .get_commit_sync_config_by_version_if_exists(large_repo_id, &commit_sync_version)
-            .await
-            .with_context(|| {
-                anyhow!("Failed to fetch commit sync config version {commit_sync_version}")
-            })?
-            .ok_or_else(|| anyhow!("Commit sync config version {commit_sync_version} not found"))?;
+        // First, we need to get the large repo commit sync mapping version
+        let large_repo_sync_config = self
+            .get_large_repo_commit_sync_version(base_changeset_id, submodule_expansion_path)
+            .await?;
         let small_repo_configs = large_repo_sync_config.small_repos;
 
         // Iterate over all small repo configs and find the one that is a prefix
@@ -273,10 +239,78 @@ impl<R: MononokeRepo> RepoContext<R> {
         // one small repo
         if let Some((small_repo_id_clash, _)) = small_repo_ids_and_adj_paths.next() {
             return Err(anyhow!(
-                "Multiple small repos being modified under path {submodule_expansion_path}: {small_repo_id} and {small_repo_id_clash}"
-            ).into());
+        "Multiple small repos being modified under path {submodule_expansion_path}: {small_repo_id} and {small_repo_id_clash}"
+    ).into());
         }
 
         Ok((small_repo_id, adjusted_sm_exp_path))
+    }
+
+    /// Get the large repo commit sync mapping version for the provided base
+    /// changeset id. If one is not found (e.g. because the changeset if recent
+    /// and the small repo throughput is small), look for the version of the
+    /// changeset that modified the provided submodule expansion path.
+    ///
+    /// This version is needed to get which small repo is being modified based
+    /// on the submodule expansion path.
+    async fn get_large_repo_commit_sync_version(
+        &self,
+        base_changeset_id: ChangesetId,
+        submodule_expansion_path: &NonRootMPath,
+    ) -> Result<CommitSyncConfig, MononokeError> {
+        let large_repo_ctx = self; // For readability
+        let synced_commit_mapping: ArcSyncedCommitMapping = large_repo_ctx
+            .repo()
+            .repo_cross_repo()
+            .synced_commit_mapping()
+            .clone();
+        let large_repo_id: RepositoryId = large_repo_ctx.repo().repo_identity().id();
+
+        let mb_commit_sync_version = synced_commit_mapping
+            .get_large_repo_commit_version(&self.ctx, large_repo_id, base_changeset_id)
+            .await
+            .context("Failed to find large repo commit sync config version for base commit")?;
+
+        let commit_sync_version = if let Some(commit_sync_version) = mb_commit_sync_version {
+            commit_sync_version
+        } else {
+            // If the base commit is very recent, it might not have been synced
+            // to any small repo yet, thus it won't have a commit sync version.
+            //
+            // In that case, get the changeset that last modified the submodule
+            // and try to use its commit sync version.
+            let base_cs_ctx = large_repo_ctx
+                .changeset(base_changeset_id)
+                .await?
+                .ok_or(anyhow!("Failed to instantiate base changeset context"))?;
+            let changeset_path_history_ctx =
+                ChangesetPathHistoryContext::new(base_cs_ctx, submodule_expansion_path.clone())
+                    .await?;
+
+            let last_modified_cs_id = changeset_path_history_ctx
+                .last_modified()
+                .await?
+                .map_or(base_changeset_id, |cs_ctx| cs_ctx.id());
+
+            synced_commit_mapping
+                .get_large_repo_commit_version(&self.ctx, large_repo_id, last_modified_cs_id)
+                .await
+                .context("Failed to find large repo commit sync config version for commit")?
+                .ok_or(anyhow!(
+                    "No large repo commit sync config version found for base commit"
+                ))?
+        };
+
+        let live_commit_sync_config = large_repo_ctx.live_commit_sync_config();
+
+        let large_repo_sync_config = live_commit_sync_config
+            .get_commit_sync_config_by_version_if_exists(large_repo_id, &commit_sync_version)
+            .await
+            .with_context(|| {
+                anyhow!("Failed to fetch commit sync config version {commit_sync_version}")
+            })?
+            .ok_or_else(|| anyhow!("Commit sync config version {commit_sync_version} not found"))?;
+
+        Ok(large_repo_sync_config)
     }
 }
