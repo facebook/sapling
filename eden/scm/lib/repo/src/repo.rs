cfeedda39f7c9f6ea62b4fe8e8817da295c4bc34
Muir Manders <muirdm@meta.com>
2024-11-04 12:45:40 -0800

    repo: always use the caching tree store
    Summary:
    Previously I added an in-memory cache for tree objects, but it was opt-in by using the `repo.caching_tree_store()` method. I've come across more places that weren't getting caching since they use the `repo.tree_store()` trait object. Let's just make everything use the caching for consistency.
    
    I made it opt-in because I didn't want to add another caching layer to EdenFS, but the tree cache only caches at the lower KeyStore trait level, not at the higher FileStore level (EdenFS backingstore uses FileStore trait).
    
    Reviewed By: liubov-dmitrieva
    
    Differential Revision: D64947726
    
    fbshipit-source-id: 08744104bddc74aff2728b4f12f4dac872eabcad

--- eden/scm/lib/repo/src/repo.rs
+++ eden/scm/lib/repo/src/repo.rs
@@ -87,7 +87,6 @@ pub struct Repo {
     locker: Arc<RepoLocker>,
     cas_client: OnceCell<Option<Arc<dyn CasClient>>>,
     tree_resolver: OnceCell<Arc<dyn ReadTreeManifest + Send + Sync>>,
-    caching_tree_store: OnceCell<Arc<dyn TreeStore>>,
 }
 
 impl Repo {
@@ -197,7 +196,6 @@ impl Repo {
             working_copy: Default::default(),
             eager_store: None,
             tree_resolver: Default::default(),
-            caching_tree_store: Default::default(),
             locker,
         })
     }
@@ -556,27 +554,28 @@ impl Repo {
 
         let ts = Arc::new(tree_builder.build()?);
         let _ = self.tree_scm_store.set(ts.clone());
-        let _ = self.tree_store.set(ts.clone());
-        Ok(ts)
+
+        self.set_tree_store(ts)
     }
 
-    pub fn caching_tree_store(&self) -> Result<Arc<dyn TreeStore>> {
-        let store = self.caching_tree_store.get_or_try_init(|| {
-            let cache_size = self
-                .config
-                // Trees are typically pretty small (and they are often kept in memory
-                // anyway within a TreeManifest object), so let's pick a sizable
-                // default. Set to 0 to disable caching.
-                .get_or("experimental", "tree-resolver-cache-size", || 10_000)?;
+    // Set self.tree_store, optionally wrapping in CachingTreeStore. Returns self.tree_store.
+    fn set_tree_store(&self, store: Arc<dyn TreeStore>) -> Result<Arc<dyn TreeStore>> {
+        self.tree_store
+            .get_or_try_init(|| {
+                let cache_size = self
+                    .config
+                    // Trees are typically pretty small (and they are often kept in memory
+                    // anyway within a TreeManifest object), so let's pick a sizable
+                    // default. Set to 0 to disable caching.
+                    .get_or("experimental", "tree-resolver-cache-size", || 10_000)?;
 
-            let inner_store = self.tree_store()?;
-            if cache_size == 0 {
-                return Ok::<_, anyhow::Error>(inner_store);
-            }
+                if cache_size == 0 {
+                    return Ok::<_, anyhow::Error>(store);
+                }
 
-            Ok(Arc::new(CachingTreeStore::new(inner_store, cache_size)))
-        });
-        Ok(store?.clone())
+                Ok(Arc::new(CachingTreeStore::new(store, cache_size)))
+            })
+            .cloned()
     }
 
     // This should only be used to share stores with Python.
@@ -595,7 +594,7 @@ impl Repo {
         let tr = self.tree_resolver.get_or_try_init(|| {
             Ok::<_, anyhow::Error>(Arc::new(TreeManifestResolver::new(
                 self.dag_commits()?,
-                self.caching_tree_store()?,
+                self.tree_store()?,
             )))
         })?;
         Ok(tr.clone())
@@ -663,8 +662,7 @@ impl Repo {
                 let file_store = out.file_store();
                 let tree_store = out.tree_store();
                 let _ = self.file_store.set(file_store.clone());
-                let _ = self.tree_store.set(tree_store.clone());
-                Ok(Some((file_store, tree_store)))
+                Ok(Some((file_store, self.set_tree_store(tree_store)?)))
             }
         }
     }
