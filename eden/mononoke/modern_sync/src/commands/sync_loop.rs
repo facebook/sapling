275cfac4d48d11f26003c46fa0f0c10cc95f7fcd
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2024-11-05 04:09:40 -0800

    modern sync: gather elements for sync
    Summary: Passing through things like CoreContext and the MononokeApp itself to perform operations like opening a repo and tailing it.
    
    Reviewed By: clara-9
    
    Differential Revision: D65446187
    
    fbshipit-source-id: 4e85f48a113b2a8f7f9765ef1bd4176710ef304f

--- eden/mononoke/modern_sync/src/commands/sync_loop.rs
+++ eden/mononoke/modern_sync/src/commands/sync_loop.rs
@@ -12,15 +12,21 @@ use anyhow::ensure;
 use anyhow::Result;
 use async_trait::async_trait;
 use clap::Parser;
+use clientinfo::ClientEntryPoint;
+use clientinfo::ClientInfo;
+use context::CoreContext;
 use executor_lib::RepoShardedProcess;
 use executor_lib::RepoShardedProcessExecutor;
+use fbinit::FacebookInit;
 use mononoke_app::args::MultiRepoArgs;
+use mononoke_app::args::RepoArg;
 use mononoke_app::MononokeApp;
 use sharding_ext::RepoShard;
 use slog::info;
 use slog::Logger;
 
 use crate::ModernSyncArgs;
+use crate::Repo;
 
 const SM_CLEANUP_TIMEOUT_SECS: u64 = 120;
 
@@ -28,29 +34,24 @@ const SM_CLEANUP_TIMEOUT_SECS: u64 = 120;
 #[derive(Parser)]
 pub struct CommandArgs {}
 
-pub async fn setup_sync(repos_args: &MultiRepoArgs, app: &MononokeApp) -> Result<()> {
-    let repos = app.multi_repo_configs(repos_args.ids_or_names()?)?;
-    let repo_count = repos.len();
+pub async fn setup_sync(repos_args: &MultiRepoArgs, app: &MononokeApp) -> Result<String> {
+    let repo_count = repos_args.repo_name.len();
     ensure!(
         repo_count == 1,
         "Modern sync only supports one repo at a time for now "
     );
 
-    info!(
-        app.logger(),
-        "Syncing repos {:?}",
-        repos.iter().map(|repo| &repo.0).collect::<Vec<_>>()
-    );
+    info!(app.logger(), "Syncing repos {:?}", repos_args.repo_name);
 
-    Ok(())
+    Ok(repos_args.repo_name[0].clone())
 }
 
 pub struct ModernSyncProcess {
-    app: MononokeApp,
+    app: Arc<MononokeApp>,
 }
 
 impl ModernSyncProcess {
-    fn new(app: MononokeApp) -> Self {
+    fn new(app: Arc<MononokeApp>) -> Self {
         Self { app }
     }
 }
@@ -67,16 +68,23 @@ impl RepoShardedProcess for ModernSyncProcess {
         setup_sync(&repos, &self.app).await?;
 
         Ok(Arc::new(ModernSyncProcessExecutor {
+            fb: self.app.fb,
             logger: self.app.logger().clone(),
             repo_name: repo_name.to_string(),
+            app: self.app.clone(),
         }))
     }
 }
 
-pub async fn sync() {}
+pub async fn sync(repo_name: String, _ctx: &CoreContext, app: Arc<MononokeApp>) -> Result<()> {
+    let _repo: Repo = app.open_repo(&RepoArg::Name(repo_name)).await?;
+    Ok(())
+}
 pub struct ModernSyncProcessExecutor {
+    fb: FacebookInit,
     logger: Logger,
     repo_name: String,
+    app: Arc<MononokeApp>,
 }
 
 #[async_trait]
@@ -86,7 +94,13 @@ impl RepoShardedProcessExecutor for ModernSyncProcessExecutor {
             self.logger,
             "Starting up modern sync for repo {}", &self.repo_name
         );
-        sync().await;
+        let ctx = CoreContext::new_with_logger_and_client_info(
+            self.fb,
+            self.logger.clone(),
+            ClientInfo::default_with_entry_point(ClientEntryPoint::ModernSync),
+        )
+        .clone_with_repo_name(&self.repo_name);
+        let _ = sync(self.repo_name.clone(), &ctx, self.app.clone()).await;
         Ok(())
     }
 
@@ -98,7 +112,8 @@ impl RepoShardedProcessExecutor for ModernSyncProcessExecutor {
 
 pub async fn run(app: MononokeApp, _args: CommandArgs) -> Result<()> {
     let sync_args = &app.args::<ModernSyncArgs>()?;
-    let process = Arc::new(ModernSyncProcess::new(app));
+
+    let process = Arc::new(ModernSyncProcess::new(Arc::new(app)));
     let logger = process.app.logger().clone();
     if let Some(mut executor) = sync_args.sharded_executor_args.clone().build_executor(
         process.app.fb,
@@ -114,8 +129,14 @@ pub async fn run(app: MononokeApp, _args: CommandArgs) -> Result<()> {
             .await?;
     } else {
         info!(logger, "Running unsharded sync loop");
-        setup_sync(&sync_args.repos, &process.app).await?;
-        sync().await;
+        let repo_name = setup_sync(&sync_args.repos, &process.app).await?;
+        let ctx = CoreContext::new_with_logger_and_client_info(
+            process.app.fb,
+            process.app.logger().clone(),
+            ClientInfo::default_with_entry_point(ClientEntryPoint::ModernSync),
+        )
+        .clone_with_repo_name(&repo_name);
+        sync(repo_name, &ctx, process.app.clone()).await?;
     }
     Ok(())
 }
