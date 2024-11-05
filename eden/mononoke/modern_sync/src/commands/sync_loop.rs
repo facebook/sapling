ed0f3351edbeb209dd9e9f3b1a6e0b49383a6a04
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2024-11-05 04:09:40 -0800

    modern sync: execution boilerplate + shardmanager
    Summary: This outlines a very simple separation of concerns so we can have a shared implementation of `setup` and `sync` methods both for sharded and unsharded  execution.
    
    Reviewed By: clara-9
    
    Differential Revision: D65422260
    
    fbshipit-source-id: 56d1be67b9a5e84f7351c5c4eab652f8aaa3c009

--- eden/mononoke/modern_sync/src/commands/sync_loop.rs
+++ eden/mononoke/modern_sync/src/commands/sync_loop.rs
@@ -5,15 +5,117 @@
  * GNU General Public License version 2.
  */
 
+use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
+
+use anyhow::ensure;
 use anyhow::Result;
+use async_trait::async_trait;
 use clap::Parser;
+use executor_lib::RepoShardedProcess;
+use executor_lib::RepoShardedProcessExecutor;
+use mononoke_app::args::MultiRepoArgs;
 use mononoke_app::MononokeApp;
+use sharding_ext::RepoShard;
+use slog::info;
+use slog::Logger;
+
+use crate::ModernSyncArgs;
+
+const SM_CLEANUP_TIMEOUT_SECS: u64 = 120;
 
 /// Replays bookmark's moves
 #[derive(Parser)]
 pub struct CommandArgs {}
 
-pub async fn run(_app: MononokeApp, _args: CommandArgs) -> Result<()> {
-    eprintln!("Modern sync loop");
+pub async fn setup_sync(repos_args: &MultiRepoArgs, app: &MononokeApp) -> Result<()> {
+    let repos = app.multi_repo_configs(repos_args.ids_or_names()?)?;
+    let repo_count = repos.len();
+    ensure!(
+        repo_count == 1,
+        "Modern sync only supports one repo at a time for now "
+    );
+
+    info!(
+        app.logger(),
+        "Syncing repos {:?}",
+        repos.iter().map(|repo| &repo.0).collect::<Vec<_>>()
+    );
+
+    Ok(())
+}
+
+pub struct ModernSyncProcess {
+    app: MononokeApp,
+}
+
+impl ModernSyncProcess {
+    fn new(app: MononokeApp) -> Self {
+        Self { app }
+    }
+}
+
+#[async_trait]
+impl RepoShardedProcess for ModernSyncProcess {
+    async fn setup(&self, repo: &RepoShard) -> anyhow::Result<Arc<dyn RepoShardedProcessExecutor>> {
+        let repo_name = repo.repo_name.as_str();
+
+        let repos = MultiRepoArgs {
+            repo_name: vec![repo_name.to_string()],
+            repo_id: vec![],
+        };
+        setup_sync(&repos, &self.app).await?;
+
+        Ok(Arc::new(ModernSyncProcessExecutor {
+            logger: self.app.logger().clone(),
+            repo_name: repo_name.to_string(),
+        }))
+    }
+}
+
+pub async fn sync() {}
+pub struct ModernSyncProcessExecutor {
+    logger: Logger,
+    repo_name: String,
+}
+
+#[async_trait]
+impl RepoShardedProcessExecutor for ModernSyncProcessExecutor {
+    async fn execute(&self) -> Result<()> {
+        info!(
+            self.logger,
+            "Starting up modern sync for repo {}", &self.repo_name
+        );
+        sync().await;
+        Ok(())
+    }
+
+    async fn stop(&self) -> Result<()> {
+        info!(self.logger, "Finishing modern sync {}", &self.repo_name);
+        Ok(())
+    }
+}
+
+pub async fn run(app: MononokeApp, _args: CommandArgs) -> Result<()> {
+    let sync_args = &app.args::<ModernSyncArgs>()?;
+    let process = Arc::new(ModernSyncProcess::new(app));
+    let logger = process.app.logger().clone();
+    if let Some(mut executor) = sync_args.sharded_executor_args.clone().build_executor(
+        process.app.fb,
+        process.app.runtime().clone(),
+        &logger,
+        || process.clone(),
+        true, // enable shard (repo) level healing
+        SM_CLEANUP_TIMEOUT_SECS,
+    )? {
+        info!(logger, "Running sharded sync loop");
+        executor
+            .block_and_execute(&logger, Arc::new(AtomicBool::new(false)))
+            .await?;
+    } else {
+        info!(logger, "Running unsharded sync loop");
+        setup_sync(&sync_args.repos, &process.app).await?;
+        sync().await;
+    }
     Ok(())
 }
--- eden/mononoke/modern_sync/src/main.rs
+++ eden/mononoke/modern_sync/src/main.rs
@@ -7,28 +7,26 @@
 
 #![feature(async_closure)]
 
-use std::sync::Arc;
-
 use anyhow::Result;
 use clap::Parser;
 use executor_lib::args::ShardedExecutorArgs;
 use fbinit::FacebookInit;
-use metaconfig_types::ShardedService;
+use mononoke_app::args::MultiRepoArgs;
 use mononoke_app::args::RepoFilterAppExtension;
 use mononoke_app::fb303::AliveService;
 use mononoke_app::fb303::Fb303AppExtension;
 use mononoke_app::MononokeApp;
 use mononoke_app::MononokeAppBuilder;
-use mononoke_app::MononokeReposManager;
 use mutable_counters::MutableCounters;
-use slog::info;
-
 mod commands;
 
 #[derive(Parser)]
 struct ModernSyncArgs {
     #[clap(flatten)]
     sharded_executor_args: ShardedExecutorArgs,
+
+    #[clap(flatten)]
+    pub repos: MultiRepoArgs,
 }
 
 #[facet::container]
@@ -36,21 +34,6 @@ struct ModernSyncArgs {
 pub struct Repo {
     #[facet]
     pub mutable_counters: dyn MutableCounters,
-}
-
-pub struct ModernSyncProcess {
-    _fb: FacebookInit,
-    _repos_mgr: Arc<MononokeReposManager<Repo>>,
-}
-
-impl ModernSyncProcess {
-    fn new(fb: FacebookInit, repos_mgr: MononokeReposManager<Repo>) -> Self {
-        let repos_mgr = Arc::new(repos_mgr);
-        Self {
-            _fb: fb,
-            _repos_mgr: repos_mgr,
-        }
-    }
 }
 
 #[fbinit::main]
@@ -64,24 +47,5 @@ fn main(fb: FacebookInit) -> Result<()> {
 }
 
 async fn async_main(app: MononokeApp) -> Result<()> {
-    let args: ModernSyncArgs = app.args()?;
-    // Service name is used for shallow or deep sharding. If sharding itself is disabled, provide
-    // service name as None while opening repos.
-    let service_name = args
-        .sharded_executor_args
-        .sharded_service_name
-        .as_ref()
-        .map(|_| ShardedService::ModernSync);
-
-    let repos_mgr = app.open_managed_repos_unredacted(service_name).await?;
-    let repos = repos_mgr.repos().clone();
-
-    let _process = Arc::new(ModernSyncProcess::new(app.fb, repos_mgr));
-
-    info!(
-        app.logger(),
-        "ModernSync process initialized for repo {:?}",
-        repos.iter_names().collect::<Vec<_>>()
-    );
     commands::dispatch(app).await
 }
