2f40d66e7eb667aad3e1b82f3fdb8eb420fcd817
John Elliott <jdelliot@meta.com>
2024-11-08 18:06:39 -0800

    Add initial implementation of eden journal-position
    Summary:
    # Context
    
    We are introducing EdenFS notifications to support scalable and ergonomic file system notifications for EdenFS mounts.
    
    # This Diff
    
    This diff builds on the work cqd has been doing. It implements a simple version of get journal position - with minimal arguments. This is being done simply to unblock testing in other areas of the code. In a subsequent diff, cqd will move this code into the `eden notification|notify` command as we are planning to refactor all notifcation related commands there.
    
    # Technical Details
    
    Code is mostly a copy-paste from `subscribe::SubscribeCmd` where it also get's the current journal position. Subsequent diffs will add more parameters and refactor shared code into the edenfs-client crate.
    
    # Discussion Points
    
    None
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D65697430
    
    fbshipit-source-id: 308b1ee10691e4dbdf15f832db33e064669a96ed

--- eden/fs/cli_rs/edenfs-commands/src/journal_position.rs
+++ eden/fs/cli_rs/edenfs-commands/src/journal_position.rs
@@ -7,20 +7,50 @@
 
 //! edenfsctl notify
 
+use std::path::PathBuf;
+
+use anyhow::anyhow;
+use anyhow::Context;
 use anyhow::Result;
 use async_trait::async_trait;
 use clap::Parser;
+use edenfs_client::EdenFsInstance;
+use edenfs_utils::bytes_from_path;
 
+use crate::util::locate_repo_root;
 use crate::ExitCode;
 
+// TODO: add a --json flag to print the output in JSON format
+// TODO: add a --mount_point flag to specify which mount to query
 #[derive(Parser, Debug)]
 #[clap(about = "Returns the current EdenFS journal position")]
 pub struct JournalPositionCmd {}
 
+impl JournalPositionCmd {
+    //TODO: refactor subscribe::get_mount_point into a separate crate
+    pub fn get_mount_point(&self) -> Result<PathBuf> {
+        locate_repo_root(
+            &std::env::current_dir().context("Unable to retrieve current working directory")?,
+        )
+        .map(|p| p.to_path_buf())
+        .ok_or_else(|| anyhow!("Unable to locate repository root"))
+    }
+}
+
 #[async_trait]
 impl crate::Subcommand for JournalPositionCmd {
     async fn run(&self) -> Result<ExitCode> {
-        println!("Not Implemented");
+        let instance = EdenFsInstance::global();
+        let client = instance.connect(None).await?;
+        let mount_point_path = self.get_mount_point()?;
+        let mount_point = bytes_from_path(mount_point_path)?;
+        let position = client.getCurrentJournalPosition(&mount_point).await?;
+        println!(
+            "{}:{}:{}",
+            position.mountGeneration,
+            position.sequenceNumber,
+            hex::encode(position.snapshotHash)
+        );
         Ok(0)
     }
 }
