b8f920c74f45e3aa6d429ddd4c524e88bf20a98e
John Elliott <jdelliot@meta.com>
2024-11-12 15:35:49 -0800

    Add mount point parameter to eden notify get-position
    Summary:
    # Context
    
    We are introducing EdenFS notifications to support scalable and ergonomic file system notifications for EdenFS mounts.
    
    # This Diff
    
    Previious version of `eden notify get-position` only reported the position of repo located in the cwd. This diff adds the ability to specify a mount point.
    
    # Technical Details
    
    Added a single `mount_point` parameter to the `get-position` paramter. This paramter is positional (no flag).
    
    # Next Steps
    
    Add the `changes-since` command to `eden notify` then iterate on the `streamChangesSinceV2` Thrift endpoint.
    
    # Discussion Points
    
    None
    
    Differential Revision: D65781302
    
    fbshipit-source-id: 637d547c38a010564a48e140e39bab07a5199655

--- eden/fs/cli_rs/edenfs-commands/src/notify/get_position.rs
+++ eden/fs/cli_rs/edenfs-commands/src/notify/get_position.rs
@@ -16,24 +16,31 @@ use async_trait::async_trait;
 use clap::Parser;
 use edenfs_client::EdenFsInstance;
 use edenfs_utils::bytes_from_path;
+use hg_util::path::expand_path;
 
 use crate::util::locate_repo_root;
 use crate::ExitCode;
 
 // TODO: add a --json flag to print the output in JSON format
-// TODO: add a --mount_point flag to specify which mount to query
 #[derive(Parser, Debug)]
 #[clap(about = "Returns the current EdenFS journal position")]
-pub struct GetPositionCmd {}
+pub struct GetPositionCmd {
+    #[clap(parse(from_str = expand_path))]
+    /// Path to the mount point
+    mount_point: Option<PathBuf>,
+}
 
 impl GetPositionCmd {
-    //TODO: refactor subscribe::get_mount_point into a separate crate
-    pub fn get_mount_point(&self) -> Result<PathBuf> {
-        locate_repo_root(
-            &std::env::current_dir().context("Unable to retrieve current working directory")?,
-        )
-        .map(|p| p.to_path_buf())
-        .ok_or_else(|| anyhow!("Unable to locate repository root"))
+    fn get_mount_point(&self) -> Result<PathBuf> {
+        if let Some(path) = &self.mount_point {
+            Ok(path.clone())
+        } else {
+            locate_repo_root(
+                &std::env::current_dir().context("Unable to retrieve current working directory")?,
+            )
+            .map(|p| p.to_path_buf())
+            .ok_or_else(|| anyhow!("Unable to locate repository root"))
+        }
     }
 }
 
