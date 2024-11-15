6a34634418c2f7b806cd83bfb3490c40e33b74d3
Saul Gutierrez <sggutier@meta.com>
2024-11-15 12:27:24 -0800

    ejeca: add a test for testing killing
    Summary: This was originally supposed to be part of D65138591. For a number of reasons we couldn't have the contents of `sighandlerScript` in a file, so the changes originally made in D65138591 couldn't be landed until D65138585 was.
    
    Reviewed By: muirdm
    
    Differential Revision: D65798485
    
    fbshipit-source-id: 1a9e75652490af266fbd7917bb98f27604df3c6b

--- addons/isl/integrationTests/ejeca.test.tsx
+++ addons/isl/integrationTests/ejeca.test.tsx
@@ -31,3 +31,64 @@ describe('test running binaries', () => {
     expect((await spawned).stdout).toBe('hemlo');
   });
 });
+
+describe('test killing process', () => {
+  const sighandlerScript = `
+const argv = process.argv;
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+(async function main() {
+let exitOnSigTerm = false;
+let delay = 0;
+
+if(argv.length > 2) {
+    delay = parseInt(argv[2]);
+    if(argv[argv.length - 1] !== "dontExitOnSigterm") {
+        exitOnSigTerm = true;
+    }
+}
+
+process.on('SIGTERM', () => {
+    console.log("I was asked to stop politely");
+    if(exitOnSigTerm) {
+        process.exit(0)
+    }
+});
+
+console.log("Hello");
+
+for(let i=0; i < delay; i++) {
+    await sleep(1000);
+}
+
+console.log("Goodbye");
+})();
+`;
+
+  const spawnAndKill = async (
+    pythonArgs: string[] = [],
+    expectedOut: string = '',
+    killArgs: Parameters<EjecaChildProcess['kill']> = [],
+  ) => {
+    let spawned = ejeca('node', ['-', ...pythonArgs], {
+      input: sighandlerScript,
+    });
+    setTimeout(() => spawned.kill(...killArgs), 1000);
+    expect((await spawned).stdout).toBe(expectedOut);
+  };
+
+  it('kill as sends sigterm by default', async () => {
+    await spawnAndKill(['3', 'dontExitOnSigterm'], 'Hello\nI was asked to stop politely\nGoodbye');
+  });
+
+  it('sigkill can be set through force kill after a timeout', async () => {
+    await spawnAndKill(['4', 'dontExitOnSigterm'], 'Hello\nI was asked to stop politely', [
+      'SIGTERM',
+      {forceKillAfterTimeout: 2000},
+    ]);
+  });
+
+  it('sending sigkill just kills', async () => {
+    await spawnAndKill(['100000000000'], 'Hello', ['SIGKILL']);
+  });
+});
