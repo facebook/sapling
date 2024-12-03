bb8b5b80fc2afc5f843d1359e67944ed0279bb37
Jun Wu <quark@meta.com>
2024-12-03 09:32:43 -0800

    tests: add a test demostrating pydag's torevs problem
    Summary:
    It does not follow id-reassignment. This can cause subtle issues like the
    "repo.heads()" is cached on the "nodes" but can still report invalid revs when
    the nodes are converted to revs.
    
    This is to demostrate an upcoming fix to the "N... not found" user reported
    issue.
    
    Reviewed By: zzl0
    
    Differential Revision: D66685618
    
    fbshipit-source-id: c3150c87e644c9e5381e2340b04944384ce8034c

---
+++ eden/scm/tests/test-pydag.py
@@ -0,0 +1,44 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+import functools
+import os
+import unittest
+
+import bindings
+import silenttestrunner
+
+
+class PyDagTests(unittest.TestCase):
+    def _tmpdir(self, name: str) -> str:
+        return os.path.join(os.getenv("TESTTMP") or "", name)
+
+    def test_torevs_should_follow_id_reassignment(self):
+        commits = bindings.dag.commits.opensegments(
+            self._tmpdir("segments"), self._tmpdir("commits"), "git"
+        )
+        # Add a single commit. By default, it is in the "NON_MASTER" group.
+        a_text = b"Commit A"
+        a_node = git_commit_hash(a_text)
+        commits.addcommits([(a_node, [], a_text)])
+        a_nodes = commits.dagalgo().sort([a_node])
+        self.assertEqual(list(commits.torevs(a_nodes)), [non_master_id(0)])
+        # Flush the commit to the MASTER group. This reassigns the node to "MASTER".
+        commits.flush([a_node])
+        self.assertEqual(list(commits.torevs([a_node])), [0])
+        # FIXME: `torevs(old_nodes)` does not respect the id reassignment
+        self.assertEqual(list(commits.torevs(a_nodes)), [non_master_id(0)])
+
+
+git_commit_hash = functools.partial(bindings.formatutil.git_sha1_digest, kind="commit")
+
+
+def non_master_id(id: int) -> int:
+    # See Rust dag's "Group". Ids in the "NON_MASTER" group (u64) have the top byte set to 1.
+    return id + (1 << 56)
+
+
+if __name__ == "__main__":
+    silenttestrunner.main(__name__)
