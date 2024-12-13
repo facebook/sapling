389ce686bf0727261f546f91c60f0f4841d6353f
Andrea Campi <acampi@meta.com>
2024-12-13 10:43:29 -0800

    Run expensive code on a separate thread
    Summary:
    ## This stack
    
    We noticed that the poll time is pretty high on some async methods, and that is affecting other calls as well. Let's try to optimize that.
    
    ## This diff
    
    The bulk of the cost of `commit_sparse_profile_delta` comes from a complex section of synchronous code; tuning that would be a fair amount of work, but we can at least ensure we don't block an executor.
    
    Reviewed By: singhsrb
    
    Differential Revision: D67136194
    
    fbshipit-source-id: d6c39ba841c34f4c6c323c62a4c9ef4c859236ef

--- eden/scm/lib/sparse/src/lib.rs
+++ eden/scm/lib/sparse/src/lib.rs
@@ -121,6 +121,9 @@ pub enum Error {
 
     #[error(transparent)]
     GlobsetError(#[from] globset::Error),
+
+    #[error(transparent)]
+    Internal(anyhow::Error),
 }
 
 #[cfg_attr(not(feature="async"), syncify([B: Future<Output = anyhow::Result<Option<Vec<u8>>>> + Send] => [], [B] => [anyhow::Result<Option<Vec<u8>>>], [Send + Sync] => []))]
@@ -226,10 +229,9 @@ impl Root {
                 only_v1 = false;
 
                 let (matcher_rules, origins) = prepare_rules(child_rules)?;
-                matchers.push(build_tree_matcher_from_rules(
-                    matcher_rules,
-                    self.prof.case_sensitive,
-                )?);
+                matchers.push(
+                    build_tree_matcher_from_rules(matcher_rules, self.prof.case_sensitive).await?,
+                );
                 rule_origins.push(origins);
             } else {
                 for rule in child_rules {
@@ -254,10 +256,8 @@ impl Root {
         ));
 
         let (matcher_rules, origins) = prepare_rules(rules)?;
-        matchers.push(build_tree_matcher_from_rules(
-            matcher_rules,
-            self.prof.case_sensitive,
-        )?);
+        matchers
+            .push(build_tree_matcher_from_rules(matcher_rules, self.prof.case_sensitive).await?);
         rule_origins.push(origins);
 
         Ok(Matcher::new(matchers, rule_origins))
@@ -274,6 +274,7 @@ impl Root {
     }
 }
 
+#[cfg(not(feature = "async"))]
 fn build_tree_matcher_from_rules(
     matcher_rules: Vec<String>,
     case_sensitive: bool,
@@ -282,6 +283,19 @@ fn build_tree_matcher_from_rules(
         matcher_rules.iter(),
         case_sensitive,
     )?)
+}
+
+#[cfg(feature = "async")]
+async fn build_tree_matcher_from_rules(
+    matcher_rules: Vec<String>,
+    case_sensitive: bool,
+) -> Result<TreeMatcher, Error> {
+    let matcher = tokio::task::spawn_blocking(move || {
+        TreeMatcher::from_rules(matcher_rules.iter(), case_sensitive)
+    })
+    .await
+    .map_err(|e| Error::Internal(e.into()))?;
+    Ok(matcher?)
 }
 
 #[cfg_attr(not(feature="async"), syncify([B: Future<Output = anyhow::Result<Option<Vec<u8>>>> + Send] => [], [B] => [anyhow::Result<Option<Vec<u8>>>], [Send + Sync] => []))]
