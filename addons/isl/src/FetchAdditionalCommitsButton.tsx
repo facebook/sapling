4a72f256044db7f85ac4770ec5fcea7786fc3ffb
Evan Krause <evangrayk@meta.com>
2025-01-13 13:38:07 -0800

    Fetch more commits from commit cloud if you've loaded all commits
    Summary:
    If you click "Load more commits" at the  bottom of ISL, we increase the fetch time range sent to `sl log`. After clicking twice, the range is removed and all draft commits are loaded.
    
    But internally, people may still not have commits visible, if they are not loaded through commit cloud. We should offer the "load more commits" button one last time, but have it run `sl cloud sync --full` instead of just increasing the `sl log` time range. This will actually load new commits, though it may take longer.
    
    One thing that's not ideal here is that "load more commits" may do nothing the first two times you click it, if there are no commits at all. Ideally we'd detect if the increased time range did nothing and load even more, but we don't do that yet.
    
    TODO: This diff does not hide this commit cloud fetch button if your repo does not support cloud. We need to detect if commit cloud is supported before showing this feature.
    
    Reviewed By: quark-zju
    
    Differential Revision: D67987542
    
    fbshipit-source-id: 984c90926305f2af5900f017222db9f887db9477

--- addons/isl/src/FetchAdditionalCommitsButton.tsx
+++ addons/isl/src/FetchAdditionalCommitsButton.tsx
@@ -7,11 +7,15 @@
 
 import serverAPI from './ClientToServerAPI';
 import {t, T} from './i18n';
+import {writeAtom} from './jotaiUtils';
+import {CommitCloudSyncOperation} from './operations/CommitCloudSyncOperation';
+import {useRunOperation} from './operationsState';
+import {useIsOperationRunningOrQueued} from './previews';
 import {commitsShownRange, isFetchingAdditionalCommits} from './serverAPIState';
 import {Button} from 'isl-components/Button';
 import {Icon} from 'isl-components/Icon';
 import {DOCUMENTATION_DELAY, Tooltip} from 'isl-components/Tooltip';
-import {useAtomValue} from 'jotai';
+import {atom, useAtomValue} from 'jotai';
 
 export function FetchingAdditionalCommitsRow() {
   return (
@@ -22,6 +26,8 @@ export function FetchingAdditionalCommitsRow() {
   );
 }
 
+const hasSyncedFromCloudAtom = atom(false);
+
 function FetchingAdditionalCommitsIndicator() {
   const isFetching = useAtomValue(isFetchingAdditionalCommits);
   return isFetching ? <Icon icon="loading" /> : null;
@@ -29,24 +35,44 @@ function FetchingAdditionalCommitsIndicator() {
 
 function FetchingAdditionalCommitsButton() {
   const shownRange = useAtomValue(commitsShownRange);
+  const isRunningSync = useIsOperationRunningOrQueued(CommitCloudSyncOperation) != null;
   const isFetching = useAtomValue(isFetchingAdditionalCommits);
-  if (shownRange === undefined) {
+  const isLoading = isFetching || isRunningSync;
+  const hasAlreadySynced = useAtomValue(hasSyncedFromCloudAtom);
+  const runOperation = useRunOperation();
+  if (shownRange === undefined && hasAlreadySynced) {
     return null;
   }
-  const commitsShownMessage = t('Showing commits from the last $numDays days', {
-    replace: {$numDays: shownRange.toString()},
-  });
+  const fetchFromCloudNext = shownRange == null;
+  const commitsShownMessage = fetchFromCloudNext
+    ? t('Showing full commit history. Click to fetch all commits from Commit Cloud')
+    : t('Showing commits from the last $numDays days', {
+        replace: {$numDays: shownRange.toString()},
+      });
   return (
     <Tooltip placement="top" delayMs={DOCUMENTATION_DELAY} title={commitsShownMessage}>
       <Button
-        disabled={isFetching}
+        disabled={isLoading}
         onClick={() => {
+          if (fetchFromCloudNext) {
+            runOperation(new CommitCloudSyncOperation(/* full */ true)).then(() =>
+              writeAtom(hasSyncedFromCloudAtom, true),
+            );
+            return;
+          }
+
           serverAPI.postMessage({
             type: 'loadMoreCommits',
           });
         }}
         icon>
-        <T>Load more commits</T>
+        {fetchFromCloudNext ? (
+          <>
+            <Icon icon={isLoading ? 'spinner' : 'cloud-download'} /> Fetch all cloud commits
+          </>
+        ) : (
+          <T>Load more commits</T>
+        )}
       </Button>
     </Tooltip>
   );
