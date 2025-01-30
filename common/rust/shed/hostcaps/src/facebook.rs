9e894f3e91d39dba524e848d0e8805c957d627ec
Mark Juggurnauth-Thomas <mbthomas@meta.com>
2025-01-30 15:26:28 -0800

    move internal implementations
    Reviewed By: zertosh, davidbarsky, dtolnay
    
    Differential Revision: D68907267
    
    fbshipit-source-id: 7cee3a691fc1b512e6e142e6b87b1c2f3f2b9fbd

--- common/rust/shed/hostcaps/src/facebook.rs
+++ common/rust/shed/hostcaps/src/facebook.rs
@@ -1,55 +1,15 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
 
-use derive_more::Display;
-use fbwhoami::FbWhoAmI;
+//! This module is only used in internal Meta builds.
 
-#[derive(PartialEq)]
-#[derive(Display)]
-#[repr(u8)]
-pub enum Env {
-    Prod = 0,
-    Corp = 1,
-    Lab = 2,
-}
+mod internal;
 
-pub fn get_env() -> Env {
-    let whoami = if let Ok(whoami) = FbWhoAmI::get() {
-        whoami
-    } else {
-        // default to corp env if no fbwhoami
-        return Env::Corp;
-    };
-
-    // Corp host e.g. a lab
-    if let Some(hostname_scheme) = &whoami.hostname_scheme {
-        if hostname_scheme.starts_with("corp_") {
-            return Env::Corp;
-        }
-        if hostname_scheme.starts_with("lab_") {
-            return Env::Lab;
-        }
-    }
-
-    // Cloud hosts e.g. AWS are treated like corp
-    if whoami.cloud_provider.is_some() {
-        return Env::Corp;
-    }
-
-    // Default to prod
-    Env::Prod
-}
-
-/// Returns true if the running host is on the production network.
-pub fn is_prod() -> bool {
-    get_env() == Env::Prod
-}
-
-/// Returns true if the running host is on the corp network.
-pub fn is_corp() -> bool {
-    get_env() == Env::Corp
-}
-
-/// Returns true if the running host is on the lab network.
-pub fn is_lab() -> bool {
-    get_env() == Env::Lab
-}
+#[allow(unused)]
+pub use internal::*;
