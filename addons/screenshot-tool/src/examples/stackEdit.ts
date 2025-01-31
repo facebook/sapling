2ceb6e106d6d7bfdd7bd39a9baff61e48a4404bc
Jun Wu <quark@meta.com>
2025-01-30 17:38:51 -0800

    screenshot: initial stack edit example
    Summary:
    Import some patches that depend on each other, it is easier to demostrate the
    stack edit feature.
    
    This diff only creates the example repo. I'll add button clicks and recording
    in follow ups.
    
    Reviewed By: evangrayk
    
    Differential Revision: D62605152
    
    fbshipit-source-id: cd42819a39c2236b34627e5f588ee980e4734f89

---
+++ addons/screenshot-tool/src/examples/stackEdit.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type {Example, TestRepo} from '../example';
+
+import {BASE_EXAMPLE} from '../example';
+import {dirname, join} from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const EXAMPLE: Example = {
+  ...BASE_EXAMPLE,
+  async populateRepo(repo: TestRepo) {
+    const patchPath = join(__dirname, 'linelog.ts.patch');
+    await repo.import(patchPath);
+  },
+  postOpenISL(): Promise<void> {
+    return this.repl();
+  },
+  openISLOptions: {
+    ...BASE_EXAMPLE.openISLOptions,
+    now: 1683084584,
+  },
+};
