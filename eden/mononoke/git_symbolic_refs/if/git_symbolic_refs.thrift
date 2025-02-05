d3d574acff3c760504897ba367b1f645c9b0995a
Rajiv Sharma <rajshar@meta.com>
2025-02-05 07:06:22 -0800

    Introduce thirft struct for GitSymbolicRefs
    Summary:
    We plan to introduce caching for `GitSymbolicRefs` since its a mostly-static piece of data that is associated with each Git repo. It stores symrefs in a given Git repo (like `HEAD` for example). Most Git repos just have the `HEAD` symref which once created is rarely changed.
    
    We need to access and query all known symrefs for a repo everytime we serve reads through Mononoke Git server. This results in a lot of unnecessary queries to the DB which can be prevented if we can just cache the results.
    
    This diff is the first in the stack to achieving that objective
    
    Reviewed By: clara-9
    
    Differential Revision: D69178880
    
    fbshipit-source-id: 7fb08af6fc956bc3adeaa866dd3c43b8becee984

---
+++ eden/mononoke/git_symbolic_refs/if/git_symbolic_refs.thrift
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+/// Code version used in memcache keys.  This should be changed whenever
+/// the layout of memcache entries is changed in an incompatible way.
+/// The corresponding sitever, which can be used to flush memcache, is
+/// in the JustKnob scm/mononoke_memcache_sitevers:git_symbolic_refs.
+const i32 MC_CODEVER = 0;
+
+typedef i32 RepoId (rust.newtype)
+
+struct GitSymbolicRefsCacheEntry {
+  1: RepoId repo_id;
+  2: string symref_name;
+  3: string ref_name;
+  4: string ref_type;
+} (rust.exhaustive)
