d5d9e6eac7c874124a1f4f7166d201fb78521695
David Tolnay <dtolnay@meta.com>
2025-02-07 11:35:13 -0800

    fbcode/eden/mononoke/microwave/
    Reviewed By: zertosh
    
    Differential Revision: D69042151
    
    fbshipit-source-id: 9777c0ba436c5200237b60ee65638bec3e8efea7

--- eden/mononoke/microwave/if/microwave.thrift
+++ eden/mononoke/microwave/if/microwave.thrift
@@ -8,10 +8,12 @@
 include "eden/mononoke/mononoke_types/serialization/id.thrift"
 include "eden/mononoke/mononoke_types/serialization/path.thrift"
 include "eden/mononoke/mercurial/types/if/mercurial_thrift.thrift"
+include "thrift/annotation/rust.thrift"
 
 // Code version constant -- update to invalidate saved state.
 const i32 CODEVER = 1;
 
+@rust.Exhaustive
 struct FilenodeSnapshot {
   // Note:  All fields must be present.  They are marked as optional so that we
   // can detect if they are missing.
@@ -21,20 +23,23 @@ struct FilenodeSnapshot {
   4: optional mercurial_thrift.HgNodeHash p2;
   5: optional CopyInfoSnapshot copyfrom;
   6: optional mercurial_thrift.HgNodeHash linknode;
-} (rust.exhaustive)
+}
 
+@rust.Exhaustive
 struct CopyInfoSnapshot {
   1: optional path.RepoPath path;
   2: optional mercurial_thrift.HgNodeHash filenode;
-} (rust.exhaustive)
+}
 
+@rust.Exhaustive
 struct ChangesetSnapshot {
   1: optional id.ChangesetId cs_id;
   2: optional list<id.ChangesetId> parents;
   3: optional i64 gen;
-} (rust.exhaustive)
+}
 
+@rust.Exhaustive
 struct RepoSnapshot {
   1: optional list<FilenodeSnapshot> filenodes;
   2: optional list<ChangesetSnapshot> changesets;
-} (rust.exhaustive)
+}
