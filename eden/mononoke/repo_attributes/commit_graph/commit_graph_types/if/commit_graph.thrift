919bd53b5758f678bfae77b022c9f8f4c923267c
David Tolnay <dtolnay@meta.com>
2025-02-06 20:22:56 -0800

    fbcode/eden/mononoke/repo_attributes/ [A]
    Reviewed By: zertosh
    
    Differential Revision: D69123711
    
    fbshipit-source-id: 010527286451887bfa394b58daa5c6bfa72cf694

--- eden/mononoke/repo_attributes/commit_graph/commit_graph_types/if/commit_graph.thrift
+++ eden/mononoke/repo_attributes/commit_graph/commit_graph_types/if/commit_graph.thrift
@@ -6,21 +6,25 @@
  */
 
 include "eden/mononoke/mononoke_types/serialization/id.thrift"
+include "thrift/annotation/rust.thrift"
 
 # Memcache constants. Should be changed when we want to invalidate memcache
 # entries
 const i32 MC_CODEVER = 1;
 const i32 MC_SITEVER = 0;
 
-typedef i64 Generation (rust.newtype)
+@rust.NewType
+typedef i64 Generation
 
+@rust.Exhaustive
 struct ChangesetNode {
   1: id.ChangesetId cs_id;
   2: Generation generation;
   3: i64 skip_tree_depth;
   4: i64 p1_linear_depth;
-} (rust.exhaustive)
+}
 
+@rust.Exhaustive
 struct ChangesetEdges {
   1: ChangesetNode node;
   2: list<ChangesetNode> parents;
@@ -28,18 +32,21 @@ struct ChangesetEdges {
   4: optional ChangesetNode skip_tree_parent;
   5: optional ChangesetNode skip_tree_skew_ancestor;
   6: optional ChangesetNode p1_linear_skew_ancestor;
-} (rust.exhaustive)
+}
 
+@rust.Exhaustive
 struct CachedChangesetEdges {
   1: ChangesetEdges edges;
   2: optional list<ChangesetEdges> prefetched_edges;
-} (rust.exhaustive)
+}
 
+@rust.Exhaustive
 struct PreloadedEdges {
   1: list<CompactChangesetEdges> edges;
   2: optional i64 max_sql_id;
-} (rust.exhaustive)
+}
 
+@rust.Exhaustive
 struct CompactChangesetEdges {
   1: id.ChangesetId cs_id;
   2: i32 unique_id;
@@ -51,4 +58,4 @@ struct CompactChangesetEdges {
   8: optional i32 skip_tree_parent;
   9: optional i32 skip_tree_skew_ancestor;
   10: optional i32 p1_linear_skew_ancestor;
-} (rust.exhaustive)
+}
