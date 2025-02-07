894bd40d387e7b115b4f160bc3fee11856293ddf
David Tolnay <dtolnay@meta.com>
2025-02-07 12:16:38 -0800

    fbcode/eden/mononoke/commit_rewriting/
    Reviewed By: zertosh
    
    Differential Revision: D69040526
    
    fbshipit-source-id: 670c88c092c890893e0c8352900788634601f898

--- eden/mononoke/commit_rewriting/synced_commit_mapping/if/synced_commit_mapping.thrift
+++ eden/mononoke/commit_rewriting/synced_commit_mapping/if/synced_commit_mapping.thrift
@@ -6,21 +6,24 @@
  */
 
 include "eden/mononoke/mononoke_types/serialization/id.thrift"
+include "thrift/annotation/rust.thrift"
 
 # Memcache constants. Should be changed when we want to invalidate memcache
 # entries
 const i32 MC_CODEVER = 0;
 const i32 MC_SITEVER = 0;
 
+@rust.Exhaustive
 struct CacheEntry {
   1: list<FetchedMappingEntry> mapping_entries;
-} (rust.exhaustive)
+}
 
+@rust.Exhaustive
 struct FetchedMappingEntry {
   1: id.ChangesetId target_bcs_id;
   2: optional string maybe_version_name;
   3: optional SyncedCommitSourceRepo maybe_source_repo;
-} (rust.exhaustive)
+}
 
 enum SyncedCommitSourceRepo {
   LARGE = 0,
