574265820bc107554cbd7d7002492acf0734737e
Luisa Vasquez Gomez <lmvasquezg@meta.com>
2025-02-18 03:23:24 -0800

    new commit tailer: read from stdin and print
    Summary: This tailer will eventually hold the processing (e.g cas upload) but while we do the integration with swift tailer let's just print the record to know what we'll be getting on this end
    
    Reviewed By: clara-9
    
    Differential Revision: D69746993
    
    fbshipit-source-id: e99f0ee575a63ad62f0c0073e509c505f74cd97e

--- eden/mononoke/cas_new_commit_tailer/src/commands/tail.rs
+++ eden/mononoke/cas_new_commit_tailer/src/commands/tail.rs
@@ -7,12 +7,23 @@
 
 use anyhow::Result;
 use clap::Parser;
+use futures::TryStreamExt;
 use mononoke_app::MononokeApp;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::BufReader;
 
 #[derive(Parser)]
 pub struct CommandArgs {}
 
 pub async fn run(_app: MononokeApp, _args: CommandArgs) -> Result<()> {
-    eprintln!("Mononoke cas new commit tailer");
+    let stdin = BufReader::new(tokio::io::stdin());
+    let lines = tokio_stream::wrappers::LinesStream::new(stdin.lines());
+
+    lines
+        .try_for_each(|line| async move {
+            println!("{line}");
+            Ok(())
+        })
+        .await?;
     Ok(())
 }
