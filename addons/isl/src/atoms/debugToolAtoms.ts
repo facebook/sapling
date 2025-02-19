f5535076d8fb2b7cbc05be63d9c3c5c6c367d131
Jun Wu <quark@meta.com>
2025-02-18 17:27:45 -0800

    atom: add state for sl --debug and --verbose flags
    Summary:
    Sometimes it's nice to get extra output from the CLI to troubleshoot issues.
    Added states for that. I didn't use localStorage state intentionally since the
    flags are most likely one-time for debugging, and are not intended to stick
    forever.
    
    Reviewed By: evangrayk
    
    Differential Revision: D69800335
    
    fbshipit-source-id: 13e6787edbb8a2fbe5c27347675ef33a97c6bda2

--- addons/isl/src/atoms/debugToolAtoms.ts
+++ addons/isl/src/atoms/debugToolAtoms.ts
@@ -5,8 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import {atom} from 'jotai';
 import {localStorageBackedAtom} from '../jotaiUtils';
 
 export const enableReduxTools = localStorageBackedAtom<boolean>('isl.debug-redux-tools', false);
 
 export const enableReactTools = localStorageBackedAtom<boolean>('isl.debug-react-tools', false);
+
+export const enableSaplingDebugFlag = atom<boolean>(false);
+
+export const enableSaplingVerboseFlag = atom<boolean>(false);
