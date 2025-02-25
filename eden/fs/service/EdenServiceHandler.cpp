952b68d3c9964fe6f74ddbd7ee3a76b7658f1478
Chris Dinh <cqd@meta.com>
2025-02-24 20:53:45 -0800

    Properly format logging for changesSinceV2 hash
    Summary: Fixes snapshotHash format from raw bytes to formatted
    
    Reviewed By: jdelliot
    
    Differential Revision: D70130108
    
    fbshipit-source-id: f871b4209a88349483e989ca5bd5d103b33833f5

--- eden/fs/service/EdenServiceHandler.cpp
+++ eden/fs/service/EdenServiceHandler.cpp
@@ -2347,7 +2347,7 @@ void EdenServiceHandler::sync_changesSinceV2(
           "fromPosition={}:{}:{}, includedRoots:{}, excludedRoots:{}, includedSuffixes:{}, excludedSuffixes:{}",
           fromPosition.mountGeneration().value(),
           fromPosition.sequenceNumber().value(),
-          fromPosition.snapshotHash().value(),
+          logHash(fromPosition.snapshotHash().value()),
           toLogArg(includedRoots),
           toLogArg(excludedRoots),
           toLogArg(includedSuffixes),
