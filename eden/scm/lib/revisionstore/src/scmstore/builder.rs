350fb19d61352028a7cdfc98fd7e7e4bbefc6a4f
Liu Yang <lyang@meta.com>
2025-03-03 04:47:18 -0800

    Add cas-mode config option
    Summary:
    This diff introduces `cas-mode` option, its value could be "files", "trees", "on", or "off".
    
    `cas-mode=trees/files/on` indicates "casc is enabled" and "off" means "casc is disabled". When it's enabled, depending on its mode, the other options (https://fburl.com/code/hoqe0cea) will be set implicitly.
    
    Eventually the only user facing nob would be "cas-mode".
    
    NOTE: This diff is only adding the new option. Removal of the redundant options will come in next diff.
    
    Reviewed By: muirdm, liubov-dmitrieva
    
    Differential Revision: D70388366
    
    fbshipit-source-id: 528a73555e08ce11514b17c3a9fc58860791a0ac

--- eden/scm/lib/revisionstore/src/scmstore/builder.rs
+++ eden/scm/lib/revisionstore/src/scmstore/builder.rs
@@ -364,14 +364,23 @@ impl<'a> FileStoreBuilder<'a> {
                 None
             };
 
-        let cas_client = if self.config.get_or_default("scmstore", "fetch-from-cas")?
+        let is_casc_enabled = match self
+            .config
+            .get_or_default::<String>("scmstore", "cas-mode")?
+            .as_str()
+        {
+            "files" | "on" => true,
+            _ => false,
+        };
+        let cas_client = if is_casc_enabled
+            || self.config.get_or_default("scmstore", "fetch-from-cas")?
             || self
                 .config
                 .get_or_default("scmstore", "fetch-files-from-cas")?
         {
             self.cas_client
         } else {
-            tracing::debug!(target: "cas", "scmstore disabled (scmstore.fetch-from-cas=false and scmstore.fetch-files-from-cas=false)");
+            tracing::debug!(target: "cas", "scmstore disabled (scmstore.fetch-from-cas=false and scmstore.fetch-files-from-cas=false and scmstore.cas-mode!=files|on)");
             None
         };
 
@@ -648,10 +657,20 @@ impl<'a> TreeStoreBuilder<'a> {
             self.build_indexedlog_cache()?
         };
 
-        tracing::trace!(target: "revisionstore::treestore", "processing tree_aux_store");
-        let tree_aux_store = if self
+        let is_casc_enabled = match self
             .config
-            .get_or("scmstore", "store-tree-aux-data", || true)?
+            .get_or_default::<String>("scmstore", "cas-mode")?
+            .as_str()
+        {
+            "trees" | "on" => true,
+            _ => false,
+        };
+
+        tracing::trace!(target: "revisionstore::treestore", "processing tree_aux_store");
+        let tree_aux_store = if is_casc_enabled
+            || self
+                .config
+                .get_or("scmstore", "store-tree-aux-data", || true)?
         {
             if let Some(tree_aux_store) = self.tree_aux_store.take() {
                 Some(tree_aux_store)
@@ -683,16 +702,19 @@ impl<'a> TreeStoreBuilder<'a> {
             .config
             .get_or_default("scmstore", "prefetch-tree-parents")?;
 
-        let tree_metadata_mode = match self.config.get("scmstore", "tree-metadata-mode").as_deref()
-        {
-            Some("always") => TreeMetadataMode::Always,
-            None | Some("opt-in") => TreeMetadataMode::OptIn,
-            _ => TreeMetadataMode::Never,
+        let tree_metadata_mode = match is_casc_enabled {
+            true => TreeMetadataMode::Always,
+            _ => match self.config.get("scmstore", "tree-metadata-mode").as_deref() {
+                Some("always") => TreeMetadataMode::Always,
+                None | Some("opt-in") => TreeMetadataMode::OptIn,
+                _ => TreeMetadataMode::Never,
+            },
         };
 
-        let fetch_tree_aux_data = self
-            .config
-            .get_or_default::<bool>("scmstore", "fetch-tree-aux-data")?;
+        let fetch_tree_aux_data = is_casc_enabled
+            || self
+                .config
+                .get_or_default::<bool>("scmstore", "fetch-tree-aux-data")?;
 
         if fetch_tree_aux_data && tree_aux_store.is_none() {
             tracing::warn!(
@@ -700,7 +722,8 @@ impl<'a> TreeStoreBuilder<'a> {
             );
         }
 
-        let cas_client = if self.config.get_or_default("scmstore", "fetch-from-cas")?
+        let cas_client = if is_casc_enabled
+            || self.config.get_or_default("scmstore", "fetch-from-cas")?
             || self
                 .config
                 .get_or_default("scmstore", "fetch-trees-from-cas")?
@@ -716,7 +739,7 @@ impl<'a> TreeStoreBuilder<'a> {
 
             self.cas_client
         } else {
-            tracing::debug!(target: "cas", "scmstore disabled (scmstore.fetch-from-cas=false and scmstore.fetch-trees-from-cas=false)");
+            tracing::debug!(target: "cas", "scmstore disabled (scmstore.fetch-from-cas=false and scmstore.fetch-trees-from-cas=false and scmstore.cas-mode!=trees|on)");
             None
         };
 
