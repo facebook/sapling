e872955bcfe781656c77116c147c1dd71f33e652
Genevieve (Genna) Helsel <helsel@meta.com>
2025-03-12 11:41:10 -0700

    Introduce a RequestPermitVendor utility for request rate limiting
    Summary:
    # Project Context
    EdenFS has a non-trivial threading model. It has a series of executors, some which have very specific purposes and some which handle generic work. Specific units of work (such as, serving a Thrift call) can jump around from one executor to another. A handful of Thrift entrypoints, like the entrypoints that handle prefetch and Watchman queries, have wide fanout, and can monopolize EdenFS' resources, leading to starvation and eventual Queue Timeouts for other Thrift methods.
    
    Furthermore, there exist foot-guns in EdenFS that have caused repeated SEVs (S412223, S399431, S401889) and have caused user-visible issues like deadlocks and exceptions (D50199539, D41390586).  These issues are easy to accidentally introduce due to the lack of guard rails in place and are difficult to root cause. Much of our threading rules/logic is mandated by in-code comments or learnt prior experience, which is not scalable and makes it hard to onboard and confidently land changes. This project's goal is to de-risk changes that touch these parts of the codebase and to make it easier to understand and optimize EdenFS' resource usage.
    
    # Subtask Context
    Most of our thread pools are managed by Folly executors. There are bounded and unbounded executors. We’ve encountered SEVs in the past where trying to enqueue work onto a full, bounded executor can cause a deadlock (S412223). To avoid these suites of problems, we can ensure all of our executors are unbounded and add backpressure manually at EdenFS' two entry points (Thrift and FSChannel) by limiting the amount of work that enters EdenFS using configurable semaphores.
    
    # This Diff
    This diff introduces a `RequestPermitVendor` class. `RequestPermitVendor` generates `RequestPermit`s which represent a resource acquired from a semaphore. `RequestPermit`s release the resource when destructed - it is a RAII guard for acquiring and releasing a request permit. A `RequestPermit`  automatically acquires a permit from the `RequestPermitVendor` when constructed and releases it when destroyed, ensuring that every `wait()` operation is matched with a `signal()` operation.
    
    # Resources
    
    Reviewed By: jdelliot
    
    Differential Revision: D70808676
    
    fbshipit-source-id: dbdd6a4d47ff062785ff5ad5dd696f93206b0c74

---
+++ eden/fs/utils/RequestPermitVendor.h
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#pragma once
+
+#include <folly/fibers/Semaphore.h>
+#include <cstdlib>
+#include <memory>
+
+namespace facebook::eden {
+
+/**
+ * RAII guard for acquiring and releasing a request permit.
+ *
+ * This class automatically acquires a permit when constructed and releases it
+ * when destroyed, ensuring that every wait() operation is matched with a
+ * signal() operation.
+ */
+class RequestPermit {
+ public:
+  explicit RequestPermit(std::weak_ptr<folly::fibers::Semaphore> sem)
+      : sem_(std::move(sem)) {
+    if (auto semPtr = sem_.lock()) {
+      semPtr->wait();
+    }
+  }
+
+  ~RequestPermit() {
+    if (auto semPtr = sem_.lock()) {
+      semPtr->signal();
+    }
+    sem_.reset();
+  }
+
+  // Disallow default construction, coping, and moving to avoid accidental
+  // permit creation/deletion. This class should be managed by a smart pointer.
+  // If RequestPermitVendor is extended to support a try_aquirePermit() or
+  // a co_aquirePermit() method, this class will likely need to be extended to
+  // offer alternative construction methods.
+  RequestPermit() = delete;
+  RequestPermit(const RequestPermit&) = delete;
+  RequestPermit& operator=(const RequestPermit&) = delete;
+  RequestPermit(RequestPermit&&) = delete;
+  RequestPermit& operator=(RequestPermit&&) = delete;
+
+ private:
+  std::weak_ptr<folly::fibers::Semaphore> sem_;
+
+  friend class RequestPermitVendor;
+};
+
+/**
+ * RequestPermitVendor generates RequestPermits which represent a resource
+ * acquired from a semaphore. RequestPermits release the resource when
+ * destructed. RequestPermitVendor has sole ownership over the underlying
+ * semaphore. This can be added to any class that wishes to implement rate
+ * limiting
+ *
+ * This class currently only offers a blocking aquire method, but it can be
+ * extended in the future to add a try_aquirePermit() method which can return
+ * immediately if the semaphore is out of capacity. It can also be extened to
+ * support a co_aquirePermit() method, see folly::fibers::Semaphore::co_wait()
+ * for more information.
+ */
+class RequestPermitVendor {
+ public:
+  explicit RequestPermitVendor(std::size_t limit)
+      : sem_(std::make_shared<folly::fibers::Semaphore>(limit)) {}
+
+  /**
+   * This will block until a permit is available.
+   */
+  inline std::unique_ptr<RequestPermit> acquirePermit() {
+    return std::make_unique<RequestPermit>(sem_);
+  }
+
+  /**
+   * Get the configured max capacity of the underlying semaphore
+   */
+  inline std::size_t capacity() const {
+    return sem_->getCapacity();
+  }
+
+  /**
+   * Get the current available headroom of the underlying semaphore
+   */
+  inline std::size_t available() const {
+    return sem_->getAvailableTokens();
+  }
+
+  /**
+   * Get the current number of inflight requests
+   */
+  inline std::size_t inflight() const {
+    return sem_->getCapacity() - sem_->getAvailableTokens();
+  }
+
+ private:
+  std::shared_ptr<folly::fibers::Semaphore> sem_;
+};
+
+} // namespace facebook::eden
---
+++ eden/fs/utils/test/RequestPermitVendorTest.cpp
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "eden/fs/utils/RequestPermitVendor.h"
+
+#include <gtest/gtest.h>
+
+using namespace facebook::eden;
+
+TEST(RequestPermitVendorTest, AcquirePermitSimple) {
+  auto vendor = RequestPermitVendor(1);
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 1);
+  EXPECT_EQ(vendor.inflight(), 0);
+
+  auto p1 = vendor.acquirePermit();
+  EXPECT_NE(p1, nullptr);
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 0);
+  EXPECT_EQ(vendor.inflight(), 1);
+
+  p1.reset();
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 1);
+  EXPECT_EQ(vendor.inflight(), 0);
+}
+
+TEST(RequestPermitVendorTest, AcquirePermitSimpleScopeDestruction) {
+  auto vendor = RequestPermitVendor(1);
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 1);
+  EXPECT_EQ(vendor.inflight(), 0);
+
+  {
+    auto p1 = vendor.acquirePermit();
+    EXPECT_NE(p1, nullptr);
+    EXPECT_EQ(vendor.capacity(), 1);
+    EXPECT_EQ(vendor.available(), 0);
+    EXPECT_EQ(vendor.inflight(), 1);
+  }
+
+  // p1 is out of scope, the capacity should be released back to the vendor
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 1);
+  EXPECT_EQ(vendor.inflight(), 0);
+}
+
+TEST(RequestPermitVendorTest, EnsureAcquirePermitOverCapacityBlocks) {
+  auto vendor = RequestPermitVendor(1);
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 1);
+  EXPECT_EQ(vendor.inflight(), 0);
+
+  auto p1 = vendor.acquirePermit();
+  EXPECT_NE(p1, nullptr);
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 0);
+  EXPECT_EQ(vendor.inflight(), 1);
+
+  folly::Future<std::unique_ptr<RequestPermit>> future =
+      folly::makeFuture()
+          .via(folly::getGlobalCPUExecutor().get())
+          .thenValue([&](auto&&) { return vendor.acquirePermit(); });
+
+  EXPECT_EQ(future.isReady(), false);
+
+  // Destroy the first RequestPermit, which should unblock the acquirePermit()
+  // in the future
+  p1.reset();
+
+  std::unique_ptr<RequestPermit> p2;
+  try {
+    p2 = std::move(future).get(std::chrono::milliseconds(100));
+  } catch (const folly::FutureInvalid&) {
+    FAIL() << "Future was not ready when it was expected to be";
+  }
+
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 0);
+  EXPECT_EQ(vendor.inflight(), 1);
+
+  p2.reset();
+  EXPECT_EQ(vendor.capacity(), 1);
+  EXPECT_EQ(vendor.available(), 1);
+  EXPECT_EQ(vendor.inflight(), 0);
+}
+
+TEST(RequestPermitVendorTest, AcquirePermitLargeLimit) {
+  auto vendor = RequestPermitVendor(100);
+  EXPECT_EQ(vendor.capacity(), 100);
+  EXPECT_EQ(vendor.available(), 100);
+  EXPECT_EQ(vendor.inflight(), 0);
+
+  auto permits = std::vector<std::unique_ptr<RequestPermit>>{};
+
+  for (int i = 0; i < 100; ++i) {
+    permits.push_back(vendor.acquirePermit());
+  }
+  EXPECT_EQ(permits.size(), 100);
+  EXPECT_EQ(vendor.capacity(), 100);
+  EXPECT_EQ(vendor.available(), 0);
+  EXPECT_EQ(vendor.inflight(), 100);
+
+  permits.clear();
+  EXPECT_EQ(permits.size(), 0);
+  EXPECT_EQ(vendor.capacity(), 100);
+  EXPECT_EQ(vendor.available(), 100);
+  EXPECT_EQ(vendor.inflight(), 0);
+
+  for (int i = 0; i < 50; ++i) {
+    permits.push_back(vendor.acquirePermit());
+  }
+  EXPECT_EQ(permits.size(), 50);
+  EXPECT_EQ(vendor.capacity(), 100);
+  EXPECT_EQ(vendor.available(), 50);
+  EXPECT_EQ(vendor.inflight(), 50);
+
+  permits.clear();
+  EXPECT_EQ(vendor.capacity(), 100);
+  EXPECT_EQ(vendor.available(), 100);
+  EXPECT_EQ(vendor.inflight(), 0);
+};
+
+TEST(RequestPermitVendorTest, DeletedVendorWithOutstandingPermit) {
+  auto vendor = std::make_unique<RequestPermitVendor>(1);
+  EXPECT_EQ(vendor->capacity(), 1);
+  EXPECT_EQ(vendor->available(), 1);
+  EXPECT_EQ(vendor->inflight(), 0);
+
+  auto p1 = vendor->acquirePermit();
+  EXPECT_NE(p1, nullptr);
+  EXPECT_EQ(vendor->capacity(), 1);
+  EXPECT_EQ(vendor->available(), 0);
+  EXPECT_EQ(vendor->inflight(), 1);
+
+  EXPECT_NO_THROW(vendor.reset());
+  EXPECT_NO_THROW(p1.reset());
+};
+
+TEST(RequestPermitVendorTest, PermitsAreMovable) {
+  auto vendor = std::make_unique<RequestPermitVendor>(2);
+  EXPECT_EQ(vendor->capacity(), 2);
+  EXPECT_EQ(vendor->available(), 2);
+  EXPECT_EQ(vendor->inflight(), 0);
+
+  auto p1 = vendor->acquirePermit();
+  EXPECT_NE(p1, nullptr);
+  EXPECT_EQ(vendor->capacity(), 2);
+  EXPECT_EQ(vendor->available(), 1);
+  EXPECT_EQ(vendor->inflight(), 1);
+
+  // Moving the permit should not increase the number of inflight requests
+  auto p1_moved = std::move(p1);
+  EXPECT_EQ(vendor->capacity(), 2);
+  EXPECT_EQ(vendor->available(), 1);
+  EXPECT_EQ(vendor->inflight(), 1);
+
+  // Reseting the original p1 pointer doesn't affect p1_moved
+  p1.reset();
+  EXPECT_EQ(vendor->capacity(), 2);
+  EXPECT_EQ(vendor->available(), 1);
+  EXPECT_EQ(vendor->inflight(), 1);
+
+  // We should be able to aquire a second permit
+  auto p2 = vendor->acquirePermit();
+  EXPECT_EQ(vendor->capacity(), 2);
+  EXPECT_EQ(vendor->available(), 0);
+  EXPECT_EQ(vendor->inflight(), 2);
+
+  // Destroying the first permit should be observable by the vendor
+  p1_moved.reset();
+  EXPECT_EQ(vendor->capacity(), 2);
+  EXPECT_EQ(vendor->available(), 1);
+  EXPECT_EQ(vendor->inflight(), 1);
+
+  p2.reset();
+  EXPECT_EQ(vendor->capacity(), 2);
+  EXPECT_EQ(vendor->available(), 2);
+  EXPECT_EQ(vendor->inflight(), 0);
+};
