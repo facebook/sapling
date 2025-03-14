b139dfb583993333ba6030117ae2dd4cbf3e4c3b
Michael Cuevas <cuev@meta.com>
2025-03-14 14:20:35 -0700

    attributes: add wrappers for all file attribute variants
    Summary:
    # This diff
    
    Adds convenience wrappers to all FileAttributes variants
    
    # Context
    
    The EdenFS thrift service often suffers from Thrift "Queue Timeout" errors which indicate that the Eden daemon cannot keep up with the number of requests it's receiving and therefore does not dequeue Thrift requests from the queue fast enough. In the past, there hasn't been an easy/reliable way to trigger these Queue Timeout errors.
    
    This diff stack adds a new command, `eden debug stress` that can be used to trigger Queue Timeout errors or simply stress test Eden to understand how it performs under certain workloads.
    
    The command is meant to be easily extendable, so that we can easily plug in new Thrift endpoints to make them stress testable in the future. This diff adds the `getAttributesFromFilesV2` as the first endpoint to be stress testable. We may want to add `getFileInformation`, `globFiles`, etc in the future (we should definitely add `catFile`/`getFileContent` when that endpoint is added this half).
    
    The subcommand takes the number of tasks, request amount, and mount point as optional arguments.
    
    Reviewed By: jdelliot
    
    Differential Revision: D71133451
    
    fbshipit-source-id: 5c8e009c8928a209043bf27a80e367b837a9320b

--- eden/fs/cli_rs/edenfs-client/src/attributes.rs
+++ eden/fs/cli_rs/edenfs-client/src/attributes.rs
@@ -11,8 +11,164 @@ use std::str::FromStr;
 use anyhow::anyhow;
 use anyhow::Context;
 use anyhow::Result;
+use edenfs_error::EdenFsError;
 use thrift_types::edenfs::FileAttributes;
 use thrift_types::fbthrift::ThriftEnum;
+
+// YES, the following code is extremely repetitive. It's unfortunately the only way (for now). We
+// could potentially use macros in the future, but that would require language feature
+// 'more_qualified_paths' to be stabilized first: https://github.com/rust-lang/rust/issues/86935
+// So for now, we will deal with the repetition... :(
+
+pub enum Sha1OrError {
+    Sha1(Vec<u8>),
+    Error(EdenFsError),
+    UnknownField(i32),
+}
+
+impl From<thrift_types::edenfs::Sha1OrError> for Sha1OrError {
+    fn from(from: thrift_types::edenfs::Sha1OrError) -> Self {
+        match from {
+            thrift_types::edenfs::Sha1OrError::sha1(sha1) => Self::Sha1(sha1),
+            thrift_types::edenfs::Sha1OrError::error(e) => {
+                Self::Error(EdenFsError::ThriftRequestError(e.into()))
+            }
+            thrift_types::edenfs::Sha1OrError::UnknownField(i) => Self::UnknownField(i),
+        }
+    }
+}
+
+pub enum SizeOrError {
+    Size(i64),
+    Error(EdenFsError),
+    UnknownField(i32),
+}
+
+impl From<thrift_types::edenfs::SizeOrError> for SizeOrError {
+    fn from(from: thrift_types::edenfs::SizeOrError) -> Self {
+        match from {
+            thrift_types::edenfs::SizeOrError::size(size) => Self::Size(size),
+            thrift_types::edenfs::SizeOrError::error(e) => {
+                Self::Error(EdenFsError::ThriftRequestError(e.into()))
+            }
+            thrift_types::edenfs::SizeOrError::UnknownField(i) => Self::UnknownField(i),
+        }
+    }
+}
+
+pub enum SourceControlType {
+    Tree,
+    RegularFile,
+    ExecutableFile,
+    Symlink,
+    Unknown,
+}
+
+impl From<thrift_types::edenfs::SourceControlType> for SourceControlType {
+    fn from(from: thrift_types::edenfs::SourceControlType) -> Self {
+        match from {
+            thrift_types::edenfs::SourceControlType::TREE => Self::Tree,
+            thrift_types::edenfs::SourceControlType::REGULAR_FILE => Self::RegularFile,
+            thrift_types::edenfs::SourceControlType::EXECUTABLE_FILE => Self::ExecutableFile,
+            thrift_types::edenfs::SourceControlType::SYMLINK => Self::Symlink,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+pub enum SourceControlTypeOrError {
+    SourceControlType(SourceControlType),
+    Error(EdenFsError),
+    UnknownField(i32),
+}
+
+impl From<thrift_types::edenfs::SourceControlTypeOrError> for SourceControlTypeOrError {
+    fn from(from: thrift_types::edenfs::SourceControlTypeOrError) -> Self {
+        match from {
+            thrift_types::edenfs::SourceControlTypeOrError::sourceControlType(scm_type) => {
+                Self::SourceControlType(scm_type.into())
+            }
+            thrift_types::edenfs::SourceControlTypeOrError::error(e) => {
+                Self::Error(EdenFsError::ThriftRequestError(e.into()))
+            }
+            thrift_types::edenfs::SourceControlTypeOrError::UnknownField(i) => {
+                Self::UnknownField(i)
+            }
+        }
+    }
+}
+
+pub enum ObjectIdOrError {
+    ObjectId(Vec<u8>),
+    Error(EdenFsError),
+    UnknownField(i32),
+}
+
+impl From<thrift_types::edenfs::ObjectIdOrError> for ObjectIdOrError {
+    fn from(from: thrift_types::edenfs::ObjectIdOrError) -> Self {
+        match from {
+            thrift_types::edenfs::ObjectIdOrError::objectId(size) => Self::ObjectId(size),
+            thrift_types::edenfs::ObjectIdOrError::error(e) => {
+                Self::Error(EdenFsError::ThriftRequestError(e.into()))
+            }
+            thrift_types::edenfs::ObjectIdOrError::UnknownField(i) => Self::UnknownField(i),
+        }
+    }
+}
+
+pub enum Blake3OrError {
+    Blake3(Vec<u8>),
+    Error(EdenFsError),
+    UnknownField(i32),
+}
+
+impl From<thrift_types::edenfs::Blake3OrError> for Blake3OrError {
+    fn from(from: thrift_types::edenfs::Blake3OrError) -> Self {
+        match from {
+            thrift_types::edenfs::Blake3OrError::blake3(size) => Self::Blake3(size),
+            thrift_types::edenfs::Blake3OrError::error(e) => {
+                Self::Error(EdenFsError::ThriftRequestError(e.into()))
+            }
+            thrift_types::edenfs::Blake3OrError::UnknownField(i) => Self::UnknownField(i),
+        }
+    }
+}
+
+pub enum DigestHashOrError {
+    DigestHash(Vec<u8>),
+    Error(EdenFsError),
+    UnknownField(i32),
+}
+
+impl From<thrift_types::edenfs::DigestHashOrError> for DigestHashOrError {
+    fn from(from: thrift_types::edenfs::DigestHashOrError) -> Self {
+        match from {
+            thrift_types::edenfs::DigestHashOrError::digestHash(size) => Self::DigestHash(size),
+            thrift_types::edenfs::DigestHashOrError::error(e) => {
+                Self::Error(EdenFsError::ThriftRequestError(e.into()))
+            }
+            thrift_types::edenfs::DigestHashOrError::UnknownField(i) => Self::UnknownField(i),
+        }
+    }
+}
+
+pub enum DigestSizeOrError {
+    DigestSize(i64),
+    Error(EdenFsError),
+    UnknownField(i32),
+}
+
+impl From<thrift_types::edenfs::DigestSizeOrError> for DigestSizeOrError {
+    fn from(from: thrift_types::edenfs::DigestSizeOrError) -> Self {
+        match from {
+            thrift_types::edenfs::DigestSizeOrError::digestSize(size) => Self::DigestSize(size),
+            thrift_types::edenfs::DigestSizeOrError::error(e) => {
+                Self::Error(EdenFsError::ThriftRequestError(e.into()))
+            }
+            thrift_types::edenfs::DigestSizeOrError::UnknownField(i) => Self::UnknownField(i),
+        }
+    }
+}
 
 pub fn all_attributes() -> &'static [&'static str] {
     FileAttributes::variants()
