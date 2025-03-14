df219443375180fe3ec18e12d879936cb7f4b535
Michael Cuevas <cuev@meta.com>
2025-03-14 14:20:35 -0700

    client: refactor glob module
    Summary:
    # This diff
    
    Slightly refactors the existing glob_files wrappers to use a common glob_files_optional function that handles all type translation/coercion/defaults
    
    # Context
    
    The EdenFS thrift service often suffers from Thrift "Queue Timeout" errors which indicate that the Eden daemon cannot keep up with the number of requests it's receiving and therefore does not dequeue Thrift requests from the queue fast enough. In the past, there hasn't been an easy/reliable way to trigger these Queue Timeout errors.
    
    This diff stack adds a new command, `eden debug stress` that can be used to trigger Queue Timeout errors or simply stress test Eden to understand how it performs under certain workloads.
    
    The command is meant to be easily extendable, so that we can easily plug in new Thrift endpoints to make them stress testable in the future. This diff adds the `getAttributesFromFilesV2` as the first endpoint to be stress testable. We may want to add `getFileInformation`, `globFiles`, etc in the future (we should definitely add `catFile`/`getFileContent` when that endpoint is added this half).
    
    The subcommand takes the number of tasks, request amount, and mount point as optional arguments.
    
    Reviewed By: jdelliot
    
    Differential Revision: D71133452
    
    fbshipit-source-id: c72a00f76ca1627f634e6abfcb5f8e40fb764aea

--- eden/fs/cli_rs/edenfs-client/src/glob_files.rs
+++ eden/fs/cli_rs/edenfs-client/src/glob_files.rs
@@ -6,6 +6,7 @@
  */
 
 use std::path::Path;
+use std::path::PathBuf;
 
 use anyhow::anyhow;
 use edenfs_error::EdenFsError;
@@ -15,6 +16,8 @@ use thrift_thriftclients::thrift::OsDtype;
 use thrift_types::edenfs::GlobParams;
 
 use crate::client::EdenFsClient;
+use crate::types::OSName;
+use crate::types::SyncBehavior;
 
 #[derive(Clone, Debug)]
 pub struct Glob {
@@ -32,12 +35,84 @@ impl From<thrift_types::edenfs::Glob> for Glob {
         }
     }
 }
+pub struct PredictiveFetchParams {
+    pub num_top_directories: Option<i32>,
+    pub user: Option<String>,
+    pub repo: Option<String>,
+    pub os: Option<OSName>,
+    pub start_time: Option<i64>,
+    pub end_time: Option<i64>,
+}
+
+impl From<PredictiveFetchParams> for thrift_types::edenfs::PredictiveFetch {
+    fn from(from: PredictiveFetchParams) -> Self {
+        Self {
+            numTopDirectories: from.num_top_directories,
+            user: from.user,
+            repo: from.repo,
+            os: from.os.map(|os| os.to_string()),
+            startTime: from.start_time,
+            endTime: from.end_time,
+            ..Default::default()
+        }
+    }
+}
 
 impl EdenFsClient {
+    async fn glob_files_optional<P: AsRef<Path>, S: AsRef<Path>>(
+        &self,
+        mount_point: P,
+        glob_patterns: Vec<String>,
+        include_dotfiles: Option<bool>,
+        prefetch_files: Option<bool>,
+        suppress_file_list: Option<bool>,
+        want_dtype: Option<bool>,
+        revisions: Option<&[&str]>,
+        prefetch_metadata: Option<bool>,
+        search_root: Option<S>,
+        background: Option<bool>,
+        predictive_glob: Option<PredictiveFetchParams>,
+        list_only_files: Option<bool>,
+        sync: Option<SyncBehavior>,
+    ) -> Result<Glob> {
+        let glob_params = GlobParams {
+            mountPoint: bytes_from_path(mount_point.as_ref().to_path_buf())?,
+            globs: glob_patterns,
+            includeDotfiles: include_dotfiles.unwrap_or_default(),
+            prefetchFiles: prefetch_files.unwrap_or_default(),
+            suppressFileList: suppress_file_list.unwrap_or_default(),
+            wantDtype: want_dtype.unwrap_or_default(),
+            revisions: revisions
+                .unwrap_or_default()
+                .iter()
+                .map(|s| s.as_bytes().to_vec())
+                .collect(),
+            prefetchMetadata: prefetch_metadata.unwrap_or_default(),
+            searchRoot: search_root
+                .and_then(|sr| bytes_from_path(sr.as_ref().to_path_buf()).ok())
+                .unwrap_or_default(),
+            background: background.unwrap_or_default(),
+            predictiveGlob: predictive_glob.map(Into::into),
+            listOnlyFiles: list_only_files.unwrap_or_default(),
+            sync: sync.map(Into::into).unwrap_or_default(),
+            ..Default::default()
+        };
+        self.with_thrift(|thrift| thrift.globFiles(&glob_params))
+            .await
+            .map_err(|err| {
+                EdenFsError::Other(anyhow!(
+                    "Failed invoking globFiles using params='{:?}' with error={:?}'",
+                    glob_params,
+                    err
+                ))
+            })
+            .map(Into::into)
+    }
+
     pub async fn glob_files<P: AsRef<Path>, S: AsRef<Path>>(
         &self,
         mount_point: P,
-        globs: Vec<String>,
+        glob_patterns: Vec<String>,
         include_dotfiles: bool,
         prefetch_files: bool,
         suppress_file_list: bool,
@@ -46,27 +121,47 @@ impl EdenFsClient {
         background: bool,
         list_only_files: bool,
     ) -> Result<Glob> {
-        let glob_params = GlobParams {
-            mountPoint: bytes_from_path(mount_point.as_ref().to_path_buf())?,
-            globs,
-            includeDotfiles: include_dotfiles,
-            prefetchFiles: prefetch_files,
-            suppressFileList: suppress_file_list,
-            wantDtype: want_dtype,
-            searchRoot: bytes_from_path(search_root.as_ref().to_path_buf())?,
-            background,
-            listOnlyFiles: list_only_files,
-            ..Default::default()
-        };
-        self.with_thrift(|thrift| thrift.globFiles(&glob_params))
-            .await
-            .map(|glob| glob.into())
-            .map_err(|err| {
-                EdenFsError::Other(anyhow!(
-                    "Failed invoking globFiles using params='{:?}' with error={:?}'",
-                    glob_params,
-                    err
-                ))
-            })
+        self.glob_files_optional(
+            mount_point,
+            glob_patterns,
+            Some(include_dotfiles),
+            Some(prefetch_files),
+            Some(suppress_file_list),
+            Some(want_dtype),
+            None,
+            None,
+            Some(search_root),
+            Some(background),
+            None,
+            Some(list_only_files),
+            None,
+        )
+        .await
+    }
+
+    #[allow(dead_code)]
+    async fn glob_files_foreground(
+        &self,
+        mount_point: &Path,
+        glob_patterns: Vec<String>,
+    ) -> Result<Glob> {
+        let glob_list = glob_patterns.join(", ");
+        tracing::trace!("resolving globs ({}) in foreground", &glob_list);
+        self.glob_files_optional(
+            mount_point,
+            glob_patterns,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None::<PathBuf>,
+            Some(false),
+            None,
+            None,
+            None,
+        )
+        .await
     }
 }
