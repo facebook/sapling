52a62e17141c4e44c453c5a041925b7c9d053816
Youssef Ibrahim <youssefsalama@meta.com>
2025-03-31 09:57:03 -0700

    scm_blob: add methods to calculate sha1 and blake3 from an ScmBlob
    Summary: Add `ScmBlob` methods to calculate sha1 and blake3 hashes. The hashes in the `IOBufShared` case need to match the `Bytes` hash if the `IOBuf` chain was coalesced
    
    Reviewed By: liubov-dmitrieva
    
    Differential Revision: D71891601
    
    fbshipit-source-id: 44ed247c2d1d28193f8818fd1732400cda1df21e

--- eden/scm/lib/blob/src/lib.rs
+++ eden/scm/lib/blob/src/lib.rs
@@ -5,6 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use bytes::Buf;
+use types::Blake3;
+use types::Sha1;
+
 #[derive(Clone, Debug)]
 pub enum ScmBlob {
     Bytes(minibytes::Bytes),
@@ -43,5 +47,80 @@ impl ScmBlob {
             #[cfg(fbcode_build)]
             Self::IOBuf(buf) => buf.is_empty(),
         }
+    }
+
+    pub fn sha1(&self) -> types::Sha1 {
+        use sha1::Digest;
+
+        let mut hasher = sha1::Sha1::new();
+
+        match self {
+            Self::Bytes(bytes) => {
+                hasher.update(bytes);
+            }
+            #[cfg(fbcode_build)]
+            Self::IOBuf(buf) => {
+                let mut cur = buf.clone().cursor();
+
+                while cur.has_remaining() {
+                    let b = cur.chunk();
+                    hasher.update(b);
+                    cur.advance(b.len());
+                }
+            }
+        }
+
+        let bytes: [u8; Sha1::len()] = hasher.finalize().into();
+        Sha1::from(bytes)
+    }
+
+    pub fn blake3(&self) -> types::Blake3 {
+        use blake3::Hasher;
+
+        #[cfg(fbcode_build)]
+        let key = blake3_constants::BLAKE3_HASH_KEY;
+        #[cfg(not(fbcode_build))]
+        let key = b"20220728-2357111317192329313741#";
+
+        let mut hasher = Hasher::new_keyed(key);
+
+        match self {
+            Self::Bytes(bytes) => {
+                hasher.update(bytes);
+            }
+            #[cfg(fbcode_build)]
+            Self::IOBuf(buf) => {
+                let mut cur = buf.clone().cursor();
+
+                while cur.has_remaining() {
+                    let b = cur.chunk();
+                    hasher.update(b);
+                    cur.advance(b.len());
+                }
+            }
+        }
+
+        let hashed_bytes: [u8; Blake3::len()] = hasher.finalize().into();
+        Blake3::from(hashed_bytes)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[cfg(fbcode_build)]
+    #[test]
+    fn test_iobuf_sha1_and_blake3() {
+        let blob1 = ScmBlob::Bytes(minibytes::Bytes::from("hello world!"));
+
+        let blob2 = {
+            let mut iobuf = iobuf::IOBufShared::from("hello");
+            iobuf.append_to_end(iobuf::IOBufShared::from(" world!"));
+            ScmBlob::IOBuf(iobuf)
+        };
+
+        assert_eq!(blob1.sha1(), blob2.sha1());
+        assert_eq!(blob1.blake3(), blob2.blake3());
     }
 }
