889316638cdac937d7be815867fe9f5a1feb53c1
John Elliott <jdelliot@meta.com>
2025-04-14 17:30:38 -0700

    Add additional mocked tests for sapling-client::commit mod
    Summary:
    # Context:
    
    We are working to refactor the EdenFs CLI client library into a supported XFN consumable library to avoid code duplication and sub-optimal usage patterns.
    
    # This Diff:
    
    Adds more mocked tests for commit mod. This moves us away from running potentially unreliable unit tests in CI.
    
    # Facebook
    
    Leveraging the internal mocking framework for Commands: https://www.internalfb.com/code/fbsource/[e06cfadfd5c60f5aa51ff7e8af93837542d632d8]/fbcode/common/rust/async_process_traits/README.md.
    
    Differential Revision: D72998197
    
    fbshipit-source-id: 99a73fd0e5e7db3c9ac42d0fe243d2d9a27117af

--- eden/fs/cli_rs/sapling-client/src/commit.rs
+++ eden/fs/cli_rs/sapling-client/src/commit.rs
@@ -7,6 +7,7 @@
 
 use async_process_traits::Command;
 use async_process_traits::CommandSpawner;
+use async_process_traits::ExitStatus;
 use async_process_traits::Output;
 use async_process_traits::TokioCommandSpawner;
 
@@ -32,12 +33,25 @@ where
 }
 
 pub async fn get_commit_timestamp(commit_id: &str) -> Result<u64> {
-    let output = tokio::process::Command::new(get_sapling_executable_path())
-        .envs(get_sapling_options())
-        .args(["log", "--traceback", "-T", "{date}", "-r", commit_id])
-        .output()
-        .await?;
-    let date = String::from_utf8(output.stdout)?;
+    get_commit_timestamp_impl(&TokioCommandSpawner, commit_id).await
+}
+
+async fn get_commit_timestamp_impl<Spawner>(spawner: &Spawner, commit_id: &str) -> Result<u64>
+where
+    Spawner: CommandSpawner,
+{
+    let mut command = Spawner::Command::new(get_sapling_executable_path());
+    command.envs(get_sapling_options()).args([
+        "log",
+        "--traceback",
+        "-T",
+        "{date}",
+        "-r",
+        commit_id,
+    ]);
+
+    let output = spawner.output(&mut command).await?;
+    let date = String::from_utf8(output.stdout().to_vec())?;
     let date = date.parse::<f64>()?;
     // NOTE: Sapling returns fractional seconds, but we only want whole seconds.
     //       Truncate to the nearest second.
@@ -45,58 +59,53 @@ pub async fn get_commit_timestamp(commit_id: &str) -> Result<u64> {
 }
 
 pub async fn is_commit_in_repo(commit_id: &str) -> Result<bool> {
-    let output = tokio::process::Command::new(get_sapling_executable_path())
-        .envs(get_sapling_options())
-        .args([
-            "log",
-            "--traceback",
-            "-r",
-            commit_id,
-            // Disable fbsource <-> www sync during lookup
-            "--config",
-            "megarepo.transparent-lookup=",
-        ])
-        .output()
-        .await?;
-    Ok(output.status.success())
+    is_commit_in_repo_impl(&TokioCommandSpawner, commit_id).await
+}
+
+async fn is_commit_in_repo_impl<Spawner>(spawner: &Spawner, commit_id: &str) -> Result<bool>
+where
+    Spawner: CommandSpawner,
+{
+    let mut command = Spawner::Command::new(get_sapling_executable_path());
+    command.envs(get_sapling_options()).args([
+        "log",
+        "--traceback",
+        "-r",
+        commit_id,
+        // Disable fbsource <-> www sync during lookup
+        "--config",
+        "megarepo.transparent-lookup=",
+    ]);
+
+    let output = spawner.output(&mut command).await?;
+    Ok(output.status().success())
 }
 
 #[cfg(test)]
 mod tests {
-    use edenfs_client::utils::get_mount_point;
-
     use crate::commit::*;
     use crate::utils::tests::get_mock_spawner;
-    use crate::utils::*;
 
-    const FBSOURCE_COMMIT_ID: &str = "5496dd87e5fe7430a1a399530cc339a479097524";
-    const WWW_COMMIT_ID: &str = "1061662d6db2072dd30308d1626a45ac11db3467";
+    const COMMIT_ID: &str = "0000111122223333444455556666777788889999";
 
     #[tokio::test]
-    pub async fn test_current_commit_id() -> Result<()> {
-        let hash = "0000111122223333444455556666777788889999".to_owned();
+    pub async fn test_get_current_commit_id() -> Result<()> {
         let spawner = get_mock_spawner(
             get_sapling_executable_path(),
-            Some((0, Some(hash.as_bytes().to_vec()))),
+            Some((0, Some(COMMIT_ID.as_bytes().to_vec()))),
         );
         let commit_id = get_current_commit_id_impl(&spawner).await?;
-        assert_eq!(commit_id, hash);
+        assert_eq!(commit_id, COMMIT_ID);
         Ok(())
     }
 
     #[tokio::test]
     pub async fn test_is_commit_in_repo() -> Result<()> {
-        let mount_point = get_mount_point(&None)?;
-        let commit_id = get_current_commit_id().await?;
-        assert!(is_commit_in_repo(&commit_id).await?);
-        assert_eq!(
-            is_commit_in_repo(FBSOURCE_COMMIT_ID).await?,
-            is_fbsource_checkout(&mount_point)
-        );
-        assert_eq!(
-            is_commit_in_repo(WWW_COMMIT_ID).await?,
-            !is_fbsource_checkout(&mount_point)
-        );
+        let spawner = get_mock_spawner(get_sapling_executable_path(), Some((0, None)));
+        assert!(is_commit_in_repo_impl(&spawner, COMMIT_ID).await?);
+
+        let spawner = get_mock_spawner(get_sapling_executable_path(), Some((255, None)));
+        assert!(!is_commit_in_repo_impl(&spawner, COMMIT_ID).await?,);
 
         Ok(())
     }
@@ -109,7 +118,12 @@ mod tests {
         //   date:        Tue, 28 Jan 2025 10:35:17 -0800
         //   summary:     [meerkat] Improve saved state support
         // timestamp should be 1738089317.028800, but we truncate to the nearest second
-        let timestamp = get_commit_timestamp(FBSOURCE_COMMIT_ID).await?;
+        let timestamp_str = "1738089317.028800".to_owned();
+        let spawner = get_mock_spawner(
+            get_sapling_executable_path(),
+            Some((0, Some(timestamp_str.as_bytes().to_vec()))),
+        );
+        let timestamp = get_commit_timestamp_impl(&spawner, COMMIT_ID).await?;
         assert_eq!(timestamp, 1738089317);
         Ok(())
     }
