acdf55694f62616b1706807e8862177c6e4b2c7f
John Elliott <jdelliot@meta.com>
2025-04-23 13:22:45 -0700

    Refactor connector code into single, flat mod
    Summary:
    # Context:
    
    We are working to refactor the EdenFs CLI client library into a supported XFN consumable library to avoid code duplication and sub-optimal usage patterns.
    
    # This Diff:
    
    With the simplified service usage, we can now dispense with two connectors and fold the one into single file.
    
    # Next Steps:
    
    * We will also add support for mocking streaming client and validate that can be done in the same manner.
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D73465320
    
    fbshipit-source-id: 59ac8d77be18f599ac424fbef200783e40d1fa83

--- eden/fs/cli_rs/edenfs-client/src/client/connector.rs
+++ eden/fs/cli_rs/edenfs-client/src/client/connector.rs
@@ -5,15 +5,22 @@
  * GNU General Public License version 2.
  */
 
-mod connector;
-mod streaming_connector;
-
 use std::path::PathBuf;
+use std::sync::Arc;
 use std::time::Duration;
 
+use anyhow::Context;
 use edenfs_error::ConnectError;
 use fbinit::FacebookInit;
-pub(crate) use streaming_connector::*;
+use futures::future::BoxFuture;
+use futures::future::FutureExt;
+use futures::future::Shared;
+use thrift_streaming_clients::StreamingEdenService;
+use thrift_streaming_thriftclients::make_StreamingEdenServiceExt_thriftclient;
+
+pub type StreamingEdenFsThriftClient = Arc<dyn StreamingEdenService + Send + Sync + 'static>;
+pub type StreamingEdenFsThriftClientFuture =
+    Shared<BoxFuture<'static, std::result::Result<StreamingEdenFsThriftClient, ConnectError>>>;
 
 // TODO: select better defaults (e.g. 1s connection timeout, 1m recv timeout)
 const DEFAULT_CONN_TIMEOUT: Duration = Duration::from_secs(120);
@@ -24,11 +31,6 @@ const DEFAULT_RECV_TIMEOUT: Duration = Duration::from_secs(300);
 /// Connectors are responsible for creating and managing connections to the EdenFS service.
 /// They abstract away the details of how to connect to EdenFS, allowing the client to focus
 /// on making requests.
-///
-/// There are two main implementations of this trait:
-/// - `EdenFsConnector`: Creates standard Thrift clients for regular operations
-/// - `StreamingEdenFsConnector`: Creates streaming Thrift clients for operations that
-///   return large amounts of data or need to stream results incrementally
 ///
 /// # Type Parameters
 ///
@@ -78,4 +80,56 @@ pub trait Connector: Send + Sync {
         conn_timeout: Option<Duration>,
         recv_timeout: Option<Duration>,
     ) -> Self::ClientFuture;
+}
+
+pub struct StreamingEdenFsConnector {
+    fb: FacebookInit,
+    socket_file: PathBuf,
+}
+
+impl Connector for StreamingEdenFsConnector {
+    type Client = StreamingEdenFsThriftClient;
+    type ClientFuture = StreamingEdenFsThriftClientFuture;
+
+    fn new(fb: FacebookInit, socket_file: PathBuf) -> Self {
+        Self { fb, socket_file }
+    }
+
+    fn connect(
+        &self,
+        conn_timeout: Option<Duration>,
+        recv_timeout: Option<Duration>,
+    ) -> Self::ClientFuture {
+        let socket_file = self.socket_file.clone();
+        let fb = self.fb;
+
+        tokio::task::spawn(async move {
+            tracing::info!(
+                "Creating a new EdenFS streaming connection via `{}`",
+                socket_file.display()
+            );
+
+            // get future for the connection
+            let client: StreamingEdenFsThriftClient = make_StreamingEdenServiceExt_thriftclient!(
+                fb,
+                protocol = CompactProtocol,
+                from_path = &socket_file,
+                with_conn_timeout =
+                    conn_timeout.map_or(DEFAULT_CONN_TIMEOUT, |t| t).as_millis() as u32,
+                with_recv_timeout =
+                    recv_timeout.map_or(DEFAULT_RECV_TIMEOUT, |t| t).as_millis() as u32,
+                with_secure = false,
+            )
+            .with_context(|| "Unable to create an EdenFS streaming thrift client")
+            .map_err(|e| ConnectError::ConnectionError(e.to_string()))?;
+
+            Ok(client)
+        })
+        .map(|r| match r {
+            Ok(r) => r,
+            Err(e) => Err(ConnectError::ConnectionError(e.to_string())),
+        })
+        .boxed()
+        .shared()
+    }
 }
