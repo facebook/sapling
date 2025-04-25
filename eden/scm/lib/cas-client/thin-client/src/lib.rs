e528c7df54cda9b26c71537546f63854c503c39c
Youssef Ibrahim <youssefsalama@meta.com>
2025-04-25 09:38:22 -0700

    Back out "cas_client: use bare thrift client in ThinCasClient"
    Summary: This broke tests. Backout while I debug it.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D73663866
    
    fbshipit-source-id: 57664a060ee60515155703451d2fb233aa15dca9

--- eden/scm/lib/cas-client/thin-client/src/lib.rs
+++ eden/scm/lib/cas-client/thin-client/src/lib.rs
@@ -163,8 +163,6 @@ impl ThinCasClient {
                     cas_daemon_address: ExternalCASDaemonAddress::port(port),
                     address: server_address,
                     connection_count: self.connection_count as i32,
-                    use_bare_thrift_client: true,
-                    use_tls: false,
                     ..Default::default()
                 });
         } else if let Some(uds_path) = self.uds_path.clone() {
@@ -174,8 +172,6 @@ impl ThinCasClient {
                     cas_daemon_address: ExternalCASDaemonAddress::uds_path(uds_path.clone()),
                     address: server_address,
                     connection_count: self.connection_count as i32,
-                    use_bare_thrift_client: true,
-                    use_tls: false,
                     ..Default::default()
                 });
         } else {
@@ -185,8 +181,7 @@ impl ThinCasClient {
                 address: None,
                 connection_count: self.connection_count as i32,
                 cas_daemon_address: ExternalCASDaemonAddress::uds_path(socket_path),
-                use_bare_thrift_client: true,
-                use_tls: false,
+                socket_activation: true,
                 ..Default::default()
             };
             match &mut re_config.cas_client_config {
