b0eb25bd8d0361cb324c768f6bcf2f7a6d78231b
George Giorgidze <giorgidze@meta.com>
2025-04-25 13:53:32 -0700

    Create a directory branch of fscatalog so that we can implement and test performance improvements to file system overlay safely without impacting the production version
    Summary:
    Subtree copy from 05bca12d4f339fe7e1212b05724540791b4d12cb
    - Copied path fbcode/eden/fs/inodes/fscatalog to fbcode/eden/fs/inodes/fscatalog_dev
    
    For now just create a directory branch and made it build with buck.
    
    For now this implementation of fscatalog is not used anywhere.
    
    Subsequent diffs will implement a config for switching to this implementation.
    
    Reviewed By: jdelliot
    
    Differential Revision: D73560991
    
    fbshipit-source-id: 65f6cad7d20c5c3a85c9947467b4e2417a1fb5e5

---
+++ eden/fs/inodes/fscatalog_dev/eden_fsck.cpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include <sysexits.h>
+#include <optional>
+
+#include <folly/Exception.h>
+#include <folly/init/Init.h>
+#include <folly/logging/Init.h>
+#include <folly/logging/LogConfigParser.h>
+#include <folly/logging/xlog.h>
+#include <gflags/gflags.h>
+
+#include "eden/fs/inodes/fscatalog_dev/FsInodeCatalog.h"
+#include "eden/fs/inodes/overlay/OverlayChecker.h"
+
+DEFINE_bool(
+    dry_run,
+    false,
+    "Only report errors, without attempting to fix any problems");
+
+DEFINE_bool(
+    force,
+    false,
+    "Force fsck to scan for errors even on checkouts that appear to currently be mounted.  It will not attempt to fix any problems, but will only scan and report possible issues");
+
+DEFINE_int64(
+    num_error_discovery_threads,
+    4,
+    "Number of threads to use for discovering errors in the overlay");
+
+using namespace facebook::eden;
+
+int main(int argc, char** argv) {
+  folly::init(&argc, &argv);
+  if (argc != 2) {
+    fprintf(stderr, "error: no overlay path provided\n");
+    fprintf(stderr, "usage: eden_fsck PATH [ARGS]\n");
+    return EX_USAGE;
+  }
+
+  auto loggingConfig = folly::parseLogConfig("eden=DBG2; default:async=true");
+  folly::LoggerDB::get().updateConfig(loggingConfig);
+
+  std::optional<FsFileContentStore> fileContentStore;
+  std::optional<FsInodeCatalog> fsInodeCatalog;
+  std::optional<InodeNumber> nextInodeNumber;
+  auto overlayPath = normalizeBestEffort(argv[1]);
+  try {
+    fileContentStore.emplace(overlayPath);
+    fsInodeCatalog.emplace(&fileContentStore.value());
+    nextInodeNumber = fsInodeCatalog->initOverlay(
+        /*createIfNonExisting=*/false, /*bypassLockFile=*/FLAGS_force);
+  } catch (std::exception& ex) {
+    XLOG(ERR) << "unable to open overlay: " << folly::exceptionStr(ex);
+    return 1;
+  }
+
+  if (!nextInodeNumber.has_value()) {
+    XLOG(INFO) << "Overlay was shut down uncleanly";
+  }
+
+  InodeCatalog::LookupCallback lookup = [](auto&&, auto&&) {
+    return makeImmediateFuture<InodeCatalog::LookupCallbackValue>(
+        std::runtime_error("no lookup callback"));
+  };
+  OverlayChecker checker(
+      &fsInodeCatalog.value(),
+      &fileContentStore.value(),
+      nextInodeNumber,
+      lookup,
+      FLAGS_num_error_discovery_threads);
+  checker.scanForErrors();
+  if (FLAGS_dry_run || FLAGS_force) {
+    checker.logErrors();
+    fsInodeCatalog->close(nextInodeNumber);
+  } else {
+    checker.repairErrors();
+    fsInodeCatalog->close(checker.getNextInodeNumber());
+  }
+  return 0;
+}
