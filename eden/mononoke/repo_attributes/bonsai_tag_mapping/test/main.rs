5dd423bccdd65e51512770efa3432d7affbb3442
Rajiv Sharma <rajshar@meta.com>
2025-04-29 14:14:02 -0700

    Enable finer JK control for bonsai tag mapping caching
    Summary: In S514675, we realized that turning off scribe-based update of our caches required not only JK change but also a restart of the servers. This isn't ideal because its disruptive to the service and more importantly it adds a considerable amount of delay in addressing the problem. With this diff, we enable immediate rollback of similar changes for bonsai tag mapping caching just through JK change, although at this level it cannot be configured per-repo but instead applies to the feature as a whole.
    
    Reviewed By: YousefSalama
    
    Differential Revision: D73862867
    
    fbshipit-source-id: f41cd70501fdecf15ff44f2e679871329583924b

--- eden/mononoke/repo_attributes/bonsai_tag_mapping/test/main.rs
+++ eden/mononoke/repo_attributes/bonsai_tag_mapping/test/main.rs
@@ -18,6 +18,11 @@ use bonsai_tag_mapping::Freshness;
 use bonsai_tag_mapping::SqlBonsaiTagMappingBuilder;
 use context::CoreContext;
 use fbinit::FacebookInit;
+use futures::FutureExt;
+use justknobs::test_helpers::JustKnobsInMemory;
+use justknobs::test_helpers::KnobVal;
+use justknobs::test_helpers::with_just_knobs_async;
+use maplit::hashmap;
 use mononoke_macros::mononoke;
 use mononoke_types::hash::GitSha1;
 use mononoke_types_mocks::changesetid as bonsai;
@@ -289,64 +294,75 @@ async fn test_delete_mappings_by_name(_: FacebookInit) -> Result<(), Error> {
 
 #[mononoke::fbinit_test]
 async fn test_cached_bonsai_tag_mappings(fb: FacebookInit) -> Result<(), Error> {
-    let mapping = Arc::new(SqlBonsaiTagMappingBuilder::with_sqlite_in_memory()?.build(REPO_ZERO));
-    let ctx = CoreContext::test_mock(fb);
-    let (sender, receiver) = broadcast::channel(10);
-    let mapping = CachedBonsaiTagMapping::new(mapping, receiver, ctx.logger().clone()).await?;
-    // Add a few mappings
-    let entry = BonsaiTagMappingEntry {
-        changeset_id: bonsai::ONES_CSID,
-        tag_name: "JustATag".to_string(),
-        tag_hash: GitSha1::from_str(ZERO_GIT_HASH)?,
-        target_is_tag: false,
-    };
-    let another_entry = BonsaiTagMappingEntry {
-        changeset_id: bonsai::TWOS_CSID,
-        tag_name: "AnotherTag".to_string(),
-        tag_hash: GitSha1::from_str(ONE_GIT_HASH)?,
-        target_is_tag: true,
-    };
-    mapping
-        .add_or_update_mappings(vec![entry, another_entry])
-        .await?;
-    let received_by = sender.send(PlainBookmarkInfo::default())?;
-    assert_eq!(
-        received_by, 1,
-        "The number of receivers for tag update does not match the expected count"
-    );
-    sleep(Duration::from_secs(3)).await;
-    // Ensure that the cache got updated with the latest value
-    let result = mapping
-        .get_all_entries()
-        .await?
-        .into_iter()
-        .map(|entry| entry.tag_name);
-    assert_eq!(
-        HashSet::from_iter(result),
-        HashSet::from(["JustATag".to_string(), "AnotherTag".to_string()])
-    );
-    // Update the tag hash and try storing the same entry. Validate that the cache catches up with that change
-    let new_entry = BonsaiTagMappingEntry {
-        changeset_id: bonsai::ONES_CSID,
-        tag_name: "JustATag".to_string(),
-        tag_hash: GitSha1::from_str(ONE_GIT_HASH)?,
-        target_is_tag: true,
-    };
-    mapping
-        .add_or_update_mappings(vec![new_entry.clone()])
-        .await?;
-    sender.send(PlainBookmarkInfo::default())?;
-    sleep(Duration::from_secs(3)).await;
-    let result = mapping
-        .get_entry_by_tag_name(new_entry.tag_name.clone(), Freshness::MaybeStale) // MaybeStale so that the cache is used
-        .await?;
-    assert_eq!(result, Some(new_entry.clone()));
-    // Remove all entries and validate that the cache catches up with that change
-    mapping
-        .delete_mappings_by_name(vec!["JustATag".to_string(), "AnotherTag".to_string()])
-        .await?;
-    sender.send(PlainBookmarkInfo::default())?;
-    sleep(Duration::from_secs(3)).await;
-    assert!(mapping.get_all_entries().await?.is_empty());
-    Ok(())
+    with_just_knobs_async(
+        JustKnobsInMemory::new(hashmap![
+            "scm/mononoke:enable_bonsai_tag_mapping_caching".to_string() => KnobVal::Bool(true),
+        ]),
+        async {
+            let mapping =
+                Arc::new(SqlBonsaiTagMappingBuilder::with_sqlite_in_memory()?.build(REPO_ZERO));
+            let ctx = CoreContext::test_mock(fb);
+            let (sender, receiver) = broadcast::channel(10);
+            let mapping =
+                CachedBonsaiTagMapping::new(mapping, receiver, ctx.logger().clone()).await?;
+            // Add a few mappings
+            let entry = BonsaiTagMappingEntry {
+                changeset_id: bonsai::ONES_CSID,
+                tag_name: "JustATag".to_string(),
+                tag_hash: GitSha1::from_str(ZERO_GIT_HASH)?,
+                target_is_tag: false,
+            };
+            let another_entry = BonsaiTagMappingEntry {
+                changeset_id: bonsai::TWOS_CSID,
+                tag_name: "AnotherTag".to_string(),
+                tag_hash: GitSha1::from_str(ONE_GIT_HASH)?,
+                target_is_tag: true,
+            };
+            mapping
+                .add_or_update_mappings(vec![entry, another_entry])
+                .await?;
+            let received_by = sender.send(PlainBookmarkInfo::default())?;
+            assert_eq!(
+                received_by, 1,
+                "The number of receivers for tag update does not match the expected count"
+            );
+            sleep(Duration::from_secs(3)).await;
+            // Ensure that the cache got updated with the latest value
+            let result = mapping
+                .get_all_entries()
+                .await?
+                .into_iter()
+                .map(|entry| entry.tag_name);
+            assert_eq!(
+                HashSet::from_iter(result),
+                HashSet::from(["JustATag".to_string(), "AnotherTag".to_string()])
+            );
+            // Update the tag hash and try storing the same entry. Validate that the cache catches up with that change
+            let new_entry = BonsaiTagMappingEntry {
+                changeset_id: bonsai::ONES_CSID,
+                tag_name: "JustATag".to_string(),
+                tag_hash: GitSha1::from_str(ONE_GIT_HASH)?,
+                target_is_tag: true,
+            };
+            mapping
+                .add_or_update_mappings(vec![new_entry.clone()])
+                .await?;
+            sender.send(PlainBookmarkInfo::default())?;
+            sleep(Duration::from_secs(3)).await;
+            let result = mapping
+                .get_entry_by_tag_name(new_entry.tag_name.clone(), Freshness::MaybeStale) // MaybeStale so that the cache is used
+                .await?;
+            assert_eq!(result, Some(new_entry.clone()));
+            // Remove all entries and validate that the cache catches up with that change
+            mapping
+                .delete_mappings_by_name(vec!["JustATag".to_string(), "AnotherTag".to_string()])
+                .await?;
+            sender.send(PlainBookmarkInfo::default())?;
+            sleep(Duration::from_secs(3)).await;
+            assert!(mapping.get_all_entries().await?.is_empty());
+            Ok(())
+        }
+        .boxed(),
+    )
+    .await
 }
