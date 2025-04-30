e27ea63b9d0215230ba40061fac7ea4d0b48d53d
Liubov Dmitrieva <liubovd@meta.com>
2025-04-30 03:42:41 -0700

    fix blocking issue within async API
    Summary:
    fix blocking issue within async API
    to use the blocking methods, we would need for the fetch itself to be also blocking
    
    Reviewed By: YousefSalama
    
    Differential Revision: D73853020
    
    fbshipit-source-id: b417f7a9afb5537a9de7bc0f46eb313607f3d8d9

--- eden/scm/lib/cas-client/re-cas-common/src/lib.rs
+++ eden/scm/lib/cas-client/re-cas-common/src/lib.rs
@@ -229,7 +229,8 @@ macro_rules! re_client {
                         #[cfg(target_os = "linux")]
                         let (data, stats) = {
                             let response = self.client()?
-                                .low_level_download_inline(self.metadata.clone(), digests.iter().map(to_re_digest).collect());
+                                .co_low_level_download_inline(self.metadata.clone(), digests.iter().map(to_re_digest).collect()).await;
+
                             if let Err(ref err) = response {
                                 if (err.code == TCode::NOT_FOUND) {
                                     $crate::tracing::warn!(target: "cas", "digest not found and can not be fetched: {:?}", digests);
@@ -331,7 +332,8 @@ macro_rules! re_client {
 
                     #[cfg(target_os = "linux")]
                     let response = self.client()?
-                        .download_digests_into_cache(self.metadata.clone(), digests.into_iter().map(to_re_digest).collect())
+                        .co_download_digests_into_cache(self.metadata.clone(), digests.into_iter().map(to_re_digest).collect())
+                        .await
                         .map_err(|err| {
                             // Unfortunately, the "download_digests_into_cache" failed entirely, record a failure.
                             let _failure_error = self.cas_success_tracker.record_failure();
@@ -347,7 +349,8 @@ macro_rules! re_client {
                         };
                         self.client()?
                         .download_digests_into_cache(self.metadata.clone(), request)
-                        .await.map_err(|err| {
+                        .await
+                        .map_err(|err| {
                             // Unfortunately, the "download_digests_into_cache" failed entirely, record a failure.
                             let _failure_error = self.cas_success_tracker.record_failure();
                             err
