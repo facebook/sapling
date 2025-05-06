575550f48b71b4bcf4f2c356978b0467e43e3ca5
Jun Wu <quark@meta.com>
2025-05-06 10:02:32 -0700

    ministate: add macros
    Summary:
    Make the APIs similar to Jotai.
    
    Primitive atom:
    
      // Jotai
      const countAtom = atom(0);
      store.set(countAtom, 1);
    
      // ministate
      atom!(CountAtom, u32, 0);
      store.set::<CountAtom>(1);
    
    Derived atom:
    
      // Jotai
      const doubledCountAtom = atom((get) => get(countAtom) * 2);
      const value = store.get(doubledCountAtom);
    
      // ministate
      atom!(DoubleCountAtom, u32, |store| Ok(Arc::new(store.get::<CountAtom>()? * 2)));
      let value = store.get::<doubledCountAtom>()?;
    
    Reviewed By: zzl0
    
    Differential Revision: D73796925
    
    fbshipit-source-id: 3bb19a04e97c5cb6394221f33b388eb421bbfbed

--- eden/scm/lib/ministate/src/lib.rs
+++ eden/scm/lib/ministate/src/lib.rs
@@ -9,8 +9,44 @@
 //!
 //! Inspired by [Jotai](https://jotai.org/). Note this library only implements
 //! a small subset of Jotai features.
+//!
+//! Primitive atom:
+//!
+//! ```typescript,ignore
+//! // Jotai
+//! const countAtom = atom(0);
+//! store.set(countAtom, 1);
+//! ```
+//!
+//! ```ignore
+//! # use std::sync::Arc;
+//! # use ministate::atom;
+//! // ministate
+//! atom!(CountAtom, u32, 0);
+//! store.set::<CountAtom>(1);
+//! ```
+//!
+//! Derived atom:
+//!
+//! ```typescript,ignore
+//! // Jotai
+//! const doubledCountAtom = atom((get) => get(countAtom) * 2);
+//! const value = store.get(doubledCountAtom);
+//! ```
+//!
+//! ```ignore
+//! # use std::sync::Arc;
+//! # use ministate::atom;
+//! # atom!(CountAtom, u32, 1);
+//! // ministate
+//! atom!(DoubledCountAtom, u32, |store| Ok(Arc::new(
+//!     *store.get::<CountAtom>()? * 2
+//! )));
+//! let value = store.get::<DoubledCountAtom>().unwrap();
+//! ```
 
 mod atom;
+mod macros;
 
 pub use anyhow::Result;
 pub use atom::Atom;
