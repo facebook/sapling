ef4ffb49122f3cf0cb373bab8ce842ba0069096d
Le Xu <lexu@meta.com>
2025-05-07 16:31:09 -0700

    Allow specify a prompt during splitting
    Summary: Add a dropdown for user to provide prompt while splitting
    
    Reviewed By: evangrayk
    
    Differential Revision: D74327669
    
    fbshipit-source-id: 883541699c40c3cbcfe06b3b5c2d8ce0f8d74474

---
+++ addons/components/ButtonWithDropdownTooltip.tsx
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {type ReactNode} from 'react';
+
+import * as stylex from '@stylexjs/stylex';
+import {Button} from './Button';
+import {Icon} from './Icon';
+import {colors, spacing} from './theme/tokens.stylex';
+import {Tooltip} from './Tooltip';
+
+const styles = stylex.create({
+  container: {
+    display: 'flex',
+    alignItems: 'stretch',
+    position: 'relative',
+  },
+  button: {
+    borderBottomRightRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  chevron: {
+    borderBottomLeftRadius: 0,
+    borderTopLeftRadius: 0,
+    borderLeft: 'unset',
+    width: '24px',
+    height: '24px',
+    paddingTop: 6,
+  },
+  builtinButtonBorder: {
+    borderLeft: 'unset',
+  },
+  iconButton: {
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    paddingRight: spacing.half,
+  },
+  iconSelect: {
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    borderLeftColor: colors.hoverDarken,
+  },
+  chevronDisabled: {
+    opacity: 0.5,
+  },
+});
+
+export function ButtonWithDropdownTooltip({
+  label,
+  kind,
+  onClick,
+  disabled,
+  icon,
+  tooltip,
+  ...rest
+}: {
+  label: ReactNode;
+  kind?: 'primary' | 'icon' | undefined;
+  onClick: () => unknown;
+  disabled?: boolean;
+  icon?: React.ReactNode;
+  tooltip: React.ReactNode;
+  'data-testId'?: string;
+}) {
+  return (
+    <div {...stylex.props(styles.container)}>
+      <Button
+        kind={kind}
+        onClick={disabled ? undefined : () => onClick()}
+        disabled={disabled}
+        xstyle={[styles.button, kind === 'icon' && styles.iconButton]}
+        {...rest}>
+        {icon ?? null} {label}
+      </Button>
+      <Tooltip
+        trigger="click"
+        component={_dismiss => <div>{tooltip}</div>}
+        group="topbar"
+        placement="bottom">
+        <Button
+          kind={kind}
+          onClick={undefined}
+          disabled={disabled}
+          xstyle={[styles.chevron]}
+          {...rest}>
+          <Icon
+            icon="chevron-down"
+            {...stylex.props(styles.chevron, disabled && styles.chevronDisabled)}
+          />
+        </Button>
+      </Tooltip>
+    </div>
+  );
+}
