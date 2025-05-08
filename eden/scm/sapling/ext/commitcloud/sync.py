43e836be78ac89d8aaa0cac47ecdd1bbb7753d27
Jun Wu <quark@meta.com>
2025-05-08 10:51:49 -0700

    commitcloud: use native config.get to obtain matcher
    Summary: This part of logic is now native.
    
    Reviewed By: muirdm
    
    Differential Revision: D74196061
    
    fbshipit-source-id: dd218e33b2abbb413966a9985eb27acc26c7e94c

--- eden/scm/sapling/ext/commitcloud/sync.py
+++ eden/scm/sapling/ext/commitcloud/sync.py
@@ -67,9 +67,8 @@ def _getheads(repo):
 
 
 def _getbookmarks(repo):
-    ignored = repo.ui.configlist("commitcloud", "ignored-bookmarks")
-    if ignored:
-        matcher = bindings.pathmatcher.treematcher(ignored, False)
+    matcher = repo.config.get.as_matcher("commitcloud", "ignored-bookmarks")
+    if matcher is not None:
         is_ignored = matcher.matches
     else:
 
