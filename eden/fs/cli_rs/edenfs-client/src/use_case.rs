f70010e9a8f728406f5f1413592702afa36a73dd
John Elliott <jdelliot@meta.com>
2025-05-16 04:54:19 -0700

    Implement simplified helper methods for setting up http_client
    Summary:
    # This Diff
    
    Implements some simplified helper methods - simplified in that they do not require Domain (Sapling construct) to use them - for setting up http_client.
    
    # Next Steps
    
    Continue to refactor the bits that are purely associated with fetching configurations from configerator. This will involve removing dependencies on Generater, ConfigSet and other Sapling specific types.
    
    # Context
    
    We would like to be able to know which partner is using our client library and collect metrics. We also want to be able to associate various configuration elements to a use case. To this end we are introducing a use-case-id, use-case configuration, and telemetry collection for a use-case.
    
    Reviewed By: andreacampi
    
    Differential Revision: D74803773
    
    fbshipit-source-id: af6c17435fd01b94b349b6fb057747803179a009

--- eden/fs/cli_rs/edenfs-client/src/use_case.rs
+++ eden/fs/cli_rs/edenfs-client/src/use_case.rs
@@ -10,6 +10,10 @@
 // on the size/complexity/cost of the outstanding requests.
 const DEFAULT_MAX_OUTSTANDING_REQUESTS: usize = 2048;
 
+use std::collections::HashSet;
+
+use anyhow::Result;
+use anyhow::bail;
 use strum::IntoStaticStr;
 
 #[derive(Clone, Copy, Debug, Eq, IntoStaticStr, Hash, PartialEq)]
@@ -64,4 +68,35 @@ impl UseCase {
     pub fn max_outstanding_requests(&self) -> usize {
         self.max_outstanding_requests
     }
+}
+
+#[allow(dead_code)]
+fn config_url(is_pub: bool) -> String {
+    format!("https://{}/hg/config/", interngraph_host(is_pub))
+}
+
+#[allow(dead_code)]
+fn interngraph_host(is_pub: bool) -> &'static str {
+    if is_pub {
+        "interngraph.internmc.facebook.com"
+    } else {
+        "interngraph.intern.facebook.com"
+    }
+}
+
+#[allow(dead_code)]
+fn get_http_config(is_pub: bool, proxy_sock_path: Option<&str>) -> Result<http_client::Config> {
+    let mut http_config = http_client::Config::default();
+    if is_pub {
+        let proxy_sock = match proxy_sock_path {
+            Some(path) => path.to_string(),
+            None => bail!("no proxy_sock_path when fetching remote config in pub domain"),
+        };
+
+        let intern_host = interngraph_host(is_pub);
+
+        http_config.unix_socket_path = Some(proxy_sock);
+        http_config.unix_socket_domains = HashSet::from([intern_host.to_string()]);
+    }
+    Ok(http_config)
 }
