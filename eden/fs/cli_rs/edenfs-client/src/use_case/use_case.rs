97c5efc8a1c8a874e3745499aa7168078577c002
John Elliott <jdelliot@meta.com>
2025-05-16 04:54:19 -0700

    Refactor use_case code + thrift types into mods
    Summary:
    # This Diff
    
    Refactor use_case code + thrift types into mods.
    
    # Next Steps
    
    Continue to refactor the bits that are purely associated with fetching configurations from configerator. This will involve removing dependencies on Generater, ConfigSet and other Sapling specific types.
    
    # Context
    
    We would like to be able to know which partner is using our client library and collect metrics. We also want to be able to associate various configuration elements to a use case. To this end we are introducing a use-case-id, use-case configuration, and telemetry collection for a use-case.
    
    Reviewed By: muirdm
    
    Differential Revision: D74803772
    
    fbshipit-source-id: 5736bb0e0607bf4c514abfc7918725db4e854c2b

---
+++ eden/fs/cli_rs/edenfs-client/src/use_case/use_case.rs
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+// This value was selected semi-randomly and should be revisited in the future. Anecdotally, we
+// have seen EdenFS struggle with <<< 2048 outstanding requests, but the exact number depends
+// on the size/complexity/cost of the outstanding requests.
+const DEFAULT_MAX_OUTSTANDING_REQUESTS: usize = 2048;
+
+use strum::IntoStaticStr;
+
+#[derive(Clone, Copy, Debug, Eq, IntoStaticStr, Hash, PartialEq)]
+#[strum(serialize_all = "kebab_case")]
+#[repr(u32)]
+pub enum UseCaseId {
+    #[strum(serialize = "buck2")]
+    Buck2,
+    #[strum(serialize = "edenfsctl")]
+    EdenFsCtl,
+    EdenFsTests,
+    ExampleUseCase,
+    Flow,
+    Hack,
+    MeerakatCli,
+    RedirectFfi,
+    #[strum(serialize = "testifyd")]
+    TestifyDaemon,
+    WatchActiveCommit,
+}
+
+pub struct UseCase {
+    id: UseCaseId,
+    oncall: String,
+    max_outstanding_requests: usize,
+}
+
+impl UseCase {
+    pub fn new(id: UseCaseId) -> Self {
+        // TODO: retrieve use case specifics from configerator
+        let oncall = match id {
+            UseCaseId::Buck2 => "build_infra",
+            UseCaseId::Flow => "flow",
+            UseCaseId::Hack => "hack",
+            _ => "scm_client_infra",
+        };
+        let max_outstanding_requests = DEFAULT_MAX_OUTSTANDING_REQUESTS;
+        Self {
+            id,
+            oncall: oncall.to_string(),
+            max_outstanding_requests,
+        }
+    }
+
+    pub fn id(&self) -> &UseCaseId {
+        &self.id
+    }
+
+    pub fn name(&self) -> &'static str {
+        self.id.into()
+    }
+
+    pub fn oncall(&self) -> &str {
+        &self.oncall
+    }
+
+    pub fn max_outstanding_requests(&self) -> usize {
+        self.max_outstanding_requests
+    }
+}
