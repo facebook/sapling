685ad6fbf45f40182f59bd26a6f06fef95e6a3c8
Jun Wu <quark@meta.com>
2025-05-22 17:48:19 -0700

    hgmain: improve compatibility building with non-PIC libpython.a
    Summary:
    I'm trying to use statically linked libpython on Linux so our final binary can
    run on various Linux distros with different versions of system pythons.
    
    Building Python statically from scratch can be tricky if I have to deal with
    dependencies like openssl, curses, curl, etc. used in Python stdlib. So I'm
    trying to use a prebuilt cpython from pypa (Python Packaging Authority) at
    https://github.com/pypa/manylinux
    
    However, their `libpython.a` weren't built with `-fPIC` and therefore
    incompatible with Rust's `-pie`:
    
        ...
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(main.o): requires dynamic R_X86_64_32 reloc which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(gcmodule.o): requires dynamic R_X86_64_32 reloc which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(atexitmodule.o): requires dynamic R_X86_64_32 reloc which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(faulthandler.o): requires dynamic R_X86_64_32 reloc which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(posixmodule.o): requires dynamic R_X86_64_32 reloc which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(signalmodule.o): requires unsupported dynamic reloc 11; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(_tracemalloc.o): requires dynamic R_X86_64_32 reloc against '_Py_NoneStruct' which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(_codecsmodule.o): requires dynamic R_X86_64_32 reloc which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(_collectionsmodule.o): requires dynamic R_X86_64_32 reloc against '_Py_NoneStruct' which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(errnomodule.o): requires dynamic R_X86_64_32 reloc which may overflow at runtime; recompile with -fPIC
        /usr/bin/ld.gold: error: /root/sapling/eden/scm/build/cargo-target/release/deps/libpython3_sys-ae68783d5fd54571.rlib(_iomodule.o): requires dynamic R_X86_64_32 reloc which may overflow at runtime; recompile with -fPIC
        ...
    
    To be compatible with such `libpython.a`s, tell Rust to drop `-pie`.
    
    Reviewed By: muirdm
    
    Differential Revision: D75250624
    
    fbshipit-source-id: 91a60bbebbe179f6772b5bf4b19c3545c38b55b2

--- eden/scm/exec/hgmain/build.rs
+++ eden/scm/exec/hgmain/build.rs
@@ -23,4 +23,43 @@ fn main() {
             );
         }
     }
+
+    #[cfg(target_os = "linux")]
+    {
+        if is_libpython_static() {
+            // libpython.a typically (ex. built by pypa/manylinux) is not built with
+            // `CFLAGS=-fPIC LDFLAGS=-fPIC` and they will fail to link, like:
+            //
+            //   /usr/bin/ld.gold: error: */libpython3_sys-*.rlib(abstract.o): requires dynamic
+            //   R_X86_64_32 reloc against '_PyRuntime' which may overflow at runtime; recompile with
+            //   -fPIC
+            //
+            // Use `no-pie` be compatible with such `libpython.a`. The downside is that the built
+            // binary is not PIE (Position Independent Executable) and ASLR (Address Space Layout
+            // Randomization) cannot be used.
+            println!("cargo:rustc-link-arg=-no-pie");
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
+fn is_libpython_static() -> bool {
+    use std::ffi::OsString;
+    use std::process::Command;
+
+    let python = match env::var_os("PYTHON_SYS_EXECUTABLE") {
+        Some(python) => python,
+        None => {
+            println!("cargo:warning=PYTHON_SYS_EXECUTABLE is recommended at build time");
+            OsString::from("python3")
+        }
+    };
+    let out = Command::new(&python)
+        .args([
+            "-Sc",
+            "print(__import__('sysconfig').get_config_var('Py_ENABLE_SHARED'))",
+        ])
+        .output()
+        .expect("Failed to get Py_ENABLE_SHARED from Python");
+    out.stdout.starts_with(b"0")
 }
