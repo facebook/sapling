99d25012c2c8108f07ace20829f9cc0bc6b66ef8
Jun Wu <quark@meta.com>
2025-06-05 09:53:36 -0700

    python-modules: use python-sysconfig in build.rs
    Summary: Avoid dealing with PYTHON_SYS_EXECUTABLE directly.
    
    Reviewed By: muirdm
    
    Differential Revision: D76012894
    
    fbshipit-source-id: 74899d2180d230b3abec63588740607b85fc27aa

--- eden/scm/lib/python-modules/build.rs
+++ eden/scm/lib/python-modules/build.rs
@@ -6,24 +6,16 @@
  */
 
 use std::env;
-use std::ffi::OsString;
 use std::path::Path;
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=PYTHON_SYS_EXECUTABLE");
-
-    let python = match env::var_os("PYTHON_SYS_EXECUTABLE") {
-        Some(python) => python,
-        None => {
-            println!("cargo:warning=PYTHON_SYS_EXECUTABLE is recommended at build time");
-            OsString::from("python3")
-        }
-    };
+    let sysconfig = python_sysconfig::PythonSysConfig::new();
+    let python = sysconfig.python();
     let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let manifest_dir = Path::new(&manifest_dir);
     let sys_path = manifest_dir.parent().unwrap().parent().unwrap();
 
-    let code = codegen::generate_code(&Path::new(&python), Some(sys_path.as_ref()));
+    let code = codegen::generate_code(python, Some(sys_path.as_ref()));
 
     let out = manifest_dir.join("src/compiled.rs");
     std::fs::write(out, code).unwrap();
