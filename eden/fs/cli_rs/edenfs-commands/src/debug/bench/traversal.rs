061da8dc4e4e5495f7428bb204b14957ac36cc08
George Giorgidze <giorgidze@meta.com>
2025-06-10 12:16:51 -0700

    Progress bar updates too frequently, update it in every 1000 files + dir
    Summary: The diff adjusts the update frequency to every 1000 files and directories. This change aims to strike a better balance between providing updates to the user and minimizing the performance impact of frequent progress bar updates.
    
    Reviewed By: liubov-dmitrieva
    
    Differential Revision: D76216130
    
    fbshipit-source-id: 7047123428c8bf66f1fe74814397033539fc227e

--- eden/fs/cli_rs/edenfs-commands/src/debug/bench/traversal.rs
+++ eden/fs/cli_rs/edenfs-commands/src/debug/bench/traversal.rs
@@ -88,14 +88,14 @@ impl InProgressTraversal {
     fn add_file(&mut self, path: PathBuf) {
         self.file_count += 1;
         self.file_paths.push(path);
-        if (self.file_count + self.dir_count) % 100 == 0 {
+        if (self.file_count + self.dir_count) % 1000 == 0 {
             self.update_progress();
         }
     }
 
     fn add_dir(&mut self) {
         self.dir_count += 1;
-        if (self.file_count + self.dir_count) % 100 == 0 {
+        if (self.file_count + self.dir_count) % 1000 == 0 {
             self.update_progress();
         }
     }
