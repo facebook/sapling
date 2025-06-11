906508e7717456d1fd58a64b3a7407acdb809bec
Jarrett Dai <jldai@meta.com>
2025-06-11 12:11:55 -0700

    debuggitmodules: add --json option for isl
    Summary: A json option for isl that doesn't necessarilly outputs human readable strings.
    
    Reviewed By: quark-zju
    
    Differential Revision: D76435559
    
    fbshipit-source-id: 127d5ed251f97e4b42338b5bc015d8fb4ffbcc44

--- eden/scm/lib/commands/debugcommands/cmddebuggitmodules/src/lib.rs
+++ eden/scm/lib/commands/debugcommands/cmddebuggitmodules/src/lib.rs
@@ -8,16 +8,28 @@
 use std::io::Write;
 
 use clidispatch::ReqCtx;
-use cmdutil::NoOpts;
+use cliparser::define_flags;
 use cmdutil::Result;
 use repo::Repo;
 use workingcopy::workingcopy::WorkingCopy;
 
-pub fn run(ctx: ReqCtx<NoOpts>, _repo: &Repo, wc: &WorkingCopy) -> Result<u8> {
+define_flags! {
+    pub struct DebugGitModulesOpts {
+        /// print in JSON string
+        json: bool,
+    }
+}
+
+pub fn run(ctx: ReqCtx<DebugGitModulesOpts>, _repo: &Repo, wc: &WorkingCopy) -> Result<u8> {
     let gitmodules = wc.parse_submodule_config()?;
 
-    for m in gitmodules {
-        write!(ctx.io().output(), "{}", m)?;
+    if ctx.opts.json {
+        let jstring = serde_json::to_string(&gitmodules)?;
+        write!(ctx.io().output(), "{}", jstring)?;
+    } else {
+        for m in gitmodules {
+            write!(ctx.io().output(), "{}", m)?;
+        }
     }
 
     Ok(0)
