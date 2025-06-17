8fa67d37966a4411ddc711843eccdf98cc6ee8a8
Youssef Ibrahim <youssefsalama@meta.com>
2025-06-17 08:52:16 -0700

    cas: replace thin client with bare rust thrift client
    Summary:
    Context: we're replacing the thin cas client with a bare rust thrift client. This is to get rid of the start up overhead from creating a service router client and to improve reliability by getting rid of the C++ dependencies that the external CAS client pull in.
    
    Replace the thin client with the rust bare thrift client.
    
    Reviewed By: liubov-dmitrieva
    
    Differential Revision: D76593861
    
    fbshipit-source-id: 13d783ab1bee52baed02d0d580a12283f62744f6

--- eden/scm/lib/cas-client/thin-client/src/lib.rs
+++ eden/scm/lib/cas-client/thin-client/src/lib.rs
@@ -5,53 +5,50 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
 use std::sync::Arc;
-use std::time::Duration;
 
 use anyhow::Result;
+use anyhow::anyhow;
 use blob::Blob;
 use cas_client::CasClient;
-use cas_client::CasSuccessTracker;
-use cas_client::CasSuccessTrackerConfig;
-#[cfg(not(target_os = "linux"))]
-use cas_client_lib::CASClientBuilder;
-#[cfg(not(target_os = "linux"))]
-use cas_client_lib::CASClientBundle;
-use cas_client_lib::CASDaemonClientCfg;
-#[cfg(not(target_os = "linux"))]
-use cas_client_lib::ClientBuilderCommonMethods;
-use cas_client_lib::ExternalCASDaemonAddress;
-use cas_client_lib::ExternalCASDaemonCfg;
-use cas_client_lib::RESessionID;
-use cas_client_lib::RemoteExecutionMetadata;
-use cas_client_lib::create_default_config;
+use cas_client::CasDigest;
+use cas_client::CasDigestType;
+use cas_client::CasFetchedStats;
+use cas_client::FetchContext;
+use cas_daemon_types::*;
+use cas_daemon_types_thriftclients::CASDaemonServiceClient;
+use cas_daemon_types_thriftclients::make_CASDaemonService_thriftclient;
 use configmodel::Config;
 use configmodel::ConfigExt;
 use configmodel::convert::ByteCount;
 use configmodel::convert::FromConfigValue;
+use futures::stream;
+use futures::stream::BoxStream;
+use futures::stream::StreamExt;
+use itertools::Either;
+use itertools::Itertools;
+use once_cell::sync::OnceCell;
 use re_cas_common::from_re_digest;
 use re_cas_common::parse_stats;
+use re_cas_common::split_up_to_max_bytes;
 use re_cas_common::to_re_digest;
-#[cfg(target_os = "linux")]
-use thin_cas_client_wrapper::CASClientWrapper as CASClientBundle;
+use remote_execution_common::TCode;
+use thriftclient::TransportType;
 
 pub struct ThinCasClient {
-    client: re_cas_common::OnceCell<CASClientBundle>,
-    cas_success_tracker: CasSuccessTracker,
-    metadata: RemoteExecutionMetadata,
-    connection_count: u32,
-    port: Option<i32>,
+    client: OnceCell<CASDaemonServiceClient>,
+    port: Option<u16>,
     uds_path: Option<String>,
-    verbose: bool,
-    log_dir: Option<String>,
+    use_case: String,
+    session_id: String,
     fetch_limit: ByteCount,
     fetch_concurrency: usize,
-    use_streaming_dowloads: bool,
-    session_id: String,
-    use_persistent_caches: bool,
+    connection_timeout_ms: u32,
+    recv_timeout_ms: u32,
 }
-
-const DEFAULT_SCM_CAS_LOGS_DIR: &str = "scm_cas";
 
 pub fn init() {
     fn construct(config: &dyn Config) -> Result<Option<Arc<dyn CasClient>>> {
@@ -64,7 +61,7 @@ pub fn init() {
         tracing::debug!(target: "cas_client", "creating thin client");
         ThinCasClient::from_config(config).map(|c| c.map(|c| Arc::new(c) as Arc<dyn CasClient>))
     }
-    factory::register_constructor("thin-client", construct);
+    factory::register_constructor("thin-cas-client", construct);
 }
 
 impl ThinCasClient {
@@ -85,118 +82,182 @@ impl ThinCasClient {
             }
         };
 
-        let verbose: bool = config.get_or_default("cas", "verbose")?;
-        let mut log_dir = None;
-        if !verbose {
-            // If we're not verbose, we don't want to log to stderr.
-            log_dir = config.get_opt("cas", "log-dir")?;
-            if let Some(ref log_dir_path) = log_dir {
-                if !std::path::Path::new(log_dir_path).exists() {
-                    if let Err(err) = std::fs::create_dir(log_dir_path) {
-                        tracing::warn!(target: "cas_client", "failed to create log dir with path {}: {}", log_dir_path, err);
-                        log_dir = None;
-                    }
-                }
-            } else {
-                let temp_dir = std::env::temp_dir();
-                let log_dir_path = temp_dir.join(DEFAULT_SCM_CAS_LOGS_DIR);
-                log_dir = Some(log_dir_path.to_string_lossy().to_string());
-                if !std::path::Path::new(&log_dir_path).exists() {
-                    if let Err(err) = std::fs::create_dir(&log_dir_path) {
-                        tracing::warn!(target: "cas_client", "failed to create log dir with path {:?}: {}", log_dir_path, err);
-                        log_dir = None;
-                    }
-                }
-            }
-        }
-
         let default_fetch_limit = ByteCount::try_from_str("200MB")?;
         let cri = clientinfo::get_client_request_info();
         let session_id = format!("{}_{}", cri.entry_point, cri.correlator);
 
         Ok(Some(Self {
             client: Default::default(),
-            connection_count: config.get_or("cas", "connection-count", || 1)?,
-            port: config.get_opt::<i32>("cas", "port")?,
+            port: config.get_opt::<u16>("cas", "port")?,
             uds_path: config.get_opt("cas", "uds-path")?,
-            verbose,
-            metadata: RemoteExecutionMetadata {
-                use_case_id: use_case,
-                re_session_id: Some(RESessionID {
-                    id: session_id.clone(),
-                    ..Default::default()
-                }),
-                ..Default::default()
-            },
-            log_dir,
+            use_case,
             session_id,
             fetch_limit: config
                 .get_or::<ByteCount>("cas", "max-batch-bytes", || default_fetch_limit)?,
             fetch_concurrency: config.get_or("cas", "fetch-concurrency", || 4)?,
-            use_streaming_dowloads: config.get_or("cas", "use-streaming-downloads", || true)?,
-            cas_success_tracker: CasSuccessTracker::new(CasSuccessTrackerConfig {
-                max_failures: config.get_or("cas", "max-failures", || 10)?,
-                downtime_on_failure: config
-                    .get_or("cas", "downtime-on-failure", || Duration::from_secs(1))?,
-            }),
-            use_persistent_caches: config.get_or("cas", "use-persistent-caches", || true)?,
+            connection_timeout_ms: config.get_or("cas", "connection-timeout-ms", || 500)?,
+            recv_timeout_ms: config.get_or("cas", "recv-timeout-ms", || 1000)?,
         }))
     }
 
-    fn build(&self) -> Result<CASClientBundle> {
-        let mut re_config = create_default_config();
-
-        re_config.client_name = Some("sapling".to_string());
-        re_config.quiet_mode = !self.verbose;
-        re_config.log_file_location = self.log_dir.clone();
-        re_config.features_config_path = "remote_execution/features/client_sapling".to_string();
-        re_config.enable_ods_logging = false;
-        re_config.enable_scuba_logging = false;
-        re_config.enable_cancellation = true;
-
-        let mut external_config = if let Some(port) = self.port {
-            ExternalCASDaemonCfg {
-                cas_daemon_port: port,
-                cas_daemon_address: ExternalCASDaemonAddress::port(port),
-                address: None,
-                connection_count: self.connection_count as i32,
-                ..Default::default()
+    fn client(&self) -> Result<&CASDaemonServiceClient> {
+        self.client.get_or_try_init(|| {
+            if !fbinit::was_performed() {
+                return Err(anyhow::anyhow!("fbinit is required to create CAS client"));
             }
-        } else if let Some(uds_path) = self.uds_path.clone() {
-            ExternalCASDaemonCfg {
-                cas_daemon_port: 0,
-                cas_daemon_address: ExternalCASDaemonAddress::uds_path(uds_path),
-                address: None,
-                connection_count: self.connection_count as i32,
-                ..Default::default()
-            }
-        } else {
-            let socket_path = std::env::var("CASD_SOCKET_PATH")
-                .unwrap_or(cas_client_lib::DEFAULT_CASD_SOCKET.to_string());
-            ExternalCASDaemonCfg {
-                address: None,
-                connection_count: self.connection_count as i32,
-                cas_daemon_address: ExternalCASDaemonAddress::uds_path(socket_path),
-                socket_activation: true,
-                ..Default::default()
-            }
-        };
 
-        if self.use_persistent_caches {
-            external_config.client_label = "pc_enabled".to_string();
+            if let Some(port) = self.port {
+                make_CASDaemonService_thriftclient!(
+                    fbinit::expect_init(),
+                    from_sock_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port),
+                    with_conn_timeout = self.connection_timeout_ms,
+                    with_recv_timeout = self.recv_timeout_ms,
+                    with_secure = false,
+                    with_transport_type = TransportType::Rocket,
+                    with_channel_pool = false,
+                )
+            } else {
+                make_CASDaemonService_thriftclient!(
+                    fbinit::expect_init(),
+                    from_path = self.uds_path.as_deref().unwrap_or("/run/casd/casd.socket"),
+                    with_conn_timeout = self.connection_timeout_ms,
+                    with_recv_timeout = self.recv_timeout_ms,
+                    with_secure = false,
+                    with_transport_type = TransportType::Rocket,
+                    with_channel_pool = false,
+                )
+            }
+        })
+    }
+
+    fn cas_call_context(&self) -> CASCallContext {
+        CASCallContext {
+            use_case_id: self.use_case.clone(),
+            session_id: self.session_id.clone(),
+            cache_session_uid: self.session_id.clone(),
+            ephemeral_cache_session: Some(true),
+            ..Default::default()
         }
-
-        re_config.cas_client_config = CASDaemonClientCfg::external_config(external_config);
-
-        #[cfg(target_os = "linux")]
-        let client = CASClientBundle::new(self.session_id.clone(), 0, re_config)?;
-        #[cfg(not(target_os = "linux"))]
-        let client = CASClientBuilder::new(fbinit::expect_init())
-            .with_config(re_config)
-            .build()?;
-
-        Ok(client)
     }
 }
 
-re_cas_common::cas_client!(ThinCasClient);
+#[async_trait::async_trait]
+impl CasClient for ThinCasClient {
+    fn fetch_single_locally_cached(
+        &self,
+        _digest: &CasDigest,
+    ) -> Result<(CasFetchedStats, Option<Blob>)> {
+        Ok((CasFetchedStats::default(), None))
+    }
+
+    async fn fetch<'a>(
+        &'a self,
+        _fctx: FetchContext,
+        digests: &'a [CasDigest],
+        log_name: CasDigestType,
+    ) -> BoxStream<'a, Result<(CasFetchedStats, Vec<(CasDigest, Result<Option<Blob>>)>)>> {
+        stream::iter(split_up_to_max_bytes(digests, self.fetch_limit.value()))
+            .map(move |digests| async move {
+                tracing::debug!(target: "cas_client", "ThinCasClient fetching {} {}(s)", digests.len(), log_name);
+
+                let request = DownloadDigestsInlineRequest {
+                    digests: digests.iter().map(to_re_digest).collect(),
+                    ctx: self.cas_call_context(),
+                    throw_on_error: false,
+                    ..Default::default()
+                };
+                let response = self.client()?.downloadDigestsInline(&request).await?;
+                let blobs = response
+                    .digests
+                    .into_iter()
+                    .map(|response| {
+                        let digest = from_re_digest(&response.digest)?;
+                        match response.status.code {
+                            TCode::OK => Ok((digest, Ok(Some(Blob::Bytes(response.blob.into()))))),
+                            TCode::NOT_FOUND => Ok((digest, Ok(None))),
+                            _ => Ok((
+                                digest,
+                                Err(anyhow!(
+                                    "bad status (code={}, message={}, group={})",
+                                    response.status.code,
+                                    response.status.message,
+                                    response.status.group
+                                )),
+                            )),
+                        }
+                    })
+                    .collect::<Result<Vec<_>>>()?;
+                let stats = parse_stats(
+                    response.storage_stats.per_backend_stats.into_iter(),
+                    response.local_cache_stats,
+                );
+                Ok((stats, blobs))
+            })
+            .buffer_unordered(self.fetch_concurrency)
+            .boxed()
+    }
+
+    async fn upload(&self, blobs: Vec<Blob>) -> Result<Vec<CasDigest>> {
+        tracing::debug!(target: "cas_client", "ThinCasClient uploading {} blobs", blobs.len());
+
+        let request = UploadInlinedBlobsRequest {
+            blobs: blobs.into_iter().map(|blob| blob.into_vec()).collect(),
+            ctx: self.cas_call_context(),
+            ..Default::default()
+        };
+
+        self.client()?
+            .uploadInlinedBlobs(&request)
+            .await?
+            .digests
+            .into_iter()
+            .map(|digest_with_status| from_re_digest(&digest_with_status.digest))
+            .collect::<Result<Vec<_>>>()
+    }
+
+    async fn prefetch<'a>(
+        &'a self,
+        _fctx: FetchContext,
+        digests: &'a [CasDigest],
+        log_name: CasDigestType,
+    ) -> BoxStream<'a, Result<(CasFetchedStats, Vec<CasDigest>, Vec<CasDigest>)>> {
+        stream::iter(split_up_to_max_bytes(digests, self.fetch_limit.value()))
+            .map(move |digests| async move {
+                tracing::debug!(target: "cas_client", "ThinCasClient prefetching {} {}(s)", digests.len(), log_name);
+
+                let request = MaterializeDigestsRequest {
+                    digests: digests.iter().map(to_re_digest).collect(),
+                    ctx: self.cas_call_context(),
+                    throw_on_error: false,
+                    ..Default::default()
+                };
+                let response = self.client()?.materializeDigestsRemotely(&request).await?;
+                let digests = response
+                    .digests
+                    .into_iter()
+                    .map(|response| {
+                        let digest = from_re_digest(&response.digest)?;
+                        match response.status.code {
+                            TCode::OK => Ok(Either::Left(digest)),
+                            TCode::NOT_FOUND => Ok(Either::Right(digest)),
+                            _ => Err(anyhow!(
+                                "bad status (code={}, message={}, group={})",
+                                response.status.code,
+                                response.status.message,
+                                response.status.group
+                            )),
+                        }
+                    })
+                    .collect::<Result<Vec<_>>>()?;
+                let (digests_prefetched, digests_not_found) =
+                    digests.into_iter().partition_map(std::convert::identity);
+                let stats = parse_stats(
+                    response.storage_stats.per_backend_stats.into_iter(),
+                    response.local_cache_stats,
+                );
+                Ok((stats, digests_prefetched, digests_not_found))
+            })
+            .buffer_unordered(self.fetch_concurrency)
+            .boxed()
+    }
+}
