ef6b6fb4cd9869b9b2019ab04ecc28ce196d79a5
Paul O'Shannessy <poshannessy@meta.com>
2025-07-01 12:42:03 -0700

    Update License Headers for Open Source
    Summary: The license header we use for our Apache + MIT dual licensed projects can be made more explicit. This has not actual implications on licensing, but does clarify what it means for a project to be dual licensed.
    
    Reviewed By: bigfootjon
    
    Differential Revision: D77550482
    
    fbshipit-source-id: 24267af9a7715bfc604b5840ce29776705428516

--- common/rust/shed/futures_ext/src/future/try_shared.rs
+++ common/rust/shed/futures_ext/src/future/try_shared.rs
@@ -1,10 +1,11 @@
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
- * This source code is licensed under both the MIT license found in the
- * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * This source code is dual-licensed under either the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree or the Apache
  * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
- * of this source tree.
+ * of this source tree. You may select, at your option, one of the
+ * above-listed licenses.
  */
 
 use anyhow::Error;
