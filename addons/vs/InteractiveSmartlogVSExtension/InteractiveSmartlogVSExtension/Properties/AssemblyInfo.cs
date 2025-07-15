35e84d50a7b94a31c0ec3feaf6b247ebacafa980
Vini Gupta <vinigupta@meta.com>
2025-07-15 10:21:59 -0700

    Add test coverage for Interactive SmartlogVSExtension
    Summary:
    **Summary:** This diff establishes the foundation for unit testing the InteractiveSmartlogVSExtension by creating a test project structure. It adds a new MSTest project with necessary dependencies and updates the solution file to include it.
    
    **Changes:**
    
    *   Create new test project file `InteractiveSmartlogVSExtension.Tests.csproj` with MSTest, Moq, and VS SDK references
    *   Update solution file to include the test project with appropriate build configurations
    *   Create directory structure for organizing tests by component
    
    Reviewed By: anaypaul, LoxFalcon
    
    Differential Revision: D78295400
    
    fbshipit-source-id: e8c7d7a4e4f33941083befab5eedb8f6e73299af

--- addons/vs/InteractiveSmartlogVSExtension/InteractiveSmartlogVSExtension/Properties/AssemblyInfo.cs
+++ addons/vs/InteractiveSmartlogVSExtension/InteractiveSmartlogVSExtension/Properties/AssemblyInfo.cs
@@ -9,6 +9,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: InternalsVisibleTo("InteractiveSmartlogVSExtension.Tests")]
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
