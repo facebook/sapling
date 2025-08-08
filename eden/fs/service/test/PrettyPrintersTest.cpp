72cf630da05825a0aa4bf6fe062719dbe50c2cb3
Genevieve (Genna) Helsel <helsel@meta.com>
2025-08-08 12:29:50 -0700

    add PrettyPrintersTest
    Summary:
    We currently use multiple string formatting approaches across the project. This diff stacks works at moving the project away from using `iostream`/`operator<<()`, `folly::format`, and `folly::to<std::string>`  and instead opting using the `fmt` library.
    
    A few different things are mixed in with this stack, a diff will do some combination of the following:
    1) removing definitions of `operator<<()` to discourage their use
    2) removing definitions of `toAppend()` to discourage the use of `folly::to<std::string>` (though note, some diffs add this as a stepping stone to `fmt`
    3) adding custom `fmt::formatter`s
    4) adding unit tests for new and existing `fmt::formatter`s
    5) cleaning up/optimizing existing `fmt::formatter`s
    
    Differential Revision: D79736621
    
    fbshipit-source-id: cb68cbe4468d5a213ec8a6580151e9ab67bf3a69

---
+++ eden/fs/service/test/PrettyPrintersTest.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "eden/fs/service/PrettyPrinters.h"
+#include "eden/common/utils/DirType.h"
+
+#include <gtest/gtest.h>
+
+using namespace facebook::eden;
+
+TEST(PrettyPrinters, ostream_format_conflict_type) {
+  std::ostringstream os;
+  os << ConflictType::MODIFIED_REMOVED;
+  EXPECT_EQ("MODIFIED_REMOVED", os.str());
+}
+
+TEST(PrettyPrinters, ostream_format_checkout_conflict) {
+  CheckoutConflict conflict;
+  conflict.path() = "some/test/path";
+  conflict.type() = ConflictType::REMOVED_MODIFIED;
+  conflict.dtype() = static_cast<Dtype>(dtype_t::Regular);
+
+  std::ostringstream os;
+  os << conflict;
+  EXPECT_EQ(
+      "CheckoutConflict(type=REMOVED_MODIFIED, path=\"some/test/path\", message=\"\")",
+      os.str());
+}
+
+TEST(PrettyPrinters, ostream_format_checkout_conflict_error) {
+  CheckoutConflict conflict;
+  conflict.path() = "some/test/path";
+  conflict.type() = ConflictType::ERROR;
+  conflict.message() = "Error invalidating path";
+
+  std::ostringstream os;
+  os << conflict;
+  EXPECT_EQ(
+      "CheckoutConflict(type=ERROR, path=\"some/test/path\", message=\"Error invalidating path\")",
+      os.str());
+}
+
+TEST(PrettyPrinters, ostream_format_scm_file_status) {
+  std::ostringstream os;
+  os << ScmFileStatus::REMOVED;
+  EXPECT_EQ("REMOVED", os.str());
+}
+
+TEST(PrettyPrinters, ostream_format_mount_state) {
+  std::ostringstream os;
+  os << MountState::RUNNING;
+  EXPECT_EQ("RUNNING", os.str());
+}
