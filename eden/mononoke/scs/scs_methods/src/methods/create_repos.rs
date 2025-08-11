17d6ba7d62fc277bba60b2ffb0bca4c62c2cf45f
Pierre Chevalier <pierrec@meta.com>
2025-08-11 09:40:14 -0700

    `create_repos`: Extract `create_repos_in_metagit`
    Summary:
    In this stack, I'll make this endpoint do more (create the repositories in Mononoke).
    
    To prepare the terrain, let's separate concerns from the current implementation.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D79998954
    
    fbshipit-source-id: a183d70b9fc724e165dc929eccc08d074c14e26c

--- eden/mononoke/scs/scs_methods/src/methods/create_repos.rs
+++ eden/mononoke/scs/scs_methods/src/methods/create_repos.rs
@@ -18,6 +18,52 @@ use source_control as thrift;
 
 use crate::source_control_impl::SourceControlServiceImpl;
 
+async fn create_repos_in_metagit(
+    params: thrift::CreateReposParams,
+) -> Result<(), scs_errors::ServiceError> {
+    let scmadmin_path = env::var("SCMADMIN_PATH")
+        .map_err(Into::<Error>::into)
+        .map_err(Into::<MononokeError>::into)?;
+    for repo in params.repos {
+        let mut command = Command::new(&scmadmin_path);
+
+        command
+            .arg("repo")
+            .arg("init")
+            .arg("--bypass-task-check")
+            .arg("-r")
+            .arg("git")
+            .arg(&repo.repo_name);
+
+        if params.dry_run {
+            command.arg("--dry-run");
+        }
+
+        match repo.custom_acl {
+            None => {
+                command.arg("--top-level-acl");
+            }
+            Some(custom_acl) => {
+                command.arg("--hipster-group").arg(custom_acl.hipster_group);
+                command.arg("--oncall").arg(repo.oncall_name);
+            }
+        }
+        let output = command
+            .output()
+            .map_err(Into::<Error>::into)
+            .map_err(Into::<MononokeError>::into)?;
+        if !output.status.success() {
+            Err(scs_errors::internal_error(format!(
+                "Failed to create repo: {}, stderr: {}, stdout: {}",
+                &repo.repo_name,
+                String::from_utf8_lossy(&output.stderr),
+                String::from_utf8_lossy(&output.stdout),
+            )))?
+        }
+    }
+    Ok(())
+}
+
 impl SourceControlServiceImpl {
     pub(crate) async fn create_repos(
         &self,
@@ -31,47 +77,8 @@ impl SourceControlServiceImpl {
         .await
         .map_err(Into::<MononokeError>::into)?;
 
-        let scmadmin_path = env::var("SCMADMIN_PATH")
-            .map_err(Into::<Error>::into)
-            .map_err(Into::<MononokeError>::into)?;
+        create_repos_in_metagit(params).await?;
 
-        for repo in params.repos {
-            let mut command = Command::new(&scmadmin_path);
-
-            command
-                .arg("repo")
-                .arg("init")
-                .arg("--bypass-task-check")
-                .arg("-r")
-                .arg("git")
-                .arg(&repo.repo_name);
-
-            if params.dry_run {
-                command.arg("--dry-run");
-            }
-
-            match repo.custom_acl {
-                None => {
-                    command.arg("--top-level-acl");
-                }
-                Some(custom_acl) => {
-                    command.arg("--hipster-group").arg(custom_acl.hipster_group);
-                    command.arg("--oncall").arg(repo.oncall_name);
-                }
-            }
-            let output = command
-                .output()
-                .map_err(Into::<Error>::into)
-                .map_err(Into::<MononokeError>::into)?;
-            if !output.status.success() {
-                Err(scs_errors::internal_error(format!(
-                    "Failed to create repo: {}, stderr: {}, stdout: {}",
-                    &repo.repo_name,
-                    String::from_utf8_lossy(&output.stderr),
-                    String::from_utf8_lossy(&output.stdout),
-                )))?
-            }
-        }
         Ok(thrift::CreateReposToken {
             ..Default::default()
         })
