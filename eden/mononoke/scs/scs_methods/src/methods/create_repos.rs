ee7f117bbe2ffeb4fce17f38f964e5e3f5025526
Pierre Chevalier <pierrec@meta.com>
2025-08-11 09:40:14 -0700

    `create_repos`: Outline the plan for repo creation in Mononoke
    Summary:
    There are a few pieces to this puzzle.
    Outline them in TODO comments, so we can tick them off one-by-one.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D80002059
    
    fbshipit-source-id: 1bc7edf7f31657905b297009d3d53b6d14491169

--- eden/mononoke/scs/scs_methods/src/methods/create_repos.rs
+++ eden/mononoke/scs/scs_methods/src/methods/create_repos.rs
@@ -10,6 +10,9 @@ use std::process::Command;
 
 use anyhow::Error;
 use anyhow::Result;
+#[cfg(fbcode_build)]
+#[allow(unused)]
+use configo::ConfigoClient;
 use context::CoreContext;
 use futures::future::try_join_all;
 use mononoke_api::MononokeError;
@@ -18,6 +21,10 @@ use repo_authorization::AuthorizationContext;
 use source_control as thrift;
 
 use crate::source_control_impl::SourceControlServiceImpl;
+
+/// See https://www.internalfb.com/wiki/Configerator/Configerator_Get_Started/Configerator_Concepts_&_Terms/Configerator_Config_Name/
+#[allow(unused)]
+const QUICK_REPO_DEFINITIONS_CONFIG_NAME: &str = "scm/mononoke/repos/quick_repo_definitions";
 
 async fn ensure_acls_allow_repo_creation(
     ctx: CoreContext,
@@ -32,6 +39,64 @@ async fn ensure_acls_allow_repo_creation(
     )
     .await
     .map_err(Into::<MononokeError>::into)?;
+    Ok(())
+}
+
+async fn update_repos_acls(
+    _params: &thrift::CreateReposParams,
+) -> Result<(), scs_errors::ServiceError> {
+    // TODO (Pierre):
+    // ## What:
+    // Ensure all these repos have the necessary ACLs set-up.
+    // We need it here as it needs to happen prior to both creating the repositories in Mononoke
+    // and in Metagit.
+    // Also, eventually, we will remove `create_repos_in_metagit` and we will need for the ACLs to
+    // be updated in that case too.
+    // It's probably OK to create them here and let `scmadmin` attempt to create them, which will
+    // become a no-op. That will mean that `scmadmin` will still function both as stand-alone or
+    // when called from here.
+    //
+    // ## How:
+    // All the ACL logic currently lives in `scmadmin`: https://www.internalfb.com/code/fbsource/[9e94dda302f98eb670145fd69e62bf10520fe414]/fbcode/scm/scmadmin/commands/repo.py?lines=240
+    // Use the `commitChangeSpecification` thrift endpoint [see code](https://www.internalfb.com/code/fbsource/[2627fc9342e5ce9f58e5dcccbc82f3ee35ca1ee8]/fbcode/infrasec/authorization/if/authorization.thrift?lines=1500)
+    // See [example use](https://www.internalfb.com/code/fbsource/[207c62c9a0ec60970a06de4e5366fe92231cdfedf]/fbcode/icsp/rust/acl_fixer/src/update_acl.rs?lines=29)
+    Ok(())
+}
+
+#[cfg(fbcode_build)]
+async fn create_repos_in_mononoke(
+    _params: &thrift::CreateReposParams,
+) -> Result<(), scs_errors::ServiceError> {
+    // TODO (Pierre):
+    // ## What:
+    // Create these repositories in Mononoke.
+    //
+    // ## How:
+    // * a) Find the free range of repository ids by calling `get_max_id` on `GitSourceOfTruthConfig`
+    //   See [code](https://www.internalfb.com/code/fbsource/[1855d0a75c2214080433754d4b70010d9a997594]/fbcode/eden/mononoke/git_source_of_truth/src/lib.rs?lines=46)
+    // * b) Try to write the list of new repos (repo, id, state == "reserved") at max_id + 1, max_id + 2, ...
+    // * c) This query can fail for 2 reasons:
+    //   * Id is not unique <- This indicates contention. Go back to a)
+    //   * Repo name is not unique <- Fail with a descritive Error
+    // * d) At this stage, we have reserved the repo ids we need, so we're not in contention
+    //   anymore. We have time to do slow things, such as configuring these repositories in
+    //   configerator to add the to Mononoke.
+    //   Use configo to make a diff adding these repositories to quick_repo_definitions using
+    //   the parameters passed in to `create_repos`via thrift
+    //   Note: for authoring a config change, the configo API is what I need:
+    //   https://www.internalfb.com/intern/rustdoc/fbcode/common/rust/configo:configo/configo/index.html
+    //   Create the mutation, validate it and land it.
+    // * e) Repo creation was successful, overwrite "reserved" with "mononoke" for these
+    //   repositories. At this point, the repos were created and empty, so it's OK for their Source
+    //   Of Truth to be in Mononoke already, ahead of creating them in Metagit and allowing pushes.
+    Ok(())
+}
+
+#[cfg(not(fbcode_build))]
+async fn create_repos_in_mononoke(
+    _params: &thrift::CreateReposParams,
+) -> Result<(), scs_errors::ServiceError> {
+    println!("No access to configo in oss build");
     Ok(())
 }
 
@@ -88,6 +153,8 @@ impl SourceControlServiceImpl {
         params: thrift::CreateReposParams,
     ) -> Result<thrift::CreateReposToken, scs_errors::ServiceError> {
         ensure_acls_allow_repo_creation(ctx, &params.repos, self.acl_provider.as_ref()).await?;
+        update_repos_acls(&params).await?;
+        create_repos_in_mononoke(&params).await?;
         create_repos_in_metagit(params).await?;
 
         Ok(thrift::CreateReposToken {
