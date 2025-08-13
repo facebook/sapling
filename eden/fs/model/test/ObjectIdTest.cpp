1c44970983b219785cf203f271f251b404cbabd0
Genevieve (Genna) Helsel <helsel@meta.com>
2025-08-13 10:35:58 -0700

    add formatting test for ObjectId
    Summary:
    We currently use multiple string formatting approaches across the project. This diff stacks works at moving the project away from using `iostream`/`operator<<()`, `folly::format`, and `folly::to<std::string>`  and instead opting using the `fmt` library.
    
    A few different things are mixed in with this stack, a diff will do some combination of the following:
    1) removing definitions of `operator<<()` to discourage their use
    2) removing definitions of `toAppend()` to discourage the use of `folly::to<std::string>` (though note, some diffs add this as a stepping stone to `fmt`
    3) adding custom `fmt::formatter`s
    4) adding unit tests for new and existing `fmt::formatter`s
    5) cleaning up/optimizing existing `fmt::formatter`s
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D79752969
    
    fbshipit-source-id: 9ac431a287901e552c85f22fcbeba9d4caa24daf

--- eden/fs/model/test/ObjectIdTest.cpp
+++ eden/fs/model/test/ObjectIdTest.cpp
@@ -28,8 +28,8 @@ TEST(ObjectId, testHashCodeExact) {
 TEST(ObjectId, testHashCodeShort) {
   auto bytes = folly::make_array<uint8_t>(0x00, 0xff);
   auto byteRange = folly::ByteRange(bytes.data(), bytes.size());
-  auto exactObjectId = ObjectId(byteRange);
-  auto hashCode = exactObjectId.getHashCode();
+  auto shortObjectId = ObjectId(byteRange);
+  auto hashCode = shortObjectId.getHashCode();
   // May not work correctly on little-endian machines.
   EXPECT_EQ(hashCode, 0xff00);
 }
@@ -67,8 +67,8 @@ TEST(ObjectId, testHashCodeLong) {
       0x04,
       0x04);
   auto byteRange = folly::ByteRange(bytes.data(), bytes.size());
-  auto exactObjectId = ObjectId(byteRange);
-  auto hashCode = exactObjectId.getHashCode();
+  auto longObjectId = ObjectId(byteRange);
+  auto hashCode = longObjectId.getHashCode();
   EXPECT_EQ(hashCode, folly::Endian::big(0x0707070707070707));
 }
 
@@ -91,8 +91,8 @@ TEST(ObjectId, testHashCodeNotMod8) {
       0x00,
       0x00);
   auto byteRange = folly::ByteRange(bytes.data(), bytes.size());
-  auto exactObjectId = ObjectId(byteRange);
-  auto hashCode = exactObjectId.getHashCode();
+  auto notMod8ObjectId = ObjectId(byteRange);
+  auto hashCode = notMod8ObjectId.getHashCode();
 
   // When length of ObjectID is not a multiple of 8, we end up overlapping
   // xor byte ranges. In this case, we'll xor as follows:
@@ -103,5 +103,87 @@ TEST(ObjectId, testHashCodeNotMod8) {
   // 0xff ff ff ff 00 00 00 00
   //
   EXPECT_EQ(hashCode, 0xffffffff00000000);
+}
+
+TEST(ObjectId, testFormattingHashCodeExact) {
+  auto bytes = folly::make_array<uint8_t>(
+      0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0xff, 0xff);
+  auto byteRange = folly::ByteRange(bytes.data(), bytes.size());
+  auto exactObjectId = ObjectId(byteRange);
+  std::ostringstream os;
+  os << exactObjectId;
+  EXPECT_EQ("0000ffff0000ffff", os.str());
+}
+
+TEST(ObjectId, testFormattingHashCodeShort) {
+  auto bytes = folly::make_array<uint8_t>(0x00, 0xff);
+  auto byteRange = folly::ByteRange(bytes.data(), bytes.size());
+  auto shortObjectId = ObjectId(byteRange);
+  std::ostringstream os;
+  os << shortObjectId;
+  EXPECT_EQ("00ff", os.str());
+}
+
+TEST(ObjectId, testFormattingHashCodeLong) {
+  auto bytes = folly::make_array<uint8_t>(
+      0x01,
+      0x01,
+      0x01,
+      0x01,
+
+      0x01,
+      0x01,
+      0x01,
+      0x01,
+
+      0x02,
+      0x02,
+      0x02,
+      0x02,
+
+      0x02,
+      0x02,
+      0x02,
+      0x02,
+
+      0x04,
+      0x04,
+      0x04,
+      0x04,
+
+      0x04,
+      0x04,
+      0x04,
+      0x04);
+  auto byteRange = folly::ByteRange(bytes.data(), bytes.size());
+  auto longObjectId = ObjectId(byteRange);
+  std::ostringstream os;
+  os << longObjectId;
+  EXPECT_EQ("010101010101010102020202020202020404040404040404", os.str());
+}
+
+TEST(ObjectId, testFormattingHashCodeNotMod8) {
+  auto bytes = folly::make_array<uint8_t>(
+      // all 1s in binary
+      0xff,
+      0xff,
+      0xff,
+      0xff,
+
+      0xff,
+      0xff,
+      0xff,
+      0xff,
+
+      // all 0s in binary
+      0x00,
+      0x00,
+      0x00,
+      0x00);
+  auto byteRange = folly::ByteRange(bytes.data(), bytes.size());
+  auto notMod8ObjectId = ObjectId(byteRange);
+  std::ostringstream os;
+  os << notMod8ObjectId;
+  EXPECT_EQ("ffffffffffffffff00000000", os.str());
 }
 } // namespace
