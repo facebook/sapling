d4014063a90a25afa53d2f7acd041ce6fd1eab22
Spencer Burakowski <sburakowski@meta.com>
2025-08-21 16:50:28 -0700

    Add modern 'thrift-python' thrift_object.py library
    Summary:
    Part 3 of the 'changes' test migration.
    
    Add the migrated 'thrift_object.py' library, so follow-on diffs can be using the modern library constructors.
    
    Reviewed By: taidaii
    
    Differential Revision: D80572014
    
    fbshipit-source-id: f957c40342b7fa475b3698def1e012508121483e

---
+++ eden/integration/lib/thrift_objects.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+# pyre-unsafe
+
+from typing import Optional
+
+from eden.fs.service.eden.thrift_types import (
+    Added,
+    ChangeNotification,
+    CommitTransition,
+    DirectoryRenamed,
+    Dtype,
+    LargeChangeNotification,
+    LostChanges,
+    LostChangesReason,
+    Modified,
+    Removed,
+    Renamed,
+    Replaced,
+    SmallChangeNotification,
+)
+
+from thrift.python.types import StructMeta
+
+
+def getSmallChangeSafe(
+    change: ChangeNotification,
+) -> Optional[SmallChangeNotification]:
+    if hasattr(change, "smallChange") and change.smallChange is not None:
+        return change.smallChange
+    return None
+
+
+def getLargeChangeSafe(
+    change: ChangeNotification,
+) -> Optional[LargeChangeNotification]:
+    if hasattr(change, "largeChange") and change.largeChange is not None:
+        return change.largeChange
+    return None
+
+
+def buildSmallChange(
+    changeType: StructMeta,
+    fileType: Dtype,
+    path: Optional[bytes] = None,
+    from_path: Optional[bytes] = None,
+    to_path: Optional[bytes] = None,
+) -> ChangeNotification:
+    if changeType is Added:
+        assert path
+        return ChangeNotification(
+            smallChange=SmallChangeNotification(
+                added=Added(fileType=fileType, path=path)
+            )
+        )
+    elif changeType is Modified:
+        assert path
+        return ChangeNotification(
+            smallChange=SmallChangeNotification(
+                modified=Modified(fileType=fileType, path=path)
+            )
+        )
+    elif changeType is Renamed:
+        assert from_path
+        assert to_path
+        return ChangeNotification(
+            smallChange=SmallChangeNotification(
+                renamed=Renamed(
+                    fileType=fileType,
+                    from_=from_path,
+                    to=to_path,
+                )
+            )
+        )
+    elif changeType is Replaced:
+        assert from_path
+        assert to_path
+        return ChangeNotification(
+            smallChange=SmallChangeNotification(
+                replaced=Replaced(
+                    fileType=fileType,
+                    from_=from_path,
+                    to=to_path,
+                )
+            )
+        )
+
+    elif changeType is Removed:
+        assert path
+        return ChangeNotification(
+            smallChange=SmallChangeNotification(
+                removed=Removed(fileType=fileType, path=path)
+            )
+        )
+    return ChangeNotification()
+
+
+def buildLargeChange(
+    changeType: StructMeta,
+    from_bytes: Optional[bytes] = None,
+    to_bytes: Optional[bytes] = None,
+    lost_change_reason: Optional[LostChangesReason] = None,
+) -> ChangeNotification:
+    if changeType is DirectoryRenamed:
+        return ChangeNotification(
+            largeChange=LargeChangeNotification(
+                directoryRenamed=DirectoryRenamed(from_=from_bytes, to=to_bytes)
+            )
+        )
+    elif changeType is CommitTransition:
+        return ChangeNotification(
+            largeChange=LargeChangeNotification(
+                commitTransition=CommitTransition(from_=from_bytes, to=to_bytes)
+            )
+        )
+    elif changeType is LostChanges:
+        return ChangeNotification(
+            largeChange=LargeChangeNotification(
+                lostChanges=LostChanges(reason=lost_change_reason)
+            )
+        )
+    return ChangeNotification()
