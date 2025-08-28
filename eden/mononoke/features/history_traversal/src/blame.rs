7495628d0b65d9e1f4966d378c6132096328dd66
Liu Yang <lyang@meta.com>
2025-08-28 03:02:05 -0700

    Fix inferred_copy_from consumption +1
    Summary:
    Looks like I still didn't get the logic "which changeset added this file" correct using fastlog.
    
    I missed the case where a file has a looong history, long enough to hit the `FastlogParent::Unknown` case:
    https://www.internalfb.com/code/fbsource/[ac002bb366ec4a2df018425e0c940226133e0954]/fbcode/eden/mononoke/derived_data/fastlog/mapping.rs?lines=42-44
    
    Reviewed By: RajivTS
    
    Differential Revision: D81172714
    
    fbshipit-source-id: 22bdfe8531ebb7e60a57a10747713ac14c5b3acc

--- eden/mononoke/features/history_traversal/src/blame.rs
+++ eden/mononoke/features/history_traversal/src/blame.rs
@@ -18,6 +18,7 @@ use blame::fetch_content_for_blame;
 use blobstore::Loadable;
 use bytes::Bytes;
 use context::CoreContext;
+use fastlog::FastlogParent;
 use fastlog::fetch_fastlog_batch_by_unode_id;
 use fastlog::fetch_flattened;
 use futures::future::try_join_all;
@@ -314,10 +315,11 @@ async fn get_csids_that_added_path(
     let unode_entry = get_unode_entry(ctx, repo, csid, path).await?;
     let fastlog_batch =
         fetch_fastlog_batch_by_unode_id(ctx, repo.repo_blobstore(), &unode_entry).await?;
+
     if let Some(fastlog_batch) = fastlog_batch {
         let flattened = fetch_flattened(&fastlog_batch, ctx, repo.repo_blobstore()).await?;
         // Find changesets with empty parents
-        Ok(flattened
+        let root_csids = flattened
             .iter()
             .filter_map(|(csid, parents)| {
                 if parents.is_empty() {
@@ -326,10 +328,28 @@ async fn get_csids_that_added_path(
                     None
                 }
             })
-            .collect())
-    } else {
-        Ok(vec![])
+            .collect::<Vec<_>>();
+        if !root_csids.is_empty() {
+            return Ok(root_csids);
+        }
+
+        // If there's no changeset with empty parents, there might be more fastlog to fetch
+        let csid_with_unknown_parent = flattened.iter().find(|(_, parents)| {
+            parents
+                .iter()
+                .any(|parent| matches!(parent, FastlogParent::Unknown))
+        });
+        if let Some((next_csid, _)) = csid_with_unknown_parent {
+            return Box::pin(get_csids_that_added_path(
+                ctx,
+                repo,
+                next_csid.clone(),
+                path,
+            ))
+            .await;
+        }
     }
+    Ok(vec![])
 }
 
 /// Fetch inferred_copy_from derived data for csid with destination path
