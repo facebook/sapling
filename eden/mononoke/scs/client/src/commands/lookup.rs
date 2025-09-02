c213506a7086d847ab1e9f8198763d890892ed29
Jose García Gimeno <jagg@meta.com>
2025-09-02 06:41:42 -0700

    Add tests support for repo_multiple_commit_lookup
    Summary: Updated the lookup SCSC command to use the new batched API. Not it supports adding several commit IDs as input, and will return a list with all the responses.
    
    Reviewed By: markbt
    
    Differential Revision: D80069229
    
    fbshipit-source-id: fb05a116976bb2e00d6c5ec43c330083c423c3c3

--- eden/mononoke/scs/client/src/commands/lookup.rs
+++ eden/mononoke/scs/client/src/commands/lookup.rs
@@ -8,18 +8,21 @@
 //! Look up a bookmark or commit id.
 
 use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::io::Write;
 
 use anyhow::Result;
 use anyhow::bail;
-use commit_id_types::CommitIdArgs;
+use commit_id_types::CommitId;
+use commit_id_types::CommitIdsArgs;
+use scs_client_raw::ScsClient;
 use scs_client_raw::thrift;
 use serde::Serialize;
 
 use crate::ScscApp;
 use crate::args::commit_id::SchemeArgs;
 use crate::args::commit_id::map_commit_ids;
-use crate::args::commit_id::resolve_commit_id;
+use crate::args::commit_id::resolve_commit_ids;
 use crate::args::repo::RepoArgs;
 use crate::errors::SelectionErrorExt;
 use crate::library::commit_id::render_commit_id;
@@ -33,7 +36,13 @@ pub(super) struct CommandArgs {
     #[clap(flatten)]
     scheme_args: SchemeArgs,
     #[clap(flatten)]
-    commit_id_args: CommitIdArgs,
+    commit_ids_args: CommitIdsArgs,
+}
+
+#[derive(Serialize)]
+pub(crate) struct CommitResponsePair {
+    pub id: String,
+    pub lookup_output: LookupOutput,
 }
 
 #[derive(Serialize)]
@@ -63,18 +72,59 @@ impl Render for LookupOutput {
     }
 }
 
-pub(super) async fn run(app: ScscApp, args: CommandArgs) -> Result<()> {
-    let repo = args.repo_args.clone().into_repo_specifier();
-    let commit_id = args.commit_id_args.clone().into_commit_id();
-    let conn = app.get_connection(Some(&repo.name))?;
-    let id = resolve_commit_id(&conn, &repo, &commit_id).await?;
+#[derive(Serialize)]
+pub(crate) struct MultipleLookupOutput {
+    pub responses: Vec<CommitResponsePair>,
+}
+
+impl Render for MultipleLookupOutput {
+    type Args = SchemeArgs;
+
+    fn render(&self, args: &Self::Args, w: &mut dyn Write) -> Result<()> {
+        let schemes = args.scheme_string_set();
+        let mut found_any = false;
+
+        for response in &self.responses {
+            if response.lookup_output.exists {
+                write!(w, "{}: ", response.id)?;
+                render_commit_id(
+                    None,
+                    " ",
+                    &response.id,
+                    &response.lookup_output.ids,
+                    &schemes,
+                    w,
+                )?;
+                write!(w, "\n")?;
+                found_any = true;
+            }
+        }
+
+        if !found_any {
+            bail!("None of the commits were found");
+        }
+        Ok(())
+    }
+
+    fn render_json(&self, _args: &Self::Args, w: &mut dyn Write) -> Result<()> {
+        Ok(serde_json::to_writer(w, self)?)
+    }
+}
+
+async fn single_lookup(
+    resolved_id: thrift::CommitId,
+    requested_id: CommitId,
+    repo: thrift::RepoSpecifier,
+    identity_schemes: std::collections::BTreeSet<thrift::CommitIdentityScheme>,
+    conn: ScsClient,
+) -> Result<LookupOutput> {
     let commit = thrift::CommitSpecifier {
         repo,
-        id,
+        id: resolved_id,
         ..Default::default()
     };
     let params = thrift::CommitLookupParams {
-        identity_schemes: args.scheme_args.clone().into_request_schemes(),
+        identity_schemes,
         ..Default::default()
     };
     let response = conn
@@ -85,10 +135,79 @@ pub(super) async fn run(app: ScscApp, args: CommandArgs) -> Result<()> {
         Some(ids) => map_commit_ids(ids.values()),
         None => BTreeMap::new(),
     };
-    let output = LookupOutput {
-        requested: commit_id.to_string(),
+    Ok(LookupOutput {
+        requested: requested_id.to_string(),
         exists: response.exists,
         ids,
-    };
-    app.target.render_one(&args.scheme_args, output).await
+    })
+}
+
+pub(super) async fn run(app: ScscApp, args: CommandArgs) -> Result<()> {
+    let repo = args.repo_args.clone().into_repo_specifier();
+    let commit_ids = args.commit_ids_args.clone().into_commit_ids();
+    if commit_ids.is_empty() {
+        bail!("expected at least one commit ID");
+    }
+
+    let conn = app.get_connection(Some(&repo.name))?;
+
+    let resolved_commit_ids = resolve_commit_ids(&conn, &repo, &commit_ids).await?;
+    let ids_index: HashMap<String, String> = resolved_commit_ids
+        .iter()
+        .map(|id| id.to_string())
+        .zip(commit_ids.iter().map(|id| id.to_string()))
+        .collect();
+
+    if commit_ids.len() == 1 {
+        let single_output = single_lookup(
+            resolved_commit_ids[0].clone(),
+            commit_ids[0].clone(),
+            repo,
+            args.scheme_args.clone().into_request_schemes(),
+            conn,
+        )
+        .await?;
+        app.target
+            .render_one(&args.scheme_args, single_output)
+            .await
+    } else {
+        let params = thrift::RepoMultipleCommitLookupParams {
+            commit_ids: resolved_commit_ids,
+            identity_schemes: args.scheme_args.clone().into_request_schemes(),
+            ..Default::default()
+        };
+        let response = conn
+            .repo_multiple_commit_lookup(&repo, &params)
+            .await
+            .map_err(|e| e.handle_selection_error(&repo))?;
+
+        let mut commit_response_pairs = Vec::new();
+        for aliases in response.responses.iter() {
+            if let Some(requested_id) = ids_index.get(&aliases.commit_id.to_string()) {
+                let ids = match &aliases.commit_lookup_response.ids {
+                    Some(ids) => map_commit_ids(ids.values()),
+                    None => BTreeMap::new(),
+                };
+                let lookup_output = LookupOutput {
+                    requested: requested_id.to_string(),
+                    exists: aliases.commit_lookup_response.exists,
+                    ids,
+                };
+                commit_response_pairs.push(CommitResponsePair {
+                    id: requested_id.to_string(),
+                    lookup_output,
+                });
+            } else {
+                bail!(
+                    "Internal error: requested id {} is not in the response",
+                    aliases.commit_id.to_string()
+                );
+            }
+        }
+        let output = MultipleLookupOutput {
+            responses: commit_response_pairs,
+        };
+
+        app.target.render_one(&args.scheme_args, output).await
+    }
 }
