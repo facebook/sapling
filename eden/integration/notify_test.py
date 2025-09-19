01033d92745437789f909a16c00b16b72606fbdc
Chris Dinh <cqd@meta.com>
2025-09-19 11:29:28 -0700

    Add integration test for deferred state subscription
    Summary:
    Test for eden subscribe with states
    
    Tests the case:
    No state asserted->Changes returned
    State asserted->State Entered reported
    State still asserted-> File changes not reported
    State left -> State Left reported
                     -> Pending changes reported
    
    Also tests the case where multiple states can be asserted at once, and all need to be deasserted to get results
    
    Choices:
    
    enter_state has a default duration of 30. This is arbitrarily chosen, but is set to prevent accidentally calling state enter and waiting on it forever.
    
    Differential Revision: D82670957
    
    fbshipit-source-id: 835389ff880eee7f97fd39b096e2a1d8ce76163a

--- eden/integration/notify_test.py
+++ eden/integration/notify_test.py
@@ -59,6 +59,55 @@ class NotifyTest(testcase.EdenRepoTest):
             cwd=self.mount,
         )
 
+    async def subscribe_with_states(self, states) -> asyncio.subprocess.Process:
+        self.mkdir(".edenfs-notifications-state")
+        for state in states:
+            self.mkdir(f".edenfs-notifications-state/{state}")
+        args = [
+            "notify",
+            "changes-since",
+            "--subscribe",
+            "--throttle",
+            "5",
+            "--json",
+            self.mount,
+        ]
+        for state in states:
+            args.append("--states")
+            args.append(state)
+        cmd, env = self.eden.get_edenfsctl_cmd_env(*args)
+        env["EDENFS_LOG"] = "edenfs=trace"
+
+        return await asyncio.create_subprocess_exec(
+            *cmd,
+            env=env,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            cwd=self.mount,
+        )
+
+    async def enter_state(self, state, duration=30) -> asyncio.subprocess.Process:
+        # set default duration to prevent accidental spinlocks
+        args = [
+            "notify",
+            "enter-state",
+            state,
+            self.mount,
+        ]
+        if duration:
+            args.append("--duration")
+            args.append(str(duration))
+        cmd, env = self.eden.get_edenfsctl_cmd_env(*args)
+        env["EDENFS_LOG"] = "edenfs=trace"
+
+        return await asyncio.create_subprocess_exec(
+            *cmd,
+            env=env,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            cwd=self.mount,
+        )
+
     async def next_event(
         self, sub: asyncio.subprocess.Process, timeout: int = 5
     ) -> t.Optional[t.Dict[str, t.Any]]:
@@ -143,3 +192,299 @@ class NotifyTest(testcase.EdenRepoTest):
                 }
             ],
         )
+
+    async def test_debug_subscribe_with_states(self) -> None:
+        subscription = await self.subscribe_with_states(["hello"])
+        event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertListEqual(event["changes"], [])
+
+        self.write_file("hello2", "test")
+        time.sleep(1)
+        event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertTrue(
+            {
+                "SmallChange": {
+                    "Added": {
+                        "file_type": "Regular",
+                        "path": [104, 101, 108, 108, 111, 50],
+                    }
+                }
+            }
+            in event["changes"],
+            msg=f"changes: {event['changes']}",
+        )
+
+        state_process = await self.enter_state("hello")
+        time.sleep(1)
+        event = await self.wait_for_next_event(subscription)
+        if event and "event_type" not in event:
+            # Sometimes the Modified change from the previous change gets chunked
+            # into a separate event from Added. Read it and bypass.
+            event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertEqual(event["event_type"], "Entered", msg=f"event: {event}")
+        self.assertEqual(event["state"], "hello", msg=f"event: {event}")
+
+        # expect this to return None since the state is asserted
+        self.write_file("hello3", "test")
+        time.sleep(1)
+        self.rename("hello3", "hello4")
+        time.sleep(1)
+        if sys.platform == "linux":
+            self.rm("hello4")
+        time.sleep(1)
+        event = await self.wait_for_next_event(subscription, attempt=1)
+        self.assertIsNone(event)
+
+        # exit the state
+        state_process.terminate()
+        event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertEqual(event["event_type"], "Left", msg=f"event: {event}")
+        self.assertEqual(event["state"], "hello", msg=f"event: {event}")
+
+        # Get deferred changes
+        changes = []
+        while event is not None:
+            event = await self.wait_for_next_event(subscription, attempt=1)
+            if event is None:
+                break
+            changes.extend(event["changes"])
+        if sys.platform != "win32":
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Added": {
+                            "file_type": "Regular",
+                            "path": [104, 101, 108, 108, 111, 51],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Renamed": {
+                            "file_type": "Regular",
+                            "from": [104, 101, 108, 108, 111, 51],
+                            "to": [104, 101, 108, 108, 111, 52],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+            # Rm behavior is different on Linux and Mac
+            # Mac will sometimes rename the file to a nfs.date.xxxx and then delete it
+            if sys.platform == "linux":
+                self.assertTrue(
+                    {
+                        "SmallChange": {
+                            "Removed": {
+                                "file_type": "Regular",
+                                "path": [104, 101, 108, 108, 111, 52],
+                            }
+                        }
+                    }
+                    in changes,
+                    msg=f"changes: {changes}",
+                )
+        else:
+            # Windows has no rename, only add/remove
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Added": {
+                            "file_type": "Regular",
+                            "path": [104, 101, 108, 108, 111, 51],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Removed": {
+                            "file_type": "Regular",
+                            "path": [104, 101, 108, 108, 111, 51],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Added": {
+                            "file_type": "Regular",
+                            "path": [104, 101, 108, 108, 111, 52],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+
+    async def test_debug_subscribe_with_multiple_states(self) -> None:
+        subscription = await self.subscribe_with_states(["hello", "goodbye"])
+        event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertListEqual(event["changes"], [])
+
+        self.write_file("hello2", "test")
+        time.sleep(1)
+        event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertTrue(
+            {
+                "SmallChange": {
+                    "Added": {
+                        "file_type": "Regular",
+                        "path": [104, 101, 108, 108, 111, 50],
+                    }
+                }
+            }
+            in event["changes"],
+            msg=f"changes: {event['changes']}",
+        )
+
+        hello_process = await self.enter_state("hello")
+        time.sleep(1)
+        event = await self.wait_for_next_event(subscription)
+        if event and "event_type" not in event:
+            # Sometimes the Modified change from the previous change gets chunked
+            # into a separate event from Added. Read it and bypass.
+            event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertEqual(event["event_type"], "Entered", msg=f"event: {event}")
+        self.assertEqual(event["state"], "hello", msg=f"event: {event}")
+
+        # expect this to return None since the state is asserted
+        self.write_file("hello3", "test")
+        time.sleep(1)
+        self.rename("hello3", "hello4")
+        time.sleep(1)
+        if sys.platform == "linux":
+            self.rm("hello4")
+        time.sleep(1)
+        event = await self.wait_for_next_event(subscription, attempt=1)
+        self.assertIsNone(event)
+
+        goodbye_process = await self.enter_state("goodbye")
+        time.sleep(1)
+        event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertEqual(event["event_type"], "Entered", msg=f"event: {event}")
+        self.assertEqual(event["state"], "goodbye", msg=f"event: {event}")
+
+        # exit the first state
+        hello_process.terminate()
+        event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertEqual(event["event_type"], "Left", msg=f"event: {event}")
+        self.assertEqual(event["state"], "hello", msg=f"event: {event}")
+
+        # expect this to return None since the second state is asserted
+        event = await self.wait_for_next_event(subscription, attempt=1)
+        self.assertIsNone(event)
+
+        # exit the second state
+        goodbye_process.terminate()
+        event = await self.wait_for_next_event(subscription)
+        self.assertIsNotNone(event)
+        self.assertEqual(event["event_type"], "Left", msg=f"event: {event}")
+        self.assertEqual(event["state"], "goodbye", msg=f"event: {event}")
+
+        # Get deferred changes
+        changes = []
+        while event is not None:
+            event = await self.wait_for_next_event(subscription, attempt=1)
+            if event is None:
+                break
+            changes.extend(event["changes"])
+        if sys.platform != "win32":
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Added": {
+                            "file_type": "Regular",
+                            "path": [104, 101, 108, 108, 111, 51],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Renamed": {
+                            "file_type": "Regular",
+                            "from": [104, 101, 108, 108, 111, 51],
+                            "to": [104, 101, 108, 108, 111, 52],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+            # Rm behavior is different on Linux and Mac
+            # Mac will sometimes rename the file to a nfs.date.xxxx and then delete it
+            if sys.platform == "linux":
+                self.assertTrue(
+                    {
+                        "SmallChange": {
+                            "Removed": {
+                                "file_type": "Regular",
+                                "path": [104, 101, 108, 108, 111, 52],
+                            }
+                        }
+                    }
+                    in changes,
+                    msg=f"changes: {changes}",
+                )
+        else:
+            # Windows has no rename, only add/remove
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Added": {
+                            "file_type": "Regular",
+                            "path": [104, 101, 108, 108, 111, 51],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Removed": {
+                            "file_type": "Regular",
+                            "path": [104, 101, 108, 108, 111, 51],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
+            self.assertTrue(
+                {
+                    "SmallChange": {
+                        "Added": {
+                            "file_type": "Regular",
+                            "path": [104, 101, 108, 108, 111, 52],
+                        }
+                    }
+                }
+                in changes,
+                msg=f"changes: {changes}",
+            )
