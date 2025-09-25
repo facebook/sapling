4f762b23b7d90726cdd100573cb828ffd4962497
Michael Cuevas <cuev@meta.com>
2025-09-25 10:55:01 -0700

    FilterUtils: add tests
    Summary:
    # This diff
    
    Adds some tests to exercise FilterUtils codepaths
    
    # Context
    
    The null byte test showcases a bug when the FilterId contains a null byte. The next diff fixes the bug and modifies the test
    
    Reviewed By: muirdm
    
    Differential Revision: D83114250
    
    fbshipit-source-id: b02c3d22648f743a4d44b7d0e7f77d447a53e1f1

---
+++ eden/fs/utils/test/FilterUtilsTest.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+#include "eden/fs/utils/FilterUtils.h"
+#include <folly/portability/GTest.h>
+#include "eden/fs/store/FilteredBackingStore.h"
+
+using namespace facebook::eden;
+
+TEST(FilterUtilsTest, filterContainsNullByte) {
+  auto filterWithNullByte = std::string{"foo\0bar", 7};
+  auto startingRootId = "OriginalRoot";
+  auto filteredRootId = FilteredBackingStore::createFilteredRootId(
+      startingRootId, filterWithNullByte);
+  auto [rootId, filter] = parseFilterIdFromRootId(RootId{filteredRootId});
+  EXPECT_EQ(startingRootId, rootId.value());
+  EXPECT_NE(filter, filterWithNullByte);
+}
+
+TEST(FilterUtilsTest, basic) {
+  auto startingFilter = "foobar";
+  auto startingRootId = "OriginalRoot";
+  auto filteredRootId = FilteredBackingStore::createFilteredRootId(
+      startingRootId, startingFilter);
+  auto [rootId, filterFromRootId] =
+      parseFilterIdFromRootId(RootId{filteredRootId});
+  EXPECT_EQ(startingFilter, filterFromRootId);
+  EXPECT_EQ(startingRootId, rootId.value());
+}
