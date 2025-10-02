849c60353c7a6ceea17fbd8d1e7e656843ffe08e
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-10-02 07:11:51 -0700

    Fix CQS signal facebook-unused-include-check in fbcode/eden/scm
    Reviewed By: q10
    
    Differential Revision: D83133342
    
    fbshipit-source-id: 207a52858201ce35baf99e09fae1e546e075876b

--- eden/scm/lib/backingstore/src/ffi.cpp
+++ eden/scm/lib/backingstore/src/ffi.cpp
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
- * This software may be used and distributed according to the terms of the
- * GNU General Public License version 2.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #include <folly/Try.h>
@@ -10,7 +10,6 @@
 #include <memory>
 
 #include "eden/fs/model/Tree.h"
-#include "eden/fs/model/TreeFwd.h"
 #include "eden/scm/lib/backingstore/include/ffi.h"
 #include "eden/scm/lib/backingstore/src/ffi.rs.h" // @manual
 
