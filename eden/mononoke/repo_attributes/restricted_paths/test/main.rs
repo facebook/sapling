52ebb19544a21aceca8f5d83903e541339ace6da
Gustavo Avena <gustavoavena@meta.com>
2025-10-02 06:00:19 -0700

    code_tenting: Add more test cases
    Reviewed By: MichaelCuevas
    
    Differential Revision: D83659155
    
    fbshipit-source-id: a43b01dd80457da78f3af2960c4b4dba629eaa11

--- eden/mononoke/repo_attributes/restricted_paths/test/main.rs
+++ eden/mononoke/repo_attributes/restricted_paths/test/main.rs
@@ -250,7 +250,7 @@ async fn test_mercurial_manifest_single_dir_many_restricted_changes(
                     .into_iter()
                     .map(NonRootMPath::new)
                     .collect::<Result<Vec<_>>>()?,
-                manifest_id: expected_manifest_id,
+                manifest_id: expected_manifest_id.clone(),
                 manifest_type: ManifestType::Hg,
                 client_identities: vec!["USER:myusername0"]
                     .into_iter()
@@ -405,23 +405,320 @@ async fn test_mercurial_manifest_multiple_restricted_dirs(fb: FacebookInit) -> R
     Ok(())
 }
 
-// TODO(T239041722): find a way to test access to the restricted paths **with
-// the proper authorization**, i.e. `has_authorization` should be logged as
-// `false`.
+// Test that if the user has access to one of the restricted paths, there will
+// be a log entry for each one with the proper authorization result.
+#[mononoke::fbinit_test]
+async fn test_mercurial_manifest_multiple_restricted_dirs_with_partial_access(
+    fb: FacebookInit,
+) -> Result<()> {
+    let restricted_paths = vec![
+        (
+            NonRootMPath::new("restricted/one").unwrap(),
+            MononokeIdentity::from_str("REPO_REGION:restricted_acl")?,
+        ),
+        (
+            // User will have access to this path
+            NonRootMPath::new("user_project/foo").unwrap(),
+            MononokeIdentity::from_str("REPO_REGION:myusername_project")?,
+        ),
+    ];
+    let RestrictedPathsTestData {
+        ctx,
+        repo,
+        log_file_path,
+    } = setup_restricted_paths_test(fb, restricted_paths, None).await?;
 
-// TODO(T239041722): test overlapping restricted directories. Top-level ACL should
-// be enforced.
+    let (manifest_id_store_entries, scuba_logs) = hg_manifest_test_with_restricted_paths(
+        &ctx,
+        repo,
+        vec![("restricted/one/a", None), ("user_project/foo/b", None)],
+        &log_file_path,
+    )
+    .await?;
 
-// TODO(T239041722): test different paths with same manifest id, where one
-// is restricted and the other isn't.
+    let expected_authorized_manifest_id =
+        ManifestId::from("5d30a65c45e695416c96abfbd745f43c711879bb");
+    let expected_unauthorized_manifest_id =
+        ManifestId::from("e53be16502cbc6afeb30ef30de7f6d9841fd4cb1");
+
+    pretty_assertions::assert_eq!(
+        manifest_id_store_entries,
+        vec![
+            RestrictedPathManifestIdEntry::new(
+                ManifestType::Hg,
+                expected_authorized_manifest_id.clone(),
+                NonRootMPath::new("user_project/foo")?
+            ),
+            RestrictedPathManifestIdEntry::new(
+                ManifestType::Hg,
+                expected_unauthorized_manifest_id.clone(),
+                NonRootMPath::new("restricted/one")?
+            ),
+        ]
+    );
+
+    pretty_assertions::assert_eq!(
+        scuba_logs,
+        vec![
+            ScubaAccessLogSample {
+                repo_id: RepositoryId::new(0),
+                restricted_paths: vec!["user_project/foo"]
+                    .into_iter()
+                    .map(NonRootMPath::new)
+                    .collect::<Result<Vec<_>>>()?,
+                manifest_id: expected_authorized_manifest_id,
+                manifest_type: ManifestType::Hg,
+                client_identities: vec!["USER:myusername0"]
+                    .into_iter()
+                    .map(String::from)
+                    .collect::<Vec<_>>(),
+                // User had access to this restricted path
+                has_authorization: true,
+                client_main_id: TEST_CLIENT_MAIN_ID.to_string(),
+            },
+            ScubaAccessLogSample {
+                repo_id: RepositoryId::new(0),
+                restricted_paths: vec!["restricted/one"]
+                    .into_iter()
+                    .map(NonRootMPath::new)
+                    .collect::<Result<Vec<_>>>()?,
+                manifest_id: expected_unauthorized_manifest_id,
+                manifest_type: ManifestType::Hg,
+                client_identities: vec!["USER:myusername0"]
+                    .into_iter()
+                    .map(String::from)
+                    .collect::<Vec<_>>(),
+                has_authorization: false,
+                client_main_id: TEST_CLIENT_MAIN_ID.to_string(),
+            },
+        ]
+    );
+
+    Ok(())
+}
+
+#[mononoke::fbinit_test]
+async fn test_mercurial_manifest_overlapping_restricted_directories(
+    fb: FacebookInit,
+) -> Result<()> {
+    // Set up overlapping restricted paths: project/restricted is nested inside project
+    let restricted_paths = vec![
+        (
+            NonRootMPath::new("project/restricted").unwrap(),
+            MononokeIdentity::from_str("REPO_REGION:more_restricted_acl")?,
+        ),
+        (
+            NonRootMPath::new("project").unwrap(),
+            MononokeIdentity::from_str("REPO_REGION:project_acl")?,
+        ),
+    ];
+
+    // Custom ACL that gives access to project but NOT to project/restricted
+    let custom_acl = r#"{
+  "repos": {
+    "default": {
+      "actions": {
+        "read": ["USER:myusername0"],
+        "write": ["USER:myusername0"]
+      }
+    }
+  },
+  "repo_regions": {
+    "project_acl": {
+      "actions": {
+        "read": ["USER:myusername0"]
+      }
+    },
+    "more_restricted_acl": {
+      "actions": {
+        "read": ["USER:other_user"]
+      }
+    }
+  }
+}"#;
+
+    let RestrictedPathsTestData {
+        ctx,
+        repo,
+        log_file_path,
+    } = setup_restricted_paths_test(fb, restricted_paths, Some(custom_acl)).await?;
+
+    // Access a file in the more restricted nested path - this should trigger both ACL checks
+    let (manifest_id_store_entries, scuba_logs) = hg_manifest_test_with_restricted_paths(
+        &ctx,
+        repo,
+        vec![("project/restricted/sensitive_file.txt", None)],
+        &log_file_path,
+    )
+    .await?;
+
+    let expected_manifest_id_root = ManifestId::from("0825286967058d61feb5b0031f4c23fa0a999965");
+    let expected_manifest_id_subdir = ManifestId::from("5629398cf56074c359a05b1f170eb2590efe11c3");
+
+    // Should have manifest entries for both overlapping paths
+    pretty_assertions::assert_eq!(
+        manifest_id_store_entries,
+        vec![
+            RestrictedPathManifestIdEntry::new(
+                ManifestType::Hg,
+                expected_manifest_id_root.clone(),
+                NonRootMPath::new("project")?
+            ),
+            RestrictedPathManifestIdEntry::new(
+                ManifestType::Hg,
+                expected_manifest_id_subdir.clone(),
+                NonRootMPath::new("project/restricted")?
+            ),
+        ]
+    );
+
+    // Should log access to both overlapping paths:
+    // - project/restricted (unauthorized - user doesn't have more_restricted_acl)
+    // - project (authorized - user has project_acl)
+    pretty_assertions::assert_eq!(
+        scuba_logs,
+        vec![
+            ScubaAccessLogSample {
+                repo_id: RepositoryId::new(0),
+                restricted_paths: vec!["project"]
+                    .into_iter()
+                    .map(NonRootMPath::new)
+                    .collect::<Result<Vec<_>>>()?,
+                manifest_id: expected_manifest_id_root,
+                manifest_type: ManifestType::Hg,
+                client_identities: vec!["USER:myusername0"]
+                    .into_iter()
+                    .map(String::from)
+                    .collect::<Vec<_>>(),
+                // User has access to the broader project ACL
+                has_authorization: true,
+                client_main_id: TEST_CLIENT_MAIN_ID.to_string(),
+            },
+            ScubaAccessLogSample {
+                repo_id: RepositoryId::new(0),
+                restricted_paths: vec!["project/restricted"]
+                    .into_iter()
+                    .map(NonRootMPath::new)
+                    .collect::<Result<Vec<_>>>()?,
+                manifest_id: expected_manifest_id_subdir,
+                manifest_type: ManifestType::Hg,
+                client_identities: vec!["USER:myusername0"]
+                    .into_iter()
+                    .map(String::from)
+                    .collect::<Vec<_>>(),
+                // User does NOT have access to the more restricted ACL
+                has_authorization: false,
+                client_main_id: TEST_CLIENT_MAIN_ID.to_string(),
+            },
+        ]
+    );
+
+    Ok(())
+}
+
+#[mononoke::fbinit_test]
+async fn test_mercurial_manifest_same_manifest_id_restricted_and_unrestricted_paths(
+    fb: FacebookInit,
+) -> Result<()> {
+    // Set up a restricted path for the "restricted" directory
+    let restricted_paths = vec![(
+        NonRootMPath::new("restricted").unwrap(),
+        MononokeIdentity::from_str("REPO_REGION:restricted_acl")?,
+    )];
+
+    let RestrictedPathsTestData {
+        ctx,
+        repo,
+        log_file_path,
+    } = setup_restricted_paths_test(fb, restricted_paths, None).await?;
+
+    // Create two files with the same content in directories that should have the same manifest ID:
+    // - restricted/foo/bar (under restricted path)
+    // - unrestricted/foo/bar (not under restricted path)
+    // Both foo/bar subdirectories should have identical manifest IDs since they contain identical content
+    let identical_content = "same file content";
+    let (manifest_id_store_entries, scuba_logs) = hg_manifest_test_with_restricted_paths(
+        &ctx,
+        repo,
+        vec![
+            ("restricted/foo/bar", Some(identical_content)),
+            ("unrestricted/foo/bar", Some(identical_content)),
+        ],
+        &log_file_path,
+    )
+    .await?;
+
+    let expected_manifest_id = ManifestId::from("0464bc4205fd3b4651678b66778299a352bac0d8");
+
+    // Should have manifest entry for the restricted path only
+    pretty_assertions::assert_eq!(
+        manifest_id_store_entries,
+        vec![RestrictedPathManifestIdEntry::new(
+            ManifestType::Hg,
+            expected_manifest_id.clone(),
+            NonRootMPath::new("restricted")?
+        ),]
+    );
+
+    // The helper function will access all directories by manifest ID. It will
+    // encounter the manifest ID for "foo/bar" twice, once for the restricted
+    // and once for the restricted directory. But it will log both samples as
+    // if the user was accessing the restricted directory, i.e. as unauthorized
+    // access.
+    pretty_assertions::assert_eq!(
+        scuba_logs,
+        vec![
+            ScubaAccessLogSample {
+                repo_id: RepositoryId::new(0),
+                restricted_paths: vec!["restricted"]
+                    .into_iter()
+                    .map(NonRootMPath::new)
+                    .collect::<Result<Vec<_>>>()?,
+                manifest_id: expected_manifest_id.clone(),
+                manifest_type: ManifestType::Hg,
+                client_identities: vec!["USER:myusername0"]
+                    .into_iter()
+                    .map(String::from)
+                    .collect::<Vec<_>>(),
+                // User does NOT have access to restricted_acl
+                has_authorization: false,
+                client_main_id: TEST_CLIENT_MAIN_ID.to_string(),
+            },
+            ScubaAccessLogSample {
+                repo_id: RepositoryId::new(0),
+                restricted_paths: vec!["restricted"]
+                    .into_iter()
+                    .map(NonRootMPath::new)
+                    .collect::<Result<Vec<_>>>()?,
+                manifest_id: expected_manifest_id.clone(),
+                manifest_type: ManifestType::Hg,
+                client_identities: vec!["USER:myusername0"]
+                    .into_iter()
+                    .map(String::from)
+                    .collect::<Vec<_>>(),
+                // User does NOT have access to restricted_acl
+                has_authorization: false,
+                client_main_id: TEST_CLIENT_MAIN_ID.to_string(),
+            },
+        ]
+    );
+
+    Ok(())
+}
 
 //
 // ----------------------------------------------------------------
 // Test helpers
 
 /// Given a list of restricted paths and a list of file paths, create a changeset
-/// modifying those paths, derive the hg manifest and return all the entries
-/// in the manifest id store.
+/// modifying those paths, derive the hg manifest and fetch all the hg manifests
+/// from the last changeset, to simulate access to all directories in the repo.
+///
+/// Returns:
+/// - All the entries in the manifest id store, to check that the manifest ids
+/// from restricted paths were stored after derivation.
+/// - All Scuba logs from accessing restricted paths.
+///
 /// Each file path can optionally specify content. If no content is provided,
 /// the file path itself is used as the content.
 async fn hg_manifest_test_with_restricted_paths(
