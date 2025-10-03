599bd88a52c58f573b46cdd0e23973c9956145e4
Nicholas Blake <nblake@meta.com>
2025-10-03 12:07:24 -0700

    Hide upsell when review is initiated
    Summary: It's a bit weird to continue to see the upsell after the "Start review" button has been clicked. We can hide the upsell after the button is clicked, and then wait for the head commit to change before showing it again.
    
    Reviewed By: evangrayk
    
    Differential Revision: D83855229
    
    fbshipit-source-id: fcfb5d77924f061872aa718ccda2ab33b602af25

--- addons/isl/src/firstPassCodeReview/AICodeReviewUpsell.tsx
+++ addons/isl/src/firstPassCodeReview/AICodeReviewUpsell.tsx
@@ -8,11 +8,13 @@
 import {Banner, BannerKind} from 'isl-components/Banner';
 import {Button} from 'isl-components/Button';
 import {Icon} from 'isl-components/Icon';
-import {useAtom} from 'jotai';
+import {useAtom, useAtomValue} from 'jotai';
 import clientToServerAPI from '../ClientToServerAPI';
 import {T} from '../i18n';
+import {latestHeadCommit} from '../serverAPIState';
 import {codeReviewStatusAtom} from './firstPassCodeReviewAtoms';
 
+import {useEffect, useState} from 'react';
 import {tracker} from '../analytics';
 import {useFeatureFlagSync} from '../featureFlags';
 import {Internal} from '../Internal';
@@ -21,12 +23,22 @@ import './AICodeReviewUpsell.css';
 
 export function AICodeReviewUpsell(): JSX.Element | null {
   const [status, setStatus] = useAtom(codeReviewStatusAtom);
+  const headCommit = useAtomValue(latestHeadCommit);
+  const [hidden, setHidden] = useState(false);
   const aiFirstPassCodeReviewEnabled = useFeatureFlagSync(
     Internal.featureFlags?.AIFirstPassCodeReview,
   );
 
+  useEffect(() => {
+    setHidden(false);
+  }, [headCommit]);
+
   // TODO: move this component to vscode/webview
   if (platform.platformName !== 'vscode') {
+    return null;
+  }
+
+  if (hidden) {
     return null;
   }
 
@@ -42,6 +54,7 @@ export function AICodeReviewUpsell(): JSX.Element | null {
         <Button // TODO: Replace with dropdown to choose between quick/thorough review
           onClick={() => {
             setStatus('running');
+            setHidden(true);
             clientToServerAPI.postMessage({
               type: 'platform/runAICodeReviewChat',
               source: 'commitInfoView',
