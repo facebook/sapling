f9adb8192ea44a0c3610e9150c70ddf19bfe6318
Pierre Chevalier <pierrec@meta.com>
2025-10-10 09:28:21 -0700

    SCS: Remove the code referring to metagit in `create_repos`
    Summary:
    * This code has been unused since [this rollout, 3 weeks ago](https://fb.workplace.com/groups/3112141815538647/permalink/24619973844328800/) as the jk is selecting repo creation in Mononoke
    * As we've updated `scmadmin` to create repos in Mononoke by calling the `create_repos` endpoint on SCS, it would end up triggering an infinite loop instead of creating the repositories in Metagit
    
    Let's delete the old code-path, and once it's deployed, we'll be able to drop the jks.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D84259582
    
    fbshipit-source-id: bbc25ac99be9f39f0dd56cb56619a9e50425740a

--- eden/mononoke/scs/scs_methods/src/methods/create_repos.rs
+++ eden/mononoke/scs/scs_methods/src/methods/create_repos.rs
@@ -7,11 +7,8 @@
 
 use std::collections::BTreeMap;
 use std::collections::HashSet;
-use std::env;
-use std::process::Command;
 use std::time::Duration;
 
-use anyhow::Error;
 use anyhow::Result;
 use auth_consts::AUTH_SET;
 use auth_consts::ONCALL;
@@ -694,52 +691,6 @@ async fn create_repos_in_mononoke(
     Ok(())
 }
 
-async fn create_repos_in_metagit(
-    params: thrift::CreateReposParams,
-) -> Result<(), scs_errors::ServiceError> {
-    let scmadmin_path = env::var("SCMADMIN_PATH")
-        .map_err(Into::<Error>::into)
-        .map_err(Into::<MononokeError>::into)?;
-    for repo in params.repos {
-        let mut command = Command::new(&scmadmin_path);
-
-        command
-            .arg("repo")
-            .arg("init")
-            .arg("--bypass-task-check")
-            .arg("-r")
-            .arg("git")
-            .arg(&repo.repo_name);
-
-        if params.dry_run {
-            command.arg("--dry-run");
-        }
-
-        match repo.custom_acl {
-            None => {
-                command.arg("--top-level-acl");
-            }
-            Some(custom_acl) => {
-                command.arg("--hipster-group").arg(custom_acl.hipster_group);
-                command.arg("--oncall").arg(repo.oncall_name);
-            }
-        }
-        let output = command
-            .output()
-            .map_err(Into::<Error>::into)
-            .map_err(Into::<MononokeError>::into)?;
-        if !output.status.success() {
-            Err(scs_errors::internal_error(format!(
-                "Failed to create repo: {}, stderr: {}, stdout: {}",
-                &repo.repo_name,
-                String::from_utf8_lossy(&output.stderr),
-                String::from_utf8_lossy(&output.stdout),
-            )))?
-        }
-    }
-    Ok(())
-}
-
 impl SourceControlServiceImpl {
     pub(crate) async fn create_repos(
         &self,
@@ -748,18 +699,8 @@ impl SourceControlServiceImpl {
     ) -> Result<thrift::CreateReposToken, scs_errors::ServiceError> {
         ensure_acls_allow_repo_creation(ctx.clone(), &params.repos, self.acl_provider.as_ref())
             .await?;
-        if justknobs::eval("scm/mononoke:scs_create_repos_in_mononoke", None, None)
-            .map_err(|e| scs_errors::internal_error(format!("{e:#}")))?
-        {
-            update_repos_acls(ctx.clone(), &params).await?;
-            create_repos_in_mononoke(ctx, self.git_source_of_truth_config.as_ref(), &params)
-                .await?;
-        }
-        if justknobs::eval("scm/mononoke:scs_create_repos_in_metagit", None, None)
-            .map_err(|e| scs_errors::internal_error(format!("{e:#}")))?
-        {
-            create_repos_in_metagit(params).await?;
-        }
+        update_repos_acls(ctx.clone(), &params).await?;
+        create_repos_in_mononoke(ctx, self.git_source_of_truth_config.as_ref(), &params).await?;
 
         Ok(thrift::CreateReposToken {
             ..Default::default()
