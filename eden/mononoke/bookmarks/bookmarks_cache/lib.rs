e23b0f69d55fc72d569e12eb7359441ff6450857
Jose García Gimeno <jagg@meta.com>
2025-10-31 02:55:44 -0700

    Introduce scoped trait
    Summary:
    ### Background
    The Bookmarks cache can be configured with various sets of warmers, depending on the specific data requirements of each service.
    
    Currently, different services use different warmer sets, but the Bookmarks service only exposes the Hg set. As a result, these services cannot stop using the Bookmarks cache directly.
    
    With this stack, we are extending the Bookmarks cache to track three distinct pointers: Hg, Git, and all configured warmers. The goal is for the Bookmarks service to configure all warmers and expose all three pointers through its API, enabling different clients to access the cache directly according to their needs.
    
    ### This change
    In this change we will create a new trait that allows clients to request the particular scope they care about. In coming diffs we will start to track these pointers, so that we can return the one that the user needs.
    
    Note that the previous unscoped trait will stay, and will keep on working exactly the same as today.
    
    Reviewed By: gustavoavena
    
    Differential Revision: D85769783
    
    fbshipit-source-id: 8bff7389d689e9fbb5e0d8b68c9f0abaa7ac632b

--- eden/mononoke/bookmarks/bookmarks_cache/lib.rs
+++ eden/mononoke/bookmarks/bookmarks_cache/lib.rs
@@ -34,3 +34,46 @@ pub trait BookmarksCache: Send + Sync {
     /// Awaits the completion of any ongoing update.
     async fn sync(&self, ctx: &CoreContext);
 }
+
+/// The warmers that are configured in the bookmarks_cache are tagged according
+/// to their use. Some of them are used for Hg, some for Git, and some for both.
+/// This enum allows selecting the warmup requirements for the requested bookmark.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum WarmerRequirement {
+    /// All the Hg Warmers must be ready
+    HgOnly,
+
+    /// All the Git Warmers must be ready
+    GitOnly,
+
+    /// All the configured Warmers must be ready
+    AllKinds,
+}
+
+#[async_trait]
+#[facet::facet]
+pub trait ScopedBookmarksCache: Send + Sync {
+    /// Get the ChangesetId pointed to by the given bookmark, scoped to the
+    /// specified WarmerRequirement. The requested changeset is guaranteed to
+    /// have the necessary derived data available.
+    async fn get(
+        &self,
+        ctx: &CoreContext,
+        bookmark: &BookmarkKey,
+        scope: WarmerRequirement,
+    ) -> Result<Option<ChangesetId>, Error>;
+
+    /// List the bookmarks starting with the specified prefix and that are warm
+    /// according to the given requirements.
+    async fn list(
+        &self,
+        ctx: &CoreContext,
+        prefix: &BookmarkPrefix,
+        pagination: &BookmarkPagination,
+        limit: Option<u64>,
+        scope: WarmerRequirement,
+    ) -> Result<Vec<(BookmarkKey, (ChangesetId, BookmarkKind))>, Error>;
+
+    /// Awaits the completion of any ongoing update.
+    async fn sync(&self, ctx: &CoreContext);
+}
