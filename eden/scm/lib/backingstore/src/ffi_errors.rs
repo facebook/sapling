cf8245e469602f566b03c595807664cbe15d503d
Jarrett Dai <jldai@meta.com>
2025-11-06 20:54:12 -0800

    ffi_errors: traverse error chains from the sapling backingstore
    Summary:
    When the backingstore fetches blobs/trees, it ends up with four sources either directly or indirectly:
    
    * SaplingRemoteApi (edenapi is the old name)
    * IndexedLog
    * LFS
    * CAS
    
    The SaplingRemoteApi, IndexedLog and LFS all have interesting error definitions that can carry helpful infos, while cas doesn't seem to have a structured one and falls back to SLAPI in most cases. Currently buck2 also doesn't see many cas errors.
    
    Let's get started with the first three sources.
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D86157233
    
    fbshipit-source-id: c0c59847da7085bff6a672ee1b6f9c34cc505c9e

--- eden/scm/lib/backingstore/src/ffi_errors.rs
+++ eden/scm/lib/backingstore/src/ffi_errors.rs
@@ -7,11 +7,47 @@
 
 use anyhow::Error;
 use cxx::UniquePtr;
+use edenapi::SaplingRemoteApiError;
+use revisionstore::error::LfsFetchError;
 
-use crate::ffi::ffi;
+use crate::ffi::ffi::BackingStoreErrorKind;
+use crate::ffi::ffi::SaplingBackingStoreError;
+use crate::ffi::ffi::backingstore_error;
+use crate::ffi::ffi::backingstore_error_with_code;
+
+fn extract_remote_api_error(_err: &SaplingRemoteApiError) -> (BackingStoreErrorKind, Option<i32>) {
+    (BackingStoreErrorKind::Generic, None)
+}
+
+fn extract_lfs_error(_err: &LfsFetchError) -> (BackingStoreErrorKind, Option<i32>) {
+    (BackingStoreErrorKind::Generic, None)
+}
+
+fn extract_indexedlog_error(_err: &indexedlog::Error) -> BackingStoreErrorKind {
+    BackingStoreErrorKind::Generic
+}
 
 /// Translate anyhow errors from the backinstore
 /// to SaplingBackingStoreError in C++ for EdenFS to consume
-pub(crate) fn into_backingstore_err(err: Error) -> UniquePtr<ffi::SaplingBackingStoreError> {
-    ffi::backingstore_error(&format!("{:?}", err), ffi::BackingStoreErrorKind::Generic)
+pub(crate) fn into_backingstore_err(err: Error) -> UniquePtr<SaplingBackingStoreError> {
+    let msg = format!("{:?}", err);
+    let mut kind = BackingStoreErrorKind::Generic;
+    let mut code: Option<i32> = None;
+    for e in err.chain() {
+        if let Some(e) = e.downcast_ref::<SaplingRemoteApiError>() {
+            (kind, code) = extract_remote_api_error(e);
+            break;
+        } else if let Some(e) = e.downcast_ref::<LfsFetchError>() {
+            (kind, code) = extract_lfs_error(e);
+            break;
+        } else if let Some(e) = e.downcast_ref::<indexedlog::Error>() {
+            kind = extract_indexedlog_error(e);
+            break;
+        }
+    }
+
+    match code {
+        Some(code) => backingstore_error_with_code(&msg, kind, code),
+        None => backingstore_error(&msg, kind),
+    }
 }
