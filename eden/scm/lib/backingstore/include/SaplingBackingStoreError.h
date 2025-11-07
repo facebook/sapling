4405a0899c277913bc300113e60790f7965dccd7
Jarrett Dai <jldai@meta.com>
2025-11-06 20:54:12 -0800

    errors: introduce error kind and error code into SaplingBackingStoreError
    Summary: Extend `SaplingBackingStoreError` class to store extracted error kind and error code at the FFI boundary.
    
    Reviewed By: MichaelCuevas
    
    Differential Revision: D86154273
    
    fbshipit-source-id: 270f0427eb7525a63598cb995b9345c757e41cdb

--- eden/scm/lib/backingstore/include/SaplingBackingStoreError.h
+++ eden/scm/lib/backingstore/include/SaplingBackingStoreError.h
@@ -7,13 +7,42 @@
 
 #pragma once
 
+#include <optional>
 #include <stdexcept>
 
 namespace sapling {
 
+enum class BackingStoreErrorKind : uint8_t {
+  Generic,
+  Network,
+  IO,
+  DataCorruption
+};
+
 class SaplingBackingStoreError : public std::runtime_error {
  public:
-  using std::runtime_error::runtime_error;
+  explicit SaplingBackingStoreError(const std::string& msg)
+      : std::runtime_error(msg),
+        kind_(BackingStoreErrorKind::Generic),
+        code_(std::nullopt) {}
+
+  SaplingBackingStoreError(
+      const std::string& msg,
+      BackingStoreErrorKind kind,
+      std::optional<int64_t> code)
+      : std::runtime_error(msg), kind_(kind), code_(code) {}
+
+  constexpr std::optional<int32_t> code() const {
+    return code_;
+  }
+
+  constexpr BackingStoreErrorKind kind() const {
+    return kind_;
+  }
+
+ private:
+  const BackingStoreErrorKind kind_;
+  const std::optional<int32_t> code_;
 };
 
 } // namespace sapling
