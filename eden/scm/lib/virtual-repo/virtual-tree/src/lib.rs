169df966e28d04ced167a6b22082e2e8d0aa99f3
Jun Wu <quark@meta.com>
2025-11-10 10:05:10 -0800

    virtual-tree: initial crate to generate test trees on the fly
    Summary: See the docstring. Intended for testing use-cases.
    
    Reviewed By: zzl0
    
    Differential Revision: D85181370
    
    fbshipit-source-id: e7e56a565569df4dcd4a2087dc7cc86629a01e45

---
+++ eden/scm/lib/virtual-repo/virtual-tree/src/lib.rs
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! # virtual-tree
+//!
+//! Virtualized trees for testing.
+//!
+//! Goals:
+//! - Random access to the N-th root tree.
+//!   Do not build up trees from 1 to N (O(N)).
+//! - Large state to make the test interesting, with limited space.
+//!   Do not simply zip a repo that can be 100s of MBs or GBs.
+//! - Looks like real-world.
+//!   Diff between N-th and (N+1)-th root trees look reasonably small.
+//!   Tree structure looks reasonably irregular.
+//!
+//! Currently, virtual-tree starts from a serialized list of trees
+//! pre-generated from a real-world repo, then "stretch" the trees
+//! to larger ones to satisfy needs.
