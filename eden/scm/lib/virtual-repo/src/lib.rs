12d9e9221d5e3904696de2c19ddcacd173bf8358
Jun Wu <quark@meta.com>
2025-11-10 11:30:42 -0800

    virtual-repo: initial crate for virtual repo features
    Summary:
    See the `lib.rs` docstring. This crate is a bridge between `virtual-tree` and
    (`eagerepo`, `dag`) to integrate with the rest of source control.
    
    Reviewed By: muirdm
    
    Differential Revision: D85615868
    
    fbshipit-source-id: 7336a5b1cfa1ef1bb145413a60e1760fd8aa9ca0

---
+++ eden/scm/lib/virtual-repo/src/lib.rs
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! # virtual-repo
+//!
+//! Virtualized repo data for testing.
+//!
+//! Implements:
+//! - SHA1-like storage to access commits, trees, and files that are all
+//!   generated on the fly, based on `virtual-tree`.
+//! - Simple `dag` location<->hash lookups
+//!
+//! Integrates with:
+//! - `EagerRepo`'s plug-in SHA1-like storage abstraction.
+//!   `EagerRepo` implements a local storage abstraction and also a remote SLAPI
+//!   abstraction (so the local storage can be `revisionstore`, closer to
+//!   production environment). `virtual-repo` does not re-implement those
+//!   features.
