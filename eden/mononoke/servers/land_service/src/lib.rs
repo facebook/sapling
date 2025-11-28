96862aa7e099c8634e68a8cfbdf005a423a21581
Gustavo Avena <gustavoavena@meta.com>
2025-11-28 10:23:41 -0800

    Migrate land_service to servers/
    Summary: See [this doc](https://docs.google.com/document/d/1JABTCGVK87rQuRALQwtvMVdNHp5Lo1EQoEN2_By6Z34/edit?tab=t.ii65um3utts9)
    
    Reviewed By: markbt
    
    Differential Revision: D87999926
    
    fbshipit-source-id: fd5dbf47e59da395de74689d4047c5795e529675

---
+++ eden/mononoke/servers/land_service/src/lib.rs
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+// This is a placeholder library crate for land_service.
+// The main implementation is in the Facebook-internal server binary.
+// This library exists to support autocargo for OSS builds.
