cddcabcbb56b09b6fefca62d016cb43dc46e09f2
Chris Dinh <cqd@meta.com>
2025-12-03 12:26:16 -0800

    Add script to generate version from commit hash
    Summary:
    Adds a script to generate a version from an input commit hash, or current if not specified.
    based on https://www.internalfb.com/diff/D88055700
    
    For use in build modernization to generate the appropriate( dash separated) commit hash
    
    Adds buck target in case that's required to get the output to fit properly into a conveyor artifact
    
    Reviewed By: vilatto
    
    Differential Revision: D88218082
    
    fbshipit-source-id: f2fdda07349a8a6f181419e19aa3e0d897dc89d3

---
+++ eden/fs/scripts/generate_version_release.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+import argparse
+import datetime
+import subprocess
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generate version/release information from latest hg changeset date"
+    )
+    parser.add_argument("--commit", help="Print commit hash")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--version", action="store_true", help="Print YYYYMMDD")
+    group.add_argument("--release", action="store_true", help="Print HHMMSS")
+    group.add_argument(
+        "--dash-combined", action="store_true", help="Print YYYYMMDD-HHMMSS"
+    )
+    group.add_argument(
+        "--dot-combined", action="store_true", help="Print YYYYMMDD.HHMMSS"
+    )
+    args = parser.parse_args()
+
+    # Obtain changeset epoch seconds from hg
+    rev_epoch = int(
+        subprocess.check_output(
+            [
+                "hg",
+                "log",
+                "-l1",
+                f"-r {args.commit}" if args.commit else "",
+                "--template",
+                "{date}",
+            ],
+            text=True,
+        ).split(".", 1)[0]
+    )
+
+    dt = datetime.datetime.fromtimestamp(rev_epoch)
+    version = dt.strftime("%Y%m%d")
+    release = dt.strftime("%H%M%S")
+
+    if args.version:
+        print(version)
+    elif args.release:
+        print(release)
+    elif args.dash_combined:
+        print(f"{version}-{release}")
+    elif args.dot_combined:
+        print(f"{version}.{release}")
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
