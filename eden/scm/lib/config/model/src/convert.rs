540d1cee61d88673b12dce5369ec6f4a8a39c7a0
Eddie Shen <xes@meta.com>
2025-12-05 01:11:05 -0800

    Replace Duration::from_secs(7200) to Duration::from_hours(2)
    Summary:
    X-link: https://github.com/meta-pytorch/monarch/pull/2067
    
    Rust 1.91 introduced `Duration::from_hours`.
    
    This diff replaces some instances of `Duration::from_secs` with the corresponding `Duration::from_hours`.
    
    Reviewed By: dtolnay
    
    Differential Revision: D88441307
    
    fbshipit-source-id: 3186fae5d60b3710b9ddc9cbf59c0bbecd5dd699

--- eden/scm/lib/config/model/src/convert.rs
+++ eden/scm/lib/config/model/src/convert.rs
@@ -538,7 +538,7 @@ mod tests {
 
         assert_eq!(Duration::try_from_str("555ns")?, Duration::from_nanos(555));
         assert_eq!(Duration::try_from_str("1m")?, Duration::from_mins(1));
-        assert_eq!(Duration::try_from_str("2h")?, Duration::from_secs(7200));
+        assert_eq!(Duration::try_from_str("2h")?, Duration::from_hours(2));
         assert_eq!(Duration::try_from_str("5ms")?, Duration::from_millis(5));
         assert_eq!(Duration::try_from_str("123us")?, Duration::from_micros(123));
         assert_eq!(Duration::try_from_str("1.5h")?, Duration::from_mins(90));
