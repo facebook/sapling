f15b940f90a1ac824070e149153b2b0433d47538
Jun Wu <quark@meta.com>
2025-12-12 17:04:14 -0800

    sampling-profiler: add Linux osutil
    Summary:
    This crate is for hybrid (native + Python) profiling.
    
    Currently we have a bunch of profiling choices, but they have their limitations:
    
    | Name      | Profiling language | Requires manual annotation | Overhead  | Type     |
    |-----------|--------------------|----------------------------|-----------|----------|
    | statprof  | Python             | No                         | Low       | Sampling |
    | traceprof | Python             | No                         | Low       | Tracing  |
    | samply    | Rust               | No                         | Low       | Sampling |
    | uftrace   | Rust               | No                         | Very high | Tracing  |
    | tracing   | Python + Rust      | Yes                        | Low       | Tracing  |
    | (this)    | Python + Rust      | No                         | Low       | Sampling |
    
    We had an always-on low-frequency sampling profiler configured (D9828548 and
    D9828550) that help pinpoint slowness for long running commands. However, that
    only works for the Python codebase. This crate intends to replicate the low-freq
    background profiling experience to the Rust + Python hybrid codebase.
    
    Reviewed By: muirdm
    
    Differential Revision: D88778612
    
    fbshipit-source-id: 00c4c1323dcf7dd94a9fcaa65ececdec1be3ceb9

---
+++ eden/scm/lib/sampling-profiler/src/lib.rs
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! Sampling profiler for Sapling.
+//!
+//! - Sample the main thread periodically (ex. every second)
+//! - Resolve Python frames (by backtrace-python)
+//!
+//! Currently implemented for Linux.
+
+mod osutil;
+
+pub use backtrace_ext; // re-export
---
+++ eden/scm/lib/sampling-profiler/src/osutil.rs
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! Operation system features used by the profiler.
+
+use std::io;
+use std::mem;
+
+// Block `sig` signals. Explicitly opt-out profiling for the current thread
+// and new threads spawned from the current thread.
+pub fn block_signal(sig: libc::c_int) {
+    sigmask_sigprof(sig, true);
+}
+
+/// Unblock `sig` to enable profiling.
+pub fn unblock_signal(sig: libc::c_int) {
+    sigmask_sigprof(sig, false);
+}
+
+// Get the current thread id. Must be async-signal-safe.
+pub fn get_thread_id() -> libc::pid_t {
+    #[cfg(target_os = "linux")]
+    unsafe {
+        return libc::syscall(libc::SYS_gettid) as libc::pid_t;
+    }
+    #[cfg(not(target_os = "linux"))]
+    unimplemented!()
+}
+
+/// Create a pipe for SIGPROF signal handler use.
+/// The SIGPROF handler sends raw stack trace info to the pipe.
+/// The other end of the pipe consumes the data and might resolve symbols.
+/// The pipe is configured to have a larger buffer, so it's less likely to block.
+/// Returns `[read_fd, write_fd]`.
+pub fn setup_pipe() -> io::Result<[libc::c_int; 2]> {
+    unsafe {
+        let mut pipe_fds: [libc::c_int; 2] = [0; 2];
+
+        if libc::pipe2(pipe_fds.as_mut_ptr(), libc::O_DIRECT) != 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        // F_SETPIPE_SZ is linux specific.
+        #[cfg(target_os = "linux")]
+        {
+            let buffer_size = 1 << 6;
+            // Failing to set buffer size is not fatal.
+            let _ = libc::fcntl(pipe_fds[1], libc::F_SETPIPE_SZ, buffer_size);
+        }
+
+        Ok(pipe_fds)
+    }
+}
+
+/// Setup the signal handler. This is POSIX-only.
+pub fn setup_signal_handler(
+    sig: libc::c_int,
+    signal_handler: extern "C" fn(libc::c_int, *const libc::siginfo_t, *const libc::c_void),
+) -> io::Result<()> {
+    unsafe {
+        let mut sa: libc::sigaction = std::mem::zeroed();
+        sa.sa_sigaction = signal_handler as usize;
+        sa.sa_flags = libc::SA_RESTART | libc::SA_SIGINFO;
+        libc::sigemptyset(&mut sa.sa_mask);
+
+        if libc::sigaction(sig, &sa, std::ptr::null_mut()) != 0 {
+            return Err(io::Error::last_os_error());
+        }
+    }
+
+    Ok(())
+}
+
+/// Send `sig` to `tid` at the specified interval. This is a Linux-only feature.
+/// Returns the timer handle that can be used to stop the timer later.
+pub fn setup_signal_timer(
+    sig: libc::c_int,
+    tid: libc::pid_t,
+    interval_secs: i64,
+    interval_nsecs: i64,
+) -> io::Result<libc::timer_t> {
+    #[cfg(target_os = "linux")]
+    unsafe {
+        let mut sev: libc::sigevent = mem::zeroed();
+        sev.sigev_notify = libc::SIGEV_THREAD_ID;
+        sev.sigev_signo = sig;
+        sev.sigev_notify_thread_id = tid;
+
+        let mut timer: libc::timer_t = mem::zeroed();
+
+        // CLOCK_MONOTONIC does not include system suspend time.
+        if libc::timer_create(libc::CLOCK_MONOTONIC, &mut sev, &mut timer) != 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        let mut spec: libc::itimerspec = mem::zeroed();
+        spec.it_interval.tv_sec = interval_secs;
+        spec.it_interval.tv_nsec = interval_nsecs;
+        spec.it_value.tv_sec = interval_secs;
+        spec.it_value.tv_nsec = interval_nsecs;
+
+        if libc::timer_settime(timer, 0, &spec, std::ptr::null_mut()) != 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        return Ok(timer);
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    unimplemented!();
+}
+
+/// Stop and delete a signal timer created by `setup_signal_timer`.
+pub fn stop_signal_timer(timer: libc::timer_t) -> io::Result<()> {
+    #[cfg(target_os = "linux")]
+    unsafe {
+        if libc::timer_delete(timer) != 0 {
+            return Err(io::Error::last_os_error());
+        }
+        return Ok(());
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    unimplemented!();
+}
+
+fn sigmask_sigprof(sig: libc::c_int, block: bool) {
+    unsafe {
+        let mut set: libc::sigset_t = mem::zeroed();
+        libc::sigemptyset(&mut set);
+        libc::sigaddset(&mut set, sig);
+        let how = match block {
+            true => libc::SIG_BLOCK,
+            _ => libc::SIG_UNBLOCK,
+        };
+        libc::pthread_sigmask(how, &set, std::ptr::null_mut());
+    }
+}
