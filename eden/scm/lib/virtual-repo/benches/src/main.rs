40ee4f7bd955338131329de2cdaceca9f8c95a39
Jun Wu <quark@meta.com>
2025-12-15 13:23:08 -0800

    virtual-repo/benches: add benchmark about reading trees+blobs
    Summary:
    This benchmark can predict EdenFS performance with the large factor_bits:
    
    Example `cargo run --release`:
    
      visit trees factor_bits=2 depth=4                   5.617 ms
        factor_bits=2 depth=4 => avg_size=14478
          TreeStat { size: 23686561, files: 1636, dirs: 380 }
      visit trees factor_bits=4 depth=2                   0.227 ms
        factor_bits=4 depth=2 => avg_size=17185
          TreeStat { size: 687410, files: 40, dirs: 35 }
      visit trees factor_bits=8 depth=2                   1.647 ms
        factor_bits=8 depth=2 => avg_size=197545
          TreeStat { size: 13433122, files: 68, dirs: 69 }
      visit trees factor_bits=12 depth=2                 32.401 ms
        factor_bits=12 depth=2 => avg_size=3113854
          TreeStat { size: 298930043, files: 96, dirs: 119 }
      visit trees factor_bits=20 depth=1                  3.236 s
        factor_bits=20 depth=1 => avg_size=824481200
          TreeStat { size: 5771368401, files: 7, dirs: 23 }
    
    It shows that the average file size grows to 0.8GB for factor_bits=20, which
    seems off and needs change.
    
    Reviewed By: zzl0
    
    Differential Revision: D89207284
    
    fbshipit-source-id: 0e40e2d20de1db2260813fcbac306aba33c7b545

--- eden/scm/lib/virtual-repo/benches/src/main.rs
+++ eden/scm/lib/virtual-repo/benches/src/main.rs
@@ -7,10 +7,20 @@
 
 //! Benchmark for virtual-tree algorithms.
 
+use std::sync::OnceLock;
+
+use dag::Dag;
 use minibench::bench;
 use minibench::elapsed;
+use nonblocking::non_blocking_result as n;
+use storemodel::TreeItemFlag;
+use types::Id20;
+use types::SerializationFormat;
+use virtual_repo::VirtualRepoProvider;
 use virtual_repo::text_gen::generate_file_content_of_length;
 use virtual_repo::text_gen::generate_paragraphs;
+
+const FORMAT: SerializationFormat = SerializationFormat::Hg;
 
 fn main() {
     for (size, n) in [(3000, 10000), (10_000_000, 1)] {
@@ -37,4 +47,70 @@ fn main() {
             })
         });
     }
+
+    // Scan the first 2-level of trees of
+    let provider = VirtualRepoProvider::new(FORMAT);
+    manifest_tree::init();
+    for (factor_bits, depth) in [(2u8, 4), (4, 2), (8, 2), (12, 2), (20, 1)] {
+        let stat_str = OnceLock::new();
+        bench(
+            format!("visit trees factor_bits={factor_bits} depth={depth}"),
+            || {
+                let dir = tempfile::tempdir().unwrap();
+                let mut dag = Dag::open(dir.path()).unwrap();
+                let head_id = n(virtual_repo::populate_dag(&mut dag, factor_bits)).unwrap();
+                let head_id20 = Id20::from_slice(head_id.as_ref()).unwrap();
+                let commit_body = provider.get_content(head_id20).unwrap();
+                let root_tree_id20 =
+                    format_util::commit_text_to_root_tree_id(&commit_body, FORMAT).unwrap();
+                elapsed(|| {
+                    let mut stat = TreeStat::default();
+                    visit_tree(&provider, root_tree_id20, depth, &mut stat);
+                    stat_str.get_or_init(|| {
+                        let avg_size = stat.size / stat.files;
+                        format!("  factor_bits={factor_bits} depth={depth} => avg_size={avg_size}\n    {stat:?}")
+                    });
+                })
+            },
+        );
+        if let Some(stat_str) = stat_str.get() {
+            eprintln!("{}", stat_str);
+        }
+    }
 }
+
+#[derive(Debug, Default)]
+struct TreeStat {
+    size: usize,
+    files: usize,
+    dirs: usize,
+}
+
+/// Recursively read trees and blobs up-to `depth`. `depth=0` means reading nothing.
+fn visit_tree(provider: &VirtualRepoProvider, tree_id20: Id20, depth: usize, stat: &mut TreeStat) {
+    if depth == 0 {
+        return;
+    }
+    let tree_data = provider.get_content(tree_id20).unwrap();
+    for entry in storemodel::basic_parse_tree(tree_data, FORMAT)
+        .unwrap()
+        .iter()
+        .unwrap()
+    {
+        let (_name, id20, flag) = entry.unwrap();
+        match flag {
+            TreeItemFlag::File(_file_type) => {
+                stat.files += 1;
+                let blob = provider.get_content(id20).unwrap();
+                stat.size += blob.len();
+            }
+            TreeItemFlag::Directory => {
+                stat.dirs += 1;
+                visit_tree(provider, id20, depth - 1, stat);
+            }
+        }
+    }
+}
+
+// Supports turning on tracing via LOG=...
+dev_logger::init!();
