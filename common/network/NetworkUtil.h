901d43f91407db69340a4afbc2d7465f4e2bbc2f
benoit-nexthop <benoit@nexthop.ai>
2025-12-18 16:27:06 -0800

    enable fboss2 CLI tests in the OSS build
    Summary:
    **Pre-submission checklist**
    - [x] I've ran the linters locally and fixed lint errors related to the files I modified in this PR. You can install the linters by running `pip install -r requirements-dev.txt && pre-commit install`
    - [x] `pre-commit run`
    
    The `fboss2` CLI has a number of unit tests, however none of them are being built/run in the OSS build. Turn the CLI into a library in the cmake build, just like how it’s done in the Buck build, and add a new cmake file for the unit tests that leverages this library.
    
    Various unit tests were including `nettools/common/TestUtils.h`, which is not open source, however they generally didn’t need that header and only needed a header from thrift from the most part.
    
    Some simple stubs were incomplete. The BGP server is taken out when building with `IS_OSS`. This change tries to keep the OSS build as close as possible to the internal build, with minimal usage of `#ifdef`s.
    
    X-link: https://github.com/facebook/fboss/pull/628
    
    Reviewed By: shiva-menta
    
    Differential Revision: D89410954
    
    Pulled By: joseph5wu
    
    fbshipit-source-id: ee163fdba31b2a0d6768cd927681daba27d4c303

--- common/network/NetworkUtil.h
+++ common/network/NetworkUtil.h
@@ -11,19 +11,89 @@
 #ifndef COMMON_NETWORKUTIL_H
 #define COMMON_NETWORKUTIL_H 1
 
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <cstring>
+#include <string>
+
 namespace facebook {
 namespace network {
 
-/* Stub util for OSS build */
+/* Network utility functions for OSS build */
 class NetworkUtil {
  public:
+  // Resolve hostname to IP address
   static std::string getHostByName(
       const std::string& host,
       bool disableIpv6 = false) {
-    return "";
+    struct addrinfo hints, *result = nullptr;
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = disableIpv6 ? AF_INET : AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int status = getaddrinfo(host.c_str(), nullptr, &hints, &result);
+    if (status != 0 || result == nullptr) {
+      return "";
+    }
+
+    char ipstr[INET6_ADDRSTRLEN];
+    void* addr = nullptr;
+
+    if (result->ai_family == AF_INET) {
+      struct sockaddr_in* ipv4 = (struct sockaddr_in*)result->ai_addr;
+      addr = &(ipv4->sin_addr);
+    } else if (result->ai_family == AF_INET6) {
+      struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)result->ai_addr;
+      addr = &(ipv6->sin6_addr);
+    }
+
+    std::string ipAddress;
+    if (addr != nullptr &&
+        inet_ntop(result->ai_family, addr, ipstr, sizeof(ipstr)) != nullptr) {
+      ipAddress = ipstr;
+    }
+
+    freeaddrinfo(result);
+    return ipAddress;
   }
+
+  // Reverse DNS lookup: IP address to hostname
   static std::string getHostByAddr(const std::string& ip) {
-    return "";
+    struct sockaddr_in sa4;
+    struct sockaddr_in6 sa6;
+    struct sockaddr* sa = nullptr;
+    socklen_t salen = 0;
+
+    // Try IPv4 first
+    if (inet_pton(AF_INET, ip.c_str(), &(sa4.sin_addr)) == 1) {
+      sa4.sin_family = AF_INET;
+      sa = (struct sockaddr*)&sa4;
+      salen = sizeof(sa4);
+    } else if (inet_pton(AF_INET6, ip.c_str(), &(sa6.sin6_addr)) == 1) {
+      sa6.sin6_family = AF_INET6;
+      sa = (struct sockaddr*)&sa6;
+      salen = sizeof(sa6);
+    } else {
+      // Invalid IP address
+      return "";
+    }
+
+    char hostname[NI_MAXHOST];
+    // Use NI_NAMEREQD to ensure we get a name (not just the IP back)
+    // and default flags to get FQDN
+    int status = getnameinfo(
+        sa, salen, hostname, sizeof(hostname), nullptr, 0, NI_NAMEREQD);
+    if (status != 0) {
+      // If NI_NAMEREQD fails, try without it (will return IP if no name found)
+      status =
+          getnameinfo(sa, salen, hostname, sizeof(hostname), nullptr, 0, 0);
+      if (status != 0) {
+        return "";
+      }
+    }
+
+    return std::string(hostname);
   }
 };
 } // namespace network
