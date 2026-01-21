03f174b9245fc64829dd607d0c245557ec23ed5e
Jun Wu <quark@meta.com>
2026-01-21 12:57:00 -0800

    sampling-profiler: add an example binary target
    Summary: Add an example that exercises the sampling profiler to see how it works.
    
    Reviewed By: muirdm
    
    Differential Revision: D88778615
    
    fbshipit-source-id: cf859273b624b575b5496e24dd3eef4dc54b9094

---
+++ eden/scm/lib/sampling-profiler/examples/main.rs
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
+use std::time::Duration;
+
+use cpython_ext::cpython::*;
+use sampling_profiler::Profiler;
+
+fn native_fib(py: Python, n: u64) -> PyResult<u64> {
+    py.allow_threads(|| std::thread::sleep(Duration::from_millis(10)));
+    let v = if n <= 1 {
+        n
+    } else {
+        let py_fib = py.eval("py_fib", None, None)?;
+        let v1: u64 = py_fib.call(py, (n - 1,), None)?.extract(py)?;
+        let v2: u64 = py_fib.call(py, (n - 2,), None)?.extract(py)?;
+        v1 + v2
+    };
+    Ok(v)
+}
+
+fn do_some_work(py: Python) {
+    let code = r#"
+import time, sys
+def py_fib(n):
+    time.sleep(0.01)
+    if n <= 1:
+        return n
+    return sys.native_fib(n - 1) + sys.native_fib(n - 2)
+
+print(f"{py_fib(11)=}")
+"#;
+    let sys = py.import("sys").unwrap();
+    sys.add(py, "native_fib", py_fn!(py, native_fib(n: u64)))
+        .unwrap();
+    py.run(code, None, None).unwrap();
+}
+
+fn print_traceback(bt: &[String]) {
+    static TICK: AtomicUsize = AtomicUsize::new(0);
+    println!(
+        "Traceback #{} ({} frames, most recent call first):",
+        TICK.fetch_add(1, Ordering::AcqRel) + 1,
+        bt.len()
+    );
+    for name in bt {
+        println!("  {}", name);
+    }
+}
+
+fn main() {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+
+    println!(
+        "Python frame resolution support: {:?}",
+        &*backtrace_python::SUPPORTED_INFO
+    );
+
+    backtrace_python::init();
+
+    let profiler = Profiler::new(Duration::from_millis(500), Box::new(print_traceback)).unwrap();
+    do_some_work(py);
+    drop(profiler);
+}
