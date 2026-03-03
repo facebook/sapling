3d3a01b84d8ab2085159e3d9df859498255d9f9d
Youssef Ibrahim <youssefsalama@meta.com>
2026-03-03 02:00:35 -0800

    blobstore_sync_queue: delete legacy sync queue code
    Summary: We've long since migrated to the WAL implementation of the sync queue. Let's delete the legacy code.
    
    Reviewed By: lmvasquezg
    
    Differential Revision: D94899470
    
    fbshipit-source-id: df15bd6f75d129ee5fe3f51b5cbb29b11933d75b

--- eden/mononoke/blobstore_sync_queue/src/lib.rs
+++ eden/mononoke/blobstore_sync_queue/src/lib.rs
@@ -7,58 +7,7 @@
 
 mod write_ahead_log;
 
-use sql::mysql;
-use sql::mysql_async::FromValueError;
-use sql::mysql_async::Value;
-use sql::mysql_async::prelude::ConvIr;
-use sql::mysql_async::prelude::FromValue;
-use uuid::Uuid;
 pub use write_ahead_log::BlobstoreWal;
 pub use write_ahead_log::BlobstoreWalEntry;
 pub use write_ahead_log::SqlBlobstoreWal;
 pub use write_ahead_log::SqlBlobstoreWalBuilder;
-
-// Identifier for given blobstore operation to facilitate correlating same operation
-// across multiple blobstores.
-#[derive(Clone, Debug, Eq, PartialEq, Hash, mysql::OptTryFromRowField)]
-pub struct OperationKey(pub Uuid);
-impl OperationKey {
-    pub fn r#gen() -> OperationKey {
-        OperationKey(Uuid::new_v4())
-    }
-
-    pub fn is_null(&self) -> bool {
-        self == &OperationKey(Uuid::nil())
-    }
-}
-
-impl From<OperationKey> for Value {
-    fn from(id: OperationKey) -> Self {
-        let OperationKey(uuid) = id;
-        Value::Bytes(uuid.as_bytes().to_vec())
-    }
-}
-
-impl ConvIr<OperationKey> for OperationKey {
-    fn new(v: Value) -> Result<Self, FromValueError> {
-        match v {
-            Value::Bytes(bytes) => Ok(OperationKey(
-                Uuid::from_slice(&bytes[..])
-                    .map_err(move |_| FromValueError(Value::Bytes(bytes)))?,
-            )),
-            v => Err(FromValueError(v)),
-        }
-    }
-
-    fn commit(self) -> Self {
-        self
-    }
-
-    fn rollback(self) -> Value {
-        self.into()
-    }
-}
-
-impl FromValue for OperationKey {
-    type Intermediate = OperationKey;
-}
