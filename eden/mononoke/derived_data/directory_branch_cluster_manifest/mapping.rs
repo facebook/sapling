87bfd6a75af8be02f8998d327512555743d2e914
generatedunixname1252589140134566 <noreply+1252589140134566@fb.com>
2026-06-01 10:52:45 -0700

    Fix clippy::uninlined_format_args issues in fbcode/eden/mononoke/derived_data/directory_branch_cluster_manifest
    Reviewed By: diliop
    
    Differential Revision: D107062343
    
    fbshipit-source-id: 0c5a6e59a9a3442899384f2fe2ed3cb913c9fed2

--- eden/mononoke/derived_data/directory_branch_cluster_manifest/mapping.rs
+++ eden/mononoke/derived_data/directory_branch_cluster_manifest/mapping.rs
@@ -33,7 +33,7 @@ pub struct RootDirectoryBranchClusterManifestId(pub(crate) DirectoryBranchCluste
 pub fn format_key(derivation_ctx: &DerivationContext, changeset_id: ChangesetId) -> String {
     let root_prefix = "derived_root_dbcm.";
     let key_prefix = derivation_ctx.mapping_key_prefix::<RootDirectoryBranchClusterManifestId>();
-    format!("{}{}{}", root_prefix, key_prefix, changeset_id)
+    format!("{root_prefix}{key_prefix}{changeset_id}")
 }
 
 impl TryFrom<BlobstoreBytes> for RootDirectoryBranchClusterManifestId {
