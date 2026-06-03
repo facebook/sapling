6ae000147ff29a50b1c0b519ee17b73ea525805d
generatedunixname1252589140134566 <noreply+1252589140134566@fb.com>
2026-06-03 10:53:20 -0700

    Fix clippy::uninlined_format_args issues in fbcode/eden/scm/lib/commands/debugcommands/cmddebugdumpindexedlog/src
    Differential Revision: D107217239
    
    fbshipit-source-id: e0c9e04b6225803c14c8190a3d8aae155889812c

--- eden/scm/lib/commands/debugcommands/cmddebugdumpindexedlog/src/lib.rs
+++ eden/scm/lib/commands/debugcommands/cmddebugdumpindexedlog/src/lib.rs
@@ -23,20 +23,20 @@ pub fn run(ctx: ReqCtx<DebugArgsOpts>) -> Result<u8> {
     let mut ferr = ctx.io().error();
     let mut fout = ctx.io().output();
     for path in ctx.opts.args {
-        let _ = write!(fout, "{}\n", path);
+        let _ = write!(fout, "{path}\n");
         let path = Path::new(&path);
         if let Ok(meta) = indexedlog::log::LogMetadata::read_file(path) {
-            write!(ferr, "Metadata File {:?}\n{:?}\n", path, meta)?;
+            write!(ferr, "Metadata File {path:?}\n{meta:?}\n")?;
         } else if path.is_dir() {
             // Treat it as Log.
             let log = indexedlog::log::Log::open(path, Vec::new())?;
-            write!(ferr, "Log Directory {:?}:\n{:#?}\n", path, log)?;
+            write!(ferr, "Log Directory {path:?}:\n{log:#?}\n")?;
         } else if path.is_file() {
             // Treat it as Index.
             let idx = indexedlog::index::OpenOptions::new().open(path)?;
-            write!(ferr, "Index File {:?}\n{:?}\n", path, idx)?;
+            write!(ferr, "Index File {path:?}\n{idx:?}\n")?;
         } else {
-            write!(ferr, "Path {:?} is not a file or directory.\n\n", path)?;
+            write!(ferr, "Path {path:?} is not a file or directory.\n\n")?;
         }
     }
     Ok(0)
