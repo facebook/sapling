3d1e52e782447f519272b0b592f7a21bd517903d
Muir Manders <muirdm@meta.com>
2026-06-11 17:26:07 -0700

    privhelper: derive owner from real uid for setuid launch
    Summary:
    Basically, don't let the user launching privhelper control privhelper's uid/gid. This applies to setuid mode, and user running privhelper directly. If user is root (i.e. "sudo edenfs_privhelper"), we still respect the CLI flags.
    
    We log when the CLI specified values mismatch with what we are using.
    
    This behavior change can be rolled back using `/etc/eden/disable_privhelper_hardening` "secure" touch file.
    
    Reviewed By: genevievehelsel
    
    Differential Revision: D108304055
    
    fbshipit-source-id: 00ab0160df8649518b562c49a88ec5fe1586564e

--- eden/fs/service/PrivHelperMain.cpp
+++ eden/fs/service/PrivHelperMain.cpp
@@ -5,17 +5,67 @@
  * GNU General Public License version 2.
  */
 
+#include <string>
+
 #include <folly/Exception.h>
 #include <folly/File.h>
 #include <folly/FileUtil.h>
 #include <folly/init/Init.h>
 #include <folly/logging/LogConfigParser.h>
 #include <folly/logging/xlog.h>
+#include <folly/portability/Unistd.h>
 #include "eden/common/utils/UserInfo.h"
 #include "eden/fs/privhelper/PrivHelperFlags.h"
+#include "eden/fs/privhelper/PrivHelperRollback.h"
 #include "eden/fs/privhelper/PrivHelperServer.h"
 
 using namespace facebook::eden;
+
+namespace {
+struct PrivHelperOwner {
+  uid_t uid;
+  gid_t gid;
+};
+
+PrivHelperOwner resolvePrivHelperOwner(
+    uid_t realUid,
+    gid_t realGid,
+    uid_t cliUid,
+    gid_t cliGid) {
+  if (realUid == 0) {
+    return {cliUid, cliGid};
+  }
+
+  // A non-root real uid means argv is controlled by the caller, including the
+  // installed setuid-root path. Only real-root launches may nominate a
+  // different Eden owner, such as the sudo/dev flow using SUDO_UID.
+  const auto hardeningDisabled = disablePrivHelperHardening();
+  const auto ownerMismatch = cliUid != realUid || cliGid != realGid;
+  if (ownerMismatch) {
+    auto reason = std::string{"using real uid/gid"};
+    if (hardeningDisabled) {
+      reason = "honoring CLI values because `" +
+          std::string{kDisablePrivHelperHardeningPath} + "` is present";
+    }
+    XLOGF(
+        WARNING,
+        "CLI-provided privhelper uid/gid {}/{} do not match real uid/gid {}/{}; {}",
+        cliUid,
+        cliGid,
+        realUid,
+        realGid,
+        reason);
+  } else if (hardeningDisabled) {
+    XLOGF(
+        WARNING,
+        "Using CLI-provided privhelper uid/gid because `{}` is present",
+        kDisablePrivHelperHardeningPath);
+  }
+
+  return hardeningDisabled ? PrivHelperOwner{cliUid, cliGid}
+                           : PrivHelperOwner{realUid, realGid};
+}
+} // namespace
 
 DEFINE_int32(
     privhelper_uid,
@@ -43,8 +93,13 @@ int main(int argc, char** argv) {
 
     folly::File serverConn(FLAGS_privhelper_fd, true);
 
-    server.init(
-        std::move(serverConn), FLAGS_privhelper_uid, FLAGS_privhelper_gid);
+    const auto realUid = getuid();
+    const auto realGid = getgid();
+    const auto cliUid = static_cast<uid_t>(FLAGS_privhelper_uid);
+    const auto cliGid = static_cast<gid_t>(FLAGS_privhelper_gid);
+    const auto owner = resolvePrivHelperOwner(realUid, realGid, cliUid, cliGid);
+
+    server.init(std::move(serverConn), owner.uid, owner.gid);
     server.run();
     return 0;
   } catch (const std::exception& ex) {
