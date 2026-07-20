3ecadbfe4d8e63a3ff2d8af49946136639550796
Jan Mazur <mzr@meta.com>
2026-07-20 07:34:27 -0700

    Back out "remove sqlblobgc"
    Summary:
    Original commit changeset: ab84cc131ead
    
    Original Phabricator Diff: D112814190
    
    Bringing this back. It was a test.
    
    Reviewed By: RajivTS
    
    Differential Revision: D112815106
    
    fbshipit-source-id: de1ee6b3ba3643319e85f0908cbbc18ea6402069

---
+++ eden/mononoke/tools/sqlblob_gc/src/commands/generation_size.rs
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+use std::collections::HashMap;
+
+use anyhow::Result;
+use bytesize::ByteSize;
+use clap::Parser;
+use futures::TryFutureExt;
+use futures::stream;
+use futures::stream::StreamExt;
+use futures::stream::TryStreamExt;
+use mononoke_app::MononokeApp;
+
+use crate::MononokeSQLBlobGCArgs;
+use crate::utils;
+
+/// measure generation sizes
+#[derive(Parser)]
+pub struct CommandArgs {}
+
+fn print_sizes(sizes: &HashMap<Option<u64>, u64>) {
+    let generations = {
+        let mut keys: Vec<_> = sizes.keys().collect();
+        keys.sort_unstable();
+        keys
+    };
+
+    println!("Generation | Size");
+    println!("-----------------");
+
+    let mut total_size = ByteSize::b(0);
+    for generation in generations {
+        let size = ByteSize::b(sizes[generation]);
+        let generation = match generation {
+            None => "NULL".to_string(),
+            Some(g) => g.to_string(),
+        };
+        println!("{:>10} | {}", generation, size.display().iec());
+        total_size += size;
+    }
+    println!("Total size: {}", total_size.display().iec());
+}
+
+pub async fn run(app: MononokeApp, _args: CommandArgs) -> Result<()> {
+    let common_args: MononokeSQLBlobGCArgs = app.args()?;
+    let ctx = app.new_basic_context();
+
+    let max_parallelism: usize = common_args.scheduled_max;
+
+    let (sqlblob, shard_range) = utils::get_sqlblob_and_shard_range(&app).await?;
+
+    let scuba_sample_builder = app.environment().scuba_sample_builder.clone();
+
+    let shard_sizes: Vec<(usize, HashMap<Option<u64>, (u64, u64)>)> = stream::iter(shard_range)
+        .map(|shard| {
+            sqlblob
+                .get_chunk_sizes_by_generation(&ctx, shard)
+                .map_ok(move |gen_to_size| (shard, gen_to_size))
+        })
+        .buffer_unordered(max_parallelism)
+        .try_collect::<Vec<(usize, HashMap<Option<u64>, (u64, u64)>)>>()
+        .await?;
+
+    let mut size_summary = HashMap::new();
+    let mut total_size: u64 = 0;
+    for (shard, sizes) in shard_sizes {
+        for (generation, (size, chunk_id_count)) in sizes.into_iter() {
+            let mut sample = scuba_sample_builder.clone();
+            sample.add("shard", shard);
+            sample.add_opt("generation", generation);
+            sample.add("size", size);
+            sample.add("chunk_id_count", chunk_id_count);
+            sample.log();
+            *size_summary.entry(generation).or_insert(0u64) += size;
+            total_size += size;
+        }
+    }
+    if total_size > 0 {
+        let mut sample = scuba_sample_builder.clone();
+        sample.add("storage_total_footprint", total_size);
+        sample.log();
+    }
+
+    if scuba_sample_builder.is_discard() {
+        print_sizes(&size_summary);
+    }
+    Ok(())
+}
---
+++ eden/mononoke/tools/sqlblob_gc/src/commands/mark.rs
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This software may be used and distributed according to the terms of the
+ * GNU General Public License version 2.
+ */
+
+use std::sync::Arc;
+use std::time::Duration;
+
+use anyhow::Context;
+use anyhow::Result;
+use anyhow::anyhow;
+use clap::Parser;
+use context::CoreContext;
+use futures::channel::mpsc;
+use futures::sink::SinkExt;
+use futures::stream;
+use futures::stream::StreamExt;
+use futures::stream::TryStreamExt;
+use futures_retry::retry;
+use mononoke_app::MononokeApp;
+use mononoke_macros::mononoke;
+use sqlblob::Sqlblob;
+use tracing::info;
+
+use crate::MononokeSQLBlobGCArgs;
+use crate::utils;
+
+const BASE_RETRY_DELAY: Duration = Duration::from_secs(1);
+const RETRIES: usize = 3;
+
+/// mark referenced blobs as not safe to delete
+#[derive(Parser)]
+pub struct CommandArgs {
+    /// Only set generation on blobs that have no generation set yet. Do not do a full mark.
+    #[clap(long)]
+    initial_generation_only: bool,
+    /// Only do the mark; do not set generation on blobs with no generation set yet.
+    #[clap(long)]
+    skip_initial_generation: bool,
+    /// Only set the generation, don't inline small values
+    #[clap(long)]
+    skip_inline_small_values: bool,
+}
+
+async fn handle_one_key(
+    ctx: CoreContext,
+    key: String,
+    store: Arc<Sqlblob>,
+    inline_small_values: bool,
+    mark_generation: u64,
+) -> Result<()> {
+    retry(
+        |_| store.set_generation(&ctx, &key, inline_small_values, mark_generation),
+        BASE_RETRY_DELAY,
+    )
+    .binary_exponential_backoff()
+    .max_attempts(RETRIES)
+    .inspect_err(|attempt, _err| info!("attempt {attempt} of {RETRIES} failed"))
+    .await
+    .with_context(|| anyhow!("Failed to handle {key} after {RETRIES} attempts"))?;
+    Ok(())
+}
+
+async fn handle_initial_generation(ctx: &CoreContext, store: &Sqlblob, shard: usize) -> Result<()> {
+    retry(
+        |_| store.set_initial_generation(ctx, shard),
+        BASE_RETRY_DELAY,
+    )
+    .binary_exponential_backoff()
+    .max_attempts(RETRIES)
+    .inspect_err(|attempt, _err| info!("attempt {attempt} of {RETRIES} failed"))
+    .await
+    .with_context(|| {
+        anyhow!("Failed to handle initial generation on shard {shard} after {RETRIES} retries",)
+    })?;
+    info!("Completed initial generation handling on shard {}", shard);
+    Ok(())
+}
+
+pub async fn run(app: MononokeApp, args: CommandArgs) -> Result<()> {
+    let common_args: MononokeSQLBlobGCArgs = app.args()?;
+    let ctx = app.new_basic_context();
+
+    let max_parallelism: usize = common_args.scheduled_max;
+
+    let (sqlblob, shard_range) = utils::get_sqlblob_and_shard_range(&app).await?;
+
+    if !args.skip_initial_generation {
+        info!("Starting initial generation set");
+        let set_initial_generation_futures: Vec<_> = shard_range
+            .clone()
+            .map(|shard| Ok(handle_initial_generation(&ctx, &sqlblob, shard)))
+            .collect();
+        stream::iter(set_initial_generation_futures)
+            .try_for_each_concurrent(max_parallelism, |fut| fut)
+            .await?;
+        info!("Completed initial generation set");
+    }
+
+    if args.initial_generation_only {
+        return Ok(());
+    }
+
+    let sqlblob = Arc::new(sqlblob);
+
+    let inline_small_values = !args.skip_inline_small_values;
+
+    // Hold mark generation constant for run
+    let mark_generation = sqlblob.get_mark_generation();
+
+    info!("Starting marking generation {}", mark_generation);
+    // Set up a task to process each key in parallel in its own task.
+    let (key_channel, processor) = {
+        let sqlblob = Arc::clone(&sqlblob);
+        let (tx, rx) = mpsc::channel(10);
+        let new_ctx = ctx.clone();
+        let task = mononoke::spawn_task(async move {
+            rx.map(Ok)
+                .try_for_each_concurrent(max_parallelism, {
+                    |key| {
+                        let sqlblob = sqlblob.clone();
+                        let new_ctx = new_ctx.clone();
+                        async move {
+                            mononoke::spawn_task(handle_one_key(
+                                new_ctx,
+                                key,
+                                sqlblob,
+                                inline_small_values,
+                                mark_generation,
+                            ))
+                            .await?
+                        }
+                    }
+                })
+                .await
+        });
+        (tx, task)
+    };
+
+    // Foreach shard in shard_range
+    for shard in shard_range {
+        info!("Starting mark on data keys from shard {}", shard);
+        let res = sqlblob
+            .get_keys_from_shard(&ctx, shard)
+            .forward(key_channel.clone().sink_err_into())
+            .await;
+        // Report processing errors ahead of key errors - that way, we don't lose the error if the channel goes away because of an error
+        if res.is_err() {
+            std::mem::drop(key_channel);
+            processor.await??;
+            return res;
+        }
+    }
+
+    // Drop the spare sender so that the processor task can exit
+    std::mem::drop(key_channel);
+
+    processor.await??;
+    info!("Completed marking generation {}", mark_generation);
+    Ok(())
+}
