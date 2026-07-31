ebece224794e021c79e679a6d0bb808a05245286
Spencer Burakowski <sburakowski@meta.com>
2026-07-31 09:58:51 -0700

    tree lookup : make getTreeOrTreeEntry an unconditional co_invoke wrapper
    Summary:
    `enableCoroutinesPhase3` is at 100%, so `getTreeOrTreeEntry` always dispatches to `co_getTreeOrTreeEntry` in production. Collapse it into a thin unconditional `co_invoke(co_getTreeOrTreeEntry)` wrapper and delete the legacy futures fallback.
    
    `getTreeOrTreeEntry` stays (PrjFS callbacks, mount init, and glob still consume its `ImmediateFuture`); only its body changes. The `TreeLookupProcessor` class was used only by that deleted fallback, so delete it (class declaration + `next()` definition). This removes the last `enableCoroutinesPhase3` read in `TreeLookupProcessor.cpp`; the now-unused `EdenConfig.h` include is dropped.
    
    Phase 3 coroutine cleanup. No behavior change.
    
    Reviewed By: muirdm
    
    Differential Revision: D112180615
    
    fbshipit-source-id: 4ebaec19692ba36afa1686253ad45afb0ca95a21

--- eden/fs/store/TreeLookupProcessor.cpp
+++ eden/fs/store/TreeLookupProcessor.cpp
@@ -6,7 +6,6 @@
  */
 
 #include "eden/fs/store/TreeLookupProcessor.h"
-#include "eden/fs/config/EdenConfig.h"
 #include "eden/fs/model/Tree.h"
 #include "eden/fs/store/ObjectStore.h"
 
@@ -15,79 +14,31 @@
 namespace facebook::eden {
 
 ImmediateFuture<std::variant<std::shared_ptr<const Tree>, TreeEntry>>
-TreeLookupProcessor::next(std::shared_ptr<const Tree> tree) {
-  using RetType = std::variant<std::shared_ptr<const Tree>, TreeEntry>;
-  if (iter_ == iterRange_.end()) {
-    return RetType{tree};
-  }
-
-  auto name = *iter_++;
-  auto it = tree->find(name);
-
-  if (it == tree->cend()) {
-    return makeImmediateFuture<RetType>(
-        std::system_error(ENOENT, std::generic_category()));
-  }
-
-  if (iter_ == iterRange_.end()) {
-    if (it->second.isTree()) {
-      return objectStore_->getTree(it->second.getObjectId(), context_)
-          .thenValue(
-              [](std::shared_ptr<const Tree> tree) -> RetType { return tree; });
-    } else {
-      return RetType{it->second};
-    }
-  } else {
-    if (!it->second.isTree()) {
-      return makeImmediateFuture<RetType>(
-          std::system_error(ENOTDIR, std::generic_category()));
-    } else {
-      return objectStore_->getTree(it->second.getObjectId(), context_)
-          .thenValue([this](std::shared_ptr<const Tree> tree) {
-            return next(std::move(tree));
-          });
-    }
-  }
-}
-
-ImmediateFuture<std::variant<std::shared_ptr<const Tree>, TreeEntry>>
 getTreeOrTreeEntry(
     std::shared_ptr<const Tree> rootTree,
     RelativePathPiece path,
     std::shared_ptr<ObjectStore> objectStore,
     ObjectFetchContextPtr context) {
-  if (objectStore->getEdenConfig()->enableCoroutinesPhase3.getValue()) {
-    return ImmediateFuture{
-        // @lint-ignore CLANGTIDY facebook-folly-coro-return-captures-local-var
-        folly::coro::co_invoke(
-            [](std::shared_ptr<const Tree> rootTree,
-               RelativePath path,
-               std::shared_ptr<ObjectStore> objectStore,
-               ObjectFetchContextPtr context)
-                -> folly::coro::Task<
-                    std::variant<std::shared_ptr<const Tree>, TreeEntry>> {
-              co_return co_await co_getTreeOrTreeEntry(
-                  std::move(rootTree),
-                  RelativePathPiece{path},
-                  std::move(objectStore),
-                  std::move(context));
-            },
-            std::move(rootTree),
-            path.copy(),
-            std::move(objectStore),
-            context.copy())
-            .semi()};
-  }
-
-  if (path.empty()) {
-    return std::variant<std::shared_ptr<const Tree>, TreeEntry>{
-        std::move(rootTree)};
-  }
-
-  auto processor = std::make_unique<TreeLookupProcessor>(
-      path, std::move(objectStore), context.copy());
-  auto future = processor->next(std::move(rootTree));
-  return std::move(future).ensure([p = std::move(processor)] {});
+  return ImmediateFuture{
+      // @lint-ignore CLANGTIDY facebook-folly-coro-return-captures-local-var
+      folly::coro::co_invoke(
+          [](std::shared_ptr<const Tree> rootTree,
+             RelativePath path,
+             std::shared_ptr<ObjectStore> objectStore,
+             ObjectFetchContextPtr context)
+              -> folly::coro::Task<
+                  std::variant<std::shared_ptr<const Tree>, TreeEntry>> {
+            co_return co_await co_getTreeOrTreeEntry(
+                std::move(rootTree),
+                RelativePathPiece{path},
+                std::move(objectStore),
+                std::move(context));
+          },
+          std::move(rootTree),
+          path.copy(),
+          std::move(objectStore),
+          context.copy())
+          .semi()};
 }
 
 folly::coro::now_task<std::variant<std::shared_ptr<const Tree>, TreeEntry>>
--- eden/fs/store/TreeLookupProcessor.h
+++ eden/fs/store/TreeLookupProcessor.h
@@ -19,29 +19,6 @@ class ObjectStore;
 class Tree;
 class TreeEntry;
 
-class TreeLookupProcessor {
- public:
-  explicit TreeLookupProcessor(
-      RelativePathPiece path,
-      std::shared_ptr<ObjectStore> objectStore,
-      ObjectFetchContextPtr context)
-      : path_{path},
-        iterRange_{path_.components()},
-        iter_{iterRange_.begin()},
-        objectStore_{std::move(objectStore)},
-        context_{std::move(context)} {}
-
-  ImmediateFuture<std::variant<std::shared_ptr<const Tree>, TreeEntry>> next(
-      std::shared_ptr<const Tree> tree);
-
- private:
-  RelativePath path_;
-  RelativePath::base_type::component_iterator_range iterRange_;
-  RelativePath::base_type::component_iterator iter_;
-  std::shared_ptr<ObjectStore> objectStore_;
-  ObjectFetchContextPtr context_;
-};
-
 /**
  * Traverse the path starting at rootTree.
  *
