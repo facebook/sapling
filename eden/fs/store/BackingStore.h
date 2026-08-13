4ad7b45428495ffb22d7014bb1231a0ca4456b6f
Spencer Burakowski <sburakowski@meta.com>
2026-08-12 19:53:08 -0700

    backing store: add temporary tree-aux future adapter
    Summary: Make `virtual folly::SemiFuture<GetTreeAuxResult> getTreeAuxData` no longer a pure virtual, make it a simpler wrapper over the coro impl `co_getTreeAuxData`. This allows us to remove the futures implementations of `getTreeAuxData` from all the backing stores in smaller diffs
    
    Reviewed By: janezhang10
    
    Differential Revision: D114921181
    
    fbshipit-source-id: abd10740cdf28e43bf377a079c7f2034ff9c702a

--- eden/fs/store/BackingStore.h
+++ eden/fs/store/BackingStore.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <folly/Range.h>
+#include <folly/coro/Invoke.h>
 #include <folly/coro/Task.h>
 #include <folly/coro/safe/NowTask.h>
 #include <folly/futures/Future.h>
@@ -272,7 +273,17 @@ class BackingStore : public RootIdCodec, public ObjectIdCodec {
    */
   virtual folly::SemiFuture<GetTreeAuxResult> getTreeAuxData(
       const ObjectId& id,
-      const ObjectFetchContextPtr& context) = 0;
+      const ObjectFetchContextPtr& context) {
+    // @lint-ignore CLANGTIDY facebook-folly-coro-return-captures-local-var
+    return folly::coro::co_invoke(
+               [this](ObjectId ownedId, ObjectFetchContextPtr ownedContext)
+                   -> folly::coro::Task<GetTreeAuxResult> {
+                 co_return co_await co_getTreeAuxData(ownedId, ownedContext);
+               },
+               ObjectId{id},
+               context.copy())
+        .semi();
+  }
 
   virtual folly::coro::now_task<GetTreeAuxResult> co_getTreeAuxData(
       const ObjectId& id,
