0b3a70db03108eaecf07048af8898384ecf85d22
Jun Wu <quark@meta.com>
2026-08-18 13:35:09 -0700

    tests: add tests about hook execution after `eden clone`
    Summary:
    `eden clone` should execute update hooks (D17244812). However, there is no test
    coverage. This diff adds tests for it.
    
    Reviewed By: eugenepan
    
    Differential Revision: D116485479
    
    fbshipit-source-id: b84342784289968dab38759f7ea63e6ee6b3d67c

--- eden/integration/hg/post_clone_test.py
+++ eden/integration/hg/post_clone_test.py
@@ -6,6 +6,7 @@
 
 # pyre-strict
 
+import configparser
 import os
 import sys
 
@@ -17,11 +18,40 @@ from .lib.hg_extension_test_base import EdenHgTestCase, hg_test
 @hg_test
 # pyre-ignore[13]: T62487924
 class SymlinkTest(EdenHgTestCase):
+    def apply_hg_config_variant(self, hgrc: configparser.ConfigParser) -> None:
+        super().apply_hg_config_variant(hgrc)
+        hgrc["experimental"]["run-python-hooks-via-pyhook"] = "true"
+        hgrc["hooks"] = {
+            "update.post-clone-test": "echo ran > update-hook-ran",
+            "update.python-post-clone-test": (
+                "python:update_hook.py:write_current_commit"
+            ),
+        }
+
     def populate_backing_repo(self, repo: hgrepo.HgRepository) -> None:
         repo.write_file("contents1", "c1\n")
         repo.write_file("contents2", "c2\n")
+        repo.write_file(
+            "update_hook.py",
+            """\
+from pathlib import Path
+
+
+def write_current_commit(io, repo, **kwargs):
+    commit = repo.working_parent_nodes()[0].hex()
+    Path(repo.path, "python-update-hook-ran").write_text(commit)
+""",
+        )
         repo.symlink("symlink", "contents1")
         repo.commit("Initial commit.")
+
+    def test_update_hook(self) -> None:
+        with open(os.path.join(self.mount, "update-hook-ran")) as f:
+            self.assertEqual("ran", f.read().strip())
+
+    def test_python_update_hook(self) -> None:
+        with open(os.path.join(self.mount, "python-update-hook-ran")) as f:
+            self.assertEqual(self.backing_repo.get_head_hash(), f.read())
 
     def test_post_clone_permissions(self) -> None:
         st = os.stat(os.path.join(self.mount, ".hg"))
